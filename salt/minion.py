ed36409dcd58302c2b16a13601773c86e4ccd880
Thomas S Hatch <thatch45@gmail.com>
2011-02-26 16:05:51 -0700

    set up some framework for the minion.py module

--- salt/minion.py
+++ salt/minion.py
@@ -7,4 +7,19 @@ import os
 import salt.crypt
 import salt.utils
 
+class Minion(object):
+    '''
+    This class instanciates a minion, runs connections for a minion, and loads
+    all of the functions into the minion
+    '''
+    def __init__(self, opts):
+        '''
+        Pass in the options dict
+        '''
+        self.opts = opts
 
+    def authenticate(self):
+        '''
+        Authenticate with the master
+        '''
+        pass
