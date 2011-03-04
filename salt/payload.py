58385190bda5c64c8ee47797c85f76825b27d605
Thomas S Hatch <thatch45@gmail.com>
2011-03-03 17:33:32 -0700

    Add payload module

---
+++ salt/payload.py
@@ -0,0 +1,7 @@
+'''
+Many aspects of the salt payload need to be managed, from the return of
+encrypted keys to general payload dynamics and packaging, these happen in here
+'''
+import os
+import cPickle as pickle
+
