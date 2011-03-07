f96b4f3516905b13267d6c918f22e76556b4b56a
Thomas S Hatch <thatch45@gmail.com>
2011-03-07 15:39:12 -0700

    Add a simple test function

--- salt/modules/cmd.py
+++ salt/modules/cmd.py
@@ -2,3 +2,9 @@
 Module for shelling out commands, inclusion of this module should be
 configurable for security reasons
 '''
+
+def echo(text):
+    '''
+    Return a string - used for testing the connection
+    '''
+    return text
