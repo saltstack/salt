1702e8e23a4eea0923e764e707a47078fb7d2704
Joseph Hall <perlhoser@gmail.com>
2011-03-15 14:39:20 -0600

    Added set and get to sysctl module

--- salt/modules/sysctl.py
+++ salt/modules/sysctl.py
@@ -21,3 +21,34 @@ def list():
         comps = line.split(' = ')
         ret[comps[0]] = comps[1]
     return ret
+
+def get( name ):
+    '''
+    Return a single sysctl parameter for this minion
+
+    CLI Example:
+    salt '*' sysctl.get net.ipv4.ip_forward
+    '''
+    cmd = 'sysctl -n %s' % name
+    ret = {}
+    out = subprocess.Popen(cmd,
+            shell=True,
+            stdout=subprocess.PIPE).communicate()[0]
+    ret[name] = out[0]
+    return out[0]
+
+def set( name, value ):
+    '''
+    Return a single sysctl parameter for this minion
+
+    CLI Example:
+    salt '*' sysctl.set net.ipv4.ip_forward 1
+    '''
+    cmd = 'sysctl -w %s=%s' % ( name, value )
+    ret = {}
+    out = subprocess.Popen(cmd,
+            shell=True,
+            stdout=subprocess.PIPE).communicate()[0].strip()
+    comps = out.split(' = ')
+    ret[comps[0]] = comps[1]
+    return ret
