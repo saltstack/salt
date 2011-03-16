b26313de204a831c763a5796cdd4ce4c77f8271c
Thomas S Hatch <thatch45@gmail.com>
2011-03-16 11:01:42 -0600

    Makes a libvirt connection to qemu

--- salt/modules/libvirt.py
+++ salt/modules/libvirt.py
@@ -20,3 +20,15 @@ VIRT_STATE_NAME_MAP = {
    5 : "shutdown",
    6 : "crashed"
 }
+
+def _get_conn():
+    '''
+    Detects what type of dom this node is and attempts to connect to the
+    correct hypervisor via libvirt.
+    '''
+    # This only supports kvm right now, it needs to be expanded to support
+    # all vm layers supported by libvirt
+    return libvirt.open("qemu:///system")
+
+
+
