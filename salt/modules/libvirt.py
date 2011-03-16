873e8b475578ca6d70d22e74fd821088680ea49a
Thomas S Hatch <thatch45@gmail.com>
2011-03-16 13:46:33 -0600

    Add vm info listsing to the libvirt module

--- salt/modules/libvirt.py
+++ salt/modules/libvirt.py
@@ -8,6 +8,7 @@ import libvirt
 import subprocess
 import StringIO
 from xml.dom import minidom
+
 # Import libvirt
 import libvirt
 
@@ -21,7 +22,7 @@ VIRT_STATE_NAME_MAP = {
    6 : "crashed"
 }
 
-def _get_conn():
+def __get_conn():
     '''
     Detects what type of dom this node is and attempts to connect to the
     correct hypervisor via libvirt.
@@ -29,6 +30,31 @@ def _get_conn():
     # This only supports kvm right now, it needs to be expanded to support
     # all vm layers supported by libvirt
     return libvirt.open("qemu:///system")
+
+def list_vms():
+    '''
+    Return a list of virtual machine names on the minion
+    '''
+    conn = __get_con()
+    vms = []
+    names = conn.listDefinedDomains()
+        for name in names:
+            vm = conn.lookupByName(name)
+            vms.append(vm)
+    return vms
+
+def _info():
+    '''
+    Return detailed information about the vms on this hyper in a dict:
+    [{'cpus': <int>,
+     'MaxMem': <int>,
+     'Mem': <int>,
+     'state': '<state>',}, ...]
+    '''
+    info = {}
+    conn = __get_conn()
+    for vm in list_vms():
+        raw = vm.info()
 
 
 
