cc89a8f32b3a4edd7f02b8f83fd32ad72d555215
Thomas S Hatch <thatch45@gmail.com>
2011-03-25 00:11:03 -0600

    Add baron.py to begin work on the tiered salt master framework

---
+++ salt/baron.py
@@ -0,0 +1,7 @@
+'''
+The baron is a minion of a higher master, the baron only allows the publisher
+to have access to the master's local api and facilitates a remote api via
+another master server
+'''
+
+import salt.minion
