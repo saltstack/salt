940d6d240cf46bc0ebca6a1c7695703bf8c6f1a4
Thomas S Hatch <thatch45@gmail.com>
2011-03-26 13:16:46 -0600

    Putting off baron support for now

--- salt/baron.py
+++
@@ -1,7 +0,0 @@
-'''
-The baron is a minion of a higher master, the baron only allows the publisher
-to have access to the master's local api and facilitates a remote api via
-another master server
-'''
-
-import salt.minion
