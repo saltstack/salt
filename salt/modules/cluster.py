245b15bf71a68b0a5a85dcc4b1428f2120daec49
Thomas S Hatch <thatch45@gmail.com>
2011-03-28 14:06:37 -0600

    Change configuration passing in cluster.py

--- salt/modules/cluster.py
+++ salt/modules/cluster.py
@@ -2,18 +2,19 @@
 The cluster module is used to distribute and activate salt HA cluster
 components
 '''
-
+# Import Python Modules
 import os
-import yaml
+# Import Salt Modules
+import salt.config
 
 def distrib(minions, master_conf, master_pem, conf_file):
     '''
     Set up this minion as a failover master
     '''
-    # Get the distributed master config opts
-    opts = yaml.load(master_conf)
     # Write the master config file
     open(conf_file, 'w+').write(master_conf)
+    # Get the distributed master config opts
+    opts = salt.config.master(conf_file)
     # Commit the minions
     minion_dir = os.path.join(opts['pki_dir'], 'minions')
     if not os.path.isdir(minion_dir):
