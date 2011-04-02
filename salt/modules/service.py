745c26ffd12f471089222632de4b68bcbdd5cc65
Thomas S Hatch <thatch45@gmail.com>
2011-04-01 20:09:24 -0600

    Add a service module

---
+++ salt/modules/service.py
@@ -0,0 +1,30 @@
+'''
+Top level package command wrapper, used to translate the os detected by facter
+to the correct package manager
+'''
+import subprocess
+
+factmap = {
+           'Archlinux': '/etc/rc.d',
+           'Fedora': '/etc/init.d',
+           'RedHat': '/etc/init.d',
+           'Debian': '/etc/init.d',
+           'Ubuntu': '/etc/init.d',
+          }
+
+def start(svc):
+    '''
+    Start the specified service
+    '''
+    cmd = os.path.join(factmap[__facter__['operatingsystem']],
+            svc + ' start')
+    return not subprocess.call(cmd, shell=True)
+
+def stop(svc):
+    '''
+    Stop the specified service
+    '''
+    cmd = os.path.join(factmap[__facter__['operatingsystem']],
+            svc + ' stop')
+    return not subprocess.call(cmd, shell=True)
+
