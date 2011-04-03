0a76bc232e6751be6b79276a2293cdfb3af29f1b
Thomas S Hatch <thatch45@gmail.com>
2011-04-03 14:48:33 -0600

    Add compute tests to test.py

--- salt/modules/test.py
+++ salt/modules/test.py
@@ -2,6 +2,8 @@
 Module for running arbitrairy tests
 '''
 
+import time
+
 def echo(text):
     '''
     Return a string - used for testing the connection
@@ -21,4 +23,39 @@ def ping():
     salt '*' test.ping
     '''
     return True
+
+def fib(num):
+    '''
+    Return a fibonachi sequence up to the passed number, and the time it took
+    to compute in seconds. Used for performance tests
+
+    CLI Example:
+    salt '*' test.fib 3
+    '''
+    start = time.time()
+    a, b = 0, 1
+    ret = [0]
+    while b < num:
+        ret.append(b)
+        a, b = b, a + b
+    return ret, time.time() - start
+
+def collatz(start):
+    '''
+    Execute the collatz conjecture from the passed starting number, returns
+    the sequence and the time it took to compute. Used for performance tests.
+
+    CLI Example:
+    salt '*' test.collatz 3
+    '''
+    start = time.time()
+    steps = []
+    while start != 1:
+        steps.append(start)
+        if start > 1:
+            if start % 2 == 0:
+                start = start / 2
+            else:
+                start = start * 3 + 1
+    return steps, time.time() - start
 
