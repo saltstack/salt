6a4c6c11cd0d2496ee33b7536b6691a34e1c81a8
Thomas S Hatch <thatch45@gmail.com>
2011-04-20 22:07:51 -0600

    Add : to the id:jobs set key

--- salt/returners/redis_return.py
+++ salt/returners/redis_return.py
@@ -21,7 +21,7 @@ def returner(ret):
             host=__opts__['redis.host'],
             port=__opts__['redis.port'],
             db=__opts__['redis.db'])
-    serv.sadd(ret['id'] + 'jobs', ret['jid'])
+    serv.sadd(ret['id'] + ':' + 'jobs', ret['jid'])
     serv.set(ret['jid'] + ':' + ret['id'], json.dumps(ret['return']))
     serv.sadd('jobs', ret['jid'])
     serv.sadd(ret['jid'], ret['id'])
