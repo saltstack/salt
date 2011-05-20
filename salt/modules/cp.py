75db5105d609a2b28f19ee675de866425e2c5c3e
Thomas S Hatch <thatch45@gmail.com>
2011-05-20 00:04:11 -0600

    Add in the minion module function to download files from the master

--- salt/modules/cp.py
+++ salt/modules/cp.py
@@ -1,11 +1,17 @@
 '''
 Minion side functions for salt-cp
 '''
+# Import python libs
 import os
+
+# Import salt libs
+import salt.simpleauth
 
 def recv(files, dest):
     '''
-    Used with salt-cp, pass the files dict, and the destination
+    Used with salt-cp, pass the files dict, and the destination.
+
+    This function recieves small fast copy files from the master via salt-cp
     '''
     ret = {}
     for path, data in files.items():
@@ -27,3 +33,38 @@ def recv(files, dest):
             ret[final] = False
 
     return ret
+
+#def get_file(path, dest):
+#    '''
+#    Used to get a single file from the salt master
+#    '''
+#    auth = salt.simpleauth.SAuth(__opts__)
+
+
+def get_files(paths):
+    '''
+    Used to gather many files from the master, the gathered files will be
+    saved in the minion cachedir reflective to the paths retrived from the
+    master.
+    '''
+    auth = salt.simpleauth.SAuth(__opts__)
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect(__opts__['master_uri'])
+    payload = {'enc': 'aes'}
+    for path in paths:
+        dest = os.path.join(__opts__['cachedir'], 'files', path)
+        dirname = os.path.dirname(dest)
+        if not os.path.isdir(dirname):
+            os.makedirs(dirname)
+        fn_ = open(dest, 'w+')
+        load = {'path': path,
+                'cmd': '_serve_file'}
+        while True:
+            load['loc'] = fn_.tell()
+            payload['load'] = self.crypticle.dumps(load)
+            socket.send_pyobj(payload)
+            data = auth.crypticle.loads(socket.recv())
+            if not data:
+                break
+            fn_.write(data)
