3066ca282b4ba65513a036288eadb71723c4c536
Thomas S Hatch <thatch45@gmail.com>
2011-05-20 10:12:26 -0600

    Move SAuth class into crypt module

--- salt/crypt.py
+++ salt/crypt.py
@@ -277,3 +277,26 @@ class Crypticle(object):
         return pickler.loads(data[len(self.PICKLE_PAD):])
 
 
+class SAuth(object):
+    '''
+    Set up an object to maintain the standalone authentication session with
+    the salt master
+    '''
+    def __init__(self, opts):
+        self.opts = opts
+        self.crypticle = self.__authenticate()
+
+    def __authenticate(self):
+        '''
+        Authenticate with the master, this method breaks the functional
+        pardigmn, it will update the master information from a fresh sign in,
+        signing in can occur as often as needed to keep up with the revolving
+        master aes key.
+        '''
+        auth = Auth(self.opts)
+        creds = auth.sign_in()
+        if creds == 'retry':
+            print 'Failed to authenticate with the master, verify that this'\
+                + ' minion\'s public key has been accepted on the salt master'
+            sys.exit(2)
+        return Crypticle(creds['aes'])
