071780a59e56236d06219783d1ad8e6cdcfd6db3
Thomas S Hatch <thatch45@gmail.com>
2011-05-25 22:28:11 -0600

    modify the cp module to use the file server client

--- salt/modules/cp.py
+++ salt/modules/cp.py
@@ -6,7 +6,7 @@ import os
 import hashlib
 
 # Import salt libs
-import salt.crypt
+import salt.minion
 
 # Import Third Party Libs
 import zmq
@@ -42,28 +42,8 @@ def get_file(path, dest):
     '''
     Used to get a single file from the salt master
     '''
-    if not path.startswith('salt://'):
-        return False
-    path = path[:7]
-    auth = salt.crypt.SAuth(__opts__)
-    context = zmq.Context()
-    socket = context.socket(zmq.REQ)
-    socket.connect(__opts__['master_uri'])
-    payload = {'enc': 'aes'}
-    if not os.path.isdir(os.path.dirname(dest)):
-        return 'Destination unavailable'
-    fn_ = open(dest, 'w+')
-    load = {'path': path,
-            'cmd': '_serve_file'}
-    while True:
-        load['loc'] = fn_.tell()
-        payload['load'] = self.crypticle.dumps(load)
-        socket.send_pyobj(payload)
-        data = auth.crypticle.loads(socket.recv_pyobj())
-        if not data:
-            break
-        fn_.write(data)
-    return dest
+    client = salt.minion.FileClient(__opts__)
+    return client.get_file(path, dest)
 
 def cache_files(paths):
     '''
@@ -71,63 +51,15 @@ def cache_files(paths):
     saved in the minion cachedir reflective to the paths retrieved from the
     master.
     '''
-    auth = salt.crypt.SAuth(__opts__)
-    context = zmq.Context()
-    socket = context.socket(zmq.REQ)
-    socket.connect(__opts__['master_uri'])
-    payload = {'enc': 'aes'}
-    ret = []
-    for path in paths:
-        if not path.startswith('salt://'):
-            return False
-    for path in paths:
-        dest = os.path.join(__opts__['cachedir'], 'files', path)
-        dirname = os.path.dirname(dest)
-        if not os.path.isdir(dirname):
-            os.makedirs(dirname)
-        fn_ = open(dest, 'w+')
-        load = {'path': path,
-                'cmd': '_serve_file'}
-        while True:
-            load['loc'] = fn_.tell()
-            payload['load'] = self.crypticle.dumps(load)
-            socket.send_pyobj(payload)
-            data = auth.crypticle.dumps(socket.recv_pyobj())
-            if not data:
-                break
-            fn_.write(data)
-        ret.append(path)
-    return ret
+    client = salt.minion.FileClient(__opts__)
+    return client.cache_files(paths)
 
 def cache_file(path):
     '''
     Used to cache a single file in the local salt-master file cache.
     '''
-    if path.startswith('salt://'):
-        path = path[7:]
-        auth = salt.crypt.SAuth(__opts__)
-        context = zmq.Context()
-        socket = context.socket(zmq.REQ)
-        socket.connect(__opts__['master_uri'])
-        payload = {'enc': 'aes'}
-        dest = os.path.join(__opts__['cachedir'], 'files', path)
-        dirname = os.path.dirname(dest)
-        if not os.path.isdir(dirname):
-            os.makedirs(dirname)
-        fn_ = open(dest, 'w+')
-        load = {'path': path,
-                'cmd': '_serve_file'}
-        while True:
-            load['loc'] = fn_.tell()
-            payload['load'] = auth.crypticle.dumps(load)
-            socket.send_pyobj(payload)
-            data = auth.crypticle.loads(socket.recv_pyobj())
-            if not data:
-                break
-            fn_.write(data)
-        return dest
-    else:
-        return False
+    client = salt.minion.FileClient(__opts__)
+    return client.cache_file(path)
 
 def hash_file(path):
     '''
@@ -137,20 +69,5 @@ def hash_file(path):
 
     CLI Example:
     '''
-    if path.startswith('salt://'):
-        path = path[7:]
-        auth = salt.crypt.SAuth(__opts__)
-        context = zmq.Context()
-        socket = context.socket(zmq.REQ)
-        socket.connect(__opts__['master_uri'])
-        payload = {'enc': 'aes'}
-        load = {'path': path,
-                'cmd': '_file_hash'}
-        payload['load'] = auth.crypticle.dumps(load)
-        socket.send_pyobj(payload)
-        return auth.crypticle.loads(socket.recv_pyobj())
-    elif os.path.isfile(path):
-        ret = {'hash_type': 'md5'}
-        ret['hsum'] = hashlib.md5(open(path, 'rb').read()).hexdigest()
-        return ret
-    return False
+    client = salt.minion.FileClient(__opts__)
+    return client.hash_file(path)
