094c07cf5f18565d84d58b2cf836a2bf011a753f
Thomas S Hatch <thatch45@gmail.com>
2011-06-19 23:51:16 -0600

    Add set_fstab option to mount

--- salt/modules/mount.py
+++ salt/modules/mount.py
@@ -50,6 +50,85 @@ def fstab(config='/etc/fstab'):
                          'pass': comps[5]}
     return ret
 
+def set_fstab(
+        name,
+        device,
+        fstype,
+        opts='defaults',
+        dump=0,
+        pass_num=0,
+        config='/etc/fstab',
+        ):
+    '''
+    Verify that this mount is represented in the fstab, chage the mount point
+    to match the data passed, or add the mount if it is not present.
+
+    CLI Example:
+    salt '*' mount.set_fstab /mnt/foo /dev/sdz1 ext4
+    '''
+    # Fix the opts type if it is a list
+    if type(opts) == type(list()):
+        opts = ','.join(opts)
+    lines = []
+    change = False
+    present = False
+    if not os.path.isfile(config):
+        return False
+    for line in open(config).readlines():
+        if line.startswith('#'):
+            # Commented
+            lines.append(line)
+            continue
+        if not line.strip():
+            # Blank line
+            lines.append(line)
+            continue
+        comps = line.split()
+        if not len(comps) == 6:
+            # Invalid entry
+            lines.append(line)
+            continue
+        if comps[1] == name:
+            # check to see if there are changes and fix them if there are
+            present = True
+            if comps[0] != device:
+                change = True
+                comps[0] = device
+            if comps[2] != fstype:
+                change = True
+                comps[2] = fstype
+            if comps[3] != opts:
+                change = True
+                comps[3] = opts
+            if comps[4] != str(dump):
+                change = True
+                comps[4] = str(dump)
+            if comps[5] != str(pass_num):
+                change = True
+                comps[5] = str(pass_num)
+            if change:
+                log.debug('fstab entry for mount point {0} is being updated'.format(name))
+                newline = '{0}\t\t{1}\t{2}\t{3}\t{4} {5}'.format(
+                        name,
+                        device,
+                        fstype,
+                        opts,
+                        dump,
+                        pass_num)
+                lines.append(newline)
+    if not change and not present:
+        # The entry is new, add it to the end of the fstab
+        newline = '{0}\t\t{1}\t{2}\t{3}\t{4} {5}'.format(
+                name,
+                device,
+                fstype,
+                opts,
+                dump,
+                pass_num)
+        lines.append(newline)
+        open(config, 'w+').writelines(lines)
+    return True
+
 def mount(name, device, mkmnt=False, fstype='', opts='defaults'):
     '''
     Mound a device
