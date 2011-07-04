0b99bf43e02c22f0aa136ca02717521b1f4f2414
Thomas S Hatch <thatch45@gmail.com>
2011-07-03 22:48:57 -0600

    Add doc printing for salt-run

--- salt/runner.py
+++ salt/runner.py
@@ -29,9 +29,22 @@ class Runner(object):
             sys.stderr.write('%s\n' % err)
             sys.exit(1)
 
+    def _print_docs(self):
+        '''
+        Print out the documentation!
+        '''
+        for fun in sorted(self.functions):
+            if fun.startswith(self.opts['fun']):
+                print fun + ':'
+                print self.functions[fun].__doc__
+                print ''
+
     def run(self):
         '''
         Execuete the runner sequence
         '''
-        self._verify_fun()
-        self.functions[self.opts['fun']](*self.opts['arg'])
+        if self.opts['doc']:
+            self._print_docs()
+        else:
+            self._verify_fun()
+            self.functions[self.opts['fun']](*self.opts['arg'])
