a6f7a5851e17447ab2448e46fb8db50855d66dc6
Jeff Schroeder <jeffschroeder@computer.org>
2011-07-07 22:04:53 -0700

    Fix the coding style of salt.output and a few nits
        - Yaml is a hard dependency so whack the check
        - Fall back to RawOutputter when TxtOutputter chokes
        - Fall back to json properly when json.dumps(data) fails
        - Make the docstrings look like the rest of the salt codebase

--- salt/output.py
+++ salt/output.py
@@ -1,26 +1,22 @@
-"""
+'''
 A simple way of setting the output format for data from modules
-"""
+'''
+import yaml
 import pprint
 
-# Conditionally import the json and yaml modules
+# Conditionally import the json module
 try:
     import json
     JSON = True
 except ImportError:
     JSON = False
-try:
-    import yaml
-    YAML = True
-except ImportError:
-    YAML = False
 
 __all__ = ('get_outputter',)
 
 class Outputter(object):
-    """
+    '''
     Class for outputting data to the screen.
-    """
+    '''
     supports = None
 
     @classmethod
@@ -31,15 +27,22 @@ class Outputter(object):
        return klass.supports == name
 
     def __call__(self, data, **kwargs):
-        print "Calling Outputter.__call__()"
         pprint.pprint(data)
 
+class RawOutputter(Outputter):
+    '''
+    Raw output. This calls repr() on the returned data.
+    '''
+    supports = "raw"
+    def __call__(self, data, **kwargs):
+        print data
+
 class TxtOutputter(Outputter):
-    """
+    '''
     Plain text output. Primarily for returning output from
     shell commands in the exact same way they would output
     on the shell when ran directly.
-    """
+    '''
     supports = "txt"
     def __call__(self, data, **kwargs):
         if hasattr(data, "keys"):
@@ -48,43 +51,42 @@ class TxtOutputter(Outputter):
                 for line in value.split('\n'):
                     print "{0}: {1}".format(key, line)
         else:
-            # For non-dictionary data, run pprint
-            super(TxtOutputter, self).__call__(data)
+            # For non-dictionary data, just use print
+            RawOutputter()(data)
 
 class JSONOutputter(Outputter):
-    """JSON output. Chokes on non-serializable objects."""
+    '''
+    JSON output.
+    '''
     supports = "json"
     enabled  = JSON
 
     def __call__(self, data, **kwargs):
         try:
             # A good kwarg might be: indent=4
-            print json.dumps(data, **kwargs)
+            ret = json.dumps(data, **kwargs)
         except TypeError:
-            super(JSONOutputter, self).__call__(data)
+            # Return valid json for unserializable objects
+            ret = json.dumps({})
+        print ret
 
 class YamlOutputter(Outputter):
-    """Yaml output. All of the cool kids are doing it."""
+    '''
+    Yaml output. All of the cool kids are doing it.
+    '''
     supports = "yaml"
-    enabled  = YAML
 
     def __call__(self, data,  **kwargs):
         print yaml.dump(data, **kwargs)
 
-class RawOutputter(Outputter):
-    """Raw output. This calls repr() on the returned data."""
-    supports = "raw"
-    def __call__(self, data, **kwargs):
-        print data
-
 def get_outputter(name=None):
-    """
+    '''
     Factory function for returning the right output class.
 
     Usage:
         printout = get_outputter("txt")
         printout(ret)
-    """
+    '''
     # Return an actual instance of the correct output class
     for i in Outputter.__subclasses__():
         if i.check(name):
