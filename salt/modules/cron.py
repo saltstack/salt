231b4ba55a0285ffd60daa58bdc98323a46b9351
Thomas S Hatch <thatch45@gmail.com>
2011-07-20 23:24:15 -0600

    Add saving cron data

--- salt/modules/cron.py
+++ salt/modules/cron.py
@@ -1,6 +1,31 @@
 '''
 Work with cron
 '''
+import tempfile
+
+def __append_special(user, special, cmd):
+    '''
+    Append the value to the crontab
+    '''
+    tmp = tempfile.mkstemp()
+    tmpd = open(tmp, 'w+')
+    tmpd.write(raw_cron(user))
+    tmpd.write(_render_special(special, cmd))
+    cmd = 'crontab {0} -u {1}'.format(tmp, user)
+    return __salt__['cmd.run'](cmd)
+
+def _render_special(special, cmd):
+    '''
+    Take a special string and a command string and render it
+    '''
+    return '{0} {1}'.format(special, cmd)
+
+def raw_cron(user):
+    '''
+    Return the contents of the user's crontab
+    '''
+    cmd = 'crontab -l -u {0}'.format(user)
+    return __salt__['cmd.run_stdout'](cmd)
 
 def list_tab(user):
     '''
@@ -9,10 +34,9 @@ def list_tab(user):
     CLI Example:
     salt '*' cron.list_tab root
     '''
-    cmd = 'crontab -l -u {0}'.format(user)
+    data = raw_cron(user)
     ret = {'crons': [],
            'special': []}
-    data = __salt__['cmd.run_stdout'](cmd)
     for line in data.split('\n'):
         if line.startswith('@'):
             # Its a "special" line
@@ -36,3 +60,18 @@ def list_tab(user):
             dat['cmd'] = ' '.join(comps[5:])
             ret['crons'].append(dat)
     return ret
+
+def set_special(user, special, cmd):
+    '''
+    Set up a special command in the crontab.
+
+    CLI Example:
+    salt '*' cron.set_special @hourly 'echo foobar'
+    '''
+    tab = list_tab(user)
+    # If the special is set, return True
+    for dat in tab['special']:
+        if dat['spec'] == special and dat['cmd'] == cmd:
+            return True
+    return __append_special(user, special, cmd)
+
