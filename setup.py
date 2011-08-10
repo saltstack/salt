86213377df8f333ec011ee095eb483b82a0446c0
Erik Nolte <enolte@beyondoblivion.com>
2011-08-10 11:20:33 -0600

    Move salt-monitor scripts and packaging to salt-monitor project

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ The setup script for salt
 '''
 import os
 import sys
-import unittest
 from distutils import log
 from distutils.cmd import Command
 from distutils.core import setup
@@ -25,51 +24,6 @@ if os.environ.has_key('SYSCONFDIR'):
 else:
     etc_path = os.path.join(os.path.dirname(PREFIX), 'etc')
 
-class UnitTest(Command):
-    description = "run unit tests"
-    user_options = []
-
-    def initialize_options(self):
-        self.test_dirs = None
-
-    def finalize_options(self):
-        if self.test_dirs is None:
-            self.test_dirs = ["test"]
-
-    def run(self):
-        errors = 0
-        failures = 0
-        for path in self.test_dirs:
-            for root, dirs, files in os.walk(path):
-                for filename in files:
-                    if filename.startswith("test_") and \
-                            filename.endswith(".py"):
-                        results = self._run_test(os.path.join(root, filename))
-                        errors += results[0]
-                        failures += results[1]
-        self.announce(
-            "unit test: {} errors and {} failures".format(errors, failures),
-            level=log.INFO)
-
-    def _run_test(self, path):
-        self.announce("run tests in " + path, log.INFO)
-        dirname, basename = os.path.split(path)
-        sys.path.insert(0, dirname)
-        try:
-            modname = os.path.splitext(basename)[0]
-            mod = __import__(modname)
-            if hasattr(mod, "test_suite"):
-                suite = mod.test_suite()
-                runner = unittest.TextTestRunner(verbosity=2)
-                results = runner.run(suite)
-                return len(results.errors), len(results.failures)
-            else:
-                return (0, 0)
-        finally:
-            if sys.path[0] == dirname:
-                del sys.path[0]
-
-
 setup(
       name=NAME,
       version=VER,
@@ -78,7 +32,6 @@ setup(
           Extension('salt.modules.cytest', ['salt/modules/cytest.pyx']),
           ],
       cmdclass={
-          'test':UnitTest,
           'build_ext': build_ext,
           },
       description=DESC,
@@ -96,23 +49,22 @@ setup(
           'Intended Audience :: System Administrators',
           'License :: OSI Approved :: Apache Software License',
           'Operating System :: POSIX :: Linux',
-          'Topic :: System :: Monitoring',
           'Topic :: System :: Clustering',
           'Topic :: System :: Distributed Computing',
           ],
       packages=['salt',
-                'salt.modules',
                 'salt.cli',
-                'salt.returners',
-                'salt.renderers',
-                'salt.runners',
+                'salt.ext',
                 'salt.grains',
+                'salt.modules',
+                'salt.renderers',
+                'salt.returners',
+                'salt.runners',
                 'salt.states',
                 'salt.utils',
                 ],
       scripts=['scripts/salt-master',
                'scripts/salt-minion',
-               'scripts/salt-monitor',
                'scripts/salt-syndic',
                'scripts/salt-key',
                'scripts/salt-cp',
@@ -122,7 +74,6 @@ setup(
       data_files=[(os.path.join(etc_path, 'salt'),
                     ['conf/master',
                      'conf/minion',
-                     'conf/monitor',
                     ]),
                 ('share/man/man1',
                     ['doc/man/salt-master.1',
@@ -131,7 +82,6 @@ setup(
                      'doc/man/salt-cp.1',
                      'doc/man/salt-run.1',
                      'doc/man/salt-minion.1',
-                     'doc/man/salt-monitor.1',
                     ]),
                 ('share/man/man7',
                     ['doc/man/salt.7',
