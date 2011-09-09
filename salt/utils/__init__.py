01c2579164e1b396670aa4391c1837711aa11a2d
Thomas S Hatch <thatch45@gmail.com>
2011-09-09 00:09:08 -0600

    missing commas

--- salt/utils/__init__.py
+++ salt/utils/__init__.py
@@ -34,24 +34,24 @@ def get_colors(use=True):
     '''
     colors = {
             'BLACK': '\033[0;30m',
-            'DARK_GRAY': '\033[1;30m'
-            'LIGHT_GRAY': '\033[0;37m'
-            'BLUE': '\033[0;34m'
-            'LIGHT_BLUE': '\033[1;34m'
-            'GREEN': '\033[0;32m'
-            'LIGHT_GREEN': '\033[1;32m'
-            'CYAN': '\033[0;36m'
-            'LIGHT_CYAN': '\033[1;36m'
-            'RED': '\033[0;31m'
-            'LIGHT_RED': '\033[1;31m'
-            'PURPLE': '\033[0;35m'
-            'LIGHT_PURPLE': '\033[1;35m'
-            'BROWN': '\033[0;33m'
-            'YELLOW': '\033[1;33m'
-            'WHITE': '\033[1;37m'
-            'DEFAULT_COLOR': '\033[00m'
-            'RED_BOLD': '\033[01;31m'
-            'ENDC': '\033[0m'
+            'DARK_GRAY': '\033[1;30m',
+            'LIGHT_GRAY': '\033[0;37m',
+            'BLUE': '\033[0;34m',
+            'LIGHT_BLUE': '\033[1;34m',
+            'GREEN': '\033[0;32m',
+            'LIGHT_GREEN': '\033[1;32m',
+            'CYAN': '\033[0;36m',
+            'LIGHT_CYAN': '\033[1;36m',
+            'RED': '\033[0;31m',
+            'LIGHT_RED': '\033[1;31m',
+            'PURPLE': '\033[0;35m',
+            'LIGHT_PURPLE': '\033[1;35m',
+            'BROWN': '\033[0;33m',
+            'YELLOW': '\033[1;33m',
+            'WHITE': '\033[1;37m',
+            'DEFAULT_COLOR': '\033[00m',
+            'RED_BOLD': '\033[01;31m',
+            'ENDC': '\033[0m',
             }
     if not use:
         for color in colors:
