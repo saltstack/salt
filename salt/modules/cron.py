0d8d0258143dcaf61d39cac54e6653e4d8a20fe7
Thomas S Hatch <thatch45@gmail.com>
2011-10-08 17:39:01 -0600

    Update the cron module to, well, work

--- salt/modules/cron.py
+++ salt/modules/cron.py
@@ -3,22 +3,47 @@ Work with cron
 '''
 import tempfile
 
-def __append_special(user, special, cmd):
-    '''
-    Append the value to the crontab
-    '''
-    tmp = tempfile.mkstemp()
-    tmpd = open(tmp, 'w+')
-    tmpd.write(raw_cron(user))
-    tmpd.write(_render_special(special, cmd))
-    cmd = 'crontab {0} -u {1}'.format(tmp, user)
-    return __salt__['cmd.run'](cmd)
+TAG = '# Lines below here are managed by Salt, do not edit\n'
 
-def _render_special(special, cmd):
+def _render_tab(lst):
     '''
-    Take a special string and a command string and render it
+    Takes a tab list structure and renders it to a list for applying it to
+    a file
     '''
-    return '{0} {1}'.format(special, cmd)
+    ret = []
+    for pre in lst['pre']:
+        ret.append(pre)
+    if len(ret):
+        if not ret[-1] == TAG:
+            ret.append(TAG)
+    for cron in lst['crons']:
+        ret.append(
+            '{0} {1} {2} {3} {4} {5}\n'.format(
+                cron['min'],
+                cron['hour'],
+                cron['daymonth'],
+                cron['month'],
+                cron['dayweek'],
+                cron['cmd']
+                )
+            )
+    for spec in lst['special']:
+        ret.append(
+            '{0} {1}\n'.format(
+                spec['spec'],
+                spec['cmd']
+                )
+            )
+    return ret
+
+def _write_cron(user, lines):
+    '''
+    Takes a list of lines to be commited to a user's crontab and writes it
+    '''
+    tmpd, path = tempfile.mkstemp()
+    open(path, 'w+').writelines(lines)
+    cmd = 'crontab -u {0} {1}'.format(user, path)
+    return __salt__['cmd.run_all'](cmd)
 
 def raw_cron(user):
     '''
@@ -35,30 +60,38 @@ def list_tab(user):
     salt '*' cron.list_tab root
     '''
     data = raw_cron(user)
-    ret = {'crons': [],
+    ret = {'pre': [],
+           'crons': [],
            'special': []}
+    flag = False
     for line in data.split('\n'):
-        if line.startswith('@'):
-            # Its a "special" line
-            dat = {}
-            comps = line.split()
-            if len(comps) < 2:
-                # Invalid line
-                continue
-            dat['spec'] = comps[0]
-            dat['cmd'] = ' '.join(comps[1:])
-            ret['special'].append(dat)
-        if len(line.split()) > 5:
-            # Appears to be a standard cron line
-            comps = line.split()
-            dat = {}
-            dat['min'] = comps[0]
-            dat['hour'] = comps[1]
-            dat['daymonth'] = comps[2]
-            dat['month'] = comps[3]
-            dat['dayweek'] = comps[4]
-            dat['cmd'] = ' '.join(comps[5:])
-            ret['crons'].append(dat)
+        if line == '# Lines below here are managed by Salt, do not edit\n':
+            flag = True
+            continue
+        if flag:
+            if line.startswith('@'):
+                # Its a "special" line
+                dat = {}
+                comps = line.split()
+                if len(comps) < 2:
+                    # Invalid line
+                    continue
+                dat['spec'] = comps[0]
+                dat['cmd'] = ' '.join(comps[1:])
+                ret['special'].append(dat)
+            if len(line.split()) > 5:
+                # Appears to be a standard cron line
+                comps = line.split()
+                dat = {}
+                dat['min'] = comps[0]
+                dat['hour'] = comps[1]
+                dat['daymonth'] = comps[2]
+                dat['month'] = comps[3]
+                dat['dayweek'] = comps[4]
+                dat['cmd'] = ' '.join(comps[5:])
+                ret['crons'].append(dat)
+        else:
+            ret['pre'].append(line)
     return ret
 
 def set_special(user, special, cmd):
@@ -68,10 +101,42 @@ def set_special(user, special, cmd):
     CLI Example:
     salt '*' cron.set_special @hourly 'echo foobar'
     '''
-    tab = list_tab(user)
-    # If the special is set, return True
-    for dat in tab['special']:
-        if dat['spec'] == special and dat['cmd'] == cmd:
-            return True
-    return __append_special(user, special, cmd)
+    lst = list_tab(user)
+    for spec in lst['special']:
+        if special == cron['special'] and \
+            cmd == cron['cmd']:
+            return 'present'
+    spec = {'special': special,
+            'cmd': cmd}
+    lst['special'].append(spec)
+    comdat = _write_cron(user, _render_tab(lst))
+    if not comdat['retcode']:
+        # Failed to commit, return the error
+        return comdat['stderr']
+    return 'new'
 
+def set_job(user, minute, hour, dom, month, dow, cmd):
+    '''
+    Sets a cron job up for a specified user.
+    '''
+    lst = list_tab(user)
+    for cron in lst['crons']:
+        if minute == cron['min'] and \
+            hour == cron['hour'] and \
+            dom == cron['daymonth'] and \
+            month == cron['month'] and \
+            dow == cron['dayweek'] and \
+            cmd == cron['cmd']:
+            return 'present'
+    cron = {'min': minute,
+            'hour': hour,
+            'daymonth': dom,
+            'month': month,
+            'dayweek': dow,
+            'cmd': cmd}
+    lst['crons'].append(cron)
+    comdat = _write_cron(user, _render_tab(lst))
+    if not comdat['retcode']:
+        # Failed to commit, return the error
+        return comdat['stderr']
+    return 'new'
