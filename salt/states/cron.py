afb172199781b3e49df202b34911a12f39f17db7
Thomas S Hatch <thatch45@gmail.com>
2011-10-08 22:12:13 -0600

    Add absent to cron state

--- salt/states/cron.py
+++ salt/states/cron.py
@@ -41,3 +41,44 @@ def present(name,
     ret['result'] = False
     return ret
 
+def absent(name,
+        user='root',
+        minute='*',
+        hour='*',
+        daymonth='*',
+        month='*',
+        dayweek='*',
+        ):
+    '''
+    Verifies that the specified cron job is absent for the specified user
+    '''
+    ret = {'name': name,
+           'result': True,
+           'changes': {},
+           'comment': ''}
+    data = __salt__['cron.rm_job'](
+            user,
+            minute,
+            hour,
+            daymonth,
+            month,
+            dayweek,
+            name,
+            )
+    if data == 'absent':
+        ret['comment'] = 'Cron {0} already absent'.format(name)
+        return ret
+    if data == 'removed':
+        ret['comment'] = 'Cron {0} removed from {1}\'s crontab'.format(
+                name,
+                user
+                )
+        ret['changes'] = {user: name}
+        return ret
+    ret['comment'] = 'Cron {0} for user {1} failed to commit with error \n{2}'.format(
+            name,
+            user,
+            data
+            )
+    ret['result'] = False
+    return ret
