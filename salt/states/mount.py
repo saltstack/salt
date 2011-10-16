5ae15a165871ab7a0c9f57b3eb360c78145c64d3
Thomas S Hatch <thatch45@gmail.com>
2011-10-16 00:38:08 -0600

    Update docstrings for the mount state

--- salt/states/mount.py
+++ salt/states/mount.py
@@ -1,5 +1,17 @@
 '''
-State enforcement for mount points
+Mount Managment
+===============
+Mount any type of mountable filesystem with the mounted function:
+
+.. code-block:: yaml
+    /mnt/sdb:
+      mount:
+        - mounted
+        - device: /dev/sdb1
+        - fstype: ext4
+        - mkmnt: True
+        - opts:
+          - defaults
 '''
 
 def mounted(
@@ -14,6 +26,52 @@ def mounted(
         remount=True,
         persist=True,
         ):
+    '''
+    Verify that a device is mounted
+
+    name
+    ~~~~
+    The path to the location where the device is to be mounted
+
+    device
+    ~~~~~~
+    The device name, typically the device node, such as /dev/sdb1
+
+    fstype
+    ~~~~~~
+    The filesystem type, this will be xfs, ext2/3/4 in the case of classic
+    filesystems, and fuse in the case of fuse mounts
+
+    mkmnt
+    ~~~~~
+    If the mount point is not present then the state will fail, set mkmnt to
+    True to create the mount point if it is otherwise not present
+
+    opts
+    ~~~~
+    A list object of options or a comma delimited list
+
+    dump
+    ~~~~
+    The dump value to be passed into the fstab, default to 0
+
+    pass_num
+    ~~~~~~~~
+    The pass value to be passed into the fstab, default to 0
+
+    config
+    ~~~~~~
+    Set an alternative location for the fstab, default to /etc/fstab
+
+    remount
+    ~~~~~~~
+    Set if the file system can be remounted with the remount option, default
+    to True
+
+    persist
+    ~~~~~~~
+    Set if the mount should be saved in the fstab, default to True
+    '''
     ret = {'name': name,
            'changes': {},
            'result': True,
