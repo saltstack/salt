d35bb5e98d45f287559c493a72059480637deb55
Thomas S Hatch <thatch45@gmail.com>
2011-10-27 17:39:58 -0600

    update renderers to have the sls and env names

--- salt/renderers/py.py
+++ salt/renderers/py.py
@@ -7,7 +7,7 @@ which returns high state data
 import imp
 import os
 
-def render(template):
+def render(template, env='', sls=''):
     '''
     Render the python module's components
     '''
@@ -20,6 +20,8 @@ def render(template):
                 )
         mod.salt = __salt__
         mod.grains = __grains__
+        mod.env = env
+        mod.sls = sls
         return mod.sls()
     except:
         return {}
