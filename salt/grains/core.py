36ec4ae19d38810e460179ef73502efd89fd826d
Doug Renn <renn@nestegg.com>
2011-11-22 17:11:40 -0700

    Looking for /etc/gentoo-version is not necessary

--- salt/grains/core.py
+++ salt/grains/core.py
@@ -171,9 +171,6 @@ def os_data():
             grains['os'] = 'Arch'
         elif os.path.isfile('/etc/debian_version'):
             grains['os'] = 'Debian'
-        elif os.path.isfile('/etc/gentoo-version'):
-            grains['os'] = 'Gentoo'
-        # All of my gentoo machines have gentoo-release, don't know if gentoo-version is necessary
         elif os.path.isfile('/etc/gentoo-release'):
             grains['os'] = 'Gentoo'
         elif os.path.isfile('/etc/fedora-version'):
