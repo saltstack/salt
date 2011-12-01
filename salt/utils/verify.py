59f284dd522b13bf2a4726fda0022eb26580f715
Jeff Schroeder <jeffschroeder@computer.org>
2011-11-30 20:20:25 -0800

    Adding initial salt.utils.verify module to check zmq version

---
+++ salt/utils/verify.py
@@ -0,0 +1,25 @@
+'''
+A few checks to make sure the environment is sane
+'''
+# Original Author: Jeff Schroeder <jeffschroeder@computer.org>
+
+import logging
+log = logging.getLogger(__name__)
+
+__all__ = ('zmq_version', 'run')
+
+def zmq_version():
+    '''ZeroMQ python bindings >= 2.1.9 are required'''
+    import zmq
+    ver = zmq.__version__
+    ver_int = int(ver.replace('.', ''))
+    if not ver_int >= 219:
+        log.critical("ZeroMQ python bindings >= 2.1.9 are required")
+        return False
+    return True
+
+def run():
+    for func in __all__:
+        if func == "run": continue
+        if not locals().get(func)():
+            sys.exit(1)
