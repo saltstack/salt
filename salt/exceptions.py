e56de8aacda1038158ed63d755ef52b6030934d1
Seth House <seth@eseth.com>
2011-12-20 00:13:15 -0700

    Moved all Salt exceptions to inherit from a common exception class

--- salt/exceptions.py
+++ salt/exceptions.py
@@ -2,39 +2,43 @@
 This module is a central location for all salt exceptions
 '''
 
-class SaltClientError(Exception):
+class SaltException(Exception):
+    '''
+    Base exception class; all Salt-specific exceptions should subclass this
+    '''
+    pass
+
+class SaltClientError(SaltException):
     '''
     Problem reading the master root key
     '''
     pass
 
-
-class AuthenticationError(Exception):
+class AuthenticationError(SaltException):
     '''
     If sha256 signature fails during decryption
     '''
     pass
 
-
-class CommandNotFoundError(Exception):
+class CommandNotFoundError(SaltException):
     '''
     Used in modules or grains when a required binary is not available
     '''
     pass
 
-class LoaderError(Exception):
+class LoaderError(SaltException):
     '''
     Problems loading the right renderer
     '''
     pass
 
-class MinionError(Exception):
+class MinionError(SaltException):
     '''
     Minion problems reading uris such as salt:// or http://
     '''
     pass
 
-class SaltInvocationError(Exception):
+class SaltInvocationError(SaltException):
     '''
     Used when the wrong number of arguments are sent to modules
     or invalid arguments are  specified  on  the  command  line
