9707047d238714b64753b23480545c1e2463079a
Martin Schnabel <mb0@mb0.org>
2011-12-23 06:25:11 +0100

    add template loader that uses the salt env files cache of the template file
    to actually include a file it must be cached. example:
    
    {% set docachefile = salt['cp.cache_file']('salt://myapp/parameterized_reqs.sls') %}
    {% set myparam = '/virtualenv/dir/for/example' %}
    {% include 'myapp/parameterized_reqs.sls' with context %}
    
    the parameter myparam can be then be used in parameterized_reqs.sls

--- salt/renderers/json_jinja.py
+++ salt/renderers/json_jinja.py
@@ -9,14 +9,15 @@ import json
 import os
 
 # Import Third Party libs
-from jinja2 import Template
+from jinja2 import Template, FileSystemLoader
+from jinja2.environment import Environment
 
 
-def render(template, env='', sls=''):
+def render(template_file, env='', sls=''):
     '''
     Render the data passing the functions and grains into the rendering system
     '''
-    if not os.path.isfile(template):
+    if not os.path.isfile(template_file):
         return {}
 
     passthrough = {}
@@ -25,7 +26,15 @@ def render(template, env='', sls=''):
     passthrough['env'] = env
     passthrough['sls'] = sls
 
-    template = Template(open(template, 'r').read())
+    file_cache = '/files/%s/' % env
+    if file_cache in template_file:
+        cache_dir, file_rel = template_file.split(file_cache, 1)
+        loader = FileSystemLoader(cache_dir + file_cache)
+        jinja_env = Environment(loader=loader)
+        template = jinja_env.get_template(file_rel)
+    else:
+        template = Template(open(template_file, 'r').read())
+
     json_data = template.render(**passthrough)
 
     return json.loads(json_data)
