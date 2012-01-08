476c22e763cd536eb411dedf6f23be5c03d0a4e4
David Boucha <boucha@gmail.com>
2012-01-08 01:51:45 -0700

    Added support for Window Service for the salt-minion
    "salt-minion -d" will install the salt-minion as a Windows
    service or restart the service if it already exists

--- salt/utils/__init__.py
+++ salt/utils/__init__.py
@@ -69,34 +69,49 @@ def daemonize():
     '''
     Daemonize a process
     '''
-    try:
-        pid = os.fork()
-        if pid > 0:
-            # exit first parent
-            sys.exit(0)
-    except OSError, e:
-        print >> sys.stderr, "fork #1 failed: %d (%s)" % (e.errno, e.strerror)
-        sys.exit(1)
-
-    # decouple from parent environment
-    os.chdir("/")
-    os.setsid()
-    os.umask(022)
-
-    # do second fork
-    try:
-        pid = os.fork()
-        if pid > 0:
-            # print "Daemon PID %d" % pid
-            sys.exit(0)
-    except OSError, e:
-        print >> sys.stderr, "fork #2 failed: %d (%s)" % (e.errno, e.strerror)
-        sys.exit(1)
-
-    dev_null = open('/dev/null', 'rw')
-    os.dup2(dev_null.fileno(), sys.stdin.fileno())
-    os.dup2(dev_null.fileno(), sys.stdout.fileno())
-    os.dup2(dev_null.fileno(), sys.stderr.fileno())
+    if os.environ['os'].startswith('Windows'):
+        import ctypes
+        if ctypes.windll.shell32.IsUserAnAdmin() == 0:
+            import win32api
+            executablepath = sys.executable
+            pypath = executablepath.split('\\')
+            win32api.ShellExecute(
+                0,
+                'runas',
+                executablepath, 
+                '\\'.join([pypath[0], pypath[1], 'Lib\\site-packages\\salt\\utils\\salt-minion-service.py']),
+                '\\'.join([pypath[0], pypath[1]]),
+                0 )
+        sys.exit(0)
+    else:
+        try:
+            pid = os.fork()
+            if pid > 0:
+                # exit first parent
+                sys.exit(0)
+        except OSError, e:
+            print >> sys.stderr, "fork #1 failed: %d (%s)" % (e.errno, e.strerror)
+            sys.exit(1)
+    
+        # decouple from parent environment
+        os.chdir("/")
+        os.setsid()
+        os.umask(022)
+    
+        # do second fork
+        try:
+            pid = os.fork()
+            if pid > 0:
+                # print "Daemon PID %d" % pid
+                sys.exit(0)
+        except OSError, e:
+            print >> sys.stderr, "fork #2 failed: %d (%s)" % (e.errno, e.strerror)
+            sys.exit(1)
+    
+        dev_null = open('/dev/null', 'rw')
+        os.dup2(dev_null.fileno(), sys.stdin.fileno())
+        os.dup2(dev_null.fileno(), sys.stdout.fileno())
+        os.dup2(dev_null.fileno(), sys.stderr.fileno())
 
 
 def profile_func(filename=None):
@@ -147,3 +162,4 @@ def list_files(directory):
             ret.add(os.path.join(root, name))
 
     return list(ret)
+
---
+++ salt/utils/winservice.py
@@ -0,0 +1,82 @@
+# winservice.py
+
+from os.path import splitext, abspath
+from sys import modules
+
+import win32serviceutil
+import win32service
+import win32event
+import win32api
+
+class Service(win32serviceutil.ServiceFramework):
+	_svc_name_ = '_unNamed'
+	_svc_display_name_ = '_Service Template'
+	def __init__(self, *args):
+		win32serviceutil.ServiceFramework.__init__(self, *args)
+		self.log('init')
+		self.stop_event = win32event.CreateEvent(None, 0, 0, None)
+	def log(self, msg):
+		import servicemanager
+		servicemanager.LogInfoMsg(str(msg))
+        def sleep(self, sec):
+                win32api.Sleep(sec*1000, True)
+	def SvcDoRun(self):
+		self.ReportServiceStatus(win32service.SERVICE_START_PENDING)
+		try:
+			self.ReportServiceStatus(win32service.SERVICE_RUNNING)
+			self.log('start')
+			self.start()
+			self.log('wait')
+			win32event.WaitForSingleObject(self.stop_event, win32event.INFINITE)
+			self.log('done')
+		except Exception, x:
+			self.log('Exception : %s' % x)
+			self.SvcStop()
+	def SvcStop(self):
+		self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+		self.log('stopping')
+		self.stop()
+		self.log('stopped')
+		win32event.SetEvent(self.stop_event)
+		self.ReportServiceStatus(win32service.SERVICE_STOPPED)
+	# to be overridden
+	def start(self): pass
+	# to be overridden
+	def stop(self): pass
+
+def instart(cls, name, display_name=None, stay_alive=True):
+    '''
+        Install and  Start (auto) a Service
+            
+            cls : the class (derived from Service) that implement the Service
+            name : Service name
+            display_name : the name displayed in the service manager
+            stay_alive : Service will stop on logout if False
+    '''
+    cls._svc_name_ = name
+    cls._svc_display_name_ = display_name or name
+    try:
+        module_path=modules[cls.__module__].__file__
+    except AttributeError:
+        # maybe py2exe went by
+        from sys import executable
+        module_path=executable
+    module_file=splitext(abspath(module_path))[0]
+    cls._svc_reg_class_ = '%s.%s' % (module_file, cls.__name__)
+    if stay_alive: win32api.SetConsoleCtrlHandler(lambda x: True, True)
+    try:
+        win32serviceutil.InstallService(
+                cls._svc_reg_class_,
+                cls._svc_name_,
+                cls._svc_display_name_,
+                startType=win32service.SERVICE_AUTO_START
+                )
+        print 'Install ok'
+        win32serviceutil.StartService(
+                cls._svc_name_
+                )
+        print 'Start ok'
+    except Exception, x:
+        print str(x)
+
+
