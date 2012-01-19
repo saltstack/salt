e640764e2173335fcf6970b3f98e836ab737e1b3
Evan Borgstrom <evan@fatbox.ca>
2012-01-18 20:40:11 -0500

    Allow file.sed to utilize regular expressions that are greedy
    This is a slight change to the logic for detecting an edit that allows
    for greedy regular expressions.
    
    My use case for this is removing ldap from nsswitch.conf.
    
        nsswitch_passwd:
          file:
            - sed
            - name: /etc/nsswitch.conf
            - before: '^passwd:.*'
            - after: 'passwd:		files'
    
    Previously `__salt__['file.contains'](name, before, limit)` would return
    the updated line because the regexp was greedily matching the after
    line. This meant it would re-apply the edit on every application of the
    state.

--- salt/states/file.py
+++ salt/states/file.py
@@ -914,14 +914,13 @@ def sed(name, before, after, limit='', backup='.bak', options='-r -e',
     # to look for ourselves
 
     # make sure the pattern(s) match
-    if not __salt__['file.contains'](name, before, limit):
-        if __salt__['file.contains'](name, after, limit):
-            ret['comment'] = "Edit already performed"
-            ret['result'] = True
-            return ret
-        else:
-            ret['comment'] = "Pattern not matched"
-            return ret
+    if __salt__['file.contains'](name, after, limit):
+        ret['comment'] = "Edit already performed"
+        ret['result'] = True
+        return ret
+    elif not __salt__['file.contains'](name, before, limit):
+        ret['comment'] = "Pattern not matched"
+        return ret
 
     # should be ok now; perform the edit
     __salt__['file.sed'](name, before, after, limit, backup, options, flags)
