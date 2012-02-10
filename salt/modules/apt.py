e509670b023dc680a344df2b33c9b4427de038a8
Seth House <seth@eseth.com>
2012-02-10 09:36:06 -0700

    Fixed missing space when using --allow-verify with the apt module
    Fixes #676. Thanks for the report, @sirkonst!

--- salt/modules/apt.py
+++ salt/modules/apt.py
@@ -108,8 +108,8 @@ def install(pkg, refresh=False, repo='', skip_verify=False, **kwargs):
 
     cmd = '{nonint} apt-get -q -y {confold}{verify}{target} install {pkg}'.format(
             nonint='DEBIAN_FRONTEND=noninteractive',
-            confold='-o DPkg::Options::=--force-confold',
-            verify='--allow-unauthenticated' if skip_verify else '',
+            confold=' -o DPkg::Options::=--force-confold',
+            verify=' --allow-unauthenticated' if skip_verify else '',
             target=' -t {0}'.format(repo) if repo else '',
             pkg=pkg)
 
