4f2564e95c5e60e1ea1c7f1c105ddfaa62031373
Thomas S Hatch <thatch45@gmail.com>
2012-02-12 16:34:42 -0700

    Add test.echo test

--- tests/modules/test.py
+++ tests/modules/test.py
@@ -1,6 +1,6 @@
-import daemon
+import saltunittest
 
-class TestModuleTest(daemon.ModuleCase):
+class TestModuleTest(saltunittest.ModuleCase):
     '''
     Validate the test module
     '''
@@ -9,3 +9,9 @@ class TestModuleTest(daemon.ModuleCase):
         test.ping
         '''
         self.assertTrue(self.run_function('test.ping'))
+
+    def test_echo(self):
+        '''
+        test.echo
+        '''
+        self.assertEqual(self.run_function('test.echo', ['text']), 'text')
