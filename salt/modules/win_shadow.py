90f71bcfd8f0a11bdde13bd43bebabda90026f1d
David Boucha <boucha@gmail.com>
2012-02-24 22:17:18 -0700

    Fixed comments and simplified some code per SEJeff's suggestions

--- salt/modules/win_shadow.py
+++ salt/modules/win_shadow.py
@@ -16,7 +16,7 @@ def __virtual__():
 def info(name):
     '''
     Return information for the specified user
-    This is just returns dummy data so that it states can work.
+    This is just returns dummy data so that salt states can work.
 
     CLI Example::
 
@@ -36,13 +36,11 @@ def info(name):
 
 def set_password(name, password):
     '''
-    Set the password for a named user. The password must be a properly defined
-    hash, the password hash can be generated with this command:
-    ``openssl passwd -1 <plaintext password>``
+    Set the password for a named user. 
 
     CLI Example::
 
-        salt '*' shadow.set_password root $1$UYCIxa628.9qXjpQCjM4a..
+        salt '*' shadow.set_password root mysecretpassword
     '''
     cmd = 'net user {0} {1}'.format(name, password)
     ret = __salt__['cmd.run_all'](cmd)
