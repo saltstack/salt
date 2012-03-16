9bca52ac1a8d5a0223fafcd7f9136403bb19f6f7
root <root@salt-minion.(none)>
2012-03-16 05:17:51 +0000

    refactor of pip

--- salt/modules/pip.py
+++ salt/modules/pip.py
@@ -17,35 +17,172 @@ def _get_pip_bin(pip, env):
     else:
         return pip if pip else __opts__['pip_bin']
 
-def install(env='', requirements='', pkgs='', pip_bin=''):
+def install(packages,
+            bin_env=None,
+            log=None,
+            proxy=None,
+            timeout=None,
+            editable=None,
+            find_links=None,
+            index_url=None,
+            extra_index_url=None,
+            no_index=False,
+            mirrors=None,
+            build=None,
+            target=None,
+            download=None,
+            download_cache=None,
+            source=None,
+            upgrade=False,
+            force_reinstall=False,
+            ignore_installed=False,
+            no_deps=False,
+            no_install=False,
+            no_download=False,
+            install_options=None):
     '''
     Install packages with pip
 
     Install packages individually or from a pip requirements file. Install
     packages globally or to a virtualenv.
 
-    env : None
-        The path to a virtualenv that pip should install to. This option takes
-        precendence over the ``pip_bin`` argument.
-    requirements : None
-        The path to a pip requirements file to install from
-    pkgs : None
-        A list of space-separated packages to install
-    pip_bin : 'pip'
-        The name (and optionally path) of the pip command to call. This option
-        will be ignored if the ``env`` argument is given since it will default
-        to the pip that is installed in the virtualenv. This option can also be
-        set in the minion config file as ``pip.pip_bin``.
+    packages 
+        package(s) or requirements file
+    bin_env
+        path to pip bin or path to virtualenv. If doing a system install,
+        and want to use a specific pip bin (pip-2.7, pip-2.6, etc..) just
+        specify the pip bin you want.
+        If installing into a virtualenv, just use the path to the virtualenv
+        (/home/code/path/to/virtualenv/)
 
     CLI Example::
 
-        salt '*' pip.install /var/www/myvirtualenv.com \\
-                /path/to/requirements.txt
+        salt '*' pip.install <package name>,<package2 name>
+
+        salt '*' pip.install /path/to/requirements.txt
+
+        salt '*' pip.install <package name> /path/to/virtualenv
+
+        salt '*' pip.install <package name> /path/to/pip_bin
     '''
-    cmd = '{pip_bin} install {reqs} {pkgs}'.format(
-        pip_bin=_get_pip_bin(pip_bin, env),
-        reqs='-r {0}'.format(requirements) if requirements else '',
-        pkgs=pkgs)
+
+    packages = packages.split(",")
+
+    if len(packages) == 1 and packages[0].endswith('.txt'):
+        # we are installing a requirements file
+        pkg = "-r {reqs}".format(reqs=packages[0])
+    else:
+        # we have a list of packages to install
+        pkg = ' '.join(packages)
+
+    if not bin_env:
+        pip_bin = 'pip'
+    else:
+        # try to get pip bin from env
+        if os.path.exists(os.path.join(bin_env, 'bin', 'pip')):
+            pip_bin = os.path.join(bin_env, 'bin', 'pip')
+        else:
+            pip_bin = bin_env
+
+    cmd = '{pip_bin} install {pkg}'.format(
+        pip_bin=pip_bin,
+        pkg=pkg)
+
+    if log:
+        try:
+            # TODO make this check if writeable
+            os.path.exists(log)
+        except IOError:
+            raise IOError("'%s' is not writeable" % log)
+        cmd = '{cmd} --{log}'.format(
+            cmd=cmd, log=log)
+
+    if proxy:
+        cmd = '{cmd} --proxy={proxy}'.format(
+            cmd=cmd, proxy=proxy)
+
+    if timeout:
+        try:
+            int(timeout)
+        except ValueError:
+            raise ValueError("'%s' is not a valid integer base 10.")
+        cmd = '{cmd} --timeout={timeout}'.format(
+            cmd=cmd, timeout=timeout)
+
+    if editable:
+        if editable.find('egg') == -1:
+            raise Exception('You must specify an egg for this editable')
+        cmd = '{cmd} --editable={editable}'.format(
+            cmd=cmd, editable=editable)
+
+    if find_links:
+        if not find_links.startswith("http://"):
+            raise Exception("'%s' must be a valid url" % find_links)
+        cmd = '{cmd} --find_links={find_links}'.format(
+            cmd=cmd, find_links=find_links)
+
+    if index_url:
+        if not index_url.startswith("http://"):
+            raise Exception("'%s' must be a valid url" % index_url)
+        cmd = '{cmd} --index_url={index_url}'.format(
+            cmd=cmd, index_url=index_url)
+
+    if extra_index_url:
+        if not extra_index_url.startswith("http://"):
+            raise Exception("'%s' must be a valid url" % extra_index_url)
+        cmd = '{cmd} --extra_index_url={extra_index_url}'.format(
+            cmd=cmd, extra_index_url=extra_index_url)
+
+    if no_index:
+        cmd = '{cmd} --no-index'.format(cmd=cmd)
+
+    if mirrors:
+        if not mirrors.startswith("http://"):
+            raise Exception("'%s' must be a valid url" % mirrors)
+        cmd = '{cmd} --use-mirrors --mirrors={mirrors}'.format(
+            cmd=cmd, mirrors=mirrors)
+
+    if build:
+        cmd = '{cmd} --build={build}'.format(
+            cmd=cmd, build=build)
+
+    if target:
+        cmd = '{cmd} --target={target}'.format(
+            cmd=cmd, target=target)
+
+    if download:
+        cmd = '{cmd} --download={download}'.format(
+            cmd=cmd, download=download)
+
+    if download_cache:
+        cmd = '{cmd} --download_cache={download_cache}'.format(
+            cmd=cmd, download_cache=download_cache)
+
+    if source:
+        cmd = '{cmd} --source={source}'.format(
+            cmd=cmd, source=source)
+
+    if upgrade:
+        cmd = '{cmd} --upgrade'.format(cmd=cmd)
+
+    if force_reinstall:
+        cmd = '{cmd} --force-reinstall'.format(cmd=cmd)
+
+    if ignore_installed:
+        cmd = '{cmd} --ignore-installed'.format(cmd=cmd)
+
+    if no_deps:
+        cmd = '{cmd} --no-deps'.format(cmd=cmd)
+
+    if no_install:
+        cmd = '{cmd} --no-install'.format(cmd=cmd)
+
+    if no_download:
+        cmd = '{cmd} --no-download'.format(cmd=cmd)
+
+    if install_options:
+        cmd = '{cmd} --install-options={install_options}'.format(
+            cmd=cmd, install_options=install_options)
 
     return __salt__['cmd.run'](cmd)
 
