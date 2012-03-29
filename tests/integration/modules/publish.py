62b709d5788c190734e0d00beb513b15351ed17c
Thomas S Hatch <thatch45@gmail.com>
2012-03-28 18:37:05 -0600

    publish tests

---
+++ tests/integration/modules/publish.py
@@ -0,0 +1,27 @@
+# Import salt libs
+import integration
+
+class PublishModuleTest(integration.ModuleCase):
+    '''
+    Validate the publish module
+    '''
+    def test_publish(self):
+        '''
+        publish.publish
+        '''
+        ret = self.run_function('publish.publish', ['minion', 'test.ping'])
+        self.assertTrue(ret['minion'])
+
+    def test_full_data(self):
+        '''
+        publish.full_data
+        '''
+        ret = self.run_function(
+                'publish.full_data',
+                [
+                    'minion',
+                    'test.fib',
+                    ['40']
+                ]
+                )
+        self.assertEqual(ret['minion']['ret'][0][-1], 34)
