fdd75e81f6d71a8f82abdd79cff6ecd20cbfdb6d
Thomas S Hatch <thatch45@gmail.com>
2012-04-04 10:58:11 -0600

    Add some pretty to the unit tests

--- tests/runtests.py
+++ tests/runtests.py
@@ -10,18 +10,32 @@ from integration import TestDaemon
 
 TEST_DIR = os.path.dirname(os.path.normpath(os.path.abspath(__file__)))
 
+PNUM = 50
+
 def run_integration_tests():
+    print '~' * PNUM
+    print 'Setting up Salt daemons to execute tests'
+    print '~' * PNUM
     with TestDaemon():
         moduleloader = saltunittest.TestLoader()
         moduletests = moduleloader.discover(os.path.join(TEST_DIR, 'integration', 'modules'), '*.py')
+        print '~' * PNUM
+        print 'Starting Module Tets'
+        print '~' * PNUM
         saltunittest.TextTestRunner(verbosity=1).run(moduletests)
         clientloader = saltunittest.TestLoader()
         clienttests = clientloader.discover(os.path.join(TEST_DIR, 'integration', 'client'), '*.py')
+        print '~' * PNUM
+        print 'Starting Client tests'
+        print '~' * PNUM
         saltunittest.TextTestRunner(verbosity=1).run(clienttests)
 
 def run_unit_tests():
     loader = saltunittest.TestLoader()
     tests = loader.discover(os.path.join(TEST_DIR, 'unit', 'templates'), '*.py')
+    print '~' * PNUM
+    print 'Starting Unit Tests'
+    print '~' * PNUM
     saltunittest.TextTestRunner(verbosity=1).run(tests)
 
 
