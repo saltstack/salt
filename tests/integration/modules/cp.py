18e1d5db6e7356a49ba21075da0ccc0e8ddb9ec2
Thomas S Hatch <thatch45@gmail.com>
2012-04-03 23:31:53 -0600

    Add cp.get_template tests

--- tests/integration/modules/cp.py
+++ tests/integration/modules/cp.py
@@ -22,3 +22,21 @@ class CPModuleTest(integration.ModuleCase):
         with open(tgt, 'r') as scene:
             data = scene.read()
             self.assertIn('KNIGHT:  They\'re nervous, sire.', data)
+            self.assertNotIn('bacon', data)
+
+    def test_get_template(self):
+        '''
+        cp.get_template
+        '''
+        tgt = os.path.join(integration.TMP, 'scene33')
+        self.run_function(
+                'cp.get_template',
+                [
+                    'salt://grail/scene33',
+                    tgt,
+                    'spam=bacon',
+                ])
+        with open(tgt, 'r') as scene:
+            data = scene.read()
+            self.assertIn('bacon', data)
+            self.assertNotIn('spam', data)
