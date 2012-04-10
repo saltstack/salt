30994e2c4b3f9d4f0fa99b8f936534ac011c5daa
Thomas S Hatch <thatch45@gmail.com>
2012-04-10 17:27:32 -0600

    Set gem to return result of None if tests applied

--- salt/states/gem.py
+++ salt/states/gem.py
@@ -26,7 +26,7 @@ def installed(name, ruby=None, runas=None):
     runas : None
         The user to run gem as.
     '''
-    ret = {'name': name, 'result': False, 'comment': '', 'changes': {}}
+    ret = {'name': name, 'result': None, 'comment': '', 'changes': {}}
     if name in __salt__['gem.list'](name, ruby, runas=runas):
         ret['result'] = True
         ret['comment'] = 'Gem is already installed.'
@@ -65,6 +65,7 @@ def removed(name, ruby=None, runas=None):
 
     if __opts__['test']:
         ret['comment'] = 'The gem {0} would have been removed'.format(name)
+        return ret
     if __salt__['gem.uninstall'](name, ruby, runas=runas):
         ret['result'] = True
         ret['changes'][name] = 'Removed'
