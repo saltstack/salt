e3a537a2951463d15b276e73e0ff5f1cbd788215
Wedge <wedge@we1.(none)>
2012-04-13 22:25:18 -0700

    network.py module code cleanup. Also fixed a bug in the netmask regex that was resulting in missing ipv4 netmasks when ifconfig is used.

--- salt/modules/network.py
+++ salt/modules/network.py
@@ -32,6 +32,192 @@ def _sanitize_host(host):
         c for c in host[0:255] if c in (ascii_letters + digits + '.')
     ])
 
+def _cidr_to_ipv4_netmask(cidr_bits):
+    '''
+    Returns an IPv4 netmask
+    '''
+    netmask = ''
+    for n in range(4):
+        if n:
+            netmask += '.'
+        if cidr_bits >= 8:
+            netmask += '255'
+            cidr_bits -= 8
+        else:
+            netmask += '%d' % (256-(2**(8-cidr_bits)))
+            cidr_bits = 0
+    return netmask
+
+
+def _number_of_set_bits_to_ipv4_netmask(set_bits):
+    '''
+    Returns an IPv4 netmask from the integer representation of that mask.
+
+    Ex. 0xffffff00 -> '255.255.255.0'
+    '''
+    return _cidr_to_ipv4_netmask(_number_of_set_bits(set_bits))
+
+def _number_of_set_bits(x):
+    '''
+    Returns the number of bits that are set in a 32bit int
+    '''
+    #Taken from http://stackoverflow.com/a/4912729. Many thanks!
+    x -= (x >> 1) & 0x55555555
+    x = ((x >> 2) & 0x33333333) + (x & 0x33333333)
+    x = ((x >> 4) + x) & 0x0f0f0f0f
+    x += x >> 8
+    x += x >> 16
+    return x & 0x0000003f
+
+def _interfaces_ip():
+    '''
+    Uses ip to return a dictionary of interfaces with various information about
+    each (up/down state, ip address, netmask, and hwaddr)
+    '''
+    ret = {}
+
+    out = __salt__['cmd.run']('ip addr show')
+    groups = re.compile('\r?\n\d').split(out)
+
+    for group in groups:
+        iface = None
+        data = {}
+        for line in group.split('\n'):
+            if not ' ' in line:
+                continue
+            m = re.match('^\d*:\s+(\w+)(?:@)?(\w+)?:\s+<(.+)>', line)
+            if m:
+                iface,parent,attrs = m.groups()
+                if 'UP' in attrs.split(','):
+                    data['up'] = True
+                if parent:
+                    data['parent'] = parent
+            else:
+                cols = line.split()
+                if len(cols) >= 2:
+                    type,value = tuple(cols[0:2])
+                    if type in ('inet', 'inet6'):
+                        def parse_network():
+                            """
+                            Return a tuple of ip, netmask, broadcast
+                            based on the current set of cols
+                            """
+                            brd = None
+                            ip,cidr = tuple(value.split('/'))
+                            if type == 'inet':
+                                mask = _cidr_to_ipv4_netmask(int(cidr))
+                                if 'brd' in cols:
+                                    brd = cols[cols.index('brd')+1]
+                            elif type == 'inet6':
+                                mask = cidr
+                            return (ip, mask, brd)
+
+                        if 'secondary' not in cols:
+                            ipaddr, netmask, broadcast = parse_network()
+                            if type == 'inet':
+                                data['ipaddr'] = ipaddr
+                                data['netmask'] = netmask
+                                data['broadcast'] = broadcast
+                            elif type == 'inet6':
+                                data['ipaddr6'] = ipaddr
+                                data['netmask6'] = netmask
+                        else:
+                            if 'secondary' not in data:
+                                data['secondary'] = []
+                            ip, mask, brd = parse_network()
+                            data['secondary'].append({
+                                'type': type,
+                                'ipaddr': ip,
+                                'netmask': mask,
+                                'broadcast': brd
+                                })
+                            del ip, mask, brd
+                    elif type.startswith('link'):
+                        data['hwaddr'] = value
+        if iface:
+            ret[iface] = data
+            del iface, data
+    return ret
+
+def _interfaces_ifconfig():
+    '''
+    Uses ifconfig to return a dictionary of interfaces with various information
+    about each (up/down state, ip address, netmask, and hwaddr)
+    '''
+    ret = {}
+
+    piface = re.compile('^(\w+)')
+    pmac = re.compile('.*?(?:HWaddr|ether) (.*?)\s')
+    pip = re.compile('.*?(?:inet addr:|inet )(.*?)\s')
+    pip6 = re.compile('.*?(?:inet6 addr: (.*?)/|inet6 )([0-9a-fA-F:]+)')
+    pmask = re.compile('.*?(?:Mask:|netmask )(?:(0x[0-9a-fA-F]{8})|([\d\.]+))')
+    pmask6 = re.compile('.*?(?:inet6 addr: [0-9a-fA-F:]+/(\d+)|prefixlen (\d+)).*')
+    pupdown = re.compile('UP')
+    pbcast = re.compile('.*?(?:Bcast:|broadcast )([\d\.]+)')
+
+    out = __salt__['cmd.run']('ifconfig -a')
+    groups = re.compile('\r?\n(?=\S)').split(out)
+
+    for group in groups:
+        data = {}
+        iface = ''
+        updown = False
+        for line in group.split('\n'):
+            miface = piface.match(line)
+            mmac = pmac.match(line)
+            mip = pip.match(line)
+            mip6 = pip6.match(line)
+            mmask = pmask.match(line)
+            mupdown = pupdown.search(line)
+            mbcast = pbcast.match(line)
+            mmask6 = pmask6.match(line)
+            if miface:
+                iface = miface.group(1)
+            if mmac:
+                data['hwaddr'] = mmac.group(1)
+            if mip:
+                data['ipaddr'] = mip.group(1)
+            if mmask:
+                if mmask.group(1):
+                    data['netmask'] =  _number_of_set_bits_to_ipv4_netmask(
+                        int(mmask.group(1), 16))
+                else:
+                    data['netmask'] = mmask.group(2)
+            if mupdown:
+                updown = True
+            if mbcast:
+                data['broadcast'] = mbcast.group(1)
+            if mip6:
+                if mip6.group(1):
+                    data['ipaddr6'] = mip6.group(1)
+                else:
+                    data['ipaddr6'] = mip6.group(2)
+            if mmask6:
+                if mmask6.group(1):
+                    data['netmask6'] = mmask6.group(1)
+                else:
+                    data['netmask6'] = mmask6.group(2)
+        data['up'] = updown
+        ret[iface] = data
+        del data
+    return ret
+
+def interfaces():
+    '''
+    Returns a dictionary of interfaces with various information about each
+    (up/down state, ip address, netmask, and hwaddr)
+
+    CLI Example::
+
+        salt '*' network.interfaces
+    '''
+    # find out which utility to use to find interface information
+    if __salt__['cmd.has_exec']('ip'):
+        return _interfaces_ip()
+    if __salt__['cmd.has_exec']('ifconfig'):
+        return _interfaces_ifconfig()
+    # no usable utility found
+    return {}
 
 def ping(host):
     '''
@@ -145,178 +331,6 @@ def isportopen(host, port):
     out = sock.connect_ex((_sanitize_host(host), int(port)))
 
     return out
-
-
-def _cidr_to_ipv4_netmask(cidr_bits):
-    '''
-    Returns an IPv4 netmask
-    '''
-    netmask = ''
-    for n in range(4):
-        if n:
-            netmask += '.'
-        if cidr_bits >= 8:
-            netmask += '255'
-            cidr_bits -= 8
-        else:
-            netmask += '%d' % (256-(2**(8-cidr_bits)))
-            cidr_bits = 0
-    return netmask
-
-
-def _number_of_set_bits_to_ipv4_netmask(set_bits):
-    '''
-    Returns an IPv4 netmask from the integer representation of that mask.
-
-    Ex. 0xffffff00 -> '255.255.255.0'
-    '''
-    return _cidr_to_ipv4_netmask(_number_of_set_bits(set_bits))
-
-def _number_of_set_bits(x):
-    '''
-    Returns the number of bits that are set in a 32bit int
-    '''
-    #Taken from http://stackoverflow.com/a/4912729. Many thanks!
-    x -= (x >> 1) & 0x55555555
-    x = ((x >> 2) & 0x33333333) + (x & 0x33333333)
-    x = ((x >> 4) + x) & 0x0f0f0f0f
-    x += x >> 8
-    x += x >> 16
-    return x & 0x0000003f
-
-def interfaces():
-    '''
-    Returns a dictionary of interfaces with various information about each
-    (up/down state, ip address, netmask, and hwaddr)
-
-    CLI Example::
-
-        salt '*' network.interfaces
-    '''
-    ret = {}
-
-    out = __salt__['cmd.run']('ip addr show')
-    groups = re.compile('\r?\n\d').split(out)
-
-    for group in groups:
-        iface = None
-        data = {}
-        for line in group.split('\n'):
-            if not ' ' in line:
-                continue
-            m = re.match('^\d*:\s+(\w+)(?:@)?(\w+)?:\s+<(.+)>', line)
-            if m:
-                iface,parent,attrs = m.groups()
-                if 'UP' in attrs.split(','):
-                    data['up'] = True
-                if parent:
-                    data['parent'] = parent
-            else:
-                cols = line.split()
-                if len(cols) >= 2:
-                    type,value = tuple(cols[0:2])
-                    if type in ('inet', 'inet6'):
-                        def parse_network():
-                            """
-                            Return a tuple of ip, netmask, broadcast
-                            based on the current set of cols
-                            """
-                            brd = None
-                            ip,cidr = tuple(value.split('/'))
-                            if type == 'inet':
-                                mask = _cidr_to_ipv4_netmask(int(cidr))
-                                if 'brd' in cols:
-                                    brd = cols[cols.index('brd')+1]
-                            elif type == 'inet6':
-                                mask = cidr
-                            return (ip, mask, brd)
-
-                        if 'secondary' not in cols:
-                            ipaddr, netmask, broadcast = parse_network()
-                            if type == 'inet':
-                                data['ipaddr'] = ipaddr
-                                data['netmask'] = netmask
-                                data['broadcast'] = broadcast
-                            elif type == 'inet6':
-                                data['ipaddr6'] = ipaddr
-                                data['netmask6'] = netmask
-                        else:
-                            if 'secondary' not in data:
-                                data['secondary'] = []
-                            ip, mask, brd = parse_network()
-                            data['secondary'].append({
-                                'type': type,
-                                'ipaddr': ip,
-                                'netmask': mask,
-                                'broadcast': brd
-                                })
-                            del ip, mask, brd
-                    elif type.startswith('link'):
-                        data['hwaddr'] = value
-        if iface:
-            ret[iface] = data
-            del iface, data
-    if ret:
-        return ret
-
-    # if nothing was returned then the minion probably doesn't have 'ip'
-    # try 'ifconfig' instead
-
-    piface = re.compile('^(\w+)')
-    pmac = re.compile('.*?(?:HWaddr|ether) (.*?)\s')
-    pip = re.compile('.*?(?:inet addr:|inet )(.*?)\s')
-    pip6 = re.compile('.*?(?:inet6 addr: (.*?)/|inet6 )([0-9a-fA-F:]+)')
-    pmask = re.compile('.*?(?:Mask:|netmask )(0x[0-9a-fA-F]{8})|([\d\.]+)')
-    pmask6 = re.compile('.*?(?:inet6 addr: [0-9a-fA-F:]+/(\d+)|prefixlen (\d+)).*')
-    pupdown = re.compile('UP')
-    pbcast = re.compile('.*?(?:Bcast:|broadcast )([\d\.]+)')
-
-    out = __salt__['cmd.run']('ifconfig -a')
-    groups = re.compile('\r?\n(?=\S)').split(out)
-
-    for group in groups:
-        data = {}
-        iface = ''
-        updown = False
-        for line in group.split('\n'):
-            miface = piface.match(line)
-            mmac = pmac.match(line)
-            mip = pip.match(line)
-            mip6 = pip6.match(line)
-            mmask = pmask.match(line)
-            mupdown = pupdown.search(line)
-            mbcast = pbcast.match(line)
-            mmask6 = pmask6.match(line)
-            if miface:
-                iface = miface.group(1)
-            if mmac:
-                data['hwaddr'] = mmac.group(1)
-            if mip:
-                data['ipaddr'] = mip.group(1)
-            if mmask:
-                if mmask.group(1):
-                    data['netmask'] =  _number_of_set_bits_to_ipv4_netmask(
-                        int(mmask.group(1), 16))
-                else:
-                    data['netmask'] = mmask.group(2)
-            if mupdown:
-                updown = True
-            if mbcast:
-                data['broadcast'] = mbcast.group(1)
-            if mip6:
-                if mip6.group(1):
-                    data['ipaddr6'] = mip6.group(1)
-                else:
-                    data['ipaddr6'] = mip6.group(2)
-            if mmask6:
-                if mmask6.group(1):
-                    data['netmask6'] = mmask6.group(1)
-                else:
-                    data['netmask6'] = mmask6.group(2)
-        data['up'] = updown
-        ret[iface] = data
-        del data
-    return ret
 
 def up(interface):
     '''
