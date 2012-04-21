7530f39269f304ef36c09ba85033e4a07c931748
Thomas S Hatch <thatch45@gmail.com>
2012-04-21 17:47:04 -0600

    Add some more key tests

--- tests/integration/shell/key.py
+++ tests/integration/shell/key.py
@@ -16,3 +16,23 @@ class KeyTest(integration.CliCase):
                 '\x1b[0;32mminion\x1b[0m',
                 '\x1b[1;34mRejected:\x1b[0m', '']
         self.assertEqual(data, expect)
+
+    def test_list_acc(self):
+        '''
+        test salt-key -l
+        '''
+        data = self.run_key('-l acc')
+        self.assertEqual(
+                data,
+                ['\x1b[0;32mminion\x1b[0m', '']
+                )
+
+    def test_list_un(self):
+        '''
+        test salt-key -l
+        '''
+        data = self.run_key('-l un')
+        self.assertEqual(
+                data,
+                ['']
+                )
