46da92cceaec842406c2f430abd6aad8a712eb11
Thomas S Hatch <thatch45@gmail.com>
2012-05-02 09:13:49 -0600

    Change pip to use which_bin

--- salt/modules/pip.py
+++ salt/modules/pip.py
@@ -2,6 +2,7 @@
 Install Python packages with pip to either the system or a virtualenv
 '''
 
+# Import python libs
 import os
 
 def _get_pip_bin(bin_env):
@@ -10,13 +11,10 @@ def _get_pip_bin(bin_env):
     passed in, or from the global modules options
     '''
     if not bin_env:
-        # this should be a define somewhere else; pip probably isn't the only
-        # thing that cares about this
-        rh_like = ('CentOS','Scientific','RedHat','Fedora')
-        if __grains__['os'] in rh_like:
-            pip_bin = 'pip-python'
-        else:
-            pip_bin = 'pip'
+        pips = ['pip2',
+                'pip',
+                'pip-python']
+        return __salt__['cmd.which_bin'](pips)
     else:
         # try to get pip bin from env
         if os.path.exists(os.path.join(bin_env, 'bin', 'pip')):
