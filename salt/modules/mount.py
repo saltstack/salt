be43d7cff9ef130582181b11431531c94f76761e
Joseph Hall <perlhoser@gmail.com>
2012-05-10 21:15:49 -0600

    Fill in some blanks on mount.active for extra device reporting

--- salt/modules/mount.py
+++ salt/modules/mount.py
@@ -27,6 +27,18 @@ def active():
         ret[comps[2]] = {'device': comps[0],
                          'fstype': comps[4],
                          'opts': comps[5][1:-1].split(',')}
+
+    # Fill in some blanks for extra device reporting
+    for line in open('/proc/self/mountinfo').readlines():
+        comps = line.split()
+        device = comps[2].split(':')
+        ret[comps[4]]['mountid'] = comps[0]
+        ret[comps[4]]['parentid'] = comps[1]
+        ret[comps[4]]['major'] = device[0]
+        ret[comps[4]]['minor'] = device[1]
+        ret[comps[4]]['root'] = comps[3]
+        ret[comps[4]]['source'] = comps[8]
+        ret[comps[4]]['superopts'] = comps[9].split(',')
     return ret
 
 
