1de35ebb2dc4281427c6660f3e3cd453d736f256
David Boucha <boucha@gmail.com>
2012-05-11 22:21:26 -0600

    Skip this signal creation on Windows.
    SIGUSR1 doesn't exist on Windows and this event was causing
    the minion to crash on Windows
    
    I'm not sure this is the best way to take care of this. But I'm not
    sure how to actually implement this in Windows yet.
    Google seems to imply that win32events would provided this
    functionality, but I haven't figured out how to use it yet.

--- salt/utils/debug.py
+++ salt/utils/debug.py
@@ -40,4 +40,9 @@ def enable_sigusr1_handler():
     Pretty print a stack trace to the console or a debug log under /tmp
     when any of the salt daemons such as salt-master are sent a SIGUSR1
     '''
-    signal.signal(signal.SIGUSR1, _handle_sigusr1)
+    #  Skip setting up this signal on Windows
+    #  SIGUSR1 doesn't exist on Windows and causes the minion to crash
+    if os.environ['os'].startswith('Windows'):
+        pass
+    else:
+        signal.signal(signal.SIGUSR1, _handle_sigusr1)
