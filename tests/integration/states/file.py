9628f9a90b14069ca51f8cbbd995520285c8eb32
Thomas S Hatch <thatch45@gmail.com>
2012-05-12 22:31:58 -0600

    Add state integration test for file.symlink

---
+++ tests/integration/states/file.py
@@ -0,0 +1,24 @@
+'''
+Tests for the file state
+'''
+# Import python libs
+import os
+#
+# Import salt libs
+from saltunittest import TestLoader, TextTestRunner
+import integration
+from integration import TestDaemon
+
+class FileTest(integration.ModuleCase):
+    '''
+    Validate the file state
+    '''
+    def test_symlink(self):
+        '''
+        file.symlink
+        '''
+        name = os.path.join(integration.TMP, 'symlink')
+        tgt = os.path.join(integration.TMP, 'target')
+        ret = self.run_state('file.symlink', name=name, target=tgt)
+        result = ret[ret.keys()[0]]['result']
+        self.assertTrue(result)
