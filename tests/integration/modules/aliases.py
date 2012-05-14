8e26ea646eb094e02ee5c440458d7e76f58a78a5
Thomas S Hatch <thatch45@gmail.com>
2012-05-14 10:44:17 -0600

    Add rm_alias test

--- tests/integration/modules/aliases.py
+++ tests/integration/modules/aliases.py
@@ -54,3 +54,21 @@ class AliasesTest(integration.ModuleCase):
                 'aliases.list_aliases')
         self.assertIsInstance(tgt_ret, dict)
         self.assertIn('alias=fred', tgt_ret)
+
+    def test_rm_alias(self):
+        '''
+        aliases.rm_alias
+        '''
+        set_ret = self.run_function(
+                'aliases.set_target',
+                alias='frank',
+                target='greg')
+        self.assertTrue(set_ret)
+        set_ret = self.run_function(
+                'aliases.rm_alias',
+                alias='frank')
+        tgt_ret = self.run_function(
+                'aliases.list_aliases')
+        self.assertIsInstance(tgt_ret, dict)
+        self.assertNotIn('alias=frank', tgt_ret)
+
