81bca9cbdececa29b82d348b93c03f7ed6fda018
Kurtis Velarde <kurtisvelarde@gmail.com>
2012-05-16 17:03:25 -0700

    First commit for rabbitmq module

---
+++ salt/modules/rabbitmq-server.py
@@ -0,0 +1,71 @@
+'''
+Module to provide rabbitMQ compatibility to salt.
+Todo: Alot, need to add cluster support, logging, and minion configuration data.
+'''
+
+def list_users():
+    '''
+    List a a users
+    '''
+    d = {}
+    res = __salt__['cmd.run']('list_users')
+    for line in res.split('\n'):
+        if '...' not in line or line == '\n': d[ line.split('\t')[0] ] = line.split('\t')[1]
+    return d
+
+
+def list_vhosts():
+    '''
+    Get vhost listing.
+    '''
+    res = __salt__['cmd.run']('rabbitmqctl list_vhosts')
+    r = res.split('\n')
+    return { 'vhost' : r }
+
+
+def add_user(name, password):
+    '''
+    Add a rabbitMQ user.
+    '''
+    res = __salt__['cmd.run']('rabbitmqctl add_user {0} {1}'.format(name, password))
+    return { 'Added' : res }
+
+
+def delete_user(name):
+    '''
+    Deletes a user.
+    '''
+    res = __salt__['cmd.run']('rabbitmqctl delete_user {0}'.format(name))
+    return { 'delete' : res }
+
+
+def add_vhost(vhost):
+    '''
+    Adds a vhost to the service.
+    '''
+    res = __salt__['cmd.run']('rabbitmqctl add_vhost {0}'.format(vhost))
+    return {'add_vhost' : res}
+
+
+def delete_vhost(vhost):
+    '''
+    Deletes a vhost.
+    '''
+    res = __salt__['cmd.run']('rabbitmqctl delete_vhost {0}'.format(vhost))
+    return {'delete' : res}
+
+
+def set_permissions(vhost,user,conf='.*',write='.*',read='.*'):
+    '''
+    Sets permissions for vhost.
+    '''
+    res = __salt__['cmd.run']('rabbitmqctl set_permissions -p {0} {1} "{2}" "{3}" "{4}"'.format(vhost,user,conf,write,read))
+    return { 'set_permissions': res }
+
+
+def list_user_permissions(name):
+    '''
+    List permissions for a user.
+    '''
+    res = __salt__['cmd.run']('rabbitmqctl list_user_permissions {0}'.format(name))
+    return { 'user_permissions' : [ r.split('\t') for r in res.split('\n') ] }
