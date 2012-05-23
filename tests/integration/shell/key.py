dd6e2da941db04f57515bbec42cd08ef36338c26
Thomas S Hatch <thatch45@gmail.com>
2012-05-23 08:05:47 -0600

    Fix salt-key test for new minion

--- tests/integration/shell/key.py
+++ tests/integration/shell/key.py
@@ -20,6 +20,7 @@ class KeyTest(integration.ShellCase):
                 '\x1b[1;31mUnaccepted Keys:\x1b[0m',
                 '\x1b[1;32mAccepted Keys:\x1b[0m',
                 '\x1b[0;32mminion\x1b[0m',
+                '\x1b[0;32msub_minion\x1b[0m',
                 '\x1b[1;34mRejected:\x1b[0m', '']
         self.assertEqual(data, expect)
 
