ddf21d2a358d38ae5dd9b9eb061b0b1d0f5ad992
Thomas S Hatch <thatch45@gmail.com>
2012-05-27 12:34:07 -0600

    Add returns to network runner and fix quotes

--- salt/runners/network.py
+++ salt/runners/network.py
@@ -16,13 +16,17 @@ def wollist(maclist, bcast='255.255.255.255', destport=9):
         salt-run '/path/to/maclist' 255.255.255.255 7
         salt-run '/path/to/maclist' 255.255.255.255 7
     '''
+    ret = []
     try:
         file = open(maclist, 'r')
         for mac in file:
             wol(mac.strip(), bcast, destport)
-            print("Waking up %s" % mac.strip())
+            print('Waking up {0}'.format(mac.strip()))
+            ret.append(mac)
     except Exception as inst:
-        print("Failed to open the MAC file. Error: %s" % inst)
+        print('Failed to open the MAC file. Error: {0}'.format(inst))
+        return []
+    return ret
 
 
 def wol(mac, bcast='255.255.255.255', destport=9):
@@ -51,4 +55,5 @@ def wol(mac, bcast='255.255.255.255', destport=9):
             ('\\x' + mac[8:10]).decode('string_escape') + \
             ('\\x' + mac[10:12]).decode('string_escape')
     s.sendto('\xff' * 6 + dest * 16, (bcast, int(destport)))
-    print("Sent magic packet to minion.")
+    print('Sent magic packet to minion.')
+    return True
