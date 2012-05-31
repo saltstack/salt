96e4eeb8ce465f1b3a43e8f63baf554f77eef2b3
Alec Koumjian <akoumjian@gmail.com>
2012-05-31 11:11:33 -0700

    more django state module functions

--- salt/states/django.py
+++ salt/states/django.py
@@ -11,10 +11,27 @@ Run django management commands.
             - bin_env: /var/www/env/
             - pythonpath: /var/www/mysite/
             - migrate: True
+
+    settings:
         django.collectstatic:
             - bin_env: /var/www/env/
             - pythonpath: /var/www/mysite/
             - clear: True
+
+    settings:
+        django.createsuperuser:
+            - username: foo
+            - email: foo@bar.com
+            - bin_env: /var/www/env/
+            - pythonpath: /var/www/mysite/
+
+    settings:
+        django.loaddata:
+            - fixtures:
+                - foo/bar/mydata.json
+                - otherdata
+            - bin_env: /var/www/env/
+            - pythonpath: /var/www/mysite/
 '''
 
 
@@ -44,6 +61,73 @@ def syncdb(name,
                               migrate=migrate,
                               database=database,
                               pythonpath=pythonpath)
+    ret['changes']['output'] = output
+    return ret
+
+
+def createsuperuser(name,
+                    username,
+                    email,
+                    bin_env=None,
+                    database=None,
+                    pythonpath=None):
+    '''
+    Create a super user for the database.
+    This function defaults to use the ``--noinput`` flag which prevents the
+    creation of a password for the superuser.
+
+    name
+      The name of the settings module.
+    username
+      Username for the new superuser
+    email
+      Email for the new superuser
+    bin_env
+      Path to a python virtual environment
+    database
+      Specifies which database to run on
+    pythonpath
+      Path from which python will execute
+    '''
+    ret = {'name': name, 'result': True, 'comment': '', 'changes': {}}
+
+    output = __salt__['django.createsuperuser'](name,
+                                                username=username,
+                                                email=email,
+                                                bin_env=bin_env,
+                                                database=database,
+                                                pythonpath=pythonpath)
+    ret['changes']['output'] = output
+    return ret
+
+
+def loaddata(name,
+             fixtures,
+             bin_env=None,
+             database=None,
+             pythonpath=None):
+    '''
+    Load fixture data
+
+    name
+        Name of the settings module
+    fixtures
+        List of fixtures to load
+    bin_env
+        Path to virtual environment
+    database
+        Specifies database to run against
+    pythonpath
+        Path from which python will execute
+
+    '''
+    ret = {'name': name, 'result': True, 'comment': '', 'changes': {}}
+    fixtures = ','.join(fixtures)
+    output = __salt__['django.loaddata'](name,
+                                         fixtures=fixtures,
+                                         bin_env=bin_env,
+                                         database=database,
+                                         pythonpath=pythonpath)
     ret['changes']['output'] = output
     return ret
 
