5b9d16ac8ad7428f6373a0efde255b9143e82f88
Thomas S Hatch <thatch45@gmail.com>
2012-06-13 15:39:01 -0600

    Caal it job_cache

--- salt/master.py
+++ salt/master.py
@@ -616,7 +616,7 @@ class AESFuncs(object):
             return False
         log.info('Got return from {0[id]} for job {0[jid]}'.format(load))
         self.event.fire_event(load, load['jid'])
-        if not self.opts['cache_jobs']:
+        if not self.opts['job_cache']:
             return
         jid_dir = salt.utils.jid_dir(
                 load['jid'],
