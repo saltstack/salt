439326c425dc20fb98ab8e7cbc6ed0f1dc2228e8
Thomas S Hatch <thatch45@gmail.com>
2012-06-18 23:24:39 -0600

    Add loader module for cloud modules

---
+++ saltcloud/loader.py
@@ -0,0 +1,15 @@
+'''
+The salt cloud module loader interface
+'''
+# Import Salt libs
+import salt.loader
+import saltcloud
+
+salt.loader.salt_base_path = os.path.dirname(saltcloud.__file__)
+
+def clouds(opts):
+    '''
+    Return the cloud functions
+    '''
+    load = salt.loader._create_loader(opts. 'clouds', 'cloud')
+    return load.gen_functions()
