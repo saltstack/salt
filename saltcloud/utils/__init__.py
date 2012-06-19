d5abd7d7b26f78904b30e7296b84262a8a43ac6a
Thomas S Hatch <thatch45@gmail.com>
2012-06-18 23:36:22 -0600

    Move some generic functions into utils

--- saltcloud/utils/__init__.py
+++ saltcloud/utils/__init__.py
@@ -25,3 +25,37 @@ def os_script(os_):
                 return data
             except (OSError, IOError):
                 continue
+
+
+def gen_keys(keysize=2048):
+    '''
+    Generate Salt minion keys and return them as PEM file strings
+    '''
+    tdir = tempfile.mkdtemp()
+    salt.crypt.gen_keys(
+            tdir,
+            'minion',
+            keysize)
+    priv_path = os.path.join(tdir, 'minion.pem')
+    pup_path = os.path.join(tdir, 'minion.pub')
+    with open(priv_path) as fp_:
+        priv = fp_.read()
+    with open(pub_path) as fp_:
+        pub = fp_.read()
+    shutil.rmtree(tdir)
+    return priv, pub
+
+
+def accept_key(pki_dir, pub, id_):
+    '''
+    If the master config was available then we will have a pki_dir key in
+    the opts directory, this method places the pub key in the accepted
+    keys dir if that is the case.
+    '''
+    key = os.path.join(
+            pki_dir,
+            'minions/{0}.pub'.format(id_)
+            )
+    with open(key, 'w+') as fp_:
+        fp_.write(pub)
+
