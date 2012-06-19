18fe1882f7717051108706d3b9effaf892a15686
Thomas S Hatch <thatch45@gmail.com>
2012-06-19 11:18:49 -0600

    Fix loader syntax errors

--- saltcloud/loader.py
+++ saltcloud/loader.py
@@ -1,6 +1,9 @@
 '''
 The salt cloud module loader interface
 '''
+# Import python libs
+import os
+
 # Import Salt libs
 import salt.loader
 import saltcloud
@@ -11,5 +14,5 @@ def clouds(opts):
     '''
     Return the cloud functions
     '''
-    load = salt.loader._create_loader(opts. 'clouds', 'cloud')
+    load = salt.loader._create_loader(opts, 'clouds', 'cloud')
     return load.gen_functions()
