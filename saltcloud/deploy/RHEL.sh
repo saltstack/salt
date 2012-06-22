a5ab3e961e61c56837cf1e681e00f3960bf990c6
Thomas S Hatch <thatch45@gmail.com>
2012-06-21 22:54:10 -0600

    yum -y is needed

--- saltcloud/deploy/RHEL.sh
+++ saltcloud/deploy/RHEL.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo rpm -Uvh --force http://mirrors.kernel.org/fedora-epel/6/x86_64/epel-release-6-7.noarch.rpm
-sudo yum install salt-minion
+sudo yum install -y salt-minion
 sudo echo '{{ vm['priv_key'] }}' > /etc/salt/pki/minion.pem
 sudo echo '{{ vm['pub_key'] }}' > /etc/salt/pki/minion.pub
 sudo echo '{{ minion }}' > /etc/salt/minion
