ab2951e433500aff95d348f2aa06bb24e996e9ce
Thomas S Hatch <thatch45@gmail.com>
2012-06-21 22:39:24 -0600

    Add minion ref to RHEL deploy script

--- saltcloud/deploy/RHEL.sh
+++ saltcloud/deploy/RHEL.sh
@@ -4,5 +4,6 @@ sudo rpm -Uvh --force http://mirrors.kernel.org/fedora-epel/6/x86_64/epel-releas
 sudo yum install salt-minion
 sudo echo '{{ vm['priv_key'] }}' > /etc/salt/pki/minion.pem
 sudo echo '{{ vm['pub_key'] }}' > /etc/salt/pki/minion.pub
+sudo echo '{{ minion }}' > /etc/salt/minion
 sudo /sbin/chkconfig salt-minion on
 sudo service salt-minion start
