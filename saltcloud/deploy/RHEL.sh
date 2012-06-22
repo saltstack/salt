b46c9dc76417d2a876b227930f3fba9cf7f2f963
Thomas S Hatch <thatch45@gmail.com>
2012-06-21 22:12:52 -0600

    pass in real locations of key pair

--- saltcloud/deploy/RHEL.sh
+++ saltcloud/deploy/RHEL.sh
@@ -2,7 +2,7 @@
 
 sudo rpm -Uvh --force http://mirrors.kernel.org/fedora-epel/6/x86_64/epel-release-6-7.noarch.rpm
 sudo yum install salt-minion
-sudo echo {{ priv_key }} > /etc/salt/pki/minion.pem
-sudo echo {{ pub_key }} > /etc/salt/pki/minion.pub
+sudo echo '{{ vm['priv_key'] }}' > /etc/salt/pki/minion.pem
+sudo echo '{{ vm['pub_key'] }}' > /etc/salt/pki/minion.pub
 sudo /sbin/chkconfig salt-minion on
 sudo service salt-minion start
