94a8aa6e2a8e9c75fc52a5473b503ce7ab036efb
avimar <avi@amarcus.com>
2012-06-27 01:16:42 +0300

    Update develop

--- salt/states/mysql_database.py
+++ salt/states/mysql_database.py
@@ -2,7 +2,8 @@
 Management of MySQL databases (schemas).
 ========================================
 
-NOTE: Ensure that salt.modules.mysql has its dependency and is properly configured.
+NOTE: This module requires the MySQLdb python module and the proper settings in the minion config file.
+See salt.modules.mysql for more information.
 
 The mysql_database module is used to create and manage MySQL databases.
 Databases can be set as either absent or present.
