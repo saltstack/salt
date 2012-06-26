d93e81d0c6b356d945ddc0d4d0481164a535a690
Thomas S Hatch <thatch45@gmail.com>
2012-06-25 20:31:56 -0600

    merge mapper class into the cloud module

--- saltcloud/cloud.py
+++ saltcloud/cloud.py
@@ -3,12 +3,17 @@ The top level interface used to translate configuration data back to the
 correct cloud modules
 '''
 # Import python libs
+import os
+import copy
 import multiprocessing
 
 # Import saltcloud libs
 import saltcloud.utils
 import saltcloud.loader
+import salt.client
 
+# Import third party libs
+import yaml
 
 class Cloud(object):
     '''
@@ -93,3 +98,47 @@ class Cloud(object):
                                 ).start()
                     else:
                         self.create(vm_)
+
+
+class Map(Cloud):
+    '''
+    Create a vm stateful map execution object
+    '''
+    def __init__(self, opts):
+        self.opts = opts
+        self.cloud = saltcloud.cloud.Cloud(self.opts)
+        self.map = self.read()
+
+    def read(self):
+        '''
+        Read in the specified map file and return the map structure
+        '''
+        if not self.opts['map']:
+            return {}
+        if not os.path.isfile(self.opts['map']):
+            return {}
+        try:
+            with open(self.opts['map'], 'rb') as fp_:
+                map_ = yaml.load(fp_.read())
+        except Exception:
+            return {}
+        if 'include' in map_:
+            map_ = salt.config.include_config(map_, self.opts['map'])
+        return map_
+
+    def run_map(self):
+        '''
+        Execute the contents of the vm map
+        '''
+        for profile in self.map:
+            for name in self.map[profile]:
+                for vm_ in self.opts['vm']:
+                    if vm_['profile'] == profile:
+                        tvm = copy.deepcopy(vm_)
+                        tvm['name'] = name
+                        if self.opts['parallel']:
+                            multiprocessing.Process(
+                                    target=lambda: self.cloud.create(tvm)
+                                    ).start()
+                        else:
+                            self.cloud.create(tvm)
