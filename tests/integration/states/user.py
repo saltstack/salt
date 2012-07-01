6a63f7c71c39fe442780c192b566e5bcd7f94f42
Larry Price <laprice@gmail.com>
2012-06-30 17:28:48 -0700

    Destructive tests now fail properly.

--- tests/integration/states/user.py
+++ tests/integration/states/user.py
@@ -29,11 +29,13 @@ class UserTest(integration.ModuleCase):
     def test_user_not_present(self):
         """
         This is a DESTRUCTIVE TEST it creates a new user on the minion.
+        And then destroys that user.
         Assume that it will break any system you run it on.
         """
         ret = self.run_state('user.present', name='salt_test') 
         result = ret[next(iter(ret))]['result']
         self.assertTrue(result)
+        ret = self.run_state('user.absent', name='salt_test')
 
     @skipIf(not (os.geteuid()==0), 'you must be this root to run this test')
     def test_user_present_nondefault(self):
@@ -44,3 +46,6 @@ class UserTest(integration.ModuleCase):
                              home='/var/lib/salt_test') 
         result = ret[next(iter(ret))]['result']
         self.assertTrue(result)
+        self.assertTrue(os.stat('/var/lib/salt_test'))
+        ret = self.run_state('user.absent', name='salt_test')
+
