71e97d6921d2bbeef047bf22565557e92ce47e1f
Dan Colish <dcolish@gmail.com>
2012-06-30 17:33:34 -0700

    Fix file.chown to report unknown users and groups. Add tests to prove it.

--- tests/integration/modules/file.py
+++ tests/integration/modules/file.py
@@ -4,9 +4,8 @@ import os
 import sys
 
 # Import salt libs
-from saltunittest import skipIf, TestLoader, TextTestRunner
+from saltunittest import skipIf
 import integration
-from integration import TestDaemon
 
 
 class FileModuleTest(integration.ModuleCase):
@@ -24,6 +23,59 @@ class FileModuleTest(integration.ModuleCase):
         super(FileModuleTest, self).tearDown()
 
     @skipIf(sys.platform.startswith('win'), 'No chgrp on Windows')
+    def test_chown(self):
+        user = os.getlogin()
+        if sys.platform == 'darwin':
+            group = 'staff'
+        elif sys.platform == 'linux':
+            group = os.getlogin()
+        ret = self.run_function('file.chown',
+                                arg=[self.myfile, user, group])
+        self.assertIsNone(ret)
+        fstat = os.stat(self.myfile)
+        self.assertTrue(fstat.st_uid, os.getuid())
+        self.assertTrue(fstat.st_gid, grp.getgrnam(group).gr_gid)
+
+    @skipIf(sys.platform.startswith('win'), 'No chgrp on Windows')
+    def test_chown_no_user(self):
+        user = 'notanyuseriknow'
+        group = os.getlogin()
+        ret = self.run_function('file.chown',
+                                arg=[self.myfile, user, group])
+        self.assertIn('not exist', ret)
+
+    @skipIf(sys.platform.startswith('win'), 'No chgrp on Windows')
+    def test_chown_no_user_no_group(self):
+        user = 'notanyuseriknow'
+        group = 'notanygroupyoushoulduse'
+        ret = self.run_function('file.chown',
+                                arg=[self.myfile, user, group])
+        self.assertIn('Group does not exist', ret)
+        self.assertIn('User does not exist', ret)
+
+    @skipIf(sys.platform.startswith('win'), 'No chgrp on Windows')
+    def test_chown_no_path(self):
+        user = os.getlogin()
+        if sys.platform == 'darwin':
+            group = 'staff'
+        elif sys.platform == 'linux':
+            group = os.getlogin()
+        ret = self.run_function('file.chown',
+                                arg=['/tmp/nosuchfile', user, group])
+        self.assertIn('File not found', ret)
+
+    @skipIf(sys.platform.startswith('win'), 'No chgrp on Windows')
+    def test_chown_noop(self):
+        user = ''
+        group = ''
+        ret = self.run_function('file.chown',
+                                arg=[self.myfile, user, group])
+        self.assertIsNone(ret)
+        fstat = os.stat(self.myfile)
+        self.assertTrue(fstat.st_uid, os.getuid())
+        self.assertTrue(fstat.st_gid, os.getgid())
+
+    @skipIf(sys.platform.startswith('win'), 'No chgrp on Windows')
     def test_chgrp(self):
         if sys.platform == 'darwin':
             group = 'everyone'
@@ -34,3 +86,10 @@ class FileModuleTest(integration.ModuleCase):
         self.assertIsNone(ret)
         fstat = os.stat(self.myfile)
         self.assertTrue(fstat.st_gid, grp.getgrnam(group).gr_gid)
+
+    @skipIf(sys.platform.startswith('win'), 'No chgrp on Windows')
+    def test_chgrp_failure(self):
+        group = 'thisgroupdoesntexist'
+        ret = self.run_function('file.chgrp',
+                                arg=[self.myfile, group])
+        self.assertIn('not exist', ret)
