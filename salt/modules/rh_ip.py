c4b01fd6073b40d76bda261e8e05ad44d9bdf0f9
Bret Palsson <bretep@gmail.com>
2012-07-05 23:40:50 -0600

    Missed a change for the 'enabled'

--- salt/modules/rh_ip.py
+++ salt/modules/rh_ip.py
@@ -533,7 +533,7 @@ def _parse_settings_eth(opts, iface_type, enabled, iface):
 
     if 'onboot' in opts:
         log.warning('''The 'onboot' option is controlled by the 'enabled' option.
-                Interface: %s Enabled: %s''' % (iface, opts['enabled']))
+                Interface: %s Enabled: %s''' % (iface, enabled))
 
     if 'enabled':
         result['onboot'] = 'yes'
