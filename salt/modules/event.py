3341d933ba482b6549df9d28dcbce2016e9f6429
Thomas S Hatch <thatch45@gmail.com>
2012-07-05 22:50:09 -0600

    use new sreq in event system

--- salt/modules/event.py
+++ salt/modules/event.py
@@ -12,18 +12,14 @@ def fire_master(data, tag):
     '''
     Fire an event off on the master server
     '''
-    serial = salt.payload.Serial(__opts__)
-    context = zmq.Context()
-    socket = context.socket(zmq.REQ)
-    socket.connect(__opts__['master_uri'])
     load = {'id': __opts__['id'],
             'tag': tag,
             'data': data,
             'cmd': '_minion_event'}
     auth = salt.crypt.SAuth(__opts__)
-    payload = {'enc': 'aes'}
-    payload['load'] = auth.crypticle.dumps(load)
-    auth = salt.crypt.SAuth(__opts__)
-    socket.send(serial.dumps(payload))
-    socket.recv()
+    sreq = salt.payload.SREQ(__opts__['master_uri'])
+    try:
+        sreq.send('aes', auth.crypticle.dumps(load))
+    except:
+        pass
     return True
