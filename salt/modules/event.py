7a3f76179735396c0fa28f245575244c501198bc
Thomas S Hatch <thatch45@gmail.com>
2012-07-06 00:47:56 -0600

    Add module function to fire events on the minion bus

--- salt/modules/event.py
+++ salt/modules/event.py
@@ -3,14 +3,16 @@ Fire events on the minion, events can be fired up to the master
 '''
 # Import Salt libs
 import salt.crypt
+import salt.utils.event
 import salt.payload
-
-# Import third party libs
-import zmq
 
 def fire_master(data, tag):
     '''
     Fire an event off on the master server
+
+    CLI Example::
+
+        salt '*' event.fire_master 'stuff to be in the event' 'tag'
     '''
     load = {'id': __opts__['id'],
             'tag': tag,
@@ -23,3 +25,15 @@ def fire_master(data, tag):
     except:
         pass
     return True
+
+
+def fire(data, tag):
+    '''
+    Fire an event on the local minion event bus
+
+    CLI Example::
+
+        salt '*' event.fire 'stuff to be in the event' 'tag'
+    '''
+    esock = salt.utils.event.MinionEvent(__opts__['sock_dir'])
+    return esock.fire_event(data, tag)
