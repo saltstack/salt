080a02c73892c07c2ba5f791ece635e030c216be
Thomas S Hatch <thatch45@gmail.com>
2012-07-09 10:52:44 -0600

    Some fixes for better logging and safety

--- salt/pillar/hiera.py
+++ salt/pillar/hiera.py
@@ -3,8 +3,25 @@ Take in a hiera configuration file location and execute it.
 Adds the hiera data to pillar
 '''
 
+# Import python libs
+import logging
+
+# Import salt libs
+import salt.utils
+
 # Import third party libs
 import yaml
+
+
+# Set up logging
+log = logging.getLogger(__name__)
+
+def __virtual__():
+    '''
+    Only return if hiera is installed
+    '''
+    return 'hiera' if salt.utils.which('hiera') else False
+
 
 def ext_pillar(conf):
     '''
@@ -14,4 +31,11 @@ def ext_pillar(conf):
     for key, val in __grains__.items():
         if isinstance(val, string_types):
             cmd += ' {0}={1}'.format(key, val)
-    return yaml.safe_load(__salt__['cmd.run'](cmd))
+    try:
+        data = yaml.safe_load(__salt__['cmd.run'](cmd))
+    except Exception:
+        log.critical(
+                'Hiera yaml data failed to parse from conf {0}'.format(conf)
+                )
+        return {}
+    return data
