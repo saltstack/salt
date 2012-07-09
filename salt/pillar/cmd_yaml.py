df909be2b443024b96677afd65b4dc9d85af9601
Thomas S Hatch <thatch45@gmail.com>
2012-07-09 11:01:56 -0600

    Add logging and checks to cmd_yaml pillar

--- salt/pillar/cmd_yaml.py
+++ salt/pillar/cmd_yaml.py
@@ -3,12 +3,24 @@ Execute a command and read the output as YAML. The YAML data is then directly
 overlaid onto the minion's pillar data
 '''
 
+# Import python libs
+import logging
+
 # Import third party libs
 import yaml
+
+# Set up logging
+log = logging.getLogger(__name__)
 
 
 def ext_pillar(command):
     '''
     Execute a command and read the output as YAML
     '''
-    return yaml.safe_load(__salt__['cmd.run'](command))
+    try:
+        return yaml.safe_load(__salt__['cmd.run'](command))
+    except Exception:
+        log.critical(
+                'YAML data from {0} failed to parse'.format(command)
+                )
+        return {}
