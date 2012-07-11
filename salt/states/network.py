fa6d36b6cd97fc76137be69830951032601e98e9
Bret Palsson <bretep@gmail.com>
2012-07-10 22:23:37 -0600

    Keep things consistent

--- salt/states/network.py
+++ salt/states/network.py
@@ -17,7 +17,7 @@ supported. This module will therefore only work on RH/CentOS/Fedora.
         - hostname: server1.example.com
         - gateway: 192.168.0.1
         - gatewaydev: eth0
-        - nozeroconf: true
+        - nozeroconf: True
         - nisdomain: example.com
         - require_reboot: True
 
