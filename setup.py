9a3e24524ff34d8f6e79e39b8f958753adfe7e95
Thomas S Hatch <thatch45@gmail.com>
2012-07-13 10:55:59 -0600

    change to Alpha status

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
           'Programming Language :: Python',
           'Programming Language :: Python :: 2.6',
           'Programming Language :: Python :: 2.7',
-          'Development Status :: 2 - Pre-Alpha'
+          'Development Status :: 3 - Alpha'
           'Environment :: Console',
           'Intended Audience :: Developers',
           'Intended Audience :: Information Technology',
