3f764e5588025ed3763c59f8852d78adac0caae2
Thomas S Hatch <thatch45@gmail.com>
2012-07-15 01:29:04 -0600

    Add avail funcs to EC2

--- saltcloud/clouds/EC2.py
+++ saltcloud/clouds/EC2.py
@@ -70,6 +70,38 @@ def script(vm_):
             )
 
 
+def avail_images():
+    '''
+    Return a dict of all available vm images on the cloud provider with
+    relevant data
+    '''
+    conn = get_conn()
+    images = conn.list_images()
+    ret = {}
+    for img in images:
+        for attr in dir(img):
+            if attr.startswith('_'):
+                continue
+            ret[attr] = getattr(img, attr)
+    return ret
+
+
+def avail_sizes():
+    '''
+    Return a dict of all available vm images on the cloud provider with
+    relevant data
+    '''
+    conn = get_conn()
+    sizes = conn.list_sizes()
+    ret = {}
+    for size in sizes:
+        for attr in dir(size):
+            if attr.startswith('_'):
+                continue
+            ret[attr] = getattr(size, attr)
+    return ret
+
+
 def get_image(conn, vm_):
     '''
     Return the image object to use
