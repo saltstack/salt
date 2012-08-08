c475e1f334408c8e463fa2c514b6e53668ee86e8
Pedro Algarvio <pedro@algarvio.me>
2012-08-08 01:35:36 +0100

    One more try travis.

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -335,6 +335,7 @@ class ShellCase(TestCase):
         )
         data = process.communicate()
         process.stdout.close()
+        process.stderr.close()
         return data[0].split('\n')
 
     def run_salt(self, arg_str):
