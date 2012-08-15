d62f6852415478b80de2296c08c628e4167173aa
Thomas S Hatch <thatch45@gmail.com>
2012-08-14 23:22:40 -0600

    user access files need to be available

--- salt/master.py
+++ salt/master.py
@@ -123,6 +123,7 @@ class SMaster(object):
                 fp_.write(key)
             os.umask(cumask)
             os.chmod(keyfile, 256)
+            os.chown(user, -1)
             keys[user] = key
         return keys
 
