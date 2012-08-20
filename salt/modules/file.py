2f12e05b5983c65acec13aa912da6ec1c7cc8085
Thomas S Hatch <thatch45@gmail.com>
2012-08-20 12:32:06 -0600

    Handle error when None is passed

--- salt/modules/file.py
+++ salt/modules/file.py
@@ -14,6 +14,7 @@ import hashlib
 import shutil
 import stat
 import sys
+import getpass
 import fnmatch
 try:
     import grp
@@ -118,6 +119,8 @@ def user_to_uid(user):
 
         salt '*' file.user_to_uid root
     '''
+    if not user:
+        user = getpass.getuser()
     try:
         return pwd.getpwnam(user).pw_uid
     except KeyError:
