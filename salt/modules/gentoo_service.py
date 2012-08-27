9b514a3926786dd0f3cd2fb05de0454750cc0d6f
Thomas S Hatch <thatch45@gmail.com>
2012-08-27 13:25:11 -0600

    Add daemonize_if to gentoo service

--- salt/modules/gentoo_service.py
+++ salt/modules/gentoo_service.py
@@ -95,6 +95,8 @@ def restart(name):
 
         salt '*' service.restart <service name>
     '''
+    if name == 'salt-minion':
+        salt.utils.daemonize_if(__opts__)
     cmd = '/etc/init.d/{0} restart'.format(name)
     return not __salt__['cmd.retcode'](cmd)
 
