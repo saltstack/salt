0d43b4dd2f5a7fbc611f0fddef92e754f9c8bdab
Thomas S Hatch <thatch45@gmail.com>
2012-08-30 15:44:45 -0600

    Clean up cassandra virtual function

--- salt/modules/cassandra.py
+++ salt/modules/cassandra.py
@@ -15,9 +15,16 @@ REQUIREMENT 2:
 The python module, 'pycassa', also needs to be installed on the
 minion.
 '''
-
+# Import Python libs
 import logging
 log = logging.getLogger(__name__)
+
+load = False
+try:
+    from pycassa.system_manager import SystemManager
+    load = True
+except ImportError:
+    pass
 
 __outputter__ = {
   'compactionstats': 'txt',
@@ -29,15 +36,20 @@ __outputter__ = {
 
 
 def __virtual__():
+    '''
+    Only load if pycassa is available and the system is configured
+    '''
     global nt
     global host
     global thrift_port
+
+    if not load:
+        return False
 
     try:
         nt = __pillar__['cassandra.nodetool']
         host = __pillar__['cassandra.host']
         thrift_port = str(__pillar__['cassandra.thrift_port'])
-        from pycassa.system_manager import SystemManager
     except ImportError:
         #log.info('Module failed to load: pycassa is not installed')
         return False
@@ -61,7 +73,6 @@ def _sys_mgr():
     '''
     Return a pycassa system manager connection object
     '''
-    from pycassa.system_manager import SystemManager
     return SystemManager(host + ':' + thrift_port)
 
 
