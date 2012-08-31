755c42f8222ce13b7af10769ef839c87aec8d9a1
Tom Vaughan <tom@software6.net>
2012-08-31 10:09:41 -0700

    Fix #1772. Use `apt-cache policy` to find available package version.
    See also issue #1454: https://github.com/saltstack/salt/issues/1454

--- salt/modules/apt.py
+++ salt/modules/apt.py
@@ -43,15 +43,11 @@ def available_version(name):
         salt '*' pkg.available_version <package name>
     '''
     version = ''
-    cmd = 'apt-cache -q show {0} | grep ^Version'.format(name)
+    cmd = 'apt-cache -q policy {0} | grep Candidate'.format(name)
 
     out = __salt__['cmd.run_stdout'](cmd)
 
     version_list = out.split()
-    for comp in version_list:
-        if comp == 'Version:':
-            continue
-        return comp
 
     if len(version_list) >= 2:
         version = version_list[-1]
