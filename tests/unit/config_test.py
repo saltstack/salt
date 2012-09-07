4f9e62eaf4ef1c44389cf0d7d5acd06c25a51d9e
Pedro Algarvio <pedro@algarvio.me>
2012-09-07 18:04:14 +0100

    Include a test case. Fixes #1989.

--- tests/unit/config_test.py
+++ tests/unit/config_test.py
@@ -8,9 +8,11 @@
 """
 
 import os
+import shutil
 import tempfile
 from saltunittest import TestCase, TestLoader, TextTestRunner
 from salt import config as sconfig
+
 
 class ConfigTestCase(TestCase):
     def test_proper_path_joining(self):
@@ -24,6 +26,19 @@ class ConfigTestCase(TestCase):
         self.assertEqual(config['key_logfile'], os.path.join('/', 'key'))
         # os.sep.join behaviour
         self.assertNotEqual(config['key_logfile'], '//key')
+
+    def test_common_prefix_stripping(self):
+        tempdir = tempfile.mkdtemp()
+        root_dir = os.path.join(tempdir, 'foo', 'bar')
+        os.makedirs(root_dir)
+        fpath = os.path.join(root_dir, 'config')
+        open(fpath, 'w').write(
+            'root_dir: {0}\n'
+            'log_file: {1}\n'.format(root_dir, fpath)
+        )
+        config = sconfig.master_config(fpath)
+        self.assertEqual(config['log_file'], fpath)
+        shutil.rmtree(tempdir)
 
 
 if __name__ == "__main__":
