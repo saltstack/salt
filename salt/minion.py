924a78ff17ff5f7173d8ff5b40cfc35bda959069
Aaron Tygart <aaron.tygart@firehost.com>
2012-09-13 13:20:28 -0500

    Clean up initial logging messages
    Minion logging was producing duplicate "Starting Minion" entries when
    subprocesses were called on Windows. Startup messages moved to bootstrappers.

--- salt/minion.py
+++ salt/minion.py
@@ -132,12 +132,6 @@ class Minion(object):
         self.functions, self.returners = self.__load_modules()
         self.matcher = Matcher(self.opts, self.functions)
         self.proc_dir = get_proc_dir(opts['cachedir'])
-        if hasattr(self, '_syndic') and self._syndic:
-            log.warn(
-                'Starting the Salt Syndic Minion "{0}"'.format(self.opts['id'])
-            )
-        else:
-            log.warn('Starting the Salt Minion "{0}"'.format(self.opts['id']))
         self.authenticate()
         opts['pillar'] = salt.pillar.get_pillar(
             opts,
