a1f1e508899dd39b92e5bca7af4db73a48e1cd50
Adam Glenn <gekitsuu@gmail.com>
2012-09-12 21:30:13 -0400

    Moved docs from .rst file to docstring

--- salt/returners/mongo_return.py
+++ salt/returners/mongo_return.py
@@ -2,6 +2,18 @@
 Return data to a mongodb server
 
 Required python modules: pymongo
+
+
+This returner will send data from the minions to a MongoDB server. To 
+configure the settings for your MongoDB server, add the following lines
+to the minion config files::
+
+    mongo.db: <database name>
+    mongo.host: <server ip address>
+    mongo.user: <MongoDB username>
+    mongo.password: <MongoDB user password>
+    mongo.port: 27017
+
 '''
 
 import logging
