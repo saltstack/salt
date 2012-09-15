815a1bcaa78c79efcc65812ec67f93bff61d158f
Scott Walton <s@cott.me.uk>
2012-09-15 18:41:01 +0100

    Added test and code for start #2026

--- salt/modules/supervisord.py
+++ salt/modules/supervisord.py
@@ -1,6 +1,7 @@
 '''
 Provide the service module for supervisord
 '''
+
 from salt import exceptions, utils
 
 
@@ -10,20 +11,32 @@ def __virtual__():
     '''
     try:
         utils.check_or_die('supervisorctl')
-        return 'supervisor'
     except exceptions.CommandNotFoundError:
         return False
+
+    return 'supervisord'
 
 
 def _ctl_cmd(cmd, name):
     return 'supervisorctl {cmd} {name}'.format(
         cmd=cmd, name=name)
 
-def restart(name):
+
+def start(name='all'):
     '''
-    Restart the named services.
+    Start the named service
 
     CLI Example::
-        salt '*' supervisord.restart <service1>
+        salt '*' supervisord.start <service>
+    '''
+    return __salt__['cmd.run_all'](_ctl_cmd('start', name))
+
+
+def restart(name):
+    '''
+    Restart the named service.
+
+    CLI Example::
+        salt '*' supervisord.restart <service>
     '''
     return not __salt__['cmd.retcode'](_ctl_cmd('restart', name))
