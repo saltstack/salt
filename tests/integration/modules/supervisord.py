9e35b770388592297eeeec9771916d0a035f86b2
Scott Walton <s@cott.me.uk>
2012-09-15 19:02:00 +0100

    Added stop and status

--- tests/integration/modules/supervisord.py
+++ tests/integration/modules/supervisord.py
@@ -1,4 +1,3 @@
-import os
 import integration
 
 
@@ -29,5 +28,53 @@ class SupervisordModuleTest(integration.ModuleCase):
         Start a specific service.
         '''
         ret = self.run_function('supervisord.start', ['null_service'])
+        self.assertEqual(len(list(ret.items())), 4)
+        self.assertEqual(ret['retcode'], 0)
+
+    def test_restart_all(self):
+        '''
+        Restart all services
+        '''
+        ret = self.run_function('supervisord.restart', [])
+        self.assertEqual(len(list(ret.items())), 4)
+        self.assertEqual(ret['retcode'], 0)
+
+    def test_restart_one(self):
+        '''
+        Restart a specific service.
+        '''
+        ret = self.run_function('supervisord.restart', ['null_service'])
+        self.assertEqual(len(list(ret.items())), 4)
+        self.assertEqual(ret['retcode'], 0)
+
+    def test_stop_all(self):
+        '''
+        stop all services
+        '''
+        ret = self.run_function('supervisord.stop', [])
+        self.assertEqual(len(list(ret.items())), 4)
+        self.assertEqual(ret['retcode'], 0)
+
+    def test_stop_one(self):
+        '''
+        stop a specific service.
+        '''
+        ret = self.run_function('supervisord.stop', ['null_service'])
+        self.assertEqual(len(list(ret.items())), 4)
+        self.assertEqual(ret['retcode'], 0)
+
+    def test_status_all(self):
+        '''
+        status all services
+        '''
+        ret = self.run_function('supervisord.status', [])
+        self.assertEqual(len(list(ret.items())), 4)
+        self.assertEqual(ret['retcode'], 0)
+
+    def test_status_one(self):
+        '''
+        status a specific service.
+        '''
+        ret = self.run_function('supervisord.status', ['null_service'])
         self.assertEqual(len(list(ret.items())), 4)
         self.assertEqual(ret['retcode'], 0)
