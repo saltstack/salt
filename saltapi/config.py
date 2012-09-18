bb0adb67b05676fc01933156ecb38cbdcae08b9a
Thomas S Hatch <thatch45@gmail.com>
2012-09-18 17:56:29 -0600

    Add config.py

---
+++ saltapi/config.py
@@ -0,0 +1,24 @@
+'''
+Manage configuration files in salt-cloud
+'''
+
+# Import python libs
+import os
+
+# Import salt libs
+import salt.config
+
+
+def api_config(path):
+    '''
+    Read in the salt master config file and add additional configs that
+    need to be stubbed out for cloudapi
+    '''
+    opts = {}
+
+    salt.config.load_config(opts, path, 'SALT_MASTER_CONFIG')
+
+    if 'include' in opts:
+        opts = salt.config.include_config(opts, path)
+
+    return opts
