7ab650ff4180ce263d789d2266391a93daa7298f
Thomas S Hatch <thatch45@gmail.com>
2012-09-24 15:10:44 -0600

    Use master_config function

--- saltapi/config.py
+++ saltapi/config.py
@@ -14,11 +14,9 @@ def api_config(path):
     Read in the salt master config file and add additional configs that
     need to be stubbed out for cloudapi
     '''
-    opts = {
-            'extension_modules': [],
-            }
+    opts = {}
 
-    salt.config.load_config(opts, path, 'SALT_MASTER_CONFIG')
+    opts = salt.config.master_config(path)
 
     if 'include' in opts:
         opts = salt.config.include_config(opts, path)
