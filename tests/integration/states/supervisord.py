ec2486c9d83787d7b17cb816f965341a5cd8cccc
Pedro Algarvio <pedro@algarvio.me>
2012-09-25 22:32:42 +0100

    Only test supervisor if conditions are proper. Refs #2026.
    * Only the root user can run the supervisor state tests.
    * Install supervisor on the travis-ci machine to run the tests.

--- tests/integration/states/supervisord.py
+++ tests/integration/states/supervisord.py
@@ -1,6 +1,7 @@
 '''
 Tests for the supervisord state
 '''
+import os
 import integration
 
 
@@ -8,6 +9,14 @@ class SupervisordTest(integration.ModuleCase):
     '''
     Validate the supervisord states.
     '''
+    def setUp(self):
+        super(SupervisordTest, self).setUp()
+        ret = self.run_function('cmd.has_exec', ['supervisorctl'])
+        if not ret:
+            self.skipTest('supervisor not installed')
+        if os.geteuid() != 0:
+            self.skipTest('you must be this root to run this test')
+
     def test_start(self):
         '''
         supervisord.running restart = False
