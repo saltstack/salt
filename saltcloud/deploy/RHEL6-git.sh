1534e7ef0f968222bc72b03347d9e496866b864f
Thomas S Hatch <thatch45@gmail.com>
2012-09-27 16:24:09 -0600

    Add git install deploy script for RHEL6

---
+++ saltcloud/deploy/RHEL6-git.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rpm -Uvh --force http://mirrors.kernel.org/fedora-epel/6/x86_64/epel-release-6-7.noarch.rpm
+yum -y install salt-minion git --enablerepo epel-testing
+rm -rf /usr/lib/python/site-packages/salt*
+rm -rf /usr/bin/salt-*
+mkdir -p /root/git
+cd /root/git
+git clone git://github.com/saltstack/salt.git
+cd salt
+python setup.py install
+cd
+mkdir -p /etc/salt/pki
+echo '{{ vm['priv_key'] }}' > /etc/salt/pki/minion.pem
+echo '{{ vm['pub_key'] }}' > /etc/salt/pki/minion.pub
+echo '{{ minion }}' > /etc/salt/minion
+/sbin/chkconfig salt-minion on
+service salt-minion start
