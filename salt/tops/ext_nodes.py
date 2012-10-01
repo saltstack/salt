e49c241e04a44b763b34f1d21c7b20068788f9d3
Thomas S Hatch <thatch45@gmail.com>
2012-09-30 23:50:35 -0600

    Add ext_nodes master tops

---
+++ salt/tops/ext_nodes.py
@@ -0,0 +1,46 @@
+'''
+The cmd_yaml replaces the original ext_nodes function
+'''
+
+# Import third party libs
+import yaml
+
+# Import Salt libs
+import salt.utils
+
+def __virtual__():
+    '''
+    Only run if properly configured
+    '''
+    if __opts__['master_tops'].get('ext_nodes'):
+        return 'ext_nodes'
+    return False
+
+
+def top(**kwargs):
+    '''
+    Run the command configured
+    '''
+    if not 'id' in kwargs:
+        return {}
+    if not salt.utils.which(__opts__['master_tops']['ext_nodes']):
+        log.error(('Specified external nodes controller {0} is not'
+                   ' available, please verify that it is installed'
+                   '').format(__opts__['master_tops']['ext_nodes']))
+        return {}
+    cmd = '{0} {1}'.format(__opts__['master_tops']['ext_nodes'], kwargs['id'])
+    ndata = yaml.safe_load(__salt__['cmd.run'](cmd))
+    ret = {}
+    if 'environment' in ndata:
+        env = ndata['environment']
+    else:
+        env = 'base'
+
+    if 'classes' in ndata:
+        if isinstance(ndata['classes'], dict):
+            ret[env] = list(ndata['classes'])
+        elif isinstance(ndata['classes'], list):
+            ret[env] = ndata['classes']
+        else:
+            return ret
+    return ret 
