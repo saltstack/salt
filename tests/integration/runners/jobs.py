bd041c31f69fa8872089619d5d846cdfb74e8ab1
Thomas S Hatch <thatch45@gmail.com>
2012-10-06 23:39:25 -0600

    sp

--- tests/integration/runners/jobs.py
+++ tests/integration/runners/jobs.py
@@ -27,7 +27,7 @@ class ManageTest(integration.ShellCase):
         jobs.lookup_jid
         '''
         ret = self.run_run_plus('jobs.lookup_jid', '', '23974239742394')
-        self.assertIsInstatnce(ret['fun'], type)
+        self.assertIsInstance(ret['fun'], type)
         self.assertFalse(ret['out'][1])
 
     def test_list_jobs(self):
