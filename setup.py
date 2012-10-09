7d97e91e8240ea67bc2e8330ec024ea5faad15bf
Seth House <seth@eseth.com>
2012-10-09 17:13:47 -0600

    Cleaned up setup.py

--- setup.py
+++ setup.py
@@ -2,48 +2,46 @@
 '''
 The setup script for saltapi
 '''
-
-import os
 from distutils.core import setup
 
-if os.environ.get('VIRTUAL_ENV'):
-    from setuptools import setup
-
-exec(compile(
-    open("saltapi/version.py").read(), "saltapi/version.py", 'exec')
-    )
+# pylint: disable-msg=W0122,E0602
+exec(compile(open('saltapi/version.py').read(), 'saltapi/version.py', 'exec'))
+VERSION = __version__
+# pylint: enable-msg=W0122,E0602
 
 NAME = 'salt-api'
-VER = __version__
-DESC = ('Generic interface for providing external access apis to Salt')
+DESC = ("Generic interface for providing external access APIs to Salt")
 
+# Specify the test suite for < 2.7
+try:
+    import unittest2
+except ImportError:
+    pass
 
 setup(
-      name=NAME,
-      version=VER,
-      description=DESC,
-      author='Thomas S Hatch',
-      author_email='thatch@saltstack.com',
-      url='http://saltstack.org',
-      classifiers=[
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2.6',
-          'Programming Language :: Python :: 2.7',
-          'Development Status :: 2 - Pre-Alpha',
-          'Environment :: Console',
-          'Intended Audience :: Developers',
-          'Intended Audience :: Information Technology',
-          'Intended Audience :: System Administrators',
-          'License :: OSI Approved :: Apache Software License',
-          'Operating System :: POSIX :: Linux',
-          'Topic :: System :: Distributed Computing',
-          ],
-      packages=['saltapi',
-                'saltapi/netapi',
-                ],
-      data_files=[('share/man/man1',
-                     ['doc/man/salt-api.1']),
-                     ('share/man/man7',
-                     ['doc/man/salt-api.7'])],
-      scripts=['scripts/salt-api'],
-     )
+    name=NAME,
+    version=VERSION,
+    description=DESC,
+    author='Thomas S Hatch',
+    author_email='thatch@saltstack.com',
+    url='http://saltstack.org',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: System :: Distributed Computing'],
+    packages=['saltapi',
+        'saltapi/netapi'],
+    data_files=[('share/man/man1',
+        ['doc/man/salt-api.1']),
+        ('share/man/man7',
+        ['doc/man/salt-api.7'])],
+    scripts=['scripts/salt-api'],
+    test_suite='unittest2.collector' if 'unittest2' in locals() else None)
