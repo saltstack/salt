saltstack/salt

commit 98090c306788ea9fbc53b5243413bdb1aa7066f5

Fix #1790 by moving generic sanitize_host function into shared lib

salt/modules/network.py
@@ -28,15 +28,6 @@ def __virtual__():
     return 'network'
 
 
-def _sanitize_host(host):
-    '''
-    Sanitize host string.
-    '''
-    return ''.join([
-        c for c in host[0:255] if c in (ascii_letters + digits + '.-')
-    ])
-
-
 def _cidr_to_ipv4_netmask(cidr_bits):
     '''
     Returns an IPv4 netmask

salt/utils/socket_util.py
@@ -1,5 +1,13 @@
 import socket
 
+def _sanitize_host(host):
+    '''
+    Sanitize host string.
+    '''
+    return ''.join([
+        c for c in host[0:255] if c in (ascii_letters + digits + '.-')
+    ])
+
 def isportopen(host, port):
     '''
     Return status of a port

commit bf0609fa4e0e02632136ed78c46f73be671c3f4e

Fixed regression from 98090c3; made sanitize_host not private
Fixes #2274

salt/modules/network.py
@@ -349,7 +349,7 @@ def ping(host):
 
         salt '*' network.ping archlinux.org
     '''
-    cmd = 'ping -c 4 {0}'.format(_sanitize_host(host))
+    cmd = 'ping -c 4 {0}'.format(sanitize_host(host))
     return __salt__['cmd.run'](cmd)
 
 
@@ -402,7 +402,7 @@ def traceroute(host):
         salt '*' network.traceroute archlinux.org
     '''
     ret = []
-    cmd = 'traceroute {0}'.format(_sanitize_host(host))
+    cmd = 'traceroute {0}'.format(sanitize_host(host))
     out = __salt__['cmd.run'](cmd)
 
     for line in out:
@@ -433,5 +433,5 @@ def dig(host):
 
         salt '*' network.dig archlinux.org
     '''
-    cmd = 'dig {0}'.format(_sanitize_host(host))
+    cmd = 'dig {0}'.format(sanitize_host(host))
     return __salt__['cmd.run'](cmd)

salt/modules/win_network.py
@@ -24,15 +24,6 @@ def __virtual__():
     return False
 
 
-def _sanitize_host(host):
-    '''
-    Sanitize host string.
-    '''
-    return ''.join([
-        c for c in host[0:255] if c in (ascii_letters + digits + '.-')
-    ])
-
-
 def ping(host):
     '''
     Performs a ping to a host
@@ -41,7 +32,7 @@ def ping(host):
 
         salt '*' network.ping archlinux.org
     '''
-    cmd = 'ping -n 4 {0}'.format(_sanitize_host(host))
+    cmd = 'ping -n 4 {0}'.format(sanitize_host(host))
     return __salt__['cmd.run'](cmd)
 
 
@@ -82,7 +73,7 @@ def traceroute(host):
         salt '*' network.traceroute archlinux.org
     '''
     ret = []
-    cmd = 'tracert {0}'.format(_sanitize_host(host))
+    cmd = 'tracert {0}'.format(sanitize_host(host))
     lines = __salt__['cmd.run'](cmd).split('\n')
     for line in lines:
         if not ' ' in line:
@@ -134,7 +125,7 @@ def nslookup(host):
         salt '*' network.nslookup archlinux.org
     '''
     ret = []
-    cmd = 'nslookup {0}'.format(_sanitize_host(host))
+    cmd = 'nslookup {0}'.format(sanitize_host(host))
     lines = __salt__['cmd.run'](cmd).split('\n')
     for line in lines:
         if line.startswith('Non-authoritative'):
@@ -155,7 +146,7 @@ def dig(host):
 
         salt '*' network.dig archlinux.org
     '''
-    cmd = 'dig {0}'.format(_sanitize_host(host))
+    cmd = 'dig {0}'.format(sanitize_host(host))
     return __salt__['cmd.run'](cmd)
 
 

salt/utils/socket_util.py
@@ -5,7 +5,7 @@ Define some generic socket functions for network modules
 import socket
 from string import ascii_letters, digits
 
-def _sanitize_host(host):
+def sanitize_host(host):
     '''
     Sanitize host string.
     '''
@@ -27,7 +27,7 @@ def isportopen(host, port):
         return False
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    out = sock.connect_ex((_sanitize_host(host), int(port)))
+    out = sock.connect_ex((sanitize_host(host), int(port)))
 
     return out
 