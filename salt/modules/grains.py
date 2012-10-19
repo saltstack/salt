76df64ae8065a18a1d35b2f84e728f4fe527dd77
Aaron Tygart <aaron.tygart@firehost.com>
2012-10-19 13:59:49 -0500

    Add 'sanitize' option for grains module

--- salt/modules/grains.py
+++ salt/modules/grains.py
@@ -12,26 +12,58 @@ __outputter__ = {
 }
 
 
-def items():
+def _str_sanitizer(instr):
+    return "{0}{1}".format(instr[:-4], 'X' * 4)
+
+# A dictionary of grain -> function mappings for sanitizing grain output. This
+# is used when the 'sanitize' flag is given.
+_sanitizers = {
+    'serialnumber': _str_sanitizer,
+    'domain': lambda x: 'domain',
+    'fqdn': lambda x: 'minion.domain',
+    'host': lambda x: 'minion',
+    'id': lambda x: 'minion.domain',
+}
+
+
+def items(sanitize=False):
     '''
     Return the grains data
 
     CLI Example::
 
         salt '*' grains.items
+
+    Sanitized CLI output::
+
+        salt '*' grains.items sanitize=True
     '''
-    return __grains__
+    if sanitize:
+        out = dict(__grains__)
+        for (k, f) in _sanitizers.items():
+            if k in out:
+                out[k] = f(out[k])
+        return out
+    else:
+        return __grains__
 
 
-def item(key=None):
+def item(key=None, sanitize=False):
     '''
     Return a singe component of the grains data
 
     CLI Example::
 
         salt '*' grains.item os
+
+    Sanitized CLI output::
+
+        salt '*' grains.items serialnumber sanitize=True
     '''
-    return __grains__.get(key, '')
+    if sanitize and key in _sanitizers:
+        return _sanitizers[key](_grains__.get(key, ''))
+    else:
+        return __grains__.get(key, '')
 
 
 def ls():
