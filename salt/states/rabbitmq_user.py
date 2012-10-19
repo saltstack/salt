7d935a84556593e00a0e4a74eddc4281fc5bbbc0
Scott Walton <scw@talktopebble.co.uk>
2012-10-19 11:42:01 +0100

    Added test framework for rabbitmq user #962

--- salt/states/rabbitmq_user.py
+++ salt/states/rabbitmq_user.py
@@ -3,6 +3,8 @@ Manage RabbitMQ Users.
 '''
 import logging
 
+log = logging.getLogger(__name__)
+
 
 def __virtual__():
     '''
@@ -41,25 +43,42 @@ def exists(name,
             user_exists = True
             break
 
-    if not user_exists:
-        result = __salt__['rabbitmq.add_user'](name, password, user=runas)
-    elif force:
-        if password is not None:
-            result = __salt__['rabbitmq.change_password'](
-                name, password, user=runas)
+    if __opts__['test']:
+        ret['result'] = None
+        if not user_exists:
+            ret['comment'] = 'User {0} is set to be created'
+        elif force:
+            ret['comment'] = 'User {0} is set to be updated'
         else:
-            result = __salt__['rabbitmq.clear_password'](name, user=runas)
+            ret['comment'] = 'User {0} is not going to be modified'
+        ret['comment'] = ret['comment'].format(name)
     else:
-        result = {'Error': 'User {0} exists and force is not set'.format(name)}
+        if not user_exists:
+            log.debug(
+                "User doesn't exist - Creating'")
+            result = __salt__['rabbitmq.add_user'](name, password, user=runas)
+        elif force:
+            log.debug('User exists and force is set - Overriding password')
+            if password is not None:
+                result = __salt__['rabbitmq.change_password'](
+                    name, password, user=runas)
+            else:
+                log.debug('Password is not set - Clearing password')
+                result = __salt__['rabbitmq.clear_password'](name, user=runas)
+        else:
+            log.debug('User exists, and force is not set - Abandoning')
+            result = {
+                'Error': 'User {0} exists and force is not set'.format(name)
+            }
 
-    if 'Error' in result:
-        ret['result'] = False
-        ret['comment'] = result['Error']
-    elif 'Added' in result:
-        ret['comment'] = result['Added']
-    elif 'Password Changed' in result:
-        ret['comment'] = result['Password Changed']
-    elif 'Password Cleared' in result:
-        ret['comment'] = result['Password Cleared']
+        if 'Error' in result:
+            ret['result'] = False
+            ret['comment'] = result['Error']
+        elif 'Added' in result:
+            ret['comment'] = result['Added']
+        elif 'Password Changed' in result:
+            ret['comment'] = result['Password Changed']
+        elif 'Password Cleared' in result:
+            ret['comment'] = result['Password Cleared']
 
     return ret
