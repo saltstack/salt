340616a379fde9f1e2b44764abfb3194150cd60a
Scott Walton <scw@talktopebble.co.uk>
2012-10-19 11:42:01 +0100

    Added test framework for rabbitmq user #962

---
+++ tests/integration/states/rabbitmq_user.py
@@ -0,0 +1,27 @@
+'''
+Tests for the rabbitmq state
+'''
+import os
+import integration
+
+
+class RabbitUserTestCase(integration.ModuleCase):
+    '''
+    Validate the rabbitmq states.
+    '''
+    def setUp(self):
+        super(RabbitUserTestCase, self).setUp()
+        rabbit_installed = self.run_function('cmd.has_exec', ['rabbitmqctl'])
+
+        if not rabbit_installed:
+            self.skipTest('rabbitmq-server not installed')
+        if os.geteuid() != 0:
+            self.skipTest('You must be root to run this test')
+
+    def test_add_user(self):
+        '''
+        '''
+        ret = self.run_state('rabbitmq_user.exists', name='null_name')
+
+        self.assertTrue(ret)
+        self.assertEqual(ret.items()[0][1]['result'], False)
