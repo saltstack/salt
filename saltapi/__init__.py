bc3d4927917dd121f453bd2a6831e91608630f54
Thomas S Hatch <thatch45@gmail.com>
2012-10-24 10:26:38 -0600

    port format_all to salt-api pending updates in salt core

--- saltapi/__init__.py
+++ saltapi/__init__.py
@@ -22,7 +22,7 @@ class API(object):
         if not 'client' in low:
             raise SaltException('No client specified')
         l_fun = getattr(self, low['client'])
-        fcall = salt.utils.format_call(l_fun, low)
+        fcall = format_call(l_fun, low)
         if 'kwargs' in fcall:
             ret = l_fun(*fcall['args'], **fcall['kwargs'])
         else:
@@ -55,3 +55,62 @@ class API(object):
         runner = salt.runner.RunnerClient(opts)
         return salt.runner.low(fun, kwargs)
         
+### Remove when salt 0.10.5 is released!
+def _getargs(func):
+    '''
+    A small wrapper around getargspec that also supports callable classes
+    '''
+    if not callable(func):
+        raise TypeError('{0} is not a callable'.format(func))
+
+    if inspect.isfunction(func):
+        aspec = inspect.getargspec(func)
+    elif inspect.ismethod(func):
+        aspec = inspect.getargspec(func)
+        del aspec.args[0] # self
+    elif isinstance(func, object):
+        aspec = inspect.getargspec(func.__call__)
+        del aspec.args[0]  # self
+    else:
+        raise TypeError("Cannot inspect argument list for '{0}'".format(func))
+
+    return aspec
+
+
+def format_call(fun, data):
+    '''
+    Pass in a function and a dict containing arguments to the function.
+
+    A dict with the keys args and kwargs is returned
+    '''
+    ret = {}
+    ret['args'] = []
+    aspec = _getargs(fun)
+    arglen = 0
+    deflen = 0
+    if isinstance(aspec[0], list):
+        arglen = len(aspec[0])
+    if isinstance(aspec[3], tuple):
+        deflen = len(aspec[3])
+    if aspec[2]:
+        # This state accepts kwargs
+        ret['kwargs'] = {}
+        for key in data:
+            # Passing kwargs the conflict with args == stack trace
+            if key in aspec[0]:
+                continue
+            ret['kwargs'][key] = data[key]
+    kwargs = {}
+    for ind in range(arglen - 1, 0, -1):
+        minus = arglen - ind
+        if deflen - minus > -1:
+            kwargs[aspec[0][ind]] = aspec[3][-minus]
+    for arg in kwargs:
+        if arg in data:
+            kwargs[arg] = data[arg]
+    for arg in aspec[0]:
+        if arg in kwargs:
+            ret['args'].append(kwargs[arg])
+        else:
+            ret['args'].append(data[arg])
+    return ret
