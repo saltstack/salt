36175303fcf27639c9f767ed274a4afd5ea90dc4
Thomas S Hatch <thatch45@gmail.com>
2012-10-25 17:45:17 -0600

    Gonna use a class for the wheel system

--- salt/wheel/__init__.py
+++ salt/wheel/__init__.py
@@ -4,16 +4,37 @@ Modules used to control the master itself
 
 # Import salt libs
 import salt.loader
+import salt.payload
 import salt.utils
 
 
-def call_func(mod, fun, opts, **kwargs):
+class Wheel(object):
     '''
-    Execute a master control function
+    Manage calls to the salt wheel system
     '''
-    func = '{0}.{1}'.format(mod, fun)
-    w_funcs = salt.loader.wheels(opts)
-    if not func in w_funcs:
-        return 'Unknown wheel function'
-    f_call = salt.utils.format_call(w_funcs[func], kwargs)
-    return w_funcs[func](*f_call.get('args', ()), **f_call.get('kwargs', {}))
+    def __init__(self, opts):
+        self.opts = opts
+        self.w_funcs = salt.loader.wheels(opts)
+
+    def call_func(self, mod, fun, opts, **kwargs):
+        '''
+        Execute a master control function
+        '''
+        func = '{0}.{1}'.format(mod, fun)
+        if not func in self.w_funcs:
+            return 'Unknown wheel function'
+        f_call = salt.utils.format_call(self.w_funcs[func], kwargs)
+        return self.w_funcs[func](*f_call.get('args', ()), **f_call.get('kwargs', {}))
+
+    def master_call(self, mod, fun, **kwargs):
+        '''
+        Send a function call to a wheel module through the master network interface
+        '''
+        load = kwargs
+        load['cmd'] = 'wheel'
+        load['mod'] = mod
+        load['fun'] = fun
+        sreq = salt.payload.SREQ(
+                'tcp://{0[interface]}:{0[ret_port]}'.format(self.opts),
+                )
+        return sreq.send('clear', load)
