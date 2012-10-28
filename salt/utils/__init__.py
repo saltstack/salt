244bd0fa8324a3c3c915b63f00a3b1a4102ba595
Thomas S Hatch <thatch45@gmail.com>
2012-10-27 23:33:22 -0600

    Add function to detect if a file is text or binary

--- salt/utils/__init__.py
+++ salt/utils/__init__.py
@@ -629,3 +629,27 @@ def arg_lookup(fun):
             ret['args'].append(arg)
     return ret
 
+
+def istextfile(fp_, blocksize=512):
+    '''
+    Uses heuristics to guess whether the given file is text or binary,
+    by reading a single block of bytes from the file.
+    If more than 30% of the chars in the block are non-text, or there
+    are NUL ('\x00') bytes in the block, assume this is a binary file.
+    '''
+    PY3 = sys.version_info[0] == 3
+    int2byte = (lambda x: bytes((x,))) if PY3 else chr
+    text_characters = (
+        b''.join(int2byte(i) for i in range(32, 127)) +
+        b'\n\r\t\f\b')
+    block = fp_.read(blocksize)
+    if b'\x00' in block:
+        # Files with null bytes are binary
+        return False
+    elif not block:
+        # An empty file is considered a valid text file
+        return True
+
+    nontext = block.translate(None, text_characters)
+    return float(len(nontext)) / len(block) <= 0.30
+
