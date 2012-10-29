8d8c424feb7ca98a9df10159c86d5002b60ca137
K Jonathan Harker <jesusaurus@inbox.com>
2012-10-29 21:33:58 +0000

    Make the openstack provider sudo-aware

--- saltcloud/utils/__init__.py
+++ saltcloud/utils/__init__.py
@@ -206,7 +206,7 @@ def wait_for_passwd(host, port=22, timeout=900, username='root',
 
 def deploy_script(host, port=22, timeout=900, username='root',
                   password=None, key_filename=None, script=None,
-                  deploy_command='bash /tmp/deploy.sh', tty=None,
+                  deploy_command='/tmp/deploy.sh', sudo=False, tty=None,
                   name=None, pub_key=None, sock_dir=None):
     '''
     Copy a deploy script to a remote server, execute it, and remove it
@@ -240,6 +240,9 @@ def deploy_script(host, port=22, timeout=900, username='root',
             sftp.put(tmppath, '/tmp/deploy.sh')
             os.remove(tmppath)
             ssh.exec_command('chmod +x /tmp/deploy.sh')
+
+            if sudo:
+                deploy_command = 'sudo ' + deploy_command
 
             newtimeout = timeout - (time.mktime(time.localtime()) - starttime)
             queue = multiprocessing.Queue()
