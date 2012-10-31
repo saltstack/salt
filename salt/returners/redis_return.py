fe6f9be7bafe29068493b71d79130958b4b926d5
Thomas S Hatch <thatch45@gmail.com>
2012-10-31 14:45:18 -0600

    bump the redis returner to use the new config module

--- salt/returners/redis_return.py
+++ salt/returners/redis_return.py
@@ -1,11 +1,16 @@
 '''
 Return data to a redis server
-This is a VERY simple example for pushing data to a redis server and is not
-necessarily intended as a usable interface.
 
-Required python modules: redis
+To enable this returner the minion will need the python client for redis
+installed and the following values configured in the minion or master
+config, these are the defaults:
+
+    redis.db: '0'
+    redis.host: 'salt'
+    redis.port: 6379
 '''
 
+# Import python libs
 import json
 
 try:
@@ -14,10 +19,6 @@ try:
 except ImportError:
     has_redis = False
 
-__opts__ = {'redis.db': '0',
-            'redis.host': 'redis',
-            'redis.port': 6379}
-
 
 def __virtual__():
     if not has_redis:
@@ -25,14 +26,22 @@ def __virtual__():
     return 'redis_return'
 
 
+def _get_serv():
+    '''
+    Return a redis server object
+    '''
+    return redis.Redis(
+            host=__salt__['config.option']('redis.host'),
+            port=__salt__['config.option']('redis.port'),
+            db=__salt__['config.option']('redis.db'))
+
+
+
 def returner(ret):
     '''
     Return data to a redis data store
     '''
-    serv = redis.Redis(host=__opts__['redis.host'],
-                       port=__opts__['redis.port'],
-                       db=__opts__['redis.db'])
-
+    serv = _get_serv()
     serv.sadd('{0}:jobs'.format(ret['id']))
     serv.set('{0}:{1}'.format(ret['jid'], json.dumps(ret['return'])))
     serv.sadd('jobs', ret['jid'])
