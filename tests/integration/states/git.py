7625ebe42976ab14e6f2053a7291df8adbdb8005
ffa <adam.ffa@gmail.com>
2012-10-31 22:41:43 -0700

    add integration test to test failure

--- tests/integration/states/git.py
+++ tests/integration/states/git.py
@@ -28,6 +28,23 @@ class GitTest(integration.ModuleCase):
         self.assertTrue(result)
         shutil.rmtree(name, ignore_errors=True)
 
+    def test_latest_failure(self):
+        '''
+        git.latest
+        '''
+        name = os.path.join(integration.TMP, 'salt_repo')
+        ret = self.run_state(
+            'git.latest',
+            name='https://youSpelledGithubWrong.com/saltstack/salt.git',
+            rev='develop',
+            target=name,
+            submodules=True
+        )
+        self.assertFalse(os.path.isdir(os.path.join(name, '.git')))
+        result = self.state_result(ret)
+        self.assertFalse(result)
+        shutil.rmtree(name, ignore_errors=True)
+
     def test_present(self):
         '''
         git.present
@@ -42,6 +59,24 @@ class GitTest(integration.ModuleCase):
         result = self.state_result(ret)
         self.assertTrue(result)
         shutil.rmtree(name, ignore_errors=True)
+
+    def test_present_failure(self):
+        '''
+        git.present
+        '''
+        name = os.path.join(integration.TMP, 'salt_repo')
+        with file(fname, 'a'):
+            pass
+        ret = self.run_state(
+            'git.present',
+            name=name,
+            bare=True
+        )
+        self.assertFalse(os.path.isfile(os.path.join(name, 'HEAD')))
+        result = self.state_result(ret)
+        self.assertFalse(result)
+        shutil.rmtree(name, ignore_errors=True)
+
 
 if __name__ == '__main__':
     from integration import run_tests
