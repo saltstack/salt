6448dd87dadd5f120e4e14c53082ddf657f57129
Pedro Algarvio <pedro@algarvio.me>
2012-11-04 09:58:05 +0000

    Some fixes on the git state tests.
    * Always remove the repository directory.
    * When testing recursive with git 1.7.9.5, we cannot test for a directory because it's a plain file with the contents:
      gitdir: /tmp/salt-tests-tmpdir/salt_repo/.git/modules/vendor/modules/js/receiptverifier

--- tests/integration/states/git.py
+++ tests/integration/states/git.py
@@ -16,118 +16,149 @@ class GitTest(integration.ModuleCase):
         git.latest
         '''
         name = os.path.join(integration.TMP, 'salt_repo')
-        ret = self.run_state(
-            'git.latest',
-            name='https://github.com/saltstack/salt.git',
-            rev='develop',
-            target=name,
-            submodules=True
-        )
-        self.assertTrue(os.path.isdir(os.path.join(name, '.git')))
-        result = self.state_result(ret)
-        self.assertTrue(result)
-        shutil.rmtree(name, ignore_errors=True)
+        try:
+            ret = self.run_state(
+                'git.latest',
+                name='https://github.com/saltstack/salt.git',
+                rev='develop',
+                target=name,
+                submodules=True
+            )
+            self.assertTrue(os.path.isdir(os.path.join(name, '.git')))
+            result = self.state_result(ret)
+            self.assertTrue(result)
+        finally:
+            shutil.rmtree(name, ignore_errors=True)
 
     def test_latest_failure(self):
         '''
         git.latest
         '''
         name = os.path.join(integration.TMP, 'salt_repo')
-        ret = self.run_state(
-            'git.latest',
-            name='https://youSpelledGithubWrong.com/saltstack/salt.git',
-            rev='develop',
-            target=name,
-            submodules=True
-        )
-        self.assertFalse(os.path.isdir(os.path.join(name, '.git')))
-        result = self.state_result(ret)
-        self.assertFalse(result)
-        shutil.rmtree(name, ignore_errors=True)
+        try:
+            ret = self.run_state(
+                'git.latest',
+                name='https://youSpelledGithubWrong.com/saltstack/salt.git',
+                rev='develop',
+                target=name,
+                submodules=True
+            )
+            self.assertFalse(os.path.isdir(os.path.join(name, '.git')))
+            result = self.state_result(ret)
+            self.assertFalse(result)
+        finally:
+            shutil.rmtree(name, ignore_errors=True)
 
     def test_latest_empty_dir(self):
         '''
         git.latest
         '''
         name = os.path.join(integration.TMP, 'salt_repo')
-        os.mkdir(name)
-        ret = self.run_state(
-            'git.latest',
-            name='https://github.com/saltstack/salt.git',
-            rev='develop',
-            target=name,
-            submodules=True
-        )
-        self.assertTrue(os.path.isdir(os.path.join(name, '.git')))
-        result = self.state_result(ret)
-        self.assertTrue(result)
-        shutil.rmtree(name, ignore_errors=True)
+        if not os.path.isdir(name):
+            os.mkdir(name)
+        try:
+            ret = self.run_state(
+                'git.latest',
+                name='https://github.com/saltstack/salt.git',
+                rev='develop',
+                target=name,
+                submodules=True
+            )
+            self.assertTrue(os.path.isdir(os.path.join(name, '.git')))
+            result = self.state_result(ret)
+            self.assertTrue(result)
+        finally:
+            shutil.rmtree(name, ignore_errors=True)
 
     def test_latest_recursive(self):
         '''
         git.latest
         '''
         name = os.path.join(integration.TMP, 'salt_repo')
-        ret = self.run_state(
-            'git.latest',
-            name='https://github.com/mozilla/zamboni.git',
-            target=name,
-            submodules=True
-        )
-        self.assertTrue(os.path.isdir(os.path.join(name, 'vendor', 'js', 'receiptverifier', '.git')))
-        result = self.state_result(ret)
-        self.assertTrue(result)
-        shutil.rmtree(name, ignore_errors=True)
+        try:
+            ret = self.run_state(
+                'git.latest',
+                name='https://github.com/mozilla/zamboni.git',
+                target=name,
+                submodules=True
+            )
+            self.assertTrue(
+                # with git 1.7.9.5, it's not a directory, it's a file with the
+                # contents:
+                #   gitdir: /tmp/salt-tests-tmpdir/salt_repo/.git/modules/vendor/modules/js/receiptverifier
+                #
+                # let's change it to exists!?!?!?
+                #
+                #os.path.isdir(
+                os.path.exists(
+                    os.path.join(
+                        name, 'vendor', 'js', 'receiptverifier', '.git'
+                    )
+                )
+            )
+            result = self.state_result(ret)
+            self.assertTrue(result)
+        finally:
+            pass
+            #shutil.rmtree(name, ignore_errors=True)
 
     def test_present(self):
         '''
         git.present
         '''
         name = os.path.join(integration.TMP, 'salt_repo')
-        ret = self.run_state(
-            'git.present',
-            name=name,
-            bare=True
-        )
-        self.assertTrue(os.path.isfile(os.path.join(name, 'HEAD')))
-        result = self.state_result(ret)
-        self.assertTrue(result)
-        shutil.rmtree(name, ignore_errors=True)
+        try:
+            ret = self.run_state(
+                'git.present',
+                name=name,
+                bare=True
+            )
+            self.assertTrue(os.path.isfile(os.path.join(name, 'HEAD')))
+            result = self.state_result(ret)
+            self.assertTrue(result)
+        finally:
+            shutil.rmtree(name, ignore_errors=True)
 
     def test_present_failure(self):
         '''
         git.present
         '''
         name = os.path.join(integration.TMP, 'salt_repo')
-        os.mkdir(name)
-        fname = os.path.join(name, 'stoptheprocess')
-        with file(fname, 'a'):
-            pass
-        ret = self.run_state(
-            'git.present',
-            name=name,
-            bare=True
-        )
-        self.assertFalse(os.path.isfile(os.path.join(name, 'HEAD')))
-        result = self.state_result(ret)
-        self.assertFalse(result)
-        shutil.rmtree(name, ignore_errors=True)
+        if not os.path.isdir(name):
+            os.mkdir(name)
+        try:
+            fname = os.path.join(name, 'stoptheprocess')
+            with file(fname, 'a'):
+                pass
+            ret = self.run_state(
+                'git.present',
+                name=name,
+                bare=True
+            )
+            self.assertFalse(os.path.isfile(os.path.join(name, 'HEAD')))
+            result = self.state_result(ret)
+            self.assertFalse(result)
+        finally:
+            shutil.rmtree(name, ignore_errors=True)
 
     def test_present_empty_dir(self):
         '''
         git.present
         '''
         name = os.path.join(integration.TMP, 'salt_repo')
-        os.mkdir(name)
-        ret = self.run_state(
-            'git.present',
-            name=name,
-            bare=True
-        )
-        self.assertTrue(os.path.isfile(os.path.join(name, 'HEAD')))
-        result = self.state_result(ret)
-        self.assertTrue(result)
-        shutil.rmtree(name, ignore_errors=True)
+        if not os.path.isdir(name):
+            os.mkdir(name)
+        try:
+            ret = self.run_state(
+                'git.present',
+                name=name,
+                bare=True
+            )
+            self.assertTrue(os.path.isfile(os.path.join(name, 'HEAD')))
+            result = self.state_result(ret)
+            self.assertTrue(result)
+        finally:
+            shutil.rmtree(name, ignore_errors=True)
 
 
 if __name__ == '__main__':
