afd3ebc7c1d80fa315cbd94048d42f85ea43cb0e
Thomas S Hatch <thatch45@gmail.com>
2012-11-04 18:35:20 -0700

    Add redis get_jid and get)fun returner functions

--- salt/returners/redis_return.py
+++ salt/returners/redis_return.py
@@ -44,3 +44,33 @@ def returner(ret):
     serv.set('{0}:{1}'.format(ret['id'], ret['jid']), json.dumps(ret))
     serv.lpush('{0}:{1}'.format(ret['id'], ret['fun']), ret['jid'])
     serv.sadd('minions', ret['id'])
+
+
+def get_jid(jid):
+    '''
+    Return the information returned when the specified job id was executed
+    '''
+    serv = _get_serv()
+    ret = {}
+    for minion in serv.smembers('minions'):
+        data = serv.get('{0}:{1}'.format(minion, jid))
+        if data:
+            ret[minion] = json.loads(data)
+    return ret
+
+def get_fun(fun):
+    '''
+    Return a dict of the last function called for all minions
+    '''
+    serv = _get_serv()
+    ret = {}
+    for minion in serv.smembers('minions'):
+        ind_str = '{0}:{1}'.format(minion, fun)
+        try:
+            jid = serv.lindex(ind_str, serv.llen(ind_str) - 1)
+        except Exception:
+            continue
+        data = serv.get('{0}:{1}'.format(minion, jid))
+        if data:
+            ret[minion] = json.loads(data)
+    return ret
