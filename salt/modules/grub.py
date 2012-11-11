48228fd91a3c5700890a5c496d76717485edb3f5
Jeff Schroeder <jeffschroeder@computer.org>
2012-11-10 21:49:46 -0800

    salt.modules.grub: Fix some bugs and move from boot to grub
    Since it makes sense to have separate grub/grub2, syslinux, and lilo
    modules because things like conf keys will be TOTALLY different, move
    the name from boot to grub.
    
    Use a with statement for reading the config file and also wrap it all
    up in a fancy try/except that raises CommandExecutionError on issue.

--- salt/modules/grub.py
+++ salt/modules/grub.py
@@ -12,7 +12,7 @@ def __virtual__():
     '''
     conf = _detect_conf()
     if os.path.exists(conf):
-        return 'boot'
+        return 'grub'
     return False
 
 def _detect_conf():
@@ -49,31 +49,37 @@ def conf():
     instanza = 0
     ret = {}
     pos = 0
-    for line in open(_detect_conf(), 'r'):
-        if line.startswith('#'):
-            continue
-        if line.startswith('\n'):
-            instanza = 0
-            if 'title' in stanza:
+    try:
+        with open(_detect_conf(), 'r') as _fp:
+            for line in _fp:
+                if line.startswith('#'):
+                    continue
+                if line.startswith('\n'):
+                    instanza = 0
+                    if 'title' in stanza:
+                        stanza += 'order {0}'.format(pos)
+                        pos += 1
+                        stanzas.append(stanza)
+                    stanza = ''
+                    continue
+                if line.startswith('title'):
+                    instanza = 1
+                if instanza == 1:
+                    stanza += line
+                if instanza == 0:
+                    key, value = _parse_line(line)
+                    ret[key] = value
+            if instanza == 1:
+                if not line.endswith('\n'):
+                    line += '\n'
+                stanza += line
                 stanza += 'order {0}'.format(pos)
                 pos += 1
                 stanzas.append(stanza)
-            stanza = ''
-            continue
-        if line.startswith('title'):
-            instanza = 1
-        if instanza == 1:
-            stanza += line
-        if instanza == 0:
-            key, value = _parse_line(line)
-            ret[key] = value
-    if instanza == 1:
-        if not line.endswith('\n'):
-            line += '\n'
-        stanza += line
-        stanza += 'order {0}'.format(pos)
-        pos += 1
-        stanzas.append(stanza)
+    except (IOError, OSError) as exc:
+        msg = "Could not read grub config '{0}': {1}"
+        raise CommandExecutionError(msg.format(conf, str(exc)))
+
     ret['stanzas'] = []
     for stanza in stanzas:
         mydict = {}
