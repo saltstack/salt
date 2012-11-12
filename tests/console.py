042b2490c62b28d311f8db645bd0bdb57d8e2a3d
Thomas S Hatch <thatch45@gmail.com>
2012-11-12 16:29:26 -0700

    Clean up console.py
    SINGLE QUOTES
    Bare exceptions are bad
    vars being used without being assigned

--- tests/console.py
+++ tests/console.py
@@ -1,17 +1,25 @@
 # -*- coding: utf-8 -*-
 # vim: sw=4 ts=4 fenc=utf-8
-""" getTerminalSize()
+'''
+getTerminalSize()
  - get width and height of console
  - works on linux,os x,windows,cygwin(windows)
  - taken from http://stackoverflow.com/questions/566746/how-to-get-console-window-width-in-python
-"""
+'''
 
+# Import python libs
+import platform
+import struct
+import ctypes
+import subprocess
+import fcntl
+import termios
+import struct
 
 __all__ = ['getTerminalSize']
 
 
 def getTerminalSize():
-   import platform
    current_os = platform.system()
    tuple_xy=None
    if current_os == 'Windows':
@@ -22,7 +30,7 @@ def getTerminalSize():
    if current_os == 'Linux' or current_os == 'Darwin' or current_os.startswith('CYGWIN'):
        tuple_xy = _getTerminalSize_linux()
    if tuple_xy is None:
-       print "default"
+       print 'default'
        tuple_xy = (80, 25)      # default value
    return tuple_xy
 
@@ -30,21 +38,18 @@ def getTerminalSize():
 def _getTerminalSize_windows():
     res=None
     try:
-        from ctypes import windll, create_string_buffer
-
         # stdin handle is -10
         # stdout handle is -11
         # stderr handle is -12
 
-        h = windll.kernel32.GetStdHandle(-12)
-        csbi = create_string_buffer(22)
-        res = windll.kernel32.GetConsoleScreenBufferInfo(h, csbi)
-    except:
+        h = ctypes.windll.kernel32.GetStdHandle(-12)
+        csbi = ctypes.create_string_buffer(22)
+        res = ctypes.windll.kernel32.GetConsoleScreenBufferInfo(h, csbi)
+    except Exception:
         return None
     if res:
-        import struct
         (bufx, bufy, curx, cury, wattr,
-         left, top, right, bottom, maxx, maxy) = struct.unpack("hhhhHhhhhhh", csbi.raw)
+         left, top, right, bottom, maxx, maxy) = struct.unpack('hhhhHhhhhhh', csbi.raw)
         sizex = right - left + 1
         sizey = bottom - top + 1
         return sizex, sizey
@@ -56,24 +61,22 @@ def _getTerminalSize_tput():
     # get terminal width
     # src: http://stackoverflow.com/questions/263890/how-do-i-find-the-width-height-of-a-terminal-window
     try:
-       import subprocess
-       proc=subprocess.Popen(["tput", "cols"],stdin=subprocess.PIPE,stdout=subprocess.PIPE)
+       proc=subprocess.Popen(['tput', 'cols'],stdin=subprocess.PIPE,stdout=subprocess.PIPE)
        output=proc.communicate(input=None)
        cols=int(output[0])
-       proc=subprocess.Popen(["tput", "lines"],stdin=subprocess.PIPE,stdout=subprocess.PIPE)
+       proc=subprocess.Popen(['tput', 'lines'],stdin=subprocess.PIPE,stdout=subprocess.PIPE)
        output=proc.communicate(input=None)
        rows=int(output[0])
        return (cols,rows)
-    except:
+    except Exception:
        return None
 
 
 def _getTerminalSize_linux():
     def ioctl_GWINSZ(fd):
         try:
-            import fcntl, termios, struct, os
             cr = struct.unpack('hh', fcntl.ioctl(fd, termios.TIOCGWINSZ,'1234'))
-        except:
+        except Exception:
             return None
         return cr
     cr = ioctl_GWINSZ(0) or ioctl_GWINSZ(1) or ioctl_GWINSZ(2)
@@ -82,16 +85,16 @@ def _getTerminalSize_linux():
             fd = os.open(os.ctermid(), os.O_RDONLY)
             cr = ioctl_GWINSZ(fd)
             os.close(fd)
-        except:
+        except Exception:
             pass
     if not cr:
         try:
-            cr = (env['LINES'], env['COLUMNS'])
-        except:
+            cr = (os.environ['LINES'], os.environ['COLUMNS'])
+        except Exception:
             return None
     return int(cr[1]), int(cr[0])
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     sizex,sizey=getTerminalSize()
     print  'width =',sizex,'height =',sizey
