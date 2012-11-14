71efcf68d71f8dbce3981e11b5b978b6c8112024
Thomas S Hatch <thatch45@gmail.com>
2012-11-13 20:55:13 -0700

    Add Some initial docs to stateconf

--- salt/states/stateconf.py
+++ salt/states/stateconf.py
@@ -1,3 +1,13 @@
+'''
+Stateconf System
+================
+
+The stateconf system is inteded for use only with the stateconf renderer. This
+State module presents the set function. This function does not execute any
+functionality, but is used to interact with the stateconf renderer.
+'''
+
+
 def _no_op(name, **kws):
     '''
     No-op state to support state config via the stateconf renderer.
