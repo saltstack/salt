187e01dcd90fa90689a2f331220cd6a69b314e49
mafro <github@mafro.net>
2012-11-15 02:38:35 +0000

    removed two superfluous statements

--- saltcloud/clouds/aws.py
+++ saltcloud/clouds/aws.py
@@ -213,12 +213,10 @@ def create(vm_):
     else:
         ip_address = data.public_ips[0]
     if saltcloud.utils.wait_for_ssh(ip_address):
-        username = 'ec2-user'
         for user in usernames:
             if saltcloud.utils.wait_for_passwd(host=ip_address, username=user, timeout=60, key_filename=__opts__['AWS.private_key']):
                 username = user
                 break
-        kwargs['ssh_username'] = username
     deploy_command='bash /tmp/deploy.sh'
     if username == 'root':
         deploy_command='/tmp/deploy.sh'
