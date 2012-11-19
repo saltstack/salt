3999a72b3e1cc882f54f4d3f22f27b22c6061e07
Thomas S Hatch <thatch45@gmail.com>
2012-11-19 16:12:54 -0700

    Add outputter to display overstate stages

---
+++ salt/output/overstatestage.py
@@ -0,0 +1,31 @@
+'''
+Display clean output of an overstate stage
+'''
+
+
+#[{'group2': {'match': ['fedora17-2', 'fedora17-3'],
+#             'require': ['group1'],
+#             'sls': ['nginx', 'edit']}
+#             }
+#             ]
+
+# Import Salt libs
+import salt.utils
+
+
+def output(data):
+    '''
+    Format the data for printing stage information from the overstate system
+    '''
+    colors = salt.utils.get_colors(__opts__.get('color'))
+    ostr = ''
+    for comp in data:
+        for name, stage in comp.items():
+            ostr += '{0}{1}:{2}\n'.format(colors['LIGHT_BLUE'], name, colors['ENDC'])
+            for key in sorted(stage):
+                ostr += '    {0}{1}:{2}{3}\n'.format(
+                        colors['LIGHT_BLUE'],
+                        key,
+                        stage[key],
+                        colors['ENDC'])
+    return ostr
