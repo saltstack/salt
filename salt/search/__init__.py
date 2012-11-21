a52e129bf04b5fe93635de667ab9ee3298548e61
Thomas S Hatch <thatch45@gmail.com>
2012-11-20 17:57:55 -0700

    start setting up a search backend

---
+++ salt/search/__init__.py
@@ -0,0 +1,107 @@
+'''
+Set up the correct search system
+'''
+
+# Import Python libs
+import os
+
+# Import salt libs
+import salt.minion
+import salt.loader
+import salt.utils
+
+
+def iter_ret(opts, ret):
+    '''
+    Yield returner data if the external job cache is enabled
+    '''
+    if not opts['ext_job_cache']:
+        raise StopIteration
+    get_load = '{0}.get_load'.format(opts['ext_job_cache'])
+    get_jid = '{0}.get_jid'.format(opts['ext_job_cache'])
+    get_jids = '{0}.get_jids'.format(opts['ext_job_cache'])
+    if not get_load in ret:
+        raise StopIteration
+    else:
+        get_load = ret[get_load]
+    if not get_jid in ret:
+        raise StopIteration
+    else:
+        get_jid = ret[get_jid]
+    if not get_jids in ret:
+        raise StopIteration
+    else:
+        get_jids = ret[get_jids]
+    for jid in get_jids():
+        jids = {}
+        jids['load'] = get_load(jid)
+        jids['ret'] = get_jid(jid)
+        jids['jid'] = jid
+        yield jids
+
+
+def _iter_dir(dir_, env):
+    ret = []
+    for fn_ in os.listdir(dir_):
+        path = os.path.join(dir_, fn_)
+        if os.path.isdir(path):
+            yield _iter_dir(path, env)
+        elif os.path.isfile(path):
+            with open(path) as fp_:
+                if salt.utils.istextfile(fp_):
+                    ret.append(
+                        {'path': path,
+                         'env': env,
+                         'content': fp_.read()}
+                        )
+                else:
+                    ret.append(
+                        {'path': path,
+                         'env': env,
+                         'content': 'bin'}
+                        )
+    yield ret
+                    
+
+def iter_roots(roots):
+    '''
+    Accepts the file_roots or the pillar_roots structures and yields
+    {'path': <path>,
+     'env': <env>,
+     'cont': <contents>}
+    '''
+    for env, dirs in roots.items():
+        for dir_ in dirs:
+            for ret in _iter_dir(dir_, env):
+                yield ret
+
+class Search(object):
+    '''
+    Set up the object than manages search operations
+    '''
+    def __init__(self, opts):
+        self.opts = opts
+        self.mminion = salt.minion.MasterMinion(
+                self.opts,
+                states=False,
+                rend=False,
+                matcher=False)
+        self.search = salt.loader.search(self.opts, self.mminion.returners)
+
+    def index(self):
+        '''
+        Execute a search index run
+        '''
+        ifun = '{0}.index'.format(self.opts.get('search', ''))
+        if not ifun in self.search:
+            return
+        return self.search[ifun]()
+
+    def query(self, term):
+        '''
+        Search the index for the given term
+        '''
+        qfun = '{0}.query'.format(self.opts.get('search', ''))
+        if not qfun in self.search:
+            return
+        return self.search[qfun]()
---
+++ salt/search/whoosh_search.py
@@ -0,0 +1,69 @@
+'''
+Routines to manage interactions with the whoosh search system
+'''
+# Import salt libs
+import salt.search
+
+# Import whoosh libs
+has_whoosh = False
+try:
+    import whoosh.index
+    import whoosh.fields
+    import whoosh.store
+    import whoosh.qparser
+    has_whoosh = True
+except ImportError:
+    pass
+
+
+def __virtual__():
+    '''
+    Only load if the whoosh libs are available
+    '''
+    return 'whoosh' if has_whoosh else False
+
+
+def index():
+    '''
+    Build the search index
+    '''
+    schema = whoosh.fields.Schema(
+            path=whoosh.fields.TEXT, # Path for sls files
+            content=whoosh.fields.TEXT, # All content is indexed here
+            env=whoosh.fields.ID, # The environment associated with a file
+            fn_type=whoosh.fields.ID, # Set to pillar or state
+            minion=whoosh.fields.ID, # The minion id associated with the content
+            jid=whoosh.fields.ID, # The job id
+            load=whoosh.fields.ID, # The load data
+            )
+    index_dir = os.path.join(__opts__['cachedir'], 'whoosh')
+    if not os.path.isdir(index_dir):
+        os.makedirs(index_dir)
+    if whoosh.index.exists_in(index_dir):
+        ix_ = whoosh.index.open_dir(index_dir)
+    else:
+        ix_ = whoosh.index.create_in(index_dir, schema)
+
+    try:
+        writer = ix_.writer()
+    except whoosh.store.LockError:
+        return False
+
+    for data in salt.search.iter_roots(__opts__['file_roots']):
+        for chunk in data:
+            writer.add_document(fn_type='file', **data)
+            writer.commit()
+
+    for data in salt.search.iter_roots(__opts__['pillar_roots']):
+        for chunk in data:
+            writer.add_document(fn_type='pillar', **data)
+            writer.commit()
+
+    for data in salt.search.iter_ret(__opts__, __ret__):
+        writer.add_document(jid=data['jid'], load=data['load'])
+        for minion in data['ret']:
+            writer.add_document(
+                    jid=data['jid'],
+                    minion=minion,
+                    content=data['ret'][minion])
+            writer.commit()
