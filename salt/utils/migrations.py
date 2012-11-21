c6b00c59c1e26695baab21133cb77f66b03e9f19
Pedro Algarvio <pedro@algarvio.me>
2012-11-21 09:58:42 +0000

    Don't bother migrating configuration if does not exist.

--- salt/utils/migrations.py
+++ salt/utils/migrations.py
@@ -1,43 +1,48 @@
 '''
 Migration tools
 '''
-# import
+# Import python libs
 import os.path
 import shutil
+
 
 def migrate_paths(opts):
     '''
     Migrate old minion and master pki file paths to new ones.
     '''
     oldpki_dir = '/etc/salt/pki'
+
+    if not os.path.exists(oldpki_dir):
+        # There's not even a pki directory, don't bother migrating
+        return
+
     newpki_dir = opts['pki_dir']
-    
+
     if opts['default_include'].startswith('master'):
         keepers = ['master.pem',
-                    'master.pub',
-                    'syndic_master.pub',
-                    'minions',
-                    'minions_pre',
-                    'minions_rejected',
-                    ]
+                   'master.pub',
+                   'syndic_master.pub',
+                   'minions',
+                   'minions_pre',
+                   'minions_rejected',
+                   ]
         if not os.path.exists(newpki_dir):
             os.makedirs(newpki_dir)
         for item in keepers:
-            if (os.path.exists(os.path.join(oldpki_dir, item)) == True and 
-                    os.path.exists(os.path.join(newpki_dir, item)) == False):
-                shutil.move('{0}/{1}'.format(oldpki_dir, item),
-                            '{0}/{1}'.format(newpki_dir, item))
-                    
+            oi_path = os.path.join(oldpki_dir, item)
+            ni_path = os.path.join(newpki_dir, item)
+            if os.path.exists(oi_path) and not os.path.exists(ni_path):
+                shutil.move(oi_path, ni_path)
+
     if opts['default_include'].startswith('minion'):
         keepers = ['minion_master.pub',
-                    'minion.pem',
-                    'minion.pub',
-                    ]
+                   'minion.pem',
+                   'minion.pub',
+                   ]
         if not os.path.exists(newpki_dir):
             os.makedirs(newpki_dir)
         for item in keepers:
-            if (os.path.exists(os.path.join(oldpki_dir, item)) == True and 
-                    os.path.exists(os.path.join(newpki_dir, item)) == False):
-                shutil.move('{0}/{1}'.format(oldpki_dir, item),
-                            '{0}/{1}'.format(newpki_dir, item))
-
+            oi_path = os.path.join(oldpki_dir, item)
+            ni_path = os.path.join(newpki_dir, item)
+            if os.path.exists(oi_path) and not os.path.exists(ni_path):
+                shutil.move(oi_path, ni_path)
