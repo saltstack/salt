saltstack/salt

commit 6c3b69cc56e9519df9e49645e1e4869a6607c65a

make it possible to add a user with uid/gid 0
Test uid and gid against None instead of plain falsity,
since uid/gid 0 is valid.

salt/modules/useradd.py
@@ -81,9 +81,9 @@ def add(name,
     cmd = 'useradd '
     if shell:
         cmd += '-s {0} '.format(shell)
-    if uid:
+    if uid is not None:
         cmd += '-u {0} '.format(uid)
-    if gid:
+    if gid is not None:
         cmd += '-g {0} '.format(gid)
     if groups:
         cmd += '-G {0} '.format(','.join(groups))

commit 927f79eb94ca8981e972f781b799e157f403f87f

Fix bug introduced in 6c3b69cc56e9519df9e49645e1e4869a6607c65a. Refs #2672.

salt/modules/useradd.py
@@ -81,9 +81,9 @@ def add(name,
     cmd = 'useradd '
     if shell:
         cmd += '-s {0} '.format(shell)
-    if uid is not None:
+    if uid not in (None, ''):
         cmd += '-u {0} '.format(uid)
-    if gid is not None:
+    if gid not in (None, ''):
         cmd += '-g {0} '.format(gid)
     if groups:
         cmd += '-G {0} '.format(','.join(groups))