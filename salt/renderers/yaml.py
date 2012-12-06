b4678cc080d6a1cdd12dfda6c4d76fd8ac0ff8be
Henrik Holmboe <henrik@holmboe.se>
2012-12-06 23:31:06 +0100

    Change HAS_ORDERED_DICT to has_ordered_dict.
    It seems that this is more commonplace.

--- salt/renderers/yaml.py
+++ salt/renderers/yaml.py
@@ -11,10 +11,8 @@ from salt.exceptions import SaltRenderError
 
 log = logging.getLogger(__name__)
 
-
-HAS_ORDERED_DICT = True
-
 # code fragment taken from https://gist.github.com/844388
+has_ordered_dict = True
 try:
     # included in standard lib from Python 2.7
     from collections import OrderedDict
@@ -24,7 +22,7 @@ except ImportError:
     try:
         from ordereddict import OrderedDict
     except ImportError:
-        HAS_ORDERED_DICT = False
+        has_ordered_dict = False
 
 
 def get_yaml_loader(argline):
@@ -40,7 +38,7 @@ Options:
 ''')
         raise
     if ('-o', '') in opts:
-        if HAS_ORDERED_DICT:
+        if has_ordered_dict:
             def Loader(*args):
                 return CustomLoader(*args, dictclass=OrderedDict)
             return Loader
