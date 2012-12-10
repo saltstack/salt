3431ffb7848786de283543baa0334b3723a42dd9
Pedro Algarvio <pedro@algarvio.me>
2012-12-10 12:19:27 +0000

    Fix `salt/modules/alternatives.py`, `__salt__` is not available at `__virtual__` stage.
    * Since when `__virtual__()` is called, `__salt__` is not yet defined, we cannot use `__salt__['cmd.has_exec']`. Import `salt.utils.which` and use that, it's the same.
    * Switch double quotes for single quotes.
    * PEP-8

--- salt/modules/alternatives.py
+++ salt/modules/alternatives.py
@@ -1,8 +1,20 @@
+# -*- coding: utf-8 -*-
 '''
-Support for Alternatives system
+    salt.modules.alternatives
+    ~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Support for Alternatives system
+
+    :codeauthor: :email:`Radek Rada <radek.rada@gmail.com>`
+    :copyright: © 2012 by the SaltStack Team, see AUTHORS for more details.
+    :license: Apache 2.0, see LICENSE for more details.
 '''
 
+# Import python libs
 import os
+
+# Import salt libs
+from salt.utils import which
 
 __outputter__ = {
     'display': 'txt',
@@ -15,12 +27,12 @@ def __virtual__():
     '''
     Only if update-alternatives command is available
     '''
-    return 'alternatives' if __salt__['cmd.has_exec']('update-alternatives') else False
+    return 'alternatives' if which('update-alternatives') else False
 
 
 def display(name):
     '''
-    Display alteratives settings for defined command name.
+    Display alternatives settings for defined command name.
 
     CLI Example::
 
@@ -33,10 +45,10 @@ def display(name):
         return out['stderr']
     return out['stdout']
 
+
 def show_current(name):
     '''
     '''
-
     alt_link_path = '/etc/alternatives/{0}'.format(name)
     if os.path.islink(alt_link_path):
         path = os.path.realpath(alt_link_path)
@@ -68,7 +80,9 @@ def install(name, link, path, priority):
         salt '*' alternatives.install name link path priority
     '''
 
-    cmd = "update-alternatives --install {0} {1} {2} {3}".format(link, name, path, priority)
+    cmd = 'update-alternatives --install {0} {1} {2} {3}'.format(
+        link, name, path, priority
+    )
     out = __salt__['cmd.run_all'](cmd)
     if out['retcode'] > 0 and out['stderr'] != '':
         return out['stderr']
@@ -84,9 +98,8 @@ def remove(name, path):
         salt '*' alternatives.remove name path
     '''
 
-    cmd = "update-alternatives --remove {0} {1}".format(name, path)
+    cmd = 'update-alternatives --remove {0} {1}'.format(name, path)
     out = __salt__['cmd.run_all'](cmd)
     if out['retcode'] > 0:
         return out['stderr']
     return out['stdout']
-
