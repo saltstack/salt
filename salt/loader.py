saltstack/salt

commit 3fa8b42e3abe441a2b53165176d6e2d71b906a20

loader: don't debug log when virtual name == module_name
This is just pointless debug spam. Often times, virtual isn't used
to change the module name, but to ensure the requirements for a
given module are satisfied. Only debug log a module was loaded as
virtual if the module name is different from the virtual name ie:
cmd vs cmdmod

salt/loader.py
@@ -579,7 +579,7 @@ class Loader(object):
                     if hasattr(mod, '__virtual__'):
                         if callable(mod.__virtual__):
                             virtual = mod.__virtual__()
-                            if virtual:
+                            if virtual and module_name != virtual:
                                 log.debug(('Loaded {0} as virtual '
                                            '{1}').format(module_name, virtual))
                                 # update the module name with the new name

commit 13c5719940d05a5b0e9e54abd81ea94a6e074333

Keep logging information when `__virtual__()` changes the module name.
This addresses the fix on saltstack/salt@3fa8b42e3abe441a2b53165176d6e2d71b906a20 which introduced a bug, and saltstack/salt@9dbd90a3e621f441c8555452364a56f2a4afd9ea which fixed the bug. A win-win situation :)

salt/loader.py
@@ -579,13 +579,21 @@ class Loader(object):
                     if hasattr(mod, '__virtual__'):
                         if callable(mod.__virtual__):
                             virtual = mod.__virtual__()
-                            if virtual:
-                                # update the module name with the new name
-                                module_name = virtual
-                            else:
+                            if virtual is False:
                                 # if __virtual__() returns false then the
-                                # module wasn't meant for this platform.
+                                # module wasn't meant for this platform or it's
+                                # not supposed to load for some other reason.
                                 continue
+
+                            if module_name != virtual:
+                                # update the module name with the new name
+                                log.debug(
+                                    'Loaded {0} as virtual {1}').format(
+                                        module_name, virtual
+                                    )
+                                )
+                                module_name = virtual
+
                 except Exception:
                     # If the module throws an exception during __virtual__()
                     # then log the information and continue to the next.