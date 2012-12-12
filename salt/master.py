b3b732c73e29b27fe190668913af90f0a0a6619a
Thomas S Hatch <thatch45@gmail.com>
2012-12-12 14:49:36 -0700

    Add outputter data in syndic single event fires
    Fix #2748

--- salt/master.py
+++ salt/master.py
@@ -898,6 +898,8 @@ class AESFuncs(object):
             ret = {'jid': load['jid'],
                    'id': key,
                    'return': item}
+            if 'out' in load:
+                ret['out'] = load['out']
             self._return(ret)
         if os.path.isfile(wtag):
             os.remove(wtag)
