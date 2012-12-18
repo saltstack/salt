a5df00189891773c15f4ffe4408e11690bfdeb02
Denis Generalov <giantlock@gmail.com>
2012-12-19 03:09:38 +0400

    cron.raw_cron needs exactly raw stdout from cmd.run
    Conflicts:
    	salt/modules/cmdmod.py

--- salt/modules/cmdmod.py
+++ salt/modules/cmdmod.py
@@ -200,7 +200,8 @@ def _run_all_quiet(cmd, cwd=None, runas=None, shell=DEFAULT_SHELL, env=()):
     return _run(cmd, runas=runas, cwd=cwd, shell=shell, env=env, quiet=True)
 
 
-def run(cmd, cwd=None, runas=None, shell=DEFAULT_SHELL, env=()):
+def run(cmd, cwd=None, runas=None, shell=DEFAULT_SHELL, env=(),
+        template=None, rstrip=True):
     '''
     Execute the passed command and return the output as a string
 
@@ -208,13 +209,15 @@ def run(cmd, cwd=None, runas=None, shell=DEFAULT_SHELL, env=()):
 
         salt '*' cmd.run "ls -l | awk '/foo/{print $2}'"
     '''
-    out = _run(cmd, runas=runas, shell=shell,
-               cwd=cwd, stderr=subprocess.STDOUT, env=env)['stdout']
+    out = _run(cmd, runas=runas, shell=shell, cwd=cwd,
+               stderr=subprocess.STDOUT, env=env, template=template,
+               rstrip=rstrip)['stdout']
     log.debug('output: {0}'.format(out))
     return out
 
 
-def run_stdout(cmd, cwd=None, runas=None, shell=DEFAULT_SHELL, env=()):
+def run_stdout(cmd, cwd=None, runas=None, shell=DEFAULT_SHELL, env=(),
+               template=None, rstrip=True):
     '''
     Execute a command, and only return the standard out
 
@@ -222,12 +225,14 @@ def run_stdout(cmd, cwd=None, runas=None, shell=DEFAULT_SHELL, env=()):
 
         salt '*' cmd.run_stdout "ls -l | awk '/foo/{print $2}'"
     '''
-    stdout = _run(cmd, runas=runas, cwd=cwd, shell=shell, env=())["stdout"]
+    stdout = _run(cmd, runas=runas, cwd=cwd, shell=shell, env=(),
+                  template=template, rstrip=rstrip)["stdout"]
     log.debug('stdout: {0}'.format(stdout))
     return stdout
 
 
-def run_stderr(cmd, cwd=None, runas=None, shell=DEFAULT_SHELL, env=()):
+def run_stderr(cmd, cwd=None, runas=None, shell=DEFAULT_SHELL, env=(),
+               template=None, rstrip=True):
     '''
     Execute a command and only return the standard error
 
@@ -235,12 +240,14 @@ def run_stderr(cmd, cwd=None, runas=None, shell=DEFAULT_SHELL, env=()):
 
         salt '*' cmd.run_stderr "ls -l | awk '/foo/{print $2}'"
     '''
-    stderr = _run(cmd, runas=runas, cwd=cwd, shell=shell, env=env)["stderr"]
+    stderr = _run(cmd, runas=runas, cwd=cwd, shell=shell, env=env,
+                  template=template, rstrip=rstrip)["stderr"]
     log.debug('stderr: {0}'.format(stderr))
     return stderr
 
 
-def run_all(cmd, cwd=None, runas=None, shell=DEFAULT_SHELL, env=()):
+def run_all(cmd, cwd=None, runas=None, shell=DEFAULT_SHELL, env=(),
+            template=None, rstrip=True):
     '''
     Execute the passed command and return a dict of return data
 
@@ -248,7 +255,8 @@ def run_all(cmd, cwd=None, runas=None, shell=DEFAULT_SHELL, env=()):
 
         salt '*' cmd.run_all "ls -l | awk '/foo/{print $2}'"
     '''
-    ret = _run(cmd, runas=runas, cwd=cwd, shell=shell, env=env)
+    ret = _run(cmd, runas=runas, cwd=cwd, shell=shell, env=env,
+               template=template, rstrip=rstrip)
 
     if ret['retcode'] != 0:
         rcode = ret['retcode']
