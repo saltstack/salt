3c13138a6f150fc33b3d021cf13a5e9db6914262
David Boucha <boucha@gmail.com>
2012-12-19 12:42:28 -0700

    genrepo now creates a repo cache file
    logging doesn't seem to be working correctly, but it does
    create the cache file correctly.

--- salt/runners/winrepo.py
+++ salt/runners/winrepo.py
@@ -2,15 +2,42 @@
 Runner to manage Windows software repo
 '''
 
+# Import python libs
+import os
+import yaml
+from pprint import pprint
+import msgpack
+
 # Import salt libs
 import salt.output
+import salt.utils
+import logging
+
+log = logging.getLogger(__name__)
 
 
 def genrepo():
     '''
     Generate win_repo_cachefile based on sls files in the win_repo
     '''
-
-    result = __opts__['win_repo']
-    salt.output.display_output(result, 'pprint', __opts__)
-    return result
+    ret = {}
+    repo = __opts__['win_repo']
+    for root, dirs, files in os.walk(repo):
+        for name in files:
+            if name.endswith('.sls'):
+                with salt.utils.fopen(os.path.join(root, name), 'r') as slsfile:
+                    try:
+                        config = yaml.safe_load(slsfile.read()) or {}
+                    except yaml.parser.ParserError as exc:
+                        # log.debug doesn't seem to be working
+                        # delete the following print statement 
+                        # when log.debug works
+                        log.debug('Failed to compile'
+                                '{0}: {1}'.format(os.path.join(root, name), exc))
+                        print 'Failed to compile {0}: {1}'.format(os.path.join(root, name), exc)
+                if config:
+                    ret.update(config)
+    with salt.utils.fopen(os.path.join(repo, 'winrepo.cache'), 'w') as repo:
+        repo.write(msgpack.dumps(ret))
+    salt.output.display_output(ret, 'pprint', __opts__)
+    return ret
