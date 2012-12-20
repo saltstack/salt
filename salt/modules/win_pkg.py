1dfe105829cbddcf37d0e029b6f8351c72a58269
David Boucha <boucha@gmail.com>
2012-12-20 16:59:35 -0700

    moar bug fixes

--- salt/modules/win_pkg.py
+++ salt/modules/win_pkg.py
@@ -308,10 +308,10 @@ def install(name=None, refresh=False, **kwargs):
     pkginfo = _get_package_info(name)
     pprint(pkginfo)
     print 'pkg file: ', pkginfo['installer']
-    cached_pkg = __salt__['cp.is_cached'](pkginfo['installer'])
+    cached_pkg = __salt__['cp.is_cached'](str(pkginfo['installer']))
     if not cached_pkg:
         # It's not cached. Cache it, mate.
-        cached_pkg = __salt__['cp.cache_file'](pkginfo['installer'])
+        cached_pkg = __salt__['cp.cache_file'](str(pkginfo['installer']))
     pprint(cached_pkg)
     cmd = cached_pkg, pkginfo['install_flags']
     print cmd
