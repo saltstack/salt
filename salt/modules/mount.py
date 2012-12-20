saltstack/salt

commit e01c3d52b11ec90c1334dbdabb3af434047699b7

salt.modules.mount: relatively major robustness cleanup
    - Make is_fuse_exec() actually work and remove code from it
    - Wrap all open() calls in try/except and use a with statement

salt/modules/mount.py
@@ -2,10 +2,12 @@
 Salt module to manage unix mounts and the fstab file
 '''
 
-import logging
 import os
+import logging
 
 from salt._compat import string_types
+from salt.utils import which as _which
+from salt.exceptions import CommandNotFoundError, CommandExecutionError
 
 
 # Set up logger
@@ -13,7 +15,12 @@ log = logging.getLogger(__name__)
 
 
 def _active_mountinfo(ret):
-    with open('/proc/self/mountinfo') as fh:
+    filename = '/proc/self/mountinfo'
+    if not os.access(filename, os.R_OK):
+        msg = 'File not readable {0}'
+        raise CommandExecutionError(msg.format(filename))
+
+    with open(filename) as fh:
         for line in fh:
             comps = line.split()
             device = comps[2].split(':')
@@ -30,7 +37,12 @@ def _active_mountinfo(ret):
 
 
 def _active_mounts(ret):
-    with open('/proc/self/mounts') as fh:
+    filename = '/proc/self/mountinfo'
+    if not os.access(filename, os.R_OK):
+        msg = 'File not readable {0}'
+        raise CommandExecutionError(msg.format(filename))
+
+    with open(filename) as fh:
         for line in fh:
             comps = line.split()
             ret[comps[1]] = {'device': comps[0],
@@ -98,25 +110,36 @@ def rm_fstab(name, config='/etc/fstab'):
         return True
     # The entry is present, get rid of it
     lines = []
-    for line in open(config).readlines():
-        if line.startswith('#'):
-            # Commented
-            lines.append(line)
-            continue
-        if not line.strip():
-            # Blank line
-            lines.append(line)
-            continue
-        comps = line.split()
-        if not len(comps) == 6:
-            # Invalid entry
-            lines.append(line)
-            continue
-        comps = line.split()
-        if comps[1] == name:
-            continue
-        lines.append(line)
-    open(config, 'w+').writelines(lines)
+    try:
+        with open(config, 'r') as fh:
+            for line in fh:
+                if line.startswith('#'):
+                    # Commented
+                    lines.append(line)
+                    continue
+                if not line.strip():
+                    # Blank line
+                    lines.append(line)
+                    continue
+                comps = line.split()
+                if not len(comps) == 6:
+                    # Invalid entry
+                    lines.append(line)
+                    continue
+                comps = line.split()
+                if comps[1] == name:
+                    continue
+                lines.append(line)
+    except (IOError, OSError) as exc:
+        msg = "Couldn't read from {0}: {1}"
+        raise CommandExecutionError(msg.format(config, str(exc)))
+
+    try:
+        with open(config, 'w+') as fh:
+            fh.writelines(lines)
+    except (IOError, OSError) as exc:
+        msg = "Couldn't write to {0}: {1}"
+        raise CommandExecutionError(msg.format(config, str(exc)))
     return True
 
 
@@ -130,7 +153,7 @@ def set_fstab(
         config='/etc/fstab',
         ):
     '''
-    Verify that this mount is represented in the fstab, chage the mount point
+    Verify that this mount is represented in the fstab, change the mount
     to match the data passed, or add the mount if it is not present.
 
     CLI Example::
@@ -143,52 +166,68 @@ def set_fstab(
     lines = []
     change = False
     present = False
+
     if not os.path.isfile(config):
-        return 'bad config'
-    for line in open(config).readlines():
-        if line.startswith('#'):
-            # Commented
-            lines.append(line)
-            continue
-        if not line.strip():
-            # Blank line
-            lines.append(line)
-            continue
-        comps = line.split()
-        if not len(comps) == 6:
-            # Invalid entry
-            lines.append(line)
-            continue
-        if comps[1] == name:
-            # check to see if there are changes and fix them if there are
-            present = True
-            if comps[0] != device:
-                change = True
-                comps[0] = device
-            if comps[2] != fstype:
-                change = True
-                comps[2] = fstype
-            if comps[3] != opts:
-                change = True
-                comps[3] = opts
-            if comps[4] != str(dump):
-                change = True
-                comps[4] = str(dump)
-            if comps[5] != str(pass_num):
-                change = True
-                comps[5] = str(pass_num)
-            if change:
-                log.debug('fstab entry for mount point {0} is being updated'
-                          .format(name))
-                newline = ('{0}\t\t{1}\t{2}\t{3}\t{4} {5}\n'
-                           .format(device, name, fstype, opts, dump, pass_num))
-                lines.append(newline)
-        else:
-            lines.append(line)
+        raise CommandExecutionError("Bad config file '{0}'".format(config))
+
+    try:
+        with open(config, 'r') as fh:
+            for line in fh:
+                if line.startswith('#'):
+                    # Commented
+                    lines.append(line)
+                    continue
+                if not line.strip():
+                    # Blank line
+                    lines.append(line)
+                    continue
+                comps = line.split()
+                if not len(comps) == 6:
+                    # Invalid entry
+                    lines.append(line)
+                    continue
+                if comps[1] == name:
+                    # check to see if there are changes
+                    # and fix them if there are any
+                    present = True
+                    if comps[0] != device:
+                        change = True
+                        comps[0] = device
+                    if comps[2] != fstype:
+                        change = True
+                        comps[2] = fstype
+                    if comps[3] != opts:
+                        change = True
+                        comps[3] = opts
+                    if comps[4] != str(dump):
+                        change = True
+                        comps[4] = str(dump)
+                    if comps[5] != str(pass_num):
+                        change = True
+                        comps[5] = str(pass_num)
+                    if change:
+                        log.debug('fstab entry for mount point {0} is being updated'
+                                  .format(name))
+                        newline = ('{0}\t\t{1}\t{2}\t{3}\t{4} {5}\n'
+                                   .format(device, name, fstype, opts, dump, pass_num))
+                        lines.append(newline)
+                else:
+                    lines.append(line)
+    except (IOError, OSError) as exc:
+        msg = "Couldn't write to {0}: {1}"
+        raise CommandExecutionError(msg.format(conf, str(exc)))
+
     if change:
-        # The line was changed, commit it!
-        open(config, 'w+').writelines(lines)
+        try:
+            with open(config, 'w+') as fh:
+                # The line was changed, commit it!
+                fh.writelines(lines)
+        except (IOError, OSError) as exc:
+            msg = 'File not writable {0}'
+            raise CommandExecutionError(msg.format(filename))
+
         return 'change'
+
     if not change and not present:
         # The entry is new, add it to the end of the fstab
         newline = '{0}\t\t{1}\t{2}\t{3}\t{4} {5}\n'.format(
@@ -199,7 +238,13 @@ def set_fstab(
                 dump,
                 pass_num)
         lines.append(newline)
-        open(config, 'w+').writelines(lines)
+        try:
+            with open(config, 'w+') as fh:
+                # The line was changed, commit it!
+                fh.writelines(lines)
+        except (IOError, OSError) as exc:
+            msg = 'File not writable {0}'
+            raise CommandExecutionError(msg.format(filename))
     if present and not change:
         # The right entry is already here
         return 'present'
@@ -252,8 +297,8 @@ def remount(name, device, mkmnt=False, fstype='', opts='defaults'):
         if out['retcode']:
             return out['stderr']
         return True
-    else:
-        return mount(name, device, mkmnt, fstype, opts)
+    # Mount a filesystem that isn't already
+    return mount(name, device, mkmnt, fstype, opts)
 
 
 def umount(name):
@@ -282,13 +327,11 @@ def is_fuse_exec(cmd):
 
         salt '*' mount.is_fuse_exec sshfs
     '''
-    if not __salt__['cmd.has_exec'](cmd):
+    cmd_path = _which(cmd)
+
+    # No point in running ldd on a command that doesn't exist
+    if not cmd_path:
         return False
-    for path in os.environ['PATH'].split(os.pathsep):
-        if not __salt__['cmd.has_exec'](path):
-            continue
-        out = __salt__['cmd.run']('ldd {0}'.format(path))
-        for line in out.splitlines():
-            if 'libfuse' in line:
-                return True
-    return False
+
+    out = __salt__['cmd.run']('ldd {0}'.format(cmd_path))
+    return 'libfuse' in out

commit 16bc496b119dc6ee6d20111be4c579f2a2a9e02c

Fix mounting issue introduced by e01c3d52b1

salt/modules/mount.py
@@ -40,7 +40,7 @@ def _active_mountinfo(ret):
 
 
 def _active_mounts(ret):
-    filename = '/proc/self/mountinfo'
+    filename = '/proc/self/mounts'
     if not os.access(filename, os.R_OK):
         msg = 'File not readable {0}'
         raise CommandExecutionError(msg.format(filename))
@@ -65,7 +65,7 @@ def active():
     ret = {}
     try:
         _active_mountinfo(ret)
-    except IOError:
+    except CommandExecutionError:
         _active_mounts(ret)
     return ret
 