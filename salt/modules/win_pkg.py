acbaf246b1ddcd8a309fe0d6416ce61a0fe34f8b
David Boucha <boucha@gmail.com>
2012-12-20 16:48:58 -0700

    fix moar bugs

--- salt/modules/win_pkg.py
+++ salt/modules/win_pkg.py
@@ -307,6 +307,7 @@ def install(name=None, refresh=False, **kwargs):
     old = list_pkgs()
     pkginfo = _get_package_info(name)
     pprint(pkginfo)
+    print 'pkg file: ', pkginfo['installer']
     cached_pkg = __salt__['cp.is_cached'](pkginfo['installer'])
     if not cached_pkg:
         # It's not cached. Cache it, mate.
