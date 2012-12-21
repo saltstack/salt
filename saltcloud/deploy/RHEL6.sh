4282ee93dfa971a098ca28782f77b26493913fd7
Joseph Hall <perlhoser@gmail.com>
2012-12-21 17:20:57 +0000

    Updating EPEL version for RHEL6

--- saltcloud/deploy/RHEL6.sh
+++ saltcloud/deploy/RHEL6.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rpm -Uvh --force http://mirrors.kernel.org/fedora-epel/6/x86_64/epel-release-6-7.noarch.rpm
+rpm -Uvh --force http://mirrors.kernel.org/fedora-epel/6/x86_64/epel-release-6-8.noarch.rpm
 yum -y install salt-minion --enablerepo epel-testing
 mkdir -p /etc/salt/pki
 echo '{{ vm['priv_key'] }}' > /etc/salt/pki/minion.pem
