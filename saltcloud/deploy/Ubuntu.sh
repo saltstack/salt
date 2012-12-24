2ce2051b60788853dd7899adeeb6084620220c85
Sean Channel <pentabular@gmail.com>
2012-12-24 11:06:08 -0800

    stray sudos

--- saltcloud/deploy/Ubuntu.sh
+++ saltcloud/deploy/Ubuntu.sh
@@ -5,8 +5,8 @@ echo '{{ vm['priv_key'] }}' > /etc/salt/pki/minion.pem
 echo '{{ vm['pub_key'] }}' > /etc/salt/pki/minion.pub
 echo "{{ minion }}" > /etc/salt/minion
 
-echo deb http://ppa.launchpad.net/saltstack/salt/ubuntu `lsb_release -sc` main | sudo tee /etc/apt/sources.list.d/saltstack.list
-wget -q -O- "http://keyserver.ubuntu.com:11371/pks/lookup?op=get&search=0x4759FA960E27C0A6" | sudo apt-key add -
+echo deb http://ppa.launchpad.net/saltstack/salt/ubuntu `lsb_release -sc` main | tee /etc/apt/sources.list.d/saltstack.list
+wget -q -O- "http://keyserver.ubuntu.com:11371/pks/lookup?op=get&search=0x4759FA960E27C0A6" | apt-key add -
 apt-get update
 
 apt-get install -y salt-minion
