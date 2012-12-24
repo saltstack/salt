2338286de10f54bd76ba8af928b03ac87622fc94
Sean Channel <pentabular@gmail.com>
2012-12-24 10:55:50 -0800

    add repos directly, start/stop

--- saltcloud/deploy/Ubuntu-git.sh
+++ saltcloud/deploy/Ubuntu-git.sh
@@ -5,11 +5,14 @@ echo '{{ vm['priv_key'] }}' > /etc/salt/pki/minion.pem
 echo '{{ vm['pub_key'] }}' > /etc/salt/pki/minion.pub
 echo "{{ minion }}" > /etc/salt/minion
 
+echo deb http://ppa.launchpad.net/saltstack/salt/ubuntu `lsb_release -sc` main | sudo tee /etc/apt/sources.list.d/saltstack.list
+wget -q -O- "http://keyserver.ubuntu.com:11371/pks/lookup?op=get&search=0x4759FA960E27C0A6" | sudo apt-key add -
 apt-get update
-apt-get install -y python-software-properties
-echo | add-apt-repository  ppa:saltstack/salt
-apt-get update
+
 apt-get install -y salt-minion git-core
+# minion will be started automatically by install
+service salt-minion stop
+
 rm -rf /usr/share/pyshared/salt*
 rm -rf /usr/bin/salt-*
 mkdir -p /root/git
