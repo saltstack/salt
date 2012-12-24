ac2339708a3fb37ac7c923d92bb8a6efcd964ff7
Sean Channel <pentabular@gmail.com>
2012-12-24 11:46:39 -0800

    also Debian

---
+++ saltcloud/deploy/Debian.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p /etc/salt/pki
+echo '{{ vm['priv_key'] }}' > /etc/salt/pki/minion.pem
+echo '{{ vm['pub_key'] }}' > /etc/salt/pki/minion.pub
+echo "{{ minion }}" > /etc/salt/minion
+
+# echo deb http://ftp.debian.org/debian experimental main | tee -a /etc/apt/sources.list
+echo deb http://backports.debian.org/debian-backports squeeze-backports main | tee -a /etc/apt/sources.list
+echo deb http://debian.madduck.net/repo squeeze-backports main | tee -a /etc/apt/sources.list
+wget -q -O- "http://debian.madduck.net/repo/gpg/archive.key" | apt-key add -
+
+apt-get update
+apt-get install -y salt-minion
+# minion will be started automatically by install
