195fb88be47e50cf5adfa1379c2f19466d6c5046
shadowfax-chc <tim.messier@gmail.com>
2012-12-29 00:51:51 -0500

    Added functions for sync and update to eix module

--- salt/modules/eix.py
+++ salt/modules/eix.py
@@ -13,4 +13,24 @@ def __virtual__():
         return 'eix'
     return False
 
+def sync():
+    '''
+    Sync portage/overlay trees and update the eix database
 
+    CLI Example::
+
+        salt '*' eix.sync
+    '''
+    cmd = 'eix-sync -q'
+    return __salt__['cmd.retcode'](cmd) == 0
+
+def update():
+    '''
+    Update the eix database
+
+    CLI Example::
+
+        salt '*' eix.update
+    '''
+    cmd = 'eix-update --quiet'
+    return __salt__['cmd.retcode'](cmd) == 0
