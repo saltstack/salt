e9eee2dcba6427f3771eaa6763340644b77e1419
Thomas S Hatch <thatch45@gmail.com>
2012-12-31 15:51:42 -0700

    Add fileserver wrapper class

--- salt/fileserver/__init__.py
+++ salt/fileserver/__init__.py
@@ -6,6 +6,8 @@ File server pluggable modules and generic backend functions
 import re
 import fnmatch
 import logging
+# Import salt libs
+import salt.loader
 
 
 log = logging.getLogger(__name__)
@@ -37,3 +39,149 @@ def is_file_ignored(opts, fname):
                 )
                 return True
     return False
+
+
+class Fileserver(object):
+    '''
+    Create a fileserver wrapper object that wraps the fileserver functions and
+    iterates over them to execute the desired function within the scope of the
+    desired fileserver backend.
+    '''
+    def __init__(self, opts):
+        self.opts = opts
+        self.servers = salt.loader.fileserver(opts, opts['fileserver_backend'])
+
+    def _gen_back(back):
+        '''
+        Return the backend list
+        '''
+        if not back:
+            back = self.opts['fileserver_backend']
+        if isinstance(back, str):
+            back = [back]
+        return back
+
+    def update(back=None):
+        '''
+        Update all of the fileservers that support the update function or the
+        named fileserver only.
+        '''
+        back = self._gen_back(back)
+        for fsb in back:
+            fstr = '{0}.update'.format(fsb)
+            if fstr in self.servers:
+                self.servers[fstr]()
+
+    def envs(back=None, sources=False):
+        '''
+        Return the environments for the named backend or all backends
+        '''
+        back = self._gen_back(back)
+        ret = set()
+        if sources:
+            ret = {}
+        for fsb in back:
+            fstr = '{0}.envs'.format(fsb)
+            if sources:
+                ret[fsb] = self.servers[fstr]()
+            else:
+                ret.update(self.servers[fstr]())
+        if sources:
+            return ret
+        return list(ret)
+
+    def init(back=None):
+        '''
+        Initialize the backend, only do so if the fs supports an init function
+        '''
+        back = self._gen_back(back)
+        for fsb in back:
+            fstr = '{0}.init'.format(fsb)
+            if fstr in self.servers:
+                self.servers[fstr]()
+
+    def find_file(path, env, back=None):
+        '''
+        Find the path and return the fnd structure, this structure is passed
+        to other backend interfaces.
+        '''
+        back = self._gen_back(back)
+        fnd = {'path': '',
+               'rel': ''}
+        for fsb in back:
+            fstr = '{0}.find_file'.format(fsb)
+            if fstr in self.servers:
+                fnd = self.servers[fstr](path, env)
+                if fnd.get('path'):
+                    fnd['back'] = fsb
+                    return fnd
+        return fnd
+
+    def serve_file(load):
+        '''
+        Serve up a chunk of a file
+        '''
+        ret = {'data': '',
+               'dest': ''}
+        if 'path' not in load or 'loc' not in load or 'env' not in load:
+            return ret
+        fnd = self.find_file(load['path'], load['env'])
+        if not fnd.get('back'):
+            return ret
+        fstr = '{0}.serve_file'.format(fnd['back'])
+        if fstr is self.servers:
+            return self.servers[fstr](load, fnd)
+        return ret
+
+    def file_hash(load):
+        '''
+        Return the hash of a given file
+        '''
+        if 'path' not in load or 'env' not in load:
+            return ''
+        fnd = self.find_file(load['path'], load['env'])
+        if not fnd.get('back'):
+            return ''
+        fstr = '{0}.file_hash'.format(fnd['back'])
+        if fstr is self.servers:
+            return self.servers[fstr](load, fnd)
+        return ''
+
+    def file_list(load):
+        '''
+        Return a list of files from the dominant environment
+        '''
+        ret = set()
+        if 'env' not in load:
+            return []
+        for fsb in self._get_back(None):
+            fstr = '{0}.file_list'.format(fsb)
+            if fstr in self.servers:
+                ret.update(self.servers[fstr](load))
+        return sorted(ret)
+
+    def file_list_emptydirs(load):
+        '''
+        List all emptydirs in the given environment
+        '''
+        ret = set()
+        if 'env' not in load:
+            return []
+        for fsb in self._get_back(None):
+            fstr = '{0}.file_list_emptydirs'.format(fsb)
+            if fstr in self.servers:
+                ret.update(self.servers[fstr](load))
+        return sorted(ret)
+
+    def dir_list(load):
+        '''
+        List all directories in the given environment
+        '''
+        ret = set()
+        if 'env' not in load:
+            return []
+        for fsb in self._get_back(None):
+            fstr = '{0}.dir_list'.format(fsb)
+            if fstr in self.servers:
+                ret.update(self.servers[fstr](load))
+        return sorted(ret)
