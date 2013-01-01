bf4501c1bd1afdef0e8ee475fc6b754aa8eca527
Thomas S Hatch <thatch45@gmail.com>
2013-01-01 13:31:35 -0700

    Fix bug in environment gathering on the master

--- salt/master.py
+++ salt/master.py
@@ -667,8 +667,8 @@ class AESFuncs(object):
         mopts = dict(self.opts)
         file_roots = dict(mopts['file_roots'])
         envs = self._file_envs()
-        for env in file_roots:
-            if not env in envs:
+        for env in envs:
+            if not env in file_roots:
                 file_roots[env] = []
         mopts['file_roots'] = file_roots
         return mopts
