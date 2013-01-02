ae3d07788bb5f95127d83c837976e46d369f0629
Thomas S Hatch <thatch45@gmail.com>
2013-01-02 13:59:12 -0700

    Make output data cleaner on the eyes :)

--- salt/runners/state.py
+++ salt/runners/state.py
@@ -12,10 +12,12 @@ def over(env='base', os_fn=None):
     Execute an overstate sequence to orchestrate the executing of states
     over a group of systems
     '''
+    stage_num = 0
     overstate = salt.overstate.OverState(__opts__, env, os_fn)
     for stage in overstate.stages_iter():
         if isinstance(stage, dict):
             # This is highstate data
+            print('Stage execution results:')
             for key, val in stage.items():
                 salt.output.display_output(
                         {key: val},
@@ -23,7 +25,12 @@ def over(env='base', os_fn=None):
                         opts=__opts__)
         elif isinstance(stage, list):
             # This is a stage
+            if stage_num == 0:
+                print('Executing the following Over State:')
+            else:
+                print('Executed Stage:')
             salt.output.display_output(stage, 'overstatestage', opts=__opts__)
+            stage_num += 1
     return overstate.over_run
 
 def show_stages(env='base', os_fn=None):
