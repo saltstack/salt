869185a341c87e498b41c6c024b88257eb1a4757
Erik Johnson <palehose@gmail.com>
2013-01-05 16:47:33 -0600

    Removed notify-upower.sh
    This was mistakenly added in my last commit.

--- salt/modules/notify-upower.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-[ "$1" = "post" ] && exec /usr/bin/dbus-send	\
-	--system --type=signal			\
-	--dest=org.freedesktop.UPower		\
-	/org/freedesktop/UPower			\
-	org.freedesktop.UPower.Resuming
