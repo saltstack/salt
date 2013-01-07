c1c1cacefe07e73e54f3a0e615cde6fdd30d179a
Paul Traylor <kungfudiscomonkey@gmail.com>
2013-01-06 17:18:38 -0800

    Provide a way to survey minion's version

--- salt/runners/manage.py
+++ salt/runners/manage.py
@@ -3,6 +3,8 @@ General management functions for salt, tools like seeing what hosts are up
 and what hosts are down
 '''
 
+import distutils.version
+
 # Import salt libs
 import salt.key
 import salt.client
@@ -35,3 +37,33 @@ def up():  # pylint: disable-msg=C0103
         print(minion)
 
     return sorted(minions)
+
+
+def versions():
+    '''
+    Check the version of active minions
+    '''
+    client = salt.client.LocalClient(__opts__['conf_file'])
+    minions = client.cmd('*', 'test.version', timeout=__opts__['timeout'])
+
+    labels = {
+        -1: 'Minion requires update',
+        0: 'Up to date',
+        1: 'Minion newer than master',
+    }
+
+    sorted_minions = {}
+
+    master_version = distutils.version.StrictVersion(salt.__version__)
+    for minion in minions:
+        minion_version = distutils.version.StrictVersion(minions[minion])
+        ver_diff = cmp(minion_version, master_version)
+
+        if ver_diff not in sorted_minions:
+            sorted_minions[ver_diff] = []
+        sorted_minions[ver_diff].append(minion)
+
+    for key in sorted_minions:
+        print labels[key]
+        for minion in sorted(sorted_minions[key]):
+            print '\t', minion
