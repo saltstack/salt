ad452f0b64e171fa2e20e249f826917c30031965
Thomas S Hatch <thatch45@gmail.com>
2013-01-14 23:08:36 -0700

    clean up stray prints

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -624,8 +624,6 @@ class ModuleCase(TestCase, SaltClientTestCaseMixIn):
         '''
         Run the state.single command and return the state return structure
         '''
-        print function
-        print kwargs
         return self.run_function('state.single', [function], **kwargs)
 
     @property
