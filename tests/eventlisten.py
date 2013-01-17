e354080dd7270e68ff11a7f249dbe4075a971e15
Thomas S Hatch <thatch45@gmail.com>
2013-01-17 14:00:53 -0700

    don explicitly add node on eventlisten

--- tests/eventlisten.py
+++ tests/eventlisten.py
@@ -42,8 +42,6 @@ def parse():
         if v is not None:
             opts[k] = v
 
-    opts['sock_dir'] = os.path.join(opts['sock_dir'], opts['node'])
-
     return opts
 
 
@@ -55,6 +53,7 @@ def listen(sock_dir, node):
             node,
             sock_dir,
             )
+    print event.puburi
     while True:
         ret = event.get_event(full=True)
         if ret is None:
