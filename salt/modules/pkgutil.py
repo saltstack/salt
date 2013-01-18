saltstack/salt

commit 40f89cece82ca19d7a755715ab911005d21e217f

Prevent traceback using refresh param
This commit forces the 'refresh' param to be cast as a string to keep
things like 'refresh: 123' in a pkg state from causing a TypeError.

salt/modules/pkgutil.py
@@ -81,7 +81,7 @@ def list_upgrades(refresh=True):
         salt '*' pkgutil.list_upgrades
     '''
     # Catch both boolean input from state and string input from CLI
-    if refresh is True or refresh.lower() == 'true':
+    if refresh is True or str(refresh).lower() == 'true':
         refresh_db()
     upgrades = {}
     lines = __salt__['cmd.run_stdout']('/opt/csw/bin/pkgutil -A --parse').splitlines()
@@ -109,7 +109,7 @@ def upgrade(refresh=True, **kwargs):
         salt '*' pkgutil.upgrade
     '''
     # Catch both boolean input from state and string input from CLI
-    if refresh is True or refresh.lower() == 'true':
+    if refresh is True or (refresh).lower() == 'true':
         refresh_db()
 
     # Get a list of the packages before install so we can diff after to see

commit 8ec4f86f4d309ec17d99c2e07845b9fb486d3cec

Fix typo introduced in 40f89ce

salt/modules/pkgutil.py
@@ -109,7 +109,7 @@ def upgrade(refresh=True, **kwargs):
         salt '*' pkgutil.upgrade
     '''
     # Catch both boolean input from state and string input from CLI
-    if refresh is True or (refresh).lower() == 'true':
+    if refresh is True or str(refresh).lower() == 'true':
         refresh_db()
 
     # Get a list of the packages before install so we can diff after to see