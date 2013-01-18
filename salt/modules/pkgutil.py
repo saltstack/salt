saltstack/salt

commit f0d10431d70c65307ce79b73445db59e5cdda579

Prevent traceback using refresh param
This commit forces the 'refresh' param to be cast as a string to keep
things like 'refresh: 123' in a pkg state from causing a TypeError.

salt/modules/pkgutil.py
@@ -81,7 +81,7 @@ def list_upgrades(refresh=True):
         salt '*' pkgutil.list_upgrades
     '''
     # Catch both boolean input from state and string input from CLI
-    if refresh is True or refresh.lower() == 'true':
+    if refresh is True or str(refresh).lower() == 'true':
         refresh_db()
     upgrades = {}
     lines = __salt__['cmd.run_stdout']('/opt/csw/bin/pkgutil -A --parse').splitlines()
@@ -109,7 +109,7 @@ def upgrade(refresh=True, **kwargs):
         salt '*' pkgutil.upgrade
     '''
     # Catch both boolean input from state and string input from CLI
-    if refresh is True or refresh.lower() == 'true':
+    if refresh is True or (refresh).lower() == 'true':
         refresh_db()
 
     # Get a list of the packages before install so we can diff after to see

salt/modules/apt.py
@@ -204,7 +204,7 @@ def install(name=None, refresh=False, fromrepo=None, skip_verify=False,
     salt.utils.daemonize_if(__opts__, **kwargs)
 
     # Catch both boolean input from state and string input from CLI
-    if refresh is True or refresh == 'True':
+    if refresh is True or str(refresh).lower() == 'true':
         refresh_db()
 
     if debconf:
@@ -330,7 +330,7 @@ def upgrade(refresh=True, **kwargs):
     salt.utils.daemonize_if(__opts__, **kwargs)
 
     # Catch both boolean input from state and string input from CLI
-    if refresh is True or refresh == 'True':
+    if refresh is True or str(refresh).lower() == 'true':
         refresh_db()
 
     ret_pkgs = {}
@@ -450,7 +450,7 @@ def list_upgrades(refresh=True):
         salt '*' pkg.list_upgrades
     '''
     # Catch both boolean input from state and string input from CLI
-    if refresh is True or refresh == 'True':
+    if refresh is True or str(refresh).lower() == 'true':
         refresh_db()
     return _get_upgradable()
 

salt/modules/ebuild.py
@@ -130,7 +130,7 @@ def list_upgrades(refresh=True):
         salt '*' pkg.list_upgrades
     '''
     # Catch both boolean input from state and string input from CLI
-    if refresh is True or refresh.lower() == 'true':
+    if refresh is True or str(refresh).lower() == 'true':
         refresh_db()
     return _get_upgradable()
 
@@ -270,7 +270,7 @@ def install(name=None, refresh=False, pkgs=None, sources=None, **kwargs):
         }
     ))
     # Catch both boolean input from state and string input from CLI
-    if refresh is True or refresh.lower() == 'true':
+    if refresh is True or str(refresh).lower() == 'true':
         refresh_db()
 
     pkg_params, pkg_type = __salt__['pkg_resource.parse_targets'](name,
@@ -342,7 +342,7 @@ def upgrade(refresh=True):
         salt '*' pkg.upgrade
     '''
     # Catch both boolean input from state and string input from CLI
-    if refresh is True or refresh.lower() == 'true':
+    if refresh is True or str(refresh).lower() == 'true':
         refresh_db()
 
     ret_pkgs = {}

commit a21c49e58caf777bf790dd0b5bed041de876d882

Fix typo introduced in f0d1043

salt/modules/pkgutil.py
@@ -109,7 +109,7 @@ def upgrade(refresh=True, **kwargs):
         salt '*' pkgutil.upgrade
     '''
     # Catch both boolean input from state and string input from CLI
-    if refresh is True or (refresh).lower() == 'true':
+    if refresh is True or str(refresh).lower() == 'true':
         refresh_db()
 
     # Get a list of the packages before install so we can diff after to see