4b67ed5045a254cf5cf268b1d6df2018dfbc2003
Thomas S Hatch <thatch45@gmail.com>
2013-01-21 17:00:19 -0700

    Fix #3351

--- salt/cli/caller.py
+++ salt/cli/caller.py
@@ -88,6 +88,8 @@ class Caller(object):
             oput = self.minion.functions[fun].__outputter__
             if isinstance(oput, string_types):
                 ret['out'] = oput
+            if oput == 'highstate':
+                ret['return'] = {'local': ret['return']}
         if self.opts.get('return', ''):
             ret['id'] = self.opts['id']
             ret['fun'] = fun
