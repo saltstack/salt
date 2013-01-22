8bc35045470718d9143e38f16e042c47fab2547e
Jakub Vysoky <jakub@borka.cz>
2013-01-22 11:46:06 +0100

    add new process in supervisor state
    process is added if it wasn't there before
    
    some nice supervisor modules included:
     * supervisor.{add,remove,reread,update}
     * supervisor.list - list all process states
     * supervisor.custom - run any custom command (eg from plugins)

--- salt/modules/supervisord.py
+++ salt/modules/supervisord.py
@@ -55,14 +55,79 @@ def restart(name='all', user=None):
 def stop(name='all', user=None):
     '''
     Stop the named service.
-    
+
     CLI Example::
         salt '*' supervisord.stop <service>
     '''
     ret = __salt__['cmd.run_all'](_ctl_cmd('stop', name), runas=user)
     return _get_return(ret)
 
+def add(name, user=None):
+    '''
+    Activates any updates in config for process/group
+
+    CLI Example::
+        salt '*' supervisord.add <name>
+    '''
+    ret = __salt__['cmd.run_all'](_ctl_cmd('add', name), runas=user)
+    return _get_return(ret)
+
+def remove(name, user=None):
+    '''
+    Removes process/group from active config
+
+    CLI Example::
+        salt '*' supervisord.remove <name>
+    '''
+    ret = __salt__['cmd.run_all'](_ctl_cmd('remove', name), runas=user)
+    return _get_return(ret)
+
+def reread(user=None):
+    '''
+    Reload the daemon's configuration files
+
+    CLI Example::
+        salt '*' supervisord.reread
+    '''
+    ret = __salt__['cmd.run_all'](_ctl_cmd('reread', None), runas=user)
+    return _get_return(ret)
+
+def update(user=None):
+    '''
+    Reload config and add/remove as necessary
+
+    CLI Example::
+        salt '*' supervisord.update
+    '''
+    ret = __salt__['cmd.run_all'](_ctl_cmd('update', None), runas=user)
+    return _get_return(ret)
+
+def list(name=None, user=None):
+    '''
+    List programms and its state
+
+    CLI Example::
+        salt '*' supervisord.list
+    '''
+    all_process = {}
+    for line in status(name, user).splitlines():
+        if len(line.split()) > 2:
+            process, state, reason = line.split(None, 2)
+        else:
+            process, state, reason = line.split() + ['']
+        all_process[process] = {'state': state, 'reason': reason}
+    return all_process
 
 def status(name=None, user=None):
     ret = __salt__['cmd.run_all'](_ctl_cmd('status', name), runas=user)
+    return _get_return(ret)
+
+def custom(command, user=None):
+    '''
+    Run any custom supervisord command
+
+    CLI Example::
+        salt '*' supervisord.custom "mstop '*gunicorn*'"
+    '''
+    ret = __salt__['cmd.run_all'](_ctl_cmd(command, None), runas=user)
     return _get_return(ret)
