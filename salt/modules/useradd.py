49a80f8cdbcbeb3f8f3cf32d7160473a02cd19f4
Wiesław Herr <herself@makhleb.net>
2013-01-24 15:35:02 +0100

    Removing the _format_info function on Darwin doesn't make much sense

--- salt/modules/useradd.py
+++ salt/modules/useradd.py
@@ -28,7 +28,7 @@ def __virtual__():
         mod = sys.modules[__name__]
         for attr in dir(mod):
             if callable(getattr(mod, attr)):
-                if not attr in ('getent', 'info', 'list_groups', 'list_users', '__virtual__'):
+                if not attr in ('_format_info', 'getent', 'info', 'list_groups', 'list_users', '__virtual__'):
                     delattr(mod, attr)
     return 'user' if __grains__['kernel'] in ('Linux', 'Darwin') else False
 
