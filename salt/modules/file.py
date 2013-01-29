854f317bb4db985a940cb2fbf487cf4591d9513b
Thomas S Hatch <thatch45@gmail.com>
2013-01-29 15:19:31 -0700

    Fix #3405

--- salt/modules/file.py
+++ salt/modules/file.py
@@ -1331,7 +1331,7 @@ def manage_file(name,
             # If the downloaded file came from a non salt server source verify
             # that it matches the intended sum value
             if urlparse(source).scheme != 'salt':
-                dl_sum = get_hash(name, source_sum['hash_type'])
+                dl_sum = get_hash(sfn, source_sum['hash_type'])
                 if dl_sum != source_sum['hsum']:
                     ret['comment'] = ('File sum set for file {0} of {1} does '
                                       'not match real sum of {2}'
