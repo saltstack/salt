b898f1d9454105855f5a9a3b0804c0bd2a4e5239
Pedro Algarvio <pedro@algarvio.me>
2013-01-31 10:04:49 +0000

    Single quotes and don't redefine outer function.

--- saltcloud/utils/__init__.py
+++ saltcloud/utils/__init__.py
@@ -538,17 +538,17 @@ def check_name(name, pattern):
 
 
 def namespaced_function(function, global_dict, defaults=None):
-    """
+    '''
     Redefine(clone) a function under a different globals() namespace scope
-    """
+    '''
     if defaults is None:
         defaults = function.__defaults__
 
-    namespaced_function = types.FunctionType(
+    new_namespaced_function = types.FunctionType(
         function.__code__,
         global_dict,
         name=function.__name__,
         argdefs=defaults
     )
-    namespaced_function.__dict__.update(function.__dict__)
-    return namespaced_function
+    new_namespaced_function.__dict__.update(function.__dict__)
+    return new_namespaced_function
