4528059f1914226c8f81bc7eae9ea3a627312b0e
Thomas S Hatch <thatch45@gmail.com>
2013-01-31 15:09:01 -0700

    fix spacing in job ctrl-c message

--- salt/client.py
+++ salt/client.py
@@ -337,8 +337,8 @@ class LocalClient(object):
                     yield fn_ret
             except KeyboardInterrupt:
                 msg = ('Exiting on Ctrl-C\nThis job\'s jid is:\n{0}\n'
-                       'The minions may not have all finished running and any'
-                       'remaining minions will return upon completion. To look'
+                       'The minions may not have all finished running and any '
+                       'remaining minions will return upon completion. To look '
                        'up the return data for this job later run:\n'
                        'salt-run jobs.lookup_jid {0}').format(pub_data['jid'])
                 raise SystemExit(msg)
