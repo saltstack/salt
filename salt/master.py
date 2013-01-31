10541567d7c198ce2f032dd6835c21702a7e3797
Thomas S Hatch <thatch45@gmail.com>
2013-01-31 15:55:59 -0700

    Add ipv6_enable option

--- salt/master.py
+++ salt/master.py
@@ -341,7 +341,10 @@ class Publisher(multiprocessing.Process):
             pub_sock.setsockopt(zmq.SNDHWM, 1)
             pub_sock.setsockopt(zmq.RCVHWM, 1)
         if hasattr(zmq, 'IPV4ONLY'):
-            pub_sock.setsockopt(zmq.IPV4ONLY, 0)
+            pub_sock.setsockopt(
+                zmq.IPV4ONLY,
+                int(not int(self.opts.get('ipv6_enable')))
+                )
         pub_uri = 'tcp://{interface}:{publish_port}'.format(**self.opts)
         # Prepare minion pull socket
         pull_sock = context.socket(zmq.PULL)
@@ -396,7 +399,7 @@ class ReqServer(object):
         self.clients = self.context.socket(zmq.ROUTER)
         if hasattr(zmq, 'IPV4ONLY'):
             self.clients.setsockopt(
-                zmq.IPV4ONLY, 0
+                zmq.IPV4ONLY, int(not int(self.opts.get('ipv6_enable')))
             )
         self.workers = self.context.socket(zmq.DEALER)
         self.w_uri = 'ipc://{0}'.format(
--- salt/minion.py
+++ salt/minion.py
@@ -729,7 +729,7 @@ class Minion(object):
             )
         if hasattr(zmq, 'IPV4ONLY'):
             self.socket.setsockopt(
-                zmq.IPV4ONLY, 0
+                zmq.IPV4ONLY, int(not int(self.opts.get('ipv6_enable')))
             )
         self.socket.connect(self.master_pub)
         self.poller.register(self.socket, zmq.POLLIN)
