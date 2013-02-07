1644c225edec4090722d534f6c674c5601b690c6
Thomas S Hatch <thatch45@gmail.com>
2013-02-07 15:52:34 -0700

    Remove unused imports

--- salt/config.py
+++ salt/config.py
@@ -8,7 +8,6 @@ import os
 import re
 import socket
 import logging
-import time
 import urlparse
 
 # import third party libs
@@ -24,7 +23,6 @@ import salt.crypt
 import salt.loader
 import salt.utils
 import salt.pillar
-from salt.exceptions import SaltClientError
 
 log = logging.getLogger(__name__)
 
