5203654421e73561b204a570855b38f106648a31
Thomas S Hatch <thatch45@gmail.com>
2013-02-08 11:14:00 -0700

    Add test to verify loader modules

--- tests/integration/modules/sysmod.py
+++ tests/integration/modules/sysmod.py
@@ -21,6 +21,25 @@ class SysModuleTest(integration.ModuleCase):
         self.assertTrue('hosts' in mods)
         self.assertTrue('pkg' in mods)
 
+    def test_valid_docs(self):
+        '''
+        Make sure no functions are exposed that don't have valid docstrings
+        '''
+        docs = self.run_function('sys.doc')
+        bad = set()
+        for fun in docs:
+            if fun.startswith('runtests_helpers'):
+                continue
+            if not isinstance(docs[fun], basestring):
+                bad.add(fun)
+            elif not 'Example::' in docs[fun]:
+                if not 'Examples::' in docs[fun]:
+                    bad.add(fun)
+        if bad:
+            import pprint
+            pprint.pprint(sorted(bad))
+        self.assertFalse(bool(bad))
+
 
 if __name__ == '__main__':
     from integration import run_tests
