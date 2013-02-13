e93e07f6fb0ec70c335daca5b83ef5f1bdd40ec8
Thomas S Hatch <thatch45@gmail.com>
2013-02-13 02:26:41 -0500

    Get the outputter going for virt

---
+++ salt/output/virt_query.py
@@ -0,0 +1,14 @@
+def output(data):
+    '''
+    '''
+    out = ''
+    for id_ in data:
+        out += '{0}\n'.format(id_)
+        for vm_ in data[id_]['vm_info']:
+            out += '    {0}\n'.format(vm_)
+            vm_data = data[id_]['vm_info'][vm_]
+            if 'cpu' in vm_data:
+                out += '      CPUS: {0}\n'.format(vm_data['cpu'])
+            if 'mem' in vm_data:
+                out += '      MEMORY: {0}\n'.format(vm_data['mem'])
+    return out
