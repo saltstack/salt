cf2c1aee158ebd68c717411979954cae6a327050
Thomas S Hatch <thatch45@gmail.com>
2013-02-13 13:33:33 -0500

    Add state check to virt_query

--- salt/output/virt_query.py
+++ salt/output/virt_query.py
@@ -11,4 +11,6 @@ def output(data):
                 out += '      CPUS: {0}\n'.format(vm_data['cpu'])
             if 'mem' in vm_data:
                 out += '      MEMORY: {0}\n'.format(vm_data['mem'])
+            if 'state' in vm_data:
+                out += '      STATE: {0}\n'.format(vm_data['state'])
     return out
