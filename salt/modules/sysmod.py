saltstack/salt

commit 03d264b4401ad5815945aaa59f7e959a86a32607

Allow matching of the sys module without also matching sysctl.
Also specifying "sys." (ending dot) will work correctly.

salt/modules/sysmod.py
@@ -12,14 +12,23 @@ def __virtual__():
 
 def doc(module=''):
     '''
-    Return the docstrings for all modules, these strings are aggregated into
-    a single document on the master for easy reading.
+    Return the docstrings for all modules. Optionally, specify a module or a
+    function to narrow te selection.
+
+    The strings are aggregated into a single document on the master for easy
+    reading.
 
     CLI Example::
 
         salt \* sys.doc
+        salt \* sys.doc sys
+        salt \* sys.doc sys.doc
     '''
     docs = {}
+    if module:
+        # allow both "sys" and "sys." to match sys, without also matching
+        # sysctl
+        module = module + '.' if not module.endswith('.') else module
     for fun in __salt__:
         if fun.startswith(module):
             docs[fun] = __salt__[fun].__doc__
@@ -28,18 +37,21 @@ def doc(module=''):
 
 def list_functions(module=''):
     '''
-    List the functions.  Optionally, specify a module to list from.
+    List the functions for all modules. Optionally, specify a module to list
+    from.
 
     CLI Example::
 
         salt \* sys.list_functions
+        salt \* sys.list_functions sys
     '''
     names = set()
+    if module:
+        # allow both "sys" and "sys." to match sys, without also matching
+        # sysctl
+        module = module + '.' if not module.endswith('.') else module
     for func in __salt__:
-        if module:
-            if func.startswith('{0}.'.format(module)):
-                names.add(func)
-        else:
+        if func.startswith(module):
             names.add(func)
     return sorted(names)
 

commit 0affb35ef4692fb5a9ecf4557e1048dd5c8cb799

Fix broken sys.doc function
03d264b broke sys.doc by adding a '.' to the end of the module if it is
not an empty string. While this succeeds in making 'sys' not match the
sysctl modules functions, it also means that passing a specific function
name (ex. 'network.interfaces') would result in the passed value having
that same '.' appended (i.e. 'network.interfaces.', note the trailing
dot). This causes sys.doc to fail in returning the docstring for this
function since 'network.interfaces.' will not match any of the functions
in the __salt__ dict.

salt/modules/sysmod.py
@@ -3,6 +3,12 @@ The sys module provides information about the available functions on the
 minion.
 '''
 
+# Import python libs
+import logging
+
+log = logging.getLogger(__name__)
+
+
 def __virtual__():
     '''
     Return as sys
@@ -28,9 +34,11 @@ def doc(module=''):
     if module:
         # allow both "sys" and "sys." to match sys, without also matching
         # sysctl
-        module = module + '.' if not module.endswith('.') else module
+        target_mod = module + '.' if not module.endswith('.') else module
+    else:
+        target_mod = ''
     for fun in __salt__:
-        if fun.startswith(module):
+        if fun == module or fun.startswith(target_mod):
             docs[fun] = __salt__[fun].__doc__
     return docs
 