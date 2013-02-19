bde83ef3e4ada9159417d32a7317a51bf86e8cc5
David Boucha <boucha@gmail.com>
2013-02-18 22:03:59 -0700

    Update available_version docstring to standard

--- salt/modules/win_pkg.py
+++ salt/modules/win_pkg.py
@@ -49,11 +49,18 @@ def _list_removed(old, new):
 
 def available_version(name):
     '''
-    The available version of the package in the repository
+    Return the latest version of the named package available for upgrade or
+    installation. If more than one package name is specified, a dict of
+    name/version pairs is returned.
+
+    If the latest version of a given package is already installed, an empty
+    string will be returned for that package.
 
     CLI Example::
 
         salt '*' pkg.available_version <package name>
+        salt '*' pkg.available_version <package1> <package2> <package3> ...
+
     '''
     ret = {}
     pkginfo = _get_package_info(name)
