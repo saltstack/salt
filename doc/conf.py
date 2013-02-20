7a9814b911ba18539264c44cc5fa6d779c8fcbb3
Joseph Hall <perlhoser@gmail.com>
2013-02-20 09:57:59 -0700

    Update copyright date in doc/conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -93,7 +93,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'salt-cloud'
-copyright = u'2012, SaltStack, Inc.'
+copyright = u'2012 - 2013, Salt Stack, Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
