82f09372bcdc34df961e45c0529c2dfd2da57511
Thomas S Hatch <thatch45@gmail.com>
2013-02-28 01:58:04 -0700

    refine libvirt ext_pillar

--- salt/pillar/libvirt.py
+++ salt/pillar/libvirt.py
@@ -13,7 +13,6 @@ def ext_pillar(pillar, command):
     '''
     key_dir = os.path.join(
             __opts__['pki_dir'],
-            'master',
             'libvirt',
             __grains__['id'])
     if not os.path.isdir(key_dir):
@@ -21,6 +20,8 @@ def ext_pillar(pillar, command):
         return {}
     ret = {}
     for key in os.listdir(key_dir):
+        if not key.endswith('.pem'):
+            continue
         fn_ = os.path.join(key_dir, key)
         with open(fn_, 'r') as fp_:
             ret['libvirt.{0}'.format(key)] = fp_.read()
