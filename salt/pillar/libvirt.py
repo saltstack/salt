f9d9a6532f401d66b3110afbe6a28eb9f17d3018
Thomas S Hatch <thatch45@gmail.com>
2013-02-27 18:08:38 -0700

    Initial commit for libvirt key distribution

---
+++ salt/pillar/libvirt.py
@@ -0,0 +1,27 @@
+'''
+Load up the libvirt keys into pillar for a given minion if said keys have
+been generated using the libvirt key runner.
+'''
+
+# Import python libs
+import os
+
+
+def ext_pillar(pillar, command):
+    '''
+    Read in the generated libvirt keys
+    '''
+    key_dir = os.path.join(
+            __opts__['pki_dir'],
+            'master',
+            'libvirt',
+            __grains__['id'])
+    if not os.path.isdir(key_dir):
+        # No keys have been generated
+        return {}
+    ret = {}
+    for key in os.listdir(key_dir):
+        fn_ = os.path.join(key_dir, key)
+        with open(fn_, 'r') as fp_:
+            ret['libvirt.{0}'.format(key)] = fp_.read()
+    return ret
