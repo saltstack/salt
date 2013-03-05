01b58d092643fd136b137e93c53b3a5eae58bc0b
Russell Ballestrini <fox@blanka.foxhop.net>
2013-03-05 01:25:42 -0500

    Correcting a logic error where for loop would continue instead of returning when the catalina_home is found.
    Additionally using glob.glob('/usr/local/tomcat*') might be
    dangerous because it could return invalid catalina_homes::
    
     >>> import glob
     >>> glob.glob( '/usr/share/tomcat*' )
     ['/usr/share/tomcat7-root', '/usr/share/tomcat7']
    
    	modified:   tomcat.py

--- salt/modules/tomcat.py
+++ salt/modules/tomcat.py
@@ -21,8 +21,9 @@ def __catalina_home():
     locations = ['/usr/share/tomcat*', '/opt/tomcat']
     for location in locations:
         catalina_home = glob.glob(location)
-    if catalina_home:
-        return catalina_home[-1]
+        if catalina_home:
+            return catalina_home[-1]
+    return False
 
 
 def version():
