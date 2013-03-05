43ce4945e68e6e8a67f6c66202a14d14744cfc8e
David Boucha <boucha@gmail.com>
2013-03-05 01:21:01 -0700

    make version a string

--- salt/modules/win_pkg.py
+++ salt/modules/win_pkg.py
@@ -72,7 +72,7 @@ def available_version(*names):
         pkginfo = _get_package_info(name)
         if len(pkginfo) == 1:
             ret[name] = pkginfo.keys()[0]
-        version = 0
+        version = '0'
         for ver in pkginfo.keys():
             print '-------------'
             print 'version: ', version
