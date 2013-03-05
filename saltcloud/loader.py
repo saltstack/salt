72f6434be76d889c445ce26005e8edfbb24e2124
Pedro Algarvio <pedro@algarvio.me>
2013-03-05 11:59:50 +0000

    PEP-8 formatting.

--- saltcloud/loader.py
+++ saltcloud/loader.py
@@ -8,9 +8,17 @@ import os
 import salt.loader
 import saltcloud
 
+
 def clouds(opts):
     '''
     Return the cloud functions
     '''
-    load = salt.loader._create_loader(opts, 'clouds', 'cloud', base_path=os.path.dirname(saltcloud.__file__))
+    load = salt.loader._create_loader(
+        opts,
+        'clouds',
+        'cloud',
+        base_path=os.path.dirname(
+            saltcloud.__file__
+        )
+    )
     return load.gen_functions()
