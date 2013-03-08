d8ba95ddb1e469600c735316a1aeafa115399b3c
Thomas S Hatch <thatch45@gmail.com>
2013-03-08 00:22:25 -0700

    Add an execution module called key to return minion public key finger

---
+++ salt/modules/key.py
@@ -0,0 +1,21 @@
+'''
+Functions to view the minion's public key information
+'''
+
+# Import python libs
+import os
+
+# Import Salt libs
+import salt.utils
+
+def finger():
+    '''
+    Return the minion's public key fingerprint
+
+    CLI Example::
+
+        salt '*' key.finger
+    '''
+    return salt.utils.pem_finger(
+            os.path.join(__opts__['pki_dir'], 'minion.pub')
+            )
