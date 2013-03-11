74410fc72b9db67134102268216a3e3db3f4aa1a
Joseph Hall <perlhoser@gmail.com>
2013-03-10 19:08:52 -0600

    Restructure report output to be more sensical

--- salt/modules/quota.py
+++ salt/modules/quota.py
@@ -39,11 +39,17 @@ def _parse_quota(mount, opts):
     '''
     Parse the output from repquota. Requires that -u -g are passed in
     '''
-    ret = {}
     cmd = 'repquota -p {0} {1}'.format(opts, mount)
     out = __salt__['cmd.run'](cmd).splitlines()
     mode = 'header'
     device = ''
+
+    if '-u' in opts:
+        quotatype = 'Users'
+    elif '-g' in opts:
+        quotatype = 'Groups'
+    ret = {quotatype: {}}
+
     for line in out:
         if not line:
             continue
@@ -51,25 +57,38 @@ def _parse_quota(mount, opts):
         if mode == 'header':
             if 'Report for' in line:
                 device = comps[-1:][0]
-                ret[device] = {}
             elif 'Block grace time' in line:
                 blockg, inodeg = line.split(';')
                 blockgc = blockg.split(': ')
                 inodegc = inodeg.split(': ')
-                ret[device]['Block Grace Time'] = blockgc[-1:]
-                ret[device]['Inode Grace Time'] = inodegc[-1:]
+                ret['Block Grace Time'] = blockgc[-1:]
+                ret['Inode Grace Time'] = inodegc[-1:]
             elif line.startswith('-'):
                 mode = 'quotas'
         elif mode == 'quotas':
-            if not comps[0] in ret[device]:
-                ret[device][comps[0]] = {}
-            ret[device][comps[0]]['block-used'] = comps[2]
-            ret[device][comps[0]]['block-soft-limit'] = comps[3]
-            ret[device][comps[0]]['block-hard-limit'] = comps[4]
-            ret[device][comps[0]]['block-grace'] = comps[5]
-            ret[device][comps[0]]['file-used'] = comps[6]
-            ret[device][comps[0]]['file-soft-limit'] = comps[7]
-            ret[device][comps[0]]['file-hard-limit'] = comps[8]
-            ret[device][comps[0]]['file-grace'] = comps[9]
+            if not comps[0] in ret[quotatype]:
+                ret[quotatype][comps[0]] = {}
+            ret[quotatype][comps[0]]['block-used'] = comps[2]
+            ret[quotatype][comps[0]]['block-soft-limit'] = comps[3]
+            ret[quotatype][comps[0]]['block-hard-limit'] = comps[4]
+            ret[quotatype][comps[0]]['block-grace'] = comps[5]
+            ret[quotatype][comps[0]]['file-used'] = comps[6]
+            ret[quotatype][comps[0]]['file-soft-limit'] = comps[7]
+            ret[quotatype][comps[0]]['file-hard-limit'] = comps[8]
+            ret[quotatype][comps[0]]['file-grace'] = comps[9]
     return ret
+
+
+def set(device, kwargs):
+    '''
+    Calls out to setquota, for a specific user or group
+
+    CLI Example::
+
+        salt '*' quota.report /media/data user=larry softblock=1048576
+        salt '*' quota.report /media/data group=painters
+    '''
+
+
+    return kwargs
 
