5f5e570f0c60af8a962eb46ee6d7979583e5d050
Thomas S Hatch <thatch45@gmail.com>
2013-03-10 18:49:43 -0600

    All calls are now daemonized

--- salt/modules/puppet.py
+++ salt/modules/puppet.py
@@ -126,9 +126,6 @@ def run(*args, **kwargs):
         salt '*' puppet.run apply /a/b/manifest.pp modulepath=/a/b/modules tags=basefiles::edit,apache::server
     '''
     _check_puppet()
-
-    salt.utils.daemonize_if(__opts__, **kwargs)
-
     puppet = _Puppet()
 
     if args:
