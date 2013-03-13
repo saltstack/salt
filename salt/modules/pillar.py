f5931811f321146770c16bf54dfa4a2f8a969605
Denis Generalov <giantlock@gmail.com>
2013-03-13 13:33:00 +0400

    * some cleanup * pep8 fix

--- salt/modules/pillar.py
+++ salt/modules/pillar.py
@@ -9,7 +9,7 @@ import yaml
 import salt.pillar
 
 
-def data(key=''):
+def data(key=None):
     '''
     Returns the pillar derived from the configured pillar source. The pillar
     source is derived from the file_client option in the minion config
@@ -24,25 +24,20 @@ def data(key=''):
         salt '*' pillar.data key='roles'
     '''
     pillar = salt.pillar.get_pillar(
-            __opts__,
-            __grains__,
-            __opts__['id'],
-            __opts__['environment'])
+        __opts__,
+        __grains__,
+        __opts__['id'],
+        __opts__['environment'])
 
-    compiled_pillar = pillar.compile_pillar()
+    ret = pillar.compile_pillar()
 
     if key:
-        try:
-            ret = compiled_pillar[key]
-        except KeyError:
-            ret = {}
-    else:
-        ret = compiled_pillar
+        ret = ret.get(key, {})
 
     return ret
 
 
-def raw(key=''):
+def raw(key=None):
     '''
     Return the raw pillar data that is available in the module. This will
     show the pillar as it is loaded as the __pillar__ dict.
@@ -57,10 +52,7 @@ def raw(key=''):
         salt '*' pillar.raw key='roles'
     '''
     if key:
-        try:
-            ret = __pillar__[key]
-        except KeyError:
-            ret = {}
+        ret = __pillar__.get(key, {})
     else:
         ret = __pillar__
 
@@ -70,7 +62,7 @@ def raw(key=''):
 def ext(external):
     '''
     Generate the pillar and apply an explicit external pillar
-    
+
     CLI Example::
 
         salt '*' pillar.ext 'libvirt: _'
@@ -78,15 +70,12 @@ def ext(external):
     if isinstance(external, basestring):
         external = yaml.load(external)
     pillar = salt.pillar.get_pillar(
-            __opts__,
-            __grains__,
-            __opts__['id'],
-            __opts__['environment'],
-            external)
+        __opts__,
+        __grains__,
+        __opts__['id'],
+        __opts__['environment'],
+        external)
 
-    compiled_pillar = pillar.compile_pillar()
-
-    ret = compiled_pillar
+    ret = pillar.compile_pillar()
 
     return ret
-
