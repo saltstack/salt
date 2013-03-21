46f61453106a5cc61f2b3cd3c18f085b10c40a8f
Colton Myers <colton.myers@gmail.com>
2013-03-21 16:49:41 -0600

    Remove trailing whitespace and fix PEP8 issues

--- tests/unit/modules/postgres_test.py
+++ tests/unit/modules/postgres_test.py
@@ -9,8 +9,8 @@ except ImportError:
 from saltunittest import TestCase, TestLoader, TextTestRunner
 
 from salt.modules import postgres
-postgres.__grains__ = None # in order to stub it w/patch below
-postgres.__salt__ = None # in order to stub it w/patch below
+postgres.__grains__ = None  # in order to stub it w/patch below
+postgres.__salt__ = None  # in order to stub it w/patch below
 
 SALT_STUB = {
     'config.option': Mock(),
@@ -19,18 +19,20 @@ SALT_STUB = {
 
 
 class PostgresTestCase(TestCase):
-    @patch.multiple(postgres, __grains__={ 'os_family': 'FreeBSD' })
+    @patch.multiple(postgres, __grains__={'os_family': 'FreeBSD'})
     def test_get_runas_bsd(self):
         self.assertEqual('pgsql', postgres._get_runas())
 
-    @patch.multiple(postgres, __grains__={ 'os_family': 'Linux' })
+    @patch.multiple(postgres, __grains__={'os_family': 'Linux'})
     def test_get_runas_other(self):
         self.assertEqual('postgres', postgres._get_runas())
 
-    @patch.multiple(postgres, __grains__={ 'os_family': 'Linux' }, __salt__=SALT_STUB)
+    @patch.multiple(postgres,
+                    __grains__={'os_family': 'Linux'},
+                    __salt__=SALT_STUB)
     def test_run_psql(self):
         postgres._run_psql('echo "hi"')
-        cmd = SALT_STUB['cmd.run_all'] 
+        cmd = SALT_STUB['cmd.run_all']
 
         self.assertEquals('postgres', cmd.call_args[1]['runas'])
 
