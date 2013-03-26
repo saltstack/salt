54c6d1ffc9230da5bf3e9f688ff8bddd5d75ed99
Thomas S Hatch <thatch45@gmail.com>
2013-03-26 11:08:03 -0600

    Fix #4279

--- salt/key.py
+++ salt/key.py
@@ -98,10 +98,9 @@ class KeyCLI(object):
                     matches,
                     'key',
                     self.opts)
-            veri = raw_input('Proceed? [n/Y] ')
-            if veri.lower().startswith('n'):
-                return
-        self.key.delete_key(match)
+            veri = raw_input('Proceed? [N/y] ')
+            if veri.lower().startswith('y'):
+                self.key.delete_key(match)
 
     def delete_all(self):
         '''
