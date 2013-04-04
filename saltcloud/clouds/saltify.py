ed37bffffdddfc28f7194810f3318857eacc8caa
Pedro Algarvio <pedro@algarvio.me>
2013-04-04 18:45:33 +0100

    Updated the Saltify cloud provider to the new syntax and helper functions

--- saltcloud/clouds/saltify.py
+++ saltcloud/clouds/saltify.py
@@ -5,8 +5,8 @@ The Saltify module is designed to install Salt on a remote machine, virtual or
 bare metal, using SSH. This module is useful for provisioning machines which
 are already installed, but not Salted.
 
-Use of this module requires no configuration in the main cloud config file.
-However, profiles must still be configured, as described in the saltify
+Use of this module requires no configuration in the main cloud configuration
+file. However, profiles must still be configured, as described in the saltify
 documentation.
 '''
 
@@ -24,13 +24,15 @@ import xml.etree.ElementTree as ET
 # Import salt libs
 import salt.utils.event
 import salt.utils.xmlutil
+
+# Import salt cloud libs
 import saltcloud.utils
+import saltcloud.config as config
 
 # Get logging started
 log = logging.getLogger(__name__)
 
 
-# Only load in this module if the PARALLELS configurations are in place
 def __virtual__():
     '''
     Needs no special configuration
@@ -40,60 +42,60 @@ def __virtual__():
 
 def avail_locations():
     '''
-    Because this module is not specific to any cloud providers, there will be no
-    locations to list.
+    Because this module is not specific to any cloud providers, there will be
+    no locations to list.
     '''
-    return {'Error':
-               {'Not Supported':
-                   '--list-locations not supported by Saltify'
-               }
-           }
+    return {
+        'Error': {
+            'Not Supported': '--list-locations not supported by Saltify'
+        }
+    }
 
 
 def avail_images():
     '''
-    Because this module is not specific to any cloud providers, there will be no
-    images to list.
+    Because this module is not specific to any cloud providers, there will be
+    no images to list.
     '''
-    return {'Error':
-               {'Not Supported':
-                   '--list-images not supported by Saltify'
-               }
-           }
+    return {
+        'Error': {
+            'Not Supported': '--list-images not supported by Saltify'
+        }
+    }
 
 
 def avail_sizes():
     '''
-    Because this module is not specific to any cloud providers, there will be no
-    sizes to list.
+    Because this module is not specific to any cloud providers, there will be
+    no sizes to list.
     '''
-    return {'Error':
-               {'Not Supported':
-                   '--list-sizes not supported by Saltify'
-               }
-           }
+    return {
+        'Error': {
+            'Not Supported': '--list-sizes not supported by Saltify'
+        }
+    }
 
 
 def list_nodes():
     '''
-    Because this module is not specific to any cloud providers, there will be no
-    nodes to list.
+    Because this module is not specific to any cloud providers, there will be
+    no nodes to list.
     '''
     return {}
 
 
 def list_nodes_full():
     '''
-    Because this module is not specific to any cloud providers, there will be no
-    nodes to list.
+    Because this module is not specific to any cloud providers, there will be
+    no nodes to list.
     '''
     return {}
 
 
 def list_nodes_select():
     '''
-    Because this module is not specific to any cloud providers, there will be no
-    nodes to list.
+    Because this module is not specific to any cloud providers, there will be
+    no nodes to list.
     '''
     return {}
 
@@ -103,13 +105,13 @@ def create(vm_):
     Provision a single machine
     '''
     log.info('Provisioning existing machine {0}'.format(vm_['name']))
-    deploy_script = script(vm_)
 
-    if __opts__['deploy'] is True:
+    if config.get_config_value('deploy', vm_, __opts__) is True:
         deploy_script = script(vm_)
+        ssh_username = config.get_config_value('ssh_username', vm_, __opts__)
         deploy_kwargs = {
             'host': vm_['ssh_host'],
-            'username': vm_['ssh_username'],
+            'username': ssh_username,
             'script': deploy_script,
             'name': vm_['name'],
             'deploy_command': '/tmp/deploy.sh',
@@ -119,20 +121,18 @@ def create(vm_):
             'minion_pem': vm_['priv_key'],
             'minion_pub': vm_['pub_key'],
             'keep_tmp': __opts__['keep_tmp'],
-            }
-
-        if 'password' in vm_:
-            deploy_kwargs['password'] = vm_['password']
-        elif 'ssh_keyfile' in vm_:
-            deploy_kwargs['key_filename'] = vm_['ssh_keyfile']
-
-        if 'sudo' in vm_:
-            deploy_kwargs['sudo'] = vm_['sudo']
-
-        if 'script_args' in vm_:
-            deploy_kwargs['script_args'] = vm_['script_args']
-
-        deploy_kwargs['minion_conf'] = saltcloud.utils.minion_conf_string(__opts__, vm_)
+            'sudo': config.get_config_value(
+                'sudo', vm_, __opts__, default=(ssh_username != 'root')
+            ),
+            'password': config.get_config_value('password', vm_, __opts__),
+            'ssh_keyfile': config.get_config_value(
+                'ssh_keyfile', vm_, __opts__
+            ),
+            'script_args': config.get_config_value(
+                'script_args', vm_, __opts__
+            ),
+            'minion_conf': saltcloud.utils.minion_conf_string(__opts__, vm_)
+        }
 
         deployed = saltcloud.utils.deploy_script(**deploy_kwargs)
         if deployed:
@@ -149,14 +149,8 @@ def script(vm_):
     '''
     minion = saltcloud.utils.minion_conf_string(__opts__, vm_)
     script = saltcloud.utils.os_script(
-        saltcloud.utils.get_option(
-            'script',
-            __opts__,
-            vm_
-        ),
-        vm_,
-        __opts__,
-        minion,
+        config.get_config_value('script', vm_, __opts__),
+        vm_, __opts__, minion
     )
     return script
 
@@ -166,8 +160,9 @@ def destroy(name, call=None):
     Because this module is not specific to any cloud providers, it is not
     possible to destroy a node.
     '''
-    return {'Error':
-               {'Not Supported':
-                   '--destroy and --action destroy not supported by Saltify'
-               }
-           }
+    return {
+        'Error': {
+            'Not Supported': '--destroy and --action destroy not supported '
+                             'by Saltify'
+        }
+    }
