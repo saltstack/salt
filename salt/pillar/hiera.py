e218e975b3fe4a21720b1d59b27a8f949072a1ab
Thomas S Hatch <thatch45@gmail.com>
2013-04-10 10:48:47 -0600

    update hiera command with -c and escape vars

--- salt/pillar/hiera.py
+++ salt/pillar/hiera.py
@@ -29,10 +29,10 @@ def ext_pillar(pillar, conf):
     '''
     Execute hiera and return the data
     '''
-    cmd = 'hiera {0}'.format(conf)
+    cmd = 'hiera -c {0}'.format(conf)
     for key, val in __grains__.items():
         if isinstance(val, string_types):
-            cmd += ' {0}={1}'.format(key, val)
+            cmd += ' {0}=\'{1}\''.format(key, val)
     try:
         data = yaml.safe_load(__salt__['cmd.run'](cmd))
     except Exception:
