46d923dd49f3582360cab8dea9d96d81ce99e17a
Colton Myers <colton.myers@gmail.com>
2013-04-12 16:07:37 -0600

    Add salt.utils.parse_docstring()
    Currently only parses out dependencies, for use in
    required_module_list().  Can be added to later as needed

--- salt/utils/__init__.py
+++ salt/utils/__init__.py
@@ -969,3 +969,41 @@ def valid_url(url, protos):
     if salt._compat.urlparse.urlparse(url).scheme in protos:
         return True
     return False
+
+
+def parse_docstring(docstring):
+    '''
+    Parse a docstring into its parts.
+
+    Currently only parses dependencies, can be extended to parse whatever is
+    needed.
+
+    Parses into a dictionary:
+        {
+            'full': full docstring,
+            'deps': list of dependencies (empty list if none)
+        }
+    '''
+    # First try with regex search for :depends:
+    ret = {}
+    ret['full'] = docstring
+    regex = r'([ \t]*):depends:[ \t]+- (\w+)[^\n]*\n(\1[ \t]+- (\w+)[^\n]*\n)*'
+    match = re.search(regex, docstring, re.M)
+    if match:
+        deps = []
+        regex = r'- (\w+)'
+        for line in match.group(0).strip().splitlines():
+            deps.append(re.search(regex, line).group(1))
+        ret['deps'] = deps
+        return ret
+    # Try searching for a one-liner instead
+    else:
+        txt = 'Required python modules: '
+        data = docstring.splitlines()
+        dep_list = list(x for x in data if x.strip().startswith(txt))
+        if not dep_list:
+            ret['deps'] = []
+            return ret
+        deps = dep_list[0].replace(txt, '').strip().split(', ')
+        ret['deps'] = deps
+        return ret
