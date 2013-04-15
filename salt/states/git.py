6b0f760107f0cfe999f50954fe1e2e321133bfb2
elfixit <el@xiala.net>
2013-04-15 02:49:11 +0200

    extend git.latest state with remote handling - don't trust origin
    because the name of git.latest is the repository where the target shut be based on.
    I think it is importent that this state does handle remotes correctly and forces them.

--- salt/states/git.py
+++ salt/states/git.py
@@ -39,6 +39,7 @@ def latest(name,
            submodules=False,
            mirror=False,
            bare=False,
+           remote_name='origin',
            identity=None,
            **kwargs):
     '''
@@ -63,6 +64,9 @@ def latest(name,
     bare
         True if the repository is to be a bare clone of the remote repository.
         This is incompatible with rev, as nothing will be checked out.
+    remote_name
+        default ist the well known origin you can also define a different name for the first clone
+        this given name is set to default, else it is just to a additional remote. (Default: 'origin')
     identity
         A path to a private key to use over SSH
     '''
@@ -93,9 +97,29 @@ def latest(name,
                         ('Repository {0} update is probably required (current '
                          'revision is {1})').format(target, current_rev))
 
+                # if remote_name is defined set fetch_opts to remote_name
+                if not remote_name == 'origin':
+                    fetch_opts = remote_name
+                else:
+                    fetch_opts = ''
+
+                # check remote if fetch_url not == name set it
+                remote = __salt__['git.remote_get'](target,
+                                                    remote=remote_name,
+                                                    user=runas,
+                                                    identity=identity)
+                if remote == None or not remote[0] == name:
+                    __salt__['git.remote_set'](target,
+                                               remote_name=remote_name,
+                                               remote_url=name,
+                                               user=runas,
+                                               identity=identity)
+                    ret['changes']['remote/{}'.format(remote_name)] = "{} => {}".format(str(remote), name)
+
                 # check if rev is already present in repo, git-fetch otherwise
                 if bare:
                     __salt__['git.fetch'](target,
+                                          opts=fetch_opts,
                                           user=runas,
                                           identity=identity)
                 elif rev:
@@ -106,6 +130,7 @@ def latest(name,
                                                       runas=runas)
                     if 0 != retcode:
                         __salt__['git.fetch'](target,
+                                              opts=fetch_opts,
                                               user=runas,
                                               identity=identity)
 
@@ -161,6 +186,10 @@ def latest(name,
         try:
             # make the clone
             opts = '--mirror' if mirror else '--bare' if bare else ''
+            # if remote_name is not origin add --origin <name> to opts
+            if not remote_name == 'origin':
+                opts += ' --origin {}'.format(remote_name)
+            # do the clone
             __salt__['git.clone'](target,
                                   name,
                                   user=runas,
