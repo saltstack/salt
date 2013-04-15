a293b3d854f3824b8cb11df624be642a96834f4c
Colton Myers <colton.myers@gmail.com>
2013-04-15 09:56:08 -0600

    Import sed_contains from file.py in win_file.py
    Also fix small inconsistency in the sed escape function

--- salt/modules/win_file.py
+++ salt/modules/win_file.py
@@ -29,7 +29,7 @@ import salt.utils
 from salt.exceptions import SaltInvocationError
 from file import check_hash, check_managed, check_perms, contains_regex,\
         directory_exists, get_managed, makedirs, makedirs_perms, manage_file,\
-        patch, remove, source_list
+        patch, remove, source_list, sed_contains
 
 
 log = logging.getLogger(__name__)
@@ -374,11 +374,16 @@ def find(path, **kwargs):
     return ret
 
 
-def _sed_esc(string):
+def _sed_esc(string, escape_all=False):
     '''
     Escape single quotes and forward slashes
     '''
-    return '{0}'.format(string).replace("'", "'\"'\"'").replace("/", "\/")
+    special_chars = "^.[$()|*+?{"
+    string = string.replace("'", "'\"'\"'").replace("/", "\/")
+    if escape_all is True:
+        for char in special_chars:
+            string = string.replace(char, "\\" + char)
+    return string
 
 
 def sed(path, before, after, limit='', backup='.bak', options='-r -e',
