637b7c63b1f7ccb393b60644c63ec04f737cd4f4
Colton Myers <colton.myers@gmail.com>
2013-04-19 18:56:48 -0600

    Clean up some PEP8 stuff

--- salt/renderers/jinja.py
+++ salt/renderers/jinja.py
@@ -16,23 +16,24 @@ def render(template_file, env='', sls='', argline='',
 
     :rtype: string
     '''
-    from_str = argline=='-s'
+    from_str = argline == '-s'
     if not from_str and argline:
         raise SaltRenderError(
-                  'Unknown renderer option: {opt}'.format(opt=argline)
-              )
-    tmp_data = salt.utils.templates.JINJA(template_file, to_str=True,
-                    salt=__salt__,
-                    grains=__grains__,
-                    opts=__opts__,
-                    pillar=__pillar__,
-                    env=env,
-                    sls=sls,
-                    context=context,
-                    tmplpath=tmplpath,
-                    **kws)
+                'Unknown renderer option: {opt}'.format(opt=argline)
+        )
+    tmp_data = salt.utils.templates.JINJA(template_file,
+                                          to_str=True,
+                                          salt=__salt__,
+                                          grains=__grains__,
+                                          opts=__opts__,
+                                          pillar=__pillar__,
+                                          env=env,
+                                          sls=sls,
+                                          context=context,
+                                          tmplpath=tmplpath,
+                                          **kws)
     if not tmp_data.get('result', False):
-        raise SaltRenderError(tmp_data.get('data',
-            'Unknown render error in jinja renderer'))
+        raise SaltRenderError(
+                tmp_data.get('data', 'Unknown render error in jinja renderer')
+        )
     return StringIO(tmp_data['data'])
-
