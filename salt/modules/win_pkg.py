32027a1beba30ee00945911840acd24a758ab42c
Erik Johnson <palehose@gmail.com>
2013-04-21 23:37:46 -0500

    win_pkg: multi-pkg for remove/purge, optimizations
    This commit brings support for the "pkgs" argument to the remove and
    purge functions. In addition, the use of __context__ has been added to
    list_pkgs. All functions which modify the package database clear the
    __context__ variable before running list_pkgs again to ensure that
    changes are detected.

--- salt/modules/win_pkg.py
+++ salt/modules/win_pkg.py
@@ -18,6 +18,7 @@ except ImportError:
     HAS_DEPENDENCIES = False
 
 # Import python libs
+import copy
 import logging
 import msgpack
 import os
@@ -36,17 +37,6 @@ def __virtual__():
     if salt.utils.is_windows() and HAS_DEPENDENCIES:
         return 'pkg'
     return False
-
-
-def _list_removed(old, new):
-    '''
-    List the packages which have been removed between the two package objects
-    '''
-    pkgs = []
-    for pkg in old:
-        if pkg not in new:
-            pkgs.append(pkg)
-    return pkgs
 
 
 def latest_version(*names, **kwargs):
@@ -208,22 +198,32 @@ def list_pkgs(*args, **kwargs):
     '''
     versions_as_list = \
         salt.utils.is_true(kwargs.get('versions_as_list'))
-    pkgs = {}
+
+    if 'pkg.list_pkgs' in __context__:
+        if versions_as_list:
+            return __context__['pkg.list_pkgs']
+        else:
+            ret = copy.deepcopy(__context__['pkg.list_pkgs'])
+            __salt__['pkg_resource.stringify'](ret)
+            return ret
+
+    ret = {}
     with salt.utils.winapi.Com():
         if len(args) == 0:
             for key, val in _get_reg_software().iteritems():
-                __salt__['pkg_resource.add_pkg'](pkgs, key, val)
+                __salt__['pkg_resource.add_pkg'](ret, key, val)
             for key, val in _get_msi_software().iteritems():
-                __salt__['pkg_resource.add_pkg'](pkgs, key, val)
+                __salt__['pkg_resource.add_pkg'](ret, key, val)
         else:
             # get package version for each package in *args
             for arg in args:
                 for key, val in _search_software(arg).iteritems():
-                    __salt__['pkg_resource.add_pkg'](pkgs, key, val)
+                    __salt__['pkg_resource.add_pkg'](ret, key, val)
 
-    __salt__['pkg_resource.sort_pkglist'](pkgs)
+    __salt__['pkg_resource.sort_pkglist'](ret)
+    __context__['pkg.list_pkgs'] = ret
     if not versions_as_list:
-        __salt__['pkg_resource.stringify'](pkgs)
+        __salt__['pkg_resource.stringify'](ret)
     return pkgs
 
 
@@ -431,18 +431,16 @@ def install(name=None, refresh=False, **kwargs):
         cached_pkg = __salt__['cp.is_cached'](pkginfo[version]['installer'])
         if not cached_pkg:
             # It's not cached. Cache it, mate.
-            cached_pkg = __salt__['cp.cache_file'](pkginfo[
-                                                   version]['installer'])
+            cached_pkg = \
+                __salt__['cp.cache_file'](pkginfo[version]['installer'])
     else:
         cached_pkg = pkginfo[version]['installer']
     cached_pkg = cached_pkg.replace('/', '\\')
     cmd = '"' + str(cached_pkg) + '"' + str(pkginfo[version]['install_flags'])
-    if 'msiexec' in pkginfo[version]:
-        if pkginfo[version]['msiexec']:
-            cmd = 'msiexec /i ' + cmd
-    stderr = __salt__['cmd.run_all'](cmd).get('stderr', '')
-    if stderr:
-        log.error(stderr)
+    if pkginfo[version].get('msiexec'):
+        cmd = 'msiexec /i ' + cmd
+    __salt__['cmd.run_all'](cmd)
+    __context__.pop('pkg.list_pkgs', None)
     new = list_pkgs()
     return __salt__['pkg_resource.find_changes'](old, new)
 
@@ -469,56 +467,98 @@ def upgrade(refresh=True):
     return {}
 
 
-def remove(name, version=None, **kwargs):
+def remove(name=None, pkgs=None, version=None, **kwargs):
     '''
-    Remove a single package
+    Remove packages.
 
-    Return a list containing the removed packages.
+    name
+        The name of the package to be deleted.
+
+    version
+        The version of the package to be deleted. If this option is used in
+        combination with the ``pkgs`` option below, then this version will be
+        applied to all targeted packages.
+
+    Multiple Package Options:
+
+    pkgs
+        A list of packages to delete. Must be passed as a python list. The
+        ``name`` parameter will be ignored if this option is passed.
+
+
+    Returns a dict containing the changes.
 
     CLI Example::
 
         salt '*' pkg.remove <package name>
+        salt '*' pkg.remove <package1>,<package2>,<package3>
+        salt '*' pkg.remove pkgs='["foo", "bar"]'
     '''
+    pkg_params = __salt__['pkg_resource.parse_targets'](name, pkgs)[0]
     old = list_pkgs()
-    pkginfo = _get_package_info(name)
-    if not version:
-        version = _get_latest_pkg_version(pkginfo)
+    targets = [x for x in pkg_params if x in old]
+    if not targets:
+        return {}
 
-    if pkginfo[version]['uninstaller'].startswith('salt:'):
-        cached_pkg = __salt__['cp.is_cached'](pkginfo[version]['uninstaller'])
-        if not cached_pkg:
-            # It's not cached. Cache it, mate.
-            cached_pkg = __salt__['cp.cache_file'](pkginfo[
-                                                   version]['uninstaller'])
-    else:
-        cached_pkg = pkginfo[version]['uninstaller']
-    cached_pkg = cached_pkg.replace('/', '\\')
-    if not os.path.exists(os.path.expandvars(cached_pkg)) and '(x86)' in cached_pkg:
-        cached_pkg = cached_pkg.replace('(x86)', '')
-    cmd = '"' + str(os.path.expandvars(
-        cached_pkg)) + '"' + str(pkginfo[version]['uninstall_flags'])
-    if 'msiexec'in pkginfo[version]:
-        if pkginfo[version]['msiexec']:
+    for target in targets:
+        pkginfo = _get_package_info(target)
+        if not version:
+            version = _get_latest_pkg_version(pkginfo)
+
+        if pkginfo[version]['uninstaller'].startswith('salt:'):
+            cached_pkg = \
+                __salt__['cp.is_cached'](pkginfo[version]['uninstaller'])
+            if not cached_pkg:
+                # It's not cached. Cache it, mate.
+                cached_pkg = \
+                    __salt__['cp.cache_file'](pkginfo[version]['uninstaller'])
+        else:
+            cached_pkg = pkginfo[version]['uninstaller']
+        cached_pkg = cached_pkg.replace('/', '\\')
+        if not os.path.exists(os.path.expandvars(cached_pkg)) \
+                and '(x86)' in cached_pkg:
+            cached_pkg = cached_pkg.replace('(x86)', '')
+        cmd = '"' + str(os.path.expandvars(
+            cached_pkg)) + '"' + str(pkginfo[version]['uninstall_flags'])
+        if pkginfo[version].get('msiexec'):
             cmd = 'msiexec /x ' + cmd
-    stderr = __salt__['cmd.run_all'](cmd).get('stderr', '')
-    if stderr:
-        log.error(stderr)
+        __salt__['cmd.run_all'](cmd)
+
+    __context__.pop('pkg.list_pkgs', None)
     new = list_pkgs()
     return __salt__['pkg_resource.find_changes'](old, new)
 
 
-def purge(name, version=None, **kwargs):
+def purge(name=None, pkgs=None, version=None, **kwargs):
     '''
-    Recursively remove a package and all dependencies which were installed
-    with it
+    Package purges are not supported, this function is identical to
+    ``remove()``.
 
-    Return a list containing the removed packages.
+    name
+        The name of the package to be deleted.
+
+    version
+        The version of the package to be deleted. If this option is used in
+        combination with the ``pkgs`` option below, then this version will be
+        applied to all targeted packages.
+
+
+    Multiple Package Options:
+
+    pkgs
+        A list of packages to delete. Must be passed as a python list. The
+        ``name`` parameter will be ignored if this option is passed.
+
+
+    Returns a dict containing the changes.
 
     CLI Example::
 
         salt '*' pkg.purge <package name>
+        salt '*' pkg.purge <package1>,<package2>,<package3>
+        salt '*' pkg.purge pkgs='["foo", "bar"]'
     '''
-    return remove(name, version, **kwargs)
+    return remove(name=name, pkgs=pkgs, version=version, **kwargs)
 
 
 def _get_package_info(name):
