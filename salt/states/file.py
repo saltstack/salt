e02f6d5b3d26f9adcd17fdb97b661ffd727d6ceb
Pedro Algarvio <pedro@algarvio.me>
2013-04-25 21:00:12 +0100

    Fix `salt.states.file.directory()`. The logic was not accurate.

--- salt/states/file.py
+++ salt/states/file.py
@@ -904,14 +904,21 @@ def directory(name,
 
     if not os.path.isdir(name):
         # The dir does not exist, make it
-        if makedirs:
-            __salt__['file.mkdir'](
-                name, user=user, group=group, mode=dir_mode
-            )
-        else:
-            return _error(
-                ret, 'No directory to create {0} in'.format(name))
+        if not os.path.isdir(os.path.dirname(name)):
+            # The parent directory does not exist, create them
+            if makedirs:
+                __salt__['file.makedirs'](
+                    name, user=user, group=group, mode=dir_mode, makedirs=True
+                )
+            else:
+                return _error(
+                    ret, 'No directory to create {0} in'.format(name))
+
+        __salt__['file.mkdir'](
+            name, user=user, group=group, mode=dir_mode
+        )
         ret['changes'][name] = 'New Dir'
+
     if not os.path.isdir(name):
         return _error(ret, 'Failed to create directory {0}'.format(name))
 
