b3c1f5913df2c0176cf37276c9e8b702ba56b6f4
Thomas S Hatch <thatch45@gmail.com>
2013-05-01 12:30:15 -0600

    Add get_hash function to utils

--- salt/utils/__init__.py
+++ salt/utils/__init__.py
@@ -1065,3 +1065,26 @@ def safe_walk(dir_):
     for sdir in dirs:
         for ret in walk(sdir):
             yield ret
+
+
+def get_hash(path, form='md5', chunk_size=4096):
+    '''
+    Get the hash sum of a file
+
+    This is better than ``get_sum`` for the following reasons:
+        - It does not read the entire file into memory.
+        - It does not return a string on error. The returned value of
+            ``get_sum`` cannot really be trusted since it is vulnerable to
+            collisions: ``get_sum(..., 'xyz') == 'Hash xyz not supported'``
+    '''
+    try:
+        hash_type = getattr(hashlib, form)
+    except AttributeError:
+        raise ValueError('Invalid hash type: {0}'.format(form))
+    with salt.utils.fopen(path, 'rb') as ifile:
+        hash_obj = hash_type()
+        while True:
+            chunk = ifile.read(chunk_size)
+            if not chunk:
+                return hash_obj.hexdigest()
+            hash_obj.update(chunk)
