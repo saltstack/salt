10175caf400f8b3377430ecdfc418353dda3943f
Chris Rebert <chris.rebert@hulu.com>
2013-04-30 19:40:11 -0700

    add whitespace around operators based on pylint report

--- salt/runners/launchd.py
+++ salt/runners/launchd.py
@@ -7,7 +7,7 @@ def write_launchd_plist(program):
     '''
     Write a launchd plist for managing salt-master or salt-minion
     '''
-    plist_sample_text="""
+    plist_sample_text = """
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
