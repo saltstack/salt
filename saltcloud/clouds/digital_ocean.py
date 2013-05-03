8215e70385bd3e99ac598b34ae87295e559e1470
Pedro Algarvio <pedro@algarvio.me>
2013-05-03 13:19:35 +0100

    Implement master deployment to Digital Ocean along with minion keys pre-seeding support.

--- saltcloud/clouds/digital_ocean.py
+++ saltcloud/clouds/digital_ocean.py
@@ -275,14 +275,33 @@ def create(vm_):
             'minion_pem': vm_['priv_key'],
             'minion_pub': vm_['pub_key'],
             'keep_tmp': __opts__['keep_tmp'],
+            'preseed_minion_keys': vm_.get('preseed_minion_keys', None),
+            'display_ssh_output': config.get_config_value(
+                'display_ssh_output', vm_, __opts__, default=True
+            ),
             'script_args': config.get_config_value(
                 'script_args', vm_, __opts__
-            )
+            ),
+            'minion_conf': saltcloud.utils.minion_conf_string(__opts__, vm_)
         }
 
-        deploy_kwargs['minion_conf'] = saltcloud.utils.minion_conf_string(
-            __opts__, vm_
+        # Deploy salt-master files, if necessary
+        if config.get_config_value('make_master', vm_, __opts__) is True:
+            deploy_kwargs['make_master'] = True
+            deploy_kwargs['master_pub'] = vm_['master_pub']
+            deploy_kwargs['master_pem'] = vm_['master_pem']
+            master_conf = saltcloud.utils.master_conf_string(__opts__, vm_)
+            if master_conf:
+                deploy_kwargs['master_conf'] = master_conf
+
+            if master_conf.get('syndic_master', None):
+                deploy_kwargs['make_syndic'] = True
+
+        deploy_kwargs['make_minion'] = config.get_config_value(
+            'make_minion', vm_, __opts__, default=True
         )
+
+        # Store what was used to the deploy the VM
         ret['deploy_kwargs'] = deploy_kwargs
 
         deployed = saltcloud.utils.deploy_script(**deploy_kwargs)
