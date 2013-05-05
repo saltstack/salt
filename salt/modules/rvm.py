1d6be142e1585ad5f8b8a0ac638213baed6f9cac
Chris Rebert <github@rebertia.com>
2013-05-05 02:53:24 -0700

    should fix gemset_empty WRT #4681

--- salt/modules/rvm.py
+++ salt/modules/rvm.py
@@ -269,7 +269,7 @@ def gemset_empty(ruby, gemset, runas=None):
 
         salt '*' rvm.gemset_empty 2.0.0 foobar
     '''
-    return _rvm_do(ruby, 'rvm --force gemset empty', gemset, runas=runas)
+    return _rvm_do(ruby, 'rvm --force gemset empty {gemset}'.format(gemset=gemset), runas=runas)
 
 
 def gemset_copy(source, destination, runas=None):
