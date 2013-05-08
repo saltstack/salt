e576a27940d9c4d64804d32bde55dbe12dae6503
Pedro Algarvio <pedro@algarvio.me>
2013-05-08 17:54:20 +0100

    Fail as soon as possible if the SSH key cannot be found for Libcloud AWS

--- saltcloud/clouds/libcloud_aws.py
+++ saltcloud/clouds/libcloud_aws.py
@@ -285,6 +285,16 @@ def create(vm_):
     '''
     Create a single VM from a data dict
     '''
+    key_filename = config.get_config_value(
+        'private_key', vm_, __opts__, search_global=False, default=None
+    )
+    if key_filename is not None and not os.path.isfile(key_filename):
+        raise SaltCloudConfigError(
+            'The defined key_filename {0!r} does not exist'.format(
+                key_filename
+            )
+        )
+
     location = get_location(vm_)
     log.info('Creating Cloud VM {0} in {1}'.format(vm_['name'], location))
     conn = get_conn(location=location)
@@ -354,10 +364,10 @@ def create(vm_):
     username = 'ec2-user'
     if saltcloud.utils.wait_for_ssh(ip_address):
         for user in usernames:
-            if saltcloud.utils.wait_for_passwd(
-                    host=ip_address, username=user, ssh_timeout=60,
-                    key_filename=config.get_config_value(
-                        'private_key', vm_, __opts__, search_global=False)):
+            if saltcloud.utils.wait_for_passwd(host=ip_address,
+                                               username=user,
+                                               ssh_timeout=60,
+                                               key_filename=key_filename):
                 username = user
                 break
         else:
@@ -369,9 +379,7 @@ def create(vm_):
         deploy_kwargs = {
             'host': ip_address,
             'username': username,
-            'key_filename': config.get_config_value(
-                'private_key', vm_, __opts__, search_global=False
-            ),
+            'key_filename': key_filename,
             'deploy_command': 'sh /tmp/deploy.sh',
             'tty': True,
             'script': deploy_script.script,
