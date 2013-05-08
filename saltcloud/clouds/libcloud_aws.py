8a2924904c7d1f0ab36fb029a080f082abe90f22
Pedro Algarvio <pedro@algarvio.me>
2013-05-08 19:16:34 +0100

    Fix the logic for make_syndic in Libcloud AWS

--- saltcloud/clouds/libcloud_aws.py
+++ saltcloud/clouds/libcloud_aws.py
@@ -408,9 +408,10 @@ def create(vm_):
             deploy_kwargs['make_master'] = True
             deploy_kwargs['master_pub'] = vm_['master_pub']
             deploy_kwargs['master_pem'] = vm_['master_pem']
-            master_conf = saltcloud.utils.master_conf_string(__opts__, vm_)
-            if master_conf:
-                deploy_kwargs['master_conf'] = master_conf
+            master_conf = saltcloud.utils.master_conf(__opts__, vm_)
+            deploy_kwargs['master_conf'] = saltcloud.utils.salt_config_to_yaml(
+                master_conf
+            )
 
             if master_conf.get('syndic_master', None):
                 deploy_kwargs['make_syndic'] = True
