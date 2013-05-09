9e76df06c1566c2796a9d3129ab4fa18c32a5136
Pedro Algarvio <pedro@algarvio.me>
2013-05-09 21:17:27 +0100

    Simplify the non blocking Popen.

--- saltcloud/utils/nb_popen.py
+++ saltcloud/utils/nb_popen.py
@@ -18,71 +18,83 @@ import logging
 import subprocess
 
 
+log = logging.getLogger(__name__)
+
+
 class NonBlockingPopen(subprocess.Popen):
 
     def __init__(self, *args, **kwargs):
         self.stream_stds = kwargs.pop('stream_stds', False)
-        self.olog = logging.getLogger('{0}.stdout'.format(__name__))
-        self.elog = logging.getLogger('{0}.stderr'.format(__name__))
         super(NonBlockingPopen, self).__init__(*args, **kwargs)
-        if self.stdout is not None and self.stream_stds:
+
+        if self.stdout is not None:
             fod = self.stdout.fileno()
             fol = fcntl.fcntl(fod, fcntl.F_GETFL)
             fcntl.fcntl(fod, fcntl.F_SETFL, fol | os.O_NONBLOCK)
-            self.obuff = ''
+        self.obuff = ''
 
-        if self.stderr is not None and self.stream_stds:
+        if self.stderr is not None:
             fed = self.stderr.fileno()
             fel = fcntl.fcntl(fed, fcntl.F_GETFL)
             fcntl.fcntl(fed, fcntl.F_SETFL, fel | os.O_NONBLOCK)
-            self.ebuff = ''
+        self.ebuff = ''
+
+        log.info('Running command {0!r}'.format(*args))
 
     def poll(self):
         poll = super(NonBlockingPopen, self).poll()
 
-        if self.stdout is not None and self.stream_stds:
+        if self.stdout is not None:
             try:
                 obuff = self.stdout.read()
                 self.obuff += obuff
                 if obuff:
-                    self.olog.warn(obuff)
-                #sys.stdout.write(obuff)
+                    logging.getLogger(
+                        'saltcloud.Popen.STDOUT.PID-{0}'.format(self.pid)
+                    ).warn(obuff.rstrip())
+                    if self.stream_stds:
+                        sys.stdout.write(obuff)
             except IOError, err:
                 if err.errno not in (11, 35):
                     # We only handle Resource not ready properly, any other
                     # raise the exception
                     raise
-        if self.stderr is not None and self.stream_stds:
+
+        if self.stderr is not None:
             try:
                 ebuff = self.stderr.read()
                 self.ebuff += ebuff
                 if ebuff:
-                    self.elog.warn(ebuff)
-                #sys.stderr.write(ebuff)
+                    logging.getLogger(
+                        'saltcloud.Popen.STDERR.PID-{0}'.format(self.pid)
+                    ).warn(ebuff.rstrip())
+                    if self.stream_stds:
+                        sys.stderr.write(ebuff)
             except IOError, err:
                 if err.errno not in (11, 35):
                     # We only handle Resource not ready properly, any other
                     # raise the exception
                     raise
 
-        if poll is None:
-            # Not done yet
-            return poll
-
-        if not self.stream_stds:
-            # Allow the same attribute access even though not streaming to stds
-            try:
-                self.obuff = self.stdout.read()
-            except IOError, err:
-                if err.errno not in (11, 35):
-                    # We only handle Resource not ready properly, any other
-                    # raise the exception
-                    raise
-            try:
-                self.ebuff = self.stderr.read()
-            except IOError, err:
-                if err.errno not in (11, 35):
-                    # We only handle Resource not ready properly, any other
-                    # raise the exception
-                    raise
         return poll
+
+    def __del__(self):
+        if self.stdout is not None:
+            try:
+                fod = self.stdout.fileno()
+                fol = fcntl.fcntl(fod, fcntl.F_GETFL)
+                fcntl.fcntl(fod, fcntl.F_SETFL, fol & ~os.O_NONBLOCK)
+            except ValueError:
+                # Closed FD
+                pass
+
+        if self.stderr is not None:
+            try:
+                fed = self.stderr.fileno()
+                fel = fcntl.fcntl(fed, fcntl.F_GETFL)
+                fcntl.fcntl(fed, fcntl.F_SETFL, fel & ~os.O_NONBLOCK)
+            except ValueError:
+                # Closed FD
+                pass
+
+        super(NonBlockingPopen, self).__del__()
