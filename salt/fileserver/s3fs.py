d8d39423fa08ebd3fde4fe9ab4303df6724ef7d7
Alan Garfield <alan@fromorbit.com>
2013-05-10 20:29:28 +1000

    [WIP] added cache expiry and fixing hashing

--- salt/fileserver/s3fs.py
+++ salt/fileserver/s3fs.py
@@ -1,25 +1,29 @@
 '''
-EXPERIMENTAL, subject to change
-----------------------------------
-The backend for the s3 service.
+The backend for the s3 based file server system.
 
-Example configuration options in master file:
+S3 credentials can be either set in the master file using:
 
 s3_key_id: XXXXXXXXXXXXXXX
 s3_server_access_key: XXXXXXXXXXXXXXX
+
+
+If on EC2 these credentials can be automatically loaded from the
+instance metadata
+
+Example configuration options in master file:
+
 s3_buckets:
   env:
     - bucket1
-    - bucket2    
-    
-
-:depends: git-python Python module
+    - bucket2
 '''
 
 # Import python libs
 import os
 import hashlib
 import logging
+import time
+import json
 
 # Import salt libs
 import salt.fileserver
@@ -27,156 +31,233 @@ import salt.utils
 import salt.utils.s3 as s3
 import salt.modules
 
-
 log = logging.getLogger(__name__)
+
+_s3_cache_expire = 300 # cache for 5 minutes
+_s3_cache_dir = 's3cache'
+_s3_cache_file = os.path.join(_s3_cache_dir, 'buckets_files.cache')
+
+#__opts__ = []
 
 def init():
     '''
-        Fetches data for the rest of the module to work off
+    Connects to S3 and downloads metadata about each file in all
+    buckets specified within and environment and saves them to disk
     '''
-    fetch_result_set()   
+    cache_file = os.path.join(__opts__['cachedir'], _s3_cache_file)
 
-def find_file(path, env='base', **kwargs):
+    # check for cache file
+    if os.path.isfile(cache_file) and os.path.getmtime(cache_file) > time.time() - _s3_cache_expire:
+        log.error('CACHE HIT!')
+        with salt.utils.fopen(cache_file, 'rb') as fp_:
+            data = fp_.read()
+            return eval(data)
+
+    else:
+        log.error('CACHE MISS!')
+        return refresh_cache()
+
+def refresh_cache():
     '''
-    Looks through the buckets to find the first matching file and formats it 
+    Retrieves the contents of the buckets and caches them to a file
     '''
+    s3_key_id =  __opts__['s3_key_id'] if __opts__['s3_key_id'] else None
+    s3_server_access_key = __opts__['s3_server_access_key'] if __opts__['s3_server_access_key'] else None
 
-    fnd = {'bucket':   '',
-           'path'  :   '',
-           'md5'   :   ''}
+    cache_file = os.path.join(__opts__['cachedir'], _s3_cache_file)
 
-    if env not in __opts__['s3_buckets']:
-        return fnd    
-    ret = []
-    resultset = get_result_set()
-    for path,val in resultset[env].items():
-        fileData = filter(lambda key: key.has_key("Key"), val)
-        filePaths = map(lambda key: key["Key"], fileData)
-        filesOnly = filter(lambda key: key.endswith('/') == False, filePaths)
-        filesWithBucket = map(lambda key: path+"//"+key, filesOnly)
-        ret += filesWithBucket
+    resultset = {}
+    for env, buckets in __opts__['s3_buckets'].items():
+        bucket_files = {}
+        for bucket_name in buckets:
+            try:
+                bucket_files[bucket_name] = s3.query(
+                        key = s3_server_access_key,
+                        keyid = s3_key_id,
+                        bucket = bucket_name,
+                        return_bin = False)
 
-    fnd['bucket'] = filter(lambda key: key.split('//')[0] == path , ret)[0].split('//')[0]
-    fnd['path']   = filter(lambda key: key.split('//')[0] == path , ret)[0].split('//')[1]
-    fnd['rel'] = fnd['path']
-    bucket = fnd["bucket"]
-    path = fnd["path"]
-    fileData = filter(lambda key: key.has_key("Key"),  resultset[env][bucket])
-    item =  filter(lambda key: key["Key"] == path , fileData)[0]
-    fnd['md5'] = item["Key"]
+            except TypeError:
+                log.error('There was an error accessing the {0} bucket'.format(bucket_name))
 
-    return fnd        
+        resultset[env] = bucket_files
+
+    if not os.path.exists(_s3_cache_dir):
+        os.makedirs(_s3_cache_dir)
+
+    if os.path.isfile(cache_file):
+        os.remove(cache_file)
+
+    with salt.utils.fopen(cache_file, 'w') as fp_:
+        fp_.write(str(resultset))
+
+    # return the resultset
+    return resultset
+
+def update():
+    '''
+    Check if the cache_file is up to date
+    '''
+    init()
+
 def envs():
     '''
     Return the file server environments
     '''
     return __opts__['s3_buckets'].keys()
 
+def find_file(path, env='base', **kwargs):
+    '''
+    Looks through the buckets to find the first matching file and formats it
+    '''
+    log.error('******************************** find_file');
+    log.error('path: {0}'.format(json.dumps(path)))
+    log.error('env: {0}'.format(json.dumps(env)))
+    log.error('args: {0}'.format(json.dumps(kwargs)))
+
+    fnd = {'bucket': None,
+            'path' : None}
+
+    filepath = '/'.join([env, path])
+
+    resultset = init()
+    if env not in resultset:
+        return fnd
+
+    ret = _find_file(resultset[env])
+
+    for bucket, files in ret.iteritems():
+        if filepath in files:
+            fnd['bucket'] = bucket
+            fnd['path'] = filepath
+
+    # grab the file from S3 if it doesn't exist in the cache
+    #if fnd['path'] and fnd['bucket']:
+    #if not os.path.exists(absdirectory):
+        #os.makedirs(absdirectory)
+
+    #if os.path.isfile(abspath):
+        #with salt.utils.fopen(abspath, 'rb') as fp_:
+            #diskmd5 = hashlib.md5(fp_.read()).hexdigest()
+
+            #if diskmd5 == fnd["md5"]:
+                #ret['dest'] = bucket+"//"+path
+                #ret['data'] = fp_.read()
+    #else:
+        #with salt.utils.fopen(abspath, 'w') as fp_:
+            #fetchedFile = s3.query(s3_server_access_key,s3_key_id,bucket=bucket,path=path,return_bin=True)
+            #fp_.write(fetchedFile)
+
+    return fnd
 
 def serve_file(load, fnd):
-    s3_key_id =  __opts__['s3_key_id'] if __opts__['s3_key_id'] else None 
-    s3_server_access_key = __opts__['s3_server_access_key'] if __opts__['s3_server_access_key'] else None 
+    '''
+    Serve the file chunks out to the minion
+    '''
+    log.error('******************************** serve_file');
+    log.error('load: {0}'.format(json.dumps(load)))
+    log.error('fnd: {0}'.format(json.dumps(fnd)))
 
-    env = load["env"]
+    ret = {}
+    #s3_key_id =  __opts__['s3_key_id'] if __opts__['s3_key_id'] else None
+    #s3_server_access_key = __opts__['s3_server_access_key'] if __opts__['s3_server_access_key'] else None
+
+    env = load["env"] # FIXME - IMPLEMENT ENV
     bucket = fnd["bucket"]
     path = fnd["path"]
 
-    abspath = os.path.join( __opts__['cachedir'],'s3cache',bucket,fnd["path"])
-    absdirectory = abspath[0:abspath.rfind("/")]
-    ret = {}
+    abspath = os.path.join( __opts__['cachedir'], _s3_cache_dir, bucket, fnd['path'])
+    absdirectory = os.path.dirname(abspath)
+
+    log.error(json.dumps(fnd))
+    log.error(env)
+    log.error(abspath)
+    log.error(absdirectory)
+
+    return ret
+
+    #if not os.path.exists(absdirectory):
+        #os.makedirs(absdirectory)
+
+    #if os.path.isfile(abspath):
+        #with salt.utils.fopen(abspath, 'rb') as fp_:
+            #diskmd5 = hashlib.md5(fp_.read()).hexdigest()
+
+            #if diskmd5 == fnd["md5"]:
+                #ret['dest'] = bucket+"//"+path
+                #ret['data'] = fp_.read()
+    #else:
+        #with salt.utils.fopen(abspath, 'w') as fp_:
+            #fetchedFile = s3.query(s3_server_access_key,s3_key_id,bucket=bucket,path=path,return_bin=True)
+            #fp_.write(fetchedFile)
 
 
-    if not os.path.exists(absdirectory):
-        os.makedirs(absdirectory)
-    if os.path.isfile(abspath):
-            with salt.utils.fopen(abspath, 'rb') as fp_:
-                diskmd5 = hashlib.md5(fp_.read()).hexdigest()
-                if diskmd5 == fnd["md5"]:
-                    ret['dest'] = bucket+"//"+path
-                    ret['data'] = fp_.read()
-    else:
-        with salt.utils.fopen(abspath, 'w') as fp_:
-            fetchedFile = s3.query(s3_server_access_key,s3_key_id,bucket=bucket,path=path,return_bin=True)
-            fp_.write(fetchedFile)
-
-
-    gzip = load.get('gzip', None)
-    with salt.utils.fopen(abspath, 'rb') as fp_:
-        fp_.seek(load['loc'])
-        data = fp_.read(__opts__['file_buffer_size'])
-        if gzip and data:
-            data = salt.utils.gzip_util.compress(data, gzip)
-            ret['gzip'] = gzip
-        ret['data'] = data
-        ret['dest'] = bucket+"//"+path
-        return ret
+    #gzip = load.get('gzip', None)
+    #with salt.utils.fopen(abspath, 'rb') as fp_:
+        #fp_.seek(load['loc'])
+        #data = fp_.read(__opts__['file_buffer_size'])
+        #if gzip and data:
+            #data = salt.utils.gzip_util.compress(data, gzip)
+            #ret['gzip'] = gzip
+        #ret['data'] = data
+        #ret['dest'] = bucket+"//"+path
+        #return ret
 
 def file_hash(load, fnd):
     '''
-        Provides Salt with 
+    Provides Salt with
     '''
-    ret = {}
-    resultset = get_result_set()
-    env = load["env"]
-    bucket = fnd["bucket"]
-    path = fnd["path"]
+    log.error('******************************** find_hash')
+    log.error('load: {0}'.format(json.dumps(load)))
+    log.error('fnd: {0}'.format(json.dumps(fnd)))
 
-    fileData = filter(lambda key: key.has_key("Key"),  resultset[env][bucket])
-    item =  filter(lambda key: key["Key"] == path , fileData)[0]
-    ret['hsum'] = item["ETag"]
-    ret['hash_type'] = "md5"
-    return ret 
+    #ret = {}
+    #resultset = get_result_set()
+    #env = load["env"]
+    #bucket = fnd["bucket"]
+    #path = fnd["path"]
+
+    #fileData = filter(lambda key: key.has_key("Key"),  resultset[env][bucket])
+    #item =  filter(lambda key: key["Key"] == path , fileData)[0]
+    #ret['hsum'] = item["ETag"]
+    #ret['hash_type'] = "md5"
+    #return ret
 
 def file_list(load):
     '''
     Return a list of all files on the file server in a specified
     environment
-    '''    
+    '''
+    log.error('******************************** file_list')
+    log.error('load: {0}'.format(json.dumps(load)))
+
     ret = []
-    resultset = get_result_set()
     env = load['env']
-    for path,val in resultset[env].items():
-        fileData = filter(lambda key: key.has_key("Key"), val)
-        filePaths = map(lambda key: key["Key"], fileData)
-        filesOnly = filter(lambda key: key.endswith('/') == False, filePaths)
-        filesWithBucket = map(lambda key: path+"//"+key, filesOnly)
-        ret += filesWithBucket
+
+    resultset = init()
+
+    if env not in resultset:
+        return ret
+
+    for files in _find_file(resultset[env]).values():
+        ret += files
+
+    log.error(json.dumps(ret))
     return ret
 
-def fetch_result_set():
+def _find_file(resultset):
     '''
-    Connects to S3 and downloads metadata about each file in all 
-    buckets specified within and environment and saves them to disk
-    '''    
-
-    s3_key_id =  __opts__['s3_key_id'] if __opts__['s3_key_id'] else None 
-    s3_server_access_key = __opts__['s3_server_access_key'] if __opts__['s3_server_access_key'] else None 
-
-    resultset = {}
-    for env,buckets in __opts__['s3_buckets'].items():
-        _buckets = {}
-        for val in buckets:
-            try:
-                _buckets[val] = s3.query(s3_server_access_key,s3_key_id,bucket=val,return_bin=False)
-            except TypeError:
-                log.error('There was an error accessing the ' + path + " Bucket")
-        resultset[env] = _buckets
-
-    abspath = os.path.join( __opts__['cachedir'],'s3cache',"apiReturn")
-    absdirectory = abspath[0:abspath.rfind("/")]
-    if not os.path.exists(absdirectory):
-        os.makedirs(absdirectory)
-    if os.path.isfile(abspath):
-        os.remove(abspath)
-    with salt.utils.fopen(abspath, 'w') as fp_:
-        fp_.write(str(resultset))
-
-def get_result_set():
+    Looks for the file in the S3 bucket cache 
     '''
-    Returns cached result set from S3 Api Call in fetch_result_set()
-    '''    
-    abspath = os.path.join( __opts__['cachedir'],'s3cache',"apiReturn")
-    with salt.utils.fopen(abspath, 'rb') as fp_:
-        data = fp_.read()
-        return eval(data)
-   
+    ret = {}
+
+    for bucket in resultset.keys():
+        ret[bucket] = []
+
+    for bucket, data in resultset.iteritems():
+        fileData = filter(lambda key: key.has_key('Key'), data)
+        filePaths = map(lambda key: key['Key'], fileData)
+        ret[bucket] += filter(lambda key: key.endswith('/') == False, filePaths)
+
+    return ret
+
