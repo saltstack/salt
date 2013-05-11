18e04ec39ee27a78ef24d8308e2d8a996d9c552f
rszabo50 <rszabo50@gmail.com>
2013-05-11 08:31:52 -0400

    See note #577
    Significant changes are being made to the joyent api, many new feature and few minor changes to old ones.
    
    The new features will allow you to create images etc.
    
    The changes to the old features appear to be:
    
    list of packages may not have a default field (found this broke the joyent driver in libcloud)
    list of machines request no longer provides a list across all datacenters
    create_node, no longer returns the ip addresses for the node, you have to poll until you get them
    
    Many of you may start to notice things not working. The new api can be tested on the eu-ams-1 location, the other locations still use the old api.

--- saltcloud/libcloudfuncs.py
+++ saltcloud/libcloudfuncs.py
@@ -28,6 +28,11 @@ import saltcloud.utils
 import saltcloud.config as config
 from saltcloud.exceptions import SaltCloudNotFound
 
+#required for determining private subnets
+import socket
+import struct
+
+
 # Get logging started
 log = logging.getLogger(__name__)
 
@@ -331,4 +336,20 @@ def conn_has_method(conn, method_name):
             method_name
         )
     )
+    return False
+
+def is_private_subnet(ip):
+    priv_subnets = [{'subnet': '10.0.0.0', 'mask': '255.0.0.0'},
+                    {'subnet': '172.16.0.0', 'mask': '255.240.0.0'},
+                    {'subnet': '192.168.0.0', 'mask': '255.255.0.0'}]
+
+    ip = struct.unpack('I', socket.inet_aton(ip))[0]
+
+    for network in priv_subnets:
+        subnet = struct.unpack('I', socket.inet_aton(network['subnet']))[0]
+        mask = struct.unpack('I', socket.inet_aton(network['mask']))[0]
+
+        if (ip & mask) == (subnet & mask):
+            return True
+
     return False
