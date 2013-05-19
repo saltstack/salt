8a68dee40825b2efc617aef72469c492abdbb5bc
Pedro Algarvio <pedro@algarvio.me>
2013-05-19 20:28:27 +0100

    Missing import and some formatting love to `salt.{modules,states}.eselect`.
    * Add missing import.
    * Use function alias in order not to shadow `set`
    * Simplify some reading logic
    * PEP-8 and formatting fixes.

--- salt/states/eselect.py
+++ salt/states/eselect.py
@@ -11,13 +11,20 @@ A state module to manage Gentoo configuration via eselect
             target: hardened/linux/amd64
 '''
 
+# Define a function alias in order not to shadow built-in's
+__func_alias__ = {
+    'set_': 'set'
+}
+
+
 def __virtual__():
     '''
     Only load if the eselect module is available in __salt__
     '''
     return 'eselect' if 'eselect.exec_action' in __salt__ else False
 
-def set(name, target):
+
+def set_(name, target):
     '''
     Verify that the given module is set to the given target
 
@@ -25,28 +32,40 @@ def set(name, target):
         The name of the module
     '''
     ret = {'changes': {},
-             'comment': '',
-             'name': name,
-             'result': True}
-    
+           'comment': '',
+           'name': name,
+           'result': True}
+
     old_target = __salt__['eselect.get_current_target'](name)
-    
+
     if target == old_target:
-        ret['comment'] = 'Target "{0}" is already set on "{1}" module.'.format(target,name)
+        ret['comment'] = 'Target {0!r} is already set on {1!r} module.'.format(
+            target, name
+        )
     elif target not in __salt__['eselect.get_target_list'](name):
-        ret['comment'] = 'Target "{0}" is not available for "{1}" module.'.format(target,name)
+        ret['comment'] = (
+            'Target {0!r} is not available for {1!r} module.'.format(
+                target, name
+            )
+        )
         ret['result'] = False
     elif __opts__['test']:
-        ret['comment'] = 'Target "{0}" will be set on "{1}" module.'.format(target,name)
+        ret['comment'] = 'Target {0!r} will be set on {1!r} module.'.format(
+            target, name
+        )
         ret['result'] = None
-        return ret
     else:
-        result = __salt__['eselect.set_target'](name,target)
+        result = __salt__['eselect.set_target'](name, target)
         if result:
-            ret['comment'] = 'Target "{0}" failed to be set on "{1}" module.'.format(target,name)
+            ret['comment'] = (
+                'Target {0!r} failed to be set on {1!r} module.'.format(
+                    target, name
+                )
+            )
             ret['result'] = False
         else:
-            ret['changes'][name] = {'old':old_target,'new':target}
-            ret['comment'] = 'Target "{0}" set on "{1}" module.'.format(target,name)
-    
+            ret['changes'][name] = {'old': old_target, 'new': target}
+            ret['comment'] = 'Target {0!r} set on {1!r} module.'.format(
+                target, name
+            )
     return ret
