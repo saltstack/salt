90af42d759848797aab980262e70b8cb4035f6e8
faust <faust@abnorm.org>
2013-05-23 09:40:16 +0200

    Added correct path for Gentoo iptables config file. Gentoo also have a service to enforce iptables rules, maybe you can consider to handle it in some way. I would have done that by myself, but this module looks to be still under heavy development and I don't want to create new issues.

--- salt/modules/iptables.py
+++ salt/modules/iptables.py
@@ -29,6 +29,8 @@ def _conf():
         return '/etc/sysconfig/iptables'
     elif __grains__['os_family'] == 'Arch':
         return '/etc/iptables/iptables.rules'
+    elif __grains__['os'] == 'Gentoo':
+        return '/var/lib/iptables/rules-save'
     else:
         return False
 
