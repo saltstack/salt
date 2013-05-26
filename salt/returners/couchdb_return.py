d8187c140016832942a895bfbd851eb071c47750
Robert Keizer <robert@keizer.ca>
2013-05-26 09:01:13 -0500

    Working on custom options.

--- salt/returners/couchdb_return.py
+++ salt/returners/couchdb_return.py
@@ -6,7 +6,10 @@ couchdb.db:		'salt'
 couchdb.url:		'http://salt:5984/'
 
 '''
+import logging
 import time
+
+log = logging.getLogger( __name__ )
 
 # Import the required modules.
 try:
@@ -25,23 +28,40 @@ def _get_options( ):
 	Get the couchdb options from salt. Apply defaults
 	if required.
 	'''
-	# Get the configuration options, and set some defaults
-	server_url	= __salt__['config.option']('couchdb.url')
+	server_url = __salt__['config.option']('couchdb.url')
 	if not server_url:
 		server_url = "http://salt:5984/"
-	db_name		= __salt__['config.option']('couchdb.db')
+
+	db_name = __salt__['config.option']('couchdb.db')
 	if not db_name:
 		db_name = "salt"
 
-	return { "url": server_url, "create": create_db, "db": db_name }
+	doc = __salt__['config.option']('couchdb.doc')
+	if not doc:
+		doc = { "time": { "import": ["time"], "key": "timestamp", "value": "time.time()" } }
+
+	return { "url": server_url, "create": create_db, "db": db_name, "doc": doc }
 
 def _generate_doc( ret, options ):
 	'''
 	Create a object that will be saved into the database based on
 	options.
 	'''
+	
+	# Just set the document ID to the jid.
 	r		= ret
 	r["_id"]	= ret["jid"]
+
+	# Iterate though the options["doc"]
+	for _obj in options["doc"]:
+		# If import is defined, iterate through the list and import.
+		if hasattr( _obj, "import" ):
+			pass
+
+		# Set the return object[key] to eval(value) in effect.
+		r[_obj["key"]] = eval( _obj["value"] )
+		
+
 	r["timestamp"]	= time.time( )
 	return r
 
