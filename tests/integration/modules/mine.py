c4c2b7936a89ffbf97d63e4b5d33ea59201c94b0
Thomas S Hatch <thatch45@gmail.com>
2013-05-26 08:55:18 -0600

    Add tests for mine.send

--- tests/integration/modules/mine.py
+++ tests/integration/modules/mine.py
@@ -9,7 +9,15 @@ class MineTest(integration.ModuleCase):
     '''
     def test_get(self):
         '''
-        test mine.get
+        test mine.get and mine.update
         '''
         self.assertTrue(self.run_function('mine.update'))
         self.assertTrue(self.run_function('mine.get', ['minion', 'test.ping']))
+
+    def test_send(self):
+        '''
+        test mine.send
+        '''
+        self.assertFalse(self.run_function('mine.send', ['foo.__spam_and_cheese']))
+        self.assertTrue(self.run_function('mine.send', ['test.retcode']))
+        self.assertTrue(self.run_function('mine.get', ['minion', 'test.retcode']))
