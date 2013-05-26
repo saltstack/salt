b1ec36c1631947a3d3a1c4bd03ecb061a9426ae3
Robert Keizer <robert@keizer.ca>
2013-05-26 00:47:38 -0500

    Little more modular. Paving the way for options.

--- salt/returners/couchdb_return.py
+++ salt/returners/couchdb_return.py
@@ -16,36 +16,51 @@ def __virtual__( ):
 		return False
 	return 'couchdb'
 
-# Get the configuration options, and set some defaults
-server_url	= __salt__['config.option']('couchdb.url')
-if not server_url:
-	server_url = "http://salt:5984/"
-create_db	= __salt__['config.option']('couchdb.create_db')
-if create_db == None:
-	create_db = True
-db_name		= __salt__['config.option']('couchdb.db')
-if not db_name:
-	db_name = "salt"
+def _get_options( ):
+	'''
+	Get the couchdb options from salt. Apply defaults
+	if required.
+	'''
+	# Get the configuration options, and set some defaults
+	server_url	= __salt__['config.option']('couchdb.url')
+	if not server_url:
+		server_url = "http://salt:5984/"
+	create_db	= __salt__['config.option']('couchdb.create_db')
+	if create_db == None:
+		create_db = True
+	db_name		= __salt__['config.option']('couchdb.db')
+	if not db_name:
+		db_name = "salt"
+
+	return { "url": server_url, "create": create_db, "db": db_name }
+
+def _generate_doc( ret, options ):
+	'''
+	Create a object that will be saved into the database.
+	
+	Takes a return object that gets passed into the returner, and
+	also a set of options from the salt configuration.
+	'''
+	r		= ret
+	r["_id"]	= ret["jid"]
+	r["timestamp"]	= time.time( )
+	return r
 
 # Actual returner.
 def returner( ret ):
 	'''
 	Take in the return and shove it into the couchdb database.
 	'''
-	
-	# Get a connection to the couchdb server.
-	couchdb_server	= couchdb.client.Server( server_url )
-	
-	# Create the server if the create_db flag is set and the db
-	# doesn't already exist.
-	if create_db and not db_name in couchdb_server:
-		couchdb_server.create( db_name )
 
-	# Get the database object specifically.
-	db = couchdb_server[db_name]
+	# Get the options from configuration.
+	options = _get_options( )
+	
+	# Create a connection to the server.
+	_s	= couchdb.client.Server( options["server_url"] )
 
-	# Create a temporary obj that we can add to.
-	obj			= ret
-	obj["_id"]		= ret["jid"]
-	obj["timestamp"]	= time.time( )
-	db.save( obj )
+	# Create the database if the configuration calls for it.
+	if options["create_db"] and not options["db"] in _s:
+		server.create( options["db"] )
+	
+	# Save the document that comes out of _generate_doc.
+	_s[options["db"]].save( _generate_doc( ret, options ) )
