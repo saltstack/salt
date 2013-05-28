cb8a169099d10c72a97e0071152cf9555e18f1ac
Chris Rebert <chris.rebert@hulu.com>
2013-05-28 12:09:08 -0700

    fix spacing to comply with PEP 8

--- salt/returners/couchdb_return.py
+++ salt/returners/couchdb_return.py
@@ -15,7 +15,7 @@ couchdb.hooks:      [ { "key": "timestamp", "value": "time.time()", "eval": "imp
 '''
 import logging
 
-log = logging.getLogger( __name__ )
+log = logging.getLogger(__name__)
 
 # Import the required modules.
 try:
@@ -24,68 +24,72 @@ try:
 except ImportError:
     HAS_COUCHDB = False
 
-def __virtual__( ):
+
+def __virtual__():
     if not HAS_COUCHDB:
         return False
     return 'couchdb'
 
-def _get_options( ):
+
+def _get_options():
     '''
     Get the couchdb options from salt. Apply defaults
     if required.
     '''
     server_url = __salt__['config.option']('couchdb.url')
     if not server_url:
-        log.debug( "Using default url." )
+        log.debug("Using default url.")
         server_url = "http://salt:5984/"
 
     db_name = __salt__['config.option']('couchdb.db')
     if not db_name:
-        log.debug( "Using default database." )
+        log.debug("Using default database.")
         db_name = "salt"
 
     hooks = __salt__['config.option']('couchdb.hooks')
     if not hooks:
-        log.debug( "Using default hooks" )
-        hooks = [ { "key": "timestamp", "value": "time.time()", "eval": "import time" } ]
+        log.debug("Using default hooks")
+        hooks = [{"key": "timestamp", "value": "time.time()", "eval": "import time"}]
 
-    return { "url": server_url, "db": db_name, "hooks": hooks }
+    return {"url": server_url, "db": db_name, "hooks": hooks}
 
-def _generate_doc( ret, options ):
+
+def _generate_doc(ret, options):
     '''
     Create a object that will be saved into the database based on
     options.
     '''
-    
+
     # Just set the document ID to the jid.
-    r       = ret
-    r["_id"]    = ret["jid"]
+    r = ret
+    r["_id"] = ret["jid"]
 
     for hook in options["hooks"]:
 
         # Eval if specified.
-        if hasattr( hook, "eval" ):
-            eval( hook["eval"] )
+        if hasattr(hook, "eval"):
+            eval(hook["eval"])
 
-        r[hook["key"]] = eval( hook["value"] )
-        
+        r[hook["key"]] = eval(hook["value"])
+
     return r
 
-def returner( ret ):
+
+def returner(ret):
     '''
     Take in the return and shove it into the couchdb database.
     '''
 
     # Get the options from configuration.
-    options = _get_options( )
-    
+    options = _get_options()
+
     # Create a connection to the server.
-    server  = couchdb.client.Server( options["url"] )
+    server = couchdb.client.Server(options["url"])
 
     # Create the database if the configuration calls for it.
-    if not options["db"] in server:
-        log.debug( "Creating database %s" % options["db"] )
-        server.create( options["db"] )
+    if options["db"] not in server:
+        log.debug("Creating database %s" % options["db"])
+        server.create(options["db"])
 
     # Save the document that comes out of _generate_doc.
-    server[options["db"]].save( _generate_doc( ret, options ) )
+    server[options["db"]].save(_generate_doc(ret, options))
