63859e29159ff860ae1368e9a2d8856a793a5015
Pedro Algarvio <pedro@algarvio.me>
2013-05-29 22:52:58 +0100

    Even more info from travis env.

--- tests/runtests.py
+++ tests/runtests.py
@@ -460,6 +460,8 @@ if __name__ == '__main__':
         code_coverage.save()
         print('Current Directory: {0}'.format(os.getcwd()))
         print('Coverage data file exists? {0}'.format(os.path.isfile('.coverage')))
+        import subprocess
+        subprocess.call(['ls', '-lah'])
 
         report_dir = os.path.join(os.path.dirname(__file__), 'coverage-report')
         print(
