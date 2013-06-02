61a31869a2d87ef8ae1343bd36b8aefc97eb20c9
Sam <sam@edgylogic.com>
2013-06-02 14:40:51 +1000

    Made the new 'host' property actually optional.

--- saltapi/netapi/rest_cherrypy/__init__.py
+++ saltapi/netapi/rest_cherrypy/__init__.py
@@ -92,7 +92,7 @@ def start():
                 apiopts['ssl_crt'], apiopts['ssl_key'])
         wsgi_d = wsgiserver.WSGIPathInfoDispatcher({'/': application})
         server = wsgiserver.CherryPyWSGIServer(
-                (apiopts['host'], apiopts['port']),
+                (apiopts.get('host', '0.0.0.0'), apiopts['port']),
                 wsgi_app=wsgi_d)
         server.ssl_adapter = ssl_a
 
