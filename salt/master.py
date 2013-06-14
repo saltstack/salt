0e3f4d7a2e503bb6b5912a6a3f09da8a6dd389e1
Colton Myers <colton.myers@gmail.com>
2013-06-14 15:23:55 -0600

    Fix regex (\Z, not \z) for autosign checking, Fix #5543

--- salt/master.py
+++ salt/master.py
@@ -1532,7 +1532,7 @@ class ClearFuncs(object):
                 if fnmatch.fnmatch(keyid, line):
                     return True
                 try:
-                    if re.match(r'\A{0}\z'.format(line), keyid):
+                    if re.match(r'\A{0}\Z'.format(line), keyid):
                         return True
                 except re.error:
                     log.warn(
