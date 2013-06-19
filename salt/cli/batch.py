cd6e3ebcb766e1a3cf3bc837316a97a56042a0fb
Volker Schwicking <manager@hop.intern.webpack.hosteurope.de>
2013-06-19 15:41:36 +0200

    the 'ret' dict is not being updated properly which makes the while-loop in line 84 an infinite loop. by adding the response to 'ret' the while loop brakes once all minions have responded

--- salt/cli/batch.py
+++ salt/cli/batch.py
@@ -128,6 +128,7 @@ class Batch(object):
                 if self.opts.get('raw'):
                     yield data
                 else:
+		    ret[minion] = data['ret']
                     yield {minion: data['ret']}
                 if not self.quiet:
                     ret[minion] = data['ret']
