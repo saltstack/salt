ec2ab4d5d3296cbf8f7b7a6ab38f21171439874d
Thomas S Hatch <thatch45@gmail.com>
2013-06-20 11:10:14 -0600

    Fix up tests for decorators

--- tests/integration/files/file/base/_modules/runtests_decorators.py
+++ tests/integration/files/file/base/_modules/runtests_decorators.py
@@ -13,16 +13,22 @@ def working_function():
 
 @salt.utils.decorators.depends('time')
 def depends():
-    return True, time.time()
+    ret = {'ret': True,
+           'time': time.time()}
+    return ret
 
 @salt.utils.decorators.depends('time123')
 def missing_depends():
-    return True, time.time()
+    return True
 
 @salt.utils.decorators.depends('time', fallback_funcion=_fallbackfunc)
 def depends_will_fallback():
-    return True, time.time()
+    ret = {'ret': True,
+           'time': time.time()}
+    return ret
 
 @salt.utils.decorators.depends('time123', fallback_funcion=_fallbackfunc)
 def missing_depends_will_fallback():
-    return True, time.time()
+    ret = {'ret': True,
+           'time': time.time()}
+    return ret
--- tests/integration/modules/decorators.py
+++ tests/integration/modules/decorators.py
@@ -3,21 +3,34 @@ import integration
 
 class DecoratorTest(integration.ModuleCase):
     def test_module(self):
-        self.assertTrue(self.run_function('runtests_decorators.working_function'))
+        self.assertTrue(
+                self.run_function(
+                    'runtests_decorators.working_function'
+                    )
+                )
 
     def test_depends(self):
-        ret_val, time = self.run_function('runtests_decorators.depends')
-        self.assertTrue(ret_val)
-        self.assertTrue(type(time) == float)
+        ret = self.run_function('runtests_decorators.depends')
+        self.assertTrue(ret['ret'])
+        self.assertTrue(type(ret['time']) == float)
 
     def test_missing_depends(self):
-        self.assertTrue('is not available' in self.run_function('runtests_decorators.missing_depends'))
+        self.assertIn(
+                'is not available',
+                self.run_function('runtests_decorators.missing_depends'
+                    )
+                )
 
     def test_depends_will_fallback(self):
-        ret_val, time = self.run_function('runtests_decorators.depends_will_fallback')
-        self.assertTrue(ret_val)
-        self.assertTrue(type(time) == float)
+        ret = self.run_function('runtests_decorators.depends_will_fallback')
+        self.assertTrue(ret['ret'])
+        self.assertTrue(type(ret['time']) == float)
 
-    def test_missing_depends(self):
-        self.assertTrue('fallback' in self.run_function('runtests_decorators.missing_depends_will_fallback'))
+    def test_missing_depends_again(self):
+        self.assertIn(
+                'fallback',
+                self.run_function(
+                    'runtests_decorators.missing_depends_will_fallback'
+                    )
+                )
 
