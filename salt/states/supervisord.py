2776a0b0cf9b7652ff3391c2139be842ded4e7cd
Jorge E. Cardona <jorge@cardona.co>
2013-06-24 01:38:41 -0500

    Fix restart/update problem in supervisord state, adding a way to force update and checking if changes were detected in case a restart is also needed. (Result of module supervisord.update is the actual stdout)

--- salt/states/supervisord.py
+++ salt/states/supervisord.py
@@ -106,7 +106,7 @@ def running(name,
         changes.append(comment)
         log.debug(comment)
 
-        if '{0}: updated' in result['stdout']:
+        if result.find('{0}: updated'.format(name)) >= 0:
             just_updated = True
 
     if name in all_processes and not _is_stopped_state(all_processes[name]['state']):
