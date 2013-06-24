92876dd91a108565aa8fc9ede016795a72d4f947
root <root@saltstack-git.(none)>
2013-06-24 01:04:19 +0000

    Added some lines to the docs explaining how to use Powershell as an alternate shell.

--- salt/modules/cmdmod.py
+++ salt/modules/cmdmod.py
@@ -408,6 +408,10 @@ def run(cmd,
 
         salt '*' cmd.run template=jinja "ls -l /tmp/{{grains.id}} | awk '/foo/{print \\$2}'"
 
+    Specify an alternate shell with the shell parameter::
+
+        salt '*' cmd.run "Get-ChildItem C:\ " shell='powershell'
+
     '''
     out = _run(cmd,
                runas=runas,
@@ -621,6 +625,7 @@ def script(
 
         salt '*' cmd.script salt://scripts/runme.sh
         salt '*' cmd.script salt://scripts/runme.sh 'arg1 arg2 "arg 3"'
+        salt '*' cmd.script salt://scripts/windows_task.ps1 args=' -Input c:\tmp\infile.txt' shell='powershell'
     '''
     if not salt.utils.is_windows():
         path = salt.utils.mkstemp(dir=cwd)
