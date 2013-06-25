e772f0d8b67d19769b82b8683f0dd15f50d13565
Pedro Algarvio <pedro@algarvio.me>
2013-06-25 14:41:26 +0100

    Re-use salt-testing mixin.

--- tests/integration/shell/minion.py
+++ tests/integration/shell/minion.py
@@ -8,15 +8,13 @@
     :license: Apache 2.0, see LICENSE for more details.
 '''
 
-# Import python libs
-import sys
-
 # Import salt libs
 try:
     import integration
 except ImportError:
     if __name__ == '__main__':
         import os
+        import sys
         sys.path.insert(
             0, os.path.abspath(
                 os.path.join(
