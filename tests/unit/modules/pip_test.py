d245b174121cc1e3242dc4f4726e1b973a001f84
Pedro Algarvio <pedro@algarvio.me>
2013-06-25 09:24:05 +0100

    Added support for `__main__` execution and formatting fixes.

--- tests/unit/modules/pip_test.py
+++ tests/unit/modules/pip_test.py
@@ -1,3 +1,24 @@
+# Import salt libs
+try:
+    import integration
+except ImportError:
+    if __name__ == '__main__':
+        import os
+        import sys
+        sys.path.insert(
+            0, os.path.abspath(
+                os.path.join(
+                    os.path.dirname(__file__), '../../'
+                )
+            )
+        )
+    import integration
+from salt.modules import pip
+
+# Import Salt Testing libs
+from salttesting import TestCase, skipIf
+
+
 try:
     from mock import MagicMock, patch
     has_mock = True
@@ -5,17 +26,16 @@ except ImportError:
     has_mock = False
     patch = lambda x: lambda y: None
 
-from salttesting import TestCase, skipIf
-from salt.modules import pip
 
-pip.__salt__ = {"cmd.which_bin":lambda _:"pip"}
+pip.__salt__ = {"cmd.which_bin": lambda _: "pip"}
+
 
 @skipIf(has_mock is False, "mock python module is unavailable")
 class PipTestCase(TestCase):
 
     def test_fix4361(self):
         mock = MagicMock(return_value={'retcode': 0, 'stdout': ''})
-        with patch.dict(pip.__salt__, {'cmd.run_all':mock}):
+        with patch.dict(pip.__salt__, {'cmd.run_all': mock}):
             pip.install(requirements="requirements.txt")
             expected_cmd = 'pip install --requirement "requirements.txt" '
             mock.assert_called_once_with(expected_cmd, runas=None, cwd=None)
@@ -35,15 +55,24 @@ class PipTestCase(TestCase):
             pip.install(requirements="salt://requirements.txt")
             expected_cmd = 'pip install --requirement "my_cached_reqs" '
             mock.assert_called_once_with(expected_cmd, runas=None, cwd=None)
+
     @patch('os.path')
     def test_fix_activate_env(self, mock_path):
         mock_path.is_file.return_value = True
         mock_path.isdir.return_value = True
+
         def join(*args):
             return '/'.join(args)
         mock_path.join = join
         mock = MagicMock(return_value={'retcode': 0, 'stdout': ''})
         with patch.dict(pip.__salt__, {'cmd.run_all': mock}):
             pip.install('mock', bin_env='/test_env', activate=True)
-            expected_cmd = '. /test_env/bin/activate && /test_env/bin/pip install mock '
-            mock.assert_called_once_with(expected_cmd, runas=None, cwd=None)
+            mock.assert_called_once_with(
+                '. /test_env/bin/activate && /test_env/bin/pip install mock ',
+                runas=None,
+                cwd=None)
+
+
+if __name__ == '__main__':
+    from integration import run_tests
+    run_tests(PipTestCase, needs_daemon=False)
