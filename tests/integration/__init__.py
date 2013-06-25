b17bc305c8e95ca811e65873f50190ba8d31ecc7
Pedro Algarvio <pedro@algarvio.me>
2013-06-25 14:13:14 +0100

    Reuse salt-testing shell test case.

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -11,10 +11,8 @@ import pprint
 import tempfile
 import logging
 import time
-import signal
 import subprocess
 from hashlib import md5
-from subprocess import PIPE, Popen
 from datetime import datetime, timedelta
 try:
     import pwd
@@ -45,6 +43,7 @@ from salt.utils.verify import verify_env
 
 # Import Salt Testing libs
 from salttesting import TestCase
+from salttesting.case import ShellTestCase
 from salttesting.parser import PNUM, print_header, SaltTestcaseParser
 from salttesting.helpers import RedirectStdStreams
 
@@ -657,114 +656,14 @@ class SyndicCase(TestCase, SaltClientTestCaseMixIn):
         return orig['minion']
 
 
-class ShellCase(TestCase):
+class ShellCase(ShellTestCase):
     '''
     Execute a test for a shell command
     '''
-    def run_script(self, script, arg_str, catch_stderr=False, timeout=None):
-        '''
-        Execute a script with the given argument string
-        '''
-        path = os.path.join(SCRIPT_DIR, script)
-        if not os.path.isfile(path):
-            return False
-        ppath = 'PYTHONPATH={0}:{1}'.format(CODE_DIR, ':'.join(sys.path[1:]))
-        cmd = '{0} {1} {2} {3}'.format(ppath, PYEXEC, path, arg_str)
 
-        popen_kwargs = {
-            'shell': True,
-            'stdout': PIPE
-        }
-
-        if catch_stderr is True:
-            popen_kwargs['stderr'] = PIPE
-
-        if not sys.platform.lower().startswith('win'):
-            popen_kwargs['close_fds'] = True
-
-            def detach_from_parent_group():
-                # detach from parent group (no more inherited signals!)
-                os.setpgrp()
-
-            popen_kwargs['preexec_fn'] = detach_from_parent_group
-
-        elif sys.platform.lower().startswith('win') and timeout is not None:
-            raise RuntimeError('Timeout is not supported under windows')
-
-        process = Popen(cmd, **popen_kwargs)
-
-        if timeout is not None:
-            stop_at = datetime.now() + timedelta(seconds=timeout)
-            term_sent = False
-            while True:
-                process.poll()
-                if process.returncode is not None:
-                    break
-
-                if datetime.now() > stop_at:
-                    if term_sent is False:
-                        # Kill the process group since sending the term signal
-                        # would only terminate the shell, not the command
-                        # executed in the shell
-                        os.killpg(os.getpgid(process.pid), signal.SIGINT)
-                        term_sent = True
-                        continue
-
-                    # As a last resort, kill the process group
-                    os.killpg(os.getpgid(process.pid), signal.SIGKILL)
-
-                    out = [
-                        'Process took more than {0} seconds to complete. '
-                        'Process Killed!'.format(timeout)
-                    ]
-                    if catch_stderr:
-                        return out, [
-                            'Process killed, unable to catch stderr output'
-                        ]
-                    return out
-
-        if catch_stderr:
-            if sys.version_info < (2, 7):
-                # On python 2.6, the subprocess'es communicate() method uses
-                # select which, is limited by the OS to 1024 file descriptors
-                # We need more available descriptors to run the tests which
-                # need the stderr output.
-                # So instead of .communicate() we wait for the process to
-                # finish, but, as the python docs state "This will deadlock
-                # when using stdout=PIPE and/or stderr=PIPE and the child
-                # process generates enough output to a pipe such that it
-                # blocks waiting for the OS pipe buffer to accept more data.
-                # Use communicate() to avoid that." <- a catch, catch situation
-                #
-                # Use this work around were it's needed only, python 2.6
-                process.wait()
-                out = process.stdout.read()
-                err = process.stderr.read()
-            else:
-                out, err = process.communicate()
-            # Force closing stderr/stdout to release file descriptors
-            process.stdout.close()
-            process.stderr.close()
-            try:
-                return out.splitlines(), err.splitlines()
-            finally:
-                try:
-                    process.terminate()
-                except OSError as err:
-                    # process already terminated
-                    pass
-
-        data = process.communicate()
-        process.stdout.close()
-
-        try:
-            return data[0].splitlines()
-        finally:
-            try:
-                process.terminate()
-            except OSError as err:
-                # process already terminated
-                pass
+    _code_dir_ = CODE_DIR
+    _script_dir_ = SCRIPT_DIR
+    _python_executable_ = PYEXEC
 
     def run_salt(self, arg_str):
         '''
