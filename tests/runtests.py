007545eb70ad16a51dc533f960ed8720a92b8597
Pedro Algarvio <pedro@algarvio.me>
2013-06-26 21:16:39 +0100

    Fix NameError

--- tests/runtests.py
+++ tests/runtests.py
@@ -141,7 +141,7 @@ class SaltTestsuiteParser(SaltCoverageTestingParser):
 
         self.start_coverage(
             branch=True,
-            source=[os.path.join(SALTC_ROOT, 'salt')],
+            source=[os.path.join(SALT_ROOT, 'salt')],
             track_processes=True
         )
 
