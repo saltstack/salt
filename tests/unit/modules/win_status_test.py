0631e31cb1c2e8d901d5e65373198e9f7668d396
Pedro Algarvio <pedro@algarvio.me>
2013-06-27 14:07:16 +0100

    Updated `unit.modules.win_status_test` test suite to use `salttesting.helpers.ensure_in_syspath()`

--- tests/unit/modules/win_status_test.py
+++ tests/unit/modules/win_status_test.py
@@ -1,27 +1,11 @@
 # Import python libs
-import os
 import new
 import sys
 
-# Import salt libs
-try:
-    import integration
-except ImportError:
-    if __name__ == '__main__':
-        import sys
-        sys.path.insert(
-            0, os.path.abspath(
-                os.path.join(
-                    os.path.dirname(__file__), '../../'
-                )
-            )
-        )
-    import integration
-
-
 # Import Salt Testing libs
-from salttesting import TestCase, skipIf
-
+from salttesting import skipIf, TestCase
+from salttesting.helpers import ensure_in_syspath
+ensure_in_syspath('../../')
 
 # wmi and pythoncom modules are platform specific...
 wmi = new.module('wmi')
