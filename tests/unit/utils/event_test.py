2081aff70767bdba9b6d11b96e76baa02e700099
Pedro Algarvio <pedro@algarvio.me>
2013-06-27 14:12:19 +0100

    Updated `unit.utils.event_test` test suite to use `salttesting.helpers.ensure_in_syspath()`

--- tests/unit/utils/event_test.py
+++ tests/unit/utils/event_test.py
@@ -12,24 +12,14 @@
 import os
 import hashlib
 
-# Import salt libs
-try:
-    import integration
-except ImportError:
-    if __name__ == '__main__':
-        import sys
-        sys.path.insert(
-            0, os.path.abspath(
-                os.path.join(
-                    os.path.dirname(__file__), '../../'
-                )
-            )
-        )
-    import integration
-from salt.utils import event
-
 # Import Salt Testing libs
 from salttesting import TestCase
+from salttesting.helpers import ensure_in_syspath
+ensure_in_syspath('../../')
+
+# Import salt libs
+import integration
+from salt.utils import event
 
 SOCK_DIR = os.path.join(integration.TMP, 'test-socks')
 
