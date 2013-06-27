155987dc086bb074a5cc64e230bca0824b4b95cf
Pedro Algarvio <pedro@algarvio.me>
2013-06-27 14:16:25 +0100

    Updated `unit.utils.path_join_test` test suite to use `salttesting.helpers.ensure_in_syspath()`

--- tests/unit/utils/path_join_test.py
+++ tests/unit/utils/path_join_test.py
@@ -16,24 +16,13 @@ import ntpath
 import platform
 import tempfile
 
-# Import salt libs
-try:
-    import integration
-except ImportError:
-    if __name__ == '__main__':
-        import sys
-        sys.path.insert(
-            0, os.path.abspath(
-                os.path.join(
-                    os.path.dirname(__file__), '../../'
-                )
-            )
-        )
-    import integration
-from salt.utils import path_join
-
 # Import Salt Testing libs
 from salttesting import TestCase
+from salttesting.helpers import ensure_in_syspath
+ensure_in_syspath('../../')
+
+# Import salt libs
+from salt.utils import path_join
 
 
 class PathJoinTestCase(TestCase):
