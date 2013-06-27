de41ca31364c7d4aee181d313a50ef236ea0a7b7
Pedro Algarvio <pedro@algarvio.me>
2013-06-27 12:49:15 +0100

    Updated `integration.modules.supervisord` test suite to use `salttesting.helpers.ensure_in_syspath()`

--- tests/integration/modules/supervisord.py
+++ tests/integration/modules/supervisord.py
@@ -1,21 +1,14 @@
+# Import python
 import os
 import time
 import subprocess
 
+# Import Salt Testing libs
+from salttesting.helpers import ensure_in_syspath
+ensure_in_syspath('../../')
+
 # Import salt libs
-try:
-    import integration
-except ImportError:
-    if __name__ == '__main__':
-        import sys
-        sys.path.insert(
-            0, os.path.abspath(
-                os.path.join(
-                    os.path.dirname(__file__), '../../'
-                )
-            )
-        )
-    import integration
+import integration
 
 
 class SupervisordModuleTest(integration.ModuleCase):
