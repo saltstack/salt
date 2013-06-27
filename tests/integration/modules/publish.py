501cb0da3101be67dfcdc9ad37fb2cb01c78d7ef
Pedro Algarvio <pedro@algarvio.me>
2013-06-27 12:36:37 +0100

    Updated `integration.modules.publish` test suite to use `salttesting.helpers.ensure_in_syspath()`

--- tests/integration/modules/publish.py
+++ tests/integration/modules/publish.py
@@ -1,17 +1,9 @@
-try:
-    import integration
-except ImportError:
-    if __name__ == '__main__':
-        import os
-        import sys
-        sys.path.insert(
-            0, os.path.abspath(
-                os.path.join(
-                    os.path.dirname(__file__), '../../'
-                )
-            )
-        )
-    import integration
+# Import Salt Testing libs
+from salttesting.helpers import ensure_in_syspath
+ensure_in_syspath('../../')
+
+# Import salt libs
+import integration
 
 
 class PublishModuleTest(integration.ModuleCase,
