f3b145b11801265efe4e506d04f4b93002814d75
Pedro Algarvio <pedro@algarvio.me>
2013-06-27 13:48:18 +0100

    Updated `integration.states.rabbitmq_vhost` test suite to use `salttesting.helpers.ensure_in_syspath()`

--- tests/integration/states/rabbitmq_vhost.py
+++ tests/integration/states/rabbitmq_vhost.py
@@ -1,22 +1,16 @@
 '''
 Tests for the rabbitmq state
 '''
+
+# Import python libs
 import os
 
+# Import Salt Testing libs
+from salttesting.helpers import ensure_in_syspath
+ensure_in_syspath('../../')
+
 # Import salt libs
-try:
-    import integration
-except ImportError:
-    if __name__ == '__main__':
-        import sys
-        sys.path.insert(
-            0, os.path.abspath(
-                os.path.join(
-                    os.path.dirname(__file__), '../../'
-                )
-            )
-        )
-    import integration
+import integration
 
 
 class RabbitVHostTestCase(integration.ModuleCase,
