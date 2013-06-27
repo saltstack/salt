c780c8b1989e64ca3ea71f7e2f0776819b57d491
Pedro Algarvio <pedro@algarvio.me>
2013-06-27 12:11:20 +0100

    Updated `integration.modules.mine` test suite to use `salttesting.helpers.ensure_in_syspath()`

--- tests/integration/modules/mine.py
+++ tests/integration/modules/mine.py
@@ -1,22 +1,12 @@
 '''
 Test the salt mine system
 '''
+# Import Salt Testing libs
+from salttesting.helpers import ensure_in_syspath
+ensure_in_syspath('../../')
 
 # Import salt libs
-try:
-    import integration
-except ImportError:
-    if __name__ == '__main__':
-        import os
-        import sys
-        sys.path.insert(
-            0, os.path.abspath(
-                os.path.join(
-                    os.path.dirname(__file__), '../../'
-                )
-            )
-        )
-    import integration
+import integration
 
 
 class MineTest(integration.ModuleCase):
