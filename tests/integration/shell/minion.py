331dc6d936495b6523e882a8c162035b971e1621
Pedro Algarvio <pedro@algarvio.me>
2013-06-27 13:25:43 +0100

    Updated `integration.shell.minion` test suite to use `salttesting.helpers.ensure_in_syspath()`

--- tests/integration/shell/minion.py
+++ tests/integration/shell/minion.py
@@ -8,21 +8,12 @@
     :license: Apache 2.0, see LICENSE for more details.
 '''
 
+# Import Salt Testing libs
+from salttesting.helpers import ensure_in_syspath
+ensure_in_syspath('../../')
+
 # Import salt libs
-try:
-    import integration
-except ImportError:
-    if __name__ == '__main__':
-        import os
-        import sys
-        sys.path.insert(
-            0, os.path.abspath(
-                os.path.join(
-                    os.path.dirname(__file__), '../../'
-                )
-            )
-        )
-    import integration
+import integration
 
 
 class MinionTest(integration.ShellCase, integration.ShellCaseCommonTestsMixIn):
