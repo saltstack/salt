d62211f734d77eb577f13ad3cbf07438eec6757a
Pedro Algarvio <pedro@algarvio.me>
2013-06-27 11:57:33 +0100

    Updated `integration.modules.decorators` test suite to use `salttesting.helpers.ensure_in_syspath()`

--- tests/integration/modules/decorators.py
+++ tests/integration/modules/decorators.py
@@ -1,18 +1,9 @@
+# Import Salt Testing libs
+from salttesting.helpers import ensure_in_syspath
+ensure_in_syspath('../../')
+
 # Import salt libs
-try:
-    import integration
-except ImportError:
-    if __name__ == '__main__':
-        import os
-        import sys
-        sys.path.insert(
-            0, os.path.abspath(
-                os.path.join(
-                    os.path.dirname(__file__), '../../'
-                )
-            )
-        )
-    import integration
+import integration
 
 
 class DecoratorTest(integration.ModuleCase):
