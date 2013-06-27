34ccae8c465c660dca38ff343c01269a1fe4f464
Pedro Algarvio <pedro@algarvio.me>
2013-06-27 13:32:54 +0100

    Updated `integration.states.cmd` test suite to use `salttesting.helpers.ensure_in_syspath()`

--- tests/integration/states/cmd.py
+++ tests/integration/states/cmd.py
@@ -1,23 +1,16 @@
 '''
 Tests for the file state
 '''
+
+# Import python libs
 import tempfile
 
+# Import Salt Testing libs
+from salttesting.helpers import ensure_in_syspath
+ensure_in_syspath('../../')
+
 # Import salt libs
-try:
-    import integration
-except ImportError:
-    if __name__ == '__main__':
-        import os
-        import sys
-        sys.path.insert(
-            0, os.path.abspath(
-                os.path.join(
-                    os.path.dirname(__file__), '../../'
-                )
-            )
-        )
-    import integration
+import integration
 
 
 class CMDTest(integration.ModuleCase,
