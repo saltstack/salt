f55263631d39e1d027cb637a0f2119baacdc2512
Pedro Algarvio <pedro@algarvio.me>
2013-06-27 13:45:55 +0100

    Updated `integration.states.pip` test suite to use `salttesting.helpers.ensure_in_syspath()`

--- tests/integration/states/pip.py
+++ tests/integration/states/pip.py
@@ -12,20 +12,12 @@
 import os
 import shutil
 
+# Import Salt Testing libs
+from salttesting.helpers import ensure_in_syspath
+ensure_in_syspath('../../')
+
 # Import salt libs
-try:
-    import integration
-except ImportError:
-    if __name__ == '__main__':
-        import sys
-        sys.path.insert(
-            0, os.path.abspath(
-                os.path.join(
-                    os.path.dirname(__file__), '../../'
-                )
-            )
-        )
-    import integration
+import integration
 
 
 class PipStateTest(integration.ModuleCase, integration.SaltReturnAssertsMixIn):
