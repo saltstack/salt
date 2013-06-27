5fa18d41ec7a162478c440b95907343f2e0df1da
Pedro Algarvio <pedro@algarvio.me>
2013-06-27 12:51:04 +0100

    Updated `integration.modules.sysctl` test suite to use `salttesting.helpers.ensure_in_syspath()`

--- tests/integration/modules/sysctl.py
+++ tests/integration/modules/sysctl.py
@@ -1,23 +1,13 @@
 # Import python libs
 import sys
 
-# Import salt libs
-try:
-    import integration
-except ImportError:
-    if __name__ == '__main__':
-        import os
-        sys.path.insert(
-            0, os.path.abspath(
-                os.path.join(
-                    os.path.dirname(__file__), '../../'
-                )
-            )
-        )
-    import integration
-
 # Import Salt Testing libs
 from salttesting import skipIf
+from salttesting.helpers import ensure_in_syspath
+ensure_in_syspath('../../')
+
+# Import salt libs
+import integration
 
 
 class SysctlModuleTest(integration.ModuleCase):
