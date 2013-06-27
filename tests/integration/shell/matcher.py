66b6acd0e0953bdc05c9dcedaaf1fc83a2fd9457
Pedro Algarvio <pedro@algarvio.me>
2013-06-27 13:29:48 +0100

    Updated `integration.shell.matcher` test suite to use `salttesting.helpers.ensure_in_syspath()`

--- tests/integration/shell/matcher.py
+++ tests/integration/shell/matcher.py
@@ -1,21 +1,12 @@
 # Import python libs
-import sys
 import yaml
 
+# Import Salt Testing libs
+from salttesting.helpers import ensure_in_syspath
+ensure_in_syspath('../../')
+
 # Import salt libs
-try:
-    import integration
-except ImportError:
-    if __name__ == '__main__':
-        import os
-        sys.path.insert(
-            0, os.path.abspath(
-                os.path.join(
-                    os.path.dirname(__file__), '../../'
-                )
-            )
-        )
-    import integration
+import integration
 
 
 class MatchTest(integration.ShellCase, integration.ShellCaseCommonTestsMixIn):
