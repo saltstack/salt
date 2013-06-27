7c49cf1cdc6d870846acd3a51ca99789e2019e76
Pedro Algarvio <pedro@algarvio.me>
2013-06-27 13:43:26 +0100

    Updated `integration.states.match` test suite to use `salttesting.helpers.ensure_in_syspath()`

--- tests/integration/states/match.py
+++ tests/integration/states/match.py
@@ -11,24 +11,14 @@
 # Import python libs
 import os
 
-# Import salt libs
-try:
-    import integration
-except ImportError:
-    if __name__ == '__main__':
-        import sys
-        sys.path.insert(
-            0, os.path.abspath(
-                os.path.join(
-                    os.path.dirname(__file__), '../../'
-                )
-            )
-        )
-    import integration
-import salt.utils
-
 # Import Salt Testing libs
 from salttesting import skipIf
+from salttesting.helpers import ensure_in_syspath
+ensure_in_syspath('../../')
+
+# Import salt libs
+import integration
+import salt.utils
 
 STATE_DIR = os.path.join(integration.FILES, 'file', 'base')
 
@@ -41,7 +31,8 @@ class StateMatchTest(integration.ModuleCase):
     def test_issue_2167_exsel_no_AttributeError(self):
         ret = self.run_function('state.top', ['issue-2167-exsel-match.sls'])
         self.assertNotIn(
-            "AttributeError: 'Matcher' object has no attribute 'functions'",
+            'AttributeError: \'Matcher\' object has no '
+            'attribute \'functions\'',
             ret
         )
 
