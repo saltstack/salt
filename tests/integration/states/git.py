415c3560e251892ed7632413acb758c7cea1e638
Pedro Algarvio <pedro@algarvio.me>
2013-06-27 13:40:41 +0100

    Updated `integration.states.git` test suite to use `salttesting.helpers.ensure_in_syspath()`

--- tests/integration/states/git.py
+++ tests/integration/states/git.py
@@ -1,23 +1,18 @@
 '''
 Tests for the Git state
 '''
+
+# Import python libs
 import os
 import shutil
 import socket
-try:
-    import integration
-except ImportError:
-    if __name__ == '__main__':
-        import os
-        import sys
-        sys.path.insert(
-            0, os.path.abspath(
-                os.path.join(
-                    os.path.dirname(__file__), '../../'
-                )
-            )
-        )
-    import integration
+
+# Import Salt Testing libs
+from salttesting.helpers import ensure_in_syspath
+ensure_in_syspath('../../')
+
+# Import salt libs
+import integration
 
 
 class GitTest(integration.ModuleCase, integration.SaltReturnAssertsMixIn):
