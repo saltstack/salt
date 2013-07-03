12b4bf1619f56f695952ba25a7f0b810b7a1f007
Seth House <seth@eseth.com>
2013-07-03 15:48:00 -0600

    rST fixes in reclass docstrings

--- salt/tops/reclass_adapter.py
+++ salt/tops/reclass_adapter.py
@@ -1,10 +1,10 @@
 '''
 master_tops adapter for reclass.
 
-Please refer to the file `README.Salt` in the reclass source for more
+Please refer to the file ``README.Salt`` in the reclass source for more
 information on how to use these. In a nutshell, you'll just add the plugin to
 the master_tops hash in the master config and tell reclass by way of a few
-options how and where to find the inventory:
+options how and where to find the inventory::
 
     ---
     master_tops:
@@ -13,20 +13,20 @@ options how and where to find the inventory:
             base_inventory_uri: /srv/salt
 
 This would cause reclass to read the inventory from YAML files in
-`/srv/salt/nodes` and `/srv/salt/classes`.
+``/srv/salt/nodes`` and ``/srv/salt/classes``.
 
 More information about reclass: http://github.com/madduck/reclass
 
 There is currently no way to avoid having to specify the same configuration
-for `ext_pillar` and `master_tops`.
+for ``ext_pillar`` and ``master_tops``.
 
 Unfortunately, there is currently no way to specify the location of the
 reclass source in the master config, because Salt provides no way to access
-the configuration file data at the module scope (`__opts__` is injected by the
-Salt loader), where we need to know about whether reclass is import-able to be
-able to define the `__virtual__` function. You will hence either have to
-install reclass to `PYTHONPATH`, or extend `PYTHONPATH` when running the
-master, e.g.:
+the configuration file data at the module scope (``__opts__`` is injected by
+the Salt loader), where we need to know about whether reclass is import-able to
+be able to define the ``__virtual__`` function. You will hence either have to
+install reclass to ``PYTHONPATH``, or extend ``PYTHONPATH`` when running the
+master, e.g.::
 
     PYTHONPATH=~/code/reclass:$PYTHONPATH salt-master ...
 '''
