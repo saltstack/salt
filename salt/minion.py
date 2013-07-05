saltstack/salt

commit c985e609a34f80fb6805ee31b5a699131b261980

Fix traceback in peer publish system
This fixes #5958 by adding a default value for the call to getattr().

salt/minion.py
@@ -534,7 +534,8 @@ class Minion(object):
         # Verify that the publication applies to this minion
         if 'tgt_type' in data:
             if not getattr(self.matcher,
-                           '{0}_match'.format(data['tgt_type']))(data['tgt']):
+                           '{0}_match'.format(data['tgt_type']))(data['tgt'],
+                           False):
                 return
         else:
             if not self.matcher.glob_match(data['tgt']):

commit 09cc692c232ccd32232fcd1d89ded575805ac661

Fix traceback introduced in c985e60
c985e60 from earlier today broke a lot of things, because of a
misreading of how getattr was being called. This commit fixes that.

salt/minion.py
@@ -533,9 +533,9 @@ class Minion(object):
             return
         # Verify that the publication applies to this minion
         if 'tgt_type' in data:
-            if not getattr(self.matcher,
-                           '{0}_match'.format(data['tgt_type']))(data['tgt'],
-                           False):
+            match_func = getattr(self.matcher,
+                                 '{0}_match'.format(data['tgt_type']), None)
+            if match_func is None or not match_func(data['tgt']):
                 return
         else:
             if not self.matcher.glob_match(data['tgt']):