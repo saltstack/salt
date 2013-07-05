3caf82294636bc4480b33597ef6311b3e368fb1c
Thomas S Hatch <thatch45@gmail.com>
2013-07-05 09:56:23 -0600

    Minor cleanup to spacing and comments

--- salt/states/win_dns_client.py
+++ salt/states/win_dns_client.py
@@ -7,7 +7,6 @@ def __virtual__():
     '''
     Load if the module win_dns_client is loaded
     '''
-
     return 'win_dns_client' if 'win_dns_client.add_dns' in __salt__ else False
 
 
@@ -23,7 +22,6 @@ def dns_exists(name, servers=None, interface='Local Area Connection'):
               - 8.8.8.8
               - 8.8.8.9
     '''
-    
     ret = {'name': name,
            'result': True,
            'changes': {},
@@ -38,7 +36,7 @@ def dns_exists(name, servers=None, interface='Local Area Connection'):
     # Do nothing is already configured
     configured_list = __salt__['win_dns_client.get_dns_servers'](interface)
     if configured_list == servers:
-        ret['comment'] = '{0} are already configured'.format( servers )
+        ret['comment'] = '{0} are already configured'.format(servers)
         return ret
     else:
         ret['changes'] = {'configure servers': servers}
@@ -49,7 +47,9 @@ def dns_exists(name, servers=None, interface='Local Area Connection'):
     # add the dns servers
     for i in range(0, len(servers)):
         if not __salt__['win_dns_client.add_dns'](servers[i] ,interface, i+1):
-            ret['comment'] = 'failed to add {0} as dns server number {1}'.format(servers[i] ,i+1)
+            ret['comment'] = (
+                    'Failed to add {0} as dns server number {1}'
+                    ).format(servers[i] ,i+1)
             ret['result'] = False
             if i != 0:
                 ret['changes'] = {'configure servers': servers[0,i]}
@@ -73,7 +73,8 @@ def dns_dhcp(name, interface='Local Area Connection'):
     # Check the config
     config = __salt__['win_dns_client.get_dns_config'](interface)
     if config == 'dhcp':
-        ret['comment'] = '{0} already configured with dns from dhcp'.format( interface )
+        ret['comment'] = '{0} already configured with dns from dhcp'.format(
+                interface)
         return ret
     else:
         ret['changes'] = {'dns': 'configured from dhcp'}
@@ -85,7 +86,9 @@ def dns_dhcp(name, interface='Local Area Connection'):
     ret['result'] = __salt__['win_dns_client.dns_dhcp'](interface)
     if not ret['result']:
         ret['changes'] = {}
-        ret['comment'] = 'could not configure "{0}" dns servers from dhcp'.format( interface )
+        ret['comment'] = (
+                'Could not configure "{0}" dns servers from dhcp'
+                ).format(interface)
     
     return ret
     
