829a8dfd1e86dae135220c4eee67d00205332e34
Thomas S Hatch <thatch45@gmail.com>
2013-07-05 09:53:27 -0600

    fix spacing and add CLI Examples

--- salt/modules/win_dns_client.py
+++ salt/modules/win_dns_client.py
@@ -21,19 +21,29 @@ def __virtual__():
 
 def get_dns_servers(interface='Local Area Connection'):
     '''
-    return a list of the configured dns servers of the specific interface
+    Return a list of the configured dns servers of the specific interface
+
+    CLI Example::
+
+        salt '*' win_dns_client.get_dns_servers <interface>
     '''
-    
-    out = __salt__['cmd.run']( 'netsh interface ip show dns "{0}"'.format( interface ) )
+    out = __salt__['cmd.run'](
+            'netsh interface ip show dns "{0}"'.format(interface))
     return re.findall("(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})", out)
 
 
 def rm_dns(ip, interface='Local Area Connection'):
     '''
     Remove the dns server to the nertwork interface
+
+    CLI Example::
+
+        salt '*' win_dns_client.rm_dns <interface>
     '''
-    
-    return __salt__['cmd.retcode']( 'netsh interface ip delete dns "{0}" {1} validate=no'.format( interface, ip ) ) == 0
+    return __salt__['cmd.retcode'](
+            'netsh interface ip delete dns "{0}" {1} validate=no'.format(
+                interface, ip)
+            ) == 0
 
 
 def add_dns(ip, interface='Local Area Connection', index=1):
@@ -41,10 +51,13 @@ def add_dns(ip, interface='Local Area Connection', index=1):
     Add the dns server to the nertwork interface
     (index starts from 1)
     
-    Note: if the interface dns is configured by DHCP all the dns servers will be removed from the
-    interface and the requested dns will be the only one
-    '''
+    Note: if the interface dns is configured by DHCP all the dns servers will
+    be removed from the interface and the requested dns will be the only one
     
+    CLI Example::
+
+        salt '*' win_dns_client.add_dns <interface> <index>
+    '''
     servers = get_dns_servers(interface)
     
     # Return true if configured
@@ -57,28 +70,40 @@ def add_dns(ip, interface='Local Area Connection', index=1):
     # If configured in the wrong order delete it
     if ip in servers:
         rm_dns(ip, interface)
+    cmd = 'netsh interface ip add dns "{0}" {1} index={2} validate=no'.format(
+        interface, ip, index
+        )
     
-    retcode = __salt__['cmd.retcode']( 'netsh interface ip add dns "{0}" {1} index={2} validate=no'.format( interface, ip, index ) )
+    retcode = __salt__['cmd.retcode'](cmd)
     return retcode == 0
 
 
 def dns_dhcp(interface='Local Area Connection'):
     '''
     Configure the interface to get it's DNS servers from the DHCP server
+    
+    CLI Example::
+
+        salt '*' win_dns_client.dns_dhcp <interface>
     '''
     
-    return __salt__['cmd.retcode']( 'netsh interface ip set dns "{0}" source=dhcp'.format( interface ) ) == 0
+    return __salt__['cmd.retcode'](
+            'netsh interface ip set dns "{0}" source=dhcp'.format(interface)
+            ) == 0
 
 
 def get_dns_config(interface='Local Area Connection'):
     '''
     Get the type of dns configuration (dhcp / static)
+
+    CLI Example::
+
+        salt '*' win_dns_client.get_dns_config <interface>
     '''
-    
-    out = __salt__['cmd.run']( 'netsh interface ip show dns "{0}"'.format( interface) )
+    out = __salt__['cmd.run'](
+            'netsh interface ip show dns "{0}"'.format(interface)
+            )
     if re.search('DNS servers configured through DHCP', out):
         return 'dhcp'
     else:
         return 'static'
-
-
