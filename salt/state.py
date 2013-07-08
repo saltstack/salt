ed4a9e6c69a155eb90e0f486047005d57a769ea9
Chris Faulkner <thefaulkner@gmail.com>
2013-07-08 13:27:35 -0700

    Update state.load_dynamic to handle changes to saltutil.sync_all.

--- salt/state.py
+++ salt/state.py
@@ -1855,7 +1855,7 @@ class BaseHighState(object):
         if not self.opts['autoload_dynamic_modules']:
             return
         syncd = self.state.functions['saltutil.sync_all'](list(matches))
-        if syncd[2]:
+        if syncd['grains']:
             self.opts['grains'] = salt.loader.grains(self.opts)
         self.state.module_refresh()
 
