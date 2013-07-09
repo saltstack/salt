63f2c51a25fa9a5c7f993fb4533eae032ceccc7e
Erik Johnson <palehose@gmail.com>
2013-07-09 12:39:01 -0500

    Correct innacuracy in log message
    This log entry was apparently copied directly from the git fileserver
    backend, but this isn't the git fileserver backend, it's the git
    external pillar.

--- salt/pillar/git_pillar.py
+++ salt/pillar/git_pillar.py
@@ -41,8 +41,8 @@ def __virtual__():
     if not any(['git' in x for x in ext_pillar_sources]):
         return False
     if not HAS_GIT:
-        log.error('Git fileserver backend is enabled in configuration but '
-                  'could not be loaded, is git-python installed?')
+        log.error('Git-based ext_pillar is enabled in configuration but '
+                  'could not be loaded, is GitPython installed?')
         return False
     if not git.__version__ > '0.3.0':
         return False
