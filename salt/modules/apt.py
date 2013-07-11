saltstack/salt

commit a43a2af0055203ffb87b5a1e6d7a6c449a40f4fe

string.find(substr) <comparison> <0 OR -1>  ==>  substr <`in` OR `not in`> string

salt/modules/apt.py
@@ -92,9 +92,7 @@ def _pkgname_without_arch(name):
     Check for ':arch' appended to pkg name (i.e. 32 bit installed on 64 bit
     machine is ':i386')
     '''
-    if name.find(':') >= 0:
-        return name.split(':')[0]
-    return name
+    return name.rpartition(':')[0]
 
 
 def _get_repo(**kwargs):

salt/modules/ebuild.py
@@ -437,7 +437,7 @@ def install(name=None,
                 else:
                     target = '"{0}"'.format(param)
 
-                if target.find('[') != -1:
+                if '[' in target:
                     old = __salt__['portage_config.get_flags_from_package_conf']('use', target[1:-1])
                     __salt__['portage_config.append_use_flags'](target[1:-1])
                     new = __salt__['portage_config.get_flags_from_package_conf']('use', target[1:-1])

salt/modules/portage_config.py
@@ -230,7 +230,7 @@ def append_to_package_conf(conf, atom='', flags=None, string='', overwrite=False
         flags = []
     if conf in SUPPORTED_CONFS:
         if not string:
-            if atom.find('/') == -1:
+            if '/' not in atom:
                 atom = _p_to_cp(atom)
                 if not atom:
                     return
@@ -239,7 +239,7 @@ def append_to_package_conf(conf, atom='', flags=None, string='', overwrite=False
         else:
             atom = string.strip().split()[0]
             new_flags = portage.dep.strip_empty(string.strip().split(' '))[1:]
-            if atom.find('/') == -1:
+            if '/' not in atom:
                 atom = _p_to_cp(atom)
                 string = '{0} {1}'.format(atom, ' '.join(new_flags))
                 if not atom:
@@ -358,7 +358,7 @@ def get_flags_from_package_conf(conf, atom):
     '''
     if conf in SUPPORTED_CONFS:
         package_file = '{0}/{1}'.format(BASE_PATH.format(conf), _p_to_cp(atom))
-        if atom.find('/') == -1:
+        if '/' not in atom:
             atom = _p_to_cp(atom)
         match_list = set(_porttree().dbapi.xmatch("match-all", atom))
         flags = []

salt/modules/xapi.py
@@ -469,7 +469,7 @@ def vcpu_pin(vm_, vcpu, cpus):
             for c in cpulist.split(','):
                 if c == '':
                     continue
-                if c.find('-') != -1:
+                if '-' in c:
                     (x, y) = c.split('-')
                     for i in range(int(x), int(y) + 1):
                         cpus.append(int(i))

salt/states/supervisord.py
@@ -106,7 +106,7 @@ def running(name,
         changes.append(comment)
         log.debug(comment)
 
-        if result.find('{0}: updated'.format(name)) >= 0:
+        if '{0}: updated'.format(name) in result:
             just_updated = True
 
     if name in all_processes and not _is_stopped_state(all_processes[name]['state']):

commit d0b38597623118b272ca95843b1f7eef2b7d1d09

Fix regression in apt.py
This fixes a regression introduced in a43a2af a few days ago.

salt/modules/apt.py
@@ -92,7 +92,7 @@ def _pkgname_without_arch(name):
     Check for ':arch' appended to pkg name (i.e. 32 bit installed on 64 bit
     machine is ':i386')
     '''
-    return name.rpartition(':')[0]
+    return name.split(':')[0]
 
 
 def _get_repo(**kwargs):