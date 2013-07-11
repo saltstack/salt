4b5f865404bf4df3dd4669671004ef2a9baf1a5b
Pedro Algarvio <pedro@algarvio.me>
2013-07-11 19:22:32 +0100

    Warn about using option not available for `virtualenv` and `pyvenv` respectively.
    `False` defaults have been turned into `None` to know if the user is even passing `False` and fail.

--- salt/modules/virtualenv_mod.py
+++ salt/modules/virtualenv_mod.py
@@ -25,16 +25,16 @@ def __virtual__():
 
 def create(path,
            venv_bin=None,
-           no_site_packages=False,
+           no_site_packages=None,
            system_site_packages=False,
            distribute=False,
            clear=False,
            python=None,
            extra_search_dir=None,
-           never_download=False,
+           never_download=None,
            prompt=None,
-           symlinks=False,
-           upgrade=False,
+           symlinks=None,
+           upgrade=None,
            runas=None):
     '''
     Create a virtualenv
@@ -44,27 +44,27 @@ def create(path,
     venv_bin : 'virtualenv'
         The name (and optionally path) of the virtualenv command. This can also
         be set globally in the minion config file as ``virtualenv.venv_bin``.
-    no_site_packages : False
-        Passthrough argument given to virtualenv. Deprecated since salt>=0.17.0
-        Use ``system_site_packages=False`` instead.
+    no_site_packages : None
+        Passthrough argument given to virtualenv if True. Deprecated since
+        ``salt>=0.17.0``. Use ``system_site_packages=False`` instead.
     system_site_packages : False
         Passthrough argument given to virtualenv or pyvenv
     distribute : False
         Passthrough argument given to virtualenv
     clear : False
         Passthrough argument given to virtualenv or pyvenv
-    python : (default)
+    python : None (default)
         Passthrough argument given to virtualenv
-    extra_search_dir : (default)
+    extra_search_dir : None (default)
         Passthrough argument given to virtualenv
-    never_download : (default)
-        Passthrough argument given to virtualenv
-    prompt : (default)
-        Passthrough argument given to virtualenv
-    symlinks : False
-        Passthrough argument given to pyvenv
-    upgrade : False
-        Passthrough argument given to pyvenv
+    never_download : None (default)
+        Passthrough argument given to virtualenv if True
+    prompt : None (default)
+        Passthrough argument given to virtualenv if not None
+    symlinks : None
+        Passthrough argument given to pyvenv if True
+    upgrade : None
+        Passthrough argument given to pyvenv if True
     runas : None
         Set ownership for the virtualenv
 
@@ -77,7 +77,7 @@ def create(path,
     # raise CommandNotFoundError if venv_bin is missing
     salt.utils.check_or_die(venv_bin)
 
-    if no_site_packages:
+    if no_site_packages is not None:
         # Show a deprecation warning
         # XXX: Remove deprecation warning message on 0.18.0
         warnings.filterwarnings(
@@ -90,7 +90,7 @@ def create(path,
             DeprecationWarning
         )
 
-    if no_site_packages and system_site_packages:
+    if no_site_packages is True and system_site_packages is True:
         raise salt.exceptions.CommandExecutionError(
             '\'no_site_packages\' and \'system_site_packages\' are mutually '
             'exclusive options. Please use only one, and prefer '
@@ -101,6 +101,21 @@ def create(path,
     cmd = [venv_bin]
 
     if 'pyvenv' not in venv_bin:
+        # ----- Stop the user if pyvenv only options are used --------------->
+        # If any of the following values are not None, it means that the user
+        # is actually passing a True or False value. Stop Him!
+        if upgrade is not None:
+            raise salt.exceptions.CommandExecutionError(
+                'The `upgrade`(`--upgrade`) option is not supported '
+                'by {0!r}'.format(venv_bin)
+            )
+        elif symlinks is not None:
+            raise salt.exceptions.CommandExecutionError(
+                'The `symlinks`(`--symlinks`) option is not supported '
+                'by {0!r}'.format(venv_bin)
+            )
+        # <---- Stop the user if pyvenv only options are used ----------------
+
         # Virtualenv package
         try:
             import virtualenv
@@ -118,10 +133,11 @@ def create(path,
                     'Returned data: {1!r}'.format(version_cmd, ret)
                 )
             VIRTUALENV_VERSION_INFO = tuple(
-                [int(i) for i in ret['stdout'].split('rc')[0].split('.')]
+                [int(i) for i in
+                 ret['stdout'].strip().split('rc')[0].split('.')]
             )
 
-        if no_site_packages:
+        if no_site_packages is True:
             cmd.append('--no-site-packages')
         if distribute:
             if VIRTUALENV_VERSION_INFO >= (1, 10):
@@ -135,7 +151,7 @@ def create(path,
                 cmd.append('--distribute')
         if python:
             cmd.append('--python={0}'.format(python))
-        if extra_search_dir:
+        if extra_search_dir is not None:
             if isinstance(extra_search_dir, basestring):
                 if ',' in extra_search_dir:
                     extra_search_dir = [
@@ -145,7 +161,7 @@ def create(path,
                     extra_search_dir = [extra_search_dir]
             for entry in extra_search_dir:
                 cmd.append('--extra-search-dir={0}'.format(entry))
-        if never_download:
+        if never_download is True:
             if VIRTUALENV_VERSION_INFO >= (1, 10):
                 log.info(
                     'The virtualenv \'--never-download\' option has been '
@@ -159,15 +175,46 @@ def create(path,
             cmd.append('--prompt={0}'.format(prompt))
     else:
         # venv module from the Python >= 3.3 standard library
-        if upgrade:
+
+        # ----- Stop the user if virtualenv only options are being used ----->
+        # If any of the following values are not None, it means that the user
+        # is actually passing a True or False value. Stop Him!
+        if no_site_packages is not None:
+            raise salt.exceptions.CommandExecutionError(
+                'The `no_site_packages`(`--no-site-packages`) option is not '
+                'supported by {0!r}'.format(venv_bin)
+            )
+        elif python is not None:
+            raise salt.exceptions.CommandExecutionError(
+                'The `python`(`--python`) option is not supported '
+                'by {0!r}'.format(venv_bin)
+            )
+        elif extra_search_dir is not None:
+            raise salt.exceptions.CommandExecutionError(
+                'The `extra_search_dir`(`--extra-search-dir`) option is not '
+                'supported by {0!r}'.format(venv_bin)
+            )
+        elif never_download is not None:
+            raise salt.exceptions.CommandExecutionError(
+                'The `never_download`(`--never-download`) option is not '
+                'supported by {0!r}'.format(venv_bin)
+            )
+        elif prompt is not None:
+            raise salt.exceptions.CommandExecutionError(
+                'The `prompt`(`--prompt`) option is not supported '
+                'by {0!r}'.format(venv_bin)
+            )
+        # <---- Stop the user if virtualenv only options are being used ------
+
+        if upgrade is True:
             cmd.append('--upgrade')
-        if symlinks:
+        if symlinks is True:
             cmd.append('--symlinks')
 
-    # Common options
-    if clear:
+    # Common options to virtualenv and pyvenv
+    if clear is True:
         cmd.append('--clear')
-    if system_site_packages:
+    if system_site_packages is True:
         cmd.append('--system-site-packages')
 
     # Finally the virtualenv path
