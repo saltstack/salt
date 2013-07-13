8c38b62e49e0db8ff4a398526cdc1623ee0faf24
Thomas S Hatch <thatch45@gmail.com>
2013-07-12 21:20:38 -0600

    Add docstrings to some state methods

--- salt/state.py
+++ salt/state.py
@@ -1974,6 +1974,9 @@ class BaseHighState(object):
         return state, errors
 
     def _handle_state_decls(self, state, sls, env, errors):
+        '''
+        Add sls and env components to the state
+        '''
         for name in state:
             if not isinstance(state[name], dict):
                 if name == '__extend__':
@@ -2028,6 +2031,10 @@ class BaseHighState(object):
                 state[name]['__env__'] = env
 
     def _handle_extend(self, state, sls, env, errors):
+        '''
+        Take the extend dec out of state and apply to the highstate global
+        dec
+        '''
         if 'extend' in state:
             ext = state.pop('extend')
             if not isinstance(ext, dict):
@@ -2056,6 +2063,10 @@ class BaseHighState(object):
             state.setdefault('__extend__', []).append(ext)
 
     def _handle_exclude(self, state, sls, env, errors):
+        '''
+        Take the exclude dec out of the state and apply it to the highstate
+        global dec
+        '''
         if 'exclude' in state:
             exc = state.pop('exclude')
             if not isinstance(exc, list):
