89fa6cecf4aeb650c04e3ed9cacebd23f3529ec4
Pedro Algarvio <pedro@algarvio.me>
2013-07-14 17:17:45 +0100

    Added a mocked test case for `--ignore-installed` passing to `pip install`.

--- tests/unit/modules/pip_test.py
+++ tests/unit/modules/pip_test.py
@@ -546,6 +546,17 @@ class PipTestCase(TestCase):
                 cwd=None
             )
 
+    def test_ignore_installed_argument_in_resulting_command(self):
+        mock = MagicMock(return_value={'retcode': 0, 'stdout': ''})
+        with patch.dict(pip.__salt__, {'cmd.run_all': mock}):
+            pip.install('pep8', ignore_installed=True)
+            mock.assert_called_once_with(
+                'pip install --ignore-installed pep8',
+                runas=None,
+                cwd=None
+            )
+
+
 
 if __name__ == '__main__':
     from integration import run_tests
