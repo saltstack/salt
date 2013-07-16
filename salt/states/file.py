c52c2719f8f222f3b8c065e389716401eed8e3f1
chenming12345 <chenming12345@gmail.com>
2013-07-16 14:03:15 +0800

    fixed from Issue #6163
    This string just needs to be quoted

--- salt/states/file.py
+++ salt/states/file.py
@@ -1716,7 +1716,7 @@ def append(name,
           file.append:
             - text:
               - Trust no one unless you have eaten much salt with him.
-              - Salt is born of the purest of parents: the sun and the sea.
+              - "Salt is born of the purest of parents: the sun and the sea."
 
     .. versionadded:: 0.9.5
     '''
