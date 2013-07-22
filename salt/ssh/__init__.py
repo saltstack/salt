15557f71343ae76a3157b14f0c81726524c922fb
Thomas S Hatch <thatch45@gmail.com>
2013-07-21 23:06:17 -0600

    clean up auto deploy

--- salt/ssh/__init__.py
+++ salt/ssh/__init__.py
@@ -92,14 +92,16 @@ class SSH(object):
         '''
         Deploy the ssh key if the minions don't auth
         '''
+        if not isinstance(ret[host], basestring):
+            return ret
         if ret[host].startswith('Permission denied'):
             target = self.targets[host]
             # permission denied, attempt to auto deploy ssh key
             print(('Permission denied for host {0}, do you want to '
-                    'deploy the salt-ssh key?'))
+                    'deploy the salt-ssh key?').format(host))
             deploy = raw_input('[Y/n]')
             if deploy.startswith(('n', 'N')):
-                continue
+                return ret
             target['passwd'] = getpass.getpass(
                     'Password for {0}:'.format(host)
                     )
