da46fc6605005401454c37fccab4381ec670801a
Thomas S Hatch <thatch45@gmail.com>
2013-07-24 10:02:17 -0600

    Add file.missing state

--- salt/states/file.py
+++ salt/states/file.py
@@ -655,6 +655,25 @@ def exists(name):
     return ret
 
 
+def missing(name):
+    '''
+    Verify that the named file or directory is missing, this returns True only
+    if the named file is missing but does not remove the file if it is present.
+
+    name
+        Absolute path which must NOT exist
+    '''
+    ret = {'name': name,
+           'changes': {},
+           'result': True,
+           'comment': ''}
+    if os.path.exists(name):
+        return _error(ret, ('Specified path {0} exists').format(name))
+
+    ret['comment'] = 'Path {0} is missing'.format(name)
+    return ret
+
+
 def managed(name,
             source=None,
             source_hash='',
