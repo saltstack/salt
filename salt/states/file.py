4251341a7b2dd5f80a2da0afb1554c584c2db692
Thomas S Hatch <thatch45@gmail.com>
2013-07-30 10:41:34 -0600

    Add file.copy state
    @UtahDave can you please double check this? fix for #6440

--- salt/states/file.py
+++ salt/states/file.py
@@ -1989,6 +1989,90 @@ def touch(name, atime=None, mtime=None, makedirs=False):
     return ret
 
 
+def copy(name, source, force=False, makedirs=False):
+    '''
+    If the source file exists on the system, copy it to the named file. The
+    named file will not be overwritten if it already exists unless the force
+    option is set to True.
+
+    name
+        The location of the file to copy to
+
+    source
+        The location of the file to copy to the location specified with name
+
+    force
+        If the target location is present then the file will not be moved,
+        specify "force: True" to overwrite the target file
+
+    makedirs
+        If the target subdirectories don't exist create them
+
+    '''
+    ret = {
+        'name': name,
+        'changes': {},
+        'comment': '',
+        'result': True}
+
+    if not os.path.isabs(name):
+        return _error(
+            ret, 'Specified file {0} is not an absolute path'.format(name))
+
+    if not os.path.exists(source):
+        return _error(ret, 'Source file "{0}" is not present'.format(source))
+
+    if os.path.lexists(source) and os.path.lexists(name):
+        if not force:
+            ret['comment'] = ('The target file "{0}" exists and will not be '
+                              'overwritten'.format(name))
+            ret['result'] = True
+            return ret
+        elif not __opts__['test']:
+            # Remove the destination to prevent problems later
+            try:
+                if os.path.islink(name):
+                    os.unlink(name)
+                elif os.path.isfile(name):
+                    os.remove(name)
+                else:
+                    shutil.rmtree(name)
+            except (IOError, OSError):
+                return _error(
+                    ret,
+                    'Failed to delete "{0}" in preparation for '
+                    'forced move'.format(name)
+                )
+
+    if __opts__['test']:
+        ret['comment'] = 'File "{0}" is set to be copied to "{1}"'.format(
+            source,
+            name
+        )
+        ret['result'] = None
+        return ret
+
+    # Run makedirs
+    dname = os.path.dirname(name)
+    if not os.path.isdir(dname):
+        if makedirs:
+            os.makedirs(dname)
+        else:
+            return _error(
+                ret,
+                'The target directory {0} is not present'.format(dname))
+    # All tests pass, move the file into place
+    try:
+        shutil.copy(source, name)
+    except (IOError, OSError):
+        return _error(
+            ret, 'Failed to copy "{0}" to "{1}"'.format(source, name))
+
+    ret['comment'] = 'Copied "{0}" to "{1}"'.format(source, name)
+    ret['changes'] = {name: source}
+    return ret
+
+
 def rename(name, source, force=False, makedirs=False):
     '''
     If the source file exists on the system, rename it to the named file. The
