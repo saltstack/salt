753fcac10a31ae2dce2575f91bbf086e64d4eb01
Thomas S Hatch <thatch45@gmail.com>
2013-08-10 09:11:24 -0600

    whitespace

--- salt/states/reg.py
+++ salt/states/reg.py
@@ -7,7 +7,6 @@ def __virtual__():
     '''
     Load this state if the reg module exists
     '''
-    
     return 'reg' if 'reg.read_key' in __salt__ else False
 
 
@@ -15,7 +14,6 @@ def _parse_key(key):
     '''
     split the full path in the registry to the key and the rest
     '''
-    
     splt = key.split('\\')
     hive = splt.pop(0)
     key = splt.pop(-1)
@@ -34,12 +32,11 @@ def present(name, value, vtype='REG_DWORD'):
             - value: 0.15.3
             - vtype: REG_SZ
     '''
-    
     ret = {'name': name,
            'result': True,
            'changes': {},
            'comment': ''}
-    
+
     # determine what to do
     hive, path, key = _parse_key(name)
     if value == __salt__['reg.read_key'](hive, path, key):
@@ -47,17 +44,15 @@ def present(name, value, vtype='REG_DWORD'):
         return ret
     else:
         ret['changes'] = {'reg': 'configured to {}'.format(value) }
-    
+
     if __opts__['test']:
         ret['result'] = None
         return ret
-    
+
     # configure the key
     ret['result'] = __salt__['reg.set_key'](hive, path, key, value, vtype)
     if not ret:
         ret['changes'] = {}
         ret['comment'] = 'could not configure the registry key'
-    
+
     return ret
-
-
