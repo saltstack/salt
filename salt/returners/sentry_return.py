cb96439fce214ae9f0b46f3ab465c0ff9f40388f
Thomas S Hatch <thatch45@gmail.com>
2013-08-10 09:14:08 -0600

    whitespace

--- salt/returners/sentry_return.py
+++ salt/returners/sentry_return.py
@@ -65,7 +65,7 @@ def returner(ret):
         if pillar_data['raven'].has_key('tags'):
             for tag  in pillar_data['raven']['tags']:
                 tags[tag] = grains[tag]
-         
+
         if ret['return']:
             data['level'] = 'info'
 
