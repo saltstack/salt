b1dc8c63ee3fb5de9f911987c02527741e1e882e
Thomas S Hatch <thatch45@gmail.com>
2013-08-10 09:06:37 -0600

    whitespace fix

--- salt/states/win_path.py
+++ salt/states/win_path.py
@@ -9,14 +9,12 @@ def __virtual__():
     '''
     Load this state if the win_path module exists
     '''
-    
     return 'win_path' if 'win_path.rehash' in __salt__ else False
 
 def _normalize_dir(string):
     '''
     Normalize the directory to make comparison possible
     '''
-    
     return re.sub(r'\\$', '', string.lower())
 
 
@@ -31,21 +29,20 @@ def absent(name):
         'C:\\sysinternals':
           win_path.absent
     '''
-    
     ret = {'name': name,
            'result': True,
            'changes': {},
            'comment': ''}
-    
+
     if __salt__['win_path.exists'](name):
         ret['changes']['removed'] = name
     else:
         ret['comment'] = '{0} is not in the PATH'.format(name)
-    
+
     if __opts__['test']:
         ret['result'] = None
         return ret
-    
+
     ret['result'] = __salt__['win_path.remove'](name)
     if not ret['result']:
         ret['comment'] = 'could not remove {0} from the PATH'.format(name)
@@ -66,22 +63,21 @@ def exists(name, index=0):
           win_path.exists:
             index: 0
     '''
-    
     ret = {'name': name,
            'result': True,
            'changes': {},
            'comment': ''}
-    
+
     # determine what to do
     sysPath = __salt__['win_path.get_path']()
     path = _normalize_dir(name)
-    
+
     index = int(index)
     if index < 0:
         index = len(sysPath) + index
     if index > len(sysPath)-1:
         index = len(sysPath)-1
-    
+
     try:
         currIndex = sysPath.index(path)
         if currIndex != index:
@@ -92,12 +88,12 @@ def exists(name, index=0):
             return ret
     except ValueError:
         pass
-    
+
     ret['changes']['added'] = '{0} will be added at index {1}'.format(name, index)
     if __opts__['test']:
         ret['result'] = None
         return ret
-    
+
     # Add it
     ret['result'] = __salt__['win_path.add'](path, index)
     if not ret['result']:
