4b2a968a22a6beef04054f6d5bf03b035de51ce6
Pedro Algarvio <pedro@algarvio.me>
2013-08-10 14:57:21 +0100

    Typo, add option with `-` which is what's common and remove unneeded code.

--- salt/utils/parsers.py
+++ salt/utils/parsers.py
@@ -959,19 +959,16 @@ class SaltCMDOptionParser(OptionParser, ConfigDirMixIn, MergeConfigMixIn,
             help=('Run the salt command but don\'t wait for a reply')
         )
         self.add_option(
-            '--state_output',
+            '--state-output', '--state_output',
             default='full',
-            dest='state_output',
-            type=str,
             help=('Override the configured state_output value for minion output'
                   '. Default: full')
         )
         self.add_option(
             '--subset',
             default=0,
-            dest='subset',
             type=int,
-            help=('Execute the routine on a random subset of the targetted '
+            help=('Execute the routine on a random subset of the targeted '
                   'minions. The minions will be verified that they have the '
                   'named function before executing')
         )
