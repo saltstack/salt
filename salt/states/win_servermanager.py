12c9fbc12f0aa55e1db6936bbd192b165983fa8f
Thomas S Hatch <thatch45@gmail.com>
2013-08-10 09:09:15 -0600

    whitespace

--- salt/states/win_servermanager.py
+++ salt/states/win_servermanager.py
@@ -7,7 +7,6 @@ def __virtual__():
     '''
     Load only if win_servermanager is loaded
     '''
-    
     return 'win_servermanager' if 'win_servermanager.install' in __salt__ else False
 
 
@@ -29,12 +28,11 @@ def installed(name, recurse=False, force=False):
     Some features requires reboot after un/installation, if so until the server is restarted
     Other features can not be installed !
     '''
-    
     ret = {'name': name,
            'result': True,
            'changes': {},
            'comment': ''}
-    
+
     # Determine if the feature is installed
     if name not in __salt__['win_servermanager.list_installed']():
         ret['changes'] = {'feature': '{0} will be installed recurse={1}'.format(name, recurse)}
@@ -43,16 +41,15 @@ def installed(name, recurse=False, force=False):
     else:
         ret['comment'] = 'The feature {0} is already installed'.format(name)
         return ret
-    
+
     if __opts__['test']:
         ret['result'] = None
         return ret
-    
+
     # Install the features
     ret['changes'] = {'feature': __salt__['win_servermanager.install'](name, recurse)}
     ret['result'] = ret['changes']['feature']['Success'] == 'True'
     if not ret['result']:
         ret['comment'] = 'failed to install the feature: {0}'.format( ret['changes']['feature']['ExitCode'] )
-    
-    return ret
 
+    return ret
