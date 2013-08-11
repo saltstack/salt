5573846e9952287a759f4605875651894381d4a4
Thomas S Hatch <thatch45@gmail.com>
2013-08-10 23:26:37 -0600

    Add super call to yumpkg

--- salt/modules/yumpkg.py
+++ salt/modules/yumpkg.py
@@ -39,6 +39,7 @@ try:
         implementation.
         '''
         def __init__(self):
+            super(yum.rpmtrans.RPMBaseCallback).__init__()
             self.messages = {}
             self.failed = []
             self.action = {
