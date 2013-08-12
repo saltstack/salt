24048da37ca66890796dc375e7701a7574c999b1
Samuel M Smith <smith.samuel.m@gmail.com>
2013-08-12 17:39:16 -0600

    Fixed bug. Check for empty clear_load['jid'] was occuring after fire_event which produced empty tag in event. Fix now ensures tag is valid. Note this may fix bugs with syndic FYI "Colton"

--- salt/master.py
+++ salt/master.py
@@ -2152,7 +2152,6 @@ class ClearFuncs(object):
                         'minions': minions
                     }
                 }
-        self.event.fire_event({'minions': minions}, clear_load['jid'])
         # Retrieve the jid
         if not clear_load['jid']:
             clear_load['jid'] = salt.utils.prep_jid(
@@ -2160,6 +2159,7 @@ class ClearFuncs(object):
                     self.opts['hash_type'],
                     extra.get('nocache', False)
                     )
+        self.event.fire_event({'minions': minions}, clear_load['jid'])
         jid_dir = salt.utils.jid_dir(
                 clear_load['jid'],
                 self.opts['cachedir'],
