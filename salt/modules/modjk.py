a1c636a8c2c093e252de1f040f59d48b885fa1c6
Erik Johnson <palehose@gmail.com>
2013-08-13 11:39:26 -0500

    Cleanup docstrings in modjk state/module
    Fixed spelling/grammar errors, improved formatting, and introduced links
    to improve doc navigation.

--- salt/modules/modjk.py
+++ salt/modules/modjk.py
@@ -1,24 +1,34 @@
 '''
-Control Modjk via the Status worker
-http://tomcat.apache.org/connectors-doc/reference/status.html
+Control Modjk via the Apache Tomcat `Status worker`_
 
-The following configuration needs to exists in pillar/grains
-each entry nested in modjk is a profile of the loadbalancer in question
-this would give support to multiple modjk balancers in different hosts/vhosts
+.. _`Status worker`: http://tomcat.apache.org/connectors-doc/reference/status.html
 
-modjk:
-  'default':
-    'url': http://localhost/jkstatus
-    'user': modjk
-    'pass': secret
-    'realm': 'authentication realm for digest passwords'
-    'timeout': 5
-  'otherVhost:
-    'url': http://otherVhost/jkstatus
-    'user': modjk
-    'pass': secret2
-    'realm': 'authentication realm2 for digest passwords'
-    'timeout': 600
+Below is an example of the configuration needed for this module. This
+configuration data can be placed either in :doc:`grains
+</topics/targeting/grains>` or :doc:`pillar </topics/pillar/index>`.
+
+If using grains, this can be accomplished :ref:`statically
+<static-custom-grains>` or via a :ref:`grain module <writing-grains>`.
+
+If using pillar, the yaml configuration can be placed directly into a pillar
+SLS file, making this both the easier and more dynamic method of configuring
+this module.
+
+.. code-block:: yaml
+
+    modjk:
+      default:
+        url: http://localhost/jkstatus
+        user: modjk
+        pass: secret
+        realm: authentication realm for digest passwords
+        timeout: 5
+      otherVhost:
+        url: http://otherVhost/jkstatus
+        user: modjk
+        pass: secret2
+        realm: authentication realm2 for digest passwords
+        timeout: 600
 '''
 
 # Python libs
@@ -30,7 +40,7 @@ def __virtual__():
     '''
     Always load
     '''
-    
+
     return 'modjk'
 
 
@@ -50,36 +60,36 @@ def _do_http(opts, profile='default'):
     '''
     Make the http request and return the data
     '''
-    
+
     ret = {}
-    
-    url = __salt__['config.get']('modjk:'+profile+':url', '')
-    user = __salt__['config.get']('modjk:'+profile+':user', '')
-    passwd = __salt__['config.get']('modjk:'+profile+':pass', '')
-    realm = __salt__['config.get']('modjk:'+profile+':realm', '')
-    timeout = __salt__['config.get']('modjk:'+profile+':timeout', 5)
-    
+
+    url = __salt__['config.get']('modjk:' + profile + ':url', '')
+    user = __salt__['config.get']('modjk:' + profile + ':user', '')
+    passwd = __salt__['config.get']('modjk:' + profile + ':pass', '')
+    realm = __salt__['config.get']('modjk:' + profile + ':realm', '')
+    timeout = __salt__['config.get']('modjk:' + profile + ':timeout', 5)
+
     if not url:
         raise Exception('missing url in profile {0}'.format(profile))
-    
+
     if user and passwd:
         auth = _auth(url, realm, user, passwd)
         urllib2.install_opener(auth)
-    
+
     url += '?{0}'.format(urllib.urlencode(opts))
-    
+
     for line in urllib2.urlopen(url, timeout=timeout).read().splitlines():
         splt = line.split('=', 1)
         ret[splt[0]] = splt[1]
-    
+
     return ret
 
-   
+
 def _workerCtl(worker, lb, vwa, profile='default'):
     '''
     enable/disable/stop a worker
     '''
-    
+
     cmd = {
         'cmd': 'update',
         'mime': 'prop',
@@ -98,13 +108,13 @@ def _workerCtl(worker, lb, vwa, profile='default'):
 def version(profile='default'):
     '''
     Return the modjk version
-    
+
     CLI Examples::
 
         salt '*' modjk.version
         salt '*' modjk.version other-profile
     '''
-    
+
     cmd = {
         'cmd': 'version',
         'mime': 'prop',
@@ -115,13 +125,13 @@ def version(profile='default'):
 def get_running(profile='default'):
     '''
     Get the current running config (not from disk)
-    
+
     CLI Examples::
 
         salt '*' modjk.get_running
         salt '*' modjk.get_running other-profile
     '''
-    
+
     cmd = {
         'cmd': 'list',
         'mime': 'prop',
@@ -132,13 +142,13 @@ def get_running(profile='default'):
 def dump_config(profile='default'):
     '''
     Dump the original configuration that was loaded from disk
-    
+
     CLI Examples::
 
         salt '*' modjk.dump_config
         salt '*' modjk.dump_config other-profile
     '''
-    
+
     cmd = {
         'cmd': 'dump',
         'mime': 'prop',
@@ -154,33 +164,33 @@ def dump_config(profile='default'):
 def list_configured_members(lb, profile='default'):
     '''
     Return a list of member workers from the configuration files
-    
+
     CLI Examples::
 
         salt '*' modjk.list_configured_members loadbalancer1
         salt '*' modjk.list_configured_members loadbalancer1 other-profile
     '''
-    
+
     config = dump_config(profile)
-    
+
     try:
         ret = config['worker.{0}.balance_workers'.format(lb)].strip().split(',')
     except KeyError:
         return []
-    
+
     return filter(None, ret)
 
 
 def list_running_members(lb, profile='default'):
     '''
     Return a list of member workers
-    
+
     CLI Examples::
 
         salt '*' modjk.list_running_members loadbalancer1
         salt '*' modjk.list_running_members loadbalancer1 other-profile
     '''
-    
+
     config = get_running()
     try:
         return config['worker.{0}.balance_workers'.format(lb)].split(',')
@@ -191,15 +201,15 @@ def list_running_members(lb, profile='default'):
 def recover_all(lb, profile='default'):
     '''
     Set the all the workers in lb to recover and activate them if they are not
-    
+
     CLI Examples::
 
         salt '*' modjk.recover_all loadbalancer1
         salt '*' modjk.recover_all loadbalancer1 other-profile
     '''
-    
+
     ret = {}
-    
+
     workers = list_running_members(lb, profile)
     for worker in workers:
         curr_state = worker_status(worker, profile)
@@ -208,20 +218,20 @@ def recover_all(lb, profile='default'):
         if not curr_state['state'].startswith('OK'):
             worker_recover(worker, lb, profile)
         ret[worker] = worker_status(worker, profile)
-    
+
     return ret
 
 
 def reset_stats(lb, profile='default'):
     '''
     Reset all runtime statistics for the load balancer
-    
+
     CLI Examples::
 
         salt '*' modjk.reset_stats loadbalancer1
         salt '*' modjk.reset_stats loadbalancer1 other-profile
     '''
-    
+
     cmd = {
         'cmd': 'reset',
         'mime': 'prop',
@@ -233,21 +243,21 @@ def reset_stats(lb, profile='default'):
 def lb_edit(lb, settings, profile='default'):
     '''
     Edit the loadbalancer settings
-    
+
     Note: http://tomcat.apache.org/connectors-doc/reference/status.html
     Data Parameters for the standard Update Action
-    
+
     CLI Examples::
 
         salt '*' modjk.lb_edit loadbalancer1 "{'vlr': 1, 'vlt': 60}"
         salt '*' modjk.lb_edit loadbalancer1 "{'vlr': 1, 'vlt': 60}" other-profile
     '''
-    
+
     settings['cmd'] = 'update'
     settings['mime'] = 'prop'
     settings['w'] = lb
-    
-    return _do_http(settings, profile)['worker.result.type'] == 'OK'    
+
+    return _do_http(settings, profile)['worker.result.type'] == 'OK'
 
 
 ########################
@@ -258,13 +268,13 @@ def lb_edit(lb, settings, profile='default'):
 def worker_status(worker, profile='default'):
     '''
     Return the state of the worker
-    
+
     CLI Examples::
 
         salt '*' modjk.worker_status node1
         salt '*' modjk.worker_status node1 other-profile
     '''
-    
+
     config = get_running(profile)
     try:
         return {
@@ -279,13 +289,13 @@ def worker_recover(worker, lb, profile='default'):
     '''
     Set the worker to recover
     this module will fail if it is in OK state
-    
+
     CLI Examples::
 
         salt '*' modjk.worker_recover node1 loadbalancer1
         salt '*' modjk.worker_recover node1 loadbalancer1 other-profile
     '''
-    
+
     cmd = {
         'cmd': 'recover',
         'mime': 'prop',
@@ -298,59 +308,58 @@ def worker_recover(worker, lb, profile='default'):
 def worker_disable(worker, lb, profile='default'):
     '''
     Set the worker to disable state in the lb load balancer
-    
+
     CLI Examples::
 
         salt '*' modjk.worker_disable node1 loadbalancer1
         salt '*' modjk.worker_disable node1 loadbalancer1 other-profile
     '''
-    
+
     return _workerCtl(worker, lb, 'd', profile)
 
 
 def worker_activate(worker, lb, profile='default'):
     '''
     Set the worker to activate state in the lb load balancer
-    
+
     CLI Examples::
 
         salt '*' modjk.worker_activate node1 loadbalancer1
         salt '*' modjk.worker_activate node1 loadbalancer1 other-profile
     '''
-    
+
     return _workerCtl(worker, lb, 'a', profile)
 
 
 def worker_stop(worker, lb, profile='default'):
     '''
     Set the worker to stopped state in the lb load balancer
-    
+
     CLI Examples::
 
         salt '*' modjk.worker_activate node1 loadbalancer1
         salt '*' modjk.worker_activate node1 loadbalancer1 other-profile
     '''
-    
+
     return _workerCtl(worker, lb, 's', profile)
 
 
 def worker_edit(worker, lb, settings, profile='default'):
     '''
     Edit the worker settings
-    
+
     Note: http://tomcat.apache.org/connectors-doc/reference/status.html
     Data Parameters for the standard Update Action
-    
+
     CLI Examples::
 
         salt '*' modjk.lb_edit node1 loadbalancer1 "{'vwf': 500, 'vwd': 60}"
         salt '*' modjk.lb_edit node1 loadbalancer1 "{'vwf': 500, 'vwd': 60}" other-profile
     '''
-    
+
     settings['cmd'] = 'update'
     settings['mime'] = 'prop'
     settings['w'] = lb
     settings['sw'] = worker
-    
-    return _do_http(settings, profile)['worker.result.type'] == 'OK'
 
+    return _do_http(settings, profile)['worker.result.type'] == 'OK'
