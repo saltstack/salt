b498fcba4200f933c035c3417c41ff4a11ea0dbe
Kevin Stone <kevinastone@gmail.com>
2013-08-13 14:31:57 -0700

    Update host/ip pairing rather than add the hostname a second time

--- salt/states/host.py
+++ salt/states/host.py
@@ -46,8 +46,11 @@ def present(name, ip):  # pylint: disable=C0103
         ret['comment'] = 'Host {0} already present'.format(name)
         return ret
     if __opts__['test']:
-        ret['comment'] = 'Host {0} needs to be added'.format(name)
+        ret['comment'] = 'Host {0} needs to be added/updated'.format(name)
         return ret
+    current_ip = __salt__['hosts.get_ip'](name)
+    if current_ip and current_ip != ip:
+        __salt__['hosts.rm_host'](current_ip, name)
     if __salt__['hosts.add_host'](ip, name):
         ret['changes'] = {'host': name}
         ret['result'] = True
