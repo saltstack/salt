saltstack/salt

commit 25512be9a5cd6375fb0224a782c19a3402856b3f

Register an encoding error handler in order to force `ascii`. Fixes #687.
This error handler will try to match known `unicode` characters returned by the could drivers and only those and replace them with an `ascii` one.

saltcloud/output/double_layer.py
@@ -6,12 +6,6 @@ printed
 # Import Salt libs
 import salt.utils
 
-UNICODE_TRANS = {
-    0xA0:           u' ',   # Convert non-breaking space to space
-    '\xe2\x80\x93': u'-',   # Convert en dash to dash
-    u'\u2013':      u'-',   # Convert en dash to dash
-}
-
 
 def output(data):
     '''
@@ -25,10 +19,9 @@ def output(data):
             '{0}{1}{2}'.format(colors['GREEN'], top_key, colors['ENDC'])
         )
         for sec_key in sorted(data[top_key]):
-            newval = unicode(sec_key).translate(UNICODE_TRANS)
             ret.append('  {0}{1}{2}'.format(
                 colors['YELLOW'],
-                newval.encode('ascii', 'ignore'),
+                sec_key.encode('ascii', 'salt-cloud-force-ascii'),
                 colors['ENDC'])
             )
             tval = data[top_key][sec_key]
@@ -48,11 +41,10 @@ def output(data):
                         colors['ENDC'])
                     )
                     for ekey in sorted(val):
-                        newval = unicode(val[ekey]).translate(UNICODE_TRANS)
                         ret.append('      {0}{1}: {2}{3}'.format(
                             colors['LIGHT_GREEN'],
                             ekey,
-                            newval.encode('ascii', 'ignore'),
+                            ekey.encode('ascii', 'salt-cloud-force-ascii'),
                             colors['ENDC'])
                         )
     return '\n'.join(ret)

saltcloud/libcloudfuncs.py
@@ -21,6 +21,7 @@ from libcloud.compute.deployment import (
 
 
 # Import salt libs
+import salt._compat
 import salt.utils.event
 
 # Import salt cloud libs
@@ -154,10 +155,22 @@ def get_location(conn, vm_):
     Return the location object to use
     '''
     locations = conn.list_locations()
-    vm_location = config.get_config_value('location', vm_, __opts__)
+    vm_location = config.get_config_value('location', vm_, __opts__).encode(
+        'ascii', 'salt-cloud-force-ascii'
+    )
 
     for img in locations:
-        if vm_location and str(vm_location) in (str(img.id), str(img.name)):
+        if isinstance(img.id, salt._compat.string_types):
+            img_id = img.id.encode('ascii', 'salt-cloud-force-ascii')
+        else:
+            img_id = str(img.id)
+
+        if isinstance(img.name, salt._compat.string_types):
+            img_name = img.name.encode('ascii', 'salt-cloud-force-ascii')
+        else:
+            img_name = str(img.name)
+
+        if vm_location and vm_location in (img_id, img_name):
             return img
 
     raise SaltCloudNotFound('The specified location could not be found.')
@@ -169,10 +182,22 @@ def get_image(conn, vm_):
     '''
     images = conn.list_images()
 
-    vm_image = config.get_config_value('image', vm_, __opts__)
+    vm_image = config.get_config_value('image', vm_, __opts__).encode(
+        'ascii', 'salt-cloud-force-ascii'
+    )
 
     for img in images:
-        if vm_image and str(vm_image) in (str(img.id), str(img.name)):
+        if isinstance(img.id, salt._compat.string_types):
+            img_id = img.id.encode('ascii', 'salt-cloud-force-ascii')
+        else:
+            img_id = str(img.id)
+
+        if isinstance(img.name, salt._compat.string_types):
+            img_name = img.name.encode('ascii', 'salt-cloud-force-ascii')
+        else:
+            img_name = str(img.name)
+
+        if vm_image and vm_image in (img_id, img_name):
             return img
 
     raise SaltCloudNotFound('The specified image could not be found.')

saltcloud/utils/__init__.py
@@ -6,6 +6,7 @@ Utility functions for saltcloud
 import os
 import pwd
 import sys
+import codecs
 import shutil
 import socket
 import tempfile
@@ -1045,3 +1046,21 @@ class CloudProviderContext(object):
         # Reset to previous value
         mod = sys.modules[self.__function.__module__]
         mod.__active_provider_name__ = self.__default
+
+
+def salt_cloud_force_ascii(exc):
+    if not isinstance(exc, (UnicodeEncodeError, UnicodeTranslateError)):
+        raise TypeError('Can\'t handle {0}'.format(exc))
+
+    unicode_trans = {
+        u'\xa0':    u' ',   # Convert non-breaking space to space
+        u'\u2013':  u'-',   # Convert en dash to dash
+    }
+
+    if exc.object[exc.start:exc.end] in unicode_trans:
+        return unicode_trans[exc.object[exc.start:exc.end]], exc.end
+
+    # There's nothing else we can do, raise the exception
+    raise exc
+
+codecs.register_error('salt-cloud-force-ascii', salt_cloud_force_ascii)

commit da9e614f53979feae006148253e4377c9f15932f

Fix double layer outputter bug introduced in 25512be by me.

saltcloud/output/double_layer.py
@@ -44,7 +44,8 @@ def output(data):
                         ret.append('      {0}{1}: {2}{3}'.format(
                             colors['LIGHT_GREEN'],
                             ekey,
-                            ekey.encode('ascii', 'salt-cloud-force-ascii'),
+                            val[ekey].encode(
+                                'ascii', 'salt-cloud-force-ascii'),
                             colors['ENDC'])
                         )
     return '\n'.join(ret)