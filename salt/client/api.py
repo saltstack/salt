149d4058b9cb6f08690d5a94c43168b4ab9d14ed
Samuel M Smith <smith.samuel.m@gmail.com>
2013-08-14 16:18:52 -0600

    refactor tokenify

--- salt/client/api.py
+++ salt/client/api.py
@@ -15,15 +15,15 @@ import salt.utils
 from salt.exceptions import SaltException, EauthAuthenticationError
 from salt.utils.event import tagify
 
-def tokenify(low, token=None):
+def tokenify(cmd, token=None):
     '''
-    If token is not None Then assign token to 'token' key of lowstate cmd dict
-        low and return low
-    Otherwise return low
+    If token is not None Then assign token to 'token' key of cmd dict
+        and return cmd
+    Otherwise return cmd
     '''
     if token is not None:
-        low['token'] = token
-    return low
+        cmd['token'] = token
+    return cmd
 
 
 class APIClient(object):
