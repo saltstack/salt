649998c23d7921808bfe57ec7a04cfa8f71aa1ac
Joseph Hall <perlhoser@gmail.com>
2013-08-16 15:39:12 -0600

    Fix typo introduced in #729

--- saltcloud/clouds/saltify.py
+++ saltcloud/clouds/saltify.py
@@ -172,7 +172,7 @@ def get_configured_provider():
     '''
     return config.is_provider_configured(
         __opts__,
-        __active_provider_name__ or 'aws',
+        __active_provider_name__ or 'saltify',
         ()
     )
 
