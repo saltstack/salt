58ad040872e5ec5364913b4976bbe50399933991
Pedro Algarvio <pedro@algarvio.me>
2013-08-18 06:45:23 +0100

    Use `pip.req` for parsing package requirements. Minimal tests.

--- tests/unit/states/pip_test.py
+++ tests/unit/states/pip_test.py
@@ -18,7 +18,7 @@ ensure_in_syspath('../../')
 
 # Import salt libs
 import integration
-from salt.states import pip
+from salt.states import pip_state as pip
 from salt.exceptions import CommandExecutionError
 
 # Import 3rd-party libs
@@ -110,6 +110,56 @@ class PipStateTest(TestCase, integration.SaltReturnAssertsMixIn):
                 user='Me!',
                 runas='Not Me!'
             )
+
+    def test_install_requirements_parsing(self):
+        mock = MagicMock(return_value={'retcode': 0, 'stdout': ''})
+        pip_list = MagicMock(return_value={'pep8': '1.3.3'})
+        with patch.dict(pip.__salt__, {'cmd.run_all': mock,
+                                       'pip.list': pip_list}):
+            with patch.dict(pip.__opts__, {'test': True}):
+                ret = pip.installed('pep8>=1.3.2')
+                self.assertSaltTrueReturn({'test': ret})
+                self.assertInSaltComment(
+                    'Python package pep8>=1.3.2 already installed',
+                    {'test': ret}
+                )
+
+        mock = MagicMock(return_value={'retcode': 0, 'stdout': ''})
+        pip_list = MagicMock(return_value={'pep8': '1.3.3'})
+        with patch.dict(pip.__salt__, {'cmd.run_all': mock,
+                                       'pip.list': pip_list}):
+            with patch.dict(pip.__opts__, {'test': True}):
+                ret = pip.installed('pep8<1.3.2')
+                self.assertSaltNoneReturn({'test': ret})
+                self.assertInSaltComment(
+                    'Python package pep8<1.3.2 is set to be installed',
+                    {'test': ret}
+                )
+
+        mock = MagicMock(return_value={'retcode': 0, 'stdout': ''})
+        pip_list = MagicMock(return_value={'pep8': '1.3.2'})
+        with patch.dict(pip.__salt__, {'cmd.run_all': mock,
+                                       'pip.list': pip_list}):
+            with patch.dict(pip.__opts__, {'test': True}):
+                ret = pip.installed('pep8>1.3.1,<1.3.3')
+                self.assertSaltTrueReturn({'test': ret})
+                self.assertInSaltComment(
+                    'Python package pep8>1.3.1;<1.3.3 already installed',
+                    {'test': ret}
+                )
+
+        mock = MagicMock(return_value={'retcode': 0, 'stdout': ''})
+        pip_list = MagicMock(return_value={'pep8': '1.3.1'})
+        with patch.dict(pip.__salt__, {'cmd.run_all': mock,
+                                       'pip.list': pip_list}):
+            with patch.dict(pip.__opts__, {'test': True}):
+                ret = pip.installed('pep8>1.3.1,<1.3.3')
+                self.assertSaltNoneReturn({'test': ret})
+                self.assertInSaltComment(
+                    'Python package pep8>1.3.1;<1.3.3 is set to be installed',
+                    {'test': ret}
+                )
+
 
 
 if __name__ == '__main__':
