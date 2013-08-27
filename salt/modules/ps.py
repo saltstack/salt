cbc6a8a94b51d045717aa3bd520993b43214926d
Thomas S Hatch <thatch45@gmail.com>
2013-08-27 16:40:38 -0600

    catch trace in ps module virtual function

--- salt/modules/ps.py
+++ salt/modules/ps.py
@@ -24,9 +24,12 @@ def __virtual__():
     # The python 2.6 version of psutil lacks several functions
     # used in this salt module so instead of spaghetti  string
     # code to try to bring sanity to everything, disable it.
-    if sys.version_info[0] == 2 and sys.version_info[1] < 7:
+    try:
+        if sys.version_info[0] == 2 and sys.version_info[1] < 7:
+            return False
+    except Exception:
         return False
-    return "ps"
+    return 'ps'
 
 
 def top(num_processes=5, interval=3):
