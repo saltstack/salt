7d0692b0670eb377f20aedba36d717f644586b7b
Paul Belanger <paul.belanger@polybeacon.com>
2013-08-29 23:20:42 -0400

    Flake8 E121 fixes
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- doc/conf.py
+++ doc/conf.py
@@ -237,8 +237,8 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'salt-cloud.tex', u'salt-cloud Documentation',
-   u'Thomas S Hatch', 'manual'),
+    ('index', 'salt-cloud.tex', u'salt-cloud Documentation',
+     u'Thomas S Hatch', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -286,9 +286,9 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'salt-cloud', u'salt-cloud Documentation',
-   u'Thomas S Hatch', 'salt-cloud', 'One line description of project.',
-   'Miscellaneous'),
+    ('index', 'salt-cloud', u'salt-cloud Documentation',
+     u'Thomas S Hatch', 'salt-cloud', 'One line description of project.',
+     'Miscellaneous'),
 ]
 
 # Documents to append as an appendix to all manuals.
