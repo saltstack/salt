saltstack/salt

commit 342a6ae3bc04a4cb647e0c0471f1388ba2a15a72

Initial addition of fileserver event
This event is intended to "notify" salt that there are changes to the underlying fileserver module. This can be used to (for example) trigger a module sync within the reactor system-- but really you can do whatever you want with it.

salt/fileserver/roots.py
@@ -81,6 +81,36 @@ def update():
     '''
     salt.fileserver.reap_fileserver_cache_dir(os.path.join(__opts__['cachedir'], 'roots/hash'), find_file)
 
+    mtime_map_path = os.path.join(__opts__['cachedir'], 'roots/mtime_map')
+    # data to send on event
+    data = {'changed': False,
+            'backend': 'roots'}
+
+    old_mtime_map = {}
+    # if you have an old map, load that
+    if os.path.exists(mtime_map_path):
+        with salt.utils.fopen(mtime_map_path, 'rb') as fp_:
+            for line in fp_:
+                file_path, mtime = line.split(':', 1)
+                old_mtime_map[file_path] = mtime
+
+    # generate the new map
+    new_mtime_map = salt.fileserver.generate_mtime_map(__opts__['file_roots'])
+
+    # compare the maps, set changed to the return value
+    data['changed'] = salt.fileserver.diff_mtime_map(old_mtime_map, new_mtime_map)
+
+
+    # write out the new map
+    with salt.utils.fopen(mtime_map_path, 'w') as fp_:
+        for file_path, mtime in new_mtime_map.iteritems():
+            fp_.write('{file_path}:{mtime}\n'.format(file_path=file_path,
+                                                     mtime=mtime))
+
+    # if there is a change, fire an event
+    event = salt.utils.event.MasterEvent(__opts__['sock_dir'])
+    event.fire_event(data, 'fileserver')
+
 def file_hash(load, fnd):
     '''
     Return a file hash, the hash type is set in the master config file

salt/fileserver/__init__.py
@@ -14,6 +14,37 @@ import salt.loader
 
 log = logging.getLogger(__name__)
 
+def generate_mtime_map(path_map):
+    '''
+    Generate a dict of filename -> mtime
+    '''
+    file_map = {}
+    for env, path_list in path_map.iteritems():
+        for path in path_list:
+            for directory, dirnames, filenames in os.walk(path):
+                for item in filenames:
+                    file_path = os.path.join(directory, item)
+                    file_map[file_path] = os.path.getmtime(file_path)
+    return file_map
+
+def diff_mtime_map(map1, map2):
+    '''
+    Is there a change to the mtime map? return a boolean
+    '''
+    # check if the file lists are different
+    if cmp(sorted(map1.keys()), sorted(map2.keys())) != 0:
+        log.debug('diff_mtime_map: the keys are different')
+        return True
+
+    # check if the mtimes are the same
+    if cmp(sorted(map1), sorted(map2)) != 0:
+        log.debug('diff_mtime_map: the maps are different')
+        return True
+
+    # we made it, that means we have no changes
+    log.debug('diff_mtime_map: the maps are the same')
+    return False
+
 def reap_fileserver_cache_dir(cache_base, find_func):
     '''
     Remove unused cache items assuming the cache directory follows a directory convention:

salt/fileserver/gitfs.py
@@ -140,6 +140,9 @@ def update():
     '''
     Execute a git pull on all of the repos
     '''
+    # data for the fileserver event
+    data = {'changed': False,
+            'backend': 'gitfs'}
     pid = os.getpid()
     repos = init()
     for repo in repos:
@@ -147,12 +150,17 @@ def update():
         lk_fn = os.path.join(repo.working_dir, 'update.lk')
         with salt.utils.fopen(lk_fn, 'w+') as fp_:
             fp_.write(str(pid))
-        origin.fetch()
+        for fetch in origin.fetch():
+            if fetch.old_commit is not None:
+                data['changed'] = True
         try:
             os.remove(lk_fn)
         except (OSError, IOError):
             pass
 
+    # if there is a change, fire an event
+    event = salt.utils.event.MasterEvent(__opts__['sock_dir'])
+    event.fire_event(data, 'fileserver')
     salt.fileserver.reap_fileserver_cache_dir(os.path.join(__opts__['cachedir'], 'gitfs/hash'), find_file)
 
 def envs():

commit 0dda5ece20cd2c200f4a23eca3a1326436bc2191

Fix exception caught when mtime_map parent dir does not exist
This fixes an exception that was introduced in 342a6ae. Also, using
os.error does not catch IOError execeptions, so this has been changed as
well.

salt/fileserver/roots.py
@@ -85,10 +85,9 @@ def update():
             os.path.join(__opts__['cachedir'], 'roots/hash'),
             find_file
         )
-    except os.error:
+    except (IOError, OSError):
         # Hash file won't exist if no files have yet been served up
         pass
-
 
     mtime_map_path = os.path.join(__opts__['cachedir'], 'roots/mtime_map')
     # data to send on event
@@ -109,8 +108,10 @@ def update():
     # compare the maps, set changed to the return value
     data['changed'] = salt.fileserver.diff_mtime_map(old_mtime_map, new_mtime_map)
 
-
     # write out the new map
+    mtime_map_path_dir = os.path.dirname(mtime_map_path)
+    if not os.path.exists(mtime_map_path_dir):
+        os.makedirs(mtime_map_path_dir)
     with salt.utils.fopen(mtime_map_path, 'w') as fp_:
         for file_path, mtime in new_mtime_map.iteritems():
             fp_.write('{file_path}:{mtime}\n'.format(file_path=file_path,
@@ -119,6 +120,7 @@ def update():
     # if there is a change, fire an event
     event = salt.utils.event.MasterEvent(__opts__['sock_dir'])
     event.fire_event(data, 'salt.fileserver.roots.update')
+
 
 def file_hash(load, fnd):
     '''