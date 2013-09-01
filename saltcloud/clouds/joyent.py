ae86c24c7a3e8f9c2301e75a5b6442bc426fa94c
Joseph Hall <perlhoser@gmail.com>
2013-09-01 03:27:09 +0000

    Update namespaced tags to be more specific in joyent

--- saltcloud/clouds/joyent.py
+++ saltcloud/clouds/joyent.py
@@ -188,7 +188,7 @@ def create(vm_):
     saltcloud.utils.fire_event(
         'event',
         'starting create',
-        'salt.cloud.create',
+        'salt.cloud.{0}.creating'.format(vm_['name']),
         {
             'name': vm_['name'],
             'profile': vm_['profile'],
@@ -216,7 +216,7 @@ def create(vm_):
     saltcloud.utils.fire_event(
         'event',
         'requesting instance',
-        'salt.cloud.create',
+        'salt.cloud.{0}.requesting'.format(vm_['name']),
         {'kwargs': kwargs},
     )
 
@@ -332,7 +332,7 @@ def create(vm_):
         saltcloud.utils.fire_event(
             'event',
             'executing deploy script',
-            'salt.cloud.create',
+            'salt.cloud.{0}.deploying'.format(vm_['name']),
             {'kwargs': deploy_kwargs},
         )
 
@@ -358,7 +358,7 @@ def create(vm_):
     saltcloud.utils.fire_event(
         'event',
         'created instance',
-        'salt.cloud.create',
+        'salt.cloud.{0}.created'.format(vm_['name']),
         {
             'name': vm_['name'],
             'profile': vm_['profile'],
@@ -413,7 +413,7 @@ def destroy(name, call=None):
     saltcloud.utils.fire_event(
         'event',
         'destroying instance',
-        'salt.cloud.destroy',
+        'salt.cloud.{0}.destroying'.format(name),
         {'name': name},
     )
 
@@ -424,7 +424,7 @@ def destroy(name, call=None):
     saltcloud.utils.fire_event(
         'event',
         'destroyed instance',
-        'salt.cloud.destroy',
+        'salt.cloud.{0}.destroyed'.format(name),
         {'name': name},
     )
 
