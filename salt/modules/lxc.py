fd170781decc9044b7a08c93d5995d2dfcba1f9e
Matthew Williams <mgwilliams@gmail.com>
2013-09-05 06:18:57 +0000

    add lxc.info function

--- salt/modules/lxc.py
+++ salt/modules/lxc.py
@@ -7,6 +7,7 @@ Work with linux containers
 # Import python libs
 import logging
 import tempfile
+import os
 
 #import salt libs
 import salt.utils
@@ -359,3 +360,44 @@ def state(name):
         lines = ret['stdout'].splitlines()
         r = dict([l.split() for l in lines])
         return r['state:'].lower()
+
+
+def info(name):
+    f = '/var/lib/lxc/{0}/config'.format(name)
+    cgroup_dir = '/sys/fs/cgroup/memory/lxc/{0}/'.format(name)
+    if not os.path.isfile(f):
+        return None
+
+    ret = {}
+
+    config = [(v[0].strip(), v[1].strip()) for v in
+              [l.split('#', 1)[0].strip().split('=', 1) for l in
+               open(f).readlines()] if len(v) == 2]
+
+    ifaces = []
+    current = None
+
+    for k, v in config:
+        if k == 'lxc.network.type':
+            current = {'type': v}
+            ifaces.append(current)
+        elif not current:
+            continue
+        elif k.startswith('lxc.network.'):
+            current[k.replace('lxc.network.', '', 1)] = v
+    if ifaces:
+        ret['ifaces'] = ifaces
+
+    ret['rootfs'] = next((i[1] for i in config if i[0] == 'lxc.rootfs'), None)
+    ret['state'] = state(name)
+
+    if ret['state'] == 'running':
+        with open(cgroup_dir + 'memory.limit_in_bytes') as f:
+            limit = int(f.read())
+        with open(cgroup_dir + 'memory.usage_in_bytes') as f:
+            memory = int(f.read())
+        free = limit - memory
+        ret['memory_limit'] = limit
+        ret['memory_free'] = free
+
+    return ret
