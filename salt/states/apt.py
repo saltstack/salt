eb39ec42078994eac9a5d085f35bcb35dea77a64
Henrik Holmboe <henrik@holmboe.se>
2013-09-06 02:16:10 +0200

    Make dict updates more dense

--- salt/states/apt.py
+++ salt/states/apt.py
@@ -14,36 +14,26 @@ def held(name):
     name
         The name of the package, e.g., 'tmux'
     '''
-    ret = {'name': name}
+    ret = {'name': name, 'changes': {}, 'result': False, 'comment': ''}
     state = __salt__['pkg.get_selections'](
         pattern=name,
     )
     if not state:
-        ret.update({'changes': {},
-                    'result': False,
-                    'comment': 'Package {0} does not have a state'.format(
-                        name
-                    )})
+        ret.update(comment='Package {0} does not have a state'.format(name))
         return ret
     if not salt.utils.is_true(state.get('hold', False)):
         if not __opts__['test']:
             result = __salt__['pkg.set_selections'](
                 selection={'hold': [name]}
             )
-            ret.update({'changes': result[name],
-                        'result': True,
-                        'comment': 'Package {0} is now being held'.format(
-                            name
-                        )})
+            ret.update(changes=result[name],
+                       result=True,
+                       comment='Package {0} is now being held'.format(name))
         else:
-            ret.update({'changes': {},
-                        'result': None,
-                        'comment': 'Package {0} is set to be held'.format(
-                            name
-                        )})
+            ret.update(result=None,
+                       comment='Package {0} is set to be held'.format(name))
     else:
-        ret.update({'changes': {},
-                    'result': True,
-                    'comment': 'Package {0} is already held'.format(name)})
+        ret.update(result= True,
+                   comment='Package {0} is already held'.format(name))
 
     return ret
