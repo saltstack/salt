ccc224b40079feb6bdb7477ee6499f77dffc45f4
Robert Keizer <robert@keizer.ca>
2013-09-11 18:07:57 -0500

    Fighting for some reason methods not showing up in module. _create_func being run though.

--- salt/modules/vpc.py
+++ salt/modules/vpc.py
@@ -4,16 +4,19 @@ Amazon VPC Module
 import logging
 import inspect
 import re
+import sys
 import boto
 import boto.vpc
 
 log = logging.getLogger(__name__)
 
+#__func_alias__ = { }
+
 def __virtual__( ):
     return 'vpc'
 
 def debug( ):
-    return "Go away"
+    return "Go away,now"
 
 def _get_connection( ):
     '''
@@ -23,7 +26,10 @@ def _get_connection( ):
 
     Also checks if one already exists.
     '''
-    pass
+
+    # Just a placeholder for now. ( Dummy values to simply trick boto into 
+    # not throwing an exception ).
+    return boto.vpc.VPCConnection( "foo", "bar" )
 
 def _create_func( function_name, function_obj ):
     '''
@@ -33,7 +39,7 @@ def _create_func( function_name, function_obj ):
 
     # Get the documentation from the object.
     doc = inspect.getdoc( function_obj )
-    
+
     # Get the signature of the function.
     spec = inspect.getargspec( function_obj )
 
@@ -45,9 +51,33 @@ def _create_func( function_name, function_obj ):
         # getattr( _get_connection( ), function_name ) is the actual boto obj.
 
         pass
+
     return _f
 
-# Iterate over the boto class.
-for member_name, member_method in inspect.getmembers( boto.vpc.VPCConnection ):
-    
-    log.debug( "I have member name of %s" % member_name )
+# Iterate over the boto object. Note that we're using an instance not a class
+# because inspect.isfunction checks if the function is bound or not.
+for member_name, member_method in inspect.getmembers( _get_connection( ) ):
+
+    # We only want methods.
+    if not inspect.ismethod( member_method ):
+        continue
+
+    # Blacklist __ functions..
+    if re.compile( "^__" ).match( member_name ):
+        continue
+
+    # Call _create_func with the particular member name and function.
+    # Then set it to be class wide. Note that we append of suffix
+    # of '_' to methods that are generated out of _create_func.
+
+    setattr(
+            sys.modules[__name__],
+            '{0}_'.format( member_name ),
+            _create_func( member_name, member_method )
+    )
+
+    # Update func alias so that salt finds the new function.
+    #__func_alias__['{0}_'.format( member_name )] = member_name
+
+def foo( ):
+    return "bar"
