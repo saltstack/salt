8e06b0d35e52a3489bdf3742031ee31d7301b5de
Robert Keizer <robert@keizer.ca>
2013-09-11 16:59:57 -0500

    Stub of _make_connection. Also _create_func stub. Removed some dynamic stuff as we only care about boto.vpc.VPCConnection.

--- salt/modules/vpc.py
+++ salt/modules/vpc.py
@@ -12,32 +12,27 @@ log = logging.getLogger(__name__)
 def __virtual__( ):
     return 'vpc'
 
-# Define a whitelist of functions that we will match while inspecting boto.
-whitelist = { "VPCConnection": [    re.compile("^create"),
-                                    re.compile("^delete"),
-                                    re.compile("^get") ] }
+def debug( ):
+    return "Go away"
 
-for key in whitelist:
-    # Get the actual class object.
-    _class = getattr( boto.vpc, key )
+def _make_connection( ):
+    '''
+    Helper method to handle creation of the actual
+    boto.vpc.VPCConnection object. This uses
+    configuration values from salt.
+    '''
+    pass
 
-    # Iterate over all the members of that class.
-    for member_name, member_method in inspect.getmembers( _class ):
+def _create_func( function_name, function_obj ):
+    '''
+    Create a python function that is directly based on
+    function_obj. Note that introspection is used to do this.
+    '''
+    def _f( ):
+        pass
+    return _f
 
-        # Iterate over all the valid patterns for this class, if we find a match then break out.
-        found = False
-        for _regex in whitelist[key]:
-            if _regex.match( member_name ):
-                found = True
-                break
+# Iterate over the boto class.
+for member_name, member_method in inspect.getmembers( boto.vpc.VPCConnection ):
 
-        # Skip members that don't match the regex.
-        if not found:
-            continue
-
-        log.debug( "I have member name of %s" % member_name )
-
-# VPCConnection iteration..
-
-# Iterate over boto.vpc to figure out what options are available.
-
+    log.debug( "I have member name of %s" % member_name )
