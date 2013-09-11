399118b5be9d756c59591aaf3c1e4d44b83da845
Robert Keizer <robert@keizer.ca>
2013-09-11 17:15:01 -0500

    Renamed _make_connection to _get_connection. Also some more content in _create_func.

--- salt/modules/vpc.py
+++ salt/modules/vpc.py
@@ -15,11 +15,13 @@ def __virtual__( ):
 def debug( ):
     return "Go away"
 
-def _make_connection( ):
+def _get_connection( ):
     '''
     Helper method to handle creation of the actual
     boto.vpc.VPCConnection object. This uses
     configuration values from salt.
+
+    Also checks if one already exists.
     '''
     pass
 
@@ -28,11 +30,24 @@ def _create_func( function_name, function_obj ):
     Create a python function that is directly based on
     function_obj. Note that introspection is used to do this.
     '''
-    def _f( ):
+
+    # Get the documentation from the object.
+    doc = inspect.getdoc( function_obj )
+    
+    # Get the signature of the function.
+    spec = inspect.getargspec( function_obj )
+
+    # Define the actual function we will return.
+    def _f( *args ):
+        # Use spec to reconcile what we get from *args
+        # and call the boto function.
+
+        # getattr( _get_connection( ), function_name ) is the actual boto obj.
+
         pass
     return _f
 
 # Iterate over the boto class.
 for member_name, member_method in inspect.getmembers( boto.vpc.VPCConnection ):
-
+    
     log.debug( "I have member name of %s" % member_name )
