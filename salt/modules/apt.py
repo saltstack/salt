saltstack/salt

commit bd72e44a2ff35105468aae5ccbbbb6fc3d692026

pkg mods: latest_version: (1) always allow refresh arg (2) error on invalid kwargs

salt/modules/apt.py
@@ -124,13 +124,8 @@ def latest_version(*names, **kwargs):
         salt '*' pkg.latest_version <package name> fromrepo=unstable
         salt '*' pkg.latest_version <package1> <package2> <package3> ...
     '''
-    if len(names) == 0:
-        return ''
-    ret = {}
-    # Initialize the dict with empty strings
-    for name in names:
-        ret[name] = ''
-    pkgs = list_pkgs(versions_as_list=True)
+    refresh = salt.utils.is_true(kwargs.pop('refresh', True))
+
     if 'repo' in kwargs:
         # Remember to kill _get_repo() too when removing this warning.
         salt.utils.warn_until(
@@ -139,11 +134,24 @@ def latest_version(*names, **kwargs):
             'removed in 0.18.0. Please use \'fromrepo\' instead.'
         )
     fromrepo = _get_repo(**kwargs)
+    kwargs.pop('fromrepo', None)
+    kwargs.pop('repo', None)
+
+    if kwargs:
+        raise TypeError('Got unexpected keyword argument(s): {0!r}'.format(kwargs))
+
+    if len(names) == 0:
+        return ''
+    ret = {}
+    # Initialize the dict with empty strings
+    for name in names:
+        ret[name] = ''
+    pkgs = list_pkgs(versions_as_list=True)
     repo = ' -o APT::Default-Release="{0}"'.format(fromrepo) \
         if fromrepo else ''
 
     # Refresh before looking for the latest version available
-    if salt.utils.is_true(kwargs.get('refresh', True)):
+    if refresh:
         refresh_db()
 
     for name in names:

salt/modules/brew.py
@@ -120,6 +120,10 @@ def latest_version(*names, **kwargs):
         salt '*' pkg.latest_version <package name>
         salt '*' pkg.latest_version <package1> <package2> <package3>
     '''
+    kwargs.pop('refresh', True)
+    if kwargs:
+        raise TypeError('Got unexpected keyword argument(s): {0!r}'.format(kwargs))
+
     if len(names) <= 1:
         return ''
     else:

salt/modules/ebuild.py
@@ -170,11 +170,15 @@ def latest_version(*names, **kwargs):
         salt '*' pkg.latest_version <package name>
         salt '*' pkg.latest_version <package1> <package2> <package3> ...
     '''
+    refresh = salt.utils.is_true(kwargs.pop('refresh', True))
+    if kwargs:
+        raise TypeError('Got unexpected keyword argument(s): {0!r}'.format(kwargs))
+
     if len(names) == 0:
         return ''
 
     # Refresh before looking for the latest version available
-    if salt.utils.is_true(kwargs.get('refresh', True)):
+    if refresh:
         refresh_db()
 
     ret = {}

salt/modules/freebsdpkg.py
@@ -68,10 +68,14 @@ def latest_version(*names, **kwargs):
         salt '*' pkg.latest_version <package1> <package2> <package3> ...
     '''
 
+    refresh = salt.utils.is_true(kwargs.pop('refresh', True))
+    if kwargs:
+        raise TypeError('Got unexpected keyword argument(s): {0!r}'.format(kwargs))
+
     ret = {}
 
     # Refresh before looking for the latest version available
-    if salt.utils.is_true(kwargs.get('refresh', True)):
+    if refresh:
         refresh_db()
 
     if _check_pkgng():

salt/modules/openbsdpkg.py
@@ -80,6 +80,10 @@ def latest_version(*names, **kwargs):
 
         salt '*' pkg.latest_version <package name>
     '''
+    kwargs.pop('refresh', True)
+    if kwargs:
+        raise TypeError('Got unexpected keyword argument(s): {0!r}'.format(kwargs))
+
     pkgs = list_pkgs()
     ret = {}
     # Initialize the dict with empty strings

salt/modules/pacman.py
@@ -44,11 +44,15 @@ def latest_version(*names, **kwargs):
         salt '*' pkg.latest_version <package name>
         salt '*' pkg.latest_version <package1> <package2> <package3> ...
     '''
+    refresh = salt.utils.is_true(kwargs.pop('refresh', True))
+    if kwargs:
+        raise TypeError('Got unexpected keyword argument(s): {0!r}'.format(kwargs))
+
     if len(names) == 0:
         return ''
 
     # Refresh before looking for the latest version available
-    if salt.utils.is_true(kwargs.get('refresh', True)):
+    if refresh:
         refresh_db()
 
     ret = {}

salt/modules/pkgin.py
@@ -112,13 +112,17 @@ def latest_version(*names, **kwargs):
         salt '*' pkg.latest_version <package1> <package2> ...
     '''
 
+    refresh = salt.utils.is_true(kwargs.pop('refresh', True))
+    if kwargs:
+        raise TypeError('Got unexpected keyword argument(s): {0!r}'.format(kwargs))
+
     pkglist = {}
     pkgin = _check_pkgin()
     if not pkgin:
         return pkglist
 
     # Refresh before looking for the latest version available
-    if salt.utils.is_true(kwargs.get('refresh', True)):
+    if refresh:
         refresh_db()
 
     for name in names:

salt/modules/pkgng.py
@@ -72,6 +72,10 @@ def latest_version(pkg_name, **kwargs):
         salt '*' pkgng.latest_version <package name>
     '''
 
+    kwargs.pop('refresh', True)
+    if kwargs:
+        raise TypeError('Got unexpected keyword argument(s): {0!r}'.format(kwargs))
+
     cmd = 'pkg info {0}'.format(pkg_name)
     out = __salt__['cmd.run'](cmd).split()
     return out[0]

commit ab29cc8cc1a9a1bd1c235f7224bed37d1feb05aa

Fix regression in apt module
This fixes a regression introduced in bd72e44. Resolves #7257.

salt/modules/apt.py
@@ -137,9 +137,6 @@ def latest_version(*names, **kwargs):
     kwargs.pop('fromrepo', None)
     kwargs.pop('repo', None)
 
-    if kwargs:
-        raise TypeError('Got unexpected keyword argument(s): {0!r}'.format(kwargs))
-
     if len(names) == 0:
         return ''
     ret = {}