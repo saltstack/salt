1dbb1f7c3a4e64858fec208213fd96fe1c82b140
Erik Johnson <palehose@gmail.com>
2013-09-15 18:58:53 -0500

    Remove other kwargs checks that would cause tracebacks
    We need to do better arg checking, but this is not the way to go about
    it.

--- salt/modules/pkgin.py
+++ salt/modules/pkgin.py
@@ -113,8 +113,6 @@ def latest_version(*names, **kwargs):
     '''
 
     refresh = salt.utils.is_true(kwargs.pop('refresh', True))
-    if kwargs:
-        raise TypeError('Got unexpected keyword argument(s): {0!r}'.format(kwargs))
 
     pkglist = {}
     pkgin = _check_pkgin()
