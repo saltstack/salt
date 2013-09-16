e18ba0758bd1541ae0f4c38f8d59f58c67b2c44c
Pedro Algarvio <pedro@algarvio.me>
2013-09-16 17:24:00 +0100

    Don't include authorship in generated documentation, the Sphinx way.

--- tests/unit/version_test.py
+++ tests/unit/version_test.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 '''
+    :codeauthor: :email:`Pedro Algarvio (pedro@algarvio.me)`
+    :copyright: © 2013 by the SaltStack Team, see AUTHORS for more details
+    :license: Apache 2.0, see LICENSE for more details.
+
     tests.unit.version_test
     ~~~~~~~~~~~~~~~~~~~~~~~
 
     Test salt's regex git describe version parsing
-
-    :codeauthor: :email:`Pedro Algarvio (pedro@algarvio.me)`
-    :copyright: © 2013 by the SaltStack Team, see AUTHORS for more details.
-    :license: Apache 2.0, see LICENSE for more details.
 '''
 
 # Import python libs
