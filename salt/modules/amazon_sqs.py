e37550c9b52a86c6264a9639804d4dc5d9d2bba9
Scott Walton <s@cott.me.uk>
2013-09-22 10:53:56 +0100

    Added delete and some return values #6444

--- salt/modules/amazon_sqs.py
+++ salt/modules/amazon_sqs.py
@@ -41,22 +41,68 @@ def create_queue(name, region, default_timeout=None, force=False):
 
     create = True
 
+    out = ''
+    err = ''
+    rtn = 0
+
     if queue_exists(name, region):
         # Force or quit
         if force:
             sqs_region = _connect_to_region(region)
+            out = u'Deleting {0} in {1} and recreating it'.format(
+                name, region)
             sqs_region.delete_queue(name)
         else:
+            err = (
+                u'Queue {0} in region {1} exists and force is not set'.format(
+                name, region))
             create = False
+            rtn = 1
 
     if create:
         # Create the queue
         sqs_region = _connect_to_region(region)
         sqs_region.create_queue(name, default_timeout)
+        if not out:
+            out = u'Creating queue {0} in region {1}'.format(
+                name, region)
+    return {
+        'stdout': out,
+        'stderr': err,
+        'retcode': rtn,
+    }
+
+def delete_queue(name, region):
+    '''
+    Deletes a queue in the region.
+
+    name
+        Name of the SQS queue to deletes
+    region
+        Name of the region to delete the queue from
+    '''
+    out = ''
+    err = ''
+    rtn = 0
+
+    if queue_exists(name, region):
+        sqs_region = _connect_to_region(region)
+        out = u'Deleting {0} from {1}'.format(name, region)
+        sqs_region.delete_queue(name)
+    else:
+        err = u'Queue {0} does not exist in {1}'.format(name, region)
+        rtn = 1
+
+    return {
+        'stdout': out,
+        'stderr': err,
+        'retcode': rtn,
+    }
 
 def queue_exists(name, region):
     '''
     Returns True or False on whether the queue exists in the region
+
     name
         Name of the SQS queue to search for
     region
