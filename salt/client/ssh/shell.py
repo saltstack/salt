fb26ab15a38301075fe33f21d87fa1a725c49f42
Thomas S Hatch <thatch45@gmail.com>
2013-09-30 17:33:09 -0600

    Finish fixing #7480

--- salt/client/ssh/shell.py
+++ salt/client/ssh/shell.py
@@ -108,7 +108,7 @@ class Shell(object):
         Return the string to execute ssh-copy-id
         '''
         if self.passwd and salt.utils.which('sshpass'):
-            return 'sshpass -p {0} {1} {2} "{3} -p {4} {5}@{6}"'.format(
+            return 'sshpass -p "{0}" {1} {2} "{3} -p {4} {5}@{6}"'.format(
                     self.passwd,
                     'ssh-copy-id',
                     '-i {0}.pub'.format(self.priv),
