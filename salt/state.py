a504c02902f941b327eb72e6a525363805e9871b
Thomas S Hatch <thatch45@gmail.com>
2013-10-01 12:14:40 -0600

    Add method to fire an event on the master

--- salt/state.py
+++ salt/state.py
@@ -1379,6 +1379,14 @@ class State(object):
             return 'change'
         return 'met'
 
+    def event(self, chunk_ret):
+        '''
+        Fire an event on the master bus
+        '''
+        if not self.opts.get('local') and self.opts.get('state_events', True):
+            tag = salt.utils.event.tagify([self.jid, chunk_ret['__run_num__']])
+            self.functions['event.fire_master'](chunk_ret, tag)
+
     def call_chunk(self, low, running, chunks):
         '''
         Check if a chunk has any requires, execute the requires and then
