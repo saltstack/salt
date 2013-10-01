5d12247e8552de882e01ec2e2b7aa79041f3f9a8
Joseph Hall <perlhoser@gmail.com>
2013-10-01 20:50:45 +0000

    Allow softlayer driver to display custom images

--- saltcloud/clouds/softlayer.py
+++ saltcloud/clouds/softlayer.py
@@ -150,6 +150,31 @@ def avail_images():
     return ret
 
 
+def list_custom_images(call=None):
+    '''
+    Return a dict of all custom VM images on the cloud provider.
+    '''
+    if call != 'function':
+        raise SaltCloudSystemExit(
+            'The list_vlans function must be called with -f or --function.'
+        )
+
+    ret = {}
+    conn = get_conn('SoftLayer_Account')
+    response = conn.getBlockDeviceTemplateGroups()
+    for image in response:
+        if not 'globalIdentifier' in image:
+            continue
+        ret[image['name']] = {
+            'id': image['id'],
+            'name': image['name'],
+            'globalIdentifier': image['globalIdentifier'],
+        }
+        if 'note' in image:
+            ret[image['name']]['note'] = image['note']
+    return ret
+
+
 def get_location(vm_=None):
     '''
     Return the location to use, in this order:
