4daec2e23b6f28f6f3f9aef8b34110520d39765c
Henrik Holmboe <henrik@holmboe.se>
2013-10-05 00:31:03 +0200

    Honor tmplpath in wrapped template renderers
    Before this change you had to directly use `render_*_tmpl()` in order to
    use `tmplpath`. Failing that, by using the renderers wrapped with
    `wrap_tmpl_func()` the `tmplpath` argument was not used.
    
    Fixes saltstack/salt#7626

--- salt/utils/templates.py
+++ salt/utils/templates.py
@@ -55,6 +55,8 @@ def wrap_tmpl_func(render_str):
                 tmplstr = tmplsrc
             else:
                 try:
+                    if tmplpath is not None:
+                        tmplsrc = os.path.join(tmplpath, tmplsrc)
                     with codecs.open(tmplsrc, 'r', SLS_ENCODING) as _tmplsrc:
                         tmplstr = _tmplsrc.read()
                 except (UnicodeDecodeError, ValueError) as exc:
