1c191fb8bf1024324aecdf1c76437c12027689f1
Pedro Algarvio <pedro@algarvio.me>
2013-10-04 09:52:53 +0100

    Support named version strings mathematical comparison.

--- salt/version.py
+++ salt/version.py
@@ -208,6 +208,8 @@ class SaltStackVersion(object):
 
     @classmethod
     def parse(cls, version_string):
+        if version_string.lower() in cls.LNAMES:
+            return cls.from_name(version_string)
         match = cls.git_describe_regex.match(version_string)
         if not match:
             raise ValueError(
