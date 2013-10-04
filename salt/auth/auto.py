351a4e0067d3c15bfb2f537132950a496aaadee5
Thomas S Hatch <thatch45@gmail.com>
2013-10-03 18:21:14 -0600

    Add auto auth system for testing eauth

---
+++ salt/auth/auto.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+'''
+An "Always Approved" eauth interface to test against, not intended for
+production use
+'''
+def auth(username, password):
+    '''
+    Authenticate!
+    '''
+    return True
