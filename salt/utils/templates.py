8a5566c19ed335b24b244fe06ccc16926ed3cee8
Pedro Algarvio <pedro@algarvio.me>
2013-10-08 00:11:44 +0100

    Jinja needs to be "fed" Unicode data. Fixes #7576.
    Additionally, what's returned from `render_jinja_tmpl` is also a Unicode string to that the `SLS_ENCODER` can properly encode them. Encoding UTF-8 data as UTF-8 data using the `SLS_ENCODER` does not, of course, work.

--- salt/utils/templates.py
+++ salt/utils/templates.py
@@ -37,10 +37,17 @@ SLS_ENCODER = codecs.getencoder(SLS_ENCODING)
 
 
 def wrap_tmpl_func(render_str):
-    def render_tmpl(tmplsrc, from_str=False, to_str=False,
-                    context=None, tmplpath=None, **kws):
+
+    def render_tmpl(tmplsrc,
+                    from_str=False,
+                    to_str=False,
+                    context=None,
+                    tmplpath=None,
+                    **kws):
+
         if context is None:
             context = {}
+
         # We want explicit context to overwrite the **kws
         kws.update(context)
         context = kws
@@ -58,8 +65,12 @@ def wrap_tmpl_func(render_str):
                     if salt.utils.is_bin_file(tmplsrc):
                         # Template is a bin file, return the raw file
                         return dict(result=True, data=tmplsrc)
-                    log.error('Exception ocurred while reading file {0}: {1}'
-                              .format(tmplsrc, exc))
+                    log.error(
+                        'Exception occurred while reading file '
+                        '{0}: {1}'.format(tmplsrc, exc),
+                        # Show full traceback if debug logging is enabled
+                        exc_info=log.isEnabledFor(logging.DEBUG)
+                    )
                     raise exc
         else:  # assume tmplsrc is file-like.
             tmplstr = tmplsrc.read()
@@ -94,16 +105,26 @@ def render_jinja_tmpl(tmplstr, context, tmplpath=None):
     loader = None
     newline = False
 
+    if tmplstr and not isinstance(tmplstr, unicode):
+        # http://jinja.pocoo.org/docs/api/#unicode
+        tmplstr = tmplstr.decode(SLS_ENCODING)
+
     if tmplstr.endswith('\n'):
         newline = True
 
     if not env:
         if tmplpath:
             # ie, the template is from a file outside the state tree
+            #
+            # XXX: FileSystemLoader is not being properly instantiated here is
+            # it? At least it ain't according to:
+            #
+            #   http://jinja.pocoo.org/docs/api/#jinja2.FileSystemLoader
             loader = jinja2.FileSystemLoader(
                 context, os.path.dirname(tmplpath))
     else:
         loader = JinjaSaltCacheLoader(opts, context['env'])
+
     env_args = {'extensions': [], 'loader': loader}
 
     if hasattr(jinja2.ext, 'with_'):
@@ -117,8 +138,9 @@ def render_jinja_tmpl(tmplstr, context, tmplpath=None):
     if opts.get('allow_undefined', False):
         jinja_env = jinja2.Environment(**env_args)
     else:
-        jinja_env = jinja2.Environment(
-                        undefined=jinja2.StrictUndefined, **env_args)
+        jinja_env = jinja2.Environment(undefined=jinja2.StrictUndefined,
+                                       **env_args)
+
     jinja_env.filters['strftime'] = salt.utils.date_format
 
     unicode_context = {}
@@ -133,10 +155,6 @@ def render_jinja_tmpl(tmplstr, context, tmplpath=None):
 
     try:
         output = jinja_env.from_string(tmplstr).render(**unicode_context)
-        if isinstance(output, unicode):
-            # Let's encode the output back to utf-8 since that's what's assumed
-            # in salt
-            output = output.encode('utf-8')
     except jinja2.exceptions.TemplateSyntaxError as exc:
         line = traceback.extract_tb(sys.exc_info()[2])[-1][1]
         marker = '    <======================'
