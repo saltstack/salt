26ec39c8b6856f823948ef20bc726a285fe59315
Colton Myers <colton.myers@gmail.com>
2013-10-09 10:46:33 -0600

    Nevermind:  cache in CONFIG_DIR/minion_id

--- salt/config.py
+++ salt/config.py
@@ -683,15 +683,7 @@ def get_id():
     '''
 
     # Check for cached minion ID
-    libdir = os.path.join('/', 'var', 'lib', 'salt')
-    if salt.utils.is_windows():
-        libdir = os.path.join(r'c:\salt', 'var', 'lib')
-    id_cache = os.path.join(libdir, 'minion_id')
-    try:
-        if not os.path.isdir(libdir):
-            os.makedirs(libdir)
-    except Exception as e:
-        log.error('Could not create directory {0}: {1}'.format(libdir, e))
+    id_cache = os.path.join(syspaths.CONFIG_DIR, 'minion_id')
     try:
         with salt.utils.fopen(id_cache) as idf:
             name = idf.read().strip()
