394455805b7dc1b950021c5ebc75398970c2030e
Erik Johnson <palehose@gmail.com>
2013-10-09 14:53:03 -0500

    Fix windows service.restart
    Instead of doing weird sleep/recheck stuff, just use the existing
    stop/start functions.

--- salt/modules/win_service.py
+++ salt/modules/win_service.py
@@ -190,17 +190,8 @@ def restart(name):
 
         salt '*' service.restart <service name>
     '''
-    stopcmd = 'sc stop "{0}"'.format(name)
-    __salt__['cmd.run'](stopcmd)
-    servicestate = status(name)
-    while True:
-        servicestate = status(name)
-        if servicestate == '':
-            break
-        else:
-            time.sleep(2)
-    startcmd = 'sc start "{0}"'.format(name)
-    return not __salt__['cmd.retcode'](startcmd)
+    stop(name)
+    return start(name)
 
 
 def status(name, sig=None):
