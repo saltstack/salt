9d75c7230d1c0af62313fb5ae46cda877d7cf6bb
Henrik Holmboe <henrik@holmboe.se>
2013-10-14 19:06:13 +0200

    Validate MAC using salt.utils.validate.net.mac()
    That was introduced in 01eb5f5.

--- salt/modules/rh_ip.py
+++ salt/modules/rh_ip.py
@@ -17,6 +17,7 @@ import jinja2.exceptions
 # Import salt libs
 import salt.utils
 import salt.utils.templates
+import salt.utils.validate.net
 
 # Set up logging
 log = logging.getLogger(__name__)
@@ -60,7 +61,6 @@ _RH_CONFIG_BONDING_OPTS = [
 _RH_NETWORK_SCRIPT_DIR = '/etc/sysconfig/network-scripts'
 _RH_NETWORK_FILE = '/etc/sysconfig/network'
 _RH_NETWORK_CONF_FILES = '/etc/modprobe.d'
-_MAC_REGEX = re.compile('([0-9A-F]{1,2}:){5}[0-9A-F]{1,2}')
 _CONFIG_TRUE = ['yes', 'on', 'true', '1', True]
 _CONFIG_FALSE = ['no', 'off', 'false', '0', False]
 _IFACE_TYPES = [
@@ -575,7 +575,7 @@ def _parse_settings_eth(opts, iface_type, enabled, iface):
 
     if iface_type not in ['bond', 'vlan', 'bridge']:
         if 'addr' in opts:
-            if _MAC_REGEX.match(opts['addr']):
+            if salt.utils.validate.net.mac(opts['addr']):
                 result['addr'] = opts['addr']
             else:
                 _raise_error_iface(iface, opts['addr'], ['AA:BB:CC:DD:EE:FF'])
