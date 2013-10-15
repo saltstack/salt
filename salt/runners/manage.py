f9970c3e9e3365373dc2800c520817b80f4dbbab
Thomas S Hatch <thatch45@gmail.com>
2013-10-15 16:20:39 -0600

    clean out redundant backslashes

--- salt/runners/manage.py
+++ salt/runners/manage.py
@@ -246,8 +246,8 @@ def bootstrap_psexec(hosts='', master=None, version=None, arch='win32',
         base_url = 'http://saltstack.com/downloads/'
         source = urllib.urlopen(base_url).read()
         salty_rx = re.compile('>(Salt-Minion-(.+?)-(.+)-Setup.exe)</a></td><td align="right">(.*?)\\s*<')
-        source_list = sorted([[path, ver, plat, time.strptime(date, "%d-%b-%Y %H:%M")] \
-                              for path, ver, plat, date in salty_rx.findall(source)], \
+        source_list = sorted([[path, ver, plat, time.strptime(date, "%d-%b-%Y %H:%M")]
+                              for path, ver, plat, date in salty_rx.findall(source)],
                              key=operator.itemgetter(3), reverse=True)
         if version:
             source_list = [s for s in source_list if s[1] == version]
