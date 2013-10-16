022a695988a0adfcad53fb44e4f99c3185dada58
Seth House <seth@eseth.com>
2013-10-16 15:18:07 -0600

    Removed :codeauthor: field lists from logging handler docs
    Fixes #7692
    
    There is a conflict between author field list fields in rST and the
    regular PDF metadata. If there is a workaround for this, I'd love to
    know it but I'm pulling this out for now so the PDF will build. Besides,
    who is Pedro Alonso and why is he claiming ownership of our modules?

--- salt/log/handlers/__init__.py
+++ salt/log/handlers/__init__.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
 '''
-    :codeauthor: :email:`Pedro Algarvio (pedro@algarvio.me)`
-    :copyright: © 2013 by the SaltStack Team, see AUTHORS for more details.
-    :license: Apache 2.0, see LICENSE for more details.
-
-
     salt.log.handlers
     ~~~~~~~~~~~~~~~~~
 
