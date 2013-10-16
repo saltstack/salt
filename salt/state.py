2f71e6fb35afd1b09a640012c96ca375ae8d2e42
Thomas S Hatch <thatch45@gmail.com>
2013-10-16 14:50:28 -0600

    revert fix for #7526 as it is breaking pydsl, this check should
    be added higher up[ the stack...

--- salt/state.py
+++ salt/state.py
@@ -1932,8 +1932,6 @@ class BaseHighState(object):
         '''
         err = ''
         errors = []
-        if sls in mods:
-            return {}, errors
         state_data = self.client.get_state(sls, env)
         fn_ = state_data.get('dest', False)
         if not fn_:
