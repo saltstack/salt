6ee8b9db03ce9ade97fdc12421fd663879ee4a4f
C. R. Oldham <cr@saltstack.com>
2013-10-17 17:56:13 -0600

    Fix state compiler stack trace, github issue #5767

--- salt/state.py
+++ salt/state.py
@@ -1325,7 +1325,7 @@ class State(object):
         if pre:
             reqs['prerequired'] = []
         for r_state in reqs:
-            if r_state in low:
+            if r_state in low and low[r_state] is not None:
                 for req in low[r_state]:
                     req = trim_req(req)
                     found = False
