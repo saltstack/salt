859804b689510dd03aadaee7ace6b8013b5bfe78
Pedro Algarvio <pedro@algarvio.me>
2013-10-18 10:39:06 +0100

    Don't change `pidfile` ownership if the current user already owns it.

--- salt/utils/process.py
+++ salt/utils/process.py
@@ -44,6 +44,11 @@ def set_pidfile(pidfile, user):
             )
         )
         sys.exit(2)
+
+    if os.getuid() == uid:
+        # The current user already owns the pidfile. Return!
+        return
+
     try:
         os.chown(pidfile, uid, gid)
     except OSError as err:
