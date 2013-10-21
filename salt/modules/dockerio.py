498d9013b719eef85782f34425fce1afaaf9cd14
Erik Johnson <palehose@gmail.com>
2013-10-21 10:24:29 -0500

    Add versionadded directive to dockerio module
    Also changed the top line of the docstring, as this affects its
    appearance in the docs, and this top line should be a simple explanation
    of the module. Having "overview of this module" in this line is
    redundant.

--- salt/modules/dockerio.py
+++ salt/modules/dockerio.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
-Management of dockers: overview of this module
-==============================================
+Management of dockers
+=====================
+
+.. versionadded:: Hydrogen
 
 .. note::
 
