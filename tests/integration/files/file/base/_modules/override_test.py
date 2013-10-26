8108d52c52507587db461a7451518b5405942a5a
Pedro Algarvio <pedro@algarvio.me>
2013-10-26 18:59:44 +0100

    Remove unnecessary imports.

--- tests/integration/files/file/base/_modules/override_test.py
+++ tests/integration/files/file/base/_modules/override_test.py
@@ -3,18 +3,6 @@
 Module for running arbitrary tests
 '''
 
-# Import Python libs
-import os
-import sys
-import time
-import random
-
-# Import Salt libs
-import salt
-import salt.version
-import salt.loader
-
-
 __virtualname__ = 'test'
 
 def __virtual__():
