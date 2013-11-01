ecbe75593c301f9ccd44a1b395bf71be29160045
Mike Place <mp@saltstack.com>
2013-11-01 11:57:52 -0600

    Don't depend on string variable when logging a debug message. (This may be an indicator of a bug elsewhere.)

--- salt/fileserver/__init__.py
+++ salt/fileserver/__init__.py
@@ -135,7 +135,7 @@ class Fileserver(object):
         for fsb in back:
             fstr = '{0}.update'.format(fsb)
             if fstr in self.servers:
-                log.debug('Updating fileserver cache for the {0} backend'.format(fsb))
+                log.debug('Updating fileserver cache')
                 self.servers[fstr]()
 
     def envs(self, back=None, sources=False):
