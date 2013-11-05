8b90d8c758709555398b1d836d91f62f28f50d21
Scott Walton <s@cott.me.uk>
2013-11-05 09:43:16 +0000

    Changed the amazon_sqs module to use the awscli module #6444

--- salt/modules/amazon_sqs.py
+++ salt/modules/amazon_sqs.py
@@ -5,44 +5,90 @@ boto config must be in /etc/boto.cfg with your AWS credentials.
 '''
 
 # Import salt libs
-from salt.exceptions import CommandExecutionError, CommandNotFoundError
+from salt import utils
+from salt.exceptions import CommandExecutionError
 
-def _check_boto():
+_OUTPUT = '-output json'
+
+
+def _check_aws():
     '''
     Make sure boto is installed
     '''
-    try:
-        import boto
-    except ImportError:
-        raise CommandNotFoundError
+    utils.check_or_die('aws')
     return 'amazon_sqs'
 
-def _connect_to_region(region):
-    '''
-    Connect to the given region.
-    '''
-    from boto.sqs import connect_to_region
-    sqs_region = connect_to_region(region)
-    
-    if sqs_region is None:
-        _msg = u'Could not connect to region {0}'.format(region)
-        raise CommandExecutionError(_msg)
 
-    return sqs_region
+def _region(region):
+    '''
+    Return the region argument.
+    '''
+    return ' --region {r}'.format(r=region)
 
-def create_queue(name, region, default_timeout=None):
+
+def _run_aws(cmd, region, opts, user, **kwargs):
+    '''
+    Runs the given command against AWS.
+    cmd
+        Command to run
+    region
+        Region to execute cmd in
+    opts
+        Pass in from salt
+    user
+        Pass in from salt
+    kwargs
+        Key-value arguments to pass to the command
+    '''
+    _formatted_args = [
+        '--{0} {1}'.format(k, v) for k, v in kwargs.iteritems()]
+
+    cmd = 'aws sqs {cmd} {args} {region}'.format(
+        cmd=cmd,
+        args=' '.join(_formatted_args),
+        region=_region(region))
+
+    rtn = __salt__['cmd.run'](cmd, runas=user)
+
+
+def list_queues(region, opts=None, user=None):
+    '''
+    List the queues in the selected region.
+
+    region
+        Region to list SQS queues for
+
+    opts : None
+        Any additional options to add to the command line
+
+    user : None
+        Run hg as a user other than what the minion runs as
+    '''
+    return _run_aws('list-queues', region, opts, user)
+
+
+def create_queue(name, region, opts=None, user=None):
     '''
     Creates a queue with the correct name.
     
     name
         Name of the SQS queue to create
+
     region
         Region to create the SQS queue in
-    default_timeout
-        The default message timeout to use
-    '''
-    _check_boto()
 
+    opts : None
+        Any additional options to add to the command line
+
+    user : None
+        Run hg as a user other than what the minion runs as
+    '''
+    _check_aws()
+
+    return _run_aws(
+        'create-queue', name=name, region=region, opts=opts,
+        user=user)
+    
     create = True
 
     out = ''
@@ -69,7 +115,8 @@ def create_queue(name, region, default_timeout=None):
         'retcode': rtn,
     }
 
-def delete_queue(name, region):
+
+def delete_queue(name, region, opts=None, user=None):
     '''
     Deletes a queue in the region.
 
@@ -77,6 +124,12 @@ def delete_queue(name, region):
         Name of the SQS queue to deletes
     region
         Name of the region to delete the queue from
+
+    opts : None
+        Any additional options to add to the command line
+
+    user : None
+        Run hg as a user other than what the minion runs as
     '''
     out = ''
     err = ''
@@ -96,14 +149,21 @@ def delete_queue(name, region):
         'retcode': rtn,
     }
 
-def queue_exists(name, region):
+def queue_exists(name, region, opts=None, user=None):
     '''
     Returns True or False on whether the queue exists in the region
 
     name
         Name of the SQS queue to search for
+
     region
         Name of the region to search for the queue in
+
+    opts : None
+        Any additional options to add to the command line
+
+    user : None
+        Run hg as a user other than what the minion runs as
     '''
     sqs_region = _connect_to_region(region)
     return sqs_region.get_queue(name) != None
