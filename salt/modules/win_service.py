da401aebd2479b51e844a9f45a116ad4c102b657
David Boucha <boucha@gmail.com>
2013-11-12 14:10:17 -0700

    properly quote Windows sc commands

--- salt/modules/win_service.py
+++ salt/modules/win_service.py
@@ -40,7 +40,7 @@ def get_enabled():
                 continue
             services.append(comps[1].strip())
     for service in services:
-        cmd2 = 'sc qc "{0} {1}"'.format(service, BUFFSIZE)
+        cmd2 = 'sc qc "{0}" {1}'.format(service, BUFFSIZE)
         lines = __salt__['cmd.run'](cmd2).splitlines()
         for line in lines:
             if 'AUTO_START' in line:
@@ -69,7 +69,7 @@ def get_disabled():
                 continue
             services.append(comps[1].strip())
     for service in services:
-        cmd2 = 'sc qc "{0} {1}"'.format(service, BUFFSIZE)
+        cmd2 = 'sc qc "{0}" {1}'.format(service, BUFFSIZE)
         lines = __salt__['cmd.run'](cmd2).splitlines()
         for line in lines:
             if 'DEMAND_START' in line:
@@ -217,7 +217,7 @@ def status(name, sig=None):
 
         salt '*' service.status <service name> [service signature]
     '''
-    cmd = 'sc query "{0} {1}"'.format(name, BUFFSIZE)
+    cmd = 'sc query "{0}" {1}'.format(name, BUFFSIZE)
     statuses = __salt__['cmd.run'](cmd).splitlines()
     for line in statuses:
         if 'RUNNING' in line:
