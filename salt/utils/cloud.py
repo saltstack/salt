99cde40cefd2eb21b7ef92cbe31f435e877c9ebd
Pedro Algarvio <pedro@algarvio.me>
2013-11-20 11:47:25 +0000

    A single `namespaced_function` is enough.

--- salt/utils/cloud.py
+++ salt/utils/cloud.py
@@ -1098,23 +1098,6 @@ def check_name(name, safe_chars):
         )
 
 
-def namespaced_function(function, global_dict, defaults=None):
-    '''
-    Redefine(clone) a function under a different globals() namespace scope
-    '''
-    if defaults is None:
-        defaults = function.__defaults__
-
-    new_namespaced_function = types.FunctionType(
-        function.__code__,
-        global_dict,
-        name=function.__name__,
-        argdefs=defaults
-    )
-    new_namespaced_function.__dict__.update(function.__dict__)
-    return new_namespaced_function
-
-
 def remove_sshkey(host, known_hosts=None):
     '''
     Remove a host from the known_hosts file
