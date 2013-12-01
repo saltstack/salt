7190d94bd7b1a0a3a1988e524888feac72678365
Joe Healy <joehealy@gmail.com>
2013-12-01 18:29:16 +1100

    Doc fixes to remove errors and warnings

--- salt/cloud/clouds/ec2.py
+++ salt/cloud/clouds/ec2.py
@@ -757,7 +757,9 @@ def list_availability_zones():
 
 def block_device_mappings(vm_):
     '''
-    Return the block device mapping::
+    Return the block device mapping:
+
+    ::
 
         [{'DeviceName': '/dev/sdb', 'VirtualName': 'ephemeral0'},
           {'DeviceName': '/dev/sdc', 'VirtualName': 'ephemeral1'}]
