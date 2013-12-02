fda42c2f0fb41d774a4e58e26520cbc711a9c709
C. R. Oldham <cr@saltstack.com>
2013-12-02 12:50:18 -0700

    Investigating using pillar

---
+++ salt/pillar/test_pillar.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+'''
+Dumb test pillar module
+'''
+
+# Import python libs
+import logging
+import re
+    
+def __virtual__():
+    return 'test_pillar'
+
+# Set up logging
+log = logging.getLogger(__name__)
+
+def ext_pillar(minion_id,
+               pillar,
+               collection='pillar',
+               id_field='_id',
+               re_pattern=None,
+               re_replace='',
+               fields=None):
+
+     my_pillar = { 'junos_hosts': ['junos', 'junos2']}
+     return my_pillar
