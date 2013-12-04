28f0e7e6f70859fa41c787d52e1e923ca024258c
Mike Place <mp@saltstack.com>
2013-12-03 22:47:57 -0700

    Refactor to no longer shadow built-ins of min and max

--- salt/states/disk.py
+++ salt/states/disk.py
@@ -10,7 +10,7 @@ __monitor__ = [
         ]
 
 
-def status(name, max=None, min=None):
+def status(name, maximum=None, minimum=None):
     '''
     Return the current disk usage stats for the named device
     '''
@@ -26,34 +26,34 @@ def status(name, max=None, min=None):
         ret['result'] = False
         ret['comment'] += 'Named disk mount not present '
         return ret
-    if max:
+    if maximum:
         try:
-            if isinstance(max, basestring):
-                max = int(max.strip('%'))
+            if isinstance(maximum, basestring):
+                maximum = int(maximum.strip('%'))
         except Exception:
             ret['comment'] += 'Max argument must be an integer '
-    if min:
+    if minimum:
         try:
-            if isinstance(min, basestring):
-                min = int(min.strip('%'))
+            if isinstance(minimum, basestring):
+                minimum = int(minimum.strip('%'))
         except Exception:
             ret['comment'] += 'Min argument must be an integer '
-    if min and max:
-        if min >= max:
+    if minimum and maximum:
+        if minimum >= maximum:
             ret['comment'] += 'Min must be less than max'
     if ret['comment']:
         return ret
     cap = int(data[name]['capacity'].strip('%'))
     ret['data'] = data[name]
-    if min:
-        if cap < min:
+    if minimum:
+        if cap < minimum:
             ret['comment'] = 'Disk is below minimum of {0} at {1}'.format(
-                    min, cap)
+                    minimum, cap)
             return ret
-    if max:
-        if cap > max:
+    if maximum:
+        if cap > maximum:
             ret['comment'] = 'Disk is below maximum of {0} at {1}'.format(
-                    max, cap)
+                    maximum, cap)
             return ret
     ret['comment'] = 'Disk in acceptable range'
     ret['result'] = True
