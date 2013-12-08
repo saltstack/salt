6c0b53547db79a62e0a59ebb681306f3d1a9768f
regilero <regis.leroy@makina-corpus.com>
2013-12-08 17:10:37 +0100

    MySQl: fix states integration tests

--- tests/integration/states/mysql.py
+++ tests/integration/states/mysql.py
@@ -76,84 +76,97 @@ class MysqlDatabaseStateTest(integration.ModuleCase,
         else:
             self.skipTest('No MySQL Server running, or no root access on it.')
 
+
+    def _test_database(self, db_name, second_db_name, test_conn, **kwargs):
+        '''
+        Create db two times, test conn, remove it two times
+        '''
+        # In case of...
+        ret = self.run_state('mysql_database.absent',
+                             name=db_name,
+                             **kwargs
+        )
+        ret = self.run_state('mysql_database.present',
+                             name=db_name,
+                             **kwargs
+        )
+        self.assertSaltTrueReturn(ret)
+        self.assertInSaltComment(
+            'The database ' + db_name + ' has been created',
+            ret
+        )
+        #2nd run
+        ret = self.run_state('mysql_database.present',
+                             name=second_db_name,
+                             **kwargs
+        )
+        self.assertSaltTrueReturn(ret)
+        self.assertInSaltComment(
+            'Database ' + db_name + ' is already present',
+            ret
+        )
+        if test_conn:
+            # test root connection
+            ret = self.run_function(
+                'mysql.query',
+                database=db_name, 
+                query='SELECT 1',
+                **kwargs
+            )
+            if not isinstance(ret, dict) or not 'results' in ret:
+                raise AssertionError(
+                    ('Unexpected result while testing connection'
+                    ' on db {0!r}: {1}').format(
+                        db_name,
+                        repr(ret)
+                    )
+                )
+            self.assertEqual([['1']], ret['results'])
+
+        # Now removing databases
+        kwargs.pop('character_set')
+        kwargs.pop('collate')
+        ret = self.run_state('mysql_database.absent',
+                             name=db_name,
+                             **kwargs
+        )
+        self.assertSaltTrueReturn(ret)
+        self.assertInSaltComment(
+            'Database ' + db_name + ' has been removed',
+            ret
+        )
+        #2nd run
+        ret = self.run_state('mysql_database.absent',
+                             name=second_db_name,
+                            ** kwargs
+        )
+        self.assertSaltTrueReturn(ret)
+        self.assertInSaltComment(
+            'Database ' + db_name + ' is not present, so it cannot be removed',
+            ret
+        )
+        self.assertSaltStateChangesEqual(ret, {})
+
+
     @destructiveTest
     def test_present_absent(self):
         '''
         mysql_database.present
         '''
-        # In case of...
-        ret = self.run_state('mysql_database.absent',
-                             name='testdb1',
-                             character_set='utf8',
-                             collate='utf8_general_ci',
-                             connection_user=self.user,
-                             connection_pass=self.password
-        )
-        ret = self.run_state('mysql_database.present',
-                             name='testdb1',
-                             character_set='utf8',
-                             collate='utf8_general_ci',
-                             connection_user=self.user,
-                             connection_pass=self.password
-        )
-        self.assertSaltTrueReturn(ret)
-        self.assertInSaltComment('The database testdb1 has been created', ret)
-        #2nd run
-        ret = self.run_state('mysql_database.present',
-                             name='testdb1',
-                             character_set='utf8',
-                             collate='utf8_general_ci',
-                             connection_user=self.user,
-                             connection_pass=self.password
-        )
-        self.assertSaltTrueReturn(ret)
-        self.assertInSaltComment('Database testdb1 is already present', ret)
-        self.assertSaltStateChangesEqual(ret, {})
-
-        # Test root connection on db
-        ret = self.run_function(
-            'mysql.query',
-            database='testdb1',
-            query='SELECT 1',
+        self._test_database(
+            'testdb1',
+            'testdb1',
+            test_conn=True,
+            character_set='utf8',
+            collate='utf8_general_ci',
             connection_user=self.user,
             connection_pass=self.password,
-            connection_host='localhost'
+            connection_use_unicode=True,
+            connection_charset='utf8'
         )
-        if not isinstance(ret, dict) or not 'results' in ret:
-            raise AssertionError(
-                ('Unexpected result while testing connection'
-                ' on db {0!r}: {1}').format(
-                    'testdb1',
-                    repr(ret)
-                )
-            )
-        self.assertEqual([['1']], ret['results'])
 
     # TODO: test with variations on collate and charset, check for db alter
     # once it will be done in mysql_database.present state
-
-        ret = self.run_state('mysql_database.absent',
-                             name='testdb1',
-                             connection_user=self.user,
-                             connection_pass=self.password
-        )
-        self.assertSaltTrueReturn(ret)
-        self.assertInSaltComment(
-            'Database testdb1 has been removed',
-            ret
-        )
-        #2nd run
-        ret = self.run_state('mysql_database.absent',
-                             name='testdb1',
-                             connection_user=self.user,
-                             connection_pass=self.password
-        )
-        self.assertSaltTrueReturn(ret)
-        self.assertInSaltComment(
-            'Database testdb1 is not present, so it cannot be removed',
-            ret
-        )
-        self.assertSaltStateChangesEqual(ret, {})
 
 
     @destructiveTest
@@ -161,110 +174,39 @@ class MysqlDatabaseStateTest(integration.ModuleCase,
         '''
         mysql_database.present with utf-8 andf fuzzy db name
         '''
+        # this is : ":() ;,?@=`&'\
+        dbname_fuzzy = '":() ;,?@=`&/\'\\'
         # \xe6\xa8\x99\ = \u6a19 = 標
-        # this is : ":;,?:@=`&/標'\
-        dbname_utf8 = '":;,?:@=`&/\xe6\xa8\x99\'\\'
-        dbname_unicode = u'":;,?:@=`&/\u6a19\'\\'
-        #dbname_utf8 = '":;,?:@=`&/\u6a19\'\\'
-        #dbname_unicode = '":;,?:@=`&/\u6a19\'\\'
-        # In case of...
-        ret = self.run_state('mysql_database.absent',
-                             name=dbname_utf8,
-                             character_set='utf8',
-                             collate='utf8_general_ci',
-                             connection_user=self.user,
-                             connection_pass=self.password,
-                             connection_use_unicode=True,
-                             connection_charset='utf8',
-                             saltenv={"LC_ALL": "en_US.utf8"}
-        )
-        ret = self.run_state('mysql_database.present',
-                             name=dbname_utf8,
-                             character_set='utf8',
-                             collate='utf8_general_ci',
-                             connection_user=self.user,
-                             connection_pass=self.password,
-                             connection_use_unicode=True,
-                             connection_charset='utf8',
-                             saltenv={"LC_ALL": "en_US.utf8"}
-        )
-        self.assertSaltTrueReturn(ret)
-        # 'The database ":;,?: has been created' not found in 'Database ":;,?: is already present'
-        self.assertInSaltComment(
-            'The database ' + dbname_utf8 + ' has been created',
-            ret
-        )
-        #2nd run
-        ret = self.run_state('mysql_database.present',
-                             name=dbname_unicode,
-                             character_set='utf8',
-                             collate='utf8_general_ci',
-                             connection_user=self.user,
-                             connection_pass=self.password,
-                             connection_use_unicode=True,
-                             connection_charset='utf8',
-                             saltenv={"LC_ALL": "en_US.utf8"}
-        )
-        self.assertSaltTrueReturn(ret)
-        self.assertInSaltComment(
-            'Database ' + dbname_utf8 + ' is already present',
-            ret
-        )
-        self.assertSaltStateChangesEqual(ret, {})
+        # this is : "();,?:@=`&/標'\
+        dbname_utf8 = '"();,?@=`&//\xe6\xa8\x99\'\\'
+        dbname_unicode = u'"();,?@=`&//\u6a19\'\\'
 
-        # Test root connection on db
-        ret = self.run_function(
-            'mysql.query',
-            database=dbname_utf8,
-            query='SELECT 1',
+        self._test_database(
+            dbname_fuzzy,
+            dbname_fuzzy,
+            test_conn=True,
+            character_set='utf8',
+            collate='utf8_general_ci',
             connection_user=self.user,
             connection_pass=self.password,
-            connection_host='localhost',
+            connection_use_unicode=True,
+            connection_charset='utf8'
+        )
+
+        # FIXME: MySQLdb bugs on dbnames with utf-8?
+        self._test_database(
+            dbname_utf8,
+            dbname_unicode,
+            test_conn=False,
+            character_set='utf8',
+            collate='utf8_general_ci',
+            connection_user=self.user,
+            connection_pass=self.password,
             connection_use_unicode=True,
             connection_charset='utf8',
+            #saltenv={"LC_ALL": "en_US.utf8"}
         )
-        if not isinstance(ret, dict) or not 'results' in ret:
-            raise AssertionError(
-                ('Unexpected result while testing connection'
-                ' on db {0!r}: {1}').format(
-                    dbname_utf8,
-                    repr(ret)
-                )
-            )
-        self.assertEqual([['1']], ret['results'])
-        
-        ret = self.run_state('mysql_database.absent',
-                             name=dbname_utf8,
-                             character_set='utf8',
-                             collate='utf8_general_ci',
-                             connection_user=self.user,
-                             connection_pass=self.password,
-                             connection_use_unicode=True,
-                             connection_charset='utf8',
-                             saltenv={"LC_ALL": "en_US.utf8"}
-        )
-        self.assertSaltTrueReturn(ret)
-        self.assertInSaltComment(
-            'Database ' + dbname_utf8 + ' has been removed',
-            ret
-        )
-        #2nd run
-        ret = self.run_state('mysql_database.absent',
-                             name=dbname_unicode,
-                             character_set='utf8',
-                             collate='utf8_general_ci',
-                             connection_user=self.user,
-                             connection_pass=self.password,
-                             connection_use_unicode=True,
-                             connection_charset='utf8',
-                             saltenv={"LC_ALL": "en_US.utf8"}
-        )
-        self.assertSaltTrueReturn(ret)
-        self.assertInSaltComment(
-            'Database ' + dbname_utf8 + ' is not present, so it cannot be removed',
-            ret
-        )
-        self.assertSaltStateChangesEqual(ret, {})
+
 
     @destructiveTest
     @skipIf(True, 'This tests needs issue #8947 to be fixed first')
