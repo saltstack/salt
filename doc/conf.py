35a800eef9012081305edc00c39a11b07924dbb2
Colton Myers <colton.myers@gmail.com>
2013-12-09 14:47:25 -0700

    Add 0.17.4 release notes

--- doc/conf.py
+++ doc/conf.py
@@ -103,7 +103,7 @@ copyright = '2013 SaltStack, Inc.'
 
 version = salt.version.__version__
 #release = '.'.join(map(str, salt.version.__version_info__))
-release = '0.17.2'
+release = '0.17.4'
 
 language = 'en'
 locale_dirs = [
