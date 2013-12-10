82c1cb8e6431bd2ad93e7d375ed1d989c1cfc94f
regilero <regis.leroy@makina-corpus.com>
2013-12-11 00:54:05 +0100

    MySQL states, grants integration tests

--- tests/integration/states/mysql.py
+++ tests/integration/states/mysql.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 '''
-Tests for the MySQL state
+Tests for the MySQL states
 '''
 
 # Import python libs
@@ -19,6 +19,7 @@ ensure_in_syspath('../../')
 # Import salt libs
 import integration
 import salt.utils
+from salt.modules import mysql as mysqlmod
 
 log = logging.getLogger(__name__)
 
@@ -200,7 +201,6 @@ class MysqlDatabaseStateTest(integration.ModuleCase,
             collate='utf8_general_ci',
             connection_user=self.user,
             connection_pass=self.password,
-            connection_use_unicode=True,
             connection_charset='utf8',
             #saltenv={"LC_ALL": "en_US.utf8"}
         )
@@ -239,6 +239,350 @@ class MysqlDatabaseStateTest(integration.ModuleCase,
             }
         self.assertEqual(expected_result, result)
 
+
+@skipIf(
+    NO_MYSQL,
+    'Please install MySQL bindings and a MySQL Server before running'
+    'MySQL integration tests.'
+)
+class MysqlGrantsStateTest(integration.ModuleCase,
+                           integration.SaltReturnAssertsMixIn):
+    '''
+    Validate the mysql_grants states
+    '''
+
+    user = 'root'
+    password = 'poney'
+    # yep, theses are valid MySQL db names
+    # very special chars are _ % and .
+    testdb1 = 'tes.t\'"saltdb'
+    testdb2 = 't_st `(:=salt%b)'
+    testdb3 = 'test `(:=salteeb)'
+    table1 = 'foo'
+    table2 = "foo `\'%_bar"
+    users = {
+        'user1': {
+            'name': 'foo',
+            'pwd': 'bar',
+        },
+        'user2': {
+            'name': 'user ";--,?:&/\\',
+            'pwd': '";--(),?:@=&/\\',
+        },
+        # this is : passwd 標標
+        'user3': {
+            'name': 'user( @ )=foobar',
+            'pwd': '\xe6\xa8\x99\xe6\xa8\x99',
+        },
+        # this is : user/password containing 標標
+        'user4': {
+            'name': 'user \xe6\xa8\x99',
+            'pwd': '\xe6\xa8\x99\xe6\xa8\x99',
+        },
+    }
+
+    @destructiveTest
+    def setUp(self):
+        '''
+        Test presence of MySQL server, enforce a root password
+        '''
+        super(MysqlGrantsStateTest, self).setUp()
+        NO_MYSQL_SERVER = True
+        # now ensure we know the mysql root password
+        # one of theses two at least should work
+        ret1 = self.run_state(
+            'cmd.run',
+             name='mysqladmin -u '
+               + self.user
+               + ' flush-privileges password "'
+               + self.password
+               + '"'
+        )
+        ret2 = self.run_state(
+            'cmd.run',
+             name='mysqladmin -u '
+               + self.user
+               + ' --password="'
+               + self.password
+               + '" flush-privileges password "'
+               + self.password
+               + '"'
+        )
+        key, value = ret2.popitem()
+        if value['result']:
+            NO_MYSQL_SERVER = False
+        else:
+            self.skipTest('No MySQL Server running, or no root access on it.')
+        # Create some users and a test db
+        for user,userdef in self.users.iteritems():
+            self._userCreation(uname=userdef['name'], password=userdef['pwd'])
+        self.run_state(
+            'mysql_database.present',
+            name=self.testdb1,
+            character_set='utf8',
+            collate='utf8_general_ci',
+            connection_user=self.user,
+            connection_pass=self.password,
+        )
+        self.run_state(
+            'mysql_database.present',
+            name=self.testdb2,
+            character_set='utf8',
+            collate='utf8_general_ci',
+            connection_user=self.user,
+            connection_pass=self.password,
+        )
+        create_query = ('CREATE TABLE %(tblname)s ('
+            ' id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,'
+            ' data VARCHAR(100)) ENGINE=%(engine)s;') % dict(
+            tblname=mysqlmod.quote_identifier(self.table1),
+            engine='MYISAM',
+        )
+        log.info('Adding table {0!r}'.format(self.table1,))
+        self.run_function(
+            'mysql.query',
+            database=self.testdb2,
+            query=create_query,
+            connection_user=self.user,
+            connection_pass=self.password
+        )
+        create_query = ('CREATE TABLE %(tblname)s ('
+            ' id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,'
+            ' data VARCHAR(100)) ENGINE=%(engine)s;') % dict(
+            tblname=mysqlmod.quote_identifier(self.table2),
+            engine='MYISAM',
+        )
+        log.info('Adding table {0!r}'.format(self.table2,))
+        self.run_function(
+            'mysql.query',
+            database=self.testdb2,
+            query=create_query,
+            connection_user=self.user,
+            connection_pass=self.password
+        )
+
+
+    @destructiveTest
+    def tearDown(self):
+        '''
+        Removes created users and db
+        '''
+        for user,userdef in self.users.iteritems():
+            self._userRemoval(uname=userdef['name'] ,password=userdef['pwd'])
+        self.run_state(
+            'mysql_database.absent',
+            name=self.testdb1,
+            connection_user=self.user,
+            connection_pass=self.password,
+       )
+        self.run_function(
+            'mysql_database.absent',
+            name=self.testdb2,
+            connection_user=self.user,
+            connection_pass=self.password,
+       )
+
+
+    def _userCreation(self,
+                      uname,
+                      password=None):
+        '''
+        Create a test user
+        '''
+        self.run_state(
+            'mysql_user.present',
+            name=uname,
+            host='localhost',
+            password=password,
+            connection_user=self.user,
+            connection_pass=self.password,
+            connection_charset='utf8',
+            saltenv={"LC_ALL": "en_US.utf8"}
+        )
+
+
+    def _userRemoval(self,
+                     uname,
+                     password=None):
+        '''
+        Removes a test user
+        '''
+        self.run_state(
+            'mysql_user.absent',
+            name=uname,
+            host='localhost',
+            connection_user=self.user,
+            connection_pass=self.password,
+            connection_charset='utf8',
+            saltenv={"LC_ALL": "en_US.utf8"}
+        )
+
+    def _test_grant(self,
+                    user,
+                    host,
+                    grant,
+                    target,
+                    **kwargs):
+        '''
+        Create db two times, test conn, remove it two times
+        '''
+        # In case of...
+        ret = self.run_state('mysql_database.absent',
+                             name=db_name,
+                             **kwargs
+        )
+        ret = self.run_state('mysql_database.present',
+                             name=db_name,
+                             **kwargs
+        )
+        self.assertSaltTrueReturn(ret)
+        self.assertInSaltComment(
+            'The database ' + db_name + ' has been created',
+            ret
+        )
+        #2nd run
+        ret = self.run_state('mysql_database.present',
+                             name=second_db_name,
+                             **kwargs
+        )
+        self.assertSaltTrueReturn(ret)
+        self.assertInSaltComment(
+            'Database ' + db_name + ' is already present',
+            ret
+        )
+        if test_conn:
+            # test root connection
+            ret = self.run_function(
+                'mysql.query',
+                database=db_name, 
+                query='SELECT 1',
+                **kwargs
+            )
+            if not isinstance(ret, dict) or not 'results' in ret:
+                raise AssertionError(
+                    ('Unexpected result while testing connection'
+                    ' on db {0!r}: {1}').format(
+                        db_name,
+                        repr(ret)
+                    )
+                )
+            self.assertEqual([['1']], ret['results'])
+
+        # Now removing databases
+        kwargs.pop('character_set')
+        kwargs.pop('collate')
+        ret = self.run_state('mysql_database.absent',
+                             name=db_name,
+                             **kwargs
+        )
+        self.assertSaltTrueReturn(ret)
+        self.assertInSaltComment(
+            'Database ' + db_name + ' has been removed',
+            ret
+        )
+        #2nd run
+        ret = self.run_state('mysql_database.absent',
+                             name=second_db_name,
+                            ** kwargs
+        )
+        self.assertSaltTrueReturn(ret)
+        self.assertInSaltComment(
+            'Database ' + db_name + ' is not present, so it cannot be removed',
+            ret
+        )
+        self.assertSaltStateChangesEqual(ret, {})
+
+    @destructiveTest
+    def test_grant_present_absent(self):
+        '''
+        mysql_database.present
+        '''
+        ret = self.run_state(
+            'mysql_grants.present',
+            name='grant test 1',
+            grant='SELECT, INSERT',
+            database=self.testdb1 + '.*',
+            user=self.users['user1']['name'],
+            host='localhost',
+            grant_option=True,
+            revoke_first=True,
+            connection_user=self.user,
+            connection_pass=self.password,
+            connection_charset='utf8'
+        )
+        self.assertSaltTrueReturn(ret)
+        ret = self.run_state(
+            'mysql_grants.present',
+            name='grant test 2',
+            grant='SELECT, ALTER,CREATE TEMPORARY tables, execute',
+            database=self.testdb1 + '.*',
+            user=self.users['user1']['name'],
+            host='localhost',
+            grant_option=True,
+            revoke_first=True,
+            connection_user=self.user,
+            connection_pass=self.password,
+            connection_charset='utf8'
+        )
+        self.assertSaltTrueReturn(ret)
+        ret = self.run_state(
+            'mysql_grants.present',
+            name='grant test 3',
+            grant='SELECT, INSERT',
+            database=self.testdb2 + '.' + self.table2,
+            user=self.users['user2']['name'],
+            host='localhost',
+            grant_option=True,
+            revoke_first=True,
+            connection_user=self.user,
+            connection_pass=self.password,
+            connection_charset='utf8'
+        )
+        self.assertSaltTrueReturn(ret)
+        ret = self.run_state(
+            'mysql_grants.present',
+            name='grant test 4',
+            grant='SELECT, INSERT',
+            database=self.testdb2 + '.' + self.table2,
+            user=self.users['user2']['name'],
+            host='localhost',
+            grant_option=True,
+            revoke_first=True,
+            connection_user=self.user,
+            connection_pass=self.password,
+            connection_charset='utf8'
+        )
+        self.assertSaltTrueReturn(ret)
+        ret = self.run_state(
+            'mysql_grants.present',
+            name='grant test 5',
+            grant='SELECT, UPDATE',
+            database=self.testdb2 + '.*',
+            user=self.users['user1']['name'],
+            host='localhost',
+            grant_option=True,
+            revoke_first=False,
+            connection_user=self.user,
+            connection_pass=self.password,
+            connection_charset='utf8'
+        )
+        self.assertSaltTrueReturn(ret)
+        ret = self.run_state(
+            'mysql_grants.absent',
+            name='grant test 6',
+            grant='SELECT,update',
+            database=self.testdb2 + '.*',
+            user=self.users['user1']['name'],
+            host='localhost',
+            grant_option=True,
+            revoke_first=False,
+            connection_user=self.user,
+            connection_pass=self.password,
+            connection_charset='utf8'
+        )
+        self.assertSaltTrueReturn(ret)
+
+
 if __name__ == '__main__':
     from integration import run_tests
-    run_tests(MysqlDatabaseStateTest)
+    run_tests(MysqlDatabaseStateTest, MysqlGrantsStateTest)
