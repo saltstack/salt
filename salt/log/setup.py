6ef8bcd40a88a1930940c6661c0c6ce999765494
Erik Johnson <palehose@gmail.com>
2013-12-12 12:50:04 -0600

    Add 'critical' loglevel to salt.log.setup.LOG_LEVELS

--- salt/log/setup.py
+++ salt/log/setup.py
@@ -40,6 +40,7 @@ LOG_LEVELS = {
     'all': logging.NOTSET,
     'debug': logging.DEBUG,
     'error': logging.ERROR,
+    'critical': logging.CRITICAL,
     'garbage': GARBAGE,
     'info': logging.INFO,
     'quiet': QUIET,
