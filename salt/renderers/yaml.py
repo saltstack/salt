7e912c61066c550a90ebc7f368f97de2f2155aa7
Thomas S Hatch <thatch45@gmail.com>
2013-12-13 14:36:44 -0700

    check for config.get

--- salt/renderers/yaml.py
+++ salt/renderers/yaml.py
@@ -57,8 +57,9 @@ def render(yaml_data, saltenv='base', sls='', argline='', **kws):
         if not data:
             data = {}
         else:
-            if __salt__['config.get']('yaml_utf8', False):
-                data = _yaml_result_unicode_to_utf8(data)
+            if 'config.get' in __salt__:
+                if __salt__['config.get']('yaml_utf8', False):
+                    data = _yaml_result_unicode_to_utf8(data)
         log.debug('Results of YAML rendering: \n{0}'.format(data))
         return data
 
