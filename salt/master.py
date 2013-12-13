saltstack/salt

commit 1de62049a11c46bf2a5fc0eb3a9bc98e428cb90d

Accept runner and wheel api calls with nested kwargs
This takes care of a namespace collision issue

salt/master.py
@@ -1789,7 +1789,7 @@ class ClearFuncs(object):
             try:
                 fun = clear_load.pop('fun')
                 runner_client = salt.runner.RunnerClient(self.opts)
-                return runner_client.async(fun, clear_load)
+                return runner_client.async(fun, clear_load.get('kwarg', {}))
             except Exception as exc:
                 log.error('Exception occurred while '
                         'introspecting {0}: {1}'.format(fun, exc))
@@ -1831,7 +1831,7 @@ class ClearFuncs(object):
             try:
                 fun = clear_load.pop('fun')
                 runner_client = salt.runner.RunnerClient(self.opts)
-                return runner_client.async(fun, clear_load)
+                return runner_client.async(fun, clear_load.get('kwarg', {}))
             except Exception as exc:
                 log.error('Exception occurred while '
                         'introspecting {0}: {1}'.format(fun, exc))
@@ -1878,7 +1878,7 @@ class ClearFuncs(object):
 
             try:
                 fun = clear_load.pop('fun')
-                return self.wheel_.call_func(fun, **clear_load)
+                return self.wheel_.call_func(fun, **clear_load.get('kwarg', {}))
             except Exception as exc:
                 log.error('Exception occurred while '
                         'introspecting {0}: {1}'.format(fun, exc))
@@ -1919,7 +1919,7 @@ class ClearFuncs(object):
 
             try:
                 fun = clear_load.pop('fun')
-                return self.wheel_.call_func(fun, **clear_load)
+                return self.wheel_.call_func(fun, **clear_load.get('kwarg', {}))
             except Exception as exc:
                 log.error('Exception occurred while '
                         'introspecting {0}: {1}'.format(fun, exc))

commit 36f70bdcecb7ad89dc6e939d8743f8db3eb4b48b

Correctly pass kwargs through to wheel.call_func
The change in 1de6204 broke what format_call() expects in
wheel.Wheel().call_func().

salt/master.py
@@ -2193,7 +2193,7 @@ class ClearFuncs(object):
                     'user': token['name']}
             try:
                 self.event.fire_event(data, tagify([jid, 'new'], 'wheel'))
-                ret = self.wheel_.call_func(fun, **clear_load.get('kwarg', {}))
+                ret = self.wheel_.call_func(fun, **clear_load)
                 data['return'] = ret
                 data['success'] = True
                 self.event.fire_event(data, tagify([jid, 'ret'], 'wheel'))
@@ -2263,7 +2263,7 @@ class ClearFuncs(object):
                     'user': clear_load.get('username', 'UNKNOWN')}
             try:
                 self.event.fire_event(data, tagify([jid, 'new'], 'wheel'))
-                ret = self.wheel_.call_func(fun, **clear_load.get('kwarg', {}))
+                ret = self.wheel_.call_func(fun, **clear_load)
                 data['return'] = ret
                 data['success'] = True
                 self.event.fire_event(data, tagify([jid, 'ret'], 'wheel'))

salt/utils/event.py
@@ -598,9 +598,8 @@ class ReactWrap(object):
         '''
         Wrap Wheel to enable executing :ref:`wheel modules <all-salt.wheel>`
         '''
-        kwargs['fun'] = fun
         wheel = salt.wheel.Wheel(self.opts)
-        return wheel.call_func(**kwargs)
+        return wheel.call_func(fun, **kwargs)
 
 
 class StateFire(object):