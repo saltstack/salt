b7581d7bf6f2a91e4955af12d886d8498a3138ad
Pedro Algarvio <pedro@algarvio.me>
2013-12-20 20:02:05 +0000

    White-space related lint fixes.

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -1031,7 +1031,7 @@ class ClientCase(AdaptedConfigurationTestCaseMixIn, TestCase):
     def mkdir_p(self, path):
         try:
             os.makedirs(path)
-        except OSError as exc: # Python >2.5
+        except OSError as exc:  # Python >2.5
             if exc.errno == errno.EEXIST and os.path.isdir(path):
                 pass
             else:
