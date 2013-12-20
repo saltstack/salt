6f4cabf99cda4ffe996f1d66d3ac4b22bb6f227d
Seth House <seth@eseth.com>
2013-12-19 20:39:20 -0700

    Added wheel test for testing token authentication

--- tests/integration/wheel/__init__.py
+++ tests/integration/wheel/__init__.py
@@ -1,5 +1,8 @@
 # coding: utf-8
 
+# Import Python libs
+import os
+
 # Import Salt Testing libs
 import integration
 
@@ -27,6 +30,31 @@ class WheelModuleTest(integration.ClientCase):
             'eauth': 'auto',
             'username': 'saltdev',
             'password': 'saltdev',
+        })
+
+    def test_token(self):
+        '''
+        Test executing master_call with lowdata
+
+        The choice of using key.list_all for this is arbitrary and should be
+        changed to some mocked function that is more testing friendly.
+        '''
+        import salt.auth
+
+        opts = self.get_opts()
+        self.mkdir_p(os.path.join(opts['root_dir'], 'cache', 'tokens'))
+
+        auth = salt.auth.LoadAuth(opts)
+        token = auth.mk_token({
+            'username': 'saltdev',
+            'password': 'saltdev',
+            'eauth': 'auto',
+        })
+
+        self.wheel.master_call(**{
+            'client': 'wheel',
+            'fun': 'key.list_all',
+            'token': token['token'],
         })
 
 if __name__ == '__main__':
