53469f0e9dcbe1619ec2d38ade94e6465785c168
David Boucha <dave@saltstack.com>
2014-01-09 20:14:21 +0000

    Don't try fstat if on Windows

--- salt/utils/__init__.py
+++ salt/utils/__init__.py
@@ -642,12 +642,14 @@ def copyfile(source, dest, backup_mode='', cachedir=''):
         pass
     # Get current file stats to they can be replicated after the new file is
     # moved to the destination path.
-    try:
-        fstat = os.stat(dest)
-    except OSError:
-        fstat = None
+    fstat = None
+    if not salt.utils.is_windows():
+        try:
+            fstat = os.stat(dest)
+        except OSError:
+            pass
     shutil.move(tgt, dest)
-    if fstat is not None and not salt.utils.is_windows():
+    if fstat is not None:
         os.chown(dest, fstat.st_uid, fstat.st_gid)
         os.chmod(dest, fstat.st_mode)
     # If SELINUX is available run a restorecon on the file
