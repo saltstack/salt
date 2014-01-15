0f2729b45fcb07d831cc40fa8e4cf8c192f3c381
C. R. Oldham <cr@saltstack.com>
2014-01-15 13:47:59 -0700

    Fix some pylint errors, remove salt_proxy_minion function in scripts.

--- salt/grains/junos.py
+++ salt/grains/junos.py
@@ -9,6 +9,7 @@ __proxyenabled__ = ['junos']
 
 __virtualname__ = 'junos'
 
+
 def __virtual__():
     if 'proxyconn' not in __opts__:
         return False
@@ -16,15 +17,15 @@ def __virtual__():
         return __virtualname__
 
 def location():
-    return {'location':'dc-1-europe'}
+    return {'location': 'dc-1-europe'}
+
 
 def os_family():
-    return {'os_family':'junos'}
+    return {'os_family': 'junos'}
+
 
 def os_data():
     facts = __opts__['proxyconn'].facts
-    facts['version_info'] = { 'major': '12,1', 'type':'I', 'minor': '20131108_srx_12q1_x46_intgr', 'build':'0-613414' }
+    facts['version_info'] = { 'major': '12,1', 'type': 'I', 'minor': '20131108_srx_12q1_x46_intgr', 'build': '0-613414' }
     facts['os_family'] = 'proxy'
     return facts
-
-
