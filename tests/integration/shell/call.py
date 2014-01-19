a5f22869601c946f73513d25ce14e8f521e45264
Pedro Algarvio <pedro@algarvio.me>
2014-01-19 06:44:45 +0000

    Fix unit test expected results since we now exit with an error code.

--- tests/integration/shell/call.py
+++ tests/integration/shell/call.py
@@ -220,11 +220,19 @@ class CallTest(integration.ShellCase, integration.ShellCaseCommonTestsMixIn):
             '--config-dir {0} cmd.run "echo foo"'.format(
                 config_dir
             ),
-            timeout=15
+            timeout=15,
+            catch_stderr=True,
+            with_retcode=True
         )
         try:
-            self.assertIn('local:', ret)
+            self.assertIn('local:', ret[0])
             self.assertFalse(os.path.isdir(os.path.join(config_dir, 'file:')))
+        except AssertionError:
+            # We now fail when we're unable to properly set the syslog logger
+            self.assertIn(
+                'Failed to setup the Syslog logging handler', '\n'.join(ret[1])
+            )
+            self.assertEqual(ret[2], 2)
         finally:
             os.chdir(old_cwd)
             if os.path.isdir(config_dir):
