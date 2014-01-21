b2ebc7f6443df4f23ce5611d7add7607bcfc293f
Thomas S Hatch <thatch45@gmail.com>
2014-01-20 17:10:48 -0700

    Don't stack trace when running an old pycrypto

--- salt/auth/pki.py
+++ salt/auth/pki.py
@@ -16,12 +16,28 @@ TODO: Add a 'ca_dir' option to configure a directory of CA files, a la Apache.
 :depends:    - pyOpenSSL module
 '''
 
+# Import third party libs
+try:
+    from Crypto.Util import asn1
+    import OpenSSL
+    HAS_DEPS = True
+except ImportError:
+    HAS_DEPS = False
+
 # Import python libs
-from Crypto.Util import asn1
-import OpenSSL
 import logging
 
 log = logging.getLogger(__name__)
+
+
+def __virtual__():
+    '''
+    Requires newer pycrypto and pyOpenSSL
+    '''
+    if HAS_DEPS:
+        return 'pki'
+    return False
+
 
 def auth(pem, **kwargs):
     '''
@@ -43,7 +59,7 @@ def auth(pem, **kwargs):
 
     cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, pem)
 
-    cacert_file =  __salt__['config.get']('external_auth:pki:ca_file')
+    cacert_file = __salt__['config.get']('external_auth:pki:ca_file')
     with open(cacert_file) as f:
         cacert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, f.read())
 
@@ -55,7 +71,7 @@ def auth(pem, **kwargs):
     algo = cert.get_signature_algorithm()
 
     # Get the ASN1 format of the certificate
-    cert_asn1 =c .dump_certificate(c.FILETYPE_ASN1, cert)
+    cert_asn1 = c.dump_certificate(c.FILETYPE_ASN1, cert)
 
     # Decode the certificate
     der = asn1.DerSequence()
@@ -93,7 +109,6 @@ def auth(pem, **kwargs):
         c.verify(cacert, sig, der_cert, algo)
         log.info('Successfully authenticated certificate: {0}'.format(pem))
         return True
-    except OpenSSL.crypto.Error, e:
-        log.info("Failed to authenticate certificate: {0}".format(pem))
+    except OpenSSL.crypto.Error:
+        log.info('Failed to authenticate certificate: {0}'.format(pem))
     return False
-
