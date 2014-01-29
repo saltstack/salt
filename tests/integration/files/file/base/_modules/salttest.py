a6b3bf416727cf59bdcfa5efabbbe23962d6a05a
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2014-01-29 02:42:58 +0100

    Lint fix

--- tests/integration/files/file/base/_modules/salttest.py
+++ tests/integration/files/file/base/_modules/salttest.py
@@ -5,4 +5,12 @@ Module for running arbitrary tests
 
 
 def jinja_error():
+    '''
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' salttest.jinja_error
+    '''
     raise Exception('hehehe')
