159146a87f5731648e78a3664bd129a59891a964
Samuel M Smith <smith.samuel.m@gmail.com>
2014-02-05 14:19:55 -0700

    Added ephemeral test directory

---
+++ salt/transport/table/test/test_table.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+'''
+Tests to try out table. Potentially ephemeral
+
+'''
+
+from salt.transport import table
+
+def test_table():
+    public =  table.Public()
+
+
+
+
+
+if __name__ == "__main__":
+    test_table()
