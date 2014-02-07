e1db96807ebe1b344c63f89364cdccb6f49d88f0
David Boucha <boucha@gmail.com>
2014-02-07 11:17:46 -0700

    Remove process.join()
    This caused the windows minion to block

--- salt/minion.py
+++ salt/minion.py
@@ -694,7 +694,6 @@ class Minion(object):
                 target=target, args=(instance, self.opts, data)
             )
         process.start()
-        process.join()
 
     @classmethod
     def _thread_return(cls, minion_instance, opts, data):
