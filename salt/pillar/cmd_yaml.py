6beab792756f0cdf52e09fc2e24828193e90b300
dboyd <dboyd@jumptrading.com>
2014-02-07 13:57:29 -0500

    Improve formatting.

--- salt/pillar/cmd_yaml.py
+++ salt/pillar/cmd_yaml.py
@@ -21,7 +21,8 @@ def ext_pillar(minion_id, pillar, command):
     Execute a command and read the output as YAML
     '''
     try:
-        return yaml.safe_load(__salt__['cmd.run']('{0} {1}'.format(command, minion_id)))
+        return yaml.safe_load(
+          __salt__['cmd.run']('{0} {1}'.format(command, minion_id)))
     except Exception:
         log.critical(
                 'YAML data from {0} failed to parse'.format(command)
