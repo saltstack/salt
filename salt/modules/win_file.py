b51066c8ea08b575cc529e1118ede7f85cc49fc2
Pedro Algarvio <pedro@algarvio.me>
2014-02-08 06:58:00 +0000

    Remove unused imports

--- salt/modules/win_file.py
+++ salt/modules/win_file.py
@@ -14,10 +14,10 @@ import os
 import stat
 import os.path
 import logging
-import contextlib
-import difflib
+# pylint: disable=W0611
 import tempfile  # do not remove. Used in salt.modules.file.__clean_tmp
 import itertools  # same as above, do not remove, it's used in __clean_tmp
+# pylint: enable=W0611
 
 # Import third party libs
 try:
