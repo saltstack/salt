d1c4489e9319c5bbd2c839d70f891942e0763871
Kenneth Jung <ken@mybasis.com>
2014-02-10 12:00:11 -0800

    Caches loading of modules

--- salt/modules/master.py
+++ salt/modules/master.py
@@ -14,30 +14,39 @@ import salt
 # this is the only way I could figure out how to get the REAL file_roots
 # __opt__['file_roots'] is set to  __opt__['pillar_root']
 class MMinion(object):
-    _mminion = None
-    def __new__(cls, *args, **kwargs):
-        if not cls._mminion:
-            cls._mminion = super(MMinion, cls).__new__( cls, *args, **kwargs)
+    def __new__(cls, env, reload_env=False):
+        # this is to break out of salt.loaded.int and make this a true singleton
+        # hack until https://github.com/saltstack/salt/pull/10273 is resolved
+        # this is starting to look like PHP
+        global _mminions
+        if '_mminions' not in globals():
+            _mminions = {}
+        if env not in _mminions or reload_env:
             opts = copy.deepcopy(__opts__)
             del opts['file_roots']
             # grains at this point are in the context of the minion
+            global __grains__
             grains = copy.deepcopy(__grains__)
-            cls._mminion = salt.minion.MasterMinion(opts)
+            m = salt.minion.MasterMinion(opts)
+
             # this assignment is so that the rest of fxns called by salt still
             # have minion context
             __grains__ = grains
+
             # this assignment is so that fxns called by mminion have minion
             # context
-           cls._mminion.opts['grains'] = grains
-        return cls._mminion
+            m.opts['grains'] = grains
+
+            env_roots = m.opts['file_roots'][env]
+            m.opts['module_dirs'] = [fp + '/_modules' for fp in env_roots]
+            m.gen_modules()
+            _mminions[env] = m
+        return _mminions[env]
 
 def mmodule(env, fun, *args, **kwargs):
     '''
     Loads minion modules from an environment so that they can be used in pillars
     for that environment
     '''
-    mminion = MMinion()
-    env_roots = mminion.opts['file_roots'][env]
-    mminion.opts['module_dirs'] = [fp + '/_modules' for fp in env_roots]
-    mminion.gen_modules()
+    mminion = MMinion(env)
     return mminion.functions[fun](*args, **kwargs)
