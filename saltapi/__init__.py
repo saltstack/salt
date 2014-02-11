805d7fedb1aa0360988b582b7af4f21f028d3cfe
Seth House <seth@eseth.com>
2014-02-11 14:18:21 -0700

    Revert "Added new client and fun naming scheme so can have sync or async modes"
    This reverts commit 545daaed9f447fd318ab4952606378a9aa87926b.
    
    This change is cosmetic and was never documented. It was originally
    introduced for Halite and is no longer needed. We may move to a lowdata
    structure similar to this once this project is in Salt core and once
    salt.client.api.APIClient() is stable. At the time of this writing there
    are no sync runners or async wheels available that use Salt's
    master_call interface.
    
    Conflicts:
    	saltapi/__init__.py

--- saltapi/__init__.py
+++ saltapi/__init__.py
@@ -28,29 +28,9 @@ class APIClient(object):
         '''
         Execute the specified function in the specified client by passing the
         lowstate
-
-        New backwards compatible client and fun naming scheme.
-        In new scheme low['client'] is the client mode either 'sync' or 'async'.
-        Default is 'async'
-        If 'wheel' or 'runner' prefixes fun then use associated salt client given
-            by prefix in the specified 'sync' or 'async' mode.
-        Otherwise use local salt client in the given 'sync' or 'async' mode
         '''
-
         if not 'client' in low:
-            low['client'] = 'async'
-            #raise SaltException('No client specified')
-
-        # check for wheel or runner prefix to fun name
-        funparts = low.get('fun', '').split('.')
-        if len(funparts) > 2 and funparts[0] in ['wheel', 'runner']:
-            if low['client'] not in ['sync', 'async']: #client should be only 'sync' or 'async'
-                raise SaltException('With fun of "{1}", client must be "sync" or "async" not "{0}".'\
-                                    .format(low['client'], low['fun']))
-
-            low['client'] = '{0}_{1}'.format(funparts[0], low['client'])
-            low['fun'] = '.'.join(funparts[1:]) #strip prefix
-
+            raise SaltException('No client specified')
 
         if not ('token' in low or 'eauth' in low):
             raise EauthAuthenticationError(
@@ -73,9 +53,7 @@ class APIClient(object):
         local = salt.client.LocalClient(self.opts['conf_file'])
         return local.run_job(*args, **kwargs)
 
-    async = local_async # default async client
-
-    def local_sync(self, *args, **kwargs):
+    def local(self, *args, **kwargs):
         '''
         Run :ref:`execution modules <all-salt.modules>` syncronously
 
@@ -85,9 +63,6 @@ class APIClient(object):
         '''
         local = salt.client.LocalClient(self.opts['conf_file'])
         return local.cmd(*args, **kwargs)
-
-    local = local_sync  # backwards compatible alias
-    sync = local_sync # default sync client
 
     def local_batch(self, *args, **kwargs):
         '''
@@ -101,7 +76,7 @@ class APIClient(object):
         local = salt.client.LocalClient(self.opts['conf_file'])
         return local.cmd_batch(*args, **kwargs)
 
-    def runner_sync(self, fun, **kwargs):
+    def runner(self, fun, **kwargs):
         '''
         Run `runner modules <all-salt.runners>`
 
@@ -112,10 +87,7 @@ class APIClient(object):
         runner = salt.runner.RunnerClient(self.opts)
         return runner.low(fun, kwargs)
 
-    runner = runner_sync #backwards compatible alias
-    runner_async = runner_sync # until we get an runner_async
-
-    def wheel_sync(self, fun, **kwargs):
+    def wheel(self, fun, **kwargs):
         '''
         Run :ref:`wheel modules <all-salt.wheel>`
 
@@ -126,6 +98,3 @@ class APIClient(object):
         kwargs['fun'] = fun
         wheel = salt.wheel.Wheel(self.opts)
         return wheel.master_call(**kwargs)
-
-    wheel = wheel_sync # backwards compatible alias
-    wheel_async = wheel_sync # so it works either mode
