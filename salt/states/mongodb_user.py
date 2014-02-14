972b6694770646efb224fc7b8db3a605040323f1
Colton Myers <colton.myers@gmail.com>
2014-02-14 14:35:55 -0700

    Check for valid port in mongodb_user.present, Ref #10430

--- salt/states/mongodb_user.py
+++ salt/states/mongodb_user.py
@@ -50,6 +50,15 @@ def present(name,
            'changes': {},
            'result': True,
            'comment': 'User {0} is already present'.format(name)}
+
+    # Check for valid port
+    try:
+        port = int(port)
+    except TypeError:
+        ret['result'] = False
+        ret['comment'] = 'Port ({0}) is not an integer.'.format(port)
+        return ret
+
     # check if user exists
     if __salt__['mongodb.user_exists'](name, user, password, host, port, database):
         return ret
