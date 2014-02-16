saltstack/salt

commit 42ca06c90a3c33cf6128d68865b51d805d0ed2f6

Allow the new master's minion to answer to local master

salt/cloud/__init__.py
@@ -835,9 +835,9 @@ class Cloud(object):
                 )
                 vm_['master_pub'] = master_pub
                 vm_['master_pem'] = master_priv
-        elif local_master is True and deploy is True:
-            # Since we're not creating a master, and we're deploying, accept
-            # the key on the local master
+
+        if local_master is True and deploy is True:
+            # Accept the key on the local master
             salt.utils.cloud.accept_key(
                 self.opts['pki_dir'], vm_['pub_key'], key_id
             )
@@ -1487,6 +1487,7 @@ class Map(Cloud):
                 (name, profile) for name, profile in create_list
                 if profile.get('make_master', False) is True
             ).next()
+            master_minion_name = master_name
             log.debug('Creating new master {0!r}'.format(master_name))
             if salt.config.get_cloud_config_value('deploy',
                                        master_profile,
@@ -1516,10 +1517,13 @@ class Map(Cloud):
 
             if master_profile.get('make_minion', True) is True:
                 master_profile.setdefault('minion', {})
+                if 'id' in master_profile['minion']:
+                    master_minion_name = master_profile['minion']['id']
                 # Set this minion's master as local if the user has not set it
-                master_profile['minion'].setdefault('master', '127.0.0.1')
-                if master_finger is not None:
-                    master_profile['master_finger'] = master_finger
+                if 'master' not in master_profile['minion']:
+                    master_profile['minion']['master'] = '127.0.0.1'
+                    if master_finger is not None:
+                        master_profile['master_finger'] = master_finger
 
             # Generate the minion keys to pre-seed the master:
             for name, profile in create_list:
@@ -1542,7 +1546,15 @@ class Map(Cloud):
                 master_profile.setdefault('preseed_minion_keys', {})
                 master_profile['preseed_minion_keys'].update({name: pub})
 
-            out = self.create(master_profile, local_master=False)
+            local_master = False
+            if master_profile['minion'].get('local_master', False) and \
+                    master_profile['minion'].get('master', None) is not None:
+                # The minion is explicitly defining a master and it's
+                # explicitely saying it's the local one
+                local_master = True
+
+            out = self.create(master_profile, local_master=local_master)
+
             if not isinstance(out, dict):
                 log.debug(
                     'Master creation details is not a dictionary: {0}'.format(
@@ -1594,7 +1606,7 @@ class Map(Cloud):
             opts['display_ssh_output'] = False
 
         for name, profile in create_list:
-            if name == master_name:
+            if name in (master_name, master_minion_name):
                 # Already deployed, it's the master's minion
                 continue
 

commit 0085232d1da80029af028da7fa20bfd0571e0aab

[salt.cloud] master_minion_name referenced before assignment
Fixes error caused by 42ca06c9

[ERROR   ] There was a query error: local variable 'master_minion_name' referenced before assignment
Traceback (most recent call last):
  File "/usr/lib/python2.7/site-packages/salt/cloud/cli.py", line 389, in run
    ret = mapper.run_map(dmap)
  File "/usr/lib/python2.7/site-packages/salt/cloud/__init__.py", line 1611, in run_map
    if name in (master_name, master_minion_name):
UnboundLocalError: local variable 'master_minion_name' referenced before assignment

salt/cloud/__init__.py
@@ -1480,6 +1480,7 @@ class Map(Cloud):
         if self.opts['parallel']:
             parallel_data = []
         master_name = None
+        master_minion_name = None
         master_host = None
         master_finger = None
         try: