6b55fb031e292f680462fb5c8d4839856e0d7f73
Sébastien Luttringer <seblu@seblu.net>
2014-02-18 20:57:14 +0100

    Make pacman system upgrade optional
    Split upgrade and refresh options when installing a pacman package.
    
    refresh option control pacman -Sy
    sysupgrade option control pacman -Su
    
    Important note: refresh=True no more upgrade your whole system.
    
    Linked to this: https://github.com/saltstack/salt/pull/10519/

--- salt/modules/pacman.py
+++ salt/modules/pacman.py
@@ -224,11 +224,13 @@ def refresh_db():
 
 def install(name=None,
             refresh=False,
+            sysupgrade=False,
             pkgs=None,
             sources=None,
             **kwargs):
     '''
-    Install the passed package, add refresh=True to install with an -Sy.
+    Install (pacman -S) the passed package, add refresh=True to install with -y,
+	add sysupgrade=True to install with -u.
 
     name
         The name of the package to be installed. Note that this parameter is
@@ -245,6 +247,9 @@ def install(name=None,
 
     refresh
         Whether or not to refresh the package database before installing.
+
+    sysupgrade
+        Whether or not to upgrade the system packages before installing.
 
 
     Multiple Package Installation Options:
@@ -307,6 +312,7 @@ def install(name=None,
     elif pkg_type == 'repository':
         targets = []
         problems = []
+        options = ['--noprogressbar', '--noconfirm', '--needed']
         for param, version_num in pkg_params.iteritems():
             if version_num is None:
                 targets.append(param)
@@ -328,14 +334,12 @@ def install(name=None,
                 log.error(problem)
             return {}
 
-        # It is critical that -Syu is run instead of -Sy:
-        # http://gist.io/5660494
         if salt.utils.is_true(refresh):
-            cmd = 'pacman -Syu --noprogressbar --noconfirm --needed ' \
-                  '"{0}"'.format('" "'.join(targets))
-        else:
-            cmd = 'pacman -S --noprogressbar --noconfirm --needed ' \
-                  '"{0}"'.format('" "'.join(targets))
+            options += '-y'
+        if salt.utils.is_true(sysupgrade):
+            options += '-u'
+
+        cmd = 'pacman -S "{0}"'.format('" "'.join(options+targets))
 
     old = list_pkgs()
     __salt__['cmd.run'](cmd, output_loglevel='debug')
