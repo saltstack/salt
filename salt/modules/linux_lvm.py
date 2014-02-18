0affe6636c46cc1bc6a1ef64b74cd65e76823f53
Andrew Hammond <andrew.george.hammond@gmail.com>
2014-02-18 11:45:22 -0800

    support additional kwargs to lvcreate

--- salt/modules/linux_lvm.py
+++ salt/modules/linux_lvm.py
@@ -230,7 +230,7 @@ def vgcreate(vgname, devices, **kwargs):
     return vgdata
 
 
-def lvcreate(lvname, vgname, size=None, extents=None, snapshot=None, pv=''):
+def lvcreate(lvname, vgname, size=None, extents=None, snapshot=None, pv='', **kwargs):
     '''
     Create a new logical volume, with option for which physical volume to be used
 
@@ -245,13 +245,24 @@ def lvcreate(lvname, vgname, size=None, extents=None, snapshot=None, pv=''):
     if size and extents:
         return 'Error: Please specify only size or extents'
 
+    valid = ('activate', 'chunksize', 'contiguous', 'discards', 'stripes',
+             'stripesize', 'minor', 'persistent', 'mirrors', 'noudevsync',
+             'monitor', 'ignoremonitoring', 'permission', 'poolmetadatasize',
+             'readahead', 'regionsize', 'thin', 'thinpool', 'type', 'virtualsize',
+             'zero',)
+    no_parameter = ('noudevsync', 'ignoremonitoring', )
+    extra_arguments = ' '.join([
+        '--{0}'.format(k) if k in no_parameter else '--{0} {1}'.format(k, v)
+        for k, v in kwargs.iteritems() if k in valid
+    ])
+
     if snapshot:
         vgname = '-s ' + vgname + '/' + snapshot
 
     if size:
-        cmd = 'lvcreate -n {0} {1} -L {2} {3}'.format(lvname, vgname, size, pv)
+        cmd = 'lvcreate -n {0} {1} -L {2} {3} {4}'.format(lvname, vgname, size, extra_arguments, pv)
     elif extents:
-        cmd = 'lvcreate -n {0} {1} -l {2} {3}'.format(lvname, vgname, extents, pv)
+        cmd = 'lvcreate -n {0} {1} -l {2} {3} {4}'.format(lvname, vgname, extents, extra_arguments, pv)
     else:
         return 'Error: Either size or extents must be specified'
     out = __salt__['cmd.run'](cmd).splitlines()
