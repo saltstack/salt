0012518cf8c2561c556347b683aed148fbeae8a2
Pedro Algarvio <pedro@algarvio.me>
2014-02-18 21:16:23 +0000

    Have pylint ignore the import

--- salt/renderers/pyobjects.py
+++ salt/renderers/pyobjects.py
@@ -110,7 +110,10 @@ a state.
 import logging
 
 from salt.loader import states
-from salt.utils.pyobjects import StateFactory, StateRegistry
+from salt.utils.pyobjects import StateRegistry
+from salt.utils.pyobjects import StateFactory  # pylint: disable=W0611
+# DO NOT REMOVE THE DISABLE ABOVE, it's used in an exec call below
+
 
 log = logging.getLogger(__name__)
 
