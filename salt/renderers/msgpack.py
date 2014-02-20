6fa414e5203455b55e626682cef814a0ff0def31
Thomas S Hatch <thatch45@gmail.com>
2014-02-20 14:04:59 -0700

    Add docstring to msgpack rednerer

--- salt/renderers/msgpack.py
+++ salt/renderers/msgpack.py
@@ -7,8 +7,15 @@ import msgpack
 
 def render(msgpack_data, saltenv='base', sls='', **kws):
     '''
-    Accepts JSON as a string or as a file object and runs it through the JSON
-    parser.
+    Accepts a message pack string or a file object, renders said data back to
+    a python dict.
+
+    .. note:
+    
+        This renderer is NOT intended for use in creating sls files by hand,
+        but exists to allow for data backends to serialize the highdata
+        structure in an easily transportable way. This is to allow for more
+        fluid fileserver backends that rely on pure data sources.
 
     :rtype: A Python data structure
     '''
