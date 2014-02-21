4e1c73f9e983967d5cd36a8d9ecc9d0353151ee9
rallytime <nicole@saltstack.com>
2014-02-21 16:11:09 -0700

    Even more mac_user tests!

--- tests/unit/modules/mac_user_test.py
+++ tests/unit/modules/mac_user_test.py
@@ -9,6 +9,7 @@ from salttesting.mock import MagicMock, patch
 
 # Import Salt Libs
 from salt.modules import mac_user
+from salt.exceptions import SaltInvocationError, CommandExecutionError
 
 # Import python Libs
 import pwd
@@ -16,6 +17,9 @@ import grp
 
 
 class MacUserTestCase(TestCase):
+    '''
+    TestCase for the salt.modules.mac_user modules
+    '''
 
     mac_user.__context__ = {}
     mac_user.__grains__ = {}
@@ -34,9 +38,14 @@ class MacUserTestCase(TestCase):
     mock_getgrgid = grp.struct_group(('_TEST_GROUP', '*', 83, []))
     mock_getgrall = [grp.struct_group(('accessibility', '*', 90, [])),
                      grp.struct_group(('admin', '*', 80, ['root', 'admin']))]
-    mock_dscl = {'pid': 4948, 'retcode': 0, 'stderr': '', 'stdout': ''}
+    mock_info_ret = {'shell': '/bin/bash', 'name': 'test', 'gid': 4376,
+                     'groups': ['TEST_GROUP'], 'home': '/var/test',
+                     'fullname': 'TEST USER', 'uid': 4376}
 
     def test_osmajor(self):
+        '''
+        Tests version of OS X
+        '''
         with patch.dict(mac_user.__grains__, {'kernel': 'Darwin',
                                               'osrelease': '10.9.1'}):
             self.assertEqual(mac_user._osmajor(), 10.9)
@@ -47,6 +56,9 @@ class MacUserTestCase(TestCase):
         pass
 
     def test_dscl(self):
+        '''
+        Tests the creation of a dscl node
+        '''
         mac_mock = MagicMock(return_value={'pid': 4948,
                                            'retcode': 0,
                                            'stderr': '',
@@ -61,6 +73,9 @@ class MacUserTestCase(TestCase):
 
     @patch('pwd.getpwall', MagicMock(return_value=mock_pwall))
     def test_first_avail_uid(self):
+        '''
+        Tests the availability of the next uid
+        '''
         self.assertEqual(mac_user._first_avail_uid(), 501)
 
     @skipIf(True, 'Test not implemented yet')
@@ -77,6 +92,9 @@ class MacUserTestCase(TestCase):
     @patch('salt.modules.mac_user.list_groups',
            MagicMock(return_value=['TEST_GROUP']))
     def test_getent(self):
+        '''
+        Tests the list of information for all users
+        '''
         ret = [{'shell': '/usr/bin/false', 'name': '_amavisd', 'gid': 83,
                 'groups': ['TEST_GROUP'], 'home': '/var/virusmails',
                 'fullname': 'AMaViS Daemon', 'uid': 83},
@@ -88,18 +106,56 @@ class MacUserTestCase(TestCase):
                 'fullname': 'Application Owner', 'uid': 87}]
         self.assertEqual(mac_user.getent(), ret)
 
-    @patch('salt.modules.mac_user._dscl', MagicMock(return_value=mock_dscl))
-    def test_chuid(self):
-        mock_info_ret = {'shell': '/bin/bash', 'name': 'test', 'gid': 0,
-                     'groups': ['TEST_GROUP'], 'home': '/var/test',
-                     'fullname': 'TEST USER', 'uid': 4376}
-        with patch('salt.modules.mac_user.info',
-                   MagicMock(return_value=mock_info_ret)):
-            self.assertTrue(mac_user.chuid('foo', 4376))
+    def test_chuid_int(self):
+        '''
+        Tests if the uid is an int
+        '''
+        with self.assertRaises(SaltInvocationError):
+            mac_user.chuid('foo', 'foo')
+
+    @patch('salt.modules.mac_user.info', MagicMock(return_value={}))
+    def test_chuid_user_exists(self):
+        '''
+        Tests if the user exists or not
+        '''
+        with self.assertRaises(CommandExecutionError):
+            mac_user.chuid('foo', 4376)
+
+    @patch('salt.modules.mac_user.info', MagicMock(return_value=mock_info_ret))
+    def test_chuid_same_uid(self):
+        '''
+        Tests if the user's uid is the same as as the argument
+        '''
+        self.assertTrue(mac_user.chuid('foo', 4376))
+
+    def test_chgid_int(self):
+        '''
+        Tests if the gid is an int
+        '''
+        with self.assertRaises(SaltInvocationError):
+            mac_user.chgid('foo', 'foo')
+
+    @patch('salt.modules.mac_user.info', MagicMock(return_value={}))
+    def test_chgid_user_exists(self):
+        '''
+        Tests if the user exists or not
+        '''
+        with self.assertRaises(CommandExecutionError):
+            mac_user.chgid('foo', 4376)
+
+    @patch('salt.modules.mac_user.info', MagicMock(return_value=mock_info_ret))
+    def test_chgid_same_gid(self):
+        '''
+        Tests if the user's gid is the same as as the argument
+        '''
+        self.assertTrue(mac_user.chgid('foo', 4376))
 
     @patch('salt.modules.mac_user.list_groups',
            MagicMock(return_value=['_TEST_GROUP']))
     def test_info(self):
+        '''
+        Tests the return of user information
+        '''
         mock_pwnam = pwd.struct_passwd(('test', '*', 0, 0, 'TEST USER',
                                         '/var/test', '/bin/bash'))
         ret = {'shell': '/bin/bash', 'name': 'test', 'gid': 0,
@@ -111,6 +167,9 @@ class MacUserTestCase(TestCase):
     @patch('salt.modules.mac_user.list_groups',
            MagicMock(return_value=['_TEST_GROUP']))
     def test_format_info(self):
+        '''
+        Tests the formatting of returned user information
+        '''
         data = pwd.struct_passwd(('_TEST_GROUP', '*', 83, 83, 'AMaViS Daemon',
                                   '/var/virusmails', '/usr/bin/false'))
         ret = {'shell': '/usr/bin/false', 'name': '_TEST_GROUP', 'gid': 83,
@@ -122,9 +181,15 @@ class MacUserTestCase(TestCase):
     @patch('grp.getgrgid', MagicMock(return_value=mock_getgrgid))
     @patch('grp.getgrall', MagicMock(return_value=mock_getgrall))
     def test_list_groups(self):
+        '''
+        Tests the list of groups the user belongs to
+        '''
         self.assertEqual(mac_user.list_groups('name'), ['_TEST_GROUP'])
 
     @patch('pwd.getpwall', MagicMock(return_value=mock_pwall))
     def test_list_users(self):
+        '''
+        Tests the list of all users
+        '''
         ret = ['_amavisd', '_appleevents', '_appowner']
         self.assertEqual(mac_user.list_users(), ret)
