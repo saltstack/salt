359e73d605211056e976b2c67bf1a01ba62676bc
Pedro Algarvio <pedro@algarvio.me>
2014-02-21 12:43:06 +0000

    Provide an ordered safe yaml dumper.

--- salt/utils/yamldumper.py
+++ salt/utils/yamldumper.py
@@ -8,8 +8,10 @@
 from __future__ import absolute_import
 try:
     from yaml import CDumper as Dumper
+    from yaml import CSafeDumper as SafeDumper
 except ImportError:
     from yaml import Dumper
+    from yaml import SafeDumper
 
 from salt.utils.odict import OrderedDict
 
@@ -18,10 +20,17 @@ class OrderedDumper(Dumper):
     '''
     A YAML dumper that represents python OrderedDict as simple YAML map.
     '''
-    pass
+
+
+class SafeOrderedDumper(SafeDumper):
+    '''
+    A YAML safe dumper that represents python OrderedDict as simple YAML map.
+    '''
 
 
 def represent_ordereddict(dumper, data):
     return dumper.represent_dict(data.items())
 
+
 OrderedDumper.add_representer(OrderedDict, represent_ordereddict)
+SafeOrderedDumper.add_representer(OrderedDict, represent_ordereddict)
