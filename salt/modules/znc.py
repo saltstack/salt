79c1b1e682ba70c7343c65bd91592a2f5c21c31f
George Marshall <george@georgemarshall.name>
2014-02-23 11:29:03 -0800

    Added makepassword private method for znc

--- salt/modules/znc.py
+++ salt/modules/znc.py
@@ -4,7 +4,9 @@ znc - An advanced IRC bouncer
 '''
 
 # Import python libs
+import hashlib
 import logging
+import random
 
 # Import salt libs
 import salt.utils
@@ -19,6 +21,34 @@ def __virtual__():
     if salt.utils.which('znc'):
         return 'znc'
     return False
+
+
+def _makepass(password, hasher='sha256'):
+    '''
+    Create a znc compatiable hashed password
+    '''
+    # Setup the hasher
+    if hasher == 'sha256':
+        h = hashlib.sha256(password)
+    elif hasher == 'md5':
+        h = hashlib.md5(password)
+    else:
+        return NotImplemented
+
+    c = "abcdefghijklmnopqrstuvwxyz" \
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ" \
+        "0123456789!?.,:;/*-+_()"
+    r = {
+        'Method': h.name,
+        'Salt': ''.join(random.choice(c) for x in xrange(20)),
+    }
+
+    # Salt the password hash
+    h.update(r['Salt'])
+    r['Hash'] = h.hexdigest()
+
+    return r
+
 
 
 def version():
