29fd497e86b42d2e6dcbda9ce39f259959c62031
Samuel M Smith <smith.samuel.m@gmail.com>
2014-02-24 16:46:38 -0700

    Starting to integrate ioflo daemons

--- salt/daemons/__init__.py
+++ salt/daemons/__init__.py
@@ -2,4 +2,61 @@
 '''
 The daemons package is used to store implimentations of the Salt Master and
 Minion enabling different transports
+
+Package for ioflo and raet based daemons
 '''
+
+# Import python libs
+import sys
+
+# Import ioflo libs
+import ioflo.app.run
+
+class IofloMaster(object):
+    '''
+    IofloMaster Class
+    '''
+    def __init__(self, opts):
+        '''
+        Assign self.opts
+        '''
+        self.opts = opts
+
+    def start(self):
+        '''
+        Start up ioflo
+        '''
+        behaviors = []
+        behavior.append('salt.transport.road.raet', 'salt.daemons.ioflo', )
+
+        ioflo.app.run.run(  name='master',
+                            filename=self.opts['master_floscript'],
+                            period=float(self.opts['ioflo_period']),
+                            verbose=int(self.opts['ioflo_verbose']),
+                            realtime=self.opts['ioflo_realtime'],
+                            behaviors=behaviors,)
+
+
+class IofloMinion(object):
+    '''
+    IofloMinion Class
+    '''
+    def __init__(self, opts):
+        '''
+        Assign self.opts
+        '''
+        self.opts = opts
+
+    def start(self):
+        '''
+        Start up ioflo
+        '''
+        behaviors = []
+        behavior.append('salt.transport.road.raet', 'salt.daemons.ioflo', )
+
+        ioflo.app.run.run(  name='minion',
+                            filename=self.opts['minion_floscript'],
+                            period=float(self.opts['ioflo_period']),
+                            verbose=int(self.opts['ioflo_verbose']),
+                            realtime=self.opts['ioflo_realtime'],
+                            behaviors=behaviors,)
