305e9fea3d5425046f89a900a8b7724124e2fbc4
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2014-03-05 11:30:13 +0000

    lxc: Separate more concerns and remove out of scope functions

--- salt/states/lxc.py
+++ salt/states/lxc.py
@@ -21,6 +21,11 @@ def absent(name):
             - name: my_instance_name2
 
     '''
+    if __opts__['test']:
+        return {'name': name,
+                'comment': '{0} will be removed'.format(name),
+                'result': True,
+                'changes': {}}
     changes = {}
     exists = __salt__['lxc.exists'](name)
     ret = {'name': name, 'changes': changes,
@@ -62,34 +67,17 @@ def stopped(name):
             - name: my_instance_name2
 
     '''
-    changes = {}
-    exists = __salt__['lxc.exists'](name)
-    ret = {'name': name, 'changes': changes,
-           'result': True, 'comment': 'Stopped'}
-    if not exists:
-        ret = {'name': name, 'changes': {},
-               'result': True, 'comment': 'Container does not exists'}
-    if exists:
-        ret['result'] = False
-        infos = __salt__['lxc.info'](name)
-        try:
-            if infos['state'] == 'running':
-                __salt__['lxc.stop'](name)
-                ret['changes'] = {name: 'stopped'}
-            infos = __salt__['lxc.info'](name)
-            ret['result'] = infos['state'] != 'running'
-            if not ret['result']:
-                raise Exception('Container won\'t stop')
-        except Exception, ex:
-            trace = traceback.format_exc()
-            ret['result'] = False
-            ret['comment'] = 'Error in stopping container'
-            changes['msg'] = '{1}\n{0}\n'.format(ex, trace)
-        return ret
-    return ret
+    if __opts__['test']:
+        return {'name': name,
+                'comment': '{0} will be stopped'.format(name),
+                'result': True,
+                'changes': {}}
+    cret = __salt__['lxc.stop'](name)
+    cret['name'] = name
+    return cret
 
 
-def started(name, force=False):
+def started(name, restart=False):
     '''Start a container
 
     name
@@ -104,36 +92,17 @@ def started(name, force=False):
             - name: my_instance_name2
 
     '''
-    changes = {}
-    exists = __salt__['lxc.exists'](name)
-    ret = {'name': name, 'changes': changes,
-           'result': True, 'comment': 'Started'}
-    if not exists:
-        ret = {'name': name, 'changes': {},
-               'result': True, 'comment': 'Container does not exists'}
-    if exists:
-        if force:
-            stopped(name)
-        ret['result'] = False
-        infos = __salt__['lxc.info'](name)
-        try:
-            if infos['state'] != 'running':
-                __salt__['lxc.start'](name)
-                ret['changes'] = {name: 'started'}
-            infos = __salt__['lxc.info'](name)
-            ret['result'] = infos['state'] == 'running'
-            if not ret['result']:
-                ret['comment'] = 'Container won\'t start'
-        except Exception, ex:
-            trace = traceback.format_exc()
-            ret['result'] = False
-            ret['comment'] = 'Error in starting container'
-            ret['comment'] += '{0}\n{1}\n'.format(ex, trace)
-        return ret
-    return ret
+    if __opts__['test']:
+        return {'name': name,
+                'comment': '{0} will be started'.format(name),
+                'result': True,
+                'changes': {}}
+    cret = __salt__['lxc.start'](name, restart=restart)
+    cret['name'] = name
+    return cret
 
 
-def edited_conf(name, lxc_conf, lxc_conf_unset):
+def edited_conf(name, lxc_conf=None, lxc_conf_unset=None):
     '''Edit LXC configuration options
 
     .. code-block:: bash
@@ -143,9 +112,18 @@ def edited_conf(name, lxc_conf, lxc_conf_unset):
             - name: ubuntu
             - lxc_conf:
                 - network.ipv4.ip: 10.0.3.6
-            - lxc_conf_unset:
+        - lxc_conf_unset:
                 - lxc.utsname
     '''
+    if __opts__['test']:
+        return {'name': name,
+                'comment': '{0} lxc.conf will be edited'.format(name),
+                'result': True,
+                'changes': {}}
+    if not lxc_conf_unset:
+        lxc_conf_unset = {}
+    if not lxc_conf:
+        lxc_conf = {}
     cret = __salt__['lxc.update_lxc_conf'](name,
                                            lxc_conf=lxc_conf,
                                            lxc_conf_unset=lxc_conf_unset)
@@ -181,6 +159,11 @@ def set_pass(name, password=None, user=None, users=None):
               - foo
               - bar
     '''
+    if __opts__['test']:
+        return {'name': name,
+                'comment': 'Passwords for {0} will be udpated'.format(name),
+                'result': True,
+                'changes': {}}
     if not isinstance(users, list):
         users = [users]
     if user and (not user in users):
@@ -235,6 +218,11 @@ def created(name,
             - template: ubuntu
 
     '''
+    if __opts__['test']:
+        return {'name': name,
+                'result': True,
+                'comment': '{0} will be created'.format(name),
+                'changes': {}}
     changes = {}
     ret = {'name': name, 'changes': changes, 'result': True, 'comment': ''}
     exists = __salt__['lxc.exists'](name)
@@ -293,6 +281,12 @@ def cloned(name,
             - vgname: lxc
             - snapshot: true
     '''
+    if __opts__['test']:
+        return {'name': name,
+                'comment': '{0} will be cloned from {1}'.format(
+                    name, orig),
+                'result': True,
+                'changes': {}}
     changes = {}
     ret = {'name': name, 'changes': changes, 'result': True, 'comment': ''}
     exists = __salt__['lxc.exists'](name)
@@ -323,208 +317,5 @@ def cloned(name,
             ret['comment'] += 'Container cloned\n'
             changes['status'] = 'cloned'
     return ret
-
-
-def present(name,
-            from_container=None,
-            template='ubuntu',
-            vgname=None,
-            backing=None,
-            snapshot=True,
-            profile=None,
-            fstype=None,
-            lvname=None,
-            ip=None,
-            mac=None,
-            netmask='24',
-            bridge='lxcbr0',
-            gateway='10.0.3.1',
-            size='10G',
-            lxc_conf_unset=None,
-            lxc_conf=None,
-            stopped=False,
-            master=None,
-            script=None,
-            script_args=None,
-            users=None,
-            user='ubuntu',
-            password='ubuntu',
-            *args, **kw):
-
-    '''
-    Provision a single container
-
-    This do not use lxc.init to provide a more generic fashion to tie minions
-    to their master (if any and defined) to allow custom association code.
-
-    Idea is whenever your lxc is accessible via ssh, you give it a script +
-    params ALA salt cloud to make the association
-
-    Order of operation
-
-        - Spin up the lxc template container using salt.modules.lxc
-          on desired minion (clone or template)
-        - Change lxc config option if any to be changed
-        - Start container
-        - Change base passwords if any
-        - Wait for lxc container to be up and ready for ssh
-        - Test ssh connection and bailout in error
-        - Generate seeds
-        - Via ssh (with the help of salt cloud):
-
-            - Upload deploy script and seeds
-            - Run the script with args inside the context of the container
-
-        - Ping the result minion
-
-            - if ok:
-
-                - Remove the authorized key path
-                - Restart the container
-
-            - else bailout in error
-
-    name
-        Name of the container
-    profile
-        lxc pillar profile
-    Container creation/clone options
-        Use a container using clone
-            from_container
-                Name of an original container using clone
-            snapshot
-                Do we use snapshots on cloned filesystems
-        lxc template using total creation
-            template
-                template to use
-            backing
-                Backing store type (None, lvm, brtfs)
-            lvname
-                LVM lvname if any
-            fstype
-                fstype
-        Common
-            size
-                Size of the container
-            vgname
-                LVM vgname if any
-    users
-        sysadmin users [ubuntu] of the container
-    user
-        sysadmin user (ubuntu) of the container
-    password
-        password for root and sysadmin (ubuntu)
-    mac
-        mac address to associate
-    ip
-        ip to link to
-    netmask
-        netmask for ip
-    bridge
-        bridge to use
-    lxc_conf_unset
-        Configuration variables to unset in lxc conf
-    lxc_conf
-        LXC configuration variables to set in lxc_conf
-    master
-        Master to link to if any
-    '''
-    changes = {}
-    changed = False
-    ret = {'name': name, 'changes': changes, 'result': None, 'comment': ''}
-    if not users:
-        users = ['root']
-        if (
-            __grains__['os'] in ['Ubuntu']
-            and not 'ubuntu' in users
-        ):
-            users.append('ubuntu')
-    if not user in users:
-        users.append(user)
-    if not users:
-        users = []
-    if not lxc_conf:
-        lxc_conf = []
-    if not lxc_conf_unset:
-        lxc_conf_unset = []
-    if from_container:
-        method = 'clone'
-    else:
-        method = 'create'
-    if ip is not None:
-        lxc_conf.append({'lxc.network.ipv4': '{0}/{1}'.format(ip, netmask)})
-    if mac is not None:
-        lxc_conf.append({'lxc.network.hwaddr': mac})
-    if gateway is not None:
-        lxc_conf.append({'lxc.network.ipv4.gateway': gateway})
-    if bridge is not None:
-        lxc_conf.append({'lxc.network.link': bridge})
-    changes['a_creation'] = ''
-    if method == 'clone':
-        cret = cloned(
-            name=name,
-            orig=from_container,
-            snapshot=snapshot,
-            size=size,
-            vgname=vgname,
-            profile=profile,
-        )
-    elif method == 'create':
-        cret = created(
-            name=name,
-            template=template,
-            profile=profile,
-            fstype=fstype,
-            vgname=vgname,
-            size=size,
-            lvname=lvname,
-            backing=backing,
-        )
-    if not cret['result']:
-        ret['result'] = False
-        ret['comment'] = cret['comment']
-        return ret
-    if cret['changes']:
-        changed = True
-    changes['a_creation'] = cret['comment']
-
-    # edit lxc conf if any
-    changes['b_conf'] = ''
-    cret = edited_conf(name, lxc_conf, lxc_conf_unset)
-    if not cret['result']:
-        ret['result'] = False
-        ret['comment'] = cret['comment']
-        return ret
-    if cret['changes']:
-        changed = True
-    changes['b_conf'] = 'lxc conf ok'
-
-    # start
-    changes['c_start'] = ''
-    # reboot if conf has changed
-    cret = started(name, force=changed)
-    if not cret['result']:
-        ret['result'] = False
-        ret['comment'] = cret['comment']
-        return ret
-    if cret['changes']:
-        changed = True
-    changes['c_start'] = 'started'
-
-    # first time provisionning only, set the default user/password
-    changes['d_password'] = 'Password in place'
-    gid = 'lxc.{0}.initial_pass'.format(name, False)
-    if not __grains__.get(gid):
-        cret = set_pass(name, password=password, users=users)
-        if not cret['result']:
-            ret['result'] = False
-            ret['comment'] = cret['comment']
-            return ret
-        if __salt__['grains.setval'](gid, True):
-            __salt__['saltutil.sync_grains']()
-        changes['d_password'] = 'Password updated'
-        changed = True
-    return ret
-
 
 # vim:set et sts=4 ts=4 tw=80:
