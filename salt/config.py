eb139e7c46a65c142243f3e523e58f4ed94248e1
Colton Myers <colton.myers@gmail.com>
2014-03-07 15:51:13 -0700

    Bump default gather_job_timeout to 5 for safety

--- salt/config.py
+++ salt/config.py
@@ -435,7 +435,7 @@ DEFAULT_MASTER_OPTS = {
     'keysize': 4096,
     'transport': 'zeromq',
     'enumerate_proxy_minions': False,
-    'gather_job_timeout': 2,
+    'gather_job_timeout': 5,
     'syndic_event_forward_timeout': 0.5,
     'syndic_max_event_process_time': 0.5,
     'ssh_passwd': '',
