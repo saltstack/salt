601f869023cf2a08f8f556a61854379356bc18e1
linxiao.jz <jiaion21@gmail.com>
2014-03-12 19:04:11 +0800

    fix remove hash cache file error (cp from 0.17)
    Conflicts:
    	salt/fileserver/roots.py

--- salt/fileserver/roots.py
+++ salt/fileserver/roots.py
@@ -201,7 +201,7 @@ def file_hash(load, fnd):
                     # check if mtime changed
                     ret['hsum'] = hsum
                     return ret
-        except os.error:  # Can't use Python select() because we need Windows support
+        except (os.error, IOError):  # Can't use Python select() because we need Windows support
             log.debug("Fileserver encountered lock when reading cache file. Retrying.")
             # Delete the file since its incomplete (either corrupted or incomplete)
             try:
