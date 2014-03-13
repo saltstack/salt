05a6f9738b2956ffca96f0f1596baa409952c15c
Mike Place <mp@saltstack.com>
2014-03-13 13:21:32 -0600

    Fix fail on unknown target in job runner
    Closes #11151

--- salt/runners/jobs.py
+++ salt/runners/jobs.py
@@ -181,7 +181,7 @@ def print_job(job_id):
 
 
 def _format_job_instance(job):
-    return {'Function': job['fun'],
+    return {'Function': job.get('fun', 'unknown-function'),
             'Arguments': list(job.get('arg', [])),
             'Target': job['tgt'],
             'Target-type': job.get('tgt_type', []),
