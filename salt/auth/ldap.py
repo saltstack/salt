64c58b369c15e86cfff794c8f9f28ea75d3aa1ee
max demian <mikar gmx de>
2014-03-15 16:02:13 +0100

    minor style for for len usage and one "== None" occurrence

--- salt/auth/ldap.py
+++ salt/auth/ldap.py
@@ -160,10 +160,10 @@ def auth(username, password):
                 )
             )
             result = _ldap.search_s(basedn, int(scope), paramvalues['filter'])
-            if len(result) < 1:
+            if not len(result):
                 log.warn('Unable to find user {0}'.format(username))
                 return False
-            elif len(result) > 1:
+            elif len(result):
                 log.warn('Found multiple results for user {0}'.format(username))
                 return False
             connargs['binddn'] = result[0][0]
