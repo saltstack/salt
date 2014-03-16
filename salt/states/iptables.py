97ef8e45aa74f4d9d7d262c3029a11d833cfecb3
Carlos Perelló Marín <carlos@pemas.net>
2014-03-16 18:22:58 +0100

    Fixed salt states iptables implementation so you can safely test it without getting changes applied

--- salt/states/iptables.py
+++ salt/states/iptables.py
@@ -141,6 +141,12 @@ def chain_present(name, table='filter', family='ipv4'):
                           .format(name, table, family))
         return ret
 
+    if __opts__['test']:
+        ret['comment'] = 'iptables {0} chain in {1} table needs to be set for {2}'.format(
+            name,
+            table,
+            family)
+        return ret
     command = __salt__['iptables.new_chain'](table, name, family)
     if command is True:
         ret['changes'] = {'locale': name}
@@ -180,7 +186,12 @@ def chain_absent(name, table='filter', family='ipv4'):
         ret['comment'] = ('iptables {0} chain is already absent in {1} table for {2}'
                           .format(name, table, family))
         return ret
-
+    if __opts__['test']:
+        ret['comment'] = 'iptables {0} chain in {1} table needs to be removed {2}'.format(
+            name,
+            table,
+            family)
+        return ret
     flush_chain = __salt__['iptables.flush'](table, name, family)
     if not flush_chain:
         command = __salt__['iptables.delete_chain'](table, name, family)
@@ -439,10 +450,17 @@ def set_policy(name, family='ipv4', **kwargs):
             kwargs['chain'],
             family) == kwargs['policy']:
         ret['result'] = True
-        ret['comment'] = ('iptables default policy for {0} for {1} already set to {2}'
-                          .format(kwargs['table'], family, kwargs['policy']))
+        ret['comment'] = ('iptables default policy for chain {0} on table {1} for {2} already set to {3}'
+                          .format(kwargs['chain'], kwargs['table'], family, kwargs['policy']))
         return ret
-
+    if __opts__['test']:
+        ret['comment'] = 'iptables default policy for chain {0} on table {1} for {2} needs to be set to {3}'.format(
+            kwargs['chain'],
+            kwargs['table'],
+            family,
+            kwargs['policy']
+        )
+        return ret
     if not __salt__['iptables.set_policy'](
             kwargs['table'],
             kwargs['chain'],
@@ -494,7 +512,12 @@ def flush(name, family='ipv4', **kwargs):
 
     if not 'chain' in kwargs:
         kwargs['chain'] = ''
-
+    if __opts__['test']:
+        ret['comment'] = 'iptables rules in {0} table {1} chain {2} family needs to be flushed'.format(
+            name,
+            table,
+            family)
+        return ret
     if not __salt__['iptables.flush'](kwargs['table'], kwargs['chain'], family):
         ret['changes'] = {'locale': name}
         ret['result'] = True
