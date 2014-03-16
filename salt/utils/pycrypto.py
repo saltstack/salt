4657196427338d7304a44a0d0fcae10ef43ec262
Daniel Wallace <danielwallace@gtmanfred.com>
2014-03-16 13:32:38 -0500

    add functions for generating passwords
    and salted hashes. #11256

---
+++ salt/utils/pycrypto.py
@@ -0,0 +1,34 @@
+
+# -*- coding: utf-8 -*-
+'''
+Use pycrypto to generate random passwords on the fly.
+'''
+
+# Import python libraries
+import Crypto.Random
+import crypt
+import re
+
+
+def secure_password(length=20):
+    '''
+    Generate a secure password.
+    '''
+    pw = ''
+    while len(pw) < length:
+        pw += re.sub('\W', '', Crypto.Random.get_random_bytes(1))
+    return pw
+
+
+def gen_hash(salt=None, password=None):
+    '''
+    Generate /etc/shadow hash
+    '''
+
+    if password is None:
+        password = secure_password()
+
+    if salt is None:
+        salt = '$6' + secure_password(8)
+
+    return crypt.crypt(password, salt)
