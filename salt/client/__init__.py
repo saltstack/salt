733217bb0318ee7aa1f8f240e3b7d14b6b9dd7bd
Ollie Walsh <ollie.walsh@gmail.com>
2014-03-19 14:21:55 +0000

    Stop passing **kwargs to find_job

--- salt/client/__init__.py
+++ salt/client/__init__.py
@@ -192,7 +192,7 @@ class LocalClient(object):
                                 arg=arg,
                                 expr_form=tgt_type,
                                 timeout=timeout,
-                                **kwargs)
+                               )
 
         if not pub_data:
             return pub_data
