052022169aaa331f285ca3cb65389743c00b8653
Thomas S Hatch <thatch45@gmail.com>
2014-03-18 22:33:40 -0600

    bump to 2014.1.1

--- salt/version.py
+++ salt/version.py
@@ -334,7 +334,7 @@ class SaltStackVersion(object):
 #
 # Please bump version information for __saltstack_version__ on new releases
 # ----------------------------------------------------------------------------
-__saltstack_version__ = SaltStackVersion(2014, 1, 0)
+__saltstack_version__ = SaltStackVersion(2014, 1, 1)
 __version_info__ = __saltstack_version__.info
 __version__ = __saltstack_version__.string
 # <---- Hardcoded Salt Version Information -----------------------------------
