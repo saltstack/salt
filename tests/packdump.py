357ceac8edf0cef568a20d3a76d888f91d956115
Thomas S Hatch <thatch45@gmail.com>
2014-03-20 14:31:26 -0600

    Add VERY simple script to read msgpack files
    This is meant to make debugging data in msgpack files a little easier

---
+++ tests/packdump.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+'''
+Simple script to dump the contents of msgpack files to the terminal
+'''
+
+# Import python libs
+import os
+import sys
+import pprint
+
+# Import third party libs
+import msgpack
+
+
+def dump(path):
+    '''
+    Read in a path and dump the contents to the screen
+    '''
+    if not os.path.isfile(path):
+        print 'Not a file'
+        return
+    with open(path, 'rb') as fp_:
+        data = msgpack.loads(fp_.read())
+        pprint.pprint(data)
+
+
+if __name__ == '__main__':
+    dump(sys.argv[1])
