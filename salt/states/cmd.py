6922a2261df70e820fd4b8b7c8e5caeea2400e94
Colton Myers <colton.myers@gmail.com>
2014-03-20 16:09:26 -0600

    Update error message for `cmd.script` on cache fail to use saltenv
    Conflicts:
    	salt/states/cmd.py

--- salt/states/cmd.py
+++ salt/states/cmd.py
@@ -758,7 +758,8 @@ def script(name,
             ret['result'] = not bool(cmd_all['retcode'])
         if ret.get('changes', {}).get('cache_error'):
             ret['comment'] = 'Unable to cache script {0} from saltenv ' \
-                             '{1!r}'.format(source, env)
+                             '{1!r}'.format(source, kwargs.get('saltenv',
+                                                               'base'))
         else:
             ret['comment'] = 'Command {0!r} run'.format(name)
         return _reinterpreted_state(ret) if stateful else ret
