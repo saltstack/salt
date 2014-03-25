cae8f60b075ca4e3ac186a1cabf34243138317e7
Colton Myers <colton.myers@gmail.com>
2014-03-25 10:14:59 -0600

    Update released version to 2014.1.1

--- doc/conf.py
+++ doc/conf.py
@@ -117,7 +117,7 @@ copyright = '2013 SaltStack, Inc.'
 
 version = salt.version.__version__
 #release = '.'.join(map(str, salt.version.__version_info__))
-release = '2014.1.0'
+release = '2014.1.1'
 
 language = 'en'
 locale_dirs = [
