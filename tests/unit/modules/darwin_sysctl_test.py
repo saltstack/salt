b17164cfe881924cd64b5bedaca4e2a8ded768d0
rallytime <nicole@saltstack.com>
2014-03-25 11:18:04 -0600

    More darwin_sysctl tests

--- tests/unit/modules/darwin_sysctl_test.py
+++ tests/unit/modules/darwin_sysctl_test.py
@@ -12,7 +12,7 @@ from salt.exceptions import CommandExecutionError
 # Import Salt Testing Libs
 from salttesting import TestCase
 from salttesting.helpers import ensure_in_syspath
-from salttesting.mock import MagicMock, mock_open, patch
+from salttesting.mock import MagicMock, mock_open, patch, call
 
 ensure_in_syspath('../../')
 
@@ -61,6 +61,19 @@ class DarwinSysctlTestCase(TestCase):
                 'net.inet.icmp.icmplim', 50), ret)
 
     @patch('os.path.isfile', MagicMock(return_value=False))
+    def test_persist_no_conf_failure(self):
+        '''
+        Tests adding of config file failure
+        '''
+        mock = mock_open()
+        with patch('salt.utils.fopen', mock_open(read_data=mock)) as m_open:
+            helper_open = m_open()
+            helper_open.write.assertRaises(CommandExecutionError,
+                                           darwin_sysctl.persist,
+                                           'net.inet.icmp.icmplim',
+                                           50, config=None)
+
+    @patch('os.path.isfile', MagicMock(return_value=False))
     def test_persist_no_conf_success(self):
         '''
         Tests successful add of config file when previously not one
@@ -71,6 +84,19 @@ class DarwinSysctlTestCase(TestCase):
             helper_open = m_open()
             helper_open.write.assert_called_once_with(
                 '#\n# Kernel sysctl configuration\n#\n')
+
+    @patch('os.path.isfile', MagicMock(return_value=True))
+    def test_persist_success(self):
+        '''
+        Tests successful write to existing sysctl file
+        '''
+        to_write = '#\n# Kernel sysctl configuration\n#\n'
+        m_calls_list = [call.writelines(['net.inet.icmp.icmplim=50'])]
+        with patch('salt.utils.fopen', mock_open(read_data=to_write)) as m_open:
+            darwin_sysctl.persist('net.inet.icmp.icmplim', 50, config=to_write)
+            helper_open = m_open()
+            calls_list = helper_open.method_calls
+            self.assertEqual(calls_list, m_calls_list)
 
 
 if __name__ == '__main__':
