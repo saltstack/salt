3835d30096a92c28aa19ae18114d6a4262748742
K Jonathan Harker <k.jonathan.harker@hp.com>
2014-03-25 20:08:48 +0000

    GitPython may raise a bad AssertionError
    GitPython may raise an AssertionError during a successful fetch due to
    poorly grepping the fetch output for success. If this is the case, a
    second fetch will be a no-op. If the second fetch also raises the error,
    then the error is legit.

--- salt/fileserver/gitfs.py
+++ salt/fileserver/gitfs.py
@@ -736,8 +736,14 @@ def update():
         with salt.utils.fopen(lk_fn, 'w+') as fp_:
             fp_.write(str(pid))
         try:
+            log.debug("Fetching from {0}".format(origin.url))
             if provider == 'gitpython':
-                for fetch in origin.fetch():
+                _f = []
+                try:
+                    _f = origin.fetch()
+                except AssertionError:
+                    _f = origin.fetch()
+                for fetch in _f:
                     if fetch.old_commit is not None:
                         data['changed'] = True
             elif provider == 'pygit2':
