597f19508faa957ef62eab7af2b1fcff8b9f46d1
Thomas S Hatch <thatch45@gmail.com>
2014-03-31 18:52:30 -0600

    lint

--- salt/modules/win_file.py
+++ salt/modules/win_file.py
@@ -21,11 +21,11 @@ import contextlib  # do not remove, used in imported file.py functions
 import difflib  # do not remove, used in imported file.py functions
 import errno  # do not remove, used in imported file.py functions
 import shutil  # do not remove, used in imported file.py functions
-import re # do not remove, used in imported file.py functions
-import sys # do not remove, used in imported file.py functions
-import fileinput # do not remove, used in imported file.py functions
-import salt.utils.atomicfile # do not remove, used in imported file.py functions
-import salt._compat # do not remove, used in imported file.py functions
+import re  # do not remove, used in imported file.py functions
+import sys  # do not remove, used in imported file.py functions
+import fileinput  # do not remove, used in imported file.py functions
+import salt.utils.atomicfile  # do not remove, used in imported file.py functions
+import salt._compat  # do not remove, used in imported file.py functions
 from salt.exceptions import CommandExecutionError, SaltInvocationError
 # pylint: enable=W0611
 
