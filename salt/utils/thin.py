26c6db974aad589c12fbc31481060f1994a3c620
Thomas S Hatch <thatch45@gmail.com>
2014-04-02 10:20:53 -0600

    Add requests to salt-thin

--- salt/utils/thin.py
+++ salt/utils/thin.py
@@ -11,6 +11,7 @@ import tarfile
 import jinja2
 import yaml
 import msgpack
+import requests
 try:
     import markupsafe
     HAS_MARKUPSAFE = True
@@ -62,6 +63,7 @@ def gen_thin(cachedir, extra_mods='', overwrite=False):
             os.path.dirname(jinja2.__file__),
             os.path.dirname(yaml.__file__),
             os.path.dirname(msgpack.__file__),
+            os.path.dirname(requests.__file__),
             ]
     for mod in [m for m in extra_mods.split(',') if m]:
         if mod not in locals() and mod not in globals():
