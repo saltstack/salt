a1406183de1803240c5f79547ab28eb94deacaf8
Erik Johnson <palehose@gmail.com>
2014-04-03 14:40:56 -0500

    Add versionchanged directive

--- salt/modules/file.py
+++ salt/modules/file.py
@@ -2179,6 +2179,9 @@ def check_perms(name, ret, user, group, mode, follow_symlinks=False):
     .. code-block:: bash
 
         salt '*' file.check_perms /etc/sudoers '{}' root root 400
+
+    .. versionchanged:: 2014.1.2
+        ``follow_symlinks`` option added
     '''
     if not ret:
         ret = {'name': name,
