4b4ffdcd5dbe0083a2f01d7d8fa163d2db2b0c65
Colton Myers <colton.myers@gmail.com>
2014-04-04 13:18:29 -0600

    Pylint fixes for state.py

--- salt/state.py
+++ salt/state.py
@@ -35,7 +35,7 @@ from salt.utils import context
 from salt._compat import string_types
 from salt.template import compile_template, compile_template_str
 from salt.exceptions import SaltRenderError, SaltReqTimeoutError, SaltException
-from salt.utils.odict import  OrderedDict, DefaultOrderedDict
+from salt.utils.odict import OrderedDict, DefaultOrderedDict
 
 log = logging.getLogger(__name__)
 
@@ -1951,7 +1951,7 @@ class BaseHighState(object):
         '''
         Cleanly merge the top files
         '''
-        top = DefaultOrderedDict(OrderedDict) 
+        top = DefaultOrderedDict(OrderedDict)
         for ctops in tops.values():
             for ctop in ctops:
                 for saltenv, targets in ctop.items():
