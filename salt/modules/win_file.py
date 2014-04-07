7f2ca809359a4983cec0210881752aadd6d6df6f
Samuel Lai <sam@edgylogic.com>
2014-04-07 13:51:15 +1000

    Grrr, used the wrong format for log strings.

--- salt/modules/win_file.py
+++ salt/modules/win_file.py
@@ -183,7 +183,7 @@ def gid_to_group(gid):
     '''
     func_name = '{}.gid_to_group'.format(__virtualname__)
     if __opts__.get('fun', '') == func_name:
-        log.info('The function {} should not be used on Windows systems; see function docs for details.', func_name)
+        log.info('The function %s should not be used on Windows systems; see function docs for details.', func_name)
 
     return uid_to_user(gid)
 
@@ -208,7 +208,7 @@ def group_to_gid(group):
     '''
     func_name = '{}.group_to_gid'.format(__virtualname__)
     if __opts__.get('fun', '') == func_name:
-        log.info('The function {} should not be used on Windows systems; see function docs for details.', func_name)
+        log.info('The function %s should not be used on Windows systems; see function docs for details.', func_name)
 
     return user_to_uid(group)
 
@@ -295,7 +295,7 @@ def get_gid(path, follow_symlinks=True):
     '''
     func_name = '{}.get_gid'.format(__virtualname__)
     if __opts__.get('fun', '') == func_name:
-        log.info('The function {} should not be used on Windows systems; see function docs for details.', func_name)
+        log.info('The function %s should not be used on Windows systems; see function docs for details.', func_name)
 
     return get_uid(path, follow_symlinks)
 
@@ -327,7 +327,7 @@ def get_group(path, follow_symlinks=True):
     '''
     func_name = '{}.get_group'.format(__virtualname__)
     if __opts__.get('fun', '') == func_name:
-        log.info('The function {} should not be used on Windows systems; see function docs for details.', func_name)
+        log.info('The function %s should not be used on Windows systems; see function docs for details.', func_name)
 
     return get_user(path, follow_symlinks)
 
@@ -466,8 +466,8 @@ def lchown(path, user, group=None, pgroup=None):
     if group:
         func_name = '{}.lchown'.format(__virtualname__)
         if __opts__.get('fun', '') == func_name:
-            log.info('The group parameter has no effect when using {} on Windows systems; see function docs for details.', func_name)
-        log.debug('win_file.py {} Ignoring the group parameter for {}', func_name, path)
+            log.info('The group parameter has no effect when using %s on Windows systems; see function docs for details.', func_name)
+        log.debug('win_file.py %s Ignoring the group parameter for %s', func_name, path)
         group = None
 
     return chown(path, user, group, pgroup, follow_symlinks=False)
@@ -498,8 +498,8 @@ def chown(path, user, group=None, pgroup=None, follow_symlinks=True):
     if group:
         func_name = '{}.chown'.format(__virtualname__)
         if __opts__.get('fun', '') == func_name:
-            log.info('The group parameter has no effect when using {} on Windows systems; see function docs for details.', func_name)
-        log.debug('win_file.py {} Ignoring the group parameter for {}', func_name, path)
+            log.info('The group parameter has no effect when using %s on Windows systems; see function docs for details.', func_name)
+        log.debug('win_file.py %s Ignoring the group parameter for %s', func_name, path)
         group = None
 
     err = ''
@@ -614,8 +614,8 @@ def chgrp(path, group):
     '''
     func_name = '{}.chgrp'.format(__virtualname__)
     if __opts__.get('fun', '') == func_name:
-        log.info('The function {} should not be used on Windows systems; see function docs for details.', func_name)
-    log.debug('win_file.py {} Doing nothing for {}', func_name, path)
+        log.info('The function %s should not be used on Windows systems; see function docs for details.', func_name)
+    log.debug('win_file.py %s Doing nothing for %s', func_name, path)
 
     return None
 
