bcb66f160f384cc06c49e1b79aa80093c2cbe310
Seth House <seth@eseth.com>
2014-04-15 18:01:31 -0600

    Add Sphinx extension to generate an index for short-URLs

---
+++ doc/_ext/shorturls.py
@@ -0,0 +1,22 @@
+'''
+Short-URL redirects
+'''
+import json
+import os
+
+import sphinx.ext.intersphinx
+
+DOCS_URL = 'http://docs.saltstack.com/en/latest/'
+
+def write_urls_index(app, exc):
+    '''
+    Generate a JSON file to serve as an index for short-URL lookups
+    '''
+    inventory = os.path.join(app.builder.outdir, 'objects.inv')
+    objects = sphinx.ext.intersphinx.fetch_inventory(app, DOCS_URL, inventory)
+
+    with open(os.path.join(app.builder.outdir, 'shorturls.json'), 'wb') as f:
+        json.dump(objects, f)
+
+def setup(app):
+    app.connect('build-finished', write_urls_index)
