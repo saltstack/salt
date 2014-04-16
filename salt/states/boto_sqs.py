7947ad9a57f78b90447f84fe7e6b188384c52df7
Ryan Lane <rlane@lyft.com>
2014-04-16 15:43:40 -0700

    Initial commit of boto_sqs state and module
    This state and module manage SQS queues via boto, rather than the
    aws commands. Since it's using boto, it supports IAM instance profile
    credentials natively. The module and state also support the ability
    to accept key/keyid credentials as parameters, through pillars or
    through minion configuration.
    
    In addition, the state and module supports managing queue attributes.
    In the state this is added as an argument. In the module it's a separate
    function. The module also has support for fetching the attributes for a
    queue.

---
+++ salt/states/boto_sqs.py
@@ -0,0 +1,156 @@
+# -*- coding: utf-8 -*-
+'''
+Manage SQS Queues
+=================
+
+Create and destroy SQS queues. Be aware that this interacts with Amazon's
+services, and so may incur charges.
+
+This module uses boto, which can be installed via package, or pip.
+
+This module accepts explicit sqs credentials but can also utilize
+IAM roles assigned to the instance trough Instance Profiles. Dynamic
+credentials are then automatically obtained from AWS API and no further
+configuration is necessary. More Information available at::
+
+   http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/iam-roles-for-amazon-ec2.html
+
+If IAM roles are not used you need to specify them either in a pillar or
+in the minion's config file::
+
+    sqs.keyid: GKTADJGHEIQSXMKKRBJ08H
+    sqs.key: askdjghsdfjkghWupUjasdflkdfklgjsdfjajkghs
+
+.. code-block:: yaml
+
+    myqueue:
+        aws_sqs.exists:
+            - region: us-east-1
+            - key: GKTADJGHEIQSXMKKRBJ08H
+            - keyid: askdjghsdfjkghWupUjasdflkdfklgjsdfjajkghs
+            - attributes:
+                ReceiveMessageWaitTimeSeconds: 20
+'''
+
+
+def __virtual__():
+    '''
+    Only load if aws is available.
+    '''
+    return 'boto_sqs' if 'boto_sqs.exists' in __salt__ else False
+
+
+def created(
+        name,
+        attributes=None,
+        region=None,
+        key=None,
+        keyid=None):
+    '''
+    Ensure the SQS queue exists.
+
+    name
+        Name of the SQS queue.
+
+    region
+        Region to create the queue
+
+    key
+        Secret key to be used.
+
+    keyid
+        Access key to be used.
+    '''
+    ret = {'name': name, 'result': True, 'comment': '', 'changes': {}}
+
+    is_created = __salt__['boto_sqs.exists'](name, region, key, keyid)
+
+    if not is_created:
+        ret['comment'] = 'AWS SQS queue {0} is set to be created.'.format(name)
+        if __opts__['test']:
+            ret['result'] = None
+            return ret
+        created = __salt__['boto_sqs.create'](name, region, key, keyid)
+        if created:
+            ret['changes']['old'] = None
+            ret['changes']['new'] = {'queue': name}
+        else:
+            ret['result'] = False
+            ret['comment'] = 'Failed to create {0} AWS queue'.format(name)
+            return ret
+    else:
+        ret['comment'] = '{0} present.'.format(name)
+    attrs_to_set = {}
+    _attributes = __salt__['boto_sqs.get_attributes'](name, region, key, keyid)
+    for attr, val in attributes.iteritems():
+        _val = _attributes.get(attr, None)
+        if str(_val) != str(val):
+            attrs_to_set[attr] = val
+    if attrs_to_set:
+        if __opts__['test']:
+            ret['result'] = None
+            ret['comment'] = 'Attributes {0} to be set on {1}'.format(
+                attrs_to_set.keys(), name)
+            return ret
+        attr_names = ','.join(attrs_to_set.keys())
+        msg = (' Setting {0} attributes.'.format(attr_names))
+        ret['comment'] = ret['comment'] + msg
+        ret['result'] = True
+        if 'new' in ret['changes']:
+            ret['changes']['new']['attributes_set'] = []
+        else:
+            ret['changes']['new'] = {'attributes_set': []}
+        for attr, val in attrs_to_set.iteritems():
+            set_attr = __salt__['boto_sqs.set_attributes'](name, {attr: val},
+                                                           region, key, keyid)
+            if not set_attr:
+                ret['result'] = False
+            msg = 'Set attribute {0}.'.format(attr)
+            ret['changes']['new']['attributes_set'].append(attr)
+    else:
+        ret['comment'] = ret['comment'] + ' Attributes set.'
+    return ret
+
+
+def deleted(
+        name,
+        region=None,
+        key=None,
+        keyid=None):
+    '''
+    Delete the named SQS queue if it exists.
+
+    name
+        Name of the SQS queue.
+
+    region
+        Region to remove the queue from
+
+    key
+        Secret key to be used.
+
+    keyid
+        Access key to be used.
+    '''
+    ret = {'name': name, 'result': None, 'comment': '', 'changes': {}}
+
+    is_created = __salt__['boto_sqs.exists'](name, region, key, keyid)
+
+    if is_created:
+        if __opts__['test']:
+            ret['result'] = None
+            ret['comment'] = 'AWS SQS queue {0} is set to be removed'.format(
+                    name)
+            return ret
+        deleted = __salt__['boto_sqs.delete'](name, region, key, keyid)
+        if deleted:
+            ret['result'] = True
+            ret['changes']['old'] = name
+            ret['changes']['new'] = None
+        else:
+            ret['result'] = False
+            ret['comment'] = 'Failed to remove {0} sqs queue.'.format(name)
+    else:
+        ret['comment'] = u'{0} does not exist in {1}'.format(name, region)
+
+    return ret
