d495231d7d7f3813171af2b24f60ddc986c0990d
David Boucha <boucha@gmail.com>
2014-04-17 17:04:28 -0600

    pylint wanted whitespace after the trailing comma

--- salt/modules/aws_sqs.py
+++ salt/modules/aws_sqs.py
@@ -95,7 +95,9 @@ def receive_message(queue, region, num=1, opts=None, user=None):
 
     out = _run_aws('receive-message', region, opts, user, queue=url_map[queue],
                    num=num)
-    ret = {'Messages': out['Messages'],}
+    ret = {
+            'Messages': out['Messages'],
+          }
     return ret
 
 
