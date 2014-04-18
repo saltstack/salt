205a3ac9c4ca40e46fb8ac41e4217cfd18ddadfe
Mike Place <mp@saltstack.com>
2014-04-18 11:25:15 -0600

    Grrrr

--- tests/unit/modules/zcbuildout_test.py
+++ tests/unit/modules/zcbuildout_test.py
@@ -297,7 +297,7 @@ class BuildoutTestCase(Base):
 
 @skipIf(salt.utils.which_bin(KNOWN_VIRTUALENV_BINARY_NAMES) is None,
         'The \'virtualenv\' packaged needs to be installed')
-@skipIf(True)  # These are not running reliably and have been disabled.
+@skipIf(True, 'These tests are not running reliably')
 class BuildoutOnlineTestCase(Base):
 
     @classmethod
