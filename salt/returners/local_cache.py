09814d42124305fa12f29f99dcd0b1374471fb93
Thomas Jackson <jacksontj.89@gmail.com>
2014-04-18 14:55:57 -0700

    Move the job cleanup into the returner

--- salt/returners/local_cache.py
+++ salt/returners/local_cache.py
@@ -216,3 +216,45 @@ def get_jids():
         ret[jid] = _format_jid_instance(jid, job)
     return ret
 
+def clean_old_jobs():
+    '''
+    Clean out the old jobs from the job cache
+    '''
+    if __opts__['keep_jobs'] != 0:
+        jid_root = os.path.join(__opts__['cachedir'], 'jobs')
+        cur = datetime.datetime.now()
+
+        if os.path.exists(jid_root):
+            for top in os.listdir(jid_root):
+                t_path = os.path.join(jid_root, top)
+                for final in os.listdir(t_path):
+                    f_path = os.path.join(t_path, final)
+                    jid_file = os.path.join(f_path, 'jid')
+                    if not os.path.isfile(jid_file):
+                        # No jid file means corrupted cache entry, scrub it
+                        shutil.rmtree(f_path)
+                    else:
+                        with salt.utils.fopen(jid_file, 'r') as fn_:
+                            jid = fn_.read()
+                        if len(jid) < 18:
+                            # Invalid jid, scrub the dir
+                            shutil.rmtree(f_path)
+                        else:
+                            # Parse the jid into a proper datetime object.
+                            # We only parse down to the minute, since keep
+                            # jobs is measured in hours, so a minute
+                            # difference is not important.
+                            try:
+                                jidtime = datetime.datetime(int(jid[0:4]),
+                                                            int(jid[4:6]),
+                                                            int(jid[6:8]),
+                                                            int(jid[8:10]),
+                                                            int(jid[10:12]))
+                            except ValueError as e:
+                                # Invalid jid, scrub the dir
+                                shutil.rmtree(f_path)
+                            difference = cur - jidtime
+                            hours_difference = difference.seconds / 3600.0
+                            if hours_difference > __opts__['keep_jobs']:
+                                shutil.rmtree(f_path)
+
