38099ec15601f057d13bdf8e5e95374b13acdb9b
Erik Johnson <palehose@gmail.com>
2014-04-25 17:02:41 -0500

    2014.1.2 -> 2014.1.3
    2014.1.2 was not officially released, fix these RST directives.

--- salt/states/file.py
+++ salt/states/file.py
@@ -3047,7 +3047,7 @@ def serialize(name,
     makedirs
         Create parent directories for destination file.
 
-        .. versionadded:: 2014.1.2
+        .. versionadded:: 2014.1.3
 
     show_diff
         If set to False, the diff will not be shown.
