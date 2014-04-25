7fbb5fcb787ac7a1866ea114902107157ca8cdc1
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2014-04-25 17:22:48 +0100

    add tests for keyring utils

--- tests/unit/utils/cloud_test.py
+++ tests/unit/utils/cloud_test.py
@@ -19,6 +19,8 @@ ensure_in_syspath('../../')
 # Import salt libs
 from salt.utils import cloud
 
+# Import external deps
+import keyring
 
 class CloudUtilsTestCase(TestCase):
 
@@ -40,6 +42,35 @@ class CloudUtilsTestCase(TestCase):
                 cloud.SSH_PASSWORD_PROMP_RE.match(pattern.lower().strip()), None
             )
 
+    def test__save_password_in_keyring(self):
+        '''
+        Test storing password in the keyring
+        '''
+        cloud._save_password_in_keyring(
+            'salt.cloud.provider.test_case_provider',
+            'fake_username',
+            'fake_password_c8231'
+        )
+        stored_pw = keyring.get_password(
+                'salt.cloud.provider.test_case_provider',
+                'fake_username',
+        )
+        keyring.delete_password(
+                    'salt.cloud.provider.test_case_provider',
+                    'fake_username',
+        )
+        self.assertEqual(stored_pw, 'fake_password_c8231')
+
+    def test_retrieve_password_from_keyring(self):
+        keyring.set_password(
+                'salt.cloud.provider.test_case_provider',
+                'fake_username',
+                'fake_password_c8231'
+        )
+        pw_in_keyring = cloud.retrieve_password_from_keyring(
+            'salt.cloud.provider.test_case_provider',
+            'fake_username')
+        self.assertEqual(pw_in_keyring, 'fake_password_c8231')
 
 if __name__ == '__main__':
     from integration import run_tests
