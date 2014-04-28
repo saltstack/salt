7854c158dc31dfbae794790695f1cf3c7efb629b
Thayne Harbaugh <thayne.l.h@gmail.com>
2014-04-28 17:35:04 -0600

    Catalog Salt exit values.  Ensure that there is consistency and that exit values are not duplicated with conflicting values.
    NOTE: This needs much more work to locate all of the occurences of
    os.exit() throughout the codebase.

---
+++ salt/exitcodes.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+'''
+Classification of Salt exit codes.  These are intended to augment
+universal exit codes (found in Python's `os` module with the `EX_`
+prefix or in `sysexits.h`).
+'''
+
+# Too many situations use "exit 1" - try not to use it when something
+# else is more appropriate.
+EX_GENERIC     = 1
+
+EX_DEPLOY_THIN = 3
+EX_PYTHON_OLD  = 4
+EX_CHECKSUM    = 5
+
