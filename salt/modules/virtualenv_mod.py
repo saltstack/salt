a44c13578a499f909b5692c641615f432ca17653
David Lindquist <david.lindquist@hulu.com>
2014-04-29 12:52:38 -0700

    Fix url for git-pip.py
    Per
    https://developer.github.com/changes/2014-04-25-user-content-security/
    raw.github.com is now served from raw.githubusercontent.com.

--- salt/modules/virtualenv_mod.py
+++ salt/modules/virtualenv_mod.py
@@ -304,7 +304,7 @@ def create(path,
     # Install pip
     if pip and not os.path.exists(venv_pip):
         _ret = _install_script(
-            'https://raw.github.com/pypa/pip/master/contrib/get-pip.py',
+            'https://raw.githubusercontent.com/pypa/pip/master/contrib/get-pip.py',
             path, venv_python, user, saltenv=saltenv
         )
         # Let's update the return dictionary with the details from the pip
