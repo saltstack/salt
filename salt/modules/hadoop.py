71f0bd987f1f3cec7b3888924c0c952d867429cc
Pedro Algarvio <pedro@algarvio.me>
2014-04-30 12:53:09 +0100

    Replace string substitution with string formatting

--- salt/modules/hadoop.py
+++ salt/modules/hadoop.py
@@ -53,7 +53,7 @@ def _hadoop_cmd(module, command, *args):
     out = None
     if module and command:
         if module in __authorized_modules__:
-            cmd = 'hadoop %s -%s %s' % (module, command, ' '.join(args))
+            cmd = 'hadoop {0} -{1} {2}'.format(module, command, ' '.join(args))
             out = __salt__['cmd.run'](cmd)
         else:
             return 'Error: Unknown module'
