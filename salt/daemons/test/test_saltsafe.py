50cc7a00a4390c64d0b2e7d7d738a21c0299c9a5
Samuel M Smith <smith.samuel.m@gmail.com>
2014-05-05 17:14:23 -0600

    updated test to bootstrap with SaltSafe

--- salt/daemons/test/test_saltsafe.py
+++ salt/daemons/test/test_saltsafe.py
@@ -35,246 +35,6 @@ def setUpModule():
 def tearDownModule():
     pass
 
-
-def test():
-    '''
-    Test keeping.
-    '''
-    pkiDirpath = os.path.join('/tmp', 'raet', 'keyo', 'master', 'pki')
-    if not os.path.exists(pkiDirpath):
-            os.makedirs(pkiDirpath)
-
-    acceptedDirpath = os.path.join(pkiDirpath, 'accepted')
-    if not os.path.exists(acceptedDirpath):
-        os.makedirs(acceptedDirpath)
-
-    pendingDirpath = os.path.join(pkiDirpath, 'pending')
-    if not os.path.exists(pendingDirpath):
-        os.makedirs(pendingDirpath)
-
-    rejectedDirpath = os.path.join(pkiDirpath, 'rejected')
-    if not os.path.exists(rejectedDirpath):
-        os.makedirs(rejectedDirpath)
-
-    localFilepath = os.path.join(pkiDirpath, 'local.key')
-    if os.path.exists(localFilepath):
-        mode = os.stat(localFilepath).st_mode
-        print mode
-        os.chmod(localFilepath, mode | stat.S_IWUSR | stat.S_IRUSR)
-        mode = os.stat(localFilepath).st_mode
-        print mode
-
-    cacheDirpath = os.path.join('/tmp/raet', 'cache', 'master')
-    if not os.path.exists(cacheDirpath):
-        os.makedirs(cacheDirpath)
-
-    sockDirpath = os.path.join('/tmp/raet', 'sock', 'master')
-    if not os.path.exists(sockDirpath):
-            os.makedirs(sockDirpath)
-
-    opts = dict(
-                pki_dir=pkiDirpath,
-                sock_dir=sockDirpath,
-                cachedir=cacheDirpath,
-                open_mode=True,
-                auto_accept=True,
-                transport='raet',
-                )
-
-    masterSafe = salting.SaltSafe(opts=opts)
-    print("masterSafe local =\n{0}".format(masterSafe.loadLocalData()))
-    print("masterSafe remote =\n{0}".format(masterSafe.loadAllRemoteData()))
-
-    pkiDirpath = os.path.join('/tmp', 'raet', 'keyo', 'minion', 'pki')
-    if not os.path.exists(pkiDirpath):
-            os.makedirs(pkiDirpath)
-
-    acceptedDirpath = os.path.join(pkiDirpath, 'accepted')
-    if not os.path.exists(acceptedDirpath):
-        os.makedirs(acceptedDirpath)
-
-    pendingDirpath = os.path.join(pkiDirpath, 'pending')
-    if not os.path.exists(pendingDirpath):
-        os.makedirs(pendingDirpath)
-
-    rejectedDirpath = os.path.join(pkiDirpath, 'rejected')
-    if not os.path.exists(rejectedDirpath):
-        os.makedirs(rejectedDirpath)
-
-    localFilepath = os.path.join(pkiDirpath, 'local.key')
-    if os.path.exists(localFilepath):
-        mode = os.stat(localFilepath).st_mode
-        os.chmod(localFilepath, mode | stat.S_IWUSR | stat.S_IRUSR)
-
-
-
-    cacheDirpath = os.path.join('/tmp/raet', 'cache', 'minion')
-    sockDirpath = os.path.join('/tmp/raet', 'sock', 'minion')
-
-    opts = dict(
-                pki_dir=pkiDirpath,
-                sock_dir=sockDirpath,
-                cachedir=cacheDirpath,
-                open_mode=True,
-                auto_accept=True,
-                transport='raet',
-                )
-
-    minionSafe = salting.SaltSafe(opts=opts)
-    print("minionSafe local =\n{0}".format(minionSafe.loadLocalData()))
-    print("minionSafe remote =\n{0}".format(minionSafe.loadAllRemoteData()))
-
-    masterName = 'master'
-    masterDirpath = os.path.join('/tmp', 'raet', 'keep', masterName )
-    signer = nacling.Signer()
-    masterSignKeyHex = signer.keyhex
-    masterVerKeyHex = signer.verhex
-    privateer = nacling.Privateer()
-    masterPriKeyHex = privateer.keyhex
-    masterPubKeyHex = privateer.pubhex
-
-    m1Name = 'minion1'
-    m1Dirpath = os.path.join('/tmp', 'raet', 'keep', m1Name)
-    signer = nacling.Signer()
-    m1SignKeyHex = signer.keyhex
-    m1VerKeyHex = signer.verhex
-    privateer = nacling.Privateer()
-    m1PriKeyHex = privateer.keyhex
-    m1PubKeyHex = privateer.pubhex
-
-    m2Name = 'minion2'
-    signer = nacling.Signer()
-    m2SignKeyHex = signer.keyhex
-    m2VerKeyHex = signer.verhex
-    privateer = nacling.Privateer()
-    m2PriKeyHex = privateer.keyhex
-    m2PubKeyHex = privateer.pubhex
-
-    m3Name = 'minion3'
-    signer = nacling.Signer()
-    m3SignKeyHex = signer.keyhex
-    m3VerKeyHex = signer.verhex
-    privateer = nacling.Privateer()
-    m3PriKeyHex = privateer.keyhex
-    m3PubKeyHex = privateer.pubhex
-
-    keeping.clearAllKeepSafe(masterDirpath)
-    keeping.clearAllKeepSafe(m1Dirpath)
-
-    #saltsafe = salting.SaltSafe(opts=opts)
-    #print saltsafe.loadLocalData()
-    #print saltsafe.loadAllRemoteData()
-
-    #master stack
-    local = estating.LocalEstate(  eid=1,
-                                    name=masterName,
-                                    sigkey=masterSignKeyHex,
-                                    prikey=masterPriKeyHex,)
-    stack0 = stacking.RoadStack(local=local,
-                               dirpath=masterDirpath,
-                               safe=masterSafe)
-
-    stack0.addRemote(estating.RemoteEstate(eid=2,
-                                    name=m1Name,
-                                    ha=('127.0.0.1', 7532),
-                                    verkey=m1VerKeyHex,
-                                    pubkey=m1PubKeyHex,))
-
-    stack0.addRemote(estating.RemoteEstate(eid=3,
-                                    name=m2Name,
-                                    ha=('127.0.0.1', 7533),
-                                    verkey=m2VerKeyHex,
-                                    pubkey=m2PubKeyHex,))
-
-    #minion stack
-    local = estating.LocalEstate(   eid=2,
-                                     name=m1Name,
-                                     ha=("", raeting.RAET_TEST_PORT),
-                                     sigkey=m1SignKeyHex,
-                                     prikey=m1PriKeyHex,)
-    stack1 = stacking.RoadStack(local=local,
-                               dirpath=m1Dirpath,
-                               safe=minionSafe)
-
-
-    stack1.addRemote(estating.RemoteEstate(eid=1,
-                                    name=masterName,
-                                    ha=('127.0.0.1', 7532),
-                                    verkey=masterVerKeyHex,
-                                    pubkey=masterPubKeyHex,))
-
-    stack1.addRemote(estating.RemoteEstate(eid=4,
-                                    name=m3Name,
-                                    ha=('127.0.0.1', 7534),
-                                    verkey=m3VerKeyHex,
-                                    pubkey=m3PubKeyHex,))
-
-    #stack0.clearLocal()
-    stack0.clearRemoteKeeps()
-    #stack1.clearLocal()
-    stack1.clearRemoteKeeps()
-
-    stack0.dumpLocal()
-    stack0.dumpRemotes()
-
-    stack1.dumpLocal()
-    stack1.dumpRemotes()
-
-    print "Road {0}".format(stack0.name)
-    print stack0.keep.loadLocalData()
-    print stack0.keep.loadAllRemoteData()
-    print "Safe {0}".format(stack0.name)
-    print stack0.safe.loadLocalData()
-    print stack0.safe.loadAllRemoteData()
-    print
-
-    print "Road {0}".format(stack1.name)
-    print stack1.keep.loadLocalData()
-    print stack1.keep.loadAllRemoteData()
-    print "Safe {0}".format(stack1.name)
-    print stack1.safe.loadLocalData()
-    print stack1.safe.loadAllRemoteData()
-
-    stack0.server.close()
-    stack1.server.close()
-
-    #master stack
-    dirpath = os.path.join('/tmp/raet', 'keep', 'master')
-    local = estating.LocalEstate(  eid=1,
-                                    name='master',
-                                    sigkey=masterSignKeyHex,
-                                    prikey=masterPriKeyHex,)
-    stack0 = stacking.RoadStack(local=local,
-                               dirpath=masterDirpath,
-                               safe=masterSafe)
-
-    #minion stack
-    dirpath = os.path.join('/tmp/raet', 'keep', 'minion1')
-    local = estating.LocalEstate(   eid=2,
-                                     name='minion1',
-                                     ha=("", raeting.RAET_TEST_PORT),
-                                     sigkey=m1SignKeyHex,
-                                     prikey=m1PriKeyHex,)
-    stack1 = stacking.RoadStack(local=local,
-                               dirpath=m1Dirpath,
-                               safe=minionSafe)
-
-
-    stack0.loadLocal()
-    print stack0.local.name, stack0.local.eid, stack0.local.sid, stack0.local.ha, stack0.local.signer, stack0.local.priver
-    stack1.loadLocal()
-    print stack1.local.name, stack1.local.eid, stack1.local.sid, stack1.local.ha, stack1.local.signer, stack1.local.priver
-
-    stack0.clearLocal()
-    stack0.clearRemoteKeeps()
-    stack1.clearLocal()
-    stack1.clearRemoteKeeps()
-
-
-#if __name__ == "__main__":
-    #test()
-
-
 class BasicTestCase(unittest.TestCase):
     """"""
 
@@ -302,7 +62,7 @@ class BasicTestCase(unittest.TestCase):
         if os.path.exists(self.baseDirpath):
             shutil.rmtree(self.baseDirpath)
 
-    def createOpts(self, dirpath, openMode=True, autoAccept=True):
+    def createOpts(self, dirpath, openMode=False, autoAccept=True):
         '''
         Create associated pki directories for stack and return opts
         '''
@@ -525,9 +285,10 @@ class BasicTestCase(unittest.TestCase):
 
         self.assertEqual(len(main.remotes.values()), 2)
 
-
         self.assertEqual(self.otherSafe.loadLocalData(), None)
         self.assertEqual(self.otherSafe.loadAllRemoteData(), {})
+
+        # other stack
 
         dataOther = self.createRoadData(name='other', base=self.baseDirpath)
         other = self.createRoadStack(data=dataOther,
@@ -561,18 +322,105 @@ class BasicTestCase(unittest.TestCase):
 
         other.dumpRemotes()
         self.assertDictEqual(other.safe.loadAllRemoteData(),
-            {3:
+            {'3':
                 {'eid': 3,
                  'name': data3['name'],
                  'acceptance': 1,
                  'verhex': data3['verhex'],
                  'pubhex': data3['pubhex']},
-            4:
+            '4':
                 {'eid': 4,
                  'name': data4['name'],
                  'acceptance': 1,
                  'verhex': data4['verhex'],
                  'pubhex': data4['pubhex']}})
+        other.server.close()
+
+        self.assertDictEqual(other.keep.loadAllRemoteData(),
+            {'3':
+                {'eid': 3,
+                 'name': 'remote3',
+                 'host': '127.0.0.1',
+                 'port': 7534,
+                 'sid': 0,
+                 'rsid': 0},
+            '4':
+                {'eid': 4,
+                 'name': 'remote4',
+                 'host': '127.0.0.1',
+                 'port': 7535,
+                 'sid': 0,
+                 'rsid': 0}})
+
+        main.server.close()
+        other.server.close()
+
+    def testBootstrapClean(self):
+        '''
+        Basic keep setup for stack keep and safe persistence load and dump
+        '''
+        console.terse("{0}\n".format(self.testBasic.__doc__))
+
+        self.assertEqual(self.mainSafe.loadLocalData(), None)
+        self.assertEqual(self.mainSafe.loadAllRemoteData(), {})
+
+        dataMain = self.createRoadData(name='main', base=self.baseDirpath)
+        main = self.createRoadStack(data=dataMain,
+                                     eid=1,
+                                     main=True,
+                                     ha=None, #default ha is ("", raeting.RAET_PORT)
+                                     safe=self.mainSafe)
+
+        self.assertTrue(main.keep.dirpath.endswith('road/keep/main'))
+        self.assertTrue(main.safe.dirpath.endswith('pki'))
+        self.assertTrue(main.local.ha, ("0.0.0.0", raeting.RAET_PORT))
+        self.assertTrue(main.safe.auto)
+        self.assertDictEqual(main.keep.loadLocalData(), {'eid': 1,
+                                                         'name': 'main',
+                                                         'main': True,
+                                                         'host': '0.0.0.0',
+                                                         'port': 7530,
+                                                         'sid': 0})
+        self.assertDictEqual(main.safe.loadLocalData(), {'prihex': dataMain['prihex'],
+                                                     'sighex': dataMain['sighex']})
+
+        self.assertEqual(self.otherSafe.loadLocalData(), None)
+        self.assertEqual(self.otherSafe.loadAllRemoteData(), {})
+
+        dataOther = self.createRoadData(name='other', base=self.baseDirpath)
+        other = self.createRoadStack(data=dataOther,
+                                     eid=0,
+                                     main=None,
+                                     ha=("", raeting.RAET_TEST_PORT),
+                                     safe=self.otherSafe)
+
+        console.terse("{0} keep dirpath = {1} safe dirpath = {2}\n".format(
+                other.name, other.keep.dirpath, other.safe.dirpath))
+        self.assertTrue(other.keep.dirpath.endswith('road/keep/other'))
+        self.assertTrue(other.safe.dirpath.endswith('pki'))
+        self.assertEqual(other.local.ha, ("0.0.0.0", raeting.RAET_TEST_PORT))
+        self.assertTrue(other.safe.auto)
+
+        self.assertDictEqual(other.safe.loadLocalData(), {'prihex': dataOther['prihex'],
+                                                        'sighex': dataOther['sighex']})
+
+        self.join(other, main)
+        self.assertEqual(len(main.transactions), 0)
+        remote = main.remotes.values()[0]
+        self.assertTrue(remote.joined)
+        self.assertEqual(len(other.transactions), 0)
+        remote = other.remotes.values()[0]
+        self.assertTrue(remote.joined)
+
+        self.allow(other, main)
+        self.assertEqual(len(main.transactions), 0)
+        remote = main.remotes.values()[0]
+        self.assertTrue(remote.allowed)
+        self.assertEqual(len(other.transactions), 0)
+        remote = other.remotes.values()[0]
+        self.assertTrue(remote.allowed)
+
+        main.server.close()
         other.server.close()
 
 
@@ -610,8 +458,8 @@ if __name__ == '__main__' and __package__ is None:
 
     #console.reinit(verbosity=console.Wordage.concise)
 
-    #runAll() #run all unittests
+    runAll() #run all unittests
 
     #runSome()#only run some
 
-    runOne('testBasic')
+    #runOne('testBootstrap')
