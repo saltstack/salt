5d779444be91f543ba1f8e9992bf8ef4ab3790fc
Pedro Algarvio <pedro@algarvio.me>
2014-05-12 19:09:55 +0100

    No need to be root to run these tests

--- tests/integration/output/output.py
+++ tests/integration/output/output.py
@@ -3,14 +3,10 @@
     :codeauthor: :email:`Nicole Thomas <nicole@saltstack.com>`
 '''
 
-# Import Python Libs
-import os
-
 # Import Salt Libs
 import integration
 
 # Import Salt Testing Libs
-from salttesting import skipIf
 from salttesting.helpers import ensure_in_syspath
 
 ensure_in_syspath('../../')
@@ -23,7 +19,6 @@ class OutputReturnTest(integration.ShellCase):
     right outputter even though it was explicitly requested.
     '''
 
-    @skipIf(os.geteuid() != 0, 'You must be logged in as root to run this test')
     def test_output_json(self):
         '''
         Tests the return of json-formatted data
@@ -32,7 +27,6 @@ class OutputReturnTest(integration.ShellCase):
         ret = self.run_call('test.ping --out=json')
         self.assertEqual(ret, expected)
 
-    @skipIf(os.geteuid() != 0, 'You must be logged in as root to run this test')
     def test_output_nested(self):
         '''
         Tests the return of nested-formatted data
@@ -41,7 +35,6 @@ class OutputReturnTest(integration.ShellCase):
         ret = self.run_call('test.ping --out=nested')
         self.assertEqual(ret, expected)
 
-    @skipIf(os.geteuid() != 0, 'You must be logged in as root to run this test')
     def test_output_quiet(self):
         '''
         Tests the return of an out=quiet query
@@ -50,7 +43,6 @@ class OutputReturnTest(integration.ShellCase):
         ret = self.run_call('test.ping --out=quiet')
         self.assertEqual(ret, expected)
 
-    @skipIf(os.geteuid() != 0, 'You must be logged in as root to run this test')
     def test_output_pprint(self):
         '''
         Tests the return of pprint-formatted data
@@ -59,7 +51,6 @@ class OutputReturnTest(integration.ShellCase):
         ret = self.run_call('test.ping --out=pprint')
         self.assertEqual(ret, expected)
 
-    @skipIf(os.geteuid() != 0, 'You must be logged in as root to run this test')
     def test_output_raw(self):
         '''
         Tests the return of raw-formatted data
@@ -68,7 +59,6 @@ class OutputReturnTest(integration.ShellCase):
         ret = self.run_call('test.ping --out=raw')
         self.assertEqual(ret, expected)
 
-    @skipIf(os.geteuid() != 0, 'You must be logged in as root to run this test')
     def test_output_txt(self):
         '''
         Tests the return of txt-formatted data
@@ -77,7 +67,6 @@ class OutputReturnTest(integration.ShellCase):
         ret = self.run_call('test.ping --out=txt')
         self.assertEqual(ret, expected)
 
-    @skipIf(os.geteuid() != 0, 'You must be logged in as root to run this test')
     def test_output_yaml(self):
         '''
         Tests the return of yaml-formatted data
