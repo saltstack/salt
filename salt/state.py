a0a9060a1b7cbee028c3260633aee1caff3ac905
Thomas S Hatch <thatch45@gmail.com>
2014-05-14 15:18:54 -0600

    NO FIRST PERSON!!!

--- salt/state.py
+++ salt/state.py
@@ -1039,14 +1039,11 @@ class State(object):
                     else:
                         errors.append(
                             'Cannot extend ID {0} in \'{1}:{2}\'. It is not '
-                            'part of the high state.\n\n'
-                            'This means that when we were compiling the '
-                            'state tree, we encountered a requsite '
-                            'that could not be resolved.\n\nCheck to make '
-                            'sure that you have created a state with an '
-                            'ID of \'{0}\'and that it is present in the '
-                            'environment \'{1}\' in order for the state '
-                            '\'{2}\' to run correctly.'.format(
+                            'part of the high state.\n'
+                            'This is likey due to a missing include statement '
+                            'or an incorrectly typed ID.\nEnsure that a'
+                            'state with an ID of \'{0}\' is available\nin '
+                            'environment {1} and to SLS {2}'.format(
                                 name,
                                 body.get('__env__', 'base'),
                                 body.get('__sls__', 'base'))
