d17f66a5ae1de61dd5da290f85c21c3caf8bf9fb
Mike Place <mp@saltstack.com>
2014-05-15 13:58:08 -0600

    Didn't remove debugging. Oops.

--- salt/utils/network.py
+++ salt/utils/network.py
@@ -779,7 +779,6 @@ def ip_addrs(interface=None, include_loopback=False):
             if include_loopback \
                     or (not include_loopback
                             and (ipv4['address'] != '127.0.0.1' and ipv4['label'] != 'lo')):
-                print(ipv4)
                 ret.add(ipv4['address'])
         for secondary in ipv4_info.get('secondary', []):
             addr = secondary.get('address')
