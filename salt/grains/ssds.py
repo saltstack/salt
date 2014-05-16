bb30b6c95826ee9f01874c25d19b45c3ec07627d
Elias Probst <mail@eliasprobst.eu>
2014-05-16 23:40:07 +0200

    Read the model + revision of SSDs as well

--- salt/grains/ssds.py
+++ salt/grains/ssds.py
@@ -16,7 +16,7 @@ def ssds():
     '''
     Return list of disk devices that are SSD (non-rotational)
     '''
-    ssd_devices = []
+    ssd_devices = {}
     for entry in glob.glob('/sys/block/*/queue/rotational'):
         with salt.utils.fopen(entry) as entry_fp:
             device = entry.split('/')[3]
@@ -28,12 +28,28 @@ def ssds():
                 log.trace('Device {0} is no physical device'.format(device))
                 continue
             log.trace('Device {0} is a physical device'.format(device))
+
             flag = entry_fp.read(1)
             if flag == '0':
-                ssd_devices.append(device)
+                ssd_devices[device] = {}
                 log.trace('Device {0} reports itself as an SSD'.format(device))
             elif flag == '1':
                 log.trace('Device {0} does not report itself as an SSD'.format(device))
+                continue
             else:
                 log.trace('Unable to identify device {0} as an SSD or not. It does not report 0 or 1'.format(device))
+                continue
+
+            model_file = os.path.join('/sys/block', device, 'device/model')
+            with salt.utils.fopen(model_file) as model_fp:
+                model = model_fp.readline().rstrip()
+                if model:
+                    ssd_devices[device]['model'] = model
+
+            revision_file = os.path.join('/sys/block', device, 'device/rev')
+            with salt.utils.fopen(revision_file) as revision_fp:
+                revision = revision_fp.readline().rstrip()
+                if revision:
+                    ssd_devices[device]['revision'] = revision
+
     return {'SSDs': ssd_devices}
