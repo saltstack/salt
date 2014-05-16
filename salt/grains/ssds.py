31fcc79e70693a279ac2c73eb08cd0aae659b31e
Elias Probst <mail@eliasprobst.eu>
2014-05-16 22:41:18 +0200

    Filter out non-physical devices when listing SSDs.

--- salt/grains/ssds.py
+++ salt/grains/ssds.py
@@ -7,6 +7,7 @@
 import glob
 import salt.utils
 import logging
+import os
 
 log = logging.getLogger(__name__)
 
@@ -19,6 +20,14 @@ def ssds():
     for entry in glob.glob('/sys/block/*/queue/rotational'):
         with salt.utils.fopen(entry) as entry_fp:
             device = entry.split('/')[3]
+            # check, whether the device belongs to a physical device to filter out non-physical devices
+            # such as device-mapper, NBD, loopback, ...
+            try:
+                os.stat(os.path.join('/sys/block', device, 'device'))
+            except OSError:
+                log.trace('Device {0} is no physical device'.format(device))
+                continue
+            log.trace('Device {0} is a physical device'.format(device))
             flag = entry_fp.read(1)
             if flag == '0':
                 ssd_devices.append(device)
