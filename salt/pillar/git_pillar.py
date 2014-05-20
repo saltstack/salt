f544dd954d64f0757ef3d66bd419b0df7b3c2875
jonathan stasiak <crasiak@gmail.com>
2014-05-19 22:39:13 -0700

    removing print from git_pillar

--- salt/pillar/git_pillar.py
+++ salt/pillar/git_pillar.py
@@ -69,7 +69,7 @@ def __virtual__():
     Only load if gitpython is available
     '''
     ext_pillar_sources = [x for x in __opts__.get('ext_pillar', [])]
-    print
+
     if not any(['git' in x for x in ext_pillar_sources]):
         return False
     if not HAS_GIT:
