059a7a2f7627abf019428e73facb5eb63d91c661
pengyao <pengyao@pengyao.org>
2014-05-21 00:59:37 +0800

    compatible lower Crypto version
    RHEL6/CentOS python-Crypto is v2.0.1

--- salt/utils/pycrypto.py
+++ salt/utils/pycrypto.py
@@ -12,18 +12,22 @@ except ImportError:
     HAS_RANDOM = False
 import crypt
 import re
-import salt.exceptions
+import string
+import random
 
+# Import salt libs
+import salt.exceptions
 
 def secure_password(length=20):
     '''
     Generate a secure password.
     '''
-    if not HAS_RANDOM:
-        raise ImportError('generating passwords requires >= pycrypto v2.1.0')
     pw = ''
     while len(pw) < length:
-        pw += re.sub(r'\W', '', Crypto.Random.get_random_bytes(1))
+        if HAS_RANDOM:
+            pw += re.sub(r'\W', '', Crypto.Random.get_random_bytes(1))
+        else:
+            pw += random.choice(string.ascii_letters + string.digits)
     return pw
 
 
