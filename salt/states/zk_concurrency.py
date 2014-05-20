2c49e66de7fa44c155789c38956e665016a68c74
Thomas Jackson <jacksontj.89@gmail.com>
2014-05-20 08:45:02 -0700

    Better, but it now has retries and sleeps

--- salt/states/zk_concurrency.py
+++ salt/states/zk_concurrency.py
@@ -5,6 +5,7 @@ execution. It maintains no other state
 
 import logging
 import os
+import time
 
 try:
     from kazoo.client import KazooClient
@@ -39,39 +40,64 @@ def _close_zk_conn():
     ZK_CONNECTION.stop()
     ZK_CONNECTION = None
 
-def lock(zk_hosts, path, max_concurrency):
+def lock(zk_hosts,
+         path,
+         max_concurrency,
+         timeout=None):
+    '''
+    Block state execution until you are able to get the lock (or hit the timeout)
+    '''
+    SLEEP_TIME = 0.2
     ret = {'name': path,
            'changes': {},
            'result': False,
            'comment': ''}
+
+    if __opts__['test']:
+        ret['result'] = None
+        ret['comment'] = 'attempt to aqcuire lock'
+        return ret
+
     zk = _get_zk_conn(zk_hosts)
     # makse sure the base lock path
     zk.ensure_path(path)
 
-    children, znode = zk.get_children(path, include_data=True)
+    slot = False
+    if timeout is not None:
+        timeout = time.time() + timeout
+    # TODO: the timeout
+    while slot is False and (timeout is None or time.time() < timeout):
+        children, znode = zk.get_children(path, include_data=True)
+        if len(children) >= max_concurrency:
+            time.sleep(SLEEP_TIME)
+            continue
+        # if we got here, get a lock
+        lock_path = os.path.join(path, __grains__['fqdn'])
 
-    lock_path = os.path.join(path, __grains__['fqdn'])
+        # TODO: fix this obvious race condition (create after len check-- need to check cversion somehow)
+        try:
+            zk.create(lock_path, '')
+        except kazoo.exceptions.NodeExistsError:
+            # TODO clobber? Maybe have an option for that
+            ret['result'] = False
+            ret['comment'] = 'lock already exists for this host... something weird'
+            return ret
 
-    if len(children) >= max_concurrency:
-        ret['comment'] = 'unable to acquire lock curr:{0} >= max:{1}'.format(len(children), max_concurrency)
-        return ret
+        # check # of connections again
+        children, znode = zk.get_children(path, include_data=True)
+        if len(children) > max_concurrency:
+            zk.delete(lock_path)
+            time.sleep(SLEEP_TIME)
+            continue
 
-    if __opts__['test']:
-        ret['result'] = None
-        ret['comment'] = 'could aqcuire lock'
-        return ret
+        slot = True
 
+    if slot:
+        ret['result'] = True
+        ret['comment'] = 'lock acquired'
+    else:
+        ret['comment'] = 'Unable to acquire lock'
 
-    # TODO: fix this obvious race condition (create after len check-- need to check cversion somehow)
-    try:
-        zk.create(lock_path, '')
-    except kazoo.exceptions.NodeExistsError:
-        ret['result'] = False
-        ret['comment'] = 'lock already exists for this host... something weird'
-        return ret
-
-    ret['result'] = True
-    ret['comment'] = 'lock acquired'
     return ret
 
 
