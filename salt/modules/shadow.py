saltstack/salt

commit 60cf38f64adf86bbe4f55fc0fa8ac98166d5456a

Use salt.utils.pycrypt to generate hashed password

salt/modules/shadow.py
@@ -13,6 +13,7 @@ except ImportError:
 
 # Import salt libs
 import salt.utils
+import salt.utils.pycrypt
 
 
 def __virtual__():
@@ -129,7 +130,7 @@ def set_mindays(name, mindays):
     return False
 
 
-def generate_password(password, crypt_salt=None, hash_algorithm='sha512'):
+def gen_password(password, crypt_salt=None, algorithm='sha512'):
     '''
     Generate hashed password
 
@@ -139,43 +140,17 @@ def generate_password(password, crypt_salt=None, hash_algorithm='sha512'):
     crypt_salt
         Crpytographic salt. If not given, will generate random 8-characters
 
-    hash_algorithm
+    algorithm
         Hashing algorithm. Support ``md5``, ``blowfish``, ``sha256``, ``sha512``(default).
 
     CLI Example:
 
     .. code-block:: bash
 
-        salt '*' shadow.generate_password 'I_am_password'
-        salt '*' shadow.generate_password 'I_am_password' 'I_am_salt' sha256
+        salt '*' shadow.gen_password 'I_am_password'
+        salt '*' shadow.gen_password 'I_am_password' 'I_am_salt' sha256
     '''
-    hash_algorithm = hash_algorithm.lower()
-    hash_algorithms = {'md5':'$1$', 'blowfish':'$2a$', 'sha256':'$5$', 'sha512':'$6$'}
-
-    ret = {}
-
-    try:
-        import crypt
-    except ImportError:
-        ret['error'] = 'Should installed crypt module'
-        return ret
-
-    # check hashing algorithm
-    if hash_algorithm not in hash_algorithms:
-        ret['error'] = 'Not support {0} hash algorithm'.format(hash_algorithm)
-        return ret
-
-    # generate crypt_salt
-    if not crypt_salt:
-        import random, string
-        crypt_salt = ''.join(random.sample(string.ascii_letters+string.digits, 8))
-    ret['salt'] = crypt_salt
-
-    crypt_salt = hash_algorithms[hash_algorithm] + crypt_salt
-    ret['hash_algorithm'] = hash_algorithm
-    ret['password'] = password
-    ret['hashed_password'] = crypt.crypt(password, crypt_salt)
-    return ret
+    return salt.utils.pycrypt.gen_hash(crypt_salt, password, algorithm)
 
 
 def set_password(name, password, use_usermod=False):

commit 1a934cd6e53a878a8c064a2a9052cd6935d68905

Fix incorrect reference to module
This fixes breakage introduced in 60cf38f6.

salt/modules/shadow.py
@@ -13,7 +13,7 @@ except ImportError:
 
 # Import salt libs
 import salt.utils
-import salt.utils.pycrypt
+import salt.utils.pycrypto
 
 
 def __virtual__():
@@ -150,7 +150,7 @@ def gen_password(password, crypt_salt=None, algorithm='sha512'):
         salt '*' shadow.gen_password 'I_am_password'
         salt '*' shadow.gen_password 'I_am_password' 'I_am_salt' sha256
     '''
-    return salt.utils.pycrypt.gen_hash(crypt_salt, password, algorithm)
+    return salt.utils.pycrypto.gen_hash(crypt_salt, password, algorithm)
 
 
 def set_password(name, password, use_usermod=False):