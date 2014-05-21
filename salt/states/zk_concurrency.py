8840c6567f9b3cd039866c0ea18aa04ea9fdf2aa
Thomas Jackson <jacksontj.89@gmail.com>
2014-05-21 09:00:34 -0700

    Switch to using a semaphore (mostly a built-in of the kazoo client)

--- salt/states/zk_concurrency.py
+++ salt/states/zk_concurrency.py
@@ -4,25 +4,34 @@ execution. It maintains no other state
 '''
 
 import logging
-import os
 import time
 
 try:
     from kazoo.client import KazooClient
-    import kazoo.exceptions
+
+    from kazoo.retry import (
+        KazooRetry,
+        RetryFailedError,
+        ForceRetryError
+    )
+    import kazoo.recipe.lock
+    from kazoo.exceptions import CancelledError
     HAS_DEPS = True
 except ImportError:
     HAS_DEPS = False
 
 ZK_CONNECTION = None
+SEMAPHORE_MAP = {}
 
 __virtualname__ = 'zk_concurrency'
+
 
 def __virtual__():
     if not HAS_DEPS:
         return False
 
     return __virtualname__
+
 
 def _get_zk_conn(hosts):
     global ZK_CONNECTION
@@ -32,6 +41,7 @@ def _get_zk_conn(hosts):
 
     return ZK_CONNECTION
 
+
 def _close_zk_conn():
     global ZK_CONNECTION
     if ZK_CONNECTION is None:
@@ -40,10 +50,13 @@ def _close_zk_conn():
     ZK_CONNECTION.stop()
     ZK_CONNECTION = None
 
+
 def lock(zk_hosts,
          path,
          max_concurrency,
-         timeout=None):
+         timeout=None,
+         ephemeral_lease=False):
+    global SEMAPHORE_MAP
     '''
     Block state execution until you are able to get the lock (or hit the timeout)
 
@@ -59,7 +72,6 @@ def lock(zk_hosts,
         all children in the queue should watch the one in front of them (its FIFO)
         and the one at the head of the line should watch /slots for a delete
     '''
-    SLEEP_TIME = 0.2
     ret = {'name': path,
            'changes': {},
            'result': False,
@@ -71,40 +83,19 @@ def lock(zk_hosts,
         return ret
 
     zk = _get_zk_conn(zk_hosts)
-    # makse sure the base lock path
-    zk.ensure_path(path)
+    if path not in SEMAPHORE_MAP:
+        SEMAPHORE_MAP[path] = Semaphore(zk,
+                                        path,
+                                        __grains__['fqdn'],
+                                        max_leases=max_concurrency,
+                                        ephemeral_lease=ephemeral_lease)
+    # block waiting for lock acquisition
+    if timeout:
+        SEMAPHORE_MAP[path].acquire(timeout=timeout)
+    else:
+        SEMAPHORE_MAP[path].acquire()
 
-    slot = False
-    if timeout is not None:
-        timeout = time.time() + timeout
-    # TODO: the timeout
-    while slot is False and (timeout is None or time.time() < timeout):
-        children, znode = zk.get_children(path, include_data=True)
-        if len(children) >= max_concurrency:
-            time.sleep(SLEEP_TIME)
-            continue
-        # if we got here, get a lock
-        lock_path = os.path.join(path, __grains__['fqdn'])
-
-        # TODO: fix this obvious race condition (create after len check-- need to check cversion somehow)
-        try:
-            zk.create(lock_path, '')
-        except kazoo.exceptions.NodeExistsError:
-            # TODO clobber? Maybe have an option for that
-            ret['result'] = False
-            ret['comment'] = 'lock already exists for this host... something weird'
-            return ret
-
-        # check # of connections again
-        children, znode = zk.get_children(path, include_data=True)
-        if len(children) > max_concurrency:
-            zk.delete(lock_path)
-            time.sleep(SLEEP_TIME)
-            continue
-
-        slot = True
-
-    if slot:
+    if SEMAPHORE_MAP[path].is_acquired:
         ret['result'] = True
         ret['comment'] = 'lock acquired'
     else:
@@ -114,26 +105,71 @@ def lock(zk_hosts,
 
 
 def unlock(zk_hosts, path):
+    '''
+    Remove lease from semaphore
+    '''
+    global SEMAPHORE_MAP
     ret = {'name': path,
            'changes': {},
            'result': False,
            'comment': ''}
-    zk = _get_zk_conn(zk_hosts)
-
-    lock_path = os.path.join(path, __grains__['fqdn'])
 
     if __opts__['test']:
         ret['result'] = None
         ret['comment'] = 'released lock if its here'
         return ret
 
-    try:
-        zk.delete(lock_path)
-        ret['comment'] = 'lock released'
-    except kazoo.exceptions.NoNodeError:
-        ret['comment'] = 'lock did not exist'
-
-    zk.stop()
+    if path in SEMAPHORE_MAP:
+        SEMAPHORE_MAP[path].release()
+        del SEMAPHORE_MAP[path]
+    else:
+        ret['comment'] = 'Unable to find lease for path {0}'.format(path)
+        return ret
 
     ret['result'] = True
     return ret
+
+
+# TODO: use the kazoo one, waiting for pull req: https://github.com/python-zk/kazoo/pull/206
+class Semaphore(kazoo.recipe.lock.Semaphore):
+    def __init__(self,
+                 client,
+                 path,
+                 identifier=None,
+                 max_leases=1,
+                 ephemeral_lease=True,
+                 ):
+        super(Semaphore, self).__init__(client,
+                                        path,
+                                        identifier=identifier,
+                                        max_leases=max_leases)
+
+        self.ephemeral_lease = ephemeral_lease
+
+    def _get_lease(self, data=None):
+        # Make sure the session is still valid
+        if self._session_expired:
+            raise ForceRetryError("Retry on session loss at top")
+
+        # Make sure that the request hasn't been canceled
+        if self.cancelled:
+            raise CancelledError("Semaphore cancelled")
+
+        # Get a list of the current potential lock holders. If they change,
+        # notify our wake_event object. This is used to unblock a blocking
+        # self._inner_acquire call.
+        children = self.client.get_children(self.path,
+                                            self._watch_lease_change)
+
+        # If there are leases available, acquire one
+        if len(children) < self.max_leases:
+            self.client.create(self.create_path, self.data, ephemeral=self.ephemeral_lease)
+
+        # Check if our acquisition was successful or not. Update our state.
+        if self.client.exists(self.create_path):
+            self.is_acquired = True
+        else:
+            self.is_acquired = False
+
+        # Return current state
+        return self.is_acquired
