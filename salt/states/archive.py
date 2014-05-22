d6e23388441eabb946930a9e5d3bf1fefc287339
Diego Woitasen <diego@woitasen.com.ar>
2014-05-22 16:15:15 -0300

    Document the options that should not be used in the tar_options parameter. Let the command tar to fail if the user dosn't use the right options like --file, -a, -x, etc.
    It doesn't worth the effort to put a logic to detect forbidden options
    in the module.
    
    The current code of removing 'f' breaks long options that have 'f', like
    --transform.

--- salt/states/archive.py
+++ salt/states/archive.py
@@ -80,7 +80,7 @@ def extracted(name,
         Using this option means that the tar executable on the target will
         be used, which is less platform independent.
         Main operators like -x, --extract, --get, -c, etc. and -f/--file are
-        **not allowed** here.
+        **shoult not be used** here.
         If this option is not set, then the Python tarfile module is used.
         The tarfile module supports gzip and bz2 in Python 2.
     '''
