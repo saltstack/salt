4d7ca9142403ff9eb0f8c2684273d4f6a067a557
Pedro Algarvio <pedro@algarvio.me>
2014-05-26 19:26:15 +0100

    Remove unnecessary `:copyright:` and `:license:` from file headers

--- salt/cloud/exceptions.py
+++ salt/cloud/exceptions.py
@@ -6,8 +6,6 @@
     Salt cloud related exceptions.
 
     :codeauthor: :email:`Pedro Algarvio (pedro@algarvio.me)`
-    :copyright: © 2013 by the SaltStack Team, see AUTHORS for more details.
-    :license: Apache 2.0, see LICENSE for more details.
 '''
 
 # Import salt libs
--- tests/unit/modules/mdadm_test.py
+++ tests/unit/modules/mdadm_test.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 '''
     :codeauthor: :email:`Ted Strzalkowski (tedski@gmail.com)`
-    :copyright: © 2013 by the SaltStack Team, see AUTHORS for more details
-    :license: Apache 2.0, see LICENSE for more details.
 
 
     tests.unit.modules.mdadm_test
