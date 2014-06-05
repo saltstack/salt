819a40da9f953d51b4b3b55d4bc5d366aeedb134
Khris Richardson <krichardson@mfluent.com>
2014-06-05 18:13:39 -0500

    optionally get etcd vals from master, squash stack traces, and fix lint issues

--- salt/returners/etcd_return.py
+++ salt/returners/etcd_return.py
@@ -49,7 +49,7 @@ import logging
 try:
     import salt.utils.etcd_util
     HAS_LIBS = True
-except Exception:
+except ImportError:
     HAS_LIBS = False
 
 log = logging.getLogger(__name__)
@@ -65,20 +65,28 @@ def __virtual__():
     return __virtualname__ if HAS_LIBS else False
 
 
-def _get_conn(opts):
+def _get_conn():
     '''
     Establish a connection to etcd
     '''
-    profile = opts.get('etcd.returner', None)
-    path = opts.get('etcd.returner_root', '/salt/return')
-    return salt.utils.etcd_util.get_conn(opts, profile), path
+    path = __salt__['config.option']('etcd.returner_root', '/salt/return')
+    profile = __salt__['config.option']('etcd.returner', None)
+    if profile:
+        etcd = __salt__['config.option']('profile')
+        host = etcd.get('etcd.host', '127.0.0.1')
+        port = etcd.get('etcd.port', 4001)
+    else:
+        host = __salt__['config.option']('etcd.host', '127.0.0.1')
+        port = __salt__['config.option']('etcd.port', 4001)
+
+    return salt.utils.etcd_util.get_conn(host, port), path
 
 
 def returner(ret):
     '''
     Return data to an etcd server or cluster
     '''
-    client, path = _get_conn(__opts__)
+    client, path = _get_conn()
 
     # Make a note of this minion for the external job cache
     client.write(
@@ -102,7 +110,7 @@ def save_load(jid, load):
     '''
     Save the load to the specified jid
     '''
-    client, path = _get_conn(__opts__)
+    client, path = _get_conn()
     client.write(
         '/'.join((path, 'jobs', jid, '.load.p')),
         json.dumps(load)
@@ -113,7 +121,7 @@ def get_load(jid):
     '''
     Return the load data that marks a specified jid
     '''
-    client, path = _get_conn(__opts__)
+    client, path = _get_conn()
     return json.loads(client.get('/'.join(path, 'jobs', jid, '.load.p')))
 
 
@@ -121,17 +129,17 @@ def get_jid(jid):
     '''
     Return the information returned when the specified job id was executed
     '''
-    client, path = _get_conn(__opts__)
+    client, path = _get_conn()
     jid_path = '/'.join((path, 'jobs', jid))
     return salt.utils.etcd_util.tree(client, jid_path)
 
 
-def get_fun(fun):
+def get_fun():
     '''
     Return a dict of the last function called for all minions
     '''
     ret = {}
-    client, path = _get_conn(__opts__)
+    client, path = _get_conn()
     items = client.get('/'.join((path, 'minions')))
     for item in items.children:
         comps = str(item.key).split('/')
@@ -144,7 +152,7 @@ def get_jids():
     Return a list of all job ids
     '''
     ret = []
-    client, path = _get_conn(__opts__)
+    client, path = _get_conn()
     items = client.get('/'.join((path, 'jobs')))
     for item in items.children:
         if item.dir is True:
@@ -158,7 +166,7 @@ def get_minions():
     Return a list of minions
     '''
     ret = []
-    client, path = _get_conn(__opts__)
+    client, path = _get_conn()
     items = client.get('/'.join((path, 'minions')))
     for item in items.children:
         comps = str(item.key).split('/')
