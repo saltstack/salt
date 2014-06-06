5af83395e43f9c37453b22824f601304225f7f8d
Khris Richardson <krichardson@mfluent.com>
2014-06-05 21:52:37 -0500

    removing redundancy

--- salt/returners/etcd_return.py
+++ salt/returners/etcd_return.py
@@ -73,11 +73,11 @@ def _get_conn():
     profile = __salt__['config.option']('etcd.returner', None)
     if profile:
         etcd = __salt__['config.option']('profile')
-        host = etcd.get('etcd.host', '127.0.0.1')
-        port = etcd.get('etcd.port', 4001)
+        host = etcd.get('etcd.host')
+        port = etcd.get('etcd.port')
     else:
-        host = __salt__['config.option']('etcd.host', '127.0.0.1')
-        port = __salt__['config.option']('etcd.port', 4001)
+        host = __salt__['config.option']('etcd.host')
+        port = __salt__['config.option']('etcd.port')
 
     return salt.utils.etcd_util.get_conn(host, port), path
 
