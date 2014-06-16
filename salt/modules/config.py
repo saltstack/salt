1d3e01095db618176884f8ee449724924cbef29e
Joseph Hall <perlhoser@gmail.com>
2014-06-16 08:05:33 -0600

    Break out SDB functions from config module

--- salt/modules/config.py
+++ salt/modules/config.py
@@ -11,6 +11,7 @@ import os
 import salt.utils
 import salt._compat
 import salt.syspaths as syspaths
+import salt.utils.sdb as sdb
 
 __proxyenabled__ = ['*']
 
@@ -226,71 +227,21 @@ def get(key, default=''):
     '''
     ret = salt.utils.traverse_dict_and_list(__opts__, key, '_|-')
     if ret != '_|-':
-        return sdb_get(ret)
+        return sdb.sdb_get(ret, __opts__)
 
     ret = salt.utils.traverse_dict_and_list(__grains__, key, '_|-')
     if ret != '_|-':
-        return sdb_get(ret)
+        return sdb.sdb_get(ret, __opts__)
 
     ret = salt.utils.traverse_dict_and_list(__pillar__, key, '_|-')
     if ret != '_|-':
-        return sdb_get(ret)
+        return sdb.sdb_get(ret, __opts__)
 
     ret = salt.utils.traverse_dict_and_list(__pillar__.get('master', {}), key, '_|-')
     if ret != '_|-':
-        return sdb_get(ret)
+        return sdb.sdb_get(ret, __opts__)
 
     return default
-
-
-def sdb_get(uri):
-    '''
-    Get a value from a db, using a uri in the form of sdb://<profile>/<key>. If
-    the uri provided does not start with sdb://, then it will be returned as-is.
-    '''
-    if not uri.startswith('sdb://'):
-        return uri
-
-    comps = uri.replace('sdb://', '').split('/')
-
-    if len(comps) < 2:
-        return uri
-
-    profile = __opts__.get(comps[0], {})
-    if 'driver' not in profile:
-        return uri
-
-    fun = '{0}.get'.format(profile['driver'])
-    query = comps[1]
-
-    import salt.loader
-    loaded_db = salt.loader.db(__opts__, fun)
-    return loaded_db[fun](query, profile=profile)
-
-
-def sdb_set(uri, value):
-    '''
-    Get a value from a db, using a uri in the form of sdb://<profile>/<key>. If
-    the uri provided does not start with sdb://, then it will be returned as-is.
-    '''
-    if not uri.startswith('sdb://'):
-        return False
-
-    comps = uri.replace('sdb://', '').split('/')
-
-    if len(comps) < 2:
-        return False
-
-    profile = __opts__.get(comps[0], {})
-    if 'driver' not in profile:
-        return False
-
-    fun = '{0}.set'.format(profile['driver'])
-    query = comps[1]
-
-    import salt.loader
-    loaded_db = salt.loader.db(__opts__, fun)
-    return loaded_db[fun](query, value, profile=profile)
 
 
 def dot_vals(value):
