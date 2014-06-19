123b6e1e4196a97f8ae8079c4582b3b948c06010
Daniel Wallace <danielwallace@gtmanfred.com>
2014-06-18 19:14:08 -0500

    if value is None and thus the default continue

--- salt/config.py
+++ salt/config.py
@@ -901,14 +901,18 @@ def apply_sdb(opts, sdb_opts=None):
     '''
     if sdb_opts is None:
         sdb_opts = opts
-    if isinstance(sdb_opts, dict):
+    if isinstance(sdb_opts, string_types) and sdb_opts.startswith('sdb://'):
+        return salt.utils.sdb.sdb_get(opts, sdb_opts)
+    elif isinstance(sdb_opts, dict):
         for key, value in sdb_opts.items():
+            if value is None:
+                continue
             sdb_opts[key] = apply_sdb(opts, value)
     elif isinstance(sdb_opts, list):
         for key, value in enumerate(sdb_opts):
+            if value is None:
+                continue
             sdb_opts[key] = apply_sdb(opts, value)
-    elif isinstance(sdb_opts, string_types) and sdb_opts.startswith('sdb://'):
-        return salt.utils.sdb.sdb_get(opts, sdb_opts)
 
     return sdb_opts
 
