01ec6a2e6270ed769a7d41b66c98af96f1cffbfd
Mike Place <mp@saltstack.com>
2014-06-19 17:04:05 -0600

    Do not throw incomprehensible error messages.

--- salt/daemons/masterapi.py
+++ salt/daemons/masterapi.py
@@ -468,7 +468,7 @@ class RemoteFuncs(object):
         file_recv_max_size = 1024*1024 * self.opts.get('file_recv_max_size', 100)
 
         if 'loc' in load and load['loc'] < 0:
-            log.error('Should not happen: load[loc] < 0')
+            log.error('Invalid file pointer: load[loc] < 0')
             return False
 
         if len(load['data']) + load.get('loc', 0) > file_recv_max_size:
