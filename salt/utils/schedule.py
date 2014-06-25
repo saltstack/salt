d2c90689bec5a189733db9d66b03de610215dff4
David Boucha <boucha@gmail.com>
2014-06-25 12:26:45 -0600

    open files with 'rb'
    Windows needs this to be opened with 'rb' to avoid stacktraces.
    
    Conflicts:
    	salt/utils/schedule.py

--- salt/utils/schedule.py
+++ salt/utils/schedule.py
@@ -278,7 +278,7 @@ def clean_proc_dir(opts):
 
     for basefilename in os.listdir(salt.minion.get_proc_dir(opts['cachedir'])):
         fn = os.path.join(salt.minion.get_proc_dir(opts['cachedir']), basefilename)
-        with salt.utils.fopen(fn, 'r') as fp_:
+        with salt.utils.fopen(fn, 'rb') as fp_:
             job = salt.payload.Serial(opts).load(fp_)
             log.debug('schedule.clean_proc_dir: checking job {0} for process '
                       'existence'.format(job))
