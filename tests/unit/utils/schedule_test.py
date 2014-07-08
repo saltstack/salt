cf11ca22d029ee6ba69fc54c9409bcecfa6a37c7
rallytime <nicole@saltstack.com>
2014-07-08 11:27:32 -0600

    More scheduler unit tests and fixed keyerror possibility

--- tests/unit/utils/schedule_test.py
+++ tests/unit/utils/schedule_test.py
@@ -66,6 +66,68 @@ class ScheduleTestCase(TestCase):
         data = {'key1': 'value1', 'key2': 'value2'}
         self.assertRaises(ValueError, Schedule.add_job, self.schedule, data)
 
+    # enabled_schedule tests
+
+    def test_enable_schedule(self):
+        '''
+        Tests enabling the scheduler
+        '''
+        self.schedule.opts = {'schedule': {'enabled': 'foo'}}
+        Schedule.enable_schedule(self.schedule)
+        self.assertTrue(self.schedule.opts['schedule']['enabled'])
+
+    # disable_schedule tests
+
+    def test_disable_schedule(self):
+        '''
+        Tests disabling the scheduler
+        '''
+        self.schedule.opts = {'schedule': {'enabled': 'foo'}}
+        Schedule.disable_schedule(self.schedule)
+        self.assertFalse(self.schedule.opts['schedule']['enabled'])
+
+    # reload tests
+
+    def test_reload_update_schedule_key(self):
+        '''
+        Tests reloading and updating the schedule from saved schedule file that
+        contains a schedule key
+        '''
+        saved = {'schedule': {'foo': 'bar'}}
+        ret = {'schedule': {'foo': 'bar', 'hello': 'world'}}
+        self.schedule.opts = {'schedule': {'hello': 'world'}}
+        Schedule.reload(self.schedule, saved)
+        self.assertEqual(self.schedule.opts, ret)
+
+    def test_reload_update_schedule_no_key(self):
+        '''
+        Tests reloading and updating the schedule from saved schedule file that
+        contains a schedule key
+        '''
+        saved = {'foo': 'bar'}
+        ret = {'schedule': {'foo': 'bar', 'hello': 'world'}}
+        self.schedule.opts = {'schedule': {'hello': 'world'}}
+        Schedule.reload(self.schedule, saved)
+        self.assertEqual(self.schedule.opts, ret)
+
+    def test_reload_no_schedule_in_opts(self):
+        '''
+        Tests reloading the schedule
+        '''
+        saved = {'foo': 'bar'}
+        ret = {'schedule': {'foo': 'bar'}}
+        Schedule.reload(self.schedule, saved)
+        self.assertEqual(self.schedule.opts, ret)
+
+    def test_reload_schedule_in_saved_but_not_opts(self):
+        '''
+        Tests reloading the schedule
+        '''
+        saved = {'schedule': {'foo': 'bar'}}
+        ret = {'schedule': {'schedule': {'foo': 'bar'}}}
+        Schedule.reload(self.schedule, saved)
+        self.assertEqual(self.schedule.opts, ret)
+
 
 if __name__ == '__main__':
     from integration import run_tests
