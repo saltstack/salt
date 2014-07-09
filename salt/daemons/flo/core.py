75f38ae74764425518c57f6bdfab6802bd8a420c
Nitin Madhok <nmadhok@g.clemson.edu>
2014-07-09 11:50:17 -0400

    Fixing lint

--- salt/daemons/flo/core.py
+++ salt/daemons/flo/core.py
@@ -100,7 +100,6 @@ class SaltRaetCleanup(ioflo.base.deeding.Deed):
                     raise
 
 
-
 class SaltRaetRoadStackSetup(ioflo.base.deeding.Deed):
     '''
     Initialize and run raet udp stack for Salt
@@ -952,7 +951,7 @@ class NixExecutor(ioflo.base.deeding.Deed):
         ret_stack = LaneStack(
                 name=stackname,
                 lanename=mid,
-                yid=yid, # jid
+                yid=yid,  # jid
                 sockdirpath=self.opts['sock_dir'])
 
         ret_stack.Pk = raeting.packKinds.pack
