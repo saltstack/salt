bb021bc1e5db9aec5064bf722d64a47fd28106a7
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2014-07-27 16:41:18 +0200

    file.copy: be smart with file changes

--- salt/states/file.py
+++ salt/states/file.py
@@ -3306,6 +3306,7 @@ def copy(name, source, force=False, makedirs=False):
         'comment': '',
         'result': True}
 
+    changed = True
     if not os.path.isabs(name):
         return _error(
             ret, 'Specified file {0} is not an absolute path'.format(name))
@@ -3314,11 +3315,14 @@ def copy(name, source, force=False, makedirs=False):
         return _error(ret, 'Source file "{0}" is not present'.format(source))
 
     if os.path.lexists(source) and os.path.lexists(name):
+        # if this is a file which did not changed, do not update
+        if force and os.path.isfile(name):
+            hash1 = salt.utils.get_hash(name)
+            hash2 = salt.utils.get_hash(source)
+            if hash2 != hash1:
+                changed = False
         if not force:
-            ret['comment'] = ('The target file "{0}" exists and will not be '
-                              'overwritten'.format(name))
-            ret['result'] = True
-            return ret
+            changed = False
         elif not __opts__['test']:
             # Remove the destination to prevent problems later
             try:
@@ -3341,6 +3345,12 @@ def copy(name, source, force=False, makedirs=False):
             name
         )
         ret['result'] = None
+        return ret
+
+    if not changed:
+        ret['comment'] = ('The target file "{0}" exists and will not be '
+                          'overwritten'.format(name))
+        ret['result'] = True
         return ret
 
     # Run makedirs
