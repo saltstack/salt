9af0fe19d58b81d5076c7393c0dd0e1282751e5b
Chris Rebert <chris.rebert@hulu.com>
2014-07-28 17:34:40 -0700

    utils.cloud.wait_for_passwd(): fix bad string formatting in debug logging
    ************* Module salt.utils.cloud
    salt/utils/cloud.py:727: [E1305(too-many-format-args), wait_for_passwd] Too many arguments for format string

--- salt/utils/cloud.py
+++ salt/utils/cloud.py
@@ -724,7 +724,7 @@ def wait_for_passwd(host, port=22, ssh_timeout=15, username='root',
                 log.debug('Using {0} as the key_filename'.format(key_filename))
             elif password:
                 kwargs['password'] = password
-                log.debug('Using password authentication'.format(password))
+                log.debug('Using password authentication')
 
             trycount += 1
             log.debug(
