e3cdae3e63f02ce1b778a70bc22247a391c3711c
Ryan Lane <rlane@lyft.com>
2014-08-07 16:17:04 -0700

    Fix versionadded string

--- salt/modules/boto_cloudwatch.py
+++ salt/modules/boto_cloudwatch.py
@@ -2,7 +2,7 @@
 '''
 Connection module for Amazon CloudWatch
 
-.. versionadded:: Helium
+.. versionadded:: 2014.7.0
 
 :configuration: This module accepts explicit credentials but can also utilize
     IAM roles assigned to the instance trough Instance Profiles. Dynamic
