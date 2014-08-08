12b7f414e9890944b5b4109ad964989c86fe38bf
Erik Johnson <palehose@gmail.com>
2014-08-08 12:10:29 -0500

    rename "delim" argument to "delimiter"

--- salt/cli/__init__.py
+++ salt/cli/__init__.py
@@ -113,7 +113,7 @@ class SaltCMD(parsers.SaltCMDOptionParser):
                 except IOError:
                     kwargs['token'] = self.config['token']
 
-            kwargs['delim'] = self.options.delim
+            kwargs['delimiter'] = self.options.delimiter
 
             if self.selected_target_option:
                 kwargs['expr_form'] = self.selected_target_option
