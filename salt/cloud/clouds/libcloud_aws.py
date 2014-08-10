26c28d36464d6ae43f4c6868bc1df4680c3f9b0c
Pedro Algarvio <pedro@algarvio.me>
2014-08-10 03:59:21 +0100

    Use `exc_info_on_loglevel` instead of `exc_info`

--- salt/cloud/clouds/libcloud_aws.py
+++ salt/cloud/clouds/libcloud_aws.py
@@ -369,7 +369,7 @@ def create(vm_):
                 vm_['name'], exc
             ),
             # Show the traceback if the debug logging level is enabled
-            exc_info=log.isEnabledFor(logging.DEBUG)
+            exc_info_on_loglevel=logging.DEBUG
         )
         return False
 
@@ -744,7 +744,7 @@ def rename(name, kwargs, call=None):
                 name, kwargs['newname'], exc
             ),
             # Show the traceback if the debug logging level is enabled
-            exc_info=log.isEnabledFor(logging.DEBUG)
+            exc_info_on_loglevel=logging.DEBUG
         )
     return kwargs['newname']
 
