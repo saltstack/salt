0208baa53c97f055a58feb80718fb611cac06921
Nitin Madhok <nmadhok@g.clemson.edu>
2014-08-25 13:32:15 -0400

    Fixing some of the formatting issues

--- salt/modules/file.py
+++ salt/modules/file.py
@@ -2036,6 +2036,8 @@ def access(path, mode):
     Test whether the Salt process has the specified access to the file. One of
     the following modes must be specified:
 
+    .. code-block::text
+
         f: Test the existence of the path
         r: Test the readability of the path
         w: Test the writability of the path
