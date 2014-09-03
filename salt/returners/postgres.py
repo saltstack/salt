43c48f0bbf8e9c9720ff4945ee8aee031654c732
Gareth J. Greenaway <gareth@wiked.org>
2014-09-02 17:57:04 -0700

    Consolidating the _get_options function from all the returners that were using it into the config module.  Additionally some cleanup and ability to pass defaults to the get_returner_options function.
    fixing lint issues
    
    moving the get_options function into a utils.py under returners.  previous attempt at using a call to the salt config module doesn't work if the returner is called from the scheduler.
    
    adding missing docstring
    
    Unit tests were failing for SMTP returner because __opts__ was not available.  Ensuring it's available, empty, if it doesn't already exist.
    
    removing the check and population with an empty dict for __salt__ and __opts__.  Adding an empty __opts__ to the smtp returner unit tests.

--- salt/returners/postgres.py
+++ salt/returners/postgres.py
@@ -80,6 +80,8 @@ Required python modules: psycopg2
 # Import python libs
 import json
 
+import salt.returners.utils
+
 # Import third party libs
 try:
     import psycopg2
@@ -99,47 +101,19 @@ def __virtual__():
 
 def _get_options(ret=None):
     '''
-    Get the odbc options from salt.
+    Get the postgres options from salt.
     '''
-    if ret:
-        ret_config = '{0}'.format(ret['ret_config']) if 'ret_config' in ret else ''
-    else:
-        ret_config = None
-
     attrs = {'host': 'host',
              'user': 'user',
              'passwd': 'passwd',
              'db': 'db',
              'port': 'port'}
 
-    _options = {}
-    for attr in attrs:
-        if 'config.option' in __salt__:
-            cfg = __salt__['config.option']
-            c_cfg = cfg('returner.{0}'.format(__virtualname__), {})
-            if ret_config:
-                ret_cfg = cfg('{0}.returner.{1}'.format(ret_config, __virtualname__), {})
-                if ret_cfg.get(attrs[attr], cfg('{0}.returner.{1}.{2}'.format(ret_config, __virtualname__, attrs[attr]))):
-                    _attr = ret_cfg.get(attrs[attr], cfg('{0}.returner.{1}.{2}'.format(ret_config, __virtualname__, attrs[attr])))
-                else:
-                    _attr = c_cfg.get(attrs[attr], cfg('returner.{0}.{1}'.format(__virtualname__, attrs[attr])))
-            else:
-                _attr = c_cfg.get(attrs[attr], cfg('returner.{0}.{1}'.format(__virtualname__, attrs[attr])))
-        else:
-            cfg = __opts__
-            c_cfg = cfg.get('returner.{0}'.format(__virtualname__), {})
-            if ret_config:
-                ret_cfg = cfg.get('{0}.returner.{1}'.format(ret_config, __virtualname__), {})
-                if ret_cfg.get(attrs[attr], cfg.get('{0}.returner.{1}.{2}'.format(ret_config, __virtualname__, attrs[attr]))):
-                    _attr = ret_cfg.get(attrs[attr], cfg.get('{0}.returner.{1}.{2}'.format(ret_config, __virtualname__, attrs[attr])))
-                else:
-                    _attr = c_cfg.get(attrs[attr], cfg.get('returner.{0}.{1}'.format(__virtualname__, attrs[attr])))
-            else:
-                _attr = c_cfg.get(attrs[attr], cfg.get('returner.{0}.{1}'.format(__virtualname__, attrs[attr])))
-        if not _attr:
-            _options[attr] = None
-            continue
-        _options[attr] = _attr
+    _options = salt.returners.utils.get_returner_options('returner.{0}'.format(__virtualname__),
+                                                         ret,
+                                                         attrs,
+                                                         __salt__=__salt__,
+                                                         __opts__=__opts__)
     return _options
 
 
--- salt/returners/smtp_return.py
+++ salt/returners/smtp_return.py
@@ -70,6 +70,8 @@ import logging
 import smtplib
 from email.utils import formatdate
 
+import salt.returners.utils
+
 try:
     import gnupg
     HAS_GNUPG = True
@@ -88,13 +90,8 @@ def __virtual__():
 
 def _get_options(ret=None):
     '''
-    Get the redis options from salt.
+    Get the SMTP options from salt.
     '''
-    if ret:
-        ret_config = '{0}'.format(ret['ret_config']) if 'ret_config' in ret else ''
-    else:
-        ret_config = None
-
     attrs = {'from': 'from',
              'to': 'to',
              'host': 'host',
@@ -105,34 +102,11 @@ def _get_options(ret=None):
              'fields': 'fields',
              'tls': 'tls'}
 
-    _options = {}
-    for attr in attrs:
-        if 'config.option' in __salt__:
-            cfg = __salt__['config.option']
-            c_cfg = cfg('{0}'.format(__virtualname__), {})
-            if ret_config:
-                ret_cfg = cfg('{0}.{1}'.format(ret_config, __virtualname__), {})
-                if ret_cfg.get(attrs[attr], cfg('{0}.{1}.{2}'.format(ret_config, __virtualname__, attrs[attr]))):
-                    _attr = ret_cfg.get(attrs[attr], cfg('{0}.{1}.{2}'.format(ret_config, __virtualname__, attrs[attr])))
-                else:
-                    _attr = c_cfg.get(attrs[attr], cfg('{0}.{1}'.format(__virtualname__, attrs[attr])))
-            else:
-                _attr = c_cfg.get(attrs[attr], cfg('{0}.{1}'.format(__virtualname__, attrs[attr])))
-        else:
-            cfg = __opts__
-            c_cfg = cfg.get('{0}'.format(__virtualname__), {})
-            if ret_config:
-                ret_cfg = cfg.get('{0}.{1}'.format(ret_config, __virtualname__), {})
-                if ret_cfg.get(attrs[attr], cfg.get('{0}.{1}.{2}'.format(ret_config, __virtualname__, attrs[attr]))):
-                    _attr = ret_cfg.get(attrs[attr], cfg.get('{0}.{1}.{2}'.format(ret_config, __virtualname__, attrs[attr])))
-                else:
-                    _attr = c_cfg.get(attrs[attr], cfg.get('{0}.{1}'.format(__virtualname__, attrs[attr])))
-            else:
-                _attr = c_cfg.get(attrs[attr], cfg.get('{0}.{1}'.format(__virtualname__, attrs[attr])))
-        if not _attr:
-            _options[attr] = None
-            continue
-        _options[attr] = _attr
+    _options = salt.returners.utils.get_returner_options(__virtualname__,
+                                                         ret,
+                                                         attrs,
+                                                         __salt__=__salt__,
+                                                         __opts__=__opts__)
     return _options
 
 
