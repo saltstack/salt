dfa47a98f15b6fc4b1299d3701b1183826cc1061
Jille Timmermans <jille@hexon.cx>
2014-09-11 14:52:13 +0200

    Fixed the Result of file.replace

--- salt/states/file.py
+++ salt/states/file.py
@@ -2317,12 +2317,16 @@ def replace(name,
 
     if changes:
         ret['changes'] = {'diff': changes}
-        ret['comment'] = ('Changes were made'
-                if not __opts__['test'] else 'Changes would have been made')
+        if __opts__['test']:
+            ret['result'] = None
+            ret['comment'] = 'Changes would have been made'
+        else:
+            ret['result'] = True
+            ret['comment'] = 'Changes were made'
     else:
-        ret['comment'] = 'No changes were made'
+        ret['result'] = True
+        ret['comment'] = 'No changes needed to be made'
 
-    ret['result'] = True if not __opts__['test'] else None
     return ret
 
 
