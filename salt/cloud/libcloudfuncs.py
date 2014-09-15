483e5bb6cdcb6e4a3483190861f4c99660ea2268
Pedro Algarvio <pedro@algarvio.me>
2014-09-15 21:28:13 +0100

    Support extra apache-libcloud>0.15 node states

--- salt/cloud/libcloudfuncs.py
+++ salt/cloud/libcloudfuncs.py
@@ -48,7 +48,11 @@ def node_state(id_):
               1: 'REBOOTING',
               2: 'TERMINATED',
               3: 'PENDING',
-              4: 'UNKNOWN'}
+              4: 'UNKNOWN',
+              5: 'STOPPED',
+              6: 'SUSPENDED',
+              7: 'ERROR',
+              8: 'PAUSED'}
     return states[id_]
 
 
