6bfe517fae587696a6b9512c36e770404ec18ea5
ror6ax <ror6ax@gmail.com>
2014-09-16 14:29:41 +0100

    function names changed as discussed in pull req

--- salt/modules/devmap.py
+++ salt/modules/devmap.py
@@ -3,22 +3,26 @@ import logging
 log = logging.getLogger(__name__)
 
 
-def multipath():
+def multipath_list():
     '''
     Device-Mapper Multipath module
     CLI Example:
     .. code-block:: bash
-    salt '*' devmap.multipath list
-    salt '*' devmap.multipath flush mpath1
+    salt '*' devmap.multipath_list
     '''
-    ret = {}
-    if ret[0] == 'list':
-        cmd = 'multipath -l'
-    elif ret[0] == "flush":
-        try:
-            cmd = 'multipath -f {0}'.format(ret[1])
-        except ValueError:
-            return 'Error: No device to flush has been provided!'
-    else:
-        return 'Error: Unknown option provided!'
+    cmd = 'multipath -l'
     return __salt__['cmd.run'](cmd).splitlines()
+
+
+def multipath_flush(args=None):
+    '''
+    Device-Mapper Multipath module
+    CLI Example:
+    .. code-block:: bash
+    salt '*' devmap.multipath_flush mpath1
+    '''
+    ret={}
+    try:
+        cmd = 'multipath -f {0}'.format(ret[0])
+    except ValueError:
+        return 'Error: No device to flush has been provided!'
