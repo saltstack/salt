6b251cbb05c358fd494e24ea7f7e5566f4e62533
Arnold Bechtoldt <mail@arnoldbechtoldt.com>
2014-09-21 21:19:03 +0200

    fix integration test in fileserver runner, refs #16008

--- tests/integration/runners/fileserver.py
+++ tests/integration/runners/fileserver.py
@@ -20,38 +20,38 @@ class ManageTest(integration.ShellCase):
         '''
         fileserver.dir_list
         '''
-        ret = self.run_run_plus('fileserver.dir_list')
+        ret = self.run_run_plus(fun='fileserver.dir_list')
         self.assertIsInstance(ret['fun'], list)
 
     def test_envs(self):
         '''
         fileserver.envs
         '''
-        ret = self.run_run_plus('fileserver.envs')
+        ret = self.run_run_plus(fun='fileserver.envs')
         self.assertIsInstance(ret['fun'], list)
 
-        ret = self.run_run_plus('fileserver.envs back={0}'.format(['root']))
+        ret = self.run_run_plus(fun='fileserver.envs', args=['back="{0}"'.format(['root'])])
         self.assertIsInstance(ret['fun'], list)
 
     def test_file_list(self):
         '''
         fileserver.file_list
         '''
-        ret = self.run_run_plus('fileserver.file_list')
+        ret = self.run_run_plus(fun='fileserver.file_list')
         self.assertIsInstance(ret['fun'], list)
 
     def test_symlink_list(self):
         '''
         fileserver.symlink_list
         '''
-        ret = self.run_run_plus('fileserver.symlink_list')
+        ret = self.run_run_plus(fun='fileserver.symlink_list')
         self.assertIsInstance(ret['fun'], dict)
 
     def test_update(self):
         '''
         fileserver.update
         '''
-        ret = self.run_run_plus('fileserver.update')
+        ret = self.run_run_plus(fun='fileserver.update')
         self.assertTrue(ret['fun'])
 
 if __name__ == '__main__':
