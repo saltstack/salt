d08acaee667c743b4cdc4b8457210d1fba03789b
Daniel Wallace <danielwallace@gtmanfred.com>
2014-09-21 15:45:50 -0500

    fix authentication

--- salt/utils/openstack/pyrax/authenticate.py
+++ salt/utils/openstack/pyrax/authenticate.py
@@ -1,15 +1,13 @@
 import pyrax
 
 class Authenticate(object):
-    def __init__(self, username, password, region, auth_endpoint=None, **kwargs):
+    def __init__(self, username, password, region, **kwargs):
         cloud_kwargs = {
-            'tenant_name': username,
             'password': password,
-            'region': region,
-            'identity_type': kwargs.get('identity_type', 'keystone')
+            'region': region
         }
-        if auth_endpoint:
-            cloud_kwargs['auth_endpoint'] = auth_endpoint
+        pyrax.settings.set('identity_type', kwargs.get('identity_type', 'rackspace'))
+        pyrax.settings.set('auth_endpoint', kwargs.get('auth_endpoint', 'https://identity.api.rackspacecloud.com/v2.0'))
 
-        pyrax.set_credentials(**cloud_kwargs)
-        self.conn = pyrax.keyring_auth()
+        pyrax.set_credentials(username, **cloud_kwargs)
+        self.conn = pyrax
