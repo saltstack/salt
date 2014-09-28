4004f09cd00f5210444b451a4b24266d2a705b6b
Omer Katz <omer.drow@gmail.com>
2014-09-28 18:31:23 +0300

    Refactoring the test suite.

--- tests/unit/modules/boto_vpc_test.py
+++ tests/unit/modules/boto_vpc_test.py
@@ -77,6 +77,20 @@ class BotoVpcTestCase(TestCase):
     TestCase for salt.modules.boto_vpc module
     '''
 
+    conn = None
+
+    def _create_vpc(self):
+        if not self.conn:
+            self.conn = boto.vpc.connect_to_region(region)
+
+        return self.conn.create_vpc(cidr_block)
+
+    def _create_subnet(self, vpc_id, cidr_block='10.0.0.0/25'):
+        if not self.conn:
+            self.conn = boto.vpc.connect_to_region(region)
+
+        return self.conn.create_subnet(vpc_id, cidr_block)
+
     @mock_ec2
     def test_get_subnet_association_single_subnet(self):
         '''
@@ -84,12 +98,11 @@ class BotoVpcTestCase(TestCase):
         returned. The test is valuable because it uses a string as an argument
         to subnets as opposed to a list.
         '''
-        conn = boto.vpc.connect_to_region(region)
-        vpc = conn.create_vpc(cidr_block)
-        subnet = conn.create_subnet(vpc.id, '10.0.0.0/25')
-        subnet_assocation = boto_vpc.get_subnet_association(subnets=subnet.id,
+        vpc = self._create_vpc()
+        subnet = self._create_subnet(vpc.id)
+        subnet_association = boto_vpc.get_subnet_association(subnets=subnet.id,
                                                             **conn_parameters)
-        self.assertEqual(vpc.id, subnet_assocation)
+        self.assertEqual(vpc.id, subnet_association)
 
     @mock_ec2
     def test_get_subnet_association_multiple_subnets_same_vpc(self):
@@ -97,13 +110,12 @@ class BotoVpcTestCase(TestCase):
         tests that given multiple subnet ids in the same VPC that the VPC ID is
         returned.
         '''
-        conn = boto.vpc.connect_to_region(region)
-        vpc = conn.create_vpc(cidr_block)
-        subnet_a = conn.create_subnet(vpc.id, '10.0.0.0/25')
-        subnet_b = conn.create_subnet(vpc.id, '10.0.0.128/25')
-        subnet_assocation = boto_vpc.get_subnet_association([subnet_a.id, subnet_b.id],
+        vpc = self._create_vpc()
+        subnet_a = self._create_subnet(vpc.id, '10.0.0.0/25')
+        subnet_b = self._create_subnet(vpc.id, '10.0.0.128/25')
+        subnet_association = boto_vpc.get_subnet_association([subnet_a.id, subnet_b.id],
                                                             **conn_parameters)
-        self.assertEqual(vpc.id, subnet_assocation)
+        self.assertEqual(vpc.id, subnet_association)
 
     @mock_ec2
     def test_get_subnet_association_multiple_subnets_different_vpc(self):
@@ -111,11 +123,10 @@ class BotoVpcTestCase(TestCase):
         tests that given multiple subnet ids in different VPCs that False is
         returned.
         '''
-        conn = boto.vpc.connect_to_region(region)
-        vpc_a = conn.create_vpc(cidr_block)
-        vpc_b = conn.create_vpc(cidr_block)
-        subnet_a = conn.create_subnet(vpc_a.id, '10.0.0.0/24')
-        subnet_b = conn.create_subnet(vpc_b.id, '10.0.0.0/24')
+        vpc_a = self._create_vpc()
+        vpc_b = self.conn.create_vpc(cidr_block)
+        subnet_a = self._create_subnet(vpc_a.id, '10.0.0.0/24')
+        subnet_b = self._create_subnet(vpc_b.id, '10.0.0.0/24')
         subnet_assocation = boto_vpc.get_subnet_association([subnet_a.id, subnet_b.id],
                                                             **conn_parameters)
         self.assertFalse(subnet_assocation)
@@ -125,8 +136,7 @@ class BotoVpcTestCase(TestCase):
         '''
         tests True existence of a VPC.
         '''
-        conn = boto.vpc.connect_to_region(region)
-        vpc = conn.create_vpc(cidr_block)
+        vpc = self._create_vpc()
         vpc_exists = boto_vpc.exists(vpc.id, **conn_parameters)
         self.assertTrue(vpc_exists)
 
@@ -151,8 +161,7 @@ class BotoVpcTestCase(TestCase):
 
     @mock_ec2
     def test_that_when_creating_a_subnet_succeeds_the_create_subnet_method_returns_true(self):
-        conn = boto.vpc.connect_to_region(region)
-        vpc = conn.create_vpc(cidr_block)
+        vpc = self._create_vpc()
 
         subnet_creation_result = boto_vpc.create_subnet(vpc.id, '10.0.0.0/24', **conn_parameters)
 
@@ -160,8 +169,7 @@ class BotoVpcTestCase(TestCase):
 
     @mock_ec2
     def test_that_when_creating_a_subnet_fails_the_create_subnet_method_returns_false(self):
-        conn = boto.vpc.connect_to_region(region)
-        vpc = conn.create_vpc(cidr_block)
+        vpc = self._create_vpc()
 
         with patch('moto.ec2.models.SubnetBackend.create_subnet', side_effect=BotoServerError(400, 'Mocked error')):
             subnet_creation_result = boto_vpc.create_subnet(vpc.id, '10.0.0.0/24', **conn_parameters)
