3e4221c4434f250d60696b003a0f3fd5d1000ca9
Thomas S Hatch <thatch45@gmail.com>
2014-09-29 22:45:46 -0600

    envs is not a load passing function

--- salt/fileserver/__init__.py
+++ salt/fileserver/__init__.py
@@ -564,7 +564,7 @@ class FSChan(object):
         if cmd in self.cmd_stub:
             return self.cmd_stub[cmd]
         if cmd == 'file_envs':
-            cmd = 'envs'
+            return self.fs.envs()
         if not hasattr(self.fs, cmd):
             log.error('Malformed request, invalid cmd: {0}'.format(load))
             return {}
