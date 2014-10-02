ef6dc12fc2745b8581b122ea11d2a70c5090f2fb
Samuel M Smith <smith.samuel.m@gmail.com>
2014-10-02 13:58:33 -0600

    fix worker bug

--- salt/daemons/flo/core.py
+++ salt/daemons/flo/core.py
@@ -735,7 +735,7 @@ class SaltRaetRouter(ioflo.base.deeding.Deed):
         if s_estate is None:  # drop
             return
 
-        log.debug("**** Road Router rxMsg ****\n   id {0} estate {1} yard {2}\n{3}\n".format(
+        log.debug("**** Road Router rxMsg **** id={0} estate={1} yard={2}\n   msg= {3}\n".format(
                 self.opts.value['id'],
                 self.udp_stack.value.local.name,
                 self.uxd_stack.value.local.name,
@@ -795,7 +795,7 @@ class SaltRaetRouter(ioflo.base.deeding.Deed):
             s_estate = self.udp_stack.value.local.name
             msg['route']['src'] = (s_estate, s_yard, s_share)
 
-        log.debug("**** UXD Router rxMsg ****\n  id {0} estate {1} yard {2}\n{3}\n".format(
+        log.debug("**** Lane Router rxMsg ****\n  id {0} estate {1} yard {2}\n{3}\n".format(
                 self.opts.value['id'],
                 self.udp_stack.value.local.name,
                 self.uxd_stack.value.local.name,
