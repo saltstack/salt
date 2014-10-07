ebc477586a1100d33ee35f3635747b681ddbbcff
ror6ax <ror6ax@gmail.com>
2014-10-07 13:15:32 +0100

    added rescan all

--- salt/modules/scsi.py
+++ salt/modules/scsi.py
@@ -15,3 +15,17 @@ def lsscsi():
     '''
     cmd = 'lsscsi'
     return __salt__['cmd.run'](cmd).splitlines()
+
+
+def rescan_all(host):
+    '''
+    List scsi devices
+    CLI Example:
+    .. code-block:: bash
+    salt '*' scsi.rescan_all(0)
+    '''
+    if os.path.isdir("/sys/class/scsi_host/host{0}").format(host):
+        cmd = 'echo "- - -" > /sys/class/scsi_host/host{0}/scan'.format(host)
+    else:
+        return 'Host {0} does not exist'.format(host)
+    return __salt__['cmd.run'](cmd).splitlines()
