75a7048252a948ac366befb6a6708f29f9ae8ba2
rallytime <nicole@saltstack.com>
2014-10-07 15:51:28 -0600

    Fix OrderedDict import

--- salt/daemons/__init__.py
+++ salt/daemons/__init__.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 '''
 The daemons package is used to store implimentations of the Salt Master and
-Minion enabling different transports
-
-
-
+Minion enabling different transports.
 '''
 
-from collections import namedtuple, OrderedDict
+# Import Python Libs
+from collections import namedtuple
+
+# Import Salt Libs
+from salt.utils.odict import OrderedDict
 
 # Python equivalent of an enum
 APPL_KINDS = OrderedDict([('master', 0), ('minion', 1), ('syndic', 2), ('call', 3)])
