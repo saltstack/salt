457687cc4f48c9994d729a4ac6155793509f7afe
Tim O'Guin <tim.oguin@juiceanalytics.com>
2014-10-10 15:07:23 -0500

    updated versionadded directive to a few modules (Helium -> 2014.7.0)

--- salt/modules/boto_vpc.py
+++ salt/modules/boto_vpc.py
@@ -2,7 +2,7 @@
 '''
 Connection module for Amazon VPC
 
-.. versionadded:: Helium
+.. versionadded:: 2014.7.0
 
 :configuration: This module accepts explicit autoscale credentials but can also
     utilize IAM roles assigned to the instance trough Instance Profiles.
