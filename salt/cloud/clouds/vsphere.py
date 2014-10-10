c47ffd9daf38f14caf421d4da34f0e6d4caa27be
Steve Weber <steverweber@gmail.com>
2014-10-10 12:35:02 -0400

    lint

--- salt/cloud/clouds/vsphere.py
+++ salt/cloud/clouds/vsphere.py
@@ -457,7 +457,7 @@ def list_nodes_min(kwargs=None, call=None):  # pylint: disable=W0613
     '''
     ret = {}
     conn = get_conn()
-    property_names=['name']
+    property_names = ['name']
     result = conn._retrieve_properties_traversal(
         property_names=property_names, obj_type='VirtualMachine'
     )
