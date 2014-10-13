saltstack/salt

commit f229f62f407cc6d5a1bd2e60f64e76c7b5e15463

Make `result=False` if retcode is not 0 in module.run/wait, Fix #4971

salt/states/module.py
@@ -149,6 +149,8 @@ def run(name, **kwargs):
             returners[kwargs['returner']](ret_ret)
     ret['comment'] = 'Module function {0} executed'.format(name)
     ret['result'] = True
+    if ret['changes'].get('retcode', 0) != 0:
+        ret['result'] = False
     return ret
 
 mod_watch = run  # pylint: disable-msg=C0103

commit eee4aa198548b896aebffc1c0e4fca74de5083ac

Make result=False if retcode is not 0 in module.run/wait
This fixes commit f229f62f407cc6d5a1bd2e60f64e76c7b5e15463 which
introduced this code, but seems to check the wrong part of `ret`

salt/states/module.py
@@ -211,7 +211,7 @@ def run(name, **kwargs):
             returners[kwargs['returner']](ret_ret)
     ret['comment'] = 'Module function {0} executed'.format(name)
     ret['result'] = True
-    if ret['changes'].get('retcode', 0) != 0:
+    if ret['changes'].get('ret', {}).get('retcode', 0) != 0:
         ret['result'] = False
     return ret
 