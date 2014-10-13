35c0ea27d95b2c0d6696829ca16cbc1908d8828d
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2014-10-13 15:04:49 +0200

    fix absent key dir

--- salt/key.py
+++ salt/key.py
@@ -577,9 +577,13 @@ class Key(object):
 
         for dir_ in key_dirs:
             ret[os.path.basename(dir_)] = []
-            for fn_ in salt.utils.isorted(os.listdir(dir_)):
-                if os.path.isfile(os.path.join(dir_, fn_)):
-                    ret[os.path.basename(dir_)].append(fn_)
+            try:
+                for fn_ in salt.utils.isorted(os.listdir(dir_)):
+                    if os.path.isfile(os.path.join(dir_, fn_)):
+                        ret[os.path.basename(dir_)].append(fn_)
+            except (OSError, IOError):
+                # key dir kind is not created yet, just skip
+                continue
         return ret
 
     def all_keys(self):
