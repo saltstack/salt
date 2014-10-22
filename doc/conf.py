879ee67947563c33444654a2cde06da0b87fab12
Henrik Holmboe <henrik@holmboe.se>
2014-10-22 17:10:18 +0200

    Update conf.py as well

--- doc/conf.py
+++ doc/conf.py
@@ -152,7 +152,7 @@ copyright = '2014 SaltStack, Inc.'
 
 version = salt.version.__version__
 #release = '.'.join(map(str, salt.version.__version_info__))
-release = '2014.1.11'
+release = '2014.1.13'
 
 spelling_lang = 'en_US'
 language = 'en'
