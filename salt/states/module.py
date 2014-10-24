c4d609722093b55b5af5c07e5312fbb418a6320c
rallytime <nicole@saltstack.com>
2014-10-24 17:23:33 -0600

    Remove debugging lines

--- salt/states/module.py
+++ salt/states/module.py
@@ -59,9 +59,6 @@ import datetime
 import salt.loader
 import salt.utils
 
-import logging
-logger = logging.getLogger(__name__)
-
 
 def wait(name, **kwargs):
     '''
@@ -121,7 +118,6 @@ def run(name, **kwargs):
         return ret
 
     aspec = salt.utils.get_function_argspec(__salt__[name])
-    logger.info(aspec)
     args = []
     defaults = {}
 
