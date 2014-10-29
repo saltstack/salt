24c12c5866b7457909f8fe8b6472690e970a8536
vs <vs@hosteurope.de>
2014-10-29 16:42:50 +0100

    pylint

--- salt/wheel/minions.py
+++ salt/wheel/minions.py
@@ -7,16 +7,17 @@ from salt.utils.cache import CacheCli
 import salt.config
 import salt.utils.minion
 
+
 def connected():
-    ''' 
+    '''
     List all connected minions on a salt-master
     '''
     opts = salt.config.master_config(__opts__['conf_file'])
     minions = []
 
     if opts.get('con_cache'):
-        minions = set(cache_cli = CacheCli(opts))
+        cache_cli = CacheCli(opts)
+        minions = cache_cli.get_cached()
     else:
         minions = list(salt.utils.minions.CkMinions(opts).connected_ids())
     return minions
-
