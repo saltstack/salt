e29449a896c21afde499055beae7bae0df069b92
Antoine Reversat <a.reversat@gmail.com>
2014-10-29 23:05:43 -0400

    Add a projectid configuration. Allows specifying in what project to create the VM

--- salt/cloud/clouds/cloudstack.py
+++ salt/cloud/clouds/cloudstack.py
@@ -194,6 +194,22 @@ def get_networkid(vm_):
     else:
         return False
 
+def get_project(conn, vm_):
+    '''
+    Return the project to use.
+    '''
+    projects = conn.ex_list_projects()
+    projid = config.get_cloud_config_value('projectid', vm_, __opts__)
+
+    if not projid:
+        return False
+
+    for project in projects:
+        if str(projid) in (str(project.id), str(project.name)):
+            return project
+
+    log.warning("Couldn't find project {0} in projects".format(projid))
+    return False
 
 def create(vm_):
     '''
@@ -230,6 +246,9 @@ def create(vm_):
                                                    kwargs['networkids'],
                                                    None, None),
                              )
+
+    if get_project(conn, vm_) is not False:
+        kwargs['project'] = get_project(conn, vm_)
 
     salt.utils.cloud.fire_event(
         'event',
