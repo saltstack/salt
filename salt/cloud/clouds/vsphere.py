f8556451e517953cb980623fc680e79332ac2a19
Steve Weber <steverweber@gmail.com>
2014-10-31 11:22:25 -0400

    removed unused place holder
    removed unused string place holder

--- salt/cloud/clouds/vsphere.py
+++ salt/cloud/clouds/vsphere.py
@@ -290,7 +290,7 @@ def wait_for_ip(vm_):
         time.sleep(1)
         return False
 
-    log.debug('Pulling VM {0} {1} seconds for an IP address'.format(vm_['name']))
+    log.debug('Pulling VM {0} for an IP address'.format(vm_['name']))
     ip_address = salt.utils.cloud.wait_for_fun(poll_ip)
 
     if ip_address is not False:
