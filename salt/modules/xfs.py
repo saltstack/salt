0b2f574d820dfcd7465eab6ac6ea39cc3f7fc6e2
Bo Maryniuk <bo@suse.de>
2014-11-01 18:03:50 +0100

    Move general verification to a separate function

--- salt/modules/xfs.py
+++ salt/modules/xfs.py
@@ -43,6 +43,18 @@ def __virtual__():
     return not salt.utils.is_windows()
 
 
+def _verify_run(out):
+    '''
+    Crash to the log if command execution was not successful.
+    '''
+    if out.get("retcode", 0) and out['stderr']:
+        log.debug('XFS dump command: "{0}"'.format(cmd))
+        log.debug('Return code: {0}'.format(out.get('retcode')))
+        log.debug('Error output:\n{0}'.format(out.get('stderr', "N/A")))
+
+        raise CommandExecutionError(out['stderr'])
+
+
 def _get_xfs_devices():
     '''
     Get xfs devices
