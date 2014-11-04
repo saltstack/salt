a692bff6ab6ef1fd0848bf5ee33f4c61c020d1d3
Eric Brunson <ebrunson@quicinc.com>
2014-11-04 15:50:41 -0700

    Fix changes from rebase
    pep8: --config=/usr2/ebrunson/.config/pep8
    
    Change-Id: I500ff2fa7cdfe5ad577ca42d2dbf8b34b830a8cb

--- salt/utils/find.py
+++ salt/utils/find.py
@@ -88,6 +88,8 @@ import re
 import stat
 import sys
 import time
+import shlex
+from subprocess import Popen, PIPE
 try:
     import grp
     import pwd
@@ -547,7 +549,7 @@ class ExecOption(Option):
     def __init__(self, key, value):
         self.command = value
 
-    def execute(self, fullpath, fstat):
+    def execute(self, fullpath, fstat, test=False):
         try:
             command = self.command.format(fullpath)
             print(shlex.split(command))
@@ -557,14 +559,14 @@ class ExecOption(Option):
             (out, err) = p.communicate()
             if err:
                 log.error(
-                    'Error running command: {}\n\n{}'.format(
+                    'Error running command: {0}\n\n{1}'.format(
                     command,
                     err))
-            return "{}:\n{}\n".format(command, out)
+            return "{0}:\n{1}\n".format(command, out)
 
         except Exception, e:
             log.error(
-                'Exception while executing command "{}":\n\n{}'.format(
+                'Exception while executing command "{0}":\n\n{1}'.format(
                     command,
                     e))
             return "{}: Failed".format(fullpath)
