d553d4e85ffd003d7bb8a00400647e5ddd13d958
benosman <dev@smoothify.com>
2014-11-06 15:51:39 +0000

    Read Pillar files into OrderedDict to preserve source order

--- salt/utils/dictupdate.py
+++ salt/utils/dictupdate.py
@@ -9,11 +9,13 @@ from __future__ import absolute_import
 import collections
 import six
 
+# Import salt libs
+from salt.utils.odict import OrderedDict
 
 def update(dest, upd):
     for key, val in six.iteritems(upd):
         if isinstance(val, collections.Mapping):
-            ret = update(dest.get(key, {}), val)
+            ret = update(dest.get(key, OrderedDict()), val)
             dest[key] = ret
         else:
             dest[key] = upd[key]
