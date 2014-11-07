f17c4a9f7d614a0a20bb91ba35967e5b57060d40
Mike Place <mp@saltstack.com>
2014-11-07 08:35:25 -0700

    Added warning for new --quiet requirement

--- salt/runners/http.py
+++ salt/runners/http.py
@@ -3,11 +3,14 @@
 Module for making various web calls. Primarily designed for webhooks and the
 like, but also useful for basic http testing.
 '''
+# Import Python libs
+import logging
 
 # Import salt libs
 import salt.output
 import salt.utils.http
 
+log = logging.getLogger(__name__)
 
 def query(url, output=True, **kwargs):
     '''
@@ -23,6 +26,8 @@ def query(url, output=True, **kwargs):
         salt-run http.query http://somelink.com/ method=POST \
             data='<xml>somecontent</xml>'
     '''
+    if output is not True:
+        log.warn('Output option has been deprecated. Please use --quiet.')
     if 'node' not in kwargs:
         kwargs['node'] = 'master'
 
