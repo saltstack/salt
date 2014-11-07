9a383f77e364f5dfde307817590c91a7d0891979
Samuel M Smith <smith.samuel.m@gmail.com>
2014-11-06 19:59:30 -0700

    redo stuff trying to figure out best approach

--- salt/cli/__init__.py
+++ salt/cli/__init__.py
@@ -428,55 +428,6 @@ class SaltCall(parsers.SaltCallOptionParser):
         caller.run()
 
 
-class SaltCallPrep(parsers.SaltCallOptionParser):
-    '''
-    Used to prepare opts in order to decide if need to stand up caller minion
-    or use existing minion
-    '''
-
-    def prep(self):
-        '''
-        Do prep to get valid opts in self.config
-        '''
-        self.parse_args()
-
-        if self.config['verify_env']:
-            verify_env([
-                    self.config['pki_dir'],
-                    self.config['cachedir'],
-                ],
-                self.config['user'],
-                permissive=self.config['permissive_pki_access'],
-                pki_dir=self.config['pki_dir'],
-            )
-            if not self.config['log_file'].startswith(('tcp://',
-                                                       'udp://',
-                                                       'file://')):
-                # Logfile is not using Syslog, verify
-                verify_files(
-                    [self.config['log_file']],
-                    self.config['user']
-                )
-
-        if self.options.file_root:
-            # check if the argument is pointing to a file on disk
-            file_root = os.path.abspath(self.options.file_root)
-            self.config['file_roots'] = {'base': _expand_glob_path([file_root])}
-
-        if self.options.pillar_root:
-            # check if the argument is pointing to a file on disk
-            pillar_root = os.path.abspath(self.options.pillar_root)
-            self.config['pillar_roots'] = {'base': _expand_glob_path([pillar_root])}
-
-        if self.options.local:
-            self.config['file_client'] = 'local'
-        if self.options.master:
-            self.config['master'] = self.options.master
-
-        # Setup file logging!
-        self.setup_logfile_logger()
-
-
 class SaltRun(parsers.SaltRunOptionParser):
     '''
     Used to execute Salt runners
--- salt/scripts.py
+++ salt/scripts.py
@@ -19,7 +19,6 @@ import salt
 from salt.exceptions import SaltSystemExit, SaltClientError, SaltReqTimeoutError
 import salt.cli
 
-from salt.utils import kinds
 
 log = logging.getLogger(__name__)
 
@@ -216,13 +215,6 @@ def salt_call():
     '''
     if '' in sys.path:
         sys.path.remove('')
-
-    prepper = salt.cli.SaltCallPrep()
-    prepper.prep()
-    if (prepper.config.get('__role') ==
-            kinds.APPL_KIND_NAMES[kinds.applKinds.caller]):
-        pass
-
     client = None
     try:
         client = salt.cli.SaltCall()
