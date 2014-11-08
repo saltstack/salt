2718af62be135f353efc0ae0e73620831e73225a
Damian Myerscough <Damian.Myerscough@gmail.com>
2014-11-08 10:40:18 -0800

    Initial user management for ilo

--- salt/modules/ilo.py
+++ salt/modules/ilo.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 '''
-Manage HP iLO
+Manage HP ILOM
 '''
 
 import xml.etree.cElementTree as ET
@@ -27,6 +27,7 @@ def __execute_cmd(name, xml):
     Execute ilom commands
     '''
     ret = {name.replace('_', ' '): {}}
+    id = 0
 
     with salt.utils.fopen('/tmp/{0}.{1}'.format(name, os.getpid()), 'w') as fh:
         fh.write(xml)
@@ -41,14 +42,26 @@ def __execute_cmd(name, xml):
         return False
 
     for i in ET.fromstring(''.join(cmd['stdout'].splitlines()[3:-1])):
-        ret[name.replace('_', ' ')].update({i.tag: i.attrib.get('VALUE', None)})
+        # Ensure a key is unique since the ilo return the same key multiple times depending
+        # on the action
+        if ret[name.replace('_', ' ')].get(i.tag, None):
+            ret[name.replace('_', ' ')].update({i.tag + '_' + str(id): i.attrib.get('VALUE', None)})
+            id += 1
+        else:
+            ret[name.replace('_', ' ')].update({i.tag: i.attrib.get('VALUE', None)})
 
     return ret
 
 
 def global_settings():
     '''
+    Show global settings
 
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' ilo.global_settings
     '''
     _xml = """<!-- Sample file for Get Global command -->
               <RIBCL VERSION="2.0">
@@ -60,3 +73,24 @@ def global_settings():
                </RIBCL>"""
 
     return __execute_cmd('Global_Settings', _xml)
+
+
+def all_users():
+    '''
+    List all users
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' ilo.all_users
+    '''
+    _xml = """<RIBCL VERSION="2.0">
+                <LOGIN USER_LOGIN="x" PASSWORD="x">
+                    <USER_INFO MODE="read">
+                      <GET_ALL_USERS />
+                    </USER_INFO>
+                </LOGIN>
+              </RIBCL>"""
+
+    return __execute_cmd('All_users', _xml)
