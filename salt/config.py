04177a1e5ec34fa068dcdd7321c5c2097fccfba5
Pedro Algarvio <pedro@algarvio.me>
2014-11-08 01:31:29 +0000

    We imported the function, not the module

--- salt/config.py
+++ salt/config.py
@@ -997,7 +997,7 @@ def syndic_config(master_config_path,
         'autosign_file', 'autoreject_file', 'token_dir'
     ]
     for config_key in ('log_file', 'key_logfile'):
-        if urlparse.urlparse(opts.get(config_key, '')).scheme == '':
+        if urlparse(opts.get(config_key, '')).scheme == '':
             prepend_root_dirs.append(config_key)
     prepend_root_dir(opts, prepend_root_dirs)
     return opts
