c6547c3772c1854cf34a4735b6bfa451cba78905
Erik Johnson <palehose@gmail.com>
2014-11-09 03:54:40 -0600

    Fix creation of container from image
    Also changed return data for create() to match destroy(), clone(), etc.

--- salt/modules/lxc.py
+++ salt/modules/lxc.py
@@ -1357,7 +1357,7 @@ def create(name,
             'Only one of \'template\' and \'image\' is permitted'
         )
 
-    options = select('options')
+    options = select('options') or {}
     backing = select('backing')
     if vgname and not backing:
         backing = 'lvm'
@@ -1378,7 +1378,7 @@ def create(name,
                 'templates',
                 'lxc',
                 'salt_tarball')
-        profile['imgtar'] = img_tar
+        options['imgtar'] = img_tar
     if config:
         cmd += ' -f {0}'.format(config)
     if template:
@@ -1415,7 +1415,10 @@ def create(name,
                                   output_loglevel='trace')
     _clear_context()
     if ret['retcode'] == 0 and exists(name):
-        return True
+        c_state = state(name)
+        return {'result': True,
+                'changes': {'old': None, 'new': c_state},
+                'state': c_state}
     else:
         if exists(name):
             # destroy the container if it was partially created
