2bd59ef3b4478fc68f75f70312ee7b2efdccd689
Damian Myerscough <Damian.Myerscough@gmail.com>
2014-11-09 22:56:19 -0800

    Implemented acl.present test option

--- salt/states/linux_acl.py
+++ salt/states/linux_acl.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 '''
 Linux File Access Control Lists
 '''
@@ -8,32 +7,57 @@ Linux File Access Control Lists
 import salt.utils
 from salt.exceptions import CommandExecutionError
 
+__virtualname__ = 'acl'
 
 def __virtual__():
     '''
     Ensure getfacl & setfacl exist
     '''
     if salt.utils.which('getfacl') and salt.utils.which('setfacl'):
-        return True
+        return __virtualname__
 
     return False
 
 
-def present(name, obj, acl):
+def present(name, acl_type, acl_name, perms, recursive=False):
     '''
     Ensure a Linux ACL is present
     '''
+    ret = {'name': name,
+           'result': True,
+           'changes': {},
+           'comment': ''}
+
+    _octal = {'r': 4, 'w': 2, 'x': 1}
+
+    if __opts__['test']:
+        _current_perms = __salt__['acl.getfacl'](name)
+
+        if _current_perms[name].get(acl_type, None):
+            try:
+                user = [i for i in _current_perms[name][acl_type] if i.keys()[0] == acl_name].pop()
+            except IndexError:
+                pass
+
+            if user:
+                if user[acl_name]['octal'] == sum([_octal.get(i, i) for i in perms]):
+                    ret['comment'] = '{0} has the desired permissions'.format(name)
+                else:
+                    ret['comment'] = '{0} permissions will be set'.format(perms)
+            else:
+                ret['comment'] = '{0} permissions will be set'.format(perms)
+                ret['changes'] = {acl_name: 'will be set'}
+        else:
+            ret['comment'] = 'ACL Type does not exist'
+            ret['result'] = False
+
+        return ret
+    #__salt__['acl.modfacl'](acl_type, acl_name, perms, name)
     pass
 
 
-def absent(name, obj, acl):
+def absent(name, obj, acl, recurse=False):
     '''
     Ensure a Linux ACL does not exist
     '''
     pass
-
-
-def absolute(name, obj, acl):
-    '''
-    Ensure a Linux ACL exists and deletes any existing ones
-    '''
