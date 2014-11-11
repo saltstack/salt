75504e286ea9dfc2520191a84a023ea0fa98dee8
rallytime <nicole@saltstack.com>
2014-11-11 11:38:22 -0700

    Resolve related pylint errors

--- salt/runners/test.py
+++ salt/runners/test.py
@@ -5,8 +5,6 @@ This runner is used only for test purposes and servers no production purpose
 # Import python libs
 import time
 
-# Import salt requirements
-import salt.utils.event
 
 def arg(*args, **kwargs):
     '''
@@ -34,12 +32,13 @@ def raw_arg(*args, **kwargs):
     }
     return ret
 
+
 def stream():
     '''
     Return True
     '''
     ret = True
-    for i in range(1,100):
+    for i in range(1, 100):
         progress('Runner is {0}% done'.format(i), outputter='pprint')
         time.sleep(0.1)
     return ret
