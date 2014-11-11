df440844f1cca4aae5e9e21cc18fe797249f64a0
Nitin Madhok <nmadhok@g.clemson.edu>
2014-11-10 22:27:52 -0500

    List call not needed. Changing it back to what it was

--- salt/modules/grains.py
+++ salt/modules/grains.py
@@ -138,7 +138,7 @@ def items(sanitize=False):
     '''
     if salt.utils.is_true(sanitize):
         out = dict(__grains__)
-        for key, func in list(_SANITIZERS.items()):
+        for key, func in _SANITIZERS.items():
             if key in out:
                 out[key] = func(out[key])
         return out
@@ -170,7 +170,7 @@ def item(*args, **kwargs):
         except KeyError:
             pass
     if salt.utils.is_true(kwargs.get('sanitize')):
-        for arg, func in list(_SANITIZERS.items()):
+        for arg, func in _SANITIZERS.items():
             if arg in ret:
                 ret[arg] = func(ret[arg])
     return ret
@@ -216,7 +216,7 @@ def setvals(grains, destructive=False):
                 return 'Unable to read existing grains file: {0}'.format(e)
         if not isinstance(grains, dict):
             grains = {}
-    for key, val in list(new_grains.items()):
+    for key, val in new_grains.items():
         if val is None and destructive is True:
             if key in grains:
                 del grains[key]
