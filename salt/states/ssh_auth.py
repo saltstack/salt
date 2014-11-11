550c29a9a5e28a7d34a8168cc13ec9a0f5f9d613
Nitin Madhok <nmadhok@g.clemson.edu>
2014-11-11 02:33:34 -0500

    Making salt.states.ssh_auth python3 compatible

--- salt/states/ssh_auth.py
+++ salt/states/ssh_auth.py
@@ -48,6 +48,7 @@ to use a YAML 'explicit key', as demonstrated in the second example below.
           - option3="value3" ssh-dss AAAAB3NzaC1kcQ9J5bYTEyY== other@testdomain
           - AAAAB3NzaC1kcQ9fJFF435bYTEyY== newcomment
 '''
+from __future__ import absolute_import
 
 # Import python libs
 import re
@@ -67,7 +68,7 @@ def _present_test(user, name, enc, comment, options, source, config):
                 saltenv=__env__)
         if keys:
             comment = ''
-            for key, status in keys.items():
+            for key, status in list(keys.items()):
                 if status == 'exists':
                     continue
                 comment += 'Set to {0}: {1}\n'.format(status, key)
