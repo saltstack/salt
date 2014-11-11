a2bb92ec6db416c59d9c9ea12f679bb8a22782af
Nitin Madhok <nmadhok@g.clemson.edu>
2014-11-10 23:47:26 -0500

    Reorganizing imports

--- salt/modules/win_repo.py
+++ salt/modules/win_repo.py
@@ -11,9 +11,9 @@ Place all Windows package files in the 'win_repo' directory.
 '''
 
 # Import python libs
-import os
-from __future__ import print_function
 from __future__ import absolute_import
+from __future__ import print_function
+import os
 
 # Import third party libs
 import yaml
