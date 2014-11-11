cfe0bec2d3061f115fb60d23db6ef2a9b2878a86
Nitin Madhok <nmadhok@g.clemson.edu>
2014-11-11 14:11:43 -0500

    List call not needed. Changing it back to what it was

--- salt/states/ssh_auth.py
+++ salt/states/ssh_auth.py
@@ -68,7 +68,7 @@ def _present_test(user, name, enc, comment, options, source, config):
                 saltenv=__env__)
         if keys:
             comment = ''
-            for key, status in list(keys.items()):
+            for key, status in keys.items():
                 if status == 'exists':
                     continue
                 comment += 'Set to {0}: {1}\n'.format(status, key)
