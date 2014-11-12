744d0586794e02071b3a3e0840041e41843ad213
Nitin Madhok <nmadhok@g.clemson.edu>
2014-11-12 15:39:00 -0500

    Reorganizing import

--- salt/netapi/rest_cherrypy/tools/websockets.py
+++ salt/netapi/rest_cherrypy/tools/websockets.py
@@ -1,5 +1,5 @@
-from __future__ import absolute_import
 # encoding: utf-8
+from __future__ import absolute_import
 import cherrypy
 
 from ws4py.server.cherrypyserver import WebSocketPlugin, WebSocketTool
