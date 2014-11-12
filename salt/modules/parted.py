97140139f087b8d8feec0a34a2a7d567778f11b4
akilesh1597 <akilesh1597@gmail.com>
2014-11-12 16:28:08 +0530

    do not raise exception if fs_type is None only raise when fs_type is not valid. If fs_type is None the method by default creates ext3 filesystem

--- salt/modules/parted.py
+++ salt/modules/parted.py
@@ -432,7 +432,7 @@ def mkpart(device, part_type, fs_type=None, start=None, end=None):
             'Invalid part_type passed to partition.mkpart'
         )
 
-    if fs_type not in set(['ext2', 'fat32', 'fat16', 'linux-swap', 'reiserfs',
+    if fs_type and fs_type not in set(['ext2', 'fat32', 'fat16', 'linux-swap', 'reiserfs',
                           'hfs', 'hfs+', 'hfsx', 'NTFS', 'ufs', 'xfs']):
         raise CommandExecutionError(
             'Invalid fs_type passed to partition.mkpart'
