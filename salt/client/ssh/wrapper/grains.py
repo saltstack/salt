fbbda12cc8f05ba19659cc4320d182224c94215e
Nitin Madhok <nmadhok@g.clemson.edu>
2014-11-12 15:50:06 -0500

    Removing all lists

--- salt/client/ssh/wrapper/grains.py
+++ salt/client/ssh/wrapper/grains.py
@@ -89,7 +89,7 @@ def items(sanitize=False):
     '''
     if salt.utils.is_true(sanitize):
         out = dict(__grains__)
-        for key, func in list(_SANITIZERS.items()):
+        for key, func in _SANITIZERS.items():
             if key in out:
                 out[key] = func(out[key])
         return out
@@ -121,7 +121,7 @@ def item(*args, **kwargs):
         except KeyError:
             pass
     if salt.utils.is_true(kwargs.get('sanitize')):
-        for arg, func in list(_SANITIZERS.items()):
+        for arg, func in _SANITIZERS.items():
             if arg in ret:
                 ret[arg] = func(ret[arg])
     return ret
