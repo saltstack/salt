f8c9508d89a39ae3a4ea751d165a17ea95e68bbc
Thomas Jackson <jacksontj.89@gmail.com>
2014-11-17 14:11:32 -0800

    Change log line to trace

--- salt/utils/__init__.py
+++ salt/utils/__init__.py
@@ -2359,7 +2359,7 @@ def import_json():
     for fast_json in ('ujson', 'yajl', 'json'):
         try:
             mod = __import__(fast_json)
-            log.info('loaded {0} json lib'.format(fast_json))
+            log.trace('loaded {0} json lib'.format(fast_json))
             return mod
         except ImportError:
             continue
