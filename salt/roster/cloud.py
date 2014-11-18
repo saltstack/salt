a4e142790da6e3a4f30d054beccaa1a52e18709b
Joseph Hall <perlhoser@gmail.com>
2014-11-18 10:22:25 -0700

    Cleanup for @s0undt3ch and lint

--- salt/roster/cloud.py
+++ salt/roster/cloud.py
@@ -22,7 +22,7 @@ import salt.utils.cloud
 import salt.utils.validate.net
 import salt.config
 from salt import syspaths
-from salt._compat import string_types
+from salt.utils.six import string_types
 
 
 def targets(tgt, tgt_type='glob', **kwargs):  # pylint: disable=W0613
