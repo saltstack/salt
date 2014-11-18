4c709ccd9d595b16cbaee4b462ab8e713c37fdf6
Nitin Madhok <nmadhok@g.clemson.edu>
2014-11-18 00:42:09 -0500

    Replaced module six in file /salt/utils/templates.py

--- salt/utils/templates.py
+++ salt/utils/templates.py
@@ -28,7 +28,7 @@ from salt.utils.jinja import SaltCacheLoader as JinjaSaltCacheLoader
 from salt.utils.jinja import SerializerExtension as JinjaSerializerExtension
 from salt.utils.odict import OrderedDict
 from salt import __path__ as saltpath
-from six import string_types
+from salt.utils.six import string_types
 import salt.utils.six as six
 
 log = logging.getLogger(__name__)
