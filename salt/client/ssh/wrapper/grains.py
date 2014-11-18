efa994e4452d8161accb2734bca27ae5409e8e79
Christophe Vedel <cv@scality.com>
2014-11-18 18:00:58 +0100

    Fix for #18136

--- salt/client/ssh/wrapper/grains.py
+++ salt/client/ssh/wrapper/grains.py
@@ -206,7 +206,7 @@ def filter_by(lookup_dict, grain='os_family', merge=None):
 
         salt '*' grains.filter_by '{Debian: Debheads rule, RedHat: I love my hat}'
     '''
-    ret = lookup_dict.get(__grains__[grain], None)
+    ret = lookup_dict.get(__grains__[grain], {})
 
     if merge:
         salt.utils.dictupdate.update(ret, merge)
