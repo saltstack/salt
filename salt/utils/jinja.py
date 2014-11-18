f147dd97ece8e2787fd93e020b47aa50947ad35f
Nitin Madhok <nmadhok@g.clemson.edu>
2014-11-18 00:42:03 -0500

    Replaced module six in file /salt/utils/jinja.py

--- salt/utils/jinja.py
+++ salt/utils/jinja.py
@@ -23,7 +23,7 @@ import yaml
 import salt
 import salt.fileclient
 from salt.utils.odict import OrderedDict
-from six import string_types
+from salt.utils.six import string_types
 
 log = logging.getLogger(__name__)
 
