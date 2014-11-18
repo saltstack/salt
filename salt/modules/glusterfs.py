e2e3d1a85761cb6e40b4bfbd763360a6b0da9650
Nitin Madhok <nmadhok@g.clemson.edu>
2014-11-18 00:43:26 -0500

    Replaced module six in file /salt/modules/glusterfs.py

--- salt/modules/glusterfs.py
+++ salt/modules/glusterfs.py
@@ -10,7 +10,7 @@ import logging
 # Import salt libs
 import salt.utils
 import salt.utils.cloud as suc
-from six.moves import range
+from salt.utils.six.moves import range
 
 log = logging.getLogger(__name__)
 
