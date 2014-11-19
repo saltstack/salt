26b9c9420715fb25bc30ecf29f7f5bff1879033c
C. R. Oldham <cro@ncbt.org>
2014-11-19 13:44:17 -0700

    Properly quote some parameters to sed

--- salt/modules/makeconf.py
+++ salt/modules/makeconf.py
@@ -41,7 +41,7 @@ def _add_var(var, value):
     if __salt__['file.contains'](makeconf, layman):
         # TODO perhaps make this a function in the file module?
         cmd = ['sed', '-i', '/{0}/'.format(layman.replace('/', '\\/')),
-              fullvar, makeconf]
+               fullvar, makeconf]
         __salt__['cmd.run'](cmd)
     else:
         __salt__['file.append'](makeconf, fullvar)
