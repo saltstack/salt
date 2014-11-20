6e0b8ccfeb95e46bf8c4907bada289bfad5179cc
Pedro Algarvio <pedro@algarvio.me>
2014-11-20 18:31:10 +0000

    Fix import

--- salt/modules/cyg.py
+++ salt/modules/cyg.py
@@ -13,7 +13,7 @@ import os
 import bz2
 
 # Import 3rd-party libs
-from salt.ext.six.urllib.request import urlopen as _urlopen  # pylint: disable=no-name-in-module,import-error
+from salt.ext.six.moves.urllib.request import urlopen as _urlopen  # pylint: disable=no-name-in-module,import-error
 
 # Import Salt libs
 import salt.utils
