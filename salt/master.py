9af0605a15cbbc6905d612f71c1aaea9410fd113
Pedro Algarvio <pedro@algarvio.me>
2014-11-20 23:11:30 +0000

    Py3 compatability fixes

--- salt/master.py
+++ salt/master.py
@@ -9,18 +9,23 @@ from __future__ import absolute_import
 # Import python libs
 import os
 import re
+import sys
 import time
 import errno
 import shutil
 import logging
 import hashlib
+import binascii
 import resource
 import multiprocessing
-import sys
 
 # Import third party libs
 import zmq
 from M2Crypto import RSA
+# pylint: disable=import-error,no-name-in-module,redefined-builtin
+import salt.exit.six as six
+from salt.ext.six.moves import range
+# pylint: enable=import-error,no-name-in-module,redefined-builtin
 
 # Import salt libs
 import salt.crypt
@@ -47,14 +52,12 @@ import salt.utils.process
 from salt.defaults import DEFAULT_TARGET_DELIM
 from salt.utils.debug import enable_sigusr1_handler, enable_sigusr2_handler, inspect_stack
 from salt.utils.event import tagify
-import binascii
 from salt.utils.master import ConnectedCache
 from salt.utils.cache import CacheCli
-from salt.ext.six.moves import range
 
 # Import halite libs
 try:
-    import halite
+    import halite  # pylint: disable=import-error
     HAS_HALITE = True
 except ImportError:
     HAS_HALITE = False
@@ -1193,7 +1196,7 @@ class AESFuncs(object):
             return False
         load['grains']['id'] = load['id']
         mods = set()
-        for func in self.mminion.functions.values():
+        for func in six.itervalues(self.mminion.functions):
             mods.add(func.__module__)
         for mod in mods:
             sys.modules[mod].__grains__ = load['grains']
@@ -1301,7 +1304,7 @@ class AESFuncs(object):
             self.mminion.returners[fstr](load['jid'], load)
 
         # Format individual return loads
-        for key, item in load['return'].items():
+        for key, item in six.iteritems(load['return']):
             ret = {'jid': load['jid'],
                    'id': key,
                    'return': item}
