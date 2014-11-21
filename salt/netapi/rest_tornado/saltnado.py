a86a0dd95c7d7f0448e085ca2f43f566437931a8
Pedro Algarvio <pedro@algarvio.me>
2014-11-21 22:30:51 +0000

    Py3 compatibility fixes

--- salt/netapi/rest_tornado/saltnado.py
+++ salt/netapi/rest_tornado/saltnado.py
@@ -146,25 +146,25 @@ a return like::
 '''
 # pylint: disable=W0232
 
+# Import Python libs
+from __future__ import absolute_import
+import time
+import math
+import fnmatch
 import logging
 from copy import copy
+from collections import defaultdict
 
-import time
-
+# pylint: disable=import-error
+import yaml
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 import tornado.gen
-
 from tornado.concurrent import Future
-
-from collections import defaultdict
-
-import math
-import yaml
-import fnmatch
-
 from zmq.eventloop import ioloop, zmqstream
+import salt.ext.six as six
+# pylint: enable=import-error
 
 # instantiate the zmq IOLoop (specialized poller)
 ioloop.install()
@@ -313,7 +313,7 @@ class EventListener(object):
         '''
         mtag, data = self.event.unpack(raw[0], self.event.serial)
         # see if we have any futures that need this info:
-        for tag_prefix, futures in self.tag_map.items():
+        for tag_prefix, futures in six.iteritems(self.tag_map):
             if mtag.startswith(tag_prefix):
                 for future in futures:
                     if future.done():
@@ -448,7 +448,7 @@ class BaseSaltAPIHandler(tornado.web.RequestHandler, SaltClientsMixIn):
         ignore the data passed in and just get the args from wherever they are
         '''
         data = {}
-        for key, val in self.request.arguments.iteritems():
+        for key, val in six.iteritems(self.request.arguments):
             if len(val) == 1:
                 data[key] = val[0]
             else:
@@ -686,7 +686,7 @@ class SaltAPIHandler(BaseSaltAPIHandler, SaltClientsMixIn):
 
             {"clients": ["local", "local_batch", "local_async","runner"], "return": "Welcome"}
         '''
-        ret = {"clients": self.saltclients.keys(),
+        ret = {"clients": list(self.saltclients.keys()),
                "return": "Welcome"}
         self.write(self.serialize(ret))
 
@@ -815,7 +815,7 @@ class SaltAPIHandler(BaseSaltAPIHandler, SaltClientsMixIn):
 
         if not isinstance(ping_ret, dict):
             raise tornado.gen.Return(chunk_ret)
-        minions = ping_ret.keys()
+        minions = list(ping_ret.keys())
 
         maxflight = get_batch_size(f_call['kwargs']['batch'], len(minions))
         inflight_futures = []
@@ -857,7 +857,7 @@ class SaltAPIHandler(BaseSaltAPIHandler, SaltClientsMixIn):
     @tornado.gen.coroutine
     def _disbatch_local(self, chunk):
         '''
-        Disbatch local client commands
+        Dispatch local client commands
         '''
         chunk_ret = {}
 
