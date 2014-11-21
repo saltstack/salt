6ada1fdf2001d4d10696d054eabef9a2de3324e6
Pedro Algarvio <pedro@algarvio.me>
2014-11-21 20:29:20 +0000

    Py3 compatibility fixes

--- tests/integration/states/pkgrepo.py
+++ tests/integration/states/pkgrepo.py
@@ -19,6 +19,9 @@ ensure_in_syspath('../../')
 import integration
 import salt.utils
 
+# Import 3rd-party libs
+import salt.ext.six as six
+
 
 class PkgrepoTest(integration.ModuleCase,
                   integration.SaltReturnAssertsMixIn):
@@ -44,7 +47,7 @@ class PkgrepoTest(integration.ModuleCase,
         # tests/integration/files/file/base/pkgrepo/managed.sls needs to be
         # corrected.
         self.assertReturnNonEmptySaltType(ret)
-        for state_id, state_result in ret.iteritems():
+        for state_id, state_result in six.iteritems(ret):
             self.assertSaltTrueReturn(dict([(state_id, state_result)]))
 
     @destructiveTest
@@ -59,7 +62,7 @@ class PkgrepoTest(integration.ModuleCase,
         # tests/integration/files/file/base/pkgrepo/absent.sls needs to be
         # corrected.
         self.assertReturnNonEmptySaltType(ret)
-        for state_id, state_result in ret.iteritems():
+        for state_id, state_result in six.iteritems(ret):
             self.assertSaltTrueReturn(dict([(state_id, state_result)]))
 
 
