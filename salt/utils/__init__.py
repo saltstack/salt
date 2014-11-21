f59f1933749006ebe39d54acda6d775da7dfa462
Thomas S Hatch <thatch45@gmail.com>
2014-11-21 12:05:22 -0700

    get the right locale

--- salt/utils/__init__.py
+++ salt/utils/__init__.py
@@ -2455,10 +2455,9 @@ def get_encodings():
     return a list of string encodings to try
     '''
     encodings = []
-    locales = locale.getdefaultlocale()
-    for loc in locales:
-        if loc:
-            encodings.append(loc)
+    loc = locale.getdefaultlocale()[-1]
+    if loc:
+        encodings.append(loc)
     encodings.append(sys.getdefaultencoding())
     encodings.extend(['utf-8', 'latin-1'])
     return encodings
