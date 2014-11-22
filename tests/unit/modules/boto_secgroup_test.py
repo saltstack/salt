4d55ad31625ce3ee7ab88bb171c1f8e4f747fdd9
Pedro Algarvio <pedro@algarvio.me>
2014-11-22 12:15:11 +0000

    Py3 compatibility fixes

--- tests/unit/modules/boto_secgroup_test.py
+++ tests/unit/modules/boto_secgroup_test.py
@@ -7,8 +7,16 @@ import string
 from copy import deepcopy
 from distutils.version import LooseVersion  # pylint: disable=import-error,no-name-in-module
 
-# import Python Third Party Libs
+# Import Salt Testing Libs
+from salttesting.unit import skipIf, TestCase
+from salttesting.mock import NO_MOCK, NO_MOCK_REASON
+from salttesting.helpers import ensure_in_syspath
+
+ensure_in_syspath('../../')
+
+# Import Third Party Libs
 # pylint: disable=import-error
+from salt.ext.six.moves import range  # pylint: disable=redefined-builtin
 try:
     import boto
     HAS_BOTO = True
@@ -37,12 +45,6 @@ except ImportError:
 from salt.utils.odict import OrderedDict
 from salt.modules import boto_secgroup
 
-# Import Salt Testing Libs
-from salttesting import skipIf, TestCase
-from salttesting.mock import NO_MOCK, NO_MOCK_REASON
-from salttesting.helpers import ensure_in_syspath
-
-ensure_in_syspath('../../')
 
 required_boto_version = '2.4.0'
 vpc_id = 'vpc-mjm05d27'
