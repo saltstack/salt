8521175368aee26b936584850d5ef9ab59f8d9b0
Pedro Algarvio <pedro@algarvio.me>
2014-11-22 13:08:27 +0000

    Py3 compatibility fixes.

--- salt/states/linux_acl.py
+++ salt/states/linux_acl.py
@@ -29,6 +29,9 @@ from __future__ import absolute_import
 # Import salt libs
 import salt.utils
 
+# Import 3rd-party libs
+import salt.ext.six as six
+
 __virtualname__ = 'acl'
 
 
@@ -56,7 +59,7 @@ def present(name, acl_type, acl_name, perms, recurse=False):
 
     if _current_perms[name].get(acl_type, None):
         try:
-            user = [i for i in _current_perms[name][acl_type] if i.keys()[0] == acl_name].pop()
+            user = [i for i in _current_perms[name][acl_type] if next(six.iterkeys(i)) == acl_name].pop()
         except IndexError:
             user = None
 
@@ -105,7 +108,7 @@ def absent(name, acl_type, acl_name, perms, recurse=False):
 
     if _current_perms[name].get(acl_type, None):
         try:
-            user = [i for i in _current_perms[name][acl_type] if i.keys()[0] == acl_name].pop()
+            user = [i for i in _current_perms[name][acl_type] if next(six.iterkeys(i)) == acl_name].pop()
         except IndexError:
             user = None
 
