a12ab1d077eb778493af6c06d2f26eda01316e7e
Pedro Algarvio <pedro@algarvio.me>
2014-11-22 11:21:28 +0000

    Py3 compatibility fixes

--- tests/integration/modules/disk.py
+++ tests/integration/modules/disk.py
@@ -6,13 +6,16 @@ import os
 import shutil
 
 # Import Salt Testing libs
+from salttesting import skipIf
 from salttesting.helpers import (ensure_in_syspath, destructiveTest)
 ensure_in_syspath('../../')
 
 # Import salt libs
 import integration
 import salt.utils
-from salttesting import skipIf
+
+# Import 3rd-party libs
+import salt.ext.six as six
 
 
 class DiskModuleVirtualizationTest(integration.ModuleCase):
@@ -53,7 +56,7 @@ class DiskModuleTest(integration.ModuleCase):
         self.assertTrue(isinstance(ret, dict))
         if not isinstance(ret, dict):
             return
-        for key, val in ret.items():
+        for key, val in six.iteritems(ret):
             self.assertTrue('filesystem' in val)
             self.assertTrue('1K-blocks' in val)
             self.assertTrue('used' in val)
@@ -68,7 +71,7 @@ class DiskModuleTest(integration.ModuleCase):
         self.assertTrue(isinstance(ret, dict))
         if not isinstance(ret, dict):
             return
-        for key, val in ret.items():
+        for key, val in six.iteritems(ret):
             self.assertTrue('inodes' in val)
             self.assertTrue('used' in val)
             self.assertTrue('free' in val)
