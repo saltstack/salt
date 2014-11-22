e05f1c36e20b41b9b55a3917af53e16f2e2a54e5
Sebastien Estienne <sebastien.estienne@gmail.com>
2014-11-22 12:04:55 -0800

    fixing some documentation in boto_* states

--- salt/states/boto_route53.py
+++ salt/states/boto_route53.py
@@ -31,7 +31,7 @@ as a passed in dict, or as a string to pull from pillars or minion config:
     myprofile:
         keyid: GKTADJGHEIQSXMKKRBJ08H
         key: askdjghsdfjkghWupUjasdflkdfklgjsdfjajkghs
-            region: us-east-1
+        region: us-east-1
 
 .. code-block:: yaml
 
