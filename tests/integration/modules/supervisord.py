f1e9ee093d038288a7f6b602e9e0c4014f3c6efd
Pedro Algarvio <pedro@algarvio.me>
2014-11-22 11:12:37 +0000

    Py3 compatibility fixes

--- tests/integration/modules/supervisord.py
+++ tests/integration/modules/supervisord.py
@@ -214,7 +214,7 @@ class SupervisordModuleTest(integration.ModuleCase):
         ret = self.run_function(
             'supervisord.status', [], conf_file=self.supervisor_conf,
             bin_env=self.venv_dir)
-        self.assertEqual(ret.keys(), ['sleep_service', 'sleep_service2'])
+        self.assertEqual(list(ret.keys()), ['sleep_service', 'sleep_service2'])
 
     def test_status_one(self):
         '''
