a3c02e1fa5b7604a0f184f405500380763bd4085
Justin Findlay <jfindlay@gmail.com>
2014-11-25 15:29:16 -0700

    quote strs in virtualenv mod

--- salt/modules/virtualenv_mod.py
+++ salt/modules/virtualenv_mod.py
@@ -9,6 +9,10 @@ import shutil
 import logging
 import os
 import os.path
+try:
+    from shlex import quote as _cmd_quote  # pylint: disable=E0611
+except ImportError:
+    from pipes import quote as _cmd_quote
 
 # Import salt libs
 import salt.utils
@@ -122,7 +126,7 @@ def create(path,
     elif runas is not None and not user:
         user = str(runas)
 
-    cmd = [venv_bin]
+    cmd = [_cmd_quote(venv_bin)]
 
     if 'pyvenv' not in venv_bin:
         # ----- Stop the user if pyvenv only options are used --------------->
@@ -150,7 +154,7 @@ def create(path,
             )
         except ImportError:
             # Unable to import?? Let's parse the version from the console
-            version_cmd = '{0} --version'.format(venv_bin)
+            version_cmd = '{0} --version'.format(_cmd_quote(venv_bin))
             ret = __salt__['cmd.run_all'](version_cmd, runas=user)
             if ret['retcode'] > 0 or not ret['stdout'].strip():
                 raise salt.exceptions.CommandExecutionError(
@@ -179,7 +183,7 @@ def create(path,
                     'Requested python ({0}) does not appear '
                     'executable.'.format(python)
                 )
-            cmd.append('--python={0}'.format(python))
+            cmd.append('--python={0}'.format(_cmd_quote(python)))
         if extra_search_dir is not None:
             if isinstance(extra_search_dir, string_types) and \
                     extra_search_dir.strip() != '':
@@ -187,7 +191,7 @@ def create(path,
                     e.strip() for e in extra_search_dir.split(',')
                 ]
             for entry in extra_search_dir:
-                cmd.append('--extra-search-dir={0}'.format(entry))
+                cmd.append('--extra-search-dir={0}'.format(_cmd_quote(entry)))
         if never_download is True:
             if virtualenv_version_info >= (1, 10):
                 log.info(
@@ -199,7 +203,7 @@ def create(path,
             else:
                 cmd.append('--never-download')
         if prompt is not None and prompt.strip() != '':
-            cmd.append('--prompt={0!r}'.format(prompt))
+            cmd.append('--prompt={0!r}'.format(_cmd_quote(prompt)))
     else:
         # venv module from the Python >= 3.3 standard library
 
@@ -240,7 +244,7 @@ def create(path,
         cmd.append('--system-site-packages')
 
     # Finally the virtualenv path
-    cmd.append(path)
+    cmd.append(_cmd_quote(path))
 
     # Let's create the virtualenv
     ret = __salt__['cmd.run_all'](' '.join(cmd), runas=user)
@@ -306,7 +310,7 @@ def get_site_packages(venv):
         raise salt.exceptions.CommandExecutionError(
             "Path does not appear to be a virtualenv: '{0}'".format(bin_path))
 
-    return __salt__['cmd.exec_code'](bin_path,
+    return __salt__['cmd.exec_code'](_cmd_quote(bin_path),
             'from distutils import sysconfig; print sysconfig.get_python_lib()')
 
 
@@ -323,7 +327,7 @@ def _install_script(source, cwd, python, user, saltenv='base'):
         os.chown(tmppath, __salt__['file.user_to_uid'](user), -1)
     try:
         return __salt__['cmd.run_all'](
-            '{0} {1}'.format(python, tmppath),
+            '{0} {1}'.format(_cmd_quote(python), _cmd_quote(tmppath)),
             runas=user,
             cwd=cwd,
             env={'VIRTUAL_ENV': cwd}
