4797099536027ce6559024fc1346c1d0ce903190
rallytime <nicole@saltstack.com>
2014-12-02 09:15:10 -0700

    If the asg does not exist in the region provided, don't return True.
    Refs #18566

--- salt/modules/boto_asg.py
+++ salt/modules/boto_asg.py
@@ -78,8 +78,13 @@ def exists(name, region=None, key=None, keyid=None, profile=None):
     if not conn:
         return False
     try:
-        conn.get_all_groups(names=[name])
-        return True
+        _conn = conn.get_all_groups(names=[name])
+        if _conn:
+            return True
+        else:
+            msg = 'The asg does not exist in region {0}'.format(region)
+            log.debug(msg)
+            return False
     except boto.exception.BotoServerError as e:
         log.debug(e)
         return False
