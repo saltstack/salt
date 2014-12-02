7e2811886a070c426d3024803e4f4626ed2c4303
Tomas Neme <lacrymology@gmail.com>
2014-12-02 13:09:44 +0800

    add mongodb.remove function

--- salt/modules/mongodb.py
+++ salt/modules/mongodb.py
@@ -295,3 +295,33 @@ def insert(objects, collection, user=None, password=None,
         return err.message
 
 
+def remove(collection, query=None, user=None, password=None,
+           host=None, port=None, database='admin', w=1):
+    """
+    Remove an object or list of objects into a collection
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' mongodb.remove mycollection '[{"foo": "FOO", "bar": "BAR"}, {"foo": "BAZ", "bar": "BAM"}]' <user> <password> <host> <port> <database>
+
+    """
+    conn = _connect(user, password, host, port)
+    if not conn:
+        return 'Failed to connect to mongo database'
+
+    try:
+        query = _to_dict(query)
+    except Exception, err:
+        return err.message
+
+    try:
+        log.info("Removing %r from %s", query, collection)
+        mdb = pymongo.database.Database(conn, database)
+        col = getattr(mdb, collection)
+        ret = col.remove(query, w=w)
+        return "%d objects removed" % ret['n']
+    except pymongo.errors.PyMongoError as err:
+        log.error("Removing objects failed with error: %s", err.message)
+        return err.message
