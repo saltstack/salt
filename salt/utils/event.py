8c7d66d6dc643bfbe4bd7c6e3b64b4630855df8a
Erik Johnson <palehose@gmail.com>
2014-12-04 14:31:35 -0600

    Revert "Historically the recator has just called the "async" method of the runner and wheel clients, but this actually creates daemonized processes. In addition to creating a new daemonized process each event, the number of process it creates is unbounded, meaning that the reactor can easily use all available PIDs on a fairly busy master. In addition, there is no bound on the CPU that these are allowed to use (since they can create ALL the pids). This changes the reactor to create a threadpool for executing its master-side clients (runner/wheel). This threadpool has a configurable number of workers (max parallelism) and hwm (max queue size before dropping events)."
    This reverts commit 2a8d2a462a5cecad3a4d7e88395f7a871a6bfdd9.
    
    Conflicts:
    	salt/utils/event.py

--- salt/utils/event.py
+++ salt/utils/event.py
@@ -73,7 +73,6 @@ import salt.loader
 import salt.state
 import salt.utils
 import salt.utils.cache
-import salt.utils.process
 from salt._compat import string_types
 log = logging.getLogger(__name__)
 
@@ -699,11 +698,6 @@ class ReactWrap(object):
         if ReactWrap.client_cache is None:
             ReactWrap.client_cache = salt.utils.cache.CacheDict(opts['reactor_refresh_interval'])
 
-        self.pool = salt.utils.process.ThreadPool(
-            self.opts['reactor_worker_threads'],  # number of workers for runner/wheel
-            queue_size=self.opts['reactor_worker_hwm']  # queue size for those workers
-        )
-
     def run(self, low):
         '''
         Execute the specified function in the specified state by passing the
@@ -712,12 +706,14 @@ class ReactWrap(object):
         l_fun = getattr(self, low['state'])
         try:
             f_call = salt.utils.format_call(l_fun, low)
-            l_fun(*f_call.get('args', ()), **f_call.get('kwargs', {}))
+            ret = l_fun(*f_call.get('args', ()), **f_call.get('kwargs', {}))
         except Exception:
             log.error(
                     'Failed to execute {0}: {1}\n'.format(low['state'], l_fun),
                     exc_info=True
                     )
+            return False
+        return ret
 
     def local(self, *args, **kwargs):
         '''
@@ -725,7 +721,7 @@ class ReactWrap(object):
         '''
         if 'local' not in self.client_cache:
             self.client_cache['local'] = salt.client.LocalClient(self.opts['conf_file'])
-        self.client_cache['local'].cmd_async(*args, **kwargs)
+        return self.client_cache['local'].cmd_async(*args, **kwargs)
 
     cmd = local
 
@@ -735,7 +731,7 @@ class ReactWrap(object):
         '''
         if 'runner' not in self.client_cache:
             self.client_cache['runner'] = salt.runner.RunnerClient(self.opts)
-        self.pool.fire_async(self.client_cache['runner'].low, kwargs)
+        return self.client_cache['runner'].async(fun, kwargs, fire_event=False)
 
     def wheel(self, fun, **kwargs):
         '''
@@ -743,7 +739,7 @@ class ReactWrap(object):
         '''
         if 'wheel' not in self.client_cache:
             self.client_cache['wheel'] = salt.wheel.Wheel(self.opts)
-        self.pool.fire_async(self.client_cache['wheel'].low, kwargs)
+        return self.client_cache['wheel'].async(fun, kwargs, fire_event=False)
 
 
 class StateFire(object):
