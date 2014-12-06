3e09438d0b713201bdd9b2ee852207531fa1907b
Clint Armstrong <clint@clintarmstrong.net>
2014-12-05 22:10:42 -0500

    Added sysrc state and execution modules

---
+++ salt/states/sysrc.py
@@ -0,0 +1,105 @@
+import salt.exceptions
+
+# define the module's virtual name
+
+def __virtual__():
+    '''
+    Only load if sysrc executable exists
+    '''
+    return __salt__['cmd.has_exec']('sysrc')
+
+def managed(name, value, **kwargs):
+    '''
+    Ensure a sysrc variable is set to a specific value.
+
+    name
+        The variable name to set
+    value
+        Value to set the variable to
+    file
+        (optional) The rc file to add the variable to.
+    jail
+        (option) the name or JID of the jail to set the value in.
+    '''
+
+    ret = {'name': name, 'changes': {}, 'result': False, 'comment': ''}
+
+    # Check the current state
+    current_state = __salt__['sysrc.get'](name=name, **kwargs)
+    if current_state != None:
+        for rcname, rcdict in current_state.iteritems():
+            if rcdict[name] == value:
+                ret['result'] = True
+                ret['comment'] = '{0} is already set to the desired value.'.format(name)
+                return ret
+
+    if __opts__['test'] == True:
+        ret['comment'] = 'The value of "{0}" will be changed!'.format(name)
+        ret['changes'] = {
+            'old': current_state,
+            'new': name+' = '+value+' will be set.'
+        }
+
+        # When test=true return none
+        ret['result'] = None
+
+        return ret
+
+    new_state = __salt__['sysrc.set'](name=name, value=value, **kwargs)
+
+    ret['comment'] = 'The value of "{0}" was changed!'.format(name)
+
+    ret['changes'] = {
+        'old': current_state,
+        'new': new_state
+    }
+
+    ret['result'] = True
+
+    return ret
+
+def absent(name, **kwargs):
+    '''
+    Ensure a sysrc variable is absent.
+
+    name
+        The variable name to set
+    file
+        (optional) The rc file to add the variable to.
+    jail
+        (option) the name or JID of the jail to set the value in.
+    '''
+
+    ret = {'name': name, 'changes': {}, 'result': False, 'comment': ''}
+
+    # Check the current state
+    current_state = __salt__['sysrc.get'](name=name, **kwargs)
+    if current_state == None:
+        ret['result'] = True
+        ret['comment'] = '"{0}" is already absent.'.format(name)
+        return ret
+
+    if __opts__['test'] == True:
+        ret['comment'] = '"{0}" will be removed!'.format(name)
+        ret['changes'] = {
+            'old': current_state,
+            'new': '"{0}" will be removed.'.format(name)
+        }
+
+        # When test=true return none
+        ret['result'] = None
+
+        return ret
+
+    new_state = __salt__['sysrc.remove'](name=name, **kwargs)
+
+    ret['comment'] = '"{0}" was removed!'.format(name)
+
+    ret['changes'] = {
+        'old': current_state,
+        'new': new_state
+    }
+
+    ret['result'] = True
+
+    return ret
