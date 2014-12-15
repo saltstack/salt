5d81614cd5975ff655d16f2c434b0400022415cd
Mike Place <mp@saltstack.com>
2014-12-15 10:59:08 -0700

    Restore warnings

--- salt/__init__.py
+++ salt/__init__.py
@@ -2,3 +2,25 @@
 '''
 Salt package
 '''
+import warnings
+# All salt related deprecation warnings should be shown once each!
+warnings.filterwarnings(
+        'once',  # Show once
+        '',  # No deprecation message match
+        DeprecationWarning,  # This filter is for DeprecationWarnings
+        r'^(salt|salt\.(.*))$'  # Match module(s) 'salt' and 'salt.<whatever>'
+)
+
+# While we are supporting Python2.6, hide nested with-statements warnings
+warnings.filterwarnings(
+ 'ignore',
+ 'With-statements now directly support multiple context managers',
+ DeprecationWarning
+)
+
+# Filter the backports package UserWarning about being re-imported
+warnings.filterwarnings(
+ 'ignore',
+ '^Module backports was already imported from (.*), but (.*) is being added to sys.path$',
+ UserWarning
+)
