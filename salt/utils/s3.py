saltstack/salt

commit 8ed7c92e798a4bb41efd007e42957a852df0160a

Switch ec2 and s3 from urllib2 to requests

salt/utils/s3.py
@@ -12,7 +12,8 @@ import json
 import logging
 import time
 import urllib
-import urllib2
+import requests
+import pprint
 
 # Import Salt libs
 import salt.utils
@@ -29,23 +30,15 @@ def _retry_get_url(url, num_retries=10, timeout=5):
     '''
     for i in range(0, num_retries):
         try:
-            # disable any environmental proxy settings
-            proxy_handler = urllib2.ProxyHandler({})
-            opener = urllib2.build_opener(proxy_handler)
-
-            req = urllib2.Request(url)
-
-            # timeout only in > 2.6
-            r = opener.open(req, timeout=timeout)
-            return r.read()
-        except urllib2.HTTPError:
+            result = requests.get(url)
+            return result.text
+        except requests.exceptions.HTTPError as exc:
             return ''
-        except urllib2.URLError:
-            pass
-        except Exception:
+        except Exception as exc:
             pass
 
         log.warning('Caught exception reading from URL. Retry no. {0}'.format(i))
+        log.warning(pprint.pformat(exc))
         time.sleep(2 ** i)
     log.error('Failed to read from URL for {0} times. Giving up.'.format(num_retries))
     return ''
@@ -201,40 +194,26 @@ def query(key, keyid, method='GET', params=None, headers=None,
         if querystring:
             requesturl += '?{0}'.format(querystring)
 
-    req = urllib2.Request(url=requesturl)
     if method == 'PUT':
         if local_file:
             with salt.utils.fopen(local_file, 'r') as ifile:
                 data = ifile.read()
-            req = urllib2.Request(url=requesturl, data=data)
-        req.get_method = lambda: 'PUT'
-    elif method == 'HEAD':
-        req.get_method = lambda: 'HEAD'
-    elif method == 'DELETE':
-        req.get_method = lambda: 'DELETE'
 
     log.debug('S3 Request: {0}'.format(requesturl))
     log.debug('S3 Headers::')
-    for header in sorted(headers.keys()):
-        if header == 'Authorization':
-            continue
-        req.add_header(header, headers[header])
-        log.debug('    {0}: {1}'.format(header, headers[header]))
     log.debug('    Authorization: {0}'.format(headers['Authorization']))
-    req.add_header('Authorization', headers['Authorization'])
 
     try:
-        result = urllib2.urlopen(req)
-        response = result.read()
-    except Exception as exc:
+        result = requests.request(method, requesturl, headers=headers)
+        response = result.text
+    except requests.exceptions.HTTPError as exc:
         log.error('There was an error::')
         if hasattr(exc, 'code') and hasattr(exc, 'msg'):
             log.error('    Code: {0}: {1}'.format(exc.code, exc.msg))
         log.error('    Content: \n{0}'.format(exc.read()))
         return False
 
-    log.debug('S3 Response Status Code: {0}'.format(result.getcode()))
-    result.close()
+    log.debug('S3 Response Status Code: {0}'.format(result.status_code))
 
     if method == 'PUT':
         if result.getcode() == 200:

salt/cloud/clouds/ec2.py
@@ -79,7 +79,7 @@ import hashlib
 import binascii
 import datetime
 import urllib
-import urllib2
+import requests
 
 # Import salt libs
 from salt._compat import ElementTree as ET
@@ -318,19 +318,19 @@ def query(params=None, setname=None, requesturl=None, location=None,
             sig = binascii.b2a_base64(hashed.digest())
             params['Signature'] = sig.strip()
 
-            querystring = urllib.urlencode(params)
-            requesturl = 'https://{0}/?{1}'.format(endpoint, querystring)
+            requesturl = 'https://{0}/'.format(endpoint)
 
         log.debug('EC2 Request: {0}'.format(requesturl))
         try:
-            result = urllib2.urlopen(requesturl)
+            result = requests.get(requesturl, params=params)
             log.debug(
                 'EC2 Response Status Code: {0}'.format(
-                    result.getcode()
+                    #result.getcode()
+                    result.status_code
                 )
             )
             break
-        except urllib2.URLError as exc:
+        except requests.exceptions.HTTPError as exc:
             root = ET.fromstring(exc.read())
             data = _xml_to_dict(root)
 
@@ -366,7 +366,7 @@ def query(params=None, setname=None, requesturl=None, location=None,
             return {'error': data}, requesturl
         return {'error': data}
 
-    response = result.read()
+    response = result.text
     result.close()
 
     root = ET.fromstring(response)

commit 0788cff5d63bbbb3e3c39d805ac0912012ee099b

Fix PUT/DELETE in s3.query
Was broken in 8ed7c92 during move to requests.

- PUT now uploads data
- Fixes AttributeError

salt/utils/s3.py
@@ -150,6 +150,7 @@ def query(key, keyid, method='GET', params=None, headers=None,
 
     try:
         result = requests.request(method, requesturl, headers=headers,
+                                  data=data,
                                   verify=verify_ssl)
         response = result.content
     except requests.exceptions.HTTPError as exc:
@@ -162,7 +163,7 @@ def query(key, keyid, method='GET', params=None, headers=None,
     log.debug('S3 Response Status Code: {0}'.format(result.status_code))
 
     if method == 'PUT':
-        if result.getcode() == 200:
+        if result.status_code == 200:
             if local_file:
                 log.debug('Uploaded from {0} to {1}'.format(local_file, path))
             else:
@@ -172,14 +173,14 @@ def query(key, keyid, method='GET', params=None, headers=None,
                 log.debug('Failed to upload from {0} to {1}: {2}'.format(
                                                     local_file,
                                                     path,
-                                                    result.getcode(),
+                                                    result.status_code,
                                                     ))
             else:
                 log.debug('Failed to create bucket {0}'.format(bucket))
         return
 
     if method == 'DELETE':
-        if str(result.getcode()).startswith('2'):
+        if str(result.status_code).startswith('2'):
             if path:
                 log.debug('Deleted {0} from bucket {1}'.format(path, bucket))
             else:
@@ -189,7 +190,7 @@ def query(key, keyid, method='GET', params=None, headers=None,
                 log.debug('Failed to delete {0} from bucket {1}: {2}'.format(
                                                     path,
                                                     bucket,
-                                                    result.getcode(),
+                                                    result.status_code,
                                                     ))
             else:
                 log.debug('Failed to delete bucket {0}'.format(bucket))