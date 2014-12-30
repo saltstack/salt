739254a4fae29ee1ba6def000b85b979608b6b2a
Mike Place <mp@saltstack.com>
2014-12-30 11:07:38 -0700

    MongoDB lint

--- salt/modules/mongodb.py
+++ salt/modules/mongodb.py
@@ -18,7 +18,7 @@ from __future__ import absolute_import
 # Import python libs
 import logging
 import json
-from distutils.version import StrictVersion
+import distutils.version
 
 # Import salt libs
 from salt.ext.six import string_types
@@ -158,7 +158,7 @@ def user_list(user=None, password=None, host=None, port=None, database='admin'):
         output = []
         mongodb_version = mdb.eval('db.version()')
 
-        if StrictVersion(mongodb_version) >= StrictVersion('2.6'):
+        if distutils.version.StrictVersion(mongodb_version) >= distutils.version.StrictVersion('2.6'):
             for user in mdb.eval('db.getUsers()'):
                 output.append([
                     ('user', user['user']),
