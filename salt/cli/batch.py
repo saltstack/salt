bf2b8de840d50d7907711de0737a6a7610b6832d
Mike Place <mp@saltstack.com>
2014-12-30 14:48:28 -0700

    Just use ckminions in batch mode.
    Refs #18358

--- salt/cli/batch.py
+++ salt/cli/batch.py
@@ -12,6 +12,7 @@ import copy
 # Import salt libs
 import salt.client
 import salt.output
+import salt.utils.minions
 from salt.utils import print_cli
 
 
@@ -30,25 +31,13 @@ class Batch(object):
         '''
         Return a list of minions to use for the batch run
         '''
-        args = [self.opts['tgt'],
-                'test.ping',
-                [],
-                5,
-                ]
-
+        ckminions = salt.utils.minions.CkMinions(self.opts)
         selected_target_option = self.opts.get('selected_target_option', None)
         if selected_target_option is not None:
-            args.append(selected_target_option)
+            expr_form = selected_target_option
         else:
-            args.append(self.opts.get('expr_form', 'glob'))
-
-        fret = []
-        for ret in self.local.cmd_iter(*args, **self.eauth):
-            for minion in ret:
-                if not self.quiet:
-                    print_cli('{0} Detected for this batch run'.format(minion))
-                fret.append(minion)
-        return sorted(fret)
+            expr_form = self.opts.get('expr_form', 'glob')
+        return ckminions.check_minions(self.opts['tgt'], expr_form=expr_form)
 
     def get_bnum(self):
         '''
