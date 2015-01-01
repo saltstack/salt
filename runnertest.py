a70b3906cc115363b6b992657e4931836213b535
Thomas Jackson <jacksontj.89@gmail.com>
2014-12-31 17:47:59 -0800

    Remove local testing file

--- runnertest.py
+++
@@ -1,7 +0,0 @@
-import salt.config
-import salt.runner
-
-opts = salt.config.master_config('/etc/salt/master')
-runner = salt.runner.RunnerClient(opts)
-print runner.cmd('test.stream', [])
-print runner.cmd('jobs.list_jobs', [])
