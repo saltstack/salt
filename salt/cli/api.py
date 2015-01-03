434376cd69fb265dded4e1a4b2d0a30e74f4e3a0
C. R. Oldham <cro@ncbt.org>
2015-01-03 09:09:38 -0700

    Use salt.ext.six instead of "import six"

--- salt/cli/api.py
+++ salt/cli/api.py
@@ -2,7 +2,7 @@
 from __future__ import print_function
 from __future__ import absolute_import
 
-import six
+import salt.ext.six
 import sys
 import logging
 
--- salt/utils/yamlencoding.py
+++ salt/utils/yamlencoding.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import io
 import yaml
-import six
+import salt.ext.six
 
 
 def yaml_dquote(text):
