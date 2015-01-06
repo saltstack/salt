dfeb42d15f163087fe98c7cdf6cac9653d9236c5
Justin Findlay <jfindlay@gmail.com>
2015-01-06 15:59:49 -0700

    split win_servermanager commands

--- salt/modules/win_servermanager.py
+++ salt/modules/win_servermanager.py
@@ -21,7 +21,10 @@ def _srvmgr(func):
     '''
     Execute a function from the ServerManager PS module and return the STDOUT
     '''
-    return __salt__['cmd.run']('Import-Module ServerManager ; {0}'.format(func), shell='powershell')
+    return __salt__['cmd.run'](
+            'Import-Module ServerManager ; {0}'.format(func),
+            shell='powershell',
+            python_shell=True)
 
 
 def _parse_powershell_list(lst):
