eff122c0b75d08a0cac4021d66df77d0b7b103c0
Thomas S Hatch <thatch45@gmail.com>
2015-01-07 15:32:27 -0700

    kmod shell migration

--- salt/modules/kmod.py
+++ salt/modules/kmod.py
@@ -219,7 +219,7 @@ def load(mod, persist=False):
         salt '*' kmod.load kvm
     '''
     pre_mods = lsmod()
-    response = __salt__['cmd.run_all']('modprobe {0}'.format(mod))
+    response = __salt__['cmd.run_all']('modprobe {0}'.format(mod), python_shell=False)
     if response['retcode'] == 0:
         post_mods = lsmod()
         mods = _new_mods(pre_mods, post_mods)
@@ -265,7 +265,7 @@ def remove(mod, persist=False, comment=True):
         salt '*' kmod.remove kvm
     '''
     pre_mods = lsmod()
-    __salt__['cmd.run_all']('modprobe -r {0}'.format(mod))
+    __salt__['cmd.run_all']('modprobe -r {0}'.format(mod), python_shell=False)
     post_mods = lsmod()
     mods = _rm_mods(pre_mods, post_mods)
     persist_mods = set()
