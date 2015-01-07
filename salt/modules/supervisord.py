3d8d355849a5768bc15dd8d12309cf45ac0526e1
Thomas S Hatch <thatch45@gmail.com>
2015-01-07 11:35:13 -0700

    shelism migration for supervisord

--- salt/modules/supervisord.py
+++ salt/modules/supervisord.py
@@ -46,6 +46,9 @@ def _get_supervisorctl_bin(bin_env):
 
 
 def _ctl_cmd(cmd, name, conf_file, bin_env):
+    '''
+    Return the command list to use
+    '''
     ret = [_get_supervisorctl_bin(bin_env)]
     if conf_file is not None:
         ret += ['-c', conf_file]
@@ -85,7 +88,9 @@ def start(name='all', user=None, conf_file=None, bin_env=None):
     if name.endswith(':*'):
         name = name[:-1]
     ret = __salt__['cmd.run_all'](
-        _ctl_cmd('start', name, conf_file, bin_env), runas=user
+        _ctl_cmd('start', name, conf_file, bin_env),
+        runas=user,
+        python_shell=False,
     )
     return _get_return(ret)
 
@@ -113,7 +118,9 @@ def restart(name='all', user=None, conf_file=None, bin_env=None):
     if name.endswith(':*'):
         name = name[:-1]
     ret = __salt__['cmd.run_all'](
-        _ctl_cmd('restart', name, conf_file, bin_env), runas=user
+        _ctl_cmd('restart', name, conf_file, bin_env),
+        runas=user,
+        python_shell=False,
     )
     return _get_return(ret)
 
@@ -141,7 +148,9 @@ def stop(name='all', user=None, conf_file=None, bin_env=None):
     if name.endswith(':*'):
         name = name[:-1]
     ret = __salt__['cmd.run_all'](
-        _ctl_cmd('stop', name, conf_file, bin_env), runas=user
+        _ctl_cmd('stop', name, conf_file, bin_env),
+        runas=user,
+        python_shell=False,
     )
     return _get_return(ret)
 
@@ -169,7 +178,9 @@ def add(name, user=None, conf_file=None, bin_env=None):
     elif name.endswith(':*'):
         name = name[:-2]
     ret = __salt__['cmd.run_all'](
-        _ctl_cmd('add', name, conf_file, bin_env), runas=user
+        _ctl_cmd('add', name, conf_file, bin_env),
+        runas=user,
+        python_shell=False,
     )
     return _get_return(ret)
 
@@ -197,7 +208,9 @@ def remove(name, user=None, conf_file=None, bin_env=None):
     elif name.endswith(':*'):
         name = name[:-2]
     ret = __salt__['cmd.run_all'](
-        _ctl_cmd('remove', name, conf_file, bin_env), runas=user
+        _ctl_cmd('remove', name, conf_file, bin_env),
+        runas=user,
+        python_shell=False,
     )
     return _get_return(ret)
 
@@ -221,7 +234,9 @@ def reread(user=None, conf_file=None, bin_env=None):
         salt '*' supervisord.reread
     '''
     ret = __salt__['cmd.run_all'](
-        _ctl_cmd('reread', None, conf_file, bin_env), runas=user
+        _ctl_cmd('reread', None, conf_file, bin_env),
+        runas=user,
+        python_shell=False,
     )
     return _get_return(ret)
 
@@ -245,7 +260,9 @@ def update(user=None, conf_file=None, bin_env=None):
         salt '*' supervisord.update
     '''
     ret = __salt__['cmd.run_all'](
-        _ctl_cmd('update', None, conf_file, bin_env), runas=user
+        _ctl_cmd('update', None, conf_file, bin_env),
+        runas=user,
+        python_shell=False,
     )
     return _get_return(ret)
 
@@ -297,7 +314,9 @@ def status_raw(name=None, user=None, conf_file=None, bin_env=None):
         salt '*' supervisord.status_raw
     '''
     ret = __salt__['cmd.run_all'](
-        _ctl_cmd('status', name, conf_file, bin_env), runas=user
+        _ctl_cmd('status', name, conf_file, bin_env),
+        runas=user,
+        python_shell=False,
     )
     return _get_return(ret)
 
@@ -321,7 +340,9 @@ def custom(command, user=None, conf_file=None, bin_env=None):
         salt '*' supervisord.custom "mstop '*gunicorn*'"
     '''
     ret = __salt__['cmd.run_all'](
-        _ctl_cmd(command, None, conf_file, bin_env), runas=user
+        _ctl_cmd(command, None, conf_file, bin_env),
+        runas=user,
+        python_shell=False,
     )
     return _get_return(ret)
 
