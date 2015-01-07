f62e19c7bb79ead7f0d0b27e9c0e4373fc85d5d7
Thomas S Hatch <thatch45@gmail.com>
2015-01-07 14:23:03 -0700

    nftables migration

--- salt/modules/nftables.py
+++ salt/modules/nftables.py
@@ -286,7 +286,7 @@ def get_rules(family='ipv4'):
     rules = []
     cmd = '{0} --numeric --numeric --numeric list tables {1}'.\
         format(_nftables_cmd(), nft_family)
-    out = __salt__['cmd.run'](cmd)
+    out = __salt__['cmd.run'](cmd, python_shell=False)
     if not out:
         return rules
 
@@ -295,7 +295,7 @@ def get_rules(family='ipv4'):
         table_name = table.split(' ')[1]
         cmd = '{0} --numeric --numeric --numeric list table {1} {2}'.format(_nftables_cmd(),
                 nft_family, table_name)
-        out = __salt__['cmd.run'](cmd)
+        out = __salt__['cmd.run'](cmd, python_shell=False)
         rules.append(out)
     return rules
 
@@ -374,7 +374,7 @@ def get_rule_handle(table='filter', chain=None, rule=None, family='ipv4'):
     nft_family = _NFTABLES_FAMILIES[family]
     cmd = '{0} --numeric --numeric --numeric --handle list chain {1} {2} {3}'.\
         format(_nftables_cmd(), nft_family, table, chain)
-    out = __salt__['cmd.run'](cmd)
+    out = __salt__['cmd.run'](cmd, python_shell=False)
     rules = re.split('\n+', out)
 
     pat = re.compile(r'{0} # handle (?P<handle>\d+)'.format(rule))
@@ -423,7 +423,7 @@ def check(table='filter', chain=None, rule=None, family='ipv4'):
     cmd = '{0} --handle --numeric --numeric --numeric list chain {1} {2} {3}'.\
             format(_nftables_cmd(), nft_family, table, chain)
     search_rule = '{0} #'.format(rule)
-    out = __salt__['cmd.run'](cmd).find(search_rule)
+    out = __salt__['cmd.run'](cmd, python_shell=False).find(search_rule)
 
     if out != -1:
         out = ''
@@ -456,7 +456,7 @@ def check_chain(table='filter', chain=None, family='ipv4'):
 
     nft_family = _NFTABLES_FAMILIES[family]
     cmd = '{0} list table {1} {2}' . format(_nftables_cmd(), nft_family, table)
-    out = __salt__['cmd.run'](cmd).find('chain {0} {{'.format(chain))
+    out = __salt__['cmd.run'](cmd, python_shell=False).find('chain {0} {{'.format(chain))
 
     if out != -1:
         out = ''
@@ -481,7 +481,7 @@ def check_table(table=None, family='ipv4'):
 
     nft_family = _NFTABLES_FAMILIES[family]
     cmd = '{0} list tables {1}' . format(_nftables_cmd(), nft_family)
-    out = __salt__['cmd.run'](cmd).find('table {0}'.format(table))
+    out = __salt__['cmd.run'](cmd, python_shell=False).find('table {0}'.format(table))
 
     if out != -1:
         out = ''
@@ -518,7 +518,7 @@ def new_table(table, family='ipv4'):
 
     nft_family = _NFTABLES_FAMILIES[family]
     cmd = '{0} add table {1} {2}'.format(_nftables_cmd(), nft_family, table)
-    out = __salt__['cmd.run'](cmd)
+    out = __salt__['cmd.run'](cmd, python_shell=False)
 
     if not out:
         out = True
@@ -549,7 +549,7 @@ def delete_table(table, family='ipv4'):
 
     nft_family = _NFTABLES_FAMILIES[family]
     cmd = '{0} delete table {1} {2}'.format(_nftables_cmd(), nft_family, table)
-    out = __salt__['cmd.run'](cmd)
+    out = __salt__['cmd.run'](cmd, python_shell=False)
 
     if not out:
         out = True
@@ -604,7 +604,7 @@ def new_chain(table='filter', chain=None, table_type=None, hook=None, priority=N
             # Specifiy one, rqeuire all
             return 'Error: table_type hook and priority required'
 
-    out = __salt__['cmd.run'](cmd)
+    out = __salt__['cmd.run'](cmd, python_shell=False)
 
     if not out:
         out = True
@@ -645,7 +645,7 @@ def delete_chain(table='filter', chain=None, family='ipv4'):
     nft_family = _NFTABLES_FAMILIES[family]
     cmd = '{0} delete chain {1} {2} {3}'.\
             format(_nftables_cmd(), nft_family, table, chain)
-    out = __salt__['cmd.run'](cmd)
+    out = __salt__['cmd.run'](cmd, python_shell=False)
 
     if not out:
         out = True
@@ -693,7 +693,7 @@ def append(table='filter', chain=None, rule=None, family='ipv4'):
     nft_family = _NFTABLES_FAMILIES[family]
     cmd = '{0} add rule {1} {2} {3} {4}'.\
             format(_nftables_cmd(), nft_family, table, chain, rule)
-    out = __salt__['cmd.run'](cmd)
+    out = __salt__['cmd.run'](cmd, python_shell=False)
     if len(out) == 0:
         return True
     else:
@@ -754,7 +754,7 @@ def insert(table='filter', chain=None, position=None, rule=None, family='ipv4'):
     else:
         cmd = '{0} insert rule {1} {2} {3} {4}'.\
                 format(_nftables_cmd(), nft_family, table, chain, rule)
-    out = __salt__['cmd.run'](cmd)
+    out = __salt__['cmd.run'](cmd, python_shell=False)
 
     if len(out) == 0:
         return True
@@ -812,7 +812,7 @@ def delete(table, chain=None, position=None, rule=None, family='ipv4'):
     nft_family = _NFTABLES_FAMILIES[family]
     cmd = '{0} delete rule {1} {2} {3} handle {4}'.\
             format(_nftables_cmd(), nft_family, table, chain, position)
-    out = __salt__['cmd.run'](cmd)
+    out = __salt__['cmd.run'](cmd, python_shell=False)
 
     if len(out) == 0:
         return True
@@ -851,7 +851,7 @@ def flush(table='filter', chain='', family='ipv4'):
     else:
         cmd = '{0} flush table {1} {2}'.\
                 format(_nftables_cmd(), nft_family, table)
-    out = __salt__['cmd.run'](cmd)
+    out = __salt__['cmd.run'](cmd, python_shell=False)
 
     if len(out) == 0:
         return True
