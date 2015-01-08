ab23bf03324670ee2c2e58ec765ef8ede524984d
Erik Johnson <palehose@gmail.com>
2015-01-08 13:50:15 -0600

    Update archive tests to use python_shell=False

--- tests/unit/modules/archive_test.py
+++ tests/unit/modules/archive_test.py
@@ -31,14 +31,13 @@ class ArchiveTestCase(TestCase):
                 'zcvf', 'foo.tar',
                 ['/tmp/something-to-compress-1',
                  '/tmp/something-to-compress-2'],
-                cwd=None, template=None
+                template=None, cwd=None
             )
             self.assertEqual(['salt'], ret)
             mock.assert_called_once_with(
-                'tar -zcvf foo.tar /tmp/something-to-compress-1 '
-                '/tmp/something-to-compress-2',
-                cwd=None,
-                template=None
+                ['tar', '-zcvf', 'foo.tar', '/tmp/something-to-compress-1',
+                 '/tmp/something-to-compress-2'],
+                python_shell=False, template=None, cwd=None
             )
 
         mock = MagicMock(return_value='salt')
@@ -46,14 +45,13 @@ class ArchiveTestCase(TestCase):
             ret = archive.tar(
                 'zcvf', 'foo.tar',
                 '/tmp/something-to-compress-1,/tmp/something-to-compress-2',
-                cwd=None, template=None
+                template=None, cwd=None
             )
             self.assertEqual(['salt'], ret)
             mock.assert_called_once_with(
-                'tar -zcvf foo.tar /tmp/something-to-compress-1 '
-                '/tmp/something-to-compress-2',
-                cwd=None,
-                template=None
+                ['tar', '-zcvf', 'foo.tar', '/tmp/something-to-compress-1',
+                 '/tmp/something-to-compress-2'],
+                python_shell=False, template=None, cwd=None
             )
 
     @patch('salt.utils.which', lambda exe: None)
@@ -76,8 +74,8 @@ class ArchiveTestCase(TestCase):
             ret = archive.gzip('/tmp/something-to-compress')
             self.assertEqual(['salt'], ret)
             mock.assert_called_once_with(
-                'gzip /tmp/something-to-compress',
-                template=None
+                ['gzip', '/tmp/something-to-compress'],
+                python_shell=False, template=None
             )
 
     @patch('salt.utils.which', lambda exe: None)
@@ -97,8 +95,8 @@ class ArchiveTestCase(TestCase):
             ret = archive.gunzip('/tmp/something-to-decompress.tar.gz')
             self.assertEqual(['salt'], ret)
             mock.assert_called_once_with(
-                'gunzip /tmp/something-to-decompress.tar.gz',
-                template=None
+                ['gunzip', '/tmp/something-to-decompress.tar.gz'],
+                python_shell=False, template=None
             )
 
     @patch('salt.utils.which', lambda exe: None)
@@ -119,13 +117,13 @@ class ArchiveTestCase(TestCase):
             ret = archive.zip_(
                 '/tmp/salt.{{grains.id}}.zip',
                 '/tmp/tmpePe8yO,/tmp/tmpLeSw1A',
-                template='jinja'
+                template='jinja', cwd=None
             )
             self.assertEqual(['salt'], ret)
             mock.assert_called_once_with(
-                'zip /tmp/salt.{{grains.id}}.zip '
-                '/tmp/tmpePe8yO /tmp/tmpLeSw1A',
-                template='jinja'
+                ['zip', '/tmp/salt.{{grains.id}}.zip',
+                 '/tmp/tmpePe8yO', '/tmp/tmpLeSw1A'],
+                 python_shell=False, template='jinja', cwd=None
             )
 
         mock = MagicMock(return_value='salt')
@@ -133,13 +131,13 @@ class ArchiveTestCase(TestCase):
             ret = archive.zip_(
                 '/tmp/salt.{{grains.id}}.zip',
                 ['/tmp/tmpePe8yO', '/tmp/tmpLeSw1A'],
-                template='jinja'
+                template='jinja', cwd=None
             )
             self.assertEqual(['salt'], ret)
             mock.assert_called_once_with(
-                'zip /tmp/salt.{{grains.id}}.zip '
-                '/tmp/tmpePe8yO /tmp/tmpLeSw1A',
-                template='jinja'
+                ['zip', '/tmp/salt.{{grains.id}}.zip',
+                 '/tmp/tmpePe8yO', '/tmp/tmpLeSw1A'],
+                python_shell=False, template='jinja', cwd=None
             )
 
     @patch('salt.utils.which', lambda exe: None)
@@ -167,9 +165,9 @@ class ArchiveTestCase(TestCase):
             )
             self.assertEqual(['salt'], ret)
             mock.assert_called_once_with(
-                'unzip /tmp/salt.{{grains.id}}.zip -d /tmp/dest '
-                '-x /tmp/tmpePe8yO /tmp/tmpLeSw1A',
-                template='jinja'
+                ['unzip', '/tmp/salt.{{grains.id}}.zip', '-d', '/tmp/dest',
+                 '-x', '/tmp/tmpePe8yO', '/tmp/tmpLeSw1A'],
+                python_shell=False, template='jinja'
             )
 
         mock = MagicMock(return_value='salt')
@@ -182,9 +180,9 @@ class ArchiveTestCase(TestCase):
             )
             self.assertEqual(['salt'], ret)
             mock.assert_called_once_with(
-                'unzip /tmp/salt.{{grains.id}}.zip -d /tmp/dest '
-                '-x /tmp/tmpePe8yO /tmp/tmpLeSw1A',
-                template='jinja'
+                ['unzip', '/tmp/salt.{{grains.id}}.zip', '-d', '/tmp/dest',
+                 '-x', '/tmp/tmpePe8yO', '/tmp/tmpLeSw1A'],
+                python_shell=False, template='jinja'
             )
 
         mock = MagicMock(return_value='salt')
@@ -198,9 +196,9 @@ class ArchiveTestCase(TestCase):
             )
             self.assertEqual(['salt'], ret)
             mock.assert_called_once_with(
-                'unzip -fo /tmp/salt.{{grains.id}}.zip -d /tmp/dest '
-                '-x /tmp/tmpePe8yO /tmp/tmpLeSw1A',
-                template='jinja',
+                ['unzip', '-fo', '/tmp/salt.{{grains.id}}.zip', '-d',
+                 '/tmp/dest', '-x', '/tmp/tmpePe8yO', '/tmp/tmpLeSw1A'],
+                python_shell=False, template='jinja'
             )
 
         mock = MagicMock(return_value='salt')
@@ -214,9 +212,9 @@ class ArchiveTestCase(TestCase):
             )
             self.assertEqual(['salt'], ret)
             mock.assert_called_once_with(
-                'unzip -fo /tmp/salt.{{grains.id}}.zip -d /tmp/dest '
-                '-x /tmp/tmpePe8yO /tmp/tmpLeSw1A',
-                template='jinja'
+                ['unzip', '-fo', '/tmp/salt.{{grains.id}}.zip', '-d',
+                 '/tmp/dest', '-x', '/tmp/tmpePe8yO', '/tmp/tmpLeSw1A'],
+                python_shell=False, template='jinja'
             )
 
     @patch('salt.utils.which', lambda exe: None)
@@ -243,9 +241,9 @@ class ArchiveTestCase(TestCase):
             )
             self.assertEqual(['salt'], ret)
             mock.assert_called_once_with(
-                'rar a -idp /tmp/rarfile.rar '
-                '/tmp/sourcefile1 /tmp/sourcefile2',
-                template=None
+                ['rar', 'a', '-idp', '/tmp/rarfile.rar',
+                 '/tmp/sourcefile1', '/tmp/sourcefile2'],
+                python_shell=False, template=None, cwd=None
             )
 
         mock = MagicMock(return_value='salt')
@@ -256,9 +254,9 @@ class ArchiveTestCase(TestCase):
             )
             self.assertEqual(['salt'], ret)
             mock.assert_called_once_with(
-                'rar a -idp /tmp/rarfile.rar '
-                '/tmp/sourcefile1 /tmp/sourcefile2',
-                template=None
+                ['rar', 'a', '-idp', '/tmp/rarfile.rar',
+                 '/tmp/sourcefile1', '/tmp/sourcefile2'],
+                python_shell=False, template=None, cwd=None
             )
 
     @patch('salt.utils.which', lambda exe: None)
@@ -285,9 +283,9 @@ class ArchiveTestCase(TestCase):
             )
             self.assertEqual(['salt'], ret)
             mock.assert_called_once_with(
-                'unrar x -idp /tmp/rarfile.rar '
-                '-x file_1 -x file_2 /home/strongbad/',
-                template=None
+                ['unrar', 'x', '-idp', '/tmp/rarfile.rar',
+                 '-x', 'file_1', '-x', 'file_2', '/home/strongbad/'],
+                python_shell=False, template=None
             )
 
         mock = MagicMock(return_value='salt')
@@ -299,9 +297,9 @@ class ArchiveTestCase(TestCase):
             )
             self.assertEqual(['salt'], ret)
             mock.assert_called_once_with(
-                'unrar x -idp /tmp/rarfile.rar '
-                '-x file_1 -x file_2 /home/strongbad/',
-                template=None
+                ['unrar', 'x', '-idp', '/tmp/rarfile.rar',
+                 '-x', 'file_1', '-x', 'file_2', '/home/strongbad/'],
+                python_shell=False, template=None
             )
 
     @patch('salt.utils.which_bin', lambda exe: None)
