941f196a0ee9572a09cdb46502e2b7df4baeec86
Justin Findlay <jfindlay@gmail.com>
2015-01-10 17:26:15 -0700

    fix hg cmds

--- salt/modules/hg.py
+++ salt/modules/hg.py
@@ -44,7 +44,7 @@ def revision(cwd, rev='tip', short=False, user=None):
         short=' --debug' if not short else '',
         rev=' -r {0}'.format(rev))
 
-    result = __salt__['cmd.run_all'](cmd, cwd=cwd, runas=user)
+    result = __salt__['cmd.run_all'](cmd, python_shell=False, cwd=cwd, runas=user)
 
     if result['retcode'] == 0:
         return result['stdout']
@@ -75,7 +75,7 @@ def describe(cwd, rev='tip', user=None):
 
     cmd = "hg log -r {0} --template"\
             " '{{latesttag}}-{{latesttagdistance}}-{{node|short}}'".format(rev)
-    desc = __salt__['cmd.run_stdout'](cmd, cwd=cwd, runas=user)
+    desc = __salt__['cmd.run_stdout'](cmd, python_shell=False, cwd=cwd, runas=user)
 
     return desc or revision(cwd, rev, short=True)
 
@@ -120,7 +120,7 @@ def archive(cwd, output, rev='tip', fmt=None, prefix=None, user=None):
         fmt=' --type {0}'.format(fmt) if fmt else '',
         prefix=' --prefix "{0}"'.format(prefix if prefix else ''))
 
-    return __salt__['cmd.run'](cmd, cwd=cwd, runas=user)
+    return __salt__['cmd.run'](cmd, python_shell=False, cwd=cwd, runas=user)
 
 
 def pull(cwd, opts=None, user=None):
@@ -146,7 +146,7 @@ def pull(cwd, opts=None, user=None):
 
     if not opts:
         opts = ''
-    return __salt__['cmd.run']('hg pull {0}'.format(opts), cwd=cwd, runas=user)
+    return __salt__['cmd.run']('hg pull {0}'.format(opts), python_shell=False, cwd=cwd, runas=user)
 
 
 def update(cwd, rev, force=False, user=None):
@@ -174,7 +174,7 @@ def update(cwd, rev, force=False, user=None):
     _check_hg()
 
     cmd = 'hg update {0}{1}'.format(rev, ' -C' if force else '')
-    return __salt__['cmd.run'](cmd, cwd=cwd, runas=user)
+    return __salt__['cmd.run'](cmd, python_shell=False, cwd=cwd, runas=user)
 
 
 def clone(cwd, repository, opts=None, user=None):
@@ -204,4 +204,4 @@ def clone(cwd, repository, opts=None, user=None):
     if not opts:
         opts = ''
     cmd = 'hg clone {0} {1} {2}'.format(repository, cwd, opts)
-    return __salt__['cmd.run'](cmd, runas=user)
+    return __salt__['cmd.run'](cmd, python_shell=False, runas=user)
