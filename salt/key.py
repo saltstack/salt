fd98239ad14afcc296a015662282df9d2a90f88c
Thomas S Hatch <thatch45@gmail.com>
2015-01-16 14:03:30 -0700

    Make a cli system for the multi key interface

--- salt/key.py
+++ salt/key.py
@@ -40,12 +40,77 @@ def get_key(opts):
         return RaetKey(opts)
 
 
+class MultiKeyCLI(object):
+    '''
+    Manage multiple key backends from the CLI
+    '''
+    def __init__(self, opts):
+        self.opts = opts
+        self.keys = {'RAET Keys': KeyCLI(self.opts, 'raet'),
+                'ZMQ Keys': KeyCLI(self.opts, 'zeromq')}
+
+    def _call_all(self, fun, *args):
+        '''
+        Call the given function on all backend keys
+        '''
+        for kback in self.keys:
+            print(kback)
+            getattr(self.keys[kback], fun)(*args)
+
+    def list_status(self, status):
+        self._call_all('list_status', status)
+
+    def list_all(self):
+        self._call_all('list_all')
+
+    def accept(self, match, include_rejected=False):
+        self._call_all('accept', match, include_rejected)
+
+    def accept_all(self, include_rejected=False):
+        self._call_all('accept_all'. include_rejected)
+
+    def delete(self, match):
+        self._call_all('delete', match)
+
+    def delete_all(self):
+        self._call_all('delete_all')
+
+    def reject(self, match, include_accepted=False):
+        self._call_all('reject', match, include_accepted)
+
+    def reject_all(self, include_accepted=False):
+        self._call_all('reject_all', include_accepted)
+
+    def print_key(self, match):
+        self._call_all('print_key', match)
+
+    def print_all(self):
+        self._call_all('print_all')
+
+    def finger(self, match):
+        self._call_all('finger', match)
+
+    def finger_all(self):
+        self._call_all('finger_all')
+
+    def prep_signature(self):
+        self._call_all('prep_signature')
+
+    def run(self):
+        self.keys['RAET Keys'].run()
+
+
 class KeyCLI(object):
     '''
     Manage key CLI operations
     '''
-    def __init__(self, opts):
+    def __init__(self, opts, backend=None):
         self.opts = opts
+        if backend:
+            if backend == 'zeromq':
+                self.key = Key(opts)
+            else:
+                self.key = RaetKey(opts)
         if self.opts['transport'] == 'zeromq':
             self.key = Key(opts)
         else:
