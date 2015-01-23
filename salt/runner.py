156b55d748be827466869040632c1947deb63dc1
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2015-01-23 19:11:35 +0100

    pylint

--- salt/runner.py
+++ salt/runner.py
@@ -7,7 +7,6 @@ Execute salt convenience routines
 from __future__ import print_function
 from __future__ import absolute_import
 import logging
-import threading
 
 # Import salt libs
 import salt.exceptions
