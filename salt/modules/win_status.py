1c252c9aa5c4bacf0c6e88a07836d74d20b6ea91
Toben Archer <sandslash+github@gmail.com>
2015-01-29 10:52:28 +0000

    Update win_status.py
    added a some new features to the win_status.py module. Things I'm using to help debug the salt minion in my enviorment.

--- salt/modules/win_status.py
+++ salt/modules/win_status.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 '''
 Module for returning various status data about a minion.
-These data can be useful for compiling into stats later.
+These data can be useful for compiling into stats later,
+or for problem solving if your minion is having problems.
 
 :depends:   - pythoncom
             - wmi
@@ -11,6 +12,11 @@ from __future__ import absolute_import
 import logging
 import salt.utils
 
+import os
+import ctypes
+import sys
+import collections
+import subprocess
 
 log = logging.getLogger(__name__)
 
@@ -40,26 +46,122 @@ def __virtual__():
     return False
 
 
-def procs():
+def procs(count=False):
     '''
     Return the process data
+    for quick use, the argument count tells you how many processes there are.
 
     CLI Example:
 
     .. code-block:: bash
 
         salt '*' status.procs
+        salt '*' status.procs count
     '''
     with salt.utils.winapi.Com():
         wmi_obj = wmi.WMI()
         processes = wmi_obj.win32_process()
 
-        process_info = {}
-        for proc in processes:
-            process_info[proc.ProcessId] = _get_process_info(proc)
+	#this short circuit's the function to get a short simple proc count.
+	if count:
+		return len(processes)
 
-        return process_info
+	#a propper run of the function, creating a nonsensically long out put.
+    process_info = {}
+    for proc in processes:
+        process_info[proc.ProcessId] = _get_process_info(proc)
 
+    return process_info
+
+def cpu_load():
+    '''
+    Return the processor load as a percentage.
+
+    CLI Example:
+
+    .. code-block:: bash
+
+       salt '*' status.cpu_load
+    '''
+
+    #open up a subprocess to get information from WMIC
+    proc = subprocess.Popen('wmic cpu',stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    #pull in the information from WMIC
+    outs = proc.communicate()
+
+    #split that out the information into tuples.
+    info = outs[0].split('\n')
+
+    #find the location of LoadPercentage
+    column = info[0].index('LoadPercentage')
+
+    #get the end of the number.
+    end = info[1].index(' ',column+1)
+
+    #return pull it out of the informatin and cast it to an int.
+    return int(info[1][column:end])
+
+def disk_usage(human_readable=False,path=None):
+    '''
+    return the disk usage for this minion
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' status.disk_usage path=c:/salt
+
+    '''
+
+    if not path:
+        path = 'c:/'
+
+    #Credit for the source and ideas for this function:
+    # http://code.activestate.com/recipes/577972-disk-usage/?in=user-4178764
+    _, total, free = ctypes.c_ulonglong(), ctypes.c_ulonglong(), ctypes.c_longlong()
+    if sys.version_info >= (3,) or isinstance(path,unicode):
+        fun = ctypes.windll.kernel32.GetDiskFreeSpaceExw
+    else:
+        fun = ctypes.windll.kernel32.GetDiskFreeSpaceExA
+    ret = fun(path, ctypes.byref(_), ctypes.byref(total), ctypes.byref(free))
+    if ret == 0 :
+        raise ctypes.WinError()
+    used = total.value - free.value
+
+    if human_readable:
+        tstr = _byte_calc(total.value)
+        ustr = _byte_calc(used)
+        fstr = _byte_calc(free.value)
+        return 'Total: {0}, Used: {1}, Free: {2}'.format(tstr,ustr,fstr)
+    else:
+        return {'total':total.value,'used':used,'free':free.value}
+
+def salt_mem(human_readable=False):
+    '''
+    Returns the amount of memory that salt is using.
+
+    human_readable: return the value in a nicely formated number.
+
+    CLI Example:
+
+    .. code-black:: bash
+
+        salt '*' status.salt_mem
+        salt '*' status.salt_mem human_readable
+    '''
+    with salt.utils.winapi.Com():
+        wmi_obj = wmi.WMI()
+        result = wmi_obj.query('SELECT WorkingSet FROM Win32_PerfRawData_PerfProc_Process WHERE IDProcess={0}'.format(os.getpid()))
+        mem = int(result[0].wmi_property('WorkingSet').value)
+        if human_readable:
+            if mem < 1024:
+                return str(mem)+'B'
+            elif mem < 1038336:
+                return str(mem/1024)+'kB'
+            else:
+                return str(mem/1038336)+'MB'
+        return mem
 
 def _get_process_info(proc):
     '''
@@ -95,3 +197,16 @@ def _get_process_owner(process):
         log.warning('Error getting owner of process; PID=\'{0}\'; Error: {1}'
                     .format(process.ProcessId, error_code))
     return owner
+
+def _byte_calc(val):
+    if val < 1024:
+        tstr = str(val)+'B'
+        elif val < 1038336:
+            tstr = str(val/1024)+'kB'
+        elif val < 1073741824:
+            tstr = str(val/1038336)+'MB'
+        elif val < 1099511627776:
+            tstr = str(val/1073741824)+'GB'
+        else:
+            tstr = str(val/1099511627776)+'TB'
+        return tstr
