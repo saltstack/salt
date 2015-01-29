saltstack/salt

commit e806ae98fc72e3b53cd5f596f07b2c180765fc2a

Don't *pop* the outputter, just pass opts of None

salt/output/__init__.py
@@ -39,9 +39,8 @@ def try_printout(data, out, opts):
         return get_printout(out, opts)(data).rstrip()
     except (KeyError, AttributeError):
         log.debug(traceback.format_exc())
-        opts.pop('output', None)
         try:
-            return get_printout('nested', opts)(data).rstrip()
+            return get_printout('nested', None)(data).rstrip()
         except (KeyError, AttributeError):
             log.error('Nested output failed: ', exec_info=True)
             return get_printout('raw', opts)(data).rstrip()

commit 79516222cc0077d88581121ff272a289882f39f6

Fix for #20170
fix bug introduced in e806ae98fc72e3b53cd5f596f07b2c180765fc2a

salt/output/__init__.py
@@ -40,7 +40,7 @@ def try_printout(data, out, opts):
     except (KeyError, AttributeError):
         log.debug(traceback.format_exc())
         try:
-            return get_printout('nested', None)(data).rstrip()
+            return get_printout('nested', opts)(data).rstrip()
         except (KeyError, AttributeError):
             log.error('Nested output failed: ', exc_info=True)
             return get_printout('raw', opts)(data).rstrip()