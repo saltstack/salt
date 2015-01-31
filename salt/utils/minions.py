4c3a816c59c9567f9e300f51be03994881202448
Marvin Frick <marvin.frick@sinnerschrader.com>
2015-01-31 21:56:41 +0100

    fixes weird wording in comment

--- salt/utils/minions.py
+++ salt/utils/minions.py
@@ -632,11 +632,9 @@ class CkMinions(object):
 
     def fill_auth_list_from_groups(self, auth_provider, user_groups, auth_list):
         '''
-        Returns the provided list auf permission matchers, plus any matchers
-        that are given to a group the user is in.
-
-        Groups are defined as any dict in which a key has a trailing '%'
-        and the values are permission matchers.
+        Returns a list of authorisation matchers that a user is eligible for.
+        This list is a combination of the provided personal matchers plus the
+        matchers of any group the user is in.
         '''
         group_names = [item for item in auth_provider if item.endswith('%')]
         if group_names:
