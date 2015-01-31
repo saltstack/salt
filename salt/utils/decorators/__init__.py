fc6dea06575d151f6c389290802b32da5b543edf
Thomas Jackson <jacksontj.89@gmail.com>
2015-01-30 16:21:11 -0800

    Add support for multiple types of functions by namespacing out the dep dict
    Conflicts:
    	salt/utils/decorators/__init__.py

--- salt/utils/decorators/__init__.py
+++ salt/utils/decorators/__init__.py
@@ -24,8 +24,8 @@ class Depends(object):
     dependencies passed in are in the globals of the module. If not, it will
     cause the function to be unloaded (or replaced)
     '''
-    # Dependency -> list of things that depend on it
-    dependency_dict = defaultdict(set)
+    # kind -> Dependency -> list of things that depend on it
+    dependency_dict = defaultdict(lambda: defaultdict(set))
 
     def __init__(self, *dependencies, **kwargs):
         '''
@@ -60,8 +60,9 @@ class Depends(object):
         class wide depandancy_dict
         '''
         module = inspect.getmodule(inspect.stack()[1][0])
+        kind = module.__name__.rsplit('.', 1)[0]
         for dep in self.dependencies:
-            self.dependency_dict[dep].add(
+            self.dependency_dict[kind][dep].add(
                 (module, function, self.fallback_function)
             )
         return function
@@ -74,7 +75,8 @@ class Depends(object):
         It will modify the "functions" dict and remove/replace modules that
         are missing dependencies.
         '''
-        for dependency, dependent_set in cls.dependency_dict.items():
+        kind = next(functions.itervalues()).__module__.rsplit('.', 1)[0]
+        for dependency, dependent_set in cls.dependency_dict[kind].items():
             # check if dependency is loaded
             for module, func, fallback_function in dependent_set:
                 # check if you have the dependency
