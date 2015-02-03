01fa6afc1b831f96dd1066cc93584ccfbaad9e3c
Thomas Jackson <jacksontj.89@gmail.com>
2015-02-02 18:46:05 -0800

    Change default to false, since everyone seems to pass them in

--- salt/loader.py
+++ salt/loader.py
@@ -205,7 +205,6 @@ def pillars(opts, functions):
                         opts,
                         tag='pillar',
                         pack=pack,
-                        singleton=False,  # TODO: re-enable
                         )
     return FilterDictWrapper(ret, '.ext_pillar')
 
@@ -392,7 +391,6 @@ def render(opts, functions, states=None):
                         opts,
                         tag='renderers',
                         pack=pack,
-                        singleton=False,  # TODO: re-enable
                         )
     rend = FilterDictWrapper(ret, '.render')
 
@@ -716,7 +714,7 @@ class NewLazyLoader(salt.utils.lazy.LazyDict):
                 pack=None,
                 whitelist=None,
                 virtual_enable=True,
-                singleton=True,
+                singleton=False,
                 ):
         def new_object():
             ret = object.__new__(cls)
