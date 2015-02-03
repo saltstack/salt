147643b62b957ef5705030fb03b3560bee99184f
Amir Pakdel <pakdel@gmail.com>
2015-02-03 11:59:49 -0500

    PyLint adjustments in modules/mssql.py

--- salt/modules/mssql.py
+++ salt/modules/mssql.py
@@ -8,7 +8,7 @@ Module to provide MS SQL Server compatibility to salt.
 :configuration: In order to connect to MS SQL Server, certain configuration is
     required in minion configs/pillars on the relevant minions. Some sample
     pillars might look like::
-        
+
         mssql.server: 'localhost'
         mssql.port:   1433
         mssql.user:   'sysdba'
@@ -86,7 +86,7 @@ def tsql_query(query, **kwargs):
         return loads(_MssqlEncoder().encode({'resultset': cur.fetchall()}))['resultset']
     except Exception as e:
         # Trying to look like the output of cur.fetchall()
-        return ( ('Could not run the query', ), (str(e), ) )
+        return (('Could not run the query',),(str(e),))
 
 
 def version(**kwargs):
@@ -112,7 +112,7 @@ def db_list(**kwargs):
 
         salt minion mssql.db_list
     '''
-    return [ row[0] for row in tsql_query('SELECT name FROM sys.databases', as_dict=False, **kwargs) ]
+    return [row[0] for row in tsql_query('SELECT name FROM sys.databases', as_dict=False, **kwargs)]
 
 
 def db_exists(database_name, **kwargs):
@@ -126,13 +126,13 @@ def db_exists(database_name, **kwargs):
         salt minion mssql.db_exists database_name='DBNAME'
     '''
     # We should get one, and only one row
-    return len( tsql_query("SELECT database_id FROM sys.databases WHERE NAME='%s'" % database_name, **kwargs) ) == 1
+    return len(tsql_query("SELECT database_id FROM sys.databases WHERE NAME='{0}'".format(database_name), **kwargs)) == 1
 
 
 def db_remove(database_name, **kwargs):
     '''
     Drops a specific database from the MS SQL server.
-    It will not drop any of 'master','model','msdb' or 'tempdb'.
+    It will not drop any of 'master', 'model', 'msdb' or 'tempdb'.
 
     CLI Example:
 
@@ -141,12 +141,12 @@ def db_remove(database_name, **kwargs):
         salt minion mssql.db_remove database_name='DBNAME'
     '''
     try:
-        if db_exists(database_name) and database_name not in ['master','model','msdb','tempdb']:
+        if db_exists(database_name) and database_name not in ['master', 'model', 'msdb', 'tempdb']:
             conn = _get_connection(**kwargs)
             conn.autocommit(True)
             cur = conn.cursor()
-            cur.execute('ALTER DATABASE %s SET SINGLE_USER WITH ROLLBACK IMMEDIATE' % (database_name))
-            cur.execute('DROP DATABASE %s' % (database_name))
+            cur.execute('ALTER DATABASE {0} SET SINGLE_USER WITH ROLLBACK IMMEDIATE'.format(database_name))
+            cur.execute('DROP DATABASE {0}'.format(database_name))
             conn.autocommit(False)
             conn.close()
             return True
@@ -173,7 +173,6 @@ def role_list(**kwargs):
 def role_exists(role, **kwargs):
 
     '''
-    
     Checks if a role exists.
 
     CLI Example:
@@ -183,7 +182,7 @@ def role_exists(role, **kwargs):
         salt minion mssql.role_exists db_owner
     '''
     # We should get one, and only one row
-    return len(tsql_query(query='sp_helprole "%s"' % role, as_dict=True, **kwargs)) == 1
+    return len(tsql_query(query='sp_helprole "{0}"'.format(role), as_dict=True, **kwargs)) == 1
 
 
 def role_create(role, owner=None, **kwargs):
@@ -203,9 +202,9 @@ def role_create(role, owner=None, **kwargs):
         conn.autocommit(True)
         cur = conn.cursor()
         if owner:
-            cur.execute('CREATE ROLE %s AUTHORIZATION %s' % (role, owner))
+            cur.execute('CREATE ROLE {0} AUTHORIZATION {1}'.format(role, owner))
         else:
-            cur.execute('CREATE ROLE %s' % (role))
+            cur.execute('CREATE ROLE {0}'.format(role))
         conn.autocommit(True)
         conn.close()
         return True
@@ -227,7 +226,7 @@ def role_remove(role, **kwargs):
         conn = _get_connection(**kwargs)
         conn.autocommit(True)
         cur = conn.cursor()
-        cur.execute('DROP ROLE %s' % (role))
+        cur.execute('DROP ROLE {0}'.format(role))
         conn.autocommit(True)
         conn.close()
         return True
@@ -247,7 +246,7 @@ def login_exists(login, **kwargs):
     '''
     try:
         # We should get one, and only one row
-        return len(tsql_query(query="SELECT name FROM sys.syslogins WHERE name='%s'" % (login), **kwargs)) == 1
+        return len(tsql_query(query="SELECT name FROM sys.syslogins WHERE name='{0}'".format(login), **kwargs)) == 1
 
     except Exception as e:
         return 'Could not find the login: {0}'.format(e)
@@ -256,7 +255,7 @@ def login_exists(login, **kwargs):
 def user_exists(username, **kwargs):
     '''
     Find if an user exists in a specific database on the MS SQL server.
-    
+
     Note:
         *database* argument is mandatory
 
@@ -271,7 +270,7 @@ def user_exists(username, **kwargs):
         return False
 
     # We should get one, and only one row
-    return len(tsql_query(query="SELECT name FROM sysusers WHERE name='%s'" % (username), **kwargs)) == 1
+    return len(tsql_query(query="SELECT name FROM sysusers WHERE name='{0}'".format(username), **kwargs)) == 1
 
 
 def user_list(**kwargs):
@@ -284,8 +283,8 @@ def user_list(**kwargs):
 
         salt minion mssql.user_list [database='DBNAME']
     '''
-    return [ row[0] for row in tsql_query("SELECT name FROM sysusers where issqluser=1 or isntuser=1", as_dict=False, **kwargs) ]
-    
+    return [row[0] for row in tsql_query("SELECT name FROM sysusers where issqluser=1 or isntuser=1", as_dict=False, **kwargs)]
+
 
 def user_create(username, new_login_password=None, **kwargs):
     '''
@@ -313,10 +312,10 @@ def user_create(username, new_login_password=None, **kwargs):
             if login_exists(username, **kwargs):
                 conn.close()
                 return False
-            cur.execute("CREATE LOGIN %s WITH PASSWORD='%s',check_policy = off" % (username, new_login_password))
-            cur.execute("CREATE USER %s FOR LOGIN %s" % (username, username))
+            cur.execute("CREATE LOGIN {0} WITH PASSWORD='{1}',check_policy = off".format(username, new_login_password))
+            cur.execute("CREATE USER {0} FOR LOGIN {1}".format(username, username))
         else:  # new_login_password is not specified
-            cur.execute("CREATE USER %s WITHOUT LOGIN" % (username))
+            cur.execute("CREATE USER {0} WITHOUT LOGIN".format(username))
 
         conn.autocommit(False)
         conn.close()
@@ -342,7 +341,7 @@ def user_remove(username, **kwargs):
         conn = _get_connection(**kwargs)
         conn.autocommit(True)
         cur = conn.cursor()
-        cur.execute("DROP USER %s" % (username))
+        cur.execute("DROP USER {0}".format(username))
         conn.autocommit(False)
         conn.close()
         return True
