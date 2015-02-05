1772a280f470a6bf39b45a6a14fab3336b9b2ed3
Thomas Jackson <jacksontj.89@gmail.com>
2015-02-04 17:55:08 -0800

    If you have the module loaded, lets not require another hit to disk

--- salt/loader.py
+++ salt/loader.py
@@ -643,7 +643,7 @@ class LazyLoader(salt.utils.lazy.LazyDict):
 
     # TODO:
         - move modules_max_memory into here
-        - re-enable singletons
+        - re-enable singletons (TODO: tests)
     '''
     instances = {}
 
@@ -1042,7 +1042,7 @@ class LazyLoader(salt.utils.lazy.LazyDict):
         while True:
             try:
                 ret = _inner_load(mod_name)
-                if not reloaded:
+                if not reloaded and ret is not True:
                     self.refresh_file_mapping()
                     reloaded = True
                     continue
