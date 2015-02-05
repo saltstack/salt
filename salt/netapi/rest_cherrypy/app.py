e62e23a18a9ff742139264845035387c375631a1
Seth House <seth@eseth.com>
2015-02-04 18:16:57 -0700

    Note non-standard SSE tag field; update examples for new event tag style
    The addition in a720149 was incorrect. The SSE spec has an optional
    field called 'event' not 'tag' which specifies the event type.
    
    Unfortunately the EventSource client can only subscribe to either all
    events without a type or to events with a specified type. There is no
    way to subscribe to all events with varying types. The best workaround
    is to keep the tag in the event data, subscribe to all events, and
    filter on tags manually.
    
    Fortunately the SSE spec specifies that unknown fields should be ignored
    so this addition is still useful for clients that don't mind deviating
    from the spec and we can keep from breaking backward-compat.

--- salt/netapi/rest_cherrypy/app.py
+++ salt/netapi/rest_cherrypy/app.py
@@ -1539,6 +1539,11 @@ class Events(object):
 
         **Example response:**
 
+        Note, the ``tag`` field is not part of the spec. SSE compliant clients
+        should ignore unknown fields. This addition allows non-compliant
+        clients to only watch for certain tags without having to deserialze the
+        JSON object each time.
+
         .. code-block:: http
 
             HTTP/1.1 200 OK
@@ -1547,9 +1552,12 @@ class Events(object):
             Content-Type: text/event-stream;charset=utf-8
 
             retry: 400
-            data: {'tag': '', 'data': {'minions': ['ms-4', 'ms-3', 'ms-2', 'ms-1', 'ms-0']}}
 
-            data: {'tag': '20130802115730568475', 'data': {'jid': '20130802115730568475', 'return': True, 'retcode': 0, 'success': True, 'cmd': '_return', 'fun': 'test.ping', 'id': 'ms-1'}}
+            tag: salt/job/20130802115730568475/new
+            data: {'tag': 'salt/job/20130802115730568475/new', 'data': {'minions': ['ms-4', 'ms-3', 'ms-2', 'ms-1', 'ms-0']}}
+
+            tag: salt/job/20130802115730568475/ret/jerry
+            data: {'tag': 'salt/job/20130802115730568475/ret/jerry', 'data': {'jid': '20130802115730568475', 'return': True, 'retcode': 0, 'success': True, 'cmd': '_return', 'fun': 'test.ping', 'id': 'ms-1'}}
 
         The event stream can be easily consumed via JavaScript:
 
@@ -1559,7 +1567,10 @@ class Events(object):
             var source = new EventSource('/events');
             source.onopen = function() { console.debug('opening') };
             source.onerror = function(e) { console.debug('error!', e) };
-            source.onmessage = function(e) { console.debug(e.data) };
+            source.onmessage = function(e) {
+                console.debug('Tag: ', e.data.tag)
+                console.debug('Data: ', e.data.data)
+            };
 
         Or using CORS:
 
