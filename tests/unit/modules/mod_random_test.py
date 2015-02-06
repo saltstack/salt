7b79128309da9478d09250445e3a59c7b9d08397
Rupesh Tare <rupesht@cybage.com>
2015-02-06 20:09:45 +0530

    added some imports

--- tests/unit/modules/mod_random_test.py
+++ tests/unit/modules/mod_random_test.py
@@ -3,6 +3,9 @@
     :codeauthor: :email:`Rupesh Tare <rupesht@saltstack.com>`
 '''
 
+# Import Python Libs
+from __future__ import absolute_import
+
 # Import Salt Testing Libs
 from salttesting import TestCase, skipIf
 from salttesting.mock import (
@@ -10,6 +13,9 @@ from salttesting.mock import (
     NO_MOCK,
     NO_MOCK_REASON
 )
+from salttesting.helpers import ensure_in_syspath
+
+ensure_in_syspath('../../')
 
 # Import Salt Libs
 from salt.modules import mod_random
