41039e0e03c58eb020a2cbe9bbcd13587cb41027
Greg Thompson <greg@greg9.com>
2015-02-09 12:04:35 +0000

    Added the option to-source to the after_jump list, to enable the configuration of SNAT rules with the iptables module.

--- salt/modules/iptables.py
+++ salt/modules/iptables.py
@@ -288,6 +288,10 @@ def build_rule(table=None, chain=None, command=None, position='', full=None, fam
         after_jump.append('--to-destination {0} '.format(kwargs['to-destination']))
         del kwargs['to-destination']
 
+    if 'to-source' in kwargs:
+        after_jump.append('--to-source {0} '.format(kwargs['to-source']))
+        del kwargs['to-source']
+
     if 'reject-with' in kwargs:
         after_jump.append('--reject-with {0} '.format(kwargs['reject-with']))
         del kwargs['reject-with']
