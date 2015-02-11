7c6b8f12217aa862200703fd8f1157324390139c
Alexander Schwartz <alexander.schwartz@gmx.net>
2015-02-11 19:57:14 +0000

    clean up lint messages

--- tests/unit/states/archive_test.py
+++ tests/unit/states/archive_test.py
@@ -10,16 +10,16 @@ from __future__ import absolute_import
 from salttesting import TestCase, skipIf
 from salttesting.helpers import ensure_in_syspath
 from salttesting.mock import NO_MOCK, NO_MOCK_REASON, MagicMock, patch
-from StringIO import StringIO
 
 ensure_in_syspath('../../')
 
-from salt.states import archive as archive 
+from salt.states import archive as archive
 
 # Globals
 archive.__salt__ = {}
-archive.__opts__ = { "cachedir": "/tmp", "test": False }
+archive.__opts__ = {"cachedir": "/tmp", "test": False}
 archive.__env__ = 'base'
+
 
 @skipIf(NO_MOCK, NO_MOCK_REASON)
 class ArchiveTestCase(TestCase):
@@ -34,10 +34,10 @@ class ArchiveTestCase(TestCase):
         '''
         Tests the call of extraction with gnutar
         '''
-        gnutar = MagicMock(return_value = 'tar (GNU tar)')
-        missing = MagicMock(return_value = False)
-        nop = MagicMock(return_value = True)
-        run_all = MagicMock(return_value = {'retcode': 0, 'stdout': 'stdout', 'stderr': 'stderr'})
+        gnutar = MagicMock(return_value='tar (GNU tar)')
+        missing = MagicMock(return_value=False)
+        nop = MagicMock(return_value=True)
+        run_all = MagicMock(return_value={'retcode': 0, 'stdout': 'stdout', 'stderr': 'stderr'})
         with patch.dict(archive.__salt__, {'cmd.run': gnutar, 'file.directory_exists': missing, 'file.file_exists': missing, 'state.single': nop, 'file.makedirs': nop, 'cmd.run_all': run_all}):
             ret = archive.extracted('/tmp/out', '/tmp/foo.tar.gz', 'tar', tar_options='xvzf', keep=True)
             self.assertEqual(ret['changes']['extracted_files'], 'stdout')
@@ -46,10 +46,10 @@ class ArchiveTestCase(TestCase):
         '''
         Tests the call of extraction with bsdtar
         '''
-        bsdtar = MagicMock(return_value = 'tar (bsdtar)')
-        missing = MagicMock(return_value = False)
-        nop = MagicMock(return_value = True)
-        run_all = MagicMock(return_value = {'retcode': 0, 'stdout': 'stdout', 'stderr': 'stderr'})
+        bsdtar = MagicMock(return_value='tar (bsdtar)')
+        missing = MagicMock(return_value=False)
+        nop = MagicMock(return_value=True)
+        run_all = MagicMock(return_value={'retcode': 0, 'stdout': 'stdout', 'stderr': 'stderr'})
         with patch.dict(archive.__salt__, {'cmd.run': bsdtar, 'file.directory_exists': missing, 'file.file_exists': missing, 'state.single': nop, 'file.makedirs': nop, 'cmd.run_all': run_all}):
             ret = archive.extracted('/tmp/out', '/tmp/foo.tar.gz', 'tar', tar_options='xvzf', keep=True)
             self.assertEqual(ret['changes']['extracted_files'], 'stderr')
