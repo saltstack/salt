46cee9d842202b27da60924b7f19779fa15f0a51
Claudiu Popescu <claudiu.popescu@rocket-internet.de>
2015-02-11 11:54:52 +0100

    Cleaned ast since it is not needed anymore

--- salt/modules/boto_rds.py
+++ salt/modules/boto_rds.py
@@ -38,7 +38,6 @@ from __future__ import absolute_import
 # Import Python libs
 import logging
 import json
-import ast
 import salt.ext.six as six
 from salt.ext.six import string_types
 import salt.utils.odict as odict
