e56d962143b3557ec5fcc318c31549a4f864c74b
Seth House <seth@eseth.com>
2015-02-13 17:17:12 -0700

    Minor clarifications to the rest_cherrypy setup/usage instructions

--- salt/netapi/rest_cherrypy/app.py
+++ salt/netapi/rest_cherrypy/app.py
@@ -28,12 +28,13 @@ A REST API for Salt
     authentication credentials will be sent over the wire.
 
     A self-signed certificate can be generated using the
-    :py:func:`~salt.modules.tls.create_self_signed_cert` function in Salt (note
-    the dependencies for this module).
+    :py:func:`~salt.modules.tls.create_self_signed_cert` execution function.
+    Running this function requires pyOpenSSL and the ``salt-call`` script is
+    available in the ``salt-minion`` package.
 
     .. code-block:: bash
 
-        salt-call tls.create_self_signed_cert
+        salt-call --local tls.create_self_signed_cert
 
     All available configuration options are detailed below. These settings
     configure the CherryPy HTTP server and do not apply when using an external
@@ -146,7 +147,6 @@ The token may be sent in one of two ways:
       curl -sSk https://localhost:8000 \
             -b ~/cookies.txt \
             -H 'Accept: application/x-yaml' \
-            -H 'X-Auth-Token: 697adbdc8fe971d09ae4c2a3add7248859c87079'\
             -d client=local \
             -d tgt='*' \
             -d fun=test.ping
