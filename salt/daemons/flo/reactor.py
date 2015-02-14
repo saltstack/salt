a40c617ea605bd667a9906f6c9400fc9562d7c0a
Thomas S Hatch <thatch45@gmail.com>
2015-02-14 00:06:53 -0700

    Add event return fork behavior

--- salt/daemons/flo/reactor.py
+++ salt/daemons/flo/reactor.py
@@ -4,6 +4,7 @@ Start the reactor!
 '''
 # Import salt libs
 import salt.utils.reactor
+import salt.utils.event
 # Import ioflo libs
 import ioflo.base.deeding
 
@@ -19,4 +20,18 @@ def reactor_fork(self):
     '''
     self.proc_mgr.add_process(
             salt.utils.reactor.Reactor,
+            args=(self.opts.value,))
+
+
+@ioflo.base.deeding.deedify(
+        'SaltRaetEventReturnFork',
+        ioinit={
+            'opts': '.salt.opts',
+            'proc_mgr': '.salt.usr.proc_mgr'})
+def event_return_fork(self):
+    '''
+    Add a reactor object to the process manager
+    '''
+    self.proc_mgr.add_process(
+            salt.utils.event.EventReturn,
             args=(self.opts.value,))
