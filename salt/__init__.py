c047a1beb89179358fa777048f7f17af42cbb8cb
Pedro Algarvio <pedro@algarvio.me>
2015-02-26 09:49:36 +0000

    Correct indentation.

--- salt/__init__.py
+++ salt/__init__.py
@@ -2,27 +2,29 @@
 '''
 Salt package
 '''
+# Import Python Libs
 import warnings
+
 # All salt related deprecation warnings should be shown once each!
 warnings.filterwarnings(
-        'once',  # Show once
-        '',  # No deprecation message match
-        DeprecationWarning,  # This filter is for DeprecationWarnings
-        r'^(salt|salt\.(.*))$'  # Match module(s) 'salt' and 'salt.<whatever>'
+    'once',  # Show once
+    '',  # No deprecation message match
+    DeprecationWarning,  # This filter is for DeprecationWarnings
+    r'^(salt|salt\.(.*))$'  # Match module(s) 'salt' and 'salt.<whatever>'
 )
 
 # While we are supporting Python2.6, hide nested with-statements warnings
 warnings.filterwarnings(
- 'ignore',
- 'With-statements now directly support multiple context managers',
- DeprecationWarning
+    'ignore',
+    'With-statements now directly support multiple context managers',
+    DeprecationWarning
 )
 
 # Filter the backports package UserWarning about being re-imported
 warnings.filterwarnings(
- 'ignore',
- '^Module backports was already imported from (.*), but (.*) is being added to sys.path$',
- UserWarning
+    'ignore',
+    '^Module backports was already imported from (.*), but (.*) is being added to sys.path$',
+    UserWarning
 )
 
 
