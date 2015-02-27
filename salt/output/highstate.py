26f41c0c6e53eb3fba0c54de5c67b064f59b447c
Pedro Algarvio <pedro@algarvio.me>
2015-02-27 10:23:23 +0000

    Make the warnings standout
    Since in yellow they can be confused with unchanged state returns

--- salt/output/highstate.py
+++ salt/output/highstate.py
@@ -260,7 +260,7 @@ def _format_host(host, data):
                     subsequent_indent=u' ' * 14
                 )
                 hstrs.append(
-                    u'   {colors[YELLOW]} Warnings: {0}{colors[ENDC]}'.format(
+                    u'   {colors[LIGHT_RED]} Warnings: {0}{colors[ENDC]}'.format(
                         wrapper.fill('\n'.join(ret['warnings'])).lstrip(),
                         colors=colors
                     )
@@ -335,7 +335,7 @@ def _format_host(host, data):
         if num_warnings:
             hstrs.append(
                 colorfmt.format(
-                    colors['YELLOW'],
+                    colors['LIGHT_RED'],
                     _counts(rlabel['warnings'], num_warnings),
                     colors
                 )
