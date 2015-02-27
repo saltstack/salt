0efb1d13dabcba5fdc772081c76b1feed2093533
Andrea-Redi Tsarida <andrea-redi.tsarida@rocket-internet.de>
2015-02-27 01:11:05 +0100

    Added boto_elb.register_instances state function

--- salt/states/boto_elb.py
+++ salt/states/boto_elb.py
@@ -339,6 +339,63 @@ def present(
     return ret
 
 
+def register_instances(name, instances, region=None, key=None, keyid=None,
+                       profile=None):
+    '''
+    Add instance/s to load balancer
+
+    add-instances:
+      boto_elb.register_instances:
+        - name: myloadbalancer
+        - instances:
+          - instance1
+          - instance2
+    '''
+    ret = {'name': name, 'result': None, 'comment': '', 'changes': {}}
+    ret['name'] = name
+    lb = __salt__['boto_elb.exists'](name, region, key, keyid, profile)
+    log.debug(lb)
+    if lb:
+        health = __salt__['boto_elb.get_instance_health'](name,
+                                                          region,
+                                                          key,
+                                                          keyid,
+                                                          profile)
+        nodes = []
+        new = []
+        for value in health:
+            nodes.append(value['instance_id'])
+        for value in instances:
+            if value not in nodes:
+                new.append(value)
+        if len(new) == 0:
+            ret['comment'] = 'Instance/s {0} already exist.' \
+                              ''.format(str(instances).strip('[]'))
+            ret['result'] = True
+        else:
+            state = __salt__['boto_elb.register_instances'](name,
+                                                            instances,
+                                                            region,
+                                                            key,
+                                                            keyid,
+                                                            profile)
+            if state:
+                ret['comment'] = 'Load Balancer {0} has been changed' \
+                                 ''.format(name)
+                ret['changes']['old'] = '\n'.join(nodes)
+                log.debug(nodes)
+                new = set().union(nodes,instances)
+                ret['changes']['new'] = '\n'.join(list(new))
+                ret['result'] = True
+            else:
+                ret['comment'] = 'Load balancer {0} failed to add instances' \
+                                 ''.format(name)
+                ret['result'] = False
+    else:
+        ret['comment'] = 'Could not find lb {0}'.format(name)
+    return ret
+
+
 def _elb_present(
         name,
         availability_zones,
