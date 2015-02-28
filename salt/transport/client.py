5731a01209c22404f6391d00950811f9e2c97c16
Thomas Jackson <jacksontj.89@gmail.com>
2015-02-28 11:32:16 -0800

    Remove testing code

--- salt/transport/client.py
+++ salt/transport/client.py
@@ -21,9 +21,6 @@ class ReqChannel(object):
         elif 'transport' in opts.get('pillar', {}).get('master', {}):
             ttype = opts['pillar']['master']['transport']
 
-        # TODO: remove
-        ttype = 'tcp'
-
         # switch on available ttypes
         if ttype == 'zeromq':
             import salt.transport.zeromq
