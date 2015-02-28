011fb252e0c0d5baf4fecb7a2065575162ff7c0c
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2015-02-28 11:28:35 +0100

    lxc: add wrappers for cmd_run alike

--- salt/modules/lxc.py
+++ salt/modules/lxc.py
@@ -3337,6 +3337,39 @@ def run_all(name,
                 keep_env=keep_env)
 
 
+def _wrapper_cmd(func, *args, **kw):
+    # no plan to remove, just warn
+    log.warn(
+        'lxc.cmd_{0} has been renamed to lxc.{0},'
+        ' please update your code !'.format(func)
+    )
+    kw = copy.deepcopy(kw)
+    for i in [a for a in six.iterkeys(kw)]:
+        if i.startswith('__'):
+            kw.pop(i, None)
+    return globals()[func](*args, **kw)
+
+
+def cmd_run_stdout(name, *args, **kw):
+    return _wrapper_cmd('run_stdout', name, *args, **kw)
+
+
+def cmd_run_stderr(name, *args, **kw):
+    return _wrapper_cmd('run_stderr', name, *args, **kw)
+
+
+def cmd_run_all(name, *args, **kw):
+    return _wrapper_cmd('run_all', name, *args, **kw)
+
+
+def cmd_run(name, *args, **kw):
+    return _wrapper_cmd('run', name, *args, **kw)
+
+
+def cmd_retcode(name, *args, **kw):
+    return _wrapper_cmd('retcode', name, *args, **kw)
+
+
 def _get_md5(name, path):
     '''
     Get the MD5 checksum of a file from a container
