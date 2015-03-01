2cf56e466e9d86c952f42a81436de41d1ae1077f
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2015-03-01 23:47:28 +0100

    Bring back runas in zc.buildout
    This fixes #21196

--- salt/states/zcbuildout.py
+++ salt/states/zcbuildout.py
@@ -40,6 +40,7 @@ from __future__ import absolute_import
 # Import python libs
 import logging
 import sys
+import salt.utils
 
 # Import salt libs
 from salt.ext.six import string_types
@@ -122,6 +123,7 @@ def installed(name,
               config='buildout.cfg',
               quiet=False,
               parts=None,
+              runas=None,
               user=None,
               env=(),
               buildout_ver=None,
@@ -137,7 +139,7 @@ def installed(name,
               onlyif=None,
               use_vt=False,
               loglevel='debug',
-              **kwargs):
+              output_loglevel = None):
     '''
     Install buildout in a specific directory
 
@@ -155,6 +157,11 @@ def installed(name,
 
     parts
         specific buildout parts to run
+
+    runas
+        user used to run buildout as
+
+        .. deprecated:: 2014.1.4
 
     user
         user used to run buildout as
@@ -204,13 +211,33 @@ def installed(name,
         loglevel for buildout commands
     '''
     ret = {}
-
-    if 'group' in kwargs:
-        log.warn('Passing \'group\' is deprecated, just remove it')
-    output_loglevel = kwargs.get('output_loglevel', None)
     if output_loglevel and not loglevel:
         log.warn('Passing \'output_loglevel\' is deprecated,'
                  ' please use loglevel instead')
+    salt.utils.warn_until(
+        'Lithium',
+        'Please remove \'runas\' support at this stage. \'user\' support was '
+        'added in 2014.1.4.',
+        _dont_call_warnings=True
+    )
+    if runas:
+        # Warn users about the deprecation
+        ret.setdefault('warnings', []).append(
+            'The \'runas\' argument is being deprecated in favor of \'user\', '
+            'please update your state files.'
+        )
+    if user is not None and runas is not None:
+        # user wins over runas but let warn about the deprecation.
+        ret.setdefault('warnings', []).append(
+            'Passed both the \'runas\' and \'user\' arguments. Please don\'t. '
+            '\'runas\' is being ignored in favor of \'user\'.'
+        )
+        runas = None
+    elif runas is not None:
+        # Support old runas usage
+        user = runas
+        runas = None
+
     try:
         test_release = int(test_release)
     except ValueError:
