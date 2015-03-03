7ed18e26882eb68bdad6f2097ffd0826a73e59a9
Erik Johnson <palehose@gmail.com>
2015-03-03 09:38:16 -0700

    Add function to get systemd version

--- salt/utils/systemd.py
+++ salt/utils/systemd.py
@@ -5,6 +5,7 @@ Contains systemd related help files
 # import python libs
 from __future__ import absolute_import
 import os
+import subprocess
 
 
 def booted(context):
@@ -14,7 +15,7 @@ def booted(context):
     systemd.sd_booted key to represent if systemd is running
     '''
     # We can cache this for as long as the minion runs.
-    if "systemd.sd_booted" not in context:
+    if 'systemd.sd_booted' not in context:
         try:
             # This check does the same as sd_booted() from libsystemd-daemon:
             # http://www.freedesktop.org/software/systemd/man/sd_booted.html
@@ -22,5 +23,34 @@ def booted(context):
                 context['systemd.sd_booted'] = True
         except OSError:
             context['systemd.sd_booted'] = False
-
     return context['systemd.sd_booted']
+
+
+def version(context=None):
+    '''
+    Attempts to run systemctl --version. Returns None if unable to determine
+    version.
+    '''
+    if isinstance(context, dict):
+        if 'systemd.version' in context:
+            return context['systemd.version']
+    elif context is not None:
+        raise SaltInvocationError('context must be a dictionary or None')
+    stdout = subprocess.Popen(
+        ['systemctl', '--version'],
+        close_fds=True,
+        stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
+    try:
+        ret = int(stdout.splitlines()[0].split()[-1])
+    except (IndexError, ValueError):
+        log.error(
+            'Unable to determine systemd version from systemctl '
+            '--version, output follows:\n{0}'.format(stdout)
+        )
+        return None
+    else:
+        try:
+            context['systemd.version'] = ret
+        except TypeError:
+            pass
+        return ret
