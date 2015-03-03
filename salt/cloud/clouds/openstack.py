f29673a03b22ea4dca0f9824962b818d494ad870
Daniel Wallace <danielwallace@gtmanfred.com>
2015-03-03 15:10:27 -0700

    use ex_set_password

--- salt/cloud/clouds/openstack.py
+++ salt/cloud/clouds/openstack.py
@@ -620,7 +620,7 @@ def create(vm_):
             )
         if vm_['key_filename'] is None and 'change_password' in __opts__ and __opts__['change_password'] is True:
             vm_['password'] = sup.secure_password()
-            conn.root_password(vm_['instance_id'], vm_['password'])
+            conn.ex_set_password(vm_['instance_id'], vm_['password'])
         data = conn.ex_get_node_details(vm_['instance_id'])
     else:
         # Put together all of the information required to request the instance,
