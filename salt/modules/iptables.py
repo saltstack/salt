82e020ed3adcf0be840d8bba4af7f059a49e438f
Justin Findlay <jfindlay@gmail.com>
2015-03-03 23:47:38 -0700

    check for check then check, not check twice
    fixes #20818; if --check is available, use it rather than use the more
    elaborate logic in the check function to workaround older iptables that
    don't have --check.

--- salt/modules/iptables.py
+++ salt/modules/iptables.py
@@ -507,6 +507,9 @@ def check(table='filter', chain=None, rule=None, family='ipv4'):
     ipt_cmd = _iptables_cmd(family)
 
     if _has_option('--check', family):
+        cmd = '{0} -t {1} -C {2} {3}'.format(ipt_cmd, table, chain, rule)
+        out = __salt__['cmd.run'](cmd)
+    else:
         _chain_name = hex(uuid.getnode())
 
         # Create temporary table
@@ -525,9 +528,6 @@ def check(table='filter', chain=None, rule=None, family='ipv4'):
                     return True
 
         return False
-    else:
-        cmd = '{0} -t {1} -C {2} {3}'.format(ipt_cmd, table, chain, rule)
-        out = __salt__['cmd.run'](cmd)
 
     if not out:
         return True
