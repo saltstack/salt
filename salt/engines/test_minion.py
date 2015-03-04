565ceff7c7b24c646c9194cea13247a22e25d54f
Matthew Williams <mgwilliams@gmail.com>
2015-03-04 08:50:31 +0000

    remove unused imports

--- salt/engines/test_minion.py
+++ salt/engines/test_minion.py
@@ -5,11 +5,7 @@ A simple test engine, not intended for real use but as an example
 # Import python libs
 import time
 
-# Import salt libs
-import salt.utils.event
-
 # Import python libs
-import json
 import logging
 
 log = logging.getLogger(__name__)
