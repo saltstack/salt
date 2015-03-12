edaaf290eb285d05db490328b624d94682f9e4c6
Corin Kochenower <ckochenower@saltstack.com>
2015-03-12 10:32:45 -0600

    Log exceptions for returner functions not implemented

--- salt/utils/job.py
+++ salt/utils/job.py
@@ -15,6 +15,9 @@ log = logging.getLogger(__name__)
 
 
 def store_job(opts, load, event=None, mminion=None):
+    '''
+    Store job information using the configured master_job_cache
+    '''
     # If the return data is invalid, just ignore it
     if any(key not in load for key in ('return', 'jid', 'id')):
         return False
@@ -22,27 +25,44 @@ def store_job(opts, load, event=None, mminion=None):
         return False
     if mminion is None:
         mminion = salt.minion.MasterMinion(opts, states=False, rend=False)
+
+    job_cache = opts['master_job_cache']
     if load['jid'] == 'req':
         # The minion is returning a standalone job, request a jobid
         load['arg'] = load.get('arg', load.get('fun_args', []))
         load['tgt_type'] = 'glob'
         load['tgt'] = load['id']
+
         prep_fstr = '{0}.prep_jid'.format(opts['master_job_cache'])
-        load['jid'] = mminion.returners[prep_fstr](
-            nocache=load.get('nocache', False))
+        try:
+            load['jid'] = mminion.returners[prep_fstr](nocache=load.get('nocache', False))
+        except KeyError:
+            emsg = "Returner '{0}' does not support function prep_jid".format(job_cache)
+            log.error(emsg)
+            raise KeyError(emsg)
 
         # save the load, since we don't have it
-        saveload_fstr = '{0}.save_load'.format(opts['master_job_cache'])
-        mminion.returners[saveload_fstr](load['jid'], load)
+        saveload_fstr = '{0}.save_load'.format(job_cache)
+        try:
+            mminion.returners[saveload_fstr](load['jid'], load)
+        except KeyError:
+            emsg = "Returner '{0}' does not support function save_load".format(job_cache)
+            log.error(emsg)
+            raise KeyError(emsg)
     elif salt.utils.jid.is_jid(load['jid']):
         # Store the jid
-        jidstore_fstr = '{0}.prep_jid'.format(opts['master_job_cache'])
-        mminion.returners[jidstore_fstr](False, passed_jid=load['jid'])
+        jidstore_fstr = '{0}.prep_jid'.format(job_cache)
+        try:
+            mminion.returners[jidstore_fstr](False, passed_jid=load['jid'])
+        except KeyError:
+            emsg = "Returner '{0}' does not support function prep_jid".format(job_cache)
+            log.error(emsg)
+            raise KeyError(emsg)
+
     if event:
         # If the return data is invalid, just ignore it
         log.info('Got return from {id} for job {jid}'.format(**load))
-        event.fire_event(
-            load, tagify([load['jid'], 'ret', load['id']], 'job'))
+        event.fire_event(load, tagify([load['jid'], 'ret', load['id']], 'job'))
         event.fire_ret_load(load)
 
     # if you have a job_cache, or an ext_job_cache, don't write to
@@ -51,12 +71,18 @@ def store_job(opts, load, event=None, mminion=None):
         return
 
     # otherwise, write to the master cache
-    fstr = '{0}.returner'.format(opts['master_job_cache'])
+    fstr = '{0}.returner'.format(job_cache)
     if 'fun' not in load and load.get('return', {}):
         ret_ = load.get('return', {})
         if 'fun' in ret_:
             load.update({'fun': ret_['fun']})
         if 'user' in ret_:
             load.update({'user': ret_['user']})
-    mminion.returners[fstr](load)
+    try:
+        mminion.returners[fstr](load)
+    except KeyError:
+        emsg = "Returner '{0}' does not support function returner".format(job_cache)
+        log.error(emsg)
+        raise KeyError(emsg)
+
 # vim:set et sts=4 ts=4 tw=80:
