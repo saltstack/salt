590caae14035b795b9e967e6865175a934017751
Joseph Hall <perlhoser@gmail.com>
2015-03-13 10:48:13 -0600

    Remove hardcoded images, sizes from softlayer_hw driver

--- salt/cloud/clouds/softlayer_hw.py
+++ salt/cloud/clouds/softlayer_hw.py
@@ -95,6 +95,24 @@ def get_conn(service='SoftLayer_Hardware'):
     return client[service]
 
 
+def create_options(call=None):
+    '''
+    List all available locations
+    '''
+    if call == 'action':
+        raise SaltCloudSystemExit(
+            'The create_options function must be called with '
+            '-f or --function'
+        )
+
+    ret = {}
+    conn = get_conn(service='SoftLayer_Hardware')
+    ret['service'] = conn
+    ret['service_dir'] = dir(conn)
+
+    return ret
+
+
 def avail_locations(call=None):
     '''
     List all available locations
@@ -135,34 +153,16 @@ def avail_sizes(call=None):
             '-f or --function, or with the --list-sizes option'
         )
 
-    ret = {
-        'Bare Metal Instance': {
-            '1921': {
-                'id': '1921',
-                'name': '2 x 2.0 GHz Core Bare Metal Instance - 2 GB Ram'},
-            '1922': {
-                'id': '1922',
-                'name': '4 x 2.0 GHz Core Bare Metal Instance - 4 GB Ram'},
-            '1923': {
-                'id': '1923',
-                'name': '8 x 2.0 GHz Core Bare Metal Instance - 8 GB Ram'},
-            '1924': {
-                'id': '1924',
-                'name': '16 x 2.0 GHz Core Bare Metal Instance - 16 GB Ram'},
-            '2164': {
-                'id': '2164',
-                'name': '2 x 2.0 GHz Core Bare Metal Instance - 8 GB Ram '},
-            '2165': {
-                'id': '2165',
-                'name': '4 x 2.0 GHz Core Bare Metal Instance - 16 GB Ram'},
-            '2166': {
-                'id': '2166',
-                'name': '8 x 2.0 GHz Core Bare Metal Instance - 32 GB Ram'},
-            '2167': {
-                'id': '2167',
-                'name': '16 x 2.0 GHz Core Bare Metal Instance - 64 GB Ram'},
-            }
-        }
+    ret = {}
+    conn = get_conn(service='SoftLayer_Product_Package')
+    for category in conn.getCategories(id=50):
+        if category['categoryCode'] != 'server_core':
+            continue
+        for group in category['groups']:
+            for price in group['prices']:
+                ret[price['id']] = price['item'].copy()
+                del ret[price['id']]['id']
+        pprint.pprint(category)
     return ret
 
 
@@ -176,198 +176,16 @@ def avail_images(call=None):
             '-f or --function, or with the --list-images option'
         )
 
-    ret = {'Operating System': {
-        '13962': {
-            'id': '13962',
-            'name': 'CentOS 6.0 - Minimal Install (32 bit)'},
-        '13963': {
-            'id': '13963',
-            'name': 'CentOS 6.0 - Minimal Install (64 bit)'},
-        '13960': {
-            'id': '13960',
-            'name': 'CentOS 6.0 - LAMP Install (32 bit)'},
-        '13961': {
-            'id': '13961',
-            'name': 'CentOS 6.0 - LAMP Install (64 bit)'},
-        '1930': {
-            'id': '1930',
-            'name': 'CentOS 5 - Minimal Install (32 bit)'},
-        '1931': {
-            'id': '1931',
-            'name': 'CentOS 5 - Minimal Install (64 bit)'},
-        '1928': {
-            'id': '1928',
-            'name': 'CentOS 5 - LAMP Install (32 bit)'},
-        '1929': {
-            'id': '1929',
-            'name': 'CentOS 5 - LAMP Install (64 bit)'},
-        '14075': {
-            'id': '14075',
-            'name': 'Debian GNU/Linux 6.0 Squeeze/Stable - Minimal Install (32 bit)'},
-        '14077': {
-            'id': '14077',
-            'name': 'Debian GNU/Linux 6.0 Squeeze/Stable - Minimal Install (64 bit)'},
-        '14074': {
-            'id': '14074',
-            'name': 'Debian GNU/Linux 6.0 Squeeze/Stable - LAMP Install (32 bit)'},
-        '14076': {
-            'id': '14076',
-            'name': 'Debian GNU/Linux 6.0 Squeeze/Stable - LAMP Install (64 bit)'},
-        '21774': {
-            'id': '21774',
-            'name': 'CloudLinux 6 (32 bit)'},
-        '21777': {
-            'id': '21777',
-            'name': 'CloudLinux 6 (64 bit)'},
-        '21768': {
-            'id': '21768',
-            'name': 'CloudLinux 5 (32 bit)'},
-        '21771': {
-            'id': '21771',
-            'name': 'CloudLinux 5 (64 bit)'},
-        '22247': {
-            'id': '22247',
-            'name': 'Debian GNU/Linux 7.0 Wheezy/Stable - Minimal Install (32 bit)'},
-        '22251': {
-            'id': '22251',
-            'name': 'Debian GNU/Linux 7.0 Wheezy/Stable - Minimal Install (64 bit)'},
-        '21265': {
-            'id': '21265',
-            'name': 'FreeBSD 9 Latest (32 bit)'},
-        '21269': {
-            'id': '21269',
-            'name': 'FreeBSD 9 Latest (64 bit)'},
-        '21257': {
-            'id': '21257',
-            'name': 'FreeBSD 8 Latest (32 bit)'},
-        '21261': {
-            'id': '21261',
-            'name': 'FreeBSD 8 Latest (64 bit)'},
-        '2143': {
-            'id': '2143',
-            'name': 'Ubuntu Linux 10.04 LTS Lucid Lynx - Minimal Install (32 bit)'},
-        '2145': {
-            'id': '2145',
-            'name': 'Ubuntu Linux 10.04 LTS Lucid Lynx - Minimal Install (64 bit)'},
-        '2138': {
-            'id': '2138',
-            'name': 'Ubuntu Linux 10.04 LTS Lucid Lynx - LAMP Install (32 bit)'},
-        '2141': {
-            'id': '2141',
-            'name': 'Ubuntu Linux 10.04 LTS Lucid Lynx - LAMP Install (64 bit)'},
-        '17436': {
-            'id': '17436',
-            'name': 'Ubuntu Linux 12.04 LTS Precise Pangolin - Minimal Install (32 bit)'},
-        '17438': {
-            'id': '17438',
-            'name': 'Ubuntu Linux 12.04 LTS Precise Pangolin - Minimal Install (64 bit)'},
-        '17432': {
-            'id': '17432',
-            'name': 'Ubuntu Linux 12.04 LTS Precise Pangolin - LAMP Install (32 bit)'},
-        '17434': {
-            'id': '17434',
-            'name': 'Ubuntu Linux 12.04 LTS Precise Pangolin - LAMP Install (64 bit)'},
-        '20948': {
-            'id': '20948',
-            'name': 'Windows Server 2012 Standard Edition (64 bit)'},
-        '21074': {
-            'id': '21074',
-            'name': 'Windows Server 2008 Standard SP1 with R2 (64 bit)'},
-        '1857': {
-            'id': '1857',
-            'name': 'Windows Server 2008 R2 Standard Edition (64bit)'},
-        '1860': {
-            'id': '1860',
-            'name': 'Windows Server 2008 R2 Enterprise Edition (64bit)'},
-        '1742': {
-            'id': '1742',
-            'name': 'Windows Server 2008 Standard Edition SP2 (32bit)'},
-        '1752': {
-            'id': '1752',
-            'name': 'Windows Server 2008 Standard Edition SP2 (64bit)'},
-        '1756': {
-            'id': '1756',
-            'name': 'Windows Server 2008 Enterprise Edition SP2 (32bit)'},
-        '1761': {
-            'id': '1761',
-            'name': 'Windows Server 2008 Enterprise Edition SP2 (64bit)'},
-        '1766': {
-            'id': '1766',
-            'name': 'Windows Server 2008 Datacenter Edition SP2 (32bit)'},
-        '1770': {
-            'id': '1770',
-            'name': 'Windows Server 2008 Datacenter Edition SP2 (64bit)'},
-        '21060': {
-            'id': '21060',
-            'name': 'Windows Server 2012 Datacenter Edition With Hyper-V (64bit)'},
-        '20971': {
-            'id': '20971',
-            'name': 'Windows Server 2012 Datacenter Edition (64bit)'},
-        '21644': {
-            'id': '21644',
-            'name': 'Windows Server 2008 R2 Datacenter Edition With Hyper-V (64bit)'},
-        '13866': {
-            'id': '13866',
-            'name': 'Windows Server 2008 R2 Datacenter Edition (64bit)'},
-        '1700': {
-            'id': '1700',
-            'name': 'Windows Server 2003 Standard SP2 with R2 (32 bit)'},
-        '1701': {
-            'id': '1701',
-            'name': 'Windows Server 2003 Standard SP2 with R2 (64 bit)'},
-        '1716': {
-            'id': '1716',
-            'name': 'Windows Server 2003 Datacenter SP2 with R2 (32 bit)'},
-        '1715': {
-            'id': '1715',
-            'name': 'Windows Server 2003 Datacenter SP2 with R2 (64 bit)'},
-        '1702': {
-            'id': '1702',
-            'name': 'Windows Server 2003 Enterprise SP2 with R2 (32 bit)'},
-        '1703': {
-            'id': '1703',
-            'name': 'Windows Server 2003 Enterprise SP2 with R2 (64 bit)'},
-        '22418': {
-            'id': '22418',
-            'name': 'Citrix XenServer 6.2'},
-        '21133': {
-            'id': '21133',
-            'name': 'Citrix XenServer 6.1'},
-        '17228': {
-            'id': '17228',
-            'name': 'Citrix XenServer 6.0.2'},
-        '14059': {
-            'id': '14059',
-            'name': 'Citrix XenServer 6.0.0'},
-        '13891': {
-            'id': '13891',
-            'name': 'Citrix XenServer 5.6.2'},
-        '2380': {
-            'id': '2380',
-            'name': 'Citrix XenServer 5.6.1'},
-        '2214': {
-            'id': '2214',
-            'name': 'Citrix XenServer 5.6'},
-        '1806': {
-            'id': '1806',
-            'name': 'Citrix XenServer 5.5'},
-        '21158': {
-            'id': '21158',
-            'name': 'VMware ESXi 5.1'},
-        '14048': {
-            'id': '14048',
-            'name': 'VMware ESX 4.1'},
-        '2032': {
-            'id': '2032',
-            'name': 'VMware ESX 4.0'},
-        '21396': {
-            'id': '21396',
-            'name': 'Vyatta 6.5 Community Edition (64 bit)'},
-        '22177': {
-            'id': '22177',
-            'name': 'Vyatta 6.x Subscription Edition (64 bit)'},
-        }
-    }
+    ret = {}
+    conn = get_conn(service='SoftLayer_Product_Package')
+    for category in conn.getCategories(id=50):
+        if category['categoryCode'] != 'os':
+            continue
+        for group in category['groups']:
+            for price in group['prices']:
+                ret[price['id']] = price['item'].copy()
+                del ret[price['id']]['id']
+        pprint.pprint(category)
     return ret
 
 
