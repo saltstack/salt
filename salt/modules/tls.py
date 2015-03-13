3f02df9ce0c34c5ee70aa716237c1e5f8d30e39b
Justin Phelps <linuturk@onitato.com>
2015-03-13 14:27:47 -0500

    Adding additional output to assist in troubleshooting. Fixes #21638

--- salt/modules/tls.py
+++ salt/modules/tls.py
@@ -45,7 +45,7 @@ def __virtual__():
     '''
     if HAS_SSL:
         return True
-    return False
+    return False, ['PyOpenSSL must be installed before this module can be used.']
 
 
 def cert_base_path(cacert_path=None):
