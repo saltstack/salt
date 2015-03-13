4921d3175921d10caf43b2b879e2acb89672da11
Aditya Kulkarni <adi@saltstack.com>
2015-03-13 19:26:47 +0000

    Format ret list and update docstring for service beacon

--- salt/beacons/service.py
+++ salt/beacons/service.py
@@ -3,12 +3,30 @@
 Send events covering service status
 '''
 
+import logging
+log=logging.getLogger(__name__)
 
 def beacon(config):
     '''
     Scan for the configured services and fire events
+
+    Example Config
+
+    .. code-block:: yaml
+
+        beacons:
+          service:
+            salt-master:
+            mysql:
+
+
+    The config above sets up beacons to check for
+    the salt-master and mysql services.
     '''
-    ret = {}
+    ret = []
     for service in config:
-        ret[service] = __salt__['service.status'](service)
+        ret_dict = {}
+        ret_dict[service] = __salt__['service.status'](service)
+        ret.append(ret_dict)
+
     return ret
