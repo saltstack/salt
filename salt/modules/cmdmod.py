2e530d8c618c4592e5b9e8e6bba19cadabbf8e6d
Sven R. Kunze <srkunze@tbz-pariv.de>
2015-03-16 15:14:35 +0100

    always consider retcode because module function can be used in jinja templates

--- salt/modules/cmdmod.py
+++ salt/modules/cmdmod.py
@@ -1393,6 +1393,28 @@ def exec_code(lang, code, cwd=None):
     return ret
 
 
+def exec_code_all(lang, code, cwd=None):
+    '''
+    Pass in two strings, the first naming the executable language, aka -
+    python2, python3, ruby, perl, lua, etc. the second string containing
+    the code you wish to execute. All cmd artifacts (stdout, stderr, retcode, pid)
+    will be returned
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' cmd.exec_code_all ruby 'puts "cheese"'
+    '''
+    codefile = salt.utils.mkstemp()
+    with salt.utils.fopen(codefile, 'w+t') as fp_:
+        fp_.write(code)
+    cmd = [lang, codefile]
+    ret = run_all(cmd, cwd=cwd, python_shell=False)
+    os.remove(codefile)
+    return ret
+
+
 def tty(device, echo=None):
     '''
     Echo a string to a specific tty
