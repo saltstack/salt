743a53bae60781f883d89182f055c0e7c3a44553
Joseph Hall <perlhoser@gmail.com>
2015-03-16 07:51:10 -0600

    Proper check for decode_out

--- salt/utils/http.py
+++ salt/utils/http.py
@@ -404,7 +404,7 @@ def query(url,
         else:
             text = True
 
-        if os.path.exists(decode_out):
+        if decode_out and os.path.exists(decode_out):
             with salt.utils.fopen(decode_out, 'w') as dof:
                 dof.write(result_text)
 
