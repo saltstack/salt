6c912decf3c0036fd5c1152e66d80b32c967f17c
Gideon Redelinghuys <gideon@cloudflare.com>
2015-03-17 14:53:49 -0700

    Fix postgres_local_cache handling of abnormal return data

--- salt/returners/postgres_local_cache.py
+++ salt/returners/postgres_local_cache.py
@@ -194,6 +194,12 @@ def returner(load):
     '''
     Return data to a postgres server
     '''
+    # salt guarantees that there will be 'fun', 'jid', 'return' and 'id' but not
+    # 'success'
+    success = 'Unknown'
+    if 'success' in load:
+        success = load['success']
+
     conn = _get_conn()
     if conn is None:
         return None
@@ -205,9 +211,9 @@ def returner(load):
         sql, (
             load['fun'],
             load['jid'],
-            json.dumps(load['return']),
+            json.dumps(unicode(str(load['return']), 'utf-8', 'replace')),
             load['id'],
-            load['success']
+            success
         )
     )
     _close_conn(conn)
