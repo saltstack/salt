6560ad09a278c8e3715e15fdfe352591a1a3d253
Bo Maryniuk <bo@suse.de>
2015-03-18 16:02:09 +0100

    Added diff support between current and vendor file.

--- salt/modules/zypper.py
+++ salt/modules/zypper.py
@@ -1125,3 +1125,36 @@ def download(*packages):
         return pkg_ret
 
     raise CommandExecutionError("Unable to download packages: {0}.".format(', '.join(packages)))
+
+
+def diff(*paths):
+    '''
+    Return a formatted diff between current files and original in a package.
+    NOTE: this function includes all files (configuration and not), but does
+    not work on binary content.
+
+    :param path: Full path to the installed file
+    :return: Difference string or raises and exception if examined file is binary.
+
+    .. code-block:: bash
+
+        salt '*' pkg.diff httpd /etc/apache2/httpd.conf
+    '''
+    ret = {}
+
+    pkg_to_paths = {}
+    for pth in paths:
+        pth_pkg = __salt__['lowpkg.owner'](pth)
+        if not pth_pkg:
+            ret[pth] = os.path.exists(pth) and 'Not managed' or 'N/A'
+        else:
+            if pkg_to_paths.get(pth_pkg) is None:
+                pkg_to_paths[pth_pkg] = []
+            pkg_to_paths[pth_pkg].append(pth)
+
+    local_pkgs = __salt__['pkg.download'](*pkg_to_paths.keys())
+    for pkg, files in pkg_to_paths.items():
+        for path in files:
+            ret[path] = __salt__['lowpkg.diff'](local_pkgs[pkg]['path'], path) or 'Unchanged'
+
+    return ret
