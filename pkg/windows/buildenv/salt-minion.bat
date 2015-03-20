d5da92e58c38d63f153f0868d631a02a5e8ee483
twangboy <slee@saltstack.com>
2015-03-20 14:12:48 -0600

    Added batchfiles to mimick the behavior of the exe's

---
+++ pkg/windows/buildenv/salt-minion.bat
@@ -0,0 +1,9 @@
+@ echo off
+:: Script for starting the Salt-Minion
+:: Accepts all parameters that Salt-Minion Accepts
+
+:: Define Variables
+Set Python="%cd%\bin\python.exe"
+Set Script="%cd%\bin\Scripts\salt-minion"
+
+"%Python%" "%Script%" %*
