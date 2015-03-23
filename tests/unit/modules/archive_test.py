e5e287cbb482c276ecd7655aba036bbfafb88a80
rallytime <nicole@saltstack.com>
2015-03-23 10:03:56 -0600

    Pythong 3 compat fix for pylint

--- tests/unit/modules/archive_test.py
+++ tests/unit/modules/archive_test.py
@@ -13,7 +13,14 @@ from __future__ import absolute_import
 # Import Salt Testing libs
 from salttesting import skipIf, TestCase
 from salttesting.helpers import ensure_in_syspath
-from salttesting.mock import NO_MOCK, NO_MOCK_REASON, MagicMock, patch
+from salttesting.mock import (
+    NO_MOCK,
+    NO_MOCK_REASON,
+    MagicMock,
+    patch
+)
+from salt.ext.six.moves import zip  # pylint: disable=import-error,redefined-builtin
+
 ensure_in_syspath('../../')
 
 # Import salt libs
