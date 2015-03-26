fdb91146861f77a1d81a8547b99c9c84afa38773
Nitin Madhok <nmadhok@g.clemson.edu>
2015-03-26 18:52:49 -0400

    Importing required libs, adding virtual function etc

--- salt/cloud/clouds/vmware.py
+++ salt/cloud/clouds/vmware.py
@@ -29,3 +29,54 @@ up authentication. Set up the cloud configuration at:
       password: verybadpass
       host: 'vcenter01.domain.com'
 '''
+from __future__ import absolute_import
+
+# Import python libs
+import pprint
+import logging
+import time
+
+# Import salt libs
+import salt.utils.cloud
+import salt.utils.xmlutil
+from salt.exceptions import SaltCloudSystemExit
+
+# Import salt cloud libs
+import salt.config as config
+
+# Attempt to import pyVim and pyVmomi libs
+HAS_LIBS = False
+try:
+    from pyVim.connect import SmartConnect
+    from pyVmomi import vim
+    HAS_LIBS = True
+except Exception:
+    pass
+
+# Get logging started
+log = logging.getLogger(__name__)
+
+
+# Only load in this module if the VMware configurations are in place
+def __virtual__():
+    '''
+    Check for VMware configuration and if required libs are available.
+    '''
+    if not HAS_LIBS:
+        return False
+
+    if get_configured_provider() is False:
+        return False
+
+    return True
+
+
+def get_configured_provider():
+    '''
+    Return the first configured instance.
+    '''
+    return config.is_provider_configured(
+        __opts__,
+        __active_provider_name__ or 'vmware',
+        ('user',)
+    )
