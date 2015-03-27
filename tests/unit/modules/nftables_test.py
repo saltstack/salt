3351e1c58da06ba8454210c6af25de6de8745b12
Jayesh Kariya <jayeshk@saltstack.com>
2015-03-27 17:41:48 +0530

    added python libs

--- tests/unit/modules/nftables_test.py
+++ tests/unit/modules/nftables_test.py
@@ -3,15 +3,21 @@
     :codeauthor: :email:`Jayesh Kariya <jayeshk@saltstack.com>`
 '''
 
+# Import Python Libs
+from __future__ import absolute_import
+
 # Import Salt Testing Libs
 from salttesting import TestCase, skipIf
 from salttesting.mock import (
     MagicMock,
     patch,
-    mock_open,
     NO_MOCK,
     NO_MOCK_REASON
 )
+
+from salttesting.helpers import ensure_in_syspath
+
+ensure_in_syspath('../../')
 
 # Import Salt Libs
 from salt.modules import nftables
