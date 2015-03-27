7de8ed3c54969513ba8908fd432974da582acff3
Matthew Williams <mgwilliams@gmail.com>
2015-03-27 00:44:48 +0000

    states.boto_vpc: add present and absent states for subnets and internet gateways

--- salt/states/boto_vpc.py
+++ salt/states/boto_vpc.py
@@ -100,13 +100,8 @@ def present(name, cidr_block, instance_tenancy=None, dns_support=None,
            'changes': {}
            }
 
-    _id = {'salt_id': name}
-    if tags:
-        _tags = tags.update(_id)
-    else:
-        _tags = _id
-    exists = __salt__['boto_vpc.exists'](tags=_id, region=region, key=key,
-                                         keyid=keyid, profile=profile)
+    exists = __salt__['boto_vpc.exists'](name=name, tags=tags, region=region,
+                                         key=key, keyid=keyid, profile=profile)
     if not exists:
         if __opts__['test']:
             ret['comment'] = 'VPC {0} is set to be created.'.format(name)
@@ -114,7 +109,7 @@ def present(name, cidr_block, instance_tenancy=None, dns_support=None,
             return ret
         created = __salt__['boto_vpc.create'](cidr_block, instance_tenancy,
                                               name, dns_support, dns_hostnames,
-                                              _tags, region, key, keyid,
+                                              tags, region, key, keyid,
                                               profile)
         if not created:
             ret['result'] = False
@@ -131,20 +126,38 @@ def present(name, cidr_block, instance_tenancy=None, dns_support=None,
 
 
 def absent(name, tags=None, region=None, key=None, keyid=None, profile=None):
+    '''
+    Ensure VPC with passed properties is absent.
+
+    name
+        Name of the VPC.
+
+    tags
+        A list of tags. All tags must match.
+
+    region
+        Region to connect to.
+
+    key
+        Secret key to be used.
+
+    keyid
+        Access key to be used.
+
+    profile
+        A dict with region, key and keyid, or a pillar key (string) that
+        contains a dict with region, key and keyid.
+    '''
+
     ret = {'name': name,
            'result': True,
            'comment': '',
            'changes': {}
            }
 
-    _id = {'salt_id': name}
-    if tags:
-        _tags = tags.update(_id)
-    else:
-        _tags = _id
-    exists = __salt__['boto_vpc.exists'](tags=_id, region=region, key=key,
-                                         keyid=keyid, profile=profile)
-    if not exists:
+    vpc_id = __salt__['boto_vpc.get_id'](name=name, tags=tags, region=region,
+                                         key=key, keyid=keyid, profile=profile)
+    if not vpc_id:
         ret['comment'] = '{0} VPC does not exist.'.format(name)
         return ret
 
@@ -159,7 +172,253 @@ def absent(name, tags=None, region=None, key=None, keyid=None, profile=None):
         ret['result'] = False
         ret['comment'] = 'Failed to delete {0} VPC.'.format(name)
         return ret
-    ret['changes']['old'] = {'vpc': name}
+    ret['changes']['old'] = {'vpc': vpc_id}
     ret['changes']['new'] = {'vpc': None}
     ret['comment'] = 'VPC {0} deleted.'.format(name)
+    return ret
+
+
+def subnet_present(name, cidr_block, vpc_name=None, vpc_id=None,
+                   availability_zone=None, tags=None, region=None,
+                   key=None, keyid=None, profile=None):
+    '''
+    Ensure a subnet exists.
+
+    name
+        Name of the subnet.
+
+    cidr_block
+        The range if IPs for the subnet, in CIDR format. For example:
+        10.0.0.0/24. Block size must be between /16 and /28 netmask.
+
+    vpc_name
+        Name of the VPC in which the subnet should be placed. Either
+        vpc_name or vpc_id must be provided.
+
+    vpc_id
+        Id of the VPC in which the subnet should be placed. Either vpc_name
+        or vpc_id must be provided.
+
+    availability_zone
+        AZ in which the subnet should be placed.
+
+    tags
+        A list of tags.
+
+    region
+        Region to connect to.
+
+    key
+        Secret key to be used.
+
+    keyid
+        Access key to be used.
+
+    profile
+        A dict with region, key and keyid, or a pillar key (string) that
+        contains a dict with region, key and keyid.
+    '''
+
+    ret = {'name': name,
+           'result': True,
+           'comment': '',
+           'changes': {}
+           }
+
+    exists = __salt__['boto_vpc.subnet_exists'](subnet_name=name, tags=tags,
+                                                region=region, key=key,
+                                                keyid=keyid, profile=profile)
+    if not exists:
+        if __opts__['test']:
+            ret['comment'] = 'Subnet {0} is set to be created.'.format(name)
+            ret['result'] = None
+            return ret
+        created = __salt__['boto_vpc.create_subnet'](subnet_name=name,
+                                                     cidr_block=cidr_block,
+                                                     availability_zone=availability_zone,
+                                                     vpc_name=vpc_name, vpc_id=vpc_id,
+                                                     tags=tags, region=region,
+                                                     key=key, keyid=keyid,
+                                                     profile=profile)
+        if not created:
+            ret['result'] = False
+            ret['comment'] = 'Failed to create {0} subnet.'.format(name)
+            return ret
+        ret['changes']['old'] = {'vpc': None}
+        ret['changes']['new'] = {'vpc': created}
+        ret['comment'] = 'Subnet {0} created.'.format(name)
+        return ret
+    ret['comment'] = 'Subnet present.'
+    return ret
+
+
+def subnet_absent(name, region=None, key=None, keyid=None, profile=None):
+    '''
+    Ensure subnet with passed properties is absent.
+
+    name
+        Name of the subnet.
+
+    region
+        Region to connect to.
+
+    key
+        Secret key to be used.
+
+    keyid
+        Access key to be used.
+
+    profile
+        A dict with region, key and keyid, or a pillar key (string) that
+        contains a dict with region, key and keyid.
+    '''
+
+    ret = {'name': name,
+           'result': True,
+           'comment': '',
+           'changes': {}
+           }
+
+    subnet_id = __salt__['boto_vpc.get_resource_id']('subnet', name=name,
+                                                     region=region, key=key,
+                                                     keyid=keyid, profile=profile)
+    if not subnet_id:
+        ret['comment'] = '{0} subnet does not exist.'.format(name)
+        return ret
+
+    if __opts__['test']:
+        ret['comment'] = 'Subnet {0} is set to be removed.'.format(name)
+        ret['result'] = None
+        return ret
+    deleted = __salt__['boto_vpc.delete_subnet'](subnet_name=name,
+                                                 region=region, key=key,
+                                                 keyid=keyid, profile=profile)
+    if not deleted:
+        ret['result'] = False
+        ret['comment'] = 'Failed to delete {0} subnet.'.format(name)
+        return ret
+    ret['changes']['old'] = {'subnet': subnet_id}
+    ret['changes']['new'] = {'subnet': None}
+    ret['comment'] = 'Subnet {0} deleted.'.format(name)
+    return ret
+
+
+def internet_gateway_present(name, vpc_name=None, vpc_id=None,
+                             tags=None, region=None, key=None,
+                             keyid=None, profile=None):
+    '''
+    Ensure an internet gateway exists.
+
+    name
+        Name of the internet gateway.
+
+    vpc_name
+        Name of the VPC to which the internet gateway should be attached.
+
+    vpc_id
+        Id of the VPC to which the internet_gateway should be attached.
+        Only one of vpc_name or vpc_id may be provided.
+
+    tags
+        A list of tags.
+
+    region
+        Region to connect to.
+
+    key
+        Secret key to be used.
+
+    keyid
+        Access key to be used.
+
+    profile
+        A dict with region, key and keyid, or a pillar key (string) that
+        contains a dict with region, key and keyid.
+    '''
+
+    ret = {'name': name,
+           'result': True,
+           'comment': '',
+           'changes': {}
+           }
+
+    exists = __salt__['boto_vpc.resource_exists']('internet_gateway', name=name,
+                                                  region=region, key=key,
+                                                  keyid=keyid, profile=profile)
+    if not exists:
+        if __opts__['test']:
+            ret['comment'] = 'Internet gateway {0} is set to be created.'.format(name)
+            ret['result'] = None
+            return ret
+        created = __salt__['boto_vpc.create_internet_gateway'](internet_gateway_name=name,
+                                                               vpc_name=vpc_name, vpc_id=vpc_id,
+                                                               tags=tags, region=region,
+                                                               key=key, keyid=keyid,
+                                                               profile=profile)
+        if not created:
+            ret['result'] = False
+            ret['comment'] = 'Failed to create internet gateway {0}.'.format(name)
+            return ret
+
+        ret['changes']['old'] = {'vpc': None}
+        ret['changes']['new'] = {'vpc': created}
+        ret['comment'] = 'Internet gateway {0} created.'.format(name)
+        return ret
+    ret['comment'] = 'Internet gateway {0} present.'.format(name)
+    return ret
+
+
+def internet_gateway_absent(name, detach=False, region=None,
+                            key=None, keyid=None, profile=None):
+    '''
+    Ensure the named internet gateway is absent.
+
+    name
+        Name of the internet gateway.
+
+    detach
+        First detach the internet gateway from a VPC, if attached.
+
+    region
+        Region to connect to.
+
+    key
+        Secret key to be used.
+
+    keyid
+        Access key to be used.
+
+    profile
+        A dict with region, key and keyid, or a pillar key (string) that
+        contains a dict with region, key and keyid.
+    '''
+
+    ret = {'name': name,
+           'result': True,
+           'comment': '',
+           'changes': {}
+           }
+
+    igw_id = __salt__['boto_vpc.get_resource_id']('internet_gateway', name=name,
+                                                  region=region, key=key,
+                                                  keyid=keyid, profile=profile)
+    if not igw_id:
+        ret['comment'] = 'Internet gateway {0} does not exist.'.format(name)
+        return ret
+
+    if __opts__['test']:
+        ret['comment'] = 'Internet gateway {0} is set to be removed.'.format(name)
+        ret['result'] = None
+        return ret
+    deleted = __salt__['boto_vpc.delete_internet_gateway'](internet_gateway_name=name,
+                                                           detach=detach, region=region,
+                                                           key=key, keyid=keyid,
+                                                           profile=profile)
+    if not deleted:
+        ret['result'] = False
+        ret['comment'] = 'Failed to delete internet gateway {0}.'.format(name)
+        return ret
+    ret['changes']['old'] = {'internet_gateway': igw_id}
+    ret['changes']['new'] = {'internet_gateway': None}
+    ret['comment'] = 'Internet gateway {0} deleted.'.format(name)
     return ret
