saltstack/salt

commit 15bc3af68edd1d75b5e72a0e58f58c96dd8d2a50

Fix used-before-defined error with branch var in git_pillar

salt/pillar/git_pillar.py
@@ -285,17 +285,16 @@ def ext_pillar(minion_id,
         else:
             log.warning('Unrecognized extra parameter: {0}'.format(key))
 
-    gitpil = GitPillar(branch, repo_location, __opts__)
-
-    branch = gitpil.branch
-
     # environment is "different" from the branch
-    _, _, environment = branch_env.partition(':')
+    branch, _, environment = branch_env.partition(':')
     if environment == '':
         if branch == 'master':
             environment = 'base'
         else:
             environment = branch
+
+    gitpil = GitPillar(branch, repo_location, __opts__)
+    branch = gitpil.branch
 
     # normpath is needed to remove appended '/' if root is empty string.
     pillar_dir = os.path.normpath(os.path.join(gitpil.working_dir, root))

commit a7e95471e273cb2d4cbecf70ff24b1fb4cd85e74

Fix bug introduced in 15bc3af68edd1d75b5e72a0e58f58c96dd8d2a50
The branch must be used _after_ possibly modified in GitPillar()

salt/pillar/git_pillar.py
@@ -287,14 +287,15 @@ def ext_pillar(minion_id,
 
     # environment is "different" from the branch
     branch, _, environment = branch_env.partition(':')
+
+    gitpil = GitPillar(branch, repo_location, __opts__)
+    branch = gitpil.branch
+
     if environment == '':
         if branch == 'master':
             environment = 'base'
         else:
             environment = branch
-
-    gitpil = GitPillar(branch, repo_location, __opts__)
-    branch = gitpil.branch
 
     # normpath is needed to remove appended '/' if root is empty string.
     pillar_dir = os.path.normpath(os.path.join(gitpil.working_dir, root))