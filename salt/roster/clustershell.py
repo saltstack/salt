e4ca9f0405833346b4bed3d162adb375233541f5
Mathieu Drapeau <mathieu.drapeau@intel.com>
2015-03-31 15:37:29 -0400

    Roster to provide PDSH/ClusterShell like hostname inputs
    see: https://github.com/saltstack/salt/issues/22208

---
+++ salt/roster/clustershell.py
@@ -0,0 +1,32 @@
+# Import python libs
+from __future__ import absolute_import
+
+import socket
+from ClusterShell.NodeSet import NodeSet
+
+def targets(tgt, tgt_type='glob', **kwargs):
+    '''
+    Return the targets
+    '''
+    ret = {}
+    ports = __opts__['ssh_scan_ports']
+    if not isinstance(ports, list):
+        # Comma-separate list of integers
+        ports = list(map(int, str(ports).split(',')))
+
+    hosts = list(NodeSet(tgt))
+    addrs = [socket.gethostbyname(h) for h in hosts]
+
+    for addr in addrs:
+        addr = str(addr)
+        for port in ports:
+            try:
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock.settimeout(float(__opts__['ssh_scan_timeout']))
+                sock.connect((addr, port))
+                sock.shutdown(socket.SHUT_RDWR)
+                sock.close()
+                ret[addr] = {'host': addr, 'port': port}
+            except socket.error:
+                pass
+    return ret
