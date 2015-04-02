df4930801d6bac36bec2edbbdbdd9e5aedd64eb2
rallytime <nicole@saltstack.com>
2015-04-02 15:48:54 -0600

    Fix code-block display for beacons.load doc example

--- salt/beacons/load.py
+++ salt/beacons/load.py
@@ -31,19 +31,19 @@ def beacon(config):
     and only emit a beacon if any of them are
     exceeded.
 
-    code_block:: yaml
+    code-block:: yaml
 
         beacons:
-            - load:
-              - 1m:
-                - 0.0
-                - 2.0
-              - 5m:
-                - 0.0
-                - 1.5
-              - 15m:
-                - 0.1
-                - 1.0
+          - load:
+            - 1m:
+              - 0.0
+              - 2.0
+            - 5m:
+              - 0.0
+              - 1.5
+            - 15m:
+              - 0.1
+              - 1.0
 
     '''
     log.trace('load beacon starting')
