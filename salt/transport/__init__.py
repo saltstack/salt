2781cb83c1496ec8d9c3abc07e655d954ad00ed1
Thomas Jackson <jacksontj.89@gmail.com>
2015-04-03 07:46:17 -0700

    Make sure to hand out RAET channels on the old interface

--- salt/transport/__init__.py
+++ salt/transport/__init__.py
@@ -10,6 +10,20 @@ from salt.transport.client import ReqChannel
 class Channel(ReqChannel):
     @staticmethod
     def factory(opts, **kwargs):
+        # Default to ZeroMQ for now
+        ttype = 'zeromq'
+
+        # determine the ttype
+        if 'transport' in opts:
+            ttype = opts['transport']
+        elif 'transport' in opts.get('pillar', {}).get('master', {}):
+            ttype = opts['pillar']['master']['transport']
+
+        # the raet ioflo implementation still uses this channel, we need
+        # this as compatibility
+        if ttype == 'raet':
+            import salt.transport.raet
+            return salt.transport.raet.RAETReqChannel(opts, **kwargs)
         # TODO: deprecation warning, should use
         # salt.transport.channel.Channel.factory()
         return ReqChannel.factory(opts, **kwargs)
--- salt/transport/raet.py
+++ salt/transport/raet.py
@@ -1,5 +1,5 @@
 '''
-Zeromq transport classes
+RAET transport classes
 '''
 
 from __future__ import absolute_import
