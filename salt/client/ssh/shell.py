1452e9c875447f0e7056c31f0fbcf471d2cfa3fb
Colton Myers <colton.myers@gmail.com>
2015-04-03 14:22:42 -0600

    Backport salt.client.ssh.shell fixes from 2015.2

--- salt/client/ssh/shell.py
+++ salt/client/ssh/shell.py
@@ -6,6 +6,7 @@ Manage transport commands via ssh
 # Import python libs
 import re
 import os
+import json
 import time
 import logging
 import subprocess
@@ -92,7 +93,7 @@ class Shell(object):
             options.append('PasswordAuthentication=yes')
         else:
             options.append('PasswordAuthentication=no')
-        if self.opts['_ssh_version'] > '4.9':
+        if self.opts.get('_ssh_version', '') > '4.9':
             options.append('GSSAPIAuthentication=no')
         options.append('ConnectTimeout={0}'.format(self.timeout))
         if self.opts.get('ignore_host_keys'):
@@ -153,7 +154,7 @@ class Shell(object):
         '''
         if self.passwd:
             # Using single quotes prevents shell expansion and
-            # passwords containig '$'
+            # passwords containing '$'
             return "{0} {1} '{2} -p {3} {4}@{5}'".format(
                     'ssh-copy-id',
                     '-i {0}.pub'.format(self.priv),
@@ -170,7 +171,7 @@ class Shell(object):
         '''
         if self.passwd:
             # Using single quotes prevents shell expansion and
-            # passwords containig '$'
+            # passwords containing '$'
             return "{0} {1} {2} -p {3} {4}@{5}".format(
                     'ssh-copy-id',
                     '-i {0}.pub'.format(self.priv),
@@ -197,6 +198,7 @@ class Shell(object):
         # TODO: if tty, then our SSH_SHIM cannot be supplied from STDIN Will
         # need to deliver the SHIM to the remote host and execute it there
 
+        opts = ''
         tty = self.tty
         if ssh != 'ssh':
             tty = False
@@ -317,52 +319,38 @@ class Shell(object):
         sent_passwd = 0
         ret_stdout = ''
         ret_stderr = ''
-        while True:
-            stdout, stderr = term.recv()
-            if stdout:
-                ret_stdout += stdout
-            if stderr:
-                ret_stderr += stderr
-            if stdout and SSH_PASSWORD_PROMPT_RE.search(stdout):
-                if len(stdout) > 256:
-                    pass
-                elif not self.passwd:
-                    try:
-                        term.close(terminate=True, kill=True)
-                    except salt.utils.vt.TerminalException:
-                        pass
-                    return '', 'Permission denied, no authentication information', 254
-                if sent_passwd < passwd_retries:
-                    term.sendline(self.passwd)
-                    sent_passwd += 1
-                    continue
-                else:
-                    # asking for a password, and we can't seem to send it
-                    try:
-                        term.close(terminate=True, kill=True)
-                    except salt.utils.vt.TerminalException:
-                        pass
-                    return '', 'Password authentication failed', 254
-            elif stdout and KEY_VALID_RE.search(stdout):
-                if key_accept:
-                    term.sendline('yes')
-                    continue
-                else:
-                    term.sendline('no')
-                    ret_stdout = ('The host key needs to be accepted, to '
-                                  'auto accept run salt-ssh with the -i '
-                                  'flag:\n{0}').format(stdout)
-                    return ret_stdout, '', 254
-            if not term.isalive():
-                while True:
-                    stdout, stderr = term.recv()
-                    if stdout:
-                        ret_stdout += stdout
-                    if stderr:
-                        ret_stderr += stderr
-                    if stdout is None and stderr is None:
-                        break
-                term.close(terminate=True, kill=True)
-                break
-            time.sleep(0.5)
-        return ret_stdout, ret_stderr, term.exitstatus
+
+        try:
+            while term.has_unread_data:
+                stdout, stderr = term.recv()
+                if stdout:
+                    ret_stdout += stdout
+                if stderr:
+                    ret_stderr += stderr
+                if stdout and SSH_PASSWORD_PROMPT_RE.search(stdout):
+                    if not self.passwd:
+                        return '', 'Permission denied, no authentication information', 254
+                    if sent_passwd < passwd_retries:
+                        term.sendline(self.passwd)
+                        sent_passwd += 1
+                        continue
+                    else:
+                        # asking for a password, and we can't seem to send it
+                        return '', 'Password authentication failed', 254
+                elif stdout and KEY_VALID_RE.search(stdout):
+                    if key_accept:
+                        term.sendline('yes')
+                        continue
+                    else:
+                        term.sendline('no')
+                        ret_stdout = ('The host key needs to be accepted, to '
+                                      'auto accept run salt-ssh with the -i '
+                                      'flag:\n{0}').format(stdout)
+                        return ret_stdout, '', 254
+                elif stdout and stdout.endswith('_||ext_mods||_'):
+                    mods_raw = json.dumps(self.mods, separators=(',', ':')) + '|_E|0|'
+                    term.sendline(mods_raw)
+                time.sleep(0.5)
+            return ret_stdout, ret_stderr, term.exitstatus
+        finally:
+            term.close(terminate=True, kill=True)
