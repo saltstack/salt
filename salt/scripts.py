efb9d411bc887909f8588c9c7191d8fe09ede7d9
Thomas Jackson <jacksontj.89@gmail.com>
2015-04-02 19:00:50 -0700

    Fix suicide_when_without_parent
    If you call sys.exit within a thread it raises SystemExit-- which inside of a thread just exits the thread. This os._exit() will *actually* allow the thread to cause the process to exit

--- salt/scripts.py
+++ salt/scripts.py
@@ -70,7 +70,9 @@ def minion_process(queue):
                 # check pid alive (Unix only trick!)
                 os.kill(parent_pid, 0)
             except OSError:
-                sys.exit(999)
+                # forcibly exit, regular sys.exit raises an exception-- which
+                # isn't sufficient in a thread
+                os._exit(999)
     if not salt.utils.is_windows():
         thread = threading.Thread(target=suicide_when_without_parent, args=(os.getppid(),))
         thread.start()
