5b19d110fcd2f6916a393bc24444462d0f5b2ac2
Andrea-Redi Tsarida <andrea-redi.tsarida@rocket-internet.de>
2015-04-05 09:32:33 +0200

    pylint + syntax

--- salt/states/boto_cfn.py
+++ salt/states/boto_cfn.py
@@ -234,7 +234,7 @@ def _get_template(template, name):
 
 
 def _validate(template_body=None, template_url=None, region=None, key=None, keyid=None, profile=None):
-    # Validates template. returns true if template syndax is correct.
+    # Validates template. returns true if template syntax is correct.
     validate = __salt__['boto_cfn.validate_template'](template_body, template_url, region, key, keyid, profile)
     log.debug('Validate is result is {0}.'.format(str(validate)))
     if isinstance(validate, str):
