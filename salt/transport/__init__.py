5c7df1431ba90b6f76aed46b4827f7cfdc21ae1f
Thomas Jackson <jacksontj.89@gmail.com>
2015-04-08 08:08:42 -0700

    pylint cleanup

--- salt/transport/__init__.py
+++ salt/transport/__init__.py
@@ -7,6 +7,7 @@ from __future__ import absolute_import
 # for backwards compatibility
 from salt.transport.client import ReqChannel
 
+
 class Channel(ReqChannel):
     @staticmethod
     def factory(opts, **kwargs):
