7433194e246ce806cdccb9acd055188e670c764e
Thomas Jackson <jacksontj.89@gmail.com>
2015-04-08 12:28:35 -0700

    fix typo

--- salt/crypt.py
+++ salt/crypt.py
@@ -870,7 +870,7 @@ class SAuth(AsyncAuth):
 
     # has to remain empty for singletons, since __init__ will *always* be called
     def __init__(self, opts, io_loop=None):
-        super(SAuth, self).__init__(opts, io_loop=lo_loop)
+        super(SAuth, self).__init__(opts, io_loop=io_loop)
         pass
 
     # an init for the singleton instance to call
