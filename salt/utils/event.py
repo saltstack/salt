0f4126a13e22f2873786d9fd865ef885d9a9c0a0
Thomas Jackson <jacksontj.89@gmail.com>
2015-04-07 20:29:49 -0700

    Cleanup AsyncEvent publisher
    And remove PollingEventPublisher

--- salt/utils/event.py
+++ salt/utils/event.py
@@ -649,16 +649,14 @@ class AsyncEventPublisher(object):
     def __init__(self, opts, publish_handler, io_loop=None):
         self.opts = opts
         self.publish_handler = publish_handler
-        if io_loop is None:
-            self.io_loop = zmq.eventloop.ioloop.ZMQIOLoop()  # TODO: auto detect if zmq? instead of always
-        else:
-            self.io_loop = io_loop
+
+        self.io_loop = io_loop or zmq.eventloop.ioloop.ZMQIOLoop()
         self.context = zmq.Context()
 
         hash_type = getattr(hashlib, self.opts.get('hash_type', 'md5'))
         # Only use the first 10 chars to keep longer hashes from exceeding the
         # max socket path length.
-        id_hash = hash_type(self.opts['id']).hexdigest()[:10]
+        id_hash = hash_type(self.opts.get('id', '')).hexdigest()[:10]
         epub_sock_path = os.path.join(
             self.opts['sock_dir'],
             'minion_event_{0}_pub.ipc'.format(id_hash)
@@ -765,138 +763,8 @@ class AsyncEventPublisher(object):
             return None
 
     def destroy(self):
-        if hasattr(self, 'epub_sock') and self.epub_sock.closed is False:
-            self.epub_sock.close()
-        if hasattr(self, 'epull_sock') and self.epull_sock.closed is False:
-            self.epull_sock.close()
-
-    def __del__(self):
-        self.destroy()
-
-
-class PollingEventPublisher(object):
-    '''
-    An event publisher class intended to run *in* a process, instead of a separate one
-
-    TODO: remove references to "minion_event" whenever we need to use this for other things
-    '''
-    def __init__(self, opts):
-        self.opts = opts
-        self.context = zmq.Context()
-
-        hash_type = getattr(hashlib, self.opts.get('hash_type', 'md5'))
-        # Only use the first 10 chars to keep longer hashes from exceeding the
-        # max socket path length.
-        id_hash = hash_type(self.opts['id']).hexdigest()[:10]
-        epub_sock_path = os.path.join(
-            self.opts['sock_dir'],
-            'minion_event_{0}_pub.ipc'.format(id_hash)
-        )
-        if os.path.exists(epub_sock_path):
-            os.unlink(epub_sock_path)
-        epull_sock_path = os.path.join(
-            self.opts['sock_dir'],
-            'minion_event_{0}_pull.ipc'.format(id_hash)
-        )
-        if os.path.exists(epull_sock_path):
-            os.unlink(epull_sock_path)
-
-        self.epub_sock = self.context.socket(zmq.PUB)
-
-        if self.opts.get('ipc_mode', '') == 'tcp':
-            epub_uri = 'tcp://127.0.0.1:{0}'.format(
-                self.opts['tcp_pub_port']
-            )
-            epull_uri = 'tcp://127.0.0.1:{0}'.format(
-                self.opts['tcp_pull_port']
-            )
-        else:
-            epub_uri = 'ipc://{0}'.format(epub_sock_path)
-            salt.utils.zeromq.check_ipc_path_max_len(epub_uri)
-            epull_uri = 'ipc://{0}'.format(epull_sock_path)
-            salt.utils.zeromq.check_ipc_path_max_len(epull_uri)
-
-        log.debug(
-            '{0} PUB socket URI: {1}'.format(
-                self.__class__.__name__, epub_uri
-            )
-        )
-        log.debug(
-            '{0} PULL socket URI: {1}'.format(
-                self.__class__.__name__, epull_uri
-            )
-        )
-
-        # Check to make sure the sock_dir is available, create if not
-        default_minion_sock_dir = os.path.join(
-            salt.syspaths.SOCK_DIR,
-            'minion'
-        )
-        minion_sock_dir = self.opts.get('sock_dir', default_minion_sock_dir)
-
-        if not os.path.isdir(minion_sock_dir):
-            # Let's try to create the directory defined on the configuration
-            # file
-            try:
-                os.makedirs(minion_sock_dir, 0o755)
-            except OSError as exc:
-                log.error('Could not create SOCK_DIR: {0}'.format(exc))
-                # Let's not fail yet and try using the default path
-                if minion_sock_dir == default_minion_sock_dir:
-                    # We're already trying the default system path, stop now!
-                    raise
-
-            if not os.path.isdir(default_minion_sock_dir):
-                try:
-                    os.makedirs(default_minion_sock_dir, 0o755)
-                except OSError as exc:
-                    log.error('Could not create SOCK_DIR: {0}'.format(exc))
-                    # Let's stop at this stage
-                    raise
-
-        # Create the pull socket
-        self.epull_sock = self.context.socket(zmq.PULL)
-
-        # Securely bind the event sockets
-        if self.opts.get('ipc_mode', '') != 'tcp':
-            old_umask = os.umask(0o177)
-        try:
-            log.info('Starting pub socket on {0}'.format(epub_uri))
-            self.epub_sock.bind(epub_uri)
-            log.info('Starting pull socket on {0}'.format(epull_uri))
-            self.epull_sock.bind(epull_uri)
-        finally:
-            if self.opts.get('ipc_mode', '') != 'tcp':
-                os.umask(old_umask)
-
-    def handle_publish(self):
-        '''
-        Get something from epull, publish it out epub, and return the package (or None)
-        '''
-        package = self.epull_sock.recv(zmq.NOBLOCK)
-        try:
-            self.epub_sock.send(package)
-            return package
-        # Add an extra fallback in case a forked process leeks through
-        except zmq.ZMQError as exc:
-            # The interrupt caused by python handling the
-            # SIGCHLD. Throws this error with errno == EINTR.
-            # Nothing to receive on the zmq socket throws this error
-            # with EAGAIN.
-            # Both are safe to ignore
-            if exc.errno != errno.EAGAIN and exc.errno != errno.EINTR:
-                log.critical('Unexpected ZMQError while polling minion',
-                             exc_info=True)
-            return None
-
-    @property
-    def socket(self):
-        '''
-        Socket to do poll() on
-        '''
-        return self.epull_sock
-
-    def destroy(self):
+        if hasattr(self, 'stream') and self.stream.closed is False:
+            self.stream.close()
         if hasattr(self, 'epub_sock') and self.epub_sock.closed is False:
             self.epub_sock.close()
         if hasattr(self, 'epull_sock') and self.epull_sock.closed is False:
