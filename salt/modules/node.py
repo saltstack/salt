bbed7c06a38d477fef7c52f8781ae5c0881a7380
Bo Maryniuk <bo@suse.de>
2015-04-13 16:10:45 +0200

    Initial commit for Inspector module

---
+++ salt/modules/node.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2015 SUSE LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+'''
+Module for full system inspection.
+'''
+from __future__ import absolute_import
+import os
+import re
+import uuid
+import logging
+import importlib
+
+# Import Salt libs
+import salt.utils
+import salt.utils.fsutils
+from salt.exceptions import CommandExecutionError
+
+
+log = logging.getLogger(__name__)
+
+# Import inspectlib
+from salt.modules.inspectlib import query as q
+
+
+def __virtual__():
+    '''
+    Only work on POSIX-like systems
+    '''
+    return not salt.utils.is_windows()
+
+
+def _(module):
+    '''
+    Get inspectlib module for the lazy loader.
+
+    :param module:
+    :return:
+    '''
+
+    mod = importlib.import_module("salt.modules.inspectlib.{0}".format(module))
+    mod.__grains__ = __grains__
+    mod.__pillar__ = __pillar__
+    mod.__salt__ = __salt__
+
+    return mod
+
+
+def query(scope):
+    '''
+    Query the node for specific information.
+
+    Parameters:
+
+    * **scope**: Specify scope of the query.
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' node.query scope=os
+    '''
+
+    return _("query").Query(scope)()
