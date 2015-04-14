f87e2f1fcab4f442feae16093b14e36d5e77b59f
Bo Maryniuk <bo@suse.de>
2015-04-14 12:40:51 +0200

    Get OS data

--- salt/modules/inspectlib/query.py
+++ salt/modules/inspectlib/query.py
@@ -117,6 +117,18 @@ class SysInfo(object):
 
         return data
 
+    def _get_os(self):
+        '''
+        Get operating system summary
+        '''
+        return {
+            'name': self._grain('os'),
+            'family': self._grain('os_family'),
+            'arch': self._grain('osarch'),
+            'release': self._grain('osrelease'),
+        }
+
+
 
 class Query(object):
     '''
