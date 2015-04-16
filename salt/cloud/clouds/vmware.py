ebf7add2ffea044f15d721dd4a1b65e7590a7d73
Ralph M Goodberlet <rgoodbe@g.clemson.edu>
2015-04-16 16:00:38 -0400

    Removing unwanted newlines and justified comments to code blocks.

--- salt/cloud/clouds/vmware.py
+++ salt/cloud/clouds/vmware.py
@@ -1569,7 +1569,6 @@ def create_cluster(kwargs=None, call=None):
 
 
 def vm_tools_upgrade(vm_name, reboot=False, call=None):
-    
     '''
     To check status and upgrade VMware Tools on a VM using vm name.
     Default is ``reboot=False``  which attempts to suppress the reboot
@@ -1582,7 +1581,6 @@ def vm_tools_upgrade(vm_name, reboot=False, call=None):
         salt-cloud -a vm_tools_upgrade vmname
         salt-cloud -a vm_tools_upgrade vmname reboot=True
     '''
-
     if call != 'action':
         raise SaltCloudSystemExit(
             'The start action must be called with -a or --action.'
@@ -1595,46 +1593,39 @@ def vm_tools_upgrade(vm_name, reboot=False, call=None):
         if vm.guest.toolsStatus == "toolsOk":
             ret = 'VMware tools is up to date.'
             log.info('VM {0} {1}'.format(vm_name, ret))
-
             return ret
         
-    # If vmware tools is out of date, check major OS family.
-    # Upgrade tools on Linux guests.
+        # If vmware tools is out of date, check major OS family.
+        # Upgrade tools on Linux guests.
         elif vm.guest.toolsStatus == "toolsOld":
             if vm.guest.guestFamily == "linuxGuest":
                 try:
                     log.info('Upgrading VMware tools on {0}'.format(vm_name))
                     task = vm.UpgradeTools()
                     _wait_for_task(task, vm_name, "Upgrade tools", 5, "info")
-
                 except Exception as exc:
                     log.error('Could not upgrade VMware tools on VM {0}: {1}'.format(vm_name, exc))
-
                     return 'failed to upgrade VMware tools'
                 
-    # If Windows, check if reboot is true.
+            # If Windows, check if reboot is true.
             elif vm.guest.guestFamily == "windowsGuest":
                 if reboot:
                     try:
                         log.info('Upgrading VMware tools on {0}'.format(vm_name))
                         task = vm.UpgradeTools()
                         _wait_for_task(task, vm_name, "Upgrade tools", 5, "info")
-
                     except Exception as exc:
                         log.error('Could not upgrade VMware tools on VM {0}: {1}'.format(vm_name, exc))
-
                         return 'failed to upgrade VMware tools'
 
-    # If reboot is false, upgrade tools while supressing the reboot.
+                # If reboot is false, upgrade tools while supressing the reboot.
                 else:
                     try:
                         log.info('Upgrading VMware tools on {0}'.format(vm_name))
                         task = vm.UpgradeTools('/S /v"/qn REBOOT=R"')
                         _wait_for_task(task, vm_name, "Upgrade tools", 5, "info")
-
                     except Exception as exc:
                         log.error('Could not upgrade VMware tools on VM {0}: {1}'.format(vm_name, exc))
-
                         return 'failed to upgrade VMware tools'
 
             return 'VMware tools upgraded'
@@ -1645,4 +1636,3 @@ def vm_tools_upgrade(vm_name, reboot=False, call=None):
         log.info('VM {0} {1}'.format(vm_name, ret))
 
     return ret
-
