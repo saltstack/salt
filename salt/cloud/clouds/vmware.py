b90471b2753c450f4f5090365a05aaf34bb2148e
Nitin Madhok <nmadhok@g.clemson.edu>
2015-04-16 12:01:02 -0400

    Lint fix

--- salt/cloud/clouds/vmware.py
+++ salt/cloud/clouds/vmware.py
@@ -437,7 +437,7 @@ def _wait_for_task(task, vm_name, task_type, sleep_seconds=1, log_level='debug')
     time_counter = 0
     while task.info.state == 'running':
         message = "[ {0} ] Waiting for {1} task to finish [{2} s]".format(vm_name, task_type, time_counter)
-        if log_level=='info':
+        if log_level == 'info':
             log.info(message)
         else:
             log.debug(message)
@@ -445,7 +445,7 @@ def _wait_for_task(task, vm_name, task_type, sleep_seconds=1, log_level='debug')
         time_counter += int(sleep_seconds)
     if task.info.state == 'success':
         message = "[ {0} ] Successfully completed {1} task in {2} seconds".format(vm_name, task_type, time_counter)
-        if log_level=='info':
+        if log_level == 'info':
             log.info(message)
         else:
             log.debug(message)
