cd528b472792159aebc9056d6067c587785587e5
Colton Myers <colton.myers@gmail.com>
2015-04-17 14:21:20 -0600

    Dict, not list

--- salt/roster/flat.py
+++ salt/roster/flat.py
@@ -78,7 +78,7 @@ class RosterMatcher(object):
         '''
         Return minions that match via list
         '''
-        minions = []
+        minions = {}
         for minion in self.raw:
             if minion in self.tgt:
                 data = self.get_data(minion)
