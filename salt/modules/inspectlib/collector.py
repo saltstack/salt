c7d8e9471f3ef2523cb76e255efbe8fedc6d17a9
Bo Maryniuk <bo@suse.de>
2015-04-17 17:36:19 +0200

    Do not purge database at snapshot moment

--- salt/modules/inspectlib/collector.py
+++ salt/modules/inspectlib/collector.py
@@ -240,7 +240,6 @@ class Inspector(object):
         '''
         Take a snapshot of the system.
         '''
-        self.db.purge()
         self._save_cfg_pkgs(self._get_changed_cfg_pkgs(self._get_cfg_pkgs()))
 
     def request_snapshot(self, mode, priority=19):
