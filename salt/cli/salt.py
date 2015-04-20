2c760f262b7f5e9261c03af37c235b4506796022
Thomas Jackson <jacksontj.89@gmail.com>
2015-04-20 08:22:06 -0700

    Add failhard option to salt batching
    This resolves #11721
    
    In addition, this consolidates the determining of retcodes into a utils library

--- salt/cli/salt.py
+++ salt/cli/salt.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import
 import os
 import sys
 
+import salt.utils.job
 from salt._compat import string_types
 from salt.utils import parsers, print_cli
 from salt.utils.verify import verify_files
@@ -92,7 +93,11 @@ class SaltCMD(parsers.SaltCMDOptionParser):
                 batch = salt.cli.batch.Batch(self.config, eauth=eauth)
                 # Printing the output is already taken care of in run() itself
                 for res in batch.run():
-                    pass
+                    if self.options.failhard:
+                        for ret in res.itervalues():
+                            retcode = salt.utils.job.get_retcode(ret)
+                            if retcode != 0:
+                                sys.exit(retcode)
 
         else:
             if self.options.timeout <= 0:
@@ -293,8 +298,9 @@ class SaltCMD(parsers.SaltCMDOptionParser):
             ret[key] = data['ret']
             if 'out' in data:
                 out = data['out']
-            if 'retcode' in data:
-                retcode = data['retcode']
+            ret_retcode = salt.utils.job.get_retcode(data)
+            if ret_retcode > retcode:
+                retcode = ret_retcode
         return ret, out, retcode
 
     def _format_error(self, minion_error):
