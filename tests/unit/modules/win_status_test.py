5d6b91f7846ccc18f1f13ba6e694e58cd1b17d65
Justin Findlay <jfindlay@gmail.com>
2015-04-20 19:07:49 -0600

    restore test in win_status_test

--- tests/unit/modules/win_status_test.py
+++ tests/unit/modules/win_status_test.py
@@ -171,16 +171,16 @@ class TestEmptyCommandLine(TestProcsBase):
         self.assertEqual(self.proc['cmd'], '')
 
 
-#class TestProcsComInitialization(TestProcsBase):
-#    def setUp(self):
-#        call_count = 5
-#        for _ in range(call_count):
-#            self.call_procs()
-#        self.expected_calls = [call()] * call_count
-#
-#    def test_initialize_and_uninitialize_called(self):
-#        pythoncom.CoInitialize.assert_has_calls(self.expected_calls)
-#        pythoncom.CoUninitialize.assert_has_calls(self.expected_calls)
+class TestProcsComInitialization(TestProcsBase):
+    def setUp(self):
+        call_count = 0
+        for _ in range(call_count):
+            self.call_procs()
+        self.expected_calls = [call()] * call_count
+
+    def test_initialize_and_uninitialize_called(self):
+        pythoncom.CoInitialize.assert_has_calls(self.expected_calls)
+        pythoncom.CoUninitialize.assert_has_calls(self.expected_calls)
 
 
 if __name__ == '__main__':
