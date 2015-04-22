f89d5d8cdb4e8d3ad5d9e5c4f540540d42e991f4
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2015-04-22 15:18:01 +0200

    raise appropriate AttributeEror for certain collectionmapping attributes

--- salt/loader.py
+++ salt/loader.py
@@ -1076,10 +1076,19 @@ class LazyLoader(salt.utils.lazy.LazyDict):
         '''
         Load a single item if you have it
         '''
+        # We must limit ourselves here to only exceptions which are technically
+        # needed and not to prevent certain modules from loading.
+        # Indeed, for now, the only case we need to filter out is the
+        # is the mapping.copy method which is normal to be absent from the
+        # loader but from which other code will test and rely on the
+        # raised AttributeError to perform their own duty
+        if key in ['copy']:
+            raise AttributeError(key)
         # if the key doesn't have a '.' then it isn't valid for this mod dict
         if not isinstance(key, six.string_types) or '.' not in key:
             raise KeyError
         mod_name, _ = key.split('.', 1)
+
         if mod_name in self.missing_modules:
             return True
         # if the modulename isn't in the whitelist, don't bother
