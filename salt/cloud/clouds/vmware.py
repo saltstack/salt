6a2a5c87b84fb1e997054926bb68cbf76c34cd12
Matt Moersen <mpmoers@clemson.edu>
2015-04-23 11:20:47 -0400

    Added function list_hosts_by_cluster()
    This function will list each cluster in the vcenter and the hosts in
    that cluster (if any). If a cluster name is passed in, only hosts in
    that cluster are listed.

--- salt/cloud/clouds/vmware.py
+++ salt/cloud/clouds/vmware.py
@@ -1987,3 +1987,53 @@ def rescan_hba(kwargs=None, call=None):
         return {host_name: 'failed to rescan HBA'}
 
     return {host_name: ret}
+
+
+def list_hosts_by_cluster(kwargs=None, call=None):
+    '''
+    To list all hosts in all clusters or all hosts in a specified cluster
+
+    CLI Example:
+
+    ..code-block:: bash
+
+        salt-cloud -f list_hosts_by_cluster my-vmware-config
+        salt-cloud -f list_hosts_by_cluster my-vmware-config cluster="clusterName"
+    '''
+    if call != 'function':
+        raise SaltCloudSystemExit(
+                'The list_hosts_by_cluster function must be called with -f or --function'
+        )
+
+    cluster_name = kwargs.get('cluster') if kwargs else None
+    
+    properties_list = ["name"]
+    cluster_list = _get_mors_with_properties(vim.ClusterComputeResource, properties_list)
+    
+    ret=[]
+ 
+    try:
+        if cluster_name:
+            if not any(cluster["name"] == cluster_name for cluster in cluster_list):
+                raise SaltCloudSystemExit(
+                    'Provided cluster name is not valid'
+                )
+
+            for cluster in cluster_list:
+                if cluster["name"] == cluster_name:
+                    ret.append(cluster["name"])
+                    cluster_ref = _get_mor_by_property(vim.ClusterComputeResource, cluster["name"])
+                    
+                    for host in cluster_ref.host:
+                        ret.append("\t" + host.name)
+        else:
+            for cluster in cluster_list:
+                ret.append(cluster["name"])
+                cluster_ref = _get_mor_by_property(vim.ClusterComputeResource, cluster["name"])
+                
+                for host in cluster_ref.host:
+                    ret.append("\t" + host.name)
+    except Exception as exc:
+        log.error(exc)
+
+    return {'Hosts': ret}
