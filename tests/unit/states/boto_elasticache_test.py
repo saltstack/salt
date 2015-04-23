06bf61822d086de7e1e5694593103d156e809c30
Jayesh Kariya <jayeshk@saltstack.com>
2015-04-23 17:56:04 +0530

    changes as per 2015.2 branch

--- tests/unit/states/boto_elasticache_test.py
+++ tests/unit/states/boto_elasticache_test.py
@@ -36,6 +36,8 @@ class BotoElasticacheTestCase(TestCase):
         Test to ensure the cache cluster exists.
         '''
         name = 'myelasticache'
+        engine = 'redis'
+        cache_node_type = 'cache.t1.micro'
 
         ret = {'name': name,
                'result': None,
@@ -49,21 +51,28 @@ class BotoElasticacheTestCase(TestCase):
                          'boto_elasticache.create': mock_bool}):
             comt = ('Failed to retrieve cache cluster info from AWS.')
             ret.update({'comment': comt})
-            self.assertDictEqual(boto_elasticache.present(name), ret)
+            self.assertDictEqual(boto_elasticache.present(name, engine,
+                                                          cache_node_type), ret)
 
             with patch.dict(boto_elasticache.__opts__, {'test': True}):
                 comt = ('Cache cluster {0} is set to be created.'.format(name))
                 ret.update({'comment': comt})
-                self.assertDictEqual(boto_elasticache.present(name), ret)
+                self.assertDictEqual(boto_elasticache.present(name, engine,
+                                                              cache_node_type),
+                                     ret)
 
             with patch.dict(boto_elasticache.__opts__, {'test': False}):
                 comt = ('Failed to create {0} cache cluster.'.format(name))
                 ret.update({'comment': comt, 'result': False})
-                self.assertDictEqual(boto_elasticache.present(name), ret)
+                self.assertDictEqual(boto_elasticache.present(name, engine,
+                                                              cache_node_type),
+                                     ret)
 
                 comt = ('Cache cluster {0} is present.'.format(name))
                 ret.update({'comment': comt, 'result': True})
-                self.assertDictEqual(boto_elasticache.present(name), ret)
+                self.assertDictEqual(boto_elasticache.present(name, engine,
+                                                              cache_node_type),
+                                     ret)
 
     # 'absent' function tests: 1
 
@@ -89,30 +98,6 @@ class BotoElasticacheTestCase(TestCase):
                 comt = ('Cache cluster {0} is set to be removed.'.format(name))
                 ret.update({'comment': comt, 'result': None})
                 self.assertDictEqual(boto_elasticache.absent(name), ret)
-
-    # 'creategroup' function tests: 1
-
-    def test_creategroup(self):
-        '''
-        Test to ensure the a replication group is create.
-        '''
-        name = 'new_table'
-        primary_cluster_id = 'A'
-        replication_group_description = 'my description'
-
-        ret = {'name': name,
-               'result': True,
-               'changes': {},
-               'comment': ''}
-
-        mock = MagicMock(return_value=True)
-        with patch.dict(boto_elasticache.__salt__,
-                        {'boto_elasticache.group_exists': mock}):
-            comt = ('{0} replication group exists .'.format(name))
-            ret.update({'comment': comt})
-            self.assertDictEqual(boto_elasticache.creategroup
-                                 (name, primary_cluster_id,
-                                  replication_group_description), ret)
 
 
 if __name__ == '__main__':
