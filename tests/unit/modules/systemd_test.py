46f703348a4ea4a8301d8c888f60258571587251
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2015-04-25 17:03:23 +0200

    fix centos tests

--- tests/unit/modules/systemd_test.py
+++ tests/unit/modules/systemd_test.py
@@ -193,6 +193,7 @@ class SystemdTestCase(TestCase):
         '''
             Test to enable the named service to start when the system boots
         '''
+        exe = MagicMock(return_value='foo')
         tmock = MagicMock(return_value=True)
         mock = MagicMock(return_value=False)
         with patch.object(systemd, '_untracked_custom_unit_found', mock):
@@ -200,14 +201,16 @@ class SystemdTestCase(TestCase):
                 with patch.dict(systemd.__salt__, {'cmd.retcode': mock}):
                     with patch.object(systemd, "_service_is_sysv", mock):
                         self.assertTrue(systemd.enable("sshd"))
-                    with patch.object(systemd, "_service_is_sysv", tmock):
-                        self.assertTrue(systemd.enable("sshd"))
+                    with patch.object(systemd, "_get_service_exec", exe):
+                        with patch.object(systemd, "_service_is_sysv", tmock):
+                            self.assertTrue(systemd.enable("sshd"))
 
     def test_disable(self):
         '''
             Test to disable the named service to not
             start when the system boots
         '''
+        exe = MagicMock(return_value='foo')
         tmock = MagicMock(return_value=True)
         mock = MagicMock(return_value=False)
         with patch.object(systemd, '_untracked_custom_unit_found', mock):
@@ -215,8 +218,9 @@ class SystemdTestCase(TestCase):
                 with patch.dict(systemd.__salt__, {'cmd.retcode': mock}):
                     with patch.object(systemd, "_service_is_sysv", mock):
                         self.assertTrue(systemd.disable("sshd"))
-                    with patch.object(systemd, "_service_is_sysv", tmock):
-                        self.assertTrue(systemd.disable("sshd"))
+                    with patch.object(systemd, "_get_service_exec", exe):
+                        with patch.object(systemd, "_service_is_sysv", tmock):
+                            self.assertTrue(systemd.disable("sshd"))
 
     def test_enabled(self):
         '''
