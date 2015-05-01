9b9c30f5ad68e9ca9d53c77af153e760e55b12fa
Colton Myers <colton.myers@gmail.com>
2015-05-01 13:08:49 -0600

    Reduce salt-ssh debug log verbosity
    Anything that involves the SHIM should be logged to TRACE

--- salt/client/ssh/shell.py
+++ salt/client/ssh/shell.py
@@ -285,7 +285,11 @@ class Shell(object):
         logmsg = 'Executing command: {0}'.format(cmd)
         if self.passwd:
             logmsg = logmsg.replace(self.passwd, ('*' * 6))
-        log.debug(logmsg)
+        if 'decode("base64")' in logmsg:
+            log.debug('Executed SHIM command. Command logged to TRACE')
+            log.trace(logmsg)
+        else:
+            log.debug(logmsg)
 
         ret = self._run_cmd(cmd)
         return ret
