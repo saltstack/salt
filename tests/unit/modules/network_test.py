d814b7f9a31f784324ef3f8f41b147bfd6fae690
Ronald van Zantvoort <ronald@pcextreme.nl>
2015-05-01 20:29:25 +0200

    Better unit test fix, add 2 unit tests for IPv6

--- tests/unit/modules/network_test.py
+++ tests/unit/modules/network_test.py
@@ -275,14 +275,20 @@ class NetworkTestCase(TestCase):
         with patch.object(salt.ext.ipaddress.IPv4Address, 'is_private',
                           return_value=True):
             self.assertTrue(network.is_private('0.0.0.0'))
+        with patch.object(salt.ext.ipaddress.IPv6Address, 'is_private',
+                          return_value=True):
+            self.assertTrue(network.is_private('::1'))
 
     def test_is_loopback(self):
         '''
         Test for Check if the given IP address is a loopback address
         '''
         with patch.object(salt.ext.ipaddress.IPv4Address, 'is_loopback',
-                          return_value=False):
-            self.assertFalse(network.is_private('0.0.0.0'))
+                          return_value=True):
+            self.assertTrue(network.is_loopback('127.0.0.1'))
+        with patch.object(salt.ext.ipaddress.IPv6Address, 'is_loopback',
+                          return_value=True):
+            self.assertTrue(network.is_loopback('::1'))
 
     def test_get_bufsize(self):
         '''
