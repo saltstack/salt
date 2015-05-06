8c20e6b59b6187f768a9f6a9b1e8beef9dd3aaf2
Colton Myers <colton.myers@gmail.com>
2015-05-06 10:38:18 -0600

    Revert "lint"
    This reverts commit 59d1459942a97c5b362ac1ea8028a15962502ff0.

--- salt/modules/win_useradd.py
+++ salt/modules/win_useradd.py
@@ -981,7 +981,7 @@ def _getnetbiosusernamefromsid(adspath):
             user_info = win32security.LookupAccountSid(
                     None, pywintypes.SID(nt.GetObject('', adspath).objectSID))
             return ('{0}\\{1}').format(user_info[1], user_info[0])
-        except Exception:
+        except:
             return adspath.replace('WinNT://', '').replace('/', '\\')
 
 
@@ -999,7 +999,7 @@ def _fixlocaluser(username):
                 user_info = win32security.LookupAccountSid(
                     None, pywintypes.SID(nt.GetObject('', 'WinNT://./' + username).objectSID))
                 username = (('{0}\\{1}').format(user_info[1], user_info[0]))
-            except Exception:
+            except:
                 username = ('{0}\\{1}').format(__salt__['grains.get']('host'), username)
 
     return username
