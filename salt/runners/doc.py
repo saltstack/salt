906ac66842729aa59d556e9c1316a3ba5689c088
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2015-05-14 15:05:51 +0200

    merge with develop

--- salt/runners/doc.py
+++ salt/runners/doc.py
@@ -79,30 +79,3 @@ def execution():
     ret = dict(list(i))
 
     return ret
-
-
-# Still need to modify some of the backend for auth checks to make this work
-def __list_functions(user=None):
-    '''
-    List all of the functions, optionally pass in a user to evaluate
-    permissions on
-    '''
-    client = salt.client.get_local_client(__opts__['conf_file'])
-    funcs = {}
-    try:
-        gener = client.cmd_iter(
-                '*',
-                'sys.list_functions',
-                timeout=__opts__['timeout'])
-    except SaltClientError as client_error:
-        print(client_error)
-        return funcs
-
-    for ret in gener:
-        funcs.update(ret)
-    if not user:
-        __jid_event__.fire_event({'message': funcs}, 'progress')
-        return funcs
-    for _, val in __opts__['external_auth'].items():
-        if user in val:
-            pass
