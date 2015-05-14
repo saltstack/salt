d622a7c9f80493738883c7ab27683377ce22bbee
Stephen Spencer <stephen@revsys.com>
2015-05-14 18:55:37 -0500

    Re-add the original __virtual__ check for ca.cert_base_path

--- salt/modules/tls.py
+++ salt/modules/tls.py
@@ -45,9 +45,14 @@ def __virtual__():
     Only load this module if the ca config options are set
     '''
     if HAS_SSL and float(OpenSSL_version) < 0.14:
-        return True
-    return False, ['PyOpenSSL version 0.14 or later  must be installed before '
-                  ' this module can be used.']
+        if __opts__.get('ca.cert_base_path', None):
+            return True
+        else:
+            log.error('tls module not loaded: ca.cert_base_path not set')
+            return False
+    else:
+        return False, ['PyOpenSSL version 0.14 or later  must be installed before '
+                       ' this module can be used.']
 
 
 def cert_base_path(cacert_path=None):
