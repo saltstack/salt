667ba57128f16d9416964e0d24fc67187ae0a7a6
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2015-05-15 18:10:36 +0200

    lint
    Signed-off-by: Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>

--- salt/states/service.py
+++ salt/states/service.py
@@ -276,7 +276,6 @@ def running(name, enable=None, sig=None, init_delay=None, **kwargs):
     if not _available(name, ret):
         return ret
 
-
     # lot of custom init script wont or mis implement the status
     # command, so it is just an indicator but can not be fully trusted
     before_toggle_status = __salt__['service.status'](name)
@@ -521,4 +520,3 @@ def mod_watch(name,
     ret['comment'] = 'Service {0}'.format(past_participle) if result else \
                      'Failed to {0} the service'.format(verb)
     return ret
-
