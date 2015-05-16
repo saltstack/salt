aea1562054a51ebaa3de2f525b4f5287f2c87510
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2015-05-16 17:25:08 +0200

    lxc/reconfigure: default nic

--- salt/modules/lxc.py
+++ salt/modules/lxc.py
@@ -3775,7 +3775,7 @@ def reconfigure(name,
             return profile_match
         return kw_overrides_match
     if nic_opts is not None and not network_profile:
-        network_profile = 'eth0'
+        network_profile = DEFAULT_NIC
 
     if autostart is not None:
         autostart = select('autostart', autostart)
