a2974e162c7510fdc8a2b97949a178bad7bcbb3e
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2015-05-17 13:02:20 +0200

    fix tls virtual
    This fixes #23826
    
    Signed-off-by: Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>

--- salt/modules/tls.py
+++ salt/modules/tls.py
@@ -48,11 +48,16 @@ def __virtual__():
     Only load this module if the ca config options are set
     '''
     if HAS_SSL and LooseVersion(OpenSSL_version) < LooseVersion('0.14'):
-        if __opts__.get('ca.cert_base_path', None):
-            return True
-        else:
-            log.error('tls module not loaded: ca.cert_base_path not set')
-            return False
+        # never EVER reactivate this code, this has been done too many times.
+        # not having configured a cert path in the configuration does not
+        # mean that users cant use this module as we provide methods
+        # to configure it afterwards.
+        # if __opts__.get('ca.cert_base_path', None):
+        #     return True
+        # else:
+        #     log.error('tls module not loaded: ca.cert_base_path not set')
+        #     return False
+        return True
     else:
         return False, ['PyOpenSSL version 0.14 or later  must be installed before '
                        ' this module can be used.']
