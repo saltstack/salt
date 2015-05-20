927247e3179b8fd95b2c5c77b8b20fdd8d2093e6
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2015-05-20 19:08:58 +0200

    lxc: more path forwards
    Signed-off-by: Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>

--- salt/modules/lxc.py
+++ salt/modules/lxc.py
@@ -1490,6 +1490,7 @@ def init(name,
         if not any(retcode(name,
                            'test -e "{0}"'.format(x),
                            chroot_fallback=True,
+                           path=path,
                            ignore_retcode=True) == 0
                    for x in gids):
             try:
@@ -1506,6 +1507,7 @@ def init(name,
                            ('sh -c \'touch "{0}"; test -e "{0}"\''
                             .format(gid)),
                            chroot_fallback=True,
+                           path=path,
                            ignore_retcode=True) != 0:
                     ret['comment'] = 'Failed to set DNS marker'
                     changes[-1]['dns'] += '. ' + ret['comment'] + '.'
