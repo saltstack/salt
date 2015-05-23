787c9d3061b3f30273fd30391d15938ac45ecbdf
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2015-05-24 00:51:22 +0200

    lxc/applynet: conservative
    Signed-off-by: Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>

--- salt/modules/lxc.py
+++ salt/modules/lxc.py
@@ -926,6 +926,7 @@ def _get_veths(net_data):
         net_data = list(net_data.items())
     nics = salt.utils.odict.OrderedDict()
     current_nic = salt.utils.odict.OrderedDict()
+    no_names = True
     for item in net_data:
         if item and isinstance(item, dict):
             item = list(item.items())[0]
@@ -940,8 +941,13 @@ def _get_veths(net_data):
         if item[0] == 'lxc.network.type':
             current_nic = salt.utils.odict.OrderedDict()
         if item[0] == 'lxc.network.name':
+            no_names = False
             nics[item[1].strip()] = current_nic
         current_nic[item[0].strip()] = item[1].strip()
+    # if not ethernet card name has been collected, assuming we collected
+    # data for eth0
+    if no_names and current_nic:
+        nics[DEFAULT_NIC] = current_nic
     return nics
 
 
