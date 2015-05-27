bec21b7379e21c2d4a80dcdb814e733b3a828459
Matthias Erll <matthias@precisdigital.com>
2015-05-27 11:12:15 +0200

    Use ids if user or group is not present.

--- salt/modules/file.py
+++ salt/modules/file.py
@@ -145,7 +145,8 @@ def gid_to_group(gid):
     try:
         return grp.getgrgid(gid).gr_name
     except (KeyError, NameError):
-        return ''
+        # If group is not present, fall back to the gid.
+        return gid
 
 
 def group_to_gid(group):
@@ -211,7 +212,8 @@ def uid_to_user(uid):
     try:
         return pwd.getpwuid(uid).pw_name
     except (KeyError, NameError):
-        return ''
+        # If user is not present, fall back to the uid.
+        return uid
 
 
 def user_to_uid(user):
