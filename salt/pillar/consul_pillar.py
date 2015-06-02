2ce52ee95fa5afed4921052f440f707d611f9fff
Brett Mack <brett.mack@flubit.com>
2015-06-02 22:29:08 +0100

    Style changes to bring code in line with coding standards

--- salt/pillar/consul_pillar.py
+++ salt/pillar/consul_pillar.py
@@ -61,10 +61,10 @@ from salt.exceptions import CommandExecutionError
 
 # Import third party libs
 try:
-  import consul
-  HAS_CONSUL = True
+    import consul
+    HAS_CONSUL = True
 except ImportError:
-  HAS_CONSUL = False
+    HAS_CONSUL = False
 
 __virtualname__ = 'consul'
 
@@ -73,126 +73,132 @@ log = logging.getLogger(__name__)
 
 
 def __virtual__():
-  '''
-  Only return if python-consul is installed
-  '''
-  return __virtualname__ if HAS_CONSUL else False
+    '''
+    Only return if python-consul is installed
+    '''
+    return __virtualname__ if HAS_CONSUL else False
 
 
 def ext_pillar(minion_id,
                pillar,  # pylint: disable=W0613
                conf):
-  '''
-  Check consul for all data
-  '''
-  comps = conf.split()
+    '''
+    Check consul for all data
+    '''
+    comps = conf.split()
 
-  profile = None
-  if comps[0]:
-    profile = comps[0]
-  client = get_conn(__opts__, profile)
+    profile = None
+    if comps[0]:
+        profile = comps[0]
+    client = get_conn(__opts__, profile)
 
-  path = ''
-  if len(comps) > 1 and comps[1].startswith('root='):
-    path = comps[1].replace('root=', '')
+    path = ''
+    if len(comps) > 1 and comps[1].startswith('root='):
+        path = comps[1].replace('root=', '')
 
-  # put the minion's ID in the path if necessary
-  path %= {
-    'minion_id': minion_id
-  }
+    # put the minion's ID in the path if necessary
+    path %= {
+        'minion_id': minion_id
+    }
 
-  try:
-    pillar = fetch_tree(client, path)
-  except KeyError:
-    log.error('No such key in consul profile {0}: {1}'.format(profile, path))
-    pillar = {}
+    try:
+        pillar = fetch_tree(client, path)
+    except KeyError:
+        log.error('No such key in consul profile {0}: {1}'
+                  .format(profile, path))
+        pillar = {}
 
-  return pillar
+    return pillar
+
 
 def consul_fetch(client, path):
-  '''
-  Query consul for all keys/values within base path
-  '''
-  return client.kv.get(path, recurse=True)
+    '''
+    Query consul for all keys/values within base path
+    '''
+    return client.kv.get(path, recurse=True)
+
 
 def fetch_tree(client, path):
-  '''
-  Grab data from consul, trim base path and remove any keys which
-  are folders. Take the remaining data and send it to be formatted
-  in such a way as to be used as pillar data.
-  '''
-  index, items = consul_fetch(client, path)
-  ret = {}
-  has_children = re.compile(r'/$')
+    '''
+    Grab data from consul, trim base path and remove any keys which
+    are folders. Take the remaining data and send it to be formatted
+    in such a way as to be used as pillar data.
+    '''
+    index, items = consul_fetch(client, path)
+    ret = {}
+    has_children = re.compile(r'/$')
 
-  log.debug('Fetched items: %r', format(items))
+    log.debug('Fetched items: %r', format(items))
 
-  for item in reversed(items):
-    key = re.sub(r'^' + path + '/?', '', item['Key'])
-    if key != "":
-      log.debug('key/path - {0}: {1}'.format(path, key))
-      log.debug('has_children? %r', format(has_children.search(key)))
-      if has_children.search(key) == None:
-        ret = pillar_format(ret, key.split('/'), item['Value'])
-        log.debug('Fetching subkeys for key: %r', format(item))
+    for item in reversed(items):
+        key = re.sub(r'^' + path + '/?', '', item['Key'])
+        if key != "":
+            log.debug('key/path - {0}: {1}'.format(path, key))
+            log.debug('has_children? %r', format(has_children.search(key)))
+        if has_children.search(key) is None:
+            ret = pillar_format(ret, key.split('/'), item['Value'])
+            log.debug('Fetching subkeys for key: %r', format(item))
 
-  return ret
+    return ret
+
 
 def pillar_format(ret, keys, value):
-  '''
-  Perform data formatting to be used as pillar data and
-  merge it with the current pillar data
-  '''
-  if value == None:
-    return ret
-  array_data = value.split('\n')
-  pillar_value = array_data[0] if len(array_data) == 1 else array_data
-  keyvalue = keys.pop()
-  pil = {keyvalue: pillar_value}
-  keys.reverse()
-  for k in keys:
-    pil = {k: pil}
+    '''
+    Perform data formatting to be used as pillar data and
+    merge it with the current pillar data
+    '''
+    if value is None:
+        return ret
+    array_data = value.split('\n')
+    pillar_value = array_data[0] if len(array_data) == 1 else array_data
+    keyvalue = keys.pop()
+    pil = {keyvalue: pillar_value}
+    keys.reverse()
+    for k in keys:
+        pil = {k: pil}
 
-  return dict_merge(ret, pil)
+    return dict_merge(ret, pil)
+
 
 def dict_merge(d1, d2):
-  '''
-  Take 2 dictionaries and deep merge them
-  '''
-  master = d1.copy()
-  for (k, v) in d2.iteritems():
-    if k in master and isinstance(master[k], dict):
-      master[k] = dict_merge(master[k], v)
-    else:
-      master[k] = v
-  return master
+    '''
+    Take 2 dictionaries and deep merge them
+    '''
+    master = d1.copy()
+    for (k, v) in d2.iteritems():
+        if k in master and isinstance(master[k], dict):
+            master[k] = dict_merge(master[k], v)
+        else:
+            master[k] = v
+    return master
+
 
 def get_conn(opts, profile):
 
-  '''
-  Return a client object for accessing consul
-  '''
-  opts_pillar = opts.get('pillar', {})
-  opts_master = opts_pillar.get('master', {})
+    '''
+    Return a client object for accessing consul
+    '''
+    opts_pillar = opts.get('pillar', {})
+    opts_master = opts_pillar.get('master', {})
 
-  opts_merged = {}
-  opts_merged.update(opts_master)
-  opts_merged.update(opts_pillar)
-  opts_merged.update(opts)
+    opts_merged = {}
+    opts_merged.update(opts_master)
+    opts_merged.update(opts_pillar)
+    opts_merged.update(opts)
 
-  if profile:
-    conf = opts_merged.get(profile, {})
-  else:
-    conf = opts_merged
+    if profile:
+        conf = opts_merged.get(profile, {})
+    else:
+        conf = opts_merged
 
-  consul_host = conf.get('consul.host', '127.0.0.1')
-  consul_port = conf.get('consul.port', 8500)
+    consul_host = conf.get('consul.host', '127.0.0.1')
+    consul_port = conf.get('consul.port', 8500)
 
-  if HAS_CONSUL:
-    return consul.Consul(host=consul_host, port=consul_port)
-  else:
-    raise CommandExecutionError(
+    if HAS_CONSUL:
+        return consul.Consul(host=consul_host, port=consul_port)
+    else:
+        raise CommandExecutionError(
             '(unable to import consul, '
             'module most likely not installed. Download python-consul '
             'module and be sure to import consul)'
-    )
+        )
