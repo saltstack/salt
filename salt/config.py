bfe70933f32d82f8941237b567c6076e20c6bd8b
Dmitry Kuzmenko <dmitry.kuzmenko@dsr-company.com>
2015-06-03 14:59:36 +0300

    Support IdentitiesOnly ssh option in salt-ssh

--- salt/config.py
+++ salt/config.py
@@ -999,6 +999,7 @@ DEFAULT_MASTER_OPTS = {
     'ssh_user': 'root',
     'ssh_scan_ports': '22',
     'ssh_scan_timeout': 0.01,
+    'ssh_identities_only': False,
     'master_floscript': os.path.join(FLO_DIR, 'master.flo'),
     'worker_floscript': os.path.join(FLO_DIR, 'worker.flo'),
     'maintenance_floscript': os.path.join(FLO_DIR, 'maint.flo'),
