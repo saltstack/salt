3a155e62488302587a899b774b311af64cd00df3
Andrei Nistor <coder.tux@gmail.com>
2015-06-05 15:33:46 +0300

    Update modules path for operating systems using systemd

--- salt/modules/kmod.py
+++ salt/modules/kmod.py
@@ -55,7 +55,7 @@ def _get_modules_conf():
     Return location of modules config file.
     Default: /etc/modules
     '''
-    if __grains__['os'] == 'Arch':
+    if  'systemd' in __grains__:
         return '/etc/modules-load.d/salt_managed.conf'
     return '/etc/modules'
 
