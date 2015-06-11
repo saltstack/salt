3283a5d21b87216397882735049dbe8476a4e406
rallytime <nicole@saltstack.com>
2015-06-11 09:44:51 -0600

    Fix doc example test that is failing on develop

--- salt/modules/slack_notify.py
+++ salt/modules/slack_notify.py
@@ -23,12 +23,13 @@ import logging
 # Import 3rd-party libs
 # pylint: disable=import-error,no-name-in-module,redefined-builtin
 from salt.ext.six.moves.urllib.parse import urljoin as _urljoin
-from salt.ext.six.moves.urllib.parse import urlencode
+from salt.ext.six.moves.urllib.parse import urlencode as _urlencode
 from salt.ext.six.moves import range
 import salt.ext.six.moves.http_client
 # pylint: enable=import-error,no-name-in-module
 
 log = logging.getLogger(__name__)
+
 __virtualname__ = 'slack'
 
 
@@ -276,7 +277,7 @@ def post_message(channel,
                     api_key=api_key,
                     method='POST',
                     header_dict={'Content-Type': 'application/x-www-form-urlencoded'},
-                    data=urlencode(parameters))
+                    data=_urlencode(parameters))
 
     if result['res']:
         return True
