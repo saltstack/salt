4cbf86a483be65d384ebdad5321412b2da638b73
Mike Place <mp@saltstack.com>
2015-06-17 14:35:31 -0600

    Fix extreme edge case where system encoding can not be detected

--- salt/__init__.py
+++ salt/__init__.py
@@ -47,8 +47,9 @@ def __define_global_system_encoding_variable__():
         del locale
         if not encoding:
             # This is most likely asccii which is not the best but we were
-            # unable to find a better encoding
-            encoding = sys.getdefaultencoding()
+            # unable to find a better encoding. If this fails, we fall all
+            # the way back to ascii
+            encoding = sys.getdefaultencoding() or 'ascii'
 
     # We can't use six.moves.builtins because these builtins get deleted sooner
     # than expected. See:
