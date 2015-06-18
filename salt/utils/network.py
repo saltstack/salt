036e654237b620aab35ac5ec56b26ed7bae5ba6b
Ronald van Zantvoort <The-Loeki@users.noreply.github.com>
2015-06-18 15:06:36 +0200

    leading zero fix for mac2eui64
    and update to improved mac input handling

--- salt/utils/network.py
+++ salt/utils/network.py
@@ -994,14 +994,14 @@ def mac2eui64(mac, prefix=None):
     # http://tools.ietf.org/html/rfc4291#section-2.5.1
     eui64 = re.sub(r'[.:-]', '', mac).lower()
     eui64 = eui64[0:6] + 'fffe' + eui64[6:]
-    eui64 = hex(int(eui64[0:2], 16) | 2)[2:] + eui64[2:]
+    eui64 = hex(int(eui64[0:2], 16) | 2)[2:].zfill(2) + eui64[2:]
 
     if prefix is None:
-        return '{0}{1}:{2}{3}:{4}{5}:{6}{7}'.format(*eui64)
+        return ':'.join(re.findall(r'.{4}', eui64))
     else:
         try:
             net = ipaddress.ip_network(prefix, strict=False)
-            euil = int('0x{0}'.format(''.join(eui64)), 16)
+            euil = int('0x{0}'.format(eui64), 16)
             return '{0}/{1}'.format(net[euil], net.prefixlen)
         except:  # pylint: disable=bare-except
             return
