81f37df4a3857911a189852021e3f426218e2c7a
Ronald van Zantvoort <ronald@pcextreme.nl>
2015-06-23 13:55:02 +0200

    Merging blockdev into disk as per #24745

--- salt/modules/disk.py
+++ salt/modules/disk.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 '''
-Module for gathering disk information
+Module for managing disks and blockdevices
 '''
 from __future__ import absolute_import
 
 # Import python libs
 import logging
 import os
+import subprocess
 import re
 
 # Import salt libs
@@ -241,3 +242,112 @@ def blkid(device=None):
         ret[device] = info
 
     return ret
+
+
+def tune(device, **kwargs):
+    '''
+    Set attributes for the specified device
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' disk.tune /dev/sda1 read-ahead=1024 read-write=True
+
+    Valid options are: ``read-ahead``, ``filesystem-read-ahead``,
+    ``read-only``, ``read-write``.
+
+    See the ``blockdev(8)`` manpage for a more complete description of these
+    options.
+    '''
+
+    kwarg_map = {'read-ahead': 'setra',
+                 'filesystem-read-ahead': 'setfra',
+                 'read-only': 'setro',
+                 'read-write': 'setrw'}
+    opts = ''
+    args = []
+    for key in kwargs:
+        if key in kwarg_map:
+            switch = kwarg_map[key]
+            if key != 'read-write':
+                args.append(switch.replace('set', 'get'))
+            else:
+                args.append('getro')
+            if kwargs[key] == 'True' or kwargs[key] is True:
+                opts += '--{0} '.format(key)
+            else:
+                opts += '--{0} {1} '.format(switch, kwargs[key])
+    cmd = 'blockdev {0}{1}'.format(opts, device)
+    out = __salt__['cmd.run'](cmd, python_shell=False).splitlines()
+    return dump(device, args)
+
+
+def wipe(device):
+    '''
+    Remove the filesystem information
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' disk.wipe /dev/sda1
+    '''
+
+    cmd = 'wipefs {0}'.format(device)
+    try:
+        out = __salt__['cmd.run_all'](cmd, python_shell=False)
+    except subprocess.CalledProcessError as err:
+        return False
+    if out['retcode'] == 0:
+        return True
+
+
+def dump(device, args=None):
+    '''
+    Return all contents of dumpe2fs for a specified device
+
+    CLI Example:
+    .. code-block:: bash
+
+        salt '*' disk.dump /dev/sda1
+    '''
+    cmd = 'blockdev --getro --getsz --getss --getpbsz --getiomin --getioopt --getalignoff ' \
+          '--getmaxsect --getsize --getsize64 --getra --getfra {0}'.format(device)
+    ret = {}
+    opts = [c[2:] for c in cmd.split() if c.startswith('--')]
+    out = __salt__['cmd.run_all'](cmd, python_shell=False)
+    if out['retcode'] == 0:
+        lines = [line for line in out['stdout'].splitlines() if line]
+        count = 0
+        for line in lines:
+            ret[opts[count]] = line
+            count = count+1
+        if args:
+            temp_ret = {}
+            for arg in args:
+                temp_ret[arg] = ret[arg]
+            return temp_ret
+        else:
+            return ret
+    else:
+        return False
+
+
+def resize2fs(device):
+    '''
+    Resizes the filesystem.
+
+    CLI Example:
+    .. code-block:: bash
+
+        salt '*' disk.resize2fs /dev/sda1
+    '''
+    ret = {}
+    cmd = 'resize2fs {0}'.format(device)
+    try:
+        out = __salt__['cmd.run_all'](cmd, python_shell=False)
+    except subprocess.CalledProcessError as err:
+        return False
+    if out['retcode'] == 0:
+        return True
