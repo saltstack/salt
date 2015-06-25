a6c23aa82da4d29dae40e5a60ecacd26442bc174
David Boucha <boucha@gmail.com>
2015-06-25 15:05:14 -0600

    run genrepo when calling refresh_db

--- salt/modules/win_pkg.py
+++ salt/modules/win_pkg.py
@@ -372,6 +372,7 @@ def refresh_db(saltenv='base'):
     __context__.pop('winrepo.data', None)
     repo = __opts__['win_repo_source_dir']
     cached_files = __salt__['cp.cache_dir'](repo, saltenv, include_pat='*.sls')
+    genrepo(saltenv=saltenv)
     return cached_files
 
 
