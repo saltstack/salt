4b9d7426cca7c525fcdadb67389398a218364212
Peter Tripp <peter@chartio.com>
2015-06-26 16:21:45 -0700

    Add file as supported protocol for file source_hash. Fixes #23764.
    Conflicts:
    	salt/modules/file.py

--- salt/modules/file.py
+++ salt/modules/file.py
@@ -2933,7 +2933,7 @@ def get_managed(
         elif source.startswith('/'):
             source_sum = get_hash(source)
         elif source_hash:
-            protos = ('salt', 'http', 'https', 'ftp', 'swift', 's3')
+            protos = ('salt', 'http', 'https', 'ftp', 'swift', 's3', 'file')
             if _urlparse(source_hash).scheme in protos:
                 # The source_hash is a file on a server
                 hash_fn = __salt__['cp.cache_file'](source_hash, saltenv)
