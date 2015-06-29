b86ff4dc901f649271c6545b2884de735413a211
Pedro Algarvio <pedro@algarvio.me>
2015-06-29 15:49:01 +0100

    Move SPM's parser to `salt.utils.parsers` and add `-y` to it

--- salt/cli/spm.py
+++ salt/cli/spm.py
@@ -9,43 +9,22 @@
 '''
 
 # Import Python libs
-from __future__ import absolute_import, print_function
-import os.path
-import logging
+from __future__ import absolute_import
 
 # Import Salt libs
+import salt.client.spm
 import salt.utils.parsers as parsers
-import salt.version
-import salt.syspaths as syspaths
-
-# Import 3rd-party libs
-import salt.ext.six as six
-
-log = logging.getLogger(__name__)
 
 
-class SPM(six.with_metaclass(parsers.OptionParserMeta,  # pylint: disable=W0232
-        parsers.OptionParser, parsers.ConfigDirMixIn,
-        parsers.LogLevelMixIn, parsers.MergeConfigMixIn)):
+class SPM(parsers.SPMParser):
     '''
     The cli parser object used to fire up the salt spm system.
     '''
-
-    VERSION = salt.version.__version__
-
-    # ConfigDirMixIn config filename attribute
-    _config_filename_ = 'spm'
-    # LogLevelMixIn attributes
-    _default_logging_logfile_ = os.path.join(syspaths.LOGS_DIR, 'spm')
-
-    def setup_config(self):
-        return salt.config.spm_config(self.get_config_file_path())
 
     def run(self):
         '''
         Run the api
         '''
-        import salt.client.spm
         self.parse_args()
         self.setup_logfile_logger()
         client = salt.client.spm.SPMClient(self.config)
