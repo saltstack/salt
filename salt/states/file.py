a5978d30c2df1f94eddf038b01e7731e4a86dc6b
Justin Findlay <jfindlay@gmail.com>
2015-06-30 14:22:52 -0600

    iterate an iterable source othwerise list+str it
    Fixes #25041.

--- salt/states/file.py
+++ salt/states/file.py
@@ -729,7 +729,7 @@ def _validate_str_list(arg):
     elif isinstance(arg, Iterable) and not isinstance(arg, Mapping):
         return [str(item) for item in arg]
     else:
-        return False
+        return [str(arg)]
 
 
 def symlink(
@@ -2098,8 +2098,6 @@ def recurse(name,
 
     # expand source into source_list
     source_list = _validate_str_list(source)
-    if not source_list:
-        return _error(ret, '\'source\' parameter is not a string or list of strings')
 
     for idx, val in enumerate(source_list):
         source_list[idx] = val.rstrip('/')
@@ -3031,8 +3029,6 @@ def append(name,
         text = tmpret['data']
 
     text = _validate_str_list(text)
-    if not text:
-        return _error(ret, 'Given text is not a string or a list of strings')
 
     with salt.utils.fopen(name, 'rb') as fp_:
         slines = fp_.readlines()
@@ -3198,8 +3194,6 @@ def prepend(name,
         text = tmpret['data']
 
     text = _validate_str_list(text)
-    if not text:
-        return _error(ret, 'Given text is not a string or a list of strings')
 
     with salt.utils.fopen(name, 'rb') as fp_:
         slines = fp_.readlines()
