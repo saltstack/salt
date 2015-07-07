f0dbbd5d6c80e462c61ad91a9f2ba806dac3bb5d
gene <gene@uitox.com>
2015-07-07 12:07:41 +0800

    Don't need to update when replace is True and record is exist in dns server

--- salt/modules/ddns.py
+++ salt/modules/ddns.py
@@ -163,10 +163,18 @@ def update(zone, name, ttl, rdtype, data, nameserver='127.0.0.1', replace=False,
     keyname = _config('keyname', **kwargs)
     keyalgorithm = _config('keyalgorithm', **kwargs) or 'HMAC-MD5.SIG-ALG.REG.INT'
 
+    is_exist = False
+    for rrset in answer.answer:
+        if rdata in rrset.items:
+            if ttl == rrset.ttl:
+                if (len(answer.answer) >= 1 or len(rrset.items) >= 1):
+                    is_exist = True
+                    break
+
     dns_update = dns.update.Update(zone, keyring=keyring, keyname=keyname, keyalgorithm=keyalgorithm)
     if replace:
         dns_update.replace(name, ttl, rdata)
-    else:
+    elif not is_exist:
         dns_update.add(name, ttl, rdata)
     answer = dns.query.udp(dns_update, nameserver)
     if answer.rcode() > 0:
