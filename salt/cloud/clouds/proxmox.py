b799a804809e9360eae74c17360b4187c9d36778
Joseph Hall <perlhoser@gmail.com>
2015-07-20 09:42:55 -0600

    Linting

--- salt/cloud/clouds/proxmox.py
+++ salt/cloud/clouds/proxmox.py
@@ -28,7 +28,6 @@ Set up the cloud configuration at ``/etc/salt/cloud.providers`` or
 
 # Import python libs
 from __future__ import absolute_import
-import copy
 import time
 import pprint
 import logging
