f7638abdf7414a6999ec6dbae1cf4ff5deb0ee42
twangboy <slee@saltstack.com>
2015-07-22 17:45:50 -0600

    Clarified functionality of reg module, fixed state to work with new module

--- salt/modules/reg.py
+++ salt/modules/reg.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
 '''
-Manage the registry on Windows
+Manage the registry on Windows.
+
+The read_key and set_key functions will be updated in Boron to reflect proper
+registry usage. The registry has three main components. Hives, Keys, and Values.
+
+### Hives
+Hives are the main sections of the registry and all begin with the word HKEY.
+- HKEY_LOCAL_MACHINE
+- HKEY_CURRENT_USER
+- HKEY_USER
+
+### Keys
+Keys are the folders in the registry. Keys can have many nested subkeys. Keys
+can have a value assigned to them under the (Default)
+
+### Values
+Values are name/data pairs. There can be many values in a key. The (Default)
+value corresponds to the Key, the rest are their own value pairs.
 
 :depends:   - winreg Python module
 '''
@@ -48,6 +65,22 @@ class Registry(object):
             False: _winreg.KEY_ALL_ACCESS | _winreg.KEY_WOW64_64KEY,
             }
 
+        self.vtype = {
+            "REG_BINARY": _winreg.REG_BINARY,
+            "REG_DWORD": _winreg.REG_DWORD,
+            "REG_EXPAND_SZ": _winreg.REG_EXPAND_SZ,
+            "REG_MULTI_SZ": _winreg.REG_MULTI_SZ,
+            "REG_SZ": _winreg.REG_SZ
+        }
+
+        self.vtype_reverse = {
+            _winreg.REG_BINARY: "REG_BINARY",
+            _winreg.REG_DWORD: "REG_DWORD",
+            _winreg.REG_EXPAND_SZ: "REG_EXPAND_SZ",
+            _winreg.REG_MULTI_SZ: "REG_MULTI_SZ",
+            _winreg.REG_SZ: "REG_SZ"
+        }
+
     def __getattr__(self, k):
         try:
             return self.hkeys[k]
@@ -66,9 +99,28 @@ def __virtual__():
     return False
 
 
-def read_key(hkey, path, key, reflection=True):
+def read_key(hkey, path, key=None):
     '''
+    *** Incorrect Usage ***
+    The name of this function is misleading and will be changed to reflect
+    proper usage in the Boron release of Salt. The path option will be removed
+    and the key will be the actual key. See the following issue:
+
+    https://github.com/saltstack/salt/issues/25618
+
+    In order to not break existing state files this function will call the
+    read_value function if a key is passed. Key will be passed as the value
+    name. If key is not passed, this function will return the default value for
+    the key.
+
+    In the Boron release this function will be removed in favor of read_value.
+    ***
+
     Read registry key value
+
+    Returns the first unnamed value (Default) as a string.
+    Returns none if first unnamed value is empty.
+    Returns False if key not found.
 
     CLI Example:
 
@@ -77,19 +129,96 @@ def read_key(hkey, path, key, reflection=True):
         salt '*' reg.read_key HKEY_LOCAL_MACHINE 'SOFTWARE\\Salt' 'version'
     '''
 
+    ret = {'hive': hkey,
+           'key': path,
+           'vdata': None,
+           'success': True}
+
+    if key:  # This if statement will be removed in Boron
+        salt.utils.warn_until('Boron', 'Use reg.read_value to read a registry'
+                                       'value. This functionality will be'
+                                       'removed in Salt Boron')
+        return read_value(hive=hkey,
+                          key=path,
+                          vname=key)
+
     registry = Registry()
-    hkey2 = getattr(registry, hkey)
-    access_mask = registry.reflection_mask[reflection]
+    hive = registry.hkeys[hkey]
+
     try:
-        handle = _winreg.OpenKeyEx(hkey2, path, 0, access_mask)
-        return _winreg.QueryValueEx(handle, key)[0]
-    except Exception:
-        return None
+        value = _winreg.QueryValue(hive, path)
+        if value:
+            ret['vdata'] = value
+        else:
+            ret['vdata'] = None
+            ret['comment'] = 'Empty Value'
+    except WindowsError as exc:
+        log.debug(exc)
+        ret['comment'] = '{0}'.format(exc)
+        ret['success'] = False
+
+    return ret
 
 
-def set_key(hkey, path, key, value, vtype='REG_DWORD', reflection=True):
+def read_value(hive, key, vname=None):
     '''
+
+    :param hive:
+    :param key:
+    :param vname:
+    :return:
+    '''
+
+    # Setup the return array
+    ret = {'hive': hive,
+           'key': key,
+           'vname': vname,
+           'vdata': None,
+           'success': True}
+
+    # If no name is passed, the default value of the key will be returned
+    # The value name is Default
+    if not vname:
+        ret['vname'] = '(Default)'
+
+    registry = Registry()
+    hive = registry.hkeys[hive]
+
+    try:
+        handle = _winreg.OpenKey(hive, key)
+        value, vtype = _winreg.QueryValueEx(handle, vname)
+        if value:
+            ret['vdata'] = value
+            ret['vtype'] = registry.vtype_reverse[vtype]
+        else:
+            ret['comment'] = 'Empty Value'
+    except WindowsError as exc:
+        log.debug(exc)
+        ret['comment'] = '{0}'.format(exc)
+        ret['success'] = False
+
+    return ret
+
+
+def set_key(hkey, path, value, key=None, vtype='REG_DWORD', reflection=True):
+    '''
+    *** Incorrect Usage ***
+    The name of this function is misleading and will be changed to reflect
+    proper usage in the Boron release of Salt. The path option will be removed
+    and the key will be the actual key. See the following issue:
+
+    https://github.com/saltstack/salt/issues/25618
+
+    In order to not break existing state files this function will call the
+    set_value function if a key is passed. Key will be passed as the value
+    name. If key is not passed, this function will return the default value for
+    the key.
+
+    In the Boron release this function will be removed in favor of set_value.
+    ***
+
     Set a registry key
+
     vtype: http://docs.python.org/2/library/_winreg.html#value-types
 
     CLI Example:
@@ -98,29 +227,73 @@ def set_key(hkey, path, key, value, vtype='REG_DWORD', reflection=True):
 
         salt '*' reg.set_key HKEY_CURRENT_USER 'SOFTWARE\\Salt' 'version' '0.97' REG_DWORD
     '''
+
+    if key:  # This if statement will be removed in Boron
+        salt.utils.warn_until('Boron', 'Use reg.set_value to set a registry'
+                                       'value. This functionality will be'
+                                       'removed in Salt Boron')
+        return set_value(hive=hkey,
+                         key=path,
+                         vname=key,
+                         vdata=value,
+                         vtype=vtype)
+
     registry = Registry()
-    hkey2 = getattr(registry, hkey)
+    hive = registry.hkeys[hkey]
+    vtype = registry.vtype['REG_SZ']
+
+    try:
+        _winreg.SetValue(hive, path, vtype, value)
+        return True
+    except WindowsError as exc:
+        log.error(exc)
+        return False
+
+
+def set_value(hive, key, vname=None, vdata=None, vtype='REG_SZ', reflection=True):
+    '''
+
+    :param hive:
+    :param key:
+    :param vname:
+    :param vdata:
+    :param vtype:
+    :param reflection:
+    :return:
+    '''
+    registry = Registry()
+    hive = registry.hkeys[hive]
+    vtype = registry.vtype[vtype]
     access_mask = registry.reflection_mask[reflection]
 
     try:
-        _type = getattr(_winreg, vtype)
-    except AttributeError:
-        return False
-
-    try:
-        handle = _winreg.OpenKey(hkey2, path, 0, access_mask)
-        _winreg.SetValueEx(handle, key, 0, _type, value)
+        handle = _winreg.CreateKeyEx(hive, key, 0, access_mask)
+        _winreg.SetValueEx(handle, vname, 0, vtype, vdata)
         _winreg.CloseKey(handle)
         return True
-    except Exception:
-        handle = _winreg.CreateKeyEx(hkey2, path, 0, access_mask)
-        _winreg.SetValueEx(handle, key, 0, _type, value)
-        _winreg.CloseKey(handle)
-    return True
+    except WindowsError as exc:
+        log.error(exc)
+        return False
 
 
-def create_key(hkey, path, key, value=None, reflection=True):
+def create_key(hkey, path, key=None, value=None, reflection=True):
     '''
+    *** Incorrect Usage ***
+    The name of this function is misleading and will be changed to reflect
+    proper usage in the Boron release of Salt. The path option will be removed
+    and the key will be the actual key. See the following issue:
+
+    https://github.com/saltstack/salt/issues/25618
+
+    In order to not break existing state files this function will call the
+    set_value function if key is passed. Key will be passed as the value name.
+    If key is not passed, this function will return the default value for the
+    key.
+
+    In the Boron release path will be removed and key will be the path. You will
+    not pass value.
+    ***
+
     Create a registry key
 
     CLI Example:
@@ -129,23 +302,80 @@ def create_key(hkey, path, key, value=None, reflection=True):
 
         salt '*' reg.create_key HKEY_CURRENT_USER 'SOFTWARE\\Salt' 'version' '0.97'
     '''
+    if key:  # This if statement will be removed in Boron
+        salt.utils.warn_until('Boron', 'Use reg.set_value to set a registry'
+                                       'value. This functionality will be'
+                                       'removed in Salt Boron')
+        return set_value(hive=hkey,
+                         key=path,
+                         vname=key,
+                         vdata=value,
+                         vtype='REG_SZ')
+
     registry = Registry()
-    hkey2 = getattr(registry, hkey)
+    hive = registry.hkeys[hkey]
+    key = path
     access_mask = registry.reflection_mask[reflection]
 
     try:
-        handle = _winreg.OpenKey(hkey2, path, 0, access_mask)
+        handle = _winreg.CreateKeyEx(hive, key, 0, access_mask)
         _winreg.CloseKey(handle)
         return True
-    except Exception:
-        handle = _winreg.CreateKeyEx(hkey2, path, 0, access_mask)
-        if value:
-            _winreg.SetValueEx(handle, key, 0, _winreg.REG_DWORD, value)
-        _winreg.CloseKey(handle)
-    return True
+    except WindowsError as exc:
+        log.error(exc)
+        return False
 
 
-def delete_key(hkey, path, key, reflection=True):
+def delete_key(hkey, path, key=None, reflection=True):
+    '''
+    *** Incorrect Usage ***
+    The name of this function is misleading and will be changed to reflect
+    proper usage in the Boron release of Salt. The path option will be removed
+    and the key will be the actual key. See the following issue:
+
+    https://github.com/saltstack/salt/issues/25618
+
+    In order to not break existing state files this function will call the
+    delete_value function if a key is passed. Key will be passed as the value
+    name. If key is not passed, this function will return the default value for
+    the key.
+
+    In the Boron release path will be removed and key will be the path.
+    ***
+
+    Delete a registry key
+
+    Note: This cannot delete a key with subkeys
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' reg.delete_key HKEY_CURRENT_USER 'SOFTWARE\\Salt' 'version'
+    '''
+
+    if key:  # This if statement will be removed in Boron
+        salt.utils.warn_until('Boron', 'Use reg.set_value to set a registry'
+                                       'value. This functionality will be'
+                                       'removed in Salt Boron')
+        return delete_value(hive=hkey,
+                            key=path,
+                            vname=key,
+                            reflection=reflection)
+
+    registry = Registry()
+    hive = registry.hkeys[hkey]
+    key = path
+
+    try:
+        _winreg.DeleteKey(hive, key)
+        return True
+    except WindowsError as exc:
+        log.error(exc)
+        return False
+
+
+def delete_value(hive, key, vname=None, reflection=True):
     '''
     Delete a registry key
 
@@ -158,21 +388,15 @@ def delete_key(hkey, path, key, reflection=True):
         salt '*' reg.delete_key HKEY_CURRENT_USER 'SOFTWARE\\Salt' 'version'
     '''
     registry = Registry()
-    hkey2 = getattr(registry, hkey)
+    hive = registry.hkeys[hive]
     access_mask = registry.reflection_mask[reflection]
 
     try:
-        handle = _winreg.OpenKey(hkey2, path, 0, access_mask)
-        _winreg.DeleteKeyEx(handle, key)
+        handle = _winreg.OpenKey(hive, key, 0, access_mask)
+        _winreg.DeleteValue(handle, vname)
         _winreg.CloseKey(handle)
         return True
-    except Exception:
-        pass
-
-    try:
-        _winreg.DeleteValue(handle, key)
+    except WindowsError as exc:
         _winreg.CloseKey(handle)
-        return True
-    except Exception:
-        _winreg.CloseKey(handle)
+        log.error(exc)
         return False
