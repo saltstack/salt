caded5bc9f9f6bffb63a0454247d01fa5b6285aa
Mark McGuire <mmcguire@backstopsolutions.com>
2015-07-22 16:13:58 -0500

    fix bad merge 99fc7ec

--- salt/modules/file.py
+++ salt/modules/file.py
@@ -3157,7 +3157,7 @@ def get_managed(
         elif source.startswith('/'):
             source_sum = get_hash(source)
         elif source_hash:
-            protos = ('salt', 'http', 'https', 'ftp', 'swift')
+            protos = ('salt', 'http', 'https', 'ftp', 'swift', 's3')
             if _urlparse(source_hash).scheme in protos:
                 # The source_hash is a file on a server
                 hash_fn = __salt__['cp.cache_file'](source_hash, saltenv)
