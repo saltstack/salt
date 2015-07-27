1ad9da30e5b4cadf160202ba485415302c90a9c8
Daniel Hobley <danielh@unity3d.com>
2015-07-27 12:36:49 +0200

    Added a way to wait for a reboot to complete on Windows
    - This is useful if you have many states that are being run in a highstate and you don't want the rest of the states, running just before a reboot as this could lead to half complete states i.e installing software

--- tests/unit/modules/win_system_test.py
+++ tests/unit/modules/win_system_test.py
@@ -58,6 +58,38 @@ class WinSystemTestCase(TestCase):
         mock = MagicMock(return_value='salt')
         with patch.dict(win_system.__salt__, {'cmd.run': mock}):
             self.assertEqual(win_system.reboot(), 'salt')
+            mock.assert_called_once_with(['shutdown', '/r', '/t', '300'], python_shell=False)
+
+    def test_reboot_with_timeout_in_minutes(self):
+        '''
+            Test to reboot the system with a timeout
+        '''
+        mock = MagicMock(return_value='salt')
+        with patch.dict(win_system.__salt__, {'cmd.run': mock}):
+            self.assertEqual(win_system.reboot(5, in_seconds=False), 'salt')
+            mock.assert_called_once_with(['shutdown', '/r', '/t', '300'], python_shell=False)
+
+    def test_reboot_with_timeout_in_seconds(self):
+        '''
+            Test to reboot the system with a timeout
+        '''
+        mock = MagicMock(return_value='salt')
+        with patch.dict(win_system.__salt__, {'cmd.run': mock}):
+            self.assertEqual(win_system.reboot(5, in_seconds=True), 'salt')
+            mock.assert_called_once_with(['shutdown', '/r', '/t', '5'], python_shell=False)
+
+    def test_reboot_with_wait(self):
+        '''
+            Test to reboot the system with a timeout and
+            wait for it to finish
+        '''
+        mock = MagicMock(return_value='salt')
+        sleep_mock = MagicMock(return_value='salt')
+        with patch.dict(win_system.__salt__, {'cmd.run': mock}):
+            with patch('time.sleep', sleep_mock):
+                self.assertEqual(win_system.reboot(wait_for_reboot=True), 'salt')
+                mock.assert_called_once_with(['shutdown', '/r', '/t', '300'], python_shell=False)
+                sleep_mock.assert_called_once_with(330)
 
     def test_shutdown(self):
         '''
