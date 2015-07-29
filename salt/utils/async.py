fb4cdc475fbbfbc7a75d840fadce68d25f4cc4e7
Thomas Jackson <jacksontj.89@gmail.com>
2015-07-29 08:43:10 -0700

    Keep track of SyncWrapper's IOLoop usage
    While thinking through issues that might cause #25718 I ran into this case-- where you can create 2 sync wrappers with the same parent IOLoop and run into problems because you can't start/stop the loop multiple times like that and have the call tree work correctly. To solve this (since it *should* be the exceptional case) I'm making SyncWrapper keep track of which loops are in use-- then if someone attempts this situation it will simply make another one.

--- salt/utils/async.py
+++ salt/utils/async.py
@@ -7,11 +7,14 @@ from __future__ import absolute_import
 
 import tornado.ioloop
 import tornado.concurrent
+LOOP_CLASS = tornado.ioloop.IOLoop
+# attempt to use zmq-- if we have it otherwise fallback to tornado loop
 try:
     import zmq.eventloop.ioloop
     # support pyzmq 13.0.x, TODO: remove once we force people to 14.0.x
     if not hasattr(zmq.eventloop.ioloop, 'ZMQIOLoop'):
         zmq.eventloop.ioloop.ZMQIOLoop = zmq.eventloop.ioloop.IOLoop
+    LOOP_CLASS = zmq.eventloop.ioloop.ZMQIOLoop
 except ImportError:
     pass  # salt-ssh doesn't dep zmq
 
@@ -47,6 +50,7 @@ class SyncWrapper(object):
     ret = sync.async_method()
     '''
     loop_map = weakref.WeakKeyDictionary()  # keep a mapping of parent io_loop -> sync_loop
+    loops_in_use = weakref.WeakSet()  # set of sync_loops in use
 
     def __init__(self, method, args=tuple(), kwargs=None):
         if kwargs is None:
@@ -54,13 +58,24 @@ class SyncWrapper(object):
 
         parent_io_loop = tornado.ioloop.IOLoop.current()
         if parent_io_loop not in SyncWrapper.loop_map:
-            SyncWrapper.loop_map[parent_io_loop] = zmq.eventloop.ioloop.ZMQIOLoop()
+            SyncWrapper.loop_map[parent_io_loop] = LOOP_CLASS()
 
-        self.io_loop = SyncWrapper.loop_map[parent_io_loop]
+        io_loop = SyncWrapper.loop_map[parent_io_loop]
+        if io_loop in self.loops_in_use:
+            io_loop = LOOP_CLASS()
+        self.io_loop = io_loop
+        self.loops_in_use.add(self.io_loop)
         kwargs['io_loop'] = self.io_loop
 
         with current_ioloop(self.io_loop):
             self.async = method(*args, **kwargs)
+
+    def __del__(self):
+        '''
+        Once the async wrapper is complete, remove our loop from the in use set
+        so someone else can use it without making another one
+        '''
+        self.loops_in_use.remove(self.io_loop)
 
     def __getattribute__(self, key):
         try:
