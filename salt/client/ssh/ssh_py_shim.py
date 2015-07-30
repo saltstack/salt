dd9b2fd97ad6f8891b987488fc35dd5f72a57dc8
Sam Norbury <sam.norbury@ukfast.co.uk>
2015-07-30 14:18:15 +0200

    Needed popen.wait().

--- salt/client/ssh/ssh_py_shim.py
+++ salt/client/ssh/ssh_py_shim.py
@@ -16,6 +16,7 @@ import shutil
 import sys
 import os
 import stat
+import subprocess
 
 THIN_ARCHIVE = 'salt-thin.tgz'
 EXT_ARCHIVE = 'salt-ext_mods.tgz'
@@ -133,8 +134,8 @@ def main(argv):  # pylint: disable=W0613
         unpack_thin(thin_path)
         # Salt thin now is available to use
     else:
-        scpstat = subprocess.Popen(["command", "-v", "scp"])
-        if not scpstat.returncode = 0
+        scpstat = subprocess.Popen(['/bin/bash', '-c', 'command -v scp']).wait()
+        if not scpstat == 0:
             sys.exit(EX_SCP_NOT_FOUND)
 
         if not os.path.exists(OPTIONS.saltdir):
@@ -211,14 +212,12 @@ def main(argv):  # pylint: disable=W0613
     sys.stderr.write(OPTIONS.delimiter + '\n')
     sys.stderr.flush()
     if OPTIONS.tty:
-        import subprocess
         stdout, _ = subprocess.Popen(salt_argv, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
         sys.stdout.write(stdout)
         sys.stdout.flush()
         if OPTIONS.wipe:
             shutil.rmtree(OPTIONS.saltdir)
     elif OPTIONS.wipe:
-        import subprocess
         subprocess.call(salt_argv)
         shutil.rmtree(OPTIONS.saltdir)
     else:
