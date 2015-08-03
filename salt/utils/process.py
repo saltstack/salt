cfa9ddebd15dcd2505b1fe169dc9329afe1c026c
Pedro Algarvio <pedro@algarvio.me>
2015-08-03 11:51:22 +0100

    Custom multiprocessing Process class which sets up the logging queue for us

--- salt/utils/process.py
+++ salt/utils/process.py
@@ -10,12 +10,15 @@ import signal
 import subprocess
 import logging
 import multiprocessing
+import multiprocessing.util
 
 import threading
 
 # Import salt libs
 import salt.defaults.exitcodes
 import salt.utils
+import salt.log.setup
+from salt.log.mixins import NewStyleClassMixIn
 
 # Import 3rd-party libs
 import salt.ext.six as six
@@ -292,8 +295,8 @@ class ProcessManager(object):
                 if not salt.utils.is_windows():
                     pid, exit_status = os.wait()
                     if pid not in self._process_map:
-                        log.debug(('Process of pid {0} died, not a known'
-                                   ' process, will not restart').format(pid))
+                        log.debug('Process of pid {0} died, not a known'
+                                  ' process, will not restart'.format(pid))
                         continue
                     self.restart_process(pid)
                 else:
@@ -361,3 +364,13 @@ class ProcessManager(object):
             # in case the process has since decided to die, os.kill returns OSError
             except OSError:
                 pass
+
+
+class MultiprocessingProcess(multiprocessing.Process, NewStyleClassMixIn):
+    def __init__(self, *args, **kwargs):
+        self.log_queue = kwargs.pop('log_queue', salt.log.setup.get_multiprocessing_logging_queue())
+        multiprocessing.util.register_after_fork(self, MultiprocessingProcess.__setup_process_logging)
+        super(MultiprocessingProcess, self).__init__(*args, **kwargs)
+
+    def __setup_process_logging(self):
+        salt.log.setup.setup_multiprocessing_logging(self.log_queue)
