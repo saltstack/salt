f68ade20add91d86ccc0e861e090df2f02a43425
C. R. Oldham <cro@ncbt.org>
2015-08-06 16:31:57 -0600

    Fix lint.

--- salt/grains/junos.py
+++ salt/grains/junos.py
@@ -6,18 +6,20 @@ Thus, some grains make sense to get them from the minion (PYTHONPATH), but other
 don't (ip_interfaces)
 '''
 import logging
-import json
+
 __proxyenabled__ = ['junos']
 
 __virtualname__ = 'junos'
 
 log = logging.getLogger(__name__)
 
+
 def __virtual__():
     if 'proxy' not in __opts__:
         return False
     else:
         return __virtualname__
+
 
 def _remove_complex_types(dictionary):
     '''
@@ -27,19 +29,21 @@ def _remove_complex_types(dictionary):
 
     for k, v in dictionary.iteritems():
         if isinstance(v, dict):
-            dictionary[k] = remove_complex_types(v)
+            dictionary[k] = _remove_complex_types(v)
         elif hasattr(v, 'to_eng_string'):
             dictionary[k] = v.to_eng_string()
 
+
 def defaults():
-    return {'os': 'proxy', 'kernel':'unknown', 'osrelease':'proxy'}
+    return {'os': 'proxy', 'kernel': 'unknown', 'osrelease': 'proxy'}
+
 
 def facts():
     log.debug('----------- Trying to get facts')
     facts = __opts__['proxymodule']['junos.facts']()
     facts['version_info'] = 'override'
     return facts
-    log.debug('----------- Facts call to junos returned')
+
 
 def os_family():
     return {'os_family': 'junos'}
