b5c5a4728f660c6e03a377f74f7dc8a6a7808238
Erik Johnson <palehose@gmail.com>
2015-08-09 02:50:39 -0500

    Deprecate old winrepo config params in salt.runners.winrepo
    Also made a couple bugfixes found in testing

--- salt/runners/winrepo.py
+++ salt/runners/winrepo.py
@@ -25,12 +25,12 @@ import salt.template
 
 log = logging.getLogger(__name__)
 
-PER_REMOTE_PARAMS = ('branch', 'root', 'ssl_verify')
+PER_REMOTE_PARAMS = ('ssl_verify',)
 
 
 def genrepo():
     '''
-    Generate win_repo_cachefile based on sls files in the win_repo
+    Generate winrepo_cachefile based on sls files in the winrepo_dir
 
     CLI Example:
 
@@ -38,13 +38,31 @@ def genrepo():
 
         salt-run winrepo.genrepo
     '''
+    if 'win_repo' in __opts__:
+        salt.utils.warn_until(
+            'Nitrogen',
+            'The \'win_repo\' config option is deprecated, please use '
+            '\'winrepo_dir\' instead.'
+        )
+        winrepo_dir = __opts__['win_repo']
+    else:
+        winrepo_dir = __opts__['winrepo_dir']
+
+    if 'win_repo_mastercachefile' in __opts__:
+        salt.utils.warn_until(
+            'Nitrogen',
+            'The \'win_repo_mastercachefile\' config option is deprecated, '
+            'please use \'winrepo_cachefile\' instead.'
+        )
+        winrepo_cachefile = __opts__['win_repo_mastercachefile']
+    else:
+        winrepo_cachefile = __opts__['winrepo_cachefile']
+
     ret = {}
-    repo = __opts__['win_repo']
-    if not os.path.exists(repo):
-        os.makedirs(repo)
-    winrepo = __opts__['win_repo_mastercachefile']
+    if not os.path.exists(winrepo_dir):
+        os.makedirs(winrepo_dir)
     renderers = salt.loader.render(__opts__, __salt__)
-    for root, _, files in os.walk(repo):
+    for root, _, files in os.walk(winrepo_dir):
         for name in files:
             if name.endswith('.sls'):
                 try:
@@ -89,7 +107,8 @@ def genrepo():
                             revmap[repodata['full_name']] = pkgname
                     ret.setdefault('repo', {}).update(config)
                     ret.setdefault('name_map', {}).update(revmap)
-    with salt.utils.fopen(os.path.join(repo, winrepo), 'w+b') as repo:
+    with salt.utils.fopen(
+            os.path.join(winrepo_dir, winrepo_cachefile), 'w+b') as repo:
         repo.write(msgpack.dumps(ret))
     return ret
 
@@ -104,35 +123,56 @@ def update_git_repos():
 
         salt-run winrepo.update_git_repos
     '''
+    if 'win_repo' in __opts__:
+        salt.utils.warn_until(
+            'Nitrogen',
+            'The \'win_repo\' config option is deprecated, please use '
+            '\'winrepo_dir\' instead.'
+        )
+        winrepo_dir = __opts__['win_repo']
+    else:
+        winrepo_dir = __opts__['winrepo_dir']
+
+    if 'win_gitrepos' in __opts__:
+        salt.utils.warn_until(
+            'Nitrogen',
+            'The \'win_gitrepos\' config option is deprecated, please use '
+            '\'winrepo_remotes\' instead.'
+        )
+        winrepo_remotes = __opts__['win_gitrepos']
+    else:
+        winrepo_remotes = __opts__['winrepo_remotes']
+
     if not any((salt.utils.gitfs.HAS_GITPYTHON, salt.utils.gitfs.HAS_PYGIT2)):
         # Use legacy code
-        repo = __opts__['win_repo']
-        salt.utils.warn_until(
-            'Carbon',
-            'winrepo git support now requires either GitPython or pygit2. '
-            'Please install either GitPython >= {0} (or pygit2 >= {1} with '
-            'libgit2 >= {2}), clear out the win_repo directory ({3}), and '
-            'restart the salt-master service.'.format(
-                salt.utils.gitfs.GITPYTHON_MINVER,
-                salt.utils.gitfs.PYGIT2_MINVER,
-                salt.utils.gitfs.LIBGIT2_MINVER,
-                repo
+        if not salt.utils.is_windows():
+            # Don't warn on Windows, because Windows can't do cool things like
+            # use pygit2. It has to fall back to git.latest.
+            salt.utils.warn_until(
+                'Nitrogen',
+                'winrepo git support now requires either GitPython or pygit2. '
+                'Please install either GitPython >= {0} (or pygit2 >= {1} with '
+                'libgit2 >= {2}), clear out the winrepo_dir ({3}), and '
+                'restart the salt-master service.'.format(
+                    salt.utils.gitfs.GITPYTHON_MINVER,
+                    salt.utils.gitfs.PYGIT2_MINVER,
+                    salt.utils.gitfs.LIBGIT2_MINVER,
+                    winrepo_dir
+                )
             )
-        )
         ret = {}
-        gitrepos = __opts__['win_gitrepos']
         mminion = salt.minion.MasterMinion(__opts__)
-        for gitrepo in gitrepos:
-            if '/' in gitrepo:
-                targetname = gitrepo.split('/')[-1]
+        for remote in winrepo_remotes:
+            if '/' in remote:
+                targetname = remote.split('/')[-1]
             else:
-                targetname = gitrepo
+                targetname = remote
             rev = None
             # If a revision is specified, use it.
-            if len(gitrepo.strip().split(' ')) > 1:
-                rev, gitrepo = gitrepo.strip().split(' ')
-            gittarget = os.path.join(repo, targetname)
-            result = mminion.states['git.latest'](gitrepo,
+            if len(remote.strip().split(' ')) > 1:
+                rev, remote = remote.strip().split(' ')
+            gittarget = os.path.join(winrepo_dir, targetname)
+            result = mminion.states['git.latest'](remote,
                                                   rev=rev,
                                                   target=gittarget,
                                                   force=True)
@@ -142,11 +182,11 @@ def update_git_repos():
         # New winrepo code utilizing salt.utils.gitfs
         try:
             winrepo = salt.utils.gitfs.WinRepo(__opts__)
-            winrepo.init_remotes(__opts__['win_gitrepos'], PER_REMOTE_PARAMS)
+            winrepo.init_remotes(winrepo_remotes, PER_REMOTE_PARAMS)
             winrepo.fetch_remotes()
             winrepo.checkout()
         except Exception as exc:
-            msg = 'Failed to update win_gitrepos: {0}'.format(exc)
+            msg = 'Failed to update winrepo_remotes: {0}'.format(exc)
             log.error(msg, exc_info_on_loglevel=logging.DEBUG)
             return msg
-        return winrepo.win_repo_dirs
+        return winrepo.winrepo_dirs
