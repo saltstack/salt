b7747d47d1436a644e2828a8ba41d827f3d66ec7
Erik Johnson <palehose@gmail.com>
2015-08-09 02:51:21 -0500

    Deprecate old winrepo config params in salt.modules.winrepo
    Also corrected bad path handling in genrepo()

--- salt/modules/win_repo.py
+++ salt/modules/win_repo.py
@@ -2,10 +2,11 @@
 r'''
 Module to manage Windows software repo on a Standalone Minion
 
-The following options must be set in the Minion config:
-  file_client: local
-  win_repo_cachefile: c:\salt\file_roots\winrepo\winrepo.p
-  win_repo: c:\salt\file_roots\winrepo
+``file_client: local`` must be set in the minion config file. Other config
+options of interest include:
+
+* :conf_minion:`winrepo_dir`
+* :conf_minion:`winrepo_cachefile`
 
 Place all Windows package files in the 'win_repo' directory.
 '''
@@ -46,7 +47,7 @@ def __virtual__():
 
 def genrepo():
     r'''
-    Generate win_repo_cachefile based on sls files in the win_repo
+    Generate winrepo_cachefile based on sls files in the win_repo
 
     CLI Example:
 
@@ -54,13 +55,31 @@ def genrepo():
 
         salt-call winrepo.genrepo -c c:\salt\conf
     '''
+    if 'win_repo' in __opts__:
+        salt.utils.warn_until(
+            'Nitrogen',
+            'The \'win_repo\' config option is deprecated, please use '
+            '\'winrepo_dir\' instead.'
+        )
+        winrepo_dir = __opts__['win_repo']
+    else:
+        winrepo_dir = __opts__['winrepo_dir']
+
+    if 'win_repo_cachefile' in __opts__:
+        salt.utils.warn_until(
+            'Nitrogen',
+            'The \'win_repo_cachefile\' config option is deprecated, please '
+            'use \'winrepo_cachefile\' instead.'
+        )
+        winrepo_cachefile = __opts__['win_repo_cachefile']
+    else:
+        winrepo_cachefile = __opts__['winrepo_cachefile']
+
     ret = {}
-    repo = __opts__['win_repo']
-    if not os.path.exists(repo):
-        os.makedirs(repo)
-    winrepo = __opts__['win_repo_cachefile']
+    if not os.path.exists(winrepo_dir):
+        os.makedirs(winrepo_dir)
     renderers = salt.loader.render(__opts__, __salt__)
-    for root, dirs, files in os.walk(repo):
+    for root, dirs, files in os.walk(winrepo_dir):
         for name in files:
             if name.endswith('.sls'):
                 config = salt.template.compile_template(
@@ -77,7 +96,8 @@ def genrepo():
                             revmap[repodata['full_name']] = pkgname
                     ret.setdefault('repo', {}).update(config)
                     ret.setdefault('name_map', {}).update(revmap)
-    with salt.utils.fopen(os.path.join(repo, winrepo), 'w+b') as repo:
+    with salt.utils.fopen(
+            os.path.join(winrepo_dir, winrepo_cachefile), 'w+b') as repo:
         repo.write(msgpack.dumps(ret))
     salt.output.display_output(ret, 'pprint', __opts__)
     return ret
@@ -91,25 +111,43 @@ def update_git_repos():
 
         This function will not work unless git is installed and the git module
         is further updated to work on Windows. In the meantime just place all
-        Windows package files in the ``win_repo`` directory.
+        Windows package files in the :conf_minion:`winrepo_dir` directory.
     '''
+    if 'win_repo' in __opts__:
+        salt.utils.warn_until(
+            'Nitrogen',
+            'The \'win_repo\' config option is deprecated, please use '
+            '\'winrepo_dir\' instead.'
+        )
+        winrepo_dir = __opts__['win_repo']
+    else:
+        winrepo_dir = __opts__['winrepo_dir']
+
+    if 'win_gitrepos' in __opts__:
+        salt.utils.warn_until(
+            'Nitrogen',
+            'The \'win_gitrepos\' config option is deprecated, please use '
+            '\'winrepo_remotes\' instead.'
+        )
+        winrepo_remotes = __opts__['win_gitrepos']
+    else:
+        winrepo_remotes = __opts__['winrepo_remotes']
+
     ret = {}
     #mminion = salt.minion.MasterMinion(__opts__)
-    repo = __opts__['win_repo']
-    gitrepos = __opts__['win_gitrepos']
-    for gitrepo in gitrepos:
-        #if '/' in gitrepo:
-            #targetname = gitrepo.split('/')[-1]
+    for remote in winrepo_remotes:
+        #if '/' in remote:
+            #targetname = remote.split('/')[-1]
         #else:
-            #targetname = gitrepo
-        targetname = gitrepo
+            #targetname = remote
+        targetname = remote
         rev = None
         # If a revision is specified, use it.
-        if len(gitrepo.strip().split(' ')) > 1:
-            rev, gitrepo = gitrepo.strip().split(' ')
-        gittarget = os.path.join(repo, targetname)
-        #result = mminion.states['git.latest'](gitrepo,
-        result = __salt__['git.latest'](gitrepo,
+        if len(remote.strip().split(' ')) > 1:
+            rev, remote = remote.strip().split(' ')
+        gittarget = os.path.join(winrepo_dir, targetname)
+        #result = mminion.states['git.latest'](remote,
+        result = __salt__['git.latest'](remote,
                                         rev=rev,
                                         target=gittarget,
                                         force=True)
