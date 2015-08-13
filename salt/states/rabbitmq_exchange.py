a2dc4d0e23c7862b965a4d1ebea71b1bbe6127e8
James Michael DuPont <mdupont@caltesting.org>
2015-08-13 16:25:04 -0500

    update documentation and checks
    look if the admin tool is in the path before loading

--- salt/states/rabbitmq_exchange.py
+++ salt/states/rabbitmq_exchange.py
@@ -9,8 +9,12 @@ Example:
 
     someexchange:
         rabbitmq_exchange.present:
-            - vhost: somehost
-            - durable: true
+            - name: some_exchange
+            - vhost: /
+            - typename: fanout
+            - durable: True
+            - auto_delete: False
+            - internal: False
 '''
 from __future__ import absolute_import
 
@@ -25,9 +29,9 @@ log = logging.getLogger(__name__)
 
 def __virtual__():
     '''
-    Only load if RabbitMQ is installed.
+    Only load if RabbitMQ rabbitmqcadmin is installed.
     '''
-    return salt.utils.which('rabbitmqctl') is not None
+    return salt.utils.which('rabbitmqadmin') is not None
 
 
 def present(name, vhost, typename, durable, auto_delete, internal):
@@ -37,18 +41,21 @@ def present(name, vhost, typename, durable, auto_delete, internal):
     name
         Exchange name
 
-    typename 
-        fanout etc
+    typename
+        Direct, Fanout, Topic, Headers
 
     vhost
         VHost name
 
-    durable boolean
-    
-    auto_delete boolean
+    durable
+        Is the exhange durable? Will it survive a broker crash.
 
-    internal boolean
-    
+    auto_delete
+        Is the exchange to be removed when the last queue is removed.
+
+    internal
+        Normally always False
+
     runas
         Name of the user to run the command
 
