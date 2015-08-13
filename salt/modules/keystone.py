5edabfd271ec4c08800de437af0495726f58a1a3
rallytime <nicole@saltstack.com>
2015-08-13 10:40:53 -0600

    It's a dict - git problems...

--- salt/modules/keystone.py
+++ salt/modules/keystone.py
@@ -713,7 +713,7 @@ def user_get(user_id=None, name=None, profile=None, **connection_args):
                 user_id
             )
         )
-        return {'Could not find user \'{0}\''.format(user_id)}
+        return {'Error': 'Could not find user \'{0}\''.format(user_id)}
 
     ret[user.name] = {'id': user.id,
                       'name': user.name,
