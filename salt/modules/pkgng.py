e26070e135d64a3b626c15d6909fd38b4f9951f6
0xf10e <florian.ermisch@alumni.tu-berlin.de>
2015-08-15 20:41:05 +0200

    remove redundant backslashes

--- salt/modules/pkgng.py
+++ salt/modules/pkgng.py
@@ -63,7 +63,7 @@ def __virtual__():
             providers = __opts__['providers']
         log.debug('__opts__.providers: {0}'.format(providers))
         if providers and 'pkg' in providers and providers['pkg'] == 'pkgng':
-            log.debug('Configuration option \'providers:pkg\' is set to ' \
+            log.debug('Configuration option \'providers:pkg\' is set to '
                 '\'pkgng\', using \'pkgng\' in favor of \'freebsdpkg\'.')
             return __virtualname__
     return False
