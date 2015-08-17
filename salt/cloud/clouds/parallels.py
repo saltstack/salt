92d9253125f66949cf55b7126f5d5252c3d3e371
rallytime <nicole@saltstack.com>
2015-08-17 10:41:05 -0600

    Allow the creation of a VM without a profile
    Fixes #26369

--- salt/cloud/clouds/parallels.py
+++ salt/cloud/clouds/parallels.py
@@ -271,9 +271,9 @@ def create(vm_):
     '''
     try:
         # Check for required profile parameters before sending any API calls.
-        if config.is_profile_configured(__opts__,
-                                        __active_provider_name__ or 'parallels',
-                                        vm_['profile']) is False:
+        if vm_['profile'] and config.is_profile_configured(__opts__,
+                                                           __active_provider_name__ or 'parallels',
+                                                           vm_['profile']) is False:
             return False
     except AttributeError:
         pass
