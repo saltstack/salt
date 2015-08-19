c6626bad0f5afaaed80d2526a9c0fc37bf86879a
Mike Place <mp@saltstack.com>
2015-08-19 11:21:15 -0600

    Check socket too

--- salt/transport/zeromq.py
+++ salt/transport/zeromq.py
@@ -716,7 +716,8 @@ class AsyncReqMessageClient(object):
     def destroy(self):
         if hasattr(self, 'stream') and self.stream is not None:
             # TODO: Optionally call stream.close() on newer pyzmq? It is broken on some.
-            self.stream.socket.close()
+            if self.stream.socket:
+                self.stream.socket.close()
             self.stream.io_loop.remove_handler(self.stream.socket)
             # set this to None, more hacks for messed up pyzmq
             self.stream.socket = None
