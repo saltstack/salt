598500034fa31485287ba728d38952b879938240
TheBigBear <TheBigBear@users.noreply.github.com>
2015-08-19 19:25:07 +0100

    Update file.py

--- salt/states/file.py
+++ salt/states/file.py
@@ -2491,7 +2491,7 @@ def replace(name,
                 CentOS \(2.6.32[^\n]+\n\s+root[^\n]+\n\)+
     '''
     .. note::
-       When using YAML multiline string syntax in pattern, make sure to also use that synyax in the repl part, or you might loose line feeds.  
+       When using YAML multiline string syntax in pattern, make sure to also use that syntax in the repl part, or you might loose line feeds.  
     '''    
     name = os.path.expanduser(name)
 
