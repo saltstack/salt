b59be121d45111b08a54b702d47bc3603a52f742
Erik Johnson <palehose@gmail.com>
2015-08-21 13:48:58 -0500

    Revert "Remove genrepo function"
    This reverts commit e4310d07f36ff78e3f20d4b047b1462f5836c435.

--- salt/modules/win_pkg.py
+++ salt/modules/win_pkg.py
@@ -411,6 +411,54 @@ def _get_local_repo_dir(saltenv='base'):
     return os.sep.join(dirs)
 
 
+def genrepo(saltenv='base'):
+    '''
+    Generate winrepo_cachefile based on sls files in the winrepo
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt-run winrepo.genrepo
+    '''
+    ret = {}
+    repo = _get_local_repo_dir(saltenv)
+    if not os.path.exists(repo):
+        os.makedirs(repo)
+    winrepo = 'winrepo.p'
+    renderers = salt.loader.render(__opts__, __salt__)
+    for root, _, files in os.walk(repo):
+        for name in files:
+            if name.endswith('.sls'):
+                try:
+                    config = salt.template.compile_template(
+                            os.path.join(root, name),
+                            renderers,
+                            __opts__['renderer'])
+                except SaltRenderError as exc:
+                    log.debug('Failed to compile {0}.'.format(os.path.join(root, name)))
+                    log.debug('Error: {0}.'.format(exc))
+                    continue
+
+                if config:
+                    revmap = {}
+                    for pkgname, versions in six.iteritems(config):
+                        for version, repodata in six.iteritems(versions):
+                            if not isinstance(version, six.string_types):
+                                config[pkgname][str(version)] = \
+                                    config[pkgname].pop(version)
+                            if not isinstance(repodata, dict):
+                                log.debug('Failed to compile'
+                                          '{0}.'.format(os.path.join(root, name)))
+                                continue
+                            revmap[repodata['full_name']] = pkgname
+                    ret.setdefault('repo', {}).update(config)
+                    ret.setdefault('name_map', {}).update(revmap)
+    with salt.utils.fopen(os.path.join(repo, winrepo), 'w+b') as repo_cache:
+        repo_cache.write(msgpack.dumps(ret))
+    return ret
+
+
 def install(name=None, refresh=False, pkgs=None, saltenv='base', **kwargs):
     '''
     Install the passed package from the winrepo
