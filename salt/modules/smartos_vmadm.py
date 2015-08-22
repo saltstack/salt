72a6b7d7a3cf2b43e0afa731f5ca3df35fa8e39f
Jorge Schrauwen <sjorge@blackdot.be>
2015-08-22 11:15:00 +0000

    vmadm.stop with support for uuid, alias or hostname

--- salt/modules/smartos_vmadm.py
+++ salt/modules/smartos_vmadm.py
@@ -72,7 +72,48 @@ def _exit_status(retcode):
 #validate create [-f <filename>]
 #validate update <brand> [-f <filename>]
 
-# vmadm stop <uuid> [-F]
+
+def stop(vm=None, force=False, key='uuid'):
+    '''
+    Stop a vm
+
+    vm : string
+        Specifies the vm to be stopped
+    force : boolean
+        Specifies if the vm should be force stopped
+    key : string
+        Specifies what 'vm' is. Value = uuid|alias|hostname
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' vmadm.stop 186da9ab-7392-4f55-91a5-b8f1fe770543
+        salt '*' vmadm.stop 186da9ab-7392-4f55-91a5-b8f1fe770543 True
+        salt '*' vmadm.stop vm=nacl key=alias
+        salt '*' vmadm.stop vm=nina.example.org key=hostname
+    '''
+    ret = {}
+    vmadm = _check_vmadm()
+    if key not in ['uuid', 'alias', 'hostname']:
+        ret['Error'] = 'Key must be either uuid, alias or hostname'
+        return ret
+    if key != 'uuid':
+        vm = lookup('{0}={1}'.format(key, vm), one=True)
+        if 'Error' in vm:
+            return vm
+    # vmadm stop <uuid> [-F]
+    cmd = '{vmadm} stop {force} {uuid}'.format(
+        vmadm=vmadm,
+        force='-F' if force else '',
+        uuid=vm
+    )
+    res = __salt__['cmd.run_all'](cmd)
+    retcode = res['retcode']
+    if retcode != 0:
+        ret['Error'] = _exit_status(retcode)
+        return ret
+    return True
 
 
 def list_vms(search=None, sort=None, order='uuid,type,ram,state,alias', keyed=False):
