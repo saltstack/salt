95b05a990a264886c4bd3527c9b50b9999179bea
Jorge Schrauwen <sjorge@blackdot.be>
2015-08-24 17:31:40 +0000

    vmadm.delete_snapshot + minor doc fixes

--- salt/modules/smartos_vmadm.py
+++ salt/modules/smartos_vmadm.py
@@ -52,7 +52,6 @@ def _exit_status(retcode):
 
 ## TODO
 #create [-f <filename>]
-#delete-snapshot <uuid> <snapname>
 #info <uuid> [type,...]
 #receive [-f <filename>]
 #reprovision [-f <filename>]
@@ -451,8 +450,8 @@ def create_snapshot(vm=None, name=None, key='uuid'):
 
     .. code-block:: bash
 
-        salt '*' vmadm.delete 186da9ab-7392-4f55-91a5-b8f1fe770543
-        salt '*' vmadm.delete nacl key=alias
+        salt '*' vmadm.create_snapshot 186da9ab-7392-4f55-91a5-b8f1fe770543 baseline
+        salt '*' vmadm.create_snapshot nacl baseline key=alias
     '''
     ret = {}
     vmadm = _check_vmadm()
@@ -470,13 +469,76 @@ def create_snapshot(vm=None, name=None, key='uuid'):
         return vm
     vmobj = get(vm)
     if 'datasets' in vmobj:
-        ret['Error'] = 'VM cannot have one or more datasets'
+        ret['Error'] = 'VM cannot have datasets'
         return ret
     if vmobj['brand'] in ['kvm']:
         ret['Error'] = 'VM must be of type OS'
         return ret
+    if vmobj['zone_state'] not in ['running']: # work around a vmadm bug
+        ret['Error'] = 'VM must be running to take a snapshot'
+        return ret
     # vmadm create-snapshot <uuid> <snapname>
     cmd = '{vmadm} create-snapshot {uuid} {snapshot}'.format(
+        vmadm=vmadm,
+        snapshot=name,
+        uuid=vm
+    )
+    res = __salt__['cmd.run_all'](cmd)
+    retcode = res['retcode']
+    if retcode != 0:
+        ret['Error'] = res['stderr'] if 'stderr' in res else _exit_status(retcode)
+        return ret
+    return True
+
+
+def delete_snapshot(vm=None, name=None, key='uuid'):
+    '''
+    Delete snapshot of a vm
+
+    vm : string
+        Specifies the vm
+    name : string
+        Name of snapshot.
+        The snapname must be 64 characters or less
+        and must only contain alphanumeric characters and
+        characters in the set [-_.:%] to comply with ZFS restrictions.
+
+    key : string
+        Specifies what 'vm' is. Value = uuid|alias|hostname
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' vmadm.delete_snapshot 186da9ab-7392-4f55-91a5-b8f1fe770543 baseline
+        salt '*' vmadm.delete_snapshot nacl baseline key=alias
+    '''
+    ret = {}
+    vmadm = _check_vmadm()
+    if vm is None:
+        ret['Error'] = 'uuid, alias or hostname must be provided'
+        return ret
+    if name is None:
+        ret['Error'] = 'Snapshot name most be specified'
+        return ret
+    if key not in ['uuid', 'alias', 'hostname']:
+        ret['Error'] = 'Key must be either uuid, alias or hostname'
+        return ret
+    vm = lookup('{0}={1}'.format(key, vm), one=True)
+    if 'Error' in vm:
+        return vm
+    vmobj = get(vm)
+    if 'datasets' in vmobj:
+        ret['Error'] = 'VM cannot have datasets'
+        return ret
+    if vmobj['brand'] in ['kvm']:
+        ret['Error'] = 'VM must be of type OS'
+        return ret
+    if vmobj[''] in ['zone_state']:
+        ret['Error'] = 'VM must be of type OS'
+        return ret
+    # vmadm delete-snapshot <uuid> <snapname>
+    cmd = '{vmadm} delete-snapshot {uuid} {snapshot}'.format(
         vmadm=vmadm,
         snapshot=name,
         uuid=vm
