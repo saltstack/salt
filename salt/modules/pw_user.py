33e02b46971db1960e98c81aa9dd6c4d89799251
Julien Cigar <julien.cigar@gmail.com>
2015-09-01 18:08:31 +0200

    add .. versionadded:: Boron

--- salt/modules/pw_user.py
+++ salt/modules/pw_user.py
@@ -440,11 +440,14 @@ def get_loginclass(name):
     '''
     Get the login class of the user
 
+    .. versionadded:: Boron
+
     CLI Example:
 
     .. code-block:: bash
 
         salt '*' user.get_loginclass foo
+
     '''
 
     userinfo = __salt__['cmd.run_stdout']('pw usershow -n {0}'.format(name))
