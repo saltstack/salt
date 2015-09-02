3df8345ca864f43b3ddb33d5d429e7554f17cafd
David Boucha <boucha@gmail.com>
2015-09-02 11:29:27 -0600

    add logging and check exc for both SessionErrors

--- salt/utils/smb.py
+++ salt/utils/smb.py
@@ -14,7 +14,8 @@ log = logging.getLogger(__name__)
 
 try:
     import impacket.smbconnection
-    from impacket.smb3 import SessionError
+    from impacket.smbconnection import SessionError as smbSessionError
+    from impacket.smb3 import SessionError as smb3SessionError
     HAS_IMPACKET = True
 except ImportError:
     HAS_IMPACKET = False
@@ -70,7 +71,8 @@ def mkdirs(path, share='C$', conn=None, host=None, username=None, password=None)
         cwd = '\\'.join(comps[0:pos])
         try:
             conn.listPath(share, cwd)
-        except SessionError:
+        except (smbSessionError, smb3SessionError) as exc:
+            log.debug('Exception: {0}'.format(exc))
             conn.createDirectory(share, cwd)
         pos += 1
 
