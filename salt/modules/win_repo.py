0c4578163709421a7c5b225a4dde9f693979f08b
Erik Johnson <palehose@gmail.com>
2015-09-08 22:56:14 -0500

    Don't clean old remotes by default
    This keeps non-git repo definitions from being removed win the winrepo
    remotes are updated, unless clean=True is explicitly passed.

--- salt/modules/win_repo.py
+++ salt/modules/win_repo.py
@@ -86,7 +86,7 @@ def genrepo():
     return _genrepo(opts=__opts__, fire_event=False)
 
 
-def update_git_repos():
+def update_git_repos(clean=False):
     '''
     Checkout git repos containing :ref:`Windows Software Package Definitions
     <windows-package-manager>`
@@ -97,6 +97,24 @@ def update_git_repos():
         permits the git executable to be run from the Command Prompt.
 
     .. _`Git for Windows`: https://git-for-windows.github.io/
+
+    clean : False
+        Clean repo cachedirs which are not configured under
+        :conf_minion:`winrepo_remotes`.
+
+        .. note::
+            This option only applies if either pygit2_ or GitPython_ is
+            installed into Salt's bundled Python.
+
+        .. warning::
+            This argument should not be set to ``True`` if a mix of git and
+            non-git repo definitions are being used, as it will result in the
+            non-git repo definitions being removed.
+
+        .. versionadded:: 2015.8.0
+
+        .. _GitPython: https://github.com/gitpython-developers/GitPython
+        .. _pygit2: https://github.com/libgit2/pygit2
 
     CLI Example:
 
@@ -109,7 +127,7 @@ def update_git_repos():
             'Git for Windows is not installed, or not configured to be '
             'accessible from the Command Prompt'
         )
-    return _update_git_repos(opts=__opts__, masterless=True)
+    return _update_git_repos(opts=__opts__, clean=clean, masterless=True)
 
 
 def show_sls(name, saltenv='base'):
