4cd11826e80d7a04053900be8ea2810f8abfc22d
turnred <ferdinant@turnred.net>
2015-09-10 14:09:55 +0200

    Fix 'dict' object has no attribute split
    Remove trailing whitespace

--- salt/modules/win_path.py
+++ salt/modules/win_path.py
@@ -75,9 +75,9 @@ def get_path():
     ret = __salt__['reg.read_key']('HKEY_LOCAL_MACHINE',
                                    'SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment',
                                    'PATH')
-    if isinstance(ret, dict):         
-        ret = ret['vdata'].split(';') 
-    if isinstance(ret, str):          
+    if isinstance(ret, dict):
+        ret = ret['vdata'].split(';')
+    if isinstance(ret, str):
         ret = ret.split(';')
 
     # Trim ending backslash
