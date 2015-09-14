e0192d2e3499393911cdeb96aa204030673ce342
Pedro Algarvio <pedro@algarvio.me>
2015-09-14 17:58:29 +0100

    Make Pylint happier

--- salt/modules/lxc.py
+++ salt/modules/lxc.py
@@ -22,7 +22,7 @@ import time
 import shutil
 import re
 import random
-import distutils.version
+import distutils.version  # pylint: disable=no-name-in-module,import-error
 
 # Import salt libs
 import salt
