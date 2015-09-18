35b2c5df0d8463fb5d02830b57e42154be64e828
Seth House <seth@eseth.com>
2015-09-17 18:00:51 -0600

    Remove old versionadded from local_batch client

--- salt/netapi/__init__.py
+++ salt/netapi/__init__.py
@@ -97,8 +97,6 @@ class NetapiClient(object):
         '''
         Run :ref:`execution modules <all-salt.modules>` against batches of minions
 
-        .. versionadded:: 0.8.4
-
         Wraps :py:meth:`salt.client.LocalClient.cmd_batch`
 
         :return: Returns the result from the exeuction module for each batch of
