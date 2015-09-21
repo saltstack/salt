c6716b0191fb7c3190de0185567d079663775752
Erik Johnson <palehose@gmail.com>
2015-09-21 17:49:21 -0500

    Add documentation for GPG-encrypted CLI pillar data

--- salt/renderers/gpg.py
+++ salt/renderers/gpg.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-'''
+r'''
 Renderer that will decrypt GPG ciphers
 
 Any key in the SLS file can be a GPG cipher, and this renderer will decrypt it
@@ -11,56 +11,65 @@ The typical use-case would be to use ciphers in your pillar data, and keep a
 secret key on your master. You can put the public key in source control so that
 developers can add new secrets quickly and easily.
 
-This renderer requires the gpg binary.
+This renderer requires the gpg_ binary. No python libraries are required as of
+the 2015.8.0 release.
 
-**No python libraries are required as of the Beryllium release.**
+.. _gpg: https://gnupg.org
 
-To set things up, you will first need to generate a keypair. On your master,
-run:
+Setup
+-----
 
-.. code-block:: shell
+To set things up, first generate a keypair. On the master, run the following:
+
+.. code-block:: bash
 
     # mkdir -p /etc/salt/gpgkeys
     # chmod 0700 /etc/salt/gpgkeys
     # gpg --gen-key --homedir /etc/salt/gpgkeys
 
-Do not supply a password for your keypair, and use a name that makes sense
-for your application. Be sure to back up your gpg directory someplace safe!
+Do not supply a password for the keypair, and use a name that makes sense for
+your application. Be sure to back up the ``gpgkeys`` directory someplace safe!
 
 .. note::
     Unfortunately, there are some scenarios - for example, on virtual machines
     which don’t have real hardware - where insufficient entropy causes key
-    generation to be extremely slow. If you come across this problem, you should
-    investigate means of increasing the system entropy. On virtualised Linux
-    systems, this can often be achieved by installing the rng-tools package.
+    generation to be extremely slow. In these cases, there are usually means of
+    increasing the system entropy. On virtualised Linux systems, this can often
+    be achieved by installing the ``rng-tools`` package.
 
-To retrieve the public key:
 
-.. code-block:: shell
+Export the Public Key
+---------------------
 
-    # gpg --homedir /etc/salt/gpgkeys --armor --export <KEY-NAME> \
-        > exported_pubkey.gpg
+.. code-block:: bash
 
-Now, to encrypt secrets, copy the public key to your local machine and run:
+    # gpg --homedir /etc/salt/gpgkeys --armor --export <KEY-NAME> > exported_pubkey.gpg
 
-.. code-block:: shell
+
+Import the Public Key
+---------------------
+
+To encrypt secrets, copy the public key to your local machine and run:
+
+.. code-block:: bash
 
     $ gpg --import exported_pubkey.gpg
 
 To generate a cipher from a secret:
 
-.. code-block:: shell
+.. code-block:: bash
 
-   $ echo -n "supersecret" | gpg --armor --encrypt -r <KEY-name>
+   $ echo -n "supersecret" | gpg --armor --batch --trust-model always --encrypt -r <KEY-name>
 
 To apply the renderer on a file-by-file basis add the following line to the
 top of any pillar with gpg data in it:
 
-    .. code-block:: yaml
+.. code-block:: yaml
 
-        #!yaml|gpg
+    #!yaml|gpg
 
-Now with your renderer configured, you can include your ciphers in your pillar data like so:
+Now with your renderer configured, you can include your ciphers in your pillar
+data like so:
 
 .. code-block:: yaml
 
@@ -77,9 +86,126 @@ Now with your renderer configured, you can include your ciphers in your pillar d
       m4wBkfCAd8Eyo2jEnWQcM4TcXiF01XPL4z4g1/9AAxh+Q4d8RIRP4fbw7ct4nCJv
       Gr9v2DTF7HNigIMl4ivMIn9fp+EZurJNiQskLgNbktJGAeEKYkqX5iCuB1b693hJ
       FKlwHiJt5yA8X2dDtfk8/Ph1Jx2TwGS+lGjlZaNqp3R1xuAZzXzZMLyZDe5+i3RJ
-      skqmFTbOiA==
-      =Eqsm
+      skqmFTbOiA===Eqsm
       -----END PGP MESSAGE-----
+
+
+.. _encrypted-cli-pillar-data
+
+Encrypted CLI Pillar Data
+-------------------------
+
+.. versionadded:: Boron
+
+Functions like :py:func:`state.highstate <salt.modules.state.highstate>` and
+:py:func:`state.sls <salt.modules.state.sls>` allow for pillar data to be
+passed on the CLI.
+
+.. code-block:: bash
+
+    salt myminion state.highstate pillar="{'mypillar': 'foo'}"
+
+Starting with the Boron release of Salt, it is now possible for this pillar
+data to be GPG-encrypted, and to use the GPG renderer to decrypt it.
+
+
+Replacing Newlines
+******************
+
+To pass encrypted pillar data on the CLI, the ciphertext must have its newlines
+replaced with a literal backslash-n (``\n``), as newlines are not supported
+within Salt CLI arguments. There are a number of ways to do this:
+
+With awk or Perl:
+
+.. code-block:: bash
+
+    # awk
+    ciphertext=`echo -n "supersecret" | gpg --armor --batch --trust-model always --encrypt -r user@domain.com | awk '{printf "%s\\n",$0} END {print ""}'`
+    # Perl
+    ciphertext=`echo -n "supersecret" | gpg --armor --batch --trust-model always --encrypt -r user@domain.com | perl -pe 's/\n/\\n/g'`
+
+With Python:
+
+.. code-block:: python
+
+    import subprocess
+
+    secret, stderr = subprocess.Popen(
+        ['gpg', '--armor', '--batch', '--trust-model', 'always', '--encrypt',
+         '-r', 'user@domain.com'],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE).communicate(input='supersecret')
+
+    if secret:
+        print(secret.replace('\n', r'\n'))
+    else:
+        raise ValueError('No ciphertext found: {0}'.format(stderr))
+
+.. code-block:: bash
+
+    ciphertext=`python /path/to/script.py`
+
+
+The ciphertext can be included in the CLI pillar data like so:
+
+.. code-block:: bash
+
+    salt myminion state.sls secretstuff pillar_enc=gpg pillar="{secret_pillar: '$ciphertext'}"
+
+The ``pillar_enc=gpg`` argument tells Salt that there is GPG-encrypted pillar
+data, so that the CLI pillar data is passed through the GPG renderer, which
+will iterate recursively though the CLI pillar dictionary to decrypt any
+encrypted values.
+
+
+Encrypting the Entire CLI Pillar Dictionary
+*******************************************
+
+If several values need to be encrypted, it may be more convenient to encrypt
+the entire CLI pillar dictionary. Again, this can be done in several ways:
+
+With awk or Perl:
+
+.. code-block:: bash
+
+    # awk
+    ciphertext=`echo -n "{'secret_a': 'CorrectHorseBatteryStaple', 'secret_b': 'GPG is fun!'}" | gpg --armor --batch --trust-model always --encrypt -r user@domain.com | awk '{printf "%s\\n",$0} END {print ""}'`
+    # Perl
+    ciphertext=`echo -n "{'secret_a': 'CorrectHorseBatteryStaple', 'secret_b': 'GPG is fun!'}" | gpg --armor --batch --trust-model always --encrypt -r user@domain.com | perl -pe 's/\n/\\n/g'`
+
+With Python:
+
+.. code-block:: python
+
+    import subprocess
+
+    pillar_data = {'secret_a': 'CorrectHorseBatteryStaple',
+                   'secret_b': 'GPG is fun!'}
+
+    secret, stderr = subprocess.Popen(
+        ['gpg', '--armor', '--batch', '--trust-model', 'always', '--encrypt',
+         '-r', 'user@domain.com'],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE).communicate(input=repr(pillar_data))
+
+    if secret:
+        print(secret.replace('\n', r'\n'))
+    else:
+        raise ValueError('No ciphertext found: {0}'.format(stderr))
+
+.. code-block:: bash
+
+    ciphertext=`python /path/to/script.py`
+
+With the entire pillar dictionary now encrypted, it can be included in the CLI
+pillar data like so:
+
+.. code-block:: bash
+
+    salt myminion state.sls secretstuff pillar_enc=gpg pillar="$ciphertext"
 '''
 
 # Import python libs
