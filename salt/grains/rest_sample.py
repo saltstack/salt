17a1db59ecdaeda8f4b2f9208df9d933950dfd3e
C. R. Oldham <cro@ncbt.org>
2015-09-23 19:36:37 -0600

    Pylint

--- salt/grains/rest_sample.py
+++ salt/grains/rest_sample.py
@@ -2,17 +2,12 @@
 '''
 Generate baseline proxy minion grains
 '''
-import logging
-
 __proxyenabled__ = ['rest_sample']
 
 __virtualname__ = 'rest_sample'
 
-log = logging.getLogger(__file__)
-
 
 def __virtual__():
-    log.debug('In RestExample grains virtual-------------------------------')
     if 'proxy' not in __opts__:
         return False
     else:
