9fe4fd5ad5d76a2d5b03a645ae3a442fdb77fba1
Oleg Gashev <oleg@gashev.net>
2015-10-02 17:43:43 +0000

    Fix subprocess.mswindows check for Python 3.5.
    In Python 3.5 the attribute subprocess.mswindows has been renamed
    to subprocess._mswindows. Use sys.platform == 'win32' instead.

--- salt/utils/vt.py
+++ salt/utils/vt.py
@@ -31,7 +31,9 @@ import select
 import logging
 import subprocess
 
-if subprocess.mswindows:
+mswindows = (sys.platform == "win32")
+
+if mswindows:
     # pylint: disable=F0401,W0611
     from win32file import ReadFile, WriteFile
     from win32pipe import PeekNamedPipe
@@ -349,7 +351,7 @@ class Terminal(object):
     # <---- Context Manager Methods ------------------------------------------
 
 # ----- Platform Specific Methods ------------------------------------------->
-    if subprocess.mswindows:
+    if mswindows:
         # ----- Windows Methods --------------------------------------------->
         def _execute(self):
             raise NotImplementedError
