779d8344f72ab271cb5888f368a9b114edd3aff6
Julien Cigar <jcigar@ulb.ac.be>
2015-10-02 17:06:40 +0200

    ; may be used as well to comment

--- salt/grains/core.py
+++ salt/grains/core.py
@@ -1648,7 +1648,7 @@ def dns():
                         domain = arg[0]
                     elif directive == 'search':
                         search = list(itertools.takewhile(
-                            lambda x: not x.startswith('#'), arg[1:]))
+                            lambda x: x[0] not in ('#', ';'), arg[1:]))
                 except (IndexError, RuntimeError):
                     continue
 
