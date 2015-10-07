3d00f913fa4567a373e6470f58527a8053edf4e5
Tom Williams <tom.williams@hotschedules.com>
2015-10-07 16:26:27 -0400

    Parameter defs for new functions per @ryan-lane's suggestion.

--- salt/states/boto_ec2.py
+++ salt/states/boto_ec2.py
@@ -470,9 +470,133 @@ def instance_present(name, instance_name=None, instance_id=None, image_id=None,
                      attributes=None, target_state=None, region=None, key=None,
                      keyid=None, profile=None):
     ### TODO - implement 'target_state={running, stopped}'
-    ### TODO - Describe all the above params :-(
+    ### TODO - implement image_name->image_id lookups
     '''
     Ensure an EC2 instance is running with the given attributes and state.
+
+    name
+        (string) - The name of the state definition.  Recommended that this
+        match the instance_name attribute (generally the FQDN of the instance).
+    instance_name
+        (string) - The name of the instance, generally its FQDN.  Exclusive with
+        'instance_id'.
+    instance_id
+        (string) - The ID of the instance (if known).  Exclusive with
+        'instance_name'.
+    image_id
+        (string) – The ID of the AMI image to run.
+    image_name
+        (string) – The name of the AMI image to run.  NOT IMPLEMENTED.
+    tags
+        (dict) - Tags to apply to the instance.
+    key_name
+        (string) – The name of the key pair with which to launch instances.
+    security_groups
+        (list of strings) – The names of the EC2 classic security groups with
+        which to associate instances
+    user_data
+        (string) – The Base64-encoded MIME user data to be made available to the
+        instance(s) in this reservation.
+    instance_type
+        (string) – The EC2 instance size/type.  Note that only certain types are
+        compatible with HVM based AMIs.
+    placement
+        (string) – The Availability Zone to launch the instance into.
+    kernel_id
+        (string) – The ID of the kernel with which to launch the instances.
+    ramdisk_id
+        (string) – The ID of the RAM disk with which to launch the instances.
+    vpc_id
+        (string) - The ID of a VPC to attach the instance to.
+    vpc_name
+        (string) - The name of a VPC to attach the instance to.
+    monitoring_enabled
+        (bool) – Enable detailed CloudWatch monitoring on the instance.
+    subnet_id
+        (string) – The ID of the subnet within which to launch the instances for
+        VPC.
+    subnet_name
+        (string) – The name of the subnet within which to launch the instances
+        for VPC.
+    private_ip_address
+        (string) – If you’re using VPC, you can optionally use this parameter to
+        assign the instance a specific available IP address from the subnet
+        (e.g., 10.0.0.25).
+    block_device_map
+        (boto.ec2.blockdevicemapping.BlockDeviceMapping) – A BlockDeviceMapping
+        data structure describing the EBS volumes associated with the Image.
+    disable_api_termination
+        (bool) – If True, the instances will be locked and will not be able to
+        be terminated via the API.
+    instance_initiated_shutdown_behavior
+        (string) – Specifies whether the instance stops or terminates on
+        instance-initiated shutdown. Valid values are:
+            'stop'
+            'terminate'
+    placement_group
+        (string) – If specified, this is the name of the placement group in
+        which the instance(s) will be launched.
+    client_token
+        (string) – Unique, case-sensitive identifier you provide to ensure
+        idempotency of the request. Maximum 64 ASCII characters.
+    security_group_ids
+        (list of strings) – The IDs of the VPC security groups with which to
+        associate instances.
+    security_group_names
+        (list of strings) – The names of the VPC security groups with which to
+        associate instances.
+    additional_info
+        (string) – Specifies additional information to make available to the
+        instance(s).
+    tenancy
+        (string) – The tenancy of the instance you want to launch. An instance
+        with a tenancy of ‘dedicated’ runs on single-tenant hardware and can
+        only be launched into a VPC. Valid values are:”default” or “dedicated”.
+        NOTE: To use dedicated tenancy you MUST specify a VPC subnet-ID as well.
+    instance_profile_arn
+        (string) – The Amazon resource name (ARN) of the IAM Instance Profile
+        (IIP) to associate with the instances.
+    instance_profile_name
+        (string) – The name of the IAM Instance Profile (IIP) to associate with
+        the instances.
+    ebs_optimized
+        (bool) – Whether the instance is optimized for EBS I/O. This
+        optimization provides dedicated throughput to Amazon EBS and a tuned
+        configuration stack to provide optimal EBS I/O performance. This
+        optimization isn’t available with all instance types.
+    network_interfaces
+        (boto.ec2.networkinterface.NetworkInterfaceCollection) – A
+        NetworkInterfaceCollection data structure containing the ENI
+        specifications for the instance.
+    attributes
+        (dict) - Instance attributes and value to be applied to the instance.
+        Available options are:
+            instanceType - A valid instance type (m1.small)
+            kernel - Kernel ID (None)
+            ramdisk - Ramdisk ID (None)
+            userData - Base64 encoded String (None)
+            disableApiTermination - Boolean (true)
+            instanceInitiatedShutdownBehavior - stop|terminate
+            blockDeviceMapping - List of strings - ie: [‘/dev/sda=false’]
+            sourceDestCheck - Boolean (true)
+            groupSet - Set of Security Groups or IDs
+            ebsOptimized - Boolean (false)
+            sriovNetSupport - String - ie: ‘simple’
+    target_state
+        (string) - The desired target state of the instance.  Available options
+        are:
+            running
+            stopped
+    region
+        (string) - Region to connect to.
+    key
+        (string) - Secret key to be used.
+    keyid
+        (string) - Access key to be used.
+    profile
+        (variable) - A dict with region, key and keyid, or a pillar key (string)
+        that contains a dict with region, key and keyid.
+
     .. versionadded:: Boron
     '''
     ret = {'name': name,
@@ -576,8 +700,26 @@ def instance_absent(name, instance_name=None, instance_id=None,
     '''
     Ensure an EC2 instance does not exist (is stopped and removed).
 
+    name
+        (string) - The name of the state definition.
+    instance_name
+        (string) - The name of the instance.
+    instance_id
+        (string) - The ID of the instance.
+    region
+        (string) - Region to connect to.
+    key
+        (string) - Secret key to be used.
+    keyid
+        (string) - Access key to be used.
+    profile
+        (variable) - A dict with region, key and keyid, or a pillar key (string)
+        that contains a dict with region, key and keyid.
+
     .. versionadded:: Boron
     '''
+    ### TODO - Implement 'force' option??  Would automagically turn off
+    ###        'disableApiTermination',  as needed before trying to delete.
     ret = {'name': name,
            'result': True,
            'comment': '',
