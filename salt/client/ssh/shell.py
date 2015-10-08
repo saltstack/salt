2597d13fc871b80e665c905e37dd50e846590ae2
Andreas Lutro <anlutro@gmail.com>
2015-10-08 14:02:25 +0200

    update code that changes log level of salt-ssh shim command
    str.`decode("base64")` has been changed to `base64.b64decode`, but the code
    that checks for `decode("base64")` has not been changed

--- salt/client/ssh/shell.py
+++ salt/client/ssh/shell.py
@@ -292,7 +292,7 @@ class Shell(object):
         logmsg = 'Executing command: {0}'.format(cmd)
         if self.passwd:
             logmsg = logmsg.replace(self.passwd, ('*' * 6))
-        if 'decode("base64")' in logmsg:
+        if 'decode("base64")' in logmsg or 'base64.b64decode(' in logmsg:
             log.debug('Executed SHIM command. Command logged to TRACE')
             log.trace(logmsg)
         else:
