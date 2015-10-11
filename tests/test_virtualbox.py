c07cd63678d89cd5c72180fb590ec985c96ee610
LoveIsGrief <LoveIsGrief@users.noreply.github.com>
2015-10-11 22:11:33 +0200

    Improve tests Test VM creation, destruction and cloning

--- tests/test_virtualbox.py
+++ tests/test_virtualbox.py
@@ -2,7 +2,7 @@
 # being in PYTHONPATH, or installed system-wide
 import unittest
 import logging
-import time
+from tests.helpers import VirtualboxTestCase
 
 import virtualbox
 
@@ -15,26 +15,46 @@ info = log.info
 
 
 class BaseVirtualboxTests(unittest.TestCase):
-
     def test_get_manager(self):
         self.assertIsNotNone(virtualbox.vb_get_manager())
 
 
-class CloneVirtualboxTests(unittest.TestCase):
-
+class CreationDestructionVirtualboxTests(VirtualboxTestCase):
     def setUp(self):
+        super(CreationDestructionVirtualboxTests, self).setUp()
+
+    def test_vm_creation_and_destruction(self):
+        vm_name = "__temp_test_vm__"
+        virtualbox.vb_create_machine(vm_name)
+        self.assertMachineExists(vm_name)
+
+        virtualbox.vb_destroy_machine(vm_name)
+        self.assertMachineDoesNotExist(vm_name)
+
+
+class CloneVirtualboxTests(VirtualboxTestCase):
+    def setUp(self):
+        self.vbox = virtualbox.vb_get_manager()
+
         self.name = "SaltCloudVirtualboxTestVM"
+        virtualbox.vb_create_machine(self.name)
+        self.assertMachineExists(self.name)
 
     def tearDown(self):
-        pass
+        virtualbox.vb_destroy_machine(self.name)
+        self.assertMachineDoesNotExist(self.name)
 
     def test_create_machine(self):
-        return
         vb_name = "NewTestMachine"
         virtualbox.vb_clone_vm(
             name=vb_name,
             clone_from=self.name
         )
+        self.assertMachineExists(vb_name)
+
+        virtualbox.vb_destroy_machine(vb_name)
+        self.assertMachineDoesNotExist(vb_name)
+
 
 if __name__ == '__main__':
     unittest.main()
