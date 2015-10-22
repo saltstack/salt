aafcef8a5b0087111ee7b2c56bc32a35aa1b9756
Andrew Hammond <ahammond@AF6302.local>
2015-10-22 11:49:05 -0700

    remove spurious return

--- salt/states/ipset.py
+++ salt/states/ipset.py
@@ -309,7 +309,6 @@ def absent(name, set_name=None, entry=None, entries=None, family='ipv4', **kwarg
                                       'Attempted entry was {1} for {2}.\n{3}\n').format(
                                           set_name,
                                           _entry, family, command)
-            return ret
     return ret
 
 
