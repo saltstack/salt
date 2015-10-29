f6583358be34aeeb79ccc626fef8399e762bf098
Dmitry Kuzmenko <dmitry.kuzmenko@dsr-company.com>
2015-10-29 18:25:12 +0300

    Don't request creds if auth with key.

--- salt/cli/salt.py
+++ salt/cli/salt.py
@@ -98,7 +98,7 @@ class SaltCMD(parsers.SaltCMDOptionParser):
 
             # If using eauth and a token hasn't already been loaded into
             # kwargs, prompt the user to enter auth credentials
-            if 'token' not in kwargs and self.options.eauth:
+            if 'token' not in kwargs and 'key' not in kwargs and self.options.eauth:
                 resolver = salt.auth.Resolver(self.config)
                 res = resolver.cli(self.options.eauth)
                 if self.options.mktoken and res:
