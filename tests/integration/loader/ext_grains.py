89e7578d214d4fc58482921fb113c14aa13661d5
Christophe Drevet-Droguet <christophe.drevet@gmail.com>
2015-11-01 20:36:48 +0100

    loader test for external grains deep merge (non-functional)
    This test is disabled until we have a way to reload the minion, or
    just the custom grains, with the grains_deep_merge option set.

--- tests/integration/loader/ext_grains.py
+++ tests/integration/loader/ext_grains.py
@@ -11,6 +11,7 @@ from __future__ import absolute_import
 
 # Import Salt Testing libs
 from salttesting.helpers import ensure_in_syspath
+from salttesting.unit import skipIf
 ensure_in_syspath('../')
 
 # Import salt libs
@@ -37,6 +38,29 @@ class LoaderGrainsTest(integration.ModuleCase):
         self.assertEqual({'k2': 'v2'}, grains['a_custom'])
 
 
+@skipIf(True, "needs a way to reload minion after config change")
+class LoaderGrainsMergeTest(integration.ModuleCase):
+    '''
+    Test the loader deep merge behavior with external grains
+    '''
+
+    def setUp(self):
+        self.opts = minion_config(None)
+        self.opts['grains_deep_merge'] = True
+        self.assertTrue(self.opts['grains_deep_merge'])
+        self.opts['disable_modules'] = ['pillar']
+        __grains__ = grains(self.opts)
+
+    def test_grains_merge(self):
+        __grain__ = self.run_function('grains.item', ['a_custom'])
+
+        # Check that the grain is present
+        self.assertIn('a_custom', __grain__)
+        # Check that the grains are merged
+        self.assertEqual({'k1': 'v1', 'k2': 'v2'}, __grain__['a_custom'])
+
+
 if __name__ == '__main__':
     from integration import run_tests
     run_tests(LoaderGrainsTest)
+    run_tests(LoaderGrainsMergeTest)
