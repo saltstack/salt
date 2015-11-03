6ab2ce615cdfb20524debb2b3a0a7caaffff1f03
rallytime <nicole@saltstack.com>
2015-11-03 08:50:41 -0700

    Fix spacing in doc examples for boto_route53 state and module

--- salt/states/boto_route53.py
+++ salt/states/boto_route53.py
@@ -29,46 +29,46 @@ passed in as a dict, or as a string to pull from pillars or minion config:
 .. code-block:: yaml
 
     myprofile:
-        keyid: GKTADJGHEIQSXMKKRBJ08H
-        key: askdjghsdfjkghWupUjasdflkdfklgjsdfjajkghs
-        region: us-east-1
+      keyid: GKTADJGHEIQSXMKKRBJ08H
+      key: askdjghsdfjkghWupUjasdflkdfklgjsdfjajkghs
+      region: us-east-1
 
 .. code-block:: yaml
 
     mycnamerecord:
-        boto_route53.present:
-            - name: test.example.com.
-            - value: my-elb.us-east-1.elb.amazonaws.com.
-            - zone: example.com.
-            - ttl: 60
-            - record_type: CNAME
-            - region: us-east-1
-            - keyid: GKTADJGHEIQSXMKKRBJ08H
-            - key: askdjghsdfjkghWupUjasdflkdfklgjsdfjajkghs
+      boto_route53.present:
+        - name: test.example.com.
+        - value: my-elb.us-east-1.elb.amazonaws.com.
+        - zone: example.com.
+        - ttl: 60
+        - record_type: CNAME
+        - region: us-east-1
+        - keyid: GKTADJGHEIQSXMKKRBJ08H
+        - key: askdjghsdfjkghWupUjasdflkdfklgjsdfjajkghs
 
     # Using a profile from pillars
     myarecord:
-        boto_route53.present:
-            - name: test.example.com.
-            - value: 1.1.1.1
-            - zone: example.com.
-            - ttl: 60
-            - record_type: A
-            - region: us-east-1
-            - profile: myprofile
+      boto_route53.present:
+        - name: test.example.com.
+        - value: 1.1.1.1
+        - zone: example.com.
+        - ttl: 60
+        - record_type: A
+        - region: us-east-1
+        - profile: myprofile
 
     # Passing in a profile
     myarecord:
-        boto_route53.present:
-            - name: test.example.com.
-            - value: 1.1.1.1
-            - zone: example.com.
-            - ttl: 60
-            - record_type: A
-            - region: us-east-1
-            - profile:
-                keyid: GKTADJGHEIQSXMKKRBJ08H
-                key: askdjghsdfjkghWupUjasdflkdfklgjsdfjajkghs
+      boto_route53.present:
+        - name: test.example.com.
+        - value: 1.1.1.1
+        - zone: example.com.
+        - ttl: 60
+        - record_type: A
+        - region: us-east-1
+        - profile:
+            keyid: GKTADJGHEIQSXMKKRBJ08H
+            key: askdjghsdfjkghWupUjasdflkdfklgjsdfjajkghs
 '''
 
 
