3bedbb713bc1b7b5a52dff8b00094221b1c3fb03
Thayne Harbaugh <tharbaug@adobe.com>
2015-11-03 14:10:37 -0700

    Use "test.versions" instead of "-g" (pid in grains changes).
    integration.shell.call.test_issue_15074_output_file_append test just needs any
    output that is repeatable.  Previously it used the grains output ("-g"
    argument).  Unfortunately grains now includes the PID which changes for each
    invocation of "salt-call" and is therefore not predictably comparable.  Use
    "test.versions" that should have constant output across multiple invocations.

--- tests/integration/shell/call.py
+++ tests/integration/shell/call.py
@@ -307,7 +307,7 @@ class CallTest(integration.ShellCase, integration.ShellCaseCommonTestsMixIn):
             # Let's create an initial output file with some data
             ret = self.run_script(
                 'salt-call',
-                '-c {0} --output-file={1} -g'.format(
+                '-c {0} --output-file={1} test.versions'.format(
                     self.get_config_dir(),
                     output_file_append
                 ),
@@ -320,7 +320,7 @@ class CallTest(integration.ShellCase, integration.ShellCaseCommonTestsMixIn):
 
             self.run_script(
                 'salt-call',
-                '-c {0} --output-file={1} --output-file-append -g'.format(
+                '-c {0} --output-file={1} --output-file-append test.versions'.format(
                     self.get_config_dir(),
                     output_file_append
                 ),
