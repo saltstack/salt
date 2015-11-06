21ff6e250f166861c11c502b0e00837d3ce3da3e
Clark Perkins <clark.perkins@digitalreasoning.com>
2015-11-06 12:21:19 -0600

    #28526 fixed yumpkg module

--- salt/modules/yumpkg.py
+++ salt/modules/yumpkg.py
@@ -746,10 +746,11 @@ def check_db(*names, **kwargs):
         __context__['pkg._avail'] = avail
 
     ret = {}
-    repoquery_cmd = repoquery_base + list(names)
-    provides = sorted(
-        set(x.name for x in _repoquery_pkginfo(repoquery_cmd))
-    )
+    if names:
+        repoquery_cmd = repoquery_base + list(names)
+        provides = sorted(
+            set(x.name for x in _repoquery_pkginfo(repoquery_cmd))
+        )
     for name in names:
         ret.setdefault(name, {})['found'] = name in avail
         if not ret[name]['found']:
