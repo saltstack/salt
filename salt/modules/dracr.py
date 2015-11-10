035de968ca33271f5022dda5f2bba9f2a6054925
Aditya Kulkarni <adi@saltstack.com>
2015-11-10 13:06:06 -0500

    Raise error

--- salt/modules/dracr.py
+++ salt/modules/dracr.py
@@ -13,6 +13,7 @@ import os
 import re
 
 # Import Salt libs
+from salt.exceptions import CommandExecutionError
 import salt.utils
 
 # Import 3rd-party libs
@@ -1333,9 +1334,9 @@ def update_firmware(filename,
                                 admin_username=None,
                                 admin_password=None)
     else:
-        ret['return'] = False
-        ret['comment'] = 'Unable to find firmware file {0}'.format(filename)
-        return ret
+        raise CommandExecutionError('Unable to find firmware file {0}'
+                                    .format(filename))
+
 
 def update_firmware_nfs_or_cifs(filename, share,
                                 host=None,
@@ -1378,6 +1379,5 @@ def update_firmware_nfs_or_cifs(filename, share,
                                 admin_username=None,
                                 admin_password=None)
     else:
-        ret['return'] = False
-        ret['comment'] = 'Unable to find firmware file {0}'.format(filename)
-        return ret
+        raise CommandExecutionError('Unable to find firmware file {0}'
+                                    .format(filename))
