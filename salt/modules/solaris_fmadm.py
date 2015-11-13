74ac77b8a81215784e871abef694d0155989e182
Jorge Schrauwen <sjorge@blackdot.be>
2015-11-13 22:35:30 +0000

    implement fmadm.config

--- salt/modules/solaris_fmadm.py
+++ salt/modules/solaris_fmadm.py
@@ -3,7 +3,15 @@
 Module for running fmadm and fmdump on Solaris
 TODO:
  - show (fmdump -u xxx -v)
- - list (-t and -T)
+ - fmadm faulty [-afgiprsv] [-u <uuid>] [-n <max_fault>] - display list of faulty resources
+ - fmadm flush <fmri> ...         - flush cached state for resource
+ - fmadm load <path>              - load specified fault manager module
+ - fmadm repaired <fmri>|label>   - notify fault manager that resource has been repaired
+ - fmadm acquit <fmri> [<uuid>] | label [<uuid>] | <uuid> - acquit resource or acquit case
+ - fmadm replaced <fmri>|label    - notify fault manager that resource has been replaced
+ - fmadm reset [-s serd] <module> - reset module or sub-component
+ - fmadm rotate <logname>         - rotate log file
+ - fmadm unload <module>          - unload specified fault manager module
 '''
 from __future__ import absolute_import
 
@@ -73,15 +81,47 @@ def _parse_fmdump(output):
         for field in header:
             fault[field] = entry[header.index(field)] 
 
-        log.error(fault)
         result.append(fault)
+
+    return result
+
+def _parse_fmadm_config(output):
+    '''
+    Parses fmdump/fmadm output
+    '''
+    result = []
+    output = output.split("\n")
+
+    # extract header
+    header = [field for field in output[0].lower().split(" ") if field]
+    del output[0]
+
+    # parse entries
+    for entry in output:
+        entry = [item for item in entry.split(" ") if item]
+        entry = entry[0:3] + [" ".join(entry[3:])]
+
+        # prepare component
+        component = OrderedDict()
+        for field in header:
+            component[field] = entry[header.index(field)] 
+        
+        result.append(component)
+
+    # keying
+    keyed_result = OrderedDict()
+    for component in result:
+        keyed_result[component['module']] = component
+        del keyed_result[component['module']]['module']
+
+    result = keyed_result
 
     return result
 
 
 def list_records(after=None, before=None):
     '''
-    Return a list of records (fmdump)
+    Display fault management logs
 
     after : string
         filter events after time, see man fmdump for format
@@ -93,7 +133,7 @@ def list_records(after=None, before=None):
 
     .. code-block:: bash
 
-        salt '*' fmadm.list [verbose=True]
+        salt '*' fmadm.list
     '''
     ret = {}
     fmdump = _check_fmdump()
@@ -109,6 +149,31 @@ def list_records(after=None, before=None):
         result['Error'] = 'error executing fmdump'
     else:
         result = _parse_fmdump(res['stdout'])
+
+    return result
+
+def config():
+    '''
+    Display fault manager configuration
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' fmadm.config
+    '''
+    ret = {}
+    fmadm = _check_fmadm()
+    cmd = '{cmd} config'.format(
+        cmd=fmadm
+    )
+    res = __salt__['cmd.run_all'](cmd)
+    retcode = res['retcode']
+    result = {}
+    if retcode != 0:
+        result['Error'] = 'error executing fmdump'
+    else:
+        result = _parse_fmadm_config(res['stdout'])
 
     return result
 
