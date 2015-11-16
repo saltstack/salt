613dd0b7a273d7b7ed784b7fcb7c6f51cabc4947
Aditya Kulkarni <adi@saltstack.com>
2015-11-16 13:01:58 -0500

    Make sure creds are set before ``racadm update``

--- salt/modules/dracr.py
+++ salt/modules/dracr.py
@@ -1328,6 +1328,12 @@ def _update_firmware(cmd,
                      host=None,
                      admin_username=None,
                      admin_password=None):
+
+    if not admin_username:
+        admin_username = __pillar__['proxy']['admin_username']
+    if not admin_username:
+        admin_password = __pillar__['proxy']['admin_password']
+
     ret = __execute_ret(cmd,
                         host=host,
                         admin_username=admin_username,
