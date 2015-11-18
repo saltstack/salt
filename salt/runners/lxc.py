b2678f9e2d7401f011a1ce31745ae8e3f42c1de3
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2015-11-18 13:27:05 +0100

    lxc: remove useless and error prone uses_systemd knob

--- salt/runners/lxc.py
+++ salt/runners/lxc.py
@@ -313,8 +313,6 @@ def init(names, host=None, saltcloud_mode=False, quiet=False, **kwargs):
             kw = copy.deepcopy(kw)
             kw['name'] = name
             saved_kwargs = {}
-            if 'uses_systemd' in kw:
-                saved_kwargs['uses_systemd'] = kw['uses_systemd']
             kw = client.cmd(
                 host, 'lxc.cloud_init_interface', args + [kw],
                 expr_form='list', timeout=600).get(host, {})
