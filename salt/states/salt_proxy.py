1f6ec23e92d8fe69b5c8c78e81874bb98948f6b3
Aditya Kulkarni <adi@saltstack.com>
2015-11-18 14:35:56 -0500

    Add state function

--- salt/states/salt_proxy.py
+++ salt/states/salt_proxy.py
@@ -1,5 +1,24 @@
 # -*- coding: utf-8 -*-
+'''
+    State to deploy and run salt-proxy processes
+    on a minion.
 
+    Set up pillar data for your proxies per the documentation.
+
+    Run the state as below
+
+    ..code-block:: yaml
+
+        salt-proxy-configure:
+            salt_proxy.configure_proxy:
+                - proxyname: p8000
+                - start: True
+
+    This state will configure the salt proxy settings
+    within /etc/salt/proxy (if /etc/salt/proxy doesn't exists)
+    and start the salt-proxy process (default true),
+    if it isn't already running.
+'''
 import os
 import logging
 
@@ -24,42 +43,64 @@ def _write_proxy_conf(proxyfile):
 
 
 def _proxy_conf_file(proxyfile):
-    changes = []
+    changes_old = []
+    changes_new = []
     success = True
     if not os.path.exists(proxyfile):
         try:
-            changes.append(_write_proxy_conf(proxyfile))
+            changes_new.append(_write_proxy_conf(proxyfile))
             msg = 'Salt Proxy: Wrote proxy conf {0}'.format(proxyfile)
         except (OSError, IOError) as err:
             success = False
             msg = 'Salt Proxy: Error writing proxy file {0}'.format(str(err))
             log.error(msg)
-            changes.append(msg)
-        changes.append(msg)
+            changes_new.append(msg)
+        changes_new.append(msg)
         log.debug(msg)
     else:
         msg = 'Salt Proxy: {0} already exists, skipping'.format(proxyfile)
-        changes.append(msg)
+        changes_old.append(msg)
         log.debug(msg)
-    return success, changes
+    return success, changes_new, changes_old
 
 
 def configure_proxy(proxyname='p8000', start=True, **kwargs):
-    changes = []
+    '''
+    Create the salt proxy file and start the proxy process
+    if required
+    '''
+    changes_new = []
+    changes_old = []
     status = True
 
     # write the proxy file if necessary
     proxyfile = '/etc/salt/proxy'
-    status, msg = _proxy_conf_file(proxyfile)
-    changes.extend(msg)
+    status, msg_new, msg_old = _proxy_conf_file(proxyfile)
+    changes_new.extend(msg_new)
+    changes_old.extend(msg_old)
     # start the proxy process
     if start:
-        __salt__['cmd.run'](
-            'salt-proxy --proxyid={0} -l info -d'.format(proxyname),
-            timeout=5)
-        msg = 'Started salt proxy for {0}'.format(proxyname)
-        changes.append(msg)
-        log.info(msg)
+        cmdout = __salt__['cmd.run_all'](
+            'ps ax | grep "salt-proxy --proxyid={0}"  | grep -v grep'
+            .format(proxyname),
+            timeout=5, python_shell=True)
+        if not cmdout['stdout']:
+            __salt__['cmd.run'](
+                'salt-proxy --proxyid={0} -l info -d'.format(proxyname),
+                timeout=5)
+            changes_new.append('Started salt proxy for {0}'.format(proxyname))
+            log.debug('Proxy started')
+        else:
+            changes_old.append('Salt proxy already running for {0}'
+                               .format(proxyname))
+            log.debug('Proxy already running')
 
-    return {'result': status, 'changes': '\n'.join(changes), 'name': proxyname,
-            'comment': 'Proxy config messages'}
+    return {
+        'result': status,
+        'changes': {
+            'old': '\n'.join(changes_old),
+            'new': '\n'.join(changes_new),
+        },
+        'name': proxyname,
+        'comment': 'Proxy config messages'
+    }
