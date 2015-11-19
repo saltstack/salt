64e0f198baa543943683f5630833b419f1143730
twangboy <slee@saltstack.com>
2015-11-19 16:07:58 -0700

    More updates to dsc module
    - Added ability to cache config from salt master
    - Added get_lcm_config function

--- salt/modules/dsc.py
+++ salt/modules/dsc.py
@@ -200,22 +200,54 @@ def remove(name):
     return name not in list_modules()
 
 
-def run_config(path):
+def run_config(path, source=None, saltenv='base'):
     '''
     Run an existing DSC configuration
 
     :param str path: Path to the directory that contains the .mof configuration
-    file
+    file to apply. Required.
+
+    :param str source: Path to the directory that contains the .mof file on the
+    ``file_roots``. The directory source and path directories must be the same.
+    The source directory will be copied to the path directory and then executed.
+    If source is not passed, the config located at 'path' will be applied.
+    Optional.
+
+    :param str saltenv: The salt environment to use when copying your source.
+    Default is 'base'
 
     :return: True if successful, otherwise False
     :rtype: bool
 
     CLI Example:
 
+    To apply a config that already exists on the the system
+
     .. code-block:: bash
 
-        salt '*' dsc.run_config C:\\DSC\\WebSiteConfiguration
+        salt '*' dsc.run_config C:\DSC\WebSiteConfiguration
+
+    To cache a configuration for the master and apply it:
+
+    .. code-block:: bash
+
+        salt '*' dsc.run_config C:\DSC\WebSiteConfiguration salt://dsc/configs/WebSiteConfiguration
     '''
+    if source:
+        # Make sure the folder names match
+        pathname = os.path.basename(os.path.normpath(path))
+        sourcename = os.path.basename(os.path.normpath(source))
+        if pathname.lower() != sourcename.lower():
+            raise CommandExecutionError('Path and Source folder names don\'t '
+                                        'match')
+
+        # Destination path minus the basename
+        dest_path = os.path.dirname(os.path.normpath(path))
+        log.info('Caching {0}'.format(source))
+        cached_files = __salt__['cp.get_dir'](source, dest_path, saltenv)
+        if not cached_files:
+            raise CommandExecutionError('Failed to copy {0}'.format(source))
+
     # Make sure the path exists
     if not os.path.exists(path):
         raise CommandExecutionError('Must pass a valid path')
@@ -253,6 +285,25 @@ def test_config():
         return False
 
 
+def get_lcm_config():
+    '''
+    Get the current Local Configuration Manager settings
+
+    :return: A dictionary representing the Local Configuration Manager settings
+     on the machine
+    :rtype: dict
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' dsc.get_lcm_config
+    '''
+    cmd = 'Get-DscLocalConfigurationManager | ' \
+          'Select-Object * -ExcludeProperty Cim*'
+    return _pshell(cmd)
+
+
 def get_config():
     '''
     Get the current DSC Configuration
@@ -269,6 +320,21 @@ def get_config():
     cmd = 'Get-DscConfiguration | ' \
           'Select-Object * -ExcludeProperty Cim*'
     return _pshell(cmd)
+
+
+def set_config_status():
+    cmd = 'Configuration SaltConfig {'
+    cmd += '    Node "SAVDALHV01 {'
+    cmd += '        LocalConfigurationManager {'
+    cmd += '            ConfigurationMode = "ApplyAndAutoCorrect"'
+    cmd += '            ConfigurationModeFrequencyMins = 30 #must be a multiple of the RefreshFrequency and how often configuration is checked'
+    cmd += '            RefreshFrequencyMins = 15  #15 is minimum and this is how often configuration is pulled from pull server'
+    cmd += '        }}};'
+    cmd += 'SaltConfig -OutputPath "C:\DSC\SaltConfig"'
+    # Execute
+
+    # Apply
+    Set-DscLocalConfigurationManager -Path "C:\Users\administrator\dsc" -Verbose
 
 
 def get_config_status():
