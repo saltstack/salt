403451471337873c24ae780f1ab77136c3c6f94c
twangboy <slee@saltstack.com>
2015-11-20 17:44:22 -0700

    Added set_lcm_config function

--- salt/modules/dsc.py
+++ salt/modules/dsc.py
@@ -17,7 +17,7 @@ import os
 
 # Import salt libs
 import salt.utils
-from salt.exceptions import CommandExecutionError
+from salt.exceptions import CommandExecutionError, SaltInvocationError
 
 # Set up logging
 log = logging.getLogger(__name__)
@@ -238,19 +238,24 @@ def run_config(path, source=None, saltenv='base'):
         pathname = os.path.basename(os.path.normpath(path))
         sourcename = os.path.basename(os.path.normpath(source))
         if pathname.lower() != sourcename.lower():
-            raise CommandExecutionError('Path and Source folder names don\'t '
-                                        'match')
+            error = 'Path and Source folder names must match.'
+            log.error(error)
+            raise CommandExecutionError(error)
 
         # Destination path minus the basename
         dest_path = os.path.dirname(os.path.normpath(path))
         log.info('Caching {0}'.format(source))
         cached_files = __salt__['cp.get_dir'](source, dest_path, saltenv)
         if not cached_files:
-            raise CommandExecutionError('Failed to copy {0}'.format(source))
+            error = 'Failed to copy {0}'.format(source)
+            log.error(error)
+            raise CommandExecutionError(error)
 
     # Make sure the path exists
     if not os.path.exists(path):
-        raise CommandExecutionError('Must pass a valid path')
+        error = '"{0} not found.'
+        log.error(error)
+        raise CommandExecutionError(error)
 
     # Run the DSC Configuration
     # Putting quotes around the parameter protects against command injection
@@ -259,9 +264,29 @@ def run_config(path, source=None, saltenv='base'):
            'return $job.State'
     ret = _pshell(cmd)
     if ret == 'Completed':
+        log.info('DSC Run Config: {0}'.format(ret))
         return True
     else:
+        log.info('DSC Run Config: {0}'.format(ret))
         return False
+
+
+def get_config():
+    '''
+    Get the current DSC Configuration
+
+    :return: A dictionary representing the DSC Configuration on the machine
+    :rtype: dict
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' dsc.get_config
+    '''
+    cmd = 'Get-DscConfiguration | ' \
+          'Select-Object -Property -ExcludeProperty Cim*'
+    return _pshell(cmd)
 
 
 def test_config():
@@ -285,58 +310,6 @@ def test_config():
         return False
 
 
-def get_lcm_config():
-    '''
-    Get the current Local Configuration Manager settings
-
-    :return: A dictionary representing the Local Configuration Manager settings
-     on the machine
-    :rtype: dict
-
-    CLI Example:
-
-    .. code-block:: bash
-
-        salt '*' dsc.get_lcm_config
-    '''
-    cmd = 'Get-DscLocalConfigurationManager | ' \
-          'Select-Object * -ExcludeProperty Cim*'
-    return _pshell(cmd)
-
-
-def get_config():
-    '''
-    Get the current DSC Configuration
-
-    :return: A dictionary representing the DSC Configuration on the machine
-    :rtype: dict
-
-    CLI Example:
-
-    .. code-block:: bash
-
-        salt '*' dsc.get_config
-    '''
-    cmd = 'Get-DscConfiguration | ' \
-          'Select-Object * -ExcludeProperty Cim*'
-    return _pshell(cmd)
-
-
-def set_config_status():
-    cmd = 'Configuration SaltConfig {'
-    cmd += '    Node "SAVDALHV01 {'
-    cmd += '        LocalConfigurationManager {'
-    cmd += '            ConfigurationMode = "ApplyAndAutoCorrect"'
-    cmd += '            ConfigurationModeFrequencyMins = 30 #must be a multiple of the RefreshFrequency and how often configuration is checked'
-    cmd += '            RefreshFrequencyMins = 15  #15 is minimum and this is how often configuration is pulled from pull server'
-    cmd += '        }}};'
-    cmd += 'SaltConfig -OutputPath "C:\DSC\SaltConfig"'
-    # Execute
-
-    # Apply
-    Set-DscLocalConfigurationManager -Path "C:\Users\administrator\dsc" -Verbose
-
-
 def get_config_status():
     '''
     Get the status of the current DSC Configuration
@@ -356,3 +329,166 @@ def get_config_status():
           '@{Name="StartDate";Expression={Get-Date ($_.StartDate) -Format g}}, ' \
           'Type, Mode, RebootRequested, NumberofResources'
     return _pshell(cmd)
+
+
+def get_lcm_config():
+    '''
+    Get the current Local Configuration Manager settings
+
+    :return: A dictionary representing the Local Configuration Manager settings
+     on the machine
+    :rtype: dict
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' dsc.get_lcm_config
+    '''
+    cmd = 'Get-DscLocalConfigurationManager | ' \
+          'Select-Object -Property ConfigurationModeFrequencyMins, LCMState, ' \
+          'RebootNodeIfNeeded, ConfigurationMode, ActionAfterReboot, ' \
+          'RefreshMode, CertificateID, ConfigurationID, RefreshFrequencyMins, ' \
+          'AllowModuleOverwrite, DebugMode, StatusRetentionTimeInDays '
+    return _pshell(cmd)
+
+
+def set_lcm_config(config_mode=None,
+                   config_mode_freq=None,
+                   refresh_freq=None,
+                   reboot_if_needed=None,
+                   action_after_reboot=None,
+                   refresh_mode=None,
+                   certificate_id=None,
+                   configuration_id=None,
+                   allow_module_overwrite=None,
+                   debug_mode=None,
+                   status_retention_days=None):
+    '''
+
+    For detailed descriptions of the parameters see:
+    https://msdn.microsoft.com/en-us/PowerShell/DSC/metaConfig
+
+    :param str config_mode: How the LCM applies the configuration. Valid values
+    are:
+    - ApplyOnly
+    - ApplyAndMonitor
+    - ApplyAndAutoCorrect
+
+    :param int config_mode_freq: How often, in minutes, the current
+    configuration is checked and applied. Ignored if config_mode is set to
+    ApplyOnly. Default is 15.
+
+    :param str refresh_mode: How the LCM gets configurations. Valid values are:
+    - Disabled
+    - Push
+    - Pull
+
+    :param int refresh_freq: How often, in minutes, the LCM checks for updated
+    configurations. (pull mode only) Default is 30.
+
+    .. note:: Either `config_mode_freq` or `refresh_freq` needs to be a multiple
+    of the other. See documentation on MSDN for more details.
+
+    :param bool reboot_if_needed: Reboot the machine if needed after a
+    configuration is applied. Default is False.
+
+    :param str action_after_reboot: Action to take after reboot. Valid values
+    are:
+    - ContinueConfiguration
+    - StopConfiguration
+
+    :param guid certificate_id: A GUID that specifies a certificate used to
+    access the configuration: (pull mode)
+
+    :param guid configuration_id: A GUID that identifies the config file to get
+    from a pull server. (pull mode)
+
+    :param bool allow_module_overwrite: New configs are allowed to overwrite old
+    ones on the target node.
+
+    :param str debug_mode: Sets the debug level. Valid values are:
+    - None
+    - ForceModuleImport
+    - All
+
+    :param int status_retention_days: Number of days to keep status of the
+    current config.
+
+    Returns:
+
+    '''
+    cmd = 'Configuration SaltConfig {'
+    cmd += '    Node localhost {'
+    cmd += '        LocalConfigurationManager {'
+    if config_mode:
+        if config_mode not in ('ApplyOnly', 'ApplyAndMonitor', 'ApplyAndAutoCorrect'):
+            SaltInvocationError('config_mode must be one of ApplyOnly, '
+                                'ApplyAndMonitor, or ApplyAndAutoCorrect')
+        cmd += '            ConfigurationMode = "{0}"'.format(config_mode)
+    if config_mode_freq:
+        if isinstance(config_mode_freq, int):
+            SaltInvocationError('config_mode_freq must be an integer')
+        cmd += '            ConfigurationModeFrequencyMins = {0}'.format(config_mode_freq)
+    if refresh_mode:
+        if refresh_mode not in ('Disabled', 'Push', 'Pull'):
+            SaltInvocationError('refresh_mode must be one of Disabled, Push, '
+                                'or Pull')
+        cmd += '            RefreshMode = "{0}"'.format(refresh_mode)
+    if refresh_freq:
+        if isinstance(refresh_freq, int):
+            SaltInvocationError('refresh_freq must be an integer')
+        cmd += '            RefreshFrequencyMins = {0}'.format(refresh_freq)
+    if reboot_if_needed is not None:
+        if not isinstance(reboot_if_needed, bool):
+            SaltInvocationError('reboot_if_needed must be a boolean value')
+        if reboot_if_needed:
+            reboot_if_needed = '$true'
+        else:
+            reboot_if_needed = '$false'
+        cmd += '            RebootNodeIfNeeded = {0}'.format(reboot_if_needed)
+    if action_after_reboot:
+        if action_after_reboot not in ('ContinueConfiguration, StopConfiguration'):
+            SaltInvocationError('action_after_reboot must be one of '
+                                'ContinueConfiguration or StopConfiguration')
+        cmd += '            ActionAfterReboot = "{0}"'.format(action_after_reboot)
+    if certificate_id is not None:
+        if certificate_id == '':
+            certificate_id = None
+        cmd += '            CertificateID = "{0}"'.format(certificate_id)
+    if configuration_id is not None:
+        if configuration_id == '':
+            configuration_id = None
+        cmd += '            ConfigurationID = "{0}"'.format(configuration_id)
+    if allow_module_overwrite is not None:
+        if not isinstance(allow_module_overwrite, bool):
+            SaltInvocationError('allow_module_overwrite must be a boolean value')
+        if allow_module_overwrite:
+            allow_module_overwrite = '$true'
+        else:
+            allow_module_overwrite = '$false'
+        cmd += '            AllowModuleOverwrite = {0}'.format(allow_module_overwrite)
+    if debug_mode:
+        if debug_mode not in ('None', 'ForceModuleImport', 'All'):
+            SaltInvocationError('debug_mode must be one of None, ForceModuleImport, '
+                                'ResourceScriptBreakAll, or All')
+        cmd += '            DebugMode = "{0}"'.format(debug_mode)
+    if status_retention_days:
+        if isinstance(status_retention_days, int):
+            SaltInvocationError('status_retention_days must be an integer')
+        cmd += '            StatusRetentionTimeInDays = {0}'.format(status_retention_days)
+    cmd += '        }}};'
+    cmd += 'SaltConfig -OutputPath "C:\DSC\SaltConfig"'
+
+    # Execute Config to create the .mof
+    __salt__['cmd.shell'](cmd, shell='powershell')
+
+    # Apply the config
+    cmd = 'Set-DscLocalConfigurationManager -Path "C:\DSC\SaltConfig"'
+    ret = _pshell(cmd)
+    if not ret:
+        log.info('LCM config applied successfully')
+        return True
+    else:
+        log.error('Failed to apply LCM config. Error {0}'.format(ret))
+        return False
