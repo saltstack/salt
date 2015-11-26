1b304263687a142fbea783a02bbfa6b55ba36597
Dmitry Kuzmenko <dmitry.kuzmenko@dsr-company.com>
2015-11-26 19:03:47 +0300

    Implemented batch wait timer

--- salt/cli/batch.py
+++ salt/cli/batch.py
@@ -8,6 +8,7 @@ from __future__ import absolute_import, print_function
 import math
 import time
 import copy
+from datetime import datetime, timedelta
 
 # Import salt libs
 import salt.client
@@ -80,6 +81,14 @@ class Batch(object):
                 print_cli('Invalid batch data sent: {0}\nData must be in the '
                           'form of %10, 10% or 3'.format(self.opts['batch']))
 
+    def __update_wait(self, wait):
+        now = datetime.now()
+        i = 0
+        while i < len(wait) and wait[i] <= now:
+            i += 1
+        if i:
+            del wait[:i]
+
     def run(self):
         '''
         Execute the batch run
@@ -95,6 +104,9 @@ class Batch(object):
         active = []
         ret = {}
         iters = []
+        # wait the specified time before decide a job is actually done
+        bwait = self.opts.get('batch_wait', 0)
+        wait = []
 
         # the minion tracker keeps track of responses and iterators
         # - it removes finished iterators from iters[]
@@ -107,12 +119,14 @@ class Batch(object):
         # Iterate while we still have things to execute
         while len(ret) < len(self.minions):
             next_ = []
-            if len(to_run) <= bnum and not active:
+            if bwait and wait:
+                self.__update_wait(wait)
+            if len(to_run) <= bnum - len(wait) and not active:
                 # last bit of them, add them all to next iterator
                 while to_run:
                     next_.append(to_run.pop())
             else:
-                for i in range(bnum - len(active)):
+                for i in range(bnum - len(active) - len(wait)):
                     if to_run:
                         minion_id = to_run.pop()
                         if isinstance(minion_id, dict):
@@ -190,6 +204,8 @@ class Batch(object):
             for minion, data in six.iteritems(parts):
                 if minion in active:
                     active.remove(minion)
+                    if bwait:
+                        wait.append(datetime.now() + timedelta(seconds=bwait))
                 if self.opts.get('raw'):
                     yield data
                 else:
@@ -216,3 +232,5 @@ class Batch(object):
                     for minion in minion_tracker[queue]['minions']:
                         if minion in active:
                             active.remove(minion)
+                            if bwait:
+                                wait.append(datetime.now() + timedelta(seconds=bwait))
