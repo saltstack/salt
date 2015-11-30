2690e3c8f329d77c4b3aa7589adf4848281c4525
Daniel Hobley <danielh@unity3d.com>
2015-11-30 20:19:13 +0100

    Replaced double quotes with single

--- salt/beacons/adb.py
+++ salt/beacons/adb.py
@@ -34,13 +34,13 @@ def validate(config):
     if not isinstance(config, dict):
         log.info('Configuration for adb beacon must be a dict.')
         return False
-    elif "states" not in config.keys():
+    elif 'states' not in config.keys():
         log.info('Configuration for adb beacon must include a states array.')
         return False
     else:
         states = ['offline', 'bootloader', 'device', 'host', 'recovery', 'no permissions',
                   'sideload', 'unauthorized', 'unknown', 'missing']
-        if any(s not in states for s in config["states"]):
+        if any(s not in states for s in config['states']):
             log.info('Need a one of the following adb states: {0}'.format(', '.join(states)))
             return False
     return True
@@ -74,39 +74,39 @@ def beacon(config):
     if not validate(config):
         return ret
 
-    out = __salt__['cmd.run']('adb devices', runas=config.get("user", None))
+    out = __salt__['cmd.run']('adb devices', runas=config.get('user', None))
 
-    lines = out.split("\n")[1:]
+    lines = out.split('\n')[1:]
     last_state_devices = last_state.keys()
     found_devices = []
 
     for line in lines:
         try:
-            device, state = line.split("\t")
+            device, state = line.split('\t')
             found_devices.append(device)
             if device not in last_state_devices or \
-                    ("state" in last_state[device] and last_state[device]["state"] != state):
-                if state in config["states"]:
-                    ret.append({"device": device, "state": state, 'tag': state})
-                    last_state[device] = {"state": state}
+                    ('state' in last_state[device] and last_state[device]['state'] != state):
+                if state in config['states']:
+                    ret.append({'device': device, 'state': state, 'tag': state})
+                    last_state[device] = {'state': state}
 
-            if "battery_low" in config:
+            if 'battery_low' in config:
                 val = last_state.get(device, {})
-                cmd = "adb -s {0} shell cat /sys/class/power_supply/*/capacity".format(device)
-                battery_levels = __salt__['cmd.run'](cmd, runas=config.get("user", None)).split("\n")
+                cmd = 'adb -s {0} shell cat /sys/class/power_supply/*/capacity'.format(device)
+                battery_levels = __salt__['cmd.run'](cmd, runas=config.get('user', None)).split('\n')
 
                 for l in battery_levels:
                     battery_level = int(l)
                     if 0 < battery_level < 100:
-                        if "battery" not in val or battery_level != val["battery"]:
-                            if ("battery" not in val or val["battery"] > config["battery_low"]) and \
-                                            battery_level <= config["battery_low"]:
-                                ret.append({"device": device, "battery_level": battery_level, "tag": "battery_low"})
+                        if 'battery' not in val or battery_level != val['battery']:
+                            if ('battery' not in val or val['battery'] > config['battery_low']) and \
+                                            battery_level <= config['battery_low']:
+                                ret.append({'device': device, 'battery_level': battery_level, 'tag': 'battery_low'})
 
                         if device not in last_state:
                             last_state[device] = {}
 
-                        last_state[device].update({"battery": battery_level})
+                        last_state[device].update({'battery': battery_level})
 
         except ValueError:
             continue
@@ -114,15 +114,15 @@ def beacon(config):
     # Find missing devices and remove them / send an event
     for device in last_state_devices:
         if device not in found_devices:
-            if "missing" in config["states"]:
-                ret.append({"device": device, "state": "missing", "tag": "missing"})
+            if 'missing' in config['states']:
+                ret.append({'device': device, 'state': 'missing', 'tag': 'missing'})
 
             del last_state[device]
 
     # Maybe send an event if we don't have any devices
-    if "no_devices_event" in config and config["no_devices_event"] is True:
+    if 'no_devices_event' in config and config['no_devices_event'] is True:
         if len(lines) == 0 and not last_state_no_devices:
-            ret.append({"tag": "no_devices"})
+            ret.append({'tag': 'no_devices'})
 
     # Did we have no devices listed this time around?
     last_state_no_devices = len(lines) == 0
