27219b16b73df938ee2959ea20675dc2fd99081b
Kris Raney <kraney@ixiacom.com>
2015-11-30 16:19:40 -0600

    States present and absent working.

--- salt/states/boto_lambda.py
+++ salt/states/boto_lambda.py
@@ -61,6 +61,9 @@ config:
 # Import Python Libs
 from __future__ import absolute_import
 import logging
+import os
+import os.path
+import hashlib
 
 # Import Salt Libs
 import salt.utils.dictupdate as dictupdate
@@ -75,14 +78,19 @@ def __virtual__():
     return 'boto_lambda' if 'boto_lambda.exists' in __salt__ else False
 
 
-def present(name, runtime, handler, code, role_name=None, role_id=None, 
-            description=None, timeout=None, memorysize=None, publish=False,
+def present(name, runtime, role, handler, zipfile=None, s3bucket=None,
+            s3key=None, s3objectversion=None, 
+            description='', timeout=3, memorysize=128, publish=False,
             region=None, key=None, keyid=None, profile=None):
     '''
     Ensure Lambda Function exists.
 
     name
         Name of the Lambda Function.
+
+    role
+        The name or ARN of the IAM role that Lambda assumes when it executes your
+        function to access any other AWS resources.
 
     runtime
         The runtime environment for the Lambda function. One of 
@@ -93,20 +101,23 @@ def present(name, runtime, handler, code, role_name=None, role_id=None,
         module-name.*export* value in your function. For Java, it can be package.classname::handler or
         package.class-name.
 
-    code
-        The code for the lambda function.
-        * zipfile - A .zip file containing your deployment package.
-        * s3bucket - Amazon S3 bucket name where the .zip file containing your package is stored.
-        * s3key - The Amazon S3 object (the deployment package) key name you want to upload.
-        * s3objectversion - The Amazon S3 object (the deployment package) version you want to upload.
+    zipfile
+        A path to a .zip file containing your deployment package. If this is
+        specified, s3bucket and s3key must not be specified.
 
-    role_name
-        The name of the IAM role that Lambda assumes when it executes your
-        function to access any other AWS resources. One of role_name or role_id must be specified.
+    s3bucket
+        Amazon S3 bucket name where the .zip file containing your package is
+        stored. If this is specified, s3key must be specified and zipfile must
+        NOT be specified.
 
-    role_id
-        The Amazon Resource Name (ARN) of the IAM role that Lambda assumes when it executes your
-        function to access any other AWS resources. One of role_name or role_id must be specified.
+    s3key
+        The Amazon S3 object (the deployment package) key name you want to
+        upload. If this is specified, s3key must be specified and zipfile must
+        NOT be specified.
+
+    s3objectversion
+        The version of S3 object to use. Optional, should only be specified if
+        s3bucket and s3key are specified.
 
     description
         A short, user-defined function description. Lambda does not use this value. Assign a meaningful
@@ -160,23 +171,112 @@ def present(name, runtime, handler, code, role_name=None, role_id=None,
             ret['comment'] = 'Lambda function {0} is set to be created.'.format(name)
             ret['result'] = None
             return ret
-        r = __salt__['boto_lambda.create'](name, runtime, handler, code, role_name=None, role_id=None, 
-            description=None, timeout=None, memorysize=None, publish=False,
-            region=None, key=None, keyid=None, profile=Nonecidr_block, instance_tenancy,
-                                        name, dns_support, dns_hostnames,
-                                        tags, region, key, keyid, profile)
+        r = __salt__['boto_lambda.create'](name, runtime, role, handler, 
+            zipfile, s3bucket, s3key, s3objectversion,
+            description, timeout, memorysize, publish,
+            region, key, keyid, profile)
         if not r.get('created'):
             ret['result'] = False
             ret['comment'] = 'Failed to create Lamda function: {0}.'.format(r['error']['message'])
             return ret
-        _describe = __salt__['boto_lambda.describe'](r['id'], region=region, key=key,
+        _describe = __salt__['boto_lambda.describe'](name, region=region, key=key,
                                                   keyid=keyid, profile=profile)
         ret['changes']['old'] = {'lambda': None}
         ret['changes']['new'] = _describe
         ret['comment'] = 'Lambda function {0} created.'.format(name)
         return ret
-    ret['comment'] = 'Lamda function present.'
+
+    ret['comment'] = os.linesep.join([ret['comment'], 'Lambda function {0} is present.'.format(name)])
+    ret['changes'] = {}
+    # Lambda function exists, ensure config matches
+    _ret = _lambda_config_present(name, role, handler, description, timeout,
+                                  memorysize, region, key, keyid, profile)
+    ret['changes'] = dictupdate.update(ret['changes'], _ret['changes'])
+    ret['comment'] = ' '.join([ret['comment'], _ret['comment']])
+    _ret = _lambda_code_present(name, zipfile, s3bucket, s3key, s3objectversion,
+                                 publish, region, key, keyid, profile)
+    ret['changes'] = dictupdate.update(ret['changes'], _ret['changes'])
+    ret['comment'] = ' '.join([ret['comment'], _ret['comment']])
     return ret
+
+
+def _get_role_arn(name, region=None, key=None, keyid=None, profile=None):
+    if name.startswith('arn:aws:iam:'):
+        return name
+
+    account_id = __salt__['boto_iam.get_account_id'](
+        region=region, key=key, keyid=keyid, profile=profile
+    )
+    return 'arn:aws:iam::{0}:role/{1}'.format(account_id, name)
+
+
+def _lambda_config_present(name, role, handler, description, timeout,
+                           memorysize, region, key, keyid, profile):
+    ret = {'result': True, 'comment': '', 'changes': {}}
+    lmbda = __salt__['boto_lambda.describe'](name, 
+           region=region, key=key, keyid=keyid, profile=profile)['lambda']
+    role_arn = _get_role_arn(role, region, key, keyid, profile)
+    need_update = False
+    for val, var in {
+        'Role': 'role_arn',
+        'Handler': 'handler',
+        'Description': 'description',
+        'Timeout': 'timeout',
+        'MemorySize': 'memorysize',
+    }.iteritems():
+        if lmbda[val] != locals()[var]:
+            need_update = True
+            ret['changes'].setdefault('new',{})[var] = locals()[var]
+            ret['changes'].setdefault('old',{})[var] = lmbda[val]
+    if need_update:
+        ret['comment'] = os.linesep.join([ret['comment'], 'Lambda function config to be modified'])
+        _r = __salt__['boto_lambda.update_config'](name, role, handler, description,
+                                        timeout, memorysize, region=region, key=key,
+                                        keyid=keyid, profile=profile)
+    return ret
+
+
+def _lambda_code_present(name, zipfile, s3bucket, s3key, s3objectversion,
+                         publish, region, key, keyid, profile):
+    ret = {'result': True, 'comment': '', 'changes': {}}
+    lmbda = __salt__['boto_lambda.describe'](name, 
+           region=region, key=key, keyid=keyid, profile=profile)['lambda']
+    update = False
+    if zipfile:
+        size = os.path.getsize(zipfile)
+        if size == lmbda['CodeSize']:
+            sha = hashlib.sha256()
+            with open(zipfile, 'rb') as f:
+                sha.update(f.read())
+            hashed = sha.digest().encode('base64').strip()
+            if hashed != lmbda['CodeSha256']:
+                update = True
+        else:
+            update=True
+    else:
+       # No way to judge whether the item in the s3 bucket is current without
+       # downloading it. Cheaper to just request an update every time, and still
+       # idempotent
+       update = True
+    if update:
+        ret['changes']['old'] = {
+            'CodeSha256': lmbda['CodeSha256'],
+            'CodeSize': lmbda['CodeSize'],
+        }
+        lmbda = __salt__['boto_lambda.update_code'](name, zipfile, s3bucket,
+            s3key, s3objectversion, publish,
+            region=region, key=key, keyid=keyid, profile=profile)['lambda']
+        if lmbda['CodeSha256'] != ret['changes']['old']['CodeSha256'] or \
+                lmbda['CodeSize'] != ret['changes']['old']['CodeSize']:
+            ret['comment'] = os.linesep.join([ret['comment'], 'Lambda function code to be modified'])
+            ret['changes']['new'] = {
+                'CodeSha256': lmbda['CodeSha256'],
+                'CodeSize': lmbda['CodeSize'],
+            }
+        else:
+			del(ret['changes']['old'])
+    return ret
+
 
 
 def absent(name, region=None, key=None, keyid=None, profile=None):
@@ -206,15 +306,14 @@ def absent(name, region=None, key=None, keyid=None, profile=None):
            'changes': {}
            }
 
-    r = __salt__['boto_lambda.get_id'](name=name, region=region,
+    r = __salt__['boto_lambda.exists'](name, region=region,
                                     key=key, keyid=keyid, profile=profile)
     if 'error' in r:
         ret['result'] = False
         ret['comment'] = 'Failed to delete Lambda function: {0}.'.format(r['error']['message'])
         return ret
 
-    _id = r.get('id')
-    if not _id:
+    if not r:
         ret['comment'] = 'Lambda function {0} does not exist.'.format(name)
         return ret
 
@@ -222,14 +321,14 @@ def absent(name, region=None, key=None, keyid=None, profile=None):
         ret['comment'] = 'Lambda function {0} is set to be removed.'.format(name)
         ret['result'] = None
         return ret
-    r = __salt__['boto_lambda.delete'](name=name, 
+    r = __salt__['boto_lambda.delete'](name,
                                     region=region, key=key,
                                     keyid=keyid, profile=profile)
     if not r['deleted']:
         ret['result'] = False
         ret['comment'] = 'Failed to delete Lambda function: {0}.'.format(r['error']['message'])
         return ret
-    ret['changes']['old'] = {'lambda': _id}
+    ret['changes']['old'] = {'lambda': name}
     ret['changes']['new'] = {'lambda': None}
     ret['comment'] = 'Lambda function {0} deleted.'.format(name)
     return ret
