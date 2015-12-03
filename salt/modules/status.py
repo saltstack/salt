48e7beb0eb7c69fe4184f5df0d8fbbd1d0da1ac6
Justin Findlay <jfindlay@gmail.com>
2015-12-03 10:57:17 -0700

    modules.status: add in_seconds option to uptime
    Implements #29311.

--- salt/modules/status.py
+++ salt/modules/status.py
@@ -121,9 +121,15 @@ def custom():
     return ret
 
 
-def uptime():
+def uptime(human_readable=True):
     '''
     Return the uptime for this minion
+
+    human_readable: True
+        If ``True`` return the output provided by the system.  If ``False``
+        return the output in seconds.
+
+        .. versionadded:: 2015.8.4
 
     CLI Example:
 
@@ -131,7 +137,16 @@ def uptime():
 
         salt '*' status.uptime
     '''
-    return __salt__['cmd.run']('uptime')
+    if human_readable:
+        return __salt__['cmd.run']('uptime')
+    else:
+        if os.path.exists('/proc/uptime'):
+            out = __salt__['cmd.run']('cat /proc/uptime').split()
+            if len(out):
+                return out[0]
+            else:
+                return 'unexpected format in /proc/uptime'
+        return 'cannot find /proc/uptime'
 
 
 def loadavg():
