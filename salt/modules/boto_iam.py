a42398b50d9da92ae554077e088fb1bda8b45cf5
Syed Ali <syed.ali@purestorage.com>
2015-12-03 13:16:25 -0800

    modules.boto_iam.py:__virtual__ return err msg

--- salt/modules/boto_iam.py
+++ salt/modules/boto_iam.py
@@ -68,7 +68,7 @@ def __virtual__():
     Only load if boto libraries exist.
     '''
     if not HAS_BOTO:
-        return False
+        return (False, 'The boto_iam module could not be loaded: boto libraries not found')
     return True
 
 
