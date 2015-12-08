aa393a4831d5a8ccd804dddb7fa34eed4b4cf1f9
Justin Findlay <jfindlay@gmail.com>
2015-12-08 16:33:07 -0700

    state.file: add pchanges to unit test mock

--- tests/unit/states/file_test.py
+++ tests/unit/states/file_test.py
@@ -189,7 +189,9 @@ class FileTestCase(TestCase):
                 with patch.object(os.path, 'exists', mock_f):
                     comt = ('Symlink {0} to {1}'
                             ' is set for creation').format(name, target)
-                    ret.update({'comment': comt, 'result': None})
+                    ret.update({'comment': comt,
+                                'result': None,
+                                'pchanges': {'new': name}})
                     self.assertDictEqual(filestate.symlink(name, target,
                                                            user=user,
                                                            group=group), ret)
@@ -201,7 +203,9 @@ class FileTestCase(TestCase):
             with patch.dict(filestate.__opts__, {'test': False}):
                 with patch.object(os.path, 'isdir', mock_f):
                     comt = ('Directory /etc for symlink is not present')
-                    ret.update({'comment': comt, 'result': False, 'pchanges': {'new': '/etc/grub.conf'}})
+                    ret.update({'comment': comt,
+                                'result': False,
+                                'pchanges': {'new': '/etc/grub.conf'}})
                     self.assertDictEqual(filestate.symlink(name, target,
                                                            user=user,
                                                            group=group), ret)
@@ -216,7 +220,9 @@ class FileTestCase(TestCase):
                     with patch.object(salt.states.file, '_check_symlink_ownership', mock_t):
                         comt = ('Symlink {0} is present and owned by '
                                 '{1}:{2}'.format(name, user, group))
-                        ret.update({'comment': comt, 'result': True})
+                        ret.update({'comment': comt,
+                                    'result': True,
+                                    'pchanges': {}})
                         self.assertDictEqual(filestate.symlink(name, target,
                                                                user=user,
                                                                group=group), ret)
@@ -231,7 +237,9 @@ class FileTestCase(TestCase):
                     with patch.object(os.path, 'lexists', mock_t):
                         comt = ('File exists where the backup target SALT'
                                 ' should go')
-                        ret.update({'comment': comt, 'result': False})
+                        ret.update({'comment': comt,
+                                    'result': False,
+                                    'pchanges': {'new': name}})
                         self.assertDictEqual(filestate.symlink
                                              (name, target, user=user,
                                               group=group, backupname='SALT'),
