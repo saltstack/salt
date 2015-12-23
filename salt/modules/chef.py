saltstack/salt

commit 89b9cc6812e218610e5b1572f5c9a2230ac75a38

Don't use function call for default argument

salt/modules/chef.py
@@ -50,7 +50,7 @@ def _default_logfile(exe_name):
 @decorators.which('chef-client')
 def client(whyrun=False,
            localmode=False,
-           logfile=_default_logfile('chef-client'),
+           logfile=None,
            **kwargs):
     '''
     Execute a chef client run and return a dict with the stderr, stdout,
@@ -119,6 +119,8 @@ def client(whyrun=False,
         Enable whyrun mode when set to True
 
     '''
+    if logfile is None:
+        logfile=_default_logfile('chef-client'),
     args = ['chef-client',
             '--no-color',
             '--once',
@@ -136,7 +138,7 @@ def client(whyrun=False,
 
 @decorators.which('chef-solo')
 def solo(whyrun=False,
-         logfile=_default_logfile('chef-solo'),
+         logfile=None,
          **kwargs):
     '''
     Execute a chef solo run and return a dict with the stderr, stdout,
@@ -186,6 +188,8 @@ def solo(whyrun=False,
     whyrun
         Enable whyrun mode when set to True
     '''
+    if logfile is None:
+        logfile=_default_logfile('chef-client'),
     args = ['chef-solo',
             '--no-color',
             '--logfile "{0}"'.format(logfile),

commit 7d8f9f28e9066c9a02e7b502d6161bf7efee3a75

Fix chef logfile default arg value
Removed trailing comma so that the default logfile arg is the intended string

```
>>> _default_logfile('chef-client'),
('/var/log/chef-client.log',)
>>> _default_logfile('chef-client')
'/var/log/chef-client.log'
>>>
```

Bug was introduced in https://github.com/saltstack/salt/commit/89b9cc6812e218610e5b1572f5c9a2230ac75a38

salt/modules/chef.py
@@ -124,7 +124,7 @@ def client(whyrun=False,
 
     '''
     if logfile is None:
-        logfile = _default_logfile('chef-client'),
+        logfile = _default_logfile('chef-client')
     args = ['chef-client',
             '--no-color',
             '--once',
@@ -193,7 +193,7 @@ def solo(whyrun=False,
         Enable whyrun mode when set to True
     '''
     if logfile is None:
-        logfile = _default_logfile('chef-solo'),
+        logfile = _default_logfile('chef-solo')
     args = ['chef-solo',
             '--no-color',
             '--logfile "{0}"'.format(logfile),