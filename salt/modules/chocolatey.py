6453a3cfe8da7246eadab7557cd2fe08ba089d90
Mabsou <david.mabouana@epitech.eu>
2015-12-23 14:42:14 +0100

    Correcting lint error -> Trailing whitespaces

--- salt/modules/chocolatey.py
+++ salt/modules/chocolatey.py
@@ -100,12 +100,12 @@ def chocolatey_version():
     '''
     if 'chocolatey._version' in __context__:
         return __context__['chocolatey._version']
-    
+
     cmd = [_find_chocolatey(__context__, __salt__)]
     cmd.append('-v')
     out = __salt__['cmd.run'](cmd, python_shell=False)
     __context__['chocolatey._version'] = out
-    
+
     return __context__['chocolatey._version']
 
 
