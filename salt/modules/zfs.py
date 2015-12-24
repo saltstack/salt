19649acd3c8f5df69f5a84d9d23107c03bc77af7
Jorge Schrauwen <sjorge@blackdot.be>
2015-12-24 00:12:01 +0000

    remove (broken?) dynamic code to add wrappers around none implemented commands, this gives a huge speed increase in loading the module!

--- salt/modules/zfs.py
+++ salt/modules/zfs.py
@@ -10,11 +10,6 @@ from __future__ import absolute_import
 # Import Python libs
 import logging
 
-# Some std libraries that are made
-# use of.
-import re
-import sys
-
 # Import Salt libs
 import salt.utils
 import salt.modules.cmdmod
@@ -55,45 +50,6 @@ def _check_features():
     return res['retcode'] == 0
 
 
-def _available_commands():
-    '''
-    List available commands based on 'zfs -?'. Returns a dict.
-    Does not work on illumos or solaris
-    '''
-    zfs_path = _check_zfs()
-    if not zfs_path:
-        return False
-
-    ret = {}
-    res = __salt__['cmd.run_stderr'](
-        '{0} -?'.format(zfs_path),
-        output_loglevel='trace',
-        ignore_retcode=True
-    )
-
-    # This bit is dependent on specific output from `zfs -?` - any major changes
-    # in how this works upstream will require a change.
-    for line in res.splitlines():
-        if re.match('  [a-zA-Z]', line):
-            cmds = line.split(' ')[0].split('|')
-            doc = ' '.join(line.split(' ')[1:])
-            for cmd in [cmd.strip() for cmd in cmds]:
-                if cmd not in ret:
-                    ret[cmd] = doc
-    return ret
-
-
-def _exit_status(retcode):
-    '''
-    Translate exit status of zfs
-    '''
-    ret = {0: 'Successful completion.',
-           1: 'An error occurred.',
-           2: 'Usage error.'
-           }[retcode]
-    return ret
-
-
 def __virtual__():
     '''
     Makes sure that ZFS kernel module is loaded.
@@ -118,75 +74,8 @@ def __virtual__():
     if cmd and salt.modules.cmdmod.retcode(
         cmd, output_loglevel='quiet', ignore_retcode=True
     ) == 0:
-        # Build dynamic functions and allow loading module
-        _build_zfs_cmd_list()
         return 'zfs'
     return (False, "The zfs module cannot be loaded: zfs not found")
-
-
-def _add_doc(func, doc, prefix='\n\n    '):
-    '''
-    Add documentation to a function
-    '''
-    if not func.__doc__:
-        func.__doc__ = ''
-    func.__doc__ += '{0}{1}'.format(prefix, doc)
-
-
-def _make_function(cmd_name, doc):
-    '''
-    Returns a function based on the command name.
-    '''
-    def _cmd(*args):
-        # Define a return value.
-        ret = {}
-
-        # Run the command.
-        res = __salt__['cmd.run_all'](
-                '{0} {1} {2}'.format(
-                    _check_zfs(),
-                    cmd_name,
-                    ' '.join(args)
-                    )
-                )
-
-        # Make a note of the error in the return object if retcode
-        # not 0.
-        if res['retcode'] != 0:
-            ret['error'] = _exit_status(res['retcode'])
-
-        # Set the output to be splitlines for now.
-        ret = res['stdout'].splitlines()
-
-        return ret
-
-    _add_doc(_cmd, 'This function is dynamically generated.', '\n    ')
-    _add_doc(_cmd, doc)
-    _add_doc(_cmd, '\n    CLI Example:\n\n')
-    _add_doc(_cmd, '\n        salt \'*\' zfs.{0} <args>'.format(cmd_name))
-
-    # At this point return the function we've just defined.
-    return _cmd
-
-
-def _build_zfs_cmd_list():
-    '''
-    Run through zfs command options, and build equivalent functions dynamically
-    '''
-    # Run through all the available commands
-    if _check_zfs():
-        available_cmds = _available_commands()
-        for available_cmd in available_cmds:
-            # Set the output from _make_function to be 'available_cmd_'.
-            # i.e. 'list' becomes 'list_' in local module.
-            setattr(
-                    sys.modules[__name__],
-                    '{0}_'.format(available_cmd),
-                    _make_function(available_cmd, available_cmds[available_cmd])
-                    )
-
-            # Update the function alias so that salt finds the functions properly.
-            __func_alias__['{0}_'.format(available_cmd)] = available_cmd
 
 
 def exists(name):
