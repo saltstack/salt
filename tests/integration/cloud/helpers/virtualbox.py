581baf568b49f0787539ff80619fbbae2874d6ff
LoveIsGrief <LoveIsGrief@users.noreply.github.com>
2015-12-25 16:55:24 +0100

    Move around files for clarity and separation
    Related to saltstack/salt#27089 Saltcloud virtualbox provider

---
+++ tests/integration/cloud/helpers/virtualbox.py
@@ -0,0 +1,29 @@
+import unittest
+
+from cloud.clouds import virtualbox
+
+
+class VirtualboxTestCase(unittest.TestCase):
+    def setUp(self):
+        self.vbox = virtualbox.vb_get_manager()
+
+    def assertMachineExists(self, name, msg=None):
+        try:
+            self.vbox.findMachine(name)
+        except Exception as e:
+            if msg:
+                self.fail(msg)
+            else:
+                self.fail(e.message)
+
+    def assertMachineDoesNotExist(self, name, msg=None):
+        self.assertRaisesRegexp(Exception, "Could not find a registered machine", self.vbox.findMachine, name)
+
+
+def list_machines():
+    vbox = virtualbox.vb_get_manager()
+    for machine in vbox.getArray(vbox, "Machines"):
+        print "Machine '%s' logs in '%s'" % (
+            machine.name,
+            machine.logFolder
+        )
