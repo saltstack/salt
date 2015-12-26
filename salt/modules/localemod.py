5799729aee85389c8f1f6af4b29efa5fc08ce83d
Andreas Lutro <anlutro@gmail.com>
2015-12-26 21:50:48 +0100

    locale module: don't escape the slash in \n
    this would cause the literal string "en_US.UTF8\n" to be appended to the
    locale file.

--- salt/modules/localemod.py
+++ salt/modules/localemod.py
@@ -276,7 +276,7 @@ def gen_locale(locale, **kwargs):
         __salt__['file.replace'](
             '/etc/locale.gen',
             r'^\s*#\s*{0}\s*$'.format(locale),
-            '{0}\\n'.format(locale),
+            '{0}\n'.format(locale),
             append_if_not_found=True
         )
     elif on_ubuntu:
