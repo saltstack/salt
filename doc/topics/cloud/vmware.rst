35bd10c5ba641fbd77a55ba469b9a6713a25ea28
Marno van der Molen <marno.vandermolen@gmail.com>
2015-12-28 19:28:07 +0100

    Implement suggestions from rallytime, tries to adhere to same documentation style and fixes optional thin_provision configuration

--- doc/topics/cloud/vmware.rst
+++ doc/topics/cloud/vmware.rst
@@ -239,7 +239,8 @@ Set up an initial profile at ``/etc/salt/cloud.profiles`` or
         size
             Enter the size of disk in GB
         thin_provision
-            Set to True if you want the disk to be thin provisioned. Defaults to False.
+            Specifies whether the disk should be thin provisioned or not. Default is ``thin_provision: False``.
+            .. versionadded:: Boron
         controller
             Specify the SCSI controller label to which this disk should be attached.
             This should be specified only when creating both the specified SCSI
--- salt/cloud/clouds/vmware.py
+++ salt/cloud/clouds/vmware.py
@@ -230,7 +230,7 @@ def _edit_existing_hard_disk_helper(disk, size_kb):
     return disk_spec
 
 
-def _add_new_hard_disk_helper(disk_label, size_gb, unit_number, thin_provision=False, controller_key=1000):
+def _add_new_hard_disk_helper(disk_label, size_gb, unit_number, controller_key=1000, thin_provision=False):
     random_key = randint(-2099, -2000)
 
     size_kb = int(size_gb * 1024.0 * 1024.0)
@@ -651,8 +651,8 @@ def _manage_devices(devices, vm=None):
         for disk_label in disks_to_create:
             # create the disk
             size_gb = float(devices['disk'][disk_label]['size'])
-            thin_provision = bool(devices['disk'][disk_label]['thin_provision'])
-            disk_spec = _add_new_hard_disk_helper(disk_label, size_gb, unit_number, thin_provision)
+            thin_provision = bool(devices['disk'][disk_label]['thin_provision']) if 'thin_provision' in devices['disk'][disk_label] else False
+            disk_spec = _add_new_hard_disk_helper(disk_label, size_gb, unit_number, thin_provision=thin_provision)
 
             # when creating both SCSI controller and Hard disk at the same time we need the randomly
             # assigned (temporary) key of the newly created SCSI controller
