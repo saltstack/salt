fac994e7f599f76c194afe44785799992e0ead44
rallytime <nicole@saltstack.com>
2015-12-30 13:40:52 -0700

    Add the list_nodes_select function to linode driver

--- salt/cloud/clouds/linode.py
+++ salt/cloud/clouds/linode.py
@@ -1076,6 +1076,15 @@ def list_nodes_min(call=None):
     return ret
 
 
+def list_nodes_select(call=None):
+    '''
+    Return a list of the VMs that are on the provider, with select fields.
+    '''
+    return salt.utils.cloud.list_nodes_select(
+        list_nodes_full(), __opts__['query.selection'], call,
+    )
+
+
 def reboot(name, call=None):
     '''
     Reboot a linode.
