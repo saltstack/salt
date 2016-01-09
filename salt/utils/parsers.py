9d888e9a6de746eee54c0bbf64982d95a3dfbb89
Pedro Algarvio <pedro@algarvio.me>
2016-01-09 12:50:44 +0000

    When daemonizing allow stopping multiprocessing.
    Allow stopping the multiprocessing logging queue listener from the main
    process while daemonizing.
    
    Fixes #30247

--- salt/utils/parsers.py
+++ salt/utils/parsers.py
@@ -898,7 +898,7 @@ class DaemonMixIn(six.with_metaclass(MixInMeta, object)):
             if self._setup_mp_logging_listener_ is True:
                 # Stop the logging queue listener for the current process
                 # We'll restart it once forked
-                log.shutdown_multiprocessing_logging_listener()
+                log.shutdown_multiprocessing_logging_listener(daemonizing=True)
 
             # Late import so logging works correctly
             salt.utils.daemonize()
