39bf3b740e3b427fc5a208018e79ccde8fbdf58d
Pedro Algarvio <pedro@algarvio.me>
2016-01-10 13:36:45 +0000

    Also close the workers

--- salt/transport/zeromq.py
+++ salt/transport/zeromq.py
@@ -492,6 +492,8 @@ class ZeroMQReqServerChannel(salt.transport.mixins.auth.AESReqServerMixin, salt.
             self._monitor = None
         if hasattr(self, 'clients') and self.clients.closed is False:
             self.clients.close()
+        if hasattr(self, 'workers') and self.workers.closed is False:
+            self.workers.close()
         if hasattr(self, 'stream'):
             self.stream.close()
         if hasattr(self, 'context') and self.context.closed is False:
