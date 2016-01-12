saltstack/salt

commit dae738fda3f12e312f81d7f69e4773e0624f7ef2

Use common code to detect envs
This commit adds a parent-class function which makes the calculation of
environment names identical regardless of the provider being used. The
function simply accepts a list of the ref paths (i.e. refs/heads/foo,
refs/remotes/origin/bar, refs/tags/baz).

This commit also makes a slight tweak to how dulwich determines which
ref paths to use for env detection. Before it used any ref with a path
beginning with refs/heads or refs/tags, but this switches it to
refs/remotes so that the common logic introduced in the new function
described above works.

It seems that dulwich adds local heads for any remote heads pulled down
when the remote is fetched, which is why this would have worked for env
detection before.

This commit contains a more complete fix for #27432, as an earlier
commit only fixed this for pygit2.

salt/utils/gitfs.py
@@ -269,6 +269,35 @@ class GitProvider(object):
             log.critical(msg, exc_info_on_loglevel=logging.DEBUG)
             failhard(self.role)
 
+    def _get_envs_from_ref_paths(self, refs):
+        '''
+        Return the names of remote refs (stripped of the remote name) and tags
+        which are exposed as environments. If a branch or tag matches
+        '''
+        def _check_ref(env_set, base_ref, rname):
+            '''
+            Check the ref and resolve it as the base_ref if it matches. If the
+            resulting env is exposed via whitelist/blacklist, add it to the
+            env_set.
+            '''
+            if base_ref is not None and base_ref == rname:
+                rname = 'base'
+            if self.env_is_exposed(rname):
+                env_set.add(rname)
+
+        ret = set()
+        base_ref = getattr(self, 'base', None)
+        for ref in refs:
+            ref = re.sub('^refs/', '', ref)
+            rtype, rname = ref.split('/', 1)
+            if rtype == 'remotes':
+                parted = rname.partition('/')
+                rname = parted[2] if parted[2] else parted[0]
+                _check_ref(ret, base_ref, rname)
+            elif rtype == 'tags':
+                _check_ref(ret, base_ref, rname)
+        return ret
+
     def check_root(self):
         '''
         Check if the relative root path exists in the checked-out copy of the
@@ -542,18 +571,8 @@ class GitPython(GitProvider):
         Check the refs and return a list of the ones which can be used as salt
         environments.
         '''
-        ret = set()
-        for ref in self.repo.refs:
-            parted = ref.name.partition('/')
-            rspec = parted[2] if parted[2] else parted[0]
-            if isinstance(ref, git.Head):
-                if hasattr(self, 'base') and self.base == rspec:
-                    rspec = 'base'
-                if self.env_is_exposed(rspec):
-                    ret.add(rspec)
-            elif isinstance(ref, git.Tag) and self.env_is_exposed(rspec):
-                ret.add(rspec)
-        return ret
+        ref_paths = [x.path for x in self.repo.refs]
+        return self._get_envs_from_ref_paths(ref_paths)
 
     def fetch(self):
         '''
@@ -908,29 +927,8 @@ class Pygit2(GitProvider):
         Check the refs and return a list of the ones which can be used as salt
         environments.
         '''
-        def _check_ref(env_set, base_ref, rname):
-            '''
-            Check the ref and resolve it as the base_ref if it matches. If the
-            resulting env is exposed via whitelist/blacklist, add it to the
-            env_set.
-            '''
-            if base_ref is not None and base_ref == rname:
-                rname = 'base'
-            if self.env_is_exposed(rname):
-                env_set.add(rname)
-
-        ret = set()
-        base_ref = getattr(self, 'base', None)
-        for ref in self.repo.listall_references():
-            ref = re.sub('^refs/', '', ref)
-            rtype, rname = ref.split('/', 1)
-            if rtype == 'remotes':
-                parted = rname.partition('/')
-                rname = parted[2] if parted[2] else parted[0]
-                _check_ref(ret, base_ref, rname)
-            elif rtype == 'tags':
-                _check_ref(ret, base_ref, rname)
-        return ret
+        ref_paths = self.repo.listall_references()
+        return self._get_envs_from_ref_paths(ref_paths)
 
     def fetch(self):
         '''
@@ -1203,7 +1201,7 @@ class Dulwich(GitProvider):  # pylint: disable=abstract-method
     def __init__(self, opts, remote, per_remote_defaults,
                  override_params, cache_root, role='gitfs'):
         self.get_env_refs = lambda refs: [
-            x for x in refs if re.match('refs/(heads|tags)', x)
+            x for x in refs if re.match('refs/(remotes|tags)', x)
             and not x.endswith('^{}')
         ]
         self.provider = 'dulwich'
@@ -1251,18 +1249,8 @@ class Dulwich(GitProvider):  # pylint: disable=abstract-method
         Check the refs and return a list of the ones which can be used as salt
         environments.
         '''
-        ret = set()
-        for ref in self.get_env_refs(self.repo.get_refs()):
-            # ref will be something like 'refs/heads/master'
-            rtype, rspec = ref[5:].split('/', 1)
-            if rtype == 'heads':
-                if hasattr(self, 'base') and self.base == rspec:
-                    rspec = 'base'
-                if self.env_is_exposed(rspec):
-                    ret.add(rspec)
-            elif rtype == 'tags' and self.env_is_exposed(rspec):
-                ret.add(rspec)
-        return ret
+        ref_paths = self.get_env_refs(self.repo.get_refs())
+        return self._get_envs_from_ref_paths(ref_paths)
 
     def fetch(self):
         '''

commit a746014f7e68926778f5fa6e95ba2e84584d4d02

salt.utils.gitfs: Fix Dulwich env detection and submodule handling
I broke this in dae738f. I mistakenly thought I saw both head refs and
remote refs in the fetch results, so by consolidating the code that
resolves refnames into environment names I ensured that the head refs
which were fetched would never be seen as fileserver environments.

This commit fixes #29684 by converting the names of the fetched head
refs to remote refs. Corresponding changes were also made to the
get_tree() function (as the refnames are now different).

Additionally, I discovered that Dulwich does not track information about
submodules, resulting in a KeyError when compiling a list of files. This
is the same issue that also affected Pygit2 and was fixed in 8c4ea64.
I've made a similar fix for it here.

salt/utils/gitfs.py
@@ -1307,7 +1307,11 @@ class Dulwich(GitProvider):  # pylint: disable=abstract-method
             all the empty directories within it in the "blobs" list
             '''
             for item in six.iteritems(tree):
-                obj = self.repo.get_object(item.sha)
+                try:
+                    obj = self.repo.get_object(item.sha)
+                except KeyError:
+                    # Entry is a submodule, skip it
+                    continue
                 if not isinstance(obj, dulwich.objects.Tree):
                     continue
                 blobs.append(os.path.join(prefix, item.path))
@@ -1380,6 +1384,21 @@ class Dulwich(GitProvider):  # pylint: disable=abstract-method
                     'Unable to remove {0}: {1}'.format(self.cachedir, exc)
                 )
             return False
+        else:
+            # Dulwich does not write fetched references to the gitdir, that is
+            # done manually below (see the "Update local refs" comment). Since
+            # A) gitfs doesn't check out any local branches, B) both Pygit2 and
+            # GitPython set remote refs when fetching instead of head refs, and
+            # C) Dulwich is not supported for git_pillar or winrepo, there is
+            # no harm in simply renaming the head refs from the fetch results
+            # to remote refs. This allows the same logic (see the
+            # "_get_envs_from_ref_paths()" function) to be used for all three
+            # GitProvider subclasses to derive available envs.
+            for ref in [x for x in refs_post if x.startswith('refs/heads/')]:
+                val = refs_post.pop(ref)
+                key = ref.replace('refs/heads/', 'refs/remotes/origin/', 1)
+                refs_post[key] = val
+
         if refs_post is None:
             # Empty repository
             log.warning(
@@ -1392,7 +1411,7 @@ class Dulwich(GitProvider):  # pylint: disable=abstract-method
             for ref in self.get_env_refs(refs_post):
                 self.repo[ref] = refs_post[ref]
             # Prune stale refs
-            for ref in self.repo.get_refs():
+            for ref in refs_pre:
                 if ref not in refs_post:
                     del self.repo[ref]
             return True
@@ -1408,7 +1427,11 @@ class Dulwich(GitProvider):  # pylint: disable=abstract-method
             all the file paths and symlinks info in the "blobs" dict
             '''
             for item in six.iteritems(tree):
-                obj = self.repo.get_object(item.sha)
+                try:
+                    obj = self.repo.get_object(item.sha)
+                except KeyError:
+                    # Entry is a submodule, skip it
+                    continue
                 if isinstance(obj, dulwich.objects.Blob):
                     repo_path = os.path.join(prefix, item.path)
                     blobs.setdefault('files', []).append(repo_path)
@@ -1502,10 +1525,19 @@ class Dulwich(GitProvider):  # pylint: disable=abstract-method
         refs = self.repo.get_refs()
         # Sorting ensures we check heads (branches) before tags
         for ref in sorted(self.get_env_refs(refs)):
-            # ref will be something like 'refs/heads/master'
-            rtype, rspec = ref[5:].split('/', 1)
+            # ref will be something like 'refs/remotes/origin/master'
+            try:
+                rtype, rspec = re.split('^refs/(remotes/origin|tags)/',
+                                        ref,
+                                        1)[-2:]
+            except ValueError:
+                # No match was fount for the split regex, we don't care about
+                # this ref. We shouldn't see any of these as the refs are being
+                # filtered through self.get_env_refs(), but just in case, this
+                # will avoid a traceback.
+                continue
             if rspec == tgt_ref and self.env_is_exposed(tgt_env):
-                if rtype == 'heads':
+                if rtype == 'remotes/origin':
                     commit = self.repo.get_object(refs[ref])
                 elif rtype == 'tags':
                     tag = self.repo.get_object(refs[ref])