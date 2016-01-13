d18f6fb4281951a0632b1c218874c632535bbe7b
Daniel Wallace <danielwallace@gtmanfred.com>
2016-01-12 20:33:34 -0600

    clean up first set of bugs

--- salt/cloud/clouds/nova.py
+++ salt/cloud/clouds/nova.py
@@ -328,6 +328,8 @@ def get_image(conn, vm_):
 
     try:
         image = conn.image_show(vm_image)
+        if not image and salt.utils.cloud.valid_uuid(vm_image):
+            return vm_image
         return image['id']
     except novaclient.exceptions.NotFound as exc:
         raise SaltCloudNotFound(
@@ -1039,7 +1041,7 @@ def list_nodes(call=None, **kwargs):
             'id': server_tmp['id'],
             'image': server_tmp['image']['id'],
             'size': server_tmp['flavor']['id'],
-            'state': server_tmp['state'],
+            'state': server_tmp['status'],
             'private_ips': private,
             'public_ips': public,
         }
