306d13a7fa8a3d495b44ab399269c72d2fb99c10
twangboy <slee@saltstack.com>
2016-01-22 17:36:48 -0700

    Fixed tests

--- tests/unit/modules/mac_brew_test.py
+++ tests/unit/modules/mac_brew_test.py
@@ -6,15 +6,15 @@
 # Import Python libs
 from __future__ import absolute_import
 
+# Import Salt Libs
+from salt.modules import mac_brew
+
 # Import Salt Testing Libs
 from salttesting import skipIf, TestCase
 from salttesting.mock import MagicMock, patch, NO_MOCK, NO_MOCK_REASON
 from salttesting.helpers import ensure_in_syspath
 
 ensure_in_syspath('../../')
-
-# Import Salt Libs
-from salt.modules import brew
 
 # Global Variables
 brew.__context__ = {}
