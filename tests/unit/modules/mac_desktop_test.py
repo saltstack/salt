88841e604f652c04b95fc5d7af1d13a71cb71f63
twangboy <slee@saltstack.com>
2016-01-22 17:27:18 -0700

    Fixed some tests

--- tests/unit/modules/mac_desktop_test.py
+++ tests/unit/modules/mac_desktop_test.py
@@ -6,6 +6,9 @@
 # Import Python Libs
 from __future__ import absolute_import
 
+# Import Salt Libs
+from salt.modules import mac_desktop
+
 # Import Salt Testing Libs
 from salttesting import TestCase, skipIf
 from salttesting.mock import (
@@ -18,9 +21,6 @@ from salttesting.mock import (
 from salttesting.helpers import ensure_in_syspath
 
 ensure_in_syspath('../../')
-
-# Import Salt Libs
-from salt.modules import osxdesktop
 
 # Globals
 osxdesktop.__salt__ = {}
