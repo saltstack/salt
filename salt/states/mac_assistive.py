3c5ca1fa67a09d3d56e8a0c06224fd1ced861946
Daniel Hobley <danielh@unity3d.com>
2016-01-29 15:18:40 +0100

    Assistive devices in this form is only available on OS X 10.9+

--- salt/states/mac_assistive.py
+++ salt/states/mac_assistive.py
@@ -27,7 +27,7 @@ def __virtual__():
     '''
     Only work on Mac OS
     '''
-    if salt.utils.is_darwin():
+    if salt.utils.is_darwin() and LooseVersion(__grains__['osmajorrelease']) >= '10.9':
         return True
     return False
 
