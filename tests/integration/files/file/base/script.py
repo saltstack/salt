0acb145aa10af5bbc9b768ee6be71e0c0d8085f9
justinta89 <janderson@saltstack.com>
2016-01-29 11:52:17 -0700

    Adjusting script for cmd.script test
    After #29964 was merged the cmd.script and cmd.script_retcode tests on centos 5
    started failing because the /usr/bin/env python version is the system default.
    The script used print_fuction from __futures__ which was not introduced until
    python2.6.
    
    @The-Loeki, I noticed this after your changes to cmdmod function. If you have
    thoughts or comments as to why this behavior changed after that, I'm all ears.
    othersie, this should take care of the failures on centos 5 on jenkins.

--- tests/integration/files/file/base/script.py
+++ tests/integration/files/file/base/script.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
-from __future__ import print_function
 import sys
 
-print(' '.join(sys.argv[1:]))
+try:
+    print ' '.join(sys.argv[1:])
+except SyntaxError:
+    print(' '.join(sys.argv[1:]))
