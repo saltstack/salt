e74a419346f1d70eff09e13ed4f6ee2212f8b083
Dmitry Kuzmenko <dmitry.kuzmenko@dsr-company.com>
2016-02-02 18:23:53 +0300

    Don't send REQ while another one is waiting for response.
    The message has to be removed from the queue the only *after* it's
    already processed to don't confuse send() functionality that expects
    empty queue means: there's no active sendings.

--- salt/transport/tcp.py
+++ salt/transport/tcp.py
@@ -520,7 +520,7 @@ class SaltMessageClient(object):
         while not self._connecting_future.done() or self._connecting_future.result() is not True:
             yield self._connecting_future
         while len(self.send_queue) > 0:
-            message_id, item = self.send_queue.pop(0)
+            message_id, item = self.send_queue[0]
             try:
                 yield self._stream.write(item)
             # if the connection is dead, lets fail this send, and make sure we
@@ -528,6 +528,7 @@ class SaltMessageClient(object):
             except tornado.iostream.StreamClosedError as e:
                 self.send_future_map.pop(message_id).set_exception(Exception())
                 self.remove_message_timeout(message_id)
+                del self.send_queue[0]
                 if self._closing:
                     return
                 # if the last connect finished, then we need to make a new one
