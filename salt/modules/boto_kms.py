6146209c53b085393960442e2acd24f211b8ef7b
rallytime <nicole@saltstack.com>
2016-02-03 11:57:44 -0700

    Pass in 'pack' variable to utils.boto.assign_funcs function from ALL boto modules.

--- salt/modules/boto_kms.py
+++ salt/modules/boto_kms.py
@@ -77,10 +77,10 @@ def __virtual__():
     return True
 
 
-def __init__(opts):
+def __init__(opts, pack=None):
     salt.utils.compat.pack_dunder(__name__)
     if HAS_BOTO:
-        __utils__['boto.assign_funcs'](__name__, 'kms')
+        __utils__['boto.assign_funcs'](__name__, 'kms', pack=__salt__)
 
 
 def create_alias(alias_name, target_key_id, region=None, key=None, keyid=None,
