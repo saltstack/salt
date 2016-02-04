4555a0324c62f876087a4c73a9c83eb3b426c2bd
C. R. Oldham <cro@ncbt.org>
2016-02-04 15:05:36 -0700

    Finish the idrac state, fix problem with grains not loading sometimes.

--- salt/modules/chassis.py
+++ salt/modules/chassis.py
@@ -34,10 +34,15 @@ def __virtual__():
         return __virtualname__
     return False
 
+def chassis_credentials():
+    proxyprefix = __opts__['proxy']['proxytype']
+    (username, password) = __proxy__[proxyprefix+'.find_credentials']()
+    return (username, password)
+
 
 def cmd(cmd, *args, **kwargs):
     proxyprefix = __opts__['proxy']['proxytype']
-    (username, password) = __proxy__[proxyprefix+'.find_credentials']()
+    (username, password) = chassis_credentials()
     kwargs['admin_username'] = username
     kwargs['admin_password'] = password
     kwargs['host'] = __proxy__[proxyprefix+'.host']()
