4a4c1f1d7eed4ab223040dcb3c4a5df52469323e
Marno van der Molen <marno.vandermolen@gmail.com>
2016-02-07 11:15:00 +0100

    Check if keys exists before checking them - apparently not always set for LXC containers

--- salt/cloud/clouds/proxmox.py
+++ salt/cloud/clouds/proxmox.py
@@ -557,7 +557,7 @@ def create(vm_):
 
     ret['creation_data'] = data
     name = vm_['name']        # hostname which we know
-    if (vm_['clone']) is True:
+    if 'clone' in vm_ and vm_['clone'] is True:
         vmid = newid
     else:
         vmid = data['vmid']       # vmid which we have received
@@ -707,7 +707,7 @@ def create_node(vm_, newid):
 
     log.debug('Preparing to generate a node using these parameters: {0} '.format(
               newnode))
-    if vm_['clone'] is True and vm_['technology'] == 'qemu':
+    if 'clone' in vm_ and vm_['clone'] is True and vm_['technology'] == 'qemu':
         postParams = {}
         postParams['newid'] = newnode['vmid']
 
