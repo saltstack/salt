3f71fd07096e320b8977ec1f209cb78fee6eb53f
rallytime <nicole@saltstack.com>
2016-02-09 17:03:16 +0000

    Revert #30217
    Reverts commit 1f68ce05bcaf47397baeeb4626d4c59db948710d.
    Reverts commit 5264449fdb6a0acb02c65b715773066ab990fcd4.
    
    Conflicts:
    	salt/runners/cloud.py

--- salt/runners/cloud.py
+++ salt/runners/cloud.py
@@ -125,7 +125,12 @@ def destroy(instances):
     return info
 
 
-def action(*args, **kwargs):
+def action(func=None,
+           cloudmap=None,
+           instances=None,
+           provider=None,
+           instance=None,
+           **kwargs):
     '''
     Execute a single action on the given map/provider/instance
 
@@ -136,12 +141,7 @@ def action(*args, **kwargs):
         salt-run cloud.actions start my-salt-vm
     '''
     client = _get_client()
-    info = client.action(args[0],
-                         kwargs.get('cloudmap', None),
-                         args[1:],
-                         kwargs.get('provider', None),
-                         kwargs.get('instance', None),
-                         kwargs)
+    info = client.action(func, cloudmap, instances, provider, instance, kwargs)
     return info
 
 
