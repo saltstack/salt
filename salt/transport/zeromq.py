cac41bdc892843dc41aa4f7ba7140cde007cee6b
Dmitry Kuzmenko <dmitry.kuzmenko@dsr-company.com>
2016-02-12 18:30:54 +0300

    Fixed zeromq ReqMessageClient destroy

--- salt/transport/zeromq.py
+++ salt/transport/zeromq.py
@@ -747,6 +747,7 @@ class AsyncReqMessageClient(object):
             self.stream.io_loop.remove_handler(self.stream.socket)
             # set this to None, more hacks for messed up pyzmq
             self.stream.socket = None
+            self.stream = None
             self.socket.close()
         self.context.term()
 
