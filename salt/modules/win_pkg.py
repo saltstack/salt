860437665d31af8c78f7d7ab7df3b0758f0558df
twangboy <slee@saltstack.com>
2016-02-12 18:34:07 -0700

    Fix some lint

--- salt/modules/win_pkg.py
+++ salt/modules/win_pkg.py
@@ -384,7 +384,7 @@ def refresh_db(saltenv='base'):
         winrepo_source_dir = __opts__['winrepo_source_dir']
 
     # Clear minion repo-ng cache
-    repo_path = '{0}\\files\\{1}\win\\repo-ng\\salt-winrepo-ng'\
+    repo_path = '{0}\\files\\{1}\\win\\repo-ng\\salt-winrepo-ng'\
         .format(__opts__['cachedir'], saltenv)
     if not __salt__['file.remove'](repo_path):
         log.error('pkg.refresh_db: failed to clear existing cache')
