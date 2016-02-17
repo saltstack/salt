1c8f783e6a4000dc3601878a6aee21ac1ebbf6b7
Terence Kent <tkent@xetus.com>
2016-02-17 11:45:09 -0800

    Make the "desired_binds" comparison key consistent with the "actual_binds" comparison key when the any address (0.0.0.0) is used. This will avoid the situation where it looks like the desired changes where not applied for port_binds.
    Fixes #31282

--- salt/states/dockerng.py
+++ salt/states/dockerng.py
@@ -269,6 +269,12 @@ def _compare(actual, create_kwargs):
                                     host_port,
                                     container_port)
                     actual_binds.append(':'.join(bind_def))
+                    # The any address (0.0.0.0) is omitted from the
+                    # actual_binds comparison key, so be sure to
+                    # strip it from the desired_binds comparison
+                    # key if it's included.
+                    if bind_def.startswith('0.0.0.0:'):
+                        bind_def = bind_def.replace('0.0.0.0:', '')
 
             desired_binds = []
             for container_port, bind_list in six.iteritems(data):
