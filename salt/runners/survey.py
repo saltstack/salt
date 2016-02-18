6d10f420774efb8cb4bf27c0b0aac7038a6800b8
Hazel Smith <hazel@hazelesque.uk>
2016-02-18 19:29:47 +0000

    Don't pass kwargs with keys beginning with _ through to LocalClient

--- salt/runners/survey.py
+++ salt/runners/survey.py
@@ -154,9 +154,11 @@ def _get_pool_results(*args, **kwargs):
     if expr_form not in ['compound', 'pcre']:
         expr_form = 'compound'
 
+    kwargs_passthru = dict((k, kwargs[k]) for k in kwargs.iterkeys() if not k.startswith('_'))
+
     client = salt.client.get_local_client(__opts__['conf_file'])
     try:
-        minions = client.cmd(tgt, cmd, args[2:], timeout=__opts__['timeout'], expr_form=expr_form, kwarg=kwargs)
+        minions = client.cmd(tgt, cmd, args[2:], timeout=__opts__['timeout'], expr_form=expr_form, kwarg=kwargs_passthru)
     except SaltClientError as client_error:
         print(client_error)
         return ret
