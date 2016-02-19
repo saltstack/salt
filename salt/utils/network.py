bdf2576dfbd5e71b789b1e360c16b484ccfa8d64
Jorge Schrauwen <sjorge@blackdot.be>
2016-02-19 22:57:58 +0000

    missed a .format and messed up the join

--- salt/utils/network.py
+++ salt/utils/network.py
@@ -521,8 +521,8 @@ def _interfaces_ifconfig(out):
                 if salt.utils.is_sunos():
                     expand_mac = []
                     for chunk in data['hwaddr'].split(':'):
-                        expand_mac.append('0{0}' if len(chunk) < 2 else '{0}'.format(chunk))
-                    data['hwaddr'] = expand_mac.join(':')
+                        expand_mac.append('0{0}'.format(chunk) if len(chunk) < 2 else '{0}'.format(chunk))
+                    data['hwaddr'] = ':'.join(expand_mac)
             if mip:
                 if 'inet' not in data:
                     data['inet'] = list()
