37c4bf22d21798222ab17a5d0eaaff08caa936d2
justinta89 <janderson@saltstack.com>
2016-02-23 15:46:21 -0700

    Updated beacons integration test

--- tests/integration/modules/beacons.py
+++ tests/integration/modules/beacons.py
@@ -1,11 +1,15 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
 
 # Python Libs
 from __future__ import absolute_import
+import os
 
 # Salt Libs
 from salt.modules import beacons
+from salt.exceptions import CommandExecutionError
 import integration
+
+# Salttesting libs
 from salttesting import skipIf
 from salttesting.helpers import destructiveTest, ensure_in_syspath
 
@@ -13,49 +17,102 @@ ensure_in_syspath('../../')
 
 beacons.__opts__ = {}
 
+BEACON_CONF_DIR = os.path.join(integration.TMP, 'minion.d')
+if not os.path.exists(BEACON_CONF_DIR):
+    os.makedirs(BEACON_CONF_DIR)
 
+@destructiveTest
+@skipIf(os.geteuid() != 0, 'You must be root to run these tests')
 class BeaconsTest(integration.ModuleCase):
     '''
     Tests the beacons execution module
     '''
-    @destructiveTest
     def test_add(self):
-        ret = self.run_function('beacons.add', ['ps', {'apache2': 'stopped'}])
-        self.assertEqual(ret, {'comment': 'Added beacon: ps.', 'result': True})
+        '''
+        Test adding a beacon
+        '''
+        _add = self.run_function('beacons.add', ['ps', {'apache2': 'stopped'}])
+        self.assertTrue(_add['result'])
 
-    @destructiveTest
-    def test_list(self):
-        ret = self.run_function('beacons.list')
-        self.assertEqual({'beacons': {}}, ret)
+        # save added beacon
+        _save = self.run_function('beacons.save')
+        self.assertTrue(_save['result'])
 
-    @destructiveTest
-    def test_disable(self):
-        ret = self.run_function('beacons.disable')
-        self.assertEqual(ret, {'comment': 'Disabled beacons on minion.', 'result': True})
-
-    @destructiveTest
-    def test_disable_beacon(self):
-        self.run_function('beacons.add', ['ps', {'apache2': 'stopped'}])
-
-        ret = self.run_function('beacons.disable_beacon', ['ps'])
-        self.assertEqual(ret, {'comment': 'Disabled beacon on minion.', 'result': True})
-
-    @destructiveTest
-    def test_enable(self):
-        ret = self.run_function('beacons.enable')
-        self.assertEqual(ret, {'comment': 'Enabled beacons on minion.', 'result': True})
-
-    @destructiveTest
-    def test_enable_beacon(self):
-        self.run_function('beacons.add', ['ps', {'apache2': 'stopped'}])
-
-        ret = self.run_function('beacons.enable_beacon', ['ps'])
-        self.assertEqual(ret, {'comment': 'Enabled beacon ps on minion.', 'result': True})
-
-    @destructiveTest
     def test_delete(self):
-        ret = self.run_function('beacons.delete', ['ps'])
-        self.assertEqual(ret, {'comment': 'Deleted beacon: ps.', 'result': True})
+        '''
+        Test deleting a beacon
+        '''
+        _delete = self.run_function('beacons.delete', ['ps'])
+        self.assertTrue(_delete['result'])
+
+        # save the results
+        self.run_function('beacons.save')
+
+    def test_disable(self):
+        '''
+        Test disabling beacons
+        '''
+        try:
+            # Add beacon to disable
+            self.run_function('beacons.add', ['ps', {'apache2': 'stopped'}])
+            self.run_function('beacons.save')
+        except CommandExecutionError:
+            self.skipTest('Unable to add beacon')
+
+        # disable beacons on minion
+        ret = self.run_function('beacons.disable')
+        self.assertTrue(ret['result'])
+        # disable added beacon
+        ret = self.run_function('beacons.disable_beacon', ['ps'])
+        self.assertTrue(ret['result'])
+
+        # delete added beacon
+        self.run_function('beacons.delete', ['ps'])
+        self.run_function('beacons.save')
+
+    def test_enable(self):
+        '''
+        Test enabling beacons
+        '''
+        try:
+            # Add beacon to enable
+            self.run_function('beacons.add', ['ps', {'apache2': 'stopped'}])
+            self.run_function('beacons.save')
+        except CommandExecutionError:
+            self.skipTest('Unable to add beacon')
+
+        # enable beacons on minion
+        ret = self.run_function('beacons.enable')
+        self.assertTrue(ret['result'])
+        # enable added beacon
+        ret = self.run_function('beacons.enable_beacon', ['ps'])
+        self.assertTrue(ret['result'])
+
+        # delete added beacon
+        self.run_function('beacons.delete', ['ps'])
+        self.run_function('beacons.save')
+
+    def test_list(self):
+        '''
+        Test lising the beacons
+        '''
+        try:
+            # Add beacon to list
+            self.run_function('beacons.add', ['ps', {'apache2': 'stopped'}])
+            self.run_function('beacons.save')
+        except CommandExecutionError:
+            self.skipTest('Unable to add beacon')
+
+        # list beacons
+        ret = self.run_function('beacons.list', return_yaml=False)
+        if 'enabled' in ret:
+            self.assertEqual(ret, {'ps': {'apache2': 'stopped'}, 'enabled': True})
+        else:
+            self.assertEqual(ret, {'ps': {'apache': 'stopped'}})
+
+        # delete added beacon
+        self.run_function('beacons.delete', ['ps'])
+        self.run_function('beacons.save')
 
 
 if __name__ == '__main__':
