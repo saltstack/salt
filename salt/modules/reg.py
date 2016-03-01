41c6ab0a3ad77be0bacd2ee02cd0128cd62bd936
twangboy <slee@saltstack.com>
2016-02-29 19:01:00 -0700

    Add documentation to list_keys function

--- salt/modules/reg.py
+++ salt/modules/reg.py
@@ -143,6 +143,30 @@ def broadcast_change():
 
 
 def list_keys(hive, key=None, use_32bit_registry=False):
+    '''
+    Enumerates the subkeys in a registry key or hive.
+
+    :param str hive: The name of the hive. Can be one of the following
+
+        - HKEY_LOCAL_MACHINE or HKLM
+        - HKEY_CURRENT_USER or HKCU
+        - HKEY_USER or HKU
+
+    :param str key: The key (looks like a path) to the value name. If a key is
+        not passed, the keys under the hive will be returned.
+
+    :param bool use_32bit_registry: Accesses the 32bit portion of the registry
+        on 64 bit installations. On 32bit machines this is ignored.
+
+    :return: A list of keys/subkeys under the hive or key.
+    :rtype: list
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' reg.list_keys HKLM 'SOFTWARE'
+    '''
     registry = Registry()
     hkey = registry.hkeys[hive]
     access_mask = registry.registry_32[use_32bit_registry]
