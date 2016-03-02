f2cfc3cd06d55aab4d0f2738c44a787a084d945e
C. R. Oldham <cro@ncbt.org>
2016-03-02 11:29:02 -0700

    Lint.

--- salt/modules/rest_sample_utils.py
+++ salt/modules/rest_sample_utils.py
@@ -4,18 +4,6 @@ Utility functions for the rest_sample
 '''
 from __future__ import absolute_import
 
-# Import python libs
-import os
-import re
-import stat
-import tempfile
-
-# Import salt libs
-import salt.utils
-from salt.utils import which as _which
-from salt.exceptions import SaltInvocationError
-
-
 __proxyenabled__ = ['rest_sample']
 
 
@@ -24,5 +12,3 @@ def fix_outage():
     "Fix" the outage
     '''
     return __proxy__['rest_sample.fix_outage']()
-
-
