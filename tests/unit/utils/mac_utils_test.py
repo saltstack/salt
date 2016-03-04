e37abfd2582999a2aa838f30e5f5508be6508299
twangboy <slee@saltstack.com>
2016-03-04 10:55:23 -0700

    Add missing import

--- tests/unit/utils/mac_utils_test.py
+++ tests/unit/utils/mac_utils_test.py
@@ -9,12 +9,8 @@ from __future__ import absolute_import
 # Import Salt Testing Libs
 from salttesting import TestCase, skipIf
 from salttesting.helpers import ensure_in_syspath
-from salttesting.mock import (
-    MagicMock,
-    patch,
-    NO_MOCK,
-    NO_MOCK_REASON
-)
+from salttesting.mock import MagicMock, patch, NO_MOCK, NO_MOCK_REASON
+from salt.ext.six.moves import range
 
 ensure_in_syspath('../../')
 
