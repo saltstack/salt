4809e09f52878094eb244c3bbf001dae7750cca1
Kris Raney <kraney@ixiacom.com>
2016-03-08 11:54:03 -0600

    Fix pylint errors

--- tests/unit/states/boto_vpc_test.py
+++ tests/unit/states/boto_vpc_test.py
@@ -66,7 +66,7 @@ dhcp_options_parameters.update(conn_parameters)
 
 opts = salt.config.DEFAULT_MINION_OPTS
 ctx = {}
-utils = salt.loader.utils(opts, context=ctx, whitelist=['boto','boto3'])
+utils = salt.loader.utils(opts, context=ctx, whitelist=['boto', 'boto3'])
 serializers = salt.loader.serializers(opts)
 funcs = salt.loader.minion_mods(opts, context=ctx, utils=utils, whitelist=['boto_vpc'])
 salt_states = salt.loader.states(opts=opts, functions=funcs, utils=utils, whitelist=['boto_vpc'], serializers=serializers)
