4ad3b91dcfc4501665acf6c87cccb155ce4b714f
hoonetorg <hoonetorg@users.noreply.github.com>
2016-03-15 00:42:33 +0100

    fix lint stuff

--- salt/states/pcs.py
+++ salt/states/pcs.py
@@ -9,7 +9,7 @@ Example:
 
     pcs_config__auth:
         pcs.auth:
-          - nodes: 
+          - nodes:
             - node1.example.com
             - node2.example.com
           - pcsuser: hacluster
@@ -50,59 +50,58 @@ def auth(name, nodes, pcsuser='hacluster', pcspasswd='hacluster', extra_args=[])
     '''
 
     ret = {'name': name, 'result': True, 'comment': '', 'changes': {}}
-    result = {}
     auth_required = False
 
-    authorized  = __salt__['pcs.is_auth'](nodes=nodes)
+    authorized = _salt__['pcs.is_auth'](nodes=nodes)
     log.trace('Output of pcs.is_auth: ' + str(authorized))
 
     authorized_dict = {}
     for line in authorized['stdout'].splitlines():
-      node = line.split(':')[0].strip()
-      auth_state = line.split(':')[1].strip()
-      if node in nodes:
-        authorized_dict.update( { node : auth_state} )
+        node = line.split(':')[0].strip()
+        auth_state = line.split(':')[1].strip()
+        if node in nodes:
+            authorized_dict.update({node: auth_state})
     log.trace('authorized_dict: ' + str(authorized_dict))
 
     for node in nodes:
-      if node in authorized_dict and authorized_dict[node] == 'Already authorized':
-        ret['comment'] += 'Node {0} is already authorized\n'.format(node)
-      else:
-        auth_required = True
-        if __opts__['test']:
-          ret['comment'] += 'Node is set to authorize: {0}\n'.format(node)
-    
+        if node in authorized_dict and authorized_dict[node] == 'Already authorized':
+            ret['comment'] += 'Node {0} is already authorized\n'.format(node)
+        else:
+            auth_required = True
+            if __opts__['test']:
+                ret['comment'] += 'Node is set to authorize: {0}\n'.format(node)
+
     if not auth_required:
-      return ret
+        return ret
 
     if __opts__['test']:
-      ret['result'] = None
-      return ret
+        ret['result'] = None
+        return ret
 
     if '--force' not in extra_args:
-      extra_args += [ '--force' ]
+        extra_args += ['--force']
 
     authorize = __salt__['pcs.auth'](nodes=nodes, pcsuser=pcsuser, pcspasswd=pcspasswd, extra_args=extra_args)
     log.trace('Output of pcs.auth: ' + str(authorize))
 
     authorize_dict = {}
     for line in authorize['stdout'].splitlines():
-      node = line.split(':')[0].strip()
-      auth_state = line.split(':')[1].strip()
-      if node in nodes:
-        authorize_dict.update( { node : auth_state} )
+        node = line.split(':')[0].strip()
+        auth_state = line.split(':')[1].strip()
+        if node in nodes:
+            authorize_dict.update({node: auth_state})
     log.trace('authorize_dict: ' + str(authorize_dict))
 
     for node in nodes:
-      if node in authorize_dict and authorize_dict[node] == 'Authorized':
-        ret['comment'] += 'Authorized {0}\n'.format(node)
-        ret['changes'].update({node: {'old': '', 'new': 'Authorized'}})
-      else:
-        ret['result'] = False
-        if node in authorized_dict:
-          ret['comment'] += 'Authorization check for node {0} returned: {1}\n'.format(node, authorized_dict[node],)
-        if node in authorize_dict:
-          ret['comment'] += 'Failed to authorize {0} with error {1}\n'.format(node, authorize_dict[node],) 
+        if node in authorize_dict and authorize_dict[node] == 'Authorized':
+            ret['comment'] += 'Authorized {0}\n'.format(node)
+            ret['changes'].update({node: {'old': '', 'new': 'Authorized'}})
+        else:
+            ret['result'] = False
+            if node in authorized_dict:
+                ret['comment'] += 'Authorization check for node {0} returned: {1}\n'.format(node, authorized_dict[node])
+            if node in authorize_dict:
+                ret['comment'] += 'Failed to authorize {0} with error {1}\n'.format(node, authorize_dict[node])
 
     return ret
 
@@ -110,7 +109,7 @@ def auth(name, nodes, pcsuser='hacluster', pcspasswd='hacluster', extra_args=[])
 def cluster_setup(name, nodes, pcsclustername='pcscluster', extra_args=[]):
     '''
     Setup Pacemaker cluster on nodes.
-    Should be run on one cluster node only 
+    Should be run on one cluster node only
     (there may be races)
 
     name
@@ -124,56 +123,55 @@ def cluster_setup(name, nodes, pcsclustername='pcscluster', extra_args=[]):
     '''
 
     ret = {'name': name, 'result': True, 'comment': '', 'changes': {}}
-    result = {}
     setup_required = False
 
-    config_show  = __salt__['pcs.config_show']()
-    log.trace('Output of pcs.config_show: ' + str(config_show))
+    config_show = __salt__['pcs.config_show']()
+    log.trace('Output of pcs.config_show: '+str(config_show))
 
     for line in config_show['stdout'].splitlines():
-      if len(line.split(':')) in [ 2 ]:
-        key = line.split(':')[0].strip()
-        value = line.split(':')[1].strip()
-        if key in [ 'Cluster Name' ]: 
-          if value in [ pcsclustername ]:
-            ret['comment'] += 'Node is already set up\n'
-          else:
-            setup_required = True
-            if __opts__['test']:
-              ret['comment'] += 'Node is set to set up\n'
-    
+        if len(line.split(':')) in [2]:
+            key = line.split(':')[0].strip()
+            value = line.split(':')[1].strip()
+            if key in ['Cluster Name']:
+                if value in [pcsclustername]:
+                    ret['comment'] += 'Node is already set up\n'
+                else:
+                    setup_required = True
+                    if __opts__['test']:
+                        ret['comment'] += 'Node is set to set up\n'
+
     if not setup_required:
-      return ret
+        return ret
 
     if __opts__['test']:
-      ret['result'] = None
-      return ret
+        ret['result'] = None
+        return ret
 
     setup = __salt__['pcs.cluster_setup'](nodes=nodes, pcsclustername=pcsclustername, extra_args=extra_args)
     log.trace('Output of pcs.cluster_setup: ' + str(setup))
 
     setup_dict = {}
     for line in setup['stdout'].splitlines():
-      log.trace('line: ' + line)
-      log.trace('line.split(:).len: ' + str(len(line.split(':'))))
-      if len(line.split(':')) in [ 2 ]:
-        node = line.split(':')[0].strip()
-        setup_state = line.split(':')[1].strip()
-        if node in nodes:
-          setup_dict.update( { node : setup_state} )
+        log.trace('line: ' + line)
+        log.trace('line.split(:).len: ' + str(len(line.split(':'))))
+        if len(line.split(':')) in [2]:
+            node = line.split(':')[0].strip()
+            setup_state = line.split(':')[1].strip()
+            if node in nodes:
+                setup_dict.update({node: setup_state})
 
     log.trace('setup_dict: ' + str(setup_dict))
 
     for node in nodes:
-      if node in setup_dict and setup_dict[node] in [ 'Succeeded', 'Success' ]:
-        ret['comment'] += 'Set up {0}\n'.format(node)
-        ret['changes'].update({node: {'old': '', 'new': 'Setup'}})
-      else:
-        ret['result'] = False
-        ret['comment'] += 'Failed to setup {0}\n'.format(node)
-        if node in setup_dict:
-          ret['comment'] += '{0}: setup_dict: {1}\n'.format(node,setup_dict[node],)
-        ret['comment'] += str(setup)
+        if node in setup_dict and setup_dict[node] in ['Succeeded', 'Success']:
+            ret['comment'] += 'Set up {0}\n'.format(node)
+            ret['changes'].update({node: {'old': '', 'new': 'Setup'}})
+        else:
+            ret['result'] = False
+            ret['comment'] += 'Failed to setup {0}\n'.format(node)
+            if node in setup_dict:
+                ret['comment'] += '{0}: setup_dict: {1}\n'.format(node, setup_dict[node],)
+            ret['comment'] += str(setup)
 
     log.trace('ret: ' + str(ret))
 
@@ -182,7 +180,7 @@ def cluster_setup(name, nodes, pcsclustername='pcscluster', extra_args=[]):
 def cluster_node_add(name, node, extra_args=[]):
     '''
     Add a node to the Pacemaker cluster via PCS
-    Should be run on one cluster node only 
+    Should be run on one cluster node only
     (there may be races)
     Can only be run on a already setup/added node
 
@@ -195,77 +193,76 @@ def cluster_node_add(name, node, extra_args=[]):
     '''
 
     ret = {'name': name, 'result': True, 'comment': '', 'changes': {}}
-    result = {}
     node_add_required = True
-    current_nodes = [] 
+    current_nodes = []
 
-    is_member_cmd = [ 'pcs', 'status', 'nodes', 'corosync', ]
-    is_member  = __salt__['cmd.run_all'](is_member_cmd, output_loglevel='trace', python_shell=False)
+    is_member_cmd = ['pcs', 'status', 'nodes', 'corosync',]
+    is_member = __salt__['cmd.run_all'](is_member_cmd, output_loglevel='trace', python_shell=False)
     log.trace('Output of pcs status nodes corosync: ' + str(is_member))
 
     for line in is_member['stdout'].splitlines():
-      if len(line.split(':')) in [ 2 ]:
-        key = line.split(':')[0].strip()
-        value = line.split(':')[1].strip()
-        if key in [ 'Offline', 'Online' ]:
-          if len(value.split()) > 0:
-            if node in value.split():
-              node_add_required = False
-              ret['comment'] += 'Node {0} is already member of the cluster\n'.format(node)
-            else:
-              current_nodes += value.split()
+        if len(line.split(':')) in [2]:
+            key = line.split(':')[0].strip()
+            value = line.split(':')[1].strip()
+            if key in ['Offline', 'Online']:
+                if len(value.split()) > 0:
+                    if node in value.split():
+                        node_add_required = False
+                        ret['comment'] += 'Node {0} is already member of the cluster\n'.format(node)
+                    else:
+                        current_nodes += value.split()
 
     if not node_add_required:
-      return ret
+        return ret
 
     if __opts__['test']:
-      ret['result'] = None
-      ret['comment'] += 'Node {0} is set to be added to the cluster\n'.format(node)
-      return ret
+        ret['result'] = None
+        ret['comment'] += 'Node {0} is set to be added to the cluster\n'.format(node)
+        return ret
 
     node_add = __salt__['pcs.cluster_node_add'](node=node, extra_args=extra_args)
     log.trace('Output of pcs.cluster_node_add: ' + str(node_add))
 
     node_add_dict = {}
     for line in node_add['stdout'].splitlines():
-      log.trace('line: ' + line)
-      log.trace('line.split(:).len: ' + str(len(line.split(':'))))
-      if len(line.split(':')) in [ 2 ]:
-        current_node = line.split(':')[0].strip()
-        current_node_add_state = line.split(':')[1].strip()
-        if current_node in current_nodes + [ node ]:
-          node_add_dict.update( { current_node : current_node_add_state} )
+        log.trace('line: ' + line)
+        log.trace('line.split(:).len: ' + str(len(line.split(':'))))
+        if len(line.split(':')) in [2]:
+            current_node = line.split(':')[0].strip()
+            current_node_add_state = line.split(':')[1].strip()
+            if current_node in current_nodes + [node]:
+                node_add_dict.update({current_node: current_node_add_state})
     log.trace('node_add_dict: ' + str(node_add_dict))
 
     for current_node in current_nodes:
-      if current_node in node_add_dict:
-        if node_add_dict[current_node] not in [ 'Corosync updated' ]:
-          ret['result'] = False
-          ret['comment'] += 'Failed to update corosync.conf on node {0}\n'.format(current_node)
-          ret['comment'] += '{0}: node_add_dict: {1}\n'.format(current_node,node_add_dict[current_node],)
-      else:
-        ret['result'] = False
-        ret['comment'] += 'Failed to update corosync.conf on node {0}\n'.format(current_node)
+        if current_node in node_add_dict:
+            if node_add_dict[current_node] not in ['Corosync updated']:
+                ret['result'] = False
+                ret['comment'] += 'Failed to update corosync.conf on node {0}\n'.format(current_node)
+                ret['comment'] += '{0}: node_add_dict: {1}\n'.format(current_node, node_add_dict[current_node],)
+        else:
+            ret['result'] = False
+            ret['comment'] += 'Failed to update corosync.conf on node {0}\n'.format(current_node)
 
-    if node in node_add_dict and node_add_dict[node] in [ 'Succeeded', 'Success' ]:
-      ret['comment'] += 'Added node {0}\n'.format(node)
-      ret['changes'].update({node: {'old': '', 'new': 'Added'}})
+    if node in node_add_dict and node_add_dict[node] in ['Succeeded', 'Success']:
+        ret['comment'] += 'Added node {0}\n'.format(node)
+        ret['changes'].update({node: {'old': '', 'new': 'Added'}})
     else:
-      ret['result'] = False
-      ret['comment'] += 'Failed to add node{0}\n'.format(node)
-      if node in node_add_dict:
-        ret['comment'] += '{0}: node_add_dict: {1}\n'.format(node,node_add_dict[node],)
-      ret['comment'] += str(node_add)
+        ret['result'] = False
+        ret['comment'] += 'Failed to add node{0}\n'.format(node)
+        if node in node_add_dict:
+            ret['comment'] += '{0}: node_add_dict: {1}\n'.format(node, node_add_dict[node],)
+        ret['comment'] += str(node_add)
 
     log.trace('ret: ' + str(ret))
 
     return ret
 
-def stonith_created(name, stonith_id, stonith_device_type, stonith_device_options = []):
+def stonith_created(name, stonith_id, stonith_device_type, stonith_device_options=[]):
     '''
     Ensure that a fencing resource is created
 
-    Should be run on one cluster node only 
+    Should be run on one cluster node only
     (there may be races)
     Can only be run on a node with a functional pacemaker/corosync
 
@@ -280,36 +277,38 @@ def stonith_created(name, stonith_id, stonith_device_type, stonith_device_option
     '''
 
     ret = {'name': name, 'result': True, 'comment': '', 'changes': {}}
-    result = {}
     stonith_create_required = False
-    current_nodes = [] 
 
-    is_existing_cmd = [ 'pcs', 'stonith', 'show', stonith_id, ]
-    is_existing  = __salt__['cmd.run_all'](is_existing_cmd, output_loglevel='trace', python_shell=False)
+    is_existing_cmd = ['pcs', 'stonith', 'show', stonith_id,]
+    is_existing = __salt__['cmd.run_all'](is_existing_cmd, output_loglevel='trace', python_shell=False)
     log.trace('Output of pcs stonith show {0}: {1}'.format(stonith_id, str(is_existing)))
 
-    if is_existing['retcode'] in [ 0 ]:
-      ret['comment'] += 'Stonith resource {0} is already existing\n'.format(stonith_id)
+    if is_existing['retcode'] in [0]:
+        ret['comment'] += 'Stonith resource {0} is already existing\n'.format(stonith_id)
     else:
-      stonith_create_required = True
+        stonith_create_required = True
 
     if not stonith_create_required:
-      return ret
+        return ret
 
     if __opts__['test']:
-      ret['result'] = None
-      ret['comment'] += 'Stonith resource {0} is set to be created\n'.format(stonith_id)
-      return ret
+        ret['result'] = None
+        ret['comment'] += 'Stonith resource {0} is set to be created\n'.format(stonith_id)
+        return ret
 
-    stonith_create = __salt__['pcs.stonith_create'](stonith_id=stonith_id, stonith_device_type=stonith_device_type, stonith_device_options=stonith_device_options)
+    stonith_create = __salt__['pcs.stonith_create'](
+        stonith_id=stonith_id,
+        stonith_device_type=stonith_device_type,
+        stonith_device_options=stonith_device_options)
+
     log.trace('Output of pcs.stonith_create: ' + str(stonith_create))
 
-    if stonith_create['retcode'] in [ 0 ]:
-      ret['comment'] += 'Created stonith resource {0}\n'.format(stonith_id)
-      ret['changes'].update({stonith_id: {'old': '', 'new': stonith_id}})
+    if stonith_create['retcode'] in [0]:
+        ret['comment'] += 'Created stonith resource {0}\n'.format(stonith_id)
+        ret['changes'].update({stonith_id: {'old': '', 'new': stonith_id}})
     else:
-      ret['result'] = False
-      ret['comment'] += 'Failed to create stonith resource {0}\n'.format(stonith_id)
+        ret['result'] = False
+        ret['comment'] += 'Failed to create stonith resource {0}\n'.format(stonith_id)
 
     log.trace('ret: ' + str(ret))
 
