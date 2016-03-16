cbd868e12a900d35238090a82e1467f37578644a
twangboy <slee@saltstack.com>
2016-03-16 16:49:36 -0600

    Improve integration tests

--- tests/integration/modules/mac_xattr.py
+++ tests/integration/modules/mac_xattr.py
@@ -15,7 +15,8 @@ ensure_in_syspath('../../')
 import integration
 import salt.utils
 
-test_file = os.path.join(integration.TMP, 'xattr_test_file.txt')
+TEST_FILE = os.path.join(integration.TMP, 'xattr_test_file.txt')
+NO_FILE = os.path.join(integration.TMP, 'xattr_no_file.txt')
 
 
 class MacXattrModuleTest(integration.ModuleCase):
@@ -33,71 +34,147 @@ class MacXattrModuleTest(integration.ModuleCase):
         if not salt.utils.which('xattr'):
             self.skipTest('Test requires xattr binary')
 
-        self.run_function('file.touch', [test_file])
+        self.run_function('file.touch', [TEST_FILE])
 
     def tearDown(self):
         '''
         Clean up test file
         '''
-        if os.path.exists(test_file):
-            os.remove(test_file)
+        if os.path.exists(TEST_FILE):
+            os.remove(TEST_FILE)
 
-    def test_list_none(self):
+    def test_list_no_xattr(self):
         '''
         Make sure there are no attributes
         '''
-        self.run_function('xattr.clear', [test_file])
-        self.assertEqual(self.run_function('xattr.list', [test_file]), {})
+        # Clear existing attributes
+        self.assertTrue(self.run_function('xattr.clear', [TEST_FILE]))
+
+        # Test no attributes
+        self.assertEqual(self.run_function('xattr.list', [TEST_FILE]), {})
+
+        # Test file not found
+        self.assertEqual(self.run_function('xattr.list', [NO_FILE]),
+                         'ERROR: File not found: {0}'.format(NO_FILE))
 
     def test_write(self):
         '''
         Write an attribute
         '''
-        self.run_function('xattr.clear', [test_file])
-        self.run_function('xattr.write', [test_file, 'spongebob', 'squarepants'])
-        self.run_function('xattr.write', [test_file, 'squidward', 'plankton'])
-        self.run_function('xattr.write', [test_file, 'crabby', 'patty'])
+        # Clear existing attributes
+        self.assertTrue(self.run_function('xattr.clear', [TEST_FILE]))
+
+        # Write some attributes
+        self.assertTrue(
+            self.run_function('xattr.write',
+                              [TEST_FILE, 'spongebob', 'squarepants']))
+        self.assertTrue(
+            self.run_function('xattr.write',
+                              [TEST_FILE, 'squidward', 'plankton']))
+        self.assertTrue(
+            self.run_function('xattr.write',
+                              [TEST_FILE, 'crabby', 'patty']))
+
+        # Test that they were actually added
         self.assertEqual(
-            self.run_function('xattr.list', [test_file]),
+            self.run_function('xattr.list', [TEST_FILE]),
             {'spongebob': 'squarepants',
              'squidward': 'plankton',
              'crabby': 'patty'})
+
+        # Test file not found
+        self.assertEqual(
+            self.run_function('xattr.write', [NO_FILE, 'patrick', 'jellyfish']),
+            'ERROR: File not found: {0}'.format(NO_FILE))
 
     def test_read(self):
         '''
         Test xattr.read
         '''
-        self.run_function('xattr.clear', [test_file])
-        self.run_function('xattr.write', [test_file, 'spongebob', 'squarepants'])
+        # Clear existing attributes
+        self.assertTrue(self.run_function('xattr.clear', [TEST_FILE]))
+
+        # Write an attribute
+        self.assertTrue(
+            self.run_function('xattr.write',
+                              [TEST_FILE, 'spongebob', 'squarepants']))
+
+        # Read the attribute
         self.assertEqual(
-            self.run_function('xattr.read', [test_file, 'spongebob']),
+            self.run_function('xattr.read', [TEST_FILE, 'spongebob']),
             'squarepants')
+
+        # Test file not found
+        self.assertEqual(
+            self.run_function('xattr.read', [NO_FILE, 'spongebob']),
+            'ERROR: File not found: {0}'.format(NO_FILE))
+
+        # Test attribute not found
+        self.assertEqual(
+            self.run_function('xattr.read', [NO_FILE, 'patrick']),
+            'ERROR: Attribute not found: patrick')
 
     def test_delete(self):
         '''
         Test xattr.delete
         '''
-        self.run_function('xattr.clear', [test_file])
-        self.run_function('xattr.write', [test_file, 'spongebob', 'squarepants'])
-        self.run_function('xattr.write', [test_file, 'squidward', 'plankton'])
-        self.run_function('xattr.write', [test_file, 'crabby', 'patty'])
+        # Clear existing attributes
+        self.assertTrue(self.run_function('xattr.clear', [TEST_FILE]))
+
+        # Write some attributes
+        self.assertTrue(
+            self.run_function('xattr.write',
+                              [TEST_FILE, 'spongebob', 'squarepants']))
+        self.assertTrue(
+            self.run_function('xattr.write',
+                              [TEST_FILE, 'squidward', 'plankton']))
+        self.assertTrue(
+            self.run_function('xattr.write',
+                              [TEST_FILE, 'crabby', 'patty']))
+
+        # Delete an attribute
+        self.assertTrue(
+            self.run_function('xattr.delete', [TEST_FILE, 'squidward']))
+
+        # Make sure it was actually deleted
         self.assertEqual(
-            self.run_function('xattr.delete', [test_file, 'squidward']),
-            '')
-        self.assertEqual(
-            self.run_function('xattr.list', [test_file]),
+            self.run_function('xattr.list', [TEST_FILE]),
             {'spongebob': 'squarepants', 'crabby': 'patty'})
+
+        # Test file not found
+        self.assertEqual(
+            self.run_function('xattr.delete', [NO_FILE, 'spongebob']),
+            'ERROR: File not found: {0}'.format(NO_FILE))
+
+        # Test attribute not found
+        self.assertEqual(
+            self.run_function('xattr.delete', [NO_FILE, 'patrick']),
+            'ERROR: Attribute not found: patrick')
 
     def test_clear(self):
         '''
         Test xattr.clear
         '''
-        self.run_function('xattr.clear', [test_file])
-        self.run_function('xattr.write', [test_file, 'spongebob', 'squarepants'])
-        self.run_function('xattr.write', [test_file, 'squidward', 'plankton'])
-        self.run_function('xattr.write', [test_file, 'crabby', 'patty'])
-        self.assertEqual(self.run_function('xattr.clear', [test_file]), '')
-        self.assertEqual(self.run_function('xattr.list', [test_file]), {})
+        # Clear existing attributes
+        self.assertTrue(self.run_function('xattr.clear', [TEST_FILE]))
+
+        # Write some attributes
+        self.assertTrue(
+            self.run_function('xattr.write',
+                              [TEST_FILE, 'spongebob', 'squarepants']))
+        self.assertTrue(
+            self.run_function('xattr.write',
+                              [TEST_FILE, 'squidward', 'plankton']))
+        self.assertTrue(
+            self.run_function('xattr.write',
+                              [TEST_FILE, 'crabby', 'patty']))
+
+        # Test Clear
+        self.assertTrue(self.run_function('xattr.clear', [TEST_FILE]))
+
+        # Test file not found
+        self.assertEqual(self.run_function('xattr.clear', [NO_FILE]),
+                         'ERROR: File not found: {0}'.format(NO_FILE))
 
 
 if __name__ == '__main__':
