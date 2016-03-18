cd6fa6de723ba00d8a9413329544925a2c21671f
twangboy <slee@saltstack.com>
2016-03-18 13:59:36 -0600

    Fix mismatched variable names

--- salt/modules/mac_softwareupdate.py
+++ salt/modules/mac_softwareupdate.py
@@ -223,7 +223,7 @@ def schedule_enabled():
     except CommandExecutionError as exc:
         raise CommandExecutionError(exc)
 
-    enabled = out.split()[-1]
+    enabled = ret.split()[-1]
 
     return salt.utils.mac_utils.validate_enabled(enabled) == 'on'
 
