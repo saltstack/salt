bf782b1a4247f8545c7a0b56903e4dac28f1be3d
Bo Maryniuk <bo@suse.de>
2016-03-22 00:55:28 +0100

    Message to the log and/or raise an exception accordingly to the status of used function

--- salt/utils/decorators/__init__.py
+++ salt/utils/decorators/__init__.py
@@ -399,6 +399,30 @@ class _WithDeprecated(_DeprecationDecorator):
 
         def _decorate(*args, **kwargs):
             self._set_function(function)
+            if self._is_used_deprecated():
+                if self._curr_version < self._exp_version:
+                    msg = list()
+                    if self._with_name:
+                        msg.append('The function "{f_name}" is deprecated and will '
+                                   'expire in version "{version_name}".'.format(f_name=self._with_name,
+                                                                                version_name=self._exp_version_name))
+                    else:
+                        msg.append('The function is using its deprecated version and will '
+                                   'expire in version "{version_name}".'.format(version_name=self._exp_version_name))
+                    msg.append('Use its successor "{successor}" instead.'.format(successor=self._orig_f_name))
+                    log.warning(' '.join(msg))
+                else:
+                    msg_patt = 'The lifetime of the function "{f_name}" expired.'
+                    if '_' + self._orig_f_name == self._function.func_name:
+                        msg = [msg_patt.format(f_name=self._orig_f_name),
+                               'Please turn off its deprecated version in the configuration']
+                    else:
+                        msg = ['Although function "{f_name}" is called, an alias "{f_alias}" '
+                               'is configured as its deprecated version.'.format(f_name=self._orig_f_name,
+                                                                                 f_alias=self._with_name),
+                               msg_patt.format(f_name=self._with_name),
+                               'Please use its successor "{successor}" instead.'.format(successor=self._orig_f_name)]
+                    raise CommandExecutionError(' '.join(msg))
             return self._call_function(kwargs)
 
         return _decorate
