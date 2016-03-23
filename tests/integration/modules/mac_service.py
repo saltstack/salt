fef8f909198edef23bd66ea7cb97097573b596a4
twangboy <slee@saltstack.com>
2016-03-23 11:13:55 -0600

    Add remaining integration tests

--- tests/integration/modules/mac_service.py
+++ tests/integration/modules/mac_service.py
@@ -14,7 +14,8 @@ ensure_in_syspath('../../')
 import integration
 import salt.utils
 
-SERVICE_NAME = 'org.cups.cupsd'
+SERVICE_NAME = 'com.apple.apsd'
+SERVICE_ENABLED = False
 
 
 class MacServiceModuleTest(integration.ModuleCase):
@@ -26,6 +27,8 @@ class MacServiceModuleTest(integration.ModuleCase):
         '''
         Get current settings
         '''
+        global SERVICE_ENABLED
+
         if not salt.utils.is_darwin():
             self.skipTest('Test only available on Mac OS X')
 
@@ -38,11 +41,16 @@ class MacServiceModuleTest(integration.ModuleCase):
         if salt.utils.get_uid(salt.utils.get_user()) != 0:
             self.skipTest('Test requires root')
 
+        SERVICE_ENABLED = self.run_function('service.enabled', [SERVICE_NAME])
+
     def tearDown(self):
         '''
         Reset to original settings
         '''
-        pass
+        if SERVICE_ENABLED:
+            self.run_function('service.start', [SERVICE_NAME])
+        else:
+            self.run_function('service.stop', [SERVICE_NAME])
 
     def test_show(self):
         '''
@@ -77,7 +85,119 @@ class MacServiceModuleTest(integration.ModuleCase):
             ' Failed to error service',
             self.run_function('service.launchctl', ['error']))
 
-    def
+    def test_list(self):
+        '''
+        Test service.list
+        '''
+        # Expected Functionality
+        self.assertIn('PID', self.run_function('service.list'))
+        self.assertIn(
+            '{',
+            self.run_function('service.list', ['com.apple.coreservicesd']))
+
+        # Service not found
+        self.assertIn(
+            'Service not found',
+            self.run_function('service.list', ['spongebob']))
+
+    @destructiveTest
+    def test_start(self):
+        '''
+        Test service.start
+        Test service.stop
+        Test service.status
+        '''
+        self.assertTrue(self.run_function('service.start', [SERVICE_NAME]))
+
+        self.assertIn(
+            'Service not found',
+            self.run_function('service.start', ['spongebob']))
+
+    @destructiveTest
+    def test_stop(self):
+        '''
+        Test service.stop
+        '''
+        self.assertTrue(self.run_function('service.stop', [SERVICE_NAME]))
+
+        self.assertIn(
+            'Service not found',
+            self.run_function('service.stop', ['spongebob']))
+
+    @destructiveTest
+    def test_status(self):
+        '''
+        Test service.status
+        '''
+        # A running service
+        self.assertTrue(self.run_function('service.start', [SERVICE_NAME]))
+        self.assertTrue(
+            self.run_function('service.status', [SERVICE_NAME]).isdigit())
+
+        # A stopped service
+        self.assertTrue(self.run_function('service.stop', [SERVICE_NAME]))
+        self.assertEqual(
+            '',
+            self.run_function('service.status', [SERVICE_NAME]))
+
+        # Service not found
+        self.assertEqual('', self.run_function('service.status', ['spongebob']))
+
+    def test_available(self):
+        '''
+        Test service.available
+        '''
+        self.assertTrue(self.run_function('service.available', [SERVICE_NAME]))
+        self.assertFalse(self.run_function('service.available', ['spongebob']))
+
+    def test_missing(self):
+        '''
+        Test service.missing
+        '''
+        self.assertFalse(self.run_function('service.missing', [SERVICE_NAME]))
+        self.assertTrue(self.run_function('service.missing', ['spongebob']))
+
+    @destructiveTest
+    def test_enabled(self):
+        '''
+        Test service.enabled
+        '''
+        self.assertTrue(self.run_function('service.start', [SERVICE_NAME]))
+        self.assertTrue(self.run_function('service.enabled', [SERVICE_NAME]))
+
+        self.assertTrue(self.run_function('service.stop', [SERVICE_NAME]))
+        self.assertFalse(self.run_function('service.enabled', [SERVICE_NAME]))
+
+        self.assertFalse(self.run_function('service.enabled', ['spongebob']))
+
+    @destructiveTest
+    def test_disabled(self):
+        '''
+        Test service.disabled
+        '''
+        self.assertTrue(self.run_function('service.start', [SERVICE_NAME]))
+        self.assertFalse(self.run_function('service.disabled', [SERVICE_NAME]))
+
+        self.assertTrue(self.run_function('service.stop', [SERVICE_NAME]))
+        self.assertTrue(self.run_function('service.disabled', [SERVICE_NAME]))
+
+        self.assertTrue(self.run_function('service.enabled', ['spongebob']))
+
+    def test_get_all(self):
+        '''
+        Test service.get_all
+        '''
+        services = self.run_function('service.get_all')
+        self.assertIsInstance(services, list)
+        self.assertIn(SERVICE_NAME, services)
+
+    def test_get_enabled(self):
+        '''
+        Test service.get_enabled
+        '''
+        services = self.run_function('service.get_enabled')
+        self.assertIsInstance(services, list)
+        self.assertIn('com.apple.coreservicesd', services)
 
 
 if __name__ == '__main__':
