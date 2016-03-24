f8080b8e59d72cb700c7cf65d881742b3ed18fb6
Justin Anderson <janderson@saltstack.com>
2016-03-24 10:30:48 -0600

    Re-enable some skipped Boto tests

--- tests/unit/modules/boto_cloudtrail_test.py
+++ tests/unit/modules/boto_cloudtrail_test.py
@@ -125,7 +125,6 @@ class BotoCloudTrailTestCaseMixin(object):
     pass
 
 
-@skipIf(True, 'Skip these tests while investigating failures')
 @skipIf(HAS_BOTO is False, 'The boto module must be installed.')
 @skipIf(_has_required_boto() is False, 'The boto3 module must be greater than'
                                        ' or equal to version {0}'
