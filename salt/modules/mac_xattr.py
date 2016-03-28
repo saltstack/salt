a8a7d341196bcfd65893024a1447f210ec58f716
twangboy <slee@saltstack.com>
2016-03-28 15:25:46 -0600

    Change from 'such file' to 'No such file'

--- salt/modules/mac_xattr.py
+++ salt/modules/mac_xattr.py
@@ -166,9 +166,9 @@ def delete(path, attribute):
     try:
         salt.utils.mac_utils.execute_return_success(cmd)
     except CommandExecutionError as exc:
-        if 'such file' in exc.strerror:
+        if 'No such file' in exc.strerror:
             raise CommandExecutionError('File not found: {0}'.format(path))
-        if 'such xattr' in exc.strerror:
+        if 'No such xattr' in exc.strerror:
             raise CommandExecutionError('Attribute not found: {0}'.format(attribute))
         return False
 
