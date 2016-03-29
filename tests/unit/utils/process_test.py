b25e995efda338384d5153c981b5cd8b67f88d76
Justin Anderson <janderson@saltstack.com>
2016-03-29 10:57:46 -0600

    Lint fix

--- tests/unit/utils/process_test.py
+++ tests/unit/utils/process_test.py
@@ -5,11 +5,10 @@ from __future__ import absolute_import
 import os
 import time
 import signal
-import platform
 import multiprocessing
 
 # Import Salt Testing libs
-from salttesting import TestCase, skipIf
+from salttesting import TestCase
 from salttesting.helpers import ensure_in_syspath
 ensure_in_syspath('../../')
 
