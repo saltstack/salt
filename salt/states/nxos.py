0015ebae8b18765c51cfdc278c5d357c9c2e31b2
Daniel Wallace <dwallace@saltstack.com>
2016-03-29 16:39:52 -0500

    fix up roles for nxos proxy minion user

--- salt/states/nxos.py
+++ salt/states/nxos.py
@@ -2,53 +2,93 @@ def __virtual__():
     return 'nxos.cmd' in __salt__
 
 
-def user_present(name, password, encrypted=False, crypt_salt=None, algorithm='sha256', role=None):
+def user_present(name, password=None, roles=None, encrypted=False):
     ret = {'name': name,
            'result': False,
            'changes': {},
            'comment': ''}
 
-    change_password = not __salt__['nxos.cmd']('check_password',
-                                   username=name,
-                                   password=password,
-                                   encrypted=encrypted)
+    change_password = False
+    if password is not None:
+        change_password = not __salt__['nxos.cmd']('check_password',
+                                       username=name,
+                                       password=password,
+                                       encrypted=encrypted)
 
-    change_role = not True
+    change_roles = False
+    if roles is not None:
+        cur_roles = __salt__['nxos.cmd']('get_roles', username=name)
+        change_roles = set(roles) != cur_roles
 
     old_user = __salt__['nxos.cmd']('get_user', username=name)
 
-    if not any([change_password, change_role, not old_user]):
+    if not any([change_password, change_roles, not old_user]):
         ret['result'] = True
         ret['comment'] = 'User already exists'
         return ret
+
+    if change_roles is True:
+        remove_roles = cur_roles - set(roles)
+        add_roles = set(roles) - cur_roles
 
     if __opts__['test'] is True:
         ret['result'] = None
         if not old_user:
             ret['comment'] = 'User will be created'
-            ret['changes']['password'] = True
-            ret['changes']['role'] = True
+            if password is not None:
+                ret['changes']['password'] = True
+            if roles is not None:
+                ret['changes']['role'] = {'add': roles,
+                                          'remove': [],}
             return ret
         if change_password is True:
             ret['comment'] = 'User will be updated'
             ret['changes']['password'] = True
-        if change_role is True:
+        if change_roles is True:
             ret['comment'] = 'User will be updated'
-            ret['changes']['role'] = True
+            ret['changes']['roles'] = {'add': list(add_roles),
+                                      'remove': list(remove_roles)}
         return ret
 
     ret['changes']['old'] = old_user
-    if change_role is True:
-        ret['changes']['new'] = __salt__['nxos.cmd']('set_role',
-                                         username=name,
-                                         role=role)
     if change_password is True:
-        ret['changes']['new'] = __salt__['nxos.cmd']('set_password',
+        new_user = __salt__['nxos.cmd']('set_password',
                                          username=name,
                                          password=password,
                                          encrypted=encrypted,
-                                         crypt_salt=crypt_salt,
-                                         algorithm=algorithm,
-                                         role=role)
-    ret['result'] = True
+                                         role=roles[0] if roles else None)
+        ret['changes']['password'] = {
+            'new': new_user,
+            'old': old_user,
+        }
+    if change_roles is True:
+        for role in add_roles:
+            __salt__['nxos.cmd']('set_role', username=name, role=role)
+        for role in remove_roles:
+            __salt__['nxos.cmd']('unset_role', username=name, role=role)
+        ret['changes']['roles'] = {
+            'new': list(__salt__['nxos.cmd']('get_roles', username=name)),
+            'old': list(cur_roles),
+        }
+
+    correct_password = True
+    if password is not None:
+        correct_password = __salt__['nxos.cmd']('check_password',
+                                    username=name,
+                                    password=password,
+                                    encrypted=encrypted)
+
+    correct_roles = True
+    if roles is not None:
+        cur_roles = __salt__['nxos.cmd']('get_roles', username=name)
+        correct_roles = set(roles) != cur_roles
+
+    if not correct_roles:
+        ret['comment'] = 'Failed to set correct roles'
+    elif not correct_password:
+        ret['comment'] = 'Failed to set correct password'
+    else:
+        ret['comment'] = 'User set correctly'
+        ret['result'] = True
+
     return ret
