1c5b3ecac9954da55b243e723270f1562d39536d
Daniel Wallace <dwallace@saltstack.com>
2016-03-31 17:58:41 -0500

    include grains for nxos

--- salt/modules/nxos.py
+++ salt/modules/nxos.py
@@ -1,7 +1,9 @@
 from __future__ import absolute_import
+import contextlib
 import re
 
 import salt.utils
+from salt.utils.vt_helper import SSHConnection
 
 import logging
 log = logging.getLogger(__name__)
@@ -50,8 +52,30 @@ def _parse_plugins(data):
     return ret['plugins']
 
 
-def system_info():
-    data = __proxy__['nxos.sendline']('show ver')
+@contextlib.contextmanager
+def make_connection(opts=None):
+    if not opts:
+	if '__opts__' not in globals():
+	    raise
+	opts = __opts__
+    connection = SSHConnection(
+	host=opts['proxy']['host'],
+	username=opts['proxy']['username'],
+	password=opts['proxy']['password'],
+	key_accept=opts['proxy']['key_accept'],
+	ssh_args=opts['proxy']['ssh_args'],
+	prompt='{0}.*#'.format(opts['proxy']['hostname'])
+    )
+    yield connection
+    connection.close_connection()
+
+
+def system_info(opts=None):
+    with make_connection(opts=opts) as connection:
+	out, err = connection.sendline('terminal length 0')
+	out, err = connection.sendline('show ver')
+    _, out = out.split('\n', 1)
+    data, _, _ = out.rpartition('\n')
     info = {
         'software': _parse_software(data),
         'hardware': _parse_hardware(data),
