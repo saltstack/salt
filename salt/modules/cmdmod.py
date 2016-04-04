db7af998eedc712a6517c8533c24ff23fce7dd5a
Benjamin Drung <benjamin.drung@profitbricks.com>
2016-04-04 14:30:28 +0200

    Fix typo santized -> sanitized

--- salt/modules/cmdmod.py
+++ salt/modules/cmdmod.py
@@ -962,7 +962,7 @@ def shell(cmd,
 
         This passes the cmd argument directly to the shell
         without any further processing! Be absolutely sure that you
-        have properly santized the command passed to this function
+        have properly sanitized the command passed to this function
         and do not use untrusted inputs.
 
     Note that ``env`` represents the environment variables for the command, and
