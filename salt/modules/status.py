0d9a06b8c6f5f6e3805697322e881a4259f50c1d
Bo Maryniuk <bo@maryniuk.net>
2016-04-06 20:55:45 +0200

    Cleaner deprecation process with decorators
    * Add deprecation decorator scaffold
    
    * Capture type error and unhandled exceptions while function calls
    
    * Aware of the current and future version of deprecation
    
    * Implement initially is_deprecated decorator
    
    * Add an alias for the capitalization
    
    * Fix capitalization easier way
    
    * Remove an extra line
    
    * Add successor name to the deprecation decorator.
    
    * Granulate logging and error messages.
    
    * Implement function swapper
    
    * Raise later the caught exception
    
    * Clarify exception message
    
    * Save function original name
    
    * Remove an extra line
    
    * Hide an alternative hidden function name in the error message, preserving the error itself
    
    * Rename variable as private
    
    * Add a method to detect if a function is using its previous version
    
    * Message to the log and/or raise an exception accordingly to the status of used function
    
    * Log an error along with the exception
    
    * Add internal method documentation
    
    * Add documentation and usage process for decorator "is_deprecated"
    
    * Add documentation and process usage for the decorator "with_deprecated"
    
    * Hide private method name
    
    * Fix PEP8, re-word the error message
    
    * Deprecate basic uptime function
    
    * Add initial decorator unit test
    
    * Rename old/new functions, mock versions
    
    * Move frequent data to the test setup
    
    * Add logging on EOL exception
    
    * Rename and document high to low version test on is_deprecated
    
    * Implement a test on low to high version of is_deprecated decorator
    
    * Add a correction to the test description
    
    * Remove a dead code
    
    * Implement a test for high to low version on is_deprecated, using with_successor param
    
    * Correct typso adn mistaeks
    
    * Implement high to low version with successor param on is_deprecated
    
    * Setup a virtual name for the module
    
    * Implement test for with_deprecated should raise an exception if same deprecated function not found
    
    * Implement test for with_deprecated an old function is picked up if configured
    
    * Correct test description purpose
    
    * Implement test with_deprecated when no deprecation is requested
    
    * Add logging test to the configured deprecation request
    
    * Add logging testing when deprecated version wasn't requested
    
    * Implement test EOL for with_deprecated decorator
    
    * Correct test explanation
    
    * Rename the test
    
    * Implement with_deprecated no EOL, deprecated other function name
    
    * Implement with_deprecated, deprecated other function name, EOL reached
    
    * Add test description for the with_deprecated + with_name + EOL
    
    * Fix confusing test names
    
    * Add logging test to the is_deprecated decorator when function as not found.
    
    * Add more test point to each test, remove empty lines
    
    * Bugfix: at certain conditions a wrong alias name is reported to the log
    
    * Fix a typo in a comment
    
    * Add test for the logging
    
    * Disable a pylint: None will _never_ be raised
    
    * Fix test for the deprecated "status.uptime" version
    
    * Bugfix: Do not yank raised exceptions
    
    * Remove unnecessary decorator
    
    * Add test for the new uptime
    
    * Add test for the new uptime fails when /proc/uptime does not exists
    
    * Rename old test case
    
    * Skip test for the UTC time, unless freeze time is used.
    
    * Fix pylint
    
    * Fix documentation
    
    * Bugfix: proxy-pass the docstring of the decorated function
    
    * Lint fix

--- salt/modules/status.py
+++ salt/modules/status.py
@@ -11,6 +11,8 @@ import os
 import re
 import fnmatch
 import collections
+import time
+import datetime
 
 # Import 3rd-party libs
 import salt.ext.six as six
@@ -23,6 +25,8 @@ import salt.utils.event
 from salt.utils.network import host_to_ip as _host_to_ip
 from salt.utils.network import remote_port_tcp as _remote_port_tcp
 from salt.ext.six.moves import zip
+from salt.utils.decorators import with_deprecated
+from salt.exceptions import CommandExecutionError
 
 __virtualname__ = 'status'
 __opts__ = {}
@@ -30,7 +34,8 @@ __opts__ = {}
 
 def __virtual__():
     if salt.utils.is_windows():
-        return (False, 'Cannot load status module on windows')
+        return False, 'Windows platform is not supported by this module'
+
     return __virtualname__
 
 
@@ -120,7 +125,38 @@ def custom():
     return ret
 
 
-def uptime(human_readable=True):
+@with_deprecated(globals(), "Boron")
+def uptime():
+    '''
+    Return the uptime for this system.
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' status.uptime
+    '''
+    ut_path = "/proc/uptime"
+    if not os.path.exists(ut_path):
+        raise CommandExecutionError("File {ut_path} was not found.".format(ut_path=ut_path))
+
+    ut_ret = {
+        'seconds': int(float(open(ut_path).read().strip().split()[0]))
+    }
+
+    utc_time = datetime.datetime.utcfromtimestamp(time.time() - ut_ret['seconds'])
+    ut_ret['since_iso'] = utc_time.isoformat()
+    ut_ret['since_t'] = time.mktime(utc_time.timetuple())
+    ut_ret['days'] = ut_ret['seconds'] / 60 / 60 / 24
+    hours = (ut_ret['seconds'] - (ut_ret['days'] * 24 * 60 * 60)) / 60 / 60
+    minutes = ((ut_ret['seconds'] - (ut_ret['days'] * 24 * 60 * 60)) / 60) - hours * 60
+    ut_ret['time'] = '{0}:{1}'.format(hours, minutes)
+    ut_ret['users'] = len(__salt__['cmd.run']("who -s").split(os.linesep))
+
+    return ut_ret
+
+
+def _uptime(human_readable=True):
     '''
     Return the uptime for this minion
 
