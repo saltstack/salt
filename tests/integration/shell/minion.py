d172dd2b386664354d06711f67a38d901d774c59
Thayne Harbaugh <tharbaug@adobe.com>
2016-04-11 19:03:26 -0600

    Change timeout strategy in init script.

--- tests/integration/shell/minion.py
+++ tests/integration/shell/minion.py
@@ -121,7 +121,7 @@ class MinionTest(integration.ShellCase, integration.ShellCaseCommonTestsMixIn):
             [action],
             catch_stderr=True,
             with_retcode=True,
-            timeout=360,
+            timeout=300,
         )
 
         # Check minion state
@@ -193,7 +193,7 @@ class MinionTest(integration.ShellCase, integration.ShellCaseCommonTestsMixIn):
         with open(os.path.join(default_dir, 'salt'), 'w') as defaults:
             # Test suites is quite slow - extend the timeout
             defaults.write(
-                'TIMEOUT=270\n'
+                'TIMEOUT=230\n'
             )
 
         init_script = testprogram.TestProgram(
