ad0e94983b01c6e5a8ce45bde6d6f74ac5e7d0e3
Justin Findlay <jfindlay@gmail.com>
2016-04-13 02:33:09 -0600

    salt.version: case insensitive ordering of deps

--- salt/version.py
+++ salt/version.py
@@ -650,7 +650,8 @@ def versions_report(include_salt_cloud=False):
     info = []
     for ver_type in ('Salt Version', 'Dependency Versions', 'System Versions'):
         info.append('{0}:'.format(ver_type))
-        for name in sorted(ver_info[ver_type]):
+        # List dependencies in alphabetical, case insensitive order
+        for name in sorted(ver_info[ver_type], cmp=lambda x, y: cmp(x.lower(), y.lower())):
             ver = fmt.format(name,
                              ver_info[ver_type][name] or 'Not Installed',
                              pad=padding)
