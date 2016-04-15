30c8f28784b59f442f300f4d115fa0438cd3df0e
Vincas Dargis <vindrg@gmail.com>
2016-04-15 17:41:41 +0300

    Fix comments value in salt.states.pkgrepo example (#32604)
    'comments' option adds '#' automatically. Example contains `#http://mirror.centos.org/centos/$releasever/os/$basearch/` string which becomes prefixed with '##' in generated file.

--- salt/states/pkgrepo.py
+++ salt/states/pkgrepo.py
@@ -13,7 +13,7 @@ these states. Here is some example SLS:
         - humanname: CentOS-$releasever - Base
         - mirrorlist: http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os
         - comments:
-            - '#http://mirror.centos.org/centos/$releasever/os/$basearch/'
+            - 'http://mirror.centos.org/centos/$releasever/os/$basearch/'
         - gpgcheck: 1
         - gpgkey: file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6
 
