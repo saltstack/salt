12a74c24a370e8473bd7b386a911be11577db41c
Anthony Shaw <anthony.p.shaw@gmail.com>
2016-04-29 16:10:23 -0400

    Cisco NSO Proxy Minion (#32911)
    * PSGet module for managing powershellgallery.com packages
    
    * Linting fixes
    
    * Converted docstrings to sphinx format
    
    * Update LF
    
    * Added servicenow execution module
    
    * Fixed up the linting
    
    * Fix docstrings
    
    * arbitrary change to reset the build
    
    * Setup basic unit test runner
    
    * Setup basic loader mock and wrapper for declaring dependent packages
    
    * Updated unit test runner and setup my first test example
    
    * Update the servicenow module after a bad merge commit
    
    * Code comments for the base unit test runners
    
    * Fix linting issues
    
    * remove empty line endings
    
    * Added Cisco NSO Proxy for PR review
    
    * Added RST for autodoc of new module
    
    * Fix from comments in PR review @rallytime
    
    * Added execution module for NSO proxy
    
    * Revert "Merge branch 'unittest' into cisco_nso"
    
    This reverts commit b93c0aa6913fb90d35d8cbe94533160c91dcef1a, reversing
    changes made to cd50386031d61cdb630f7ef169062be8b3de6912.
    
    * Added state for check config present
    
    * Added autodoc for new modules
    
    * fix lint issues
    
    * Do a diff on the values
    
    * Fix linting issues

---
+++ salt/states/cisconso.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+'''
+State module for Cisco NSO Proxy minions
+
+.. versionadded: Carbon
+
+For documentation on setting up the cisconso proxy minion look in the documentation
+for :doc:`salt.proxy.cisconso</ref/proxy/all/salt.proxy.cisconso>`.
+'''
+
+
+def __virtual__():
+    return 'cisconso.set_data_value' in __salt__
+
+
+def value_present(name, datastore, path, config):
+    '''
+    Ensure a specific value exists at a given path
+
+    :param name: The name for this rule
+    :type  name: ``str``
+
+    :param datastore: The datastore, e.g. running, operational.
+        One of the NETCONF store IETF types
+    :type  datastore: :class:`DatastoreType` (``str`` enum).
+
+    :param path: The device path to set the value at,
+        a list of element names in order, / seperated
+    :type  path: ``list``, ``str`` OR ``tuple``
+
+    :param config: The new value at the given path
+    :type  config: ``dict``
+
+    Examples:
+
+    .. code-block:: yaml
+
+        enable pap auth:
+          cisconso.config_present:
+            - name: enable_pap_auth
+            - datastore: running
+            - path: devices/device/ex0/config/sys/interfaces/serial/ppp0/authentication
+            - config:
+                authentication:
+                    method: pap
+                    "list-name": foobar
+
+    '''
+    ret = {'name': name,
+           'result': False,
+           'changes': {},
+           'comment': ''}
+
+    existing = __salt__['cisconso.get_data'](datastore, path)
+
+    if cmp(existing, config):
+        ret['result'] = True
+        ret['comment'] = 'Config is already set'
+
+    elif __opts__['test'] is True:
+        ret['result'] = None
+        ret['comment'] = 'Config will be added'
+        diff = _DictDiffer(existing, config)
+        ret['changes']['new'] = diff.added()
+        ret['changes']['removed'] = diff.removed()
+        ret['changes']['changed'] = diff.changed()
+
+    else:
+        __salt__['cisconso.set_data_value'](datastore, path, config)
+        ret['result'] = True
+        ret['comment'] = 'Successfully added config'
+        diff = _DictDiffer(existing, config)
+        ret['changes']['new'] = diff.added()
+        ret['changes']['removed'] = diff.removed()
+        ret['changes']['changed'] = diff.changed()
+
+    return ret
+
+
+class _DictDiffer(object):
+    '''
+    Calculate the difference between two dictionaries as:
+    (1) items added
+    (2) items removed
+    (3) keys same in both but changed values
+    (4) keys same in both and unchanged values
+    '''
+    def __init__(self, current_dict, past_dict):
+        self.current_dict, self.past_dict = current_dict, past_dict
+        self.set_current, self.set_past = set(current_dict.keys()), set(past_dict.keys())
+        self.intersect = self.set_current.intersection(self.set_past)
+
+    def added(self):
+        return self.set_current - self.intersect
+
+    def removed(self):
+        return self.set_past - self.intersect
+
+    def changed(self):
+        return set(o for o in self.intersect if self.past_dict[o] != self.current_dict[o])
+
+    def unchanged(self):
+        return set(o for o in self.intersect if self.past_dict[o] == self.current_dict[o])
