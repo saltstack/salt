07e38bc9beb2de7566dd4dbf57c5830f1978117a
Justin Findlay <jfindlay@gmail.com>
2016-04-29 11:47:01 -0600

    salt.log.setup: process user args before format (#32796)
    * salt.log.setup: process user args before format
    
    Before:
        [DEBUG   ] too many %s
    After:
        [DEBUG   ] too many secrets
    
    * salt.log.setup: allow SaltLogRecord attrs on console

--- salt/log/setup.py
+++ salt/log/setup.py
@@ -141,11 +141,11 @@ class SaltLogRecord(logging.LogRecord):
         # pylint: enable=E1321
 
 
-class SaltColorLogRecord(logging.LogRecord):
+class SaltColorLogRecord(SaltLogRecord):
     def __init__(self, *args, **kwargs):
-        logging.LogRecord.__init__(self, *args, **kwargs)
-        reset = TextFormat('reset')
+        SaltLogRecord.__init__(self, *args, **kwargs)
 
+        reset = TextFormat('reset')
         clevel = LOG_COLORS['levels'].get(self.levelname, reset)
         cmsg = LOG_COLORS['msgs'].get(self.levelname, reset)
 
@@ -159,7 +159,7 @@ class SaltColorLogRecord(logging.LogRecord):
         self.colorprocess = '%s[%5s]%s' % (LOG_COLORS['process'],
                                            self.process,
                                            reset)
-        self.colormsg = '%s%s%s' % (cmsg, self.msg, reset)
+        self.colormsg = '%s%s%s' % (cmsg, self.getMessage(), reset)
         # pylint: enable=E1321
 
 
