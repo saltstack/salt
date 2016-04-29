87b70186c32be9b87c070911ec1e6fdef752a7fe
Nicolas Delaby <ticosax@free.fr>
2016-04-29 19:48:20 +0200

    When move a function to a new namespace, allow to keep original context. (#32943)

--- salt/utils/__init__.py
+++ salt/utils/__init__.py
@@ -2030,13 +2030,22 @@ def get_hash(path, form='sha256', chunk_size=65536):
         return hash_obj.hexdigest()
 
 
-def namespaced_function(function, global_dict, defaults=None):
+def namespaced_function(function, global_dict, defaults=None, preserve_context=False):
     '''
     Redefine (clone) a function under a different globals() namespace scope
+
+        preserve_context:
+            Allow to keep the context taken from orignal namespace,
+            and extend it with globals() taken from
+            new targetted namespace.
     '''
     if defaults is None:
         defaults = function.__defaults__
 
+    if preserve_context:
+        _global_dict = function.__globals__.copy()
+        _global_dict.update(global_dict)
+        global_dict = _global_dict
     new_namespaced_function = types.FunctionType(
         function.__code__,
         global_dict,
