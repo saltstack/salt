5f392856e69c5fd7c14fd73570cd8b360b7ea6b4
anthony-shaw <anthonyshaw@apache.org>
2016-04-29 12:54:00 -0400

    Fix linting issues

--- salt/states/cisconso.py
+++ salt/states/cisconso.py
@@ -89,11 +89,15 @@ class _DictDiffer(object):
         self.current_dict, self.past_dict = current_dict, past_dict
         self.set_current, self.set_past = set(current_dict.keys()), set(past_dict.keys())
         self.intersect = self.set_current.intersection(self.set_past)
+
     def added(self):
-        return self.set_current - self.intersect 
+        return self.set_current - self.intersect
+
     def removed(self):
-        return self.set_past - self.intersect 
+        return self.set_past - self.intersect
+
     def changed(self):
         return set(o for o in self.intersect if self.past_dict[o] != self.current_dict[o])
+
     def unchanged(self):
         return set(o for o in self.intersect if self.past_dict[o] == self.current_dict[o])
