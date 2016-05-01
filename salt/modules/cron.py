08962b82f86f44422a9e12df3439a94212d51601
Jorge Schrauwen <sjorge@blackdot.be>
2016-05-01 12:47:26 +0000

    minor tweak to make merging into 2016.3 and develop easier

--- salt/modules/cron.py
+++ salt/modules/cron.py
@@ -155,7 +155,7 @@ def _get_cron_cmdstr(path, user=None):
     '''
     cmd = 'crontab'
 
-    if __grains__.get('os_family') not in ('Solaris', 'AIX') and user:
+    if user and __grains__.get('os_family') not in ('Solaris', 'AIX'):
         cmd += ' -u {0}'.format(user)
 
     return '{0} {1}'.format(cmd, path)
