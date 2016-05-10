878d34a865cff1dc1934507984d76e3bbd1a0d37
Seth House <seth@eseth.com>
2016-05-10 08:25:13 -0600

    Doc mock decorators (#33132)
    * Add mock function for mocking decorators
    
    * Mock the stdlib user module because importing it will open the repl

--- doc/conf.py
+++ doc/conf.py
@@ -42,6 +42,9 @@ class Mock(object):
 # pylint: enable=R0903
 
 MOCK_MODULES = [
+    # Python stdlib
+    'user',
+
     # salt core
     'Crypto',
     'Crypto.Signature',
@@ -115,8 +118,28 @@ MOCK_MODULES = [
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
+def mock_decorator_with_params(*oargs, **okwargs):
+    '''
+    Optionally mock a decorator that takes parameters
+
+    E.g.:
+
+    @blah(stuff=True)
+    def things():
+        pass
+    '''
+    def inner(fn, *iargs, **ikwargs):
+        if hasattr(fn, '__call__'):
+            return fn
+        else:
+            return Mock()
+    return inner
+
 # Define a fake version attribute for libcloud so docs build as supposed
 sys.modules['libcloud'].__version__ = '0.0.0'
+
+# Define a fake version attribute for the following libs.
+sys.modules['cherrypy'].config = mock_decorator_with_params
 
 
 # -- Add paths to PYTHONPATH ---------------------------------------------------
