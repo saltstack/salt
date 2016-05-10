f4c837069a75277df149321be4beb7bb8f8b1d09
lalmeras <lalmeras@gmail.com>
2016-05-11 00:27:27 +0200

    propagate opts to salt.util.http call

--- salt/fileclient.py
+++ salt/fileclient.py
@@ -603,6 +603,7 @@ class Client(object):
                 header_callback=on_header,
                 username=url_data.username,
                 password=url_data.password,
+                opts=self.opts,
                 **get_kwargs
             )
             if 'handle' not in query:
