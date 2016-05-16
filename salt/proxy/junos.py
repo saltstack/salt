a7e1ec155e95fea316653b8d9c4a56b2db7591d3
rallytime <nicole@saltstack.com>
2016-05-16 13:48:13 -0600

    Pylint fix

--- salt/proxy/junos.py
+++ salt/proxy/junos.py
@@ -7,7 +7,6 @@ Interface with a Junos device via proxy-minion.
 from __future__ import absolute_import
 from __future__ import print_function
 import logging
-import json
 
 # Import 3rd-party libs
 try:
