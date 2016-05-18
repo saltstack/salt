5d210aa02813f6d9caf3d616d7d0be5f6a9b4289
Mike Place <mp@saltstack.com>
2016-05-18 15:39:57 -0600

    Ponies!

---
+++ salt/output/pony.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+'''
+Display Pony output data structure
+=================================
+
+Display output from a pony. Ponies are better than cows
+because everybody wants a pony.
+
+Example output::
+
+'''
+
+# Import Python libs
+from __future__ import absolute_import
+
+# Import Salt libs
+import salt.utils.locales
+import subprocess
+
+def output(data):
+    '''
+    Rather basic....
+    '''
+    return(subprocess.check_output(['ponysay', salt.utils.locales.sdecode(data)]))
