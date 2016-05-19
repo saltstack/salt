8aef134e7ec09f4d4f71b90268d3a0b52e60777d
twangboy <slee@saltstack.com>
2016-05-19 16:20:42 -0600

    Fix cmd_yaml to work on windows

--- salt/pillar/cmd_yaml.py
+++ salt/pillar/cmd_yaml.py
@@ -9,7 +9,6 @@ from __future__ import absolute_import
 
 # Import python libs
 import logging
-from subprocess import check_output
 
 # Import third party libs
 import yaml
@@ -26,7 +25,8 @@ def ext_pillar(minion_id,  # pylint: disable=W0613
     '''
     try:
         command = command.replace('%s', minion_id)
-        return yaml.safe_load(check_output(r'{0}'.format(command), shell=True))
+        return yaml.safe_load(
+            __salt__['cmd.run_stdout'](r'{0}'.format(command), python_shell=True))
     except Exception:
         log.critical('YAML data from {0} failed to parse'.format(command))
         return {}
