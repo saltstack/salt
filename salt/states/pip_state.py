be1a7659a3b3ebcf0aaa46f5138112ef7fbbb26e
Thomas S Hatch <thatch45@gmail.com>
2016-05-19 16:33:15 -0600

    maintain the fallabck because I am totally sick of this crap

--- salt/states/pip_state.py
+++ salt/states/pip_state.py
@@ -165,21 +165,24 @@ def _check_pkg_version_format(pkg):
             )
         )
         return ret
-
-    if install_req.req is None:
-        # This is most likely an url and there's no way to know what will
-        # be installed before actually installing it.
-        ret['result'] = True
-        ret['prefix'] = ''
-        ret['version_spec'] = []
-    else:
-        ret['result'] = True
-        ret['prefix'] = re.sub('[^A-Za-z0-9.]+', '-', install_req.name)
-        if hasattr(install_req, "specifier"):
-            specifier = install_req.specifier
+    try:
+        ret['prefix'] = install_req.req.project_name
+        ret['version_spec'] = install_req.req.specs
+    except Exception:
+        if install_req.req is None:
+            # This is most likely an url and there's no way to know what will
+            # be installed before actually installing it.
+            ret['result'] = True
+            ret['prefix'] = ''
+            ret['version_spec'] = []
         else:
-            specifier = install_req.req.specifier
-        ret['version_spec'] = [(spec.operator, spec.version) for spec in specifier]
+            ret['result'] = True
+            ret['prefix'] = re.sub('[^A-Za-z0-9.]+', '-', install_req.name)
+            if hasattr(install_req, "specifier"):
+                specifier = install_req.specifier
+            else:
+                specifier = install_req.req.specifier
+            ret['version_spec'] = [(spec.operator, spec.version) for spec in specifier]
 
     return ret
 
