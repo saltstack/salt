3320a2e7cc37a55438dbaa6576c38ce6fde24767
Luitzen van Gorkum <luitzen.vangorkum@duo.nl>
2016-05-20 14:14:13 +0200

    Fix LVM parameter devices as a pure list. Comma seperated lists are converted as list for backwards compatibility.
    Example:
    
    vg_test:
      lvm.vg_present:
        - devices:
          - /dev/vdb
          - /dev/vdc
    
    Old way te do it:
    
    vg_test:
      lvm.vg_present:
        - devices: /dev/vdb,/dev/vdc
    
    This fix also includes pv devices

--- salt/states/lvm.py
+++ salt/states/lvm.py
@@ -121,10 +121,12 @@ def vg_present(name, devices=None, **kwargs):
            'comment': '',
            'name': name,
            'result': True}
+    if isinstance(devices, str):
+        devices = devices.split(',')
 
     if __salt__['lvm.vgdisplay'](name):
         ret['comment'] = 'Volume Group {0} already present'.format(name)
-        for device in devices.split(','):
+        for device in devices:
             realdev = os.path.realpath(device)
             pvs = __salt__['lvm.pvdisplay'](realdev, real=True)
             if pvs and pvs.get(realdev, None):
