f8ca6ce6b091df8a86f90a984e32b37cdf668967
Nicole Thomas <nicole@saltstack.com>
2016-05-20 14:48:15 -0600

    Fix traceback in logging for config validation (#33386) (#33405)

--- salt/config.py
+++ salt/config.py
@@ -124,7 +124,11 @@ VALID_OPTS = {
     'log_datefmt': str,
     'log_datefmt_logfile': str,
     'log_fmt_console': str,
-    'log_fmt_logfile': tuple,
+
+    # The format for a given log file
+    'log_fmt_logfile': (tuple, str),
+
+    # A dictionary of logging levels
     'log_granular_levels': dict,
     'max_event_size': int,
     'test': bool,
@@ -727,11 +731,24 @@ def _validate_opts(opts):
             # passed.
             return valid_type.__name__
         else:
-            if num_types == 1:
-                return valid_type.__name__
-            elif num_types > 1:
-                ret = ', '.join(x.__name__ for x in valid_type[:-1])
-                ret += ' or ' + valid_type[-1].__name__
+            def get_types(types, type_tuple):
+                for item in type_tuple:
+                    if isinstance(item, tuple):
+                        get_types(types, item)
+                    else:
+                        try:
+                            types.append(item.__name__)
+                        except AttributeError:
+                            log.warning(
+                                'Unable to interpret type %s while validating '
+                                'configuration', item
+                            )
+            types = []
+            get_types(types, valid_type)
+
+            ret = ', '.join(types[:-1])
+            ret += ' or ' + types[-1]
+            return ret
 
     errors = []
 
@@ -772,7 +789,7 @@ def _validate_opts(opts):
                 err.format(key,
                            val,
                            type(val).__name__,
-                           format_multi_opt(VALID_OPTS[key].__name__))
+                           format_multi_opt(VALID_OPTS[key]))
             )
 
     for error in errors:
