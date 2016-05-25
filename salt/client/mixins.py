saltstack/salt

commit df6296e3ce71b0f41bd5e32d50c627a056310233

Replace args and kwargs (plural) by arg and kwarg (singular) for ``RunnerClient`` (#32938)
* Replace args and kwargs (plural) by arg and kwarg (singular)

* Add test and deprecation warning

* Add deprecation warning

salt/client/mixins.py
@@ -20,14 +20,15 @@ import salt.utils.jid
 import salt.utils.job
 import salt.transport
 import salt.log.setup
+import salt.ext.six as six
 from salt.utils.error import raise_error
 from salt.utils.event import tagify
 from salt.utils.doc import strip_rst as _strip_rst
 from salt.utils.lazy import verify_fun
 from salt.utils.process import default_signals, SignalHandlingMultiprocessingProcess
+from salt.utils import warn_until
 
 # Import 3rd-party libs
-import salt.ext.six as six
 import tornado.stack_context
 
 log = logging.getLogger(__name__)
@@ -226,8 +227,8 @@ class SyncClientMixin(object):
             self.functions[fun], arglist, pub_data
         )
         low = {'fun': fun,
-               'args': args,
-               'kwargs': kwargs}
+               'arg': args,
+               'kwarg': kwargs}
         return self.low(fun, low)
 
     @property
@@ -237,6 +238,23 @@ class SyncClientMixin(object):
         return self._mminion
 
     def low(self, fun, low):
+        '''
+        Check for deprecated usage and allow until Salt Oxygen.
+        '''
+        msg = []
+        if 'args' in low:
+            msg.append('call with arg instead')
+            low['arg'] = low.pop('args')
+        if 'kwargs' in low:
+            msg.append('call with kwarg instead')
+            low['kwarg'] = low.pop('kwargs')
+
+        if msg:
+            warn_until('Oxygen', ' '.join(msg))
+
+        return self._low(fun, low)
+
+    def _low(self, fun, low):
         '''
         Execute a function from low data
         Low data includes:
@@ -313,23 +331,23 @@ class SyncClientMixin(object):
             # we make the transition we will load "kwargs" using format_call if
             # there are no kwargs in the low object passed in
             f_call = None
-            if 'args' not in low:
+            if 'arg' not in low:
                 f_call = salt.utils.format_call(
                     self.functions[fun],
                     low,
                     expected_extra_kws=CLIENT_INTERNAL_KEYWORDS
                 )
-                args = f_call.get('args', ())
+                args = f_call.get('arg', ())
             else:
-                args = low['args']
-            if 'kwargs' not in low:
+                args = low['arg']
+            if 'kwarg' not in low:
                 if f_call is None:
                     f_call = salt.utils.format_call(
                         self.functions[fun],
                         low,
                         expected_extra_kws=CLIENT_INTERNAL_KEYWORDS
                     )
-                kwargs = f_call.get('kwargs', {})
+                kwargs = f_call.get('kwarg', {})
 
                 # throw a warning for the badly formed low data if we found
                 # kwargs using the old mechanism
@@ -339,7 +357,7 @@ class SyncClientMixin(object):
                         'kwargs must be passed inside the low under "kwargs"'
                     )
             else:
-                kwargs = low['kwargs']
+                kwargs = low['kwarg']
 
             # Initialize a context for executing the method.
             with tornado.stack_context.StackContext(self.functions.context_dict.clone):

salt/runner.py
@@ -63,7 +63,7 @@ class RunnerClient(mixins.SyncClientMixin, mixins.AsyncClientMixin, object):
         reformatted_low = {'fun': fun}
         reformatted_low.update(auth_creds)
         # Support old style calls where arguments could be specified in 'low' top level
-        if not low.get('args') and not low.get('kwargs'):  # not specified or empty
+        if not low.get('arg') and not low.get('kwarg'):  # not specified or empty
             verify_fun(self.functions, fun)
             merged_args_kwargs = salt.utils.args.condition_input([], low)
             parsed_input = salt.utils.args.parse_input(merged_args_kwargs)
@@ -73,12 +73,12 @@ class RunnerClient(mixins.SyncClientMixin, mixins.AsyncClientMixin, object):
                 self.opts,
                 ignore_invalid=True
             )
-            low['args'] = args
-            low['kwargs'] = kwargs
-        if 'kwargs' not in low:
-            low['kwargs'] = {}
-        if 'args' not in low:
-            low['args'] = []
+            low['arg'] = args
+            low['kwarg'] = kwargs
+        if 'kwarg' not in low:
+            low['kwarg'] = {}
+        if 'arg' not in low:
+            low['arg'] = []
         reformatted_low['kwarg'] = low
         return reformatted_low
 
@@ -158,8 +158,8 @@ class Runner(RunnerClient):
                     salt.utils.args.parse_input(self.opts['arg']),
                     self.opts,
                 )
-                low['args'] = args
-                low['kwargs'] = kwargs
+                low['arg'] = args
+                low['kwarg'] = kwargs
 
                 user = salt.utils.get_specific_user()
 

salt/utils/decorators/__init__.py
@@ -466,7 +466,7 @@ class _WithDeprecated(_DeprecationDecorator):
 
 
     In case there is a need to deprecate a function and rename it,
-    the decorator shuld be used with the 'with_name' parameter. This
+    the decorator should be used with the 'with_name' parameter. This
     parameter is pointing to the existing deprecated function. In this
     case deprecation process as follows:
 

tests/integration/netapi/rest_cherrypy/app_test.py
@@ -2,6 +2,7 @@
 
 # Import python libs
 from __future__ import absolute_import
+import json
 
 # Import salttesting libs
 from salttesting.unit import skipIf
@@ -152,3 +153,56 @@ class TestWebhookDisableAuth(BaseRestCherryPyTest):
                 'content-type': 'application/x-www-form-urlencoded'
         })
         self.assertEqual(response.status, '200 OK')
+
+
+class TestArgKwarg(BaseRestCherryPyTest):
+    auth_creds = (
+        ('username', 'saltdev'),
+        ('password', 'saltdev'),
+        ('eauth', 'auto'))
+
+    low = (
+        ('client', 'runner'),
+        ('fun', 'test.arg'),
+        # use singular form for arg and kwarg
+        ('arg', [1234]),
+        ('kwarg', {'ext_source': 'redis'}),
+    )
+
+    def _token(self):
+        '''
+        Return the token
+        '''
+        body = urlencode(self.auth_creds)
+        request, response = self.request(
+            '/login',
+            method='POST',
+            body=body,
+            headers={
+                'content-type': 'application/x-www-form-urlencoded'
+            }
+        )
+        return response.headers['X-Auth-Token']
+
+    def test_accepts_arg_kwarg_keys(self):
+        '''
+        Ensure that (singular) arg and kwarg keys (for passing parameters)
+        are supported by runners.
+        '''
+        cmd = dict(self.low)
+        body = json.dumps(cmd)
+
+        request, response = self.request(
+            '/',
+            method='POST',
+            body=body,
+            headers={
+                'content-type': 'application/json',
+                'X-Auth-Token': self._token(),
+                'Accept': 'application/json',
+            }
+        )
+        resp = json.loads(response.body[0])
+        self.assertEqual(resp['return'][0]['args'], [1234])
+        self.assertEqual(resp['return'][0]['kwargs'],
+                         {'ext_source': 'redis'})

commit 80794636e17bfe82c8f89b7eaedb84ceb8ccd207

Fix broken reactor function calls
Broken by PR #32938

salt/client/mixins.py:
- In `SyncClientMixin._low`, `salt.utils.format_call` is invoked. The
`salt.utils.format_call` function will return a dict containing
'args' and 'kwargs', not 'arg' and 'kwarg' as was changed by
PR #32938.

Signed-off-by: Sergey Kizunov <sergey.kizunov@ni.com>

salt/client/mixins.py
@@ -337,7 +337,7 @@ class SyncClientMixin(object):
                     low,
                     expected_extra_kws=CLIENT_INTERNAL_KEYWORDS
                 )
-                args = f_call.get('arg', ())
+                args = f_call.get('args', ())
             else:
                 args = low['arg']
             if 'kwarg' not in low:
@@ -347,7 +347,7 @@ class SyncClientMixin(object):
                         low,
                         expected_extra_kws=CLIENT_INTERNAL_KEYWORDS
                     )
-                kwargs = f_call.get('kwarg', {})
+                kwargs = f_call.get('kwargs', {})
 
                 # throw a warning for the badly formed low data if we found
                 # kwargs using the old mechanism