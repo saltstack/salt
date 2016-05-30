42d8d4195c52b9605332379821162593b64f1c55
Mihai Dinca <mdinca@suse.de>
2016-05-30 13:05:25 +0200

    Assert only gpgautoimport: True works

--- tests/unit/modules/zypper_test.py
+++ tests/unit/modules/zypper_test.py
@@ -479,6 +479,16 @@ class ZypperTestCase(TestCase):
             ):
                 zypper.mod_repo(name, **{'url': url})
             zypper.__zypper__.xml.call.assert_not_called()
+            zypper.__zypper__.refreshable.xml.call.assert_not_called()
+
+        with zypper_patcher:
+            with self.assertRaisesRegexp(
+                Exception,
+                'Specified arguments did not result in modification of repo'
+            ):
+                zypper.mod_repo(name, **{'url': url, 'gpgautoimport': 'a'})
+            zypper.__zypper__.xml.call.assert_not_called()
+            zypper.__zypper__.refreshable.xml.call.assert_not_called()
 
     def test_repo_add_mod_noref(self):
         '''
