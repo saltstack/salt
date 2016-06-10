6f191d8f51730b3283aadbed341e09146d9e63a1
Julien Cigar <julien.cigar@gmail.com>
2016-06-10 14:51:18 +0200

    Use ipaddress module, refactoring, fixes some pylint issues, add support for sortlist

--- salt/grains/core.py
+++ salt/grains/core.py
@@ -1749,11 +1749,16 @@ def dns():
 
      .. versionadded:: 2016.3.0
     '''
-
+    # Provides:
+    #   dns
     if salt.utils.is_windows() or 'proxyminion' in __opts__:
         return {}
 
     resolv = salt.utils.dns.parse_resolv()
+    for key in ('nameservers', 'ip4_nameservers', 'ip6_nameservers',
+                'sortlist'):
+        if key in resolv:
+            resolv[key] = [str(i) for i in resolv[key]]
 
     return {'dns': resolv} if resolv else {}
 
