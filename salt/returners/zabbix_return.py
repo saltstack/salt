3eb3f52923da725b5aa307f9906c7bdca17ab037
Ferry Schuller <ferry@f-inter.net>
2016-06-13 18:44:26 +0200

    Added sanity and logging. (#33935)
    * Added sanity and logging.
    
    Added a check if zabbix_sender is installed on the Minion, if not exit.
    
    * Moved code from the global namespace to a function
    
    @rallytime thanks for the suggestions, guess I've fixed them all.
    
    * Moved function zbx() below __virtual__(), for consistency.

--- salt/returners/zabbix_return.py
+++ salt/returners/zabbix_return.py
@@ -15,6 +15,7 @@ Key: salt.trap.disaster
 '''
 from __future__ import absolute_import
 import logging
+import os
 log = logging.getLogger(__name__)
 
 
@@ -23,13 +24,26 @@ __virtualname__ = 'zabbix'
 
 
 def __virtual__():
-    return __virtualname__
+    if zbx():
+        return True
+    return False, 'Zabbix returner: No zabbix_sender and zabbix_agend.conf found.'
 
 
-def zabbix_sender(key, host, output):
-    zabbix_sender = "/usr/local/zabbix/bin/zabbix_sender"
-    zabbix_config = "/usr/local/zabbix/etc/zabbix_agentd.conf"
-    cmd = zabbix_sender + " -c " + zabbix_config + " -s " + host + " -k " + key + " -o \"" + output +"\""
+def zbx():
+    if os.path.exists('/usr/local/zabbix/bin/zabbix_sender') and os.path.exists('/usr/local/zabbix/etc/zabbix_agentd.conf'):
+        zabbix_sender = '/usr/local/zabbix/bin/zabbix_sender'
+        zabbix_config = '/usr/local/zabbix/etc/zabbix_agentd.conf'
+        return {"sender": zabbix_sender, "config": zabbix_config}
+    elif os.path.exists('/usr/bin/zabbix_sender') and os.path.exists('/etc/zabbix/zabbix_agentd.conf'):
+        zabbix_sender = "/usr/bin/zabbix_sender"
+        zabbix_config = "/etc/zabbix/zabbix_agentd.conf"
+        return {"sender": zabbix_sender, "config": zabbix_config}
+    else:
+        return False
+
+
+def zabbix_send(key, host, output):
+    cmd = zbx()['sender'] + " -c " + zbx()['config'] + " -s " + host + " -k " + key + " -o \"" + output +"\""
     __salt__['cmd.shell'](cmd)
 
 
@@ -43,10 +57,10 @@ def returner(ret):
         for state, item in ret['return'].iteritems():
             if not item['result']:
                 errors = True
-                zabbix_sender("salt.trap.high", host, 'SALT:\nname: {0}\ncomment: {1}'.format(item['name'], item['comment']))
+                zabbix_send("salt.trap.high", host, 'SALT:\nname: {0}\ncomment: {1}'.format(item['name'], item['comment']))
             if item['changes']:
                 changes = True
-                zabbix_sender("salt.trap.warning", host, 'SALT:\nname: {0}\ncomment: {1}'.format(item['name'], item['comment']))
+                zabbix_send("salt.trap.warning", host, 'SALT:\nname: {0}\ncomment: {1}'.format(item['name'], item['comment']))
 
     if not changes and not errors:
-        zabbix_sender("salt.trap.info", host, 'SALT {0} OK'.format(job_minion_id))
+        zabbix_send("salt.trap.info", host, 'SALT {0} OK'.format(job_minion_id))
