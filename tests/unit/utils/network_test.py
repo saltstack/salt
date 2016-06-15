921c399305882a057dc2d4d267f0372c8f4eb57a
Bo Maryniuk <bo@suse.de>
2016-06-15 14:09:12 +0200

    Add unit test: addrinfo from the raw socket should be picked up if all other socket checks had failed to localhost

--- tests/unit/utils/network_test.py
+++ tests/unit/utils/network_test.py
@@ -344,6 +344,20 @@ class NetworkTestCase(TestCase):
         '''
         self.assertEqual(network.generate_minion_id(), 'pick.me')
 
+    @patch('platform.node', MagicMock(return_value='localhost'))
+    @patch('socket.gethostname', MagicMock(return_value='ip6-loopback'))
+    @patch('socket.getfqdn', MagicMock(return_value='ip6-localhost'))
+    @patch('socket.getaddrinfo', MagicMock(return_value=[(2, 3, 0, 'pick.me', ('127.0.1.1', 0))]))
+    @patch('salt.utils.fopen', MagicMock(return_valute=False))
+    @patch('os.path.exists', MagicMock(return_valute=False))
+    @patch('salt.utils.network.ip_addrs', MagicMock(return_value=['127.0.0.1', '::1', 'fe00::0', 'fe02::1']))
+    def test_generate_minion_id_platform_localhost_addrinfo(self):
+        '''
+        Test if addinfo is picked up.
+
+        :return:
+        '''
+        self.assertEqual(network.generate_minion_id(), 'pick.me')
 
     @patch('platform.node', MagicMock(return_value='localhost'))
     @patch('socket.gethostname', MagicMock(return_value='ip6-loopback'))
