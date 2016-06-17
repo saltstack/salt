7206dd39e3e564f49cee05c4f00aea3fb7bd966b
Mike Place <mp@saltstack.com>
2016-06-17 16:35:02 -0600

    USe py2 floor for division in status.uptime

--- salt/modules/status.py
+++ salt/modules/status.py
@@ -154,9 +154,9 @@ def uptime():
     utc_time = datetime.datetime.utcfromtimestamp(time.time() - ut_ret['seconds'])
     ut_ret['since_iso'] = utc_time.isoformat()
     ut_ret['since_t'] = time.mktime(utc_time.timetuple())
-    ut_ret['days'] = ut_ret['seconds'] / 60 / 60 / 24
-    hours = (ut_ret['seconds'] - (ut_ret['days'] * 24 * 60 * 60)) / 60 / 60
-    minutes = ((ut_ret['seconds'] - (ut_ret['days'] * 24 * 60 * 60)) / 60) - hours * 60
+    ut_ret['days'] = ut_ret['seconds'] // 60 // 60 // 24
+    hours = (ut_ret['seconds'] - (ut_ret['days'] * 24 * 60 * 60)) // 60 // 60
+    minutes = ((ut_ret['seconds'] - (ut_ret['days'] * 24 * 60 * 60)) // 60) - hours * 60
     ut_ret['time'] = '{0}:{1}'.format(hours, minutes)
     ut_ret['users'] = len(__salt__['cmd.run']("who -s").split(os.linesep))
 
