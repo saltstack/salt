2004a7428d739deb1e9cac5c13471febbb8a8c11
Bo Maryniuk <bo@suse.de>
2016-06-17 17:22:45 +0200

    Remove unnecessary method

--- salt/modules/inspectlib/fsdb.py
+++ salt/modules/inspectlib/fsdb.py
@@ -24,8 +24,6 @@ class CsvDBEntity(object):
     '''
     Serializable object for the table.
     '''
-    def bind_table(self, table):
-        self.__table = table
 
     def serialize(self, description):
         '''
