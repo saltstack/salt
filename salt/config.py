236a67b7026e941780712fcbd746087cda232190
Bo Maryniuk <bo@suse.de>
2016-06-20 17:53:59 +0200

    Move log message from INFO to DEBUG.

--- salt/config.py
+++ salt/config.py
@@ -2810,7 +2810,7 @@ def get_id(opts, cache_minion_id=False):
 
     newid = salt.utils.network.generate_minion_id()
     if '__role' in opts and opts.get('__role') == 'minion':
-        log.info('Found minion id from generate_minion_id(): {0}'.format(newid))
+        log.debug('Found minion id from generate_minion_id(): {0}'.format(newid))
     if cache_minion_id and opts.get('minion_id_caching', True):
         _cache_id(newid, id_cache)
     is_ipv4 = newid.count('.') == 3 and not any(c.isalpha() for c in newid)
