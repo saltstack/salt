d6410a03b8e0bf17ae889580262645d98f660622
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2016-06-20 11:22:01 +0100

    unit tests for SUSE os grains detection

--- tests/unit/grains/core_test.py
+++ tests/unit/grains/core_test.py
@@ -182,8 +182,208 @@ class CoreGrainsTestCase(TestCase):
                                                 with patch.dict(core.__salt__, {'cmd.run': osarch_mock}):
                                                     os_grains = core.os_data()
 
-        self.assertEqual(os_grains.get('os_family'), 'SUSE')
+        self.assertEqual(os_grains.get('os_family'), 'Suse')
         self.assertEqual(os_grains.get('os'), 'SUSE')
+
+    def _run_os_grains_tests(self, os_release_map):
+        path_isfile_mock = MagicMock(side_effect=lambda x: x in os_release_map['files'])
+        empty_mock = MagicMock(return_value={})
+        osarch_mock = MagicMock(return_value="amd64")
+        os_release_mock = MagicMock(return_value=os_release_map.get('os_release_file'))
+
+        orig_import = __import__
+
+        def _import_mock(name, *args):
+            if name == 'lsb_release':
+                raise ImportError('No module named lsb_release')
+            return orig_import(name, *args)
+
+        # Skip the first if statement
+        with patch.object(salt.utils, 'is_proxy',
+                          MagicMock(return_value=False)):
+            # Skip the selinux/systemd stuff (not pertinent)
+            with patch.object(core, '_linux_bin_exists',
+                              MagicMock(return_value=False)):
+                # Skip the init grain compilation (not pertinent)
+                with patch.object(os.path, 'exists', path_isfile_mock):
+                    # Ensure that lsb_release fails to import
+                    with patch('__builtin__.__import__',
+                               side_effect=_import_mock):
+                        # Skip all the /etc/*-release stuff (not pertinent)
+                        with patch.object(os.path, 'isfile', path_isfile_mock):
+                            with patch.object(core, '_parse_os_release', os_release_mock):
+                                # Mock platform.linux_distribution to give us the
+                                # OS name that we want.
+                                distro_mock = MagicMock(
+                                    return_value=('SUSE Linux Enterprise Server ', '12', 'x86_64')
+                                )
+                                with patch.object(salt.utils, "fopen", MagicMock(return_value=os_release_map.get('suse_release_files'))):
+                                    with patch.object(platform, 'linux_distribution', distro_mock):
+                                        with patch.object(core, '_linux_gpu_data', empty_mock):
+                                            with patch.object(core, '_linux_cpudata', empty_mock):
+                                                with patch.object(core, '_virtual', empty_mock):
+                                                    # Mock the osarch
+                                                    with patch.dict(core.__salt__, {'cmd.run': osarch_mock}):
+                                                        os_grains = core.os_data()
+
+        self.assertEqual(os_grains.get('os'), 'SUSE')
+        self.assertEqual(os_grains.get('os_family'), 'Suse')
+        self.assertEqual(os_grains.get('osfullname'), os_release_map['osfullname'])
+        self.assertEqual(os_grains.get('oscodename'), os_release_map['oscodename'])
+        self.assertEqual(os_grains.get('osrelease'), os_release_map['osrelease'])
+        self.assertListEqual(list(os_grains.get('osrelease_info')), os_release_map['osrelease_info'])
+
+    @skipIf(not salt.utils.is_linux(), 'System is not Linux')
+    def test_suse_os_grains_sles11sp3(self):
+        '''
+        Test if OS grains are parsed correctly in SLES 11 SP3
+        '''
+        _path_exists_map = {
+            '/proc/1/cmdline': False
+        }
+        _os_release_map = {
+            'suse_release_file': [
+                'SUSE Linux Enterprise Server 11 (x86_64)'
+                'VERSION = 11',
+                'PATCHLEVEL = 3',
+            ],
+            'oscodename': 'SUSE Linux Enterprise Server 12',
+            'osfullname': "SLES",
+            'osrelease': '12',
+            'osrelease_info': [12],
+            'files': ["/etc/SuSE-release", _path_exists_map],
+        }
+        self._run_os_grains_tests(_os_release_map)
+
+    @skipIf(not salt.utils.is_linux(), 'System is not Linux')
+    def test_suse_os_grains_sles11sp4(self):
+        '''
+        Test if OS grains are parsed correctly in SLES 11 SP4
+        '''
+        _path_exists_map = {
+            '/proc/1/cmdline': False
+        }
+        _os_release_map = {
+            'os_release_file': {
+                'NAME': 'SLES',
+                'VERSION': '11.4',
+                'VERSION_ID': '11.4',
+                'PRETTY_NAME': 'SUSE Linux Enterprise Server 11 SP4',
+                'ID': 'sles',
+                'ANSI_COLOR': '0;32',
+                'CPE_NAME': 'cpe:/o:suse:sles:11:4'
+            },
+            'oscodename': 'SUSE Linux Enterprise Server 11 SP4',
+            'osfullname': "SLES",
+            'osrelease': '11.4',
+            'osrelease_info': [11, 4],
+            'files': ["/etc/os-release", _path_exists_map],
+        }
+        self._run_os_grains_tests(_os_release_map)
+
+    @skipIf(not salt.utils.is_linux(), 'System is not Linux')
+    def test_suse_os_grains_sles12(self):
+        '''
+        Test if OS grains are parsed correctly in SLES 12
+        '''
+        _path_exists_map = {
+            '/proc/1/cmdline': False
+        }
+        _os_release_map = {
+            'os_release_file': {
+                'NAME': 'SLES',
+                'VERSION': '12',
+                'VERSION_ID': '12',
+                'PRETTY_NAME': 'SUSE Linux Enterprise Server 12',
+                'ID': 'sles',
+                'ANSI_COLOR': '0;32',
+                'CPE_NAME': 'cpe:/o:suse:sles:12'
+            },
+            'oscodename': 'SUSE Linux Enterprise Server 12',
+            'osfullname': "SLES",
+            'osrelease': '12',
+            'osrelease_info': [12],
+            'files': ["/etc/os-release", _path_exists_map],
+        }
+        self._run_os_grains_tests(_os_release_map)
+
+    @skipIf(not salt.utils.is_linux(), 'System is not Linux')
+    def test_suse_os_grains_sles12sp1(self):
+        '''
+        Test if OS grains are parsed correctly in SLES 12 SP1
+        '''
+        _path_exists_map = {
+            '/proc/1/cmdline': False
+        }
+        _os_release_map = {
+            'os_release_file': {
+                'NAME': 'SLES',
+                'VERSION': '12-SP1',
+                'VERSION_ID': '12.1',
+                'PRETTY_NAME': 'SUSE Linux Enterprise Server 12 SP1',
+                'ID': 'sles',
+                'ANSI_COLOR': '0;32',
+                'CPE_NAME': 'cpe:/o:suse:sles:12:sp1'
+            },
+            'oscodename': 'SUSE Linux Enterprise Server 12 SP1',
+            'osfullname': "SLES",
+            'osrelease': '12.1',
+            'osrelease_info': [12, 1],
+            'files': ["/etc/os-release", _path_exists_map],
+        }
+        self._run_os_grains_tests(_os_release_map)
+
+    @skipIf(not salt.utils.is_linux(), 'System is not Linux')
+    def test_suse_os_grains_opensuse_leap_42_1(self):
+        '''
+        Test if OS grains are parsed correctly in openSUSE Leap 42.1
+        '''
+        _path_exists_map = {
+            '/proc/1/cmdline': False
+        }
+        _os_release_map = {
+            'os_release_file': {
+                'NAME': 'openSUSE Leap',
+                'VERSION': '42.1',
+                'VERSION_ID': '42.1',
+                'PRETTY_NAME': 'openSUSE Leap 42.1 (x86_64)',
+                'ID': 'opensuse',
+                'ANSI_COLOR': '0;32',
+                'CPE_NAME': 'cpe:/o:opensuse:opensuse:42.1'
+            },
+            'oscodename': 'openSUSE Leap 42.1 (x86_64)',
+            'osfullname': "Leap",
+            'osrelease': '42.1',
+            'osrelease_info': [42, 1],
+            'files': ["/etc/os-release", _path_exists_map],
+        }
+        self._run_os_grains_tests(_os_release_map)
+
+    @skipIf(not salt.utils.is_linux(), 'System is not Linux')
+    def test_suse_os_grains_tumbleweed(self):
+        '''
+        Test if OS grains are parsed correctly in openSUSE Tumbleweed
+        '''
+        _path_exists_map = {
+            '/proc/1/cmdline': False
+        }
+        _os_release_map = {
+            'os_release_file': {
+                'NAME': 'openSUSE',
+                'VERSION': 'Tumbleweed',
+                'VERSION_ID': '20160504',
+                'PRETTY_NAME': 'openSUSE Tumbleweed (20160504) (x86_64)',
+                'ID': 'opensuse',
+                'ANSI_COLOR': '0;32',
+                'CPE_NAME': 'cpe:/o:opensuse:opensuse:20160504'
+            },
+            'oscodename': 'openSUSE Tumbleweed (20160504)',
+            'osfullname': "Tumbleweed",
+            'osrelease': '20160504',
+            'osrelease_info': [20160504],
+            'files': ["/etc/os-release", _path_exists_map],
+        }
+        self._run_os_grains_tests(_os_release_map)
 
 
 if __name__ == '__main__':
