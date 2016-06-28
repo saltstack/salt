8cbe5b8c9f2eb4f6de398e59f541806580728e39
Thomas S Hatch <thatch45@gmail.com>
2016-06-28 16:22:48 -0600

    Add reg.status

--- salt/thorium/reg.py
+++ salt/thorium/reg.py
@@ -6,6 +6,7 @@ values are stored and computed, such as averages etc.
 
 # import python libs
 from __future__ import absolute_import, division
+import time
 import fnmatch
 
 __func_alias__ = {
@@ -55,6 +56,33 @@ def list_(name, add, match):
                 if key in event['data']:
                     item[key] = event['data'][key]
             __reg__[name]['val'].append(item)
+    return ret
+
+
+def status(name):
+    '''
+    Activate this register to turn on a minion status tracking register, this
+    register keeps the current status beacon data and the time that each beacon
+    was last checked in.
+    '''
+    ret = {'name': name,
+           'changes': {},
+           'comment': '',
+           'result': True}
+    now = time.time()
+    if 'status' not in __reg__:
+        __reg__['status'] = {}
+        __reg__['status']['val'] = {}
+    for event in __events__:
+        if fnmatch.fnmatch(event['tag'], 'salt/beacon/*/status/*'):
+            # Got one!
+            idata = {'recv_time': now}
+            for key in event['data']['data']:
+                if key in ('id', 'recv_time'):
+                    continue
+                idata[key] = event['data'][key]
+            __reg__['status']['val'][event['data']['data']['id']] = idata
+            ret['changes'][event['data']['data']['id']] = True
     return ret
 
 
