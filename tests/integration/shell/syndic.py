917c05f8f590aca9da5f533f4d8142e5583df700
Thayne Harbaugh <tharbaug@adobe.com>
2016-06-28 15:10:09 -0600

    Test exit status of salt-syndic

--- tests/integration/shell/syndic.py
+++ tests/integration/shell/syndic.py
@@ -20,10 +20,11 @@ ensure_in_syspath('../../')
 
 # Import salt libs
 import integration
+from integration.utils import testprogram
 import salt.utils
 
 
-class SyndicTest(integration.ShellCase, integration.ShellCaseCommonTestsMixIn):
+class SyndicTest(integration.ShellCase, testprogram.TestProgramCase, integration.ShellCaseCommonTestsMixIn):
 
     _call_binary_ = 'salt-syndic'
 
@@ -79,7 +80,77 @@ class SyndicTest(integration.ShellCase, integration.ShellCaseCommonTestsMixIn):
             if os.path.isdir(config_dir):
                 shutil.rmtree(config_dir)
 
+    def test_exit_status_unknown_user(self):
+        '''
+        Ensure correct exit status when the syndic is configured to run as an unknown user.
+        '''
+
+        syndic = testprogram.TestDaemonSaltSyndic(
+            name='unknown_user',
+            config_base={'user': 'unknown'},
+            parent_dir=self._test_dir,
+        )
+        # Call setup here to ensure config and script exist
+        syndic.setup()
+        stdout, stderr, status = syndic.run(
+            args=['-d'],
+            catch_stderr=True,
+            with_retcode=True,
+        )
+        self.assert_exit_status(
+            status, 'EX_NOUSER',
+            message='unknown user not on system',
+            stdout=stdout, stderr=stderr
+        )
+        # syndic.shutdown() should be unnecessary since the start-up should fail
+
+    # pylint: disable=invalid-name
+    def test_exit_status_unknown_argument(self):
+        '''
+        Ensure correct exit status when an unknown argument is passed to salt-syndic.
+        '''
+
+        syndic = testprogram.TestDaemonSaltSyndic(
+            name='unknown_argument',
+            parent_dir=self._test_dir,
+        )
+        # Syndic setup here to ensure config and script exist
+        syndic.setup()
+        stdout, stderr, status = syndic.run(
+            args=['-d', '--unknown-argument'],
+            catch_stderr=True,
+            with_retcode=True,
+        )
+        self.assert_exit_status(
+            status, 'EX_USAGE',
+            message='unknown argument',
+            stdout=stdout, stderr=stderr
+        )
+        # syndic.shutdown() should be unnecessary since the start-up should fail
+
+    def test_exit_status_correct_usage(self):
+        '''
+        Ensure correct exit status when salt-syndic starts correctly.
+        '''
+
+        syndic = testprogram.TestDaemonSaltSyndic(
+            name='correct_usage',
+            parent_dir=self._test_dir,
+        )
+        # Syndic setup here to ensure config and script exist
+        syndic.setup()
+        stdout, stderr, status = syndic.run(
+            args=['-d'],
+            catch_stderr=True,
+            with_retcode=True,
+        )
+        self.assert_exit_status(
+            status, 'EX_OK',
+            message='correct usage',
+            stdout=stdout, stderr=stderr
+        )
+        syndic.shutdown()
+
 
 if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(SyndicTest)
+    integration.run_tests(SyndicTest)
