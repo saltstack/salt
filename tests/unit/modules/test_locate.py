4f1c1abbddc9e945f50dd17cc5f09899dd78f2ef
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 18:31:50 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/unit/modules/test_locate.py
+++ tests/unit/modules/test_locate.py
@@ -14,9 +14,6 @@ from salttesting.mock import (
     NO_MOCK,
     NO_MOCK_REASON
 )
-from salttesting.helpers import ensure_in_syspath
-
-ensure_in_syspath('../../')
 
 # Import Salt Libs
 from salt.modules import locate
@@ -81,8 +78,3 @@ class LocateTestCase(TestCase):
         mock = MagicMock(return_value='')
         with patch.dict(locate.__salt__, {'cmd.run': mock}):
             self.assertListEqual(locate.locate('wholename', database='myfile'), [])
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(LocateTestCase, needs_daemon=False)
