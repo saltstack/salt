34c9888b464d783882cd626561fa5809790e86c4
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 19:03:40 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/unit/states/test_hg.py
+++ tests/unit/states/test_hg.py
@@ -11,15 +11,12 @@ from salt.states import hg
 
 # Import Salt Testing Libs
 from salttesting import skipIf, TestCase
-from salttesting.helpers import ensure_in_syspath
 from salttesting.mock import (
     NO_MOCK,
     NO_MOCK_REASON,
     MagicMock,
     patch
 )
-
-ensure_in_syspath('../../')
 
 hg.__opts__ = {}
 
@@ -55,8 +52,3 @@ class HgTestCase(TestCase):
                 with patch.object(hg, '_clone_repo', mock):
                     self.assertDictEqual(hg.latest("salt", target="c:\\salt"),
                                          ret)
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(HgTestCase, needs_daemon=False)
