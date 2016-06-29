54fee4040d5b77cd96d12290cbadc591ac5c18ee
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 19:04:08 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/unit/states/test_debconfmod.py
+++ tests/unit/states/test_debconfmod.py
@@ -13,10 +13,6 @@ from salttesting.mock import (
     MagicMock,
     patch)
 
-from salttesting.helpers import ensure_in_syspath
-
-ensure_in_syspath('../../')
-
 # Import Salt Libs
 from salt.states import debconfmod
 
@@ -86,8 +82,3 @@ class DebconfmodTestCase(TestCase):
             with patch.dict(debconfmod.__opts__, {'test': True}):
                 ret.update({'changes': changes})
                 self.assertDictEqual(debconfmod.set(name, data), ret)
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(DebconfmodTestCase, needs_daemon=False)
