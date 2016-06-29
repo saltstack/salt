857a7254c08d24e7bc93f9640a4062c31c180dd9
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 13:33:47 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/unit/modules/test_modjk.py
+++ tests/unit/modules/test_modjk.py
@@ -14,10 +14,6 @@ from salttesting.mock import (
     NO_MOCK_REASON
 )
 
-from salttesting.helpers import ensure_in_syspath
-
-ensure_in_syspath('../../')
-
 # Import Salt Libs
 from salt.modules import modjk
 
@@ -230,8 +226,3 @@ class ModjkTestCase(TestCase):
                           {'worker.result.type': 'OK'}):
             self.assertTrue(modjk.worker_edit('node1', 'loadbalancer1',
                                               {'vwf': 500, 'vwd': 60}))
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(ModjkTestCase, needs_daemon=False)
