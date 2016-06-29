9b998a4703cf6314629e48c83d5d0a6e5291b4c0
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 18:36:50 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/unit/pillar/test_nodegroups.py
+++ tests/unit/pillar/test_nodegroups.py
@@ -6,9 +6,6 @@ from __future__ import absolute_import
 # Import Salt Testing libs
 from salttesting import TestCase, skipIf
 from salttesting.mock import NO_MOCK, NO_MOCK_REASON, MagicMock, call, patch
-from salttesting.helpers import ensure_in_syspath
-
-ensure_in_syspath('../../')
 
 # Import Salt Libs
 from salt.pillar import nodegroups
