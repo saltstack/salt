6f5acee25bb697c3ccc899a6e2e0c7d4b230f70d
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 13:20:40 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/unit/modules/test_pyenv.py
+++ tests/unit/modules/test_pyenv.py
@@ -15,10 +15,6 @@ from salttesting.mock import (
     NO_MOCK_REASON
 )
 
-from salttesting.helpers import ensure_in_syspath
-
-ensure_in_syspath('../../')
-
 # Import Salt Libs
 from salt.modules import pyenv
 
@@ -212,8 +208,3 @@ class PyenvTestCase(TestCase):
                                          'cmd.run_all': mock_all}):
             self.assertFalse(pyenv.do_with_python('2.0.0-p0',
                                                   'gem list bundler'))
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(PyenvTestCase, needs_daemon=False)
