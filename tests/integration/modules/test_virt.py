e49bf57e78ac6b95c0519374937f149ca6aa8f0d
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 19:33:03 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/integration/modules/test_virt.py
+++ tests/integration/modules/test_virt.py
@@ -7,8 +7,7 @@ Validate the virt module
 from __future__ import absolute_import
 
 # Import Salt Testing libs
-from salttesting.helpers import ensure_in_syspath, requires_salt_modules
-ensure_in_syspath('../../')
+from salttesting.helpers import requires_salt_modules
 
 # Import salt libs
 import integration
@@ -45,8 +44,3 @@ class VirtTest(integration.ModuleCase):
         self.assertTrue(diskp[0]['system'].get('model', '') == 'scsi')
         self.assertTrue(diskp[0]['system'].get('format', '') == 'vmdk')
         self.assertTrue(diskp[0]['system'].get('size', '') == '8192')
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(VirtTest)
