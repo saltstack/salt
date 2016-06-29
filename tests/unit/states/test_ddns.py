84b27b09398f3aee098916b65d1f68ad3b16130b
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 19:00:19 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/unit/states/test_ddns.py
+++ tests/unit/states/test_ddns.py
@@ -13,10 +13,6 @@ from salttesting.mock import (
     MagicMock,
     patch)
 
-from salttesting.helpers import ensure_in_syspath
-
-ensure_in_syspath('../../')
-
 # Import Salt Libs
 from salt.states import ddns
 
@@ -79,8 +75,3 @@ class DdnsTestCase(TestCase):
                     comt = ('No matching DNS record(s) present')
                     ret.update({'comment': comt, 'result': True})
                     self.assertDictEqual(ddns.absent(name, zone, data), ret)
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(DdnsTestCase, needs_daemon=False)
