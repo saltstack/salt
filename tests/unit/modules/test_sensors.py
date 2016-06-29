a60c6edf28f3c3f8a7f020c0d480dffe922235ca
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 13:17:21 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/unit/modules/test_sensors.py
+++ tests/unit/modules/test_sensors.py
@@ -13,10 +13,6 @@ from salttesting.mock import (
     MagicMock,
     patch)
 
-from salttesting.helpers import ensure_in_syspath
-
-ensure_in_syspath('../../')
-
 # Import Salt Libs
 from salt.modules import sensors
 
@@ -36,8 +32,3 @@ class SensorTestCase(TestCase):
         with patch.dict(sensors.__salt__,
                         {'cmd.run': MagicMock(return_value='A:a B:b C:c D:d')}):
             self.assertDictEqual(sensors.sense('chip'), {'A': 'a B'})
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(SensorTestCase, needs_daemon=False)
