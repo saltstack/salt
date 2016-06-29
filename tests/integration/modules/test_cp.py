5cad24fd0d2d45d10419b3722f68aab23580a377
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 19:36:12 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/integration/modules/test_cp.py
+++ tests/integration/modules/test_cp.py
@@ -5,10 +5,6 @@ from __future__ import absolute_import
 import os
 import hashlib
 
-# Import Salt Testing libs
-from salttesting.helpers import ensure_in_syspath
-ensure_in_syspath('../../')
-
 # Import salt libs
 import integration
 import salt.utils
@@ -321,8 +317,3 @@ class CPModuleTest(integration.ModuleCase):
                 self.assertIn('Comte', data)
         finally:
             os.unlink(tgt)
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(CPModuleTest)
