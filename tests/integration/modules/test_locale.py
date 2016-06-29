8852eb8477494de641fb508676c2cdb3a84a4b74
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 19:36:44 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/integration/modules/test_locale.py
+++ tests/integration/modules/test_locale.py
@@ -6,12 +6,10 @@ from __future__ import absolute_import
 # Import Salt Testing libs
 from salttesting import skipIf
 from salttesting.helpers import (
-    ensure_in_syspath,
     requires_salt_modules,
     requires_system_grains,
     destructiveTest,
 )
-ensure_in_syspath('../../')
 
 # Import salt libs
 import integration
