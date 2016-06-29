0fcdd3c9f662da070799669e7ce7d7ace2d7d107
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 19:39:32 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/integration/cli/test_batch.py
+++ tests/integration/cli/test_batch.py
@@ -8,15 +8,10 @@ from __future__ import absolute_import
 # Import Salt Libs
 import integration
 
-# Import Salt Testing Libs
-from salttesting.helpers import ensure_in_syspath
-
-ensure_in_syspath('../../')
-
 
 class BatchTest(integration.ShellCase):
     '''
-    Integration tests for the salt.cli.batch module
+    Integration tests for the salt.cli.test_batch module
     '''
 
     def test_batch_run(self):
@@ -88,8 +83,3 @@ class BatchTest(integration.ShellCase):
         os_grain = os_grain.strip()
         cmd = sorted(self.run_salt('-G \'os:{0}\' -b 25% test.ping'.format(os_grain)))
         self.assertListEqual(cmd, sorted(ret))
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(BatchTest)
