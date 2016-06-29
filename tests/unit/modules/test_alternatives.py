d8e06a797b91a07506f72b43cead3370898ffc97
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 13:14:38 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/unit/modules/test_alternatives.py
+++ tests/unit/modules/test_alternatives.py
@@ -12,7 +12,7 @@ from __future__ import absolute_import
 
 # Import Salt Testing libs
 from salttesting import skipIf, TestCase
-from salttesting.helpers import ensure_in_syspath, TestsLoggingHandler
+from salttesting.helpers import TestsLoggingHandler
 from salttesting.mock import NO_MOCK, NO_MOCK_REASON, MagicMock, patch
 
 ensure_in_syspath('../../')
@@ -197,8 +197,3 @@ class AlternativesTestCase(TestCase):
                     ['alternatives', '--remove', 'better-world',
                      '/usr/bin/better-world'], python_shell=False
                 )
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(AlternativesTestCase, needs_daemon=False)
