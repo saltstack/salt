e345a81c4944d05785c7543bddc5c41b32331394
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 18:58:41 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/unit/states/test_mongodb_database.py
+++ tests/unit/states/test_mongodb_database.py
@@ -13,10 +13,6 @@ from salttesting.mock import (
     MagicMock,
     patch)
 
-from salttesting.helpers import ensure_in_syspath
-
-ensure_in_syspath('../../')
-
 # Import Salt Libs
 from salt.states import mongodb_database
 
@@ -63,8 +59,3 @@ class MongodbDatabaseTestCase(TestCase):
                         .format(name))
                 ret.update({'comment': comt, 'changes': {}})
                 self.assertDictEqual(mongodb_database.absent(name), ret)
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(MongodbDatabaseTestCase, needs_daemon=False)
