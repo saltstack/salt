e67c69ef31eaf43593fa3fdb8e554d7456bea3ee
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 19:03:17 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/unit/states/test_boto_elasticache.py
+++ tests/unit/states/test_boto_elasticache.py
@@ -13,10 +13,6 @@ from salttesting.mock import (
     MagicMock,
     patch)
 
-from salttesting.helpers import ensure_in_syspath
-
-ensure_in_syspath('../../')
-
 # Import Salt Libs
 from salt.states import boto_elasticache
 
@@ -122,8 +118,3 @@ class BotoElasticacheTestCase(TestCase):
             self.assertDictEqual(boto_elasticache.creategroup
                                  (name, primary_cluster_id,
                                   replication_group_description), ret)
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(BotoElasticacheTestCase, needs_daemon=False)
