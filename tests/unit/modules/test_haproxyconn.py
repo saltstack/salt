e4885f2e3aa4fab27bae44bf86ebae12b6cd2d76
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 18:32:06 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/unit/modules/test_haproxyconn.py
+++ tests/unit/modules/test_haproxyconn.py
@@ -14,10 +14,6 @@ from salttesting.mock import (
     NO_MOCK_REASON
 )
 
-from salttesting.helpers import ensure_in_syspath
-
-ensure_in_syspath('../../')
-
 # Import Salt Libs
 from salt.modules import haproxyconn
 
@@ -169,8 +165,3 @@ class HaproxyConnTestCase(TestCase):
         Test if it get a value from etcd, by direct path
         '''
         self.assertTrue(haproxyconn.show_backends())
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(HaproxyConnTestCase, needs_daemon=False)
