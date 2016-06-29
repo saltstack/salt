6ae8b178e607c63c88a702e57863bdd5751dddc5
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 19:00:26 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/unit/states/test_boto_asg.py
+++ tests/unit/states/test_boto_asg.py
@@ -13,10 +13,6 @@ from salttesting.mock import (
     MagicMock,
     patch)
 
-from salttesting.helpers import ensure_in_syspath
-
-ensure_in_syspath('../../')
-
 # Import Salt Libs
 from salt.states import boto_asg
 
@@ -117,8 +113,3 @@ class BotoAsgTestCase(TestCase):
                 comt = ('Autoscale group does not exist.')
                 ret.update({'comment': comt, 'result': True})
                 self.assertDictEqual(boto_asg.absent(name), ret)
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(BotoAsgTestCase, needs_daemon=False)
