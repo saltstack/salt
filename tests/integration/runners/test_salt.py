04abc7f3f4cb497b0d1da973e3a7572c52087359
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 19:46:35 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/integration/runners/test_salt.py
+++ tests/integration/runners/test_salt.py
@@ -7,10 +7,6 @@ Tests for the salt runner
 # Import Python libs
 from __future__ import absolute_import
 
-# Import Salt Testing libs
-from salttesting.helpers import ensure_in_syspath
-ensure_in_syspath('../../')
-
 # Import salt libs
 import integration
 
@@ -25,8 +21,3 @@ class SaltRunnerTest(integration.ShellCase):
         '''
         ret = self.run_run_plus('salt.cmd', 'test.ping')
         self.assertTrue(ret.get('out')[0])
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(SaltRunnerTest)
