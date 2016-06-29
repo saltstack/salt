ae8606f8b6ebfd041f23d8087bffa9b22d08a564
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 19:06:28 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/unit/states/test_lvs_server.py
+++ tests/unit/states/test_lvs_server.py
@@ -13,10 +13,6 @@ from salttesting.mock import (
     MagicMock,
     patch)
 
-from salttesting.helpers import ensure_in_syspath
-
-ensure_in_syspath('../../')
-
 # Import Salt Libs
 from salt.states import lvs_server
 
@@ -129,8 +125,3 @@ class LvsServerTestCase(TestCase):
                     ' so it cannot be removed')
             ret.update({'comment': comt, 'result': True})
             self.assertDictEqual(lvs_server.absent(name), ret)
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(LvsServerTestCase, needs_daemon=False)
