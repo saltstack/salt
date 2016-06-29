0918e86a01b4a8d7b1789d328e9043a52588d8be
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 19:01:10 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/unit/states/test_eselect.py
+++ tests/unit/states/test_eselect.py
@@ -13,10 +13,6 @@ from salttesting.mock import (
     MagicMock,
     patch)
 
-from salttesting.helpers import ensure_in_syspath
-
-ensure_in_syspath('../../')
-
 # Import Salt Libs
 from salt.states import eselect
 
@@ -48,8 +44,3 @@ class EselectTestCase(TestCase):
                     .format(target, name))
             ret.update({'comment': comt})
             self.assertDictEqual(eselect.set_(name, target), ret)
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(EselectTestCase, needs_daemon=False)
