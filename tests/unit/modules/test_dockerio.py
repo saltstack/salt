85322c406e0723b7d532a5351f004247218444e0
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 18:17:58 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/unit/modules/test_dockerio.py
+++ tests/unit/modules/test_dockerio.py
@@ -8,10 +8,8 @@ from __future__ import absolute_import
 
 # Import Salt Testing libs
 from salttesting import TestCase, skipIf
-from salttesting.helpers import ensure_in_syspath
 
-ensure_in_syspath('../../')
-
+# Import salt libs
 from salt.modules import dockerio
 
 HAS_DOCKER = dockerio.__virtual__()
@@ -26,8 +24,3 @@ class DockerIoTestCase(TestCase):
         self.assertEqual('1.0 GB', dockerio._sizeof_fmt(1024**3))
         self.assertEqual('1.0 TB', dockerio._sizeof_fmt(1024**4))
         self.assertEqual('1.0 PB', dockerio._sizeof_fmt(1024**5))
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(DockerIoTestCase, needs_daemon=False)
