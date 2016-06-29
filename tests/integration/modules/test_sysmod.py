92a9178927c9d0cbf105591b90370cc269a40954
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 19:36:51 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/integration/modules/test_sysmod.py
+++ tests/integration/modules/test_sysmod.py
@@ -4,10 +4,6 @@
 from __future__ import absolute_import
 import re
 
-# Import Salt Testing libs
-from salttesting.helpers import ensure_in_syspath
-ensure_in_syspath('../../')
-
 # Import salt libs
 import integration
 
@@ -118,8 +114,3 @@ class SysModuleTest(integration.ModuleCase):
                 '\n'.join(['  - {0}'.format(f) for f in sorted(noexample)]),
             )
         )
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(SysModuleTest)
