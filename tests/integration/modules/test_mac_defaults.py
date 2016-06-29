b905c675588f88a9301a4f30caaaefb93b8ab402
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 19:37:24 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/integration/modules/test_mac_defaults.py
+++ tests/integration/modules/test_mac_defaults.py
@@ -11,10 +11,8 @@ import os
 from salttesting import skipIf
 from salttesting.helpers import (
     destructiveTest,
-    ensure_in_syspath,
     requires_system_grains
 )
-ensure_in_syspath('../../')
 
 # Import Salt Libs
 import integration
@@ -59,7 +57,3 @@ class MacDefaultsModuleTest(integration.ModuleCase):
                                          DEFAULT_KEY])
         self.assertTrue(read_domain)
         self.assertEqual(read_domain, DEFAULT_VALUE)
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(MacDefaultsModuleTest)
