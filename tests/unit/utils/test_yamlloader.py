fabfddacf8331ca91a58a93f0abaed9f7e7dbdb9
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 18:40:47 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/unit/utils/test_yamlloader.py
+++ tests/unit/utils/test_yamlloader.py
@@ -13,11 +13,7 @@ import salt.utils
 
 # Import Salt Testing Libs
 from salttesting import TestCase, skipIf
-from salttesting.helpers import ensure_in_syspath
 from salttesting.mock import patch, NO_MOCK, NO_MOCK_REASON, mock_open
-
-
-ensure_in_syspath('../../')
 
 
 @skipIf(NO_MOCK, NO_MOCK_REASON)
@@ -105,8 +101,3 @@ p2:
   <<: *p1
   v2: beta
   v2: betabeta''')
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(YamlLoaderTestCase, needs_daemon=False)
