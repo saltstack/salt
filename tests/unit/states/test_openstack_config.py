3d441c8718be2304a05ab1bd608f838edfaf8cbc
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 18:54:00 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/unit/states/test_openstack_config.py
+++ tests/unit/states/test_openstack_config.py
@@ -13,10 +13,7 @@ from salttesting.mock import (
     MagicMock,
     patch)
 
-from salttesting.helpers import ensure_in_syspath
 from salt.exceptions import CommandExecutionError
-
-ensure_in_syspath('../../')
 
 # Import Salt Libs
 from salt.states import openstack_config
@@ -98,8 +95,3 @@ class OpenstackConfigTestCase(TestCase):
             ret.update({'comment': comt, 'changes': {'Value': 'Deleted'}})
             self.assertDictEqual(openstack_config.absent(name, filename,
                                                          section), ret)
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(OpenstackConfigTestCase, needs_daemon=False)
