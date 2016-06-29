5b51c35a0b26ae3437799404cc60dc2142696431
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 18:21:37 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/unit/modules/test_boto_iot.py
+++ tests/unit/modules/test_boto_iot.py
@@ -14,9 +14,6 @@ from salttesting.mock import (
     NO_MOCK_REASON,
     patch
 )
-from salttesting.helpers import ensure_in_syspath
-
-ensure_in_syspath('../../')
 
 # Import Salt libs
 import salt.config
@@ -621,9 +618,3 @@ class BotoIoTTopicRuleTestCase(BotoIoTTestCaseBase, BotoIoTTestCaseMixin):
         self.conn.list_topic_rules.side_effect = ClientError(error_content, 'list_topic_rules')
         result = boto_iot.list_topic_rules(**conn_parameters)
         self.assertEqual(result.get('error', {}).get('message'), error_message.format('list_topic_rules'))
-
-
-if __name__ == '__main__':
-    from integration import run_tests  # pylint: disable=import-error
-    run_tests(BotoIoTPolicyTestCase, needs_daemon=False)
-    run_tests(BotoIoTTopicRuleTestCase, needs_daemon=False)
