54a1536236a32205e15040005bc3e374556334fb
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 18:26:47 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/unit/modules/test_sdb.py
+++ tests/unit/modules/test_sdb.py
@@ -13,10 +13,6 @@ from salttesting.mock import (
     NO_MOCK_REASON
 )
 
-from salttesting.helpers import ensure_in_syspath
-
-ensure_in_syspath('../../')
-
 # Import Salt Libs
 from salt.modules import sdb
 
@@ -46,8 +42,3 @@ class SdbTestCase(TestCase):
         sdb://<profile>/<key>
         '''
         self.assertFalse(sdb.set_('sdb://mymemcached/foo', 'bar'))
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(SdbTestCase, needs_daemon=False)
