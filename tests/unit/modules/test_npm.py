5982146b177bf16411f489f61b3037af166052a7
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 18:19:40 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/unit/modules/test_npm.py
+++ tests/unit/modules/test_npm.py
@@ -15,10 +15,6 @@ from salttesting.mock import (
     NO_MOCK_REASON
 )
 
-from salttesting.helpers import ensure_in_syspath
-
-ensure_in_syspath('../../')
-
 # Import Salt Libs
 from salt.modules import npm
 from salt.exceptions import CommandExecutionError
@@ -154,8 +150,3 @@ class NpmTestCase(TestCase):
                                        'stdout': '/User/salt/.npm'})
         with patch.dict(npm.__salt__, {'cmd.run_all': mock}):
             self.assertEqual(npm.cache_path(), '/User/salt/.npm')
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(NpmTestCase, needs_daemon=False)
