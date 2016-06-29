5e2638557320ab55d7cbadb4f8f5c74378788e43
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 19:04:01 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/unit/states/test_mysql_grants.py
+++ tests/unit/states/test_mysql_grants.py
@@ -13,10 +13,6 @@ from salttesting.mock import (
     MagicMock,
     patch)
 
-from salttesting.helpers import ensure_in_syspath
-
-ensure_in_syspath('../../')
-
 # Import Salt Libs
 from salt.states import mysql_grants
 
@@ -117,8 +113,3 @@ class MysqlGrantsTestCase(TestCase):
                         ' so it cannot be revoked')
                 ret.update({'comment': comt, 'result': True})
                 self.assertDictEqual(mysql_grants.absent(name), ret)
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(MysqlGrantsTestCase, needs_daemon=False)
