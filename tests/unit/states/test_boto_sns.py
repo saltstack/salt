dc426cb7c75ba20282f8d0e18f3c64123658eedd
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 18:48:37 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/unit/states/test_boto_sns.py
+++ tests/unit/states/test_boto_sns.py
@@ -13,10 +13,6 @@ from salttesting.mock import (
     MagicMock,
     patch)
 
-from salttesting.helpers import ensure_in_syspath
-
-ensure_in_syspath('../../')
-
 # Import Salt Libs
 from salt.states import boto_sns
 
@@ -149,8 +145,3 @@ class BotoSnsTestCase(TestCase):
                                     'result': False,
                                     'comment': comt})
                         self.assertDictEqual(boto_sns.absent(name), ret)
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(BotoSnsTestCase, needs_daemon=False)
