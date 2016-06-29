e1173603f7c7175035bb83811856f41c2abec34b
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 13:12:25 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/unit/modules/test_win_timezone.py
+++ tests/unit/modules/test_win_timezone.py
@@ -13,10 +13,6 @@ from salttesting.mock import (
     patch
 )
 
-from salttesting.helpers import ensure_in_syspath
-
-ensure_in_syspath('../../')
-
 # Import Salt Libs
 from salt.modules import win_timezone
 
@@ -106,8 +102,3 @@ class WinTimezoneTestCase(TestCase):
         Test if it sets the hardware clock to be either UTC or localtime
         '''
         self.assertFalse(win_timezone.set_hwclock('UTC'))
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(WinTimezoneTestCase, needs_daemon=False)
