0c852b99615074014f7e2b9a8bfe823ec29473be
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 18:57:14 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/unit/states/test_modjk_worker.py
+++ tests/unit/states/test_modjk_worker.py
@@ -13,10 +13,6 @@ from salttesting.mock import (
     MagicMock,
     patch)
 
-from salttesting.helpers import ensure_in_syspath
-
-ensure_in_syspath('../../')
-
 # Import Salt Libs
 from salt.states import modjk_worker
 
@@ -93,8 +89,3 @@ class ModjkWorkerTestCase(TestCase):
         with patch.dict(modjk_worker.__salt__, {'publish.publish': mock}):
             ret.update({'comment': comt})
             self.assertDictEqual(modjk_worker.disable(name, lbn, target), ret)
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(ModjkWorkerTestCase, needs_daemon=False)
