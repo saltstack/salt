b7e2e29dfadad2c35e599b921ca4b512ffeaae2d
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 18:37:24 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/unit/pillar/test_mysql.py
+++ tests/unit/pillar/test_mysql.py
@@ -6,9 +6,6 @@ from __future__ import absolute_import
 # Import Salt Testing libs
 from salttesting import TestCase, skipIf
 from salttesting.mock import NO_MOCK, NO_MOCK_REASON
-from salttesting.helpers import ensure_in_syspath
-
-ensure_in_syspath('../../')
 
 # Import Salt Libs
 from salt.pillar import mysql
@@ -570,8 +567,3 @@ class MysqlPillarTestCase(TestCase):
             ]},
              return_data.result
         )
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(MysqlPillarTestCase, needs_daemon=False)
