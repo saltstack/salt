763f3931a048ae0f3b1f04f451b2d06254047c0d
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 19:26:16 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/unit/utils/test_args.py
+++ tests/unit/utils/test_args.py
@@ -9,15 +9,12 @@ from salt.utils import args
 # Import Salt Testing Libs
 from salttesting import TestCase, skipIf
 from salttesting.mock import NO_MOCK, NO_MOCK_REASON
-from salttesting.helpers import ensure_in_syspath
-
-ensure_in_syspath('../../')
 
 
 @skipIf(NO_MOCK, NO_MOCK_REASON)
 class ArgsTestCase(TestCase):
     '''
-    TestCase for salt.utils.args module
+    TestCase for salt.utils.test_args module
     '''
 
     def test_condition_input_string(self):
@@ -26,8 +23,3 @@ class ArgsTestCase(TestCase):
         '''
         cmd = args.condition_input(['*', 'foo.bar', 20141020201325675584], None)
         self.assertIsInstance(cmd[2], str)
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(ArgsTestCase, needs_daemon=False)
