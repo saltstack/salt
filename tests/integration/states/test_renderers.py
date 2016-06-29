8c6a684d343ce645c5fab21930fe163429e55d32
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 19:48:26 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/integration/states/test_renderers.py
+++ tests/integration/states/test_renderers.py
@@ -6,10 +6,6 @@ Integration tests for renderer functions
 # Import Python Libs
 from __future__ import absolute_import
 
-# Import Salt Testing libs
-from salttesting.helpers import ensure_in_syspath
-ensure_in_syspath('../../')
-
 # Import Salt Libs
 import integration
 
@@ -25,8 +21,3 @@ class TestJinjaRenderer(integration.ModuleCase):
         ret = self.run_function('state.sls', ['jinja_dot_notation'])
         for state_ret in ret.values():
             self.assertTrue(state_ret['result'])
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(TestJinjaRenderer)
