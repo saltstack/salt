1cb70053d7c498441f7683bdac86ee6f9a3303c4
Pedro Algarvio <pedro@algarvio.me>
2016-06-29 19:52:23 +0100

    Test filenames conformity. Adjust for pytest.

--- tests/integration/cloud/providers/test_msazure.py
+++ tests/integration/cloud/providers/test_msazure.py
@@ -12,9 +12,7 @@ from distutils.version import LooseVersion
 
 # Import Salt Testing Libs
 from salttesting import skipIf
-from salttesting.helpers import ensure_in_syspath, expensiveTest
-
-ensure_in_syspath('../../../')
+from salttesting.helpers import expensiveTest
 
 # Import Salt Libs
 import integration
@@ -172,8 +170,3 @@ class AzureTest(integration.ShellCase):
         # if test instance is still present, delete it
         if ret_str in query:
             self.run_cloud('-d {0} --assume-yes'.format(INSTANCE_NAME))
-
-
-if __name__ == '__main__':
-    from integration import run_tests  # pylint: disable=import-error
-    run_tests(AzureTest)
