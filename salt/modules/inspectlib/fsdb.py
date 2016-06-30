9d055023afbe84a9ad9c10c4d9a1064ef6dff50e
Bo Maryniuk <bo@suse.de>
2016-06-30 14:15:59 +0200

    Add code author string

--- salt/modules/inspectlib/fsdb.py
+++ salt/modules/inspectlib/fsdb.py
@@ -14,6 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+'''
+    :codeauthor: :email:`Bo Maryniuk <bo@suse.de>`
+'''
+
 import os
 import re
 import csv
