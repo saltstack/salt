ca67d8411ac52507c1b7994fc85d96a6118ae564
Pedro Algarvio <pedro@algarvio.me>
2016-07-01 16:26:30 +0100

    Switch to the pytest skip makers

--- tests/integration/states/test_npm.py
+++ tests/integration/states/test_npm.py
@@ -7,19 +7,17 @@
 # Import Python libs
 from __future__ import absolute_import
 
-# Import Salt Testing libs
-from salttesting import skipIf
-from salttesting.helpers import destructiveTest
+# Import 3rd-party libs
+import pytest
 
 # Import salt libs
 import integration
-import salt.utils
 
 
-@skipIf(salt.utils.which('npm') is None, 'npm not installed')
+@pytest.mark.skip_if_binaries_missing('npm', message='npm not installed')
 class NpmStateTest(integration.ModuleCase, integration.SaltReturnAssertsMixIn):
 
-    @destructiveTest
+    @pytest.mark.destructive_test
     def test_npm_installed_removed(self):
         '''
         Basic test to determine if NPM module was successfully installed and
@@ -30,7 +28,7 @@ class NpmStateTest(integration.ModuleCase, integration.SaltReturnAssertsMixIn):
         ret = self.run_state('npm.removed', name='pm2')
         self.assertSaltTrueReturn(ret)
 
-    @destructiveTest
+    @pytest.mark.destructive_test
     def test_npm_install_url_referenced_package(self):
         '''
         Determine if URL-referenced NPM module can be successfully installed.
@@ -40,7 +38,7 @@ class NpmStateTest(integration.ModuleCase, integration.SaltReturnAssertsMixIn):
         ret = self.run_state('npm.removed', name='git://github.com/request/request')
         self.assertSaltTrueReturn(ret)
 
-    @destructiveTest
+    @pytest.mark.destructive_test
     def test_npm_installed_pkgs(self):
         '''
         Basic test to determine if NPM module successfully installs multiple
@@ -49,7 +47,7 @@ class NpmStateTest(integration.ModuleCase, integration.SaltReturnAssertsMixIn):
         ret = self.run_state('npm.installed', name=None, pkgs=['pm2', 'grunt'])
         self.assertSaltTrueReturn(ret)
 
-    @destructiveTest
+    @pytest.mark.destructive_test
     def test_npm_cache_clean(self):
         '''
         Basic test to determine if NPM successfully cleans it's cached packages.
