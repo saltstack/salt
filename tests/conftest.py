35e45ed301ec25a2ab744d9e66a937e1f32b9184
Pedro Algarvio <pedro@algarvio.me>
2016-07-03 19:27:16 +0100

    Add `sshd_server` fixture which prepares the server and client ends, and roster

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,8 +14,10 @@ import os
 import stat
 import socket
 import logging
+import subprocess
 
 # Import 3rd-party libs
+import yaml
 import pytest
 import salt.ext.six as six
 
@@ -24,6 +26,9 @@ import salt.utils
 
 # Define the pytest plugins we rely on
 pytest_plugins = ['pytest-salt', 'pytest_catchlog', 'tempdir', 'helpers_namespace']  # pylint: disable=invalid-name
+
+# Import pytest-salt libs
+from pytestsalt.utils import get_unused_localhost_port
 
 log = logging.getLogger('salt.testsuite')
 
@@ -447,3 +452,237 @@ def cli_bin_dir(tempdir,
 # ----- Salt Configuration ------------------------------------------------------------------------------------------>
 
 # <---- Salt Configuration -------------------------------------------------------------------------------------------
+
+
+# ----- Custom Fixtures Definitions --------------------------------------------------------------------------------->
+@pytest.fixture(scope='session')
+def sshd_port():
+    '''
+    Return an unused localhost port to use for the sshd server
+    '''
+    return get_unused_localhost_port()
+
+
+@pytest.fixture(scope='session')
+def sshd_config_lines(sshd_port):
+    '''
+    Return a list of lines which will make the sshd_config file
+    '''
+    return [
+        'Port {0}'.format(sshd_port),
+        'ListenAddress 127.0.0.1',
+        'Protocol 2',
+        'UsePrivilegeSeparation yes',
+        '# Turn strict modes off so that we can operate in /tmp',
+        'StrictModes no',
+        '# Lifetime and size of ephemeral version 1 server key',
+        'KeyRegenerationInterval 3600',
+        'ServerKeyBits 1024',
+        '# Logging',
+        'SyslogFacility AUTH',
+        'LogLevel INFO',
+        '# Authentication:',
+        'LoginGraceTime 120',
+        'PermitRootLogin without-password',
+        'StrictModes yes',
+        'RSAAuthentication yes',
+        'PubkeyAuthentication yes',
+        '#AuthorizedKeysFile	%h/.ssh/authorized_keys',
+        '#AuthorizedKeysFile	key_test.pub',
+        '# Don\'t read the user\'s ~/.rhosts and ~/.shosts files',
+        'IgnoreRhosts yes',
+        '# For this to work you will also need host keys in /etc/ssh_known_hosts',
+        'RhostsRSAAuthentication no',
+        '# similar for protocol version 2',
+        'HostbasedAuthentication no',
+        '#IgnoreUserKnownHosts yes',
+        '# To enable empty passwords, change to yes (NOT RECOMMENDED)',
+        'PermitEmptyPasswords no',
+        '# Change to yes to enable challenge-response passwords (beware issues with',
+        '# some PAM modules and threads)',
+        'ChallengeResponseAuthentication no',
+        '# Change to no to disable tunnelled clear text passwords',
+        'PasswordAuthentication no',
+        'X11Forwarding no',
+        'X11DisplayOffset 10',
+        'PrintMotd no',
+        'PrintLastLog yes',
+        'TCPKeepAlive yes',
+        '#UseLogin no',
+        'AcceptEnv LANG LC_*',
+        'Subsystem sftp /usr/lib/openssh/sftp-server',
+        '#UsePAM yes',
+    ]
+
+
+@pytest.fixture(scope='session')
+def roster_config(sshd_port, running_username, session_conf_dir):
+    return {
+        'localhost': {
+            'host': '127.0.0.1',
+            'port': sshd_port,
+            'user': running_username,
+            # XXX: Probably also create a fixture for the ssh key. Hardcoded for now
+            'priv': session_conf_dir.join('key_test').realpath().strpath
+        }
+    }
+
+
+@pytest.fixture(scope='session')
+def _roster_config(roster_config, session_conf_dir):
+    with salt.utils.fopen(session_conf_dir.join('roster').realpath().strpath, 'w') as wfh:
+        wfh.write(yaml.dump(roster_config, line_break='\n', default_flow_style=False))
+
+
+@pytest.yield_fixture(scope='session')
+def sshd_server(session_conf_dir, sshd_config_lines, _roster_config):
+    '''
+    This fixture will prepare an SSHD server to be used in tests
+    '''
+    log.debug('Initializing SSH subsystem')
+    keygen = salt.utils.which('ssh-keygen')
+    sshd = salt.utils.which('sshd')
+
+    if not (keygen and sshd):
+        pytest.skip('Could not initialize SSH subsystem')
+
+    ssh_config_dir = session_conf_dir.join('sshd')
+    ssh_config_dir.ensure(dir=True)
+
+    # Generate client key
+    pub_key_test_file = session_conf_dir.join('key_test.pub').realpath().strpath
+    priv_key_test_file = session_conf_dir.join('key_test').realpath().strpath
+    if os.path.exists(pub_key_test_file):
+        os.remove(pub_key_test_file)
+    if os.path.exists(priv_key_test_file):
+        os.remove(priv_key_test_file)
+    keygen_process = subprocess.Popen(
+        [keygen, '-t',
+                 'ecdsa',
+                 '-b',
+                 '521',
+                 '-C',
+                 '"$(whoami)@$(hostname)-$(date -I)"',
+                 '-f',
+                 'key_test',
+                 '-P',
+                 ''],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        close_fds=True,
+        cwd=session_conf_dir.realpath().strpath
+    )
+    _, keygen_err = keygen_process.communicate()
+    if keygen_err:
+        pytest.skip('ssh-keygen had errors: {0}'.format(salt.utils.to_str(keygen_err)))
+
+    auth_key_file = session_conf_dir.join('key_test.pub').realpath().strpath
+
+    # Generate server key
+    server_key_dir = ssh_config_dir.realpath().strpath
+    server_dsa_priv_key_file = os.path.join(server_key_dir, 'ssh_host_dsa_key')
+    server_dsa_pub_key_file = os.path.join(server_key_dir, 'ssh_host_dsa_key.pub')
+    server_ecdsa_priv_key_file = os.path.join(server_key_dir, 'ssh_host_ecdsa_key')
+    server_ecdsa_pub_key_file = os.path.join(server_key_dir, 'ssh_host_ecdsa_key.pub')
+    server_ed25519_priv_key_file = os.path.join(server_key_dir, 'ssh_host_ed25519_key')
+    server_ed25519_pub_key_file = os.path.join(server_key_dir, 'ssh_host.ed25519_key.pub')
+
+    for server_key_file in (server_dsa_priv_key_file,
+                            server_dsa_pub_key_file,
+                            server_ecdsa_priv_key_file,
+                            server_ecdsa_pub_key_file,
+                            server_ed25519_priv_key_file,
+                            server_ed25519_pub_key_file):
+        if os.path.exists(server_key_file):
+            os.remove(server_key_file)
+
+    keygen_process_dsa = subprocess.Popen(
+        [keygen, '-t',
+                 'dsa',
+                 '-b',
+                 '1024',
+                 '-C',
+                 '"$(whoami)@$(hostname)-$(date -I)"',
+                 '-f',
+                 'ssh_host_dsa_key',
+                 '-P',
+                 ''],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        close_fds=True,
+        cwd=server_key_dir
+    )
+    _, keygen_dsa_err = keygen_process_dsa.communicate()
+    if keygen_dsa_err:
+        pytest.skip('ssh-keygen had errors: {0}'.format(salt.utils.to_str(keygen_dsa_err)))
+
+    keygen_process_ecdsa = subprocess.Popen(
+        [keygen, '-t',
+                 'ecdsa',
+                 '-b',
+                 '521',
+                 '-C',
+                 '"$(whoami)@$(hostname)-$(date -I)"',
+                 '-f',
+                 'ssh_host_ecdsa_key',
+                 '-P',
+                 ''],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        close_fds=True,
+        cwd=server_key_dir
+    )
+    _, keygen_escda_err = keygen_process_ecdsa.communicate()
+    if keygen_escda_err:
+        pytest.skip('ssh-keygen had errors: {0}'.format(salt.utils.to_str(keygen_escda_err)))
+
+    keygen_process_ed25519 = subprocess.Popen(
+        [keygen, '-t',
+                 'ed25519',
+                 '-b',
+                 '521',
+                 '-C',
+                 '"$(whoami)@$(hostname)-$(date -I)"',
+                 '-f',
+                 'ssh_host_ed25519_key',
+                 '-P',
+                 ''],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        close_fds=True,
+        cwd=server_key_dir
+    )
+    _, keygen_ed25519_err = keygen_process_ed25519.communicate()
+    if keygen_ed25519_err:
+        pytest.skip('ssh-keygen had errors: {0}'.format(salt.utils.to_str(keygen_ed25519_err)))
+
+    sshd_config = sshd_config_lines[:]
+    sshd_config.append('AuthorizedKeysFile {0}'.format(auth_key_file))
+    if not keygen_dsa_err:
+        sshd_config.append('HostKey {0}'.format(server_dsa_priv_key_file))
+    if not keygen_escda_err:
+        sshd_config.append('HostKey {0}'.format(server_ecdsa_priv_key_file))
+    if not keygen_ed25519_err:
+        sshd_config.append('HostKey {0}'.format(server_ed25519_priv_key_file))
+
+    with salt.utils.fopen(ssh_config_dir.join('sshd_config').realpath().strpath, 'w') as wfh:
+        wfh.write('\n'.join(sshd_config))
+
+    sshd_pidfile = ssh_config_dir.join('sshd.pid').realpath().strpath
+    sshd_process = subprocess.Popen(
+        [sshd, '-f', 'sshd_config', '-oPidFile={0}'.format(sshd_pidfile)],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        close_fds=True,
+        cwd=ssh_config_dir.realpath().strpath
+    )
+    _, sshd_err = self.sshd_process.communicate()
+    if sshd_err:
+        pytest.skip('sshd had errors on startup: {0}'.format(salt.utils.to_str(sshd_err)))
+
+    # Run tests
+    yield
+
+    # Terminate sshd server
+    sshd_process.terminate()
+# <---- Custom Fixtures Definitions ----------------------------------------------------------------------------------
