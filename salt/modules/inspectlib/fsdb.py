cd42fab7773141f892096da410481c3d25c13079
Bo Maryniuk <bo@suse.de>
2016-07-04 16:39:07 +0200

    Add return value when flushing the database

--- salt/modules/inspectlib/fsdb.py
+++ salt/modules/inspectlib/fsdb.py
@@ -95,6 +95,8 @@ class CsvDB(object):
         db_path = os.path.join(self.path, dbid)
         if os.path.exists(db_path):
             shutil.rmtree(db_path, ignore_errors=True)
+            return True
+        return False
 
     def flush(self, table):
         '''
