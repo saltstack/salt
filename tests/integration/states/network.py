b2616833b0cb7eb50afff26b60bc6d5b6fc0bc11
Justin Anderson <janderson@saltstack.com>
2016-07-11 14:35:25 -0600

    Some small changes

--- tests/integration/states/network.py
+++ tests/integration/states/network.py
@@ -33,34 +33,34 @@ class NetworkTest(integration.ModuleCase, integration.SaltReturnAssertsMixIn):
         '''
         state_key = 
         ret = self.run_function('state.sls', mods='network.managed', test=True)
-                   
+
         out = ret['network_|-dummy0_|-dummy0_|-managed']['comment'].split('\n')
-                   
+
         self.assertIn('Interface dummy0 is set to be updated:', out)
         self.assertIn(' DEVICE="dummy0"', out)
         self.assertIn(' USERCTL="no"', out)
         self.assertIn(' ONBOOT="yes"', out)
         self.assertIn(' IPADDR="10.1.0.1"', out)
-                   
+
     def test_routes(self):
-        '''        
+        '''
         network.routes
-        '''        
+        '''
         state_key = 'network_|-routes_|-dummy0_|-routes'
         expected_changes = 'Interface dummy0 routes are set to be added.'
-                   
+
         ret = self.run_function('state.sls', mods='network.routes', test=True)
         print(ret) 
-                   
+
         self.assertEqual(ret[state_key]['comment'], expected_changes)
-                   
+
     def test_system(self):
-        '''        
+        '''
         network.system
-        '''        
+        '''
         state_key = 'network_|-system_|-system_|-system'
         comment_out = 'Global network settings are set to be updated:\n--- \n+++ \n@@ -1 +1,4 @@\n-# Created by anaconda\n+NETWORKING=yes\n+HOSTNAME=server1.example.com\n+GATEWAY=10.1.0.1\n+GATEWAYDEV=dummy0'
-                   
+
         ret = self.run_function('state.sls', mods='network.system', test=True)
         self.assertEqual(ret[state_key]['comment'], comment_out)
 
