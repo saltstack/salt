38488bc810a360275457997f56754cf4c74421bc
Elias Probst <mail@eliasprobst.eu>
2016-07-27 17:33:51 +0200

    Consistent error msg formatting
    Signed-off-by: Elias Probst <elias@grammofy.com>

--- salt/states/slack.py
+++ salt/states/slack.py
@@ -111,7 +111,7 @@ def post_message(name,
             icon=icon,
         )
     except SaltInvocationError as sie:
-        ret['comment'] = 'Failed to send message: {0} ({1})'.format(name, sie)
+        ret['comment'] = 'Failed to send message ({0}): {1}'.format(sie, name)
     else:
         if isinstance(result, bool) and result:
             ret['result'] = True
