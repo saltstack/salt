99214dbe84b3ad15e8a6a9abb6e2457f2280572a
twangboy <slee@saltstack.com>
2016-07-28 14:50:50 -0600

    Add missing docs and cli examples

--- salt/modules/reg.py
+++ salt/modules/reg.py
@@ -212,6 +212,14 @@ def _key_exists(hive, key, use_32bit_registry=False):
 def broadcast_change():
     '''
     Refresh the windows environment.
+
+    Returns (bool): True if successful, otherwise False
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' reg.broadcast_change
     '''
     # https://msdn.microsoft.com/en-us/library/windows/desktop/ms644952(v=vs.85).aspx
     _, res = SendMessageTimeout(HWND_BROADCAST, WM_SETTINGCHANGE, 0, 0,
