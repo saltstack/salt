634f1dded563b5547c3e36bd3f0e5aad337b6a31
Jorge Schrauwen <sjorge@acheron.be>
2016-08-02 18:25:08 +0200

    fixes #35121, causing lots of mayham (onchange) with 2016.3.2 for me

--- salt/states/file.py
+++ salt/states/file.py
@@ -3519,7 +3519,7 @@ def append(name,
 
     with salt.utils.fopen(name, 'rb') as fp_:
         slines = fp_.readlines()
-        slines = [item.rstrip() for item in slines]
+        slines = [item.rstrip(os.linesep) for item in slines]
 
     append_lines = []
     try:
