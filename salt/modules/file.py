13022c5cc4c3f383cd7ba9bfdf63ac51970a249b
Derek Maciel <derekamaciel@gmail.com>
2016-08-03 19:29:16 -0400

    return value for mkdir instead of None

--- salt/modules/file.py
+++ salt/modules/file.py
@@ -4586,6 +4586,8 @@ def mkdir(dir_path,
         # to follow the principal of least surprise method.
         makedirs_perms(directory, user, group, mode)
 
+    return os.path.isdir(directory)
+
 
 def makedirs_(path,
               user=None,
