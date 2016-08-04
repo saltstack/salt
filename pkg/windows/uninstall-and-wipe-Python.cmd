a860580c7988ced200c7c30927e4aa0da9d4bf03
twangboy <slee@saltstack.com>
2016-08-04 18:50:08 +0000

    Change GUID for python 2.7.12

--- pkg/windows/uninstall-and-wipe-Python.cmd
+++ pkg/windows/uninstall-and-wipe-Python.cmd
@@ -1,7 +1,7 @@
 @echo off
 
-:: uninstall Python 2.7 64bit
-MsiExec.exe /X {16E52445-1392-469F-9ADB-FC03AF00CD62} /QN
+:: uninstall Python 2.7.12 64bit
+MsiExec.exe /X {9DA28CE5-0AA5-429E-86D8-686ED898C666} /QN
 
 :: wipe the Python directory
 ::  DOS hack first create dir because Windows cannot test not existing without error message
