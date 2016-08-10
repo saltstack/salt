bd2c05b988b22a43bfdde430329240a52da47290
twangboy <slee@saltstack.com>
2016-08-10 15:09:38 -0700

    Fix skipped windows tests

--- tests/integration/states/cmd.py
+++ tests/integration/states/cmd.py
@@ -53,81 +53,80 @@ class CMDRunRedirectTest(integration.ModuleCase,
         self.state_name = 'run_redirect'
         state_filename = self.state_name + '.sls'
         self.state_file = os.path.join(STATE_DIR, state_filename)
-        self.fd, self.test_file = tempfile.mkstemp()
-        super(CMDRunRedirectTest, self).setUp()
 
-    def tearDown(self):
-        os.remove(self.state_file)
+        # Create the testfile and release the handle
+        self.fd, self.test_file = tempfile.mkstemp()
         try:
             os.close(self.fd)
         except OSError as exc:
             if exc.errno != errno.EBADF:
                 raise exc
+
+        super(CMDRunRedirectTest, self).setUp()
+
+    def tearDown(self):
+        os.remove(self.state_file)
         os.remove(self.test_file)
         super(CMDRunRedirectTest, self).tearDown()
 
-    @skipIf(IS_WINDOWS, 'Error 32 on Windows, file in use')
     def test_run_unless(self):
         '''
         test cmd.run unless
         '''
         state_key = 'cmd_|-/var/log/messages_|-/var/log/messages_|-run'
-        with salt.utils.fopen(self.state_file, 'w') as fp_:
-            fp_.write(textwrap.dedent('''\
-            /var/log/messages:
-              cmd.run:
-                - unless: echo cheese > {0}
-            '''.format(self.test_file)))
+        with salt.utils.fopen(self.state_file, 'w') as fb_:
+            fb_.write(textwrap.dedent('''
+                /var/log/messages:
+                  cmd.run:
+                    - unless: echo cheese > {0}
+                '''.format(self.test_file)))
 
         ret = self.run_function('state.sls', [self.state_name])
         self.assertTrue(ret[state_key]['result'])
 
-    @skipIf(IS_WINDOWS, 'Error 32 on Windows, file in use')
     def test_run_creates_exists(self):
         '''
         test cmd.run creates already there
         '''
-        state_key = 'cmd_|-touch {0}_|-touch {0}_|-run'.format(self.test_file)
-        with salt.utils.fopen(self.state_file, 'w') as fp_:
-            fp_.write(textwrap.dedent('''\
-            touch {0}:
-              cmd.run:
-                - creates: {0}
-            '''.format(self.test_file)))
+        state_key = 'cmd_|-echo >> {0}_|-echo >> {0}_|-run'.format(self.test_file)
+        with salt.utils.fopen(self.state_file, 'w') as fb_:
+            fb_.write(textwrap.dedent('''
+                echo >> {0}:
+                  cmd.run:
+                    - creates: {0}
+                '''.format(self.test_file)))
 
         ret = self.run_function('state.sls', [self.state_name])
         self.assertTrue(ret[state_key]['result'])
         self.assertEqual(len(ret[state_key]['changes']), 0)
 
-    @skipIf(IS_WINDOWS, 'Error 32 on Windows, file in use')
     def test_run_creates_new(self):
         '''
         test cmd.run creates not there
         '''
         os.remove(self.test_file)
-        state_key = 'cmd_|-touch {0}_|-touch {0}_|-run'.format(self.test_file)
-        with salt.utils.fopen(self.state_file, 'w') as fp_:
-            fp_.write(textwrap.dedent('''\
-            touch {0}:
-              cmd.run:
-                - creates: {0}
-            '''.format(self.test_file)))
+        state_key = 'cmd_|-echo >> {0}_|-echo >> {0}_|-run'.format(self.test_file)
+        with salt.utils.fopen(self.state_file, 'w') as fb_:
+            fb_.write(textwrap.dedent('''
+                echo >> {0}:
+                  cmd.run:
+                    - creates: {0}
+                '''.format(self.test_file)))
 
         ret = self.run_function('state.sls', [self.state_name])
         self.assertTrue(ret[state_key]['result'])
         self.assertEqual(len(ret[state_key]['changes']), 4)
 
-    @skipIf(IS_WINDOWS, 'Error 32 on Windows, file in use')
     def test_run_redirect(self):
         '''
         test cmd.run with shell redirect
         '''
-        state_key = 'cmd_|-date > {0}_|-date > {0}_|-run'.format(self.test_file)
-        with salt.utils.fopen(self.state_file, 'w') as fp_:
-            fp_.write(textwrap.dedent('''\
-            date > {0}:
-              cmd.run
-            '''.format(self.test_file)))
+        state_key = 'cmd_|-echo test > {0}_|-echo test > {0}_|-run'.format(self.test_file)
+        with salt.utils.fopen(self.state_file, 'w') as fb_:
+            fb_.write(textwrap.dedent('''
+                echo test > {0}:
+                  cmd.run
+                '''.format(self.test_file)))
 
         ret = self.run_function('state.sls', [self.state_name])
         self.assertTrue(ret[state_key]['result'])
@@ -148,29 +147,28 @@ class CMDRunWatchTest(integration.ModuleCase,
         os.remove(self.state_file)
         super(CMDRunWatchTest, self).tearDown()
 
-    @skipIf(IS_WINDOWS, 'Need to fix for Windows')
     def test_run_watch(self):
         '''
         test cmd.run watch
         '''
-        saltines_key = 'cmd_|-saltines_|-echo_|-run'
-        biscuits_key = 'cmd_|-biscuits_|-echo hello_|-wait'
+        saltines_key = 'cmd_|-saltines_|-echo changed=true_|-run'
+        biscuits_key = 'cmd_|-biscuits_|-echo biscuits_|-wait'
 
-        with salt.utils.fopen(self.state_file, 'w') as fp_:
-            fp_.write(textwrap.dedent('''\
-            saltines:
-              cmd.run:
-                - name: echo
-                - cwd: /
-                - stateful: True
+        with salt.utils.fopen(self.state_file, 'w') as fb_:
+            fb_.write(textwrap.dedent('''
+                saltines:
+                  cmd.run:
+                    - name: echo changed=true
+                    - cwd: /
+                    - stateful: True
 
-            biscuits:
-              cmd.wait:
-                - name: echo hello
-                - cwd: /
-                - watch:
-                    - cmd: saltines
-            '''))
+                biscuits:
+                  cmd.wait:
+                    - name: echo biscuits
+                    - cwd: /
+                    - watch:
+                        - cmd: saltines
+                '''))
 
         ret = self.run_function('state.sls', [self.state_name])
         self.assertTrue(ret[saltines_key]['result'])
