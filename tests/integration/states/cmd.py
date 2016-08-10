6b709ce9a9b1ff3903a1ab890275c2a764667997
Erik Johnson <palehose@gmail.com>
2016-08-10 14:46:35 -0500

    tests/integration/states/cmd.py: clean up open filehandles

--- tests/integration/states/cmd.py
+++ tests/integration/states/cmd.py
@@ -51,7 +51,8 @@ class CMDTest(integration.ModuleCase,
         date_file = tempfile.mkstemp()[1]
         state_key = 'cmd_|-date > {0}_|-date > {0}_|-run'.format(date_file)
         try:
-            salt.utils.fopen(state_file, 'w').write(textwrap.dedent('''\
+            with salt.utils.fopen(state_file, 'w') as fp_:
+                fp_.write(textwrap.dedent('''\
                 date > {0}:
                   cmd.run
                 '''.format(date_file)))
@@ -73,7 +74,8 @@ class CMDTest(integration.ModuleCase,
         unless_file = tempfile.mkstemp()[1]
         state_key = 'cmd_|-/var/log/messages_|-/var/log/messages_|-run'
         try:
-            salt.utils.fopen(state_file, 'w').write(textwrap.dedent('''\
+            with salt.utils.fopen(state_file, 'w') as fp_:
+                fp_.write(textwrap.dedent('''\
                 /var/log/messages:
                   cmd.run:
                     - unless: echo cheese > {0}
@@ -96,7 +98,8 @@ class CMDTest(integration.ModuleCase,
         creates_file = tempfile.mkstemp()[1]
         state_key = 'cmd_|-touch {0}_|-touch {0}_|-run'.format(creates_file)
         try:
-            salt.utils.fopen(state_file, 'w').write(textwrap.dedent('''\
+            with salt.utils.fopen(state_file, 'w') as fp_:
+                fp_.write(textwrap.dedent('''\
                 touch {0}:
                   cmd.run:
                     - creates: {0}
@@ -121,7 +124,8 @@ class CMDTest(integration.ModuleCase,
         os.remove(creates_file)
         state_key = 'cmd_|-touch {0}_|-touch {0}_|-run'.format(creates_file)
         try:
-            salt.utils.fopen(state_file, 'w').write(textwrap.dedent('''\
+            with salt.utils.fopen(state_file, 'w') as fp_:
+                fp_.write(textwrap.dedent('''\
                 touch {0}:
                   cmd.run:
                     - creates: {0}
@@ -146,7 +150,8 @@ class CMDTest(integration.ModuleCase,
         biscuits_key = 'cmd_|-biscuits_|-echo hello_|-wait'
 
         try:
-            salt.utils.fopen(state_file, 'w').write(textwrap.dedent('''\
+            with salt.utils.fopen(state_file, 'w') as fp_:
+                fp_.write(textwrap.dedent('''\
                 saltines:
                   cmd.run:
                     - name: /bin/true
