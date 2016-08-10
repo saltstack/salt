f41bdecec3eec8878c05928752260dc4401074d5
rallytime <nicole@saltstack.com>
2016-08-10 14:50:38 -0600

    Move remaining two blockdev module functions over to disk module
    The whole module was marked for deprecation to be removed in Carbon,
    but not all of the functions were moved over to the disk module or
    marked with warn_util warnings. The full removal will now take place
    in Oxygen.

--- salt/modules/blockdev.py
+++ salt/modules/blockdev.py
@@ -14,7 +14,6 @@ import logging
 
 # Import salt libs
 import salt.utils
-import salt.utils.decorators as decorators
 
 log = logging.getLogger(__name__)
 
@@ -38,14 +37,14 @@ def __virtual__():
     return __virtualname__
 
 
-@decorators.which('sync')
-@decorators.which('mkfs')
 def format_(device, fs_type='ext4',
             inode_size=None, lazy_itable_init=None, force=False):
     '''
     Format a filesystem onto a block device
 
     .. versionadded:: 2015.8.2
+
+    .. deprecated:: Carbon
 
     device
         The block device in which to create the new filesystem
@@ -83,34 +82,25 @@ def format_(device, fs_type='ext4',
 
         salt '*' blockdev.format /dev/sdX1
     '''
-    cmd = ['mkfs', '-t', str(fs_type)]
-    if inode_size is not None:
-        if fs_type[:3] == 'ext':
-            cmd.extend(['-i', str(inode_size)])
-        elif fs_type == 'xfs':
-            cmd.extend(['-i', 'size={0}'.format(inode_size)])
-    if lazy_itable_init is not None:
-        if fs_type[:3] == 'ext':
-            cmd.extend(['-E', 'lazy_itable_init={0}'.format(lazy_itable_init)])
-    if force:
-        if fs_type[:3] == 'ext':
-            cmd.append('-F')
-        elif fs_type == 'xfs':
-            cmd.append('-f')
-    cmd.append(str(device))
-
-    mkfs_success = __salt__['cmd.retcode'](cmd, ignore_retcode=True) == 0
-    sync_success = __salt__['cmd.retcode']('sync', ignore_retcode=True) == 0
-
-    return all([mkfs_success, sync_success])
+    salt.utils.warn_until(
+        'Oxygen',
+        'The blockdev module has been merged with the disk module,'
+        'and will disappear in Oxygen. Use the disk.format_ function instead.'
+    )
+    return __salt__['disk.format_'](device,
+                                    fs_type=fs_type,
+                                    inode_size=inode_size,
+                                    lazy_itable_init=lazy_itable_init,
+                                    force=force)
 
 
-@decorators.which_bin(['lsblk', 'df'])
 def fstype(device):
     '''
     Return the filesystem name of a block device
 
     .. versionadded:: 2015.8.2
+
+    .. deprecated:: Carbon
 
     device
         The name of the block device
@@ -121,20 +111,9 @@ def fstype(device):
 
         salt '*' blockdev.fstype /dev/sdX1
     '''
-    if salt.utils.which('lsblk'):
-        lsblk_out = __salt__['cmd.run']('lsblk -o fstype {0}'.format(device)).splitlines()
-        if len(lsblk_out) > 1:
-            fs_type = lsblk_out[1].strip()
-            if fs_type:
-                return fs_type
-
-    if salt.utils.which('df'):
-        # the fstype was not set on the block device, so inspect the filesystem
-        # itself for its type
-        df_out = __salt__['cmd.run']('df -T {0}'.format(device)).splitlines()
-        if len(df_out) > 1:
-            fs_type = df_out[1]
-            if fs_type:
-                return fs_type
-
-    return ''
+    salt.utils.warn_until(
+        'Oxygen',
+        'The blockdev module has been merged with the disk module,'
+        'and will disappear in Oxygen. Use the disk.fstype function instead.'
+    )
+    return __salt__['disk.fstype'](device)
