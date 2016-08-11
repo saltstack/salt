737747a4f4210993e8d2318ec16f25e1ea629762
Mike Place <mp@saltstack.com>
2016-08-11 15:49:30 +0900

    Raise SaltRenderError on bad requisite
    Refs #35226

--- tests/unit/state_test.py
+++ tests/unit/state_test.py
@@ -5,19 +5,26 @@
 
 # Import Python libs
 from __future__ import absolute_import
+import os
+import sys
 
 # Import Salt Testing libs
+from integration import TMP_CONF_DIR
 from salttesting import TestCase, skipIf
 from salttesting.helpers import ensure_in_syspath
 from salttesting.mock import (
     NO_MOCK,
     NO_MOCK_REASON,
+    patch
 )
+
 
 ensure_in_syspath('../')
 
 # Import Salt libs
-from salt import state
+import salt.state
+import salt.exceptions
+from salt.utils.odict import OrderedDict
 
 
 @skipIf(NO_MOCK, NO_MOCK_REASON)
@@ -38,7 +45,21 @@ class StateCompilerTestCase(TestCase):
         ret = {'changes': {u'Français': {'old': 'something old',
                                          'new': 'something new'}},
                'result': True}
-        state.format_log(ret)
+        salt.state.format_log(ret)
+
+    @skipIf(sys.version_info < (2, 7), 'Context manager in assertEquals only available in > Py2.7')
+    @patch('salt.state.State._gather_pillar')
+    def test_render_error_on_invalid_requisite(self, state_patch):
+        '''
+        Test that the state compiler correctly deliver a rendering
+        exception when a requisite cannot be resolved
+        '''
+        high_data = {'git': OrderedDict([('pkg', [OrderedDict([('require', [OrderedDict([('file', OrderedDict([('test1', 'test')]))])])]), 'installed', {'order': 10000}]), ('__sls__', u'issue_35226'), ('__env__', 'base')])}
+        minion_opts = salt.config.minion_config(os.path.join(TMP_CONF_DIR, 'minion'))
+        minion_opts['pillar'] = {'git': OrderedDict([('test1', 'test')]), 'my_secret': 'beer', 'gluster': OrderedDict([('hosts', OrderedDict([('gluster.w1', '10.71.60.142'), ('gluster.w2', '10.71.60.132')]))])}
+        state_obj = salt.state.State(minion_opts)
+        with self.assertRaises(salt.exceptions.SaltRenderError):
+            state_obj.call_high(high_data)
 
 
 if __name__ == '__main__':
