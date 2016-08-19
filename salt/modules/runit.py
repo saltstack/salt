b9403fb583c90f02ea1791db31493d5e05f6d5da
twangboy <slee@saltstack.com>
2016-08-19 10:13:07 -0600

    Fix __virtual__

--- salt/modules/runit.py
+++ salt/modules/runit.py
@@ -88,7 +88,7 @@ def __virtual__():
     Virtual service only on systems using runit as init process (PID 1).
     Otherwise, use this module with the provider mechanism.
     '''
-    if __grains__['init'] == 'runit':
+    if __grains__.get('init') == 'runit':
         if __grains__['os'] == 'Void':
             add_svc_avail_path('/etc/sv')
         return __virtualname__
