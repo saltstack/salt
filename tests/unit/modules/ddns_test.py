e744b795d14550b46ec3e78002b14e41fb7dc4f7
Justin Anderson <janderson@saltstack.com>
2016-08-29 16:33:54 -0600

    Make ddns_test happy with py3 and py26 (#35857)
    * Make python3 and python26 happy
    
    * Don't need both ways of nesting with statements
    
    * No longer need pylint disable
    
    * Lint fix
    
    * take out the correct import

--- tests/unit/modules/ddns_test.py
+++ tests/unit/modules/ddns_test.py
@@ -4,7 +4,6 @@
 '''
 # Import Python libs
 from __future__ import absolute_import
-import contextlib
 import textwrap
 import json
 try:
@@ -26,7 +25,6 @@ from salttesting.mock import (
 )
 
 # Import Salt Libs
-import salt.ext.six as six
 from salt.modules import ddns
 
 # Globals
@@ -99,23 +97,12 @@ class DDNSTestCase(TestCase):
         def mock_udp_query(*args, **kwargs):
             return MockAnswer
 
-        if six.PY3:
-            # pylint: disable=E0598
-            with patch.object(dns.message, 'make_query', MagicMock(return_value=mock_request)), \
-                    patch.object(dns.query, 'udp', mock_udp_query()), \
-                    patch.object(dns.rdatatype, 'from_text', MagicMock(return_value=mock_rdtype)), \
-                    patch.object(ddns, '_get_keyring', return_value=None), \
-                    patch.object(ddns, '_config', return_value=None):
-                self.assertTrue(ddns.update('zone', 'name', 1, 'AAAA', '::1'))
-            # pylint: enable=E0598
-        else:
-            with contextlib.nested(
-                    patch.object(dns.message, 'make_query', MagicMock(return_value=mock_request)),
-                    patch.object(dns.query, 'udp', mock_udp_query()),
-                    patch.object(dns.rdatatype, 'from_text', MagicMock(return_value=mock_rdtype)),
-                    patch.object(ddns, '_get_keyring', return_value=None),
-                    patch.object(ddns, '_config', return_value=None)):
-                self.assertTrue(ddns.update('zone', 'name', 1, 'AAAA', '::1'))
+        with patch.object(dns.message, 'make_query', MagicMock(return_value=mock_request)):
+            with patch.object(dns.query, 'udp', mock_udp_query()):
+                with patch.object(dns.rdatatype, 'from_text', MagicMock(return_value=mock_rdtype)):
+                    with patch.object(ddns, '_get_keyring', return_value=None):
+                        with patch.object(ddns, '_config', return_value=None):
+                            self.assertTrue(ddns.update('zone', 'name', 1, 'AAAA', '::1'))
 
     def test_delete(self):
         '''
@@ -133,23 +120,12 @@ class DDNSTestCase(TestCase):
         def mock_udp_query(*args, **kwargs):
             return MockAnswer
 
-        if six.PY3:
-            # pylint: disable=E0598
-            with patch.object(dns.query, 'udp', mock_udp_query()), \
-                    patch('salt.utils.fopen', mock_open(read_data=file_data), create=True), \
-                    patch.object(dns.tsigkeyring, 'from_text', return_value=True), \
-                    patch.object(ddns, '_get_keyring', return_value=None), \
-                    patch.object(ddns, '_config', return_value=None):
-                self.assertTrue(ddns.delete(zone='A', name='B'))
-            # pylint: enable=E0598
-        else:
-            with contextlib.nested(
-                    patch.object(dns.query, 'udp', mock_udp_query()),
-                    patch('salt.utils.fopen', mock_open(read_data=file_data), create=True),
-                    patch.object(dns.tsigkeyring, 'from_text', return_value=True),
-                    patch.object(ddns, '_get_keyring', return_value=None),
-                    patch.object(ddns, '_config', return_value=None)):
-                self.assertTrue(ddns.delete(zone='A', name='B'))
+        with patch.object(dns.query, 'udp', mock_udp_query()):
+            with patch('salt.utils.fopen', mock_open(read_data=file_data), create=True):
+                with patch.object(dns.tsigkeyring, 'from_text', return_value=True):
+                    with patch.object(ddns, '_get_keyring', return_value=None):
+                        with patch.object(ddns, '_config', return_value=None):
+                            self.assertTrue(ddns.delete(zone='A', name='B'))
 
 
 if __name__ == '__main__':
