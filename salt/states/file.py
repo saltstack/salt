saltstack/salt

commit 6fedd7f68d963c6865127bcab9684966aa653313

file.retention_schedule state

salt/states/file.py
@@ -232,6 +232,23 @@ A more complex ``recurse`` example:
         - template: jinja
         - source: salt://project/templates_dir
         - include_empty: True
+
+Retention scheduling can be applied to manage contents of backup directories.
+For example:
+
+.. code-block:: yaml
+
+    /var/backups/example_directory:
+      file.retention_schedule:
+        - strptime_format: example_name_%Y%m%dT%H%M%S.tar.bz2
+        - retain:
+            most_recent: 5
+            first_of_hour: 4
+            first_of_day: 14
+            first_of_week: 6
+            first_of_month: 6
+            first_of_year: all
+
 '''
 
 # Import python libs
@@ -241,8 +258,10 @@ import itertools
 import logging
 import os
 import shutil
+import sys
 import traceback
-from collections import Iterable, Mapping
+from collections import Iterable, Mapping, defaultdict
+from datetime import datetime   # python3 problem in the making?
 
 # Import salt libs
 import salt.loader
@@ -2601,6 +2620,195 @@ def recurse(name,
         ret['comment'] = 'The directory {0} is in the correct state'.format(
             name
         )
+
+    return ret
+
+
+def retention_schedule(name, retain, strptime_format=None, timezone=None):
+    '''
+    Apply retention scheduling to backup storage directory.
+
+    .. versionadded:: Carbon
+
+    :param name:
+        The filesystem path to the directory containing backups to be managed.
+
+    :param retain:
+        Delete the backups, except for the ones we want to keep.
+        The N below should be an integer but may also be the special value of ``all``,
+        which keeps all files matching the criteria.
+        All of the retain options default to None,
+        which means to not keep files based on this criteria.
+
+        :most_recent N:
+            Keep the most recent N files.
+
+        :first_of_hour N:
+            For the last N hours from now, keep the first file after the hour.
+
+        :first_of_day N:
+            For the last N days from now, keep the first file after midnight.
+            See also ``timezone``.
+
+        :first_of_week N:
+            For the last N weeks from now, keep the first file after Sunday midnight.
+
+        :first_of_month N:
+            For the last N months from now, keep the first file after the start of the month.
+
+        :first_of_year N:
+            For the last N years from now, keep the first file after the start of the year.
+
+    :param strptime_format:
+        A python strptime format string used to first match the filenames of backups
+        and then parse the filename to determine the datetime of the file.
+        https://docs.python.org/2/library/datetime.html#datetime.datetime.strptime
+        Defaults to None, which considers all files in the directory to be backups eligible for deletion
+        and uses ``os.path.getmtime()`` to determine the datetime.
+
+    :param timezone:
+        The timezone to use when determining midnight.
+        This is only used when datetime is pulled from ``os.path.getmtime()``.
+        Defaults to ``None`` which uses the timezone from the locale.
+
+    .. code-block: yaml
+
+        /var/backups/example_directory:
+          file.retention_schedule:
+            - retain:
+                most_recent: 5
+                first_of_hour: 4
+                first_of_day: 7
+                first_of_week: 6    # NotImplemented yet.
+                first_of_month: 6
+                first_of_year: all
+            - strptime_format: example_name_%Y%m%dT%H%M%S.tar.bz2
+            - timezone: None
+
+    '''
+    name = os.path.expanduser(name)
+    ret = {'name': name,
+           'changes': {'retained': [], 'deleted': [], 'ignored': []},
+           'pchanges': {'retained': [], 'deleted': [], 'ignored': []},
+           'result': True,
+           'comment': ''}
+    if not name:
+        return _error(ret, 'Must provide name to file.retention_schedule')
+    if not os.path.isdir(name):
+        return _error(ret, 'Name provided to file.retention must be a directory')
+
+    # get list of files in directory
+    all_files = __salt__['file.readdir'](name)
+
+    # if strptime_format is set, filter through the list to find names which parse and get their datetimes.
+    beginning_of_unix_time = datetime(1970, 1, 1)
+    
+    def get_file_time_from_strptime(f):
+        try:
+            ts = datetime.strptime(f, strptime_format)
+            ts_epoch = (ts - beginning_of_unix_time).total_seconds()
+            return (ts, ts_epoch)
+        except ValueError:
+            # Files which don't match the pattern are not relevant files.
+            return (None, None)
+
+    def get_file_time_from_mtime(f):
+        lstat = __salt__['file.lstat'](os.path.join(name, f))
+        if lstat:
+            mtime = lstat['st_mtime']
+            return (datetime.fromtimestamp(mtime, timezone), mtime)
+        else:   # maybe it was deleted since we did the readdir?
+            return (None, None)
+
+    get_file_time = get_file_time_from_strptime if strptime_format else get_file_time_from_mtime
+
+    # data structures are nested dicts:
+    # files_by_ymd = year.month.day.hour.unixtime: filename
+    # files_by_y_week_dow = year.week_of_year.day_of_week.unixtime: filename
+    # http://the.randomengineer.com/2015/04/28/python-recursive-defaultdict/
+    # TODO: move to an ordered dict model and reduce the number of sorts in the rest of the code?
+    def dict_maker():
+        return defaultdict(dict_maker)
+    files_by_ymd = dict_maker()
+    files_by_y_week_dow = dict_maker()
+    relevant_files = set()
+    ignored_files = set()
+    for f in all_files:
+        ts, ts_epoch = get_file_time(f)
+        if ts:
+            files_by_ymd[ts.year][ts.month][ts.day][ts.hour][ts_epoch] = f
+            week_of_year = ts.isocalendar()[1]
+            files_by_y_week_dow[ts.year][week_of_year][ts.weekday()][ts_epoch] = f
+            relevant_files.add(f)
+        else:
+            ignored_files.add(f)
+
+    # This is tightly coupled with the file_with_times data-structure above.
+    RETAIN_TO_DEPTH = {
+        'first_of_year': 1,
+        'first_of_month': 2,
+        'first_of_day': 3,
+        'first_of_hour': 4,
+        'most_recent': 5,
+    }
+
+    def get_first(fwt):
+        if isinstance(fwt, dict):
+            first_sub_key = sorted(fwt.keys())[0]
+            return get_first(fwt[first_sub_key])
+        else:
+            return set([fwt, ])
+
+    def get_first_n_at_depth(fwt, depth, n):
+        if depth <= 0:
+            return get_first(fwt)
+        else:
+            result_set = set()
+            for k in sorted(fwt.keys(), reverse=True):
+                needed = n - len(result_set)
+                if needed < 1:
+                    break
+                result_set |= get_first_n_at_depth(fwt[k], depth - 1, needed)
+            return result_set
+
+    # for each retain criteria, add filenames which match the criteria to the retain set.
+    retained_files = set()
+    for retention_rule, keep_count in retain.items():
+        # This is kind of a hack, since 'all' should really mean all,
+        # but I think it's a large enough number that even modern filesystems would
+        # choke if they had this many files in a single directory.
+        keep_count = sys.maxsize if 'all' == keep_count else int(keep_count)
+        if 'first_of_week' == retention_rule:
+            first_of_week_depth = 2   # year + week_of_year = 2
+            # I'm adding 1 to keep_count below because it fixed an off-by one
+            # issue in the tests. I don't understand why, and that bothers me.
+            retained_files |= get_first_n_at_depth(files_by_y_week_dow,
+                                                   first_of_week_depth,
+                                                   keep_count + 1)
+        else:
+            retained_files |= get_first_n_at_depth(files_by_ymd,
+                                                   RETAIN_TO_DEPTH[retention_rule],
+                                                   keep_count)
+
+    deletable_files = list(relevant_files - retained_files)
+    deletable_files.sort(reverse=True)
+    changes = {
+            'retained': sorted(list(retained_files), reverse=True),
+            'deleted': deletable_files,
+            'ignored': sorted(list(ignored_files), reverse=True),
+        }
+    ret['pchanges'] = changes
+
+    # TODO: track and report how much space was / would be reclaimed
+    if __opts__['test']:
+        ret['comment'] = '{0} backups would have been removed from {1}.\n'.format(len(deletable_files), name)
+        if deletable_files:
+            ret['result'] = None
+    else:
+        for f in deletable_files:
+            __salt__['file.remove'](os.path.join(name, f))
+        ret['comment'] = '{0} backups were removed from {1}.\n'.format(len(deletable_files), name)
+        ret['changes'] = changes
 
     return ret
 

tests/unit/states/file_test.py
@@ -2,6 +2,8 @@
 
 # Import python libs
 from __future__ import absolute_import
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 import json
 import pprint
 import tempfile
@@ -13,6 +15,7 @@ from salttesting.mock import (
     NO_MOCK,
     NO_MOCK_REASON,
     MagicMock,
+    call,
     mock_open,
     patch)
 
@@ -1750,6 +1753,156 @@ class FileTestCase(TestCase):
                                  ret)
 
             self.assertTrue(filestate.mod_run_check_cmd(cmd, filename))
+
+    def test_retention_schedule(self):
+        '''
+        Test to execute the retention_schedule logic.
+
+        This test takes advantage of knowing which files it is generating,
+        which means it can easily generate list of which files it should keep.
+        '''
+
+        def generate_fake_files(format='example_name_%Y%m%dT%H%M%S.tar.bz2',
+                                starting=datetime(2016, 2, 8, 9),
+                                every=relativedelta(minutes=30),
+                                ending=datetime(2015, 12, 25),
+                                maxfiles=None):
+            '''
+            For starting, make sure that it's over a week from the beginning of the month
+            For every, pick only one of minutes, hours, days, weeks, months or years
+            For ending, the further away it is from starting, the slower the tests run
+            Full coverage requires over a year of separation, but that's painfully slow.
+            '''
+
+            if every.years:
+                ts = datetime(starting.year, 1, 1)
+            elif every.months:
+                ts = datetime(starting.year, starting.month, 1)
+            elif every.weeks:
+                # This breaks if the start of the week is in a previous month.
+                ts = datetime(starting.year, starting.month, starting.day - starting.weekday())
+            elif every.days:
+                ts = datetime(starting.year, starting.month, starting.day)
+            elif every.hours:
+                ts = datetime(starting.year, starting.month, starting.day, starting.hour)
+            elif every.minutes:
+                ts = datetime(starting.year, starting.month, starting.day, starting.hour, 0)
+            else:
+                raise NotImplementedError("not sure what you're trying to do here")
+
+            fake_files = []
+            count = 0
+            while ending < ts:
+                fake_files.append(ts.strftime(format=format))
+                count += 1
+                if maxfiles and count >= maxfiles:
+                    break
+                ts -= every
+            return fake_files
+
+        fake_name = '/some/dir/name'
+        fake_retain = {
+            'most_recent': 2,
+            'first_of_hour': 4,
+            'first_of_day': 7,
+            'first_of_week': 6,
+            'first_of_month': 6,
+            'first_of_year': 'all',
+        }
+        fake_strptime_format = 'example_name_%Y%m%dT%H%M%S.tar.bz2'
+        fake_matching_file_list = generate_fake_files()
+        # Add some files which do not match fake_strptime_format
+        fake_no_match_file_list = generate_fake_files(format='no_match_%Y%m%dT%H%M%S.tar.bz2',
+                                                      every=relativedelta(days=1))
+
+        def lstat_side_effect(path):
+            import re
+            from time import mktime
+            x = re.match(r'^[^\d]*(\d{8}T\d{6})\.tar\.bz2$', path).group(1)
+            ts = mktime(datetime.strptime(x, '%Y%m%dT%H%M%S').timetuple())
+            return {'st_atime': 0.0, 'st_ctime': 0.0, 'st_gid': 0,
+                    'st_mode': 33188, 'st_mtime': ts,
+                    'st_nlink': 1, 'st_size': 0, 'st_uid': 0,
+                   }
+
+        mock_t = MagicMock(return_value=True)
+        mock_f = MagicMock(return_value=False)
+        mock_lstat = MagicMock(side_effect=lstat_side_effect)
+        mock_remove = MagicMock()
+
+        def run_checks(isdir=mock_t, strptime_format=None, test=False):
+            expected_ret = {
+                    'name': fake_name,
+                    'changes': {'retained': [], 'deleted': [], 'ignored': []},
+                    'pchanges': {'retained': [], 'deleted': [], 'ignored': []},
+                    'result': True,
+                    'comment': 'Name provided to file.retention must be a directory',
+                }
+            if strptime_format:
+                fake_file_list = sorted(fake_matching_file_list + fake_no_match_file_list)
+            else:
+                fake_file_list = sorted(fake_matching_file_list)
+            mock_readdir = MagicMock(return_value=fake_file_list)
+
+            with patch.dict(filestate.__opts__, {'test': test}):
+                with patch.object(os.path, 'isdir', isdir):
+                    mock_readdir.reset_mock()
+                    with patch.dict(filestate.__salt__, {'file.readdir': mock_readdir}):
+                        with patch.dict(filestate.__salt__, {'file.lstat': mock_lstat}):
+                            mock_remove.reset_mock()
+                            with patch.dict(filestate.__salt__, {'file.remove': mock_remove}):
+                                if strptime_format:
+                                    actual_ret = filestate.retention_schedule(fake_name, fake_retain,
+                                                                              strptime_format=fake_strptime_format)
+                                else:
+                                    actual_ret = filestate.retention_schedule(fake_name, fake_retain)
+
+            if not isdir():
+                mock_readdir.assert_has_calls([])
+                expected_ret['result'] = False
+            else:
+                mock_readdir.assert_called_once_with(fake_name)
+                ignored_files = fake_no_match_file_list if strptime_format else []
+                retained_files = set(generate_fake_files(maxfiles=fake_retain['most_recent']))
+                junk_list = [('first_of_hour', relativedelta(hours=1)),
+                             ('first_of_day', relativedelta(days=1)),
+                             ('first_of_week', relativedelta(weeks=1)),
+                             ('first_of_month', relativedelta(months=1)),
+                             ('first_of_year', relativedelta(years=1))]
+                for retainable, retain_interval in junk_list:
+                    new_retains = set(generate_fake_files(maxfiles=fake_retain[retainable], every=retain_interval))
+                    # if we generate less than the number of files expected,
+                    # then the oldest file will also be retained
+                    # (correctly, since it's the first in it's category)
+                    if len(new_retains) < fake_retain[retainable]:
+                        new_retains.add(fake_file_list[0])
+                    retained_files |= new_retains
+
+                deleted_files = sorted(list(set(fake_file_list) - retained_files - set(ignored_files)), reverse=True)
+                retained_files = sorted(list(retained_files), reverse=True)
+                changes = {'retained': retained_files, 'deleted': deleted_files, 'ignored': ignored_files}
+                expected_ret['pchanges'] = changes
+                if test:
+                    expected_ret['result'] = None
+                    expected_ret['comment'] = ('{0} backups would have been removed from {1}.\n'
+                                               ''.format(len(deleted_files), fake_name))
+                else:
+                    expected_ret['comment'] = ('{0} backups were removed from {1}.\n'
+                                               ''.format(len(deleted_files), fake_name))
+                    expected_ret['changes'] = changes
+                    mock_remove.assert_has_calls(
+                            [call(os.path.join(fake_name, x)) for x in deleted_files],
+                            any_order=True
+                        )
+
+            self.assertDictEqual(actual_ret, expected_ret)
+
+        run_checks(isdir=mock_f)
+        run_checks()
+        run_checks(test=True)
+        run_checks(strptime_format=fake_strptime_format)
+        run_checks(strptime_format=fake_strptime_format, test=True)
+
 
 if __name__ == '__main__':
     from integration import run_tests

commit 921daeea37896f72263d4e45d095dd4af7a6f665

This fixes a regression in the file state
This was introduced in 6fedd7f68d963c6865127bcab9684966aa653313

This is not supported in 2.6. Changed to use Salt's total_seconds wrapper.

salt/states/file.py
@@ -2868,7 +2868,7 @@ def retention_schedule(name, retain, strptime_format=None, timezone=None):
     def get_file_time_from_strptime(f):
         try:
             ts = datetime.strptime(f, strptime_format)
-            ts_epoch = (ts - beginning_of_unix_time).total_seconds()
+            ts_epoch = salt.utils.total_seconds(ts - beginning_of_unix_time)
             return (ts, ts_epoch)
         except ValueError:
             # Files which don't match the pattern are not relevant files.