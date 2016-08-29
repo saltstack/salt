saltstack/salt

commit 75fd92fd98364342906034fac48524ab6e9f30be

Return locally installed version if no newer is available

salt/modules/pacman.py
@@ -78,6 +78,14 @@ def latest_version(*names, **kwargs):
         except (ValueError, IndexError):
             pass
 
+    pkgs = {}
+
+    for name in names:
+        if not ret[name]:
+            if not pkgs:
+                pkgs = list_pkgs()
+            if name in pkgs:
+                ret[name] = pkgs[name]
     # Return a string if only one package name passed
     if len(names) == 1:
         return ret[names[0]]

commit 123a611066ea04e0205f2bd0cf163d6e58dc4dfb

pacman.py: Fix incorrect return in pkg.latest_version
This was initially broken in 75fd92f. The documented behavior of this
function is to return an empty string for the package when the package
is up-to-date. Changing that behavior negatively affects the pkg.latest
state, which relies on the documented behavior.

salt/modules/pacman.py
@@ -89,14 +89,6 @@ def latest_version(*names, **kwargs):
         except (ValueError, IndexError):
             pass
 
-    pkgs = {}
-
-    for name in names:
-        if not ret[name]:
-            if not pkgs:
-                pkgs = list_pkgs()
-            if name in pkgs:
-                ret[name] = pkgs[name]
     # Return a string if only one package name passed
     if len(names) == 1:
         return ret[names[0]]