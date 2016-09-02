b68d293f86a48e2d0046439c90463a9df534d02d
Nicole Thomas <nicole@saltstack.com>
2016-09-02 14:37:27 -0600

    fix redis_return's clean_old_jobs. (#36014)
    Refs #33969

--- salt/returners/redis_return.py
+++ salt/returners/redis_return.py
@@ -204,6 +204,11 @@ def get_minions():
 def clean_old_jobs():
     '''
     Clean out minions's return data for old jobs.
+
+    Normally, hset 'ret:<jid>' are saved with a TTL, and will eventually
+    get cleaned by redis.But for jobs with some very late minion return, the
+    corresponding hset's TTL will be refreshed to a too late timestamp, we'll
+    do manually cleaning here.
     '''
     serv = _get_serv(ret=None)
     living_jids = set(serv.keys('load:*'))
@@ -212,7 +217,7 @@ def clean_old_jobs():
         load_key = ret_key.replace('ret:', 'load:', 1)
         if load_key not in living_jids:
             to_remove.append(ret_key)
-    serv.delete(**to_remove)  # pylint: disable=E1134
+    serv.delete(*to_remove)
 
 
 def prep_jid(nocache=False, passed_jid=None):  # pylint: disable=unused-argument
