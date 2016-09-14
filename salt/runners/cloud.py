7684ebd0e70f752a4acb866ad98639a1c31bbe64
Mike Place <mp@saltstack.com>
2016-09-15 00:18:09 +0900

    Filter out pub kwargs from cloud runner (#36178)
    * Filter out pub kwargs from cloud runner
    
    * Fix typo
    
    * Proper function unpacking

--- salt/runners/cloud.py
+++ salt/runners/cloud.py
@@ -19,9 +19,13 @@ import salt.cloud
 log = logging.getLogger(__name__)
 
 
+def _filter_kwargs(k):
+    return dict((x, k[x]) for x in k if not x.startswith('__'))
+
+
 def _get_client():
     '''
-    Return a cloud client
+    Return cloud client
     '''
     client = salt.cloud.CloudClient(
             os.path.join(os.path.dirname(__opts__['conf_file']), 'cloud')
@@ -107,7 +111,7 @@ def profile(prof=None, instances=None, **kwargs):
         return {'Error': 'One or more instances (comma-delimited) must be set'}
 
     client = _get_client()
-    info = client.profile(prof, instances, **kwargs)
+    info = client.profile(prof, instances, **_filter_kwargs(kwargs))
     return info
 
 
@@ -116,7 +120,7 @@ def map_run(path, **kwargs):
     Execute a salt cloud map file
     '''
     client = _get_client()
-    info = client.map_run(path, **kwargs)
+    info = client.map_run(path, **_filter_kwargs(kwargs))
     return info
 
 
@@ -145,7 +149,7 @@ def action(func=None,
         salt-run cloud.actions start my-salt-vm
     '''
     client = _get_client()
-    info = client.action(func, cloudmap, instances, provider, instance, kwargs)
+    info = client.action(func, cloudmap, instances, provider, instance, **_filter_kwargs(kwargs))
     return info
 
 
