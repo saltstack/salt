430e22c04bd7ba6a0c71c7d625569fe518a4f532
Tim Fechner <tim.fechner@hs-hannover.de>
2016-09-21 13:15:08 +0200

    Fix #36461, AttributeError because of .lower()

--- salt/utils/verify.py
+++ salt/utils/verify.py
@@ -516,7 +516,7 @@ def verify_log(opts):
     '''
     If an insecre logging configuration is found, show a warning
     '''
-    level = LOG_LEVELS.get(opts.get('log_level').lower(), logging.NOTSET)
+    level = LOG_LEVELS.get(str(opts.get('log_level')).lower(), logging.NOTSET)
 
     if level < logging.INFO:
         log.warn('Insecure logging configuration detected! Sensitive data may be logged.')
