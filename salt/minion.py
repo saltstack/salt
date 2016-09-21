bf0b6f79c73d7f3d9b5761b3f74e79098ae27963
C. R. Oldham <cro@ncbt.org>
2016-09-21 09:44:59 -0600

    Revert "Match recent changes to regular minion"
    This reverts commit 74f18f7e081340ae48ce43bb00b7bd50fdf28dc9.

--- salt/minion.py
+++ salt/minion.py
@@ -1848,7 +1848,6 @@ class Minion(MinionBase):
         '''
         Handle an event from the epull_sock (all local minion events)
         '''
-        log.debug('handle_event ----------------------- ')
         if not self.ready:
             raise tornado.gen.Return()
         tag, data = salt.utils.event.SaltEvent.unpack(package)
@@ -2945,8 +2944,7 @@ class ProxyMinion(MinionManager):
         '''
         log.debug("subclassed _post_master_init")
 
-        if self.connected:
-            self.opts['master'] = master
+        self.opts['master'] = master
 
         self.opts['pillar'] = yield salt.pillar.get_async_pillar(
             self.opts,
@@ -3069,6 +3067,5 @@ class ProxyMinion(MinionManager):
             self.schedule.delete_job(master_event(type='failback'), persist=True)
 
         #  Sync the grains here so the proxy can communicate them to the master
-        # self.functions['saltutil.sync_grains'](saltenv='base')
+        self.functions['saltutil.sync_grains'](saltenv='base')
         self.grains_cache = self.opts['grains']
-        self.ready = True
