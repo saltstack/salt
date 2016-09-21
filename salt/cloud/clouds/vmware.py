7ec5ce2e0fa6614b1f3d5fa05b9182222fcd5246
C. R. Oldham <cro@ncbt.org>
2016-09-20 20:22:44 -0600

    Add support for specifying a datastore for new disks.

--- salt/cloud/clouds/vmware.py
+++ salt/cloud/clouds/vmware.py
@@ -273,6 +273,8 @@ def _edit_existing_hard_disk_helper(disk, size_kb=None, size_gb=None, mode=None)
 
 
 def _add_new_hard_disk_helper(disk_label, size_gb, unit_number, controller_key=1000, thin_provision=False, datastore=None, vm_name=None):
+
+
     random_key = randint(-2099, -2000)
     size_kb = int(size_gb * 1024.0 * 1024.0)
 
@@ -604,6 +606,7 @@ def _set_network_adapter_mapping(adapter_specs):
     return adapter_mapping
 
 
+<<<<<<< HEAD
 def _get_mode_spec(device, mode, disk_spec):
     if device.backing.diskMode != mode:
         if not disk_spec:
@@ -623,6 +626,8 @@ def _get_size_spec(device, size_gb=None, size_kb=None):
     return disk_spec
 
 
+=======
+>>>>>>> Add support for specifying a datastore for new disks.
 def _manage_devices(devices, vm=None, container_ref=None, new_vm_name=None):
     unit_number = 0
     bus_number = 0
@@ -2570,7 +2575,11 @@ def create(vm_):
         config_spec.memoryMB = memory_mb
 
     if devices:
+<<<<<<< HEAD
         specs = _manage_devices(devices, vm=object_ref, new_vm_name=vm_name)
+=======
+        specs = _manage_devices(devices, vm=object_ref, new_vm_name=vm_['name'])
+>>>>>>> Add support for specifying a datastore for new disks.
         config_spec.deviceChange = specs['device_specs']
 
     if extra_config:
