edb1baad0869425e1b7cbda295069a60fb9ca9a2
bllackeye <bllackeye@gmail.com>
2016-09-27 16:28:13 +0800

    Automate module downloading from salt-master
    1) before invoke a function, the minion check the readyness of the
    function.
    2) the minion will try to download modules and load the modules if the
    required module is not ready.

--- salt/minion.py
+++ salt/minion.py
@@ -1158,6 +1158,36 @@ class Minion(MinionBase):
         else:
             self.win_proc.append(process)
 
+			
+	def _check_moduleReadyness(self,function_name, minion_instance): 
+        if function_name not in minion_instance.functions:
+           # sync module first. _args = []_kwargs = {}
+           log.warning('could not find '+function_name+' will sync modules from master... ', exc_info_on_loglevel=logging.DEBUG)
+           func_sync=minion_instance.functions['saltutil.sync_modules']
+           args, kwargs = load_args_and_kwargs(
+                    func_sync,
+                    [])
+           func_sync(*args, **kwargs)
+           retries=0
+           while True:
+               try:
+                   retries+=1
+                   minion_instance.functions, minion_instance.returners, minion_instance.function_errors = minion_instance._load_modules()
+                   log.warning('could not find '+function_name+' will reload modules... ', exc_info_on_loglevel=logging.DEBUG)
+                   if (function_name in minion_instance.functions or retries>3):
+                       if function_name in minion_instance.functions:
+                          log.warning(function_name+' loaded @retries={0}'.format(retries), exc_info_on_loglevel=logging.DEBUG)
+                       else:
+                          log.warning(function_name+' failed to load after 3 times retries', exc_info_on_loglevel=logging.DEBUG)
+                       break; 
+                   else:
+                       time.sleep(1)
+                       msg=function_name+" still not found:"
+                       log.warning(msg, exc_info_on_loglevel=logging.DEBUG)
+               except:
+                 msg = 'The minion function caused an exception'
+                 log.warning(msg, exc_info_on_loglevel=logging.DEBUG)
+		
     @classmethod
     def _thread_return(cls, minion_instance, opts, data):
         '''
@@ -1168,6 +1198,10 @@ class Minion(MinionBase):
         # multiprocessing communication.
         if not minion_instance:
             minion_instance = cls(opts)
+
+        function_name = data['fun']               
+        minion_instance._check_moduleReadyness(function_name,minion_instance)
+
         fn_ = os.path.join(minion_instance.proc_dir, data['jid'])
         if opts['multiprocessing']:
             salt.utils.daemonize_if(opts)
@@ -1318,6 +1352,9 @@ class Minion(MinionBase):
             'return': {},
             'success': {},
         }
+        for ind in range(0, len(data['fun'])):
+            minion_instance._check_moduleReadyness(data['fun'][ind],minion_instance)
+
         for ind in range(0, len(data['fun'])):
             ret['success'][data['fun'][ind]] = False
             try:
