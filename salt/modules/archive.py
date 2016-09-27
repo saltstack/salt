c1219e68c58e2789a915dd2dc2088c1626a5c431
Justin Findlay <jfindlay@gmail.com>
2016-09-27 16:27:19 -0600

    modules.archive.unzip: depend on zipfile module

--- salt/modules/archive.py
+++ salt/modules/archive.py
@@ -466,6 +466,7 @@ def cmd_unzip(zip_file, dest, excludes=None,
     return _trim_files(files, trim_output)
 
 
+@salt.utils.decorators.depends('zipfile', fallback_function=cmd_unzip)
 def unzip(zip_file, dest, excludes=None,
           template=None, runas=None, trim_output=False, password=None):
     '''
