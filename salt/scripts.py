0e7c600e02509654a0a4cc89d937e5476e4cb60f
Erik Johnson <palehose@gmail.com>
2016-10-04 14:22:28 -0500

    Only display Ctrl-c message on SIGINT

--- salt/scripts.py
+++ salt/scripts.py
@@ -47,19 +47,22 @@ def _handle_signals(client, signum, sigframe):
     except (AttributeError, KeyError):
         hardcrash = False
 
-    exit_msg = '\nExiting gracefully on Ctrl-c'
-    try:
-        jid = client.local_client.pub_data['jid']
-        exit_msg += (
-            '\n'
-            'This job\'s jid is: {0}\n'
-            'The minions may not have all finished running and any remaining '
-            'minions will return upon completion. To look up the return data '
-            'for this job later, run the following command:\n\n'
-            'salt-run jobs.lookup_jid {0}'.format(jid)
-        )
-    except (AttributeError, KeyError):
-        pass
+    if signum == signal.SIGINT:
+        exit_msg = '\nExiting gracefully on Ctrl-c'
+        try:
+            jid = client.local_client.pub_data['jid']
+            exit_msg += (
+                '\n'
+                'This job\'s jid is: {0}\n'
+                'The minions may not have all finished running and any remaining '
+                'minions will return upon completion. To look up the return data '
+                'for this job later, run the following command:\n\n'
+                'salt-run jobs.lookup_jid {0}'.format(jid)
+            )
+        except (AttributeError, KeyError):
+            pass
+    else:
+        exit_msg = None
 
     _handle_interrupt(
         SystemExit(exit_msg),
