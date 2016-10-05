7d60e73308be9f4867781349c7cd01ef44a10bfb
Erik Johnson <palehose@gmail.com>
2016-10-05 15:44:04 -0500

    Fix pillar merging when ext_pillar_first is enabled
    ext_pillar was being merged into pillar, when it should have been the
    other way around. This means that when ext_pillar_first was enabled,
    pillar keys with the same name as ones defined in ext_pillar were being
    lost instead of overriding ext_pillar.

--- salt/pillar/__init__.py
+++ salt/pillar/__init__.py
@@ -791,8 +791,8 @@ class Pillar(object):
                 self.opts['pillar'], errors = self.ext_pillar({}, pillar_dirs)
                 matches = self.top_matches(top)
                 pillar, errors = self.render_pillar(matches, errors=errors)
-                pillar = merge(pillar,
-                               self.opts['pillar'],
+                pillar = merge(self.opts['pillar'],
+                               pillar,
                                self.merge_strategy,
                                self.opts.get('renderer', 'yaml'),
                                self.opts.get('pillar_merge_lists', False))
