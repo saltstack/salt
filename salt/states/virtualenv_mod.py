920a886ba82c018a6f325db568cea2b4d618f285
Nicole Thomas <nicole@saltstack.com>
2016-10-14 09:48:40 -0600

    Versionadded for process_dependency_links kwarg in virtualenv_mod state (#37006)

--- salt/states/virtualenv_mod.py
+++ salt/states/virtualenv_mod.py
@@ -108,6 +108,8 @@ def managed(name,
     process_dependency_links: False
         Run pip install with the --process_dependency_links flag.
 
+        .. versionadded:: Nitrogen
+
     Also accepts any kwargs that the virtualenv module will. However, some
     kwargs, such as the ``pip`` option, require ``- distribute: True``.
 
