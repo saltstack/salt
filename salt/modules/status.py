d95db293fe1fc164069c4ddad0603b2362d8855d
Justin Findlay <jfindlay@gmail.com>
2016-10-20 12:01:39 -0600

    modules.status.uptime: add macOS support

--- salt/modules/status.py
+++ salt/modules/status.py
@@ -148,26 +148,38 @@ def uptime():
 
         salt '*' status.uptime
     '''
-    ut_ret = {'seconds': 0}
+    epoch_seconds = time.time()
+
     if salt.utils.is_linux():
         ut_path = "/proc/uptime"
         if not os.path.exists(ut_path):
             raise CommandExecutionError("File {ut_path} was not found.".format(ut_path=ut_path))
-        ut_ret['seconds'] = int(float(open(ut_path).read().strip().split()[0]))
+        seconds = int(float(open(ut_path).read().split()[0]))
     elif salt.utils.is_sunos():
         cmd = "kstat -p unix:0:system_misc:boot_time | nawk '{printf \"%d\\n\", srand()-$2}'"
-        ut_ret['seconds'] = int(__salt__['cmd.shell'](cmd, output_loglevel='trace').strip() or 0)
+        seconds = int(__salt__['cmd.shell'](cmd, output_loglevel='trace').strip() or 0)
+    elif salt.utils.is_darwin():
+        bt_data = __salt__['sysctl.get']('kern.boottime')
+        if not bt_data:
+            raise CommandExecutionError('Cannot find kern.boottime system parameter')
+        sec_data, usec_data = bt_data.split('}')[0].strip(' {').split(', ')
+        seconds = int(epoch_seconds - int(sec_data.split('sec = ')[1]))
     else:
         return __salt__['cmd.run']('uptime')
 
-    utc_time = datetime.datetime.utcfromtimestamp(time.time() - ut_ret['seconds'])
-    ut_ret['since_iso'] = utc_time.isoformat()
-    ut_ret['since_t'] = time.mktime(utc_time.timetuple())
-    ut_ret['days'] = ut_ret['seconds'] // 60 // 60 // 24
-    hours = (ut_ret['seconds'] - (ut_ret['days'] * 24 * 60 * 60)) // 60 // 60
-    minutes = ((ut_ret['seconds'] - (ut_ret['days'] * 24 * 60 * 60)) // 60) - hours * 60
-    ut_ret['time'] = '{0}:{1}'.format(hours, minutes)
-    ut_ret['users'] = len(__salt__['cmd.run']("who -s").split(os.linesep))
+    ut = datetime.datetime.utcfromtimestamp(epoch_seconds - seconds)
+    days = seconds / 60 / 60 / 24
+    hours = (seconds - (days * 24 * 60 * 60)) / 60 / 60
+    minutes = ((seconds - (days * 24 * 60 * 60)) / 60) - hours * 60
+
+    ut_ret = {
+        'seconds': seconds,
+        'since_iso': ut.isoformat(),
+        'since_t': time.mktime(ut.timetuple()),
+        'days': days,
+        'time': '{0}:{1}'.format(hours, minutes),
+        'users': len(__salt__['cmd.run']("who -s").split(os.linesep)),
+    }
 
     return ut_ret
 
