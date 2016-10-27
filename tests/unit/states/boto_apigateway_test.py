6c63c4e2cd80565a7ac69f2871901da820e73c92
Mike Place <mp@saltstack.com>
2016-10-27 16:04:19 +0900

    Skip boto tests if deps are not met

--- tests/unit/states/boto_apigateway_test.py
+++ tests/unit/states/boto_apigateway_test.py
@@ -425,7 +425,7 @@ class BotoApiGatewayStateTestCaseBase(TestCase):
         self.conn = MagicMock()
         session_instance.client.return_value = self.conn
 
-
+@skipIf('boto_apigateway.describe_apis' not in funcs, 'The boto_apigateway execution module is needed and is not available.')
 @skipIf(HAS_BOTO is False, 'The boto module must be installed.')
 @skipIf(_has_required_boto() is False, 'The boto3 module must be greater than'
                                        ' or equal to version {0}'
