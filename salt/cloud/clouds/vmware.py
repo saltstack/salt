7e6129f1416c024a1bb45be2bad04667ce114f1d
Nitin Madhok <nmadhok@g.clemson.edu>
2016-10-28 08:15:52 -0400

    Function argument is called disk instead of device

--- salt/cloud/clouds/vmware.py
+++ salt/cloud/clouds/vmware.py
@@ -608,7 +608,7 @@ def _get_mode_spec(device, mode, disk_spec):
     if device.backing.diskMode != mode:
         if not disk_spec:
             disk_spec = _edit_existing_hard_disk_helper(
-                device=device,
+                disk=device,
                 mode=mode
             )
         else:
@@ -618,7 +618,7 @@ def _get_mode_spec(device, mode, disk_spec):
 
 def _get_size_spec(device, size_gb):
     size_kb = int(size_gb * 1024.0 * 1024.0)
-    disk_spec = _edit_existing_hard_disk_helper(device=device, size_kb=size_kb) if device.capacityInKB < size_kb else None
+    disk_spec = _edit_existing_hard_disk_helper(disk=device, size_kb=size_kb) if device.capacityInKB < size_kb else None
     return disk_spec
 
 
