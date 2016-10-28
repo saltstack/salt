8b23ce328edcc12e18dd0faeef7d60333f32c1fa
Nitin Madhok <nmadhok@g.clemson.edu>
2016-10-28 06:10:06 -0400

    Fixing typo

--- salt/cloud/clouds/vmware.py
+++ salt/cloud/clouds/vmware.py
@@ -618,7 +618,7 @@ def _get_mode_spec(device, mode, disk_spec):
 
 def _get_size_spec(device, size_gb):
     size_kb = int(size_gb * 1024.0 * 1024.0)
-    disk_spec = _edit_existing_hard_disk_helper(device=device, size_kb=size_kb) if device.capacityInKB < size_kb else None:
+    disk_spec = _edit_existing_hard_disk_helper(device=device, size_kb=size_kb) if device.capacityInKB < size_kb else None
     return disk_spec
 
 
