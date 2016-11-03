c32947abfc0c0493d6d206d71b7770924568fb4d
Prince  Tyagi <prince@Administrators-MacBook-Pro-32.local>
2016-11-03 15:03:50 +0530

    updated influxdb returner and fixed typo errors.

--- salt/returners/influxdb_return.py
+++ salt/returners/influxdb_return.py
@@ -109,12 +109,11 @@ def _get_version(host, port, user, password):
     # check the InfluxDB version via the HTTP API
     try:
         result = requests.get("http://{0}:{1}/ping".format(host, port), auth=(user, password))
-        if result.status_code == 200 and influxDBVersionHeader in result.headers:
+        if influxDBVersionHeader in result.headers:
             version = result.headers[influxDBVersionHeader]
     except Exception as ex:
         log.critical('Failed to query InfluxDB version from HTTP API within InfluxDB returner: {0}'.format(ex))
     return version
-
 
 def _get_serv(ret=None):
     '''
@@ -178,7 +177,7 @@ def returner(ret):
                 },
                 'fields': {
                     'return': json_return,
-                    'ful_ret': json_full_ret
+                    'full_ret': json_full_ret
                 }
             }
         ]
@@ -189,7 +188,7 @@ def returner(ret):
         log.critical('Failed to store return with InfluxDB returner: {0}'.format(ex))
 
 
-def save_load(jid, load):
+def save_load(jid, load, minions=None):
     '''
     Save the load to the specified jid
     '''
@@ -256,7 +255,10 @@ def get_jid(jid):
 
     sql = "select id, full_ret from returns where jid = '{0}'".format(jid)
 
+    log.debug(">> Now in get_jid {0}".format(jid))
     data = serv.query(sql)
+    log.debug(">> Now Data: {0}".format(data))
+
     ret = {}
     if data:
         points = data[0]['points']
@@ -278,7 +280,9 @@ def get_fun(fun):
             group by fun, id
           '''.format(fun)
 
+    log.debug(">> Now in get_fun {0}".format(fun))
     data = serv.query(sql)
+    log.debug(">> Now Data: {0}".format(data))
     ret = {}
     if data:
         points = data[0]['points']
@@ -299,7 +303,11 @@ def get_jids():
     #               [0, jid, load]],
     #   u'name': u'jids',
     #   u'columns': [u'time', u'distinct', u'load']}]
+
+    log.debug(">> Now in get_jids")
     data = serv.query(sql)
+    log.debug(">> Now Data: {0}".format(data))
+
     ret = {}
     if data:
         for _, jid, load in data[0]['points']:
