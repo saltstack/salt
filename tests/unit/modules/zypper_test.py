4ae036055da646b0f43a91739c310fe6fc18c3af
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2016-11-04 09:01:30 +0000

    Fix unit test for Zypper dist-upgrade in Carbon

--- tests/unit/modules/zypper_test.py
+++ tests/unit/modules/zypper_test.py
@@ -371,8 +371,6 @@ class ZypperTestCase(TestCase):
 
             with patch('salt.modules.zypper.list_pkgs', MagicMock(side_effect=[{"vim": "1.1"}, {"vim": "1.1"}])):
                 ret = zypper.upgrade(dist_upgrade=True, dryrun=True, fromrepo=["Dummy", "Dummy2"], novendorchange=True)
-                self.assertTrue(ret['result'])
-                self.assertDictEqual(ret['changes'], {})
                 zypper_mock.assert_any_call('dist-upgrade', '--auto-agree-with-licenses', '--dry-run', '--from', "Dummy", '--from', 'Dummy2', '--no-allow-vendor-change')
                 zypper_mock.assert_any_call('dist-upgrade', '--auto-agree-with-licenses', '--dry-run', '--from', "Dummy", '--from', 'Dummy2', '--no-allow-vendor-change', '--debug-solver')
 
