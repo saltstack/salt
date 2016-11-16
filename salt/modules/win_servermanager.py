23b8b2a3f4c4b7ea7fe3a007e6716d16c1e8d8fc
Erik Johnson <palehose@gmail.com>
2016-11-15 22:09:32 -0600

    Fix incorrectly-formatted RST

--- salt/modules/win_servermanager.py
+++ salt/modules/win_servermanager.py
@@ -143,7 +143,8 @@ def install(feature, recurse=False, source=None, restart=False, exclude=None):
     :param str exclude: The name of the feature to exclude when installing the
         named feature.
 
-        ..note:: As there is no exclude option for the ``Add-WindowsFeature``
+        .. note::
+            As there is no exclude option for the ``Add-WindowsFeature``
             command, the feature will be installed with other sub-features and
             will then be removed.
 
