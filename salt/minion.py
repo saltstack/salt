saltstack/salt

commit 9ec7c7c620d17bcae5a7b19aaa9800a93c48b60e

Fix salt-call on standalone minion case (#32521)
* skip connection error if master_type == disable

Signed-off-by: Alejandro del Castillo <alejandro.delcastillo@ni.com>

* fix salt-call when master_type == disable

salt-call errors out on salt.pillar.get_pillar if master_type ==
disable. Skip get_pillar call on standalone minion case. Also skip code
that returns job info back to the master.

Signed-off-by: Alejandro del Castillo <alejandro.delcastillo@ni.com>

salt/minion.py
@@ -628,13 +628,15 @@ class SMinion(MinionBase):
         '''
         Load all of the modules for the minion
         '''
-        self.opts['pillar'] = salt.pillar.get_pillar(
-            self.opts,
-            self.opts['grains'],
-            self.opts['id'],
-            self.opts['environment'],
-            pillarenv=self.opts.get('pillarenv'),
-        ).compile_pillar()
+        if self.opts.get('master_type') != 'disable':
+            self.opts['pillar'] = salt.pillar.get_pillar(
+                self.opts,
+                self.opts['grains'],
+                self.opts['id'],
+                self.opts['environment'],
+                pillarenv=self.opts.get('pillarenv'),
+            ).compile_pillar()
+
         self.utils = salt.loader.utils(self.opts)
         self.functions = salt.loader.minion_mods(self.opts, utils=self.utils,
                                                  include_errors=True)
@@ -2034,7 +2036,7 @@ class Minion(MinionBase):
         # add handler to subscriber
         if hasattr(self, 'pub_channel') and self.pub_channel is not None:
             self.pub_channel.on_recv(self._handle_payload)
-        else:
+        elif self.opts.get('master_type') != 'disable':
             log.error('No connection to master found. Scheduled jobs will not run.')
 
         if start:

salt/cli/caller.py
@@ -229,7 +229,8 @@ class BaseCaller(object):
             if isinstance(oput, six.string_types):
                 ret['out'] = oput
         is_local = self.opts['local'] or self.opts.get(
-            'file_client', False) == 'local'
+            'file_client', False) == 'local' or self.opts.get(
+            'master_type') == 'disable'
         returners = self.opts.get('return', '').split(',')
         if (not is_local) or returners:
             ret['id'] = self.opts['id']
@@ -246,7 +247,6 @@ class BaseCaller(object):
                 pass
 
         # return the job infos back up to the respective minion's master
-
         if not is_local:
             try:
                 mret = ret.copy()

commit 090efa876c0dac6732368eb838a348e6315fa56f

Don't skip pillar compilation when master_type=='disable'
This was broken in pull #32521. The community member who opened that PR
was apparently unaware of the fact that masterless minions will use the
Pillar instead of RemotePillar class, so skipping the Pillar compilation
here results in the packed pillar dunder being completely empty.

This PR fixes that by removing the check for master_type and allowing
local pillar data to be compiled.

salt/minion.py
@@ -673,14 +673,13 @@ class SMinion(MinionBase):
         '''
         Load all of the modules for the minion
         '''
-        if self.opts.get('master_type') != 'disable':
-            self.opts['pillar'] = salt.pillar.get_pillar(
-                self.opts,
-                self.opts['grains'],
-                self.opts['id'],
-                self.opts['environment'],
-                pillarenv=self.opts.get('pillarenv'),
-            ).compile_pillar()
+        self.opts['pillar'] = salt.pillar.get_pillar(
+            self.opts,
+            self.opts['grains'],
+            self.opts['id'],
+            self.opts['environment'],
+            pillarenv=self.opts.get('pillarenv'),
+        ).compile_pillar()
 
         self.utils = salt.loader.utils(self.opts)
         self.functions = salt.loader.minion_mods(self.opts, utils=self.utils)