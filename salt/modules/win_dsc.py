c32401630b95853ae855021ac82003b660d9b6ba
twangboy <slee@saltstack.com>
2016-11-23 11:21:48 -0700

    Add config_data and parameters
    Fix docs formatting

--- salt/modules/win_dsc.py
+++ salt/modules/win_dsc.py
@@ -5,7 +5,7 @@ Module for working with DSC (Alpha)
 :depends:
     - PowerShell 5.0
 '''
-from __future__ import absolute_import, unicode_literals
+from __future__ import absolute_import
 
 # Import python libs
 import logging
@@ -72,7 +72,13 @@ def _pshell(cmd, cwd=None, json_depth=2):
     return ret
 
 
-def run_config(path, source=None, config=None, salt_env='base'):
+def run_config(path,
+               source=None,
+               config=None,
+               config_data=None,
+               config_data_source=None,
+               script_parameters=None,
+               salt_env='base'):
     r'''
     Compile a DSC Configuration in the form of a powershell script (.ps1) and
     apply it. The powershell script can be cached from the master using the
@@ -83,27 +89,39 @@ def run_config(path, source=None, config=None, salt_env='base'):
     This command would be the equivalent of running ``dsc.compile_config`` and
     ``dsc.apply_config`` separately.
 
-    :param str path: The local path to the powershell script that contains the
-    DSC Configuration.
-    Required.
+    Args:
 
-    :param str source: The path to the script on ``file_roots`` to cache at the
-    location specified by ``path``. The source file will be cached locally and
-    then executed. If source is not passed, the config script located at
-    ``path`` will be compiled.
-    Optional.
+        path (str): The local path to the powershell script that contains the
+            DSC Configuration. Required.
 
-    :param str config: The name of the Configuration within the script to apply.
-    If the script contains multiple configurations within the file a config
-    must be specified. If the config is not specified, the name of the file will
-    be used as the config to run.
-    Optional.
+        source (str): The path to the script on ``file_roots`` to cache at the
+            location specified by ``path``. The source file will be cached
+            locally and then executed. If source is not passed, the config
+            script located at ``path`` will be compiled. Optional.
 
-    :param str salt_env: The salt environment to use when copying the source.
-    Default is 'base'
+        config (str): The name of the Configuration within the script to apply.
+            the script contains multiple configurations within the file a config
+            must be specified. If the config is not specified, the name of the
+            file will be used as the config to run. Optional.
 
-    :return: True if successfully compiled and applied, False if not
-    :rtype: bool
+        config_data (str): Configuration data in the form of a hash table that
+            will be passed to the ``ConfigurationData`` parameter when the
+            configuration script is run. This can be the path to a ``.psd1``
+            file or the PowerShell code to create the hash table.
+
+        config_data_source (str): The path to the ``.psd1`` file on
+            ``file_roots`` to cache at the location specified by
+            ``config_data``. If this is specified, ``config_data`` must be a
+            local path instead of a hash table.
+
+        script_parameters (str): Any additional parameters expected by the
+            configuration script. These must be defined in the script itself.
+
+        salt_env (str): The salt environment to use when copying the source.
+            Default is 'base'
+
+    Returns:
+        bool: True if successfully compiled and applied, False if not
 
     CLI Example:
 
@@ -119,7 +137,13 @@ def run_config(path, source=None, config=None, salt_env='base'):
 
         salt '*' dsc.compile_apply_config C:\\DSC\\WebsiteConfig.ps1 salt://dsc/configs/WebsiteConfig.ps1
     '''
-    ret = compile_config(path, source, config, salt_env)
+    ret = compile_config(path=path,
+                         source=source,
+                         config=config,
+                         config_data=config_data,
+                         config_data_source=config_data_source,
+                         script_parameters=script_parameters,
+                         salt_env=salt_env)
 
     if ret.get('Exists'):
         config_path = os.path.dirname(ret['FullName'])
@@ -128,31 +152,50 @@ def run_config(path, source=None, config=None, salt_env='base'):
         return False
 
 
-def compile_config(path, source=None, config=None, salt_env='base'):
+def compile_config(path,
+                   source=None,
+                   config=None,
+                   config_data=None,
+                   config_data_source=None,
+                   script_parameters=None,
+                   salt_env='base'):
     r'''
     Compile a config from a powershell script (``.ps1``)
 
-    :param str path: Path (local) to the script that will create the ``.mof``
-    configuration file. If no source is passed, the file must exist locally.
-    Required.
+    Args:
 
-    :param str source: Path to the script on ``file_roots`` to cache at the
-    location specified by ``path``. The source file will be cached locally and
-    then executed. If source is not passed, the config script located at
-    ``path`` will be compiled.
-    Optional.
+        path (str): Path (local) to the script that will create the ``.mof``
+            configuration file. If no source is passed, the file must exist
+            locally. Required.
 
-    :param str config: The name of the Configuration within the script to apply.
-    If the script contains multiple configurations within the file a config
-    must be specified. If the config is not specified, the name of the file will
-    be used as the config to run.
-    Optional.
+        source (str): Path to the script on ``file_roots`` to cache at the
+            location specified by ``path``. The source file will be cached
+            locally and then executed. If source is not passed, the config
+            script located at ``path`` will be compiled. Optional.
 
-    :param str salt_env: The salt environment to use when copying the source.
-    Default is 'base'
+        config (str): The name of the Configuration within the script to apply.
+            If the script contains multiple configurations within the file a
+            config must be specified. If the config is not specified, the name
+            of the file will be used as the config to run. Optional.
 
-    :return: A dictionary containing the results of the compilation
-    :rtype: dict
+        config_data (str): Configuration data in the form of a hash table that
+            will be passed to the ``ConfigurationData`` parameter when the
+            configuration script is run. This can be the path to a ``.psd1``
+            file or the PowerShell code to create the hash table.
+
+        config_data_source (str): The path to the ``.psd1`` file on
+            ``file_roots`` to cache at the location specified by
+            ``config_data``. If this is specified, ``config_data`` must be a
+            local path instead of a hash table.
+
+        script_parameters (str): Any additional parameters expected by the
+            configuration script. These must be defined in the script itself.
+
+        salt_env (str): The salt environment to use when copying the source.
+            Default is 'base'
+
+    Returns:
+        dict: A dictionary containing the results of the compilation
 
     CLI Example:
 
@@ -179,6 +222,17 @@ def compile_config(path, source=None, config=None, salt_env='base'):
             log.error(error)
             raise CommandExecutionError(error)
 
+    if config_data_source:
+        log.info('Caching {0}'.format(config_data_source))
+        cached_files = __salt__['cp.get_file'](path=config_data_source,
+                                               dest=config_data,
+                                               saltenv=salt_env,
+                                               makedirs=True)
+        if not cached_files:
+            error = 'Failed to cache {0}'.format(config_data_source)
+            log.error(error)
+            raise CommandExecutionError(error)
+
     # Make sure the path exists
     if not os.path.exists(path):
         error = '"{0} not found.'.format(path)
@@ -192,10 +246,16 @@ def compile_config(path, source=None, config=None, salt_env='base'):
     cwd = os.path.dirname(path)
 
     # Run the script and see if the compile command is in the script
-    cmd = '{0} '.format(path)
-    cmd += '| Select-Object -Property FullName, Extension, Exists, ' \
-           '@{Name="LastWriteTime";Expression={Get-Date ($_.LastWriteTime) ' \
-           '-Format g}}'
+    cmd = [path]
+    # Add any script parameters
+    if script_parameters:
+        cmd.append(script_parameters)
+    # Select fields to return
+    cmd.append('| Select-Object -Property FullName, Extension, Exists, '
+               '@{Name="LastWriteTime";Expression={Get-Date ($_.LastWriteTime) '
+               '-Format g}}')
+
+    cmd = ' '.join(cmd)
 
     ret = _pshell(cmd, cwd)
 
@@ -206,10 +266,17 @@ def compile_config(path, source=None, config=None, salt_env='base'):
             return ret
 
     # Run the script and run the compile command
-    cmd = '. {0} ; {1} '.format(path, config)
-    cmd += '| Select-Object -Property FullName, Extension, Exists, ' \
-           '@{Name="LastWriteTime";Expression={Get-Date ($_.LastWriteTime) ' \
-           '-Format g}}'
+    cmd = ['.', path]
+    if script_parameters:
+        cmd.append(script_parameters)
+    cmd.extend([';', config])
+    if config_data:
+        cmd.append(config_data)
+    cmd.append('| Select-Object -Property FullName, Extension, Exists, '
+               '@{Name="LastWriteTime";Expression={Get-Date ($_.LastWriteTime) '
+               '-Format g}}')
+
+    cmd = ' '.join(cmd)
 
     ret = _pshell(cmd, cwd)
 
