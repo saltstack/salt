5f9b6c8ba430bbd5ab8983c41606c0ada153b2fe
Elias Wulcan <elias.wulcan@apptus.com>
2016-12-14 14:41:03 +0100

    Fixed nested orchestrate not respecting failures
    Client was only setting success to false if the function called raised
    an exception. This commit changes it to verify the content of return
    value with check_state_result

--- salt/client/mixins.py
+++ salt/client/mixins.py
@@ -396,6 +396,8 @@ class SyncClientMixin(object):
             with tornado.stack_context.StackContext(self.functions.context_dict.clone):
                 data['return'] = self.functions[fun](*args, **kwargs)
                 data['success'] = True
+                if 'data' in data['return']:
+                    data['success'] = salt.utils.check_state_result(data['return']['data'])
         except (Exception, SystemExit) as ex:
             if isinstance(ex, salt.exceptions.NotImplemented):
                 data['return'] = str(ex)
