6a206025c6fca0d9362a61bebc99c20d1983d02e
rallytime <nicole@saltstack.com>
2016-12-14 16:27:28 -0700

    [develop] Bump latest release version to 2016.11.1

--- doc/conf.py
+++ doc/conf.py
@@ -232,7 +232,7 @@ on_saltstack = 'SALT_ON_SALTSTACK' in os.environ
 project = 'Salt'
 
 version = salt.version.__version__
-latest_release = '2016.11.0'  # latest release
+latest_release = '2016.11.1'  # latest release
 previous_release = '2016.3.4'  # latest release from previous branch
 previous_release_dir = '2016.3'  # path on web server for previous branch
 next_release = ''  # next release
