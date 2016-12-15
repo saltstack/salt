acbbcad1834212d8090ffa6e4446cef188db0fc7
Bruno Binet <bruno.binet@gmail.com>
2016-12-15 11:01:52 +0100

    add grafana4_org salt state

---
+++ salt/states/grafana4_org.py
@@ -0,0 +1,234 @@
+# -*- coding: utf-8 -*-
+'''
+Manage Grafana v4.0 orgs
+
+Token auth setup
+
+.. code-block:: yaml
+
+    grafana_version: 4
+    grafana:
+      grafana_timeout: 5
+      grafana_token: qwertyuiop
+      grafana_url: 'https://url.com'
+
+Basic auth setup
+
+.. code-block:: yaml
+
+    grafana_version: 4
+    grafana:
+      grafana_timeout: 5
+      grafana_org: grafana
+      grafana_password: qwertyuiop
+      grafana_url: 'https://url.com'
+
+.. code-block:: yaml
+
+    Ensure foobar org is present:
+      grafana4_org.present:
+        - name: foobar
+        - theme:  ""
+        - home_dashboard_id: 0
+        - timezone: "utc"
+        - address1: ""
+        - address2: ""
+        - city: ""
+        - zip_code: ""
+        - state: ""
+        - country: ""
+'''
+from __future__ import absolute_import
+from collections import Mapping
+from copy import deepcopy
+
+from salt.ext.six import string_types
+from salt.utils import dictupdate
+from requests.exceptions import HTTPError
+
+
+def __virtual__():
+    '''Only load if grafana v3.0 is configured.'''
+    return __salt__['config.get']('grafana_version', 1) == 4
+
+
+def present(name,
+            users={},
+            theme=None,
+            home_dashboard_id=None,
+            timezone=None,
+            address1=None,
+            address2=None,
+            city=None,
+            zip_code=None,
+            address_state=None,
+            country=None,
+            profile='grafana'):
+    '''
+    Ensure that an organization is present.
+
+    name
+        Name of the org.
+
+    users
+        Optional - Dict of user/role associated with the org. Example:
+        users:
+          foo: Viewer
+          bar: Editor 
+
+    theme
+        Optional - Selected theme for the org.
+
+    home_dashboard_id
+        Optional - Home dashboard for the org.
+    
+    timezone
+        Optional - Timezone for the org (one of: "browser", "utc", or "").
+    '''
+    if isinstance(profile, string_types):
+        profile = __salt__['config.option'](profile)
+
+    ret = {'name': name, 'result': None, 'comment': None, 'changes': {}}
+    create = False
+    try:
+        org = __salt__['grafana4.get_org'](name, profile)
+    except HTTPError as e:
+        if e.response.status_code == 404:
+            create = True
+        else:
+            raise
+
+    if create:
+        __salt__['grafana4.create_org'](profile=profile, name=name)
+        org = __salt__['grafana4.get_org'](name, profile)
+        ret['changes'] = org
+        ret['comment'] = 'New org {0} added'.format(name)
+
+    data = _get_json_data(address1=address1, address2=address2,
+        city=city, zipCode=zip_code, state=address_state, country=country,
+        defaults=org['address'])
+    if data != org['address']:
+        __salt__['grafana4.update_org_address'](name, profile=profile, **data)
+        if create:
+            dictupdate.update(ret['changes']['address'], data)
+        else:
+            dictupdate.update(ret['changes'], _deep_diff(org['address'], data))
+
+    prefs = __salt__['grafana4.get_org_prefs'](name, profile=profile)
+    data = _get_json_data(theme=theme, homeDashboardId=home_dashboard_id,
+        timezone=timezone, defaults=prefs)
+    if data != prefs:
+        __salt__['grafana4.update_org_prefs'](name, profile=profile, **data)
+        if create:
+            dictupdate.update(ret['changes'], data)
+        else:
+            dictupdate.update(ret['changes'], _deep_diff(prefs, data))
+
+    if users:
+        db_users = {}
+        for item in __salt__['grafana4.get_org_users'](name, profile=profile):
+            db_users[item['login']] = {
+                'userId': item['userId'],
+                'role': item['role'],
+                }
+        for username, role in users.items():
+            if username in db_users:
+                if role == False:
+                    __salt__['grafana4.delete_org_users'](
+                        db_users[username]['userId'], profile=profile)
+                elif role != db_users[username]['role']:
+                    __salt__['grafana4.update_org_users'](
+                        db_users[username]['userId'], loginOrEmail=username,
+                        role=role, profile=profile)
+            elif role != False:
+                __salt__['grafana4.create_org_users'](
+                    loginOrEmail=username, role=role, profile=profile)
+
+        new_db_users = {}
+        for item in __salt__['grafana4.get_org_users'](name, profile=profile):
+            new_db_users[item['login']] = {
+                'userId': item['userId'],
+                'role': item['role'],
+                }
+        if create:
+            dictupdate.update(ret['changes'], new_db_users)
+        else:
+            dictupdate.update(ret['changes'], _deep_diff(db_users, new_db_users))
+
+    ret['result'] = True
+    if not create:
+        if ret['changes']:
+            ret['comment'] = 'Org {0} updated'.format(name)
+        else:
+            ret['changes'] = None
+            ret['comment'] = 'Org {0} already up-to-date'.format(name)
+
+    return ret
+
+
+def absent(name, profile='grafana'):
+    '''
+    Ensure that a org is present.
+
+    name
+        Name of the org to remove.
+    '''
+    if isinstance(profile, string_types):
+        profile = __salt__['config.option'](profile)
+
+    ret = {'name': name, 'result': None, 'comment': None, 'changes': {}}
+    org = __salt__['grafana4.get_org'](name, profile)
+
+    if not org:
+        ret['result'] = True
+        ret['comment'] = 'Org {0} already absent'.format(name)
+        return ret
+
+    __salt__['grafana4.delete_org'](org['id'], profile=profile)
+
+    ret['result'] = True
+    ret['changes'][name] = 'Absent'
+    ret['comment'] = 'Org {0} was deleted'.format(name)
+
+    return ret
+
+
+def _get_json_data(defaults=None, **kwargs):
+    if defaults is None:
+        defaults = {}
+    for k, v in kwargs.items():
+        if v is None:
+            kwargs[k] = defaults.get(k)
+    return kwargs
+
+
+def _deep_diff(old, new, ignore=[]):
+    res = {}
+    old = deepcopy(old)
+    new = deepcopy(new)
+    stack = [(old, new, False)]
+
+    while len(stack) > 0:
+        tmps = []
+        tmp_old, tmp_new, reentrant = stack.pop()
+        for key in set(tmp_old.keys() + tmp_new.keys()):
+            if key in tmp_old and key in tmp_new \
+                    and tmp_old[key] == tmp_new[key]:
+                del tmp_old[key]
+                del tmp_new[key]
+                continue
+            if not reentrant:
+                if key in tmp_old and key in ignore:
+                    del tmp_old[key]
+                if key in tmp_new and key in ignore:
+                    del tmp_new[key]
+                if isinstance(tmp_old.get(key), Mapping) \
+                        and isinstance(tmp_new.get(key), Mapping):
+                    tmps.append((tmp_old[key], tmp_new[key], False))
+        if tmps:
+            stack.extend([(tmp_old, tmp_new, True)] + tmps)
+    if old:
+        res['old'] = old
+    if new:
+        res['new'] = new
+    return res
