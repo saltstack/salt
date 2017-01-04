5941a7c5be67a4e28401c86d1aa48860b1ca8c8f
Christian McHugh <McHughC@DNB.com>
2017-01-04 17:55:40 +0000

    Modify mysql module to return False on failure Add file hanlding to mysql module Add file hanlding to mysql state

--- salt/modules/mysql.py
+++ salt/modules/mysql.py
@@ -647,7 +647,7 @@ def query(database, query, **connection_args):
         err = 'MySQL Error {0}: {1}'.format(*exc)
         __context__['mysql.error'] = err
         log.error(err)
-        return {}
+        return False
     results = cur.fetchall()
     elapsed = (time.time() - start)
     if elapsed < 0.200:
@@ -674,6 +674,49 @@ def query(database, query, **connection_args):
     else:
         ret['rows affected'] = affected
         return ret
+
+
+def file_query(database, file_name, **connection_args):
+    if os.path.exists(file_name):
+        with salt.utils.fopen(file_name, 'r') as ifile:
+            contents = ifile.read()
+    else:
+        log.error('File "{0}" does not exist'.format(file_name))
+        return False
+
+    query_string = ""
+    ret = {'rows returned': 0, 'columns': 0, 'results': 0, 'rows affected': 0, 'query time': {'raw': 0}}
+    for line in contents.splitlines():
+        if re.match(r'--', line): #ignore sql comments
+            continue
+        if not re.search(r'[^-;]+;', line): # keep appending lines that don't end in ;
+            query_string = query_string + line
+        else:
+            query_string = query_string + line # append lines that end with ; and run query
+            query_result=query(database, query_string, **connection_args)
+            query_string = ""
+
+            if query_result == False:
+                # Fail out on error
+                return False
+
+            if 'query time' in query_result:
+                ret['query time']['raw'] += float(query_result['query time']['raw'])
+            if 'rows returned' in query_result:
+                ret['rows returned'] += query_result['rows returned']
+            if 'columns' in query_result:
+                ret['columns'] += query_result['columns']
+            if 'results' in query_result:
+                ret['results'] += query_result['results']
+            if 'rows affected' in query_result:
+                ret['rows affected'] += query_result['rows affected']
+    ret['query time']['human'] = str(round(float(ret['query time']['raw']), 2)) + 's'
+    ret['query time']['raw'] = round(float(ret['query time']['raw']), 5)
+
+    # Remove empty keys in ret
+    ret = dict((k, v) for k, v in ret.iteritems() if v)
+
+    return ret
 
 
 def status(**connection_args):
