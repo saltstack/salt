cf54216e31d8867da7fa1428d11ebd84219465f8
Mike Place <mp@saltstack.com>
2017-01-06 12:51:33 -0700

    Double-quotes to single-quotes
    This is to comply with the Salt style guide. No functional changes were made.

--- salt/modules/namecheap_dns.py
+++ salt/modules/namecheap_dns.py
@@ -52,16 +52,16 @@ __virtualname__ = 'namecheap_domains_dns'
 
 
 def __virtual__():
-    """
+    '''
     Check to make sure requests and xml are installed and requests
-    """
+    '''
     if CAN_USE_NAMECHEAP:
         return 'namecheap_domains_dns'
     return False
 
 
 def get_hosts(sld, tld):
-    """
+    '''
     Retrieves DNS host record settings for the requested domain.
 
     returns a dictionary of information about the requested domain
@@ -71,7 +71,7 @@ def get_hosts(sld, tld):
 
     tld
         string  tld of the domainname
-    """
+    '''
     opts = salt.utils.namecheap.get_opts('namecheap.domains.dns.gethosts')
     opts['TLD'] = tld
     opts['SLD'] = sld
@@ -80,13 +80,13 @@ def get_hosts(sld, tld):
     if response_xml is None:
         return {}
 
-    domaindnsgethostsresult = response_xml.getElementsByTagName("DomainDNSGetHostsResult")[0]
+    domaindnsgethostsresult = response_xml.getElementsByTagName('DomainDNSGetHostsResult')[0]
 
     return salt.utils.namecheap.xml_to_dict(domaindnsgethostsresult)
 
 
 def get_list(sld, tld):
-    """
+    '''
     Gets a list of DNS servers associated with the requested domain.
 
     returns a dictionary of information about requested domain
@@ -96,7 +96,7 @@ def get_list(sld, tld):
 
     tld
         string  tld of the domain name
-    """
+    '''
     opts = salt.utils.namecheap.get_opts('namecheap.domains.dns.getlist')
     opts['TLD'] = tld
     opts['SLD'] = sld
@@ -105,13 +105,13 @@ def get_list(sld, tld):
     if response_xml is None:
         return {}
 
-    domaindnsgetlistresult = response_xml.getElementsByTagName("DomainDNSGetListResult")[0]
+    domaindnsgetlistresult = response_xml.getElementsByTagName('DomainDNSGetListResult')[0]
 
     return salt.utils.namecheap.xml_to_dict(domaindnsgetlistresult)
 
 
 def set_hosts(sld, tld, hosts):
-    """
+    '''
     Sets DNS host records settings for the requested domain.
 
     returns True if the host records were set successfully
@@ -132,7 +132,7 @@ def set_hosts(sld, tld, hosts):
                                'recordtype' should be A,AAAA,CNAME,MX,MXE,TXT,URL,URL301,FRAME
                                'address' should be url or ip address
                                'ttl' should be an integer between 60 to 60000
-    """
+    '''
     opts = salt.utils.namecheap.get_opts('namecheap.domains.dns.setHosts')
     opts['SLD'] = sld
     opts['TLD'] = tld
@@ -153,12 +153,12 @@ def set_hosts(sld, tld, hosts):
     if response_xml is None:
         return False
 
-    dnsresult = response_xml.getElementsByTagName("DomainDNSSetHostsResult")[0]
-    return salt.utils.namecheap.string_to_value(dnsresult.getAttribute("IsSuccess"))
+    dnsresult = response_xml.getElementsByTagName('DomainDNSSetHostsResult')[0]
+    return salt.utils.namecheap.string_to_value(dnsresult.getAttribute('IsSuccess'))
 
 
 def set_custom(sld, tld, nameservers):
-    """
+    '''
     Sets domain to use custom DNS servers.
 
     returns True if the custom nameservers were set successfully
@@ -171,7 +171,7 @@ def set_custom(sld, tld, nameservers):
 
     nameservers
         array of strings  List of nameservers to be associated with this domain
-    """
+    '''
     opts = salt.utils.namecheap.get_opts('namecheap.domains.dns.setCustom')
     opts['SLD'] = sld
     opts['TLD'] = tld
@@ -180,12 +180,12 @@ def set_custom(sld, tld, nameservers):
     if response_xml is None:
         return False
 
-    dnsresult = response_xml.getElementsByTagName("DomainDNSSetCustomResult")[0]
-    return salt.utils.namecheap.string_to_value(dnsresult.getAttribute("Update"))
+    dnsresult = response_xml.getElementsByTagName('DomainDNSSetCustomResult')[0]
+    return salt.utils.namecheap.string_to_value(dnsresult.getAttribute('Update'))
 
 
 def set_default(sld, tld):
-    """
+    '''
     Sets domain to use namecheap default DNS servers.
     Required for free services like Host record management,
     URL forwarding, email forwarding, dynamic dns and other value added services.
@@ -197,7 +197,7 @@ def set_default(sld, tld):
 
     tld
         string  tld of the domain name
-    """
+    '''
     opts = salt.utils.namecheap.get_opts('namecheap.domains.dns.setDefault')
     opts['SLD'] = sld
     opts['TLD'] = tld
@@ -205,5 +205,5 @@ def set_default(sld, tld):
     if response_xml is None:
         return False
 
-    dnsresult = response_xml.getElementsByTagName("DomainDNSSetDefaultResult")[0]
-    return salt.utils.namecheap.string_to_value(dnsresult.getAttribute("Updated"))
+    dnsresult = response_xml.getElementsByTagName('DomainDNSSetDefaultResult')[0]
+    return salt.utils.namecheap.string_to_value(dnsresult.getAttribute('Updated'))
