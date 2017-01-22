42b17cdcfd33b679985c9bbe4765b13b5bd17feb
Julien BONACHERA <julien.bonachera@smile.fr>
2017-01-22 18:56:05 +0100

    openvswitch: enclose comments_* variables into a map

--- salt/states/openvswitch_port.py
+++ salt/states/openvswitch_port.py
@@ -37,57 +37,60 @@ def present(name, bridge, type=None, id=None, remote=None, dst_port=None, intern
         port_list = __salt__['openvswitch.port_list'](bridge)
 
     # Comment and change messages
-    comment_bridge_notexists = 'Bridge {0} does not exist.'.format(bridge)
-    comment_port_exists = 'Port {0} already exists.'.format(name)
-    comment_port_created = 'Port {0} created on bridge {1}.'.format(name, bridge)
-    comment_port_notcreated = 'Unable to create port {0} on bridge {1}.'.format(name, bridge)
-    changes_port_created = {name: {'old': 'No port named {0} present.'.format(name),
+    
+    comments = {}
+
+    comments['comment_bridge_notexists'] = 'Bridge {0} does not exist.'.format(bridge)
+    comments['comment_port_exists'] = 'Port {0} already exists.'.format(name)
+    comments['comment_port_created'] = 'Port {0} created on bridge {1}.'.format(name, bridge)
+    comments['comment_port_notcreated'] = 'Unable to create port {0} on bridge {1}.'.format(name, bridge)
+    comments['changes_port_created'] = {name: {'old': 'No port named {0} present.'.format(name),
                                    'new': 'Created port {1} on bridge {0}.'.format(bridge, name),
                                    }
                             }
 
-    comment_vlan_invalid_id = 'VLANs id must be between 0 and 4095.'
-    comment_vlan_invalid_name = 'Could not find network interface {0}.'.format(name)
-    comment_vlan_port_exists = 'Port {0} with access to VLAN {1} already exists on bridge {2}.'.format(name, id, bridge)
-    comment_vlan_created = 'Created port {0} with access to VLAN {1} on bridge {2}.'.format(name, id, bridge)
-    comment_vlan_notcreated = 'Unable to create port {0} with access to VLAN {1} on ' \
+    comments['comment_vlan_invalid_id'] = 'VLANs id must be between 0 and 4095.'
+    comments['comment_vlan_invalid_name'] = 'Could not find network interface {0}.'.format(name)
+    comments['comment_vlan_port_exists'] = 'Port {0} with access to VLAN {1} already exists on bridge {2}.'.format(name, id, bridge)
+    comments['comment_vlan_created'] = 'Created port {0} with access to VLAN {1} on bridge {2}.'.format(name, id, bridge)
+    comments['comment_vlan_notcreated'] = 'Unable to create port {0} with access to VLAN {1} on ' \
                               'bridge {2}.'.format(name, id, bridge)
-    changes_vlan_created = {name: {'old': 'No port named {0} with access to VLAN {1} present on '
+    comments['changes_vlan_created'] = {name: {'old': 'No port named {0} with access to VLAN {1} present on '
                                           'bridge {2} present.'.format(name, id, bridge),
                                    'new': 'Created port {1} with access to VLAN {2} on '
                                           'bridge {0}.'.format(bridge, name, id),
                                    }
                             }
 
-    comment_gre_invalid_id = 'Id of GRE tunnel must be an unsigned 32-bit integer.'
-    comment_gre_interface_exists = 'GRE tunnel interface {0} with rempte ip {1} and key {2} ' \
+    comments['comment_gre_invalid_id'] = 'Id of GRE tunnel must be an unsigned 32-bit integer.'
+    comments['comment_gre_interface_exists'] = 'GRE tunnel interface {0} with rempte ip {1} and key {2} ' \
                                    'already exists on bridge {3}.'.format(name, remote, id, bridge)
-    comment_gre_created = 'Created GRE tunnel interface {0} with remote ip {1}  and key {2} ' \
+    comments['comment_gre_created'] = 'Created GRE tunnel interface {0} with remote ip {1}  and key {2} ' \
                           'on bridge {3}.'.format(name, remote, id, bridge)
-    comment_gre_notcreated = 'Unable to create GRE tunnel interface {0} with remote ip {1} and key {2} ' \
+    comments['comment_gre_notcreated'] = 'Unable to create GRE tunnel interface {0} with remote ip {1} and key {2} ' \
                              'on bridge {3}.'.format(name, remote, id, bridge)
-    changes_gre_created = {name: {'old': 'No GRE tunnel interface {0} with remote ip {1} and key {2} '
+    comments['changes_gre_created'] = {name: {'old': 'No GRE tunnel interface {0} with remote ip {1} and key {2} '
                                          'on bridge {3} present.'.format(name, remote, id, bridge),
                                    'new': 'Created GRE tunnel interface {0} with remote ip {1} and key {2} '
                                           'on bridge {3}.'.format(name, remote, id, bridge),
                                    }
                             }
 
-    comment_dstport = ' (dst_port' + str(dst_port) + ')' if 0 < dst_port <= 65535 else ''
-    comment_vxlan_invalid_id = 'Id of VXLAN tunnel must be an unsigned 64-bit integer.'
-    comment_vxlan_interface_exists = 'VXLAN tunnel interface {0} with rempte ip {1} and key {2} ' \
-                                   'already exists on bridge {3}{4}.'.format(name, remote, id, bridge, comment_dstport)
-    comment_vxlan_created = 'Created VXLAN tunnel interface {0} with remote ip {1}  and key {2} ' \
-                          'on bridge {3}{4}.'.format(name, remote, id, bridge, comment_dstport)
-    comment_vxlan_notcreated = 'Unable to create VXLAN tunnel interface {0} with remote ip {1} and key {2} ' \
-                             'on bridge {3}{4}.'.format(name, remote, id, bridge, comment_dstport)
-    changes_vxlan_created = {name: {'old': 'No VXLAN tunnel interface {0} with remote ip {1} and key {2} '
-                                         'on bridge {3}{4} present.'.format(name, remote, id, bridge, comment_dstport),
+    comments['comment_dstport'] = ' (dst_port' + str(dst_port) + ')' if 0 < dst_port <= 65535 else ''
+    comments['comment_vxlan_invalid_id'] = 'Id of VXLAN tunnel must be an unsigned 64-bit integer.'
+    comments['comment_vxlan_interface_exists'] = 'VXLAN tunnel interface {0} with rempte ip {1} and key {2} ' \
+                                   'already exists on bridge {3}{4}.'.format(name, remote, id, bridge, comments['comment_dstport'])
+    comments['comment_vxlan_created'] = 'Created VXLAN tunnel interface {0} with remote ip {1}  and key {2} ' \
+                          'on bridge {3}{4}.'.format(name, remote, id, bridge, comments['comment_dstport'])
+    comments['comment_vxlan_notcreated'] = 'Unable to create VXLAN tunnel interface {0} with remote ip {1} and key {2} ' \
+                             'on bridge {3}{4}.'.format(name, remote, id, bridge, comments['comment_dstport'])
+    comments['changes_vxlan_created'] = {name: {'old': 'No VXLAN tunnel interface {0} with remote ip {1} and key {2} '
+                                         'on bridge {3}{4} present.'.format(name, remote, id, bridge, comments['comment_dstport']),
                                    'new': 'Created VXLAN tunnel interface {0} with remote ip {1} and key {2} '
-                                          'on bridge {3}{4}.'.format(name, remote, id, bridge, comment_dstport),
+                                          'on bridge {3}{4}.'.format(name, remote, id, bridge, comments['comment_dstport']),
                                    }
                             }
-    comment_invalid_ip = 'Remote is not valid ip address.'
+    comments['comment_invalid_ip'] = 'Remote is not valid ip address.'
 
     # Check VLANs attributes
     def _check_vlan():
@@ -95,15 +98,15 @@ def present(name, bridge, type=None, id=None, remote=None, dst_port=None, intern
         interfaces = __salt__['network.interfaces']()
         if not 0 <= id <= 4095:
             ret['result'] = False
-            ret['comment'] = comment_vlan_invalid_id
+            ret['comment'] = comments['comment_vlan_invalid_id']
         elif not internal and name not in interfaces:
             ret['result'] = False
-            ret['comment'] = comment_vlan_invalid_name
+            ret['comment'] = comments['comment_vlan_invalid_name']
         elif tag and name in port_list:
             try:
                 if int(tag[0]) == id:
                     ret['result'] = True
-                    ret['comment'] = comment_vlan_port_exists
+                    ret['comment'] = comments['comment_vlan_port_exists']
             except (ValueError, KeyError):
                 pass
 
@@ -113,16 +116,16 @@ def present(name, bridge, type=None, id=None, remote=None, dst_port=None, intern
         interface_type = __salt__['openvswitch.interface_get_type'](name)
         if not 0 <= id <= 2**32:
             ret['result'] = False
-            ret['comment'] = comment_gre_invalid_id
+            ret['comment'] = comments['comment_gre_invalid_id']
         elif not __salt__['dig.check_ip'](remote):
             ret['result'] = False
-            ret['comment'] = comment_invalid_ip
+            ret['comment'] = comments['comment_invalid_ip']
         elif interface_options and interface_type and name in port_list:
             interface_attroptions = '{key=\"' + str(id) + '\", remote_ip=\"' + str(remote) + '\"}'
             try:
                 if interface_type[0] == 'gre' and interface_options[0] == interface_attroptions:
                     ret['result'] = True
-                    ret['comment'] = comment_gre_interface_exists
+                    ret['comment'] = comments['comment_gre_interface_exists']
             except KeyError:
                 pass
 
@@ -132,17 +135,17 @@ def present(name, bridge, type=None, id=None, remote=None, dst_port=None, intern
         interface_type = __salt__['openvswitch.interface_get_type'](name)
         if not 0 <= id <= 2**64:
             ret['result'] = False
-            ret['comment'] = comment_vxlan_invalid_id
+            ret['comment'] = comments['comment_vxlan_invalid_id']
         elif not __salt__['dig.check_ip'](remote):
             ret['result'] = False
-            ret['comment'] = comment_invalid_ip
+            ret['comment'] = comments['comment_invalid_ip']
         elif interface_options and interface_type and name in port_list:
             opt_port = 'dst_port=\"' + str(dst_port) + '\", ' if 0 < dst_port <= 65535 else ''
             interface_attroptions = '{{{0}key=\"'.format(opt_port) + str(id) + '\", remote_ip=\"' + str(remote) + '\"}'
             try:
                 if interface_type[0] == 'vxlan' and interface_options[0] == interface_attroptions:
                     ret['result'] = True
-                    ret['comment'] = comment_vxlan_interface_exists
+                    ret['comment'] = comments['comment_vxlan_interface_exists']
             except KeyError:
                 pass
 
@@ -153,27 +156,27 @@ def present(name, bridge, type=None, id=None, remote=None, dst_port=None, intern
                 _check_vlan()
                 if not ret['comment']:
                     ret['result'] = None
-                    ret['comment'] = comment_vlan_created
+                    ret['comment'] = comments['comment_vlan_created']
             elif type == 'vxlan':
                 _check_vxlan()
                 if not ret['comment']:
                     ret['result'] = None
-                    ret['comment'] = comment_vxlan_created
+                    ret['comment'] = comments['comment_vxlan_created']
             elif type == 'gre':
                 _check_gre()
                 if not ret['comment']:
                     ret['result'] = None
-                    ret['comment'] = comment_gre_created
+                    ret['comment'] = comments['comment_gre_created']
             else:
                 if name in port_list:
                     ret['result'] = True
-                    ret['comment'] = comment_port_exists
+                    ret['comment'] = comments['comment_port_exists']
                 else:
                     ret['result'] = None
-                    ret['comment'] = comment_port_created
+                    ret['comment'] = comments['comment_port_created']
         else:
             ret['result'] = None
-            ret['comment'] = comment_bridge_notexists
+            ret['comment'] = comments['comment_bridge_notexists']
 
         return ret
 
@@ -184,49 +187,49 @@ def present(name, bridge, type=None, id=None, remote=None, dst_port=None, intern
                 port_create_vlan = __salt__['openvswitch.port_create_vlan'](bridge, name, id, internal)
                 if port_create_vlan:
                     ret['result'] = True
-                    ret['comment'] = comment_vlan_created
-                    ret['changes'] = changes_vlan_created
+                    ret['comment'] = comments['comment_vlan_created']
+                    ret['changes'] = comments['changes_vlan_created']
                 else:
                     ret['result'] = False
-                    ret['comment'] = comment_vlan_notcreated
+                    ret['comment'] = comments['comment_vlan_notcreated']
         elif type == 'vxlan':
             _check_vxlan()
             if not ret['comment']:
                 port_create_vxlan = __salt__['openvswitch.port_create_vxlan'](bridge, name, id, remote, dst_port)
                 if port_create_vxlan:
                     ret['result'] = True
-                    ret['comment'] = comment_vxlan_created
-                    ret['changes'] = changes_vxlan_created
+                    ret['comment'] = comments['comment_vxlan_created']
+                    ret['changes'] = comments['changes_vxlan_created']
                 else:
                     ret['result'] = False
-                    ret['comment'] = comment_vxlan_notcreated
+                    ret['comment'] = comments['comment_vxlan_notcreated']
         elif type == 'gre':
             _check_gre()
             if not ret['comment']:
                 port_create_gre = __salt__['openvswitch.port_create_gre'](bridge, name, id, remote)
                 if port_create_gre:
                     ret['result'] = True
-                    ret['comment'] = comment_gre_created
-                    ret['changes'] = changes_gre_created
+                    ret['comment'] = comments['comment_gre_created']
+                    ret['changes'] = comments['changes_gre_created']
                 else:
                     ret['result'] = False
-                    ret['comment'] = comment_gre_notcreated
+                    ret['comment'] = comments['comment_gre_notcreated']
         else:
             if name in port_list:
                 ret['result'] = True
-                ret['comment'] = comment_port_exists
+                ret['comment'] = comments['comment_port_exists']
             else:
                 port_add = __salt__['openvswitch.port_add'](bridge, name)
                 if port_add:
                     ret['result'] = True
-                    ret['comment'] = comment_port_created
-                    ret['changes'] = changes_port_created
+                    ret['comment'] = comments['comment_port_created']
+                    ret['changes'] = comments['changes_port_created']
                 else:
                     ret['result'] = False
-                    ret['comment'] = comment_port_notcreated
+                    ret['comment'] = comments['comment_port_notcreated']
     else:
         ret['result'] = False
-        ret['comment'] = comment_bridge_notexists
+        ret['comment'] = comments['comment_bridge_notexists']
 
     return ret
 
@@ -253,11 +256,11 @@ def absent(name, bridge=None):
         port_list = [name]
 
     # Comment and change messages
-    comment_bridge_notexists = 'Bridge {0} does not exist.'.format(bridge)
-    comment_port_notexists = 'Port {0} does not exist on bridge {1}.'.format(name, bridge)
-    comment_port_deleted = 'Port {0} deleted.'.format(name)
-    comment_port_notdeleted = 'Unable to delete port {0}.'.format(name)
-    changes_port_deleted = {name: {'old': 'Port named {0} may exist.'.format(name),
+    comments['comment_bridge_notexists'] = 'Bridge {0} does not exist.'.format(bridge)
+    comments['comment_port_notexists'] = 'Port {0} does not exist on bridge {1}.'.format(name, bridge)
+    comments['comment_port_deleted'] = 'Port {0} deleted.'.format(name)
+    comments['comment_port_notdeleted'] = 'Unable to delete port {0}.'.format(name)
+    comments['changes_port_deleted'] = {name: {'old': 'Port named {0} may exist.'.format(name),
                                    'new': 'Deleted port {0}.'.format(name),
                                    }
                             }
@@ -266,21 +269,21 @@ def absent(name, bridge=None):
     if __opts__['test']:
         if bridge and not bridge_exists:
             ret['result'] = None
-            ret['comment'] = comment_bridge_notexists
+            ret['comment'] = comments['comment_bridge_notexists']
         elif name not in port_list:
             ret['result'] = True
-            ret['comment'] = comment_port_notexists
+            ret['comment'] = comments['comment_port_notexists']
         else:
             ret['result'] = None
-            ret['comment'] = comment_port_deleted
+            ret['comment'] = comments['comment_port_deleted']
         return ret
 
     if bridge and not bridge_exists:
         ret['result'] = False
-        ret['comment'] = comment_bridge_notexists
+        ret['comment'] = comments['comment_bridge_notexists']
     elif name not in port_list:
         ret['result'] = True
-        ret['comment'] = comment_port_notexists
+        ret['comment'] = comments['comment_port_notexists']
     else:
         if bridge:
             port_remove = __salt__['openvswitch.port_remove'](br=bridge, port=name)
@@ -289,10 +292,10 @@ def absent(name, bridge=None):
 
         if port_remove:
             ret['result'] = True
-            ret['comment'] = comment_port_deleted
-            ret['changes'] = changes_port_deleted
+            ret['comment'] = comments['comment_port_deleted']
+            ret['changes'] = comments['changes_port_deleted']
         else:
             ret['result'] = False
-            ret['comment'] = comment_port_notdeleted
+            ret['comment'] = comments['comment_port_notdeleted']
 
     return ret
