9a86fddfa1b0a3720faeb86fd9875808395fd1a3
C. R. Oldham <cro@ncbt.org>
2017-01-24 11:40:02 -0700

    Remove extra call to salt.loader.utils.

--- salt/engines/__init__.py
+++ salt/engines/__init__.py
@@ -111,12 +111,11 @@ class Engine(SignalHandlingMultiprocessingProcess):
         '''
         if salt.utils.is_windows():
             # Calculate function references since they can't be pickled.
-            self.utils = salt.loader.utils(self.opts)
+            self.utils = salt.loader.utils(self.opts, proxy=self.proxy)
             if self.opts['__role'] == 'master':
                 self.runners = salt.loader.runner(self.opts, utils=self.utils)
             else:
                 self.runners = []
-            self.utils = salt.loader.utils(self.opts, proxy=self.proxy)
             self.funcs = salt.loader.minion_mods(self.opts, utils=self.utils, proxy=self.proxy)
 
         self.engine = salt.loader.engines(self.opts,
