e2f4a16fdd1ba001759ff058494b31ea0bcbb979
Mike Place <mp@saltstack.com>
2017-01-24 09:22:24 -0700

    Removing trailing whitespace

--- salt/modules/launchctl.py
+++ salt/modules/launchctl.py
@@ -85,7 +85,7 @@ def _available_services():
                     }
                 except AttributeError:
                     # As of MacOS 10.12 there might be plist files without Label key
-                    # in the searched directories. As these files do not represent 
+                    # in the searched directories. As these files do not represent
                     # services, thay are not added to the list.
                     pass
 
