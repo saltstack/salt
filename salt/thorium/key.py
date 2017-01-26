68e96b11aca6f2b459e59344a914d0b21c5efe8c
Thomas S Hatch <thatch45@gmail.com>
2017-01-26 12:13:06 -0700

    This is faster and cleaner

--- salt/thorium/key.py
+++ salt/thorium/key.py
@@ -71,22 +71,10 @@ def timeout(name, delete=0, reject=0):
                     reject_set.add(id_)
     for id_ in remove:
         keyapi.delete_key(id_)
-        try:
-            __reg__['status']['val'].pop(id_)
-        except KeyError:
-            pass
-        try:
-            __context__[ktr].pop(id_)
-        except KeyError:
-            pass
+        __reg__['status']['val'].pop(id_, None)
+        __context__[ktr].pop(id_, None)
     for id_ in reject_set:
         keyapi.reject(id_)
-        try:
-            __reg__['status']['val'].pop(id_)
-        except KeyError:
-            pass
-        try:
-            __context__[ktr].pop(id_)
-        except KeyError:
-            pass
+        __reg__['status']['val'].pop(id_, None)
+        __context__[ktr].pop(id_, None)
     return ret
