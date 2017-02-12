saltstack/salt

commit b8b058fe87f79c63b643880fc6843341e6c21e29

Use six.moves to import cStringIO

salt/pillar/file_tree.py
@@ -169,7 +169,6 @@ from __future__ import absolute_import
 import fnmatch
 import logging
 import os
-import cStringIO
 
 # Import salt libs
 import salt.loader
@@ -177,6 +176,10 @@ import salt.utils
 import salt.utils.dictupdate
 import salt.utils.minions
 import salt.template
+
+# Import 3rd-party libs
+from salt.ext.six.moves import cStringIO
+
 
 # Set up logging
 log = logging.getLogger(__name__)

salt/renderers/gpg.py
@@ -214,7 +214,6 @@ import os
 import re
 import logging
 from subprocess import Popen, PIPE
-import cStringIO
 
 # Import salt libs
 import salt.utils
@@ -223,6 +222,7 @@ from salt.exceptions import SaltRenderError
 
 # Import 3rd-party libs
 import salt.ext.six as six
+from salt.ext.six.moves import cStringIO
 
 log = logging.getLogger(__name__)
 

commit 7ec9bd5cb32c40ccefd18421f30d93cd0e886618

Fix gpg renderer breakage from b8b058f
b8b058f sought to make a PY3 compatibility change, but the attribute
from Python2's cStringIO module is not available via the "fake"
cStringIO which six exposes, as six's cStringIO is just the
cStringIO.StringIO function. The other cStringIO class attributes are
not available via six.

This commit adds a new module with a few functions to get information on
StringIO objects both in Python2 and 3, and modifies the files edited in
b8b058f to use this new module to determine if the object is a readable
StringIO.

Fixes #39334.

salt/pillar/file_tree.py
@@ -175,11 +175,8 @@ import salt.loader
 import salt.utils
 import salt.utils.dictupdate
 import salt.utils.minions
+import salt.utils.stringio
 import salt.template
-
-# Import 3rd-party libs
-from salt.ext.six.moves import cStringIO
-
 
 # Set up logging
 log = logging.getLogger(__name__)
@@ -275,7 +272,7 @@ def _construct_pillar(top_dir,
                                                           default=render_default,
                                                           blacklist=renderer_blacklist,
                                                           whitelist=renderer_whitelist)
-                if isinstance(data, cStringIO.InputType):
+                if salt.utils.stringio.is_readable(data):
                     pillar_node[file_name] = data.getvalue()
                 else:
                     pillar_node[file_name] = data

salt/renderers/gpg.py
@@ -217,12 +217,12 @@ from subprocess import Popen, PIPE
 
 # Import salt libs
 import salt.utils
+import salt.utils.stringio
 import salt.syspaths
 from salt.exceptions import SaltRenderError
 
 # Import 3rd-party libs
 import salt.ext.six as six
-from salt.ext.six.moves import cStringIO
 
 log = logging.getLogger(__name__)
 
@@ -279,7 +279,7 @@ def _decrypt_object(obj, translate_newlines=False):
     (string or unicode), and it contains a valid GPG header, decrypt it,
     otherwise keep going until a string is found.
     '''
-    if isinstance(obj, cStringIO.InputType):
+    if salt.utils.stringio.is_readable(obj):
         return _decrypt_object(obj.getvalue(), translate_newlines)
     if isinstance(obj, six.string_types):
         if GPG_HEADER.search(obj):