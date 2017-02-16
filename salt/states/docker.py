e7136acda58ad49c1ff92a8c4b03c8c172312fbe
Narendra Ingale <ningale@ningale-L111.local>
2017-02-16 20:35:04 +0530

    Added some changes.

--- salt/states/docker.py
+++ salt/states/docker.py
@@ -188,12 +188,12 @@ def _compare(actual, create_kwargs, defaults_from_image):
             # Comparing list vice-versa
             for key in actual_env:
                 # Need to skip PATH and no_proxy variables.
+                actual_val = actual_env.get(key)
                 if key not in ['PATH', 'no_proxy']:
                     if key not in data:
                         env_ptr = env_diff.setdefault(item, {})
                         env_ptr.setdefault('old', {})[key] = actual_val
-                        env_ptr.setdefault('new', {})[key] = None
-
+                        
             if env_diff:
                 ret.update(env_diff)
             continue
