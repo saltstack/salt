4019fc7a7004d70f299f84d3bcbf5b746292358f
Pedro Algarvio <pedro@algarvio.me>
2017-02-19 16:38:48 +0000

    Code cleanup

--- tests/unit/modules/test_aliases.py
+++ tests/unit/modules/test_aliases.py
@@ -7,22 +7,23 @@
 from __future__ import absolute_import
 
 # Import Salt Libs
-from salt.modules import aliases
+import salt.modules.aliases as aliases
 from salt.exceptions import SaltInvocationError
 
 # Import Salt Testing Libs
 from salttesting import TestCase, skipIf
 from salttesting.mock import MagicMock, patch, NO_MOCK, NO_MOCK_REASON
-from salttesting.helpers import ensure_in_syspath
 
-ensure_in_syspath('../../')
+# Import test suite libs
+from tests.utils.mixins import LoaderModuleMockMixin
 
 
 @skipIf(NO_MOCK, NO_MOCK_REASON)
-class AliasesTestCase(TestCase):
+class AliasesTestCase(TestCase, LoaderModuleMockMixin):
     '''
     TestCase for salt.modules.aliases module
     '''
+    loader_module = aliases
 
     mock_alias = [('foo', 'bar@example.com', '')]
     mock_alias_mult = [('foo', 'bar@example.com', ''),
@@ -166,8 +167,3 @@ class AliasesTestCase(TestCase):
         '''
         ret = aliases.rm_alias('foo')
         self.assertTrue(ret)
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(AliasesTestCase, needs_daemon=False)
