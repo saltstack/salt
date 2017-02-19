73069e5b44a415df1b29d2b387c45ca292fd1e17
Pedro Algarvio <pedro@algarvio.me>
2017-02-19 15:35:30 +0000

    Proper mocking

--- tests/unit/beacons/test_adb_beacon.py
+++ tests/unit/beacons/test_adb_beacon.py
@@ -4,28 +4,26 @@
 from __future__ import absolute_import
 
 # Salt libs
-from salt.beacons import adb
+import salt.beacons.adb as adb
 
 # Salt testing libs
 from salttesting import skipIf, TestCase
-from salttesting.helpers import ensure_in_syspath
 from salttesting.mock import NO_MOCK, NO_MOCK_REASON, patch, Mock
 
-# Globals
-
-adb.__salt__ = {}
-
-ensure_in_syspath('../../')
-
+# Import test suite libs
+from tests.utils.mixins import LoaderModuleMockMixin
 
 @skipIf(NO_MOCK, NO_MOCK_REASON)
-class ADBBeaconTestCase(TestCase):
+class ADBBeaconTestCase(TestCase, LoaderModuleMockMixin):
     '''
     Test case for salt.beacons.adb
     '''
-    def setUp(self):
-        adb.last_state = {}
-        adb.last_state_extra = {'no_devices': False}
+
+    loader_module = adb
+    loader_module_globals = {
+        'last_state': {},
+        'last_state_extra': {'no_devices': False}
+    }
 
     def test_no_adb_command(self):
         with patch('salt.utils.which') as mock:
@@ -208,7 +206,7 @@ class ADBBeaconTestCase(TestCase):
     def test_with_user(self):
         config = {'states': ['device'], 'user': 'fred'}
 
-        mock = Mock(return_value='* daemon started successfully *\nList of devices attached\nHTC\tdevice',)
+        mock = Mock(return_value='* daemon started successfully *\nList of devices attached\nHTC\tdevice')
         with patch.dict(adb.__salt__, {'cmd.run': mock}):
             ret = adb.beacon(config)
             mock.assert_called_once_with('adb devices', runas='fred')
