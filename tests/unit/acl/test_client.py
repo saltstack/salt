8fbe8abe05af546a1c445e2fc74efb62ebc558d3
Pedro Algarvio <pedro@algarvio.me>
2017-02-19 15:36:49 +0000

    Code cleanup. We aren't even using mock!

--- tests/unit/acl/test_client.py
+++ tests/unit/acl/test_client.py
@@ -7,17 +7,9 @@ from __future__ import absolute_import
 from salt import acl
 
 # Import Salt Testing Libs
-from salttesting import TestCase, skipIf
-from salttesting.mock import (
-    NO_MOCK,
-    NO_MOCK_REASON,
-)
-from salttesting.helpers import ensure_in_syspath
-
-ensure_in_syspath('../../')
+from salttesting import TestCase
 
 
-@skipIf(NO_MOCK, NO_MOCK_REASON)
 class ClientACLTestCase(TestCase):
     '''
     Unit tests for salt.acl.ClientACL
@@ -68,8 +60,3 @@ class ClientACLTestCase(TestCase):
 
         self.assertTrue(client_acl.cmd_is_blacklisted(['cmd.run', 'state.sls']))
         self.assertFalse(client_acl.cmd_is_blacklisted(['state.highstate', 'state.sls']))
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(ClientACLTestCase, needs_daemon=False)
