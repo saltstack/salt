32bb413611bba0a261485a96fec563599943d821
Pedro Algarvio <pedro@algarvio.me>
2017-02-20 13:56:30 +0000

    Code cleanup. Proper mocking

--- tests/unit/modules/test_bridge.py
+++ tests/unit/modules/test_bridge.py
@@ -18,15 +18,14 @@ from salttesting.mock import (
 # Import Salt Libs
 from salt.modules import bridge
 
-# Globals
-bridge.__grains__ = {}
-
 
 @skipIf(NO_MOCK, NO_MOCK_REASON)
 class BridgeTestCase(TestCase):
     '''
     Test cases for salt.modules.bridge
     '''
+    loader_module = bridge
+
     def test_show(self):
         '''
         Test for Returns bridges interfaces
@@ -118,8 +117,3 @@ class BridgeTestCase(TestCase):
 
         with patch.dict(bridge.__grains__, {'kernel': None}):
             self.assertFalse(bridge.stp())
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(BridgeTestCase, needs_daemon=False)
