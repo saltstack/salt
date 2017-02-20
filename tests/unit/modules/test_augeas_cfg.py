b01389f0fe7f915a0967c85767756fe6c6e714a1
Pedro Algarvio <pedro@algarvio.me>
2017-02-20 13:52:12 +0000

    Code cleanup

--- tests/unit/modules/test_augeas_cfg.py
+++ tests/unit/modules/test_augeas_cfg.py
@@ -16,12 +16,11 @@ from salttesting.mock import (
 from salt.modules import augeas_cfg
 from salt.exceptions import SaltInvocationError
 # Make sure augeas python interface is installed
-HAS_AUGEAS = augeas_cfg.__virtual__()
-if HAS_AUGEAS == 'augeas':
+if augeas_cfg.HAS_AUGEAS:
     from augeas import Augeas as _Augeas
 
 
-@skipIf(HAS_AUGEAS != 'augeas', "augeas python is required for this test case")
+@skipIf(augeas_cfg.HAS_AUGEAS is False, "python-augeas is required for this test case")
 class AugeasCfgTestCase(TestCase):
     '''
     Test cases for salt.modules.augeas_cfg
