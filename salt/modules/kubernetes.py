1834f8695ed7250cd2464fcaa3f12cb2aa982408
Flavio Castelli <fcastelli@suse.com>
2017-02-20 14:28:54 +0100

    Introducing the kubernetes modules
    Allow basic interaction with kubernetes, right now only deployments
    and services resources are managed.
    
    Signed-off-by: Flavio Castelli <fcastelli@suse.com>

---
+++ salt/modules/kubernetes.py
@@ -0,0 +1,500 @@
+'''
+Module for handling kubernetes calls.
+
+:optdepends:    - kubernetes Python client
+:configuration: This module is not usable until the following are specified
+    either in a pillar or in the minion's config file::
+
+        kubernetes.user: admin
+        kubernetes.password: verybadpass
+        kubernetes.api_url: 'http://127.0.0.1:8080'
+'''
+# Import Python Futures
+from __future__ import absolute_import
+from six import iteritems
+from salt.exceptions import CommandExecutionError
+import salt.utils
+import salt.utils.templates
+import yaml
+
+try:
+    import kubernetes
+    import kubernetes.client
+    from kubernetes.client.rest import ApiException
+
+    HAS_LIBS = True
+except ImportError:
+    HAS_LIBS = False
+
+
+import logging
+log = logging.getLogger(__name__)
+
+__virtualname__ = 'kubernetes'
+
+
+def __virtual__():
+    '''
+    Check dependencies
+    '''
+    if HAS_LIBS:
+        return __virtualname__
+
+    return False
+
+
+def ping():
+    '''
+    Checks connections with the kubernetes API server.
+    Returns True if the connection can be established, False otherwise.
+
+    CLI Example:
+        salt '*' kubernetes.ping
+    '''
+    status = True
+    try:
+        nodes()
+    except:
+        status = False
+
+    return status
+
+
+def nodes():
+    '''
+    Return the names of the nodes composing the kubernetes cluster
+
+    CLI Examples::
+
+        salt '*' kubernetes.nodes
+    '''
+    _setup_conn()
+
+    api_instance = kubernetes.client.CoreV1Api()
+    api_response = api_instance.list_node()
+
+    ret = []
+
+    for node in api_response.items:
+        ret.append(node.metadata.name)
+
+    return ret
+
+
+def _setup_conn():
+    '''
+    Setup kubernetes API connection singleton
+    '''
+    host = __salt__['config.option']('kubernetes.api_url',
+                                     'http://localhost:8080')
+    username = __salt__['config.option']('kubernetes.user')
+    password = __salt__['config.option']('kubernetes.password')
+    kubernetes.client.configuration.host = host
+    kubernetes.client.configuration.user = username
+    kubernetes.client.configuration.passwd = password
+
+
+def show_deployment(name, namespace):
+    '''
+    Return the kubernetes deployment defined by name and namespace
+
+    CLI Examples::
+
+        salt '*' kubernetes.show_deployment my-nginx,default
+        salt '*' kubernetes.show_deployment name=my-nginx,namespace=default
+    '''
+    _setup_conn()
+    try:
+        api_instance = kubernetes.client.ExtensionsV1beta1Api()
+        api_response = api_instance.read_namespaced_deployment(name, namespace)
+
+        return api_response.to_dict()
+    except ApiException as exc:
+        if exc.status == 404:
+            return None
+        else:
+            print(
+                "Exception when calling "
+                "ExtensionsV1beta1Api->read_namespaced_deployment: %s\n" % exc)
+            raise exc
+
+
+def show_service(name, namespace):
+    '''
+    Return the kubernetes service defined by name and namespace
+
+    CLI Examples::
+
+        salt '*' kubernetes.show_service my-nginx,default
+        salt '*' kubernetes.show_service name=my-nginx,namespace=default
+    '''
+    _setup_conn()
+    try:
+        api_instance = kubernetes.client.CoreV1Api()
+        api_response = api_instance.read_namespaced_service(name, namespace)
+
+        return api_response.to_dict()
+    except ApiException as exc:
+        if exc.status == 404:
+            return None
+        else:
+            print(
+                "Exception when calling CoreV1Api->read_namespaced_service: "
+                "%s\n" % exc)
+            raise exc
+
+
+def delete_deployment(name, namespace):
+    '''
+    Deletes the kubernetes deployment defined by name and namespace
+
+    CLI Examples::
+
+        salt '*' kubernetes.delete_deployment my-nginx,default
+        salt '*' kubernetes.delete_deployment name=my-nginx,namespace=default
+    '''
+    _setup_conn()
+    body = kubernetes.client.V1DeleteOptions(orphan_dependents=True)
+
+    try:
+        api_instance = kubernetes.client.ExtensionsV1beta1Api()
+        api_response = api_instance.delete_namespaced_deployment(
+            name=name,
+            namespace=namespace,
+            body=body)
+
+        return api_response.to_dict()
+    except ApiException as exc:
+        if exc.status == 404:
+            return None
+        else:
+            print(
+                "Exception when calling "
+                "ExtensionsV1beta1Api->delete_namespaced_deployment: "
+                "%s\n" % exc)
+            raise exc
+
+
+def delete_service(name, namespace):
+    '''
+    Deletes the kubernetes service defined by name and namespace
+
+    CLI Examples::
+
+        salt '*' kubernetes.delete_service my-nginx,default
+        salt '*' kubernetes.delete_service name=my-nginx,namespace=default
+    '''
+    _setup_conn()
+
+    try:
+        api_instance = kubernetes.client.CoreV1Api()
+        api_response = api_instance.delete_namespaced_service(
+            name=name,
+            namespace=namespace)
+
+        return api_response.to_dict()
+    except ApiException as exc:
+        if exc.status == 404:
+            return None
+        else:
+            print(
+                "Exception when calling CoreV1Api->delete_namespaced_service: "
+                "%s\n" % exc)
+            raise exc
+
+
+def create_deployment(
+        name,
+        namespace,
+        metadata,
+        spec,
+        source,
+        template,
+        saltenv):
+    '''
+    Creates the kubernetes deployment as defined by the user.
+    '''
+    body = __create_object_body(
+        kind='Deployment',
+        obj_class=kubernetes.client.V1beta1Deployment,
+        spec_creator=__dict_to_deployment_spec,
+        name=name,
+        namespace=namespace,
+        metadata=metadata,
+        spec=spec,
+        source=source,
+        template=template,
+        saltenv=saltenv)
+
+    _setup_conn()
+
+    try:
+        api_instance = kubernetes.client.ExtensionsV1beta1Api()
+        api_response = api_instance.create_namespaced_deployment(
+            namespace, body)
+
+        return api_response.to_dict()
+    except ApiException as exc:
+        if exc.status == 404:
+            return None
+        else:
+            print(
+                "Exception when calling "
+                "ExtensionsV1beta1Api->create_namespaced_deployment: "
+                "%s\n" % exc)
+            raise exc
+
+
+def create_service(
+        name,
+        namespace,
+        metadata,
+        spec,
+        source,
+        template,
+        saltenv):
+    '''
+    Creates the kubernetes service as defined by the user.
+    '''
+    body = __create_object_body(
+        kind='Service',
+        obj_class=kubernetes.client.V1Service,
+        spec_creator=__dict_to_service_spec,
+        name=name,
+        namespace=namespace,
+        metadata=metadata,
+        spec=spec,
+        source=source,
+        template=template,
+        saltenv=saltenv)
+
+    _setup_conn()
+
+    log.warning(body)
+
+    try:
+        api_instance = kubernetes.client.CoreV1Api()
+        api_response = api_instance.create_namespaced_service(
+            namespace, body)
+
+        return api_response.to_dict()
+    except ApiException as exc:
+        if exc.status == 404:
+            return None
+        else:
+            print(
+                "Exception when calling CoreV1Api->create_namespaced_service: "
+                "%s\n" % exc)
+            raise exc
+
+
+def replace_deployment(name,
+                       namespace,
+                       metadata,
+                       spec,
+                       source,
+                       template,
+                       saltenv):
+    '''
+    Replaces an existing deployment with a new one defined by name and
+    namespace, having the specificed metadata and spec.
+    '''
+    body = __create_object_body(
+        kind='Deployment',
+        obj_class=kubernetes.client.V1beta1Deployment,
+        spec_creator=__dict_to_deployment_spec,
+        name=name,
+        namespace=namespace,
+        metadata=metadata,
+        spec=spec,
+        source=source,
+        template=template,
+        saltenv=saltenv)
+
+    _setup_conn()
+
+    try:
+        api_instance = kubernetes.client.ExtensionsV1beta1Api()
+        api_response = api_instance.replace_namespaced_deployment(
+            name, namespace, body)
+
+        return api_response.to_dict()
+    except ApiException as exc:
+        if exc.status == 404:
+            return None
+        else:
+            print(
+                "Exception when calling "
+                "ExtensionsV1beta1Api->replace_namespaced_deployment: "
+                "%s\n" % exc)
+            raise exc
+
+
+def replace_service(name,
+                    namespace,
+                    metadata,
+                    spec,
+                    source,
+                    template,
+                    old_service,
+                    saltenv):
+    '''
+    Replaces an existing service with a new one defined by name and namespace,
+    having the specificed metadata and spec.
+    '''
+    body = __create_object_body(
+        kind='Service',
+        obj_class=kubernetes.client.V1Service,
+        spec_creator=__dict_to_service_spec,
+        name=name,
+        namespace=namespace,
+        metadata=metadata,
+        spec=spec,
+        source=source,
+        template=template,
+        saltenv=saltenv)
+
+    # Some attributes have to be preserved
+    # otherwise exceptions will be thrown
+    body.spec.cluster_ip = old_service["spec"]["cluster_ip"]
+    body.metadata.resource_version = old_service["metadata"]["resource_version"]
+
+    _setup_conn()
+
+    try:
+        api_instance = kubernetes.client.CoreV1Api()
+        api_response = api_instance.replace_namespaced_service(
+            name, namespace, body)
+
+        return api_response.to_dict()
+    except ApiException as exc:
+        if exc.status == 404:
+            return None
+        else:
+            print(
+                "Exception when calling "
+                "CoreV1Api->replace_namespaced_service: %s\n" % exc)
+            raise exc
+
+
+def __create_object_body(kind,
+                         obj_class,
+                         spec_creator,
+                         name,
+                         namespace,
+                         metadata,
+                         spec,
+                         source,
+                         template,
+                         saltenv):
+    '''
+    Create a Kubernetes Object body instance.
+    '''
+    if source:
+        sfn = __salt__['cp.cache_file'](source, saltenv)
+        if not sfn:
+            raise CommandExecutionError(
+                "Source file \'{0}\' not found".format(source))
+
+        with salt.utils.fopen(sfn, 'r') as src:
+            contents = src.read()
+
+            if template:
+                if template in salt.utils.templates.TEMPLATE_REGISTRY:
+                    # TODO: should we allow user to set also `context` like
+                    # `file.managed` does?
+                    # Apply templating
+                    data = salt.utils.templates.TEMPLATE_REGISTRY[template](
+                        contents,
+                        from_str=True,
+                        to_str=True,
+                        saltenv=saltenv,
+                        grains=__grains__,
+                        pillar=__pillar__,
+                        salt=__salt__,
+                        opts=__opts__)
+
+                    if not data['result']:
+                        # Failed to render the template
+                        raise CommandExecutionError(
+                            "Failed to render file path with error: "
+                            "%s" % data['data'])
+
+                    contents = data['data'].encode('utf-8')
+                else:
+                    raise CommandExecutionError(
+                        'Unknown template specified: {0}'.format(
+                            template))
+
+            src_obj = yaml.load(contents)
+            if (
+                    not isinstance(src_obj, dict) or
+                    'kind' not in src_obj or
+                    src_obj['kind'] != kind
+               ):
+                    raise CommandExecutionError(
+                        "The source file should define only "
+                        "a {0} object".format(kind))
+
+            if 'metadata' in src_obj:
+                metadata = src_obj['metadata']
+            if 'spec' in src_obj:
+                spec = src_obj['spec']
+
+    return obj_class(
+        metadata=__dict_to_object_meta(name, namespace, metadata),
+        spec=spec_creator(spec))
+
+
+def __dict_to_object_meta(name, namespace, metadata):
+    """
+    Converts a dictionary into kubernetes ObjectMetaV1 instance.
+    """
+    meta_obj = kubernetes.client.V1ObjectMeta()
+    meta_obj.namespace = namespace
+    for key, value in iteritems(metadata):
+        if hasattr(meta_obj, key):
+            setattr(meta_obj, key, value)
+
+    if meta_obj.name != name:
+        log.warning(
+            "The object already has a name attribute, overwriting it with "
+            "the one defined inside of salt")
+        meta_obj.name = name
+
+    return meta_obj
+
+
+def __dict_to_deployment_spec(spec):
+    """
+    Converts a dictionary into kubernetes V1beta1DeploymentSpec instance.
+    """
+    spec_obj = kubernetes.client.V1beta1DeploymentSpec()
+    for key, value in iteritems(spec):
+        if hasattr(spec_obj, key):
+            setattr(spec_obj, key, value)
+
+    return spec_obj
+
+
+def __dict_to_service_spec(spec):
+    """
+    Converts a dictionary into kubernetes V1ServiceSpec instance.
+    """
+    spec_obj = kubernetes.client.V1ServiceSpec()
+    for key, value in iteritems(spec):
+        if key == "ports":
+            spec_obj.ports = []
+            for port in value:
+                kube_port = kubernetes.client.V1ServicePort()
+                if isinstance(port, dict):
+                    for port_key, port_value in iteritems(port):
+                        if hasattr(kube_port, port_key):
+                            setattr(kube_port, port_key, port_value)
+                else:
+                    kube_port.port = port
+                spec_obj.ports.append(kube_port)
+        elif hasattr(spec_obj, key):
+            setattr(spec_obj, key, value)
+
+    return spec_obj
---
+++ salt/states/kubernetes.py
@@ -0,0 +1,348 @@
+'''
+Manage kubernetes resources as salt states
+==========================================
+
+NOTE: This module requires the proper pillar values set. See
+salt.modules.kubernetes for more information.
+
+The kubernetes module is used to manage different kubernetes resources.
+
+
+.. code-block:: yaml
+
+    my-nginx:
+      kubernetes.deployment_present:
+        - namespace: default
+          metadata:
+            app: frontend
+          spec:
+            replicas: 1
+            template:
+              metadata:
+                labels:
+                  run: my-nginx
+              spec:
+                containers:
+                - name: my-nginx
+                  image: nginx
+                  ports:
+                  - containerPort: 80
+
+    my-mariadb:
+      kubernetes.deployment_absent:
+        - namespace: default
+
+    # kubernetes deployment as specified inside of
+    # a file containing the definition of the the
+    # deployment using the official kubernetes format
+    redis-master-deployment:
+      kubernetes.deployment_present:
+        - name: redis-master
+        - source: salt://k8s/redis-master-deployment.yml
+      require:
+        - pip: kubernetes-python-module
+
+    # kubernetes service as specified inside of
+    # a file containing the definition of the the
+    # service using the official kubernetes format
+    redis-master-service:
+      kubernetes.service_present:
+        - name: redis-master
+        - source: salt://k8s/redis-master-service.yml
+      require:
+        - kubernetes.deployment_present: redis-master
+
+    # kubernetes deployment as specified inside of
+    # a file containing the definition of the the
+    # deployment using the official kubernetes format
+    # plus some jinja directives
+     nginx-source-template:
+      kubernetes.deployment_present:
+        - source: salt://k8s/nginx.yml.jinja
+        - template: jinja
+      require:
+        - pip: kubernetes-python-module
+
+'''
+from __future__ import absolute_import
+
+import logging
+log = logging.getLogger(__name__)
+
+
+def __virtual__():
+    '''
+    Only load if the kubernetes module is available in __salt__
+    '''
+    return 'kubernetes.ping' in __salt__
+
+
+def _error(ret, err_msg):
+    '''
+    Helper function to propagate errors to
+    the end user.
+    '''
+    ret['result'] = False
+    ret['comment'] = err_msg
+    return ret
+
+
+def deployment_absent(name, namespace="default"):
+    '''
+    Ensures that the named deployment is absent from the given namespace.
+
+    name
+        The name of the deployment
+
+    namespace
+        The name of the namespace
+    '''
+
+    ret = {'name': name,
+           'changes': {},
+           'result': False,
+           'comment': ''}
+
+    deployment = __salt__['kubernetes.show_deployment'](name, namespace)
+
+    if deployment is None:
+        ret['result'] = True
+        ret['comment'] = 'The deployment does not exist'
+        return ret
+
+    if __opts__['test']:
+        ret['comment'] = 'The deployment is not absent'
+        ret['result'] = None
+        return ret
+
+    res = __salt__['kubernetes.delete_deployment'](name, namespace)
+    if res['code'] == 200:
+        ret['result'] = True
+        ret['changes'] = {
+            'kubernetes.deployment': {
+                'new': 'absent', 'old': 'present'}}
+    ret['comment'] = res['message']
+
+    return ret
+
+
+def deployment_present(
+        name,
+        namespace="default",
+        metadata=None,
+        spec=None,
+        source="",
+        template=""):
+    '''
+    Ensures that the named deployment is present inside of the specified
+    namespace with the given metadata and spec.
+    If the deployment exists it will be replaced.
+
+    name
+        The name of the deployment.
+
+    namespace
+        The namespace holding the deployment. The "default" one is going to be
+        used unless a different one is specified.
+
+    metadata
+        The metadata of the deployment object.
+
+    spec
+        The spec of the deployment object.
+
+    source
+        A file containing the definition of the deployment (metadata and
+        spec) in the official kubernetes format.
+
+    template
+        Template engine to be used to render the source file.
+    '''
+    ret = {'name': name,
+           'changes': {},
+           'result': False,
+           'comment': ''}
+
+    if (metadata or spec) and source:
+        return _error(
+            ret,
+            '\'source\' cannot be used in combination with \'metadata\' or '
+            '\'spec\''
+        )
+
+    if metadata is None:
+        metadata = {}
+
+    if spec is None:
+        spec = {}
+
+    deployment = __salt__['kubernetes.show_deployment'](name, namespace)
+
+    if deployment is None:
+        if __opts__['test']:
+            ret['result'] = None
+            ret['comment'] = 'The deployment does not exist'
+            return ret
+        res = __salt__['kubernetes.create_deployment'](name=name,
+                                                       namespace=namespace,
+                                                       metadata=metadata,
+                                                       spec=spec,
+                                                       source=source,
+                                                       template=template,
+                                                       saltenv=__env__)
+        ret['changes']["{}.{}".format(namespace, name)] = {
+            'old': {},
+            'new': res}
+    else:
+        ret['changes'] = {
+            'metadata': metadata,
+            'spec': spec}
+        if __opts__['test']:
+            ret['comment'] = 'The deployment is already present'
+            ret['result'] = None
+            return ret
+        # TODO: improve checks
+        log.info("Forcing the recreation of the deploymentv")
+        res = __salt__['kubernetes.replace_deployment'](
+            name=name,
+            namespace=namespace,
+            metadata=metadata,
+            spec=spec,
+            source=source,
+            template=template,
+            saltenv=__env__)
+
+    ret['result'] = True
+
+    return ret
+
+
+def service_present(
+        name,
+        namespace="default",
+        metadata=None,
+        spec=None,
+        source="",
+        template=""):
+    '''
+    Ensures that the named service is present inside of the specified namespace
+    with the given metadata and spec.
+    If the deployment exists it will be replaced.
+
+    name
+        The name of the service.
+
+    namespace
+        The namespace holding the service. The "default" one is going to be
+        used unless a different one is specified.
+
+    metadata
+        The metadata of the service object.
+
+    spec
+        The spec of the service object.
+
+    source
+        A file containing the definition of the service (metadata and
+        spec) in the official kubernetes format.
+
+    template
+        Template engine to be used to render the source file.
+    '''
+    ret = {'name': name,
+           'changes': {},
+           'result': False,
+           'comment': ''}
+
+    if (metadata or spec) and source:
+        return _error(
+            ret,
+            '\'source\' cannot be used in combination with \'metadata\' or '
+            '\'spec\''
+        )
+
+    if metadata is None:
+        metadata = {}
+
+    if spec is None:
+        spec = {}
+
+    service = __salt__['kubernetes.show_service'](name, namespace)
+
+    if service is None:
+        if __opts__['test']:
+            ret['result'] = None
+            ret['comment'] = 'The service does not exist'
+            return ret
+        res = __salt__['kubernetes.create_service'](name=name,
+                                                    namespace=namespace,
+                                                    metadata=metadata,
+                                                    spec=spec,
+                                                    source=source,
+                                                    template=template,
+                                                    saltenv=__env__)
+        ret['changes']["{}.{}".format(namespace, name)] = {
+            'old': {},
+            'new': res}
+    else:
+        ret['changes'] = {
+            'metadata': metadata,
+            'spec': spec}
+        if __opts__['test']:
+            ret['comment'] = 'The service is already present'
+            ret['result'] = None
+            return ret
+        # TODO: improve checks
+        log.info("Forcing the recreation of the service")
+        res = __salt__['kubernetes.replace_service'](
+            name=name,
+            namespace=namespace,
+            metadata=metadata,
+            spec=spec,
+            source=source,
+            template=template,
+            old_service=service,
+            saltenv=__env__)
+
+    ret['result'] = True
+
+    return ret
+
+
+def service_absent(name, namespace="default"):
+    '''
+    Ensures that the named service is absent from the given namespace.
+
+    name
+        The name of the service
+
+    namespace
+        The name of the namespace
+    '''
+
+    ret = {'name': name,
+           'changes': {},
+           'result': False,
+           'comment': ''}
+
+    service = __salt__['kubernetes.show_service'](name, namespace)
+
+    if service is None:
+        ret['result'] = True
+        ret['comment'] = 'The service does not exist'
+        return ret
+
+    if __opts__['test']:
+        ret['comment'] = 'The service is not absent'
+        ret['result'] = None
+        return ret
+
+    res = __salt__['kubernetes.delete_service'](name, namespace)
+    if res['code'] == 200:
+        ret['result'] = True
+        ret['changes'] = {
+            'kubernetes.service': {
+                'new': 'absent', 'old': 'present'}}
+    ret['comment'] = res['message']
+
+    return ret
