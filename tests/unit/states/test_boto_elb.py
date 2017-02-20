2718a6730afab9a629d6e52595c8a4063eb463cd
Pedro Algarvio <pedro@algarvio.me>
2017-02-20 12:23:59 +0000

    Code cleanup. Proper mocking

--- tests/unit/states/test_boto_elb.py
+++ tests/unit/states/test_boto_elb.py
@@ -14,23 +14,19 @@ from salttesting.mock import (
     MagicMock,
     patch)
 
-from salttesting.helpers import ensure_in_syspath
-
-ensure_in_syspath('../../')
-
 # Import Salt Libs
 from salt.states import boto_elb
 
-boto_elb.__salt__ = {}
-boto_elb.__opts__ = {}
-boto_elb.__states__ = {}
+# Import test suite libs
+from tests.utils.mixins import LoaderModuleMockMixin
 
 
 @skipIf(NO_MOCK, NO_MOCK_REASON)
-class BotoElbTestCase(TestCase):
+class BotoElbTestCase(TestCase, LoaderModuleMockMixin):
     '''
     Test cases for salt.states.boto_elb
     '''
+    loader_module = boto_elb
     # 'present' function tests: 1
 
     def test_present(self):
@@ -180,8 +176,3 @@ class BotoElbTestCase(TestCase):
                 comt = ('ELB {0} is set to be removed.'.format(name))
                 ret.update({'comment': comt, 'result': None})
                 self.assertDictEqual(boto_elb.absent(name), ret)
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(BotoElbTestCase, needs_daemon=False)
