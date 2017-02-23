17deb885bd0b8329dc6b497ba8506061bdafe9db
Pedro Algarvio <pedro@algarvio.me>
2017-02-23 14:54:01 +0000

    Code cleanup. Proper mocking

--- tests/unit/states/test_boto_asg.py
+++ tests/unit/states/test_boto_asg.py
@@ -13,19 +13,18 @@ from salttesting.mock import (
     MagicMock,
     patch)
 
-from salttesting.helpers import ensure_in_syspath
-
-ensure_in_syspath('../../')
-
 # Import Salt Libs
 from salt.states import boto_asg
+
+# Import test suite libs
+from tests.utils.mixins import LoaderModuleMockMixin
 
 boto_asg.__salt__ = {}
 boto_asg.__opts__ = {}
 
 
 @skipIf(NO_MOCK, NO_MOCK_REASON)
-class BotoAsgTestCase(TestCase):
+class BotoAsgTestCase(TestCase, LoaderModuleMockMixin):
     '''
     Test cases for salt.states.boto_asg
     '''
@@ -117,8 +116,3 @@ class BotoAsgTestCase(TestCase):
                 comt = ('Autoscale group does not exist.')
                 ret.update({'comment': comt, 'result': True})
                 self.assertDictEqual(boto_asg.absent(name), ret)
-
-
-if __name__ == '__main__':
-    from integration import run_tests
-    run_tests(BotoAsgTestCase, needs_daemon=False)
