abe3d5797cbc011c5bdbc868317a68f0c56f6762
Drew Malone <drew.malone@gmail.com>
2017-02-23 18:40:52 -0500

    Fix indention lint errors

--- salt/modules/boto_vpc.py
+++ salt/modules/boto_vpc.py
@@ -1315,10 +1315,10 @@ def _maybe_set_tags(tags, obj):
     if tags:
         # Not all objects in Boto have an 'add_tags()' method.
         try:
-          obj.add_tags(tags)
+            obj.add_tags(tags)
 
         except AttributeError:
-          for tag, value in tags.items():
-            obj.add_tag(tag, value)
+            for tag, value in tags.items():
+                obj.add_tag(tag, value)
 
         log.debug('The following tags: {0} were added to {1}'.format(', '.join(tags), obj))
