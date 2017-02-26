5a3c099e4f9c57d90a6597ab14841779cb86ac21
Erik Johnson <palehose@gmail.com>
2017-02-26 12:21:00 -0600

    Rewrite the tests_valid_docs test
    This uses a function in the runtests_helpers custom module to perform
    all the logic, and only returns what failed the test. This saves us from
    having to return the entire contents of sys.doc (as well as log all of
    the function calls), and also removes the need to run sys.doc in batches
    to get around the "max message size" issue.

--- tests/integration/modules/sysmod.py
+++ tests/integration/modules/sysmod.py
@@ -2,10 +2,6 @@
 
 # Import python libs
 from __future__ import absolute_import
-import logging
-import re
-
-log = logging.getLogger(__name__)
 
 # Import Salt Testing libs
 from salttesting.helpers import ensure_in_syspath
@@ -13,9 +9,6 @@ ensure_in_syspath('../../')
 
 # Import salt libs
 import integration
-
-# Import 3rd-party libs
-import salt.ext.six as six
 
 
 class SysModuleTest(integration.ModuleCase):
@@ -26,79 +19,15 @@ class SysModuleTest(integration.ModuleCase):
         '''
         Make sure no functions are exposed that don't have valid docstrings
         '''
-        mods = self.run_function('sys.list_modules')
-        nodoc = set()
-        noexample = set()
-        allow_failure = (
-                'cp.recv',
-                'libcloud_dns.get_driver',
-                'lxc.run_cmd',
-                'ipset.long_range',
-                'pkg.expand_repo_def',
-                'runtests_decorators.depends',
-                'runtests_decorators.depends_will_fallback',
-                'runtests_decorators.missing_depends',
-                'runtests_decorators.missing_depends_will_fallback',
-                'swift.head',
-                'glance.warn_until',
-                'yumpkg.expand_repo_def',
-                'yumpkg5.expand_repo_def',
-                'container_resource.run',
-                'nspawn.stop',
-                'nspawn.restart',
-                'lowpkg.bin_pkg_info',
-                'state.apply',
-                'pip.iteritems',
-                'cmd.win_runas',
-                'status.list2cmdline'
-        )
-
-        batches = 2
-        mod_count = len(mods)
-        batch_size = mod_count / float(batches)
-        if batch_size.is_integer():
-            batch_size = int(batch_size)
-        else:
-            # Check if the module count is evenly divisible by the number of
-            # batches. If not, increase the batch_size by the number of batches
-            # being run. This ensures that we get the correct number of
-            # batches, and that we don't end up running sys.doc an extra time
-            # to cover the remainder. For example, if we had a batch count of 2
-            # and 121 modules, if we just divided by 2 we'd end up running
-            # sys.doc 3 times.
-            batch_size = int(batch_size) + batches
-
-        log.debug('test_valid_docs batch size = %s', batch_size)
-        start = 0
-        end = batch_size
-        while start <= mod_count:
-            log.debug('running sys.doc on mods[%s:%s]', start, end)
-            docs = self.run_function('sys.doc', mods[start:end])
-            if docs == 'VALUE TRIMMED':
-                self.fail(
-                    'sys.doc output trimmed. It may be necessary to increase '
-                    'the number of batches'
-                )
-            for fun in docs:
-                if fun.startswith('runtests_helpers'):
-                    continue
-                if fun in allow_failure:
-                    continue
-                if not isinstance(docs[fun], six.string_types):
-                    nodoc.add(fun)
-                elif not re.search(r'([E|e]xample(?:s)?)+(?:.*)::?', docs[fun]):
-                    noexample.add(fun)
-            start += batch_size
-            end += batch_size
-
-        if not nodoc and not noexample:
+        ret = self.run_function('runtests_helpers.get_invalid_docs')
+        if ret == {'missing_docstring': [], 'missing_cli_example': []}:
             return
 
         raise AssertionError(
             'There are some functions which do not have a docstring or do not '
             'have an example:\nNo docstring:\n{0}\nNo example:\n{1}\n'.format(
-                '\n'.join(['  - {0}'.format(f) for f in sorted(nodoc)]),
-                '\n'.join(['  - {0}'.format(f) for f in sorted(noexample)]),
+                '\n'.join(['  - {0}'.format(f) for f in ret['missing_docstring']]),
+                '\n'.join(['  - {0}'.format(f) for f in ret['missing_cli_example']]),
             )
         )
 
