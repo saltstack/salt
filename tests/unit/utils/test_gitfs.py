99e06b45edd0020c67462bfb70aaee7e0044daa2
Pedro Algarvio <pedro@algarvio.me>
2017-02-27 13:58:07 +0000

    Move whatever we need from salttesting to salt.
    Let's drop the salttesting dependency cycle.

--- tests/unit/utils/test_gitfs.py
+++ tests/unit/utils/test_gitfs.py
@@ -8,9 +8,9 @@ any remotes.
 from __future__ import absolute_import
 
 # Import Salt Testing libs
-from salttesting import skipIf, TestCase
-from salttesting.mock import MagicMock, patch, NO_MOCK, NO_MOCK_REASON
-from salttesting.helpers import ensure_in_syspath
+from tests.support.unit import skipIf, TestCase
+from tests.support.mock import MagicMock, patch, NO_MOCK, NO_MOCK_REASON
+from tests.support.helpers import ensure_in_syspath
 ensure_in_syspath('../../')
 
 # Import salt libs
