d6729035dc3ee2c64a782f32dc3b1898042c6887
twangboy <slee@saltstack.com>
2017-03-01 13:53:32 -0700

    Clarify the reason for the while loop

--- tests/integration/loader/test_ext_modules.py
+++ tests/integration/loader/test_ext_modules.py
@@ -23,8 +23,8 @@ class LoaderOverridesTest(integration.ModuleCase):
     def test_overridden_internal(self):
         # To avoid a race condition on Windows, we need to make sure the
         # `override_test.py` file is present in the _modules directory before
-        # trying to list all functions. For some reason, the files aren't copied
-        # over immediately.
+        # trying to list all functions. This test may execute before the
+        # minion has finished syncing down the files it needs.
         module = os.path.join(integration.TMP, 'rootdir', 'cache', 'files',
                               'base', '_modules', 'override_test.py')
         tries = 0
