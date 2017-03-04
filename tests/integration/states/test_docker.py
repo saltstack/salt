0da39f0742eaba920a20520fc557bac295e03437
Narendra Ingale <ningale@ningale-L111.local>
2017-03-04 20:18:41 +0530

    Removed black listes package use.

--- tests/integration/states/test_docker.py
+++ tests/integration/states/test_docker.py
@@ -9,7 +9,7 @@ import textwrap
 import os
 import json
 # Import Salt Testing libs
-from salttesting.helpers import ensure_in_syspath
+#from salttesting.helpers import ensure_in_syspath
 import tests.integration as integration
 from tests.support.unit import skipIf
 # Import salt libs
@@ -24,7 +24,7 @@ except ImportError:
 if not salt.utils.which('mysqladmin'):
     NO_DOCKER = True
 
-ensure_in_syspath('../../')
+#ensure_in_syspath('../../')
 
 STATE_DIR = os.path.join(integration.FILES, 'file', 'base')
 
