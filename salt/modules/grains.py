a7e419e35f67699af59ff3c510fdaf859eaf490c
Erik Johnson <palehose@gmail.com>
2017-03-09 12:18:51 -0600

    Scrap event-based approach for refreshing grains
    The call to refresh_modules() in saltutil.refresh_grains was resulting
    in a race condition with the event I added to refresh the grains. This
    meant that, even though self.opts['grains'] was being changed by the new
    event, before that process could finish the module refresh was itself
    re-loading the grains and re-packing these new grains into the
    __grains__ dunder, negating the refresh.
    
    Since the module refresh loads the grains anyway, this commit changes
    saltutil.refresh_grains to refresh the modules. It also removes the
    refresh argument recently added to the grains module functions, as we're
    no longer using it to conditionally refresh modules.

--- salt/modules/grains.py
+++ salt/modules/grains.py
@@ -203,17 +203,13 @@ def item(*args, **kwargs):
     return ret
 
 
-def setvals(grains, destructive=False, refresh=True):
+def setvals(grains, destructive=False):
     '''
     Set new grains values in the grains config file
 
     destructive
         If an operation results in a key being removed, delete the key, too.
         Defaults to False.
-
-    refresh
-        Refresh modules and pillar after adding the new grains.
-        Defaults to True.
 
     CLI Example:
 
@@ -290,12 +286,12 @@ def setvals(grains, destructive=False, refresh=True):
         log.error(msg.format(fn_))
     if not __opts__.get('local', False):
         # Refresh the grains
-        __salt__['saltutil.refresh_grains'](refresh=refresh)
+        __salt__['saltutil.refresh_grains']()
     # Return the grains we just set to confirm everything was OK
     return new_grains
 
 
-def setval(key, val, destructive=False, refresh=True):
+def setval(key, val, destructive=False):
     '''
     Set a grains value in the grains config file
 
@@ -309,10 +305,6 @@ def setval(key, val, destructive=False, refresh=True):
         If an operation results in a key being removed, delete the key, too.
         Defaults to False.
 
-    refresh
-        Refresh modules and pillar after adding the new grain.
-        Defaults to True.
-
     CLI Example:
 
     .. code-block:: bash
@@ -320,7 +312,7 @@ def setval(key, val, destructive=False, refresh=True):
         salt '*' grains.setval key val
         salt '*' grains.setval key "{'sub-key': 'val', 'sub-key2': 'val2'}"
     '''
-    return setvals({key: val}, destructive, refresh)
+    return setvals({key: val}, destructive)
 
 
 def append(key, val, convert=False, delimiter=DEFAULT_TARGET_DELIM):
