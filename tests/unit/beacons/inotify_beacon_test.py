be06df9b64b94e59f2570a96c5bfd6d44bc0ca8e
Pedro Algarvio <pedro@algarvio.me>
2017-03-09 17:59:00 +0000

    Remove `*args, **kwargs`. Not needed, not useful.

--- tests/unit/beacons/inotify_beacon_test.py
+++ tests/unit/beacons/inotify_beacon_test.py
@@ -36,12 +36,12 @@ class INotifyBeaconTestCase(TestCase):
     def tearDown(self):
         shutil.rmtree(self.tmpdir, ignore_errors=True)
 
-    def test_empty_config(self, *args, **kwargs):
+    def test_empty_config(self):
         config = {}
         ret = inotify.beacon(config)
         self.assertEqual(ret, [])
 
-    def test_file_open(self, *args, **kwargs):
+    def test_file_open(self):
         path = os.path.realpath(__file__)
         config = {path: {'mask': ['open']}}
         ret = inotify.beacon(config)
@@ -54,7 +54,7 @@ class INotifyBeaconTestCase(TestCase):
         self.assertEqual(ret[0]['path'], path)
         self.assertEqual(ret[0]['change'], 'IN_OPEN')
 
-    def test_dir_no_auto_add(self, *args, **kwargs):
+    def test_dir_no_auto_add(self):
         config = {self.tmpdir: {'mask': ['create']}}
         ret = inotify.beacon(config)
         self.assertEqual(ret, [])
@@ -70,7 +70,7 @@ class INotifyBeaconTestCase(TestCase):
         ret = inotify.beacon(config)
         self.assertEqual(ret, [])
 
-    def test_dir_auto_add(self, *args, **kwargs):
+    def test_dir_auto_add(self):
         config = {self.tmpdir: {'mask': ['create', 'open'], 'auto_add': True}}
         ret = inotify.beacon(config)
         self.assertEqual(ret, [])
@@ -90,7 +90,7 @@ class INotifyBeaconTestCase(TestCase):
         self.assertEqual(ret[0]['path'], fp)
         self.assertEqual(ret[0]['change'], 'IN_OPEN')
 
-    def test_dir_recurse(self, *args, **kwargs):
+    def test_dir_recurse(self):
         dp1 = os.path.join(self.tmpdir, 'subdir1')
         os.mkdir(dp1)
         dp2 = os.path.join(dp1, 'subdir2')
@@ -112,12 +112,12 @@ class INotifyBeaconTestCase(TestCase):
         self.assertEqual(ret[2]['path'], fp)
         self.assertEqual(ret[2]['change'], 'IN_OPEN')
 
-    def test_dir_recurse_auto_add(self, *args, **kwargs):
+    def test_dir_recurse_auto_add(self):
         dp1 = os.path.join(self.tmpdir, 'subdir1')
         os.mkdir(dp1)
         config = {self.tmpdir: {'mask': ['create', 'delete'],
-                           'recurse': True,
-                           'auto_add': True}}
+                                'recurse': True,
+                                'auto_add': True}}
         ret = inotify.beacon(config)
         self.assertEqual(ret, [])
         dp2 = os.path.join(dp1, 'subdir2')
