dd53f055b952d56dbe7b20b320e509c092048cbe
Pedro Algarvio <pedro@algarvio.me>
2017-03-09 15:42:33 +0000

    Code cleanup and make sure the beacons config file is deleted after testing

--- tests/support/xmlunit.py
+++ tests/support/xmlunit.py
@@ -56,9 +56,7 @@ try:
 
     class _XMLTestResult(xmlrunner.result._XMLTestResult):
         def startTest(self, test):
-            logging.getLogger(__name__).debug(
-                '>>>>> START >>>>> {0}'.format(test.id())
-            )
+            log.debug('>>>>> START >>>>> {0}'.format(test.id()))
             # xmlrunner classes are NOT new-style classes
             xmlrunner.result._XMLTestResult.startTest(self, test)
             if self.buffer:
@@ -70,9 +68,7 @@ try:
                 sys.stdout = self._stdout_buffer
 
         def stopTest(self, test):
-            logging.getLogger(__name__).debug(
-                '<<<<< END <<<<<<< {0}'.format(test.id())
-            )
+            log.debug('<<<<< END <<<<<<< {0}'.format(test.id()))
             # xmlrunner classes are NOT new-style classes
             return xmlrunner.result._XMLTestResult.stopTest(self, test)
 
