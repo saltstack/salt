8ad47e78015a2d9d7a99003208d3c1e81039b2eb
Pedro Algarvio <pedro@algarvio.me>
2017-03-09 19:20:22 +0000

    Support the new list configuration format.
    Refs #38121

--- salt/beacons/status.py
+++ salt/beacons/status.py
@@ -126,33 +126,42 @@ def beacon(config):
         }]
 
     if len(config) < 1:
-        config = {
+        config = [{
             'loadavg': ['all'],
             'cpustats': ['all'],
             'meminfo': ['all'],
             'vmstats': ['all'],
             'time': ['all'],
-        }
+        }]
 
-    for func in config:
-        try:
-            data = __salt__['status.{0}'.format(func)]()
-        except salt.exceptions.CommandExecutionError as exc:
-            log.debug('Status beacon attempted to process function {0} \
-                    but encountered error: {1}'.format(func, exc))
-            continue
-        ret[func] = {}
-        item = config[func]
-        if item == 'all':
-            ret[func] = data
-        else:
+    if not isinstance(config, list):
+        # To support the old dictionary config format
+        config = [config]
+
+    for entry in config:
+        for func in entry:
+            ret[func] = {}
             try:
-                try:
-                    ret[func][item] = data[item]
-                except TypeError:
-                    ret[func][item] = data[int(item)]
-            except KeyError as exc:
-                ret[func] = 'Status beacon is incorrectly configured: {0}'.format(exc)
+                data = __salt__['status.{0}'.format(func)]()
+            except salt.exceptions.CommandExecutionError as exc:
+                log.debug('Status beacon attempted to process function {0} '
+                          'but encountered error: {1}'.format(func, exc))
+                continue
+            if not isinstance(entry[func], list):
+                func_items = [entry[func]]
+            else:
+                func_items = entry[func]
+            for item in func_items:
+                if item == 'all':
+                    ret[func] = data
+                else:
+                    try:
+                        try:
+                            ret[func][item] = data[item]
+                        except TypeError:
+                            ret[func][item] = data[int(item)]
+                    except KeyError as exc:
+                        ret[func] = 'Status beacon is incorrectly configured: {0}'.format(exc)
 
     return [{
         'tag': ctime,
