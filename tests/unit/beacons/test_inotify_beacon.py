38ceeb6f199a98646177658fbd5d6c07cac42802
Pedro Algarvio <pedro@algarvio.me>
2017-03-09 17:50:37 +0000

    These tests are not destructive

--- tests/unit/beacons/test_inotify_beacon.py
+++ tests/unit/beacons/test_inotify_beacon.py
@@ -11,7 +11,6 @@ from salt.beacons import inotify
 
 # Salt testing libs
 from tests.support.unit import skipIf, TestCase
-from tests.support.helpers import destructiveTest
 from tests.support.mock import NO_MOCK, NO_MOCK_REASON
 
 # Third-party libs
@@ -30,6 +29,10 @@ class INotifyBeaconTestCase(TestCase):
     '''
     def setUp(self):
         inotify.__context__ = {}
+        self.tmpdir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.tmpdir, ignore_errors=True)
 
     def test_empty_config(self, *args, **kwargs):
         config = {}
@@ -49,119 +52,87 @@ class INotifyBeaconTestCase(TestCase):
         self.assertEqual(ret[0]['path'], path)
         self.assertEqual(ret[0]['change'], 'IN_OPEN')
 
-    @destructiveTest
     def test_dir_no_auto_add(self, *args, **kwargs):
-        tmpdir = None
-        try:
-            tmpdir = tempfile.mkdtemp()
-            config = {tmpdir: {'mask': ['create']}}
-            ret = inotify.beacon(config)
-            self.assertEqual(ret, [])
-            fp = os.path.join(tmpdir, 'tmpfile')
-            with open(fp, 'w') as f:
-                pass
-            ret = inotify.beacon(config)
-            self.assertEqual(len(ret), 1)
-            self.assertEqual(ret[0]['path'], fp)
-            self.assertEqual(ret[0]['change'], 'IN_CREATE')
-            with open(fp, 'r') as f:
-                pass
-            ret = inotify.beacon(config)
-            self.assertEqual(ret, [])
+        config = {self.tmpdir: {'mask': ['create']}}
+        ret = inotify.beacon(config)
+        self.assertEqual(ret, [])
+        fp = os.path.join(self.tmpdir, 'tmpfile')
+        with open(fp, 'w') as f:
+            pass
+        ret = inotify.beacon(config)
+        self.assertEqual(len(ret), 1)
+        self.assertEqual(ret[0]['path'], fp)
+        self.assertEqual(ret[0]['change'], 'IN_CREATE')
+        with open(fp, 'r') as f:
+            pass
+        ret = inotify.beacon(config)
+        self.assertEqual(ret, [])
 
-        finally:
-            if tmpdir:
-                shutil.rmtree(tmpdir)
-
-    @destructiveTest
     def test_dir_auto_add(self, *args, **kwargs):
-        tmpdir = None
-        try:
-            tmpdir = tempfile.mkdtemp()
-            config = {tmpdir: {'mask': ['create', 'open'], 'auto_add': True}}
-            ret = inotify.beacon(config)
-            self.assertEqual(ret, [])
-            fp = os.path.join(tmpdir, 'tmpfile')
-            with open(fp, 'w') as f:
-                pass
-            ret = inotify.beacon(config)
-            self.assertEqual(len(ret), 2)
-            self.assertEqual(ret[0]['path'], fp)
-            self.assertEqual(ret[0]['change'], 'IN_CREATE')
-            self.assertEqual(ret[1]['path'], fp)
-            self.assertEqual(ret[1]['change'], 'IN_OPEN')
-            with open(fp, 'r') as f:
-                pass
-            ret = inotify.beacon(config)
-            self.assertEqual(len(ret), 1)
-            self.assertEqual(ret[0]['path'], fp)
-            self.assertEqual(ret[0]['change'], 'IN_OPEN')
+        config = {self.tmpdir: {'mask': ['create', 'open'], 'auto_add': True}}
+        ret = inotify.beacon(config)
+        self.assertEqual(ret, [])
+        fp = os.path.join(self.tmpdir, 'tmpfile')
+        with open(fp, 'w') as f:
+            pass
+        ret = inotify.beacon(config)
+        self.assertEqual(len(ret), 2)
+        self.assertEqual(ret[0]['path'], fp)
+        self.assertEqual(ret[0]['change'], 'IN_CREATE')
+        self.assertEqual(ret[1]['path'], fp)
+        self.assertEqual(ret[1]['change'], 'IN_OPEN')
+        with open(fp, 'r') as f:
+            pass
+        ret = inotify.beacon(config)
+        self.assertEqual(len(ret), 1)
+        self.assertEqual(ret[0]['path'], fp)
+        self.assertEqual(ret[0]['change'], 'IN_OPEN')
 
-        finally:
-            if tmpdir:
-                shutil.rmtree(tmpdir)
-
-    @destructiveTest
     def test_dir_recurse(self, *args, **kwargs):
-        tmpdir = None
-        try:
-            tmpdir = tempfile.mkdtemp()
-            dp1 = os.path.join(tmpdir, 'subdir1')
-            os.mkdir(dp1)
-            dp2 = os.path.join(dp1, 'subdir2')
-            os.mkdir(dp2)
-            fp = os.path.join(dp2, 'tmpfile')
-            with open(fp, 'w') as f:
-                pass
-            config = {tmpdir: {'mask': ['open'], 'recurse': True}}
-            ret = inotify.beacon(config)
-            self.assertEqual(ret, [])
-            with open(fp) as f:
-                pass
-            ret = inotify.beacon(config)
-            self.assertEqual(len(ret), 3)
-            self.assertEqual(ret[0]['path'], dp1)
-            self.assertEqual(ret[0]['change'], 'IN_OPEN|IN_ISDIR')
-            self.assertEqual(ret[1]['path'], dp2)
-            self.assertEqual(ret[1]['change'], 'IN_OPEN|IN_ISDIR')
-            self.assertEqual(ret[2]['path'], fp)
-            self.assertEqual(ret[2]['change'], 'IN_OPEN')
+        dp1 = os.path.join(self.tmpdir, 'subdir1')
+        os.mkdir(dp1)
+        dp2 = os.path.join(dp1, 'subdir2')
+        os.mkdir(dp2)
+        fp = os.path.join(dp2, 'tmpfile')
+        with open(fp, 'w') as f:
+            pass
+        config = {self.tmpdir: {'mask': ['open'], 'recurse': True}}
+        ret = inotify.beacon(config)
+        self.assertEqual(ret, [])
+        with open(fp) as f:
+            pass
+        ret = inotify.beacon(config)
+        self.assertEqual(len(ret), 3)
+        self.assertEqual(ret[0]['path'], dp1)
+        self.assertEqual(ret[0]['change'], 'IN_OPEN|IN_ISDIR')
+        self.assertEqual(ret[1]['path'], dp2)
+        self.assertEqual(ret[1]['change'], 'IN_OPEN|IN_ISDIR')
+        self.assertEqual(ret[2]['path'], fp)
+        self.assertEqual(ret[2]['change'], 'IN_OPEN')
 
-        finally:
-            if tmpdir:
-                shutil.rmtree(tmpdir)
-
-    @destructiveTest
     def test_dir_recurse_auto_add(self, *args, **kwargs):
-        tmpdir = None
-        try:
-            tmpdir = tempfile.mkdtemp()
-            dp1 = os.path.join(tmpdir, 'subdir1')
-            os.mkdir(dp1)
-            config = {tmpdir: {'mask': ['create', 'delete'],
-                               'recurse': True,
-                               'auto_add': True}}
-            ret = inotify.beacon(config)
-            self.assertEqual(ret, [])
-            dp2 = os.path.join(dp1, 'subdir2')
-            os.mkdir(dp2)
-            ret = inotify.beacon(config)
-            self.assertEqual(len(ret), 1)
-            self.assertEqual(ret[0]['path'], dp2)
-            self.assertEqual(ret[0]['change'], 'IN_CREATE|IN_ISDIR')
-            fp = os.path.join(dp2, 'tmpfile')
-            with open(fp, 'w') as f:
-                pass
-            ret = inotify.beacon(config)
-            self.assertEqual(len(ret), 1)
-            self.assertEqual(ret[0]['path'], fp)
-            self.assertEqual(ret[0]['change'], 'IN_CREATE')
-            os.remove(fp)
-            ret = inotify.beacon(config)
-            self.assertEqual(len(ret), 1)
-            self.assertEqual(ret[0]['path'], fp)
-            self.assertEqual(ret[0]['change'], 'IN_DELETE')
-
-        finally:
-            if tmpdir:
-                shutil.rmtree(tmpdir)
+        dp1 = os.path.join(self.tmpdir, 'subdir1')
+        os.mkdir(dp1)
+        config = {self.tmpdir: {'mask': ['create', 'delete'],
+                           'recurse': True,
+                           'auto_add': True}}
+        ret = inotify.beacon(config)
+        self.assertEqual(ret, [])
+        dp2 = os.path.join(dp1, 'subdir2')
+        os.mkdir(dp2)
+        ret = inotify.beacon(config)
+        self.assertEqual(len(ret), 1)
+        self.assertEqual(ret[0]['path'], dp2)
+        self.assertEqual(ret[0]['change'], 'IN_CREATE|IN_ISDIR')
+        fp = os.path.join(dp2, 'tmpfile')
+        with open(fp, 'w') as f:
+            pass
+        ret = inotify.beacon(config)
+        self.assertEqual(len(ret), 1)
+        self.assertEqual(ret[0]['path'], fp)
+        self.assertEqual(ret[0]['change'], 'IN_CREATE')
+        os.remove(fp)
+        ret = inotify.beacon(config)
+        self.assertEqual(len(ret), 1)
+        self.assertEqual(ret[0]['path'], fp)
+        self.assertEqual(ret[0]['change'], 'IN_DELETE')
