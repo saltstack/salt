031ba81b5cd01d5cdaca54f0488d466d0e2c12cd
Narendra Ingale <ningale@ningale-L111.local>
2017-03-13 15:12:32 +0530

    Removed one more log.info

--- salt/utils/event.py
+++ salt/utils/event.py
@@ -152,7 +152,6 @@ def get_master_event(opts, sock_dir, listen=True, io_loop=None, raise_errors=Fal
     '''
     # TODO: AIO core is separate from transport
     if opts['transport'] in ('zeromq', 'tcp', 'detect'):
-        log.info('This is transport')
         return MasterEvent(sock_dir, opts, listen=listen, io_loop=io_loop, raise_errors=raise_errors)
     elif opts['transport'] == 'raet':
         import salt.utils.raetevent
