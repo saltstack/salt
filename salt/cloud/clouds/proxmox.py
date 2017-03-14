91d5c500474b6d877fe79e75db50f8ff439f16bf
Landry MINOZA <landry.minoza@gmail.com>
2017-03-14 15:19:38 +0100

    Stop using generator and return a list
    _get_properties() does not need to be a generator (returned list is
    short) and the whall list will be used each time.
    
    Also add a parameter to be able to use the old lists of properties (it
    is also possible to add a forced property) and make each property
    unique in the list by using a set to avoid duplicate parameters.

--- salt/cloud/clouds/proxmox.py
+++ salt/cloud/clouds/proxmox.py
@@ -639,7 +639,7 @@ def _import_api():
     api_json = re_filter.findall(returned_data.text)[0]
     api = json.loads(api_json)
 
-def _get_properties(path="",method="GET"):
+def _get_properties(path="",method="GET",forced_params=[]):
     '''
     Return the parameter list from api for defined path and HTTP method
     '''
@@ -650,6 +650,7 @@ def _get_properties(path="",method="GET"):
     path_levels = [level for level in path.split('/') if level != '']
     search_path = ''
     props = []
+    parameters = set(forced_params)
     for elem in path_levels[:-1]:
         search_path += '/' + elem
         sub = (item for item in sub if item["path"] == search_path).next()['children']
@@ -665,9 +666,10 @@ def _get_properties(path="",method="GET"):
         numerical = re.match('(\w+)\[n\]', prop)
         if numerical:
             for i in range(10):
-                yield numerical.group(1) + str(i)
+                parameters.add(numerical.group(1) + str(i))
         else:
-            yield prop
+            parameters.add(prop)
+    return parameters
 
 def create_node(vm_, newid):
     '''
