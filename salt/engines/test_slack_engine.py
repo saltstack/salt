942d9613707d62cddb1dc2021feba1a2b5ed6910
Peter C. Norton <spacey@spacey.org>
2017-03-17 11:30:42 -0400

    Break out group settings to a separate function
    - Move fire() outside of main()
    - Updated docs to only have group-based config
    - This reads permissions on a per-group basis
    , Add a permissions checker

---
+++ salt/engines/test_slack_engine.py
@@ -0,0 +1,25 @@
+def test_group_permissions():
+    groups = {
+        "default": {
+            "users": ["*"],
+            "commands": [ "test.ping" ],
+            "aliases": {
+                "list_jobs": {
+                    "cmd": "jobs.list_jobs",
+                },
+                "list_commands": {
+                    "cmd": "pillar.get salt:engines:slack:valid_commands target=saltmaster tgt_type=list"
+                }
+            }
+        },
+        "gods": {
+            "users": ["garethgreenaway"],
+            "commands": ["*"]
+        }
+    }
+
+
+
+    assert _can_user_run("pcn", "test.ping", groups), True
+    assert _can_user_run("pcn", "cmd.run", groups), False
+    assert _can_user_run("garethgreenaway", "cmd.run", groups), True
