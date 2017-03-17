a9107cde444431b79e96af2e5b115a08746f3a16
Narendra Ingale <ningale@ningale-L111.local>
2017-03-17 20:18:38 +0530

    Added if condition for broken link.

--- salt/states/file.py
+++ salt/states/file.py
@@ -2059,7 +2059,11 @@ def directory(name,
     if not os.path.isabs(name):
         return _error(
             ret, 'Specified file {0} is not an absolute path'.format(name))
-    if os.path.isfile(name) or (not allow_symlink and os.path.islink(name)):
+
+    # Check for existing file or symlink
+    if os.path.isfile(name) or (not allow_symlink and os.path.islink(name))\
+       or not os.path.exists(os.readlink(name)):
+        # Was a backupname specified
         if backupname is not None:
             # Make a backup first
             if os.path.lexists(backupname):
