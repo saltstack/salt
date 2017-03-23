3a0ca3e950739143f7fd7c7469389520145d84d7
Mircea Ulinic <mirucha@cloudflare.com>
2017-03-23 15:23:30 +0000

    Simplify and more readable px keepalive condition

--- salt/minion.py
+++ salt/minion.py
@@ -3207,8 +3207,9 @@ class ProxyMinion(Minion):
 
         # proxy keepalive
         proxy_alive_fn = fq_proxyname+'.alive'
-        if proxy_alive_fn in self.proxy and 'status.proxy_reconnect' in self.functions and \
-           ('proxy_keep_alive' not in self.opts or ('proxy_keep_alive' in self.opts and self.opts['proxy_keep_alive'])):
+        if (proxy_alive_fn in self.proxy
+            and 'status.proxy_reconnect' in self.functions and
+            self.opts.get('proxy_keep_alive', True)):
             # if `proxy_keep_alive` is either not specified, either set to False does not retry reconnecting
             self.schedule.add_job({
                 '__proxy_keepalive':
