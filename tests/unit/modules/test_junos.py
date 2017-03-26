f629feaabd4ce85d101fbfff7fce2f6b76a2dc86
rajvidhimar <rajvidhimar95@gmail.com>
2017-03-26 17:19:12 +0530

    Take care of import error and add condition to skip tests if PyEZ is not installed

--- tests/unit/modules/test_junos.py
+++ tests/unit/modules/test_junos.py
@@ -5,19 +5,24 @@ from __future__ import print_function
 
 __author__ = "Rajvi Dhimar"
 
-import unittest
 from tests.support.mock import patch, mock_open
+from tests.support.unit import skipIf, TestCase
 try:
     from lxml import etree
 except ImportError:
     from salt._compat import ElementTree as etree
 
-from jnpr.junos.utils.config import Config
-from jnpr.junos.utils.sw import SW
-from jnpr.junos.device import Device
+try:
+    from jnpr.junos.utils.config import Config
+    from jnpr.junos.utils.sw import SW
+    from jnpr.junos.device import Device
+    HAS_JUNOS = True
+except ImportError:
+    HAS_JUNOS = False
 import salt.modules.junos as junos
 
-class Test_Junos_Module(unittest.TestCase):
+@skipIf(not HAS_JUNOS, 'Missing dependencies')
+class Test_Junos_Module(TestCase):
 
     def setUp(self):
         junos.__proxy__ = {
