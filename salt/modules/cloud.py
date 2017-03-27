18ee1d1443e7d54cf8bc3ec3a72c4537c258027b
Joseph Hall <perlhoser@gmail.com>
2017-03-27 08:19:04 -0600

    Update documentation for maps

--- salt/modules/cloud.py
+++ salt/modules/cloud.py
@@ -57,7 +57,7 @@ def list_sizes(provider='all'):
 
     .. code-block:: bash
 
-        salt '*' cloud.list_sizes my-gce-config
+        salt minionname cloud.list_sizes my-gce-config
     '''
     client = _get_client()
     sizes = client.list_sizes(provider)
@@ -72,7 +72,7 @@ def list_images(provider='all'):
 
     .. code-block:: bash
 
-        salt '*' cloud.list_images my-gce-config
+        salt minionname cloud.list_images my-gce-config
     '''
     client = _get_client()
     images = client.list_images(provider)
@@ -87,7 +87,7 @@ def list_locations(provider='all'):
 
     .. code-block:: bash
 
-        salt '*' cloud.list_locations my-gce-config
+        salt minionname cloud.list_locations my-gce-config
     '''
     client = _get_client()
     locations = client.list_locations(provider)
@@ -102,9 +102,9 @@ def query(query_type='list_nodes'):
 
     .. code-block:: bash
 
-        salt '*' cloud.query
-        salt '*' cloud.query list_nodes_full
-        salt '*' cloud.query list_nodes_select
+        salt minionname cloud.query
+        salt minionname cloud.query list_nodes_full
+        salt minionname cloud.query list_nodes_select
     '''
     client = _get_client()
     info = client.query(query_type)
@@ -119,7 +119,7 @@ def full_query(query_type='list_nodes_full'):
 
     .. code-block:: bash
 
-        salt '*' cloud.full_query
+        salt minionname cloud.full_query
     '''
     return query(query_type=query_type)
 
@@ -132,7 +132,7 @@ def select_query(query_type='list_nodes_select'):
 
     .. code-block:: bash
 
-        salt '*' cloud.select_query
+        salt minionname cloud.select_query
     '''
     return query(query_type=query_type)
 
@@ -145,7 +145,7 @@ def has_instance(name, provider=None):
 
     .. code-block:: bash
 
-        salt '*' cloud.has_instance myinstance
+        salt minionname cloud.has_instance myinstance
     '''
     data = get_instance(name, provider)
     if data is None:
@@ -164,7 +164,7 @@ def get_instance(name, provider=None):
 
     .. code-block:: bash
 
-        salt '*' cloud.get_instance myinstance
+        salt minionname cloud.get_instance myinstance
 
     SLS Example:
 
@@ -191,12 +191,28 @@ def profile_(profile, names, vm_overrides=None, opts=None, **kwargs):
 
     .. code-block:: bash
 
-        salt '*' cloud.profile my-gce-config myinstance
+        salt minionname cloud.profile my-gce-config myinstance
     '''
     client = _get_client()
     if isinstance(opts, dict):
         client.opts.update(opts)
     info = client.profile(profile, names, vm_overrides=vm_overrides, **kwargs)
+    return info
+
+
+def map_run(path=None, **kwargs):
+    '''
+    Execute a salt cloud map file
+
+    CLI Examples:
+
+    .. code-block:: bash
+
+        salt minionname cloud.map_run /path/to/cloud.map
+        salt minionname cloud.map_run map_data='<actual map data>'
+    '''
+    client = _get_client()
+    info = client.map_run(path, **_filter_kwargs(kwargs))
     return info
 
 
@@ -208,7 +224,7 @@ def destroy(names):
 
     .. code-block:: bash
 
-        salt '*' cloud.destroy myinstance
+        salt minionname cloud.destroy myinstance
     '''
     client = _get_client()
     info = client.destroy(names)
@@ -229,9 +245,9 @@ def action(
 
     .. code-block:: bash
 
-        salt '*' cloud.action start instance=myinstance
-        salt '*' cloud.action stop instance=myinstance
-        salt '*' cloud.action show_image provider=my-ec2-config image=ami-1624987f
+        salt minionname cloud.action start instance=myinstance
+        salt minionname cloud.action stop instance=myinstance
+        salt minionname cloud.action show_image provider=my-ec2-config image=ami-1624987f
     '''
     client = _get_client()
     info = client.action(fun, cloudmap, names, provider, instance, kwargs)
