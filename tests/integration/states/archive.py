8b21b4c8bbf28720ad9c6bba13c5c59dee67af54
Ch3LL <mwilhite@saltstack.com>
2017-03-27 10:32:27 -0600

    add use_cmd_unzip test

--- tests/integration/states/archive.py
+++ tests/integration/states/archive.py
@@ -205,6 +205,23 @@ class ArchiveTest(integration.ModuleCase,
 
         self._check_extracted(UNTAR_FILE)
 
+    def test_archive_extracted_with_cmd_unzip_false(self):
+        '''
+        test archive.extracted using use_cmd_unzip argument as false
+        '''
+
+        ret = self.run_state('archive.extracted', name=ARCHIVE_DIR,
+                             source=ARCHIVE_TAR_SOURCE,
+                             source_hash=ARCHIVE_TAR_HASH,
+                             use_cmd_unzip=False,
+                             archive_format='tar')
+        log.debug('ret = %s', ret)
+        if 'Timeout' in ret:
+            self.skipTest('Timeout talking to local tornado server.')
+        self.assertSaltTrueReturn(ret)
+
+        self._check_extracted(UNTAR_FILE)
+
 
 if __name__ == '__main__':
     from integration import run_tests
