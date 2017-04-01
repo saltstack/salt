6af300be24549743d99765468b438a5f4ff625e0
Roche, Damien <RocheD@DNB.com>
2017-04-01 20:19:18 +0100

    Fixed lint issues. Enhanced to transprently use hdfs if available and gracefully fallback on hadoop command

--- salt/modules/hadoop.py
+++ salt/modules/hadoop.py
@@ -14,16 +14,46 @@ from __future__ import absolute_import
 # Import salt libs
 import salt.utils
 
-__authorized_modules__ = ['namenode', 'dfsadmin', 'dfs', 'fs']
+__authorized_modules__ = ['version', 'namenode', 'dfsadmin', 'dfs', 'fs']
 
 
 def __virtual__():
     '''
     Check if hadoop is present, then load the module
     '''
-    if salt.utils.which('hadoop'):
+    if salt.utils.which('hadoop') or salt.utils.which('hdfs'):
         return 'hadoop'
     return (False, 'The hadoop execution module cannot be loaded: hadoop binary not in path.')
+
+
+def _hadoop_cmd(module, command, *args):
+    '''
+       Hadoop/hdfs command wrapper
+
+       As Hadoop command has been deprecated this module will default
+       to use hdfs command and fall back to hadoop if it is not found
+
+       In order to prevent random execution the module name is checked
+
+       Follows hadoop command template:
+          hadoop module -command args
+       E.g.: hadoop dfs -ls /
+    '''
+    tool = 'hadoop'
+    if salt.utils.which('hdfs'):
+        tool = 'hdfs'
+
+    out = None
+    if module and command:
+        if module in __authorized_modules__:
+            mappings = {'tool': tool, 'module': module, 'command': command, 'args': ' '.join(args)}
+            cmd = '{tool} {module} -{command} {args}'.format(**mappings)
+            out = __salt__['cmd.run'](cmd, python_shell=False)
+        else:
+            return 'Error: Unknown module'
+    else:
+        return 'Error: Module and command not defined'
+    return out
 
 
 def version():
@@ -36,49 +66,9 @@ def version():
 
         salt '*' hadoop.version
     '''
-    cmd = 'hadoop version'
-    out = __salt__['cmd.run'](cmd).split()
+    module = 'version'
+    out = _hadoop_cmd(module, True).splitlines()[0].split()
     return out[1]
-
-
-def _hadoop_cmd(module, command, *args):
-    '''
-       Hadoop command wrapper
-
-       Hadoop command has been deprecated for use of hdfs command
-
-       In order to prevent random execution the module name is checked
-
-       Follows hadoop command template:
-          hadoop module -command args
-       E.g.: hadoop dfs -ls /
-    '''
-    out = None
-    if module and command:
-        if module in __authorized_modules__:
-            cmd = 'hadoop {0} -{1} {2}'.format(module, command, ' '.join(args))
-            out = __salt__['cmd.run'](cmd, python_shell=False)
-        else:
-            return 'Error: Unknown module'
-    else:
-        return 'Error: Module and command not defined'
-    return out
-
-
-def _hdfs_cmd(module, command, *args):
-  '''
-     hdfs command wrapper
-  '''
-  out = None
-  if module and command:
-    if module in __authorized_modules__:
-        cmd = 'hdfs {module} -{command} {args}'.format(module=module, command=command, args=' '.join(args))
-        out = __salt__['cmd.run'](cmd, python_shell=False)
-    else:
-      return 'Error: Unknown module'
-  else:
-    return 'Error: Module and command not defined'
-  return out
 
 
 def dfs(command=None, *args):
@@ -92,19 +82,14 @@ def dfs(command=None, *args):
     .. code-block:: bash
 
         salt '*' hadoop.dfs ls /
-
-        salt '*' hadoop.dfs ls / use_hdfs
     '''
     if command:
-        if 'use_hdfs' in args:
-            args = tuple([ a for a in args if a != 'use_hdfs' ])
-            return _hdfs_cmd('dfs', command, *args)
         return _hadoop_cmd('dfs', command, *args)
     else:
         return 'Error: command must be provided'
 
 
-def dfs_present(path, use_hdfs=False):
+def dfs_present(path):
     '''
     Check if a file or directory is present on the distributed FS.
 
@@ -114,22 +99,14 @@ def dfs_present(path, use_hdfs=False):
 
         salt '*' hadoop.dfs_present /some_random_file
 
-        salt '*' hadoop.dfs_present /some_random_file use_hdfs=True
-
-
     Returns True if the file is present
     '''
-    if not use_hdfs:
-        cmd_return = _hadoop_cmd('dfs', 'stat', path)
-    else:
-        cmd_return = _hdfs_cmd('dfs', 'stat', path)        
-    if 'No such file or directory' in cmd_return:
-        return False
-    else:
-        return True
+    cmd_return = _hadoop_cmd('dfs', 'stat', path)
+    match = 'No such file or directory'
+    return False if match in cmd_return else True
 
 
-def dfs_absent(path, use_hdfs=False):
+def dfs_absent(path):
     '''
     Check if a file or directory is absent on the distributed FS.
 
@@ -141,18 +118,11 @@ def dfs_absent(path, use_hdfs=False):
 
         salt '*' hadoop.dfs_absent /some_random_file
 
-        salt '*' hadoop.dfs_absent /some_random_file use_hdfs=True
-
     Returns True if the file is absent
     '''
-    if not use_hdfs:
-        cmd_return = _hadoop_cmd('dfs', 'stat', path)
-    else:
-        cmd_return = _hdfs_cmd('dfs', 'stat', path)        
-    if 'No such file or directory' in cmd_return:
-        return True
-    else:
-        return False
+    cmd_return = _hadoop_cmd('dfs', 'stat', path)
+    match = 'No such file or directory'
+    return True if match in cmd_return else False
 
 
 def namenode_format(force=None):
