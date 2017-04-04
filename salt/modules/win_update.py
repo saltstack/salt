75ccca7a2fa0e03b6c37c7ae9241b5ead24af1a7
twangboy <slee@saltstack.com>
2017-04-03 19:16:54 -0600

    Gate deprecation warning

--- salt/modules/win_update.py
+++ salt/modules/win_update.py
@@ -84,12 +84,12 @@ def __virtual__():
     '''
     Only works on Windows systems
     '''
-    salt.utils.warn_until(
-        'Oxygen',
-        'The \'win_update\' module is deprecated, and will be removed in Salt '
-        '{version}. Please use the \'win_wua\' module instead.'
-    )
     if salt.utils.is_windows() and HAS_DEPENDENCIES:
+        salt.utils.warn_until(
+            'Oxygen',
+            'The \'win_update\' module is deprecated, and will be removed in'
+            'Salt {version}. Please use the \'win_wua\' module instead.'
+        )
         return True
     return (False, "Module win_update: module has failed dependencies or is not on Windows client")
 
