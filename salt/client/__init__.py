6eec04b2db45a35af9d489d7f94f6db25dc1d617
Christian McHugh <McHughC@DNB.com>
2017-04-12 18:09:51 +0100

    pop out of kwargs

--- salt/client/__init__.py
+++ salt/client/__init__.py
@@ -516,13 +516,15 @@ class LocalClient(object):
         if 'gather_job_timeout' in kwargs:
             opts['gather_job_timeout'] = kwargs['gather_job_timeout']
 
-        eauth=kwargs.get('eauth', {})
+        eauth = {}
+        if 'eauth' in kwargs:
+            eauth['eauth'] = kwargs.pop('eauth')
         if 'username' in kwargs:
-            eauth['username'] = kwargs['username']
+            eauth['username'] = kwargs.pop('username')
         if 'password' in kwargs:
-            eauth['password'] = kwargs['password']
+            eauth['password'] = kwargs.pop('password')
         if 'token' in kwargs:
-            eauth['token'] = kwargs['token']
+            eauth['token'] = kwargs.pop('token')
 
         for key, val in six.iteritems(self.opts):
             if key not in opts:
