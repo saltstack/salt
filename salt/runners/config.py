5e8f92ef70f9dbdb62e1cdb371604aa7d4a7357a
Colton Myers <colton.myers@gmail.com>
2017-04-17 14:41:42 -0600

    Add config.get runner function

---
+++ salt/runners/config.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+'''
+This runner is designed to mirror the execution module config.py, but for
+master settings
+'''
+from __future__ import absolute_import
+from __future__ import print_function
+
+import salt.utils
+
+
+def get(key, default='', delimiter=':'):
+    '''
+    Retrieve master config options, with optional nesting via the delimiter
+    argument.
+
+    **Arguments**
+
+    default
+
+        If the key is not found, the default will be returned instead
+
+    delimiter
+
+        Override the delimiter used to separate nested levels of a data
+        structure.
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt-run config.get gitfs_remotes
+    '''
+    return salt.utils.traverse_dict_and_list(__opts__, key, delimiter=delimiter)
