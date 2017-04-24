637d0ca8658eb0e0afb17b8c35872f259292fc60
Anthony Shaw <anthonyshaw@apache.org>
2017-04-24 10:36:18 +1000

    add some assertion messages to help debug the issue

--- tests/integration/modules/test_libcloud_dns.py
+++ tests/integration/modules/test_libcloud_dns.py
@@ -13,9 +13,13 @@ class LibcloudDNSTest(ModuleCase):
         libcloud_dns.list_record_types
         '''
         # Simple profile (no special kwargs)
-        self.assertTrue('SPF' in self.run_function('libcloud_dns.list_record_types', ['profile_test1']))
+        supported_records = self.run_function(
+            'libcloud_dns.list_record_types', 
+            ['profile_test1'])
+        self.assertTrue('SPF' in supported_records, msg="Failed to find record type 'SPF' in {0}".format(supported_records))
 
         # Complex profile (special kwargs)
         accepted_record_types = self.run_function('libcloud_dns.list_record_types', ['profile_test2'])
 
-        self.assertTrue(isinstance(accepted_record_types, list) and 'SRV' in accepted_record_types)
+        self.assertTrue(isinstance(accepted_record_types, list) and 'SRV' in accepted_record_types,
+                        msg="Failed to find record type 'SRV' in {0}".format(accepted_record_types))
