7fcc17012afadc147e5ee9674a726776c2f07988
Alexandru Bleotu <Alexandru.Bleotu@morganstanley.com>
2017-05-03 17:17:26 +0100

    root_dir is root fix

--- salt/config/__init__.py
+++ salt/config/__init__.py
@@ -2053,7 +2053,7 @@ def prepend_root_dir(opts, path_options):
     Prepends the options that represent filesystem paths with value of the
     'root_dir' option.
     '''
-    root_dir = opts['root_dir']
+    root_dir = opts['root_dir'].rstrip(os.sep)
     root_opt_val = opts['root_dir']
     if not os.path.isabs(root_dir):
         # default root dir is always absolute and it needs to prefix the
