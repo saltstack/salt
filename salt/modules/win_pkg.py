9467ea9f15f6464c226125b1d4ea338ceeb17345
twangboy <slee@saltstack.com>
2017-05-04 16:51:29 -0600

    Fix some indenting issues

--- salt/modules/win_pkg.py
+++ salt/modules/win_pkg.py
@@ -895,8 +895,9 @@ def _get_msiexec(use_msiexec):
         if os.path.isfile(use_msiexec):
             return True, use_msiexec
         else:
-            log.warning(("msiexec path '{0}' not found. Using system registered"
-                     " msiexec instead").format(use_msiexec))
+            log.warning(
+                ("msiexec path '{0}' not found. Using system registered "
+                 "msiexec instead").format(use_msiexec))
         use_msiexec = True
     if use_msiexec is True:
         return True, 'msiexec'
