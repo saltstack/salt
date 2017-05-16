61801a5440e17616368b84d4d14af1d9368659bd
Bo Maryniuk <bo@suse.de>
2017-05-16 16:21:23 +0200

    Harden to 127. IP part

--- salt/utils/network.py
+++ salt/utils/network.py
@@ -99,7 +99,7 @@ def _generate_minion_id():
         Needs to work on Python 2.6, because of collections.OrderedDict only since 2.7 version.
         Override 'filter()' for custom filtering.
         '''
-        localhost_matchers = [r'localhost.*', r'ip6-.*', r'127*[.]\d', r'0\.0\.0\.0',
+        localhost_matchers = [r'localhost.*', r'ip6-.*', r'127[.]\d', r'0\.0\.0\.0',
                               r'::1.*', r'ipv6-.*', r'fe00::.*', r'fe02::.*', r'1.0.0.*.ip6.arpa']
 
         def append(self, p_object):
