5039fe12fbeed233a202f80d73f10f32a605775c
Gareth J. Greenaway <gareth@wiked.org>
2017-05-16 15:04:07 -0700

    Removing chdir as it is no needed with this change

--- salt/utils/minions.py
+++ salt/utils/minions.py
@@ -250,7 +250,6 @@ class CkMinions(object):
         If not 'greedy' return the only minions have cache data and matched by the condition.
         '''
         cache_enabled = self.opts.get('minion_data_cache', False)
-        cdir = os.path.join(self.opts['cachedir'], 'minions')
 
         def list_cached_minions():
             return self.cache.list('minions')
