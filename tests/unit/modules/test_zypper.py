0bf0950158695dcc53a6cc09fa037afd497b7388
Bo Maryniuk <bo@suse.de>
2017-05-24 11:46:55 +0200

    Add UT for typecheck on the output data

--- tests/unit/modules/test_zypper.py
+++ tests/unit/modules/test_zypper.py
@@ -1032,3 +1032,21 @@ Repository 'DUMMY' not found by its alias, number, or URI.
         _zpr.nolock.xml.call = MagicMock(return_value=minidom.parseString(xmldoc))
         assert zypper.Wildcard(_zpr)('libzypp', None) is None
 
+    def test_wildcard_to_query_typecheck(self):
+        '''
+        Test wildcard to query typecheck.
+
+        :return:
+        '''
+        xmldoc = """<?xml version='1.0'?><stream>
+        <search-result version="0.0"><solvable-list>
+        <solvable status="installed" name="libzypp" kind="package" edition="16.2.4-19.5" arch="x86_64" repository="foo"/>
+        <solvable status="other-version" name="libzypp" kind="package" edition="16.2.5-25.1" arch="x86_64" repository="foo"/>
+        <solvable status="other-version" name="libzypp" kind="package" edition="17.2.6-27.9.1" arch="x86_64" repository="foo"/>
+        </solvable-list></search-result></stream>
+        """
+        _zpr = MagicMock()
+        _zpr.nolock.xml.call = MagicMock(return_value=minidom.parseString(xmldoc))
+
+        assert type(zypper.Wildcard(_zpr)('libzypp', '*.1')) is type('')
+
