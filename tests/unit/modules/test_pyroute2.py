1f507cfa7a4107935ba990f086dc417350d35470
Peter V. Saveliev <peter@svinota.eu>
2017-05-31 20:55:36 +0200

    unit tests: add pyroute2 interface dict test
    Bug-Url: https://github.com/saltstack/salt/pull/41487
    Bug-Url: https://github.com/saltstack/salt/issues/41461

---
+++ tests/unit/modules/test_pyroute2.py
@@ -0,0 +1,12 @@
+
+from pyroute2 import IPDB
+from tests.support.unit import TestCase
+from salt.beacons.network_settings import ATTRS
+
+
+class Pyroute2TestCase(TestCase):
+
+    def test_interface_dict_fields(self):
+        with IPDB() as ipdb:
+            for attr in ATTRS:
+                self.assertIn(attr, ipdb.interfaces[1])
