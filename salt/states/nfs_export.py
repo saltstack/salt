d2bbb1572d387aaa03f6c8a5d8b97348186a01b2
Nathan Fish <lordcirth@gmail.com>
2017-06-12 16:32:18 -0500

    nfs_export: third draft, with simple & complex modes

--- salt/states/nfs_export.py
+++ salt/states/nfs_export.py
@@ -7,14 +7,32 @@ To ensure an NFS export exists:
 
 .. code-block:: yaml
 
-    add_export:
+    add_simple_export:
+      nfs_export.present:
+        - name:     '/srv/nfs'
+        - hosts:    '10.0.2.0/24'
+        - options:  'rw'
+
+For more complex exports with multiple groups of hosts:
+
+.. code-block:: yaml
+
+    add_complex_export:
       nfs_export.present:
         - name: '/srv/nfs'
         - exports:
+          # First export, same as simple one above
           - hosts:
-            - '10.0.2.0/24'
-          - options:
-            - 'rw'
+              - '10.0.2.0/24'
+            options:
+              - 'rw'
+          # Second export
+          - hosts:
+              - '192.168.0.0/24'
+              - '172.19.0.0/16'
+            options:
+              - 'ro'
+              - 'subtree_check'
 
 This creates the following in /etc/exports:
 
