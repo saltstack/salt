0d4be0220bb91752dae175bf6cbf72bd63d71df5
twangboy <slee@saltstack.com>
2017-06-12 15:48:46 -0600

    Add batch files for master

--- pkg/windows/buildenv/salt-master.bat
+++ pkg/windows/buildenv/salt-master.bat
@@ -1,12 +1,12 @@
 @ echo off
-:: Script for invoking Salt Main
-:: Accepts all parameters that Salt Main accepts
+:: Script for starting the Salt-Master
+:: Accepts all parameters that Salt-Master accepts
 
 :: Define Variables
 Set SaltDir=%~dp0
 Set SaltDir=%SaltDir:~0,-1%
 Set Python=%SaltDir%\bin\python.exe
-Set Script=%SaltDir%\bin\Scripts\salt
+Set Script=%SaltDir%\bin\Scripts\salt-master
 
 :: Launch Script
 "%Python%" "%Script%" %*
