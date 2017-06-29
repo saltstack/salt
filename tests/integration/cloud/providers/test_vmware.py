80f53cd98cc1bd2d68e3582592789ce890fb088c
rallytime <nicole@saltstack.com>
2017-06-29 16:11:07 -0600

    Replace cloud test __random_name funcs with new generate_random_name helper

--- tests/integration/cloud/providers/test_vmware.py
+++ tests/integration/cloud/providers/test_vmware.py
@@ -6,8 +6,6 @@
 # Import Python Libs
 from __future__ import absolute_import
 import os
-import random
-import string
 
 # Import Salt Libs
 from salt.config import cloud_providers_config, cloud_config
@@ -15,21 +13,10 @@ from salt.config import cloud_providers_config, cloud_config
 # Import Salt Testing LIbs
 from tests.support.case import ShellCase
 from tests.support.paths import FILES
-from tests.support.helpers import expensiveTest
-from salt.ext.six.moves import range
-
-
-def __random_name(size=6):
-    '''
-    Generates a radom cloud instance name
-    '''
-    return 'CLOUD-TEST-' + ''.join(
-        random.choice(string.ascii_uppercase + string.digits)
-        for x in range(size)
-    )
+from tests.support.helpers import expensiveTest, generate_random_name
 
 # Create the cloud instance name to be used throughout the tests
-INSTANCE_NAME = __random_name()
+INSTANCE_NAME = generate_random_name('CLOUD-TEST-')
 PROVIDER_NAME = 'vmware'
 TIMEOUT = 500
 
