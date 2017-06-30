cd7067748e8576fb7df7dec6a990609951144cc6
Gareth J. Greenaway <gareth@wiked.org>
2017-06-30 15:51:58 -0700

    Updating the telegram_bot_msg beacon to use list based configuration rather that dictionary based configuration.

--- salt/beacons/telegram_bot_msg.py
+++ salt/beacons/telegram_bot_msg.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 '''
 Beacon to emit Telegram messages
+
+Requires the python-telegram-bot library
+
 '''
 
 # Import Python libs
@@ -28,20 +31,23 @@ def __virtual__():
         return False
 
 
-def __validate__(config):
+def validate(config):
     '''
     Validate the beacon configuration
     '''
-    if not isinstance(config, dict):
+    if not isinstance(config, list):
         return False, ('Configuration for telegram_bot_msg '
-                       'beacon must be a dictionary.')
+                       'beacon must be a list.')
 
-    if not all(config.get(required_config)
+    _config = {}
+    list(map(_config.update, config))
+
+    if not all(_config.get(required_config)
                for required_config in ['token', 'accept_from']):
         return False, ('Not all required configuration for '
                        'telegram_bot_msg are set.')
 
-    if not isinstance(config.get('accept_from'), list):
+    if not isinstance(_config.get('accept_from'), list):
         return False, ('Configuration for telegram_bot_msg, '
                        'accept_from must be a list of usernames.')
 
@@ -57,18 +63,22 @@ def beacon(config):
 
         beacons:
           telegram_bot_msg:
-            token: "<bot access token>"
-            accept_from:
+            - token: "<bot access token>"
+            - accept_from:
               - "<valid username>"
-            interval: 10
+            - interval: 10
 
     '''
+
+    _config = {}
+    list(map(_config.update, config))
+
     log.debug('telegram_bot_msg beacon starting')
     ret = []
     output = {}
     output['msgs'] = []
 
-    bot = telegram.Bot(config['token'])
+    bot = telegram.Bot(_config['token'])
     updates = bot.get_updates(limit=100, timeout=0, network_delay=10)
 
     log.debug('Num updates: {0}'.format(len(updates)))
@@ -83,7 +93,7 @@ def beacon(config):
         if update.update_id > latest_update_id:
             latest_update_id = update.update_id
 
-        if message.chat.username in config['accept_from']:
+        if message.chat.username in _config['accept_from']:
             output['msgs'].append(message.to_dict())
 
     # mark in the server that previous messages are processed
