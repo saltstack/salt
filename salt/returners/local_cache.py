4dd0894d414651b1bd44d4227af01f86ba2fd8da
twangboy <slee@saltstack.com>
2017-07-05 12:19:20 -0600

    Get more accurate currnet time in local_cache

--- salt/returners/local_cache.py
+++ salt/returners/local_cache.py
@@ -391,7 +391,6 @@ def clean_old_jobs():
     Clean out the old jobs from the job cache
     '''
     if __opts__['keep_jobs'] != 0:
-        cur = time.time()
         jid_root = _job_dir()
 
         if not os.path.exists(jid_root):
@@ -421,6 +420,7 @@ def clean_old_jobs():
                     shutil.rmtree(t_path)
                 elif os.path.isfile(jid_file):
                     jid_ctime = os.stat(jid_file).st_ctime
+                    cur = time.time()
                     hours_difference = (cur - jid_ctime) / 3600.0
                     if hours_difference > __opts__['keep_jobs'] and os.path.exists(t_path):
                         # Remove the entire t_path from the original JID dir
@@ -435,6 +435,7 @@ def clean_old_jobs():
                 # Checking the time again prevents a possible race condition where
                 # t_path JID dirs were created, but not yet populated by a jid file.
                 t_path_ctime = os.stat(t_path).st_ctime
+                cur = time.time()
                 hours_difference = (cur - t_path_ctime) / 3600.0
                 if hours_difference > __opts__['keep_jobs']:
                     shutil.rmtree(t_path)
