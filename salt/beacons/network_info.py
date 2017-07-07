a04b0fe3ee2240e69a3344eb26db70459601b718
Gareth J. Greenaway <gareth@wiked.org>
2017-07-07 09:27:42 -0700

    Updating network_info beacon for list based configuration.  Adding unit tests for network_info beacon

--- salt/beacons/network_info.py
+++ salt/beacons/network_info.py
@@ -45,7 +45,7 @@ def __virtual__():
     return __virtualname__
 
 
-def __validate__(config):
+def validate(config):
     '''
     Validate the beacon configuration
     '''
@@ -58,14 +58,18 @@ def __validate__(config):
 
     # Configuration for load beacon should be a list of dicts
     if not isinstance(config, list):
-        return False, ('Configuration for load beacon must be a dictionary.')
+        return False, ('Configuration for network_info beacon must be a list.')
     else:
-        for item in config:
-            if not isinstance(config[item], dict):
-                return False, ('Configuration for load beacon must '
-                               'be a dictionary of dictionaries.')
+
+        _config = {}
+        list(map(_config.update, config))
+
+        for item in _config.get('interfaces', {}):
+            if not isinstance(_config['interfaces'][item], dict):
+                return False, ('Configuration for network_info beacon must '
+                               'be a list of dictionaries.')
             else:
-                if not any(j in VALID_ITEMS for j in config[item]):
+                if not any(j in VALID_ITEMS for j in _config['interfaces'][item]):
                     return False, ('Invalid configuration item in '
                                    'Beacon configuration.')
     return True, 'Valid beacon configuration'
@@ -86,16 +90,17 @@ def beacon(config):
 
         beacons:
           network_info:
-            - eth0:
-                type: equal
-                bytes_sent: 100000
-                bytes_recv: 100000
-                packets_sent: 100000
-                packets_recv: 100000
-                errin: 100
-                errout: 100
-                dropin: 100
-                dropout: 100
+            - interfaces:
+                - eth0:
+                    type: equal
+                    bytes_sent: 100000
+                    bytes_recv: 100000
+                    packets_sent: 100000
+                    packets_recv: 100000
+                    errin: 100
+                    errout: 100
+                    dropin: 100
+                    dropout: 100
 
     Emit beacon when any values are greater
     than configured values.
@@ -104,46 +109,53 @@ def beacon(config):
 
         beacons:
           network_info:
-            - eth0:
-                type: greater
-                bytes_sent: 100000
-                bytes_recv: 100000
-                packets_sent: 100000
-                packets_recv: 100000
-                errin: 100
-                errout: 100
-                dropin: 100
-                dropout: 100
+            - interfaces:
+                - eth0:
+                    type: greater
+                    bytes_sent: 100000
+                    bytes_recv: 100000
+                    packets_sent: 100000
+                    packets_recv: 100000
+                    errin: 100
+                    errout: 100
+                    dropin: 100
+                    dropout: 100
 
 
     '''
     ret = []
 
+    _config = {}
+    list(map(_config.update, config))
+
+    log.debug('psutil.net_io_counters {}'.format(psutil.net_io_counters))
+
     _stats = psutil.net_io_counters(pernic=True)
 
-    for interface_config in config:
-        interface = interface_config.keys()[0]
+    log.debug('_stats {}'.format(_stats))
+    for interface in _config.get('interfaces', {}):
         if interface in _stats:
+            interface_config = _config['interfaces'][interface]
             _if_stats = _stats[interface]
             _diff = False
             for attr in __attrs:
-                if attr in interface_config[interface]:
-                    if 'type' in interface_config[interface] and \
-                            interface_config[interface]['type'] == 'equal':
+                if attr in interface_config:
+                    if 'type' in interface_config and \
+                            interface_config['type'] == 'equal':
                         if getattr(_if_stats, attr, None) == \
-                                int(interface_config[interface][attr]):
+                                int(interface_config[attr]):
                             _diff = True
-                    elif 'type' in interface_config[interface] and \
-                            interface_config[interface]['type'] == 'greater':
+                    elif 'type' in interface_config and \
+                            interface_config['type'] == 'greater':
                         if getattr(_if_stats, attr, None) > \
-                                int(interface_config[interface][attr]):
+                                int(interface_config[attr]):
                             _diff = True
                         else:
                             log.debug('attr {}'.format(getattr(_if_stats,
                                                                attr, None)))
                     else:
                         if getattr(_if_stats, attr, None) == \
-                                int(interface_config[interface][attr]):
+                                int(interface_config[attr]):
                             _diff = True
             if _diff:
                 ret.append({'interface': interface,
