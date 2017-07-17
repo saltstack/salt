ed54a341b07431950e8db7b677db556eb9427ba9
Mircea Ulinic <mirucha@cloudflare.com>
2017-07-17 11:12:31 +0000

    Improve opts and provide doc

--- salt/modules/textfsm_mod.py
+++ salt/modules/textfsm_mod.py
@@ -22,6 +22,7 @@ try:
     HAS_TEXTFSM = True
 except ImportError:
     HAS_TEXTFSM = False
+
 try:
     import clitable
     HAS_CLITABLE = True
@@ -92,13 +93,15 @@ def extract(template_path, raw_text=None, raw_text_file=None, saltenv='base'):
 
     .. code-block:: bash
 
-        salt '*' textfsm.extract salt://junos_ver.textfsm raw_text_file=s3://junos_ver.txt
-        salt '*' textfsm.extract http://junos_ver.textfsm raw_text='Hostname: router.abc ... snip ...'
+        salt '*' textfsm.extract salt://textfsm/juniper_version_template raw_text_file=s3://junos_ver.txt
+        salt '*' textfsm.extract http://some-server/textfsm/juniper_version_template raw_text='Hostname: router.abc ... snip ...'
+
+    Jinja template example:
 
     .. code-block:: jinja
 
         {%- set raw_text = 'Hostname: router.abc ... snip ...' -%}
-        {%- set textfsm_extract = salt.textfsm.extract('https://junos_ver.textfsm', raw_text) -%}
+        {%- set textfsm_extract = salt.textfsm.extract('https://some-server/textfsm/juniper_version_template', raw_text) -%}
 
     Raw text example:
 
@@ -203,7 +206,9 @@ def extract(template_path, raw_text=None, raw_text_file=None, saltenv='base'):
 
 
 def index(command,
-          platform,
+          platform=None,
+          platform_grain_name=None,
+          platform_column_name=None,
           output=None,
           output_file=None,
           textfsm_path=None,
@@ -212,6 +217,147 @@ def index(command,
           include_empty=False,
           include_pat=None,
           exclude_pat=None):
+    '''
+    Dynamically identify the template required to extract the
+    information from the unstructured raw text.
+
+    The output has the same structure as the ``extract`` execution
+    function, the difference being that ``index`` is capable
+    to identify what template to use, based on the platform
+    details and the ``command``.
+
+    command
+        The command executed on the device, to get the output.
+
+    platform
+        The platform name, as defined in the TextFSM index file.
+
+        .. note::
+            For ease of use, it is recommended to define the TextFSM
+            indexfile with values that can be matches using the grains.
+
+    platform_grain_name
+        The name of the grain used to identify the platform name
+        in the TextFSM index file.
+
+        .. note::
+            This option can be also specified in the minion configuration
+            file or pillar as ``textfsm_platform_grain``.
+
+        .. note::
+            This option is ignored when ``platform`` is specified.
+
+    platform_column_name: ``Platform``
+        The column name used to identify the platform,
+        exactly as specified in the TextFSM index file.
+        Default: ``Platform``.
+
+        .. note::
+            This is field is case sensitive, make sure
+            to assign the correct value to this option,
+            exactly as defined in the index file.
+
+        .. note::
+            This option can be also specified in the minion configuration
+            file or pillar as ``textfsm_platform_column_name``.
+
+    output
+        The raw output from the device, to be parsed
+        and extract the structured data.
+
+    output_file
+        The path to a file that contains the raw output from the device,
+        used to extract the structured data.
+        This option supports the usual Salt-specific schemes: ``file://``,
+        ``salt://``, ``http://``, ``https://``, ``ftp://``, ``s3://``, ``swift://``.
+
+    textfsm_path
+        The path where the TextFSM templates can be found. This can be either
+        absolute path on the server, either specified using the following URL
+        schemes: ``file://``, ``salt://``, ``http://``, ``https://``, ``ftp://``,
+        ``s3://``, ``swift://``.
+
+        .. note::
+            This needs to be a directory with a flat structure, having an
+            index file (whose name can be specified using the ``index_file`` option)
+            and a number of TextFSM templates.
+
+        .. note::
+            This option can be also specified in the minion configuration
+            file or pillar as ``textfsm_path``.
+
+    index_file: ``index``
+        The name of the TextFSM index file, under the ``textfsm_path``. Default: ``index``.
+
+        .. note::
+            This option can be also specified in the minion configuration
+            file or pillar as ``textfsm_index_file``.
+
+    saltenv: ``base``
+        Salt fileserver envrionment from which to retrieve the file.
+        Ignored if ``textfsm_path`` is not a ``salt://`` URL.
+
+    include_empty: ``False``
+        Include empty files under the ``textfsm_path``.
+
+    include_pat
+        Glob or regex to narrow down the files cached from the given path.
+        If matching with a regex, the regex must be prefixed with ``E@``,
+        otherwise the expression will be interpreted as a glob.
+
+    exclude_pat
+        Glob or regex to exclude certain files from being cached from the given path.
+        If matching with a regex, the regex must be prefixed with ``E@``,
+        otherwise the expression will be interpreted as a glob.
+
+        .. note::
+            If used with ``include_pat``, files matching this pattern will be
+            excluded from the subset of files defined by ``include_pat``.
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' textfsm.index 'sh ver' platform=Juniper output_file=salt://textfsm/juniper_version_example textfsm_path=salt://textfsm/
+        salt '*' textfsm.index 'sh ver' output_file=salt://textfsm/juniper_version_example textfsm_path=ftp://textfsm/ platform_textfsm_key=Vendor
+        salt '*' textfsm.index 'sh ver' output_file=salt://textfsm/juniper_version_example textfsm_path=https://some-server/textfsm/ platform_textfsm_key=Vendor platform_grain_name=vendor
+
+    TextFSM index file example:
+
+    ``salt://textfsm/index``
+
+    .. code-block:: text
+
+        Template, Hostname, Vendor, Command
+        juniper_version_template, .*, Juniper, sh[[ow]] ve[[rsion]]
+
+    The usage can be simplified,
+    by defining (some of) the following options: ``textfsm_platform_grain``,
+    ``textfsm_path``, ``textfsm_platform_column_name``, or ``textfsm_index_file``,
+    in the (proxy) minion configuration file or pillar.
+
+    Configuration example:
+
+    .. code-block:: yaml
+
+        textfsm_platform_grain: vendor
+        textfsm_path: salt://textfsm/
+        textfsm_platform_column_name: Vendor
+
+    And the CLI usage becomes as simple as:
+
+    .. code-block:: bash
+
+        salt '*' textfsm.index 'sh ver' output_file=salt://textfsm/juniper_version_example
+
+    Usgae inside a Jinja template:
+
+    .. code-block:: jinja
+
+        {%- set command = 'sh ver' -%}
+        {%- set output = salt.net.cli(command) -%}
+        {%- set textfsm_extract = salt.textfsm.index(command, output=output) -%}
+    '''
     ret = {
         'out': None,
         'result': False,
@@ -221,6 +367,20 @@ def index(command,
         ret['comment'] = 'TextFSM doesnt seem that has clitable embedded.'
         log.error(ret['comment'])
         return ret
+    if not platform:
+        platform_grain_name = __opts__.get('textfsm_platform_grain') or\
+                              __pillar__.get('textfsm_platform_grain', platform_grain_name)
+        if platform_grain_name:
+            log.debug('Using the {} grain to identify the platform name'.format(platform_grain_name))
+            platform = __grains__.get(platform_grain_name)
+            if not platform:
+                ret['comment'] = 'Unable to identify the platform name using the {} grain.'.format(platform_grain_name)
+                return ret
+            log.info('Using platform: {}'.format(platform))
+        else:
+            ret['comment'] = 'No platform specified, no platform grain identifier configured.'
+            log.error(ret['comment'])
+            return ret
     if not textfsm_path:
         log.debug('No TextFSM templates path specified, trying to look into the opts and pillar')
         textfsm_path = __opts__.get('textfsm_path') or __pillar__.get('textfsm_path')
@@ -242,16 +402,19 @@ def index(command,
         log.error(ret['comment'])
         return ret
     textfsm_cachedir = os.path.dirname(textfsm_cachedir_ret[0])  # first item
-    index_file = __opts__.get('textfsm_index', 'index')
+    index_file = __opts__.get('textfsm_index_file') or __pillar__.get('textfsm_index_file', 'index')
     index_file_path = os.path.join(textfsm_cachedir, index_file)
     log.debug('Using the cached index file: {}'.format(index_file_path))
     log.debug('TextFSM templates cached under: {}'.format(textfsm_cachedir))
     textfsm_obj = clitable.CliTable(index_file_path, textfsm_cachedir)
     attrs = {
-        'Command': command,
-        'Platform': platform
+        'Command': command
     }
-    log.debug('Command: {Command}, Platform: {Platform}'.format(**attrs))
+    platform_column_name = __opts__.get('textfsm_platform_column_name') or\
+                           __pillar__.get('textfsm_platform_column_name'. 'Platform')
+    log.info('Using the TextFSM platform idenfiticator: {}'.format(platform_column_name))
+    attrs[platform_column_name] = platform
+    log.debug('Processing the TextFSM index file using the attributes: {}'.format(attrs))
     if not output and output_file:
         log.debug('Processing the output from {}'.format(output_file))
         output = __salt__['cp.get_file_str'](output_file, saltenv=saltenv)
@@ -272,5 +435,5 @@ def index(command,
         ret['result'] = True
     except clitable.CliTableError as cterr:
         log.error('Unable to proces the CliTable', exc_info=True)
-        ret['comment'] = 'Unable to process the output through the CliTable. Please see logs for more details.'
+        ret['comment'] = 'Unable to process the output: {}'.format(cterr)
     return ret
