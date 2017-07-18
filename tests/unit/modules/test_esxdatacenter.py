e041b8f486e6cb17006de940aa46dc2df91fbec7
Alexandru Bleotu <Alexandru.Bleotu@morganstanley.com>
2017-07-18 17:22:07 +0100

    Added tests for get_details in modules.esxdatacenter

---
+++ tests/unit/modules/test_esxdatacenter.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+'''
+    :codeauthor: :email:`Alexandru Bleotu <alexandru.bleotu@morganstanley.com>`
+
+    Tests for functions in salt.modules.esxdatacenter
+'''
+
+# Import Python Libs
+from __future__ import absolute_import
+
+# Import Salt Libs
+import salt.modules.esxdatacenter as esxdatacenter
+
+# Import Salt Testing Libs
+from tests.support.mixins import LoaderModuleMockMixin
+from tests.support.unit import TestCase, skipIf
+from tests.support.mock import (
+    MagicMock,
+    patch,
+    NO_MOCK,
+    NO_MOCK_REASON
+)
+
+
+# Globals
+esxdatacenter.__proxy__ = {}
+
+@skipIf(NO_MOCK, NO_MOCK_REASON)
+class GetDetailsTestCase(TestCase, LoaderModuleMockMixin):
+    '''Tests for salt.modules.esxdatacenter.get_details'''
+    def setup_loader_modules(self):
+        return {esxdatacenter: {'__virtual__':
+                                MagicMock(return_value='esxdatacenter')}}
+
+    def test_get_details(self):
+        mock_get_details = MagicMock()
+        with patch.dict(esxdatacenter.__proxy__,
+                        {'esxdatacenter.get_details': mock_get_details}):
+            esxdatacenter.get_details()
+        mock_get_details.assert_called_once_with()
