bffaa966b9dd1c140d325fb1b9ad5f0a823a1115
William Cannon <william@hospitalitypulse.com>
2017-07-26 15:00:15 -0700

    added auto check of minion config option

--- tests/unit/modules/test_saltcheck.py
+++ tests/unit/modules/test_saltcheck.py
@@ -23,7 +23,8 @@ class SaltCheckTestCase(TestCase):
 
     def test_call_salt_command(self):
         with patch.dict(saltcheck.__salt__, {'config.get': MagicMock(return_value=True),
-                                             'sys.list_modules': MagicMock(return_value=['module1'])
+                                             'sys.list_modules': MagicMock(return_value=['module1']),
+                                             'cp.cache_master': MagicMock(return_value=[True])
                                             }):
             sc = saltcheck.SaltCheck()
             returned = sc.call_salt_command(fun="test.echo", args=['hello'], kwargs=None)
@@ -31,185 +32,261 @@ class SaltCheckTestCase(TestCase):
 
     def test_call_salt_command2(self):
         with patch.dict(saltcheck.__salt__, {'config.get': MagicMock(return_value=True),
-                                             'sys.list_modules': MagicMock(return_value=['module1'])
+                                             'sys.list_modules': MagicMock(return_value=['module1']),
+                                             'cp.cache_master': MagicMock(return_value=[True])
                                             }):
-        #with patch.dict(saltcheck.__salt__, {'sys.list_modules': MagicMock(return_value=['module1'])}):
             sc = saltcheck.SaltCheck()
             returned = sc.call_salt_command(fun="test.echo", args=['hello'], kwargs=None)
             self.assertNotEqual(returned, 'not-hello')
 
 
     def test__assert_equal1(self):
-        sc = saltcheck.SaltCheck()
-        a = {'a': 1, 'b': 2}
-        b = {'a': 1, 'b': 2}
-        mybool = sc._SaltCheck__assert_equal(a, b)
-        self.assertTrue(mybool)
+        with patch.dict(saltcheck.__salt__, {'config.get': MagicMock(return_value=True),
+                                             'cp.cache_master': MagicMock(return_value=[True])
+                                            }):
+            sc = saltcheck.SaltCheck()
+            a = {'a': 1, 'b': 2}
+            b = {'a': 1, 'b': 2}
+            mybool = sc._SaltCheck__assert_equal(a, b)
+            self.assertTrue(mybool)
 
     def test__assert_equal2(self):
-        sc = saltcheck.SaltCheck()
-        a = {'a': 1, 'b': 2}
-        b = {'a': 1, 'b': 2, 'c': 3}
-        mybool = sc._SaltCheck__assert_equal(False, True)
-        self.assertNotEqual(mybool, True)
+        with patch.dict(saltcheck.__salt__, {'config.get': MagicMock(return_value=True),
+                                             'cp.cache_master': MagicMock(return_value=[True])
+                                            }):
+            sc = saltcheck.SaltCheck()
+            a = {'a': 1, 'b': 2}
+            b = {'a': 1, 'b': 2, 'c': 3}
+            mybool = sc._SaltCheck__assert_equal(False, True)
+            self.assertNotEqual(mybool, True)
 
     def test__assert_not_equal1(self):
-        sc = saltcheck.SaltCheck()
-        a = {'a': 1, 'b': 2}
-        b = {'a': 1, 'b': 2, 'c': 3}
-        mybool = sc._SaltCheck__assert_not_equal(a, b)
-        self.assertTrue(mybool)
+        with patch.dict(saltcheck.__salt__, {'config.get': MagicMock(return_value=True),
+                                             'cp.cache_master': MagicMock(return_value=[True])
+                                            }):
+            sc = saltcheck.SaltCheck()
+            a = {'a': 1, 'b': 2}
+            b = {'a': 1, 'b': 2, 'c': 3}
+            mybool = sc._SaltCheck__assert_not_equal(a, b)
+            self.assertTrue(mybool)
 
     def test__assert_not_equal2(self):
-        sc = saltcheck.SaltCheck()
-        a = {'a': 1, 'b': 2}
-        b = {'a': 1, 'b': 2}
-        mybool = sc._SaltCheck__assert_not_equal(a, b)
-        self.assertNotEqual(mybool, True)
+        with patch.dict(saltcheck.__salt__, {'config.get': MagicMock(return_value=True),
+                                             'cp.cache_master': MagicMock(return_value=[True])
+                                            }):
+            sc = saltcheck.SaltCheck()
+            a = {'a': 1, 'b': 2}
+            b = {'a': 1, 'b': 2}
+            mybool = sc._SaltCheck__assert_not_equal(a, b)
+            self.assertNotEqual(mybool, True)
 
     def test__assert_true1(self):
-        sc = saltcheck.SaltCheck()
-        mybool = sc._SaltCheck__assert_equal(True, True)
-        self.assertTrue(mybool)
+        with patch.dict(saltcheck.__salt__, {'config.get': MagicMock(return_value=True),
+                                             'cp.cache_master': MagicMock(return_value=[True])
+                                            }):
+            sc = saltcheck.SaltCheck()
+            mybool = sc._SaltCheck__assert_equal(True, True)
+            self.assertTrue(mybool)
 
     def test__assert_true2(self):
-        sc = saltcheck.SaltCheck()
-        mybool = sc._SaltCheck__assert_equal(False, True)
-        self.assertNotEqual(mybool, True)
+        with patch.dict(saltcheck.__salt__, {'config.get': MagicMock(return_value=True),
+                                             'cp.cache_master': MagicMock(return_value=[True])
+                                            }):
+            sc = saltcheck.SaltCheck()
+            mybool = sc._SaltCheck__assert_equal(False, True)
+            self.assertNotEqual(mybool, True)
 
     def test__assert_false1(self):
-        sc = saltcheck.SaltCheck()
-        mybool = sc._SaltCheck__assert_false(False)
-        self.assertTrue(mybool)
+        with patch.dict(saltcheck.__salt__, {'config.get': MagicMock(return_value=True),
+                                             'cp.cache_master': MagicMock(return_value=[True])
+                                            }):
+            sc = saltcheck.SaltCheck()
+            mybool = sc._SaltCheck__assert_false(False)
+            self.assertTrue(mybool)
 
     def test__assert_false2(self):
-        sc = saltcheck.SaltCheck()
-        mybool = sc._SaltCheck__assert_false(True)
-        self.assertNotEqual(mybool, True)
+        with patch.dict(saltcheck.__salt__, {'config.get': MagicMock(return_value=True),
+                                             'cp.cache_master': MagicMock(return_value=[True])
+                                            }):
+            sc = saltcheck.SaltCheck()
+            mybool = sc._SaltCheck__assert_false(True)
+            self.assertNotEqual(mybool, True)
 
     def test__assert_in1(self):
-        sc = saltcheck.SaltCheck()
-        a = "bob"
-        mylist = ['alice', 'bob', 'charles', 'dana']
-        mybool = sc._SaltCheck__assert_in(a, mylist)
-        self.assertTrue(mybool, True)
+        with patch.dict(saltcheck.__salt__, {'config.get': MagicMock(return_value=True),
+                                             'cp.cache_master': MagicMock(return_value=[True])
+                                            }):
+            sc = saltcheck.SaltCheck()
+            a = "bob"
+            mylist = ['alice', 'bob', 'charles', 'dana']
+            mybool = sc._SaltCheck__assert_in(a, mylist)
+            self.assertTrue(mybool, True)
 
     def test__assert_in2(self):
-        sc = saltcheck.SaltCheck()
-        a = "elaine"
-        mylist = ['alice', 'bob', 'charles', 'dana']
-        mybool = sc._SaltCheck__assert_in(a, mylist)
-        self.assertNotEqual(mybool, True)
+        with patch.dict(saltcheck.__salt__, {'config.get': MagicMock(return_value=True),
+                                             'cp.cache_master': MagicMock(return_value=[True])
+                                            }):
+            sc = saltcheck.SaltCheck()
+            a = "elaine"
+            mylist = ['alice', 'bob', 'charles', 'dana']
+            mybool = sc._SaltCheck__assert_in(a, mylist)
+            self.assertNotEqual(mybool, True)
 
     def test__assert_not_in1(self):
-        sc = saltcheck.SaltCheck()
-        a = "elaine"
-        mylist = ['alice', 'bob', 'charles', 'dana']
-        mybool = sc._SaltCheck__assert_not_in(a, mylist)
-        self.assertTrue(mybool, True)
+        with patch.dict(saltcheck.__salt__, {'config.get': MagicMock(return_value=True),
+                                             'cp.cache_master': MagicMock(return_value=[True])
+                                            }):
+            sc = saltcheck.SaltCheck()
+            a = "elaine"
+            mylist = ['alice', 'bob', 'charles', 'dana']
+            mybool = sc._SaltCheck__assert_not_in(a, mylist)
+            self.assertTrue(mybool, True)
 
     def test__assert_not_in2(self):
-        sc = saltcheck.SaltCheck()
-        a = "bob"
-        mylist = ['alice', 'bob', 'charles', 'dana']
-        mybool = sc._SaltCheck__assert_not_in(a, mylist)
-        self.assertNotEqual(mybool, True)
+        with patch.dict(saltcheck.__salt__, {'config.get': MagicMock(return_value=True),
+                                             'cp.cache_master': MagicMock(return_value=[True])
+                                            }):
+            sc = saltcheck.SaltCheck()
+            a = "bob"
+            mylist = ['alice', 'bob', 'charles', 'dana']
+            mybool = sc._SaltCheck__assert_not_in(a, mylist)
+            self.assertNotEqual(mybool, True)
 
     def test__assert_greater1(self):
-        sc = saltcheck.SaltCheck()
-        a = 110
-        b = 100
-        mybool = sc._SaltCheck__assert_greater(a, b)
-        self.assertTrue(mybool, True)
+        with patch.dict(saltcheck.__salt__, {'config.get': MagicMock(return_value=True),
+                                             'cp.cache_master': MagicMock(return_value=[True])
+                                            }):
+            sc = saltcheck.SaltCheck()
+            a = 110
+            b = 100
+            mybool = sc._SaltCheck__assert_greater(a, b)
+            self.assertTrue(mybool, True)
 
     def test__assert_greater2(self):
-        sc = saltcheck.SaltCheck()
-        a = 100
-        b = 110
-        mybool = sc._SaltCheck__assert_greater(a, b)
-        self.assertNotEqual(mybool, True)
+        with patch.dict(saltcheck.__salt__, {'config.get': MagicMock(return_value=True),
+                                             'cp.cache_master': MagicMock(return_value=[True])
+                                            }):
+            sc = saltcheck.SaltCheck()
+            a = 100
+            b = 110
+            mybool = sc._SaltCheck__assert_greater(a, b)
+            self.assertNotEqual(mybool, True)
 
     def test__assert_greater3(self):
-        sc = saltcheck.SaltCheck()
-        a = 100
-        b = 100
-        mybool = sc._SaltCheck__assert_greater(a, b)
-        self.assertNotEqual(mybool, True)
+        with patch.dict(saltcheck.__salt__, {'config.get': MagicMock(return_value=True),
+                                             'cp.cache_master': MagicMock(return_value=[True])
+                                            }):
+            sc = saltcheck.SaltCheck()
+            a = 100
+            b = 100
+            mybool = sc._SaltCheck__assert_greater(a, b)
+            self.assertNotEqual(mybool, True)
 
     def test__assert_greater_equal_equal1(self):
-        sc = saltcheck.SaltCheck()
-        a = 110
-        b = 100
-        mybool = sc._SaltCheck__assert_greater_equal(a, b)
-        self.assertTrue(mybool, True)
+        with patch.dict(saltcheck.__salt__, {'config.get': MagicMock(return_value=True),
+                                             'cp.cache_master': MagicMock(return_value=[True])
+                                            }):
+            sc = saltcheck.SaltCheck()
+            a = 110
+            b = 100
+            mybool = sc._SaltCheck__assert_greater_equal(a, b)
+            self.assertTrue(mybool, True)
 
     def test__assert_greater_equal2(self):
-        sc = saltcheck.SaltCheck()
-        a = 100
-        b = 110
-        mybool = sc._SaltCheck__assert_greater_equal(a, b)
-        self.assertNotEqual(mybool, True)
+        with patch.dict(saltcheck.__salt__, {'config.get': MagicMock(return_value=True),
+                                             'cp.cache_master': MagicMock(return_value=[True])
+                                            }):
+            sc = saltcheck.SaltCheck()
+            a = 100
+            b = 110
+            mybool = sc._SaltCheck__assert_greater_equal(a, b)
+            self.assertNotEqual(mybool, True)
 
     def test__assert_greater_equal3(self):
-        sc = saltcheck.SaltCheck()
-        a = 100
-        b = 100
-        mybool = sc._SaltCheck__assert_greater_equal(a, b)
-        self.assertEqual(mybool, True)
+        with patch.dict(saltcheck.__salt__, {'config.get': MagicMock(return_value=True),
+                                             'cp.cache_master': MagicMock(return_value=[True])
+                                            }):
+            sc = saltcheck.SaltCheck()
+            a = 100
+            b = 100
+            mybool = sc._SaltCheck__assert_greater_equal(a, b)
+            self.assertEqual(mybool, True)
 
     def test__assert_less1(self):
-        sc = saltcheck.SaltCheck()
-        a = 99
-        b = 100
-        mybool = sc._SaltCheck__assert_less(a, b)
-        self.assertTrue(mybool, True)
+        with patch.dict(saltcheck.__salt__, {'config.get': MagicMock(return_value=True),
+                                             'cp.cache_master': MagicMock(return_value=[True])
+                                            }):
+            sc = saltcheck.SaltCheck()
+            a = 99
+            b = 100
+            mybool = sc._SaltCheck__assert_less(a, b)
+            self.assertTrue(mybool, True)
 
     def test__assert_less2(self):
-        sc = saltcheck.SaltCheck()
-        a = 110
-        b = 99
-        mybool = sc._SaltCheck__assert_less(a, b)
-        self.assertNotEqual(mybool, True)
+        with patch.dict(saltcheck.__salt__, {'config.get': MagicMock(return_value=True),
+                                             'cp.cache_master': MagicMock(return_value=[True])
+                                            }):
+            sc = saltcheck.SaltCheck()
+            a = 110
+            b = 99
+            mybool = sc._SaltCheck__assert_less(a, b)
+            self.assertNotEqual(mybool, True)
 
     def test__assert_less3(self):
-        sc = saltcheck.SaltCheck()
-        a = 100
-        b = 100
-        mybool = sc._SaltCheck__assert_less(a, b)
-        self.assertNotEqual(mybool, True)
+        with patch.dict(saltcheck.__salt__, {'config.get': MagicMock(return_value=True),
+                                             'cp.cache_master': MagicMock(return_value=[True])
+                                            }):
+            sc = saltcheck.SaltCheck()
+            a = 100
+            b = 100
+            mybool = sc._SaltCheck__assert_less(a, b)
+            self.assertNotEqual(mybool, True)
 
     def test__assert_less_equal1(self):
-        sc = saltcheck.SaltCheck()
-        a = 99
-        b = 100
-        mybool = sc._SaltCheck__assert_less_equal(a, b)
-        self.assertTrue(mybool, True)
+        with patch.dict(saltcheck.__salt__, {'config.get': MagicMock(return_value=True),
+                                             'cp.cache_master': MagicMock(return_value=[True])
+                                            }):
+            sc = saltcheck.SaltCheck()
+            a = 99
+            b = 100
+            mybool = sc._SaltCheck__assert_less_equal(a, b)
+            self.assertTrue(mybool, True)
 
     def test__assert_less_equal2(self):
-        sc = saltcheck.SaltCheck()
-        a = 110
-        b = 99
-        mybool = sc._SaltCheck__assert_less_equal(a, b)
-        self.assertNotEqual(mybool, True)
+        with patch.dict(saltcheck.__salt__, {'config.get': MagicMock(return_value=True),
+                                             'cp.cache_master': MagicMock(return_value=[True])
+                                            }):
+            sc = saltcheck.SaltCheck()
+            a = 110
+            b = 99
+            mybool = sc._SaltCheck__assert_less_equal(a, b)
+            self.assertNotEqual(mybool, True)
 
     def test__assert_less_equal3(self):
-        sc = saltcheck.SaltCheck()
-        a = 100
-        b = 100
-        mybool = sc._SaltCheck__assert_less_equal(a, b)
-        self.assertEqual(mybool, True)
-    
+        with patch.dict(saltcheck.__salt__, {'config.get': MagicMock(return_value=True),
+                                             'cp.cache_master': MagicMock(return_value=[True])
+                                            }):
+            sc = saltcheck.SaltCheck()
+            a = 100
+            b = 100
+            mybool = sc._SaltCheck__assert_less_equal(a, b)
+            self.assertEqual(mybool, True)
 
-    ####################################################
-    #def test__is_valid_test(self):
-    #    my_dict = {'module_and_function': 'test.echo',
-    #                 'assertion': 'assertEqual',
-    #                 'expected-return': 'True'}
-    #    sc = saltcheck.SaltCheck()
-    #    mybool = sc.__is_valid_test(my_dict)
-    #    self.assertTrue(mybool)
-        
-    #def test_is_valid_module(self):
-    #    sc = saltcheck.SaltCheck()
-    #    returned = sc.is_valid_module('test')
-    #    self.assertTrue(returned)
+    def test_run_test_1(self):
+        with patch.dict(saltcheck.__salt__, {'config.get': MagicMock(return_value=True),
+                                             'sys.list_modules': MagicMock(return_value=['test']),
+                                             'sys.list_functions': MagicMock(return_value=['test.echo']),
+                                             'cp.cache_master': MagicMock(return_value=[True])
+                                            }):
+            returned = saltcheck.run_test(test={"module_and_function": "test.echo", "assertion": "assertEqual", "expected-return": "This works!", "args":["This works!"] })
+            self.assertEqual(returned, True)
+
+    # pillar injection not supported yet 
+    #def test_run_test_2(self):
+    #    with patch.dict(saltcheck.__salt__, {'config.get': MagicMock(return_value=True),
+    #                                         'sys.list_modules': MagicMock(return_value=['pillar']),
+    #                                         'sys.list_functions': MagicMock(return_value=['pillar.get'])
+    #                                        }):
+    #        returned = saltcheck.run_test(test={"module_and_function": "pillar.get", "pillar-data": "mykey:myvalue", "assertion": "assertEqual", "expected-return": "myvalue", "args":["mykey"] })
+    #        self.assertEqual(returned, True)
+
