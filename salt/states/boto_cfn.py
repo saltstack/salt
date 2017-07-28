3ace3ae8cfe464c8c29754583e2dfbd2c29ccd16
Slawomir Bojarski <Slawomir-Bojarski@idexx.com>
2017-07-28 12:57:10 -0400

    Fix debug message in "boto_cfn._validate" function.

--- salt/states/boto_cfn.py
+++ salt/states/boto_cfn.py
@@ -254,7 +254,7 @@ def _get_template(template, name):
 def _validate(template_body=None, template_url=None, region=None, key=None, keyid=None, profile=None):
     # Validates template. returns true if template syntax is correct.
     validate = __salt__['boto_cfn.validate_template'](template_body, template_url, region, key, keyid, profile)
-    log.debug('Validate is result is {0}.'.format(str(validate)))
+    log.debug('Validate result is {0}.'.format(str(validate)))
     if isinstance(validate, str):
         code, message = _get_error(validate)
         log.debug('Validate error is {0} and message is {1}.'.format(code, message))
