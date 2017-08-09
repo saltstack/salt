bf9c1048dc75321e1398eae0fe9ff28b0d60cd76
spenceation <spenceation@hotmail.com>
2017-08-09 15:13:38 -0400

    Update panos.py

--- salt/grains/panos.py
+++ salt/grains/panos.py
@@ -1,1 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Generate baseline proxy minion grains for panos hosts.
 
+"""
+
+# Import Python Libs
+from __future__ import absolute_import
+import logging
+
+# Import Salt Libs
+import salt.utils
+import salt.proxy.panos
+
+__proxyenabled__ = ['panos']
+__virtualname__ = 'panos'
+
+log = logging.getLogger(__file__)
+
+GRAINS_CACHE = {'os_family': 'panos'}
+
+
+def __virtual__():
+    try:
+        if salt.utils.is_proxy() and __opts__['proxy']['proxytype'] == 'panos':
+            return __virtualname__
+    except KeyError:
+        pass
+
+    return False
+
+
+def panos(proxy=None):
+    if proxy is None:
+        return {}
+    if proxy['panos.initialized']() is False:
+        return {}
+    return {'panos': proxy['panos.grains']()}
