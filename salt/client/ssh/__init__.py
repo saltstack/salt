d96c771a880fea622d5af1b1ebf5a245c029c71c
Bo Maryniuk <bo@suse.de>
2017-08-09 18:07:55 +0200

    Fix PEP8: length of the string

--- salt/client/ssh/__init__.py
+++ salt/client/ssh/__init__.py
@@ -230,7 +230,8 @@ class SSH(object):
         if self.opts['regen_thin']:
             self.opts['ssh_wipe'] = True
         if not salt.utils.which('ssh'):
-            raise salt.exceptions.SaltSystemExit('No ssh binary found in path -- ssh must be installed for salt-ssh to run. Exiting.')
+            raise salt.exceptions.SaltSystemExit('No ssh binary found in path -- ssh must be '
+                                                 'installed for salt-ssh to run. Exiting.')
         self.opts['_ssh_version'] = ssh_version()
         self.tgt_type = self.opts['selected_target_option'] \
             if self.opts['selected_target_option'] else 'glob'
