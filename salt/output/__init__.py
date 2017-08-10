6ad45c242e75510dcf1e23273cf11e941101dbb2
Erik Johnson <palehose@gmail.com>
2017-08-10 10:27:40 -0500

    Fix references to functions moved to salt.utils.platform
    These refs all were added from a branch point before #42572. This commit
    updates them to avoid a deprecation warning.

--- salt/output/__init__.py
+++ salt/output/__init__.py
@@ -174,7 +174,7 @@ def get_printout(out, opts=None, **kwargs):
     else:
         if opts.get('force_color', False):
             opts['color'] = True
-        elif opts.get('no_color', False) or salt.utils.is_windows():
+        elif opts.get('no_color', False) or salt.utils.platform.is_windows():
             opts['color'] = False
         else:
             pass
