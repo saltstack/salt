2728e5d9777dd15c6d716d3e2d13707c9ba2d2ec
Steven Joseph <steven@stevenjoseph.in>
2017-08-11 14:59:57 +1000

    Only include new tags in changes

--- salt/states/git.py
+++ salt/states/git.py
@@ -1322,10 +1322,9 @@ def latest(name,
                 ])
                 if set(all_local_tags) != remote_tags:
                     has_remote_rev = False
-                    ret['changes']['tags'] = {
-                        'old': all_local_tags,
-                        'new': list(remote_tags)
-                    }
+                    ret['changes']['new_tags'] = remote_tags.symmetric_difference(
+                        all_local_tags
+                    )
 
                 if not has_remote_rev:
                     try:
