5c4a5ca59a3131703f5e2633d640573369081bb9
Adam Mendlik <amendlik@gmail.com>
2017-08-15 15:11:02 -0600

    Add kernelpkg.remove function for YUM-based systems

--- tests/unit/modules/test_kernelpkg_linux_yum.py
+++ tests/unit/modules/test_kernelpkg_linux_yum.py
@@ -19,6 +19,7 @@ try:
     from tests.unit.modules.test_kernelpkg import KernelPkgTestCase
     import salt.modules.kernelpkg_linux_yum as kernelpkg
     import salt.modules.yumpkg as pkg
+    from salt.exceptions import CommandExecutionError
     HAS_MODULES = True
 except ImportError:
     HAS_MODULES = False
@@ -32,18 +33,22 @@ class YumKernelPkgTestCase(KernelPkgTestCase, TestCase, LoaderModuleMockMixin):
     KERNEL_LIST = ['3.10.0-327.el7', '3.11.0-327.el7', '4.9.1-100.el7']
     LATEST = KERNEL_LIST[-1]
     OS_ARCH = 'x86_64'
+    OS_NAME = 'RedHat'
 
     def setup_loader_modules(self):
         return {
             kernelpkg: {
                 '__grains__': {
+                    'os': self.OS_NAME,
                     'kernelrelease': '{0}.{1}'.format(self.KERNEL_LIST[0], self.OS_ARCH)
                 },
                 '__salt__': {
                     'pkg.normalize_name': pkg.normalize_name,
                     'pkg.upgrade': MagicMock(return_value={}),
+                    'pkg.list_pkgs': MagicMock(return_value={}),
                     'pkg.version': MagicMock(return_value=self.KERNEL_LIST),
-                    'system.reboot': MagicMock(return_value=None)
+                    'system.reboot': MagicMock(return_value=None),
+                    'config.get': MagicMock(return_value=True)
                 }
             },
             pkg: {
@@ -68,3 +73,28 @@ class YumKernelPkgTestCase(KernelPkgTestCase, TestCase, LoaderModuleMockMixin):
         mock = MagicMock(return_value=None)
         with patch.dict(self._kernelpkg.__salt__, {'pkg.version': mock}):
             self.assertListEqual(self._kernelpkg.list_installed(), [])
+
+    def test_remove_success(self):
+        '''
+        Test - remove kernel package
+        '''
+        mock = MagicMock(return_value={'retcode': 0, 'stderr': []})
+        with patch.dict(self._kernelpkg.__salt__, {'cmd.run_all': mock}):
+            with patch.object(self._kernelpkg, 'active', return_value=self.KERNEL_LIST[-1]):
+                with patch.object(self._kernelpkg, 'list_installed', return_value=self.KERNEL_LIST):
+                    result = self._kernelpkg.remove(release=self.KERNEL_LIST[0])
+                    self._kernelpkg.__salt__['cmd.run_all'].assert_called_once()
+                    self.assertIn('removed', result)
+                    target = '{0}-{1}'.format(self._kernelpkg._package_name(), self.KERNEL_LIST[0])
+                    self.assertListEqual(result['removed'], [target])
+
+    def test_remove_error(self):
+        '''
+        Test - remove kernel package
+        '''
+        mock = MagicMock(return_value={'retcode': -1, 'stderr': []})
+        with patch.dict(self._kernelpkg.__salt__, {'cmd.run_all': mock}):
+            with patch.object(self._kernelpkg, 'active', return_value=self.KERNEL_LIST[-1]):
+                with patch.object(self._kernelpkg, 'list_installed', return_value=self.KERNEL_LIST):
+                    self.assertRaises(CommandExecutionError, self._kernelpkg.remove, release=self.KERNEL_LIST[0])
+                    self._kernelpkg.__salt__['cmd.run_all'].assert_called_once()
