saltstack/salt

commit 7c6ff77c98821083ce46be0394f1723ac445ea63

Fix name_, for loop and add test cases

salt/modules/iptables.py
@@ -207,9 +207,9 @@ def build_rule(table='filter', chain=None, command=None, position='', full=None,
             match_value = match_value.split(',')
         for match in match_value:
             rule.append('-m {0}'.format(match))
-            if '_name' in kwargs and match.strip() in ('pknock', 'quota2', 'recent'):
-                rule.append('--name {0}'.format(kwargs['_name']))
-                del kwargs['_name']
+            if 'name_' in kwargs and match.strip() in ('pknock', 'quota2', 'recent'):
+                rule.append('--name {0}'.format(kwargs['name_']))
+                del kwargs['name_']
         del kwargs['match']
 
     if 'match-set' in kwargs:
@@ -414,18 +414,11 @@ def build_rule(table='filter', chain=None, command=None, position='', full=None,
                 after_jump.append('--{0} {1}'.format(after_jump_argument, value))
             del kwargs[after_jump_argument]
 
-    for item in kwargs:
-        rule.append(maybe_add_negation(item))
-        if len(item) == 1:
-            if kwargs[item] in (None, ''):
-                rule.append('-{0}'.format(item))
-            else:
-                rule.append('-{0} {1}'.format(item, kwargs[item]))
-        else:
-            if kwargs[item] in (None, ''):
-                rule.append('--{0}'.format(item))
-            else:
-                rule.append('--{0} {1}'.format(item, kwargs[item]))
+    for key, value in kwargs.items():
+        negation = maybe_add_negation(key)
+        flag = '-' if len(key) == 1 else '--'
+        value = '' if value in (None, '') else ' {0}'.format(value)
+        rule.append('{0}{1}{2}{3}'.format(negation, flag, key, value))
 
     rule += after_jump
 

tests/unit/modules/iptables_test.py
@@ -156,6 +156,14 @@ class IptablesTestCase(TestCase):
         self.assertEqual(iptables.build_rule(**{'match-set': match_sets}),
                          '-m set --match-set src1 flag -m set ! --match-set src2 flag2')
 
+        # should allow escaped name
+        self.assertEqual(iptables.build_rule(**{'match': 'recent', 'name_': 'SSH'}),
+                         '-m recent --name SSH')
+
+        # should allow empty arguments 
+        self.assertEqual(iptables.build_rule(**{'match': 'recent', 'update': None}),
+                         '-m recent --update')
+
         # Should allow the --save jump option to CONNSECMARK
         #self.assertEqual(iptables.build_rule(jump='CONNSECMARK',
         #                                     **{'save': ''}),

commit 6f19fc023cd142a02ac9b6d74d74105c5d778d08

Fix broken negation in iptables
Introduced in 7c6ff77c and released with 2017.7.

salt/modules/iptables.py
@@ -501,8 +501,11 @@ def build_rule(table='filter', chain=None, command=None, position='', full=None,
                 after_jump.append('--{0} {1}'.format(after_jump_argument, value))
             del kwargs[after_jump_argument]
 
-    for key, value in kwargs.items():
+    for key in kwargs:
         negation = maybe_add_negation(key)
+        # don't use .items() since maybe_add_negation removes the prefix from
+        # the value in the kwargs, thus we need to fetch it after that has run
+        value = kwargs[key]
         flag = '-' if len(key) == 1 else '--'
         value = '' if value in (None, '') else ' {0}'.format(value)
         rule.append('{0}{1}{2}{3}'.format(negation, flag, key, value))

tests/unit/modules/test_iptables.py
@@ -60,6 +60,9 @@ class IptablesTestCase(TestCase, LoaderModuleMockMixin):
             self.assertEqual(iptables.build_rule(**{'if': 'not eth0'}),
                              '! -i eth0')
 
+            self.assertEqual(iptables.build_rule(**{'proto': 'tcp', 'syn': '!'}),
+                            '-p tcp ! --syn')
+
             self.assertEqual(iptables.build_rule(dports=[80, 443], proto='tcp'),
                              '-p tcp -m multiport --dports 80,443')
 