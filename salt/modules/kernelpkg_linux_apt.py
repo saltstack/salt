9f5682f07613baa1f29166ed9991dd25485cb569
Adam Mendlik <amendlik@gmail.com>
2017-08-16 10:00:48 -0600

    Add kernelpkg.cleanup functions

--- salt/modules/kernelpkg_linux_apt.py
+++ salt/modules/kernelpkg_linux_apt.py
@@ -223,6 +223,34 @@ def remove(release):
     return {'removed': [target]}
 
 
+def cleanup(keep_latest=True):
+    '''
+    Remove all unused kernel packages from the system.
+
+    keep_latest : True
+        In the event that the active kernel is not the latest one installed, setting this to True
+        will retain the latest kernel package, in addition to the active one. If False, all kernel
+        packages other than the active one will be removed.
+    '''
+    removed = []
+
+    # Loop over all installed kernel packages
+    for kernel in list_installed():
+
+        # Keep the active kernel package
+        if kernel == active():
+            continue
+
+        # Optionally keep the latest kernel package
+        if keep_latest and kernel == latest_installed():
+            continue
+
+        # Remove the kernel package
+        removed.extend(remove(kernel)['removed'])
+
+    return {'removed': removed}
+
+
 def _package_prefix():
     '''
     Return static string for the package prefix
