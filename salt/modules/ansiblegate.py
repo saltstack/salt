5720908fc0dbcb891b764cde55253e07fd8513ad
Bo Maryniuk <bo@suse.de>
2017-08-17 15:26:36 +0200

    Add initial module resolver, implement modules map

--- salt/modules/ansiblegate.py
+++ salt/modules/ansiblegate.py
@@ -16,13 +16,67 @@
 # limitations under the License.
 
 import logging
+import os
 try:
     import ansible
+    import ansible.constants
+    import ansible.modules
+    from ansible.plugins import module_loader
 except ImportError:
     ansible = None
 
 __virtualname__ = 'ansible'
 log = logging.getLogger(__name__)
+
+
+class AnsibleModuleResolver(object):
+    '''
+    This class is to resolve all available modules in Ansible.
+    '''
+    def __init__(self, opts):
+        self._opts = opts
+        self._modules_map = self._get_modules_map()
+
+    def _get_modules_map(self, path=None):
+        '''
+        Get installed Ansible modules
+        :return:
+        '''
+        paths = {}
+        root = ansible.modules.__path__[0]
+        if not path:
+            path = root
+        for p_el in os.listdir(path):
+            p_el_path = os.path.join(path, p_el)
+            if os.path.islink(p_el_path): continue
+            if os.path.isdir(p_el_path):
+                paths.update(self._get_modules_map(p_el_path))
+            else:
+                if (any(p_el.startswith(elm) for elm in ['__', '.']) or
+                        not p_el.endswith('.py') or
+                        p_el in ansible.constants.IGNORE_FILES):
+                    continue
+                m_name = p_el.split('.')[0]
+                als_name = m_name[1:] if m_name.startswith('_') else m_name
+                paths[als_name] = p_el_path.replace(root, '')
+
+        return paths
+
+    def _introspect_module(self, module):
+        '''
+        Introspect Ansible module.
+
+        :param module:
+        :return:
+        '''
+
+    def resolve(self):
+        log.debug('Resolving Ansible modules')
+        return self
+
+    def install(self):
+        log.debug('Installing Ansible modules')
+        return self
 
 
 def __virtual__():
@@ -34,5 +88,6 @@ def __virtual__():
     msg = not ret and "Ansible is not installed on this system" or None
     if msg:
         log.warning(msg)
+    else:
+        AnsibleModuleResolver(__opts__).resolve().install()
     return ret, msg
-
