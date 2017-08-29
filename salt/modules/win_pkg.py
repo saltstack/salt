96db5d3d0175b0b0d95e8a616cccef1d8e3fa3ee
twangboy <slee@saltstack.com>
2017-08-29 13:27:18 -0600

    Fix regex statement, add `.`

--- salt/modules/win_pkg.py
+++ salt/modules/win_pkg.py
@@ -1243,7 +1243,9 @@ def install(name=None, refresh=False, pkgs=None, **kwargs):
 
             # Run Scheduled Task
             # Special handling for installing salt
-            if re.search(r'salt[\s-_]*minion', pkg_name, flags=re.IGNORECASE+re.UNICODE) is not None:
+            if re.search(r'salt[\s_.-]*minion',
+                         pkg_name,
+                         flags=re.IGNORECASE+re.UNICODE) is not None:
                 ret[pkg_name] = {'install status': 'task started'}
                 if not __salt__['task.run'](name='update-salt-software'):
                     log.error('Failed to install {0}'.format(pkg_name))
