14a45918549cc17b6c4edc938448c33a5045fbf5
Nathan Fish <lordcirth@gmail.com>
2017-08-31 13:59:07 -0500

    file.py docs: correct group and mode

--- salt/states/file.py
+++ salt/states/file.py
@@ -1324,13 +1324,15 @@ def managed(name,
 
     group
         The group ownership set for the file, this defaults to the group salt
-        is running as on the minion On Windows, this is ignored
+        is running as on the minion. On Windows, this is ignored
 
     mode
-        The permissions to set on this file, e.g. ``644``, ``0775``, or ``4664``.
+        The permissions to set on this file, e.g. ``644``, ``0775``, or
+        ``4664``.
 
-        The default mode for new files and directories corresponds umask of salt
-        process. For existing files and directories it's not enforced.
+        The default mode for new files and directories corresponds to the
+        umask of the salt process. The mode of existing files and directories
+        will only be changed if ``mode`` is specified.
 
         .. note::
             This option is **not** supported on Windows.
