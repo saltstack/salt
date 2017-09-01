fb862b350e180b2fac5861b3ad3d125fced17b9f
Aneesh Agrawal <aagrawal@lyft.com>
2017-09-01 22:03:29 +0000

    Delete ASG sub-resources in boto_asg.absent
    Add a new kwarg, `recurse`, to expand on the old kwarg `remove_lc`,
    which is now deprecated, to enable deletion of sub-resources.
    
    Use the boto_lc.absent state to handle deleting the launch config.
    Also, delete any associated CloudWatch alarms.

--- tests/unit/states/test_boto_asg.py
+++ tests/unit/states/test_boto_asg.py
@@ -4,13 +4,16 @@
 '''
 # Import Python libs
 from __future__ import absolute_import
+import copy
 
 # Import Salt Testing Libs
 from tests.support.mixins import LoaderModuleMockMixin
 from tests.support.unit import skipIf, TestCase
-from tests.support.mock import NO_MOCK, NO_MOCK_REASON, MagicMock, patch
+from tests.support.mock import NO_MOCK, NO_MOCK_REASON, Mock, patch
 
 # Import Salt Libs
+import salt.config
+import salt.loader
 import salt.states.boto_asg as boto_asg
 
 
@@ -22,7 +25,31 @@ class BotoAsgTestCase(TestCase, LoaderModuleMockMixin):
     # 'present' function tests: 1
 
     def setup_loader_modules(self):
-        return {boto_asg: {}}
+        utils = salt.loader.utils(
+            self.opts,
+            whitelist=['dictupdate', 'state'],
+            context={},
+        )
+        return {boto_asg: {'__utils__': utils}}
+
+    @classmethod
+    def setUpClass(cls):
+        cls.opts = salt.config.DEFAULT_MINION_OPTS
+
+        cls.name = 'my_asg'
+        cls.base_ret = {'name': cls.name, 'changes': {}}
+
+    @classmethod
+    def tearDownClass(cls):
+        del cls.opts
+
+        del cls.name
+        del cls.base_ret
+
+    def base_ret_with(self, extra_ret):
+        new_ret = copy.deepcopy(self.base_ret)
+        new_ret.update(extra_ret)
+        return new_ret
 
     maxSize = None
 
@@ -41,13 +68,13 @@ class BotoAsgTestCase(TestCase, LoaderModuleMockMixin):
                'changes': {},
                'comment': ''}
 
-        mock = MagicMock(side_effect=[False, {'min_size': 2}, ['']])
+        mock = Mock(side_effect=[False, {'min_size': 2}, ['']])
         with patch.dict(boto_asg.__salt__, {'boto_asg.get_config': mock}):
             with patch.dict(boto_asg.__opts__, {'test': True}):
                 comt = 'Autoscale group set to be created.'
                 ret.update({'comment': comt})
                 with patch.dict(boto_asg.__salt__,
-                                {'config.option': MagicMock(return_value={})}):
+                                {'config.option': Mock(return_value={})}):
                     self.assertDictEqual(
                         boto_asg.present(
                             name,
@@ -70,11 +97,11 @@ class BotoAsgTestCase(TestCase, LoaderModuleMockMixin):
                 ret.update({'comment': comt, 'result': None})
                 ret.update({'changes': {'new': {'min_size': 4},
                                         'old': {'min_size': 2}}})
-                utils_ordered_mock = MagicMock(
+                utils_ordered_mock = Mock(
                     side_effect=magic_side_effect
                 )
                 with patch.dict(boto_asg.__salt__,
-                                {'config.option': MagicMock(return_value={})}):
+                                {'config.option': Mock(return_value={})}):
                     with patch.dict(boto_asg.__utils__,
                                     {'boto3.ordered': utils_ordered_mock}):
                         call_ret = boto_asg.present(
@@ -87,9 +114,9 @@ class BotoAsgTestCase(TestCase, LoaderModuleMockMixin):
                         self.assertDictEqual(call_ret, ret)
 
                 with patch.dict(boto_asg.__salt__,
-                                {'config.option': MagicMock(return_value={})}):
+                                {'config.option': Mock(return_value={})}):
                     with patch.dict(boto_asg.__utils__,
-                                    {'boto3.ordered': MagicMock(return_value='')}):
+                                    {'boto3.ordered': Mock(return_value='')}):
                         comt = 'Autoscale group present. '
                         ret.update({'comment': comt, 'result': True})
                         ret.update({'changes': {}})
@@ -104,26 +131,154 @@ class BotoAsgTestCase(TestCase, LoaderModuleMockMixin):
                             ret
                         )
 
-    # 'absent' function tests: 1
-
-    def test_absent(self):
+    def test_absent_alone(self):
         '''
-        Test to ensure the named autoscale group is deleted.
+        Test to ensure the named autoscale group is deleted,
+        without examining subresources.
         '''
-        name = 'myasg'
+        with patch.multiple(boto_asg,
+            __salt__={'boto_asg.get_config': Mock(return_value=False)},
+            __opts__={'test': True},
+        ):
+            comment = [
+                'Autoscale group {0} does not exist.'.format(self.name),
+            ]
+            self.assertDictEqual(
+                boto_asg.absent(self.name),
+                self.base_ret_with({'result': True, 'comment': comment}),
+            )
 
-        ret = {'name': name,
-               'result': None,
-               'changes': {},
-               'comment': ''}
+        with patch.multiple(boto_asg,
+            __salt__={'boto_asg.get_config': Mock(return_value=True)},
+            __opts__={'test': True},
+        ):
+            comment = [
+                'Autoscale group {0} set to be deleted.'.format(self.name),
+            ]
+            self.assertDictEqual(
+                boto_asg.absent(self.name),
+                self.base_ret_with({
+                    'result': None,
+                    'comment': comment,
+                    'pchanges': {'asg': {'old': self.name, 'new': None}},
+                }),
+            )
 
-        mock = MagicMock(side_effect=[True, False])
-        with patch.dict(boto_asg.__salt__, {'boto_asg.get_config': mock}):
-            with patch.dict(boto_asg.__opts__, {'test': True}):
-                comt = ('Autoscale group set to be deleted.')
-                ret.update({'comment': comt})
-                self.assertDictEqual(boto_asg.absent(name), ret)
+        with patch.multiple(boto_asg,
+            __salt__={
+                'boto_asg.get_config': Mock(return_value=True),
+                'boto_asg.delete': Mock(return_value=False),
+            },
+            __opts__={'test': False},
+        ):
+            comment = [
+                'Failed to delete autoscale group {0}.'.format(self.name),
+            ]
+            self.assertDictEqual(
+                boto_asg.absent(self.name),
+                self.base_ret_with({'result': False, 'comment': comment}),
+            )
 
-                comt = ('Autoscale group does not exist.')
-                ret.update({'comment': comt, 'result': True})
-                self.assertDictEqual(boto_asg.absent(name), ret)
+        with patch.multiple(boto_asg,
+            __salt__={
+                'boto_asg.get_config': Mock(return_value=True),
+                'boto_asg.delete': Mock(return_value=True),
+            },
+            __opts__={'test': False},
+        ):
+            comment = [
+                'Deleted autoscale group {0}.'.format(self.name),
+            ]
+            self.assertDictEqual(
+                boto_asg.absent(self.name),
+                self.base_ret_with({
+                    'result': True,
+                    'comment': comment,
+                    'changes': {'asg': {'old': self.name, 'new': None}},
+                }),
+            )
+
+    def test_absent_with_subresources(self):
+        '''
+        Test to ensure the named autoscale group is deleted with subresources.
+        '''
+        mock_asg_get = Mock(return_value={'launch_config_name': 'my_lc'})
+        mock_lc_absent_test = Mock(return_value={
+            'name': 'my_lc',
+            'result': None,
+            'comment': 'Launch configuration set to be deleted.',
+            'changes': {},
+            'pchanges': {'old': 'my_lc', 'new': None},
+        })
+        with patch.multiple(boto_asg,
+            __salt__={
+                'boto_asg.get_config': mock_asg_get,
+                'config.option': Mock(return_value={}),
+            },
+            __states__={'boto_lc.absent': mock_lc_absent_test},
+            __opts__={'test': True},
+        ):
+            comment = [
+                'Autoscale group {0} set to be deleted.'.format(self.name),
+                'Launch configuration set to be deleted.',
+            ]
+            self.assertDictEqual(
+                boto_asg.absent(self.name, recurse=True),
+                self.base_ret_with({
+                    'result': None,
+                    'comment': comment,
+                    'pchanges': {
+                        'asg': {'old': 'my_asg', 'new': None},
+                        'launch_config': {'old': 'my_lc', 'new': None},
+                    },
+                }),
+            )
+
+        mock_lc_absent_real = Mock(return_value={
+            'name': 'my_lc',
+            'result': True,
+            'comment': 'Deleted launch configuration.',
+            'changes': {'old': 'my_lc', 'new': None},
+        })
+        mock_alarms_absent_real = Mock(return_value={
+            'name': '',
+            'result': True,
+            'comment': 'Deleted CloudWatch alarms alarm-1,alarm-2.',
+            'changes': {'old': ['alarm-1', 'alarm-2'], 'new': []},
+        })
+        with patch.multiple(boto_asg,
+            __salt__={
+                'boto_asg.get_config': mock_asg_get,
+                'boto_asg.delete': Mock(return_value=True),
+                'config.option': Mock(return_value={}),
+            },
+            __states__={
+                'boto_lc.absent': mock_lc_absent_real,
+                'boto_cloudwatch_alarm.absent': mock_alarms_absent_real,
+            },
+            __opts__={'test': False},
+        ):
+            comment = [
+                'Deleted autoscale group {0}.'.format(self.name),
+                'Deleted launch configuration.',
+                'Deleted CloudWatch alarms alarm-1,alarm-2.',
+            ]
+            self.assertDictEqual(
+                boto_asg.absent(
+                    self.name,
+                    alarms=dict(
+                        (name, {'name': name})
+                        for name in ['alarm-1', 'alarm-2']
+                    ),
+                    recurse=True,
+                ),
+                self.base_ret_with({
+                    'result': True,
+                    'comment': comment,
+                    'changes': {
+                        'asg': {'old': 'my_asg', 'new': None},
+                        'launch_config': {'old': 'my_lc', 'new': None},
+                        'alarms': {'old': ['alarm-1', 'alarm-2'], 'new': []},
+                    },
+                }),
+            )
