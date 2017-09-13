488756bbddbe46587c95c93193168b22fc537d4e
Bo Maryniuk <bo@suse.de>
2017-09-13 16:30:28 +0200

    Wrap caller into a class

--- salt/states/ansiblegate.py
+++ salt/states/ansiblegate.py
@@ -20,25 +20,52 @@ state:
 
 '''
 from __future__ import absolute_import
+try:
+    import ansible
+except ImportError as err:
+    ansible = None
+
 
 __virtualname__ = 'ansible'
 
 
+class AnsibleState(object):
+    def __init__(self, available):
+        self.available = available
+
+    def call(self, **kwargs):
+        '''
+        Call Ansible module.
+
+        :return:
+        '''
+
+        ret = {
+            'name': kwargs.pop('name'),
+            'changes': {},
+            'comment': '',
+            'result': None,
+        }
+
+        print "\n\n\n>>>", kwargs, "\n\n\n"
+
+        return ret
+
+
+_ansible_state = AnsibleState(ansible is not None)
+
+
 def __virtual__():
-    return True
-
-
-def call(*args, **kwargs):
     '''
+    Disable, if Ansible is not available around on the Minion.
+    '''
+    return _ansible_state.available
+
+
+def call(**kwargs):
+    '''
+    Call the Ansible module.
+    :param kwargs:
     :return:
     '''
-
-    ret = {
-        'name': kwargs.keys(),
-        'changes': {},
-        'comment': '',
-        'result': None,
-    }
-    return ret
-
-
+    return _ansible_state.call(**kwargs)
