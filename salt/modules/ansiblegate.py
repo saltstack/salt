64d3c1e300d7eaba9476b25ab80d09858d5145d4
Bo Maryniuk <bo@suse.de>
2017-09-15 09:44:26 +0200

    Lintfix: W1699

--- salt/modules/ansiblegate.py
+++ salt/modules/ansiblegate.py
@@ -15,6 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import absolute_import
 import os
 import sys
 import logging
