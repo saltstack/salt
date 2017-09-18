4f09bf5e880bfd55b3b24fdffa690723fa2554b7
Alexandru Bleotu <Alexandru.Bleotu@morganstanley.com>
2017-09-18 18:20:17 -0400

    Added tests for salt.utils.vmware.get_network_folder

--- tests/unit/utils/vmware/test_dvs.py
+++ tests/unit/utils/vmware/test_dvs.py
@@ -106,3 +106,70 @@ class GetDvssTestCase(TestCase):
                               dvs_names=['fake_dvs1', 'fake_dvs3', 'no_dvs'])
         self.assertEqual(ret, [self.mock_items[0]['object'],
                                self.mock_items[2]['object']])
+
+
+@skipIf(NO_MOCK, NO_MOCK_REASON)
+@skipIf(not HAS_PYVMOMI, 'The \'pyvmomi\' library is missing')
+class GetNetworkFolderTestCase(TestCase):
+    def setUp(self):
+        self.mock_si = MagicMock()
+        self.mock_dc_ref = MagicMock()
+        self.mock_traversal_spec = MagicMock()
+        self.mock_entries = [{'object': MagicMock(),
+                              'name': 'fake_netw_folder'}]
+        self.mock_get_mors = MagicMock(return_value=self.mock_entries)
+
+        patches = (
+            ('salt.utils.vmware.get_managed_object_name',
+             MagicMock(return_value='fake_dc')),
+            ('salt.utils.vmware.get_service_instance_from_managed_object',
+             MagicMock(return_value=self.mock_si)),
+            ('salt.utils.vmware.vmodl.query.PropertyCollector.TraversalSpec',
+             MagicMock(return_value=self.mock_traversal_spec)),
+            ('salt.utils.vmware.get_mors_with_properties',
+             self.mock_get_mors))
+        for mod, mock in patches:
+            patcher = patch(mod, mock)
+            patcher.start()
+            self.addCleanup(patcher.stop)
+
+    def tearDown(self):
+        for attr in ('mock_si', 'mock_dc_ref', 'mock_traversal_spec',
+                     'mock_entries', 'mock_get_mors'):
+            delattr(self, attr)
+
+    def test_get_managed_object_name_call(self):
+        mock_get_managed_object_name = MagicMock()
+        with patch('salt.utils.vmware.get_managed_object_name',
+                   mock_get_managed_object_name):
+            vmware.get_network_folder(self.mock_dc_ref)
+        mock_get_managed_object_name.assert_called_once_with(self.mock_dc_ref)
+
+    def test_traversal_spec(self):
+        mock_traversal_spec = MagicMock(return_value='traversal_spec')
+        with patch(
+            'salt.utils.vmware.vmodl.query.PropertyCollector.TraversalSpec',
+            mock_traversal_spec):
+
+            vmware.get_network_folder(self.mock_dc_ref)
+        mock_traversal_spec.assert_called_once_with(
+            path='networkFolder', skip=False, type=vim.Datacenter)
+
+    def test_get_mors_with_properties(self):
+        vmware.get_network_folder(self.mock_dc_ref)
+        self.mock_get_mors.assert_called_once_with(
+            self.mock_si, vim.Folder, container_ref=self.mock_dc_ref,
+            property_list=['name'], traversal_spec=self.mock_traversal_spec)
+
+    def test_get_no_network_folder(self):
+        with patch('salt.utils.vmware.get_mors_with_properties',
+                   MagicMock(return_value=[])):
+            with self.assertRaises(VMwareObjectRetrievalError) as excinfo:
+                vmware.get_network_folder(self.mock_dc_ref)
+        self.assertEqual(excinfo.exception.strerror,
+                         'Network folder in datacenter \'fake_dc\' wasn\'t '
+                         'retrieved')
+
+    def test_get_network_folder(self):
+        ret = vmware.get_network_folder(self.mock_dc_ref)
+        self.assertEqual(ret, self.mock_entries[0]['object'])
