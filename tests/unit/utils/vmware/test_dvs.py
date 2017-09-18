d31d98c2d39723186dac777485b5a833b00e0ea7
Alexandru Bleotu <Alexandru.Bleotu@morganstanley.com>
2017-09-18 18:23:00 -0400

    Added tests for salt.utils.vmware.create_dvs

--- tests/unit/utils/vmware/test_dvs.py
+++ tests/unit/utils/vmware/test_dvs.py
@@ -173,3 +173,105 @@ class GetNetworkFolderTestCase(TestCase):
     def test_get_network_folder(self):
         ret = vmware.get_network_folder(self.mock_dc_ref)
         self.assertEqual(ret, self.mock_entries[0]['object'])
+
+
+@skipIf(NO_MOCK, NO_MOCK_REASON)
+@skipIf(not HAS_PYVMOMI, 'The \'pyvmomi\' library is missing')
+class CreateDvsTestCase(TestCase):
+    def setUp(self):
+        self.mock_dc_ref = MagicMock()
+        self.mock_dvs_create_spec = MagicMock()
+        self.mock_task = MagicMock(spec=FakeTaskClass)
+        self.mock_netw_folder = \
+                MagicMock(CreateDVS_Task=MagicMock(
+                    return_value=self.mock_task))
+        self.mock_wait_for_task = MagicMock()
+
+        patches = (
+            ('salt.utils.vmware.get_managed_object_name',
+             MagicMock(return_value='fake_dc')),
+            ('salt.utils.vmware.get_network_folder',
+             MagicMock(return_value=self.mock_netw_folder)),
+            ('salt.utils.vmware.wait_for_task', self.mock_wait_for_task))
+        for mod, mock in patches:
+            patcher = patch(mod, mock)
+            patcher.start()
+            self.addCleanup(patcher.stop)
+
+    def tearDown(self):
+        for attr in ('mock_dc_ref', 'mock_dvs_create_spec',
+                     'mock_task', 'mock_netw_folder', 'mock_wait_for_task'):
+            delattr(self, attr)
+
+    def test_get_managed_object_name_call(self):
+        mock_get_managed_object_name = MagicMock()
+        with patch('salt.utils.vmware.get_managed_object_name',
+                   mock_get_managed_object_name):
+            vmware.create_dvs(self.mock_dc_ref, 'fake_dvs')
+        mock_get_managed_object_name.assert_called_once_with(self.mock_dc_ref)
+
+    def test_no_dvs_create_spec(self):
+        mock_spec = MagicMock(configSpec=None)
+        mock_config_spec = MagicMock()
+        mock_dvs_create_spec = MagicMock(return_value=mock_spec)
+        mock_vmware_dvs_config_spec = \
+                MagicMock(return_value=mock_config_spec)
+        with patch('salt.utils.vmware.vim.DVSCreateSpec',
+                   mock_dvs_create_spec):
+            with patch('salt.utils.vmware.vim.VMwareDVSConfigSpec',
+                       mock_vmware_dvs_config_spec):
+                vmware.create_dvs(self.mock_dc_ref, 'fake_dvs')
+        mock_dvs_create_spec.assert_called_once_with()
+        mock_vmware_dvs_config_spec.assert_called_once_with()
+        self.assertEqual(mock_spec.configSpec, mock_config_spec)
+        self.assertEqual(mock_config_spec.name, 'fake_dvs')
+        self.mock_netw_folder.CreateDVS_Task.assert_called_once_with(mock_spec)
+
+    def test_get_network_folder(self):
+        mock_get_network_folder = MagicMock()
+        with patch('salt.utils.vmware.get_network_folder',
+                   mock_get_network_folder):
+            vmware.create_dvs(self.mock_dc_ref, 'fake_dvs')
+        mock_get_network_folder.assert_called_once_with(self.mock_dc_ref)
+
+    def test_create_dvs_task_passed_in_spec(self):
+        vmware.create_dvs(self.mock_dc_ref, 'fake_dvs',
+                          dvs_create_spec=self.mock_dvs_create_spec)
+        self.mock_netw_folder.CreateDVS_Task.assert_called_once_with(
+            self.mock_dvs_create_spec)
+
+    def test_create_dvs_task_raises_no_permission(self):
+        exc = vim.fault.NoPermission()
+        exc.privilegeId = 'Fake privilege'
+        self.mock_netw_folder.CreateDVS_Task = MagicMock(side_effect=exc)
+        with self.assertRaises(VMwareApiError) as excinfo:
+            vmware.create_dvs(self.mock_dc_ref, 'fake_dvs',
+                              dvs_create_spec=self.mock_dvs_create_spec)
+        self.assertEqual(excinfo.exception.strerror,
+                         'Not enough permissions. Required privilege: '
+                         'Fake privilege')
+
+    def test_create_dvs_task_raises_vim_fault(self):
+        exc = vim.fault.VimFault()
+        exc.msg = 'VimFault msg'
+        self.mock_netw_folder.CreateDVS_Task = MagicMock(side_effect=exc)
+        with self.assertRaises(VMwareApiError) as excinfo:
+            vmware.create_dvs(self.mock_dc_ref, 'fake_dvs',
+                              dvs_create_spec=self.mock_dvs_create_spec)
+        self.assertEqual(excinfo.exception.strerror, 'VimFault msg')
+
+    def test_create_dvs_task_raises_runtime_fault(self):
+        exc = vmodl.RuntimeFault()
+        exc.msg = 'RuntimeFault msg'
+        self.mock_netw_folder.CreateDVS_Task = MagicMock(side_effect=exc)
+        with self.assertRaises(VMwareRuntimeError) as excinfo:
+            vmware.create_dvs(self.mock_dc_ref, 'fake_dvs',
+                              dvs_create_spec=self.mock_dvs_create_spec)
+        self.assertEqual(excinfo.exception.strerror, 'RuntimeFault msg')
+
+    def test_wait_for_tasks(self):
+        vmware.create_dvs(self.mock_dc_ref, 'fake_dvs',
+                          dvs_create_spec=self.mock_dvs_create_spec)
+        self.mock_wait_for_task.assert_called_once_with(
+            self.mock_task, 'fake_dvs',
+            '<class \'unit.utils.vmware.test_dvs.FakeTaskClass\'>')
