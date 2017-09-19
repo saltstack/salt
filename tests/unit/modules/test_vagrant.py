c8a78c7ceb6f77f0e3775622c7eec809fbd1c192
vernoncole <vernon.cole@sling.com>
2017-09-18 21:21:07 -0600

    provide unit tests

--- tests/unit/modules/test_vagrant.py
+++ tests/unit/modules/test_vagrant.py
@@ -2,21 +2,18 @@
 
 # Import python libs
 from __future__ import absolute_import
-import re
 
 # Import Salt Testing libs
 from tests.support.mixins import LoaderModuleMockMixin
 from tests.support.unit import skipIf, TestCase
-from tests.support.mock import NO_MOCK, NO_MOCK_REASON, MagicMock, patch
+from tests.support.mock import NO_MOCK, NO_MOCK_REASON
 
 # Import salt libs
 import salt.modules.vagrant as vagrant
 import salt.modules.config as config
-from salt._compat import ElementTree as ET
-import salt.utils
+import salt.exceptions
 
 # Import third party libs
-import yaml
 from salt.ext import six
 
 
@@ -25,65 +22,94 @@ class VagrantTestCase(TestCase, LoaderModuleMockMixin):
     '''
     Unit TestCase for the salt.modules.vagrant module.
     '''
-    # def setup_loader_modules(self):
-    #     return {vsphere: {'__virtual__': MagicMock(return_value='vsphere')}}
 
     def setup_loader_modules(self):
-        loader_globals = {
-            '__salt__': {
-                'config.get': config.get,
-                'config.option': config.option,
+        vagrant_globals = {
+            '__opts__': {
+                'extension_modules': '',
+                'cachedir': '/tmp/salt-tests-tmpdir/cache',
+                'cache': 'localfs'
             }
         }
-        return {vagrant: loader_globals, config: loader_globals}
+        return {vagrant: vagrant_globals}
 
-    def test_boot_default_dev(self):
-        diskp = vagrant._disk_profile('default', 'kvm')
-        nicp = vagrant._nic_profile('default', 'kvm')
-        xml_data = vagrant._gen_xml(
-            'hello',
-            1,
-            512,
-            diskp,
-            nicp,
-            'kvm'
+
+    def test_vagrant_get_vm_info(self):
+        with self.assertRaises(salt.exceptions.SaltInvocationError):
+            vagrant.get_vm_info('thisNameDoesNotExist')
+
+
+    def test_vagrant_init_positional(self):
+        resp = vagrant.init(
+            'test1',
+            '/tmp/nowhere',
+            'onetest',
+            'nobody',
+            False,
+            True,
+            'french',
+            {'different': 'very'}
             )
-        root = ET.fromstring(xml_data)
-        self.assertEqual(root.find('os/boot').attrib['dev'], 'hd')
+        self.assertIsInstance(resp, six.string_types)
+        resp = vagrant.get_vm_info('test1')
+        expected = dict(name='test1',
+                        cwd='/tmp/nowhere',
+                        machine='onetest',
+                        runas='nobody',
+                        deploy=True,
+                        vagrant_provider='french',
+                        different= 'very'
+                        )
+        self.assertEqual(resp, expected)
 
 
-    def test_gen_xml_for_telnet_console(self):
-        diskp = vagrant._disk_profile('default', 'kvm')
-        nicp = vagrant._nic_profile('default', 'kvm')
-        xml_data = vagrant._gen_xml(
-            'hello',
-            1,
-            512,
-            diskp,
-            nicp,
-            'kvm',
-            serial_type='tcp',
-            console=True,
-            telnet_port=22223
-            )
-        root = ET.fromstring(xml_data)
-        self.assertEqual(root.find('devices/serial').attrib['type'], 'tcp')
-        self.assertEqual(root.find('devices/console').attrib['type'], 'tcp')
-        self.assertEqual(root.find('devices/console/source').attrib['service'], '22223')
+    def test_vagrant_init_dict(self):
+        testdict = dict(cwd='/tmp/anywhere',
+                        machine='twotest',
+                        runas='somebody',
+                        deploy=True,
+                        vagrant_provider='english')
+        vagrant.init('test2', vm=testdict)
+        resp = vagrant.get_vm_info('test2')
+        testdict['name'] = 'test2'
+        self.assertEqual(resp, testdict)
 
 
-    def test_controller_for_kvm(self):
-        diskp = vagrant._disk_profile('default', 'kvm')
-        nicp = vagrant._nic_profile('default', 'kvm')
-        xml_data = vagrant._gen_xml(
-            'hello',
-            1,
-            512,
-            diskp,
-            nicp,
-            'kvm'
-            )
-        root = ET.fromstring(xml_data)
-        controllers = root.findall('.//devices/controller')
-        # There should be no controller
-        self.assertTrue(len(controllers) == 0)
+    def test_vagrant_get_ssh_config_fails(self):
+        testdict = dict(cwd='/tmp/there',
+                        machine='treetest',
+                        runas='anybody',
+                        deploy=False,
+                        vagrant_provider='spansh')
+        vagrant.init('test3',
+                        cwd='/tmp',
+                        machine='threetest',
+                        runas='him',
+                        deploy=True,
+                        vagrant_provider='polish',
+                        vm=testdict)
+        resp = vagrant.get_vm_info('test3')
+        expected = dict(name='test3',
+                        cwd='/tmp',
+                        machine='threetest',
+                        runas='him',
+                        deploy=True,
+                        vagrant_provider='polish')
+        self.assertEqual(resp, expected)
+
+
+    def test_vagrant_get_ssh_config_fails(self):
+        vagrant.init('test3', cwd='/tmp')
+        with self.assertRaises(salt.exceptions.CommandExecutionError):
+            vagrant.get_ssh_config('test3')  # has not been started
+
+
+    def test_vagrant_destroy_removes_cached_entry(self):
+        vagrant.init('test3', cwd='/tmp')
+        #  VM has a stored value
+        self.assertEqual(vagrant.get_vm_info('test3')['name'], 'test3')
+        #  clean up (an error is expected -- machine never started)
+        self.assertFalse(vagrant.destroy('test3'))
+        #  VM no longer exists
+        with self.assertRaises(salt.exceptions.SaltInvocationError):
+            vagrant.get_ssh_config('test3')
