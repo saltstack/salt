75764567c44002a3e71a8ca375a37b6c3dad3a09
Alexandru Bleotu <Alexandru.Bleotu@morganstanley.com>
2017-09-19 18:44:07 -0400

    Added tests for salt.utils.pbm.get_storage_policies

--- tests/unit/utils/test_pbm.py
+++ tests/unit/utils/test_pbm.py
@@ -299,3 +299,93 @@ class GetPoliciesById(TestCase):
     def test_return_policies(self):
         ret = salt.utils.pbm.get_policies_by_id(self.mock_prof_mgr, self.policy_ids)
         self.assertEqual(ret, self.mock_policies)
+
+
+@skipIf(NO_MOCK, NO_MOCK_REASON)
+@skipIf(not HAS_PYVMOMI, 'The \'pyvmomi\' library is missing')
+class GetStoragePoliciesTestCase(TestCase):
+    '''Tests for salt.utils.pbm.get_storage_policies'''
+    def setUp(self):
+        self.mock_res_type = MagicMock()
+        self.mock_policy_ids = MagicMock()
+        self.mock_prof_mgr = MagicMock(
+            QueryProfile=MagicMock(return_value=self.mock_policy_ids))
+        # Policies
+        self.mock_policies=[]
+        for i in range(4):
+            mock_obj = MagicMock(resourceType=MagicMock(
+                resourceType=pbm.profile.ResourceTypeEnum.STORAGE))
+            mock_obj.name = 'fake_policy{0}'.format(i)
+            self.mock_policies.append(mock_obj)
+        patches = (
+            ('salt.utils.pbm.pbm.profile.ResourceType',
+             MagicMock(return_value=self.mock_res_type)),
+            ('salt.utils.pbm.get_policies_by_id',
+             MagicMock(return_value=self.mock_policies)))
+        for mod, mock in patches:
+            patcher = patch(mod, mock)
+            patcher.start()
+            self.addCleanup(patcher.stop)
+
+    def tearDown(self):
+        for attr in ('mock_res_type', 'mock_policy_ids', 'mock_policies',
+                     'mock_prof_mgr'):
+            delattr(self, attr)
+
+    def test_get_res_type(self):
+        mock_get_res_type = MagicMock()
+        with patch('salt.utils.pbm.pbm.profile.ResourceType',
+                   mock_get_res_type):
+            salt.utils.pbm.get_storage_policies(self.mock_prof_mgr)
+        mock_get_res_type.assert_called_once_with(
+            resourceType=pbm.profile.ResourceTypeEnum.STORAGE)
+
+    def test_retrieve_policy_ids(self):
+        mock_retrieve_policy_ids = MagicMock(return_value=self.mock_policy_ids)
+        self.mock_prof_mgr.QueryProfile = mock_retrieve_policy_ids
+        salt.utils.pbm.get_storage_policies(self.mock_prof_mgr)
+        mock_retrieve_policy_ids.asser_called_once_with(self.mock_res_type)
+
+    def test_retrieve_policy_ids_raises_no_permissions(self):
+        exc = vim.fault.NoPermission()
+        exc.privilegeId = 'Fake privilege'
+        self.mock_prof_mgr.QueryProfile = MagicMock(side_effect=exc)
+        with self.assertRaises(VMwareApiError) as excinfo:
+            salt.utils.pbm.get_storage_policies(self.mock_prof_mgr)
+        self.assertEqual(excinfo.exception.strerror,
+                         'Not enough permissions. Required privilege: '
+                         'Fake privilege')
+
+    def test_retrieve_policy_ids_raises_vim_fault(self):
+        exc = vim.fault.VimFault()
+        exc.msg = 'VimFault msg'
+        self.mock_prof_mgr.QueryProfile = MagicMock(side_effect=exc)
+        with self.assertRaises(VMwareApiError) as excinfo:
+            salt.utils.pbm.get_storage_policies(self.mock_prof_mgr)
+        self.assertEqual(excinfo.exception.strerror, 'VimFault msg')
+
+    def test_retrieve_policy_ids_raises_runtime_fault(self):
+        exc = vmodl.RuntimeFault()
+        exc.msg = 'RuntimeFault msg'
+        self.mock_prof_mgr.QueryProfile = MagicMock(side_effect=exc)
+        with self.assertRaises(VMwareRuntimeError) as excinfo:
+            salt.utils.pbm.get_storage_policies(self.mock_prof_mgr)
+        self.assertEqual(excinfo.exception.strerror, 'RuntimeFault msg')
+
+    def test_get_policies_by_id(self):
+        mock_get_policies_by_id = MagicMock(return_value=self.mock_policies)
+        with patch('salt.utils.pbm.get_policies_by_id',
+                   mock_get_policies_by_id):
+            salt.utils.pbm.get_storage_policies(self.mock_prof_mgr)
+        mock_get_policies_by_id.assert_called_once_with(
+            self.mock_prof_mgr, self.mock_policy_ids)
+
+    def test_return_all_policies(self):
+        ret = salt.utils.pbm.get_storage_policies(self.mock_prof_mgr,
+                                                  get_all_policies=True)
+        self.assertEqual(ret, self.mock_policies)
+
+    def test_return_filtered_policies(self):
+        ret = salt.utils.pbm.get_storage_policies(
+            self.mock_prof_mgr, policy_names=['fake_policy1', 'fake_policy3'])
+        self.assertEqual(ret, [self.mock_policies[1], self.mock_policies[3]])
