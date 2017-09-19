df16bdb686446867c2f1e79c43bbfdc069232b0e
Alexandru Bleotu <Alexandru.Bleotu@morganstanley.com>
2017-09-19 18:43:22 -0400

    Added salt.utils.pbm.get_storage_policies

--- salt/utils/pbm.py
+++ salt/utils/pbm.py
@@ -169,3 +169,42 @@ def get_policies_by_id(profile_manager, policy_ids):
     except vmodl.RuntimeFault as exc:
         log.exception(exc)
         raise VMwareRuntimeError(exc.msg)
+
+
+def get_storage_policies(profile_manager, policy_names=[],
+                         get_all_policies=False):
+    '''
+    Returns a list of the storage policies, filtered by name.
+
+    profile_manager
+        Reference to the profile manager.
+
+    policy_names
+        List of policy names to filter by.
+
+    get_all_policies
+        Flag specifying to return all policies, regardless of the specified
+        filter.
+    '''
+    res_type = pbm.profile.ResourceType(
+        resourceType=pbm.profile.ResourceTypeEnum.STORAGE)
+    try:
+        policy_ids = profile_manager.QueryProfile(res_type)
+    except vim.fault.NoPermission as exc:
+        log.exception(exc)
+        raise VMwareApiError('Not enough permissions. Required privilege: '
+                             '{0}'.format(exc.privilegeId))
+    except vim.fault.VimFault as exc:
+        log.exception(exc)
+        raise VMwareApiError(exc.msg)
+    except vmodl.RuntimeFault as exc:
+        log.exception(exc)
+        raise VMwareRuntimeError(exc.msg)
+    log.trace('policy_ids = {0}'.format(policy_ids))
+    # More policies are returned so we need to filter again
+    policies = [p for p in get_policies_by_id(profile_manager, policy_ids)
+                if p.resourceType.resourceType ==
+                pbm.profile.ResourceTypeEnum.STORAGE]
+    if get_all_policies:
+        return policies
+    return [p for p in policies if p.name in policy_names]
