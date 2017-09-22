847debab7a5567a47e7a33e02173dcb9176f5f31
Alexandru Bleotu <Alexandru.Bleotu@morganstanley.com>
2017-09-22 17:09:01 -0400

    Fix failing storage and listdiffer tests

--- tests/unit/utils/test_dictdiffer.py
+++ tests/unit/utils/test_dictdiffer.py
@@ -49,7 +49,7 @@ class RecursiveDictDifferTestCase(TestCase):
     def test_changed_without_ignore_unset_values(self):
         self.recursive_diff.ignore_unset_values = False
         self.assertEqual(self.recursive_diff.changed(),
-                         ['a.c', 'a.e', 'a.g', 'a.f', 'h', 'i'])
+                         ['a.c', 'a.e', 'a.f', 'a.g', 'h', 'i'])
 
     def test_unchanged(self):
         self.assertEqual(self.recursive_diff.unchanged(),
