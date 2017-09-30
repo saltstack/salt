4cb86e5f3d2d4753e601b3fbef944970b1d9125c
Gareth J. Greenaway <gareth@wiked.org>
2017-09-30 16:59:48 -0700

    Fixing a typo.

--- salt/grains/core.py
+++ salt/grains/core.py
@@ -2517,7 +2517,7 @@ def _linux_iqn():
     if os.path.isfile(initiator):
         with salt.utils.files.fopen(initiator, 'r') as _iscsi:
             for line in _iscsi:
-                if line.starswith('InitiatorName='):
+                if line.startswith('InitiatorName='):
                     iqn = line.split('=')
                     final_iqn = iqn[1].rstrip()
                     ret.extend([final_iqn])
