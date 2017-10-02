aed80a3c8df943426ec484ce33e537d3519776e0
Gareth J. Greenaway <gareth@wiked.org>
2017-10-02 16:05:05 -0700

    Provide a grain for total amount of swap for Linux, *BSD, OS X and Solaris/SunOS

--- salt/grains/core.py
+++ salt/grains/core.py
@@ -414,22 +414,39 @@ def _memdata(osdata):
                     if comps[0].strip() == 'MemTotal':
                         # Use floor division to force output to be an integer
                         grains['mem_total'] = int(comps[1].split()[0]) // 1024
+                    if comps[0].strip() == 'SwapTotal':
+                        # Use floor division to force output to be an integer
+                        grains['swap_total'] = int(comps[1].split()[0]) // 1024
     elif osdata['kernel'] in ('FreeBSD', 'OpenBSD', 'NetBSD', 'Darwin'):
         sysctl = salt.utils.path.which('sysctl')
         if sysctl:
             if osdata['kernel'] == 'Darwin':
                 mem = __salt__['cmd.run']('{0} -n hw.memsize'.format(sysctl))
+                swap_total = __salt__['cmd.run']('{0} -n vm.swapusage').split()[2]
+                if swap_total.endswith('K'):
+                    _power = 2**10
+                elif swap_total.endswith('M'):
+                    _power = 2**20
+                elif swap_total.endswith('G'):
+                    _power = 2**30
+                swap_total = swap_total[:-1] * _power
             else:
                 mem = __salt__['cmd.run']('{0} -n hw.physmem'.format(sysctl))
+                swap_total = __salt__['cmd.run']('{0} -n vm.swap_total')
             if osdata['kernel'] == 'NetBSD' and mem.startswith('-'):
                 mem = __salt__['cmd.run']('{0} -n hw.physmem64'.format(sysctl))
             grains['mem_total'] = int(mem) // 1024 // 1024
+            grains['swap_total'] = int(swap_total) // 1024 // 1024
     elif osdata['kernel'] == 'SunOS':
         prtconf = '/usr/sbin/prtconf 2>/dev/null'
         for line in __salt__['cmd.run'](prtconf, python_shell=True).splitlines():
             comps = line.split(' ')
             if comps[0].strip() == 'Memory' and comps[1].strip() == 'size:':
                 grains['mem_total'] = int(comps[2].strip())
+
+        swap_cmd = salt.utils.path.which('swap')
+        swap_total = __salt__['cmd.run']('{0} -s'.format(swap_cmd)).split()[1]
+        grains['swap_total'] = int(swap_total) // 1024
     elif osdata['kernel'] == 'Windows' and HAS_WMI:
         # get the Total Physical memory as reported by msinfo32
         tot_bytes = win32api.GlobalMemoryStatusEx()['TotalPhys']
