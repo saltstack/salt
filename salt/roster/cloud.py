aebe76b6f8ba52caa570bd9a54b1b274e100bbaf
Corvin Mcpherson <corvinm@corvinm.com>
2017-10-03 12:56:44 -0400

    Fix issue with using roster_defaults with flat or cloud rosters. fixes #43449 fixes #43643

--- salt/roster/cloud.py
+++ salt/roster/cloud.py
@@ -63,7 +63,7 @@ def targets(tgt, tgt_type='glob', **kwargs):  # pylint: disable=W0613
         ))
         preferred_ip = extract_ipv4(roster_order, ip_list)
 
-        ret[minion_id] = __opts__.get('roster_defaults', {})
+        ret[minion_id] = __opts__.get('roster_defaults', {}).copy()
         ret[minion_id].update({'host': preferred_ip})
 
         ssh_username = salt.utils.cloud.ssh_usernames(vm_, cloud_opts)
