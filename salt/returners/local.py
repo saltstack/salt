881f1822f28882c1087a56d73c54325f33c896cc
Roald Nefs <info@roaldnefs.com>
2017-10-14 20:05:30 +0200

    Format fix code example local returner doc

--- salt/returners/local.py
+++ salt/returners/local.py
@@ -3,7 +3,9 @@
 The local returner is used to test the returner interface, it just prints the
 return data to the console to verify that it is being passed properly
 
-  To use the local returner, append '--return local' to the salt command. ex:
+To use the local returner, append '--return local' to the salt command. ex:
+
+.. code-block:: bash
 
     salt '*' test.ping --return local
 '''
