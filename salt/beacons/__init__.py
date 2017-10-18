54e6123bf6601ad461b94b355883a50be0e72198
me <sayyid.hamid.mahdavi@gmail.com>
2017-10-18 15:33:19 +0330

    remove empty line

--- salt/beacons/__init__.py
+++ salt/beacons/__init__.py
@@ -241,7 +241,6 @@ class Beacon(object):
             valid = True
             vcomment = 'Beacon %s does not have a validate function,  skipping validation.' % name
 
-
         # Fire the complete event back along with the list of beacons
         evt = salt.utils.event.get_event('minion', opts=self.opts)
         evt.fire_event({'complete': True,
