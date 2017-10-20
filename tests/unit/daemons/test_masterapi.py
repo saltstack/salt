11920372b3faa57f4fbe3f962d76d183517a8c94
Benedikt Werner <1benediktwerner@gmail.com>
2017-10-20 14:15:56 +0200

    Moved patch function outside of class to fix linting

--- tests/unit/daemons/test_masterapi.py
+++ tests/unit/daemons/test_masterapi.py
@@ -34,6 +34,23 @@ def gen_permissions(owner='', group='', others=''):
     return ret
 
 
+def patch_check_permissions(uid=1, groups=None, is_windows=False, permissive_pki=False):
+    if not groups:
+        groups = [uid]
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(self):
+            self.auto_key.opts['permissive_pki_access'] = permissive_pki
+            with patch('os.stat', self.os_stat_mock), \
+                 patch('os.getuid', MagicMock(return_value=uid)), \
+                 patch('salt.utils.user.get_gid_list', MagicMock(return_value=groups)), \
+                 patch('salt.utils.platform.is_windows', MagicMock(return_value=is_windows)):
+                func(self)
+        return wrapper
+    return decorator
+
+
 @skipIf(NO_MOCK, NO_MOCK_REASON)
 class AutoKeyTest(TestCase):
     '''
@@ -45,22 +62,6 @@ class AutoKeyTest(TestCase):
         self.auto_key = masterapi.AutoKey(opts)
         self.stats = {}
 
-    def _patch_check_permissions(uid=1, groups=None, is_windows=False, permissive_pki=False):
-        if not groups:
-            groups = [uid]
-
-        def decorator(func):
-            @wraps(func)
-            def wrapper(self):
-                self.auto_key.opts['permissive_pki_access'] = permissive_pki
-                with patch('os.stat', self.os_stat_mock), \
-                        patch('os.getuid', MagicMock(return_value=uid)), \
-                        patch('salt.utils.user.get_gid_list', MagicMock(return_value=groups)), \
-                        patch('salt.utils.platform.is_windows', MagicMock(return_value=is_windows)):
-                    func(self)
-            return wrapper
-        return decorator
-
     def os_stat_mock(self, filename):
         fmode = MagicMock()
         fstats = self.stats.get(filename, {})
@@ -68,7 +69,7 @@ class AutoKeyTest(TestCase):
         fmode.st_gid = fstats.get('gid', 0)
         return fmode
 
-    @_patch_check_permissions(uid=0, is_windows=True)
+    @patch_check_permissions(uid=0, is_windows=True)
     def test_check_permissions_windows(self):
         '''
         Assert that all files are accepted on windows
@@ -76,7 +77,7 @@ class AutoKeyTest(TestCase):
         self.stats['testfile'] = {'mode': gen_permissions('rwx', 'rwx', 'rwx'), 'gid': 2}
         self.assertTrue(self.auto_key.check_permissions('testfile'))
 
-    @_patch_check_permissions(permissive_pki=True)
+    @patch_check_permissions(permissive_pki=True)
     def test_check_permissions_others_can_write(self):
         '''
         Assert that no file is accepted, when others can write to it
@@ -84,7 +85,7 @@ class AutoKeyTest(TestCase):
         self.stats['testfile'] = {'mode': gen_permissions('', '', 'w'), 'gid': 1}
         self.assertFalse(self.auto_key.check_permissions('testfile'))
 
-    @_patch_check_permissions()
+    @patch_check_permissions()
     def test_check_permissions_group_can_write_not_permissive(self):
         '''
         Assert that a file is accepted, when group can write to it and perkissive_pki_access=False
@@ -92,7 +93,7 @@ class AutoKeyTest(TestCase):
         self.stats['testfile'] = {'mode': gen_permissions('w', 'w', ''), 'gid': 1}
         self.assertFalse(self.auto_key.check_permissions('testfile'))
 
-    @_patch_check_permissions(permissive_pki=True)
+    @patch_check_permissions(permissive_pki=True)
     def test_check_permissions_group_can_write_permissive(self):
         '''
         Assert that a file is accepted, when group can write to it and perkissive_pki_access=True
@@ -100,7 +101,7 @@ class AutoKeyTest(TestCase):
         self.stats['testfile'] = {'mode': gen_permissions('w', 'w', ''), 'gid': 1}
         self.assertTrue(self.auto_key.check_permissions('testfile'))
 
-    @_patch_check_permissions(uid=0, permissive_pki=True)
+    @patch_check_permissions(uid=0, permissive_pki=True)
     def test_check_permissions_group_can_write_permissive_root_in_group(self):
         '''
         Assert that a file is accepted, when group can write to it, perkissive_pki_access=False,
@@ -109,7 +110,7 @@ class AutoKeyTest(TestCase):
         self.stats['testfile'] = {'mode': gen_permissions('w', 'w', ''), 'gid': 0}
         self.assertTrue(self.auto_key.check_permissions('testfile'))
 
-    @_patch_check_permissions(uid=0, permissive_pki=True)
+    @patch_check_permissions(uid=0, permissive_pki=True)
     def test_check_permissions_group_can_write_permissive_root_not_in_group(self):
         '''
         Assert that no file is accepted, when group can write to it, perkissive_pki_access=False,
@@ -118,7 +119,7 @@ class AutoKeyTest(TestCase):
         self.stats['testfile'] = {'mode': gen_permissions('w', 'w', ''), 'gid': 1}
         self.assertFalse(self.auto_key.check_permissions('testfile'))
 
-    @_patch_check_permissions()
+    @patch_check_permissions()
     def test_check_permissions_only_owner_can_write(self):
         '''
         Assert that a file is accepted, when only the owner can write to it
@@ -126,7 +127,7 @@ class AutoKeyTest(TestCase):
         self.stats['testfile'] = {'mode': gen_permissions('w', '', ''), 'gid': 1}
         self.assertTrue(self.auto_key.check_permissions('testfile'))
 
-    @_patch_check_permissions(uid=0)
+    @patch_check_permissions(uid=0)
     def test_check_permissions_only_owner_can_write_root(self):
         '''
         Assert that a file is accepted, when only the owner can write to it and salt is root
