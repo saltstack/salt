8de2ea77c06af4c0edcb81871a757e73cdeb0813
Ch3LL <megan.wilhite@gmail.com>
2017-10-24 14:43:58 -0400

    add spm integration tests for remove and build

--- tests/integration/spm/test_build.py
+++ tests/integration/spm/test_build.py
@@ -9,7 +9,7 @@ import shutil
 import textwrap
 
 # Import Salt Testing libs
-from tests.support.case import SPMCase
+from tests.support.case import SPMCase, ModuleCase
 from tests.support.helpers import destructiveTest
 
 # Import Salt Libraries
@@ -17,7 +17,7 @@ import salt.utils
 
 
 @destructiveTest
-class SPMBuildTest(SPMCase):
+class SPMBuildTest(SPMCase, ModuleCase):
     '''
     Validate the spm build command
     '''
@@ -60,6 +60,44 @@ class SPMBuildTest(SPMCase):
         self.assertTrue(os.path.exists(spm_file))
         # Make sure formula path dir is created
         self.assertTrue(os.path.isdir(self.config['formula_path']))
+
+    def test_spm_build_big_file(self):
+        '''
+        test spm build
+        '''
+        big_file = self.run_function('cmd.run',
+                                     ['fallocate -l 1G {0}'.format(os.path.join(self.formula_sls_dir,
+                                                                              'bigfile.txt'))])
+        build_spm = self.run_spm('build', self.config, self.formula_dir)
+        spm_file = os.path.join(self.config['spm_build_dir'], 'apache-201506-2.spm')
+        install = self.run_spm('install', self.config, spm_file)
+
+        get_files = self.run_spm('files', self.config, 'apache')
+
+        files = ['apache.sls', 'bigfile.txt']
+        for sls in files:
+            self.assertIn(sls, ' '.join(get_files))
+
+    def test_spm_build_exclude(self):
+        '''
+        test spm build
+        '''
+        git_dir = os.path.join(self.formula_sls_dir, '.git')
+        os.makedirs(git_dir)
+        files = ['donotbuild1', 'donotbuild2', 'donotbuild3']
+
+        for git_file in files:
+            with salt.utils.fopen(os.path.join(git_dir, git_file), 'w') as fp:
+                fp.write('Please do not include me in build')
+
+        build_spm = self.run_spm('build', self.config, self.formula_dir)
+        spm_file = os.path.join(self.config['spm_build_dir'], 'apache-201506-2.spm')
+        install = self.run_spm('install', self.config, spm_file)
+
+        get_files = self.run_spm('files', self.config, 'apache')
+
+        for git_file in files:
+            self.assertNotIn(git_file, ' '.join(get_files))
 
     def tearDown(self):
         shutil.rmtree(self._tmp_spm)
