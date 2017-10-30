60185f578eebbdf5a0cdaec9da3539b50c656a33
Bo Maryniuk <bo@suse.de>
2017-10-30 16:47:58 +0100

    Add unit test for file.line usage together with before/after

--- tests/unit/modules/test_file.py
+++ tests/unit/modules/test_file.py
@@ -936,6 +936,29 @@ class FilemodLineTests(TestCase, LoaderModuleMockMixin):
                 assert 1 == len(atomic_opener().write.call_args_list)
                 assert file_modified == atomic_opener().write.call_args_list[0][0][0]
 
+    @patch('os.path.realpath', MagicMock())
+    @patch('os.path.isfile', MagicMock(return_value=True))
+    @patch('os.stat', MagicMock())
+    def test_line_insert_before_after(self):
+        '''
+        Test for file.line for insertion before specific line, using pattern and no patterns.
+
+        See issue #38670
+        :return:
+        '''
+        file_content = 'file_roots:\n  base:\n    - /srv/salt\n    - /srv/pepper\n    - /srv/sugar'
+        file_modified = 'file_roots:\n  base:\n    - /srv/salt\n    ' \
+                        '- /srv/pepper\n    - /srv/coriander\n    - /srv/sugar'
+        cfg_content = '- /srv/coriander'
+        for b_line, a_line in [('/srv/sugar', '/srv/salt')]:
+            files_fopen = mock_open(read_data=file_content)
+            with patch('salt.utils.files.fopen', files_fopen):
+                atomic_opener = mock_open()
+                with patch('salt.utils.atomicfile.atomic_open', atomic_opener):
+                    filemod.line('foo', content=cfg_content, before=b_line, after=a_line, mode='insert')
+                assert 1 == len(atomic_opener().write.call_args_list)
+                assert file_modified == atomic_opener().write.call_args_list[0][0][0]
+
 
 class FileBasicsTestCase(TestCase, LoaderModuleMockMixin):
     def setup_loader_modules(self):
