fed4849eed03e57d829d8c0f52efe72cd369de27
Tom Williams <tom.williams@hotschedules.com>
2017-11-13 23:32:46 -0500

    INFRA-5980 - fix comment application

--- salt/states/boto_dynamodb.py
+++ salt/states/boto_dynamodb.py
@@ -287,7 +287,7 @@ def present(name=None,
     if not table_exists:
         if __opts__['test']:
             ret['result'] = None
-            comments.append('DynamoDB table {0} would be created.'.format(name))
+            ret['comment'] = 'DynamoDB table {0} would be created.'.format(name)
             return ret
         else:
             is_created = __salt__['boto_dynamodb.create_table'](
