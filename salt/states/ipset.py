361205d6c52f08e387e99f10ba7c0132809a4adc
skjaro <jaro@ttx.sk>
2017-11-17 22:43:07 +0100

    add new set types hash:mac and hash:ip,mark and create option skbinfo
    Fixes state discarding options with entries

--- salt/states/ipset.py
+++ salt/states/ipset.py
@@ -221,7 +221,7 @@ def present(name, entry=None, family='ipv4', **kwargs):
                     kwargs['set_name'],
                     family)
             else:
-                command = __salt__['ipset.add'](kwargs['set_name'], entry, family, **kwargs)
+                command = __salt__['ipset.add'](kwargs['set_name'], _entry, family, **kwargs)
                 if 'Error' not in command:
                     ret['changes'] = {'locale': name}
                     ret['comment'] += 'entry {0} added to set {1} for family {2}\n'.format(
@@ -293,7 +293,7 @@ def absent(name, entry=None, entries=None, family='ipv4', **kwargs):
                     kwargs['set_name'],
                     family)
             else:
-                command = __salt__['ipset.delete'](kwargs['set_name'], entry, family, **kwargs)
+                command = __salt__['ipset.delete'](kwargs['set_name'], _entry, family, **kwargs)
                 if 'Error' not in command:
                     ret['changes'] = {'locale': name}
                     ret['result'] = True
