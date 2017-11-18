ca3e3dce5013141545fd71ce9442ae4971dea309
Pieter E Smit <pieter.smit@gmail.co>
2017-11-18 21:12:43 +1300

    remove one space to test gpg signature.

--- salt/states/mongodb_user.py
+++ salt/states/mongodb_user.py
@@ -112,8 +112,8 @@ def present(name,
     users = __salt__['mongodb.user_find'](name, user, password, host, port, database, authdb)
     if len(users) > 0:
         # check for errors returned in users e.g.
-        #     users= (False, 'Failed to connect to MongoDB database localhost:27017')
-        #     users= (False, 'not authorized on admin to execute command { usersInfo: "root" }')
+        #    users= (False, 'Failed to connect to MongoDB database localhost:27017')
+        #    users= (False, 'not authorized on admin to execute command { usersInfo: "root" }')
         if not users[0]:
             ret['result'] = False
             ret['comment'] = "Mongo Err: "+str(users[1])
