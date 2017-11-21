265de8e61cd362491e1acac0ef552f6fe0962fae
Bo Maryniuk <bo@suse.de>
2017-11-21 12:53:11 +0100

    Bugfix the logic according to the exact described purpose of the function.

--- salt/modules/state.py
+++ salt/modules/state.py
@@ -104,12 +104,7 @@ def _check_pillar(kwargs, pillar=None):
     Check the pillar for errors, refuse to run the state if there are errors
     in the pillar and return the pillar errors
     '''
-    if kwargs.get('force'):
-        return True
-    pillar_dict = pillar if pillar is not None else __pillar__
-    if '_errors' in pillar_dict:
-        return False
-    return True
+    return None if kwargs.get('force') else (pillar or {}).get('_errors', __pillar__.get('_errors')) or None
 
 
 def _wait(jid):
