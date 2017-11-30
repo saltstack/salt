0bc7c98642a984a5111cb43a1625caf830540074
twangboy <slee@saltstack.com>
2017-11-30 13:53:19 -0700

    Add upgraded state for chocolatey

--- salt/states/chocolatey.py
+++ salt/states/chocolatey.py
@@ -262,3 +262,167 @@ def uninstalled(name, version=None, uninstall_args=None, override_args=False):
     ret['changes'] = salt.utils.data.compare_dicts(pre_uninstall, post_uninstall)
 
     return ret
+
+
+def upgraded(name,
+             version=None,
+             source=None,
+             force=False,
+             pre_versions=False,
+             install_args=None,
+             override_args=False,
+             force_x86=False,
+             package_args=None):
+    '''
+    Upgrades a package. Will install the package if not installed.
+
+    .. versionadded: 2017.7.3
+
+    Args:
+
+        name (str):
+            The name of the package to be installed. Required.
+
+        version (str):
+            Install a specific version of the package. Defaults to latest
+            version. If the version is greater than the one installed then the
+            specified version will be installed. Default is None.
+
+        source (str):
+            Chocolatey repository (directory, share or remote URL, feed).
+            Defaults to the official Chocolatey feed. Default is None.
+
+        force (bool):
+            Reinstall the current version of an existing package. Do not use
+            with ``allow_multiple``. Default is False.
+
+        pre_versions (bool):
+            Include pre-release packages. Default is False.
+
+        install_args (str):
+            Install arguments you want to pass to the installation process, i.e
+            product key or feature list. Default is None.
+
+        override_args (bool):
+            Set to True if you want to override the original install arguments
+            (for the native installer) in the package and use your own. When
+            this is set to False install_args will be appended to the end of the
+            default arguments. Default is False.
+
+        force_x86 (bool):
+            Force x86 (32bit) installation on 64 bit systems. Default is False.
+
+        package_args (str):
+            Arguments you want to pass to the package. Default is None.
+
+    .. code-block:: yaml
+
+        Installsomepackage:
+          chocolatey.installed:
+            - name: packagename
+            - version: '12.04'
+            - source: 'mychocolatey/source'
+            - force: True
+    '''
+    ret = {'name': name,
+           'result': True,
+           'changes': {},
+           'pchanges': {},
+           'comment': ''}
+
+    # Get list of currently installed packages
+    pre_install = __salt__['chocolatey.list'](local_only=True)
+
+    # Determine if there are changes
+    # Package not installed
+    if name.lower() not in [package.lower() for package in pre_install.keys()]:
+        if version:
+            ret['pchanges'] = {name: 'Version {0} will be installed'
+                                     ''.format(version)}
+        else:
+            ret['pchanges'] = {name: 'Latest version will be installed'}
+
+    # Package installed
+    else:
+        version_info = __salt__['chocolatey.version'](name, check_remote=True)
+
+        # Get the actual full name out of version_info
+        full_name = name
+        for pkg in version_info:
+            if name.lower() == pkg.lower():
+                full_name = pkg
+
+        installed_version = version_info[full_name]['installed'][0]
+
+        # If version is not passed, use available... if available is available
+        if not version:
+            if 'available' in version_info[full_name]:
+                version = version_info[full_name]['available'][0]
+
+        if version:
+
+            # If installed version and new version are the same
+            if salt.utils.compare_versions(
+                    ver1=installed_version,
+                    oper="==",
+                    ver2=version):
+                if force:
+                    ret['pchanges'] = {
+                        name: 'Version {0} will be reinstalled'.format(version)}
+                    ret['comment'] = 'Reinstall {0} {1}' \
+                                     ''.format(full_name, version)
+                else:
+                    ret['comment'] = '{0} {1} is already installed' \
+                                     ''.format(name, version)
+            else:
+                # If installed version is older than new version
+                if salt.utils.compare_versions(
+                        ver1=installed_version, oper="<", ver2=version):
+                    ret['pchanges'] = {
+                        name: 'Version {0} will be upgraded to Version {1} '
+                              ''.format(installed_version, version)}
+                    ret['comment'] = 'Upgrade {0} {1} to {2}' \
+                                     ''.format(full_name, installed_version, version)
+                # If installed version is newer than new version
+                else:
+                    ret['comment'] = '{0} {1} is already installed (newer)' \
+                                     ''.format(name, version)
+
+        # Catch all for a condition where version is not passed and there is no
+        # available version
+        else:
+            ret['comment'] = 'No version found to install'
+
+    # Return if there are no changes to be made
+    if not ret['pchanges']:
+        ret['result'] = True
+        return ret
+
+    # Return if `test=True`
+    if __opts__['test']:
+        return ret
+
+    # Install the package
+    result = __salt__['chocolatey.upgrade'](name=name,
+                                            version=version,
+                                            source=source,
+                                            force=force,
+                                            pre_versions=pre_versions,
+                                            install_args=install_args,
+                                            override_args=override_args,
+                                            force_x86=force_x86,
+                                            package_args=package_args)
+
+    if 'Running chocolatey failed' not in result:
+        ret['comment'] = 'Package {0} upgraded successfully'.format(name)
+        ret['result'] = True
+    else:
+        ret['comment'] = 'Failed to upgrade the package {0}'.format(name)
+        ret['result'] = False
+
+    # Get list of installed packages after 'chocolatey.install'
+    post_install = __salt__['chocolatey.list'](local_only=True)
+
+    ret['changes'] = salt.utils.compare_dicts(pre_install, post_install)
+
+    return ret
