8160067d97e1189071240cab273c247ee7eb2ddb
Mircea Ulinic <mircea@cloudflare.com>
2017-12-02 01:12:36 +0000

    Add dummy liquid yaml

---
+++ salt/liquid/yaml.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+'''
+'''
+from __future__ import absolute_import
+
+# Import salt libs
+__virtualname__ = 'yaml'
+
+
+def fetch(**kwargs):
+    '''
+    '''
+    return {
+        'some_test_data_From_the_liquid_yaml': True
+    }
