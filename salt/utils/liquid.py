51e92cfff14b1d7c3428a80b138413119ce3ea85
Mircea Ulinic <mircea@cloudflare.com>
2017-12-02 01:11:28 +0000

    Add logging and pass only kwargs

--- salt/utils/liquid.py
+++ salt/utils/liquid.py
@@ -3,9 +3,12 @@
 Utilities for the Liquid subsystem
 '''
 from __future__ import absolute_import
+import logging
 
 # Import salt libs
 import salt.loader
+
+log = logging.getLogger(__name__)
 
 
 def fetch(opts, utils=None, **kwargs):
@@ -17,4 +20,4 @@ def fetch(opts, utils=None, **kwargs):
     driver = 'yaml'  # TODO: get the driver name from the opts
     fun = '{0}.get'.format(driver)
     loaded_lq = salt.loader.liquid(opts, fun, utils=utils)
-    return loaded_lq[fun](driver, **kwargs)
+    return loaded_lq[fun](**kwargs)
