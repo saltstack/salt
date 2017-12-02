1bd66702f822e12bd75c4bc5ab2cde8a37cad200
Mircea Ulinic <mircea@cloudflare.com>
2017-12-02 00:53:37 +0000

    Initial liquid execution module

---
+++ salt/modules/liquid.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+'''
+Module to return data fetched using the Liquid system
+=====================================================
+'''
+from __future__ import absolute_import
+
+# Import salt libs
+import salt.utils.args
+import salt.utils.liquid
+
+
+def fetch(**kwargs):
+    '''
+    '''
+    clean_kwargs = salt.utils.clean_kwargs(**kwargs)
+    return salt.utils.liquid.fetch(__opts__, utils=__utils__, **clean_kwargs)
