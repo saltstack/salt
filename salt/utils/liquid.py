0de4fc64c16ef45a739a6101873568742f29dc52
Mircea Ulinic <mircea@cloudflare.com>
2017-12-02 13:37:28 +0000

    Non-hardcoded liquid uitls

--- salt/utils/liquid.py
+++ salt/utils/liquid.py
@@ -14,10 +14,39 @@ log = logging.getLogger(__name__)
 def fetch(opts, utils=None, **kwargs):
     '''
     Fetch the configuration options using the liquid module.
+
+    Opts syntax:
+
+    .. code-block:: yaml
+
+        liquid:
+          - yaml:
+              path: https://example.com/yaml-api
+          - mongo:
+              host: example.com
+              port: 49017
+              db: common_opts
+          - mongo:
+              host: example.com
+              port: 49017
+              db: only_master_opts
     '''
+    if 'liquid' not in opts:
+        return {}
+    if not isinstance(opts['liquid'], list):
+        log.error('Liquid opts must be a list')
+        return {}
     if utils is None:
         utils = {}
-    driver = 'yaml'  # TODO: get the driver name from the opts
-    fun = '{0}.fetch'.format(driver)
-    loaded_lq = salt.loader.liquid(opts, fun, utils=utils)
-    return loaded_lq[fun](**kwargs)
+    opts = {}
+    for liquid_driver in opts['liquid']:
+        if not isinstance(liquid_driver, dict):
+            log.warning('Liquid opts must be a list of dicts')
+            return {}
+        driver_name = list(liquid_driver.keys())[0]
+        driver_kwargs = liquid_driver[driver_name]
+        fun = '{0}.fetch'.format(driver_name)
+        loaded_lq = salt.loader.liquid(opts, fun, utils=utils)
+        driver_opts = loaded_lq[fun](**driver_kwargs)
+        opts.update(driver_opts)
+    return opts
