b0bf27a09a95c34b953aeef94ff5524d8e033d55
Gareth J. Greenaway <gareth@wiked.org>
2018-01-03 08:37:38 -0800

    Updating the six import to use salt version of six.  Reverting a change in the adb beacon that broke tests.

--- salt/beacons/bonjour_announce.py
+++ salt/beacons/bonjour_announce.py
@@ -8,8 +8,9 @@ from __future__ import absolute_import
 import atexit
 import logging
 import select
-import six
 import time
+
+from salt.ext import six
 from salt.ext.six.moves import map
 
 # Import 3rd Party libs
