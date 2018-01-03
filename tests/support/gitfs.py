b868b0a07a8c878b96ac4e78b6a99bb0add89fbc
Erik Johnson <palehose@gmail.com>
2018-01-03 17:19:53 -0600

    [PY3] Add unicode_literals to ext_pillar modules

--- tests/support/gitfs.py
+++ tests/support/gitfs.py
@@ -4,7 +4,7 @@ Base classes for gitfs/git_pillar integration tests
 '''
 
 # Import python libs
-from __future__ import absolute_import
+from __future__ import absolute_import, print_function, unicode_literals
 import copy
 import errno
 import logging
