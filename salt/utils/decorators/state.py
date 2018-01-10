40a18d111952a6bac82c240a008d6f423b5e6838
Bo Maryniuk <bo@suse.de>
2018-01-10 15:15:51 +0100

    Use unicode literals

--- salt/utils/decorators/state.py
+++ salt/utils/decorators/state.py
@@ -5,7 +5,7 @@ Decorators for salt.state
 :codeauthor: :email:`Bo Maryniuk (bo@suse.de)`
 '''
 
-from __future__ import absolute_import
+from __future__ import absolute_import, unicode_literals
 from salt.exceptions import SaltException
 
 
