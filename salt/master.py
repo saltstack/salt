6199131d9ebc747498813d6c3a5908f90819101b
Bo Maryniuk <bo@suse.de>
2018-01-12 16:22:07 +0100

    Lintfix W0611

--- salt/master.py
+++ salt/master.py
@@ -86,7 +86,6 @@ import salt.utils.verify
 import salt.utils.zeromq
 from salt.config import DEFAULT_INTERVAL
 from salt.defaults import DEFAULT_TARGET_DELIM
-from salt.config import DEFAULT_MASTER_OPTS
 from salt.exceptions import FileserverConfigError
 from salt.transport import iter_transport_opts
 from salt.utils.debug import (
