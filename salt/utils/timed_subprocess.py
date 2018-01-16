ef44ca977a724c207c64507592532b664b728854
Erik Johnson <palehose@gmail.com>
2018-01-16 16:47:41 -0600

    [PY2] timed_subprocess: ensure custom env vars do not contain unicode
    subprocess will use `ascii` to encode them which will cause trouble if
    any actual unicode content is present.

--- salt/utils/timed_subprocess.py
+++ salt/utils/timed_subprocess.py
@@ -8,6 +8,7 @@ import shlex
 import subprocess
 import threading
 import salt.exceptions
+import salt.utils.data
 from salt.ext import six
 
 
@@ -66,6 +67,10 @@ class TimedProc(object):
                     kwargs['env'][key] = str(val)
                 if not isinstance(key, six.string_types):
                     kwargs['env'][str(key)] = kwargs['env'].pop(key)
+            if six.PY2 and 'env' in kwargs:
+                # Ensure no unicode in custom env dict, as it can cause
+                # problems with subprocess.
+                kwargs['env'] = salt.utils.data.encode_dict(kwargs['env'])
             self.process = subprocess.Popen(args, **kwargs)
         self.command = args
 
