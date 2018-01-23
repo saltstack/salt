4fa1a157902e34a63fbc812fd763d094ae0709c1
rallytime <nicole@saltstack.com>
2018-01-23 11:47:40 -0500

    Lint fixes

--- salt/states/pagerduty_escalation_policy.py
+++ salt/states/pagerduty_escalation_policy.py
@@ -38,9 +38,6 @@ For example:
 # Import Python libs
 from __future__ import absolute_import, print_function, unicode_literals
 
-# Import Salt libs
-from salt.ext import six
-
 
 def __virtual__():
     '''
