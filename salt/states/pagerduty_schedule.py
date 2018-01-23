a7460cebc6fe3455d60a1de9cc2d6f31075482f5
rallytime <nicole@saltstack.com>
2018-01-23 11:47:40 -0500

    Lint fixes

--- salt/states/pagerduty_schedule.py
+++ salt/states/pagerduty_schedule.py
@@ -37,9 +37,6 @@ Example:
 # Import Python libs
 from __future__ import absolute_import, print_function, unicode_literals
 
-# Import Salt libs
-from salt.ext import six
-
 
 def __virtual__():
     '''
