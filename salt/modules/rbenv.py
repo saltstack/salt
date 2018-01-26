54a69bca302b6dc6f72561a896cc6cd2116e4f59
Erik Johnson <palehose@gmail.com>
2018-01-26 08:41:39 -0600

    Add comment to explain PATH handling
    Also use salt.utils.path.join()

--- salt/modules/rbenv.py
+++ salt/modules/rbenv.py
@@ -19,6 +19,7 @@ import logging
 # Import Salt libs
 import salt.utils.args
 import salt.utils.data
+import salt.utils.path
 import salt.utils.platform
 from salt.exceptions import SaltInvocationError
 
@@ -380,11 +381,14 @@ def do(cmdline, runas=None, env=None):
     if not env:
         env = {}
 
+    # NOTE: Env vars (and their values) need to be str type on both Python 2
+    # and 3. The code below first normalizes all path components to unicode to
+    # stitch them together, and then converts the result back to a str type.
     env[str('PATH')] = salt.utils.stringutils.to_str(   # future lint: disable=blacklisted-function
-        '{0}/shims:{1}'.format(
-            path,
+        os.pathsep.join((
+            salt.utils.path.join(path, 'shims'),
             salt.utils.stringutils.to_unicode(os.environ['PATH'])
-        )
+        ))
     )
 
     try:
