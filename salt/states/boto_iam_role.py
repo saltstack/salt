103157129f0927b02981b8511633d2cf36606d6c
Herbert Buurman <abcde@ilostthegame.eu>
2018-02-08 09:54:27 +0100

    salt/states/boto_iam_role.py: Reinstated _sort_policy with added support for OrderedDict.

--- salt/states/boto_iam_role.py
+++ salt/states/boto_iam_role.py
@@ -89,6 +89,7 @@ on the IAM role to be persistent. This functionality was added in 2015.8.0.
 from __future__ import absolute_import, print_function, unicode_literals
 import logging
 import salt.utils.dictupdate as dictupdate
+from salt.utils.odict import OrderedDict
 import salt.utils.dictdiffer
 from salt.ext import six
 
@@ -272,13 +273,15 @@ def _role_present(
         if not policy_document:
             policy = __salt__['boto_iam.build_policy'](region, key, keyid,
                                                        profile)
-            if salt.utils.dictdiffer.deep_diff(role['assume_role_policy_document'],
-                                               policy):
+            if salt.utils.dictdiffer.deep_diff(
+                    _sort_policy(role['assume_role_policy_document']),
+                    _sort_policy(policy)):
                 update_needed = True
                 _policy_document = policy
         else:
-            if salt.utils.dictdiffer.deep_diff(role['assume_role_policy_document'],
-                                               policy_document):
+            if salt.utils.dictdiffer.deep_diff(
+                    _sort_policy(role['assume_role_policy_document']),
+                    _sort_policy(policy_document)):
                 update_needed = True
                 _policy_document = policy_document
         if update_needed:
@@ -356,6 +359,20 @@ def _instance_profile_associated(
             ret['result'] = False
             ret['comment'] = 'Failed to associate {0} instance profile with {0} role.'.format(name)
     return ret
+
+
+def _sort_policy(doc):
+    '''
+    List-type sub-items in policies don't happen to be order-sensitive, but
+    compare operations will render them unequal, leading to non-idempotent
+    state runs.  We'll sort any list-type subitems before comparison to reduce
+    the likelihood of false negatives.
+    '''
+    if isinstance(doc, list):
+        return sorted([_sort_policy(i) for i in doc])
+    elif isinstance(doc, (dict, OrderedDict)):
+        return dict([(k, _sort_policy(v)) for k, v in six.iteritems(doc)])
+    return doc
 
 
 def _policies_present(
