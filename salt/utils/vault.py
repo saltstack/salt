saltstack/salt

commit 6a5e0f9ac11e7731b759d7dcc873c07f6436ba1b

remove not from vault utils
If the role is 'master' then the vault configs should just be pulled from the
opts dictionary

salt/utils/vault.py
@@ -98,7 +98,7 @@ def _get_vault_connection():
     Get the connection details for calling Vault, from local configuration if
     it exists, or from the master otherwise
     '''
-    if 'vault' in __opts__ and not __opts__.get('__role', 'minion') == 'master':
+    if 'vault' in __opts__ and __opts__.get('__role', 'minion') == 'master':
         log.debug('Using Vault connection details from local config')
         try:
             return {

commit 6696f387b4939edb791321f740d952972bab0c6d

Fix regression in 6a5e0f
Fixes regression introduced in https://github.com/saltstack/salt/commit/6a5e0f9ac11e7731b759d7dcc873c07f6436ba1b and reported in https://github.com/saltstack/salt/issues/45976.

salt/utils/vault.py
@@ -96,7 +96,7 @@ def _get_vault_connection():
     Get the connection details for calling Vault, from local configuration if
     it exists, or from the master otherwise
     '''
-    if 'vault' in __opts__ and __opts__.get('__role', 'minion') == 'master':
+    if 'vault' in __opts__ and not __opts__.get('__role', 'minion') == 'master':
         log.debug('Using Vault connection details from local config')
         try:
             if __opts__['vault']['auth']['method'] == 'approle':