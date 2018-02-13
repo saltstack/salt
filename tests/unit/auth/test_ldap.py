1c349d4d9d791c4eaea26505b7c5b0205114df5d
Daniel Wallace <danielwallace@gtmanfred.com>
2018-02-13 07:45:13 -0700

    fix pylint

--- tests/unit/auth/test_ldap.py
+++ tests/unit/auth/test_ldap.py
@@ -2,14 +2,30 @@
 
 # Import python libs
 from __future__ import absolute_import
-import yaml
 
 # Import Salt Libs
 import salt.auth.ldap
 
 # Import Salt Testing Libs
-from tests.support.mock import MagicMock, patch, NO_MOCK, NO_MOCK_REASON
+from tests.support.mock import patch, NO_MOCK, NO_MOCK_REASON
 from tests.support.unit import skipIf, TestCase
+
+salt.auth.ldap.__opts__ = {}
+
+
+class Bind(object):
+    '''
+    fake search_s return
+    '''
+
+    @staticmethod
+    def search_s(*args, **kwargs):
+        return [
+            (
+                'cn=saltusers,cn=groups,cn=compat,dc=saltstack,dc=com',
+                {'memberUid': [b'saltuser'], 'cn': [b'saltusers']},
+            ),
+        ]
 
 
 @skipIf(NO_MOCK, NO_MOCK_REASON)
@@ -19,22 +35,8 @@ class LDAPAuthTestCase(TestCase):
     Unit tests for salt.auth.ldap
     '''
 
-    class Bind(object):
-        '''
-        fake search_s return
-        '''
-
-        @staticmethod
-        def search_s(*args, **kwargs):
-            return [
-                (
-                    'cn=saltusers,cn=groups,cn=compat,dc=saltstack,dc=com',
-                    {'memberUid': [b'saltuser'], 'cn': [b'saltusers']},
-                ),
-            ]
-
     def setUp(self):
-        salt.auth.ldap.__opts__ = {
+        self.opts = {
             'auth.ldap.binddn': 'uid={{username}},cn=users,cn=compat,dc=saltstack,dc=com',
             'auth.ldap.port': 389,
             'auth.ldap.tls': False,
@@ -46,37 +48,41 @@ class LDAPAuthTestCase(TestCase):
             'auth.ldap.group_filter': '(&(memberUid={{ username }})(objectClass=posixgroup))'}
 
     def tearDown(self):
-        salt.auth.ldap.__opts__['auth.ldap.freeipa'] = False
-        salt.auth.ldap.__opts__['auth.ldap.activedirectory'] = False
+        self.opts['auth.ldap.freeipa'] = False
+        self.opts['auth.ldap.activedirectory'] = False
 
     def test_config(self):
         '''
         Test that the _config function works correctly
         '''
-        self.assertEqual(salt.auth.ldap._config('basedn'), 'dc=saltstack,dc=com')
-        self.assertEqual(salt.auth.ldap._config('group_filter'), '(&(memberUid={{ username }})(objectClass=posixgroup))')
-        self.assertEqual(salt.auth.ldap._config('accountattributename'), 'memberUid')
-        self.assertEqual(salt.auth.ldap._config('groupattribute'), 'memberOf')
+        with patch.dict(salt.auth.ldap.__opts__, self.opts):
+            self.assertEqual(salt.auth.ldap._config('basedn'), 'dc=saltstack,dc=com')
+            self.assertEqual(salt.auth.ldap._config('group_filter'), '(&(memberUid={{ username }})(objectClass=posixgroup))')
+            self.assertEqual(salt.auth.ldap._config('accountattributename'), 'memberUid')
+            self.assertEqual(salt.auth.ldap._config('groupattribute'), 'memberOf')
 
     def test_groups_freeipa(self):
         '''
         test groups in freeipa
         '''
-        salt.auth.ldap.__opts__['auth.ldap.freeipa'] = True
-        with patch('salt.auth.ldap.auth', return_value=Bind):
-            self.assertIn('saltusers', salt.auth.ldap.groups('saltuser', password='password'))
+        self.opts['auth.ldap.freeipa'] = True
+        with patch.dict(salt.auth.ldap.__opts__, self.opts):
+            with patch('salt.auth.ldap.auth', return_value=Bind):
+                self.assertIn('saltusers', salt.auth.ldap.groups('saltuser', password='password'))
 
     def test_groups(self):
         '''
         test groups in ldap
         '''
-        with patch('salt.auth.ldap.auth', return_value=Bind):
-            self.assertIn('saltusers', salt.auth.ldap.groups('saltuser', password='password'))
+        with patch.dict(salt.auth.ldap.__opts__, self.opts):
+            with patch('salt.auth.ldap.auth', return_value=Bind):
+                self.assertIn('saltusers', salt.auth.ldap.groups('saltuser', password='password'))
 
     def test_groups_activedirectory(self):
         '''
         test groups in activedirectory
         '''
-        salt.auth.ldap.__opts__['auth.ldap.activedirectory'] = True
-        with patch('salt.auth.ldap.auth', return_value=Bind):
-            self.assertIn('saltusers', salt.auth.ldap.groups('saltuser', password='password'))
+        self.opts['auth.ldap.activedirectory'] = True
+        with patch.dict(salt.auth.ldap.__opts__, self.opts):
+            with patch('salt.auth.ldap.auth', return_value=Bind):
+                self.assertIn('saltusers', salt.auth.ldap.groups('saltuser', password='password'))
