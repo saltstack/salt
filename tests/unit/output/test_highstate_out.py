98cfa1ff464c1008e5c08f02bd50836cd33de87c
Matt Phillips <mphillips81@bloomberg.net>
2018-02-13 19:32:25 -0500

    salt.outputter.highstate: recursion for orch
    orchestration / nested runners emitting to highstate were not rendered as
    highstate like a normal state apply. With this change, recurse and indent
    accordingly based on nested orchestrations.

--- tests/unit/output/test_highstate_out.py
+++ tests/unit/output/test_highstate_out.py
@@ -107,3 +107,93 @@ class JsonTestCase(TestCase, LoaderModuleMockMixin):
         self.assertIn('Succeeded: 1 (changed=1)', ret)
         self.assertIn('Failed:    0', ret)
         self.assertIn('Total states run:     1', ret)
+
+
+# this should all pass the above tests
+class JsonNestedTestCase(TestCase, LoaderModuleMockMixin):
+    '''
+    Test cases for nested salt.output.highstate (ie orchestrations calling other orchs)
+    '''
+    def setup_loader_modules(self):
+        return {
+            highstate: {
+                '__opts__': {
+                    'extension_modules': '',
+                    'color': False,
+                }
+            }
+        }
+
+    def setUp(self):
+        self.data = {
+            'outputter': 'highstate',
+            'data': {
+                'local_master': {
+                    'salt_|-nested_|-state.orchestrate_|-runner': {
+                        'comment': 'Runner function \'state.orchestrate\' executed.',
+                        'name': 'state.orchestrate',
+                        '__orchestration__': True,
+                        'start_time': '09:22:53.158742',
+                        'result': True,
+                        'duration': 980.694,
+                        '__run_num__': 0,
+                        '__jid__': '20180326092253538853',
+                        '__sls__': 'orch.test.nested',
+                        'changes': {
+                            'return': {
+                                'outputter': 'highstate',
+                                'data': {
+                                    'local_master': {
+                                        'test_|-always-passes-with-changes_|-oinaosf_|-succeed_with_changes': {
+                                            'comment': 'Success!',
+                                            'name': 'oinaosf',
+                                            'start_time': '09:22:54.128415',
+                                            'result': True,
+                                            'duration': 0.437,
+                                            '__run_num__': 0,
+                                            '__sls__': 'orch.test.changes',
+                                            'changes': {
+                                                'testing': {
+                                                    'new': 'Something pretended to change',
+                                                    'old': 'Unchanged'
+                                                }
+                                            },
+                                            '__id__': 'always-passes-with-changes'
+                                        },
+                                        'test_|-always-passes_|-fasdfasddfasdfoo_|-succeed_without_changes': {
+                                            'comment': 'Success!',
+                                            'name': 'fasdfasddfasdfoo',
+                                            'start_time': '09:22:54.128986',
+                                            'result': True,
+                                            'duration': 0.25,
+                                            '__run_num__': 1,
+                                            '__sls__': 'orch.test.changes',
+                                            'changes': {},
+                                            '__id__': 'always-passes'
+                                        }
+                                    }
+                                },
+                                'retcode': 0
+                            }
+                        },
+                        '__id__': 'nested'
+                    }
+                }
+            },
+            'retcode': 0
+        }
+
+        self.addCleanup(delattr, self, 'data')
+
+    def test_nested_output(self):
+        ret = highstate.output(self.data)
+        self.assertIn('Succeeded: 1 (changed=1)', ret)
+        self.assertIn('Failed:    0', ret)
+        self.assertIn('Total states run:     1', ret)
+
+        # the whitespace is relevant in this case, it is testing that it is nested
+        self.assertIn('                        ID: always-passes-with-changes', ret)
+        self.assertIn('                   Started: 09:22:54.128415', ret)
+        self.assertIn('              Succeeded: 2 (changed=1)', ret)
+        self.assertIn('              Failed:    0', ret)
+        self.assertIn('              Total states run:     2', ret)
