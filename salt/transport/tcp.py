f55cf542bec5962887b7f7333c4254dd0834bd81
Michael Gibson <michael.x.gibson@oracle.com>
2018-02-22 15:37:15 -0700

    fixing pylint error

--- salt/transport/tcp.py
+++ salt/transport/tcp.py
@@ -504,7 +504,7 @@ class AsyncTCPPubChannel(salt.transport.mixins.auth.AESPubClientMixin, salt.tran
             if not self.auth.authenticated:
                 yield self.auth.authenticate()
             if self.auth.authenticated:
-                if self.auth.creds != None:
+                if self.auth.creds is not None:
                     self.auth.creds['publish_port'] = self.opts['tcp_pub_port']
 
                 self.message_client = SaltMessageClientPool(
