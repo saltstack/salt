341a078341c35ddda6aa6169409f05ad6eba6321
Orlando Richards <orichards@pixitmedia.com>
2018-02-24 22:50:49 +0000

    Update influx_retention_policy state
    Fixes issue #46180 - update to influx_retention_policy state to manage the state of the retention period, replication setting, and default status of the retention policy.

--- salt/states/influxdb_retention_policy.py
+++ salt/states/influxdb_retention_policy.py
@@ -59,6 +59,41 @@ def present(name, database, duration="7d",
                 .format(name)
             ret['result'] = False
             return ret
+        
+    else:
+        current_policy = __salt__['influxdb.get_retention_policy'](database=database, name=name)
+        update_policy = False
+        if current_policy['duration'] != convert_duration(duration):
+            update_policy = True
+            ret['changes']['duration'] = "Retention changed from {0} to {1}.".format(current_policy['duration'],duration)
+
+        if current_policy['replicaN'] != replication:
+            update_policy = True
+            ret['changes']['replication'] = "Replication changed from {0} to {1}.".format(current_policy['replicaN'],replication)
+
+        if current_policy['default'] != default:
+            update_policy = True
+            ret['changes']['default'] = "Default changed from {0} to {1}.".format(current_policy['default'],default)
+
+        if update_policy:
+            if __opts__['test']:
+                ret['result'] = None
+                ret['comment'] = ' {0} is present and set to be changed'\
+                    .format(name)
+                return ret
+            else:
+                if __salt__['influxdb.alter_retention_policy'](
+                    database, name,
+                    duration, replication, default, **client_args
+                ):
+                    ret['comment'] = 'retention policy {0} has been changed'\
+                        .format(name)
+                    return ret
+                else:
+                    ret['comment'] = 'Failed to update retention policy {0}'\
+                        .format(name)
+                    ret['result'] = False
+                    return ret
 
     return ret
 
