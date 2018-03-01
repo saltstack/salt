8924ff2955b752843f20dfa41832626c9967b859
JerzyX Drozdz <jerzyx.drozdz@intel.com>
2018-03-01 10:41:48 +0100

    _get_line_indent renamed to _set_line_indent

--- salt/modules/win_file.py
+++ salt/modules/win_file.py
@@ -46,20 +46,20 @@ import salt.utils.path
 import salt.utils.platform
 import salt.utils.user
 from salt.modules.file import (check_hash,  # pylint: disable=W0611
-        directory_exists, get_managed,
-        check_managed, check_managed_changes, source_list,
-        touch, append, contains, contains_regex, get_source_sum,
-        contains_glob, find, psed, get_sum, _get_bkroot, _mkstemp_copy,
-        get_hash, manage_file, file_exists, get_diff, line, list_backups,
-        __clean_tmp, check_file_meta, _binary_replace,
-        _splitlines_preserving_trailing_newline, restore_backup,
-        access, copy, readdir, read, rmdir, truncate, replace, delete_backup,
-        search, _get_flags, extract_hash, _error, _sed_esc, _psed,
-        RE_FLAG_TABLE, blockreplace, prepend, seek_read, seek_write, rename,
-        lstat, path_exists_glob, write, pardir, join, HASHES, HASHES_REVMAP,
-        comment, uncomment, _add_flags, comment_line, _regex_to_static,
-        _get_line_indent, apply_template_on_contents, dirname, basename,
-        list_backups_dir, _assert_occurrence, _starts_till)
+                               directory_exists, get_managed,
+                               check_managed, check_managed_changes, source_list,
+                               touch, append, contains, contains_regex, get_source_sum,
+                               contains_glob, find, psed, get_sum, _get_bkroot, _mkstemp_copy,
+                               get_hash, manage_file, file_exists, get_diff, line, list_backups,
+                               __clean_tmp, check_file_meta, _binary_replace,
+                               _splitlines_preserving_trailing_newline, restore_backup,
+                               access, copy, readdir, read, rmdir, truncate, replace, delete_backup,
+                               search, _get_flags, extract_hash, _error, _sed_esc, _psed,
+                               RE_FLAG_TABLE, blockreplace, prepend, seek_read, seek_write, rename,
+                               lstat, path_exists_glob, write, pardir, join, HASHES, HASHES_REVMAP,
+                               comment, uncomment, _add_flags, comment_line, _regex_to_static,
+                               _set_line_indent, apply_template_on_contents, dirname, basename,
+                               list_backups_dir, _assert_occurrence, _starts_till)
 from salt.modules.file import normpath as normpath_
 
 from salt.utils.functools import namespaced_function as _namespaced_function
