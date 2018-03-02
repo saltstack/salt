2f2464b64db88bbc3fdf4c02f7332018288aa3b2
Dmitry Kuzmenko <dmitry.kuzmenko@dsr-corporation.com>
2018-03-02 11:03:42 +0300

    Fix long int support for older msgpack versions.

--- salt/payload.py
+++ salt/payload.py
@@ -204,14 +204,40 @@ class Serial(object):
             # Nothing known exceptions found. Let msgpack raise it's own.
             return obj
 
-        if msgpack.version >= (0, 4, 0):
-            # msgpack only supports 'use_bin_type' starting in 0.4.0.
-            # Due to this, if we don't need it, don't pass it at all so
-            # that under Python 2 we can still work with older versions
-            # of msgpack.
-            return msgpack.dumps(msg, default=ext_type_encoder, use_bin_type=use_bin_type)
-        else:
-            return msgpack.dumps(msg, default=ext_type_encoder)
+        try:
+            if msgpack.version >= (0, 4, 0):
+                # msgpack only supports 'use_bin_type' starting in 0.4.0.
+                # Due to this, if we don't need it, don't pass it at all so
+                # that under Python 2 we can still work with older versions
+                # of msgpack.
+                return msgpack.dumps(msg, default=ext_type_encoder, use_bin_type=use_bin_type)
+            else:
+                return msgpack.dumps(msg, default=ext_type_encoder)
+        except (OverflowError, msgpack.exceptions.PackValueError):
+            # msgpack<=0.4.6 don't call ext encoder on very long integers raising the error instead.
+            # Convert any very long longs to strings and call dumps again.
+            def verylong_encoder(obj):
+                if isinstance(obj, dict):
+                    for key, value in six.iteritems(obj.copy()):
+                        obj[key] = verylong_encoder(value)
+                    return dict(obj)
+                elif isinstance(obj, (list, tuple)):
+                    obj = list(obj)
+                    for idx, entry in enumerate(obj):
+                        obj[idx] = verylong_encoder(entry)
+                    return obj
+                # A value of an Integer object is limited from -(2^63) upto (2^64)-1 by MessagePack
+                # spec. Here we care only of JIDs that are positive integers.
+                if isinstance(obj, six.integer_types) and obj >= pow(2, 64):
+                    return six.text_type(obj)
+                else:
+                    return obj
+
+            msg = verylong_encoder(msg)
+            if msgpack.version >= (0, 4, 0):
+                return msgpack.dumps(msg, default=ext_type_encoder, use_bin_type=use_bin_type)
+            else:
+                return msgpack.dumps(msg, default=ext_type_encoder)
 
     def dump(self, msg, fn_):
         '''
