498a2c6873f68cb0d099dc720fcdaec419a0bd92
twangboy <slee@saltstack.com>
2018-03-06 12:19:59 -0700

    Add comment on None Statement

--- salt/modules/reg.py
+++ salt/modules/reg.py
@@ -83,6 +83,7 @@ def _to_unicode(vdata):
     Converts from current users character encoding to unicode. Use this for
     parameters being pass to reg functions
     '''
+    # None does not convert to Unicode
     if vdata is None:
         return None
     return salt.utils.stringutils.to_unicode(vdata, 'utf-8')
