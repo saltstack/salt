19bd1d9db5d4418d3b488df0cad886eddddbaaba
Daniel Wallace <danielwallace@gtmanfred.com>
2018-03-14 08:40:09 -0600

    handle user-data for metadata grains

--- salt/grains/metadata.py
+++ salt/grains/metadata.py
@@ -48,9 +48,11 @@ def _search(prefix="latest/"):
     Recursively look up all grains in the metadata server
     '''
     ret = {}
-    linedata = http.query(os.path.join(HOST, prefix))
+    linedata = http.query(os.path.join(HOST, prefix), headers=True)
     if 'body' not in linedata:
         return ret
+    if linedata['headers'].get('Content-Type', 'text/plain') == 'application/octet-stream':
+        return linedata['body']
     for line in linedata['body'].split('\n'):
         if line.endswith('/'):
             ret[line[:-1]] = _search(prefix=os.path.join(prefix, line))
