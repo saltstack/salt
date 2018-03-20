3707fd862f0bfdd1a4af638d866ee54d26e9fa81
Bo Maryniuk <bo@suse.de>
2018-03-20 18:35:23 +0100

    Add unit test for missing dependencies on get_ext_tops

--- tests/unit/utils/test_thin.py
+++ tests/unit/utils/test_thin.py
@@ -15,7 +15,7 @@ from tests.support.mock import (
 from salt.ext.six.moves import zip
 from salt.ext import six
 
-import salt.utils.ssdp as ssdp
+from salt.utils import thin
 import salt.utils.stringutils
 
 try:
@@ -24,15 +24,34 @@ except ImportError:
     pytest = None
 
 
+class SaltSyetemExitException(Exception):
+    '''
+    System
+    '''
+    def __init__(self):
+        Exception.__init__(self, 'The Dilithium Crystals need to be rotated.')
+
+
 @skipIf(NO_MOCK, NO_MOCK_REASON)
 @skipIf(pytest is None, 'PyTest is missing')
 class SSHThinTestCase(TestCase):
     '''
     TestCase for SaltSSH-related parts.
     '''
-    def test_get_tops(self):
+    @patch('salt.exceptions.SaltSystemExit', MagicMock(side_effect=SaltSyetemExitException))
+    @patch('salt.utils.thin.log', MagicMock())
+    def test_get_ext_tops_cfg_missing_dependencies(self):
         '''
         Test thin.get_tops
 
         :return:
         '''
+        cfg = [
+            {'namespace': {'path': '/foo', 'dependencies': []}},
+        ]
+        with pytest.raises(Exception) as err:
+            thin.get_ext_tops(cfg)
+        assert 'Dilithium Crystals' in str(err)
+        assert thin.log.error.called
+        assert 'Missing dependencies' in thin.log.error.call_args[0][0]
+        assert 'jinja2, yaml, tornado, msgpack' in thin.log.error.call_args[0][0]
