19db8b248635746c8889b4d41c494a1676d28156
Pedro Algarvio <pedro@algarvio.me>
2018-03-22 16:21:37 +0000

    Allow getting a new schedule instance and not the singleton instance.

--- salt/utils/schedule.py
+++ salt/utils/schedule.py
@@ -75,23 +75,38 @@ class Schedule(object):
     '''
     instance = None
 
-    def __new__(cls, opts, functions, returners=None, intervals=None, cleanup=None, proxy=None, standalone=False):
+    def __new__(cls, opts, functions,
+                returners=None,
+                intervals=None,
+                cleanup=None,
+                proxy=None,
+                standalone=False,
+                new_instance=False):
         '''
         Only create one instance of Schedule
         '''
-        if cls.instance is None:
+        if cls.instance is None or new_instance is True:
             log.debug('Initializing new Schedule')
             # we need to make a local variable for this, as we are going to store
             # it in a WeakValueDictionary-- which will remove the item if no one
             # references it-- this forces a reference while we return to the caller
-            cls.instance = object.__new__(cls)
-            cls.instance.__singleton_init__(opts, functions, returners, intervals, cleanup, proxy, standalone)
+            instance = object.__new__(cls)
+            instance.__singleton_init__(opts, functions, returners, intervals, cleanup, proxy, standalone)
+            if new_instance is True:
+                return instance
+            cls.instance = instance
         else:
             log.debug('Re-using Schedule')
         return cls.instance
 
     # has to remain empty for singletons, since __init__ will *always* be called
-    def __init__(self, opts, functions, returners=None, intervals=None, cleanup=None, proxy=None, standalone=False):
+    def __init__(self, opts, functions,
+                 returners=None,
+                 intervals=None,
+                 cleanup=None,
+                 proxy=None,
+                 standalone=False,
+                 new_instance=False):
         pass
 
     # an init for the singleton instance to call
