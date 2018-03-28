c6116a83aa4f58c2adbe97c314212ab83196d8cd
rallytime <nicole@saltstack.com>
2018-03-28 09:58:13 -0400

    Add **kwargs back into grains and pillar funcs in cache runner
    This was removed during the deprecation removal process and shouldn't have been.

--- salt/runners/cache.py
+++ salt/runners/cache.py
@@ -30,7 +30,7 @@ __func_alias__ = {
 }
 
 
-def grains(tgt=None, tgt_type='glob'):
+def grains(tgt=None, tgt_type='glob', **kwargs):
     '''
     .. versionchanged:: 2017.7.0
         The ``expr_form`` argument has been renamed to ``tgt_type``, earlier
@@ -52,7 +52,7 @@ def grains(tgt=None, tgt_type='glob'):
     return cached_grains
 
 
-def pillar(tgt=None, tgt_type='glob'):
+def pillar(tgt=None, tgt_type='glob', **kwargs):
     '''
     .. versionchanged:: 2017.7.0
         The ``expr_form`` argument has been renamed to ``tgt_type``, earlier
