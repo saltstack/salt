3d288c44d46c4eec256b2eba62d871b4df75154e
Gareth J. Greenaway <gareth@wiked.org>
2018-03-30 14:47:47 -0700

    Fixing test documentation

--- tests/integration/modules/test_state.py
+++ tests/integration/modules/test_state.py
@@ -1349,13 +1349,11 @@ class StateModuleTest(ModuleCase, SaltReturnAssertsMixin):
 
     def test_issue_46762_prereqs_on_a_state_with_unfulfilled_requirements(self):
         '''
-        This tests the case where require, order, and failhard are all used together in a state definition.
+        This tests the case where state C requires state A which fails.
+        State C is a prereq for State B.
 
-        Previously, the order option, which used in tandem with require and failhard, would cause the state
-        compiler to stacktrace. This exposed a logic error in the ``check_failhard`` function of the state
-        compiler. With the logic error resolved, this test should now pass.
-
-        See https://github.com/saltstack/salt/issues/38683 for more information.
+        See https://github.com/saltstack/salt/issues/46762 for
+        more information.
         '''
         state_run = self.run_function(
             'state.sls',
