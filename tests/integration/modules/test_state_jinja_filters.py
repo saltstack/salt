c8945e4b2eab462a98994261a7d5896abf26f2c1
Gareth J. Greenaway <gareth@wiked.org>
2018-04-03 15:02:35 -0700

    cleaning up some imports.

--- tests/integration/modules/test_state_jinja_filters.py
+++ tests/integration/modules/test_state_jinja_filters.py
@@ -5,8 +5,6 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 # Import Salt Testing libs
 from tests.support.case import ModuleCase
-
-# Import Salt libs
 from tests.support.jinja_filters import JinjaFiltersTest
 
 
--- tests/integration/ssh/test_jinja_filters.py
+++ tests/integration/ssh/test_jinja_filters.py
@@ -1,6 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# Import Python libs
+from __future__ import absolute_import, print_function, unicode_literals
+
 # Import Salt Testing Libs
 from tests.support.case import SSHCase
-
 from tests.support.jinja_filters import JinjaFiltersTest
 
 
