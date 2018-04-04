saltstack/salt

commit 002aa88a97ed8a1c51f4ab1c965d22d064ea30a8

Replace yaml usage with a helper to ensure unicode is handled properly
Without allow_unicode=True, unicode characters are processed through the
str representer and on Python 2 are dumped as a Unicode code point (i.e.
a literal \u0414). This commit makes allow_unicode=True the default in
our salt.utils.yamlloader.safe_dump() helper. It also adds a new
salt.utils.yamlloader.dump() helper which wraps yaml.dump() and also
makes allow_unicode=True the default.

To make importing and using our custom yaml loader/dumper easier, a
convenience module called salt.utils.yaml has been added, which does a
wildcard import from both salt.utils.yamldumper and
salt.utils.yamlloader.

Refs to yaml.load/dump and yaml.safe_load/safe_dump have been updated to
salt.utils.yaml, to ensure that unicode is handled properly.

salt/tops/ext_nodes.py
@@ -47,12 +47,12 @@ The above essentially is the same as a top.sls containing the following:
 '''
 from __future__ import absolute_import, print_function, unicode_literals
 
-# Import python libs
+# Import Python libs
 import logging
 import subprocess
 
-# Import third party libs
-import yaml
+# Import Salt libs
+import salt.utils.yaml
 
 log = logging.getLogger(__name__)
 
@@ -76,12 +76,12 @@ def top(**kwargs):
             __opts__['master_tops']['ext_nodes'],
             kwargs['opts']['id']
             )
-    ndata = yaml.safe_load(
-            subprocess.Popen(
-                cmd,
-                shell=True,
-                stdout=subprocess.PIPE
-                ).communicate()[0])
+    ndata = salt.utils.yaml.safe_load(
+        subprocess.Popen(
+            cmd,
+            shell=True,
+            stdout=subprocess.PIPE.communicate()[0])
+    )
     if not ndata:
         log.info('master_tops ext_nodes call did not return any data')
     ret = {}

commit 3655569322389b2d7932a13d056110fd94c911d5

fixed top function which was broken since commit 002aa88a97e
```
2018-04-04 12:49:51,312 [salt.daemons.masterapi:540 ][ERROR   ][11108] Top function ext_nodes failed with error 'int' object has no attribute 'communicate' for minion random.system.local
```

salt/tops/ext_nodes.py
@@ -80,7 +80,7 @@ def top(**kwargs):
         subprocess.Popen(
             cmd,
             shell=True,
-            stdout=subprocess.PIPE.communicate()[0])
+            stdout=subprocess.PIPE).communicate()[0]
     )
     if not ndata:
         log.info('master_tops ext_nodes call did not return any data')