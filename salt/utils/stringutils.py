5d98a8bedd3f73d1921f4d8c357531c8f3a61093
Erik Johnson <palehose@gmail.com>
2018-04-12 22:12:48 -0500

    Make salt.utils.stringutils.to_binary work for bytestrings
    Also make sure we're testing bytestring equivalents to each of the
    strings in the test.

--- salt/utils/stringutils.py
+++ salt/utils/stringutils.py
@@ -184,19 +184,27 @@ def is_binary(data):
     '''
     Detects if the passed string of data is binary or text
     '''
-    if not data or not isinstance(data, six.string_types):
+    if not data or not isinstance(data, (six.string_types, six.binary_type)):
         return False
-    if str('\0') in data:
+
+    if isinstance(data, six.binary_type):
+        if b'\0' in data:
+            return True
+    elif str('\0') in data:
         return True
 
     text_characters = ''.join([chr(x) for x in range(32, 127)] + list('\n\r\t\b'))
     # Get the non-text characters (map each character to itself then use the
     # 'remove' option to get rid of the text characters.)
     if six.PY3:
-        trans = ''.maketrans('', '', text_characters)
-        nontext = data.translate(trans)
+        if isinstance(data, six.binary_type):
+            import salt.utils.data
+            nontext = data.translate(None, salt.utils.data.encode(text_characters))
+        else:
+            trans = ''.maketrans('', '', text_characters)
+            nontext = data.translate(trans)
     else:
-        if isinstance(data, unicode):  # pylint: disable=incompatible-py3-code
+        if isinstance(data, six.text_type):
             trans_args = ({ord(x): None for x in text_characters},)
         else:
             trans_args = (None, str(text_characters))  # future lint: blacklisted-function
