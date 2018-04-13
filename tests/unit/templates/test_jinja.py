46fa2c04de17597133daa5d8d0ef846cb158c4d0
Daniel A. Wozniak <dwozniak@saltstack.com>
2018-04-13 14:29:36 -0700

    Fix py3 os.linesep wart

--- tests/unit/templates/test_jinja.py
+++ tests/unit/templates/test_jinja.py
@@ -44,6 +44,7 @@ except ImportError:
     HAS_TIMELIB = False
 
 TEMPLATES_DIR = os.path.dirname(os.path.abspath(__file__))
+BLINESEP = salt.utils.to_bytes(os.linesep)
 
 
 class MockFileClient(object):
@@ -69,10 +70,10 @@ def _setup_test_dir(src_dir, test_dir):
     salt.utils.files.recursive_copy(src_dir, test_dir)
     filename = os.path.join(test_dir, 'non_ascii')
     with salt.utils.fopen(filename, 'wb') as fp:
-        fp.write(b'Assun\xc3\xa7\xc3\xa3o' + os.linesep)
+        fp.write(b'Assun\xc3\xa7\xc3\xa3o' + BLINESEP)
     filename = os.path.join(test_dir, 'hello_simple')
     with salt.utils.fopen(filename, 'wb') as fp:
-        fp.write(b'world' + os.linesep)
+        fp.write(b'world' + BLINESEP)
     filename = os.path.join(test_dir, 'hello_import')
     lines = [
         r"{% from 'macro' import mymacro -%}",
@@ -81,7 +82,7 @@ def _setup_test_dir(src_dir, test_dir):
     ]
     with salt.utils.fopen(filename, 'wb') as fp:
         for line in lines:
-            fp.write(line.encode('utf-8') + os.linesep)
+            fp.write(line.encode('utf-8') + BLINESEP)
 
 
 class TestSaltCacheLoader(TestCase):
