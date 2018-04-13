7b7dc946108c66a4dbc3916790976fad20dc738b
Erik Johnson <palehose@gmail.com>
2018-04-12 22:16:34 -0500

    Use salt.utils.stringutils.is_binary to check if contents are binary

--- salt/states/file.py
+++ salt/states/file.py
@@ -2269,9 +2269,7 @@ def managed(name,
                 .format(contents_id)
             )
 
-        contents_are_binary = \
-            isinstance(use_contents, six.string_types) and str('\0') in use_contents
-        if contents_are_binary:
+        if salt.utils.stringutils.is_binary(use_contents):
             contents = use_contents
         else:
             validated_contents = _validate_str_list(use_contents)
