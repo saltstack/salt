6c089c9de1fd6fd6366f7e27cb524f01e997f870
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2018-04-25 12:55:36 +0100

    Fix unsupported chars checking on GECOS fields

--- salt/states/user.py
+++ salt/states/user.py
@@ -460,7 +460,7 @@ def present(name,
 
     # the comma is used to separate field in GECOS, thus resulting into
     # salt adding the end of fullname each time this function is called
-    for gecos_field in ['fullname', 'roomnumber', 'workphone']:
+    for gecos_field in [fullname, roomnumber, workphone]:
         if isinstance(gecos_field, string_types) and ',' in gecos_field:
             ret['comment'] = "Unsupported char ',' in {0}".format(gecos_field)
             ret['result'] = False
