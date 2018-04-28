b3b9f87ae5f8176ec3a214195daa6ed8ad5dfd9e
John Landis <jalandis@gmail.com>
2018-04-28 11:00:34 -0400

    Adds a warning to the file.managed state to indicate 'file_mode' is ignored.  Fixes #47386

--- salt/states/file.py
+++ salt/states/file.py
@@ -2225,6 +2225,12 @@ def managed(name,
             'replace the file with, \'replace\' has been set to \'False\' to '
             'avoid reading the file unnecessarily.'.format(name)
         )
+    
+    if 'file_mode' in kwargs:
+        ret.setdefault('warnings', []).append(
+            'The \'file_mode\' argument will be ignored.  '
+            'Please use \'mode\' instead to set file permissions.'
+        )
 
     # Use this below to avoid multiple '\0' checks and save some CPU cycles
     if contents_pillar is not None:
