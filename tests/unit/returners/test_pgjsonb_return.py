a7df36d2cce5d5f0da13a7f76751fec9bec2adbb
Gareth J. Greenaway <gareth@wiked.org>
2018-05-01 16:04:28 -0700

    Removing unused imports.  Fixing function return.

--- tests/unit/returners/test_pgjsonb_return.py
+++ tests/unit/returners/test_pgjsonb_return.py
@@ -8,17 +8,10 @@ Unit tests for the PGJsonb returner (pgjsonb).
 
 # Import Python libs
 from __future__ import absolute_import, print_function, unicode_literals
-import os
-import shutil
-import time
 import logging
-import tempfile
-import time
 
 # Import Salt Testing libs
-from tests.integration import AdaptedConfigurationTestCaseMixin
 from tests.support.mixins import LoaderModuleMockMixin
-from tests.support.paths import TMP
 from tests.support.unit import TestCase, skipIf
 from tests.support.mock import (
     MagicMock,
@@ -28,12 +21,7 @@ from tests.support.mock import (
 )
 
 # Import Salt libs
-import salt.utils.files
-import salt.utils.jid
-import salt.utils.job
-import salt.utils.platform
 import salt.returners.pgjsonb as pgjsonb
-from salt.ext import six
 
 log = logging.getLogger(__name__)
 
@@ -53,8 +41,7 @@ class PGJsonbCleanOldJobsTestCase(TestCase, LoaderModuleMockMixin):
         connect_mock = MagicMock()
         with patch.object(pgjsonb, '_get_serv', connect_mock):
             with patch.dict(pgjsonb.__salt__, {'config.option': MagicMock()}):
-                ret = pgjsonb.clean_old_jobs()
-                self.assertEqual(ret, None)
+                self.assertEqual(pgjsonb.clean_old_jobs(), None)
 
     def test_clean_old_jobs_archive(self):
         '''
@@ -64,5 +51,4 @@ class PGJsonbCleanOldJobsTestCase(TestCase, LoaderModuleMockMixin):
         with patch.object(pgjsonb, '_get_serv', connect_mock):
             with patch.dict(pgjsonb.__salt__, {'config.option': MagicMock()}):
                 with patch.dict(pgjsonb.__opts__, {'archive_jobs': 1}):
-                    ret = pgjsonb.clean_old_jobs()
-                    self.assertEqual(ret, None)
+                    self.assertEqual(pgjsonb.clean_old_jobs(), None)
