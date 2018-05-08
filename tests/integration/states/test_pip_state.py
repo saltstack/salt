e4b277f82e9e4100894c2f33041b2f95e1273761
Erik Johnson <palehose@gmail.com>
2018-05-08 08:22:04 -0500

    Fix corner case where runas user's HOME env value is incorrect
    In the test suite, two of the failing `pip.installed` tests were failing
    because when we shelled out to get the user's environment, the `HOME`
    environment variable's value was incorrectly showing the root user's
    home dir.
    
    This change ensures that we use the correct value for the HOME
    environment variable when running a command as another user.

--- tests/integration/states/test_pip_state.py
+++ tests/integration/states/test_pip_state.py
@@ -296,7 +296,7 @@ class PipStateTest(ModuleCase, SaltReturnAssertsMixin):
         # pip install passing the package name in `name`
         ret = self.run_state(
             'pip.installed', name='pep8', user=username, bin_env=venv_dir,
-            no_cache_dir=True, password='PassWord1!')
+            password='PassWord1!')
         self.assertSaltTrueReturn(ret)
 
         if HAS_PWD:
@@ -340,12 +340,12 @@ class PipStateTest(ModuleCase, SaltReturnAssertsMixin):
         req_filename = os.path.join(
             RUNTIME_VARS.TMP_STATE_TREE, 'issue-6912-requirements.txt')
         with salt.utils.fopen(req_filename, 'wb') as reqf:
-            reqf.write(six.b('pep8'))
+            reqf.write(b'pep8')
 
         ret = self.run_state(
             'pip.installed', name='', user=username, bin_env=venv_dir,
             requirements='salt://issue-6912-requirements.txt',
-            no_cache_dir=True, password='PassWord1!')
+            password='PassWord1!')
         self.assertSaltTrueReturn(ret)
 
         if HAS_PWD:
@@ -430,7 +430,7 @@ class PipStateTest(ModuleCase, SaltReturnAssertsMixin):
             RUNTIME_VARS.TMP_PRODENV_STATE_TREE, 'prod-env-requirements.txt'
         )
         with salt.utils.fopen(requirements_file, 'wb') as reqf:
-            reqf.write(six.b('pep8\n'))
+            reqf.write(b'pep8\n')
 
         try:
             self.run_function('virtualenv.create', [venv_dir])
