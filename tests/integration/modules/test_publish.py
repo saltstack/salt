0dbc257448449a6b45eb27c284625f1a6e728d7c
Daniel Wallace <danielwallace@gtmanfred.com>
2018-05-24 21:32:09 -0500

    fix tests

--- tests/integration/modules/test_publish.py
+++ tests/integration/modules/test_publish.py
@@ -172,21 +172,21 @@ class PublishWheelModuleTest(ModuleCase, SaltReturnAssertsMixin):
 
     def test_publish_wheel_not_allowed(self):
         '''
-        test accepting key
+        test running a wheel function that is not allowed
         '''
         ret = self.run_function(
             'publish.wheel',
             ['key.list_all'],
         )
-        self.assertEqual(ret, {'minions': ['minion']})
+        self.assertEqual(ret, {})
 
     def test_publish_wheel_rejected(self):
         '''
-        test accepting key
+        test accepting key from a minion that isn't approved
         '''
         ret = self.run_function(
             'publish.wheel',
             ['key.accept', 'arg=minion'],
             minion_tgt='subminion',
         )
-        self.assertEqual(ret, {'minions': ['minion']})
+        self.assertEqual(ret, {})
