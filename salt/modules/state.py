2eda67a1309fc5c4b499e9b1fec815c07aeb58a5
Bo Maryniuk <bo@suse.de>
2018-06-02 20:35:46 +0200

    Use to_unicode from stringutils avoid deprecation warning

--- salt/modules/state.py
+++ salt/modules/state.py
@@ -41,7 +41,6 @@ import salt.utils.versions
 from salt.exceptions import CommandExecutionError, SaltInvocationError
 from salt.runners.state import orchestrate as _orchestrate
 from salt.utils.odict import OrderedDict
-from salt.utils import to_unicode
 
 # Import 3rd-party libs
 from salt.ext import six
@@ -2085,9 +2084,9 @@ def pkg(pkg_path,
     # Verify that the tarball does not extract outside of the intended root
     members = s_pkg.getmembers()
     for member in members:
-        if to_unicode(member.path).startswith((os.sep, '..{0}'.format(os.sep))):
+        if salt.utils.stringutils.to_unicode(member.path).startswith((os.sep, '..{0}'.format(os.sep))):
             return {}
-        elif '..{0}'.format(os.sep) in to_unicode(member.path):
+        elif '..{0}'.format(os.sep) in salt.utils.stringutils.to_unicode(member.path):
             return {}
     s_pkg.extractall(root)
     s_pkg.close()
