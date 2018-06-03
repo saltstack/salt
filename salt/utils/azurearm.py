12891d784529c1a1b10afcbadc3caa3ec8999839
nicholasmhughes <nicholasmhughes@gmail.com>
2018-06-03 16:35:46 -0400

    add azure dns modules for crud operations on dns zones and record sets

--- salt/utils/azurearm.py
+++ salt/utils/azurearm.py
@@ -133,7 +133,11 @@ def get_client(client_type, **kwargs):
     Dynamically load the selected client and return a management client object
     '''
     client_map = {'compute': 'ComputeManagement',
+                  'authorization': 'AuthorizationManagement',
+                  'dns': 'DnsManagement',
                   'storage': 'StorageManagement',
+                  'managementlock': 'ManagementLock',
+                  'monitor': 'MonitorManagement',
                   'network': 'NetworkManagement',
                   'policy': 'Policy',
                   'resource': 'ResourceManagement',
@@ -148,8 +152,10 @@ def get_client(client_type, **kwargs):
 
     map_value = client_map[client_type]
 
-    if client_type == 'subscription' or client_type == 'policy':
+    if client_type in ['policy', 'subscription']:
         module_name = 'resource'
+    elif client_type in ['managementlock']:
+        module_name = 'resource.locks'
     else:
         module_name = client_type
 
@@ -164,6 +170,7 @@ def get_client(client_type, **kwargs):
         )
 
     credentials, subscription_id, cloud_env = _determine_auth(**kwargs)
+
     if client_type == 'subscription':
         client = Client(
             credentials=credentials,
