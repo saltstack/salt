ccb0acc958fc7b3e2871500e79705b3e40e988f4
Gareth J. Greenaway <gareth@wiked.org>
2018-06-11 15:20:56 -0700

    Porting #47049 to 2017.7.

--- tests/unit/modules/test_timezone.py
+++ tests/unit/modules/test_timezone.py
@@ -325,6 +325,21 @@ class TimezoneModuleTestCase(TestCase, LoaderModuleMockMixin):
             assert timezone.get_hwclock() == hwclock
 
     @skipIf(salt.utils.is_windows(), 'os.symlink not available in Windows')
+    @patch('salt.utils.which', MagicMock(return_value=True))
+    def test_set_hwclock_timedatectl(self):
+        '''
+        Test set hwclock with timedatectl
+        :return:
+        '''
+        timezone.set_hwclock('UTC')
+        name, args, kwargs = timezone.__salt__['cmd.retcode'].mock_calls[0]
+        assert args == (['timedatectl', 'set-local-rtc', 'false'],)
+
+        timezone.set_hwclock('localtime')
+        name, args, kwargs = timezone.__salt__['cmd.retcode'].mock_calls[1]
+        assert args == (['timedatectl', 'set-local-rtc', 'true'],)
+
+    @skipIf(salt.utils.is_windows(), 'os.symlink not available in Windows')
     @patch('salt.utils.which', MagicMock(return_value=False))
     @patch('os.path.exists', MagicMock(return_value=True))
     @patch('os.unlink', MagicMock())
