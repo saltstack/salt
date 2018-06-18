205abe209d5d8af6db2a619fa0c97de26f7b4256
Tao ZHOU <angeloudy@gmail.com>
2018-06-18 15:46:29 +1000

    Exclude opt-* bytecode files
    On FreeBSD platform, the build generates bytecode files named *opt-1.pyc/ *opt-2.pyc, which caused the issue of  #47006

--- salt/loader.py
+++ salt/loader.py
@@ -1240,7 +1240,7 @@ class LazyLoader(salt.utils.lazy.LazyDict):
                 try:
                     pycache_files = [
                         os.path.join('__pycache__', x) for x in
-                        sorted(os.listdir(os.path.join(mod_dir, '__pycache__')))
+                        sorted(os.listdir(os.path.join(mod_dir, '__pycache__'))) if not x.endswith('opt-1.pyc') and not x.endswith('opt-2.pyc')
                     ]
                 except OSError:
                     pass
