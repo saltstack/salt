2fce4e5bf8d045c6b7e293057b94a1d2355fee51
Cédric Bosdonnat <cbosdonnat@suse.com>
2018-06-19 11:26:03 +0200

    Remove unused virt._qemu_image_info()
    Unused code needs to go away.

--- salt/modules/virt.py
+++ salt/modules/virt.py
@@ -642,24 +642,6 @@ def _gen_pool_xml(name,
     return template.render(**context)
 
 
-def _qemu_image_info(path):
-    '''
-    Detect information for the image at path
-    '''
-    ret = {}
-    out = __salt__['cmd.run']('qemu-img info {0}'.format(path))
-
-    match_map = {'size': r'virtual size: \w+ \((\d+) byte[s]?\)',
-                 'format': r'file format: (\w+)'}
-
-    for info, search in six.iteritems(match_map):
-        try:
-            ret[info] = re.search(search, out).group(1)
-        except AttributeError:
-            continue
-    return ret
-
-
 def _get_images_dir():
     '''
     Extract the images dir from the configuration. First attempts to
