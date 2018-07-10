5a8f0629d832ac26bd334470726affc8640bb444
Bo Maryniuk <bo@suse.de>
2018-07-10 12:47:52 +0200

    Add data collector skeleton

--- salt/cli/support.py
+++ salt/cli/support.py
@@ -6,6 +6,45 @@ import salt.utils.parsers
 import salt.utils.verify
 import salt.cli.caller
 import copy
+import yaml
+import json
+
+class SupportDataCollector(object):
+    '''
+    Data collector. It behaves just like another outputter,
+    except it grabs the data to the archive files.
+    '''
+    def __init__(self, name, path=None, format='gzip'):
+        '''
+        constructor of the data collector
+        :param name:
+        :param path:
+        :param format:
+        '''
+        self.__path = path
+        self.__name = name
+
+    def open(self):
+        '''
+        Opens archive.
+        :return:
+        '''
+
+    def close(self):
+        '''
+        Closes the archive.
+        :return:
+        '''
+
+    def add(self, name, data):
+        '''
+        Adds a data set as a name.
+        :return:
+        '''
+        print('-' * 80)
+        data = json.loads(json.dumps(data))
+        print(yaml.safe_dump(data.get('return', {}), default_flow_style=False, indent=4))
+        print('\n\n')
 
 
 class SaltSupport(salt.utils.parsers.SaltSupportOptionParser):
