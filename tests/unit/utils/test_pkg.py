fef65b64f19676b63d964ad4ac9263d82f1fd414
Guillermo Hernandez <guillermo.hernandez@ni.com>
2018-07-10 14:39:39 -0500

    pkg.installed version parameter now supports multiple conditions, this to allow version ranges and exclusions, for more control over what versions will be installed.

---
+++ tests/unit/utils/test_pkg.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# Import Salt Libs
+import salt.utils.pkg
+# Import Salt Testing Libs
+from tests.support.unit import TestCase
+
+class PkgUtilsTestCase(TestCase):
+    '''
+    TestCase for salt.utils.pkg module
+    '''
+    test_parameters = [
+        ("16.0.0.49153-0+f1", "", "16.0.0.49153-0+f1"),
+        ("> 15.0.0", ">", "15.0.0"),
+        ("< 15.0.0", "<", "15.0.0"),
+        ("<< 15.0.0", "<<", "15.0.0"),
+        (">> 15.0.0", ">>", "15.0.0"),
+        (">= 15.0.0", ">=", "15.0.0"),
+        ("<= 15.0.0", "<=", "15.0.0"),
+        ("!= 15.0.0", "!=", "15.0.0"),
+        ("<=> 15.0.0", "<=>", "15.0.0"),
+        ("<> 15.0.0", "<>", "15.0.0"),
+        ("= 15.0.0", "=", "15.0.0"),
+        (">15.0.0", ">", "15.0.0"),
+        ("<15.0.0", "<", "15.0.0"),
+        ("<<15.0.0", "<<", "15.0.0"),
+        (">>15.0.0", ">>", "15.0.0"),
+        (">=15.0.0", ">=", "15.0.0"),
+        ("<=15.0.0", "<=", "15.0.0"),
+        ("!=15.0.0", "!=", "15.0.0"),
+        ("<=>15.0.0", "<=>", "15.0.0"),
+        ("<>15.0.0", "<>", "15.0.0"),
+        ("=15.0.0", "=", "15.0.0"),
+        ("", "", "")
+    ]
+
+    def test_split_comparison(self):
+        '''
+        Test passing a jid on the command line
+        '''
+        for test_parameter in PkgUtilsTestCase.test_parameters:
+            oper, verstr = salt.utils.pkg.split_comparison(test_parameter[0])
+            self.assertEqual(test_parameter[1], oper)
+            self.assertEqual(test_parameter[2], verstr)
