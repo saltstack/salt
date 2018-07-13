2650487a7d1b12118a57c55314a92afa7238672f
Bo Maryniuk <bo@suse.de>
2018-07-13 11:30:54 +0200

    Get profile by config/options

--- salt/cli/support/__init__.py
+++ salt/cli/support/__init__.py
@@ -5,13 +5,18 @@ Get default scenario of the support.
 from __future__ import print_function, unicode_literals, absolute_import
 import yaml
 import os
+import salt.exceptions
 
 
-def get_profile(scenario='default'):
-    profile_path = os.path.join(os.path.dirname(__file__), 'profiles', scenario + '.yml')
+def get_profile(profile):
+    if os.path.basename(profile) == profile:
+        profile = profile.split('.')[0]  # Trim extension if someone added it
+        profile_path = os.path.join(os.path.dirname(__file__), 'profiles', profile + '.yml')
+    else:
+        profile_path = profile
     if os.path.exists(profile_path):
         data = yaml.load(open(profile_path))
     else:
-        data = {}
+        raise salt.exceptions.SaltException('Profile "{}" is not found.'.format(profile))
 
     return data
