d1f9063e6cf156313fa3848b0fdb6b201c7f0238
C. R. Oldham <cro@ncbt.org>
2018-07-13 16:11:38 -0600

    Clarify setup

--- salt/matchers/default.py
+++ salt/matchers/default.py
@@ -2,7 +2,7 @@
 '''
 This is the default set of matcher functions.
 
-NOTE: These functions are converted to methods on the Matcher module during master and minion startup.
+NOTE: These functions are converted to methods on the Matcher class during master and minion startup.
 This is why they all take `self` but are not defined inside a `class:` declaration.
 '''
 from __future__ import absolute_import, print_function, unicode_literals
