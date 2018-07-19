c557fe0f0930c1d17d34db4a555c3ac0ba40d658
Bruno Binet <bruno.binet@gmail.com>
2018-07-19 18:21:33 +0200

    influxdb: fix conflicts in function arg names with client_args
    Rename client_args names so that it won't conflict anymore with some
    named parameters of influx module function.
    
    For example, the "create_user" function had a "password" argument that
    was conflicting with the "password" named argument from client_args.

--- salt/modules/influx.py
+++ salt/modules/influx.py
@@ -58,22 +58,22 @@ def __virtual__():
                     'influxdb library not available.'))
 
 
-def _client(user=None, password=None, host=None, port=None, **client_args):
-    if not user:
-        user = __salt__['config.option']('influxdb.user', 'root')
-    if not password:
-        password = __salt__['config.option']('influxdb.password', 'root')
-    if not host:
-        host = __salt__['config.option']('influxdb.host', 'localhost')
-    if not port:
-        port = __salt__['config.option']('influxdb.port', 8086)
+def _client(influxdb_user=None, influxdb_password=None, influxdb_host=None, influxdb_port=None, **client_args):
+    if not influxdb_user:
+        influxdb_user = __salt__['config.option']('influxdb.user', 'root')
+    if not influxdb_password:
+        influxdb_password = __salt__['config.option']('influxdb.password', 'root')
+    if not influxdb_host:
+        influxdb_host = __salt__['config.option']('influxdb.host', 'localhost')
+    if not influxdb_port:
+        influxdb_port = __salt__['config.option']('influxdb.port', 8086)
     for ignore in _STATE_INTERNAL_KEYWORDS:
         if ignore in client_args:
             del client_args[ignore]
-    return influxdb.InfluxDBClient(host=host,
-                                   port=port,
-                                   username=user,
-                                   password=password,
+    return influxdb.InfluxDBClient(host=influxdb_host,
+                                   port=influxdb_port,
+                                   username=influxdb_user,
+                                   password=influxdb_password,
                                    **client_args)
 
 
