b8eacd0b19c857135e3c64a48dcaf1bbdbda2488
Bo Maryniuk <bo@suse.de>
2018-07-23 16:35:36 +0200

    Refactor test to add setup/teardown

--- tests/unit/cli/test_support.py
+++ tests/unit/cli/test_support.py
@@ -23,15 +23,32 @@ class SaltSupportIndentOutputTestCase(TestCase):
     Unit Tests for the salt-support indent output.
     '''
 
+    def setUp(self):
+        '''
+        Setup test
+        :return:
+        '''
+
+        self.message = 'Stubborn processes on dumb terminal'
+        self.device = MagicMock()
+        self.iout = IndentOutput(device=self.device)
+
+    def tearDown(self):
+        '''
+        Remove instances after test run
+        :return:
+        '''
+        del self.message
+        del self.device
+        del self.iout
+
     def test_standard_output(self):
         '''
         Test console standard output.
         '''
-        message = 'Stubborn processes on dumb terminal'
-        device = MagicMock()
-        iout = IndentOutput(device=device)
-        iout.put(message)
-        assert device.write.called
-        assert device.write.call_count == 5
-        for idx, data in enumerate(['', '\x1b[0;36m', message, '\x1b[0;0m', '\n']):
-            assert device.write.call_args_list[idx][0][0] == data
+        self.iout.put(self.message)
+        assert self.device.write.called
+        assert self.device.write.call_count == 5
+        for idx, data in enumerate(['', '\x1b[0;36m', self.message, '\x1b[0;0m', '\n']):
+            assert self.device.write.call_args_list[idx][0][0] == data
+
