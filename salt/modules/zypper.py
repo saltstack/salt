6b84cf481d93cb7b62d01c89cb62bbd526769954
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2018-07-23 16:32:26 +0100

    Remove old hack when reporting multiversion packages

--- salt/modules/zypper.py
+++ salt/modules/zypper.py
@@ -1481,11 +1481,6 @@ def upgrade(refresh=True,
     __zypper__(systemd_scope=_systemd_scope()).noraise.call(*cmd_update)
     _clean_cache()
     new = list_pkgs()
-
-    # Handle packages which report multiple new versions
-    # (affects only kernel packages at this point)
-    for pkg in new:
-        new[pkg] = new[pkg].split(',')[-1]
     ret = salt.utils.data.compare_dicts(old, new)
 
     if __zypper__.exit_code not in __zypper__.SUCCESS_EXIT_CODES:
