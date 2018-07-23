1a834d1fd6a6b8446d64645ffaa1a679d12ea90d
Bo Maryniuk <bo@suse.de>
2018-07-23 12:25:07 +0200

    Fix log error/info/warning and exception messages

--- salt/engines/slack.py
+++ salt/engines/slack.py
@@ -787,7 +787,7 @@ class SlackClient(object):
         :param interval: time to wait between ending a loop and beginning the next
 
         '''
-        log.debug('Going to run a command async')
+        log.debug('Going to run a command asynchronous')
         runner_functions = sorted(salt.runner.Runner(__opts__).functions)
         # Parse args and kwargs
         cmd = msg['cmdline'][0]
