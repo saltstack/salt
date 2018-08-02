d72b6ee67d7832a6002ac14b002ee92b0c7e4595
markuskramerIgitt <markuskramerIgitt@gmail.com>
2018-08-02 23:44:12 +0200

    comments

--- salt/version.py
+++ salt/version.py
@@ -737,12 +737,13 @@ def msi_conformant_version():
             noc for tags is 0, representing the final word, translates to the highest build number (65535).
 
     Examples:
-      git checkout    Display version            Internal version
-      develop         2016.11.0-742-g5ca4d20     16.200.742
-      2016.11         2016.11.2-78-gce1f01f      16.202.78
-      2016.11         2016.11.9-88-g3e844ed1df   16.209.88
-      v2016.11.0      2016.11.0                  16.200.65535
-      v2016.11.2      2016.11.2                  16.202.65535
+      git checkout    Display version               Internal version
+      develop         2016.11.0-742-g5ca4d20        16.200.742
+      2016.11         2016.11.2-78-gce1f01f         16.202.78
+      2016.11         2016.11.9-88-g3e844ed1df      16.209.88
+      2018.8          2018.3.2-1306-g1e150923aa     18.42.1306
+      v2016.11.0      2016.11.0                     16.200.65535
+      v2016.11.2      2016.11.2                     16.202.65535
 
     '''
     short_year = int(six.text_type(__saltstack_version__.major)[2:])
