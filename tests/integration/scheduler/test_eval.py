aff1b8f6d4ddb66eebadd7e1de061c0c66a4a3ed
Gareth J. Greenaway <gareth@wiked.org>
2018-08-08 14:33:25 -0700

    Lint.

--- tests/integration/scheduler/test_eval.py
+++ tests/integration/scheduler/test_eval.py
@@ -428,4 +428,3 @@ class SchedulerEvalTest(ModuleCase, SaltReturnAssertsMixin):
         self.schedule.eval(now=run_time2)
         ret = self.schedule.job_status('job1')
         self.assertEqual(ret['_last_run'], run_time2)
-
