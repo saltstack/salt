12261a543e9fdc94ae2743ca500b0cbb73bab99e
Erwin Dondorp <erwin@dondorp.com>
2018-08-18 13:55:22 +0200

    trailing whitespace removal

--- salt/output/nested.py
+++ salt/output/nested.py
@@ -81,7 +81,7 @@ class NestDisplay(object):
             )
         # Number includes all python numbers types
         #  (float, int, long, complex, ...)
-        # use repr() to get the full precision also for older python versions 
+        # use repr() to get the full precision also for older python versions
         # as until about python32 it was limited to 12 digits only by default
         elif isinstance(ret, Number):
             out.append(
