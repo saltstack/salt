607f1a535b6311b3b679a457ebcba992ee61a952
Brad Ackerman <brad@facefault.org>
2018-08-19 18:50:21 -0400

    Make OS-specific loglevels one test.

--- tests/unit/modules/test_pf.py
+++ tests/unit/modules/test_pf.py
@@ -66,12 +66,13 @@ class PfTestCase(TestCase, LoaderModuleMockMixin):
         with patch.dict(pf.__salt__, {'cmd.run_all': mock_cmd}):
             self.assertFalse(pf.disable()['changes'])
 
-    def test_loglevel_freebsd(self):
+    def test_loglevel(self):
         '''
         Tests setting a loglevel.
         '''
         ret = {}
         ret['retcode'] = 0
+        # FreeBSD and OpenBSD have different log levels available.
         mock_cmd = MagicMock(return_value=ret)
         with patch.dict(pf.__salt__, {'cmd.run_all': mock_cmd}), \
                 patch.dict(pf.__grains__, {'os': 'FreeBSD'}):
@@ -79,13 +80,6 @@ class PfTestCase(TestCase, LoaderModuleMockMixin):
             mock_cmd.assert_called_once_with('pfctl -x urgent',
                     output_loglevel='trace', python_shell=False)
             self.assertTrue(res['changes'])
-
-    def test_loglevel_openbsd(self):
-        '''
-        Tests setting a loglevel.
-        '''
-        ret = {}
-        ret['retcode'] = 0
         mock_cmd = MagicMock(return_value=ret)
         with patch.dict(pf.__salt__, {'cmd.run_all': mock_cmd}), \
                 patch.dict(pf.__grains__, {'os': 'OpenBSD'}):
