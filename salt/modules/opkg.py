c68a8184a302db07ca45bdff19ef4b70ac2abaad
Cristian Hotea <cristian.hotea@ni.com>
2018-08-30 12:58:41 +0300

    Add support to avoid calling refresh_db in opkg.del_repo
    Since refresh_db is a costly operation we should have the option of not calling that
    
    Signed-off-by: Cristian Hotea <cristian.hotea@ni.com>

--- salt/modules/opkg.py
+++ salt/modules/opkg.py
@@ -1239,6 +1239,7 @@ def del_repo(alias, **kwargs):  # pylint: disable=unused-argument
 
         salt '*' pkg.del_repo alias
     '''
+    refresh = salt.utils.data.is_true(kwargs.get('refresh', True))
     repos = list_repos()
     if repos:
         deleted_from = dict()
@@ -1264,8 +1265,8 @@ def del_repo(alias, **kwargs):  # pylint: disable=unused-argument
                     except OSError:
                         pass
                 ret += msg.format(alias, repo_file)
-            # explicit refresh after a repo is deleted
-            refresh_db()
+            if refresh:
+                refresh_db()
             return ret
 
     return "Repo {0} doesn't exist in the opkg repo lists".format(alias)
