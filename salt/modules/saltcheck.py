0ca22625426b821fb4b2e98181b37de502476dd7
Christian McHugh <mchughc@dnb.com>
2018-09-02 09:39:25 +0100

    add assertion_section. Still needs example and documentation

--- salt/modules/saltcheck.py
+++ salt/modules/saltcheck.py
@@ -425,7 +425,8 @@ class SaltCheck(object):
     def _call_salt_command(self,
                            fun,
                            args,
-                           kwargs):
+                           kwargs,
+                           assertion_section):
         '''
         Generic call of salt Caller command
         '''
@@ -443,7 +444,10 @@ class SaltCheck(object):
             raise
         except Exception:
             raise
-        return value
+        if type(value) == dict and assertion_section:
+            return value.get(assertion_section, False)
+        else:
+            return value
 
     def run_test(self, test_dict):
         '''
@@ -455,6 +459,7 @@ class SaltCheck(object):
             if skip:
                 return {'status': 'Skip', 'duration': 0.0}
             mod_and_func = test_dict['module_and_function']
+            assertion_section = test_dict.get('assertion_section', None)
             args = test_dict.get('args', None)
             kwargs = test_dict.get('kwargs', None)
             pillar_data = test_dict.get('pillar-data', None)
@@ -472,7 +477,7 @@ class SaltCheck(object):
             else:
                 assertion = test_dict['assertion']
             expected_return = test_dict.get('expected-return', None)
-            actual_return = self._call_salt_command(mod_and_func, args, kwargs)
+            actual_return = self._call_salt_command(mod_and_func, args, kwargs, assertion_section)
             if assertion not in ["assertIn", "assertNotIn", "assertEmpty", "assertNotEmpty",
                                  "assertTrue", "assertFalse"]:
                 expected_return = self._cast_expected_to_returned_type(expected_return, actual_return)
