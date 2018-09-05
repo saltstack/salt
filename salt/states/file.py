b7082a204ef2588e63c8b103314abd3d288a8cd9
Andreas Lutro <anlutro@gmail.com>
2018-09-05 19:28:24 +0200

    update changes.diff instead of changes

--- salt/states/file.py
+++ salt/states/file.py
@@ -7885,7 +7885,7 @@ def serialize(
         )
 
     if isinstance(existing_data, dict) and isinstance(merged_data, dict):
-        ret["changes"] = salt.utils.dictdiffer.recursive_diff(
+        ret["changes"]["diff"] = salt.utils.dictdiffer.recursive_diff(
             existing_data, merged_data
         )
 
