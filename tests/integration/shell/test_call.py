5125a553f18c7a9ea8f418300f30c876651e0fb9
Erik Johnson <palehose@gmail.com>
2018-09-07 11:03:22 -0500

    Un-skip and update test
    This was skipped long ago and it really shouldn't need to be anymore.
    This also rewrites the test to be a bit more robust.

--- tests/integration/shell/test_call.py
+++ tests/integration/shell/test_call.py
@@ -22,24 +22,23 @@ from tests.support.case import ShellCase
 from tests.support.unit import skipIf
 from tests.support.paths import FILES, TMP
 from tests.support.mixins import ShellCaseCommonTestsMixin
-from tests.support.helpers import destructiveTest, flaky
+from tests.support.helpers import (
+    destructiveTest,
+    flaky,
+    requires_system_grains,
+    skip_if_not_root,
+)
 from tests.integration.utils import testprogram
+from tests.integration.states.test_pkg import _PKG_TARGETS
 
 # Import salt libs
 import salt.utils.files
+import salt.utils.json
+import salt.utils.platform
 import salt.utils.yaml
 from salt.ext import six
 
 log = logging.getLogger(__name__)
-
-_PKG_TARGETS = {
-    'Arch': ['python2-django', 'libpng'],
-    'Debian': ['python-plist', 'apg'],
-    'RedHat': ['xz-devel', 'zsh-html'],
-    'FreeBSD': ['aalib', 'pth'],
-    'SUSE': ['aalib', 'python-pssh']
-}
-_PKGS_INSTALLED = set()
 
 
 class CallTest(ShellCase, testprogram.TestProgramCase, ShellCaseCommonTestsMixin):
@@ -81,26 +80,35 @@ class CallTest(ShellCase, testprogram.TestProgramCase, ShellCaseCommonTestsMixin
         self.assertIn('Succeeded: 1', ''.join(out))
 
     @destructiveTest
-    @skipIf(True, 'Skipping due to off the wall failures and hangs on most os\'s. Will re-enable when fixed.')
-    @skipIf(sys.platform.startswith('win'), 'This test does not apply on Win')
-    @skipIf(getpass.getuser() == 'root', 'Requires root to test pkg.install')
+    @skip_if_not_root
+    @skipIf(salt.utils.platform.is_windows(), 'This test does not apply on Windows')
     def test_local_pkg_install(self):
         '''
         Test to ensure correct output when installing package
+
+        This also tests to make sure that salt call does not execute the
+        function twice, see https://github.com/saltstack/salt/pull/49552
         '''
-        get_os_family = self.run_call('--local grains.get os_family')
-        pkg_targets = _PKG_TARGETS.get(get_os_family[1].strip(), [])
-        check_pkg = self.run_call('--local pkg.list_pkgs')
-        for pkg in pkg_targets:
-            if pkg not in str(check_pkg):
-                out = self.run_call('--local pkg.install {0}'.format(pkg))
-                self.assertIn('local:    ----------', ''.join(out))
-                self.assertIn('{0}:        ----------'.format(pkg), ''.join(out))
-                self.assertIn('new:', ''.join(out))
-                self.assertIn('old:', ''.join(out))
-                _PKGS_INSTALLED.add(pkg)
-            else:
-                log.debug('The pkg: {0} is already installed on the machine'.format(pkg))
+        def _run_call(cmd):
+            cmd = '--out=json --local ' + cmd
+            return salt.utils.json.loads(''.join(self.run_call(cmd)))['local']
+
+        os_family = _run_call('grains.get os_family')
+        try:
+            target = _PKG_TARGETS.get(os_family, [])[0]
+        except IndexError:
+            self.skipTest(
+                'No package targets for os_family {0}'.format(os_family))
+
+        cur_pkgs = _run_call('pkg.list_pkgs')
+        if target in cur_pkgs:
+            self.fail('Target package \'{0}\' already installed'.format(target))
+
+        out = ''.join(self.run_call('--local pkg.install {0}'.format(target)))
+        self.assertIn('local:    ----------', out)
+        self.assertIn('{0}:        ----------'.format(target), out)
+        self.assertIn('new:', out)
+        self.assertIn('old:', out)
 
     @skipIf(sys.platform.startswith('win'), 'This test does not apply on Win')
     @flaky
@@ -497,9 +505,6 @@ class CallTest(ShellCase, testprogram.TestProgramCase, ShellCaseCommonTestsMixin
         user_info = self.run_call('--local grains.get username')
         if user_info and isinstance(user_info, (list, tuple)) and isinstance(user_info[-1], six.string_types):
             user = user_info[-1].strip()
-        if user == 'root':
-            for pkg in _PKGS_INSTALLED:
-                _ = self.run_call('--local pkg.remove {0}'.format(pkg))
         super(CallTest, self).tearDown()
 
     # pylint: disable=invalid-name
