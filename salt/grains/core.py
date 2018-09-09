c0ecccc1f71f3cc339bc7941cb0248316c1ff382
Erik Johnson <palehose@gmail.com>
2018-09-08 21:05:19 -0500

    Squelch log message RE: server ID
    INFO is way too verbose since it this gets logged _every_ time the
    grains are compiled.

--- salt/grains/core.py
+++ salt/grains/core.py
@@ -2710,10 +2710,10 @@ def get_server_id():
         if bool(use_crc):
             id_hash = getattr(zlib, use_crc, zlib.adler32)(__opts__.get('id', '').encode()) & 0xffffffff
         else:
-            log.info('This server_id is computed not by Adler32 nor by CRC32. '
-                     'Please use "server_id_use_crc" option and define algorithm you '
-                     'prefer (default "Adler32"). Starting with Sodium, the '
-                     'server_id will be computed with Adler32 by default.')
+            log.debug('This server_id is computed not by Adler32 nor by CRC32. '
+                      'Please use "server_id_use_crc" option and define algorithm you '
+                      'prefer (default "Adler32"). Starting with Sodium, the '
+                      'server_id will be computed with Adler32 by default.')
             id_hash = _get_hash_by_shell()
         server_id = {'server_id': id_hash}
 
