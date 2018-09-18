77f1c446387736fdb4cad26aa5e7aa493925c41d
Bo Maryniuk <bo@suse.de>
2018-09-18 23:56:51 +0200

    Remove duplicated code

--- salt/_compat.py
+++ salt/_compat.py
@@ -95,24 +95,17 @@ Python 2: If ``s`` is an instance of ``text_type``, return
 '''
 
 
-if PY3:
-    def native_(s, encoding='latin-1', errors='strict'):
-        '''
-        If ``s`` is an instance of ``text_type``, return
-        ``s``, otherwise return ``str(s, encoding, errors)``
-        '''
-        if isinstance(s, text_type):
-            return s
-        return str(s, encoding, errors)
-else:
-    def native_(s, encoding='latin-1', errors='strict'):
-        '''
-        If ``s`` is an instance of ``text_type``, return
-        ``s.encode(encoding, errors)``, otherwise return ``str(s)``
-        '''
-        if isinstance(s, text_type):
-            return s.encode(encoding, errors)
-        return str(s)
+def native_(s, encoding='latin-1', errors='strict'):
+    '''
+    If ``s`` is an instance of ``text_type``, return
+    ``s``, otherwise return ``str(s, encoding, errors)``
+    '''
+    if PY3:
+        out = s if isinstance(s, text_type) else str(s, encoding, errors)
+    else:
+        out = s.encode(encoding, errors) if isinstance(s, text_type) else str(s)
+
+    return out
 
 native_.__doc__ = '''
 Python 3: If ``s`` is an instance of ``text_type``, return ``s``, otherwise
