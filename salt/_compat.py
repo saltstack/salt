84afc6669d8bc50a0106a22982d9537c4653fcbb
Bo Maryniuk <bo@suse.de>
2018-09-18 17:51:45 +0200

    Fix unicode imports in compat

--- salt/_compat.py
+++ salt/_compat.py
@@ -5,7 +5,7 @@ Salt compatibility code
 # pylint: disable=import-error,unused-import,invalid-name
 
 # Import python libs
-from __future__ import absolute_import
+from __future__ import absolute_import, unicode_literals, print_function
 import sys
 import types
 
