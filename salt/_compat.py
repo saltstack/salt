f5644bf617679ca7ca4161a11fb4715567443e83
Bo Maryniuk <bo@suse.de>
2018-09-19 00:04:22 +0200

    Add logging to the ip_interface

--- salt/_compat.py
+++ salt/_compat.py
@@ -215,6 +215,43 @@ def ip_address(address):
     raise ValueError('{} does not appear to be an IPv4 or IPv6 address'.format(repr(address)))
 
 
+def ip_interface(address):
+    """Take an IP string/int and return an object of the correct type.
+
+    Args:
+        address: A string or integer, the IP address.  Either IPv4 or
+          IPv6 addresses may be supplied; integers less than 2**32 will
+          be considered to be IPv4 by default.
+
+    Returns:
+        An IPv4Interface or IPv6Interface object.
+
+    Raises:
+        ValueError: if the string passed isn't either a v4 or a v6
+          address.
+
+    Notes:
+        The IPv?Interface classes describe an Address on a particular
+        Network, so they're basically a combination of both the Address
+        and Network classes.
+
+    """
+    try:
+        return ipaddress.IPv4Interface(address)
+    except (ipaddress.AddressValueError, ipaddress.NetmaskValueError) as err:
+        log.debug('Error while getting IPv4 interface for address %s', address)
+        log.debug(err)
+
+    try:
+        return ipaddress.IPv6Interface(address)
+    except (ipaddress.AddressValueError, ipaddress.NetmaskValueError) as err:
+        log.debug('Error while getting IPv6 interface for address %s', address)
+        log.debug(err)
+
+    raise ValueError('{} does not appear to be an IPv4 or IPv6 interface'.format(address))
+
+
 if ipaddress:
     ipaddress.IPv6Address = IPv6AddressScoped
     ipaddress.ip_address = ip_address
+    ipaddress.ip_interface = ip_interface
