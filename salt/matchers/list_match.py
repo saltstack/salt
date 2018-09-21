05b216ac95327ed6fea82b2da043ffccffbb79db
C. R. Oldham <cro@ncbt.org>
2018-09-21 15:44:40 -0600

    Try list matcher optimization again.

--- salt/matchers/list_match.py
+++ salt/matchers/list_match.py
@@ -3,6 +3,8 @@
 This is the default list matcher.
 '''
 from __future__ import absolute_import, print_function, unicode_literals
+import collections
+import six
 
 
 def match(tgt):
@@ -10,9 +12,14 @@ def match(tgt):
     Determines if this host is on the list
     '''
     try:
-        return __opts__['id'] == tgt \
-            or ',' + __opts__['id'] + ',' in tgt \
-            or tgt.startswith(__opts__['id'] + ',') \
-            or tgt.endswith(',' + __opts__['id'])
+        if isinstance(tgt, collections.Sequence) and not isinstance(tgt, six.string_types):
+            result = bool(__opts__['id'] in tgt)
+        else:
+            result = __opts__['id'] == tgt \
+                or ',' + __opts__['id'] + ',' in tgt \
+                or tgt.startswith(__opts__['id'] + ',') \
+                or tgt.endswith(',' + __opts__['id'])
+        return result
+
     except (AttributeError, TypeError):
         return False
