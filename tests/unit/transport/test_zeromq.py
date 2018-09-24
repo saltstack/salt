f14ff166269878f9a04ec31bfde7ee37889c4902
Jeremy McMillan <jeremy.mcmillan@gmail.com>
2018-09-23 23:24:40 -0500

    fix minion zmq connecting to master configured as IPv6 address

--- tests/unit/transport/test_zeromq.py
+++ tests/unit/transport/test_zeromq.py
@@ -361,7 +361,11 @@ class ZMQConfigTest(TestCase):
                                                          source_port=s_port) == 'tcp://0.0.0.0:{0};{1}:{2}'.format(s_port, m_ip, m_port)
 
             # pass in only master_port and ipv6 source_ip and source_port
+<<<<<<< HEAD
             assert salt.transport.zeromq._get_master_uri('::', m_port,
+=======
+            assert salt.transport.zeromq._get_master_uri(master_port=m_port,
+>>>>>>> fix minion zmq connecting to master configured as IPv6 address
                                                          source_ip=m_port,
                                                          source_port=s_port) == 'tcp://[::]:{0};[{1}]:{2}'.format(m_port, s_ip6, s_port)
 
