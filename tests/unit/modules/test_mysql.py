9265195deb0fd65f80cca8d1a788ce6d9deb8f43
Gareth J. Greenaway <gareth@wiked.org>
2018-10-05 18:06:48 -0700

    Updating the mysql update to handle changes in version 8.0.11 where PASSWORD was removed.  Updating unit tests.

--- tests/unit/modules/test_mysql.py
+++ tests/unit/modules/test_mysql.py
@@ -38,28 +38,30 @@ class MySQLTestCase(TestCase, LoaderModuleMockMixin):
 
         Do it before test_user_create_when_user_exists mocks the user_exists call
         '''
-        self._test_call(mysql.user_exists,
-                        {'sql': ('SELECT User,Host FROM mysql.user WHERE '
-                                  'User = %(user)s AND Host = %(host)s AND '
-                                  'Password = PASSWORD(%(password)s)'),
-                         'sql_args': {'host': 'localhost',
-                                      'password': 'BLUECOW',
-                                      'user': 'mytestuser'
-                                     }
-                        },
-                        user='mytestuser',
-                        host='localhost',
-                        password='BLUECOW'
-        )
+        with patch.object(mysql, 'version', return_value='8.0.10'):
+            self._test_call(mysql.user_exists,
+                            {'sql': ('SELECT User,Host FROM mysql.user WHERE '
+                                      'User = %(user)s AND Host = %(host)s AND '
+                                      'Password = PASSWORD(%(password)s)'),
+                             'sql_args': {'host': 'localhost',
+                                          'password': 'BLUECOW',
+                                          'user': 'mytestuser'
+                                         }
+                            },
+                            user='mytestuser',
+                            host='localhost',
+                            password='BLUECOW'
+            )
 
         # test_user_create_when_user_exists(self):
         # ensure we don't try to create a user when one already exists
         # mock the version of MySQL
-        with patch.object(mysql, 'version', MagicMock(return_value='8.0.10')):
+        with patch.object(mysql, 'version', return_value='8.0.10'):
             with patch.object(mysql, 'user_exists', MagicMock(return_value=True)):
                 with patch.dict(mysql.__salt__, {'config.option': MagicMock()}):
                     ret = mysql.user_create('testuser')
                     self.assertEqual(False, ret)
+
 
     def test_user_create(self):
         '''
@@ -82,19 +84,20 @@ class MySQLTestCase(TestCase, LoaderModuleMockMixin):
         '''
         connect_mock = MagicMock()
         with patch.object(mysql, '_connect', connect_mock):
-            with patch.dict(mysql.__salt__, {'config.option': MagicMock()}):
-                mysql.user_chpass('testuser', password='BLUECOW')
-                calls = (
-                    call().cursor().execute(
-                        'UPDATE mysql.user SET Password=PASSWORD(%(password)s) WHERE User=%(user)s AND Host = %(host)s;',
-                        {'password': 'BLUECOW',
-                         'user': 'testuser',
-                         'host': 'localhost',
-                        }
-                    ),
-                    call().cursor().execute('FLUSH PRIVILEGES;'),
-                )
-                connect_mock.assert_has_calls(calls, any_order=True)
+            with patch.object(mysql, 'version', return_value='8.0.10'):
+                with patch.dict(mysql.__salt__, {'config.option': MagicMock()}):
+                    mysql.user_chpass('testuser', password='BLUECOW')
+                    calls = (
+                        call().cursor().execute(
+                            'UPDATE mysql.user SET Password=PASSWORD(%(password)s) WHERE User=%(user)s AND Host = %(host)s;',
+                            {'password': 'BLUECOW',
+                             'user': 'testuser',
+                             'host': 'localhost',
+                            }
+                        ),
+                        call().cursor().execute('FLUSH PRIVILEGES;'),
+                    )
+                    connect_mock.assert_has_calls(calls, any_order=True)
 
     def test_user_remove(self):
         '''
