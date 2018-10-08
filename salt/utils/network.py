dc31498253bc5f946eefd89dd3ad0b3e8f938156
twangboy <slee@saltstack.com>
2018-10-08 15:05:01 -0600

    Add comment to change for Windows

--- salt/utils/network.py
+++ salt/utils/network.py
@@ -208,6 +208,8 @@ def get_fqhostname():
         )
         for info in addrinfo:
             # info struct [family, socktype, proto, canonname, sockaddr]
+            # On Windows `canonname` can be an empty string
+            # This can cause the function to return `None`
             if len(info) >= 4 and info[3]:
                 l = [info[3]]
     except socket.gaierror:
