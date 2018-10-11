dafe7affdffb57d792a7c6fd28f91fcf09ec6111
Austin Papp <austin.papp@gmail.com>
2018-10-11 13:45:27 -0400

    fixed more lint issues

--- tests/unit/engines/test_script.py
+++ tests/unit/engines/test_script.py
@@ -4,25 +4,19 @@ unit tests for the script engine
 '''
 # Import Python libs
 from __future__ import absolute_import, print_function, unicode_literals
-import subprocess
-import tempfile
 
 # Import Salt Testing Libs
 from tests.support.mixins import LoaderModuleMockMixin
-from tests.support.paths import TMP
 from tests.support.unit import skipIf, TestCase
 from tests.support.mock import (
     PropertyMock,
     NO_MOCK,
     NO_MOCK_REASON,
-    MagicMock,
     mock_open,
     patch)
 
 # Import Salt Libs
 import salt.engines.script as script
-import salt.config
-import salt.utils.stringutils
 from salt.exceptions import CommandExecutionError
 
 @skipIf(NO_MOCK, NO_MOCK_REASON)
@@ -31,13 +25,12 @@ class EngineScriptTestCase(TestCase, LoaderModuleMockMixin):
     Test cases for salt.engine.script
     '''
 
-
     def setup_loader_modules(self):
         return {
             script: {
                 '__opts__': {
                     '__role': '',
-                    'extension_modules' : '',
+                    'extension_modules' : ''
                 }
              }
         }
@@ -48,7 +41,7 @@ class EngineScriptTestCase(TestCase, LoaderModuleMockMixin):
         Test known serializer is returned or exception is raised
         if unknown serializer
         '''
-        self.assertTrue( script._get_serializer('yaml') )
+        self.assertTrue(script._get_serializer('yaml'))
 
         with self.assertRaises(CommandExecutionError):
             script._get_serializer('bad')
@@ -61,4 +54,3 @@ class EngineScriptTestCase(TestCase, LoaderModuleMockMixin):
         with patch('subprocess.Popen') as popen_mock:
             popen_mock.stdout.readline.return_value = 'test'
             self.assertEqual(next(script._read_stdout(popen_mock)), 'test')
-
