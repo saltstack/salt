cc331355d151c9e1a35f65f6c5754766c0904e97
Bo Maryniuk <bo@suse.de>
2018-10-12 17:02:17 +0200

    Support globbing in filetree

--- salt/cli/support/intfunc.py
+++ salt/cli/support/intfunc.py
@@ -6,6 +6,7 @@ Internal functions.
 
 from __future__ import absolute_import, print_function, unicode_literals
 import os
+import glob
 from salt.cli.support.console import MessagesOutput
 import salt.utils.files
 
@@ -21,9 +22,15 @@ def filetree(collector, *paths):
     :param path: File or directory
     :return:
     '''
+    _paths = []
+    # Unglob
     for path in paths:
+        _paths += glob.glob(path)
+    for path in set(_paths):
         if not path:
             out.error('Path not defined', ident=2)
+        elif not os.path.exists(path):
+            out.warning('Path {} does not exists'.format(path))
         else:
             # The filehandler needs to be explicitly passed here, so PyLint needs to accept that.
             # pylint: disable=W8470
@@ -37,12 +44,10 @@ def filetree(collector, *paths):
                 except Exception as err:
                     out.error(err, ident=4)
             # pylint: enable=W8470
-            elif os.path.exists(path):
+            else:
                 try:
                     for fname in os.listdir(path):
                         fname = os.path.join(path, fname)
                         filetree(collector, [fname])
                 except Exception as err:
                     out.error(err, ident=4)
-            else:
-                out.warning('Path {} does not exists'.format(path))
