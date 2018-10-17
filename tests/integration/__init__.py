94ae7cf07ffeac2cbaf172b3a4a012c95c4f4b0c
Matt Phillips <mphillips81@bloomberg.net>
2018-10-16 21:56:46 -0400

    Revert "Revert "use the minion configured publish_port, not what is provided in auth payload""
    This restores original commit a0d723f78f3f6196e11f70058dd9056fbed16676,
    alongside some changes in tests.integration syndic config generation
    that was actually subtly doing the wrong thing and copying from a base
    of the base master, not syndic master config.
    
    Conflicts:
      - tests/integration/__init__.py

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -810,6 +810,14 @@ class TestDaemon(object):
         syndic_opts['cachedir'] = 'cache'
         syndic_opts['root_dir'] = os.path.join(TMP_ROOT_DIR)
 
+        # This is the syndic for master
+        # Let's start with a copy of the syndic master configuration
+        syndic_opts = copy.deepcopy(syndic_master_opts)
+        # Let's update with the syndic configuration
+        syndic_opts.update(salt.config._read_conf_file(os.path.join(RUNTIME_VARS.CONF_DIR, 'syndic')))
+        syndic_opts['cachedir'] = os.path.join(TMP, 'rootdir', 'cache')
+        syndic_opts['config_dir'] = RUNTIME_VARS.TMP_SYNDIC_MINION_CONF_DIR
+
         # This proxy connects to master
         proxy_opts = salt.config._read_conf_file(os.path.join(CONF_DIR, 'proxy'))
         proxy_opts['cachedir'] = 'cache'
