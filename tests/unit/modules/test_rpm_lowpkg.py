9758fcaac3605b025f0c1bc066c1a410b256998a
Alberto Planas <aplanas@gmail.com>
2018-10-18 10:40:12 +0200

    lowpkg: add parameter to change root directory
    The CLI rpm command allows the --root parameter to change the
    expected location where the rpm database can be found.
    
    This patch add a new optional parameter in the public interface
    to allow the set of the new root location.
    
    Update the tests to use the extra parameter.
    
    (cherry picked from commit 57474485c8021bcd4b736b9464ca37d93ea81a71)

--- tests/unit/modules/test_rpm_lowpkg.py
+++ tests/unit/modules/test_rpm_lowpkg.py
@@ -15,6 +15,11 @@ from tests.support.mock import MagicMock, patch
 from tests.support.unit import TestCase
 
 
+def _called_with_root(mock):
+    cmd = " ".join(mock.call_args[0][0])
+    return cmd.startswith("rpm --root /")
+
+
 class RpmTestCase(TestCase, LoaderModuleMockMixin):
     """
     Test cases for salt.modules.rpm
@@ -23,7 +28,7 @@ class RpmTestCase(TestCase, LoaderModuleMockMixin):
     def setup_loader_modules(self):
         return {rpm: {"rpm": MagicMock(return_value=MagicMock)}}
 
-    # 'list_pkgs' function tests: 1
+    # 'list_pkgs' function tests: 2
 
     def test_list_pkgs(self):
         """
@@ -32,21 +37,45 @@ class RpmTestCase(TestCase, LoaderModuleMockMixin):
         mock = MagicMock(return_value="")
         with patch.dict(rpm.__salt__, {"cmd.run": mock}):
             self.assertDictEqual(rpm.list_pkgs(), {})
+            self.assertFalse(_called_with_root(mock))
 
-    # 'verify' function tests: 1
+    def test_list_pkgs_root(self):
+        """
+        Test if it list the packages currently installed in a dict,
+        called with root parameter
+        """
+        mock = MagicMock(return_value="")
+        with patch.dict(rpm.__salt__, {"cmd.run": mock}):
+            rpm.list_pkgs(root="/")
+            self.assertTrue(_called_with_root(mock))
+
+    # 'verify' function tests: 2
 
     def test_verify(self):
         """
-        Test if it runs an rpm -Va on a system,
-        and returns the results in a dict
+        Test if it runs an rpm -Va on a system, and returns the
+        results in a dict
         """
         mock = MagicMock(
             return_value={"stdout": "", "stderr": "", "retcode": 0, "pid": 12345}
         )
         with patch.dict(rpm.__salt__, {"cmd.run_all": mock}):
             self.assertDictEqual(rpm.verify("httpd"), {})
+            self.assertFalse(_called_with_root(mock))
 
-    # 'file_list' function tests: 1
+    def test_verify_root(self):
+        """
+        Test if it runs an rpm -Va on a system, and returns the
+        results in a dict, called with root parameter
+        """
+        mock = MagicMock(
+            return_value={"stdout": "", "stderr": "", "retcode": 0, "pid": 12345}
+        )
+        with patch.dict(rpm.__salt__, {"cmd.run_all": mock}):
+            rpm.verify("httpd", root="/")
+            self.assertTrue(_called_with_root(mock))
+
+    # 'file_list' function tests: 2
 
     def test_file_list(self):
         """
@@ -55,8 +84,20 @@ class RpmTestCase(TestCase, LoaderModuleMockMixin):
         mock = MagicMock(return_value="")
         with patch.dict(rpm.__salt__, {"cmd.run": mock}):
             self.assertDictEqual(rpm.file_list("httpd"), {"errors": [], "files": []})
+            self.assertFalse(_called_with_root(mock))
 
-    # 'file_dict' function tests: 1
+    def test_file_list_root(self):
+        """
+        Test if it list the files that belong to a package, using the
+        root parameter.
+        """
+
+        mock = MagicMock(return_value="")
+        with patch.dict(rpm.__salt__, {"cmd.run": mock}):
+            rpm.file_list("httpd", root="/")
+            self.assertTrue(_called_with_root(mock))
+
+    # 'file_dict' function tests: 2
 
     def test_file_dict(self):
         """
@@ -65,6 +106,16 @@ class RpmTestCase(TestCase, LoaderModuleMockMixin):
         mock = MagicMock(return_value="")
         with patch.dict(rpm.__salt__, {"cmd.run": mock}):
             self.assertDictEqual(rpm.file_dict("httpd"), {"errors": [], "packages": {}})
+            self.assertFalse(_called_with_root(mock))
+
+    def test_file_dict_root(self):
+        """
+        Test if it list the files that belong to a package
+        """
+        mock = MagicMock(return_value="")
+        with patch.dict(rpm.__salt__, {"cmd.run": mock}):
+            rpm.file_dict("httpd", root="/")
+            self.assertTrue(_called_with_root(mock))
 
     # 'owner' function tests: 1
 
@@ -78,6 +129,7 @@ class RpmTestCase(TestCase, LoaderModuleMockMixin):
         mock = MagicMock(return_value=ret)
         with patch.dict(rpm.__salt__, {"cmd.run_stdout": mock}):
             self.assertEqual(rpm.owner("/usr/bin/salt-jenkins-build"), "")
+            self.assertFalse(_called_with_root(mock))
 
         ret = {
             "/usr/bin/vim": "vim-enhanced-7.4.160-1.e17.x86_64",
@@ -91,8 +143,22 @@ class RpmTestCase(TestCase, LoaderModuleMockMixin):
         )
         with patch.dict(rpm.__salt__, {"cmd.run_stdout": mock}):
             self.assertDictEqual(rpm.owner("/usr/bin/python", "/usr/bin/vim"), ret)
+            self.assertFalse(_called_with_root(mock))
 
-    # 'checksum' function tests: 1
+    def test_owner_root(self):
+        """
+        Test if it return the name of the package that owns the file,
+        using the parameter root.
+        """
+        self.assertEqual(rpm.owner(), "")
+
+        ret = "file /usr/bin/salt-jenkins-build is not owned by any package"
+        mock = MagicMock(return_value=ret)
+        with patch.dict(rpm.__salt__, {"cmd.run_stdout": mock}):
+            rpm.owner("/usr/bin/salt-jenkins-build", root="/")
+            self.assertTrue(_called_with_root(mock))
+
+    # 'checksum' function tests: 2
 
     def test_checksum(self):
         """
@@ -109,6 +175,17 @@ class RpmTestCase(TestCase, LoaderModuleMockMixin):
             self.assertDictEqual(
                 rpm.checksum("file1.rpm", "file2.rpm", "file3.rpm"), ret
             )
+            self.assertFalse(_called_with_root(mock))
+
+    def test_checksum_root(self):
+        """
+        Test if checksum validate as expected, using the parameter
+        root
+        """
+        mock = MagicMock(side_effect=[True, 0])
+        with patch.dict(rpm.__salt__, {"file.file_exists": mock, "cmd.retcode": mock}):
+            rpm.checksum("file1.rpm", root="/")
+            self.assertTrue(_called_with_root(mock))
 
     def test_version_cmp_rpm(self):
         """
