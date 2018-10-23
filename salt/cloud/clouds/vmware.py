9aa8bb05fe9108f3fb9182ef340b731f4180a4bb
henri <henri@login.oldnickern>
2018-10-23 16:53:15 +0200

    added force recognition to reboot_host

--- salt/cloud/clouds/vmware.py
+++ salt/cloud/clouds/vmware.py
@@ -4373,7 +4373,7 @@ def reboot_host(kwargs=None, call=None):
             'Specified host system does not support reboot.'
         )
 
-    if not host_ref.runtime.inMaintenanceMode:
+    if not host_ref.runtime.inMaintenanceMode and not force:
         raise SaltCloudSystemExit(
             'Specified host system is not in maintenance mode. Specify force=True to '
             'force reboot even if there are virtual machines running or other operations '
