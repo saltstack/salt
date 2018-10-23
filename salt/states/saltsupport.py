cd696a31c3700042d924fa12135b27b81a9f9de2
Bo Maryniuk <bo@suse.de>
2018-10-23 22:09:07 +0200

    Move all functions into a callable class object

--- salt/states/saltsupport.py
+++ salt/states/saltsupport.py
@@ -49,38 +49,97 @@ log = logging.getLogger(__name__)
 __virtualname__ = 'support'
 
 
-def taken(name, profile='default', pillar=None, archive=None, output='nested'):
+class SaltSupportState(object):
     '''
-    Takes minion support config data.
-
-    :param profile:
-    :param pillar:
-    :param archive:
-    :param output:
-    :return:
+    Salt-support.
     '''
-    ret = {
-        'name': 'support.taken',
-        'changes': {},
-        'result': True,
-    }
+    def get_kwargs(self, data):
+        kwargs = {}
+        for keyset in data:
+            kwargs.update(keyset)
 
-    result = __salt__['support.run'](profile=profile, pillar=pillar, archive=archive, output=output)
-    if result.get('archive'):
-        ret['comment'] = 'Information about this system has been saved to {} file.'.format(result['archive'])
-        ret['changes']['archive'] = result['archive']
-        ret['changes']['messages'] = {}
-        for key in ['info', 'error', 'warning']:
-            if result.get('messages', {}).get(key):
-                ret['changes']['messages'][key] = result['messages'][key]
-    else:
-        ret['comment'] = ''
+        return kwargs
 
-    return ret
+    def __call__(self, *args, **kwargs):
+        '''
+        Call support.
+
+        :param args:
+        :param kwargs:
+        :return:
+        '''
+        ret = {
+            'name': kwargs.pop('name'),
+            'changes': {},
+            'result': True,
+            'comment': 'args: ' + str(args) + '\nkwargs: ' + str(kwargs),
+        }
+
+        out = {}
+        for ref_func, ref_kwargs in kwargs.items():
+            out[ref_func] = getattr(self, ref_func)(**self.get_kwargs(ref_kwargs))
+        ret['changes'] = out
+
+        return ret
+
+    def collected(self, group, filename=None, host=None, location=None, move=True, all=True):
+        '''
+        Sync archives to a central place.
+
+        :param name:
+        :param group:
+        :param filename:
+        :param host:
+        :param location:
+        :param move:
+        :param all:
+        :return:
+        '''
+        ret = {
+            'name': 'support.collected',
+            'changes': {},
+            'result': True,
+            'comment': '',
+        }
+
+        result = __salt__['support.sync'](group, name=filename, host=host, location=location, move=move, all=all)
+        ret['changes'] = result
+
+        return ret
+
+    def taken(self, profile='default', pillar=None, archive=None, output='nested'):
+        '''
+        Takes minion support config data.
+
+        :param profile:
+        :param pillar:
+        :param archive:
+        :param output:
+        :return:
+        '''
+        ret = {
+            'name': 'support.taken',
+            'changes': {},
+            'result': True,
+        }
+
+        result = __salt__['support.run'](profile=profile, pillar=pillar, archive=archive, output=output)
+        if result.get('archive'):
+            ret['comment'] = 'Information about this system has been saved to {} file.'.format(result['archive'])
+            ret['changes']['archive'] = result['archive']
+            ret['changes']['messages'] = {}
+            for key in ['info', 'error', 'warning']:
+                if result.get('messages', {}).get(key):
+                    ret['changes']['messages'][key] = result['messages'][key]
+        else:
+            ret['comment'] = ''
+
+        return ret
 
 
 def __virtual__():
     '''
     Salt Support state
     '''
+    setattr(sys.modules[__name__], 'call', lambda **kwargs: SaltSupportState()(**kwargs))   # pylint: disable=W0108
     return __virtualname__
