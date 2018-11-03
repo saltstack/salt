3748021c4ca51e9d2d12ee88f47ca01627faf77c
Bo Maryniuk <bo@suse.de>
2018-11-03 17:42:58 +0100

    Add LogCollector UT for testing regular message

--- tests/unit/modules/test_saltsupport.py
+++ tests/unit/modules/test_saltsupport.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 '''
-    :codeauthor: Bo Maryniuk <bo@maryniuk.net>
+    :codeauthor: Bo Maryniuk <bo@suse.de>
 '''
 
 # Import Python libs
@@ -9,8 +9,9 @@ from __future__ import absolute_import, print_function, unicode_literals
 # Import Salt Testing Libs
 from tests.support.mixins import LoaderModuleMockMixin
 from tests.support.unit import TestCase, skipIf
-from tests.support.mock import patch, NO_MOCK, NO_MOCK_REASON
+from tests.support.mock import patch, MagicMock, NO_MOCK, NO_MOCK_REASON
 from salt.modules import saltsupport
+import datetime
 
 try:
     import pytest
@@ -37,3 +38,18 @@ class LogCollectorTestCase(TestCase, LoaderModuleMockMixin):
     def setup_loader_modules(self):
         return {saltsupport: {}}
 
+    def test_msg(self):
+        '''
+        Test set message to the log collector.
+
+        :return:
+        '''
+        utcmock = MagicMock()
+        utcmock.utcnow = MagicMock(return_value=datetime.datetime.utcfromtimestamp(0))
+        with patch('datetime.datetime', utcmock):
+            msg = 'Upgrading /dev/null device'
+            out = saltsupport.LogCollector()
+            out.msg(msg)
+            assert 'info' in out.messages
+            assert type(out.messages['info']) == saltsupport.LogCollector.MessagesList
+            assert out.messages['info'] == ['00:00:00.000 - {}'.format(msg)]
