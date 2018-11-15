f00b97c696364859702fbce661b72c389649ed99
Bo Maryniuk <bo@suse.de>
2018-11-15 11:10:19 +0100

    Add pkg.rpm unit test case

--- tests/unit/utils/test_pkg.py
+++ tests/unit/utils/test_pkg.py
@@ -50,3 +50,34 @@ class PkgUtilsTestCase(TestCase):
             oper, verstr = salt.utils.pkg.split_comparison(test_parameter[0])
             self.assertEqual(test_parameter[1], oper)
             self.assertEqual(test_parameter[2], verstr)
+
+
+@skipIf(NO_MOCK, NO_MOCK_REASON)
+@skipIf(pytest is None, 'PyTest is missing')
+class PkgRPMTestCase(TestCase):
+    '''
+    Test case.
+    '''
+
+    @patch('salt.utils.path.which', MagicMock(return_value=True))
+    def test_get_osarch_by_rpm(self):
+        '''
+        Get os_arch if RPM package is installed.
+        :return:
+        '''
+        subprocess_mock = MagicMock()
+        subprocess_mock.Popen = MagicMock()
+        subprocess_mock.Popen().communicate = MagicMock(return_value=['Z80'])
+        with patch('salt.utils.pkg.rpm.subprocess', subprocess_mock):
+            assert rpm.get_osarch() == 'Z80'
+        assert subprocess_mock.Popen.call_count == 2  # One within the mock
+        assert subprocess_mock.Popen.call_args[1]['close_fds']
+        assert subprocess_mock.Popen.call_args[1]['shell']
+        assert len(subprocess_mock.Popen.call_args_list) == 2
+        assert subprocess_mock.Popen.call_args[0][0] == 'rpm --eval "%{_host_cpu}"'
+
+    def test_get_osarch_by_platform(self):
+        '''
+        Get os_arch if RPM package is not installed (inird image, for example).
+        :return:
+        '''
