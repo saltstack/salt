4ef4a17b951d7a2de3e96a71006bc3e717e2dd4c
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2018-11-20 09:49:01 +0000

    Use assertCountEqual when running on Python3

--- tests/unit/modules/test_pkg_resource.py
+++ tests/unit/modules/test_pkg_resource.py
@@ -150,7 +150,10 @@ class PkgresTestCase(TestCase, LoaderModuleMockMixin):
             'foobar.something': '3:1.1-23.1',
             'foobar.': '3:1.1-23.1',
         }
-        self.assertItemsEqual(pkg_resource.format_pkg_list(packages, False, None), expected_pkg_list)
+        if six.PY3:
+            self.assertCountEqual(pkg_resource.format_pkg_list(packages, False, None), expected_pkg_list)
+        else:
+            self.assertItemsEqual(pkg_resource.format_pkg_list(packages, False, None), expected_pkg_list)
 
     def test_format_pkg_list_with_attr(self):
         '''
@@ -214,7 +217,10 @@ class PkgresTestCase(TestCase, LoaderModuleMockMixin):
                 }
             ]
         }
-        self.assertItemsEqual(pkg_resource.format_pkg_list(packages, False, attr=['epoch', 'release']), expected_pkg_list)
+        if six.PY3:
+            self.assertCountEqual(pkg_resource.format_pkg_list(packages, False, attr=['epoch', 'release']), expected_pkg_list)
+        else:
+            self.assertItemsEqual(pkg_resource.format_pkg_list(packages, False, attr=['epoch', 'release']), expected_pkg_list)
 
     def test_stringify(self):
         '''
