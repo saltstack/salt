47bfba1018db9b6faa2f42da9fa621b92e337527
Claudiu Popescu <claudiu@pidginhost.com>
2018-11-23 16:11:01 +0200

    Migrated to cmd.run_all and tested on working srv

--- salt/utils/icinga2.py
+++ salt/utils/icinga2.py
@@ -10,7 +10,6 @@ This module provides common functionality for icinga2 module and state.
 
 # Import python libs
 import logging
-import subprocess
 import re
 
 # Import Salt libs
@@ -19,21 +18,9 @@ import salt.utils.path
 log = logging.getLogger(__name__)
 
 
-def execute(cmd, ret_code=False):
-    process = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    if ret_code:
-        return process.wait()
-    output, error = process.communicate()
-    if output:
-        log.debug(output)
-        return output
-    log.debug(error)
-    return error
-
-
 def get_certs_path():
-    icinga2_output = execute([salt.utils.path.which('icinga2'), "--version"])
-    version = re.search('r\d+\.\d+', icinga2_output).group(0)
+    icinga2_output = __salt__['cmd.run_all']([salt.utils.path.which('icinga2'), "--version"], python_shell=False)
+    version = re.search('r\d+\.\d+', icinga2_output['stdout']).group(0)
     # Return new certs path for icinga2 >= 2.8
     if int(version.split('.')[1]) >= 8:
         return '/var/lib/icinga2/certs/'
