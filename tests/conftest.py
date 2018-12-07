07e577445d86de71b2dff2599cb79a5de7052df2
Pedro Algarvio <pedro@algarvio.me>
2018-12-07 17:39:46 +0000

    `RUNTIME_VARS.TMP_PILLAR_TREE` must point to the base env root dir

--- tests/conftest.py
+++ tests/conftest.py
@@ -839,7 +839,7 @@ def bridge_pytest_and_runtests(session_root_dir,
                                session_secondary_conf_dir,
                                session_syndic_conf_dir,
                                session_master_of_masters_conf_dir,
-                               session_pillar_tree_root_dir,
+                               session_base_env_pillar_tree_root_dir,
                                session_base_env_state_tree_root_dir,
                                session_prod_env_state_tree_root_dir,
                                session_master_config,
@@ -854,7 +854,7 @@ def bridge_pytest_and_runtests(session_root_dir,
     RUNTIME_VARS.TMP_SUB_MINION_CONF_DIR = session_secondary_conf_dir.realpath().strpath
     RUNTIME_VARS.TMP_SYNDIC_MASTER_CONF_DIR = session_master_of_masters_conf_dir.realpath().strpath
     RUNTIME_VARS.TMP_SYNDIC_MINION_CONF_DIR = session_syndic_conf_dir.realpath().strpath
-    RUNTIME_VARS.TMP_PILLAR_TREE = session_pillar_tree_root_dir.realpath().strpath
+    RUNTIME_VARS.TMP_PILLAR_TREE = session_base_env_pillar_tree_root_dir.realpath().strpath
     RUNTIME_VARS.TMP_STATE_TREE = session_base_env_state_tree_root_dir.realpath().strpath
     RUNTIME_VARS.TMP_PRODENV_STATE_TREE = session_prod_env_state_tree_root_dir.realpath().strpath
 
