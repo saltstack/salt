3bc288eb9a99597eca20f3a29c9b4a6a488fdc25
Gareth J. Greenaway <gareth@wiked.org>
2018-12-11 16:51:27 -0800

    Logging to see what grains contains.

--- tests/integration/files/file/base/_grains/custom_grains.py
+++ tests/integration/files/file/base/_grains/custom_grains.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import logging
+log = logging.getLogger(__name__)
 
 def test(grains):
+    log.info('==== grains %s ===', grains)
     return {'custom_grain_test': 'itworked' if 'os' in grains else 'itdidntwork'}
