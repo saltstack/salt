e5203102ecb86e7e656d55becb90a78f0e6fbd81
Pedro Algarvio <pedro@algarvio.me>
2019-01-03 13:39:48 +0000

    Log the start and the end of each test

--- tests/conftest.py
+++ tests/conftest.py
@@ -198,8 +198,10 @@ def pytest_configure(config):
     )
     # Make sure the test suite "knows" this is a pytest test run
     RUNTIME_VARS.PYTEST_SESSION = True
+# <---- Register Markers ---------------------------------------------------------------------------------------------
 
 
+# ----- PyTest Tweaks ----------------------------------------------------------------------------------------------->
 def set_max_open_files_limits(min_soft=3072, min_hard=4096):
 
     # Get current limits
@@ -255,7 +257,23 @@ def set_max_open_files_limits(min_soft=3072, min_hard=4096):
 def pytest_report_header():
     soft, hard = set_max_open_files_limits()
     return 'max open files; soft: {}; hard: {}'.format(soft, hard)
-# <---- Register Markers ---------------------------------------------------------------------------------------------
+
+
+def pytest_runtest_logstart(nodeid):
+    '''
+    implements the runtest_setup/call/teardown protocol for
+    the given test item, including capturing exceptions and calling
+    reporting hooks.
+    '''
+    log.debug('>>>>> START >>>>> %s', nodeid)
+
+
+def pytest_runtest_logfinish(nodeid):
+    '''
+    called after ``pytest_runtest_call``
+    '''
+    log.debug('<<<<< END <<<<<<< %s', nodeid)
+# <---- PyTest Tweaks ------------------------------------------------------------------------------------------------
 
 
 # ----- Test Setup -------------------------------------------------------------------------------------------------->
