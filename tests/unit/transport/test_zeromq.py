f9594b8a1fd62eeb8552136ee27e277c6a116a4c
Jeremy McMillan <jeremy.mcmillan@gmail.com>
2019-01-05 18:38:09 -0600

    don't test invalid combination

--- tests/unit/transport/test_zeromq.py
+++ tests/unit/transport/test_zeromq.py
@@ -360,11 +360,6 @@ class ZMQConfigTest(TestCase):
                                                          master_port=m_port,
                                                          source_port=s_port) == 'tcp://0.0.0.0:{0};{1}:{2}'.format(s_port, m_ip, m_port)
 
-            # pass in only master_port and ipv6 source_ip and source_port
-            assert salt.transport.zeromq._get_master_uri(master_port=m_port,
-                                                         source_ip=m_port,
-                                                         source_port=s_port) == 'tcp://[{1}]:{2};[::1]:{0}'.format(m_port, s_ip6, s_port)
-
 
 class PubServerChannel(TestCase, AdaptedConfigurationTestCaseMixin):
 
