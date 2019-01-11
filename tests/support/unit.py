7374b6e034701a0df32927efd60f68df8b250197
Pedro Algarvio <pedro@algarvio.me>
2019-01-11 13:40:10 +0000

    When running under PyTest, use `pytest.mark.skipif` instead

--- tests/support/unit.py
+++ tests/support/unit.py
@@ -33,7 +33,7 @@ from unittest import (
     expectedFailure,
     TestSuite as _TestSuite,
     skip,
-    skipIf,
+    skipIf as _skipIf,
     TestResult,
     TextTestResult as _TextTestResult
 )
@@ -378,6 +378,14 @@ class TextTestRunner(_TextTestRunner):
     Custom Text tests runner to log the start and the end of a test case
     '''
     resultclass = TextTestResult
+
+
+def skipIf(skip, reason):
+    from tests.support.runtests import RUNTIME_VARS
+    if RUNTIME_VARS.PYTEST_SESSION:
+        import pytest
+        return pytest.mark.skipif(skip, reason=reason)
+    return _skipIf(skip, reason)
 
 
 __all__ = [
