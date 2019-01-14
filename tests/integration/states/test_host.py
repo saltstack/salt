724047c4ce429bfe30c17fed066e92f269fdb008
Pedro Algarvio <pedro@algarvio.me>
2019-01-14 21:58:20 +0000

    Fix hosts tests

--- tests/integration/states/test_host.py
+++ tests/integration/states/test_host.py
@@ -7,6 +7,7 @@ tests for host state
 from __future__ import absolute_import, print_function, unicode_literals
 import os
 import shutil
+import logging
 
 # Import Salt Testing libs
 from tests.support.runtests import RUNTIME_VARS
@@ -17,21 +18,29 @@ from tests.support.mixins import SaltReturnAssertsMixin
 import salt.utils.files
 import salt.utils.stringutils
 
+log = logging.getLogger(__name__)
+
 
 class HostTest(ModuleCase, SaltReturnAssertsMixin):
     '''
     Validate the host state
     '''
 
-    def setUp(self):
-        self.hfpath = os.path.join(RUNTIME_VARS.TMP, 'hosts')
-        shutil.copyfile(os.path.join(RUNTIME_VARS.FILES, 'hosts'), self.hfpath)
-        self.addCleanup(self._delete_hosts_file)
-        super(HostTest, self).setUp()
+    @classmethod
+    def setUpClass(cls):
+        cls.hosts_file = os.path.join(RUNTIME_VARS.TMP, 'hosts')
 
-    def _delete_hosts_file(self):
-        if os.path.exists(self.hfpath):
-            os.remove(self.hfpath)
+    def __clear_hosts(self):
+        '''
+        Delete the tmp hosts file
+        '''
+        if os.path.isfile(self.hosts_file):
+            os.remove(self.hosts_file)
+
+    def setUp(self):
+        shutil.copyfile(os.path.join(RUNTIME_VARS.FILES, 'hosts'), self.hosts_file)
+        self.addCleanup(self.__clear_hosts)
+        super(HostTest, self).setUp()
 
     def test_present(self):
         '''
@@ -41,6 +50,6 @@ class HostTest(ModuleCase, SaltReturnAssertsMixin):
         ip = '10.10.10.10'
         ret = self.run_state('host.present', name=name, ip=ip)
         self.assertSaltTrueReturn(ret)
-        with salt.utils.files.fopen(self.hfpath) as fp_:
+        with salt.utils.files.fopen(self.hosts_file) as fp_:
             output = salt.utils.stringutils.to_unicode(fp_.read())
             self.assertIn('{0}\t\t{1}'.format(ip, name), output)
