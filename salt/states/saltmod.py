622359607339c45f8f40c2f00d3aed3d80f9deec
Daniel A. Wozniak <dwozniak@saltstack.com>
2019-01-14 21:18:11 +0000

    Honor roster file for ssh orchestrations

--- salt/states/saltmod.py
+++ salt/states/saltmod.py
@@ -284,6 +284,8 @@ def state(name,
 
     cmd_kw['tgt_type'] = tgt_type
     cmd_kw['ssh'] = ssh
+    if 'roster' in kwargs:
+        cmd_kw['roster_file'] = kwargs['roster']
     cmd_kw['expect_minions'] = expect_minions
     if highstate:
         fun = 'state.highstate'
