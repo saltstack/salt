76770f33265dff4790ec22c0eb88c6bfa705e4aa
menglong.zhou <menglong.zhou@qunar.com>
2019-01-16 15:24:09 +0800

    ss commmand replace to netstat command

--- salt/utils/network.py
+++ salt/utils/network.py
@@ -1394,6 +1394,7 @@ def _remotes_on(port, which_end):
     Return a set of ip addrs active tcp connections
     '''
     port = int(port)
+
     ret = _netlink_tool_remote_on(port, which_end)
     if ret is not None:
         return ret
