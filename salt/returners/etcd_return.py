055cb789e1c890f997902c70618f707935236dbe
Ali Rizvi-Santiago <arizvisa@gmail.com>
2019-01-21 21:31:22 -0600

    Apparently some random guy in 2015 re-factored everything etcd-related into salt.utils but didn't test anything and hid all the exceptions. To restore functionality we just pass through to the client attribute.

--- salt/returners/etcd_return.py
+++ salt/returners/etcd_return.py
@@ -101,9 +101,10 @@ def _get_conn(opts, profile=None):
     Establish a connection to etcd
     """
     if profile is None:
-        profile = opts.get("etcd.returner")
-    path = opts.get("etcd.returner_root", "/salt/return")
-    return salt.utils.etcd_util.get_conn(opts, profile), path
+        profile = opts.get('etcd.returner')
+    path = opts.get('etcd.returner_root', '/salt/return')
+    wrapper = salt.utils.etcd_util.get_conn(opts, profile)
+    return wrapper.client, path
 
 
 def returner(ret):
