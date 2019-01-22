0d2b0c635f9539993c0487216777c2a3876aa93c
Daniel A. Wozniak <dwozniak@saltstack.com>
2019-01-22 10:40:57 -0700

    Fix linter warnings

--- tests/unit/config/__init__.py
+++ tests/unit/config/__init__.py
@@ -5,10 +5,11 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 # Import Salt Testing Libs
 from tests.support.unit import TestCase, skipIf
-import mock
+from tests.support.mock import  mock
 
 import salt.config
 import salt.syspaths
+
 
 class ConfigTest(TestCase):
 
