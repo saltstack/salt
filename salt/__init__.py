14a6d64e3062ce42c2b070f97c1daffaac0df711
Wayne Werner <wwerner@saltstack.com>
2019-01-28 10:53:10 -0600

    Add Python2 depreciation warning

--- salt/__init__.py
+++ salt/__init__.py
@@ -29,7 +29,6 @@ warnings.filterwarnings(
     UserWarning
 )
 
-
 def __define_global_system_encoding_variable__():
     import sys
     # This is the most trustworthy source of the system encoding, though, if
@@ -95,3 +94,17 @@ __define_global_system_encoding_variable__()
 
 # This is now garbage collectable
 del __define_global_system_encoding_variable__
+
+
+# REMOVEME after Python 2.7 support is dropped
+import six
+
+if six.PY2:
+    from salt.utils.versions import warn_until
+    # Message borrowed from pip's deprecation warning
+    warn_until('Sodium',
+               'Python 2.7 will reach the end of its life on January 1st, 2020.'
+               ' Please upgrade your Python as Python 2.7 won\'t be maintained'
+               ' after that date.  Salt will drop support for Python 2.7 in the'
+               ' Sodium release.')
+# END REMOVEME
