2dc51714f61904bbfd7f32ffe700e8ec9bb2ffbe
Daniel A. Wozniak <dwozniak@saltstack.com>
2019-01-30 15:06:28 -0700

    Hanlde multi_master failover when daemonized

--- salt/cli/daemons.py
+++ salt/cli/daemons.py
@@ -340,6 +340,16 @@ class Minion(parsers.MinionOptionParser, DaemonsMixin):  # pylint: disable=no-in
         NOTE: Run any required code before calling `super()`.
         '''
         super(Minion, self).start()
+        while True:
+            try:
+                self._real_start()
+            except SaltClientError as exc:
+                # Restart for multi_master failover when daemonized
+                if self.options.daemon:
+                    continue
+            break
+
+    def _real_start(self):
         try:
             if check_user(self.config['user']):
                 self.action_log_info('Starting up')
