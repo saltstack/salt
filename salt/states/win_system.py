804a56c246da91088b6382f41f8e3537b642fd19
Ali Rizvi-Santiago <arizvisa@gmail.com>
2019-02-05 02:35:27 -0600

    Modified the workgroup state to be better documented, support running in test-mode, and to validate whether the name has actually changed or not.

--- salt/states/win_system.py
+++ salt/states/win_system.py
@@ -184,33 +184,43 @@ def workgroup(name):
           system.workgroup:
             - name: local
     '''
-    ret = {
-        'name': name.upper(),
-        'changes': {},
-        'result': True,
-        'comment': ''
-    }
+    ret = {'name': name.upper(), 'result': False, 'changes': {}, 'comment': ''}
 
-    res = __salt__['system.get_domain_workgroup']()
-    current_workgroup = res['Domain'] if 'Domain' in res else res['Workgroup'] if 'Workgroup' in res else ''
+    # Grab the current domain/workgroup
+    out = __salt__['system.get_domain_workgroup']()
+    current_workgroup = out['Domain'] if 'Domain' in out else out['Workgroup'] if 'Workgroup' in out else ''
 
-    if __opts__['test']:
-        ret['result'] = None
-        ret['comment'] = 'Computer will be joined to workgroup \'{0}\''.format(name)
-        return ret
-
+    # Notify the user if the requested workgroup is the same
     if current_workgroup.upper() == name.upper():
+        ret['result'] = True
         ret['comment'] = "Workgroup is already set to '{0}'".format(name.upper())
         return ret
 
-    out = __salt__['system.set_domain_workgroup'](name.upper())
+    # If being run in test-mode, inform the user what is supposed to happen
+    if __opts__['test']:
+        ret['result'] = None
+        ret['changes'] = {}
+        ret['comment'] = 'Computer will be joined to workgroup \'{0}\''.format(name)
+        return ret
 
-    if out:
-        ret['comment'] = "The workgroup has been changed from '{0}' to '{1}".format(current_workgroup.upper(), name.upper())
-        ret['changes'] = {'workgroup': name.upper()}
+    # Set our new workgroup, and then immediately ask the machine what it
+    # is again to validate the change
+    res = __salt__['system.set_domain_workgroup'](name.upper())
+    out = __salt__['system.get_domain_workgroup']()
+    changed_workgroup = out['Domain'] if 'Domain' in out else out['Workgroup'] if 'Workgroup' in out else ''
+
+    # Return our results based on the changes
+    ret = {}
+    if res and current_workgroup.upper() == changed_workgroup.upper():
+        ret['result'] = True
+        ret['comment'] = "The new workgroup '{0}' is the same as '{1}'".format(current_workgroup.upper(), changed_workgroup.upper())
+    elif res:
+        ret['result'] = True
+        ret['comment'] = "The workgroup has been changed from '{0}' to '{1}'".format(current_workgroup.upper(), changed_workgroup.upper())
+        ret['changes'] = {'old', current_workgroup.upper(), 'new': changed_workgroup.upper()}
     else:
         ret['result'] = False
-        ret['comment'] = 'Unable to set the workgroup'
+        ret['comment'] = "Unable to join the requested workgroup '{0}'".format(changed_workgroup.upper())
 
     return ret
 
