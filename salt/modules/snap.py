be7ca380281be3b6dd77a8750d2ec094f8b38f89
Nathan Fish <lordcirth@gmail.com>
2019-02-08 22:06:41 +0000

    modules/snap: install, is_installed

--- salt/modules/snap.py
+++ salt/modules/snap.py
@@ -8,8 +8,11 @@ Manage snap packages via Salt
 
 from __future__ import absolute_import, print_function, unicode_literals
 import os
+import subprocess
 
 import salt.utils.path
+
+SNAP_BINARY_NAME='snap'
 
 __virtualname__ = 'snap'
 
@@ -19,5 +22,11 @@ def __virtual__():
 
     return (False, 'The snap execution module cannot be loaded: the "snap" binary is not in the path.')
 
-def install():
-    return True
+def install(pkg):
+    retcode = subprocess.call([SNAP_BINARY_NAME, 'install', pkg])
+    return retcode == 0
+
+def is_installed(pkg):
+    # snap returns 0 if there is at least one match, otherwise 1
+    retcode = subprocess.call([SNAP_BINARY_NAME, 'list', pkg])
+    return retcode == 0
