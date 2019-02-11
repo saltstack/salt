d824d5d1cc9bd5046e4042e61540535043d99030
Benjamin Drung <benjamin.drung@cloud.ionos.com>
2019-02-11 13:47:49 +0100

    Fix typo of initialize
    Signed-off-by: Benjamin Drung <benjamin.drung@cloud.ionos.com>

--- salt/modules/swarm.py
+++ salt/modules/swarm.py
@@ -75,7 +75,7 @@ def swarm_tokens():
 
 def swarm_init(advertise_addr=str, listen_addr=int, force_new_cluster=bool):
     """
-    Initalize Docker on Minion as a Swarm Manager
+    Initialize Docker on Minion as a Swarm Manager
 
     advertise_addr
         The ip of the manager
