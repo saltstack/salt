2e5f72bf4d6ebb56c51b3f2d2ff48867f041b11f
Benjamin Drung <benjamin.drung@cloud.ionos.com>
2019-02-11 12:52:09 +0100

    Fix typo of additional
    Signed-off-by: Benjamin Drung <benjamin.drung@cloud.ionos.com>

--- salt/modules/dockermod.py
+++ salt/modules/dockermod.py
@@ -3513,7 +3513,7 @@ def run_container(
                         exc_info.setdefault("other_errors", []).append(
                             "Failed to auto_remove container: {0}".format(rm_exc)
                         )
-                # Raise original exception with additonal info
+                # Raise original exception with additional info
                 raise CommandExecutionError(exc.__str__(), info=exc_info)
 
         # Start the container
