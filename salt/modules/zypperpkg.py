09cd99c22839d4c0a7f2338224130b5e3fef91a1
Benjamin Drung <benjamin.drung@cloud.ionos.com>
2019-02-11 13:48:12 +0100

    Fix typo of support
    Signed-off-by: Benjamin Drung <benjamin.drung@cloud.ionos.com>

--- salt/modules/zypperpkg.py
+++ salt/modules/zypperpkg.py
@@ -62,7 +62,7 @@ def __virtual__():
     Set the virtual pkg module if the os is openSUSE
     '''
     if __grains__.get('os_family', '') != 'Suse':
-        return (False, "Module zypper: non SUSE OS not suppored by zypper package manager")
+        return (False, "Module zypper: non SUSE OS not supported by zypper package manager")
     # Not all versions of SUSE use zypper, check that it is available
     if not salt.utils.path.which('zypper'):
         return (False, "Module zypper: zypper package manager not found")
