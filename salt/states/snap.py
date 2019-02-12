f5d317db6c3fcc79696e824bb3b618d2ae2f90d5
Nathan Fish <lordcirth@gmail.com>
2019-02-12 19:50:46 +0000

    states/snap: start on installed

--- salt/states/snap.py
+++ salt/states/snap.py
@@ -4,6 +4,10 @@ Management of snap packages
 ===============================================
 '''
 from __future__ import absolute_import, print_function, unicode_literals
+import salt.utils.path
+
+__virtualname__ = 'snap'
+
 
 def __virtual__():
     if salt.utils.path.which('snap'):
@@ -22,5 +26,21 @@ def installed(name, channel=None):
     channel : str
         Optional. The channel to install the package from.
     '''
+    ret = {'name': name,
+           'changes': {},
+           'result': None,
+           'comment': ''}
 
-    old = __salt__['snap.is_installed']
+    old = __salt__['snap.versions_installed'](name)
+    if not old:
+        if __opts__['test']:
+            ret['comment'] = 'Package "{0}" would have been installed'.format(name)
+            ret['pchanges']['installed'] = name
+            ret['result'] = None
+            return ret
+
+        if __salt__['snap.install'](name, channel):
+            ret['comment'] = 'Package "{0}" was installed'.format(name)
+            ret['changes']['installed'] = name
+            ret['result'] = True
+            return ret
