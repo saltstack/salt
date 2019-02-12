1101fb3443550317ad12d9ea984be961b87d34f6
Nathan Fish <lordcirth@gmail.com>
2019-02-12 18:28:51 +0000

    modules/snap: linting

--- salt/modules/snap.py
+++ salt/modules/snap.py
@@ -7,20 +7,21 @@ Manage snap packages via Salt
 '''
 
 from __future__ import absolute_import, print_function, unicode_literals
-import os
 import subprocess
 
 import salt.utils.path
 
-SNAP_BINARY_NAME='snap'
+SNAP_BINARY_NAME = 'snap'
 
 __virtualname__ = 'snap'
+
 
 def __virtual__():
     if salt.utils.path.which('snap'):
         return __virtualname__
 
     return (False, 'The snap execution module cannot be loaded: the "snap" binary is not in the path.')
+
 
 def install(pkg, channel=None):
     args = []
@@ -30,12 +31,15 @@ def install(pkg, channel=None):
     retcode = subprocess.call([SNAP_BINARY_NAME, 'install', pkg] + args)
     return retcode == 0
 
+
 def is_installed(pkg):
     return bool(versions_installed(pkg))
+
 
 def remove(pkg):
     retcode = subprocess.call([SNAP_BINARY_NAME, 'remove', pkg])
     return retcode == 0
+
 
 # Parse 'snap list' into a dict
 def versions_installed(pkg):
