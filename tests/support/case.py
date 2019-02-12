72f65fe83222ddc6f2cc587f2065f9f7a2f1f590
Daniel A. Wozniak <dwozniak@saltstack.com>
2019-02-11 17:25:22 -0700

    Disable pylint checks, only for 2017.7

--- tests/support/case.py
+++ tests/support/case.py
@@ -141,14 +141,14 @@ class ShellTestCase(TestCase, AdaptedConfigurationTestCaseMixin):
         )
         return self.run_script('salt-ssh', arg_str, with_retcode=with_retcode, catch_stderr=catch_stderr, raw=True)
 
-    def run_run(self, arg_str, with_retcode=False, catch_stderr=False, async=False, timeout=60, config_dir=None):
+    def run_run(self, arg_str, with_retcode=False, catch_stderr=False, async=False, timeout=60, config_dir=None):  # pylint: disable=W8606
         '''
         Execute salt-run
         '''
         arg_str = '-c {0}{async_flag} -t {timeout} {1}'.format(config_dir or self.get_config_dir(),
                                                  arg_str,
                                                  timeout=timeout,
-                                                 async_flag=' --async' if async else '')
+                                                 async_flag=' --async' if async else '')  # pylint: disable=W8606
         return self.run_script('salt-run', arg_str, with_retcode=with_retcode, catch_stderr=catch_stderr)
 
     def run_run_plus(self, fun, *arg, **kwargs):
@@ -498,14 +498,14 @@ class ShellCase(ShellTestCase, AdaptedConfigurationTestCaseMixin, ScriptPathMixi
                                timeout=timeout,
                                raw=True)
 
-    def run_run(self, arg_str, with_retcode=False, catch_stderr=False, async=False, timeout=60, config_dir=None):
+    def run_run(self, arg_str, with_retcode=False, catch_stderr=False, async=False, timeout=60, config_dir=None):  # pylint: disable=W8606
         '''
         Execute salt-run
         '''
         arg_str = '-c {0}{async_flag} -t {timeout} {1}'.format(config_dir or self.get_config_dir(),
                                                                arg_str,
                                                                timeout=timeout,
-                                                               async_flag=' --async' if async else '')
+                                                               async_flag=' --async' if async else '')  # pylint: disable=W8606
         return self.run_script('salt-run',
                                arg_str,
                                with_retcode=with_retcode,
