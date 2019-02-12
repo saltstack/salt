be79c79fbbb8e52ef08ba2ccc7d8c7c3bc9d6c58
Nathan Fish <lordcirth@gmail.com>
2019-02-12 17:29:24 +0000

    modules/snap: install from channel, remove

--- salt/modules/snap.py
+++ salt/modules/snap.py
@@ -22,11 +22,19 @@ def __virtual__():
 
     return (False, 'The snap execution module cannot be loaded: the "snap" binary is not in the path.')
 
-def install(pkg):
-    retcode = subprocess.call([SNAP_BINARY_NAME, 'install', pkg])
+def install(pkg, channel=None):
+    args = []
+    if type(channel) is str:
+        args += '--channel'
+        args += channel
+    retcode = subprocess.call([SNAP_BINARY_NAME, 'install', pkg] + args)
     return retcode == 0
 
 def is_installed(pkg):
     # snap returns 0 if there is at least one match, otherwise 1
     retcode = subprocess.call([SNAP_BINARY_NAME, 'list', pkg])
+    return retcode == 0
+
+def remove(pkg):
+    retcode = subprocess.call([SNAP_BINARY_NAME, 'remove', pkg])
     return retcode == 0
