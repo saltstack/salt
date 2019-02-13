0c4a48523328246815b446e9a58125ea28744882
Daniel A. Wozniak <dwozniak@saltstack.com>
2019-02-12 21:48:41 -0700

    Add missing module variable

--- tests/integration/modules/test_ssh.py
+++ tests/integration/modules/test_ssh.py
@@ -20,6 +20,9 @@ import salt.utils.platform
 # Import 3rd-party libs
 from tornado.httpclient import HTTPClient
 
+SUBSALT_DIR = os.path.join(TMP, 'subsalt')
+AUTHORIZED_KEYS = os.path.join(SUBSALT_DIR, 'authorized_keys')
+KNOWN_HOSTS = os.path.join(SUBSALT_DIR, 'known_hosts')
 GITHUB_FINGERPRINT = '9d:38:5b:83:a9:17:52:92:56:1a:5e:c4:d4:81:8e:0a:ca:51:a2:64:f1:74:20:11:2e:f8:8a:c3:a1:39:49:8f'
 
 
