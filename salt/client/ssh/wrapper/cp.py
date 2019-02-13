e49b174f7aa9dccd105cf9ac086ceaa6fd05541e
Ronald van Zantvoort <the.loeki@gmail.com>
2019-02-13 23:35:44 +0100

    cp wrapper feature-parity w/old method

--- salt/client/ssh/wrapper/cp.py
+++ salt/client/ssh/wrapper/cp.py
@@ -53,6 +53,9 @@ class SSHClient(FSClient):
         return not ret[2]
 
 
+
+
+
 def _mk_client():
     '''
     Create a file client and add it to the context.
@@ -67,120 +70,54 @@ def _mk_client():
             SSHClient(__opts__)
 
 
-def get_file(*args, **kwargs):
+def _fix_me():
     salt.modules.cp._mk_client = _mk_client
     salt.modules.cp.__context__ = __context__
     salt.modules.cp.__opts__ = __opts__
     salt.modules.cp.__salt__ = __salt__
-    log.warning('YOURE HERE')
+    log.warning('MODULE LOADED')
+    
+
+def get_file(*args, **kwargs):
+    _fix_me()
     return salt.modules.cp.get_file(*args, **kwargs)
 
 
 def cache_file(*args, **kwargs):
-    salt.modules.cp._mk_client = _mk_client
-    salt.modules.cp.__context__ = __context__
-    salt.modules.cp.__opts__ = __opts__
-    salt.modules.cp.__salt__ = __salt__
-    log.warning('YOURE HERE')
+    _fix_me()
     return salt.modules.cp.cache_file(*args, **kwargs)
 
-#
-#
-#
-# def get_file(path,
-#              dest,
-#              saltenv='base',
-#              makedirs=False,
-#              template=None,
-#              gzip=None):
-#     '''
-#     Send a file from the master to the location in specified
-#
-#     .. note::
-#
-#         gzip compression is not supported in the salt-ssh version of
-#         cp.get_file. The argument is only accepted for interface compatibility.
-#     '''
-#     if gzip is not None:
-#         log.warning('The gzip argument to cp.get_file in salt-ssh is '
-#                     'unsupported')
-#
-#     if template is not None:
-#         (path, dest) = _render_filenames(path, dest, saltenv, template)
-#
-#     src = __context__['fileclient'].cache_file(
-#         path,
-#         saltenv,
-#         cachedir=os.path.join('salt-ssh', __salt__.kwargs['id_']))
-#     single = salt.client.ssh.Single(
-#             __opts__,
-#             '',
-#             **__salt__.kwargs)
-#     ret = single.shell.send(src, dest, makedirs)
-#     return not ret[2]
-#
-#
-# def get_dir(path, dest, saltenv='base'):
-#     '''
-#     Transfer a directory down
-#     '''
-#     src = __context__['fileclient'].cache_dir(
-#         path,
-#         saltenv,
-#         cachedir=os.path.join('salt-ssh', __salt__.kwargs['id_']))
-#     src = ' '.join(src)
-#     single = salt.client.ssh.Single(
-#             __opts__,
-#             '',
-#             **__salt__.kwargs)
-#     ret = single.shell.send(src, dest)
-#     return not ret[2]
-#
-#
-# def get_url(path, dest, saltenv='base'):
-#     '''
-#     retrieve a URL
-#     '''
-#     src = __context__['fileclient'].cache_file(
-#         path,
-#         saltenv,
-#         cachedir=os.path.join('salt-ssh', __salt__.kwargs['id_']))
-#     single = salt.client.ssh.Single(
-#             __opts__,
-#             '',
-#             **__salt__.kwargs)
-#     ret = single.shell.send(src, dest)
-#     return not ret[2]
-#
-#
-# def list_states(saltenv='base'):
-#     '''
-#     List all the available state modules in an environment
-#     '''
-#     return __context__['fileclient'].list_states(saltenv)
-#
-#
-# def list_master(saltenv='base', prefix=''):
-#     '''
-#     List all of the files stored on the master
-#     '''
-#     return __context__['fileclient'].file_list(saltenv, prefix)
-#
-#
-# def list_master_dirs(saltenv='base', prefix=''):
-#     '''
-#     List all of the directories stored on the master
-#     '''
-#     return __context__['fileclient'].dir_list(saltenv, prefix)
-#
-#
-# def list_master_symlinks(saltenv='base', prefix=''):
-#     '''
-#     List all of the symlinks stored on the master
-#     '''
-#     return __context__['fileclient'].symlink_list(saltenv, prefix)
-#
-#
+
+def get_dir(*args, **kwargs):
+    _fix_me()
+    return salt.modules.cp.get_dir(*args, **kwargs)
+
+
+def get_url(*args, **kwargs):
+    _fix_me()
+    return salt.modules.cp.get_url(*args, **kwargs)
+
+
+def list_states(*args, **kwargs):
+    _fix_me()
+    return salt.modules.cp.list_states(*args, **kwargs)
+
+
+def list_master(*args, **kwargs):
+    _fix_me()
+    return salt.modules.cp.list_master(*args, **kwargs)
+
+
+def list_master_dirs(*args, **kwargs):
+    _fix_me()
+    return salt.modules.cp.list_master_dirs(*args, **kwargs)
+
+
+def list_master_symlinks(*args, **kwargs):
+    _fix_me()
+    return salt.modules.cp.list_master_symlinks(*args, **kwargs)
+
+
 # def _render_filenames(path, dest, saltenv, template):
 #     '''
 #     Process markup in the :param:`path` and :param:`dest` variables (NOT the
