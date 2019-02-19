50cd57d0308521721efa74b8b5de78349e81422d
Ali Rizvi-Santiago <arizvisa@gmail.com>
2019-02-19 15:14:22 -0600

    Fix the Cheetah template renderer in salt.utils.templates.

--- salt/utils/templates.py
+++ salt/utils/templates.py
@@ -520,7 +520,21 @@ def render_cheetah_tmpl(tmplstr, context, tmplpath=None):
     Render a Cheetah template.
     '''
     from Cheetah.Template import Template
-    return salt.utils.data.decode(Template(tmplstr, searchList=[context]))
+
+    # Compile the template and render it into the class
+    tclass = Template.compile(tmplstr)
+    data = tclass(namespaces=[context])
+
+    # Figure out which method to call based on the type of tmplstr
+    if isinstance(tmplstr, unicode):
+        res = unicode(data)
+    elif isinstance(tmplstr, bytes):
+        res = str(data)
+    else:
+        raise SaltRenderError('Unknown type {!s} for Cheetah template while trying to render.'.format(type(tmplstr)))
+
+    # Now we can decode it to the correct encoding
+    return salt.utils.data.decode(res)
 # pylint: enable=3rd-party-module-not-gated
 
 
