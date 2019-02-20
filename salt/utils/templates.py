bd60898929004ab396f1fe1109ebfc262046aadd
Ali Rizvi-Santiago <arizvisa@gmail.com>
2019-02-20 15:48:30 -0600

    Modified the render_cheetah_tmpl() function in salt.util.templates to call the correct methods depending on Python2 vs Python3.

--- salt/utils/templates.py
+++ salt/utils/templates.py
@@ -526,9 +526,14 @@ def render_cheetah_tmpl(tmplstr, context, tmplpath=None):
     data = tclass(namespaces=[context])
 
     # Figure out which method to call based on the type of tmplstr
-    if isinstance(tmplstr, unicode):
-        res = unicode(data)
-    elif isinstance(tmplstr, bytes):
+    if six.PY3 and isinstance(tmplstr, six.string_types):
+        # This should call .__unicode__()
+        res = str(data)
+    elif six.PY2 and isinstance(tmplstr, six.text_type):
+        # Expicitly call .__unicode__()
+        res = data.__unicode__()
+    elif isinstance(tmplstr, six.binary_type):
+        # This should call .__str()
         res = str(data)
     else:
         raise SaltRenderError('Unknown type {!s} for Cheetah template while trying to render.'.format(type(tmplstr)))
