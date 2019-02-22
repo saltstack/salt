07b433345fdb15adfb5726ef23600d8be927ba01
Daniel A. Wozniak <dwozniak@saltstack.com>
2019-02-21 20:44:33 -0700

    Add cwd grain for working directory

--- salt/grains/core.py
+++ salt/grains/core.py
@@ -2373,6 +2373,13 @@ def get_machine_id():
             return {'machine_id': machineid.read().strip()}
 
 
+def cwd():
+    '''
+    Current working directory
+    '''
+    return {'cwd': os.getcwd()}
+
+
 def path():
     '''
     Return the path
