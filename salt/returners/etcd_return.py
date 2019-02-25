d104d34965978105c0d2ed6e2058fe9771ebac94
Ali Rizvi-Santiago <arizvisa@gmail.com>
2019-02-25 07:16:59 -0600

    Lol! I didn't realize how fucking busted the original salt.returners.etcd_return module was....Fixed the get_jids function so that it actually returns the correct data instead of just the return field.

--- salt/returners/etcd_return.py
+++ salt/returners/etcd_return.py
@@ -649,16 +649,33 @@ def get_jids():
     # Anything that's a directory is a job id. Since that's all we're returning,
     # aggregate them into a list.
     log.debug("sdstack_etcd returner <get_jids> iterating through jobs at {path:s}".format(path=jobs.key))
-    ret = []
+    ret = {}
     for job in jobs.leaves:
-        comps = job.key.split('/')
         if not job.dir:
-            log.warning('sdstack_etcd returner <get_minions> found a non-job at {path:s} {expire:s}'.format(path=job.key, expire='that will need to be manually removed' if job.ttl is None else 'that will expire in {ttl:d} seconds'.format(ttl=job.ttl)))
+            log.warning('sdstack_etcd returner <get_jids> found a non-job at {path:s} {expire:s}'.format(path=job.key, expire='that will need to be manually removed' if job.ttl is None else 'that will expire in {ttl:d} seconds'.format(ttl=job.ttl)))
             continue
 
-        jid = comps[-1]
-        ret.append(jid)
+        jid = job.key.split('/')[-1]
+        loadp = '/'.join([job.key, '.load.p'])
 
+        # Now we can load the data from the job
+        try:
+            res = client.read(loadp)
+        except etcd.EtcdKeyNotFound as E:
+            log.error("sdstack_etcd returner <get_jids> could not find job data {jid:s} at the path {path:s}".format(jid=jid, path=loadp))
+            continue
+
+        # Decode the load data so we can stash the job data for our caller
+        try:
+            data = salt.utils.json.loads(res.value)
+
+        # If we can't decode the json, then we're screwed so log it in case the user cares
+        except Exception as E:
+            log.error("sdstack_etcd returner <get_jids> could not decode data for job {jid:s} at the path {path:s} due to exception ({exception}). Data was {data:s}".format(jid=jid, path=loadp, exception=E, data=res.value))
+            continue
+
+        # Cool. Everything seems to be good...
+        ret[jid] = salt.utils.jid.format_jid_instance(jid, data)
         log.trace("sdstack_etcd returner <get_jids> found job {jid:s} at {path:s}".format(jid=jid, path=job.key))
 
     log.debug("sdstack_etcd returner <get_jids> found {count:d} jobs at {path:s}".format(count=len(ret), path=jobs.key))
@@ -838,10 +855,13 @@ def get_jids_filter(count, filter_find_job=True):
             log.error("sdstack_etcd returner <get_jids_filter> could not find job data {jid:s} at the path {path:s}".format(jid=jid, path=loadp))
             continue
 
+        # Decode the load data so we can stash it for the caller
         try:
             data = salt.utils.json.loads(res.value)
-        except:
-            log.error("sdstack_etcd returner <get_jids_filter> could not decode job data {jid:s} at the path {path:s}".format(jid=jid, path=loadp))
+
+        # If we can't decode the json, then we're screwed so log it in case the user cares
+        except Exception as E:
+            log.error("sdstack_etcd returner <get_jids_filter> could not decode data for job {jid:s} at the path {path:s} due to exception ({exception}). Data was {data:s}".format(jid=jid, path=loadp, exception=E, data=res.value))
             continue
 
         if filter_find_job and data['fun'] == 'saltutil.find_job':
