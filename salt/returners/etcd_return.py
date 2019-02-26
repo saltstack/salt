4fb9a848d1124dc424a3b5108238f00597c17b44
Ali Rizvi-Santiago <arizvisa@gmail.com>
2019-02-26 06:37:18 -0600

    Modified salt.etcd_return.returner to update the job load data instead of overwriting it in order to preserve the createdIndex of the key.

--- salt/returners/etcd_return.py
+++ salt/returners/etcd_return.py
@@ -217,7 +217,7 @@ def returner(ret):
         try:
             res = client.write(fieldp, data)
         except Exception as E:
-            log.trace("sdstack_etcd returner <returner> unable to set field {field:s} for job {jid:s} at {path:s} to {result}".format(field=field, jid=ret['jid'], path=fieldp, result=ret[field]))
+            log.trace("sdstack_etcd returner <returner> unable to set field {field:s} for job {jid:s} at {path:s} to {result} due to exception ({exception})".format(field=field, jid=ret['jid'], path=fieldp, result=ret[field], exception=E))
             exceptions.append((E, field, ret[field]))
             continue
         log.trace("sdstack_etcd returner <returner> set field {field:s} for job {jid:s} at {path:s} to {result}".format(field=field, jid=ret['jid'], path=res.key, result=ret[field]))
@@ -236,23 +236,33 @@ def save_load(jid, load, minions=None):
     write_profile = __opts__.get('etcd.returner_write_profile')
     client, path, ttl = _get_conn(__opts__, write_profile)
 
-    # Check if the specified jid is 'req', as only incorrect code will do that
+    # Check if the specified jid is 'req', as only incorrect code will do this
     if jid == 'req':
         log.warning('sdstack_etcd returner <save_load> was called using a request job id ({jid:s}) with {data:s}'.format(jid=jid, data=load))
 
-    # Build the paths that we'll use for our job
+    # Build the paths that we'll use for registration of our job
     loadp = '/'.join([path, Schema['job-cache'], jid, '.load.p'])
     lockp = '/'.join([path, Schema['job-cache'], jid, '.lock.p'])
 
     ## Now we can just store the current load
-    data = salt.utils.json.dumps(load)
+    json = salt.utils.json.dumps(load)
 
     log.debug('sdstack_etcd returner <save_load> storing load data for job {jid:s} to {path:s} with {data:s}'.format(jid=jid, path=loadp, data=load))
     try:
-        res = client.write(loadp, data)
+        res = client.write(loadp, json, prevExist=False)
+
+    # If the key already exists, then warn the user and update the key. There
+    # isn't anything we can really do about this because it's up to Salt really.
+    except etcd.EtcdAlreadyExist as E:
+        node = client.read(loadp)
+        node.value = json
+
+        log.debug('sdstack_etcd returner <save_load> updating load data for job {jid:s} at {path:s} with {data:s}'.format(jid=jid, path=loadp, data=load))
+        res = client.update(node)
+        log.warning("sdstack_etcd returner <save_load> updated the load data for job {jid:s} at {path:s} with {data:s}. Old data was {old:s}".format(jid=jid, path=res.key, data=res.value, old=res._prev_node.value))
 
     # If we failed here, it's okay because the lock won't get written so this
-    # will get scheduled for deletion.
+    # essentially means the job will get scheduled for deletion.
     except Exception as E:
         log.trace("sdstack_etcd returner <save_load> unable to store load for job {jid:s} to the path {path:s} due to exception ({exception}) being raised".format(jid=jid, path=loadp, exception=E))
         return
@@ -767,11 +777,11 @@ def event_return(events):
         # and tampering with the createdIndex
         except etcd.EtcdAlreadyExist as E:
             log.trace("sdstack_etcd returner <event_return> fetching already existing event with the tag {name:s} at {path:s}".format(name=package['tag'], path=packagep))
-            res = client.read(packagep)
+            node = client.read(packagep)
 
             log.update("sdstack_etcd returner <event_return> updating package for event ({event:d}) with the tag {name:s} at {path:s}".format(event=res.createdIndex, name=package['tag'], path=packagep))
-            res.value = json
-            client.update(res)
+            node.value = json
+            res = client.update(node)
 
         except Exception as E:
             log.trace("sdstack_etcd returner <event_return> unable to write event with the tag {name:s} into {path:s} due to exception ({exception}) being raised".format(name=package['tag'], path=packagep, exception=E))
