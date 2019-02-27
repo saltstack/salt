b20739a7dbd990f7abb8e46235c51109a0ce3e32
Ali Rizvi-Santiago <arizvisa@gmail.com>
2019-02-26 19:21:29 -0600

    Changed the comparison in the previous commit that silenced a log in salt.returners.etcd_return to do a proper deep-comparision instead of a simple string-comparison.

--- salt/returners/etcd_return.py
+++ salt/returners/etcd_return.py
@@ -200,7 +200,7 @@ def returner(ret):
     # which will expire according to the ttl anyways..
     log.debug("sdstack_etcd returner <returner> updating (last) job id of {minion:s} at {path:s} with job {jid:s}".format(jid=ret['jid'], minion=ret['id'], path=minionp))
     res = client.write(minionp, ret['jid'], ttl=ttl if ttl > 0 else None)
-    if hasattr(res, '_prev_node'):
+    if not res.newKey:
         log.debug("sdstack_etcd returner <returner> the previous job id {old:s} for {minion:s} at {path:s} was set to {new:s}".format(old=res._prev_node.value, minion=ret['id'], path=minionp, new=res.value))
 
     # Figure out the path for the specified job and minion
@@ -259,14 +259,27 @@ def save_load(jid, load, minions=None):
 
         log.debug('sdstack_etcd returner <save_load> updating load data for job {jid:s} at {path:s} with {data:s}'.format(jid=jid, path=loadp, data=load))
         res = client.update(node)
-        if res._prev_node.value != res.value:
-            log.warning("sdstack_etcd returner <save_load> overwrote the load data for job {jid:s} at {path:s} with {data:s}. Old data was {old:s}".format(jid=jid, path=res.key, data=res.value, old=res._prev_node.value))
 
     # If we failed here, it's okay because the lock won't get written so this
     # essentially means the job will get scheduled for deletion.
     except Exception as E:
         log.trace("sdstack_etcd returner <save_load> unable to store load for job {jid:s} to the path {path:s} due to exception ({exception}) being raised".format(jid=jid, path=loadp, exception=E))
         return
+
+    # Check if the previous node value and the current node value are different
+    # so we can let the user know that something changed and that some data
+    # might've been lost.
+    try:
+        if not res.newKey:
+            d1, d2 = salt.utils.json.loads(res.value), salt.utils.json.loads(res._prev_node.value)
+            j1, j2 = salt.utils.json.dumps(res.value, sort_keys=True), salt.utils.json.dumps(res._prev_node.value, sort_keys=True)
+            if j1 != j2:
+                log.warning("sdstack_etcd returner <save_load> overwrote the load data for job {jid:s} at {path:s} with {data:s}. Old data was {old:s}".format(jid=jid, path=res.key, data=d1, old=d2))
+    except Exception as E:
+        log.debug("sdstack_etcd returner <save_load> unable to compare load data for job {jid:s} at {path:s} due to exception ({exception}) being raised".format(jid=jid, path=loadp, exception=E))
+        if not res.newKey:
+            log.trace("sdstack_etcd returner <save_load> -- old load data for job {jid:s}: {data:s}".format(jid=jid, data=res._prev_node.value))
+        log.trace("sdstack_etcd returner <save_load> -- new load data for job {jid:s}: {data:s}".format(jid=jid, data=res.value))
 
     # Since this is when a job is being created, create a lock that we can
     # check to see if the job has expired. This allows a user to signal to
