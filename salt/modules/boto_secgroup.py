b98ee961d58ef9e40c14d75fc2df4a58d656e6f3
Tom Williams <tom.williams@hotschedules.com>
2019-02-28 12:49:53 -0500

    INFRA-5209 - Avoid throwing exception for missing security group in test mode

--- salt/modules/boto_secgroup.py
+++ salt/modules/boto_secgroup.py
@@ -334,8 +334,15 @@ def convert_to_group_ids(groups, vpc_id=None, vpc_name=None, region=None, key=No
                                 key=key, keyid=keyid, profile=profile)
         if not group_id:
             # Security groups are a big deal - need to fail if any can't be resolved...
-            raise CommandExecutionError('Could not resolve Security Group name '
-                                        '{0} to a Group ID'.format(group))
+            # But... if we're running in test mode, it may just be that the SG is scheduled
+            # to be created, and thus WOULD have been there if running "for real"...
+            if __opts__['test']:
+                log.warn('Security Group `%s` could not be resolved to an ID.  This may '
+                         'cause a failure when not running in test mode.', group)
+                return []
+            else:
+                raise CommandExecutionError('Could not resolve Security Group name '
+                                            '{0} to a Group ID'.format(group))
         else:
             group_ids.append(six.text_type(group_id))
     log.debug('security group contents %s post-conversion', group_ids)
