64d8451dcbeae833909574afe058f7e1e27c25e4
Alexander Fischer <afischer@opentext.com>
2019-03-06 16:04:07 +0100

    Use default file module

--- tests/unit/modules/test_win_file.py
+++ tests/unit/modules/test_win_file.py
@@ -19,6 +19,7 @@ from tests.support.helpers import destructiveTest
 
 # Import Salt Libs
 import salt.modules.win_file as win_file
+import salt.modules.file as default_file
 import salt.modules.temp as temp
 from salt.exceptions import CommandExecutionError
 import salt.utils.platform
@@ -318,7 +319,7 @@ class WinFileCheckPermsTestCase(TestCase, LoaderModuleMockMixin):
         symlink = os.path.join(base, 'child 2', 'link')
         self.assertFalse(win_file.directory_exists(target))
         self.assertFalse(win_file.directory_exists(symlink))
-        self.assertTrue(win_file.makedirs_(target))
+        self.assertTrue(default_file.makedirs_(target))
         self.assertTrue(win_file.directory_exists(symlink))
         self.assertTrue(win_file.symlink(target, symlink))
         self.assertTrue(win_file.is_link(symlink))
