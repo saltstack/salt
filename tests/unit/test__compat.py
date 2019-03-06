a7919017759215eb468cd4dc3b88bb63382aa2e8
twangboy <slee@saltstack.com>
2019-03-05 18:38:51 -0700

    Fix compat, add tests

--- tests/unit/test__compat.py
+++ tests/unit/test__compat.py
@@ -69,3 +69,15 @@ class CompatTestCase(TestCase):
         else:
             expected = 'StringIO.StringIO instance'
         self.assertTrue(expected in repr(ret))
+
+    def test_ipv6_class__is_packed_binary(self):
+        ipv6 = compat.IPv6AddressScoped('2001:db8::')
+        self.assertEqual(str(ipv6), '2001:db8::')
+
+    def test_ipv6_class__is_packed_binary_integer(self):
+        ipv6 = compat.IPv6AddressScoped(42540766411282592856903984951653826560)
+        self.assertEqual(str(ipv6), '2001:db8::')
+
+    def test_ipv6_class__is_packed_binary__issue_51831(self):
+        ipv6 = compat.IPv6AddressScoped(b'sixteen.digit.bn')
+        self.assertEqual(str(ipv6), '7369:7874:6565:6e2e:6469:6769:742e:626e')
