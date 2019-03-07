cc45c086e079d21daeb7f9810d041546aca4b61e
C. R. Oldham <cro@ncbt.org>
2019-03-07 16:34:05 -0700

    Add Test.

---
+++ tests/integration/modules/test_baredoc.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+# Import Python libs
+from __future__ import absolute_import
+
+# Import Salt Testing libs
+from tests.support.case import ModuleCase
+from tests.support.unit import skipIf
+
+# Import Salt Libs
+import salt.utils.path
+import salt.utils.platform
+
+
+class BaredocTest(ModuleCase):
+    '''
+    Validate baredoc module
+    '''
+    def test_baredoc_module_and_args(self):
+        '''
+        baredoc.modules_and_args
+        '''
+        ret = self.run_function('baredoc.modules_and_args', states=True)
+        self.assertIn('state.highstate', ret)
+        self.assertIn('xml.value_present', ret)
+        self.assertIn('xpath', ret['xml.value_present'])
+
+
+    def test_baredoc_modules_with_test(self):
+        '''
+        baredoc.modules_and_args
+        '''
+        ret = self.run_function('baredoc.modules_with_test')
+        self.assertIn('pkg.install', ret)
