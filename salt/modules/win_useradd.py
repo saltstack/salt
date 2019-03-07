a943fb0cd51bd6e82f7342e091724ce545e1c4f0
twangboy <slee@saltstack.com>
2019-03-07 11:01:45 -0700

    Remove rstrip from reg call

--- salt/modules/win_useradd.py
+++ salt/modules/win_useradd.py
@@ -843,7 +843,7 @@ def _get_userprofile_from_registry(user, sid):
         'HKEY_LOCAL_MACHINE',
         'SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\ProfileList\\{0}'.format(sid),
         'ProfileImagePath'
-    )['vdata'].rstrip('\0')
+    )['vdata']
     log.debug(
         'user %s with sid=%s profile is located at "%s"',
         user, sid, profile_dir
