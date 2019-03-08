65d8f27d77a41cba1718b7fa4c4ccb7f56da5028
Ch3LL <megan.wilhite@gmail.com>
2019-03-08 14:27:12 -0500

    Update doc failures in azure modules

--- salt/modules/azurearm_compute.py
+++ salt/modules/azurearm_compute.py
@@ -37,7 +37,7 @@ Azure (ARM) Compute Execution Module
 
     Optional provider parameters:
 
-    **cloud_environment**: Used to point the cloud driver to different API endpoints, such as Azure GovCloud.
+**cloud_environment**: Used to point the cloud driver to different API endpoints, such as Azure GovCloud.
     Possible values:
       * ``AZURE_PUBLIC_CLOUD`` (default)
       * ``AZURE_CHINA_CLOUD``
