8f1a89e6cf554ac7ae6da27542a8a4fce29af644
C. R. Oldham <cro@ncbt.org>
2019-03-11 15:41:56 -0600

    Add test

--- tests/integration/runners/test_sync.py
+++ tests/integration/runners/test_sync.py
@@ -47,14 +47,14 @@ class SyncRunnerTest(ShellCase):
         '''
         '''
         ret_output = self.run_run('saltutil.sync_auth')
-        print(ret_output)
         assert '- auth.nullauth' in [ret_entry.strip() for ret_entry in ret_output]
+        # Clean up?
         os.unlink(os.path.join(self.master_opts['root_dir'], 'extension_modules', 'auth', 'nullauth.py'))
 
     def test_sync_all_includes_auth(self):
         '''
         '''
         ret_output = self.run_run('saltutil.sync_all')
-        print(ret_output)
         assert '- auth.nullauth' in [ret_entry.strip() for ret_entry in ret_output]
+        # Clean up?
         os.unlink(os.path.join(self.master_opts['root_dir'], 'extension_modules', 'auth', 'nullauth.py'))
