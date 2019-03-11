f729043e0cea476b71613eb033f421da9a51b8fa
Wayne Werner <wwerner@saltstack.com>
2019-03-11 16:41:32 -0500

    Port #52156 to master
    Allow pillar relative includes
    
    Fixes #8875 and Fixes #56186

--- salt/pillar/__init__.py
+++ salt/pillar/__init__.py
@@ -814,7 +814,8 @@ class Pillar(object):
             defaults = {}
         err = ""
         errors = []
-        fn_ = self.client.get_state(sls, saltenv).get("dest", False)
+        state_data = self.client.get_state(sls, saltenv)
+        fn_ = state_data.get("dest", False)
         if not fn_:
             if sls in self.ignored_pillars.get(saltenv, []):
                 log.debug(
@@ -907,6 +908,16 @@ class Pillar(object):
                                     self.avail[saltenv],
                                     sub_sls.lstrip(".").replace("/", "."),
                                 )
+                                if sub_sls.startswith("."):
+                                    if state_data.get("source", "").endswith(
+                                        "/init.sls"
+                                    ):
+                                        include_parts = sls.split(".")
+                                    else:
+                                        include_parts = sls.split(".")[:-1]
+                                    sub_sls = ".".join(include_parts + [sub_sls[1:]])
+                                matches = fnmatch.filter(self.avail[saltenv], sub_sls,)
+                                matched_pstates.extend(matches)
                             except KeyError:
                                 errors.extend(
                                     [
