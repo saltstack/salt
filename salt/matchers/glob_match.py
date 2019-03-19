31db9330808226ce5641c797959633ef2165ba8c
Gareth J. Greenaway <gareth@wiked.org>
2019-03-19 09:19:48 -0700

    swapping out if...else approach for __opts__.get approach.

--- salt/matchers/glob_match.py
+++ salt/matchers/glob_match.py
@@ -12,10 +12,7 @@ def match(tgt):
     '''
     Returns true if the passed glob matches the id
     '''
-    if 'minion_id' in __opts__:
-        minion_id = __opts__['minion_id']
-    else:
-        minion_id = __opts__['id']
+    minion_id = __opts__.get('minion_id', __opts__['id'])
     if not isinstance(tgt, six.string_types):
         return False
 
