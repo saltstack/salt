5ce4f89ba7fe32e457cfc264e2bf8f08f73cd1bd
Dmitry Kuzmenko <dmitry.kuzmenko@dsr-corporation.com>
2019-03-20 21:04:49 +0300

    Update doc conf with the new import `tornado.queues`

--- doc/conf.py
+++ doc/conf.py
@@ -132,6 +132,7 @@ MOCK_MODULES = [
     'tornado.ioloop',
     'tornado.iostream',
     'tornado.netutil',
+    'tornado.queues',
     'tornado.simple_httpclient',
     'tornado.stack_context',
     'tornado.web',
