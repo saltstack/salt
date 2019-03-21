c9eaadf8b83c81a3af0e4303744b2eaec9a8addf
Tanoti <john@tanoti.org>
2019-03-21 16:03:43 +0000

    Add support for new config option 'git_pillar_interval' so git_pillar data can be collected less frequently than the main loop_interval timer allows

--- salt/master.py
+++ salt/master.py
@@ -226,7 +226,9 @@ class Maintenance(salt.utils.process.SignalHandlingMultiprocessingProcess):
 
         # Make Start Times
         last = int(time.time())
+        last_git_pillar = last
 
+        git_pillar_interval = self.opts.get('git_pillar_interval', 0)
         old_present = set()
         while True:
             now = int(time.time())
@@ -235,7 +237,9 @@ class Maintenance(salt.utils.process.SignalHandlingMultiprocessingProcess):
                 salt.daemons.masterapi.clean_expired_tokens(self.opts)
                 salt.daemons.masterapi.clean_pub_auth(self.opts)
                 salt.daemons.masterapi.clean_proc_dir(self.opts)
-            self.handle_git_pillar()
+            if (now - last_git_pillar) >= git_pillar_interval:
+                last_git_pillar = now
+                self.handle_git_pillar()
             self.handle_schedule()
             self.handle_key_cache()
             self.handle_presence(old_present)
