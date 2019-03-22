99831c748bf180d269616b7aff408343c8339a56
Nicolas Calderon Asselin <nicolas.calderonasselin@appdirect.com>
2019-03-22 18:32:32 -0400

    Fix salt-cloud traceback with python 3
    With python 3, salt-cloud tracebacks in salt/cloud/clouds/ec2.py's
    get_imageid when searching for an image name not starting with "ami-".
    Python 3 removed the cmp parameter in favor of key.

--- salt/cloud/clouds/ec2.py
+++ salt/cloud/clouds/ec2.py
@@ -1231,10 +1231,9 @@ def _get_imageid_from_image_name(image_name):
               'Filter.0.Name': 'name',
               'Filter.0.Value.0': image_name}
     # Query AWS, sort by 'creationDate' and get the last imageId
-    _t = lambda x: datetime.datetime.strptime(x['creationDate'], '%Y-%m-%dT%H:%M:%S.%fZ')
     image_id = sorted(aws.query(params, location=get_location(),
                                  provider=get_provider(), opts=__opts__, sigver='4'),
-                      lambda i, j: salt.utils.compat.cmp(_t(i), _t(j))
+                      key=lambda x: datetime.datetime.strptime(x['creationDate'], '%Y-%m-%dT%H:%M:%S.%fZ')
                       )[-1]['imageId']
     _get_imageid_from_image_name.images[image_name] = image_id
     return image_id
