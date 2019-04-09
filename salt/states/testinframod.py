65ee13f7a528dfe7933185f48fbf7c7331ed99fc
Herbert Buurman <abcde@ilostthegame.eu>
2019-04-09 16:36:32 +0200

    Removed unused import.

--- salt/states/testinframod.py
+++ salt/states/testinframod.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals, print_function
 
-import re
 import logging
 
 from salt.utils.stringutils import camel_to_snake_case
