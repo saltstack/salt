98fb54aeb3787732442a4a8ba4365399c836425b
Daniel A. Wozniak <dwozniak@saltstack.com>
2019-04-11 02:13:17 +0000

    Fix wart in event bus context

--- salt/engines/test.py
+++ salt/engines/test.py
@@ -28,13 +28,14 @@ def event_bus_context(opts):
             sock_dir=opts['sock_dir'],
             listen=True)
         log.debug('test engine started')
+    return event_bus
 
 
 def start():
     '''
     Listen to events and write them to a log file
     '''
-    with event_bus_context(__opts__) as event:
+    with event_bus_context(__opts__) as event_bus:
         while True:
             event = event_bus.get_event()
             jevent = salt.utils.json.dumps(event)
