7f26e764fce51eafee1971274e6c693bc5de87ee
Dmitry Kuzmenko <dmitry.kuzmenko@dsr-corporation.com>
2019-04-16 15:50:50 +0300

    Revert "Update doc conf with the new import `tornado.queues`"
    This reverts commit 684bf584f68bef5d1965e81494dfbd00f5c46542.

--- doc/conf.py
+++ doc/conf.py
@@ -132,7 +132,6 @@ MOCK_MODULES = [
     'tornado.ioloop',
     'tornado.iostream',
     'tornado.netutil',
-    'tornado.queues',
     'tornado.simple_httpclient',
     'tornado.stack_context',
     'tornado.web',
