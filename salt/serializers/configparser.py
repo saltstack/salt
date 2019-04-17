032dd5d4b45f64d240e3e64522fbeb163dfd3939
lomeroe <github@proxyman.com>
2019-04-17 10:19:34 -0500

    lint fixes

--- salt/serializers/configparser.py
+++ salt/serializers/configparser.py
@@ -20,6 +20,7 @@ __all__ = ['deserialize', 'serialize', 'available']
 
 available = True
 
+
 def _check_preservecase_option(**options):
     '''
     Checks options passed to the serialize/deserialze function
@@ -33,6 +34,7 @@ def _check_preservecase_option(**options):
         options.pop('preserve_case')
     return options, preserve_case
 
+    
 def deserialize(stream_or_string, **options):
     '''
     Deserialize any string or stream like object into a Python data structure.
