78c76da05d410264360d5abaf9e44d0ba1aff8c6
Daniel A. Wozniak <dwozniak@saltstack.com>
2019-04-19 04:25:09 +0000

    Revert part of 927219c since it is not a fix

--- tests/support/mixins.py
+++ tests/support/mixins.py
@@ -651,12 +651,8 @@ def _fetch_events(q):
             queue_item.task_done()
 
     atexit.register(_clean_queue)
-    a_config = AdaptedConfigurationTestCaseMixin()
-    event = salt.utils.event.get_event(
-        'minion',
-        sock_dir=a_config.get_config('minion')['sock_dir'],
-        opts=a_config.get_config('minion'),
-    )
+    opts = RUNTIME_VARS.RUNTIME_CONFIGS['minion']
+    event = salt.utils.event.get_event('minion', sock_dir=opts['sock_dir'], opts=opts)
     while True:
         try:
             events = event.get_event(full=False)
