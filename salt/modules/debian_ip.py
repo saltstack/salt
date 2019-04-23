a5d1d3c4b6668dcbfeece317ec42ac645f84b728
David Murphy <dmurphy@saltstack.com>
2019-04-23 15:32:23 -0600

    Replace use of deprecated ifup|ifdown iface with ip link set iface up|down

--- salt/modules/debian_ip.py
+++ salt/modules/debian_ip.py
@@ -1780,7 +1780,8 @@ def down(iface, iface_type):
     # Slave devices are controlled by the master.
     # Source 'interfaces' aren't brought down.
     if iface_type not in ["slave", "source"]:
-        return __salt__["cmd.run"](["ifdown", iface])
+        cmd = ["ip", "link", "set", iface, "down"]
+        return __salt__["cmd.run"](cmd, python_shell=False)
     return None
 
 
@@ -1841,7 +1842,8 @@ def up(iface, iface_type):  # pylint: disable=C0103
     # Slave devices are controlled by the master.
     # Source 'interfaces' aren't brought up.
     if iface_type not in ("slave", "source"):
-        return __salt__["cmd.run"](["ifup", iface])
+        cmd = ["ip", "link", "set", iface, "up"]
+        return __salt__["cmd.run"](cmd, python_shell=False)
     return None
 
 
