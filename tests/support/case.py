9f6fe06f29ecfd2a1164339b29d99981404cd4e7
Daniel A. Wozniak <dwozniak@saltstack.com>
2019-05-03 04:43:11 +0000

    Fix linter

--- tests/support/case.py
+++ tests/support/case.py
@@ -791,7 +791,7 @@ class ModuleCase(TestCase, SaltClientTestCaseMixin):
     Execute a module function
     '''
 
-    def wait_for_all_jobs(self, minions=['minion', 'sub_minion'], sleep=.3):
+    def wait_for_all_jobs(self, minions=('minion', 'sub_minion',), sleep=.3):
         '''
         Wait for all jobs currently running on the list of minions to finish
         '''
