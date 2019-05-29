efb0d95d77dd100518bd9ed8abc8ce7851562b92
Rémi Jouannet <remi.jouannet@outscale.com>
2019-05-29 16:37:59 +0200

    refacto with salt.utils.roster_match

--- salt/roster/sshknownhosts.py
+++ salt/roster/sshknownhosts.py
@@ -2,43 +2,53 @@
 '''
 Parses roster entries out of Host directives from SSH known_hosts
 
+Example of configuration,
+keep in mind that known_hosts file only contain hostname/IP,
+so you have to use roster_defaults for other parameter for each minion
+
+.. code-block:: ini
+    ssh_known_hosts_file: /Users/user1/.ssh/known_hosts
+    roster_defaults:
+      user: user1
+      sudo: True
+
+Now you can use the module
+
 .. code-block:: bash
 
     salt-ssh --roster sshknownhosts '*' -r "echo hi"
+
+Or with a Saltfile
+
+.. code-block:: ini
+
+    salt-ssh:
+      ssh_known_hosts_file: /Users/user1/.ssh/known_hosts
+
+.. code-block:: bash
+
+    salt-ssh --roster sshknownhosts '*' -r "echo hi"
+
 '''
+
 from __future__ import absolute_import, print_function, unicode_literals
 
 # Import python libs
 import os
-import fnmatch
-import re
-import copy
 import logging
 
 # Import Salt libs
 import salt.utils.files
 import salt.utils.stringutils
-from salt.ext import six
 
 log = logging.getLogger(__name__)
 
 
-def _get_ssh_known_hosts_file(opts):
-    '''
-    :return: Path to the .ssh/known_hosts file - usually <home>/.ssh/known_hosts
-    '''
-    ssh_known_hosts_file = opts.get('ssh_known_hosts_file')
-    if not os.path.isfile(ssh_known_hosts_file):
-        log.error('Cannot find SSH known_hosts file')
-        raise IOError('Cannot find SSH known_hosts file')
-    if not os.access(ssh_known_hosts_file, os.R_OK):
-        log.error('Cannot access SSH known_hosts file: %s', ssh_known_hosts_file)
-        raise IOError('Cannot access SSH known_hosts file: {}'.format(ssh_known_hosts_file))
-    return ssh_known_hosts_file
-
-
 def _parse_ssh_known_hosts_line(line):
     '''
+    Parse one line from a known_hosts line
+
+    :param line: Individual lines from the ssh known_hosts file
     :return: Dict that contain the three fields from a known_hosts line
     '''
     line_unicode = salt.utils.stringutils.to_unicode(line)
@@ -56,11 +66,11 @@ def _parse_ssh_known_hosts_line(line):
     return {'names': names, 'keytype': keytype, 'key': key}
 
 
-def parse_ssh_known_hosts(lines):
+def _parse_ssh_known_hosts(lines):
     '''
     Parses lines from the SSH known_hosts to create roster targets.
 
-    :param lines: Individual lines from the ssh known_hosts file
+    :param lines: lines from the ssh known_hosts file
     :return: Dictionary of targets in similar style to the flat roster
     '''
 
@@ -76,97 +86,19 @@ def parse_ssh_known_hosts(lines):
 
 def targets(tgt, tgt_type='glob'):
     '''
-    Return the targets from the flat yaml file, checks opts for location but
-    defaults to /etc/salt/roster
+    Return the targets from a known_hosts file
     '''
-    ssh_known_hosts_file = _get_ssh_known_hosts_file(__opts__)
+
+    ssh_known_hosts_file = __opts__.get('ssh_known_hosts_file')
+
+    if not os.path.isfile(ssh_known_hosts_file):
+        log.error('Cannot find SSH known_hosts file')
+        raise IOError('Cannot find SSH known_hosts file')
+    if not os.access(ssh_known_hosts_file, os.R_OK):
+        log.error('Cannot access SSH known_hosts file: %s', ssh_known_hosts_file)
+        raise IOError('Cannot access SSH known_hosts file: {}'.format(ssh_known_hosts_file))
+
     with salt.utils.files.fopen(ssh_known_hosts_file, 'r') as hostfile:
-        all_minions = parse_ssh_known_hosts([line.rstrip() for line in hostfile])
-    rmatcher = RosterMatcher(all_minions, tgt, tgt_type)
-    return rmatcher.targets()
+        raw = _parse_ssh_known_hosts([line.rstrip() for line in hostfile])
 
-
-class RosterMatcher(object):
-    '''
-    Matcher for the roster data structure
-    '''
-    def __init__(self, raw, tgt, tgt_type):
-        self.tgt = tgt
-        self.tgt_type = tgt_type
-        self.raw = raw
-
-    def targets(self):
-        '''
-        Execute the correct tgt_type routine and return
-        '''
-        try:
-            return getattr(self, 'ret_{0}_minions'.format(self.tgt_type))()
-        except AttributeError:
-            return {}
-
-    def ret_glob_minions(self):
-        '''
-        Return minions that match via glob
-        '''
-        minions = {}
-        for minion in self.raw:
-            if fnmatch.fnmatch(minion, self.tgt):
-                data = self.get_data(minion)
-                if data:
-                    minions[minion] = data.copy()
-        return minions
-
-    def ret_pcre_minions(self):
-        '''
-        Return minions that match via pcre
-        '''
-        minions = {}
-        for minion in self.raw:
-            if re.match(self.tgt, minion):
-                data = self.get_data(minion)
-                if data:
-                    minions[minion] = data.copy()
-        return minions
-
-    def ret_list_minions(self):
-        '''
-        Return minions that match via list
-        '''
-        minions = {}
-        if not isinstance(self.tgt, list):
-            self.tgt = self.tgt.split(',')
-        for minion in self.raw:
-            if minion in self.tgt:
-                data = self.get_data(minion)
-                if data:
-                    minions[minion] = data.copy()
-        return minions
-
-    def ret_nodegroup_minions(self):
-        '''
-        Return minions which match the special list-only groups defined by
-        ssh_list_nodegroups
-        '''
-        minions = {}
-        nodegroup = __opts__.get('ssh_list_nodegroups', {}).get(self.tgt, [])
-        if not isinstance(nodegroup, list):
-            nodegroup = nodegroup.split(',')
-        for minion in self.raw:
-            if minion in nodegroup:
-                data = self.get_data(minion)
-                if data:
-                    minions[minion] = data.copy()
-        return minions
-
-    def get_data(self, minion):
-        '''
-        Return the configured ip
-        '''
-        ret = copy.deepcopy(__opts__.get('roster_defaults', {}))
-        if isinstance(self.raw[minion], six.string_types):
-            ret.update({'host': self.raw[minion]})
-            return ret
-        elif isinstance(self.raw[minion], dict):
-            ret.update(self.raw[minion])
-            return ret
-        return False
+    return __utils__['roster_matcher.targets'](raw, tgt, tgt_type, 'ipv4')
