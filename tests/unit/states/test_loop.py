9e7f475efc4899f4fc2720e70a45ac0c0aa342cc
Erik Johnson <erik.johnson@level3.com>
2019-05-30 11:24:05 -0500

    Remove unused import

--- tests/unit/states/test_loop.py
+++ tests/unit/states/test_loop.py
@@ -9,12 +9,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 # Import Salt Testing Libs
 from tests.support.mixins import LoaderModuleMockMixin
 from tests.support.unit import TestCase, skipIf
-from tests.support.mock import (
-    patch,
-    MagicMock,
-    NO_MOCK,
-    NO_MOCK_REASON
-)
+from tests.support.mock import MagicMock, NO_MOCK, NO_MOCK_REASON
 
 # Import Salt Libs
 import salt.states.loop as loop
