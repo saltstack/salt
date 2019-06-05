3efab1e81547ebea2783e9f1ae2333f8648c4ea2
Herbert Buurman <abcde@ilostthegame.eu>
2019-06-05 11:42:48 +0200

    tests/unit/modules/test_acme.py: Added test for salt.modules.acme.info using x509.read_certificates. Fixed incorrect data placement in textwrap.dedent. Updated test for acme.info to expect dict when using openssl cli.

--- tests/unit/modules/test_acme.py
+++ tests/unit/modules/test_acme.py
@@ -88,7 +88,7 @@ class AcmeTestCase(TestCase, LoaderModuleMockMixin):
         '''
         Test certificate information retrieval.
         '''
-        certinfo_result = {
+        certinfo_tls_result = {
             "not_after": 1559471377,
             "signature_algorithm": "sha256WithRSAEncryption",
             "extensions": {},
@@ -99,16 +99,46 @@ class AcmeTestCase(TestCase, LoaderModuleMockMixin):
             "not_before": 1559557777,
             "subject": {},
         }
+        certinfo_x509_result = {
+            "Not After": "2019-06-02 10:29:37",
+            "Subject Hash": "54:3B:6C:A4",
+            "Serial Number": "59:AB:CB:A0:FB:90:E8:4B",
+            "SHA1 Finger Print": "F1:8D:F3:26:1B:D3:88:32:CD:B6:FA:3B:85:58:DA:C7:6F:62:BE:7E",
+            "SHA-256 Finger Print": ("FB:A4:5F:71:D6:5D:6C:B6:1D:2C:FD:91:09:2C:1C:52:"
+                                     "3C:EC:B6:4D:1A:95:65:37:04:D0:E2:5E:C7:64:0C:9C"),
+            "MD5 Finger Print": "95:B5:96:9B:42:A5:9E:20:78:FD:99:09:4B:21:1E:97",
+            "Version": 3,
+            "Key Size": 2048,
+            "Public Key": ("-----BEGIN PUBLIC KEY-----\n"
+                           "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsVO2vwQPKU92PSBnuGid\n"
+                           "k8t6KWVE2jEBM10u7CgqQmD/JCnYflEHAo1nOsD7wxdhBrxhf5Qs+pEX1HOsh8VA\n"
+                           "HDTim0iE8nQVJ0Iuen2SrwaWMhwKmZTSJRYMgd46oCMi2RdlCvcgF2Hw6RTwF7FT\n"
+                           "hnksc4HBT91XddnP32N558tOT3YejafQNvClz5WcR+E0JzqGrV/+wfe3o+j/q5eK\n"
+                           "UowttWazeSMvuROtqj/fEk0rop4D14pgzZqWi30tjwhJNl6fSPFWBrLEHGNyDJ+O\n"
+                           "zfov0B2MRLJibH7GMkOCwsP2g1lVOReqcml+ju6zAKW8nHBTRg0iXB18Ifxef57Y\n"
+                           "AQIDAQAB\n"
+                           "-----END PUBLIC KEY-----\n"),
+            "Issuer": {},
+            "Issuer Hash": "54:3B:6C:A4",
+            "Not Before": "2019-06-03 10:29:37",
+            "Subject": {}
+        }
+
         with patch.dict(acme.__salt__, {  # pylint: disable=no-member
-                    'tls.cert_info': MagicMock(return_value=certinfo_result),
-                    'file.file_exists': MagicMock(return_value=True)
+                    'file.file_exists': MagicMock(return_value=True),
+                    'tls.cert_info': MagicMock(return_value=certinfo_tls_result),
                 }):
-            self.assertEqual(acme.info('test'), certinfo_result)
+            self.assertEqual(acme.info('test'), certinfo_tls_result)
         with patch.dict(acme.__salt__, {  # pylint: disable=no-member
+                    'file.file_exists': MagicMock(return_value=True),
+                    'x509.read_certificate': MagicMock(return_value=certinfo_x509_result),
+                }):
+            self.assertEqual(acme.info('test'), certinfo_x509_result)
+        with patch.dict(acme.__salt__, {  # pylint: disable=no-member
+                    'file.file_exists': MagicMock(return_value=True),
                     'cmd.run': MagicMock(return_value='foo'),
-                    'file.file_exists': MagicMock(return_value=True)
                 }):
-            self.assertEqual(acme.info('test'), 'foo')
+            self.assertEqual(acme.info('test'), {'text': 'foo'})
 
     def test_cert(self):
         '''
@@ -119,7 +149,8 @@ class AcmeTestCase(TestCase, LoaderModuleMockMixin):
         expired_timestamp = (datetime.datetime.now() - datetime.timedelta(days=3) -
                              datetime.datetime(1970, 1, 1, 0, 0, 0, 0)).total_seconds()
         cmd_new_cert = {
-            'stdout': textwrap.dedent('''IMPORTANT NOTES:
+            'stdout': textwrap.dedent('''
+                IMPORTANT NOTES:
                  - Congratulations! Your certificate and chain have been saved at:
                    /etc/letsencrypt/live/test/fullchain.pem
                    Your key file has been saved at:
@@ -133,7 +164,8 @@ class AcmeTestCase(TestCase, LoaderModuleMockMixin):
                    Donating to ISRG / Let's Encrypt:   https://letsencrypt.org/donate
                    Donating to EFF:                    https://eff.org/donate-le
                 '''),
-            'stderr': textwrap.dedent('''Saving debug log to /var/log/letsencrypt/letsencrypt.log
+            'stderr': textwrap.dedent('''
+                Saving debug log to /var/log/letsencrypt/letsencrypt.log
                 Plugins selected: Authenticator standalone, Installer None
                 Starting new HTTPS connection (1): acme-v02.api.letsencrypt.org
                 Obtaining a new certificate
