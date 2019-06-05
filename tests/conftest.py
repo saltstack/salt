a1d8166b0c86502c001692349a37e8b51aa8687d
Pedro Algarvio <pedro@algarvio.me>
2019-06-05 18:14:15 +0100

    Reset the root logger to it's default level(because salt changed it)

--- tests/conftest.py
+++ tests/conftest.py
@@ -108,6 +108,9 @@ for handler in logging.root.handlers[:]:
     logging.root.removeHandler(handler)
 
 
+# Reset the root logger to it's default level(because salt changed it)
+logging.root.setLevel(logging.WARNING)
+
 log = logging.getLogger('salt.testsuite')
 
 
