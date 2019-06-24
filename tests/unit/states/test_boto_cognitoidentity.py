5558a0af508619cf42c8fc84629d09508f36ca2c
twangboy <slee@saltstack.com>
2019-06-24 17:36:30 -0600

    Add reg to the whitelist

--- tests/unit/states/test_boto_cognitoidentity.py
+++ tests/unit/states/test_boto_cognitoidentity.py
@@ -132,7 +132,7 @@ class BotoCognitoIdentityStateTestCaseBase(TestCase, LoaderModuleMockMixin):
         ctx = {}
         utils = salt.loader.utils(
             self.opts,
-            whitelist=['boto', 'boto3', 'args', 'systemd', 'path', 'platform'],
+            whitelist=['boto', 'boto3', 'args', 'systemd', 'path', 'platform', 'reg'],
             context=ctx)
         serializers = salt.loader.serializers(self.opts)
         self.funcs = funcs = salt.loader.minion_mods(self.opts, context=ctx, utils=utils, whitelist=['boto_cognitoidentity'])
