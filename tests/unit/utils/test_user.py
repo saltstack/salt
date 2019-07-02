a7f68608c0bb42a68c05a18c9c760da9b434c078
xetix <patrik.westman@phw.se>
2019-07-02 21:21:17 +0200

    pylint check fixes and code cleanup

--- tests/unit/utils/test_user.py
+++ tests/unit/utils/test_user.py
@@ -5,15 +5,10 @@ from __future__ import absolute_import, print_function, unicode_literals
 import os
 import grp
 import pwd
-import sys
-import time
-import signal
-import multiprocessing
-import functools
 
 # Import Salt Testing libs
 from tests.support.unit import TestCase, skipIf
-from tests.support.helpers import (this_user) 
+from tests.support.helpers import this_user
 from tests.support.mock import (
     patch,
     NO_MOCK,
@@ -24,12 +19,11 @@ from tests.support.mock import (
 import salt.utils.platform
 import salt.utils.user
 
+
 class TestUser(TestCase):
     @skipIf(NO_MOCK, NO_MOCK_REASON)
     @skipIf(salt.utils.platform.is_windows(), "Module not available on Windows")
     def test_chugid_and_umask(self):
-
-        #with patch('grp.getgrnam', MagicMock(return_value=getgrnam)):
 
         running_user = this_user()
         running_group = grp.getgrgid(os.getgid()).gr_name
@@ -37,8 +31,8 @@ class TestUser(TestCase):
         gids = {30: 'expectedgroup', 20: running_group}
         getgrnams = {'expectedgroup': grp.struct_group(('expectedgroup', '*', 30, ['expecteduser'])),
                      running_group: grp.struct_group((running_group, '*', 20, [running_user]))}
-        getpwnams = {'expecteduser': pwd.struct_passwd(('expecteduser', 'x', 30, 30, '-', '-', '-')), 
-                running_user: pwd.struct_passwd((running_user, 'x', 20, 20, '-', '-', '-'))}
+        getpwnams = {'expecteduser': pwd.struct_passwd(('expecteduser', 'x', 30, 30, '-', '-', '-')),
+                     running_user: pwd.struct_passwd((running_user, 'x', 20, 20, '-', '-', '-'))}
 
         def getgrnam(group):
             return getgrnams[group]
@@ -51,11 +45,9 @@ class TestUser(TestCase):
 
         with patch('grp.getgrgid', getgrgid):
             with patch('grp.getgrnam', getgrnam):
-                with patch('pwd.getpwnam', getpwnam): 
+                with patch('pwd.getpwnam', getpwnam):
                     with patch('salt.utils.user.chugid') as chugid_mock:
                         salt.utils.user.chugid_and_umask('expecteduser', umask=None, group=running_group)
                         chugid_mock.assert_called_with('expecteduser', running_group)
-                        print('-------------------------------------------------------------')
-                        print('test2 start')
                         salt.utils.user.chugid_and_umask('expecteduser', umask=None, group=None)
                         chugid_mock.assert_called_with('expecteduser', 'expectedgroup')
