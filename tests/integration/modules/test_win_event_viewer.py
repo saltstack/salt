df3d6a61a27c6f4c85bdf08f659799c0aac9c618
cmcmarrow <cmcmarrow@saltstack.com>
2019-07-08 14:42:32 -0600

    adds win_event_viewer

--- tests/integration/modules/test_win_event_viewer.py
+++ tests/integration/modules/test_win_event_viewer.py
@@ -16,18 +16,6 @@ import salt.utils.platform
 @destructiveTest
 @skipIf(not salt.utils.platform.is_windows(), 'Tests for only Windows')
 class WinEventViewerTest(ModuleCase):
-
-    def test_get_events_filter(self):
-        '''
-        test get_events_filter
-        '''
-
-        self.run_function('win_event_viewer.get_events_filter', ('System',
-                                                                 'target_computer=None',
-                                                                 'all_requirements=True',
-                                                                 'eventID=1',
-                                                                 'sid=None'), timeout=180)
-
     def test_get_number_of_events(self):
         '''
         test get_number_of_events
@@ -35,10 +23,3 @@ class WinEventViewerTest(ModuleCase):
 
         ret = self.run_function('win_event_viewer.get_number_of_events', ('System',), timeout=180)
         self.assertEqual(int(ret), abs(int(ret)))
-
-    def test_log_event(self):
-        '''
-        test log_event
-        '''
-
-        self.run_function('win_event_viewer.log_event', ('Salt_Test', '101',), timeout=180)
