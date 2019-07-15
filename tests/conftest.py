df4a2047ce6fe175004495ec20ad78cdbf5b97b7
Pedro Algarvio <pedro@algarvio.me>
2019-07-15 20:51:37 +0100

    Under Py3 we need `optimization_order` set on the opts dictionary

--- tests/conftest.py
+++ tests/conftest.py
@@ -1121,7 +1121,8 @@ class GrainsMarkEvaluator(MarkEvaluator):
             opts = {
                 'root_dir': str(root_dir),
                 'cachedir': str(root_dir / 'cache'),
-                'extension_modules': str(root_dir / 'extmods')
+                'extension_modules': str(root_dir / 'extmods'),
+                'optimization_order': [0, 1, 2],
             }
             GrainsMarkEvaluator._grains = salt.loader.grains(opts)
         item_globals['grains'] = GrainsMarkEvaluator._grains.copy()
@@ -1141,7 +1142,8 @@ def grains(request):
     opts = {
         'root_dir': str(root_dir),
         'cachedir': str(root_dir / 'cache'),
-        'extension_modules': str(root_dir / 'extmods')
+        'extension_modules': str(root_dir / 'extmods'),
+        'optimization_order': [0, 1, 2],
     }
     return salt.loader.grains(opts)
 # <---- Custom Fixtures ----------------------------------------------------------------------------------------------
