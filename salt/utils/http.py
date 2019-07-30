f981946066101f270685a75c5e2a919e70526295
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2019-07-30 08:52:42 +0100

    Do not log an error message when receiving an HTTP error code

--- salt/utils/http.py
+++ salt/utils/http.py
@@ -640,7 +640,6 @@ def query(
         except salt.ext.tornado.httpclient.HTTPError as exc:
             ret["status"] = exc.code
             ret["error"] = six.text_type(exc)
-            log.error("Cannot perform 'http.query': {0} - {1}".format(url_full, ret["error"]))
             return ret
         except (socket.herror, socket.error, socket.timeout, socket.gaierror) as exc:
             if status is True:
