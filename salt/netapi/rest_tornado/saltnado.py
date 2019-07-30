d4c0a97fb22f21fc2a0f77426d593acd77149d71
Matt Phillips <mphillips81@bloomberg.net>
2019-07-30 14:05:33 -0400

    tornado: job_not_running account for finished requests
    when a client cxn prematurely bails on a request in flight on_finish
    removes the salt client instances - causing an annoying but harmless
    stack. This code reshuffle should fix that, without any change in
    behavior.

--- salt/netapi/rest_tornado/saltnado.py
+++ salt/netapi/rest_tornado/saltnado.py
@@ -1081,40 +1081,38 @@ class SaltAPIHandler(BaseSaltAPIHandler):  # pylint: disable=W0223
         Return a future which will complete once jid (passed in) is no longer
         running on tgt
         '''
-        ping_pub_data = yield self.saltclients['local'](tgt,
-                                                        'saltutil.find_job',
-                                                        [jid],
-                                                        tgt_type=tgt_type)
-        ping_tag = tagify([ping_pub_data['jid'], 'ret'], 'job')
-
-        minion_running = False
         while True:
             try:
+                # we can't do anything if the client cxn is already terminated
+                if self._finished:
+                    return
+
+                ping_pub_data = yield self.saltclients['local'](tgt,
+                                                                'saltutil.find_job',
+                                                                [jid],
+                                                                tgt_type=tgt_type)
+                ping_tag = tagify([ping_pub_data['jid'], 'ret'], 'job')
+
+                minion_running = False
+
                 event = self.application.event_listener.get_event(self,
                                                                   tag=ping_tag,
                                                                   timeout=self.application.opts['gather_job_timeout'])
                 event = yield event
+
+                # Minions can return, we want to see if the job is running...
+                if event['data'].get('return', {}) == {}:
+                    continue
+                if event['data']['id'] not in minions:
+                    minions[event['data']['id']] = False
+                minion_running = True
             except TimeoutException:
                 if not event.done():
                     event.set_result(None)
 
                 if not minion_running or is_finished.done():
                     raise tornado.gen.Return(True)
-                else:
-                    ping_pub_data = yield self.saltclients['local'](tgt,
-                                                                    'saltutil.find_job',
-                                                                    [jid],
-                                                                    tgt_type=tgt_type)
-                    ping_tag = tagify([ping_pub_data['jid'], 'ret'], 'job')
-                    minion_running = False
-                    continue
 
-            # Minions can return, we want to see if the job is running...
-            if event['data'].get('return', {}) == {}:
-                continue
-            if event['data']['id'] not in minions:
-                minions[event['data']['id']] = False
-            minion_running = True
 
     @tornado.gen.coroutine
     def _disbatch_local_async(self, chunk):
