5febb753e3481b0e7f8be70e19394c05c879ec50
Herbert Buurman <abcde@ilostthegame.eu>
2019-08-05 16:23:16 +0200

    Merged named kwargs into **kwargs as it interferes with positional arguments in *args.

--- salt/modules/mine.py
+++ salt/modules/mine.py
@@ -19,6 +19,7 @@ import salt.transport.client
 import salt.utils.mine
 import salt.utils.minions
 import salt.utils.dictupdate
+import salt.utils.functools
 from salt.exceptions import SaltClientError
 
 # Import 3rd-party libs
@@ -189,7 +190,7 @@ def update(clear=False, mine_functions=None):
         try:
             res = salt.utils.functools.call_function(
                 __salt__[function_name or function_alias],
-                func_args=function_args,
+                *function_args,
                 **function_kwargs
             )
         except Exception:
@@ -204,20 +205,22 @@ def update(clear=False, mine_functions=None):
     return _mine_store(mine_data, clear)
 
 
-def send(name, mine_function=None, allow_tgt=None, allow_tgt_type=None, *args, **kwargs):  # pylint: disable=W1113
+def send(name, *args, **kwargs):
     '''
     Send a specific function and its result to the salt mine.
     This gets stored in either the local cache, or the salt master's cache.
 
     :param str name: Name of the function to add to the mine.
+
+    The following pameters are extracted from kwargs if present:
     :param str mine_function: The name of the execution_module.function to run
         and whose value will be stored in the salt mine. Defaults to ``name``.
     :param str allow_tgt: Targeting specification for ACL. Specifies which minions
         are allowed to access this function.
     :param str allow_tgt_type: Type of the targeting specification. This value will
         be ignored if ``allow_tgt`` is not specified.
-    :param list args: args to pass to the function.
-    :param dict kwargs: kwargs to pass to the function.
+
+    Remaining args and kwargs will be passed on to the function to run.
 
     :rtype: bool
     :return: Whether executing the function and storing the information was succesful.
@@ -238,11 +241,14 @@ def send(name, mine_function=None, allow_tgt=None, allow_tgt_type=None, *args, *
         salt '*' mine.send eth0_ip_addrs mine_function=network.ip_addrs eth0 allow_tgt='G@grain:value' allow_tgt_type=compound
     '''
     kwargs = salt.utils.args.clean_kwargs(**kwargs)
+    mine_function = kwargs.pop('mine_function', None)
+    allow_tgt = kwargs.pop('allow_tgt', None)
+    allow_tgt_type = kwargs.pop('allow_tgt_type', None)
     mine_data = {}
     try:
         res = salt.utils.functools.call_function(
             __salt__[mine_function or name],
-            func_args=args,
+            *args,
             **kwargs
         )
     except Exception as exc:
