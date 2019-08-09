6bfba962f51fa1af2c5a74a16c272f57e52e8376
Tyler Johnson <tjohnson@saltstack.com>
2019-08-08 18:19:36 -0600

    removed print statements, optimized query

--- tests/integration/cloud/helpers/cloud_test_base.py
+++ tests/integration/cloud/helpers/cloud_test_base.py
@@ -27,8 +27,12 @@ log = logging.getLogger(__name__)
 
 
 class CloudTest(ShellCase):
-    PROVIDER = None
-    REQUIRED_CONFIG_ITEMS = None
+    @property
+    def instance_name(self):
+        if not hasattr(self, '_instance_name'):
+            # Create the cloud instance name to be used throughout the tests
+            self._instance_name = generate_random_name('cloud-test-').lower()
+        return self._instance_name
 
     def _instance_exists(self, instance_name=None):
         # salt-cloud -a show_instance myinstance
@@ -36,16 +40,7 @@ class CloudTest(ShellCase):
             instance_name = self.instance_name
         query = self.run_cloud('--query')
         log.debug('Checking for "{}" in => {}'.format(self.instance_name, query))
-        return any(self.instance_name == q.strip(': ') for q in query)
-
-    def assertInstanceExists(self, creation_ret=None):
-        '''
-        :param creation_ret: The return value from the run_cloud() function that created the instance
-        '''
-        if creation_ret:
-            self.assertIn(self.instance_name, [i.strip(': ') for i in creation_ret])
-            self.assertNotIn('Failed to start', six.text_type(creation_ret))
-        self.assertTrue(self._instance_exists(), 'Instance "{}" was not created successfully')
+        return any(self.instance_name is q.strip(': ') for q in query)
 
     def _destroy_instance(self):
         log.debug('Deleting instance "{}"'.format(self.instance_name))
@@ -69,68 +64,8 @@ class CloudTest(ShellCase):
             log.warning('Instance "{}" may not have been deleted properly'.format(self.instance_name))
 
         # By now it should all be over
-        self.assertFalse(self._instance_exists(), 'Could not destroy "{}"'.format(self.instance_name))
+        self.assertEqual(self._instance_exists(), False, 'Could not destroy "{}"'.format(self.instance_name))
         log.debug('Instance "{}" no longer exists'.format(self.instance_name))
-
-    @property
-    def instance_name(self):
-        if not hasattr(self, '_instance_name'):
-            # Create the cloud instance name to be used throughout the tests
-            self._instance_name = generate_random_name('cloud-test-').lower()
-        return self._instance_name
-
-    @property
-    def providers(self):
-        if not hasattr(self, '_providers'):
-            self._providers = self.run_cloud('--list-providers')
-        return self._providers
-
-    @property
-    def provider_config(self):
-        if not hasattr(self, '_provider_config'):
-            self._provider_config = cloud_providers_config(
-                path.join(
-                    FILES,
-                    'conf',
-                    'cloud.providers.d',
-                    self.PROVIDER + '.conf'
-                )
-            )
-        return self._provider_config
-
-    @property
-    def profile_str(self):
-        return self.PROVIDER + '-config'
-
-    @expensiveTest
-    def setUp(self):
-        '''
-        Sets up the test requirements.  In child classes, define PROVIDER and REQUIRED_CONFIG_ITEMS or this will fail
-        '''
-        super(CloudTest, self).setUp()
-
-        if not self.PROVIDER:
-            self.fail('A PROVIDER_NAME must be defined for this test')
-
-        # check if appropriate cloud provider and profile files are present
-        if self.profile_str + ':' not in self.providers:
-            self.skipTest(
-                'Configuration file for {0} was not found. Check {0}.conf files '
-                'in tests/integration/files/conf/cloud.*.d/ to run these tests.'
-                 .format(self.profile_str)
-            )
-
-        missing_conf_item = []
-        for att in self.REQUIRED_CONFIG_ITEMS:
-            if not self.provider_config[self.profile_str][self.PROVIDER][att]:
-                missing_conf_item.append(att)
-
-        self.assertFalse(missing_conf_item, 'Conf items are missing that must be provided to run these tests:  {}'
-                         .format(', '.join(missing_conf_item)) +
-                         'Check tests/integration/files/conf/cloud.providers.d/{0}.conf'.format(self.PROVIDER))
-
-        self.assertFalse(self._instance_exists(),
-                         'The instance "{}" exists before it was created by the test'.format(self.instance_name))
 
     def tearDown(self):
         '''
