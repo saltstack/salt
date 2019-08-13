46878ca1858d35ed4f2fbca6fc13804bc285c024
Tyler Johnson <tjohnson@saltstack.com>
2019-08-13 14:26:55 -0600

    Switched to Azurearm over msazure

--- tests/integration/cloud/clouds/test_msazure.py
+++ tests/integration/cloud/clouds/test_msazure.py
@@ -8,10 +8,8 @@ from __future__ import absolute_import, print_function, unicode_literals
 import os
 
 # Import Salt Testing Libs
-from tests.support.case import ShellCase
-from tests.support.runtests import RUNTIME_VARS
-from tests.support.helpers import expensiveTest, generate_random_name
-from tests.support.unit import skipIf, WAR_ROOM_SKIP
+from tests.integration.cloud.helpers.cloud_test_base import TIMEOUT as CLOUD_TIMEOUT, CloudTest
+from tests.support.unit import skipIf
 
 # Import Salt Libs
 from salt.config import cloud_providers_config
@@ -28,10 +26,9 @@ except ImportError:
 if HAS_AZURE and not hasattr(azure, '__version__'):
     import azure.common
 
-# Create the cloud instance name to be used throughout the tests
-INSTANCE_NAME = generate_random_name('CLOUD-TEST-')
-PROVIDER_NAME = 'azure'
-PROFILE_NAME = 'azure-test'
+log = logging.getLogger(__name__)
+
+TIMEOUT = CLOUD_TIMEOUT * 2
 REQUIRED_AZURE = '0.11.1'
 
 
@@ -50,65 +47,14 @@ def __has_required_azure():
     return False
 
 
-@skipIf(WAR_ROOM_SKIP, 'WAR ROOM TEMPORARY SKIP')
-@skipIf(HAS_AZURE is False, 'These tests require the Azure Python SDK to be installed.')
-@skipIf(__has_required_azure() is False, 'The Azure Python SDK must be >= 0.11.1.')
-@expensiveTest
-class AzureTest(ShellCase):
+@skipIf(not HAS_AZURE, 'These tests require the Azure Python SDK to be installed.')
+@skipIf(not __has_required_azure(), 'The Azure Python SDK must be >= 0.11.1.')
+class AzureTest(CloudTest):
     '''
     Integration tests for the Azure cloud provider in Salt-Cloud
     '''
-
-    def setUp(self):
-        '''
-        Sets up the test requirements
-        '''
-        super(AzureTest, self).setUp()
-
-        # check if appropriate cloud provider and profile files are present
-        provider_str = 'azure-config'
-        providers = self.run_cloud('--list-providers')
-        if provider_str + ':' not in providers:
-            self.skipTest(
-                'Configuration file for {0} was not found. Check {0}.conf files '
-                'in tests/integration/files/conf/cloud.*.d/ to run these tests.'
-                .format(PROVIDER_NAME)
-            )
-
-        # check if subscription_id and certificate_path are present in provider file
-        provider_config = cloud_providers_config(
-            os.path.join(
-                RUNTIME_VARS.FILES,
-                'conf',
-                'cloud.providers.d',
-                PROVIDER_NAME + '.conf'
-            )
-        )
-        sub_id = provider_config[provider_str][PROVIDER_NAME]['subscription_id']
-        cert_path = provider_config[provider_str][PROVIDER_NAME]['certificate_path']
-        if sub_id == '' or cert_path == '':
-            self.skipTest(
-                'A subscription_id and certificate_path must be provided to run '
-                'these tests. Check '
-                'tests/integration/files/conf/cloud.providers.d/{0}.conf'.format(
-                    PROVIDER_NAME
-                )
-            )
-
-        # check if ssh_username, ssh_password, and media_link are present
-        # in the azure configuration file
-        ssh_user = provider_config[provider_str][PROVIDER_NAME]['ssh_username']
-        ssh_pass = provider_config[provider_str][PROVIDER_NAME]['ssh_password']
-        media_link = provider_config[provider_str][PROVIDER_NAME]['media_link']
-
-        if ssh_user == '' or ssh_pass == '' or media_link == '':
-            self.skipTest(
-                'An ssh_username, ssh_password, and media_link must be provided to run '
-                'these tests. One or more of these elements is missing. Check '
-                'tests/integration/files/conf/cloud.profiles.d/{0}.conf'.format(
-                    PROVIDER_NAME
-                )
-            )
+    PROVIDER = 'azurearm'
+    REQUIRED_PROVIDER_CONFIG_ITEMS = ('subscription_id',)
 
     def test_instance(self):
         '''
