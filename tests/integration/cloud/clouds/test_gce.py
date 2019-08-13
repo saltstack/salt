b9ee4112d560d769e0272e0f7940985bbc481ea3
Tyler Johnson <tjohnson@saltstack.com>
2019-08-12 22:17:21 -0600

    Fixed broken tests

--- tests/integration/cloud/clouds/test_gce.py
+++ tests/integration/cloud/clouds/test_gce.py
@@ -6,6 +6,7 @@
 
 # Import Python Libs
 from __future__ import absolute_import, print_function, unicode_literals
+from time import sleep
 
 # Import Salt Testing Libs
 from tests.integration.cloud.helpers.cloud_test_base import TIMEOUT, CloudTest
@@ -39,5 +40,7 @@ class GCETest(CloudTest):
         ret_str = self.run_cloud('-p gce-test-extra {0}'.format(self.instance_name), timeout=TIMEOUT)
         # check if instance returned with salt installed
         self.assertInstanceExists(ret_str)
+        # Let the instance exist for half a minute before destroying it
+        sleep(30)
 
         self._destroy_instance()
