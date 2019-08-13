1a3e6e876b328a876302e4cbe02dbeef685d1209
Tyler Johnson <tjohnson@saltstack.com>
2019-08-13 16:13:31 -0600

    fixed pylint error on format string

--- tests/integration/cloud/helpers/cloud_test_base.py
+++ tests/integration/cloud/helpers/cloud_test_base.py
@@ -79,7 +79,7 @@ class CloudTest(ShellCase):
             # Create the cloud instance name to be used throughout the tests
             subclass = self.__class__.__bases__[0].__name__.strip('Test')
             # Use the first three letters of the subclass, fill with '-' if too short
-            self.__instance_name = generate_random_name('cloud-test-{.-<3.3}-'.format(subclass)).lower()
+            self.__instance_name = generate_random_name('cloud-test-{:-<3}-'.format(subclass[:3])).lower()
         return self.__instance_name
 
     @property
