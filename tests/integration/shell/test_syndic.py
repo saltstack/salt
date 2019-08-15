757382638d0fe929de36d756997173660303b435
Daniel A. Wozniak <dwozniak@saltstack.com>
2019-08-15 23:29:13 +0000

    Remove crufty un-needed tests

--- tests/integration/shell/test_syndic.py
+++ tests/integration/shell/test_syndic.py
@@ -36,54 +36,6 @@ class SyndicTest(ShellCase, testprogram.TestProgramCase, ShellCaseCommonTestsMix
     Test the salt-syndic command
     '''
 
-    _call_binary_ = 'salt-syndic'
-
-    def test_issue_7754(self):
-        old_cwd = os.getcwd()
-        config_dir = os.path.join(TMP, 'issue-7754')
-        if not os.path.isdir(config_dir):
-            os.makedirs(config_dir)
-
-        os.chdir(config_dir)
-
-        for fname in ('master', 'minion'):
-            pid_path = os.path.join(config_dir, '{0}.pid'.format(fname))
-            with salt.utils.files.fopen(self.get_config_file_path(fname), 'r') as fhr:
-                config = salt.utils.yaml.safe_load(fhr)
-                config['log_file'] = config['syndic_log_file'] = 'file:///tmp/log/LOG_LOCAL3'
-                config['root_dir'] = config_dir
-                if 'ret_port' in config:
-                    config['ret_port'] = int(config['ret_port']) + 10
-                    config['publish_port'] = int(config['publish_port']) + 10
-
-                with salt.utils.files.fopen(os.path.join(config_dir, fname), 'w') as fhw:
-                    salt.utils.yaml.safe_dump(config, fhw, default_flow_style=False)
-
-        ret = self.run_script(
-            self._call_binary_,
-            '--config-dir={0} --pid-file={1} -l debug'.format(
-                config_dir,
-                pid_path
-            ),
-            timeout=5,
-            catch_stderr=True,
-            with_retcode=True
-        )
-
-        # Now kill it if still running
-        if os.path.exists(pid_path):
-            with salt.utils.files.fopen(pid_path) as fhr:
-                try:
-                    os.kill(int(fhr.read()), SIGKILL)
-                except OSError:
-                    pass
-        try:
-            self.assertFalse(os.path.isdir(os.path.join(config_dir, 'file:')))
-        finally:
-            self.chdir(old_cwd)
-            if os.path.isdir(config_dir):
-                shutil.rmtree(config_dir)
-
     @skipIf(salt.utils.platform.is_windows(), 'Skip on Windows OS')
     def test_exit_status_unknown_user(self):
         '''
