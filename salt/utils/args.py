a385f9f64f5b4e11d111d798c27a95d4d0b1c1c9
Herbert Buurman <abcde@ilostthegame.eu>
2019-08-16 12:28:02 +0200

    Add alternative fix for "!" stomping Apparently (after watching Jenkins tests fail), what yaml.safe_load returns depends not on the version of salt, but on some other external dependency. Because of this, fix both possible return values.

--- salt/utils/args.py
+++ salt/utils/args.py
@@ -204,12 +204,15 @@ def yamlify_arg(arg):
 
         elif arg is None \
                 or isinstance(arg, (list, float, six.integer_types, six.string_types)):
-            # yaml.safe_load will load '|' as '', don't let it do that.
+            # yaml.safe_load will load '|' and '!' as '', don't let it do that.
             if arg == '' and original_arg in ('|', '!'):
                 return original_arg
             # yaml.safe_load will treat '#' as a comment, so a value of '#'
             # will become None. Keep this value from being stomped as well.
             elif arg is None and original_arg.strip().startswith('#'):
+                return original_arg
+            # Other times, yaml.safe_load will load '!' as None. Prevent that.
+            elif arg is None and original_arg == '!':
                 return original_arg
             else:
                 return arg
