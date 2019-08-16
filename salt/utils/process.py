63c616a4a7053d7a2331d5a9724067fcaf03e20a
Daniel A. Wozniak <dwozniak@saltstack.com>
2019-08-16 17:10:03 +0000

    Do not dup2 things without fileno

--- salt/utils/process.py
+++ salt/utils/process.py
@@ -6,6 +6,7 @@ Functions for daemonizing and otherwise modifying running processes
 # Import python libs
 from __future__ import absolute_import, with_statement, print_function, unicode_literals
 import copy
+import io
 import os
 import sys
 import time
@@ -102,12 +103,32 @@ def daemonize(redirect_out=True):
         with salt.utils.files.fopen('/dev/null', 'r+') as dev_null:
             # Redirect python stdin/out/err
             # and the os stdin/out/err which can be different
-            os.dup2(dev_null.fileno(), sys.stdin.fileno())
-            os.dup2(dev_null.fileno(), sys.stdout.fileno())
-            os.dup2(dev_null.fileno(), sys.stderr.fileno())
-            os.dup2(dev_null.fileno(), 0)
-            os.dup2(dev_null.fileno(), 1)
-            os.dup2(dev_null.fileno(), 2)
+            dup2(dev_null, sys.stdin)
+            dup2(dev_null, sys.stdout)
+            dup2(dev_null, sys.stderr)
+            dup2(dev_null, 0)
+            dup2(dev_null, 1)
+            dup2(dev_null, 2)
+
+
+def dup2(file1, file2):
+    if isinstance(file1, int):
+        fno1 = file1
+    else:
+        try:
+            fno1 = file1.fileno()
+        except io.UnsupportedOperation:
+            log.warn('Unsupported operation on file: %r', file1)
+            return
+    if isinstance(file2, int):
+        fno2 = file2
+    else:
+        try:
+            fno2 = file2.fileno()
+        except io.UnsupportedOperation:
+            log.warn('Unsupported operation on file: %r', file2)
+            return
+    os.dup2(fno1, fno2)
 
 
 def daemonize_if(opts):
