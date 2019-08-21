e022d2f4aa1065259237c1f6e0dd87f686bf6498
Tyler Johnson <tjohnson@saltstack.com>
2019-08-20 21:53:22 -0600

    test for ec2 instance shutting down

--- tests/integration/cloud/helpers/cloud_test_base.py
+++ tests/integration/cloud/helpers/cloud_test_base.py
@@ -111,14 +111,19 @@ class CloudTest(ShellCase):
             delete_status = delete[self.profile_str][self.PROVIDER][self.instance_name]
             if isinstance(delete_status, str):
                 self.assertEqual(delete_status, 'True')
+                return
             elif isinstance(delete_status, dict):
-                if delete_status.get('currentState'):
-                    self.assertEqual(delete_status.get('currentState').get('name'), 'shutting-down')
-                self.assertIn('.delete', delete_status.get('ACTION', ''))
-        else:
-            # It's not clear from the delete string that deletion was successful, ask salt-cloud after a delay
-            sleep(shutdown_delay)
-            self.assertIn(self.instance_name, self.query_instances())
+                current_state = delete_status.get('currentState')
+                if current_state:
+                    if current_state.get('ACTION'):
+                        self.assertIn('.delete', current_state.get('ACTION'))
+                        return
+                    else:
+                        self.assertEqual(current_state.get('name'), 'shutting-down')
+                        return
+        # It's not clear from the delete string that deletion was successful, ask salt-cloud after a delay
+        sleep(shutdown_delay)
+        self.assertIn(self.instance_name, self.query_instances())
 
     @property
     def instance_name(self):
