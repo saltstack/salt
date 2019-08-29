62c4afa6c4aee3eb314625fe2d98499ae21113d0
Aleksandr Rykalin <alexander.rykalin@venafi.com>
2019-08-29 16:01:06 +0300

    Adding test

--- tests/integration/externalapi/test_venafiapi.py
+++ tests/integration/externalapi/test_venafiapi.py
@@ -10,9 +10,12 @@ import string
 
 # Import Salt Testing libs
 from tests.support.case import ShellCase
-from tests.support.helpers import destructiveTest, expensiveTest
 from salt.ext.six.moves import range
-
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives import serialization
+import pytest
 
 def _random_name(prefix=''):
     ret = prefix
@@ -25,10 +28,12 @@ def with_random_name(func):
     '''
     generate a randomized name for a container
     '''
+
     @functools.wraps(func)
     def wrapper(self, *args, **kwargs):
         name = _random_name(prefix='salt_')
         return func(self, _random_name(prefix='salt-test-'), *args, **kwargs)
+
     return wrapper
 
 
@@ -44,10 +49,38 @@ class VenafiTest(ShellCase):
         '''
         venafi.request
         '''
-        print("Using venafi config:",self.master_opts['venafi'])
+        print("Using venafi config:", self.master_opts['venafi'])
+        cn = '{0}.example.com'.format(name)
         ret = self.run_run_plus(fun='venafi.request',
-                                minion_id='{0}.example.com'.format(name),
-                                dns_name='{0}.example.com'.format(name),
+                                minion_id=cn,
+                                dns_name=cn,
                                 zone='Default')
-        print("ret is:\n",ret)
-        # self.assertTrue('request_id' in ret['return'])
+        cert_output = ret['return'][0]
+        if not cert_output:
+            pytest.fail('venafi_certificate not found in output_value')
+
+        print("Testing certificate:\n", cert_output)
+        cert = x509.load_pem_x509_certificate(cert_output.encode(), default_backend())
+        assert isinstance(cert, x509.Certificate)
+        assert cert.subject.get_attributes_for_oid(NameOID.COMMON_NAME) == [
+            x509.NameAttribute(
+                NameOID.COMMON_NAME, cn
+            )
+        ]
+
+        pkey_output = ret['return'][1]
+        print("Testing pkey:\n", pkey_output)
+        if not pkey_output:
+            pytest.fail('venafi_private key not found in output_value')
+
+        pkey = serialization.load_pem_private_key(pkey_output.encode(), password=None, backend=default_backend())
+
+        pkey_public_key_pem = pkey.public_key().public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+        cert_public_key_pem = cert.public_key().public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+        assert pkey_public_key_pem == cert_public_key_pem
