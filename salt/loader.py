42b31f1ce040e3520f5e56be720c920163648528
Gareth J. Greenaway <gareth@wiked.org>
2019-09-18 17:12:33 -0700

    Porting PR #50736 to 2019.2.1

--- salt/loader.py
+++ salt/loader.py
@@ -1883,20 +1883,6 @@ class LazyLoader(salt.utils.lazy.LazyDict):
                     # with the new name
                     log.trace('Loaded %s as virtual %s', module_name, virtual)
 
-                    if not hasattr(mod, '__virtualname__'):
-                        salt.utils.versions.warn_until(
-                            'Hydrogen',
-                            'The \'{0}\' module is renaming itself in its '
-                            '__virtual__() function ({1} => {2}). Please '
-                            'set it\'s virtual name as the '
-                            '\'__virtualname__\' module attribute. '
-                            'Example: "__virtualname__ = \'{2}\'"'.format(
-                                mod.__name__,
-                                module_name,
-                                virtual
-                            )
-                        )
-
                     if virtualname != virtual:
                         # The __virtualname__ attribute does not match what's
                         # being returned by the __virtual__() function. This
