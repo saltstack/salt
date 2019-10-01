726019ee56b27d65e8f892e53bb53546c04f09dd
Daniel A. Wozniak <dwozniak@saltstack.com>
2019-10-01 21:52:10 +0000

    Fix minor memory leak in loader

--- salt/loader.py
+++ salt/loader.py
@@ -105,6 +105,51 @@ LIBCLOUD_FUNCS_NOT_SUPPORTED = (
 # Will be set to pyximport module at runtime if cython is enabled in config.
 pyximport = None
 
+import weakref
+import collections
+class Pack(collections.MutableMapping):
+
+    def __init__(self, obj,  mapping):
+        self.obj_ref = weakref.ref(obj)
+        self.mymap = dict()
+        for k, v in mapping.items():
+            self.__setitem__(k, v)
+
+    def __getitem__(self, name):
+        val = self.mymap[name]
+        if isinstance(val, weakref.ref):
+            return val()
+        return val
+
+    def __setitem__(self, name, value):
+        if value is self.obj_ref():
+            self.mymap[name] = weakref.ref(value)
+        elif isinstance(value, LazyLoader):
+            self.mymap[name] = weakref.ref(value)
+        else:
+            self.mymap[name] = value
+
+    def keys(self):
+        return self.mymap.keys()
+
+    def __iter__(self):
+        return iter(self.keys())
+
+    def __delitem__(self, name):
+        self.mymap.__delitem__(name)
+
+    def __len__(self):
+        return self.mymap.__len__()
+
+    def __eq__(self, other):
+        return dict(self.items()) == dict(other.items())
+
+    def items(self):
+        l = []
+        for k in self.mymap:
+            l.append((k, self[k]))
+        return l
+
 
 def static_loader(
         opts,
@@ -1137,7 +1182,7 @@ class LazyLoader(salt.utils.lazy.LazyDict):
         '''
 
         self.inject_globals = {}
-        self.pack = {} if pack is None else pack
+        self.pack = Pack(self, {} if pack is None else pack)
         if opts is None:
             opts = {}
         threadsafety = not opts.get('multiprocessing')
