ddc969c957c34b4ac1e183f06a951eac8e447751
vin01 <vinc.i@protonmail.ch>
2019-10-05 11:09:45 +0200

    Copy symlinks as symlinks in dest tree for tests

--- tests/unit/fileserver/test_gitfs.py
+++ tests/unit/fileserver/test_gitfs.py
@@ -401,7 +401,7 @@ class GitFSTestBase(object):
                 log.error("Access error removeing file %s", TMP_REPO_DIR)
             elif exc.errno != errno.ENOENT:
                 raise
-        shutil.copytree(INTEGRATION_BASE_FILES, TMP_REPO_DIR + '/')
+        shutil.copytree(INTEGRATION_BASE_FILES, TMP_REPO_DIR + '/', symlinks=True)
 
         repo = git.Repo.init(TMP_REPO_DIR)
 
