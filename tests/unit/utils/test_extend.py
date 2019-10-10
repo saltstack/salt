56169c11fb0e879731376c813046b6d69de83b8d
Pedro Algarvio <pedro@algarvio.me>
2019-10-10 13:23:14 +0100

    Use `RUNTIME_VARS` instead

--- tests/unit/utils/test_extend.py
+++ tests/unit/utils/test_extend.py
@@ -16,9 +16,9 @@ from datetime import date
 # Import Salt Testing libs
 from tests.support.unit import TestCase, skipIf
 from tests.support.mock import MagicMock, patch
+from tests.support.runtests import RUNTIME_VARS
 
 # Import salt libs
-import tests.integration as integration
 import salt.utils.extend
 import salt.utils.files
 
@@ -26,7 +26,7 @@ import salt.utils.files
 class ExtendTestCase(TestCase):
     def setUp(self):
         self.starting_dir = os.getcwd()
-        os.chdir(integration.CODE_DIR)
+        os.chdir(RUNTIME_VARS.CODE_DIR)
         self.out = None
 
     def tearDown(self):
@@ -35,11 +35,11 @@ class ExtendTestCase(TestCase):
                 shutil.rmtree(self.out, True)
         os.chdir(self.starting_dir)
 
-    @skipIf(not os.path.exists(os.path.join(integration.CODE_DIR, 'templates')),
+    @skipIf(not os.path.exists(os.path.join(RUNTIME_VARS.CODE_DIR, 'templates')),
             "Test template directory 'templates/' missing.")
     def test_run(self):
         with patch('sys.exit', MagicMock):
-            out = salt.utils.extend.run('test', 'test', 'this description', integration.CODE_DIR, False)
+            out = salt.utils.extend.run('test', 'test', 'this description', RUNTIME_VARS.CODE_DIR, False)
             self.out = out
             year = date.today().strftime('%Y')
             self.assertTrue(os.path.exists(out))
