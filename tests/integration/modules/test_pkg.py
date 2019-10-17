282310eb99166d71766d22c7475b0b4c0d54bfce
Tyler Johnson <tjohnson@saltstack.com>
2019-10-17 08:54:03 -0600

    removed useless import var

--- tests/integration/modules/test_pkg.py
+++ tests/integration/modules/test_pkg.py
@@ -6,9 +6,8 @@ import os
 import pprint
 try:
     import pwd
-    HAS_PWD = True
 except ImportError:
-    HAS_PWD = False
+    pass
 
 # Import Salt Testing libs
 from tests.support.case import ModuleCase
@@ -334,7 +333,7 @@ class PkgModuleTest(ModuleCase, SaltReturnAssertsMixin):
                 self.assertNotEqual(ret, {})
 
     @destructiveTest
-    @skipIf(salt.utils.platform.is_darwin() and not HAS_PWD, 'The jenkins user is equivalent to root on mac, causing the test to be unrunnable')
+    @skipIf(salt.utils.platform.is_darwin(), 'The jenkins user is equivalent to root on mac, causing the test to be unrunnable')
     @requires_salt_modules('pkg.remove', 'pkg.latest_version')
     @requires_system_grains
     def test_pkg_latest_version(self, grains):
