7af42b02beca1a67bb7b33fc637285781a778102
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2019-10-29 11:38:23 +0000

    Add unit test for salt.utils.pkg.rpm arches

--- tests/unit/utils/test_pkg.py
+++ tests/unit/utils/test_pkg.py
@@ -4,6 +4,7 @@
 from __future__ import absolute_import
 # Import Salt Libs
 import salt.utils.pkg
+import salt.utils.pkg.rpm
 # Import Salt Testing Libs
 from tests.support.unit import TestCase
 
@@ -45,3 +46,16 @@ class PkgUtilsTestCase(TestCase):
             oper, verstr = salt.utils.pkg.split_comparison(test_parameter[0])
             self.assertEqual(test_parameter[1], oper)
             self.assertEqual(test_parameter[2], verstr)
+
+    def test_rpm_arches(self):
+        '''
+        Test salt.utils.pkg.rpm supported arch
+        '''
+        self.assertSequenceEqual(['x86_64', 'athlon', 'amd64', 'ia32e', 'ia64', 'geode'], salt.utils.pkg.rpm.ARCHES_64)
+        self.assertSequenceEqual(['i386', 'i486', 'i586', 'i686'], salt.utils.pkg.rpm.ARCHES_32)
+        self.assertSequenceEqual(['ppc', 'ppc64', 'ppc64le', 'ppc64iseries', 'ppc64pseries'], salt.utils.pkg.rpm.ARCHES_PPC)
+        self.assertSequenceEqual(['s390', 's390x'], salt.utils.pkg.rpm.ARCHES_S390)
+        self.assertSequenceEqual(['sparc', 'sparcv8', 'sparcv9', 'sparcv9v', 'sparc64', 'sparc64v'], salt.utils.pkg.rpm.ARCHES_SPARC)
+        self.assertSequenceEqual(['alpha', 'alphaev4', 'alphaev45', 'alphaev5', 'alphaev56', 'alphapca56', 'alphaev6', 'alphaev67', 'alphaev68', 'alphaev7'], salt.utils.pkg.rpm.ARCHES_ALPHA)
+        self.assertSequenceEqual(['armv5tel', 'armv5tejl', 'armv6l', 'armv7l'], salt.utils.pkg.rpm.ARCHES_ARM)
+        self.assertSequenceEqual(['sh3', 'sh4', 'sh4a'], salt.utils.pkg.rpm.ARCHES_SH)
