7078d7a179e853f9fc922c1c95a6823597b8cfba
Robert Baker <bbaker@saltstack.com>
2019-11-21 14:03:19 -0700

    added test for _parse_mods in state.py

---
+++ tests/unit/client/ssh/wrapper/test_state.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+'''
+    :codeauthor: :email:`saltybob <bbaker@saltstack.com`
+'''
+
+# Import python libs
+
+# Import Salt Testing libs
+from tests.support.unit import TestCase
+
+# Import Salt libs
+from salt.client.ssh.wrapper import state
+
+
+class StateTests(TestCase):
+    def test_parse_mods(self):
+        '''
+        Test _parse_mods
+        '''
+        expected = ['a', 'b', 'c', 'd', 'e', 'f']
+        mods = 'a,b, c,  d,e ,f  '
+
+        actual = state._parse_mods(mods)
+        self.assertEqual(expected, actual)
