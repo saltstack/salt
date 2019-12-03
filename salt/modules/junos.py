02c1560f29992fff1cd7da2e33e167d190cbaffb
Pedro Algarvio <pedro@algarvio.me>
2019-12-03 14:40:15 +0000

    Don't catch exceptions only to raise them right after

--- salt/modules/junos.py
+++ salt/modules/junos.py
@@ -85,10 +85,7 @@ def timeoutDecorator(function):
                 conn.timeout = restore_timeout
                 raise
         else:
-            try:
-                return function(*args, **kwargs)
-            except Exception:
-                raise
+            return function(*args, **kwargs)
 
     return wrapper
 
