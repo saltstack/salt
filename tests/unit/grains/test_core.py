4a22d51e1ef1b77d76c291003f7481fdfb4103ec
cmcmarrow <cmcmarrow@saltstack.com>
2019-12-06 15:20:44 -0700

    remove core tests

--- tests/unit/grains/test_core.py
+++ tests/unit/grains/test_core.py
@@ -9,7 +9,6 @@ import logging
 import os
 import socket
 import textwrap
-import platform
 
 # Import Salt Testing Libs
 try:
@@ -34,8 +33,6 @@ import salt.utils.files
 import salt.utils.network
 import salt.utils.platform
 import salt.utils.path
-import salt.modules.cmdmod
-import salt.modules.smbios
 import salt.grains.core as core
 
 # Import 3rd-party libs
@@ -1266,49 +1263,3 @@ class CoreGrainsTestCase(TestCase, LoaderModuleMockMixin):
         finally:
             # change back to original directory
             os.chdir(cwd)
-
-    def test_virtual_set_virtual_grain(self):
-        osdata = {}
-
-        (osdata['kernel'], osdata['nodename'],
-         osdata['kernelrelease'], osdata['kernelversion'], osdata['cpuarch'], _) = platform.uname()
-
-        with patch.dict(core.__salt__, {'cmd.run': salt.modules.cmdmod.run,
-                                        'cmd.run_all': salt.modules.cmdmod.run_all,
-                                        'cmd.retcode': salt.modules.cmdmod.retcode,
-                                        'smbios.get': salt.modules.smbios.get}):
-
-            virtual_grains = core._virtual(osdata)
-
-        self.assertIn('virtual', virtual_grains)
-
-    def test_virtual_has_virtual_grain(self):
-        osdata = {'virtual': 'something'}
-
-        (osdata['kernel'], osdata['nodename'],
-         osdata['kernelrelease'], osdata['kernelversion'], osdata['cpuarch'], _) = platform.uname()
-
-        with patch.dict(core.__salt__, {'cmd.run': salt.modules.cmdmod.run,
-                                        'cmd.run_all': salt.modules.cmdmod.run_all,
-                                        'cmd.retcode': salt.modules.cmdmod.retcode,
-                                        'smbios.get': salt.modules.smbios.get}):
-
-            virtual_grains = core._virtual(osdata)
-
-        self.assertIn('virtual', virtual_grains)
-        self.assertNotEqual(virtual_grains['virtual'], 'physical')
-
-    @skipIf(not salt.utils.platform.is_windows(), 'System is not Windows')
-    def test_osdata_virtual_key_win(self):
-        with patch.dict(core.__salt__, {'cmd.run': salt.modules.cmdmod.run,
-                                        'cmd.run_all': salt.modules.cmdmod.run_all,
-                                        'cmd.retcode': salt.modules.cmdmod.retcode,
-                                        'smbios.get': salt.modules.smbios.get}):
-            with patch.object(core,
-                              '_windows_virtual',
-                              return_value=MagicMock(return_value={'virtual': 'something'})) as _windows_virtual:
-                _windows_virtual.assert_called_once()
-                osdata_grains = core.os_data()
-
-            self.assertIn('virtual', osdata_grains)
-            self.assertNotEqual(osdata_grains['virtual'], 'physical')
