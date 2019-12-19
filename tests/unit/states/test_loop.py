bad5cebbdf654c572bd906b1aac1cb8a1bf7e723
Herbert Buurman <abcde@ilostthegame.eu>
2019-12-19 16:48:19 +0100

    Increase interval and timeout for slow windows machines

--- tests/unit/states/test_loop.py
+++ tests/unit/states/test_loop.py
@@ -300,6 +300,8 @@ class LoopTestCaseNoEval(TestCase, LoaderModuleMockMixin):
                  'comment': 'Timed out while waiting for condition m_ret == 5'}
             )
 
+        # period and timeout below has been doubled to keep windows machines from
+        # returning a lower number of attempts (because it's slower).
         with \
                 patch.dict(
                     salt.states.loop.__salt__,  # pylint: disable=no-member
@@ -309,8 +311,8 @@ class LoopTestCaseNoEval(TestCase, LoaderModuleMockMixin):
                 salt.states.loop.until_no_eval(
                     name='foo.bar',
                     expected=5,
-                    period=0.01,
-                    timeout=0.03),
+                    period=0.02,
+                    timeout=0.06),
                 {'name': 'foo.bar', 'result': False, 'changes': {},
                  'comment': 'Call did not produce the expected result after 3 attempts'}
             )
