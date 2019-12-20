2d7378d1f504f5e38774ccdabc82e9e4047397c6
ch3ll <megan.wilhite@gmail.com>
2019-12-20 15:07:08 -0500

    fix win_servermanager docs

--- salt/states/win_servermanager.py
+++ salt/states/win_servermanager.py
@@ -12,7 +12,6 @@ from __future__ import absolute_import, unicode_literals, print_function
 
 # Import salt modules
 import salt.utils.data
-import salt.utils.versions
 
 
 def __virtual__():
@@ -58,8 +57,9 @@ def installed(name,
             Install all sub-features as well. If the feature is installed but
             one of its sub-features are not installed set this will install
             additional sub-features. This argument was previously renamed from
-            ``force``. To ensure backwards compatibility ``force`` will to work
-            but please update your states to use the preferred ``recurse`` arg.
+            ``force``. To ensure backwards compatibility ``force`` will
+            continue to work but please update your states to use the preferred
+            ``recurse`` arg.
 
         source (Optional[str]):
             Path to the source files if missing from the target system. None
