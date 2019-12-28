f1d2edece525fafc52a25dd786f63175384f7e4d
Pedro Algarvio <pedro@algarvio.me>
2019-12-28 12:07:58 +0000

    Add tests to ensure proper pytest pass/fail behavior.
    This is specially important because we're hacking pytest's fixture
    termination and doing so in important pytest hooks which can change the
    normal behavior of pytest.

--- tests/unit/test_module_names.py
+++ tests/unit/test_module_names.py
@@ -50,6 +50,8 @@ EXCLUDED_FILES = [
     os.path.join('tests', 'zypp_plugin.py'),
     os.path.join('tests', 'unit', 'transport', 'mixins.py'),
     os.path.join('tests', 'integration', 'utils', 'testprogram.py'),
+    os.path.join('tests', 'unit', 'test_pytest_pass_fail.py'),
+    os.path.join('tests', 'unit', 'utils', 'scheduler', 'base.py'),
 ]
 
 
@@ -107,6 +109,7 @@ class BadTestModuleNamesTestCase(TestCase):
         consider mapping it to files manually via tests/filename_map.yml.
         '''
         ignore = (
+            'unit.test_pytest_pass_fail',
             'unit.test_doc',
             'unit.test_mock',
             'unit.test_module_names',
