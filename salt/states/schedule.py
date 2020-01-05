410d02a649d550c117812959ad331791855d9266
Daniel A. Wozniak <dwozniak@saltstack.com>
2020-01-05 23:52:36 +0000

    Minor wording change

--- salt/states/schedule.py
+++ salt/states/schedule.py
@@ -136,9 +136,9 @@ def present(name,
         Requires python-croniter.
 
     run_on_start
-        Whether the job will perform the first run when Salt minion starts,
-        or the job will skip it and wait until the next scheduled run.  Value
-        should be a boolean.
+        Whether the job will run when Salt minion starts, or the job will be
+        skipped **once** and run at the next scheduled run.  Value should be a
+        boolean.
 
     function
         The function that should be executed by the scheduled job.
