72ab311f0ea6d9b42d59fc84bdc019b7c8af1f4a
Cédric Bosdonnat <cbosdonnat@suse.com>
2020-01-08 11:39:32 +0100

    Add virt.update test parameter
    In order to allow running dry-runs of virt.update module add a test
    parameter. This will later be used by the virt states.

--- salt/modules/virt.py
+++ salt/modules/virt.py
@@ -1701,6 +1701,7 @@ def update(
     graphics=None,
     live=True,
     boot=None,
+    test=False,
     **kwargs
 ):
     """
@@ -1757,6 +1758,10 @@ def update(
             }
 
         .. versionadded:: 3000
+
+    :param test: run in dry-run mode if set to True
+
+        .. versionadded:: sodium
 
     :return:
 
@@ -1927,11 +1932,15 @@ def update(
                     item in changes["disk"]["new"]
                     and source_file
                     and not os.path.isfile(source_file)
+                    and not test
                 ):
                     _qemu_image_create(all_disks[idx])
 
         try:
-            conn.defineXML(salt.utils.stringutils.to_str(ElementTree.tostring(desc)))
+            if not test:
+                conn.defineXML(
+                    salt.utils.stringutils.to_str(ElementTree.tostring(desc))
+                )
             status["definition"] = True
         except libvirt.libvirtError as err:
             conn.close()
@@ -1988,7 +1997,7 @@ def update(
 
         for cmd in commands:
             try:
-                ret = getattr(domain, cmd["cmd"])(*cmd["args"])
+                ret = getattr(domain, cmd["cmd"])(*cmd["args"]) if not test else 0
                 device_type = cmd["device"]
                 if device_type in ["cpu", "mem"]:
                     status[device_type] = not bool(ret)
