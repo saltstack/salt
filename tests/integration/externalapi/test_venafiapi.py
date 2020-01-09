16cea5f986e793c5c2b94c72ab65b8858d86a445
Aleksandr Rykalin <alexander.rykalin@venafi.com>
2020-01-10 00:20:36 +0300

    removing print

--- tests/integration/externalapi/test_venafiapi.py
+++ tests/integration/externalapi/test_venafiapi.py
@@ -59,8 +59,6 @@ class VenafiTest(ShellCase):
                                 key_password='secretPassword',
                                 zone='fake')
         cert_output = ret['return'][0]
-        print('return is', ret)
-        print("cert out is: " + cert_output)
         assert cert_output is not None, 'venafi_certificate not found in `output_value`'
 
         cert = x509.load_pem_x509_certificate(cert_output.encode(), default_backend())
@@ -138,8 +136,6 @@ xlAKgaU6i03jOm5+sww5L2YVMi1eeBN+kx7o94ogpRemC/EUidvl1PUJ6+e7an9V
                                     csr_path=csr_path,
                                     zone='fake')
             cert_output = ret['return'][0]
-            print('return is', ret)
-            print("cert out is: " + cert_output)
             assert cert_output is not None, 'venafi_certificate not found in `output_value`'
 
             cert = x509.load_pem_x509_certificate(cert_output.encode(), default_backend())
