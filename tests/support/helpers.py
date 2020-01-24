c5157aa4969eddeffef676c94cc3a4e56b0d2dfd
Pedro Algarvio <pedro@algarvio.me>
2020-01-24 14:43:43 +0000

    Add a `VirtualEnv` tests helper

--- tests/support/helpers.py
+++ tests/support/helpers.py
@@ -1614,3 +1614,71 @@ class PatchedEnviron(object):
 
 
 patched_environ = PatchedEnviron
+
+
+class VirtualEnv(object):
+    def __init__(self, venv_dir=None):
+        self.venv_dir = venv_dir or tempfile.mkdtemp(dir=RUNTIME_VARS.TMP)
+        if salt.utils.platform.is_windows():
+            self.venv_python = os.path.join(self.venv_dir, 'Scripts', 'python.exe')
+        else:
+            self.venv_python = os.path.join(self.venv_dir, 'bin', 'python')
+
+    def __enter__(self):
+        try:
+            self._create_virtualenv()
+        except subprocess.CalledProcessError:
+            raise AssertionError('Failed to create virtualenv')
+        return self
+
+    def __exit__(self, *args):
+        shutil.rmtree(self.venv_dir, ignore_errors=True)
+
+    def install(self, *args):
+        subprocess.check_call(
+            [self.venv_python, '-m', 'pip', 'install'] + list(args)
+        )
+
+    def _get_real_python(self):
+        '''
+        The reason why the virtualenv creation is proxied by this function is mostly
+        because under windows, we can't seem to properly create a virtualenv off of
+        another virtualenv(we can on linux) and also because, we really don't want to
+        test virtualenv creation off of another virtualenv, we want a virtualenv created
+        from the original python.
+        Also, on windows, we must also point to the virtualenv binary outside the existing
+        virtualenv because it will fail otherwise
+        '''
+        try:
+            if salt.utils.platform.is_windows():
+                return os.path.join(sys.real_prefix, os.path.basename(sys.executable))
+            else:
+                python_binary_names = [
+                    'python{}.{}'.format(*sys.version_info),
+                    'python{}'.format(*sys.version_info),
+                    'python'
+                ]
+                for binary_name in python_binary_names:
+                    python = os.path.join(sys.real_prefix, 'bin', binary_name)
+                    if os.path.exists(python):
+                        break
+                else:
+                    raise AssertionError(
+                        'Couldn\'t find a python binary name under \'{}\' matching: {}'.format(
+                            os.path.join(sys.real_prefix, 'bin'),
+                            python_binary_names
+                        )
+                    )
+                return python
+        except AttributeError:
+            return sys.executable
+
+    def _create_virtualenv(self):
+        subprocess.check_call(
+            [
+                sys.executable,
+                '-m', 'virtualenv',
+                '-p', self._get_real_python(),
+                self.venv_dir
+            ]
+        )
