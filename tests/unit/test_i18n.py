905f288b78d4fa1e5a8aa1365e3ce0f23234811e
markuskramerIgitt <markuskramerIgitt@gmail.com>
2020-01-30 15:03:34 +0100

    test i18n characters with file.line

---
+++ tests/unit/test_i18n.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+'''
+   Test the ability to process characters present in non English operating systems.
+   In German, volume is Datenträger, executed is ausgeführt
+   Current Windows issue: ü results in UnicodeDecodeError: 'utf8' codec can't decode byte 0xfc
+'''
+from __future__ import absolute_import
+from tests.support.unit import TestCase
+import os
+import io
+
+class i18nTestCase(TestCase):
+
+    @with_tempdir()
+    def test_i18n_characters_with_file_line(self, tempdir):
+        tempfile = os.path.join(tempdir, 'temp_file')
+        content_in = "äü"
+        with io.open(tempfile, 'w', encoding='utf8') as fp:
+            fp.write(content_in)
+        ret = self.run_state('file.line',
+                             name=tempfile,
+                             content='{}'.format(content_in),
+                             mode='insert',
+                             location='start')
+        self.assertSaltTrueReturn(ret)
+        with io.open(tempfile, 'r', encoding='utf8') as fp:
+            content_out = fp.read()
+        self.assertEqual(content_in, content_out)
