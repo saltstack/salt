91f0920563253ad3ea46f63321c13f573a371c19
Daniel A. Wozniak <dwozniak@saltstack.com>
2020-02-10 00:58:20 +0000

    Fix linter warts

--- tests/unit/test_ext.py
+++ tests/unit/test_ext.py
@@ -28,42 +28,42 @@ class VendorTornadoTest(TestCase):
     Ensure we are not using any non vendor'ed tornado
     '''
 
-# TODO After we vendor Tornado 5.1
-#    def test_import_override(self):
-#        tmp = tempfile.mkdtemp()
-#        test_source = tests.support.helpers.dedent('''
-#        from __future__ import absolute_import, print_function
-#        import salt
-#        import tornado
-#        print(tornado.__name__)
-#        ''')
-#        test_source_path = os.path.join(tmp, 'test.py')
-#        tornado_source = tests.support.helpers.dedent('''
-#        foo = 'bar'
-#        ''')
-#        tornado_source_path = os.path.join(tmp, 'tornado.py')
-#        with salt.utils.files.fopen(test_source_path, 'w') as fp:
-#            fp.write(test_source)
-#        with salt.utils.files.fopen(tornado_source_path, 'w') as fp:
-#            fp.write(tornado_source)
-#        # Preserve the virtual environment
-#        env = os.environ.copy()
-#        if salt.utils.platform.is_windows():
-#            if salt.ext.six.PY2:
-#                env[b'PYTHONPATH'] = b';'.join([a.encode() for a in sys.path])
-#            else:
-#                env['PYTHONPATH'] = ';'.join(sys.path)
-#        else:
-#            env['PYTHONPATH'] = ':'.join(sys.path)
-#        p = subprocess.Popen(
-#            [sys.executable, test_source_path],
-#            stderr=subprocess.PIPE,
-#            stdout=subprocess.PIPE,
-#            env=env
-#        )
-#        p.wait()
-#        pout = p.stdout.read().strip().decode()
-#        assert pout == 'salt.ext.tornado', pout
+    def test_import_override(self):
+        tmp = tempfile.mkdtemp()
+        test_source = tests.support.helpers.dedent('''
+        from __future__ import absolute_import, print_function
+        import salt
+        import tornado
+        print(tornado.__name__)
+        ''')
+        test_source_path = os.path.join(tmp, 'test.py')
+        tornado_source = tests.support.helpers.dedent('''
+        foo = 'bar'
+        ''')
+        tornado_source_path = os.path.join(tmp, 'tornado.py')
+        with salt.utils.files.fopen(test_source_path, 'w') as fp:
+            fp.write(test_source)
+        with salt.utils.files.fopen(tornado_source_path, 'w') as fp:
+            fp.write(tornado_source)
+        # Preserve the virtual environment
+        env = os.environ.copy()
+        if salt.utils.platform.is_windows():
+            if salt.ext.six.PY2:
+                env[b'PYTHONPATH'] = b';'.join([a.encode() for a in sys.path])
+            else:
+                env['PYTHONPATH'] = ';'.join(sys.path)
+        else:
+            env['PYTHONPATH'] = ':'.join(sys.path)
+        p = subprocess.Popen(
+            [sys.executable, test_source_path],
+            stderr=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            env=env
+        )
+        p.wait()
+        pout = p.stdout.read().strip().decode()
+        # TODO After we vendor Tornado 5.1
+        # assert pout == 'salt.ext.tornado', pout
 
     def test_vendored_tornado_import(self):
         grep_call = salt.modules.cmdmod.run_stdout(
