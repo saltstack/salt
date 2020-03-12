f430660379de9c300340625fc72cfd74e2253e52
Mike Place <mike.place@elastic.co>
2020-03-12 16:08:04 +0100

    Add test

--- tests/unit/grains/test_core.py
+++ tests/unit/grains/test_core.py
@@ -667,6 +667,11 @@ class CoreGrainsTestCase(TestCase, LoaderModuleMockMixin):
         }
         self._run_os_grains_tests("debian-9", _os_release_map, expectation)
 
+    def test_unicode_error(self):
+        raise_unicode_mock = MagicMock(name="raise_unicode_error", side_effect=UnicodeError)
+        with patch('socket.getaddrinfo'):
+            salt.grains.core.ip_fqdn()
+
     @skipIf(not salt.utils.platform.is_linux(), "System is not Linux")
     def test_ubuntu_xenial_os_grains(self):
         """
