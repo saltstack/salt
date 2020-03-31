400322f318bfc36bb3be39574e1c12e681081879
twangboy <slee@saltstack.com>
2020-03-31 14:23:03 -0600

    Add support for pending_reboot grain
    Adds pending_reboot graint
    Adds new win_system salt util
    Adds tests for new grain
    Adds tests for new salt util

---
+++ salt/grains/pending_reboot.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+'''
+Grain that indicates the system is pending a reboot
+See functions in salt.utils.win_system to see what conditions would indicate
+a reboot is pending
+'''
+from __future__ import absolute_import, print_function, unicode_literals
+
+# Import python libs
+import logging
+
+# Import salt libs
+import salt.utils.platform
+import salt.utils.win_system
+
+log = logging.getLogger(__name__)
+
+__virtualname__ = 'pending_reboot'
+
+
+def __virtual__():
+    if not salt.utils.platform.is_windows():
+        return False, "'pending_reboot' grain only available on Windows"
+    return __virtualname__
+
+
+def pending_reboot():
+    '''
+    A grain that indicates that the system is pending a reboot.
+    '''
+    return {'pending_reboot': salt.utils.win_system.get_pending_reboot()}
