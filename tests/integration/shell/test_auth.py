2ae44710e4e8baea0f97497df116fa9e7f670e35
Pedro Algarvio <pedro@algarvio.me>
2020-04-08 19:35:14 +0100

    Untangle `test/integration/shell/test_auth.py`

--- tests/integration/shell/test_auth.py
+++ tests/integration/shell/test_auth.py
@@ -7,142 +7,132 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 import logging
-import random
-import string
 
+# Import 3rd-party libs
 import pytest
+
+# Import Salt libs
 import salt.utils.platform
-from salt.ext.six.moves import range
-from salt.utils.pycrypto import gen_hash
-from tests.support.case import ModuleCase, ShellCase
-from tests.support.mixins import SaltReturnAssertsMixin
-from tests.support.unit import skipIf
-
-try:
-    import pwd
-    import grp
-except ImportError:
-    pwd, grp = None, None
-
 
 log = logging.getLogger(__name__)
 
 
-def gen_password():
-    """
-    generate a password and hash it
-    """
-    password = "".join(
-        random.choice(string.ascii_letters + string.digits) for _ in range(20)
-    )
-    hashed_pwd = gen_hash("salt", password, "sha512")
-
-    return password, hashed_pwd
+USERA = "saltdev-auth"
+USERA_PWD = "saltdev"
+HASHED_USERA_PWD = "$6$SALTsalt$ZZFD90fKFWq8AGmmX0L3uBtS9fXL62SrTk5zcnQ6EkD6zoiM3kB88G1Zvs0xm/gZ7WXJRs5nsTBybUvGSqZkT."
 
 
-@skipIf(pwd is None or grp is None, "No pwd or grp module available")
+@pytest.fixture(scope="module")
+def saltdev_account(sminion):
+    try:
+        assert sminion.functions.user.add(USERA, createhome=False)
+        assert sminion.functions.shadow.set_password(
+            USERA, USERA_PWD if salt.utils.platform.is_darwin() else HASHED_USERA_PWD
+        )
+        assert USERA in sminion.functions.user.list_users()
+        # Run tests
+        yield
+    finally:
+        sminion.functions.user.delete(USERA, remove=True)
+
+
+SALTOPS = "saltops"
+
+
+@pytest.fixture(scope="module")
+def saltops_group(sminion):
+    try:
+        assert sminion.functions.group.add(SALTOPS)
+        # Run tests
+        yield
+    finally:
+        sminion.functions.group.delete(SALTOPS)
+
+
+USERB = "saltdev-adm"
+USERB_PWD = USERA_PWD
+HASHED_USERB_PWD = HASHED_USERA_PWD
+
+
+@pytest.fixture(scope="module")
+def saltadm_account(sminion, saltops_group):
+    try:
+        assert sminion.functions.user.add(USERB, groups=[SALTOPS], createhome=False)
+        assert sminion.functions.shadow.set_password(
+            USERB, USERB_PWD if salt.utils.platform.is_darwin() else HASHED_USERB_PWD
+        )
+        assert USERB in sminion.functions.user.list_users()
+        # Run tests
+        yield
+    finally:
+        sminion.functions.user.delete(USERB, remove=True)
+
+
 @pytest.mark.skip_if_not_root
 @pytest.mark.destructive_test
-@pytest.mark.windows_whitelisted
-@pytest.mark.requires_salt_modules("shadow.set_password")
-@pytest.mark.requires_salt_states("user.absent", "user.present")
-class UserAuthTest(ModuleCase, SaltReturnAssertsMixin, ShellCase):
+@pytest.mark.skip_on_windows
+class TestUserAuth(object):
     """
     Test user auth mechanisms
     """
 
-    _call_binary_ = "salt"
-    user = "saltdev"
-
-    def setUp(self):
-        ret = self.run_state("user.present", name=self.user, createhome=False)
-        self.assertSaltTrueReturn(ret)
-
-    def tearDown(self):
-        ret = self.run_state("user.absent", name=self.user)
-        self.assertSaltTrueReturn(ret)
-
-    def test_pam_auth_valid_user(self):
+    def test_pam_auth_valid_user(self, salt_cli, saltdev_account):
         """
         test that pam auth mechanism works with a valid user
         """
-        password, hashed_pwd = gen_password()
-
-        # set user password
-        set_pw_cmd = "shadow.set_password {0} '{1}'".format(
-            self.user, password if salt.utils.platform.is_darwin() else hashed_pwd
-        )
-        self.assertRunCall(set_pw_cmd)
-
         # test user auth against pam
-        cmd = '-a pam "*" test.ping --username {0} --password {1}'.format(
-            self.user, password
+        ret = salt_cli.run(
+            "-a",
+            "pam",
+            "--username",
+            USERA,
+            "--password",
+            USERA_PWD,
+            "test.ping",
+            minion_tgt="minion",
         )
-        resp = self.run_salt(cmd)
-        log.debug("resp = %s", resp)
-        self.assertIn("minion", [r.strip(": ") for r in resp])
+        assert ret.exitcode == 0
+        assert ret.json is True
 
-    def test_pam_auth_invalid_user(self):
+    def test_pam_auth_invalid_user(self, salt_cli, saltdev_account):
         """
         test pam auth mechanism errors for an invalid user
         """
-        cmd = '-a pam "*" test.ping ' "--username nouser --password {0}".format(
-            "abcd1234"
+        ret = salt_cli.run(
+            "-a",
+            "pam",
+            "--username",
+            "nouser",
+            "--password",
+            "1234",
+            "test.ping",
+            minion_tgt="minion",
         )
-        resp = self.run_salt(cmd)
-        self.assertIn("Authentication error occurred", "".join(resp))
+        assert ret.stdout == "Authentication error occurred."
 
 
-@skipIf(pwd is None or grp is None, "No pwd or grp module available")
 @pytest.mark.skip_if_not_root
 @pytest.mark.destructive_test
-@pytest.mark.requires_salt_modules("shadow.set_password", "user.chgroups")
-@pytest.mark.requires_salt_states(
-    "group.absent", "group.present", "user.absent", "user.present"
-)
-class GroupAuthTest(ModuleCase, SaltReturnAssertsMixin, ShellCase):
+class TestGroupAuth(object):
     """
     Test group auth mechanisms
     """
 
-    _call_binary_ = "salt"
-
-    user = "saltadm"
-    group = "saltops"
-
-    def setUp(self):
-        ret = self.run_state("group.present", name=self.group)
-        self.assertSaltTrueReturn(ret)
-        ret = self.run_state(
-            "user.present", name=self.user, createhome=False, groups=[self.group]
-        )
-        self.assertSaltTrueReturn(ret)
-        self.assertRunCall(
-            "user.chgroups {0} {1} True".format(self.user, self.group), local=True
-        )
-
-    def tearDown(self):
-        ret0 = self.run_state("user.absent", name=self.user)
-        ret1 = self.run_state("group.absent", name=self.group)
-        self.assertSaltTrueReturn(ret0)
-        self.assertSaltTrueReturn(ret1)
-
-    def test_pam_auth_valid_group(self):
+    def test_pam_auth_valid_group(self, salt_cli, saltadm_account):
         """
         test that pam auth mechanism works for a valid group
         """
-        password, hashed_pwd = gen_password()
-
-        # set user password
-        set_pw_cmd = "shadow.set_password {0} '{1}'".format(
-            self.user, password if salt.utils.platform.is_darwin() else hashed_pwd
-        )
-        self.assertRunCall(set_pw_cmd)
-
         # test group auth against pam: saltadm is not configured in
         # external_auth, but saltops is and saldadm is a member of saltops
-        cmd = '-a pam "*" test.ping --username {0} --password {1}'.format(
-            self.user, password
+        ret = salt_cli.run(
+            "-a",
+            "pam",
+            "--username",
+            USERB,
+            "--password",
+            USERB_PWD,
+            "test.ping",
+            minion_tgt="minion",
         )
-        resp = self.run_salt(cmd)
-        self.assertIn("minion", [r.strip(": ") for r in resp])
+        assert ret.exitcode == 0
+        assert ret.json is True
