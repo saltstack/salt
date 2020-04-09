d23659a5763a2df2fe3f44187bf254bf7c3e801d
Leif Liddy <leif.liddy@gmail.com>
2020-04-09 18:53:58 +0200

    removed None check to maintain functional equivalence of the original state of this file, don\'t want to introduce new checks

--- salt/output/highstate.py
+++ salt/output/highstate.py
@@ -186,7 +186,7 @@ def _format_host(host, data, indent_level=1):
     nchanges = 0
     strip_colors = __opts__.get("strip_colors", True)
 
-    if isinstance(data, int) or data is None:
+    if isinstance(data, int):
         nchanges = 1
         hstrs.append(('{0}    {1}{2[ENDC]}'.format(hcolor, data, colors)))
         hcolor = colors["CYAN"]  # Print the minion name in cyan
