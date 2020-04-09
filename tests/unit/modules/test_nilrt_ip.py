fcbef0b0ba1acdbdc40c38d4c5bdcb590701b521
ch3ll <megan.wilhite@gmail.com>
2020-04-09 09:21:55 -0400

    run black on code

--- tests/unit/modules/test_nilrt_ip.py
+++ tests/unit/modules/test_nilrt_ip.py
@@ -8,34 +8,32 @@ import salt.modules.nilrt_ip as nilrt_ip
 
 # Import Salt Testing Libs
 from tests.support.mixins import LoaderModuleMockMixin
-from tests.support.unit import TestCase
 from tests.support.mock import patch
+from tests.support.unit import TestCase
 
 
 class NilrtIPTestCase(TestCase, LoaderModuleMockMixin):
-    '''
+    """
     TestCase for salt.modules.nilrt_ip module
-    '''
+    """
 
     def setup_loader_modules(self):
-        return {nilrt_ip: {'__grains__':
-                              {'lsb_distrib_id': 'not_nilrt'}}
-               }
+        return {nilrt_ip: {"__grains__": {"lsb_distrib_id": "not_nilrt"}}}
 
     def test_change_state_down_state(self):
-        '''
+        """
         Tests _change_state when not connected
         and new state is down
-        '''
-        with patch('salt.modules.nilrt_ip._interface_to_service', return_value=True):
-            with patch('salt.modules.nilrt_ip._connected', return_value=False):
-                assert nilrt_ip._change_state('test_interface', 'down')
+        """
+        with patch("salt.modules.nilrt_ip._interface_to_service", return_value=True):
+            with patch("salt.modules.nilrt_ip._connected", return_value=False):
+                assert nilrt_ip._change_state("test_interface", "down")
 
     def test_change_state_up_state(self):
-        '''
+        """
         Tests _change_state when connected
         and new state is up
-        '''
-        with patch('salt.modules.nilrt_ip._interface_to_service', return_value=True):
-            with patch('salt.modules.nilrt_ip._connected', return_value=True):
-                assert nilrt_ip._change_state('test_interface', 'up')
+        """
+        with patch("salt.modules.nilrt_ip._interface_to_service", return_value=True):
+            with patch("salt.modules.nilrt_ip._connected", return_value=True):
+                assert nilrt_ip._change_state("test_interface", "up")
