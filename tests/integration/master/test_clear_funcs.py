0491a84361098a2862005cef8d0f6c9dd1e3bb73
Daniel A. Wozniak <dwozniak@saltstack.com>
2020-04-13 06:44:58 +0000

    Fix CVE-2020-11652
    Sanitize paths in ClearFuncs methods provided by salt-master. This
    ensures we do not allow access to un-intended files and directories.

--- tests/integration/master/test_clear_funcs.py
+++ tests/integration/master/test_clear_funcs.py
@@ -127,3 +127,177 @@ class ClearFuncsPubTestCase(TestCase):
                 ret_evt = raw
                 break
         assert not os.path.exists(self.tmpfile), "Evil file created"
+
+
+class ClearFuncsConfigTest(TestCase):
+    def setUp(self):
+        master_opts = AdaptedConfigurationTestCaseMixin.get_config("master")
+        self.conf_dir = os.path.dirname(master_opts["conf_file"])
+        master = "127.0.0.1"
+        ret_port = 64506
+        user = keyuser()
+        keyfile = ".{}_key".format(user)
+        keypath = os.path.join(RUNTIME_VARS.TMP, "rootdir", "cache", keyfile)
+
+        with salt.utils.files.fopen(keypath) as keyfd:
+            self.key = keyfd.read()
+
+        self.minion_config = {
+            "transport": "zeromq",
+            "pki_dir": "/tmp",
+            "id": "root",
+            "master_ip": master,
+            "master_port": ret_port,
+            "auth_timeout": 5,
+            "auth_tries": 1,
+            "master_uri": "tcp://{0}:{1}".format(master, ret_port),
+        }
+
+    def tearDown(self):
+        try:
+            os.remove(os.path.join(self.conf_dir, "evil.conf"))
+        except OSError:
+            pass
+        delattr(self, "conf_dir")
+        delattr(self, "key")
+        delattr(self, "minion_config")
+
+    def test_clearfuncs_config(self):
+        clear_channel = salt.transport.client.ReqChannel.factory(
+            self.minion_config, crypt="clear"
+        )
+
+        msg = {
+            "key": self.key,
+            "cmd": "wheel",
+            "fun": "config.update_config",
+            "file_name": "../evil",
+            "yaml_contents": "win",
+        }
+        ret = clear_channel.send(msg, timeout=5)
+        assert not os.path.exists(
+            os.path.join(self.conf_dir, "evil.conf")
+        ), "Wrote file via directory traversal"
+
+
+class ClearFuncsFileRoots(TestCase):
+    def setUp(self):
+        self.master_opts = AdaptedConfigurationTestCaseMixin.get_config("master")
+        self.target_dir = os.path.dirname(self.master_opts["file_roots"]["base"][0])
+        master = "127.0.0.1"
+        ret_port = 64506
+        user = keyuser()
+        self.keyfile = ".{}_key".format(user)
+        keypath = os.path.join(RUNTIME_VARS.TMP, "rootdir", "cache", self.keyfile)
+
+        with salt.utils.files.fopen(keypath) as keyfd:
+            self.key = keyfd.read()
+
+        self.minion_config = {
+            "transport": "zeromq",
+            "pki_dir": "/tmp",
+            "id": "root",
+            "master_ip": master,
+            "master_port": ret_port,
+            "auth_timeout": 5,
+            "auth_tries": 1,
+            "master_uri": "tcp://{0}:{1}".format(master, ret_port),
+        }
+
+    def tearDown(self):
+        try:
+            os.remove(os.path.join(self.target_dir, "pwn.txt"))
+        except OSError:
+            pass
+        delattr(self, "master_opts")
+        delattr(self, "target_dir")
+        delattr(self, "keyfile")
+        delattr(self, "key")
+        delattr(self, "minion_config")
+
+    def test_fileroots_write(self):
+        clear_channel = salt.transport.client.ReqChannel.factory(
+            self.minion_config, crypt="clear"
+        )
+
+        msg = {
+            "key": self.key,
+            "cmd": "wheel",
+            "fun": "file_roots.write",
+            "data": "win",
+            "path": os.path.join("..", "pwn.txt"),
+            "saltenv": "base",
+        }
+        ret = clear_channel.send(msg, timeout=5)
+        assert not os.path.exists(
+            os.path.join(self.target_dir, "pwn.txt")
+        ), "Wrote file via directory traversal"
+
+    def test_fileroots_read(self):
+        rootdir = self.master_opts["root_dir"]
+        fileroot = self.master_opts["file_roots"]["base"][0]
+        # If this asserion fails the test may need to be re-written
+        assert os.path.dirname(os.path.dirname(rootdir)) == os.path.dirname(fileroot)
+        clear_channel = salt.transport.client.ReqChannel.factory(
+            self.minion_config, crypt="clear"
+        )
+        readpath = os.path.join(
+            "..", "salt-tests-tmpdir", "rootdir", "cache", self.keyfile,
+        )
+        msg = {
+            "key": self.key,
+            "cmd": "wheel",
+            "fun": "file_roots.read",
+            "path": os.path.join(
+                "..", "salt-tests-tmpdir", "rootdir", "cache", self.keyfile,
+            ),
+            "saltenv": "base",
+        }
+
+        ret = clear_channel.send(msg, timeout=5)
+        try:
+            # When vulnerable this assertion will fail.
+            assert (
+                list(ret["data"]["return"][0].items())[0][1] != self.key
+            ), "Read file via directory traversal"
+        except IndexError:
+            pass
+        # If the vulnerability is fixed, no data will be returned.
+        assert ret["data"]["return"] == []
+
+
+class ClearFuncsTokenTest(TestCase):
+    def setUp(self):
+        self.master_opts = AdaptedConfigurationTestCaseMixin.get_config("master")
+        master = "127.0.0.1"
+        ret_port = 64506
+        self.minion_config = {
+            "transport": "zeromq",
+            "pki_dir": "/tmp",
+            "id": "root",
+            "master_ip": master,
+            "master_port": ret_port,
+            "auth_timeout": 5,
+            "auth_tries": 1,
+            "master_uri": "tcp://{0}:{1}".format(master, ret_port),
+        }
+
+    def tearDown(self):
+        delattr(self, "master_opts")
+        delattr(self, "minion_config")
+
+    def test_token(self):
+        tokensdir = os.path.join(
+            self.master_opts["root_dir"], self.master_opts["cachedir"], "tokens"
+        )
+        assert os.path.exists(tokensdir), tokensdir
+        clear_channel = salt.transport.client.ReqChannel.factory(
+            self.minion_config, crypt="clear"
+        )
+        msg = {
+            "arg": [],
+            "cmd": "get_token",
+            "token": os.path.join("..", "minions", "minion", "data.p"),
+        }
+        ret = clear_channel.send(msg, timeout=5)
+        assert "pillar" not in ret, "Read minion data via directory traversal"
