f7af0c0b7db41ef76f2fa3b578df620eef131421
Luke Pitt <LukeAPitt@googlemail.com>
2020-04-14 11:50:46 +0100

    Make VT handle multibyte characters split over binary transmission blocks
    See PR https://github.com/saltstack/salt/pull/53404

--- tests/unit/utils/test_vt.py
+++ tests/unit/utils/test_vt.py
@@ -12,6 +12,8 @@
 # Import Python libs
 from __future__ import absolute_import, print_function, unicode_literals
 
+import functools
+import io
 import os
 import random
 import subprocess
@@ -19,15 +21,54 @@ import sys
 import time
 
 # Import Salt libs
+import salt.utils
 import salt.utils.files
 import salt.utils.platform
+import salt.utils.stringutils
 import salt.utils.vt
 
 # Import 3rd-party libs
 from salt.ext.six.moves import range  # pylint: disable=import-error,redefined-builtin
 
 # Import Salt Testing libs
+from tests.support.paths import CODE_DIR
 from tests.support.unit import TestCase, skipIf
+
+
+def stdout_fileno_available():
+    """
+        Tests if sys.stdout.fileno is available in this testing environment
+    """
+    try:
+        sys.stdout.fileno()
+        return True
+    except io.UnsupportedOperation:
+        return False
+
+
+def fixStdOutErrFileNoIfNeeded(func):
+    """
+        Decorator that sets stdout and stderr to their original objects if
+        sys.stdout.fileno() doesn't work and restores them after running the
+        decorated function. This doesn't check if the original objects actually
+        work. If they don't then the test environment is too broken to test
+        the VT.
+    """
+
+    @functools.wraps(func)
+    def wrapper_fixStdOutErrFileNoIfNeeded(*args, **kwargs):
+        original_stdout = os.sys.stdout
+        original_stderr = os.sys.stderr
+        if not stdout_fileno_available():
+            os.sys.stdout = os.sys.__stdout__
+            os.sys.stderr = os.sys.__stderr__
+        try:
+            return func(*args, **kwargs)
+        finally:
+            os.sys.stdout = original_stdout
+            os.sys.stderr = original_stderr
+
+    return wrapper_fixStdOutErrFileNoIfNeeded
 
 
 class VTTestCase(TestCase):
@@ -218,5 +259,138 @@ class VTTestCase(TestCase):
             self.assertFalse(term.isalive())
             self.assertIsNone(stderr)
             self.assertIsNone(stdout)
+        finally:
+            term.close(terminate=True, kill=True)
+
+    @staticmethod
+    def generate_multibyte_stdout_unicode(block_size):
+        return b"\xE2\x80\xA6" * 4 * block_size
+
+    @staticmethod
+    def generate_multibyte_stderr_unicode(block_size):
+        return b"\x2E" + VTTestCase.generate_multibyte_stdout_unicode(block_size)
+
+    @skipIf(
+        salt.utils.platform.is_windows(), "Skip VT tests on windows, due to issue 54290"
+    )
+    @fixStdOutErrFileNoIfNeeded
+    def test_split_multibyte_characters_unicode(self):
+        """
+            Tests that the vt correctly handles multibyte characters that are
+            split between blocks of transmitted data.
+        """
+        block_size = 1024
+        encoding = "utf-8"
+        stdout_content = VTTestCase.generate_multibyte_stdout_unicode(block_size)
+        # stderr is offset by one byte to guarentee a split character in
+        # one of the output streams
+        stderr_content = VTTestCase.generate_multibyte_stderr_unicode(block_size)
+
+        expected_stdout = salt.utils.stringutils.to_unicode(stdout_content, encoding)
+        expected_stderr = salt.utils.stringutils.to_unicode(stderr_content, encoding)
+        python_command = "\n".join(
+            (
+                "import sys",
+                "import os",
+                "import tests.unit.utils.test_vt as test_vt",
+                (
+                    "os.write(sys.stdout.fileno(), "
+                    "test_vt.VTTestCase.generate_multibyte_stdout_unicode("
+                    + str(block_size)
+                    + "))"
+                ),
+                (
+                    "os.write(sys.stderr.fileno(), "
+                    "test_vt.VTTestCase.generate_multibyte_stderr_unicode("
+                    + str(block_size)
+                    + "))"
+                ),
+            )
+        )
+        term = salt.utils.vt.Terminal(
+            args=[sys.executable, "-c", '"' + python_command + '"'],
+            shell=True,
+            cwd=CODE_DIR,
+            stream_stdout=False,
+            stream_stderr=False,
+            force_receive_encoding=encoding,
+        )
+        buffer_o = buffer_e = salt.utils.stringutils.to_unicode("")
+        try:
+            while term.has_unread_data:
+                stdout, stderr = term.recv(block_size)
+                if stdout:
+                    buffer_o += stdout
+                if stderr:
+                    buffer_e += stderr
+
+            self.assertEqual(buffer_o, expected_stdout)
+            self.assertEqual(buffer_e, expected_stderr)
+        finally:
+            term.close(terminate=True, kill=True)
+
+    @staticmethod
+    def generate_multibyte_stdout_shiftjis(block_size):
+        return b"\x8B\x80" * 4 * block_size
+
+    @staticmethod
+    def generate_multibyte_stderr_shiftjis(block_size):
+        return b"\x2E" + VTTestCase.generate_multibyte_stdout_shiftjis(block_size)
+
+    @skipIf(
+        salt.utils.platform.is_windows(), "Skip VT tests on windows, due to issue 54290"
+    )
+    @fixStdOutErrFileNoIfNeeded
+    def test_split_multibyte_characters_shiftjis(self):
+        """
+            Tests that the vt correctly handles multibyte characters that are
+            split between blocks of transmitted data.
+            Uses shift-jis encoding to make sure code doesn't assume unicode.
+        """
+        block_size = 1024
+        encoding = "shift-jis"
+        stdout_content = VTTestCase.generate_multibyte_stdout_shiftjis(block_size)
+        stderr_content = VTTestCase.generate_multibyte_stderr_shiftjis(block_size)
+
+        expected_stdout = salt.utils.stringutils.to_unicode(stdout_content, encoding)
+        expected_stderr = salt.utils.stringutils.to_unicode(stderr_content, encoding)
+        python_command = "\n".join(
+            (
+                "import sys",
+                "import os",
+                "import tests.unit.utils.test_vt as test_vt",
+                (
+                    "os.write(sys.stdout.fileno(), "
+                    "test_vt.VTTestCase.generate_multibyte_stdout_shiftjis("
+                    + str(block_size)
+                    + "))"
+                ),
+                (
+                    "os.write(sys.stderr.fileno(), "
+                    "test_vt.VTTestCase.generate_multibyte_stderr_shiftjis("
+                    + str(block_size)
+                    + "))"
+                ),
+            )
+        )
+        term = salt.utils.vt.Terminal(
+            args=[sys.executable, "-c", '"' + python_command + '"'],
+            shell=True,
+            cwd=CODE_DIR,
+            stream_stdout=False,
+            stream_stderr=False,
+            force_receive_encoding=encoding,
+        )
+        buffer_o = buffer_e = salt.utils.stringutils.to_unicode("")
+        try:
+            while term.has_unread_data:
+                stdout, stderr = term.recv(block_size)
+                if stdout:
+                    buffer_o += stdout
+                if stderr:
+                    buffer_e += stderr
+
+            self.assertEqual(buffer_o, expected_stdout)
+            self.assertEqual(buffer_e, expected_stderr)
         finally:
             term.close(terminate=True, kill=True)
