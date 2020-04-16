3d0e42798b6136232396e8d1c0fabada13c84976
lomeroe <github@proxyman.com>
2020-04-16 15:02:16 -0500

    fix several typos and bad copy/pastes in test

--- tests/integration/runners/test_queue.py
+++ tests/integration/runners/test_queue.py
@@ -23,34 +23,35 @@ class ManageTest(ShellCase):
         Test inserting and deleting from a queue
         """
         ret = self.run_run_plus(
-            'queue.insert'
+            'queue.insert',
             'testqueue',
             'a')
-        self.assertTrue(ret, msg='Failure inserting item into test queue')
+        self.assertTrue(ret['return'], msg='Failure inserting item into test queue')
 
         ret = self.run_run_plus(
             'queue.delete',
             'testqueue',
             'a')
-        self.assertTrue(ret, msg='Failure to delete item from test queue')
+        self.assertTrue(ret['return'], msg='Failure to delete item from test queue')
 
     def test_queue_list(self):
         """
         Test queue item listing
         """
         ret = self.run_run_plus(
-            'queue.insert'
+            'queue.insert',
             'testqueue',
             ['a', 'z', 'b', 'r', 'e'])
-        self.assertTrue(ret, msg='Failure inserting items into test queue')
+        self.assertTrue(ret['return'], msg='Failure inserting items into test queue')
+        log.debug('ELM - ret == {}'.format(ret))
 
         # default returns lexical sort of items
         ret = self.run_run_plus(
             'queue.list_items',
             'testqueue')
-        self.assertEqual(ret,
-                         ['a', 'b', 'e', 'r', 'z'],
-                         msg='queue item list not returned as expected')
+        self.assertEqual(ret['return'],
+                         [u'a', u'b', u'e', u'r', u'z'],
+                         msg='queue item list not returned as expected ("{0}" returned)'.format(ret['return']))
 
         # fifo list
         ret = self.run_run_plus(
@@ -58,126 +59,129 @@ class ManageTest(ShellCase):
             'testqueue',
             mode='fifo')
         # ensure fifo output
-        self.assertEqual(ret,
-                         ['a', 'z', 'b', 'r', 'e'],
+        self.assertEqual(ret['return'],
+                         [u'a', u'z', u'b', u'r', u'e'],
                          msg='queue items not listed in fifo order as expected')
 
         # lifo list
         ret = self.run_run_plus(
             'queue.list_items',
             'testqueue',
-            mode='fifo')
-        # ensure fifo output
-        self.assertEqual(ret,
-                         ['e', 'r', 'b', 'z', 'a'],
+            mode='lifo')
+        # ensure lifo output
+        self.assertEqual(ret['return'],
+                         [u'e', u'r', u'b', u'z', u'a'],
                          msg='queue items not listed in lifo order as expected')
 
         # delete items from queue
         ret = self.run_run_plus(
-            'queue.delete'
+            'queue.delete',
             'testqueue',
             ['a', 'z', 'b', 'r', 'e'])
-        self.assertTrue(ret, msg='Failure deleting items from test queue')
+        self.assertTrue(ret['return'], msg='Failure deleting items from test queue')
 
     def test_queue_pop(self):
         """
         Test popping items from queue
         """
         ret = self.run_run_plus(
-            'queue.insert'
+            'queue.insert',
             'testqueue',
             ['z', 't', 'a', 'r', 'b', 'e'])
-        self.assertTrue(ret, msg='Failure inserting items into test queue')
+        self.assertTrue(ret['return'], msg='Failure inserting items into test queue')
 
         # default returns first item from a lexical sorted list
+        expected_return = [u'a']
         ret = self.run_run_plus(
             'queue.pop',
             'testqueue')
-        self.assertEqual(ret,
-                         'a',
-                         msg='Item returned from queue "{0}" not the expected item "{1}"'.format(ret, 'a'))
+        self.assertEqual(ret['return'],
+                         expected_return,
+                         msg='Item returned from queue "{0}" not the expected item "{1}"'.format(ret['return'], expected_return))
 
         # pop fifo
         ret = self.run_run_plus(
             'queue.pop',
             'testqueue',
             mode='fifo')
-        self.assertEqual(ret,
-                         'z',
-                         msg='Item returned from queue "{0}" not the expected item "{1}"'.format(ret, 'z'))
+        expected_return = [u'z']
+        self.assertEqual(ret['return'],
+                         expected_return,
+                         msg='Item returned from queue "{0}" not the expected item "{1}"'.format(ret['return'], expected_return))
 
         # pop lifo
         ret = self.run_run_plus(
             'queue.pop',
             'testqueue',
             mode='lifo')
-        self.assertEqual(ret,
-                         'e',
-                         msg='Item returned from queue "{0}" not the expected item "{1}"'.format(ret, 'e'))
+        expected_return = [u'e']
+        self.assertEqual(ret['return'],
+                         expected_return,
+                         msg='Item returned from queue "{0}" not the expected item "{1}"'.format(ret['return'], expected_return))
 
         # delete items from queue
         ret = self.run_run_plus(
-            'queue.delete'
+            'queue.delete',
             'testqueue',
             ['t', 'r', 'b'])
-        self.assertTrue(ret, msg='Failure deleting items from test queue')
+        self.assertTrue(ret['return'], msg='Failure deleting items from test queue')
 
     def test_queue_process(self):
         """
         Test processing queue items
         """
         ret = self.run_run_plus(
-            'queue.insert'
+            'queue.insert',
             'testqueue',
             ['z', 't', 'a', 'r', 'b', 'e'])
-        self.assertTrue(ret, msg='Failure inserting items into test queue')
+        self.assertTrue(ret['return'], msg='Failure inserting items into test queue')
 
         # process queue items, default order is lexically by item
         ret = self.run_run_plus(
             'queue.process_queue',
             'testqueue',
             2)
-        self.assertTrue('items' in ret, msg='Return does not include "items"')
-        self.assertEqual(ret['items'],
-                        ['a', 'b'],
-                        msg='Items returned from process queue runner not as expected')
+        self.assertTrue('items' in ret['return'], msg='Return does not include "items"')
+        self.assertEqual(ret['return']['items'],
+                        [u'a', u'b'],
+                        msg='Items returned from process queue runner not as expected (items returned "{0}")'.format(ret['return']['items']))
 
         # delete items from queue
         ret = self.run_run_plus(
-            'queue.delete'
+            'queue.delete',
             'testqueue',
             ['z', 't', 'r', 'e'])
-        self.assertTrue(ret, msg='Failure deleting items from test queue')
+        self.assertTrue(ret['return'], msg='Failure deleting items from test queue')
 
     def test_queue_process_all(self):
         """
         Test processing queue items
         """
         ret = self.run_run_plus(
-            'queue.insert'
+            'queue.insert',
             'testqueue',
             ['z', 't', 'a', 'r', 'b', 'e'])
-        self.assertTrue(ret, msg='Failure inserting items into test queue')
+        self.assertTrue(ret['return'], msg='Failure inserting items into test queue')
 
         # process queue items, default order is lexically by item
         ret = self.run_run_plus(
             'queue.process_queue',
             'testqueue',
             'all')
-        self.assertTrue('items' in ret, msg='Return does not include "items"')
-        self.assertEqual(ret['items'],
-                        ['a', 'b', 'e', 'r', 't', 'z'],
-                        msg='Items returned from process queue runner not as expected')
+        self.assertTrue('items' in ret['return'], msg='Return does not include "items"')
+        self.assertEqual(ret['return']['items'],
+                        [u'a', u'b', u'e', u'r', u't', u'z'],
+                        msg='Items returned from process queue runner not as expected (items returned "{0}")'.format(ret['return']['items']))
 
     def test_queue_process_fifo(self):
         """
         Test processing queue items
         """
         ret = self.run_run_plus(
-            'queue.insert'
+            'queue.insert',
             'testqueue',
             ['z', 't', 'a', 'r', 'b', 'e'])
-        self.assertTrue(ret, msg='Failure inserting items into test queue')
+        self.assertTrue(ret['return'], msg='Failure inserting items into test queue')
 
         # process queue items, default order is lexically by item
         ret = self.run_run_plus(
@@ -185,27 +189,27 @@ class ManageTest(ShellCase):
             'testqueue',
             2,
             mode='fifo')
-        self.assertTrue('items' in ret, msg='Return does not include "items"')
-        self.assertEqual(ret['items'],
-                        ['z', 't'],
-                        msg='Items returned from process queue runner not as expected')
+        self.assertTrue('items' in ret['return'], msg='Return does not include "items"')
+        self.assertEqual(ret['return']['items'],
+                        [u'z', u't'],
+                        msg='Items returned from process queue runner not as expected (items returned "{0}")'.format(ret['return']['items']))
 
         # delete items from queue
         ret = self.run_run_plus(
-            'queue.delete'
+            'queue.delete',
             'testqueue',
             ['a', 'b', 'r', 'e'])
-        self.assertTrue(ret, msg='Failure deleting items from test queue')
+        self.assertTrue(ret['return'], msg='Failure deleting items from test queue')
 
     def test_queue_process_lifo(self):
         """
         Test processing queue items
         """
         ret = self.run_run_plus(
-            'queue.insert'
+            'queue.insert',
             'testqueue',
             ['z', 't', 'a', 'r', 'b', 'e'])
-        self.assertTrue(ret, msg='Failure inserting items into test queue')
+        self.assertTrue(ret['return'], msg='Failure inserting items into test queue')
 
         # process queue items, default order is lexically by item
         ret = self.run_run_plus(
@@ -213,27 +217,27 @@ class ManageTest(ShellCase):
             'testqueue',
             2,
             mode='lifo')
-        self.assertTrue('items' in ret, msg='Return does not include "items"')
-        self.assertEqual(ret['items'],
-                        ['e', 'b'],
-                        msg='Items returned from process queue runner not as expected')
+        self.assertTrue('items' in ret['return'], msg='Return does not include "items"')
+        self.assertEqual(ret['return']['items'],
+                        [u'e', u'b'],
+                        msg='Items returned from process queue runner not as expected (items returned "{0}")'.format(ret['return']['items']))
 
         # delete items from queue
         ret = self.run_run_plus(
-            'queue.delete'
+            'queue.delete',
             'testqueue',
-            ['a', 'b', 'r', 'e'])
-        self.assertTrue(ret, msg='Failure deleting items from test queue')
+            ['z', 't', 'a', 'r'])
+        self.assertTrue(ret['return'], msg='Failure deleting items from test queue')
 
     def test_queue_process_fifo_all(self):
         """
         Test processing queue items
         """
         ret = self.run_run_plus(
-            'queue.insert'
+            'queue.insert',
             'testqueue',
             ['z', 't', 'a', 'r', 'b', 'e'])
-        self.assertTrue(ret, msg='Failure inserting items into test queue')
+        self.assertTrue(ret['return'], msg='Failure inserting items into test queue')
 
         # process queue items, default order is lexically by item
         ret = self.run_run_plus(
@@ -241,20 +245,20 @@ class ManageTest(ShellCase):
             'testqueue',
             'all',
             mode='fifo')
-        self.assertTrue('items' in ret, msg='Return does not include "items"')
-        self.assertEqual(ret['items'],
-                        ['z', 't', 'a', 'r', 'b', 'e'],
-                        msg='Items returned from process queue runner not as expected')
+        self.assertTrue('items' in ret['return'], msg='Return does not include "items"')
+        self.assertEqual(ret['return']['items'],
+                        [u'z', u't', u'a', u'r', u'b', u'e'],
+                        msg='Items returned from process queue runner not as expected (items returned "{0}")'.format(ret['return']['items']))
 
     def test_queue_process_lifo_all(self):
         """
         Test processing queue items
         """
         ret = self.run_run_plus(
-            'queue.insert'
+            'queue.insert',
             'testqueue',
             ['z', 't', 'a', 'r', 'b', 'e'])
-        self.assertTrue(ret, msg='Failure inserting items into test queue')
+        self.assertTrue(ret['return'], msg='Failure inserting items into test queue')
 
         # process queue items, default order is lexically by item
         ret = self.run_run_plus(
@@ -262,7 +266,7 @@ class ManageTest(ShellCase):
             'testqueue',
             'all',
             mode='lifo')
-        self.assertTrue('items' in ret, msg='Return does not include "items"')
-        self.assertEqual(ret['items'],
-                        ['e', 'b', 'r', 'a', 't', 'z'],
-                        msg='Items returned from process queue runner not as expected')
+        self.assertTrue('items' in ret['return'], msg='Return does not include "items"')
+        self.assertEqual(ret['return']['items'],
+                        [u'e', u'b', u'r', u'a', u't', u'z'],
+                        msg='Items returned from process queue runner not as expected (items returned "{0}")'.format(ret['return']['items']))
