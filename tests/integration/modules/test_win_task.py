96ad2ad35ceab0ce86ec9633dc7940320ca1fd85
twangboy <slee@saltstack.com>
2020-04-20 15:34:39 -0600

    Fix black

--- tests/integration/modules/test_win_task.py
+++ tests/integration/modules/test_win_task.py
@@ -1,25 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-from tests.support.case import ModuleCase
-from tests.support.unit import skipIf
-from tests.support.helpers import destructiveTest
-
 import salt.modules.win_task as task
-from salt.exceptions import CommandExecutionError
 import salt.utils.platform
+from salt.exceptions import CommandExecutionError
+from tests.support.case import ModuleCase
+from tests.support.helpers import destructiveTest
+from tests.support.unit import skipIf
 
 
-@skipIf(not salt.utils.platform.is_windows(), 'windows test only')
+@skipIf(not salt.utils.platform.is_windows(), "windows test only")
 class WinTasksTest(ModuleCase):
     """
     Tests for salt.modules.win_task.
     """
     @destructiveTest
     def test_adding_task_with_xml(self):
-        '''
+        """
         Test adding a task using xml
-        '''
+        """
         xml_text = r"""
         <Task version="1.2" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">
           <RegistrationInfo>
@@ -66,15 +65,18 @@ class WinTasksTest(ModuleCase):
           </Actions>
         </Task>
         """
-        self.assertEquals(self.run_function('task.create_task_from_xml', 'foo', xml_text=xml_text), True)
-        all_tasks = self.run_function('task.list_tasks')
-        self.assertIn('foo', all_tasks)
+        self.assertEquals(
+            self.run_function('task.create_task_from_xml', 'foo', xml_text=xml_text),
+            True,
+        )
+        all_tasks = self.run_function("task.list_tasks")
+        self.assertIn("foo", all_tasks)
 
     @destructiveTest
     def test_adding_task_with_invalid_xml(self):
-        '''
+        """
         Test adding a task using a malformed xml
-        '''
+        """
         xml_text = r"""<Malformed"""
         with self.assertRaises(CommandExecutionError):
             task.create_task_from_xml('foo', xml_text=xml_text)
