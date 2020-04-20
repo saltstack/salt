963198501765c52596720770bc722a82502356c3
David Hilton <david.hilton.p@gmail.com>
2020-04-20 06:38:20 -0600

    mark old nxos functions as deprecated

--- salt/modules/nxos.py
+++ salt/modules/nxos.py
@@ -101,6 +101,7 @@ from salt.exceptions import CommandExecutionError, NxosError, SaltInvocationErro
 from salt.ext import six
 from salt.utils.args import clean_kwargs
 from salt.utils.pycrypto import gen_hash
+from salt.utils.versions import warn_until
 
 __virtualname__ = "nxos"
 
@@ -226,6 +227,8 @@ def cmd(command, *args, **kwargs):
         salt '*' nxos.cmd show_run
         salt '*' nxos.cmd check_password username=admin password='$5$lkjsdfoi$blahblahblah' encrypted=True
     """
+    warn_until("Silicon", "'nxos.cmd COMMAND' is deprecated in favor of 'nxos.COMMAND'")
+
     for k in list(kwargs):
         if k.startswith("__pub_"):
             kwargs.pop(k)
@@ -385,6 +388,11 @@ def show(commands, raw_text=True, **kwargs):
         salt '*' nxos.show 'show bgp sessions ; show processes' raw_text=False
         salt 'regular-minion' nxos.show 'show interfaces' host=sw01.example.com username=test password=test
     """
+    warn_until(
+        "Silicon",
+        "'nxos.show commands' is deprecated in favor of 'nxos.sendline commands'",
+    )
+
     if not isinstance(raw_text, bool):
         msg = """
         INPUT ERROR: Second argument 'raw_text' must be either True or False
@@ -450,6 +458,7 @@ def system_info(**kwargs):
 
         salt '*' nxos.system_info
     """
+    warn_until("Silicon", "'nxos.system_info' is deprecated in favor of 'nxos.grains'")
     return salt.utils.nxos.system_info(show_ver(**kwargs))["nxos"]
 
 
@@ -475,6 +484,11 @@ def add_config(lines, **kwargs):
     .. note::
         For more than one config added per command, lines should be a list.
     """
+    warn_until(
+        "Silicon",
+        "'nxos.add_config lines' is deprecated in favor of 'nxos.config commands'",
+    )
+
     kwargs = clean_kwargs(**kwargs)
     return config(lines, **kwargs)
 
