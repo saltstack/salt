492bdfcfb7d8d9b2e668d0117e8a47c7c9a4dce7
twangboy <slee@saltstack.com>
2020-04-22 12:18:26 -0600

    Fix failing tests

--- tests/unit/modules/test_win_system.py
+++ tests/unit/modules/test_win_system.py
@@ -317,11 +317,11 @@ class WinSystemTestCase(TestCase, LoaderModuleMockMixin):
             win_system, "get_computer_name", MagicMock(return_value="salt")
         ):
             reg_mock = MagicMock(return_value={"vdata": "salt"})
-            with patch.dict(win_system.__salt__, {"reg.read_value": reg_mock}):
+            with patch.dict(win_system.__utils__, {"reg.read_value": reg_mock}):
                 self.assertFalse(win_system.get_pending_computer_name())
 
             reg_mock = MagicMock(return_value={"vdata": "salt_pending"})
-            with patch.dict(win_system.__salt__, {"reg.read_value": reg_mock}):
+            with patch.dict(win_system.__utils__, {"reg.read_value": reg_mock}):
                 self.assertEqual(win_system.get_pending_computer_name(), "salt_pending")
 
     @skipIf(not win_system.HAS_WIN32NET_MODS, "Missing win32 libraries")
