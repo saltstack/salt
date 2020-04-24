16fec93ef50ba62376f96128dbd4e2153106fbbf
Score_Under <seejay.11@gmail.com>
2020-04-24 18:26:49 +0100

    Add unit test for repo up-to-date message

--- tests/unit/states/test_git.py
+++ tests/unit/states/test_git.py
@@ -73,3 +73,86 @@ class GitTestCase(TestCase, LoaderModuleMockMixin):
             )
             assert result["result"] is True, result
             git_diff.assert_not_called()
+
+    def test_uptodate_message_format_gen(self):
+        fetch_url = "https://xyz.invalid/salt/unit/test/repo.git"
+        local_path = "/local/salt/unit/test/repo.git"
+        branch = "master"
+        head_rev = "cafe0000cafe0000cafe0000cafe0000cafe0000"
+        remote_name = "salt_test_remote"
+
+        tags = []
+        remote_refs = {
+            "HEAD": head_rev,
+            "refs/heads/" + branch: head_rev
+        }
+
+        def rev_parse(cwd, rev, *args, **kwargs):
+            assert cwd == local_path
+
+            if "--abbrev-ref" in kwargs.get("opts", []) and rev.endswith("@{upstream}"):
+                return remote_name + "/" + rev.split("@")[0]
+
+            rev = rev.split("^")[0]
+            return remote_refs.get(rev) \
+                or remote_refs.get("refs/heads/" + rev) \
+                or rev
+
+        def ls_remote(cwd, remote, opts, *args, **kwargs):
+            assert cwd == local_path
+            assert remote in [remote_name, fetch_url]
+
+            if opts == "--tags":
+                return tags
+            raise NotImplementedError()
+
+        def merge_base(cwd, refs, is_ancestor=False, **kwargs):
+            assert cwd == local_path
+            assert is_ancestor
+
+            return all(ref == refs[0] for ref in refs)
+
+        dunder_salt = {
+            "git.config_get_regexp": MagicMock(return_value={}),
+            "git.remote_refs": MagicMock(return_value=remote_refs),
+            "git.version": MagicMock(return_value="1.8.3.1"),
+            "git.list_branches": MagicMock(return_value=[branch]),
+            "git.list_tags": MagicMock(return_value=tags),
+            "git.revision": MagicMock(return_value=head_rev),
+            "git.current_branch": MagicMock(return_value=branch),
+            "git.remotes": MagicMock(return_value={
+                remote_name: {
+                    "fetch": fetch_url,
+                },
+            }),
+            "git.diff": MagicMock(return_value=""),
+            "git.rev_parse": MagicMock(side_effect=rev_parse),
+            "git.ls_remote": MagicMock(side_effect=ls_remote),
+            "git.fetch": MagicMock(return_value=True),
+            "git.merge_base": MagicMock(side_effect=merge_base),
+        }
+
+        # Things that would interact with FS on test host
+        isfile_mock = MagicMock(return_value=False)
+        # expanduser reads home directory from system
+        expanduser_mock = MagicMock(side_effect=NotImplementedError)
+        isdir_mock = MagicMock(return_value=True)
+
+        with patch.dict(git_state.__salt__, dunder_salt), \
+                patch("os.path.isfile", isfile_mock), \
+                patch("os.path.expanduser", expanduser_mock), \
+                patch("os.path.isdir", isdir_mock):
+            result = git_state.latest(
+                fetch_url, rev=branch, remote=remote_name, target=local_path
+            )
+            assert result["result"] is True, result
+            # Ensure the comment is formatted correctly. Namely that the repo
+            # path is not mangled.
+            self.assertIn(
+                "Repository {repo} is up-to-date".format(repo=local_path),
+                result["comment"],
+            )
+            self.assertIn(
+                "{repo} was fetched, resulting in updated refs".format(repo=fetch_url),
+                result["comment"],
+            )
