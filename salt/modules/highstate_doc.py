ca5ea34d0f92fc28adc5fd0be4219beb9b247563
Benjamin Drung <benjamin.drung@cloud.ionos.com>
2020-04-29 14:11:31 +0200

    Fix spelling mistake of performance
    Signed-off-by: Benjamin Drung <benjamin.drung@cloud.ionos.com>

--- salt/modules/highstate_doc.py
+++ salt/modules/highstate_doc.py
@@ -59,7 +59,7 @@ State example
 
 
 To create the help document build a State that uses `highstate_doc.render`.
-For preformance it's advised to not included this state in your `top.sls` file.
+For performance it's advised to not included this state in your `top.sls` file.
 
 .. code-block:: yaml
 
