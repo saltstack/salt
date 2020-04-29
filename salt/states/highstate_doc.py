0cd04786b1e133b12c2861353cba140a12e1e980
Benjamin Drung <benjamin.drung@cloud.ionos.com>
2020-04-29 14:11:29 +0200

    Fix spelling mistake of above
    Signed-off-by: Benjamin Drung <benjamin.drung@cloud.ionos.com>

--- salt/states/highstate_doc.py
+++ salt/states/highstate_doc.py
@@ -28,7 +28,7 @@ def note(name, source=None, contents=None, **kwargs):
                     ------------------
                     This state does not do anything to the system! It is only used by a `proccesser`
                     you can use `requisites` and `order` to move your docs around the rendered file.
-                    .. this message appare aboce the `pkg: somepackage` state.
+                    .. this message appare above the `pkg: somepackage` state.
                 - source: salt://{{tpldir}}/also_include_a_file.md
 
         {{sls}} extra help:
