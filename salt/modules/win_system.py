2adfadb416ef9833bf93c93b829def6a58e0c0ed
twangboy <slee@saltstack.com>
2020-05-07 13:18:54 -0600

    Backport #53287
    I don't know why this isn't in master. It was merged into 2019.2.1.
    Something must have gotten missed in the merge forward

--- salt/modules/win_system.py
+++ salt/modules/win_system.py
@@ -598,7 +598,10 @@ def get_system_info():
             "os_version": system.Version,
             "windows_directory": system.WindowsDirectory,
         }
-
+        # Must get chassis_sku_number this way for backwards compatibility
+        # system.ChassisSKUNumber is only available on Windows 10/2016 and newer
+        product = conn.Win32_ComputerSystemProduct()[0]
+        ret.update({"chassis_sku_number": product.SKUNumber})
         system = conn.Win32_ComputerSystem()[0]
         # Get pc_system_type depending on Windows version
         if platform.release() in ["Vista", "7", "8"]:
@@ -613,7 +616,6 @@ def get_system_info():
                 "bootup_state": system.BootupState,
                 "caption": system.Caption,
                 "chassis_bootup_state": warning_states[system.ChassisBootupState],
-                "chassis_sku_number": system.ChassisSKUNumber,
                 "dns_hostname": system.DNSHostname,
                 "domain": system.Domain,
                 "domain_role": domain_role[system.DomainRole],
@@ -645,7 +647,12 @@ def get_system_info():
             ret["processors"] += 1
             ret["processors_logical"] += processor.NumberOfLogicalProcessors
             ret["processor_cores"] += processor.NumberOfCores
-            ret["processor_cores_enabled"] += processor.NumberOfEnabledCore
+            try:
+                ret["processor_cores_enabled"] += processor.NumberOfEnabledCore
+            except (AttributeError, TypeError):
+                pass
+        if ret["processor_cores_enabled"] == 0:
+            ret.pop("processor_cores_enabled", False)
 
         bios = conn.Win32_BIOS()[0]
         ret.update(
