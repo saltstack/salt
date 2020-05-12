8c0bd63fa0300c7d8cdca0964aa5059ff2f3bbc8
Lee Clemens <java@leeclemens.net>
2020-05-12 19:43:13 +0300

    Port #47741 to master

--- salt/modules/yumpkg.py
+++ salt/modules/yumpkg.py
@@ -26,6 +26,8 @@ import os
 import re
 import string
 
+import salt.defaults.exitcodes
+
 # Import Salt libs
 import salt.utils.args
 import salt.utils.data
@@ -3307,3 +3309,85 @@ def list_installed_patches():
         salt '*' pkg.list_installed_patches
     """
     return _get_patches(installed_only=True)
+
+
+@salt.utils.decorators.path.which("yum-complete-transaction")
+def complete_transaction(cleanup_only=False, recursive=False, max_attempts=3):
+    """
+    .. versionadded:: Fluorine
+
+    Execute ``yum-complete-transaction``, which is provided by the ``yum-utils`` package.
+
+    cleanup_only
+        Specify if the ``--cleanup-only`` option should be supplied.
+
+    recursive
+        Specify if ``yum-complete-transaction`` should be called recursively
+        (it only completes one transaction at a time).
+
+    max_attempts
+        If ``recursive`` is ``True``, the maximum times ``yum-complete-transaction`` should be called.
+
+    .. note::
+
+        Recursive calls will stop once ``No unfinished transactions left.`` is in the returned output.
+
+    .. note::
+
+        ``yum-utils`` will already be installed on the minion if the package
+        was installed from the Fedora / EPEL repositories.
+
+    CLI example:
+
+    .. code-block:: bash
+
+        salt '*' pkg.complete_transaction
+        salt '*' pkg.complete_transaction cleanup_only=True
+        salt '*' pkg.complete_transaction recursive=True max_attempts=5
+    """
+
+    return _complete_transaction(cleanup_only, recursive, max_attempts, 1, [])
+
+
+def _complete_transaction(
+    cleanup_only, recursive, max_attempts, run_count, cmd_ret_list
+):
+    """
+    .. versionadded:: Fluorine
+
+    Called from ``complete_transaction`` to protect the arguments
+    used for tail recursion, ``run_count`` and ``cmd_ret_list``.
+    """
+
+    cmd = ["yum-complete-transaction"]
+    if cleanup_only:
+        cmd.append("--cleanup-only")
+
+    cmd_ret_list.append(
+        __salt__["cmd.run_all"](cmd, output_loglevel="trace", python_shell=False)
+    )
+
+    if (
+        cmd_ret_list[-1]["retcode"] == salt.defaults.exitcodes.EX_OK
+        and recursive
+        and "No unfinished transactions left." not in cmd_ret_list[-1]["stdout"]
+    ):
+        if run_count >= max_attempts:
+            cmd_ret_list[-1]["retcode"] = salt.defaults.exitcodes.EX_GENERIC
+            log.error(
+                "Attempt %s/%s exceeded `max_attempts` for command: `%s`",
+                run_count,
+                max_attempts,
+                " ".join(cmd),
+            )
+            raise CommandExecutionError(
+                "The `max_attempts` limit was reached and unfinished transactions remain."
+                " You may wish to increase `max_attempts` or re-execute this module.",
+                info={"results": cmd_ret_list},
+            )
+        else:
+            return _complete_transaction(
+                cleanup_only, recursive, max_attempts, run_count + 1, cmd_ret_list
+            )
+
+    return cmd_ret_list
