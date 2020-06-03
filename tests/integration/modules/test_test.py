92ec44bb180f438f9903d32b69d3b9e1b6fc982a
Gareth J. Greenaway <gareth@saltstack.com>
2020-06-03 10:04:40 -0700

    Moving test from integration to unit, moving from testing test.versions to salt.version functionality.

--- tests/integration/modules/test_test.py
+++ tests/integration/modules/test_test.py
@@ -10,8 +10,6 @@ import salt.version
 from tests.support.case import ModuleCase
 from tests.support.helpers import slowTest
 from tests.support.mixins import AdaptedConfigurationTestCaseMixin
-from tests.support.mock import MagicMock, patch
-from tests.support.unit import skipIf
 
 log = logging.getLogger(__name__)
 
@@ -89,59 +87,3 @@ class TestModuleTest(ModuleCase, AdaptedConfigurationTestCaseMixin):
         test.outputter
         """
         self.assertEqual(self.run_function("test.outputter", ["text"]), "text")
-
-    @slowTest
-    @skipIf(not salt.utils.platform.is_linux(), "linux test only")
-    def test_versions_version_linux(self):
-        """
-        test.versions on Linux
-        """
-
-        with patch(
-            "distro.linux_distribution",
-            MagicMock(return_value=("Manjaro Linux", "20.0.2", "Lysia")),
-        ):
-            versions = self.run_function("test.versions")
-            version = "version: Manjaro Linux 20.0.2 Lysia"
-            self.assertIn(version, versions)
-
-        with patch(
-            "distro.linux_distribution",
-            MagicMock(return_value=("Debian GNU/Linux", "9", "stretch")),
-        ):
-            versions = self.run_function("test.versions")
-            version = "version: Debian GNU/Linux 9 stretch"
-            self.assertIn(version, versions)
-
-    @slowTest
-    @skipIf(not salt.utils.platform.is_darwin(), "OS X test only")
-    def test_versions_version_osx(self):
-        """
-        test.versions on OS X
-        """
-
-        with patch(
-            "platform.mac_ver",
-            MagicMock(return_value=("10.15.2", ("", "", ""), "x86_64")),
-        ):
-            versions = self.run_function("test.versions")
-            version = "version: 10.15.2 x86_64"
-            self.assertIn(version, versions)
-
-    @slowTest
-    @skipIf(not salt.utils.platform.is_windows(), "windows test only")
-    def test_versions_version_windows(self):
-        """
-        test.versions on Windows
-        """
-
-        with patch(
-            "platform.win32_ver",
-            return_value=("10", "10.0.14393", "SP0", "Multiprocessor Free"),
-        ), patch("win32api.RegOpenKey", MagicMock()), patch(
-            "win32api.RegQueryValueEx",
-            MagicMock(return_value=("Windows Server 2016 Datacenter", 1)),
-        ):
-            versions = self.run_function("test.versions")
-            version = "version: 2016Server 10.0.14393 SP0 Multiprocessor Free"
-            self.assertIn(version, versions)
