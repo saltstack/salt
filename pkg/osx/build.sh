71bdd897cd775bd2ad4e02adf95636a36ed4f458
Wesley Whetstone <wes@squareup.com>
2020-06-09 12:21:23 -0700

    remove CFLAGS var and add an option to skip the --enable-optimizations flag of python for a faster test build

--- pkg/osx/build.sh
+++ pkg/osx/build.sh
@@ -19,16 +19,19 @@
 #         $1 : <version> : the version of salt to build
 #                          (a git tag, not a branch)
 #                          (defaults to git-repo state)
-#         $2 : <package dir> : the staging area for the package
+#         $2 : <test mode> : if this script should be run in test mode, this
+#                            disables the longer optimized compile time of python.
+#                            Please DO NOT set to "true" when building a
+#                            release version.
+#                            (defaults to false)
+#         $3 : <package dir> : the staging area for the package
 #                              defaults to /tmp/salt_pkg
-#         $3 : <test mode> : (true) if this script should be run in test mode,
-#                             this disable the longer compile time of python
 #
 #     Example:
-#         The following will build Salt v3001 and
+#         The following will build Salt v3001 with an optimized python and
 #         stage all files in /tmp/custom_pkg:
 #
-#         ./build.sh v3001 /tmp/custom_pkg
+#         ./build.sh v3001 false /tmp/custom_pkg
 #
 ############################################################################
 
@@ -39,7 +42,7 @@ if [[ $(id -u) -ne 0 ]]
     then
         exec sudo /bin/bash -c "$(printf '%q ' "$BASH_SOURCE" "$@")"
 fi
-
+set -x
 ############################################################################
 # Check passed parameters, set defaults
 ############################################################################
@@ -52,6 +55,12 @@ else
 fi
 
 if [ "$2" == "" ]; then
+    TEST_MODE="false"
+else
+    TEST_MODE=$2
+fi
+
+if [ "$3" == "" ]; then
     PKGDIR=/tmp/salt_pkg
 else
     PKGDIR=$2
@@ -79,7 +88,7 @@ fi
 # Create the Build Environment
 ############################################################################
 echo -n -e "\033]0;Build: Build Environment\007"
-$PKGRESOURCES/build_env.sh
+$PKGRESOURCES/build_env.sh $TEST_MODE
 if [[ "$?" != "0" ]]; then
     echo "Failed to build the environment."
     exit -1
