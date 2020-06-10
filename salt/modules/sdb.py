885e8763ec6183435962525f0ccd308bef620f83
Proskurin Kirill <oloremo@gmail.com>
2020-06-10 23:21:38 +0100

    Added strcit argument to sdb.get

--- salt/modules/sdb.py
+++ salt/modules/sdb.py
@@ -13,18 +13,18 @@ __func_alias__ = {
 }
 
 
-def get(uri):
+def get(uri, strict=False):
     """
-    Get a value from a db, using a uri in the form of sdb://<profile>/<key>. If
-    the uri provided does not start with sdb://, then it will be returned as-is.
+    Get a value from a db, using a uri in the form of ``sdb://<profile>/<key>``. If
+    the uri provided is not valid, then it will be returned as-is, unless ``strict=True`` was passed.
 
     CLI Example:
 
     .. code-block:: bash
 
-        salt '*' sdb.get sdb://mymemcached/foo
+        salt '*' sdb.get sdb://mymemcached/foo strict=True
     """
-    return salt.utils.sdb.sdb_get(uri, __opts__, __utils__)
+    return salt.utils.sdb.sdb_get(uri, __opts__, __utils__, strict)
 
 
 def set_(uri, value):
