89ec0684dd9d9ea0b576849d617e7e139a5216e2
Erik Johnson <palehose@gmail.com>
2020-06-11 09:19:30 -0500

    Remove comment referencing _supported_dists from salt/grains/core.py
    As this has been removed in favor of `distro.linux_distribution()`, the
    comment is no longer accurate.

--- salt/grains/core.py
+++ salt/grains/core.py
@@ -1494,9 +1494,11 @@ def id_():
 _REPLACE_LINUX_RE = re.compile(r"\W(?:gnu/)?linux", re.IGNORECASE)
 
 # This maps (at most) the first ten characters (no spaces, lowercased) of
-# 'osfullname' to the 'os' grain that Salt traditionally uses.
-# Please see os_data() and _supported_dists.
-# If your system is not detecting properly it likely needs an entry here.
+# 'osfullname' to the 'os' grain that Salt traditionally uses, and is used by
+# the os_data() function to create the "os" grain.
+#
+# If your system is not detecting the "os" grain properly, it likely needs an
+# entry in this dictionary.
 _OS_NAME_MAP = {
     "redhatente": "RedHat",
     "gentoobase": "Gentoo",
