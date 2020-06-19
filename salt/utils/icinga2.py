83a6424fcc147ea9e28ec3f37e81e863559e1d99
gasmick <mike.t.c.b@gmail.com>
2020-06-19 14:54:48 +0200

    Fix broken things in salt/utils/icinga2.py
    Fixes #57713
    
    The Icinga2 version number was not properly extracted, it doesn't necessarily contain the letter r.

--- salt/utils/icinga2.py
+++ salt/utils/icinga2.py
@@ -26,7 +26,7 @@ def get_certs_path():
     icinga2_output = __salt__["cmd.run_all"](
         [salt.utils.path.which("icinga2"), "--version"], python_shell=False
     )
-    version = re.search(r"r\d+\.\d+", icinga2_output["stdout"]).group(0)
+    version = re.search(r"\d+\.\d+", icinga2_output["stdout"]).group(0)
     # Return new certs path for icinga2 >= 2.8
     if int(version.split(".")[1]) >= 8:
         return "/var/lib/icinga2/certs/"
