48c9d7c36be63dafe3c2ada903da47f1faf5c734
Gareth J. Greenaway <gareth@saltstack.com>
2020-06-21 17:50:59 -0700

    moving import salt.modules.vsphere into virtual so we have access to test proxytype in opts.  Gating the check with salt.utils.proxy.is_proxytype.  Adding tests.

---
+++ salt/utils/proxy.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Utils for proxy.
+"""
+
+# Import Python libs
+from __future__ import absolute_import, print_function, unicode_literals
+
+import logging
+
+import salt.utils.platform
+
+log = logging.getLogger(__file__)
+
+
+def is_proxytype(opts, proxytype):
+    """
+    Is this a proxy minion of type proxytype
+    """
+    return (
+        salt.utils.platform.is_proxy()
+        and opts.get("proxy", None).get("proxytype", None) == proxytype
+    )
