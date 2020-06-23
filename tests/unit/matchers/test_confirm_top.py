9668ad4a694cf4e0e46e18ac1a74cf2464c9aa78
Ali Rizvi-Santiago <arizvisa@gmail.com>
2020-06-23 12:39:56 -0500

    Fixed some lint issues in the tests.unit.matchers.confirm_top unit test.

--- tests/unit/matchers/test_confirm_top.py
+++ tests/unit/matchers/test_confirm_top.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # Import python libs
 from __future__ import absolute_import, print_function, unicode_literals
 
@@ -12,7 +14,7 @@ class ConfirmTop(TestCase):
     def setUp(self):
         opts = salt.config.DEFAULT_MINION_OPTS.copy()
         self.matchers = salt.loader.matchers(opts)
-        
+
     def test_sanity(self):
         match = self.matchers['confirm_top.confirm_top']
         self.assertTrue(match('*', []))
