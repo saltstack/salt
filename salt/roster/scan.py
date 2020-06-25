c57a946b29b2601bbe86a01716e51a054c292f2f
Dmitry Kuzmenko <dmitry.kuzmenko@dsr-corporation.com>
2020-06-25 18:37:10 +0300

    Added list target type support to the `scan` roster.

--- salt/roster/scan.py
+++ salt/roster/scan.py
@@ -50,26 +50,31 @@ class RosterMatcher(object):
         if not isinstance(ports, list):
             # Comma-separate list of integers
             ports = list(map(int, six.text_type(ports).split(",")))
-        try:
-            addrs = [ipaddress.ip_address(self.tgt)]
-        except ValueError:
+        if self.tgt_type == "list":
+            tgts = self.tgt
+        else:
+            tgts = [self.tgt]
+        for tgt in tgts:
             try:
-                addrs = ipaddress.ip_network(self.tgt).hosts()
+                addrs = [ipaddress.ip_address(tgt)]
             except ValueError:
-                pass
-        for addr in addrs:
-            addr = six.text_type(addr)
-            ret[addr] = copy.deepcopy(__opts__.get("roster_defaults", {}))
-            log.trace("Scanning host: %s", addr)
-            for port in ports:
-                log.trace("Scanning port: %s", port)
                 try:
-                    sock = salt.utils.network.get_socket(addr, socket.SOCK_STREAM)
-                    sock.settimeout(float(__opts__["ssh_scan_timeout"]))
-                    sock.connect((addr, port))
-                    sock.shutdown(socket.SHUT_RDWR)
-                    sock.close()
-                    ret[addr].update({"host": addr, "port": port})
-                except socket.error:
+                    addrs = ipaddress.ip_network(tgt).hosts()
+                except ValueError:
                     pass
+            for addr in addrs:
+                addr = six.text_type(addr)
+                ret[addr] = copy.deepcopy(__opts__.get("roster_defaults", {}))
+                log.trace("Scanning host: %s", addr)
+                for port in ports:
+                    log.trace("Scanning port: %s", port)
+                    try:
+                        sock = salt.utils.network.get_socket(addr, socket.SOCK_STREAM)
+                        sock.settimeout(float(__opts__["ssh_scan_timeout"]))
+                        sock.connect((addr, port))
+                        sock.shutdown(socket.SHUT_RDWR)
+                        sock.close()
+                        ret[addr].update({"host": addr, "port": port})
+                    except socket.error:
+                        pass
         return ret
