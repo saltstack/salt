8c508ac36f3e817a84b4e0c39b71690adc867c7c
Gareth J. Greenaway <gareth@saltstack.com>
2020-06-26 17:33:48 -0700

    Adding a test for digitalocean changes.

---
+++ tests/unit/cloud/clouds/test_digitalocean.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+    :codeauthor: `Gareth J. Greenaway <gareth@saltstack.com>`
+
+    tests.unit.cloud.clouds.digitalocean_test
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+
+# Import Python libs
+from __future__ import absolute_import, print_function, unicode_literals
+
+import logging
+
+# Import Salt Libs
+from salt.cloud.clouds import digitalocean
+from salt.exceptions import SaltCloudSystemExit
+from tests.support.unit import TestCase
+
+log = logging.getLogger(__name__)
+
+
+class DigitalOceanTestCase(TestCase):
+    """
+    Unit TestCase for salt.cloud.clouds.digitalocean module.
+    """
+
+    def test_reboot_no_call(self):
+        """
+        Tests that a SaltCloudSystemExit is raised when
+        kwargs that are provided do not include an action.
+        """
+        self.assertRaises(
+            SaltCloudSystemExit, digitalocean.reboot, name="fake_name",
+        )
+
+        with self.assertRaises(SaltCloudSystemExit) as excinfo:
+            ret = digitalocean.reboot(name="fake_name")
+        self.assertEqual(
+            "The reboot action must be called with -a or --action.",
+            excinfo.exception.strerror,
+        )
