23b85790e40bbbfccf28df7d8123336c13e2300d
Gareth J. Greenaway <gareth@saltstack.com>
2020-07-06 08:05:42 -0700

    Requested changes.  Additional test for the False state.

--- tests/unit/utils/test_proxy.py
+++ tests/unit/utils/test_proxy.py
@@ -4,19 +4,15 @@
     :codeauthor: :email:`Gareth J. Greenaway <gareth@saltstack.com>`
 """
 
-# Import python libs
 from __future__ import absolute_import, print_function, unicode_literals
 
-# Import Salt libs
 import salt.utils.proxy
 from tests.support.mock import patch
-
-# Import Salt Testing libs
 from tests.support.unit import TestCase
 
 
 class ProxyUtilsTestCase(TestCase):
-    def test_is_proxytype(self):
+    def test_is_proxytype_true(self):
         opts = {
             "proxy": {
                 "proxytype": "esxi",
@@ -29,3 +25,17 @@ class ProxyUtilsTestCase(TestCase):
         with patch("salt.utils.platform.is_proxy", return_value=True, autospec=True):
             ret = salt.utils.proxy.is_proxytype(opts, "esxi")
             self.assertTrue(ret)
+
+    def test_is_proxytype_false(self):
+        opts = {
+            "proxy": {
+                "proxytype": "esxi",
+                "host": "esxi.domain.com",
+                "username": "username",
+                "passwords": ["password1"],
+            }
+        }
+
+        with patch("salt.utils.platform.is_proxy", return_value=True, autospec=True):
+            ret = salt.utils.proxy.is_proxytype(opts, "docker")
+            self.assertFalse(ret)
