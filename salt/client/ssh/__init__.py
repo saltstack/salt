f3fc2da219c1c8dcf528c10a49946e1fe2076976
Gareth J. Greenaway <gareth@saltstack.com>
2020-07-23 13:48:58 -0700

    ensure master_tops is available.

--- salt/client/ssh/__init__.py
+++ salt/client/ssh/__init__.py
@@ -1133,6 +1133,7 @@ class Single(object):
             opts_pkg["module_dirs"] = self.opts["module_dirs"]
             opts_pkg["_ssh_version"] = self.opts["_ssh_version"]
             opts_pkg["thin_dir"] = self.opts["thin_dir"]
+            opts_pkg["master_tops"] = self.opts["master_tops"]
             opts_pkg["__master_opts__"] = self.context["master_opts"]
             if "known_hosts_file" in self.opts:
                 opts_pkg["known_hosts_file"] = self.opts["known_hosts_file"]
