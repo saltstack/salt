962fc679812a36bc1f41b2ea0c75aedf46ecdc24
Gareth J. Greenaway <gareth@saltstack.com>
2020-07-28 13:05:16 -0700

    Updating integration tests to reflect changes when diffinng and merge_if_exists is enabled.

--- tests/integration/states/test_file.py
+++ tests/integration/states/test_file.py
@@ -2011,6 +2011,12 @@ class FileTest(ModuleCase, SaltReturnAssertsMixin):
         )
         ret = ret[next(iter(ret))]
         assert ret["result"], ret
+        assert "changes" in ret
+        assert "diff" in ret["changes"]
+        assert "foo" in ret["changes"]["diff"]
+        assert "abc" in ret["changes"]["diff"]["foo"]
+        assert "new" in ret["changes"]["diff"]["foo"]["abc"]
+        assert ret["changes"]["diff"]["foo"]["abc"]["new"], 123
 
         with salt.utils.files.fopen(name) as fp_:
             serialized_data = salt.serializers.configparser.deserialize(fp_)
@@ -2057,6 +2063,12 @@ class FileTest(ModuleCase, SaltReturnAssertsMixin):
         )
         ret = ret[next(iter(ret))]
         assert ret["result"], ret
+        assert "changes" in ret
+        assert "diff" in ret["changes"]
+        assert "foo" in ret["changes"]["diff"]
+        assert "abc" in ret["changes"]["diff"]["foo"]
+        assert "new" in ret["changes"]["diff"]["foo"]["abc"]
+        assert ret["changes"]["diff"]["foo"]["abc"]["new"], 123
 
         with salt.utils.files.fopen(name, "rb") as fp_:
             serialized_data = salt.serializers.plist.deserialize(fp_)
@@ -2090,6 +2102,12 @@ class FileTest(ModuleCase, SaltReturnAssertsMixin):
         )
         ret = ret[next(iter(ret))]
         assert ret["result"], ret
+        assert "changes" in ret
+        assert "diff" in ret["changes"]
+        assert "foo" in ret["changes"]["diff"]
+        assert "abc" in ret["changes"]["diff"]["foo"]
+        assert "new" in ret["changes"]["diff"]["foo"]["abc"]
+        assert ret["changes"]["diff"]["foo"]["abc"]["new"], 123
 
         with salt.utils.files.fopen(name, "rb") as fp_:
             serialized_data = salt.serializers.plist.deserialize(fp_)
