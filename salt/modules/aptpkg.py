641a49003e04a5a063e8b29a9cce4cec7e29e68a
Anthony Arnaud <github@anthony-arnaud.fr>
2020-07-28 20:06:39 -0400

    fix 46902: wait dpkg lock (#52378)
    * module.aptpkg: retry if apt/dpkg is already running
    
    Signed-off-by: Anthony ARNAUD <github@anthony-arnaud.fr>
    
    * Adding a test to test handling of scenario when dpkg is locked.
    
    Co-authored-by: Daniel A. Wozniak <dwozniak@saltstack.com>
    Co-authored-by: Gareth J. Greenaway <gareth@wiked.org>
    Co-authored-by: Gareth J. Greenaway <gareth@saltstack.com>

--- salt/modules/aptpkg.py
+++ salt/modules/aptpkg.py
@@ -186,7 +186,14 @@ def _call_apt(args, scope=True, **kwargs):
     }
     params.update(kwargs)
 
-    return __salt__["cmd.run_all"](cmd, **params)
+    cmd_ret = __salt__["cmd.run_all"](cmd, **params)
+    count = 0
+    while "Could not get lock" in cmd_ret.get("stderr", "") and count < 10:
+        count += 1
+        log.warning("Waiting for dpkg lock release: retrying... %s/100", count)
+        time.sleep(2 ** count)
+        cmd_ret = __salt__["cmd.run_all"](cmd, **params)
+    return cmd_ret
 
 
 def _warn_software_properties(repo):
