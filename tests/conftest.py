6e2d25665b3b612208ab2339a29c23995685cd52
Pedro Algarvio <pedro@algarvio.me>
2020-08-05 16:58:59 +0100

    Migrate proxy tests to PyTest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1306,8 +1306,12 @@ def reap_stray_processes():
 
 
 @pytest.fixture(scope="session")
-def grains(request):
-    sminion = create_sminion()
+def sminion():
+    return create_sminion()
+
+
+@pytest.fixture(scope="session")
+def grains(sminion):
     return sminion.opts["grains"].copy()
 
 
