0172cefdf612db2f6e348696cd6dbeebbcf528d8
Jasper Lievisse Adriaanse <j@jasper.la>
2020-08-06 17:03:22 +0200

    add tests for html.escape

--- tests/pytests/unit/returners/test_nagios_nrdp_return.py
+++ tests/pytests/unit/returners/test_nagios_nrdp_return.py
@@ -8,16 +8,33 @@ def test_prepare_xml():
     service = "salt-minion"
 
     opts = {
-        "service": service,
         "hostname": hostname,
+        "service": service,
         "checktype": "active",
     }
-    prepared_xml = nagios_nrdp_return._prepare_xml(options=opts)
-    root = ET.fromstring(prepared_xml)
+
+    xml_ret = nagios_nrdp_return._prepare_xml(options=opts)
+    root = ET.fromstring(xml_ret)
 
     checkresult = root.find("checkresult")
     hostname_res = checkresult.find("hostname").text
     servicename_res = checkresult.find("servicename").text
 
+    # Verify the regular XML format.
     assert servicename_res == service
     assert hostname_res == hostname
+
+
+def test_escaped_xml():
+    opts = {
+        "hostname": "s&lt",
+        "output": 'output"',
+        "service": "salt-<minion>",
+        "checktype": "active",
+    }
+
+    xml_ret = nagios_nrdp_return._prepare_xml(options=opts)
+
+    assert "s&amp;lt" in xml_ret
+    assert "salt-&lt;minion&gt;" in xml_ret
+    assert "output&quot;" in xml_ret
