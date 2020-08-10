af4ff0446bb0d63c6e52fa152f894cf05d5246c6
RemiChristiaan <remi-christiaan.cool@vbhi.com>
2020-08-10 13:46:55 +0200

    Fix: 'OrderedDict' object has no attribute 'startswith'
    Excluding an id when another state has a requisite depending on e.g.
    a file that would be created by that excluded id/state. An exception is raised.

--- salt/state.py
+++ salt/state.py
@@ -1807,19 +1807,17 @@ class State(object):
                                     else:
                                         found = False
                                         for _id in iter(high):
-                                            for state in [
-                                                state
-                                                for state in iter(high[_id])
-                                                if not state.startswith("__")
-                                            ]:
-                                                for j in iter(high[_id][state]):
-                                                    if (
-                                                        isinstance(j, dict)
-                                                        and "name" in j
-                                                    ):
-                                                        if j["name"] == ind:
-                                                            ind = {state: _id}
-                                                            found = True
+                                            for state in iter(high[_id]):
+                                                # When a requisite is tied to an excluded id, state is an ordered dict.
+                                                if hasattr(state, "startswith") and not state.startswith("__"):
+                                                    for j in iter(high[_id][state]):
+                                                        if (
+                                                                isinstance(j, dict)
+                                                                and "name" in j
+                                                        ):
+                                                            if j["name"] == ind:
+                                                                ind = {state: _id}
+                                                                found = True
                                         if not found:
                                             continue
                                 if len(ind) < 1:
