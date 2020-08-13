07c18c303ffa58739e82d1572982b68f96855a15
twangboy <slee@saltstack.com>
2020-08-12 18:05:43 -0600

    Add some tests

---
+++ tests/unit/config/test_config.py
@@ -0,0 +1,68 @@
+# Import Python Libs
+import os
+
+# Import Salt Libs
+import salt.config as config
+import salt.utils.yaml
+from salt.exceptions import SaltConfigurationError
+
+# Import Salt Testing Libs
+from tests.support.mock import patch, MagicMock
+from tests.support.unit import TestCase, skipIf
+
+
+class ConfigTestCase(TestCase):
+
+    def test__read_conf_file_invalid_yaml__schedule_conf(self):
+        """
+        If ``_schedule.conf`` is an invalid file a YAMLError will be thrown
+        which should cause the invalid file to be removed
+        """
+        yaml_error = MagicMock(side_effect=[salt.utils.yaml.YAMLError])
+        with patch("salt.utils.files.fopen", MagicMock()), \
+                patch("salt.utils.yaml.safe_load", yaml_error), \
+                patch("os.remove") as mock_os:
+            path = os.sep + os.path.join("some", "path", "_schedule.conf")
+            config._read_conf_file(path)
+            mock_os.assert_called_once_with(path)
+
+    def test__read_conf_file_invalid_yaml(self):
+        """
+        Any other file that throws a YAMLError should raise a
+        SaltConfigurationError and should not trigger an os.remove
+        """
+        yaml_error = MagicMock(side_effect=[salt.utils.yaml.YAMLError])
+        with patch("salt.utils.files.fopen", MagicMock()), \
+                patch("salt.utils.yaml.safe_load", yaml_error), \
+                patch("os.remove") as mock_os:
+            path = os.sep + os.path.join("etc", "salt", "minion")
+            self.assertRaises(SaltConfigurationError, config._read_conf_file, path=path)
+            mock_os.assert_not_called()
+
+    def test__read_conf_file_empty_dict(self):
+        """
+        A config file that is not rendered as a dictionary by the YAML loader
+        should also raise a SaltConfigurationError and should not trigger
+        an os.remove
+        """
+        mock_safe_load = MagicMock(return_value="some non dict data")
+        with patch("salt.utils.files.fopen", MagicMock()), \
+                patch("salt.utils.yaml.safe_load", mock_safe_load), \
+                patch("os.remove") as mock_os:
+            path = os.sep + os.path.join("etc", "salt", "minion")
+            self.assertRaises(SaltConfigurationError, config._read_conf_file, path=path)
+            mock_os.assert_not_called()
+
+    def test__read_conf_file_integer_id(self):
+        """
+        An integer id should be a string
+        """
+        mock_safe_load = MagicMock(return_value={"id": 1234})
+        with patch("salt.utils.files.fopen", MagicMock()), \
+                patch("salt.utils.yaml.safe_load", mock_safe_load), \
+                patch("os.remove") as mock_os:
+            path = os.sep + os.path.join("etc", "salt", "minion")
+            expected = {"id": "1234"}
+            result = config._read_conf_file(path)
+            mock_os.assert_not_called()
+            self.assertEqual(expected, result)
