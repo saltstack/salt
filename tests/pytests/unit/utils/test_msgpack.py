3b8c6fa8ef2e34c5463eb2cb29c72c1758813d12
ch3ll <megan.wilhite@gmail.com>
2020-08-25 15:49:09 -0400

    use tmpdir pytest fixture

--- tests/pytests/unit/utils/test_msgpack.py
+++ tests/pytests/unit/utils/test_msgpack.py
@@ -3,18 +3,9 @@ import os
 import pytest
 import salt.utils.msgpack
 from tests.support.mock import MagicMock, patch
-from tests.support.runtests import RUNTIME_VARS
 
 
-@pytest.fixture
-def test_fn():
-    fn_ = os.path.join(RUNTIME_VARS.TMP, "test_msgpack")
-    yield fn_
-    if os.path.exists(fn_):
-        os.remove(fn_)
-
-
-def test_load_encoding(test_fn):
+def test_load_encoding(tmpdir):
     """
     test when using msgpack version >= 1.0.0 we
     can still load/dump when using unsupported
@@ -23,13 +14,14 @@ def test_load_encoding(test_fn):
 
     https://github.com/msgpack/msgpack-python/blob/master/ChangeLog.rst
     """
+    fn_ = os.path.join(tmpdir, "test_load_encoding.txt")
     salt.utils.msgpack.version = (1, 0, 0)
 
     kwargs = {"encoding": "utf-8"}
     data = [1, 2, 3]
-    with salt.utils.files.fopen(test_fn, "wb") as fh_:
+    with salt.utils.files.fopen(fn_, "wb") as fh_:
         salt.utils.msgpack.dump(data, fh_)
-    with salt.utils.files.fopen(test_fn, "rb") as fh_:
+    with salt.utils.files.fopen(fn_, "rb") as fh_:
         ret = salt.utils.msgpack.load(fh_, **kwargs)
 
     assert ret == data
@@ -38,11 +30,12 @@ def test_load_encoding(test_fn):
 @pytest.mark.parametrize(
     "version,encoding", [((2, 1, 3), False), ((1, 0, 0), False), ((0, 6, 2), True)]
 )
-def test_load_multiple_versions(version, encoding, test_fn):
+def test_load_multiple_versions(version, encoding, tmpdir):
     """
     test when using msgpack on multiple versions that
     we only remove encoding on >= 1.0.0
     """
+    fn_ = os.path.join(tmpdir, "test_load_multipl_versions.txt")
     salt.utils.msgpack.version = version
     data = [1, 2, 3]
 
@@ -54,14 +47,14 @@ def test_load_multiple_versions(version, encoding, test_fn):
 
     kwargs = {"encoding": "utf-8"}
     with patch_dump, patch_load:
-        with salt.utils.files.fopen(test_fn, "wb") as fh_:
+        with salt.utils.files.fopen(fn_, "wb") as fh_:
             salt.utils.msgpack.dump(data, fh_, encoding="utf-8")
             if encoding:
                 assert "encoding" in mock_dump.call_args.kwargs
             else:
                 assert "encoding" not in mock_dump.call_args.kwargs
 
-        with salt.utils.files.fopen(test_fn, "rb") as fh_:
+        with salt.utils.files.fopen(fn_, "rb") as fh_:
             salt.utils.msgpack.load(fh_, **kwargs)
             if encoding:
                 assert "encoding" in mock_load.call_args.kwargs
