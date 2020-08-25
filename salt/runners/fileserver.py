10376eaf9a65719349be872f1a6d58898572fd06
Mathieu Parent <math.parent@gmail.com>
2020-08-25 15:59:53 +0200

    Add pre-commit suggestions

--- salt/runners/fileserver.py
+++ salt/runners/fileserver.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
 """
 Directly manage the Salt fileserver plugins
 """
-from __future__ import absolute_import, print_function, unicode_literals
 
 # Import Salt libs
 import salt.fileserver
