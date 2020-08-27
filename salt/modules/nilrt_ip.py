35f6d6344f7fc556b5cef66270c7d37da485d753
Wayne Werner <wwerner@saltstack.com>
2020-08-27 11:33:03 -0500

    Fix issue in nilrt_ip
    Before, passing an int or anything that could not be sliced and indexed
    as the default value would cause issues when there are no matching
    config sections. Now, we will only unquote values when the option is
    present in the config file.

--- salt/modules/nilrt_ip.py
+++ salt/modules/nilrt_ip.py
@@ -280,19 +280,14 @@ def _load_config(section, options, default_value="", filename=INI_FILE):
     if not options:
         return results
     with salt.utils.files.fopen(filename, "r") as config_file:
-        config_parser = configparser.RawConfigParser(dict_type=CaseInsensitiveDict)
+        config_parser = configparser.RawConfigParser(
+            dict_type=CaseInsensitiveDict, converters={"unquoted": _remove_quotes}
+        )
         config_parser.readfp(config_file)
         for option in options:
-            if six.PY2:
-                results[option] = (
-                    _remove_quotes(config_parser.get(section, option))
-                    if config_parser.has_option(section, option)
-                    else default_value
-                )
-            else:
-                results[option] = _remove_quotes(
-                    config_parser.get(section, option, fallback=default_value)
-                )
+            results[option] = config_parser.getunquoted(
+                section, option, fallback=default_value
+            )
 
     return results
 
