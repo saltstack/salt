1653dc855a354267d99074b2bf6074c77b18d843
twangboy <slee@saltstack.com>
2020-08-27 16:14:52 -0600

    Update docs and comments

--- salt/utils/win_chcp.py
+++ salt/utils/win_chcp.py
@@ -1,7 +1,5 @@
-# -*- coding: utf-8 -*-
-
 """
-Change the code page for the Windows shell
+Functions for working with the codepage on Windows systems
 """
 
 # Import Python libs
@@ -18,13 +16,24 @@ class CodePageError(Exception):
 
 def chcp(page_id=None, raise_error=False):
     """
-    Changes or gets the code page of the shell.
-    :param page_id: None or int or str
-    :param raise_error: bool
-    :return: str: code page id
-    :raises: CodePageError
+    Gets or sets the codepage of the shell.
+
+    Args:
+
+        page_id (str, int):
+            A number representing the codepage. Default is ``None``
+
+        raise_error (bool):
+            ``True`` will raise an error if the codepage fails to change.
+            ``False`` will suppress the error
+
+    Returns:
+        str: A number representing the codepage
+
+    Raises:
+        CodePageError: On unsuccessful codepage change
     """
-    # check if code page needs to change
+    # check if codepage needs to change
     if page_id is not None:
         page_id = str(page_id)
         current_page = chcp()
@@ -33,7 +42,7 @@ def chcp(page_id=None, raise_error=False):
     else:
         page_id = ""
 
-    # change or get code page
+    # change or get codepage
     try:
         chcp_process = subprocess.Popen(
             "chcp.com {}".format(page_id),
@@ -44,11 +53,11 @@ def chcp(page_id=None, raise_error=False):
         log.error("Code Page was not found!")
         return ""
 
-    # get code page id
+    # get codepage id
     chcp_ret = chcp_process.communicate(timeout=10)[0].decode("ascii", "ignore")
     chcp_ret = "".join([c for c in chcp_ret if c in string.digits])
 
-    # check if code page changed
+    # check if codepage changed
     if page_id != "":
         if page_id != chcp_ret:
             if raise_error:
@@ -56,6 +65,7 @@ def chcp(page_id=None, raise_error=False):
             else:
                 log.error("Code page failed to change to %s!", page_id)
 
+    # If nothing returned, return the current codepage
     if chcp_ret == "":
         chcp_ret = chcp()
 
