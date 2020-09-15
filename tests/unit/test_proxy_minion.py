d34898838de5574a54c98da64b16c8ab32a82788
Pedro Algarvio <pedro@algarvio.me>
2020-09-15 10:02:36 +0100

    Only lay down the required config for tests using it

--- tests/unit/test_proxy_minion.py
+++ tests/unit/test_proxy_minion.py
@@ -2,38 +2,68 @@
     :codeauthor: Gareth J. Greenaway <gareth@saltstack.com>
 """
 
-
 import copy
 import logging
+import os
+import shutil
+import textwrap
 
 import salt.config
 import salt.ext.tornado
-import salt.ext.tornado.testing
 import salt.metaproxy.proxy
 import salt.minion
 import salt.syspaths
 from tests.support.helpers import slowTest
-from tests.support.mixins import AdaptedConfigurationTestCaseMixin
 from tests.support.mock import MagicMock, patch
 from tests.support.runtests import RUNTIME_VARS
 from tests.support.unit import TestCase
 
 log = logging.getLogger(__name__)
-__opts__ = {}
 
 
-class ProxyMinionTestCase(TestCase, AdaptedConfigurationTestCaseMixin):
+class ProxyMinionTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.proxy_conf_d = os.path.join(RUNTIME_VARS.TMP_PROXY_CONF_DIR, "proxy.d")
+        proxytest_conf_d = os.path.join(cls.proxy_conf_d, "proxytest")
+        if not os.path.exists(proxytest_conf_d):
+            os.makedirs(proxytest_conf_d)
+        with salt.utils.files.fopen(
+            os.path.join(proxytest_conf_d, "_schedule.conf"), "w"
+        ) as wfh:
+            wfh.write(
+                textwrap.dedent(
+                    """\
+                    schedule:
+                      test_job:
+                        args: [arg1, arg2]
+                        enabled: true
+                        function: test.arg
+                        jid_include: true
+                        kwargs: {key1: value1, key2: value2}
+                        maxrunning: 1
+                        name: test_job
+                        return_job: false
+                    """
+                )
+            )
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(cls.proxy_conf_d)
+
     @slowTest
     def test_post_master_init_metaproxy_called(self):
         """
         Tests that when the _post_master_ini function is called, _metaproxy_call is also called.
         """
-        mock_opts = salt.config.DEFAULT_MINION_OPTS.copy()
-        mock_opts.update(salt.config.DEFAULT_PROXY_MINION_OPTS)
-        mock_jid_queue = [123]
+        opts = salt.config.DEFAULT_MINION_OPTS.copy()
+        opts.update(salt.config.DEFAULT_PROXY_MINION_OPTS)
+
+        jid_queue = [123]
         proxy_minion = salt.minion.ProxyMinion(
-            mock_opts,
-            jid_queue=copy.copy(mock_jid_queue),
+            opts,
+            jid_queue=copy.copy(jid_queue),
             io_loop=salt.ext.tornado.ioloop.IOLoop(),
         )
         mock_metaproxy_call = MagicMock()
@@ -53,14 +83,14 @@ class ProxyMinionTestCase(TestCase, AdaptedConfigurationTestCaseMixin):
         """
         Tests that when the _handle_decoded_payload function is called, _metaproxy_call is also called.
         """
-        mock_opts = salt.config.DEFAULT_MINION_OPTS.copy()
-        mock_opts.update(salt.config.DEFAULT_PROXY_MINION_OPTS)
+        opts = salt.config.DEFAULT_MINION_OPTS.copy()
+        opts.update(salt.config.DEFAULT_PROXY_MINION_OPTS)
 
-        mock_data = {"fun": "foo.bar", "jid": 123}
-        mock_jid_queue = [123]
+        data = {"fun": "foo.bar", "jid": 123}
+        jid_queue = [123]
         proxy_minion = salt.minion.ProxyMinion(
-            mock_opts,
-            jid_queue=copy.copy(mock_jid_queue),
+            opts,
+            jid_queue=copy.copy(jid_queue),
             io_loop=salt.ext.tornado.ioloop.IOLoop(),
         )
         mock_metaproxy_call = MagicMock()
@@ -70,8 +100,8 @@ class ProxyMinionTestCase(TestCase, AdaptedConfigurationTestCaseMixin):
             autospec=True,
         ):
             try:
-                ret = proxy_minion._handle_decoded_payload(mock_data).result()
-                self.assertEqual(proxy_minion.jid_queue, mock_jid_queue)
+                ret = proxy_minion._handle_decoded_payload(data).result()
+                self.assertEqual(proxy_minion.jid_queue, jid_queue)
                 self.assert_called_once(salt.minion._metaproxy_call)
             finally:
                 proxy_minion.destroy()
@@ -81,14 +111,14 @@ class ProxyMinionTestCase(TestCase, AdaptedConfigurationTestCaseMixin):
         """
         Tests that when the _handle_payload function is called, _metaproxy_call is also called.
         """
-        mock_opts = salt.config.DEFAULT_MINION_OPTS.copy()
-        mock_opts.update(salt.config.DEFAULT_PROXY_MINION_OPTS)
+        opts = salt.config.DEFAULT_MINION_OPTS.copy()
+        opts.update(salt.config.DEFAULT_PROXY_MINION_OPTS)
 
-        mock_data = {"fun": "foo.bar", "jid": 123}
-        mock_jid_queue = [123]
+        data = {"fun": "foo.bar", "jid": 123}
+        jid_queue = [123]
         proxy_minion = salt.minion.ProxyMinion(
-            mock_opts,
-            jid_queue=copy.copy(mock_jid_queue),
+            opts,
+            jid_queue=copy.copy(jid_queue),
             io_loop=salt.ext.tornado.ioloop.IOLoop(),
         )
         mock_metaproxy_call = MagicMock()
@@ -98,8 +128,8 @@ class ProxyMinionTestCase(TestCase, AdaptedConfigurationTestCaseMixin):
             autospec=True,
         ):
             try:
-                ret = proxy_minion._handle_decoded_payload(mock_data).result()
-                self.assertEqual(proxy_minion.jid_queue, mock_jid_queue)
+                ret = proxy_minion._handle_decoded_payload(data).result()
+                self.assertEqual(proxy_minion.jid_queue, jid_queue)
                 self.assert_called_once(mock_metaproxy_call)
             finally:
                 proxy_minion.destroy()
@@ -111,7 +141,8 @@ class ProxyMinionTestCase(TestCase, AdaptedConfigurationTestCaseMixin):
         for the proxy minion, eg. /etc/salt/proxy.d/dummy/*.conf
         """
         opts = salt.config.proxy_config(
-            RUNTIME_VARS.TMP_PROXY_CONF_DIR + "/proxy", minion_id="proxytest"
+            os.path.join(RUNTIME_VARS.TMP_PROXY_CONF_DIR, "proxy"),
+            minion_id="proxytest",
         )
         self.assertIn("schedule", opts)
         self.assertIn("test_job", opts["schedule"])
