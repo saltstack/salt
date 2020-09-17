50cac11bfcded8f528e3b0b6383065c8014136ca
denis <d.mulyalin@gmail.com>
2020-09-17 18:47:29 +1000

    Fixed critical pylint warnings

--- salt/modules/ttp.py
+++ salt/modules/ttp.py
@@ -155,17 +155,19 @@ Sample template::
 """
 # Import python libs
 import logging
-import sys, traceback
+import sys
+import traceback
 
 # Import third party modules
 try:
     from ttp import ttp
 
     HAS_TTP = True
-except ImportError:
-    HAS_TTP = False
 except ModuleNotFoundError:
     HAS_TTP = False
+except ImportError:
+    HAS_TTP = False
+
 log = logging.getLogger(__name__)
 
 __virtualname__ = "ttp"
@@ -216,7 +218,7 @@ def _elasticsearch_return(data, **kwargs):
                         post_to_elk(salt.utils.json.dumps(i))
             # handle normal named groups case
             elif isinstance(input_res, dict):
-                post_to_elk(salt.utils.json.dumps(item))
+                post_to_elk(salt.utils.json.dumps(input_res))
     # handle per_template case
     elif isinstance(data, dict):
         post_to_elk(salt.utils.json.dumps(data))
