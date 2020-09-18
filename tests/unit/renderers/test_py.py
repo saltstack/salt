f066298eba561f2394dd61b2040ad35dffafb139
Dmitry Kuzmenko <dmitry.kuzmenko@dsr-corporation.com>
2020-09-18 08:23:22 +0300

    Drop py2 + black + isort

--- tests/unit/renderers/test_py.py
+++ tests/unit/renderers/test_py.py
@@ -1,12 +1,4 @@
-# -*- coding: utf-8 -*-
-
-# Import Python Libs
-from __future__ import absolute_import, print_function, unicode_literals
-
-# Import Salt libs
 import salt.renderers.py as pyrender
-
-# Import Salt Testing libs
 from tests.support.mixins import LoaderModuleMockMixin
 from tests.support.unit import TestCase
 
