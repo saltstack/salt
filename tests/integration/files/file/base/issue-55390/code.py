104c9eb947b6a69aa6500d937d0d1643dc711875
Dmitry Kuzmenko <dmitry.kuzmenko@dsr-corporation.com>
2020-09-18 08:22:08 +0300

    Mute lint

--- tests/integration/files/file/base/issue-55390/code.py
+++ tests/integration/files/file/base/issue-55390/code.py
@@ -4,4 +4,4 @@
 def run():
     # context is passed through file.managed
     # and gets the value through jinja templating
-    return context["output"]
+    return context["output"]  # pylint: disable=undefined-variable
