909a9f69144c475e78d99d392e9199884903403f
twangboy <slee@saltstack.com>
2020-09-22 12:05:38 -0600

    Squash... add tests

--- tests/unit/modules/test_network.py
+++ tests/unit/modules/test_network.py
@@ -36,6 +36,22 @@ class NetworkTestCase(TestCase, LoaderModuleMockMixin):
             network: {"__utils__": utils},
         }
 
+    @patch("salt.utils.platform.is_windows")
+    def test___virtual__is_windows_true(self, mock_is_windows):
+        mock_is_windows.return_value = True
+        result = network.__virtual__()
+        expected = (
+            False,
+            "The network execution module cannot be loaded on Windows: use win_network instead."
+        )
+        self.assertEqual(result, expected)
+
+    @patch("salt.utils.platform.is_windows")
+    def test___virtual__is_windows_false(self, mock_is_windows):
+        mock_is_windows.return_value = False
+        result = network.__virtual__()
+        self.assertEqual(result, True)
+
     def test_wol_bad_mac(self):
         """
         tests network.wol with bad mac
