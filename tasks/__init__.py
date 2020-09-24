14a7bbc8b26db487e24c1de66b949bb93b02d223
Pedro Algarvio <pedro@algarvio.me>
2020-09-24 14:32:27 +0100

    Automatically remove import comments on files.
    This is desired because now imports are automatically sorted and these
    header comments often get misplaced.
    
    Fixes #57979

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
-
 from invoke import Collection  # pylint: disable=3rd-party-module-not-gated
 
-from . import docs, filemap, loader
+from . import docs, filemap, imports, loader
 
 ns = Collection()
 ns.add_collection(Collection.from_module(docs, name="docs"), name="docs")
 ns.add_collection(Collection.from_module(loader, name="loader"), name="loader")
 ns.add_collection(Collection.from_module(filemap, name="filemap"), name="filemap")
+ns.add_collection(Collection.from_module(imports, name="imports"), name="imports")
