e5b62856e679f40ca2f6b71ed054191d123c1ad0
Mark Ferrell <major@homeonderanged.org>
2020-10-01 07:19:53 -0700

    feat: use instance metadata for default region
    Add a simple http request to test to see if the saltmaster is running
    within the AWS infrastructure and assume the instance's region as the
    default region.  If this test fails then fall-back to the hard-coded
    default of `us-east-1`.

--- salt/utils/boto3mod.py
+++ salt/utils/boto3mod.py
@@ -30,6 +30,8 @@ import logging
 import sys
 from functools import partial
 
+import requests
+
 import salt.utils.stringutils
 import salt.utils.versions
 from salt.exceptions import SaltInvocationError
@@ -45,6 +47,8 @@ except ImportError:
 
 
 log = logging.getLogger(__name__)
+default_region = "us-east-1"
+instance_data_url = "http://169.254.169.254/latest/dynamic/instance-identity/document"
 
 __virtualname__ = "boto3"
 
@@ -87,8 +91,14 @@ def _get_profile(service, region, key, keyid, profile):
         region = _option(service + ".region")
 
     if not region:
-        region = "us-east-1"
-        log.info("Assuming default region %s", region)
+        try:
+            resp = requests.get(instance_data_url)
+            instance_data = resp.json()
+            region = instance_data["region"]
+            log.info("Assuming instance region %s", region)
+        except (requests.ConnectTimeout, requests.ConnectionError):
+            region = default_region
+            log.info("Assuming default region %s", region)
 
     if not key and _option(service + ".key"):
         key = _option(service + ".key")
