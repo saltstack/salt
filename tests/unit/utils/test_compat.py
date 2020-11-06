e48b39de7d7c24493127050bf6abc0ec9258c6eb
11chrisadams11 <11chrisadams11@gmail.com>
2020-11-06 14:31:03 -0700

    Update for pre-commit check.

--- tests/unit/utils/test_compat.py
+++ tests/unit/utils/test_compat.py
@@ -1,15 +1,8 @@
-# -*- coding: utf-8 -*-
 """
 Unit tests for salt.utils.compat.py
 """
 
-# Import python libs
-from __future__ import absolute_import, print_function, unicode_literals
-
-# Import Salt libs
 import salt.utils.compat
-
-# Import Salt Testing libs
 from tests.support.unit import TestCase
 
 
@@ -28,17 +21,17 @@ class CompatUtilsTestCase(TestCase):
         self.assertEqual(1, ret)
 
         # dict x == dict y
-        dict1 = {'foo': 'bar', 'baz': 'qux'}
-        dict2 = {'baz': 'qux', 'foo': 'bar'}
+        dict1 = {"foo": "bar", "baz": "qux"}
+        dict2 = {"baz": "qux", "foo": "bar"}
         ret = salt.utils.compat.cmp(dict1, dict2)
         self.assertEqual(0, ret)
 
         # dict x != dict y
-        dict1 = {'foo': 'bar', 'baz': 'qux'}
-        dict2 = {'foobar': 'bar', 'baz': 'qux'}
+        dict1 = {"foo": "bar", "baz": "qux"}
+        dict2 = {"foobar": "bar", "baz": "qux"}
         ret = salt.utils.compat.cmp(dict1, dict2)
         self.assertEqual(-1, ret)
 
         # dict x != int y
-        dict1 = {'foo': 'bar', 'baz': 'qux'}
+        dict1 = {"foo": "bar", "baz": "qux"}
         self.assertRaises(TypeError, salt.utils.compat.cmp, dict1, 1)
