4884e59b173ea9ef10a350971f53cfb04b6459d5
Gareth J. Greenaway <gareth@saltstack.com>
2020-11-12 13:30:43 -0800

    fixing failing test.  updating pre-commit.

--- salt/returners/elasticsearch_return.py
+++ salt/returners/elasticsearch_return.py
@@ -116,7 +116,10 @@ STATE_FUNCTIONS = {
 
 def __virtual__():
     if "elasticsearch.index_exists" not in __salt__:
-        return False, "Elasticsearch module not availble.  Check that the elasticsearch library is installed."
+        return (
+            False,
+            "Elasticsearch module not availble.  Check that the elasticsearch library is installed.",
+        )
     return __virtualname__
 
 
