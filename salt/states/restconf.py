07444e8f222741be470ae9c9f95545faa74f67d1
Jamie Murphy <jamurphy@pivotal.io>
2020-11-18 16:16:48 +0000

    doc update

--- salt/states/restconf.py
+++ salt/states/restconf.py
@@ -7,14 +7,12 @@ State module for restconf Proxy minions
 
 """
 
-# Import Python libs
-from __future__ import absolute_import, print_function, unicode_literals
 
-# Import Salt libs
-import salt.utils.compat  # noqa: F401
 import json  # noqa: F401
-from salt.utils.odict import OrderedDict  # noqa: F401
+
 from deepdiff import DeepDiff
+
+# from salt.utils.odict import OrderedDict  # noqa: F401
 
 
 def __virtual__():
@@ -23,7 +21,7 @@ def __virtual__():
     return (False, "restconf module could not be loaded")
 
 
-def config_manage(name, uri, method, config, init_uri=None, init_method='PATCH'):
+def config_manage(name, uri, method, config, init_uri=None, init_method="PATCH"):
     """
     Ensure a specific value exists at a given path
     :param name: The name for this rule
@@ -54,34 +52,36 @@ def config_manage(name, uri, method, config, init_uri=None, init_method='PATCH')
     # TODO: add template function so that config var does not need to be passed
     ret = {"name": name, "result": False, "changes": {}, "comment": ""}
     found_working_uri = False
-    uri_used = ''
+    uri_used = ""
     existing_raw = __salt__["restconf.get_data"](uri)  # noqa: F821
-    request_uri = ''
-    request_method = ''
+    request_uri = ""
+    request_method = ""
     # TODO: this could probaby be a loop
-    if existing_raw['status'] in [200]:
-        existing = existing_raw['dict']
+    if existing_raw["status"] in [200]:
+        existing = existing_raw["dict"]
         found_working_uri = True
-        uri_used = 'Primary'
+        uri_used = "Primary"
         request_uri = uri
         request_method = method
 
     if not found_working_uri:
         existing_raw_init = __salt__["restconf.get_data"](init_uri)  # noqa: F821
-        if existing_raw_init['status'] in [200]:
-            existing = existing_raw_init['dict']
+        if existing_raw_init["status"] in [200]:
+            existing = existing_raw_init["dict"]
             found_working_uri = True
-            uri_used = 'init'
+            uri_used = "init"
             request_uri = init_uri
             request_method = init_method
 
     if not found_working_uri:
         ret["result"] = False
-        ret["comment"] = 'restconf could not find a working URI to get initial config'
+        ret["comment"] = "restconf could not find a working URI to get initial config"
         return ret
     # TODO: END
 
-    dict_config = json.loads(json.dumps(config))  # convert from orderedDict to Dict (which is now ordered by default in python3.8)
+    dict_config = json.loads(
+        json.dumps(config)
+    )  # convert from orderedDict to Dict (which is now ordered by default in python3.8)
 
     if existing == dict_config:
         ret["result"] = True
@@ -96,33 +96,39 @@ def config_manage(name, uri, method, config, init_uri=None, init_method='PATCH')
         ret["changes"]["changed"] = diff.changed()
 
     else:
-        resp = __salt__["restconf.set_data"](request_uri, request_method, dict_config)  # noqa: F821
+        resp = __salt__["restconf.set_data"](
+            request_uri, request_method, dict_config
+        )  # noqa: F821
         # Success
-        if resp['status'] in [201, 200, 204]:
+        if resp["status"] in [201, 200, 204]:
             ret["result"] = True
             ret["comment"] = "Successfully added config"
             diff = _restDiff(existing, dict_config)
             ret["changes"]["new"] = diff.added()
             ret["changes"]["removed"] = diff.removed()
             ret["changes"]["changed"] = diff.changed()
-            if method == 'PATCH':
+            if method == "PATCH":
                 ret["changes"]["removed"] = None
         # full failure
         else:
             ret["result"] = False
-            if 'dict' in resp:
-                why = resp['dict']
-            elif 'body' in resp:
-                why = resp['body']
+            if "dict" in resp:
+                why = resp["dict"]
+            elif "body" in resp:
+                why = resp["body"]
             else:
                 why = None
-            ret["comment"] = "failed to add / modify config. API Statuscode: {s}, API Response: {w}, URI:{u}".format(w=why, s=resp['status'], u=uri_used)
+            ret[
+                "comment"
+            ] = "failed to add / modify config. API Statuscode: {s}, API Response: {w}, URI:{u}".format(
+                w=why, s=resp["status"], u=uri_used
+            )
             print("post_content: {b}".format(b=json.dumps(dict_config)))
 
     return ret
 
 
-class _restDiff(object):
+class _restDiff:
     """
     Calculate the difference between two dictionaries as:
     (1) items added
@@ -142,18 +148,18 @@ class _restDiff(object):
     def added(self):
         # TODO: Potential for new adds to get missed here.
         # need to dig into deepdiff more
-        if 'dictionary_item_added' in self.diff.keys():
-            return str(self.diff['dictionary_item_added'])
+        if "dictionary_item_added" in self.diff.keys():
+            return str(self.diff["dictionary_item_added"])
         return None
 
     def removed(self):
-        if 'dictionary_item_removed' in self.diff.keys():
-            return str(self.diff['dictionary_item_removed'])
+        if "dictionary_item_removed" in self.diff.keys():
+            return str(self.diff["dictionary_item_removed"])
         return None
 
     def changed(self):
-        if 'values_changed' in self.diff.keys():
-            return str(self.diff['values_changed'])
+        if "values_changed" in self.diff.keys():
+            return str(self.diff["values_changed"])
         return None
 
     def unchanged(self):
