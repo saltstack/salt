5b3fee27810aca8ac022e7991596e576b65014be
Cédric Bosdonnat <cbosdonnat@suse.com>
2020-11-30 22:28:55 +0100

    Extract XML space stripping function from virt module
    Stripping spaces and indentation from XML could also be useful in other
    places, moving to xmlutil to help reuse.

--- tests/pytests/unit/utils/test_xmlutil.py
+++ tests/pytests/unit/utils/test_xmlutil.py
@@ -208,3 +208,17 @@ def test_change_xml_template_list(xml_doc):
     assert ["1024", "512"] == [
         n.get("size") for n in xml_doc.findall("memtune/hugepages/page")
     ]
+
+
+def test_strip_spaces():
+    xml_str = """<domain>
+            <name>test01</name>
+            <memory unit="MiB" >1024</memory> 
+        </domain>
+    """
+    expected_str = (
+        b'<domain><name>test01</name><memory unit="MiB">1024</memory></domain>'
+    )
+
+    node = ET.fromstring(xml_str)
+    assert expected_str == ET.tostring(xml.strip_spaces(node))
