1e6bf8fd3d26646d632eee7132461d221b14f253
Pedro Algarvio <pedro@algarvio.me>
2020-12-10 11:27:59 +0000

    Fix the `versionadded` version

--- salt/runners/state.py
+++ salt/runners/state.py
@@ -86,7 +86,7 @@ def orchestrate(
 
         Runner uses the pillar variable
 
-    .. versionchanged:: develop
+    .. versionchanged:: 2017.5
 
         Runner uses the pillar_enc variable that allows renderers to render the pillar.
         This is usable when supplying the contents of a file as pillar, and the file contains
