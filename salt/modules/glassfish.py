310f6749505e88b2fbcdb3e75d6afe7f0da9b14c
Pedro Algarvio <pedro@algarvio.me>
2020-12-14 02:11:37 +0000

    Default the glassfish module requests ssl verify to True

--- salt/modules/glassfish.py
+++ salt/modules/glassfish.py
@@ -128,7 +128,7 @@ def _api_get(path, server=None):
         url=_get_url(server["ssl"], server["url"], server["port"], path),
         auth=_get_auth(server["user"], server["password"]),
         headers=_get_headers(),
-        verify=False,
+        verify=True,
     )
     return _api_response(response)
 
@@ -143,7 +143,7 @@ def _api_post(path, data, server=None):
         auth=_get_auth(server["user"], server["password"]),
         headers=_get_headers(),
         data=salt.utils.json.dumps(data),
-        verify=False,
+        verify=True,
     )
     return _api_response(response)
 
@@ -158,7 +158,7 @@ def _api_delete(path, data, server=None):
         auth=_get_auth(server["user"], server["password"]),
         headers=_get_headers(),
         params=data,
-        verify=False,
+        verify=True,
     )
     return _api_response(response)
 
