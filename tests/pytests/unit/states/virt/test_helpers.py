e2d143224bf3b617047a1bcfc38bf39fc9168fd0
Cédric Bosdonnat <cbosdonnat@suse.com>
2020-12-15 10:04:58 +0100

    Convert virt domain state unit tests to pytest
    While converting the virt domain-related states to pytest I realized
    the __opts__["test"] == False case was not handled in some of them.
    This commit also fixes the return code for virt.shutdown,
    virt.powered_off, virt.snapshot and virt.rebooted states. It also
    prevents the actual call to be issued in test mode.

--- tests/pytests/unit/states/virt/test_helpers.py
+++ tests/pytests/unit/states/virt/test_helpers.py
@@ -44,3 +44,56 @@ def network_update_call(
         username=username,
         password=password,
     )
+
+
+def domain_update_call(
+    name,
+    cpu=None,
+    mem=None,
+    disk_profile=None,
+    disks=None,
+    nic_profile=None,
+    interfaces=None,
+    graphics=None,
+    connection=None,
+    username=None,
+    password=None,
+    boot=None,
+    numatune=None,
+    boot_dev=None,
+    hypervisor_features=None,
+    clock=None,
+    serials=None,
+    consoles=None,
+    stop_on_reboot=False,
+    live=True,
+    host_devices=None,
+    test=False,
+):
+    """
+    Create a call object with the missing default parameters from virt.update()
+    """
+    return call(
+        name,
+        cpu=cpu,
+        mem=mem,
+        disk_profile=disk_profile,
+        disks=disks,
+        nic_profile=nic_profile,
+        interfaces=interfaces,
+        graphics=graphics,
+        live=live,
+        connection=connection,
+        username=username,
+        password=password,
+        boot=boot,
+        numatune=numatune,
+        serials=serials,
+        consoles=consoles,
+        test=test,
+        boot_dev=boot_dev,
+        hypervisor_features=hypervisor_features,
+        clock=clock,
+        stop_on_reboot=stop_on_reboot,
+        host_devices=host_devices,
+    )
