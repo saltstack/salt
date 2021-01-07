cc9313d832c027ff4dc12db0eb2deb25d3d7ffc9
twangboy <slee@saltstack.com>
2021-01-06 22:28:01 -0700

    Black

--- salt/cloud/clouds/ec2.py
+++ salt/cloud/clouds/ec2.py
@@ -2348,7 +2348,11 @@ def query_instance(vm_=None, call=None):
 
 
 def wait_for_instance(
-    vm_=None, data=None, ip_address=None, display_ssh_output=True, call=None,
+    vm_=None,
+    data=None,
+    ip_address=None,
+    display_ssh_output=True,
+    call=None,
 ):
     """
     Wait for an instance upon creation from the EC2 API, to become available
@@ -3720,7 +3724,9 @@ def list_nodes_select(call=None):
     Return a list of the VMs that are on the provider, with select fields
     """
     return salt.utils.cloud.list_nodes_select(
-        list_nodes_full(get_location()), __opts__["query.selection"], call,
+        list_nodes_full(get_location()),
+        __opts__["query.selection"],
+        call,
     )
 
 
@@ -4830,7 +4836,11 @@ def describe_snapshots(kwargs=None, call=None):
 
 
 def get_console_output(
-    name=None, location=None, instance_id=None, call=None, kwargs=None,
+    name=None,
+    location=None,
+    instance_id=None,
+    call=None,
+    kwargs=None,
 ):
     """
     Show the console output from the instance.
@@ -4879,7 +4889,10 @@ def get_console_output(
 
 
 def get_password_data(
-    name=None, kwargs=None, instance_id=None, call=None,
+    name=None,
+    kwargs=None,
+    instance_id=None,
+    call=None,
 ):
     """
     Return password data for a Windows instance.
