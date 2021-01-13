92ce886c502aff01e4bd5a9dabbbde2a23b7eaa2
Joep Seuren <joepseuren@acknowledge.nl>
2021-01-13 14:26:07 +0100

    add IPv6 support to vmware cloud provider

--- salt/cloud/clouds/vmware.py
+++ salt/cloud/clouds/vmware.py
@@ -128,6 +128,7 @@ import salt.utils.network
 import salt.utils.stringutils
 import salt.utils.vmware
 import salt.utils.xmlutil
+import salt.utils.master
 from salt.exceptions import SaltCloudSystemExit
 
 try:
@@ -1197,6 +1198,50 @@ def _valid_ip(ip_address):
     return True
 
 
+def _valid_ip6(ip_address):
+    '''
+    Check if the IPv6 address is valid and routable
+    Return either True or False
+    '''
+
+    # Validate IPv6 address
+    try:
+        address = ipaddress.IPv6Address(ip_address)
+    except ipaddress.AddressValueError:
+        return False
+
+    if address.is_unspecified or \
+        address.is_loopback or \
+        address.is_link_local or \
+        address.is_multicast or \
+        address.is_reserved:
+        return False
+
+    if address.ipv4_mapped is not None:
+        return False
+
+    return True
+
+
+def _master_supports_ipv6():
+    '''
+    Check if the salt master has a valid and
+    routable IPv6 address available
+    '''
+    master_fqdn = salt.utils.network.get_fqhostname()
+    pillar_util = salt.utils.master.MasterPillarUtil(master_fqdn,
+                                                     tgt_type='glob',
+                                                     use_cached_grains=False,
+                                                     grains_fallback=False,
+                                                     opts=__opts__)
+    grains_data = pillar_util.get_minion_grains()
+    ipv6_addresses = grains_data[master_fqdn]['ipv6']
+    for address in ipv6_addresses:
+        if _valid_ip6(address):
+            return True
+    return False
+
+
 def _wait_for_ip(vm_ref, max_wait):
     max_wait_vmware_tools = max_wait
     max_wait_ip = max_wait
@@ -1214,17 +1259,23 @@ def _wait_for_ip(vm_ref, max_wait):
         if isinstance(resolved_ips, list) and resolved_ips:
             return resolved_ips[0]
         return False
+    master_supports_ipv6 = _master_supports_ipv6()
+    log.info(
+        "[ %s ] Master has IPv6 support: %s",
+        vm_ref.name, master_supports_ipv6
+    )
     time_counter = 0
     starttime = time.time()
+    ipv4_address = None
     while time_counter < max_wait_ip:
         if time_counter % 5 == 0:
             log.info(
-                "[ %s ] Waiting to retrieve IPv4 information [%s s]",
+                "[ %s ] Waiting to retrieve IPv4/6 information [%s s]",
                 vm_ref.name,
                 time_counter,
             )
 
-        if vm_ref.summary.guest.ipAddress and _valid_ip(vm_ref.summary.guest.ipAddress):
+        if vm_ref.summary.guest.ipAddress and (_valid_ip(vm_ref.summary.guest.ipAddress) or _valid_ip6(vm_ref.summary.guest.ipAddress)):
             log.info(
                 "[ %s ] Successfully retrieved IPv4 information in %s seconds",
                 vm_ref.name,
@@ -1234,18 +1285,26 @@ def _wait_for_ip(vm_ref, max_wait):
         for net in vm_ref.guest.net:
             if net.ipConfig.ipAddress:
                 for current_ip in net.ipConfig.ipAddress:
-                    if _valid_ip(current_ip.ipAddress):
+                    if master_supports_ipv6 and _valid_ip6(current_ip.ipAddress):
                         log.info(
-                            "[ %s ] Successfully retrieved IPv4 information "
-                            "in %s seconds",
-                            vm_ref.name,
-                            time_counter,
+                            "[ %s ] Successfully retrieved IPv6 information "
+                            "in %s seconds", vm_ref.name, time_counter
                         )
                         return current_ip.ipAddress
+                    if _valid_ip(current_ip.ipAddress) and not ipv4_address:
+                        # Delay return in case we have a valid IPv6 available
+                        # The first valid ipv4 adress is used as fallback
+                        ipv4_address = current_ip
+        if ipv4_address:
+            log.info(
+                "[ %s ] Successfully retrieved IPv4 information "
+                "in %s seconds", vm_ref.name, time_counter
+            )
+            return ipv4_address.ipAddress
         time.sleep(1.0 - ((time.time() - starttime) % 1.0))
         time_counter += 1
     log.warning(
-        "[ %s ] Timeout Reached. Unable to retrieve IPv4 information after "
+        "[ %s ] Timeout Reached. Unable to retrieve IPv4/6 information after "
         "waiting for %s seconds",
         vm_ref.name,
         max_wait_ip,
