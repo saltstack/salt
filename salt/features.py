6cbe1c401ea9d3df8bd6754ee08be8ad500858bd
Pedro Algarvio <pedro@algarvio.me>
2021-01-18 11:43:22 +0000

    Fix and improve the `salt.feature.Features` class and it's setup.

--- salt/features.py
+++ salt/features.py
@@ -7,18 +7,25 @@ log = logging.getLogger(__name__)
 
 
 class Features:
+
+    __slots__ = ("features", "setup")
+
     def __init__(self, _features=None):
         if _features is None:
-            self.features = {}
-        else:
-            self.features = _features
+            _features = {}
+        self.features = _features
         self.setup = False
 
     def setup_features(self, opts):
-        if not self.setup:
-            self.features.update(opts.get("features", {}))
-        else:
-            log.warn("Features already setup")
+        if self.setup is True:
+            log.warning("Features already setup")
+            return
+        self.setup = True
+        if "features" not in opts:
+            return
+        if not opts["features"]:
+            return
+        self.features.update(opts["features"])
 
     def get(self, key, default=None):
         return self.features.get(key, default)
