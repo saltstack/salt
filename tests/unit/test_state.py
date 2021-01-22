97903475a15ba642110ef8fc1a0cd510b6e65ac5
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2021-01-22 11:39:10 +0000

    Add unit tests cases to ensure proper onlyif/unless behavior

--- tests/unit/test_state.py
+++ tests/unit/test_state.py
@@ -221,7 +221,7 @@ class StateCompilerTestCase(TestCase, AdaptedConfigurationTestCaseMixin):
         with patch("salt.state.State._gather_pillar") as state_patch:
             minion_opts = self.get_temp_config("minion")
             state_obj = salt.state.State(minion_opts)
-            return_result = state_obj._run_check_onlyif(low_data, {})
+            return_result = state_obj._run_check_unless(low_data, {})
             self.assertEqual(expected_result, return_result)
 
     def test_verify_unless_list_cmd_different_order(self):
@@ -231,12 +231,34 @@ class StateCompilerTestCase(TestCase, AdaptedConfigurationTestCaseMixin):
             "__sls__": "tests.cmd",
             "__env__": "base",
             "__id__": "check unless",
-            "onlyif": ["/bin/false", "/bin/true"],
+            "unless": ["/bin/false", "/bin/true"],
             "order": 10001,
             "fun": "run",
         }
         expected_result = {
             "comment": "unless condition is true",
+            "result": True,
+            "skip_watch": True,
+        }
+        with patch("salt.state.State._gather_pillar") as state_patch:
+            minion_opts = self.get_temp_config("minion")
+            state_obj = salt.state.State(minion_opts)
+            return_result = state_obj._run_check_unless(low_data, {})
+            self.assertEqual(expected_result, return_result)
+
+    def test_verify_onlyif_list_cmd_different_order(self):
+        low_data = {
+            "state": "cmd",
+            "name": 'echo "something"',
+            "__sls__": "tests.cmd",
+            "__env__": "base",
+            "__id__": "check onlyif",
+            "onlyif": ["/bin/false", "/bin/true"],
+            "order": 10001,
+            "fun": "run",
+        }
+        expected_result = {
+            "comment": "onlyif condition is false",
             "result": True,
             "skip_watch": True,
         }
@@ -253,11 +275,73 @@ class StateCompilerTestCase(TestCase, AdaptedConfigurationTestCaseMixin):
             "__sls__": "tests.cmd",
             "__env__": "base",
             "__id__": "check unless",
-            "onlyif": ["/bin/true", "/bin/true"],
+            "unless": ["/bin/false", "/bin/false"],
             "order": 10001,
             "fun": "run",
         }
         expected_result = {"comment": "unless condition is false", "result": False}
+        with patch("salt.state.State._gather_pillar") as state_patch:
+            minion_opts = self.get_temp_config("minion")
+            state_obj = salt.state.State(minion_opts)
+            return_result = state_obj._run_check_unless(low_data, {})
+            self.assertEqual(expected_result, return_result)
+
+    def test_verify_onlyif_list_cmd_valid(self):
+        low_data = {
+            "state": "cmd",
+            "name": 'echo "something"',
+            "__sls__": "tests.cmd",
+            "__env__": "base",
+            "__id__": "check onlyif",
+            "onlyif": ["/bin/true", "/bin/true"],
+            "order": 10001,
+            "fun": "run",
+        }
+        expected_result = {"comment": "onlyif condition is true", "result": False}
+        with patch("salt.state.State._gather_pillar") as state_patch:
+            minion_opts = self.get_temp_config("minion")
+            state_obj = salt.state.State(minion_opts)
+            return_result = state_obj._run_check_onlyif(low_data, {})
+            self.assertEqual(expected_result, return_result)
+
+    def test_verify_unless_list_cmd_invalid(self):
+        low_data = {
+            "state": "cmd",
+            "name": 'echo "something"',
+            "__sls__": "tests.cmd",
+            "__env__": "base",
+            "__id__": "check unless",
+            "unless": ["/bin/true", "/bin/true"],
+            "order": 10001,
+            "fun": "run",
+        }
+        expected_result = {
+            "comment": "unless condition is true",
+            "result": True,
+            "skip_watch": True,
+        }
+        with patch("salt.state.State._gather_pillar") as state_patch:
+            minion_opts = self.get_temp_config("minion")
+            state_obj = salt.state.State(minion_opts)
+            return_result = state_obj._run_check_unless(low_data, {})
+            self.assertEqual(expected_result, return_result)
+
+    def test_verify_onlyif_list_cmd_invalid(self):
+        low_data = {
+            "state": "cmd",
+            "name": 'echo "something"',
+            "__sls__": "tests.cmd",
+            "__env__": "base",
+            "__id__": "check onlyif",
+            "onlyif": ["/bin/false", "/bin/false"],
+            "order": 10001,
+            "fun": "run",
+        }
+        expected_result = {
+            "comment": "onlyif condition is false",
+            "result": True,
+            "skip_watch": True,
+        }
         with patch("salt.state.State._gather_pillar") as state_patch:
             minion_opts = self.get_temp_config("minion")
             state_obj = salt.state.State(minion_opts)
@@ -436,28 +520,6 @@ class StateCompilerTestCase(TestCase, AdaptedConfigurationTestCaseMixin):
             "__env__": "base",
             "__id__": "check onlyif",
             "onlyif": ["/bin/true", "/bin/false"],
-            "order": 10001,
-            "fun": "run",
-        }
-        expected_result = {
-            "comment": "onlyif condition is false",
-            "result": True,
-            "skip_watch": True,
-        }
-        with patch("salt.state.State._gather_pillar") as state_patch:
-            minion_opts = self.get_temp_config("minion")
-            state_obj = salt.state.State(minion_opts)
-            return_result = state_obj._run_check_onlyif(low_data, {})
-            self.assertEqual(expected_result, return_result)
-
-    def test_verify_onlyif_list_cmd_different_order(self):
-        low_data = {
-            "state": "cmd",
-            "name": 'echo "something"',
-            "__sls__": "tests.cmd",
-            "__env__": "base",
-            "__id__": "check onlyif",
-            "onlyif": ["/bin/false", "/bin/true"],
             "order": 10001,
             "fun": "run",
         }
