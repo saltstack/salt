9bc16734b1060451dd6f0e721a92c4cd6d814aef
Wayne Werner <wwerner@vmware.com>
2021-01-26 14:25:10 -0600

    Add list_datastore_full tests

--- tests/pytests/unit/cloud/clouds/test_vmware.py
+++ tests/pytests/unit/cloud/clouds/test_vmware.py
@@ -43,24 +43,75 @@ def test_list_datastores_full_should_return_datastores_output_in_dict():
 
 
 def test_list_datastore_full_should_raise_SaltCloudSystemExit_if_call_is_not_function():
-    pytest.skip()
+    expected_message = (
+        "The list_datastore_full function must be called with -f or --function."
+    )
+    with pytest.raises(SaltCloudSystemExit) as err:
+        # This can be anything except "function"
+        vmware.list_datastore_full(call="not function")
+    actual_message = err.value.message
+
+    assert actual_message == expected_message
 
 
 def test_list_datastore_full_should_raise_SaltCloudSystemExit_if_datastore_is_not():
-    pytest.skip()
+    expected_message = "The list_datastore_full function requires a datastore."
+    with pytest.raises(SaltCloudSystemExit) as err:
+        # This can be anything except "function"
+        vmware.list_datastore_full(
+            call="function", datastore=None, kwargs={"datastore": None}
+        )
+    actual_message = err.value.message
 
-
-def test_list_datastore_full_should_call_utils_with_service_instance_and_datastore():
-    pytest.skip()
-
-
-def test_list_datastore_full_should_return_utils_result_in_dict_with_datastore_name():
-    pytest.skip()
+    assert actual_message == expected_message
 
 
 def test_list_datastore_full_should_override_provided_datastore_with_kwargs_datastore():
-    pytest.skip()
+    unexpected_datastore = "fnord datastore"
+    expected_datastore = "kwargs datastore"
+    expected_si = "some service instance"
+    expected_calls = [call(expected_si, expected_datastore)]
+    with patch("salt.cloud.clouds.vmware._get_si", return_value=expected_si), patch(
+        "salt.utils.vmware.list_datastore_full"
+    ) as fake_dsf:
+        vmware.list_datastore_full(
+            call="function",
+            datastore=unexpected_datastore,
+            kwargs={"datastore": expected_datastore},
+        )
+
+    fake_dsf.assert_has_calls(expected_calls)
 
 
-def test_list_datastore_full_should_use_provided_datastore_if_kwargs_are_provided_without_datastore():
-    pytest.skip()
+@pytest.mark.parametrize("kwargs", [{}, {"datastore": None}, None])
+def test_list_datastore_full_should_use_provided_datastore_if_kwargs_are_provided_without_datastore(
+    kwargs,
+):
+    expected_datastore = "regular datastore"
+    expected_si = "some service instance"
+    expected_calls = [call(expected_si, expected_datastore)]
+    with patch("salt.cloud.clouds.vmware._get_si", return_value=expected_si), patch(
+        "salt.utils.vmware.list_datastore_full"
+    ) as fake_dsf:
+        vmware.list_datastore_full(
+            call="function", datastore=expected_datastore, kwargs=kwargs
+        )
+
+    fake_dsf.assert_has_calls(expected_calls)
+
+
+@pytest.mark.parametrize(
+    "expected_datastore_name,expected_datastore",
+    [("fnord", "blarp"), ("bob", "tennis ball"), ("roscivs", "bottia")],
+)
+def test_list_datastore_full_should_return_utils_result_in_dict_with_datastore_name(
+    expected_datastore_name, expected_datastore
+):
+    expected_result = {expected_datastore_name: expected_datastore}
+    with patch("salt.cloud.clouds.vmware._get_si"), patch(
+        "salt.utils.vmware.list_datastore_full", return_value=expected_datastore
+    ) as fake_dsf:
+        actual_result = vmware.list_datastore_full(
+            call="function", datastore=expected_datastore_name
+        )
+    assert actual_result == expected_result
