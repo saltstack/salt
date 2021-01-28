a4dd864ef6d61c938700385097fd1635c5ef9e12
Pedro Algarvio <pedro@algarvio.me>
2021-01-28 08:21:08 +0000

    Ignore Bandit's B413(blacklist) regarding PyCrypto
    The Salt project is aware and only uses PyCrypto as a last resort.
    Additionally, see #30287

--- salt/crypt.py
+++ salt/crypt.py
@@ -62,13 +62,17 @@ if not HAS_M2:
 
 if not HAS_M2 and not HAS_CRYPTO:
     try:
-        from Crypto.Cipher import AES, PKCS1_OAEP, PKCS1_v1_5 as PKCS1_v1_5_CIPHER
-        from Crypto.Hash import SHA
-        from Crypto.PublicKey import RSA
-        from Crypto.Signature import PKCS1_v1_5
+        from Crypto.Cipher import (  # nosec
+            AES,
+            PKCS1_OAEP,
+            PKCS1_v1_5 as PKCS1_v1_5_CIPHER,
+        )
+        from Crypto.Hash import SHA  # nosec
+        from Crypto.PublicKey import RSA  # nosec
+        from Crypto.Signature import PKCS1_v1_5  # nosec
 
         # let this be imported, if possible
-        from Crypto import Random
+        from Crypto import Random  # nosec
 
         HAS_CRYPTO = True
     except ImportError:
