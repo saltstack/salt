0fb25465ea77d3bc35b29db93944540fba060c2f
Pedro Algarvio <pedro@algarvio.me>
2021-01-29 08:08:45 +0000

    Add `_get_active_provider_name` to get the value of `__active_provider_name__`

--- salt/cloud/clouds/pyrax.py
+++ salt/cloud/clouds/pyrax.py
@@ -34,13 +34,20 @@ def __virtual__():
     return __virtualname__
 
 
+def _get_active_provider_name():
+    try:
+        return __active_provider_name__.value()
+    except AttributeError:
+        return __active_provider_name__
+
+
 def get_configured_provider():
     """
     Return the first configured instance.
     """
     return config.is_provider_configured(
         __opts__,
-        __active_provider_name__ or __virtualname__,
+        _get_active_provider_name() or __virtualname__,
         ("username", "identity_url", "compute_region",),
     )
 
