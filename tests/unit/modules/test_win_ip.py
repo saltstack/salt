8139a4e16f053ceb1efff7e95c07869841f8cd25
Pedro Algarvio <pedro@algarvio.me>
2021-01-31 08:00:12 +0000

    Drop Py2 and six on tests/unit/modules/test_win_ip.py

--- tests/unit/modules/test_win_ip.py
+++ tests/unit/modules/test_win_ip.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
 """
     :codeauthor: Jayesh Kariya <jayeshk@saltstack.com>
 """
-from __future__ import absolute_import, print_function, unicode_literals
 
 import salt.modules.win_ip as win_ip
 from salt.exceptions import CommandExecutionError, SaltInvocationError
