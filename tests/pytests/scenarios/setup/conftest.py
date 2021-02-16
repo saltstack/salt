d5fb56ddcc85ba14aad466b524cfb76827a90e56
Pedro Algarvio <pedro@algarvio.me>
2021-02-16 13:49:59 +0000

    Under CI, also copy the source code

--- tests/pytests/scenarios/setup/conftest.py
+++ tests/pytests/scenarios/setup/conftest.py
@@ -4,12 +4,13 @@ import shutil
 
 import pytest
 from tests.support.helpers import VirtualEnv
+from tests.support.runtests import RUNTIME_VARS
 
 
-@pytest.fixture
-def setup_tests_path(tmp_path):
+@pytest.fixture(scope="package")
+def setup_tests_path(tmp_path_factory):
     if os.environ.get("CI_RUN", "0") == "0":
-        directory = tmp_path / "setup-tests"
+        directory = tmp_path_factory.mktemp("setup-tests")
     else:
         # Under CI, on some platforms, Arch, Fedora 33, FreeBSD 12, we run out of disk space on /tmp
         # Use a subdirectory in the current user's home directory
@@ -23,11 +24,34 @@ def setup_tests_path(tmp_path):
 
 @pytest.fixture
 def virtualenv(setup_tests_path):
-    return VirtualEnv(venv_dir=str(setup_tests_path / ".venv"))
+    venv_dir = str(setup_tests_path / ".venv")
+    try:
+        return VirtualEnv(venv_dir=venv_dir)
+    finally:
+        shutil.rmtree(venv_dir, ignore_errors=True)
 
 
 @pytest.fixture
 def cache_dir(setup_tests_path):
     _cache_dir = setup_tests_path / ".cache"
     _cache_dir.mkdir(parents=True, exist_ok=True)
-    return _cache_dir
+    try:
+        yield _cache_dir
+    finally:
+        shutil.rmtree(str(_cache_dir), ignore_errors=True)
+
+
+@pytest.fixture(scope="package")
+def src_dir(setup_tests_path):
+    if os.environ.get("CI_RUN", "0") == "0":
+        return RUNTIME_VARS.CODE_DIR
+
+    _src_dir = setup_tests_path / "src"
+    shutil.copytree(
+        RUNTIME_VARS.CODE_DIR,
+        str(_src_dir),
+        ignore=shutil.ignore_patterns(
+            "__pycache__", "*.pyc", "*.pyo", ".coverage.*", ".nox"
+        ),
+    )
+    return str(_src_dir)
