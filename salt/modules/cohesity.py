09301f304e8ec92943bd37d6c665739723a81caa
naveena-maplelabs <naveena.maplelabs@cohesity.com>
2021-02-23 23:03:18 +0530

    Minor Fix

--- salt/modules/cohesity.py
+++ salt/modules/cohesity.py
@@ -54,28 +54,45 @@ except ImportError as err:
     HAS_LIBS = False
     print("Error while importing Cohesity SDK modules.")
     print(err)
-    exit(0)
 
 log = logging.getLogger(__name__)
 ERROR_LIST = []
 __virtualname__ = "cohesity"
 
-config_path = "/etc/salt/master.d/cohesity.conf"
-cohesity_config = {}
-if os.path.isfile(config_path):
-    import yaml
 
-    with salt.utils.files.fopen(config_path, "r") as file_obj:
-        config = yaml.safe_load(file_obj)
-        cohesity_config = config.get("cohesity_config", {})
-
-cluster_vip = cohesity_config.get("cluster_vip", "")
-c_username = cohesity_config.get("username", "")
-c_password = cohesity_config.get("password", "")
-c_domain = cohesity_config.get("domain", "")
-cohesity_client = CohesityClient(
-    cluster_vip=cluster_vip, username=c_username, password=c_password, domain=c_domain,
-)
+def _get_client():
+    context_key = '{}.cohesity_client'.format(__name__)
+    opts = salt.config.master_config("/etc/salt/master")
+    print(opts)
+    #if context_key in __context__:
+    #    return __context__[context_key]
+    cohesity_config = opts.get("cohesity_config", {})
+    cluster_vip = cohesity_config.get("cluster_vip", "")
+    c_username = cohesity_config.get("username", "")
+    c_password = cohesity_config.get("password", "")
+    c_domain = cohesity_config.get("domain", "")
+    cohesity_client = CohesityClient(
+        cluster_vip=cluster_vip, username=c_username, password=c_password, domain=c_domain,
+    )
+    __context__[context_key] = cohesity_client
+    return cohesity_client
+cohesity_client = _get_client()
+# config_path = "/etc/salt/master.d/cohesity.conf"
+# cohesity_config = {}
+# if os.path.isfile(config_path):
+#     import yaml
+# 
+#     with salt.utils.files.fopen(config_path, "r") as file_obj:
+#         config = yaml.safe_load(file_obj)
+#         cohesity_config = config.get("cohesity_config", {})
+# 
+# cluster_vip = cohesity_config.get("cluster_vip", "")
+# c_username = cohesity_config.get("username", "")
+# c_password = cohesity_config.get("password", "")
+# c_domain = cohesity_config.get("domain", "")
+# cohesity_client = CohesityClient(
+#     cluster_vip=cluster_vip, username=c_username, password=c_password, domain=c_domain,
+# )
 
 
 def __virtual__():
