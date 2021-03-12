f3455dc6d35cfc8046d5cb97f794b4ddf33eb743
Gary T. Giesen <ggiesen@giesen.me>
2021-03-12 10:56:54 -0500

    Correct link to napalm installation. Resolves #56501

--- salt/modules/napalm_netacl.py
+++ salt/modules/napalm_netacl.py
@@ -26,7 +26,7 @@ it requires NAPALM_ library to be installed:  ``pip install napalm``.
 Please check Installation_ for complete details.
 
 .. _NAPALM: https://napalm.readthedocs.io
-.. _Installation: https://napalm.readthedocs.io/en/latest/installation.html
+.. _Installation: https://napalm.readthedocs.io/en/latest/installation/index.html
 """
 from __future__ import absolute_import, print_function, unicode_literals
 
