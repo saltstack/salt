8b61cdf9ff11cb3494d80f54031b65d1289ae3e3
Wayne Werner <wwerner@vmware.com>
2021-03-16 15:40:30 -0500

    Test when NQN fails to be set
    If we can't set the NVMe Qualified Name, then we need to halt creation
    and delete the host, since it was not correctly created.

--- tests/pytests/unit/modules/test_purefa.py
+++ tests/pytests/unit/modules/test_purefa.py
@@ -6,15 +6,34 @@ from tests.support.mock import MagicMock, call, create_autospec, patch
 
 @pytest.fixture
 def configure_loader_modules():
-    return {purefa: {"_get_host": create_autospec(purefa._get_host, return_value=None)}}
+    fake_pure_storage = MagicMock()
+    fake_pure_storage.PureError = Exception
+    return {
+        purefa: {
+            "_get_host": create_autospec(purefa._get_host, return_value=None),
+            "purestorage": fake_pure_storage,
+        }
+    }
 
 
 @pytest.fixture
-def fake_set_host():
-    fake_set_host = MagicMock()
+def patch_get_system():
     with patch("salt.modules.purefa._get_system", autospec=True) as fake_sys:
-        fake_sys.return_value.set_host = fake_set_host
-        yield fake_set_host
+        yield fake_sys
+
+
+@pytest.fixture
+def fake_set_host(patch_get_system):
+    fake_set_host = MagicMock()
+    patch_get_system.return_value.set_host = fake_set_host
+    yield fake_set_host
+
+
+@pytest.fixture
+def fake_delete_host(patch_get_system):
+    fake_delete_host = MagicMock()
+    patch_get_system.return_value.delete_host = fake_delete_host
+    yield fake_delete_host
 
 
 @pytest.mark.parametrize(
@@ -47,3 +66,14 @@ def test_when_nqn_is_provided_and_adding_is_successful_then_result_should_be_Tru
     result = purefa.host_create("fnord", nqn="fnordqn")
 
     assert result is True
+
+
+def test_when_nqn_is_provided_but_set_host_fails_then_creation_should_be_rolled_back(
+    fake_set_host, fake_delete_host
+):
+    expected_host = "fnord"
+    fake_set_host.side_effect = purefa.purestorage.PureError("oops!")
+    result = purefa.host_create(expected_host, nqn="badness or whatever")
+
+    assert result is False
+    fake_delete_host.assert_called_with(expected_host)
