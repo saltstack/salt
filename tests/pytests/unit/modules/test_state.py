3774907a742735bcf0cfbddab1e689cc285bb96a
Daniel A. Wozniak <dwozniak@vmware.com>
2021-03-22 13:52:52 -0700

    Clean up test

--- tests/pytests/unit/modules/test_state.py
+++ tests/pytests/unit/modules/test_state.py
@@ -15,7 +15,7 @@ def test_get_initial_pillar():
     pillar_data = {"foo": "bar"}
     named_ctx = ctx.named_context("__pillar__", pillar_data)
     opts = {"__cli": "salt-call", "pillarenv": "base"}
-    with patch("salt.modules.state.__pillar__", named_ctx):
-        with patch("salt.modules.state.__opts__", opts):
+    with patch("salt.modules.state.__pillar__", named_ctx, create=True):
+        with patch("salt.modules.state.__opts__", opts, create=True):
             pillar = salt.modules.state._get_initial_pillar(opts)
             assert pillar == pillar_data
