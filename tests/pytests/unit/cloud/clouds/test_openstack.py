4deefcf618a16475c784200c0037d5d72de4fac4
Mark Hyde <mark.hyde@rackspace.co.uk>
2021-03-25 15:46:33 +0000

    Reformat using black 19.10b0

--- tests/pytests/unit/cloud/clouds/test_openstack.py
+++ tests/pytests/unit/cloud/clouds/test_openstack.py
@@ -1,16 +1,11 @@
 import pytest
-
 import salt.cloud.clouds.openstack as openstack
 from tests.support.mock import call, patch
 
 
 @pytest.fixture
 def configure_loader_modules():
-    return {
-        openstack: {
-            "__opts__": {},
-        }
-    }
+    return {openstack: {"__opts__": {}}}
 
 
 @pytest.fixture
@@ -41,45 +36,18 @@ def test_when_getting_cloud_config_values_expected_args_should_be_provided():
 @pytest.mark.parametrize(
     "comment,example_ip,ignored_cidr,expected",
     [
-        (
-            "ip is in ignore_cidr",
-            "203.0.113.1",
-            "203.0.113.0/24",
-            True,
-        ),
-        (
-            "ip is not in ignore_cidr",
-            "192.0.2.1",
-            "203.0.113.0/24",
-            False,
-        ),
-        (
-            "ignore_cidr is empty",
-            "192.0.2.1",
-            "",
-            False,
-        ),
-        (
-            "ignore_cidr is False",
-            "192.0.2.1",
-            False,
-            False,
-        ),
-        (
-            "ignore_cidr is None",
-            "192.0.2.1",
-            None,
-            False,
-        ),
+        ("ip is in ignore_cidr", "203.0.113.1", "203.0.113.0/24", True),
+        ("ip is not in ignore_cidr", "192.0.2.1", "203.0.113.0/24", False),
+        ("ignore_cidr is empty", "192.0.2.1", "", False),
+        ("ignore_cidr is False", "192.0.2.1", False, False),
+        ("ignore_cidr is None", "192.0.2.1", None, False),
     ],
 )
 def test_when_ignore_cidr_is_configured_and_ip_is_provided_result_is_expected(
     comment, example_ip, ignored_cidr, expected
 ):
     with patch(
-        "salt.config.get_cloud_config_value",
-        autospec=True,
-        return_value=ignored_cidr,
+        "salt.config.get_cloud_config_value", autospec=True, return_value=ignored_cidr
     ):
         result = openstack.ignore_cidr("fnord", example_ip)
 
@@ -91,56 +59,31 @@ def test_when_ignore_cidr_is_configured_and_ip_is_provided_result_is_expected(
     [
         (
             "ignore_cidr matches first 2 ips, expected value will be first ip that doesn't match cidr.",
-            [
-                "203.0.113.1",
-                "203.0.113.2",
-                "192.0.2.1",
-                "192.0.2.2",
-            ],
+            ["203.0.113.1", "203.0.113.2", "192.0.2.1", "192.0.2.2"],
             "203.0.113.0/24",
             "192.0.2.1",
         ),
         (
             "ignore_cidr matches 2nd 2 IPs, expected value will be first ip in list. ",
-            [
-                "203.0.113.1",
-                "203.0.113.2",
-                "192.0.2.1",
-                "192.0.2.2",
-            ],
+            ["203.0.113.1", "203.0.113.2", "192.0.2.1", "192.0.2.2"],
             "192.0.2.0/24",
             "203.0.113.1",
         ),
         (
             "ignore_cidr doesn't match any IPs, expected value will be first ip in list.",
-            [
-                "203.0.113.1",
-                "203.0.113.2",
-                "192.0.2.1",
-                "192.0.2.2",
-            ],
+            ["203.0.113.1", "203.0.113.2", "192.0.2.1", "192.0.2.2"],
             "198.51.100.0/24",
             "203.0.113.1",
         ),
         (
             "ignore_cidr matches all IPs, expected value will be False.",
-            [
-                "203.0.113.1",
-                "203.0.113.2",
-                "203.0.113.3",
-                "203.0.113.4",
-            ],
+            ["203.0.113.1", "203.0.113.2", "203.0.113.3", "203.0.113.4"],
             "203.0.113.0/24",
             False,
         ),
         (
             "When ignore_cidr is not set, return first ip",
-            [
-                "203.0.113.1",
-                "203.0.113.2",
-                "192.0.2.1",
-                "192.0.2.2",
-            ],
+            ["203.0.113.1", "203.0.113.2", "192.0.2.1", "192.0.2.2"],
             None,
             "203.0.113.1",
         ),
@@ -150,9 +93,7 @@ def test_preferred_ip_function_returns_expected(
     comment, example_ips, ignored_cidr, expected
 ):
     with patch(
-        "salt.config.get_cloud_config_value",
-        autospec=True,
-        return_value=ignored_cidr,
+        "salt.config.get_cloud_config_value", autospec=True, return_value=ignored_cidr
     ):
         result = openstack.preferred_ip("fnord", example_ips)
 
