b98635606e089ee37d5de84cb6ec10ebe4015bca
twangboy <slee@saltstack.com>
2021-03-29 14:50:38 -0600

    Try monkeypatch

--- tests/pytests/functional/cli/test_api.py
+++ tests/pytests/functional/cli/test_api.py
@@ -1,31 +1,22 @@
 import os
 import sys
-import tempfile
 
 import pytest
 from salt.cli.api import SaltAPI
-from tests.support.mock import patch
 
 
 @pytest.mark.slow_test
-def test_start_shutdown():
-    pidfile = tempfile.mktemp()
-    logfile = tempfile.mktemp()
+def test_start_shutdown(monkeypatch, tmp_path):
+    pid_file = str(tmp_path / "pid_file")
+    log_file = str(tmp_path / "log_file")
     api = SaltAPI()
     with pytest.raises(SystemExit):
-        try:
-            # testing environment will fail if we use default pidfile
-            # overwrite sys.argv so salt-api does not use testing args
-            with patch.object(sys, "argv", [sys.argv[0], "--pid-file", pidfile]):
-                api.start()
-                assert os.path.isfile(pidfile)
-                api.shutdown()
-        finally:
-            try:
-                os.remove(pidfile)
-            except OSError:
-                pass
-            try:
-                os.remove(logfile)
-            except OSError:
-                pass
+        # testing environment will fail if we use default pidfile
+        # overwrite sys.argv so salt-api does not use testing args
+        monkeypatch.setattr(
+            "sys.argv", [sys.argv[0], "--pid-file", pid_file, "--log-file", log_file]
+        )
+        api.start()
+        assert os.path.isfile(pid_file)
+        assert os.path.isfile(log_file)
+        api.shutdown()
