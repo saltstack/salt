21540363a498ec1736b4047a3c26bd8b630556d4
Tim Dithmer <dithmer@adesso-mobile.de>
2021-04-06 13:34:54 +0200

    Change naming according to hetzner doc

--- salt/cloud/clouds/hetzner.py
+++ salt/cloud/clouds/hetzner.py
@@ -92,7 +92,7 @@ def _public_network_to_dict(net):
 
 def _private_network_to_dict(net):
     return {
-        "ipv4": getattr(net.ip, "ip", None),
+        "ip": getattr(net.ip, "ip", None),
     }
 
 
