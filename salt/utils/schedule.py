a987cb3629a302ac8574ef2cac3ecaeb2934244d
Gareth J. Greenaway <gareth@saltstack.com>
2021-04-11 11:33:56 -0700

    log.warn is deprecated, using log.warning.

--- salt/utils/schedule.py
+++ salt/utils/schedule.py
@@ -950,7 +950,7 @@ class Schedule:
 
         """
 
-        log.warn("==== evaluating schedule now %s =====", now)
+        log.warning("==== evaluating schedule now %s =====", now)
 
         jids = []
         loop_interval = self.opts["loop_interval"]
