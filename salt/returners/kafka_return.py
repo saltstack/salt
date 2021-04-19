c47c25c8ed4a32e7af8153a21d41c3d04a6285d6
Zane Mingee <zmingee@gmail.com>
2021-04-19 17:14:35 -0400

    refactor(returners): extend kafka returner
    - Remove Python 2 support code
    - Add support for generic librdkafka configuration options
    - Change Kafka Producer key to Salt minion or master ID depending on
      context
    - Add event return support

--- salt/returners/kafka_return.py
+++ salt/returners/kafka_return.py
@@ -1,43 +1,55 @@
-# -*- coding: utf-8 -*-
-
 """
 Return data to a Kafka topic
 
-:maintainer: Justin Desilets (justin.desilets@gmail.com)
-:maturity: 20181119
-:depends: confluent-kafka
+:maintainer: Justin Desilets <justin.desilets@gmail.com>, Zane Mingee <zmingee@gmail.com>
+:maturity: stable
+:depends: ``confluent-kafka``
 :platform: all
 
-To enable this returner install confluent-kafka and enable the following
-settings in the minion config:
+The following options are enabled by default:
 
-    returner.kafka.bootstrap:
-      - "server1:9092"
-      - "server2:9092"
-      - "server3:9092"
+.. code-block:: yaml
 
-    returner.kafka.topic: 'topic'
+    kafka.topic: "saltstack"
+    kafka.socket.timeout.ms: 5000
+    kafka.socket.keepalive.enable: true
+    kafka.message.send.max.retries: 3
 
-To use the kafka returner, append `--return kafka` to the Salt command, eg;
+Configuration options are automatically passed to the Kafka Producer. Options
+must be prefixed with "``kafka.``". For a list of all options available, please
+refer to the `librdkafka documentation`_.
+
+.. _librdkafka documentation: https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
+
+To use the Kafka job returner, append `--return kafka` to the Salt command:
 
     salt '*' test.ping --return kafka
 
-"""
-from __future__ import absolute_import, print_function, unicode_literals
+To use the Kafka job returner for all job returns, the
+:conf_master:`ext_job_cache` or :conf_master:`master_job_cache` options may
+be set to ``kafka``. For more information, please see
+:ref:`external-job-cache`.
 
+To use the Kafka event returner, set the :conf_master:`event_return` option to
+``kafka``.
+
+"""
+
+import copy
 import logging
 
+import salt.returners
 import salt.utils.json
+import salt.utils.stringutils
 
-# Import third-party libs
 try:
-    from confluent_kafka import Producer
+    from confluent_kafka import Producer, KafkaException
 
     HAS_KAFKA = True
 except ImportError:
     HAS_KAFKA = False
 
-log = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
 
 __virtualname__ = "kafka"
@@ -52,44 +64,116 @@ def __virtual__():
     return __virtualname__
 
 
-def _get_conn():
+def _get_options(ret=None):
+    defaults = {
+        "topic": "saltstack",
+        "socket.timeout.ms": 5000,
+        "socket.keepalive.enable": True,
+        "message.send.max.retries": 3,
+    }
+    _options = copy.deepcopy(defaults)
+
+    cfg = __salt__.get("config.option", __opts__)
+    if isinstance(cfg, dict):
+        c_cfg = cfg
+    else:
+        c_cfg = cfg("{}.*".format(__virtualname__), wildcard=True)
+
+    ret_config = salt.returners._fetch_ret_config(ret)
+
+    for option in c_cfg:
+        key = option[len(__virtualname__ + ".") :]
+        value = salt.returners._fetch_option(c_cfg, ret_config, __virtualname__, option)
+
+        if isinstance(value, bool):
+            _options[key] = str(value).lower()
+        else:
+            _options[key] = value
+
+    LOGGER.debug("Kafka returner options:\n%s", _options)
+
+    return _options
+
+
+def _get_producer(_options=None):
     """
     Return a kafka connection
     """
-    if __salt__["config.option"]("returner.kafka.bootstrap"):
-        bootstrap = ",".join(__salt__["config.option"]("returner.kafka.bootstrap"))
-    else:
-        log.error("Unable to find kafka returner config option: bootstrap")
-        return None
-    return bootstrap
+
+    if _options is None:
+        _options = _get_options()
+
+    producer = Producer(**{k: v for (k, v) in _options.items() if v is not None})
+    producer.poll(_options["socket.timeout.ms"] / 1000)
+
+    return producer
 
 
 def _delivery_report(err, msg):
     """ Called once for each message produced to indicate delivery result.
         Triggered by poll() or flush(). """
     if err is not None:
-        log.error("Message delivery failed: %s", err)
+        LOGGER.error("Message delivery failed: %s", err)
     else:
-        log.debug("Message delivered to %s [%s]", msg.topic(), msg.partition())
+        LOGGER.debug("Message delivered to %s [%s]", msg.topic(), msg.partition())
 
 
 def returner(ret):
     """
     Return information to a Kafka server
     """
-    if __salt__["config.option"]("returner.kafka.topic"):
-        topic = __salt__["config.option"]("returner.kafka.topic")
+    _options = _get_options(ret)
+    topic = _options.pop("topic")
 
-        conn = _get_conn()
-        producer = Producer({"bootstrap.servers": conn})
-        producer.poll(0)
+    try:
+        producer = _get_producer(_options)
+    except KafkaException as exc:
+        if exc.args:
+            LOGGER.error("Failed to initialize Kafka producer: %s", exc.args[0].str())
+        else:
+            LOGGER.error("Failed to initialize Kafka producer")
+        return
+
+    try:
         producer.produce(
             topic,
             salt.utils.json.dumps(ret),
-            str(ret).encode("utf-8"),
+            ret["id"].encode("utf-8"),
             callback=_delivery_report,
         )
+        producer.flush(_options["socket.timeout.ms"] / 1000)
+    except (BufferError, KafkaException):
+        LOGGER.error("Failed to send return to Kafka")
 
-        producer.flush()
-    else:
-        log.error("Unable to find kafka returner config option: topic")
+
+def event_return(events):
+    """
+    Return event to Kafka server
+    """
+    _options = _get_options()
+    topic = _options.pop("topic")
+
+    try:
+        producer = _get_producer(_options)
+    except KafkaException as exc:
+        if exc.args:
+            LOGGER.error("Failed to initialize Kafka producer: %s", exc.args[0].str())
+        else:
+            LOGGER.error("Failed to initialize Kafka producer")
+        return
+
+    try:
+        for event in events:
+            tag = event.get("tag", "")
+            data = event.get("data", "")
+
+            producer.produce(
+                topic,
+                salt.utils.json.dumps(event),
+                __opts__["id"].encode("utf-8"),
+                callback=_delivery_report,
+            )
+
+        producer.flush(_options["socket.timeout.ms"] / 1000)
+    except (BufferError, KafkaException):
+        LOGGER.error("Failed to send event to Kafka")
