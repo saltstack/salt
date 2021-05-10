4b225c53606b0eac2f0f9564b8ead19b956e9d3a
Gareth J. Greenaway <gareth@saltstack.com>
2021-05-10 16:21:02 -0700

    Update the external ipaddress to the latest 3.9.5 version which has some security fixes.  Updating the compat.p to use the vendored version if the python version is below 3.9.5 and only run the test_ipaddress.py tests if below 3.9.5.

--- salt/_compat.py
+++ salt/_compat.py
@@ -4,9 +4,9 @@ Salt compatibility code
 # pylint: disable=unused-import
 import sys
 
-# The ipaddress module included in Salt is from Python 3.9.1.
+# The ipaddress module included in Salt is from Python 3.9.5.
 # When running from Py3.9.1+ use the standard library module, use ours otherwise
-if sys.version_info >= (3, 9, 1):
+if sys.version_info >= (3, 9, 5):
     import ipaddress
 else:
     import salt.ext.ipaddress as ipaddress
