b85e7abd08ae48676ac6c633d31f66da3e882e82
Gareth J. Greenaway <gareth@saltstack.com>
2021-05-11 11:44:43 -0700

    Requested changes.

--- salt/_compat.py
+++ salt/_compat.py
@@ -5,7 +5,7 @@ Salt compatibility code
 import sys
 
 # The ipaddress module included in Salt is from Python 3.9.5.
-# When running from Py3.9.1+ use the standard library module, use ours otherwise
+# When running from Py3.9.5+ use the standard library module, use ours otherwise
 if sys.version_info >= (3, 9, 5):
     import ipaddress
 else:
