66177742a6b2aadea8afbffd5f1578480a3d107e
twangboy <leesh@vmware.com>
2021-05-24 18:30:47 -0600

    pre-commit

--- tests/pytests/unit/modules/test_win_servermanager.py
+++ tests/pytests/unit/modules/test_win_servermanager.py
@@ -7,29 +7,28 @@ from tests.support.mock import MagicMock, patch
 
 @pytest.fixture
 def configure_loader_modules():
-    return {
-        win_servermanager: {
-            "__grains__": {"osversion": "6.2"}
-        }
-    }
+    return {win_servermanager: {"__grains__": {"osversion": "6.2"}}}
 
 
 def test_install():
+    """
+    Test win_servermanager.install
+    """
     mock_out = {
-        'Success': True,
-        'RestartNeeded': 1,
-        'FeatureResult': [
+        "Success": True,
+        "RestartNeeded": 1,
+        "FeatureResult": [
             {
-                'Id': 338,
-                'Name': 'XPS-Viewer',
-                'DisplayName': 'XPS Viewer',
-                'Success': True,
-                'RestartNeeded': False,
-                'Message': '',
-                'SkipReason': 0
+                "Id": 338,
+                "Name": "XPS-Viewer",
+                "DisplayName": "XPS Viewer",
+                "Success": True,
+                "RestartNeeded": False,
+                "Message": "",
+                "SkipReason": 0,
             }
         ],
-        'ExitCode': 0
+        "ExitCode": 0,
     }
     expected = {
         "ExitCode": 0,
@@ -41,34 +40,39 @@ def test_install():
                 "Message": "",
                 "RestartNeeded": False,
                 "SkipReason": 0,
-                "Success": True
+                "Success": True,
             }
         },
-        "Success": True}
+        "Success": True,
+    }
 
     mock_reboot = MagicMock(return_value=True)
-    with patch.object(win_servermanager, "_pshell_json", return_value=mock_out), \
-        patch.dict(win_servermanager.__salt__, {"system.reboot": mock_reboot}):
+    with patch.object(
+        win_servermanager, "_pshell_json", return_value=mock_out
+    ), patch.dict(win_servermanager.__salt__, {"system.reboot": mock_reboot}):
         result = win_servermanager.install("XPS-Viewer")
         assert result == expected
 
 
 def test_install_restart():
+    """
+    Test win_servermanager.install when restart=True
+    """
     mock_out = {
-        'Success': True,
-        'RestartNeeded': 1,
-        'FeatureResult': [
+        "Success": True,
+        "RestartNeeded": 1,
+        "FeatureResult": [
             {
-                'Id': 338,
-                'Name': 'XPS-Viewer',
-                'DisplayName': 'XPS Viewer',
-                'Success': True,
-                'RestartNeeded': True,
-                'Message': '',
-                'SkipReason': 0
+                "Id": 338,
+                "Name": "XPS-Viewer",
+                "DisplayName": "XPS Viewer",
+                "Success": True,
+                "RestartNeeded": True,
+                "Message": "",
+                "SkipReason": 0,
             }
         ],
-        'ExitCode': 0
+        "ExitCode": 0,
     }
     expected = {
         "ExitCode": 0,
@@ -80,14 +84,16 @@ def test_install_restart():
                 "Message": "",
                 "RestartNeeded": True,
                 "SkipReason": 0,
-                "Success": True
+                "Success": True,
             }
         },
-        "Success": True}
+        "Success": True,
+    }
 
     mock_reboot = MagicMock(return_value=True)
-    with patch.object(win_servermanager, "_pshell_json", return_value=mock_out), \
-            patch.dict(win_servermanager.__salt__, {"system.reboot": mock_reboot}):
+    with patch.object(
+        win_servermanager, "_pshell_json", return_value=mock_out
+    ), patch.dict(win_servermanager.__salt__, {"system.reboot": mock_reboot}):
         result = win_servermanager.install("XPS-Viewer", restart=True)
         mock_reboot.assert_called_once()
         assert result == expected
