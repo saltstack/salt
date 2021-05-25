31b422f313a16926263c82e417441156feabdb85
Charles DELFLY <c.delfly@linkbynet.com>
2021-05-25 11:06:44 +0700

    Add more test

--- tests/unit/auth/test_rest.py
+++ tests/unit/auth/test_rest.py
@@ -1,5 +1,6 @@
 import salt.auth.rest
 from tests.support.mixins import LoaderModuleMockMixin
+from tests.support.mock import patch
 from tests.support.unit import TestCase
 
 
@@ -14,5 +15,29 @@ class RestAuthTestCase(TestCase, LoaderModuleMockMixin):
 
 
     def test_rest_auth_setup(self):
-        ret = salt.auth.rest.rest_auth_setup()
+        ret = salt.auth.rest._rest_auth_setup()
         assert ret == "https://test_url/rest"
+
+    def test_auth_nopass(self):
+        self.assertFalse(salt.auth.rest.auth("foo", None))
+
+    def test_auth_nouser(self):
+        self.assertFalse(salt.auth.rest.auth(None, "foo"))
+
+    def test_auth_nouserandpass(self):
+        self.assertFalse(salt.auth.rest.auth(None, None))
+
+    def test_acl_without_merge(self):
+        self.assertIn(
+            '@runner', salt.auth.rest.acl("fred", password="password")
+        )
+
+    def test_acl_merge(self):
+        cached_acl = {'fred': ['@wheel']}
+        with patch.dict(salt.auth.rest.cached_acl, cached_acl):
+            self.assertIn(
+                '@wheel', salt.auth.rest.acl("fred", password="password")
+            )
+            self.assertIn(
+                '@runner', salt.auth.rest.acl("fred", password="password")
+            )
