d19de8d67fc5eea0fd44adb5f20fede9c143e171
Daniel A. Wozniak <dwozniak@saltstack.com>
2021-06-16 15:40:01 -0700

    Fix linter

--- tests/pytests/unit/engines/test_engines.py
+++ tests/pytests/unit/engines/test_engines.py
@@ -1,16 +1,17 @@
-import salt.engines
 import pytest
-from tests.support.mock import patch, MagicMock
+import salt.engines
+from tests.support.mock import MagicMock, patch
 
 
 def test_engine_module_name():
-    engine = salt.engines.Engine({}, 'foobar.start', {}, {}, {}, {})
-    assert engine.module_name == 'foobar'
+    engine = salt.engines.Engine('foobar', {}, 'foobar.start', {}, {}, {}, {})
+    assert engine.name == 'foobar'
+
 
 def test_engine_title_set():
-    engine = salt.engines.Engine({}, 'foobar.start', {}, {}, {}, {})
+    engine = salt.engines.Engine('foobar', {}, 'foobar.start', {}, {}, {}, {})
     with patch('salt.utils.process.appendproctitle', MagicMock()) as mm:
         with pytest.raises(KeyError):
             # The method does not exist so a KeyError will be raised.
             engine.run()
-        mm.assert_called_with('Engine: foobar')
+        mm.assert_called_with('foobar')
