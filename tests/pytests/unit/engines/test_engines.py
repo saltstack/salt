8bea64017cc985a46d23b08077bc96a2cf8dc422
Daniel A. Wozniak <dwozniak@saltstack.com>
2021-06-17 14:35:17 -0700

    Fix pre commit

--- tests/pytests/unit/engines/test_engines.py
+++ tests/pytests/unit/engines/test_engines.py
@@ -4,14 +4,14 @@ from tests.support.mock import MagicMock, patch
 
 
 def test_engine_module_name():
-    engine = salt.engines.Engine('foobar', {}, 'foobar.start', {}, {}, {}, {})
-    assert engine.name == 'foobar'
+    engine = salt.engines.Engine("foobar", {}, "foobar.start", {}, {}, {}, {})
+    assert engine.name == "foobar"
 
 
 def test_engine_title_set():
-    engine = salt.engines.Engine('foobar', {}, 'foobar.start', {}, {}, {}, {})
-    with patch('salt.utils.process.appendproctitle', MagicMock()) as mm:
+    engine = salt.engines.Engine("foobar", {}, "foobar.start", {}, {}, {}, {})
+    with patch("salt.utils.process.appendproctitle", MagicMock()) as mm:
         with pytest.raises(KeyError):
             # The method does not exist so a KeyError will be raised.
             engine.run()
-        mm.assert_called_with('foobar')
+        mm.assert_called_with("foobar")
