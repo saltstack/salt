c10cfa2d950d46e4a22c45665f6055d9a6ea421d
Gareth J. Greenaway <gareth@saltstack.com>
2021-07-01 09:27:14 -0700

    Removing roster files.  Updating test with requested changes.

--- tests/pytests/unit/roster/test_dir.py
+++ tests/pytests/unit/roster/test_dir.py
@@ -4,14 +4,11 @@ Test the directory roster.
 
 
 import logging
-import os
 
 import pytest
 import salt.config
 import salt.loader
 import salt.roster.dir as dir_
-from tests.support.paths import TESTS_DIR
-from tests.support.runtests import RUNTIME_VARS
 
 
 @pytest.fixture
@@ -46,9 +43,49 @@ def expected(roster_domain):
 
 
 @pytest.fixture
-def configure_loader_modules(roster_domain):
+def create_roster_files(tmp_path):
+    badfile_contents = """
+    #!jinja|yaml
+    host: 127.0.0.2
+    port: 22
+    THIS FILE IS NOT WELL FORMED YAML
+    sudo: true
+    user: scoundrel
+    """
 
-    opts = salt.config.master_config(os.path.join(RUNTIME_VARS.TMP_CONF_DIR, "master"))
+    basic_contents = """
+    #!jinja|yaml
+    host: 127.0.0.2
+    port: 22
+    sudo: true
+    user: scoundrel
+    """
+
+    domain_contents = """
+    #!jinja|yaml
+    port: 2222
+    user: george
+    """
+
+    empty_contents = """
+    """
+
+    with pytest.helpers.temp_file(
+        "test1_us-east-2_test_badfile", badfile_contents, directory=tmp_path
+    ), pytest.helpers.temp_file(
+        "test1_us-east-2_test_basic", basic_contents, directory=tmp_path
+    ), pytest.helpers.temp_file(
+        "test1_us-east-2_test_domain", domain_contents, directory=tmp_path
+    ), pytest.helpers.temp_file(
+        "test1_us-east-2_test_empty", empty_contents, directory=tmp_path
+    ):
+        yield
+
+
+@pytest.fixture
+def configure_loader_modules(roster_domain, salt_master_factory, tmp_path):
+
+    opts = salt_master_factory.config.copy()
     utils = salt.loader.utils(opts, whitelist=["json", "stringutils", "roster_matcher"])
     runner = salt.loader.runner(opts, utils=utils, whitelist=["salt"])
     return {
@@ -59,7 +96,7 @@ def configure_loader_modules(roster_domain):
                 "renderer": "jinja|yaml",
                 "renderer_blacklist": [],
                 "renderer_whitelist": [],
-                "roster_dir": os.path.join(TESTS_DIR, "unit/files/rosters/dir"),
+                "roster_dir": tmp_path,
                 "roster_domain": roster_domain,
             },
             "__runner__": runner,
@@ -83,28 +120,28 @@ def _test_match(ret, expected):
         ), "Data for minion {} did not match expectations".format(minion)
 
 
-def test_basic_glob(expected):
+def test_basic_glob(expected, create_roster_files):
     """Test that minion files in the directory roster match and render."""
     expected = expected["basic"]
     ret = dir_.targets("*_basic", saltenv="")
     _test_match(ret, expected)
 
 
-def test_basic_re(expected):
+def test_basic_re(expected, create_roster_files):
     """Test that minion files in the directory roster match and render."""
     expected = expected["basic"]
     ret = dir_.targets(".*basic$", "pcre", saltenv="")
     _test_match(ret, expected)
 
 
-def test_basic_list(expected):
+def test_basic_list(expected, create_roster_files):
     """Test that minion files in the directory roster match and render."""
     expected = expected["basic"]
     ret = dir_.targets(expected.keys(), "list", saltenv="")
     _test_match(ret, expected)
 
 
-def test_roster_domain(expected):
+def test_roster_domain(expected, create_roster_files):
     """Test that when roster_domain is configured, it will provide a default hostname
     in the roster of {filename}.{roster_domain}, so that users can use the minion
     id as the local hostname without having to supply the fqdn everywhere."""
@@ -113,32 +150,31 @@ def test_roster_domain(expected):
     _test_match(ret, expected)
 
 
-def test_empty(expected):
+def test_empty(expected, create_roster_files):
     """Test that an empty roster file matches its hostname"""
     expected = expected["empty"]
     ret = dir_.targets("*_empty", saltenv="")
     _test_match(ret, expected)
 
 
-def test_nomatch():
+def test_nomatch(create_roster_files):
     """Test that no errors happen when no files match"""
     try:
         ret = dir_.targets("", saltenv="")
-    except:
+    except:  # pylint: disable=bare-except
         pytest.fail(
             "No files matched, which is OK, but we raised an exception and we should not have."
         )
-        raise
     assert len(ret) == 0, "Expected empty target list to yield zero targets."
 
 
-def test_badfile():
+def test_badfile(create_roster_files):
     """Test error handling when we can't render a file"""
     ret = dir_.targets("*badfile", saltenv="")
     assert len(ret) == 0
 
 
-def test_badfile_logging(caplog):
+def test_badfile_logging(caplog, create_roster_files):
     """Test error handling when we can't render a file"""
     with caplog.at_level(logging.WARNING, logger="salt.roster.dir"):
         dir_.targets("*badfile", saltenv="")
