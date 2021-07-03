96081fb5a455150dc98b26e16205119c7ac19b1b
Daniel A. Wozniak <dwozniak@saltstack.com>
2021-07-03 01:36:46 -0700

    Fix warts in pyupgrade

--- salt/utils/azurearm.py
+++ salt/utils/azurearm.py
@@ -312,13 +312,13 @@ def compare_list_of_dicts(old, new, convert_id_to_name=None):
         ret["comment"] = 'configuration dictionaries must contain the "name" key!'
         return ret
 
-    for val in local_configs:
+    for idx,  val in enumerate(local_configs):
         for key in val:
             local_val = val[key]
             if key in convert_id_to_name:
-                remote_val = remote_configs.get(key, {}).get("id", "").split("/")[-1]
+                remote_val = remote_configs[idx].get(key, {}).get("id", "").split("/")[-1]
             else:
-                remote_val = remote_configs.get(key)
+                remote_val = remote_configs[idx].get(key)
                 if isinstance(local_val, str):
                     local_val = local_val.lower()
                 if isinstance(remote_val, str):
