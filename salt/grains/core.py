e5db672a9098a8509fb8abf17b5de2755300a937
Lukas Raska <lukas@raska.me>
2021-07-09 10:29:51 +0200

    Change "manufacture" grain to "manufacturer" for Solaris on SPARC

--- salt/grains/core.py
+++ salt/grains/core.py
@@ -2884,7 +2884,7 @@ def _hw_data(osdata):
             re.compile(r) for r in [r"(?im)^\s*Domain\s+UUID:\s*(\S+)"]  # virtinfo
         ]
 
-        manufacture_regexes = [
+        manufacturer_regexes = [
             re.compile(r)
             for r in [r"(?im)^\s*System\s+Configuration:\s*(.*)(?=sun)"]  # prtdiag
         ]
@@ -2953,10 +2953,10 @@ def _hw_data(osdata):
                 grains["uuid"] = res.group(1).strip().replace("'", "")
                 break
 
-        for regex in manufacture_regexes:
+        for regex in manufacturer_regexes:
             res = regex.search(data)
             if res and len(res.groups()) >= 1:
-                grains["manufacture"] = res.group(1).strip().replace("'", "")
+                grains["manufacturer"] = res.group(1).strip().replace("'", "")
                 break
 
         for regex in product_regexes:
