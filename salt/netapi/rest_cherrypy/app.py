f1e18048a8062aa00d50dd554df2317de15bbafa
Pedro Algarvio <pedro@algarvio.me>
2021-07-14 15:52:41 +0100

    Factor out sum and sorting of permissions into separate functions.
    Fixes #56495
    
    Additionally, the same logic was applied to the rest_cherrypy netapi

--- salt/netapi/rest_cherrypy/app.py
+++ salt/netapi/rest_cherrypy/app.py
@@ -1899,18 +1899,8 @@ class Login(LowDataAdapter):
             if token["eauth"] == "django" and "^model" in eauth:
                 perms = token["auth_list"]
             else:
-                # Get sum of '*' perms, user-specific perms, and group-specific perms
-                perms = eauth.get(token["name"], []).copy()
-                perms.extend(eauth.get("*", []))
-
-                if "groups" in token and token["groups"]:
-                    user_groups = set(token["groups"])
-                    eauth_groups = {
-                        i.rstrip("%") for i in eauth.keys() if i.endswith("%")
-                    }
-
-                    for group in user_groups & eauth_groups:
-                        perms.extend(eauth["{}%".format(group)])
+                perms = salt.netapi.sum_permissions(token, eauth)
+                perms = salt.netapi.sorted_permissions(perms)
 
             if not perms:
                 logger.debug("Eauth permission list not found.")
