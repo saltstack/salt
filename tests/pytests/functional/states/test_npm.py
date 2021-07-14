17cd8c7962d9a782dd08f3ebf98d345d4a752f14
Wayne Werner <wwerner@vmware.com>
2021-07-14 09:55:05 -0500

    switch to functional tests
    They're about 3x faster than the integration style testing

---
+++ tests/pytests/functional/states/test_npm.py
@@ -0,0 +1,33 @@
+import pytest
+
+
+@pytest.fixture(scope="module", autouse=True)
+def install_npm(sminion):
+    sminion.functions.pkg.install("npm")
+
+
+@pytest.mark.slow_test
+@pytest.mark.destructive_test
+@pytest.mark.requires_network
+def test_removed_installed_cycle(sminion):
+    project_version = "pm2@5.1.0"
+    success = sminion.functions.npm.uninstall("pm2")
+    assert success, "Unable to uninstall pm2 in prep for tests"
+
+    ret = next(
+        iter(
+            sminion.functions.state.single(
+                "npm.installed", name=project_version
+            ).values()
+        )
+    )
+    success = ret["result"]
+    assert success, "Failed to states.npm.installed " + project_version + ret["comment"]
+
+    ret = next(
+        iter(
+            sminion.functions.state.single("npm.removed", name=project_version).values()
+        )
+    )
+    success = ret["result"]
+    assert success, "Failed to states.npm.removed " + project_version
