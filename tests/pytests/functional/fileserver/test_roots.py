0baebdffe741fdb6d0441364c699b5c420c45066
Twangboy <shane.d.lee@gmail.com>
2021-07-23 09:54:29 -0600

    Fix pre-commit

--- tests/pytests/functional/fileserver/test_roots.py
+++ tests/pytests/functional/fileserver/test_roots.py
@@ -13,15 +13,15 @@ def configure_loader_modules(base_env_state_tree_root_dir):
     print(base_env_state_tree_root_dir)
     opts["file_roots"]["base"] = [str(base_env_state_tree_root_dir)]
     return {
-        roots: {
-            "__opts__": opts,
-        },
+        roots: {"__opts__": opts,},
     }
 
 
 # nox -e pytest-zeromq-3.8(coverage=False) -- -vvv --run-slow --run-destructive tests\pytests\functional\fileserver\test_roots.py
 def test_symlink_list(base_env_state_tree_root_dir):
-    with pytest.helpers.temp_file("target", "data", base_env_state_tree_root_dir) as target:
+    with pytest.helpers.temp_file(
+        "target", "data", base_env_state_tree_root_dir
+    ) as target:
         link = base_env_state_tree_root_dir / "link"
         link.symlink_to(str(target))
         ret = roots.symlink_list({"saltenv": "base"})
