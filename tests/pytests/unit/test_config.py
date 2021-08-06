6fd01155247c55e52427de9e047811cb3acdd6b9
Daniel A. Wozniak <dwozniak@saltstack.com>
2021-08-06 16:38:10 -0700

    fix test

--- tests/pytests/unit/test_config.py
+++ tests/pytests/unit/test_config.py
@@ -13,8 +13,8 @@ def test_call_id_function(tmp_path):
     extmods = tmp_path / "extmods"
     opts = {
         "id_function": {"grains.get": {"key": "osfinger"}},
-        "cachedir": cache_dir,
-        "extension_modules": extmods,
+        "cachedir": str(cache_dir),
+        "extension_modules": str(extmods),
         "grains": {"osfinger": "meh"},
         "optimization_order": [0],
     }
