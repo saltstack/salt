85b3896ed0dccd1fa7f6960857559c04c7b7d8dd
Gareth J. Greenaway <gareth@saltstack.com>
2021-08-09 11:26:47 -0700

    removing unneeded globals.  Swapping out needed globals for fixtures.

--- tests/pytests/unit/beacons/test_service.py
+++ tests/pytests/unit/beacons/test_service.py
@@ -1,16 +1,10 @@
 # Python libs
 
-from collections import namedtuple
-
 import pytest
 
 # Salt libs
 import salt.beacons.service as service_beacon
 from tests.support.mock import MagicMock, patch
-
-PATCH_OPTS = dict(autospec=True, spec_set=True)
-
-FakeProcess = namedtuple("Process", "cmdline pid")
 
 
 @pytest.fixture
--- tests/pytests/unit/beacons/test_smartos_imgadm.py
+++ tests/pytests/unit/beacons/test_smartos_imgadm.py
@@ -5,42 +5,56 @@ import pytest
 import salt.beacons.smartos_imgadm as imgadm
 from tests.support.mock import MagicMock, patch
 
-# Mock Results
-MOCK_CLEAN_STATE = {"first_run": True, "images": []}
-MOCK_IMAGE_NONE = {}
-MOCK_IMAGE_ONE = {
-    "00000000-0000-0000-0000-000000000001": {
-        "description": "Example Image 1",
-        "name": "example-1",
-        "os": "smartos",
-        "published": "2018-01-01T00:42:00Z",
-        "source": "https://images.joyent.com",
-        "version": "18.1.0",
-    },
-}
-MOCK_IMAGE_TWO = {
-    "00000000-0000-0000-0000-000000000001": {
-        "description": "Example Image 1",
-        "name": "example-1",
-        "os": "smartos",
-        "published": "2018-01-01T00:42:00Z",
-        "source": "https://images.joyent.com",
-        "version": "18.1.0",
-    },
-    "00000000-0000-0000-0000-000000000002": {
-        "description": "Example Image 2",
-        "name": "example-2",
-        "os": "smartos",
-        "published": "2018-01-01T00:42:00Z",
-        "source": "https://images.joyent.com",
-        "version": "18.2.0",
-    },
-}
-
 
 @pytest.fixture
 def configure_loader_modules():
     return {imgadm: {"__context__": {}, "__salt__": {}}}
+
+
+@pytest.fixture
+def mock_clean_state():
+    return {"first_run": True, "vms": []}
+
+
+@pytest.fixture
+def mock_image_none():
+    return {}
+
+
+@pytest.fixture
+def mock_image_one():
+    return {
+        "00000000-0000-0000-0000-000000000001": {
+            "description": "Example Image 1",
+            "name": "example-1",
+            "os": "smartos",
+            "published": "2018-01-01T00:42:00Z",
+            "source": "https://images.joyent.com",
+            "version": "18.1.0",
+        },
+    }
+
+
+@pytest.fixture
+def mock_image_two():
+    return {
+        "00000000-0000-0000-0000-000000000001": {
+            "description": "Example Image 1",
+            "name": "example-1",
+            "os": "smartos",
+            "published": "2018-01-01T00:42:00Z",
+            "source": "https://images.joyent.com",
+            "version": "18.1.0",
+        },
+        "00000000-0000-0000-0000-000000000002": {
+            "description": "Example Image 2",
+            "name": "example-2",
+            "os": "smartos",
+            "published": "2018-01-01T00:42:00Z",
+            "source": "https://images.joyent.com",
+            "version": "18.2.0",
+        },
+    }
 
 
 def test_non_list_config():
@@ -53,13 +67,13 @@ def test_non_list_config():
     )
 
 
-def test_imported_startup():
+def test_imported_startup(mock_clean_state, mock_image_one):
     """
     Test with one image and startup_import_event
     """
     # NOTE: this should yield 1 imported event
-    with patch.dict(imgadm.IMGADM_STATE, MOCK_CLEAN_STATE), patch.dict(
-        imgadm.__salt__, {"imgadm.list": MagicMock(return_value=MOCK_IMAGE_ONE)}
+    with patch.dict(imgadm.IMGADM_STATE, mock_clean_state), patch.dict(
+        imgadm.__salt__, {"imgadm.list": MagicMock(return_value=mock_image_one)}
     ):
 
         config = [{"startup_import_event": True}]
@@ -80,13 +94,13 @@ def test_imported_startup():
         assert ret == res
 
 
-def test_imported_nostartup():
+def test_imported_nostartup(mock_clean_state, mock_image_one):
     """
     Test with one image and startup_import_event unset/false
     """
     # NOTE: this should yield 0 imported event
-    with patch.dict(imgadm.IMGADM_STATE, MOCK_CLEAN_STATE), patch.dict(
-        imgadm.__salt__, {"imgadm.list": MagicMock(return_value=MOCK_IMAGE_ONE)}
+    with patch.dict(imgadm.IMGADM_STATE, mock_clean_state), patch.dict(
+        imgadm.__salt__, {"imgadm.list": MagicMock(return_value=mock_image_one)}
     ):
 
         config = []
@@ -95,14 +109,14 @@ def test_imported_nostartup():
         assert imgadm.beacon(config) == []
 
 
-def test_imported():
+def test_imported(mock_clean_state, mock_image_one, mock_image_two):
     """
     Test with one image and a new image added on the 2nd pass
     """
     # NOTE: this should yield 1 imported event
-    with patch.dict(imgadm.IMGADM_STATE, MOCK_CLEAN_STATE), patch.dict(
+    with patch.dict(imgadm.IMGADM_STATE, mock_clean_state), patch.dict(
         imgadm.__salt__,
-        {"imgadm.list": MagicMock(side_effect=[MOCK_IMAGE_ONE, MOCK_IMAGE_TWO])},
+        {"imgadm.list": MagicMock(side_effect=[mock_image_one, mock_image_two])},
     ):
 
         config = []
@@ -128,14 +142,14 @@ def test_imported():
         assert ret == res
 
 
-def test_deleted():
+def test_deleted(mock_clean_state, mock_image_one, mock_image_two):
     """
     Test with two images and one gets deletes
     """
     # NOTE: this should yield 1 deleted event
-    with patch.dict(imgadm.IMGADM_STATE, MOCK_CLEAN_STATE), patch.dict(
+    with patch.dict(imgadm.IMGADM_STATE, mock_clean_state), patch.dict(
         imgadm.__salt__,
-        {"imgadm.list": MagicMock(side_effect=[MOCK_IMAGE_TWO, MOCK_IMAGE_ONE])},
+        {"imgadm.list": MagicMock(side_effect=[mock_image_two, mock_image_one])},
     ):
 
         config = []
@@ -161,16 +175,16 @@ def test_deleted():
         assert ret == res
 
 
-def test_complex():
+def test_complex(mock_clean_state, mock_image_one, mock_image_two, mock_image_none):
     """
     Test with one image, delete both, import 2
     """
     # NOTE: this should yield 1 delete and 2 import events
-    with patch.dict(imgadm.IMGADM_STATE, MOCK_CLEAN_STATE), patch.dict(
+    with patch.dict(imgadm.IMGADM_STATE, mock_clean_state), patch.dict(
         imgadm.__salt__,
         {
             "imgadm.list": MagicMock(
-                side_effect=[MOCK_IMAGE_ONE, MOCK_IMAGE_NONE, MOCK_IMAGE_TWO]
+                side_effect=[mock_image_one, mock_image_none, mock_image_two]
             )
         },
     ):
