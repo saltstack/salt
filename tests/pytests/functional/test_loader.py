58be9aeff2a9eb4e3cdfa24713a3f4196b156171
Pedro Algarvio <pedro@algarvio.me>
2021-08-16 19:07:48 +0100

    Don't inject salt extensions utils into `__utils__`

--- tests/pytests/functional/test_loader.py
+++ tests/pytests/functional/test_loader.py
@@ -72,36 +72,6 @@ def test_new_entry_points_passing_func_returning_a_dict(
     import salt.loader
 
     minion_config = json.loads(sys.stdin.read())
-    loader = salt.loader.wheels(minion_config)
-    print(json.dumps(list(loader)))
-    """
-    ret = venv.run_code(code, input=json.dumps(salt_minion_factory.config.copy()))
-    loader_functions = json.loads(ret.stdout)
-
-    # A non existing module should not appear in the loader
-    assert "monty.python" not in loader_functions
-
-    # But our extension's modules should appear on the loader
-    assert "foobar.echo1" in loader_functions
-    assert "foobar.echo2" in loader_functions
-
-
-def test_old_entry_points_returning_a_list(venv, salt_extension, salt_minion_factory):
-    # Install our extension into the virtualenv
-    venv.install(str(salt_extension.srcdir))
-    installed_packages = venv.get_installed_packages()
-    assert salt_extension.name in installed_packages
-    code = """
-    import sys
-    import json
-
-    # If the test fails, for debugging purposes, comment out the following 2 lines
-    #import salt.log.setup
-    #salt.log.setup.setup_console_logger(log_level="debug")
-
-    import salt.loader
-
-    minion_config = json.loads(sys.stdin.read())
     loader = salt.loader.runner(minion_config)
     print(json.dumps(list(loader)))
     """
@@ -146,3 +116,34 @@ def test_old_entry_points_yielding_paths(venv, salt_extension, salt_minion_facto
 
     # But our extension's modules should appear on the loader
     assert "foobar.echoed" in loader_functions
+
+
+def test_utils_loader_does_not_load_extensions(
+    venv, salt_extension, salt_minion_factory
+):
+    # Install our extension into the virtualenv
+    venv.install(str(salt_extension.srcdir))
+    installed_packages = venv.get_installed_packages()
+    assert salt_extension.name in installed_packages
+    code = """
+    import sys
+    import json
+
+    # If the test fails, for debugging purposes, comment out the following 2 lines
+    #import salt.log.setup
+    #salt.log.setup.setup_console_logger(log_level="debug")
+
+    import salt.loader
+
+    minion_config = json.loads(sys.stdin.read())
+    loader = salt.loader.utils(minion_config)
+    print(json.dumps(list(loader)))
+    """
+    ret = venv.run_code(code, input=json.dumps(salt_minion_factory.config.copy()))
+    loader_functions = json.loads(ret.stdout)
+
+    # A non existing module should not appear in the loader
+    assert "monty.python" not in loader_functions
+
+    # But our extension's modules should appear on the loader
+    assert "foobar.echo" not in loader_functions
