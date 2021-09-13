saltstack/salt

commit 76e50885b07621e9e4c16bc3f1ebc16c93983b90

Fix strings

salt/modules/cmdmod.py
@@ -139,8 +139,7 @@ def _render_cmd(
     # render the path as a template using path_template_engine as the engine
     if template not in salt.utils.templates.TEMPLATE_REGISTRY:
         raise CommandExecutionError(
-            "Attempted to render file paths with unavailable engine "
-            "{}".format(template)
+            "Attempted to render file paths with unavailable engine {}".format(template)
         )
 
     kwargs = {}
@@ -488,10 +487,9 @@ def _run(
             marker = "<<<" + str(uuid.uuid4()) + ">>>"
             marker_b = marker.encode(__salt_system_encoding__)
             py_code = (
-                "import sys, os, itertools; "
-                'sys.stdout.write("' + marker + '"); '
-                'sys.stdout.write("\\0".join(itertools.chain(*os.environ.items()))); '
-                'sys.stdout.write("' + marker + '");'
+                "import sys, os, itertools; sys.stdout.write('{0}''); "
+                "sys.stdout.write('\\0'.join(itertools.chain(*os.environ.items()))); "
+                "sys.stdout.write('{0}');".format(marker)
             )
 
             if use_sudo:
@@ -702,7 +700,9 @@ def _run(
                 proc = salt.utils.timed_subprocess.TimedProc(cmd, **new_kwargs)
             except OSError as exc:
                 msg = "Unable to run command '{}' with the context '{}', reason: {}".format(
-                    cmd if output_loglevel is not None else "REDACTED", new_kwargs, exc
+                    cmd if output_loglevel is not None else "REDACTED",
+                    new_kwargs,
+                    exc,
                 )
                 raise CommandExecutionError(msg)
 
@@ -825,7 +825,7 @@ def _run(
                         else:
                             stderr = ""
                         if timeout and (time.time() > will_timeout):
-                            ret["stderr"] = ("SALT: Timeout after {}s\n{}").format(
+                            ret["stderr"] = "SALT: Timeout after {}s\n{}".format(
                                 timeout, stderr
                             )
                             ret["retcode"] = None
@@ -3650,8 +3650,9 @@ def shell_info(shell, list_modules=False):
         pw_keys.sort(key=int)
         if not pw_keys:
             return {
-                "error": "Unable to locate 'powershell' Reason: Cannot be "
-                "found in registry.",
+                "error": (
+                    "Unable to locate 'powershell' Reason: Cannot be found in registry."
+                ),
                 "installed": False,
             }
         for reg_ver in pw_keys:
@@ -3666,8 +3667,9 @@ def shell_info(shell, list_modules=False):
             ):
                 details = salt.utils.win_reg.list_values(
                     hive="HKEY_LOCAL_MACHINE",
-                    key="Software\\Microsoft\\PowerShell\\{}\\"
-                    "PowerShellEngine".format(reg_ver),
+                    key="Software\\Microsoft\\PowerShell\\{}\\PowerShellEngine".format(
+                        reg_ver
+                    ),
                 )
 
                 # reset data, want the newest version details only as powershell
@@ -3700,8 +3702,11 @@ def shell_info(shell, list_modules=False):
     else:
         if shell not in regex_shells:
             return {
-                "error": "Salt does not know how to get the version number for "
-                "{}".format(shell),
+                "error": (
+                    "Salt does not know how to get the version number for {}".format(
+                        shell
+                    )
+                ),
                 "installed": None,
             }
         shell_data = regex_shells[shell]

commit d3634a67a998cc0d9243742a5b5ecbf9700c538b

Fix bug introduced in 76e50885b07621e9e4c16bc3f1ebc16c93983b90

salt/modules/cmdmod.py
@@ -488,7 +488,7 @@ def _run(
             marker = "<<<" + str(uuid.uuid4()) + ">>>"
             marker_b = marker.encode(__salt_system_encoding__)
             py_code = (
-                "import sys, os, itertools; sys.stdout.write('{0}''); "
+                "import sys, os, itertools; sys.stdout.write('{0}'); "
                 "sys.stdout.write('\\0'.join(itertools.chain(*os.environ.items()))); "
                 "sys.stdout.write('{0}');".format(marker)
             )