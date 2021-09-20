e811078529b2d6c7cebc5088b8f4c81cafa3eb7f
Wayne Werner <wwerner@vmware.com>
2021-09-20 17:41:02 -0500

    Remove the leftover uses
    There were a few places that didn't show up on previous searches. Also
    fix the changelong.

--- salt/config/__init__.py
+++ salt/config/__init__.py
@@ -1571,8 +1571,6 @@ DEFAULT_MASTER_OPTS = immutabletypes.freeze(
         "http_connect_timeout": 20.0,  # tornado default - 20 seconds
         "http_request_timeout": 1 * 60 * 60.0,  # 1 hour
         "http_max_body": 100 * 1024 * 1024 * 1024,  # 100GB
-        "python2_bin": "python2",
-        "python3_bin": "python3",
         "cache": "localfs",
         "memcache_expire_seconds": 0,
         "memcache_max_items": 1024,
