ab35f8330028bcd154c5b3bf5b621372c1810115
colttt <shadow_7@gmx.net>
2021-09-21 14:02:27 +0200

    fix issue with wrong group fromat if you update a host
    should fix https://github.com/saltstack/salt/issues/60795

--- salt/states/zabbix_host.py
+++ salt/states/zabbix_host.py
@@ -195,6 +195,7 @@ def present(host, groups, interfaces, **kwargs):
 
     # Ensure groups are all groupid
     groupids = []
+    groupid_dic = []
     for group in groups:
         if isinstance(group, str):
             groupid = __salt__["zabbix.hostgroup_get"](name=group, **connection_args)
@@ -205,6 +206,7 @@ def present(host, groups, interfaces, **kwargs):
                 return ret
         else:
             groupids.append(group)
+            groupid_dic.append({"groupid": group})
     groups = groupids
 
     # Get and validate proxyid
@@ -412,7 +414,7 @@ def present(host, groups, interfaces, **kwargs):
                     error.append(hostupdate["error"])
             if update_hostgroups:
                 hostupdate = __salt__["zabbix.host_update"](
-                    hostid, groups=groups, **connection_args
+                    hostid, groups=groupid_dic, **connection_args
                 )
                 ret["changes"]["groups"] = str(groups)
                 if "error" in hostupdate:
