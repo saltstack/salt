5975002eeb848f1067c96704a56dcd255464b5f2
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2021-09-30 11:06:09 +0100

    Do not break master_tops for minion with version lower to 3003

--- salt/master.py
+++ salt/master.py
@@ -1170,6 +1170,7 @@ class AESFuncs(TransportMethods):
         "_dir_list",
         "_symlink_list",
         "_file_envs",
+        "_ext_nodes",
     )
 
     def __init__(self, opts):
@@ -1367,6 +1368,9 @@ class AESFuncs(TransportMethods):
         if load is False:
             return {}
         return self.masterapi._master_tops(load, skip_verify=True)
+
+    # Needed so older minions can request master_tops
+    _ext_nodes = _master_tops
 
     def _master_opts(self, load):
         """
