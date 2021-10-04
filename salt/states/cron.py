021c7d00c24c395427e4caffe6fbb382cc735f5a
Alan Somers <asomers@gmail.com>
2021-10-04 12:17:11 -0600

    Allow cron.present to change a timespec from non-special to special
    Previously when changing a cron.present job from using a normal time
    specification to special or vice-versa, it would duplicate the entry in
    the crontab.

--- salt/states/cron.py
+++ salt/states/cron.py
@@ -355,6 +355,9 @@ def present(
         return ret
 
     if special is None:
+        antidata = __salt__["cron.rm_special"](
+            user, name, special=special, identifier=identifier
+        )
         data = __salt__["cron.set_job"](
             user=user,
             minute=minute,
@@ -368,6 +371,7 @@ def present(
             identifier=identifier,
         )
     else:
+        antidata = __salt__["cron.rm_job"](user, name, identifier=identifier)
         data = __salt__["cron.set_special"](
             user=user,
             special=special,
@@ -376,14 +380,22 @@ def present(
             commented=commented,
             identifier=identifier,
         )
-    if data == "present":
+
+    if data == "present" and antidata == "removed":
+        ret["comment"] = "Duplicate Cron {} removed".format(name)
+        return ret
+
+    if data == "present" and antidata == "absent":
         ret["comment"] = "Cron {} already present".format(name)
         return ret
 
-    if data == "new":
+    if data == "new" and antidata == "absent":
         ret["comment"] = "Cron {} added to {}'s crontab".format(name, user)
         ret["changes"] = {user: name}
         return ret
+
+    if data == "new" and antidata == "removed":
+        ret["comment"] = "Cron {} time specification changed".format(name)
 
     if data == "updated":
         ret["comment"] = "Cron {} updated".format(name)
