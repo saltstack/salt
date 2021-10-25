130cfa5a0a8f2b5b39e9371692da11b1d137cc2d
Pedro Algarvio <palgarvio@vmware.com>
2021-10-25 11:42:30 +0100

    Migrate ``rest_cherrypy`` tests to pytest

--- tests/pytests/functional/netapi/rest_tornado/conftest.py
+++ tests/pytests/functional/netapi/rest_tornado/conftest.py
@@ -3,13 +3,21 @@ import tests.support.netapi as netapi
 
 
 @pytest.fixture
+def client_config(client_config, netapi_port):
+    client_config["rest_tornado"] = {"port": netapi_port}
+    return client_config
+
+
+@pytest.fixture
 def app(app_urls, load_auth, client_config, minion_config):
     return netapi.build_tornado_app(app_urls, load_auth, client_config, minion_config)
 
 
 @pytest.fixture
-def http_server(io_loop, app):
-    with netapi.TestsTornadoHttpServer(io_loop=io_loop, app=app) as server:
+def http_server(io_loop, app, netapi_port):
+    with netapi.TestsTornadoHttpServer(
+        io_loop=io_loop, app=app, port=netapi_port
+    ) as server:
         yield server
 
 
