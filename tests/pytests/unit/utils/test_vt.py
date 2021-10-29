80fac1dd4fb03c2acf578f5473db52c7dc2c0a1b
Thomas Phipps <whytewolf@vmware.com>
2021-10-29 21:01:10 +0000

    trying to fix test to work in jenkins enviroment

--- tests/pytests/unit/utils/test_vt.py
+++ tests/pytests/unit/utils/test_vt.py
@@ -6,9 +6,12 @@ import salt.utils.vt as vt
 
 def test_isalive_no_child():
     term = vt.Terminal(
-        "echo 'Alive'", shell=True, stream_stdout=False, stream_stderr=False
+        "for i in {1..9}; do echo $i;sleep $i; done",
+        shell=True,
+        stream_stdout=False,
+        stream_stderr=False,
     )
-    buffer_o = buffer_e = ""
+    # make sure we have a valid term before we kill the term
     assert term.isalive() is True
     # use a large hammer to make sure pid is really dead which will cause it to
     # raise an exception that wewant to test for.
