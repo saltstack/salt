143ecdc6407ab3fac677f74cbf33baba1e856065
Thomas Phipps <whytewolf@vmware.com>
2021-10-29 17:30:58 +0000

    run pre-commit

--- tests/pytests/unit/utils/test_vt.py
+++ tests/pytests/unit/utils/test_vt.py
@@ -1,15 +1,17 @@
-
 import os
 import signal
-import pytests
 
 import salt.utils.vt as vt
 
 
 def test_isalive_no_child():
-    term = vt.Terminal("echo 'Alive'", shell=True, stream_stdout=False, stream_stderr=False)
+    term = vt.Terminal(
+        "echo 'Alive'", shell=True, stream_stdout=False, stream_stderr=False
+    )
     buffer_o = buffer_e = ""
-    assert term.isalive() == True
-    os.kill(term.pid,signal.SIGKILL)
-    os.waitpid(term.pid,0)
-    assert term.isalive() == False
+    assert term.isalive() is True
+    # use a large hammer to make sure pid is really dead which will cause it to
+    # raise an exception that wewant to test for.
+    os.kill(term.pid, signal.SIGKILL)
+    os.waitpid(term.pid, 0)
+    assert term.isalive() is False
