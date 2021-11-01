3f32376ae2c79a040612f5d670d8c266c51fd294
Thomas Phipps <tphipps@vmware.com>
2021-11-01 20:03:26 +0000

    salt.utils.vt.Terminal looks like it doesn't run on windows. as windows is lacking _spawn

--- tests/pytests/unit/utils/test_vt.py
+++ tests/pytests/unit/utils/test_vt.py
@@ -2,28 +2,17 @@ import os
 import signal
 
 import pytest
-import salt.utils.platform
 import salt.utils.vt as vt
 
 
-@pytest.mark.slow_test
+@pytest.mark.skip_on_windows(reason="salt.utils.vt.Terminal doesn't have _spawn.")
 def test_isalive_no_child():
-    if salt.utils.platform.is_windows():
-        term = vt.Terminal(
-            "timeout 10",
-            shell=True,
-            stream_stdout=False,
-            stream_stderr=False,
-            rows=40,
-            cols=80,
-        )
-    else:
-        term = vt.Terminal(
-            "for i in {1..9}; do echo $i;sleep $i; done",
-            shell=True,
-            stream_stdout=False,
-            stream_stderr=False,
-        )
+    term = vt.Terminal(
+        "for i in {1..9}; do echo $i;sleep $i; done",
+        shell=True,
+        stream_stdout=False,
+        stream_stderr=False,
+    )
 
     # make sure we have a valid term before we kill the term
     # commenting out for now, terminal seems to be stopping before this point
