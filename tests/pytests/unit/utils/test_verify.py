c1b7ed1c48102a8fd7c0e875d39b3a5b07d63089
Lukas Raska <lukas@raska.me>
2021-11-10 08:18:10 +0100

    Try/except for os.chown as well

--- tests/pytests/unit/utils/test_verify.py
+++ tests/pytests/unit/utils/test_verify.py
@@ -6,8 +6,9 @@ from tests.support.mock import patch
 
 
 @pytest.mark.skip_on_windows(reason="Not applicable for Windows.")
+@patch("os.chown")
 @patch("os.stat")
-def test_verify_env_race_condition(mock_stat):
+def test_verify_env_race_condition(mock_stat, mock_chown):
     def _stat(path):
         """
         Helper function for mock_stat, we want to raise errors for specific paths, but not until we get into the proper path.
@@ -20,9 +21,23 @@ def test_verify_env_race_condition(mock_stat):
             raise FileNotFoundError(
                 "[Errno 2] No such file or directory: this exception should not be visible"
             )
+
+        # we need to return at least different st_uid in order to trigger chown for these paths
+        if path in ("/tmp/salt-dir/file4", "/tmp/salt-dir/dir4"):
+            return os.stat_result([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+
         return os.stat_result([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
+    def _chown(path, uid, gid):
+        if path in ("/tmp/salt-dir/file4", "/tmp/salt-dir/dir4"):
+            raise FileNotFoundError(
+                "[Errno 2] No such file or directory: this exception should not be visible"
+            )
+
+        return
+
     mock_stat.side_effect = _stat
+    mock_chown.side_effect = _chown
 
     with patch("salt.utils.verify._get_pwnam", return_value=(None, None, 0, 0)), patch(
         "os.getuid", return_value=0
@@ -31,7 +46,11 @@ def test_verify_env_race_condition(mock_stat):
     ), patch(
         "salt.utils.path.os_walk",
         return_value=[
-            ("/tmp/salt-dir", ["dir1", "dir2", ".dir3"], ["file1", "file2", ".file3"])
+            (
+                "/tmp/salt-dir",
+                ["dir1", "dir2", ".dir3", "dir4"],
+                ["file1", "file2", ".file3", "file4"],
+            )
         ],
     ):
 
@@ -41,3 +60,9 @@ def test_verify_env_race_condition(mock_stat):
         # and verify it got actually called with the valid paths
         mock_stat.assert_any_call("/tmp/salt-dir/file1")
         mock_stat.assert_any_call("/tmp/salt-dir/dir1")
+
+        mock_stat.assert_any_call("/tmp/salt-dir/file4")
+        mock_stat.assert_any_call("/tmp/salt-dir/dir4")
+
+        mock_chown.assert_any_call("/tmp/salt-dir/file4", 0, 0)
+        mock_chown.assert_any_call("/tmp/salt-dir/dir4", 0, 0)
