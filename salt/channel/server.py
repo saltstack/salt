485ca2ff804b7a21bc2cb2c2acbc259f1c2e65dc
Daniel A. Wozniak <dwozniak@saltstack.com>
2021-11-26 14:48:17 -0700

    Add message logging

--- salt/channel/server.py
+++ salt/channel/server.py
@@ -819,7 +819,7 @@ class PubServerChannel:
         """
         payload = self.wrap_payload(load)
         log.debug(
-            "Sending payload to publish daemon. jid=%s",
-            load.get("jid", None),
+            "Sending payload to publish daemon. jid=%s load=%r",
+            load.get("jid", None), load
         )
         self.transport.publish(payload)
