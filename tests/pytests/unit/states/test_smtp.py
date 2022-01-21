e61118a3e9285e766cdc6f97db6de1e36abe3964
Priyank <piyurupareliya@gmail.com>
2022-01-21 08:11:00 +0530

    Added unit test for testing html property

--- tests/pytests/unit/states/test_smtp.py
+++ tests/pytests/unit/states/test_smtp.py
@@ -45,3 +45,40 @@ def test_send_msg():
                 "admin@example.com",
                 "my-smtp-account",
             )
+
+
+def test_send_msg_html():
+    """
+    Test to send a message via SMTP with is_html=True
+    """
+    name = "This is a salt states module"
+
+    comt = "Need to send message to admin@example.com: This is a salt states module"
+
+    ret = {"name": name, "changes": {}, "result": None, "comment": comt}
+
+    with patch.dict(smtp.__opts__, {"test": True}):
+        assert ret == smtp.send_msg(
+            name,
+            "admin@example.com",
+            "Message from Salt",
+            "admin@example.com",
+            "my-smtp-account",
+            is_html=True,
+        )
+
+    comt = "Sent message to admin@example.com: This is a salt states module"
+
+    with patch.dict(smtp.__opts__, {"test": False}):
+        mock = MagicMock(return_value=True)
+        with patch.dict(smtp.__salt__, {"smtp.send_msg": mock}):
+            ret["comment"] = comt
+            ret["result"] = True
+            assert ret == smtp.send_msg(
+                name,
+                "admin@example.com",
+                "Message from Salt",
+                "admin@example.com",
+                "my-smtp-account",
+                is_html=True,
+            )
