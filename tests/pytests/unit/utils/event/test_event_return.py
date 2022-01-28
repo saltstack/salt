8bedf8b30ecee8b328ebab31a86e1f0e0c4c2273
MKLeb <calebb@vmware.com>
2022-01-28 16:02:26 -0500

    port tests and add deprecation warning

---
+++ tests/pytests/unit/utils/event/test_event_return.py
@@ -0,0 +1,28 @@
+"""
+    :codeauthor: Pedro Algarvio (pedro@algarvio.me)
+
+
+    tests.unit.utils.event_test
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+
+import pytest
+import salt.config
+import salt.ext.tornado.ioloop
+import salt.utils.event
+import salt.utils.stringutils
+from saltfactories.utils.processes import terminate_process
+
+
+@pytest.mark.slow_test
+def test_event_return():
+    evt = None
+    try:
+        evt = salt.utils.event.EventReturn(salt.config.DEFAULT_MASTER_OPTS.copy())
+        evt.start()
+    except TypeError as exc:
+        if "object" in str(exc):
+            pytest.fail("'{}' TypeError should have not been raised".format(exc))
+    finally:
+        if evt is not None:
+            terminate_process(evt.pid, kill_children=True)
