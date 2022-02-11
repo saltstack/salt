570857f269da3b762b87a3d47199ef9b07caa5cd
MKLeb <calebb@vmware.com>
2022-02-11 15:51:56 -0500

    change to run git.latest from minion module rather than directly

---
+++ tests/pytests/functional/runners/test_winrepo.py
@@ -0,0 +1,54 @@
+from tests.support.mock import Mock, patch
+import pytest
+from salt.runners import winrepo
+import salt.utils.gitfs
+
+
+pytestmark = [
+    pytest.mark.windows_whitelisted
+]
+
+
+@pytest.fixture
+def configure_loader_modules(minion_opts):
+    return {
+        winrepo: {
+            "__opts__": minion_opts,
+        }
+    }
+
+
+@pytest.fixture
+def winrepo_remotes(minion_opts):
+    winrepo_remotes = minion_opts.get("winrepo_remotes", [])
+    winrepo_remotes_ng = minion_opts.get("winrepo_remotes_ng", [])
+    winrepo_remotes.extend(winrepo_remotes_ng)
+    return winrepo_remotes
+
+
+def test_update_git_repos(winrepo_remotes):
+    """
+    Ensure update git repos works as intended.
+    """
+    res = winrepo.update_git_repos()
+
+    assert res
+
+    for remote in winrepo_remotes:
+        assert remote in res
+        assert res[remote]
+
+
+def test_legacy_update_git_repos(winrepo_remotes):
+    """
+    Ensure update git repos works as intended with legacy (non-gitfs) code.
+    """
+    with patch.object(salt.utils.gitfs, "GITPYTHON_VERSION", False):
+        with patch.object(salt.utils.gitfs, "PYGIT2_VERSION", False):
+            res = winrepo.update_git_repos()
+
+            assert res
+
+            for remote in winrepo_remotes:
+                assert remote in res
+                assert res[remote]
