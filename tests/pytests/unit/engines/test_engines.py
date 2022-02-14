2b48d5cefd54f7f3c81cca3c1111a0a21900adeb
Pedro Algarvio <palgarvio@vmware.com>
2022-02-14 07:25:27 +0000

    The ``KeyError`` is logged, not raised.
    Because `salt.engines.Engine` actually catches all exceptions.
    
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- tests/pytests/unit/engines/test_engines.py
+++ tests/pytests/unit/engines/test_engines.py
@@ -1,4 +1,3 @@
-import pytest
 import salt.engines
 from tests.support.mock import MagicMock, patch
 
@@ -11,7 +10,5 @@ def test_engine_module_name():
 def test_engine_title_set():
     engine = salt.engines.Engine({}, "foobar.start", {}, {}, {}, {}, name="foobar")
     with patch("salt.utils.process.appendproctitle", MagicMock()) as mm:
-        with pytest.raises(KeyError):
-            # The method does not exist so a KeyError will be raised.
-            engine.run()
-        mm.assert_called_with("foobar")
+        engine.run()
+    mm.assert_called_with("foobar")
