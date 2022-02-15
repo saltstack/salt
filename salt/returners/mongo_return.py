bbf86a67be7c46a9dc98a30bb5e3cd4d0f993388
Wayne Werner <wwerner@vmware.com>
2022-02-15 16:38:01 -0600

    Add ssl argument to MongoClient for mongo returner

--- salt/returners/mongo_return.py
+++ salt/returners/mongo_return.py
@@ -107,6 +107,7 @@ def _get_options(ret):
     attrs = {
         "host": "host",
         "port": "port",
+        "ssl": "ssl",
         "db": "db",
         "user": "user",
         "password": "password",
@@ -127,6 +128,7 @@ def _get_conn(ret):
 
     host = _options.get("host")
     port = _options.get("port")
+    ssl = _options.get("ssl") or False
     db_ = _options.get("db")
     user = _options.get("user")
     password = _options.get("password")
@@ -136,8 +138,8 @@ def _get_conn(ret):
     # pymongo versions < 2.3 until then there are
     # a bunch of these sections that need to be supported
 
-    if PYMONGO_VERSION > Version("2.3"):
-        conn = pymongo.MongoClient(host, port)
+    if PYMONGO_VERSION > _LooseVersion("2.3"):
+        conn = pymongo.MongoClient(host=host, port=port, ssl=ssl)
     else:
         conn = pymongo.Connection(host, port)
     mdb = conn[db_]
