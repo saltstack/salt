f51454733fbc0123db57f1336733d0e43a4ab8c6
cmcmarrow <charles.mcmarrow.4@gmail.com>
2022-02-27 14:07:27 -0600

    fix tests

--- tests/pytests/unit/returners/test_local_cache.py
+++ tests/pytests/unit/returners/test_local_cache.py
@@ -3,13 +3,14 @@ from tests.support.mock import patch
 
 
 def test_remove_job_dir():
-    # Test that _remove_job_dir job will NotADirectoryError error
-    with patch("shutil.rmtree", side_effect=NotADirectoryError("Node Corruption!")):
-        _remove_job_dir("cache.json")
+    # Test that _remove_job_dir job will catch error
+    for e in (NotADirectoryError, OSError):
+        with patch("shutil.rmtree", side_effect=e("Node Corruption!")):
+            _remove_job_dir("cache.json")
 
-    # Test that _remove_job_dir job will not catch other file errors
-    with patch("shutil.rmtree", side_effect=OSError()):
+    # Test that _remove_job_dir job will not catch other errors
+    with patch("shutil.rmtree", side_effect=FileExistsError()):
         try:
             _remove_job_dir("cache.json")
-        except OSError:
+        except FileExistsError:
             pass
