424a11ec1126b25b07abc6cc8d6a062bc3b17dfb
Priyank <piyurupareliya@gmail.com>
2022-03-20 12:12:37 +0530

    modified modules/test_smtp.py

--- tests/pytests/unit/modules/test_smtp.py
+++ tests/pytests/unit/modules/test_smtp.py
@@ -2,11 +2,11 @@ import pytest
 import salt.modules.smtp as smtp
 from tests.support.mock import MagicMock, patch
 
+
 @pytest.fixture
 def configure_loader_modules():
-    return {smtp: {
-        "HAS_LIBS": True
-    }}
+    return {smtp: {"HAS_LIBS": True}}
+
 
 def test_send_msg_html():
     """
@@ -17,16 +17,19 @@ def test_send_msg_html():
     comt = "Need to send message to admin@example.com: This is a salt module"
 
     ret = True
-    mock = MagicMock(return_value={
-        "ehlo": MagicMock(return_value=True),
-        "has_extn": MagicMock(return_value=False),
-        "sendmail": MagicMock(return_value=True)
-    })
+    mock = MagicMock(
+        return_value={
+            "ehlo": MagicMock(return_value=True),
+            "has_extn": MagicMock(return_value=False),
+            "sendmail": MagicMock(return_value=True),
+        }
+    )
 
-    with patch.dict(smtp.smtplib, {"SMTP_SSL":mock}):
+    with patch.object(smtp, "smtplib", return_value={"SMTP_SSL": mock}):
         assert ret == smtp.send_msg(
+            message="Test Email Message",
             recipient="recipient@gmail.com",
             sender="admin@example.com",
             subject="Message from Salt",
-            is_html=True
+            is_html=True,
         )
