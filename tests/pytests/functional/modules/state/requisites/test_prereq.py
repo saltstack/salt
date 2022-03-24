d1c19cddba71b2ce1c5dab83d9c36f4c7fa425d5
Thomas Phipps <tphipps@vmware.com>
2022-03-24 22:19:25 +0000

    test for #56957

--- tests/pytests/functional/modules/state/requisites/test_prereq.py
+++ tests/pytests/functional/modules/state/requisites/test_prereq.py
@@ -642,3 +642,25 @@ def test_infinite_recursion_sls_prereq(state, state_tree):
         ret = state.sls("requisite")
         for state_return in ret:
             assert state_return.result is True
+
+
+def test_infinite_recursion__prereq(state, state_tree):
+    sls_contents = """
+    A:
+      test.nop:
+        - prereq:
+          - test: B
+    B:
+      test.nop:
+        - require:
+          - name: non-existant
+    C:
+      test.nop:
+        - require:
+          - test: B
+    """
+
+    with pytest.helpers.temp_file("requisite.sls", sls_contents, state_tree):
+        ret = state.sls("requisite")
+        for state_return in ret:
+            assert state_return.result is True
