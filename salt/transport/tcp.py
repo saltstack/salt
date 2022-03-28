1399acfb8cb466f13fbd8f7177653f27746d6ed7
Lukas Raska <lukas@raska.me>
2022-03-28 21:38:44 +0200

    Assign packaged payload to proper variable in TCP transport layer

--- salt/transport/tcp.py
+++ salt/transport/tcp.py
@@ -1561,7 +1561,7 @@ class PubServer(salt.ext.tornado.tcpserver.TCPServer):
     @salt.ext.tornado.gen.coroutine
     def publish_payload(self, package, _):
         log.debug("TCP PubServer sending payload: %s", package)
-        payload = self.pack_publish(package)
+        package = self.pack_publish(package)
         payload = salt.transport.frame.frame_msg(package["payload"])
 
         to_remove = []
