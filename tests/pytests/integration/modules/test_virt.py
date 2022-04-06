0f75d428cf3b1f760c2353aefaa057c1567416cd
MKLeb <calebb@vmware.com>
2022-04-06 15:26:25 -0400

    revert silly flaky marker

--- tests/pytests/integration/modules/test_virt.py
+++ tests/pytests/integration/modules/test_virt.py
@@ -545,7 +545,6 @@ class TestVirtMigrateTest:
         assert isinstance(domains, list)
         assert domains == [virt_domain], "Failed to migrate VM"
 
-    @pytest.mark.flaky(max_runs=4)
     def test_tls_migration(
         self, salt_cli, virt_minion_0, virt_minion_1, prep_virt, virt_domain
     ):
