05435ef2b82cbcf13dac940a00b4bb698a3a1144
Gareth J. Greenaway <gareth@saltstack.com>
2022-04-12 15:04:40 -0700

    Running pre-commit manually.

--- tests/pytests/unit/transport/test_tcp.py
+++ tests/pytests/unit/transport/test_tcp.py
@@ -19,9 +19,7 @@ def fake_keys():
 
 @pytest.fixture
 def fake_crypto():
-    with patch(
-        "salt.transport.tcp.PKCS1_OAEP", create=True
-    ) as fake_crypto:
+    with patch("salt.transport.tcp.PKCS1_OAEP", create=True) as fake_crypto:
         yield fake_crypto
 
 
