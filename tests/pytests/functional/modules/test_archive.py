saltstack/salt

commit 64fdb3ecda217132b8281e1535f486b210531906

fixes saltstack/salt#38605 archive.extracted top level directory ownership handling

tests/pytests/functional/modules/test_archive.py
@@ -303,3 +303,31 @@ def test_unrar(archive, unicode_filename):
         ret = archive.unrar(str(arch.archive), str(arch.dst))
         assert isinstance(ret, list)
         arch.assert_artifacts_in_ret(ret)
+
+
+@pytest.mark.skip_on_windows
+@pytest.mark.skip_if_binaries_missing("tar")
+def test_tar_unpack_no_explicit_top_level_directory_member(archive, tmp_path):
+    rel_path = pathlib.Path("archive_top/second_level/file.txt")
+    srcfile = tmp_path / rel_path
+    srcfile.parent.mkdir(parents=True, exist_ok=True)
+    srcfile.write_text("hey there")
+    archive.tar(
+        "-cvzf",
+        str(tmp_path / "tld_test.tar.gz"),
+        sources=str(rel_path.parent),
+        cwd=str(tmp_path),
+    )
+
+    expected = {
+        "dirs": ["archive_top/second_level/"],
+        "files": ["archive_top/second_level/file.txt"],
+        "links": [],
+        "top_level_dirs": ["archive_top/"],
+        "top_level_files": [],
+        "top_level_links": [],
+    }
+    ret = archive.list(
+        str(tmp_path / "tld_test.tar.gz"), archive_format="tar", verbose=True
+    )
+    assert ret["top_level_dirs"] == expected["top_level_dirs"]

commit f4f7a24f7d871e6c8ca457d16305be840b8c5ec9

fix(modules/archive): fix regression introduced re: `common_prefix`
Regression introduced in 64fdb3ecda217132b8281e1535f486b210531906 (#61896).

Closes #61967.

tests/pytests/functional/modules/test_archive.py
@@ -330,4 +330,46 @@ def test_tar_list_no_explicit_top_level_directory_member(archive, tmp_path):
     ret = archive.list(
         str(tmp_path / "tld_test.tar.gz"), archive_format="tar", verbose=True
     )
-    assert ret["top_level_dirs"] == expected["top_level_dirs"]
+    assert ret == expected
+
+
+@pytest.mark.skip_on_windows
+@pytest.mark.skip_if_binaries_missing("tar")
+def test_tar_list_with_similar_top_level_dirs(archive, tmp_path):
+    rel_paths = (
+        "archive_top/second_level/file.txt",
+        "archives/second_level/file.txt",
+    )
+
+    for rel_path in rel_paths:
+        srcfile = tmp_path / str(pathlib.Path(rel_path))
+        srcfile.parent.mkdir(parents=True, exist_ok=True)
+        srcfile.write_text("hey there")
+
+    archive.tar(
+        "-cvzf",
+        str(tmp_path / "tld_test.tar.gz"),
+        sources=["archive_top", "archives"],
+        cwd=str(tmp_path),
+    )
+
+    expected = {
+        "dirs": [
+            "archive_top/",
+            "archive_top/second_level/",
+            "archives/",
+            "archives/second_level/",
+        ],
+        "files": [
+            "archive_top/second_level/file.txt",
+            "archives/second_level/file.txt",
+        ],
+        "links": [],
+        "top_level_dirs": ["archive_top/", "archives/"],
+        "top_level_files": [],
+        "top_level_links": [],
+    }
+    ret = archive.list(
+        str(tmp_path / "tld_test.tar.gz"), archive_format="tar", verbose=True
+    )
+    assert ret == expected