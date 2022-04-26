08acb9ba67dfdf86cfc240d62386605d0e63a6ce
Wayne Werner <wwerner@vmware.com>
2022-04-25 20:33:03 -0500

    pre-commit fixes

--- tests/pytests/unit/auth/test_pam.py
+++ tests/pytests/unit/auth/test_pam.py
@@ -15,7 +15,7 @@ def configure_loader_modules():
 @pytest.fixture
 def mock_pam():
     with patch("salt.auth.pam.CALLOC", autospec=True), patch(
-        "salt.auth.pam.pointer", autospec=True, 
+        "salt.auth.pam.pointer", autospec=True,
     ), patch("salt.auth.pam.PamHandle", autospec=True), patch(
         "salt.auth.pam.PAM_START", autospec=True, return_value=0
     ), patch(
