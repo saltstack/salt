291c14ec748f0016b5cc5ce0dd7e0c58d1ffa4fb
Megan Wilhite <mwilhite@vmware.com>
2022-04-29 10:25:28 -0600

    GPG tests do not work on windows yet

--- tests/integration/minion/test_pillar.py
+++ tests/integration/minion/test_pillar.py
@@ -302,7 +302,6 @@ class BasePillarTest(_CommonBase):
 
 
 @skipIf(not salt.utils.path.which("gpg"), "GPG is not installed")
-@pytest.mark.windows_whitelisted
 class DecryptGPGPillarTest(_CommonBase):
     """
     Tests for pillar decryption
