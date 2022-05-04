898cc3130eca55b271a35568fbc1129c215174fd
Twangboy <leesh@vmware.com>
2022-05-04 17:39:36 -0600

    Fix some linting errors

--- salt/utils/path.py
+++ salt/utils/path.py
@@ -372,7 +372,7 @@ def os_walk(top, *args, **kwargs):
 def expand(path):
     """
     Expands all user and environment variables
-    .. versionadded: Sulfur
+    .. versionadded:: 3006
 
     Args:
 
