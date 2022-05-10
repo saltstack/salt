11cf791ba5346014a15d8f9d3159fa2d922603b8
Jakub Recman <jakub.recman@tieto.com>
2022-05-10 23:52:29 +0200

    Catch 0 OSError during ssl_handshake

--- salt/ext/tornado/iostream.py
+++ salt/ext/tornado/iostream.py
@@ -1339,7 +1339,7 @@ class SSLIOStream(IOStream):
             # those errors quiet as well.
             # https://groups.google.com/forum/?fromgroups#!topic/python-tornado/ApucKJat1_0
             if (self._is_connreset(err) or
-                    err.args[0] in (errno.EBADF, errno.ENOTCONN)):
+                    err.args[0] in (0, errno.EBADF, errno.ENOTCONN)):
                 return self.close(exc_info=True)
             raise
         except AttributeError:
