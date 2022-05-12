bb6bcb54eea8a6cbc80c87dc11ee84874f0ca283
nicholasmhughes <nicholasmhughes@gmail.com>
2022-05-12 13:35:14 -0400

    fixes saltstack/salt#61946 sync_after_install immutabledict error

--- salt/cloud/__init__.py
+++ salt/cloud/__init__.py
@@ -1225,14 +1225,33 @@ class Cloud:
             ):
                 output = self.clouds[func](vm_)
             if output is not False and "sync_after_install" in self.opts:
-                if self.opts["sync_after_install"] not in (
+                if self.opts["sync_after_install"] and self.opts[
+                    "sync_after_install"
+                ] not in (
                     "all",
-                    "modules",
-                    "states",
+                    "beacons",
+                    "clouds",
+                    "engines",
+                    "executors",
                     "grains",
+                    "log",
+                    "matchers",
+                    "modules",
+                    "output",
+                    "pillar",
+                    "proxymodules",
+                    "renderers",
+                    "returners",
+                    "sdb",
+                    "serializers",
+                    "states",
+                    "thorium",
+                    "utils",
                 ):
-                    log.error("Bad option for sync_after_install")
-                    return output
+                    log.warning(
+                        "Bad option for sync_after_install. Defaulting to 'all'"
+                    )
+                    self.opts["sync_after_install"] = "all"
 
                 # A small pause helps the sync work more reliably
                 time.sleep(3)
@@ -1240,7 +1259,7 @@ class Cloud:
                 start = int(time.time())
                 while int(time.time()) < start + 60:
                     # We'll try every <timeout> seconds, up to a minute
-                    mopts_ = salt.config.DEFAULT_MASTER_OPTS
+                    mopts_ = copy.deepcopy(salt.config.DEFAULT_MASTER_OPTS)
                     conf_path = "/".join(self.opts["conf_file"].split("/")[:-1])
                     mopts_.update(
                         salt.config.master_config(os.path.join(conf_path, "master"))
