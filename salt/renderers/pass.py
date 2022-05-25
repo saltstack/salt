f37669a31bb0fb65f978beed8a9316d76c76ec9f
Marcus Rückert <darix@nordisch.org>
2022-05-25 15:13:12 +0200

    Make the pass renderer more configurable
    1. Allow us to make the pass renderer fail during pillar rendering
       when a secret corresponding with a pass path cannot be fetched.
       For this we add a master config variable pass_strict_fetch.
    
    2. Allow to have prefix for variables that should be processed
       with the pass renderer.
       For this we add a master config variable pass_variable_prefix.
    
    3. Allow us to configure pass' GNUPGHOME and PASSWORD_STORE_DIR
       environmental variables.
       For this we add master config variables pass_gnupghome and pass_dir.

--- salt/renderers/pass.py
+++ salt/renderers/pass.py
@@ -45,6 +45,34 @@ Install pass binary
 
         pass:
           pkg.installed
+
+Salt master configuration options
+
+.. code-block:: yaml
+
+        # If the prefix is *not* set (default behavior), all template variables are
+        # considered for fetching secrets from Pass. Those that cannot be resolved
+        # to a secret are passed through.
+        #
+        # If the prefix is set, only the template variables with matching prefix are
+        # considered for fetching the secrets, other variables are passed through.
+        #
+        # For ease of use it is recommended to set the following options as well:
+        #       renderer: 'jinja|yaml|pass'
+        #       pass_strict_fetch: true
+        #
+        pass_variable_prefix: 'pass:'
+
+        # If set to 'true', error out when unable to fetch a secret for a template variable.
+        pass_strict_fetch: true
+
+        # Set GNUPGHOME env for Pass.
+        # Defaults to: ~/.gnupg
+        pass_gnupghome: <path>
+
+        # Set PASSWORD_STORE_DIR env for Pass.
+        # Defaults to: ~/.password-store
+        pass_dir: <path>
 """
 
 
@@ -54,7 +82,7 @@ from os.path import expanduser
 from subprocess import PIPE, Popen
 
 import salt.utils.path
-from salt.exceptions import SaltRenderError
+from salt.exceptions import SaltConfigurationError, SaltRenderError
 
 log = logging.getLogger(__name__)
 
@@ -80,6 +108,23 @@ def _fetch_secret(pass_path):
     # Make a backup in case we want to return the original value without stripped whitespaces
     original_pass_path = pass_path
 
+    # Remove the optional prefix from pass path
+    pass_prefix = __opts__["pass_variable_prefix"]
+    if pass_prefix:
+        # If we do not see our prefix we do not want to process this variable
+        # and we return the unmodified pass path
+        if not pass_path.startswith(pass_prefix):
+            return pass_path
+
+        # strip the prefix from the start of the string
+        pass_path = pass_path[len(pass_prefix) :]
+
+    # The pass_strict_fetch option must be used with pass_variable_prefix
+    pass_strict_fetch = __opts__["pass_strict_fetch"]
+    if pass_strict_fetch and not pass_prefix:
+        msg = "The 'pass_strict_fetch' option requires 'pass_variable_prefix' option enabled"
+        raise SaltConfigurationError(msg)
+
     # Remove whitespaces from the pass_path
     pass_path = pass_path.strip()
 
@@ -91,14 +136,32 @@ def _fetch_secret(pass_path):
     env = os.environ.copy()
     env["HOME"] = expanduser("~")
 
+    pass_dir = __opts__["pass_dir"]
+    if pass_dir:
+        env["PASSWORD_STORE_DIR"] = pass_dir
+
+    pass_gnupghome = __opts__["pass_gnupghome"]
+    if pass_gnupghome:
+        env["GNUPGHOME"] = pass_gnupghome
+
     proc = Popen(cmd, stdout=PIPE, stderr=PIPE, env=env)
     pass_data, pass_error = proc.communicate()
 
     # The version of pass used during development sent output to
     # stdout instead of stderr even though its returncode was non zero.
     if proc.returncode or not pass_data:
-        log.warning("Could not fetch secret: %s %s", pass_data, pass_error)
-        return original_pass_path
+        try:
+            pass_error = pass_error.decode("utf-8")
+        except (AttributeError, ValueError):
+            pass
+        msg = "Could not fetch secret '{}' from the password store: {}".format(
+            pass_path, pass_error
+        )
+        if pass_strict_fetch:
+            raise SaltRenderError(msg)
+        else:
+            log.warning(msg)
+            return original_pass_path
     return pass_data.rstrip("\r\n")
 
 
