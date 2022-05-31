a570ef6e656e6de85877235eca543e503472fd09
Caleb Beard <53276404+MKLeb@users.noreply.github.com>
2022-05-31 09:40:35 -0400

    Add etcd API v3 implementation (#61911)
    * add etcd3-py to CI requirements for python_version >= 3.6
    
    * Add intial etcd3 implementation and adjust the functional tests
    
    * adjust etcd test suite to test new v3 impl where applicable
    
    * pre-commit fun
    
    * add documentation for new option
    
    * change warnings per suggestions
    
    * change names for backwards compatibility and add back profile kwarg
    
    * pre-commit and lint fixes
    
    * switch docker logic in etcd sdb integration tests
    
    * fix sdb etcd tests v2
    
    * add ls pseudo-functionality to etcd api v3
    
    * move towards recursive kwarg deprecation by adding recurse kwarg as preferred
    
    * add encoding feature and other etcd client args for v3
    
    * precommit
    
    * add deprecations changelog and deprecate waitIndex/index kwargs in favor of start_revision
    
    * pre-commit and revert etcd sdb tests to master version
    
    * remove unused import
    
    * fix mod_watch disbatching
    
    * Make sure to warn if no profile is given and we have a possible connection error
    
    * precommit
    
    * Fix test failures with wrong profile
    
    * Fix v3 -> no v2 error
    
    Previously, when the API was v3-only, but the profile wasn't present
    leading to the v2 fallback, it would 404 in certain circumstances.
    
    We can't use a more particular exception in the `_etcd_action` because
    the salt loader plays havoc with things.
    
    Co-authored-by: Wayne Werner <wwerner@vmware.com>
    Co-authored-by: Gareth J. Greenaway <gareth@saltstack.com>

--- tests/pytests/functional/modules/test_etcd_mod.py
+++ tests/pytests/functional/modules/test_etcd_mod.py
@@ -4,7 +4,7 @@ import time
 
 import pytest
 import salt.modules.etcd_mod as etcd_mod
-from salt.utils.etcd_util import HAS_LIBS, EtcdClient, get_conn
+from salt.utils.etcd_util import HAS_ETCD_V2, HAS_ETCD_V3, get_conn
 from saltfactories.daemons.container import Container
 from saltfactories.utils import random_string
 from saltfactories.utils.ports import get_unused_localhost_port
@@ -15,7 +15,6 @@ log = logging.getLogger(__name__)
 
 pytestmark = [
     pytest.mark.windows_whitelisted,
-    pytest.mark.skipif(not HAS_LIBS, reason="Need etcd libs to test etcd_util!"),
     pytest.mark.skip_if_binaries_missing("docker", "dockerd", check_all=False),
 ]
 
@@ -79,14 +78,29 @@ def etcd_apiv2_container(salt_factories, docker_client, etcd_port, docker_image_
         yield factory
 
 
+@pytest.fixture(scope="module", params=(True, False))
+def use_v2(request):
+    if request.param and not HAS_ETCD_V2:
+        pytest.skip("No etcd library installed")
+    if not request.param and not HAS_ETCD_V3:
+        pytest.skip("No etcd3 library installed")
+    return request.param
+
+
 @pytest.fixture(scope="module")
 def profile_name():
     return "etcd_util_profile"
 
 
 @pytest.fixture(scope="module")
-def etcd_profile(profile_name, etcd_port):
-    profile = {profile_name: {"etcd.host": "127.0.0.1", "etcd.port": etcd_port}}
+def etcd_profile(profile_name, etcd_port, use_v2):
+    profile = {
+        profile_name: {
+            "etcd.host": "127.0.0.1",
+            "etcd.port": etcd_port,
+            "etcd.require_v2": use_v2,
+        }
+    }
 
     return profile
 
@@ -98,7 +112,7 @@ def minion_config_overrides(etcd_profile):
 
 @pytest.fixture(scope="module")
 def etcd_client(minion_opts, profile_name):
-    return EtcdClient(minion_opts, profile=profile_name)
+    return get_conn(minion_opts, profile=profile_name)
 
 
 @pytest.fixture(scope="module")
@@ -115,12 +129,12 @@ def cleanup_prefixed_entries(etcd_client, prefix):
         assert etcd_client.get(prefix, recurse=True) is None
         yield
     finally:
-        etcd_client.delete(prefix, recursive=True)
+        etcd_client.delete(prefix, recurse=True)
 
 
-def test_basic_operations(subtests, profile_name, prefix):
+def test_basic_operations(subtests, profile_name, prefix, use_v2):
     """
-    Client creation using EtcdClient, just need to assert no errors.
+    Make sure we can do the basics
     """
     with subtests.test("There should be no entries at the start with our prefix."):
         assert etcd_mod.get_(prefix, recurse=True, profile=profile_name) is None
@@ -178,3 +192,31 @@ def test_basic_operations(subtests, profile_name, prefix):
         modified = return_list.pop()
         assert modified["key"] == "{}/1".format(prefix)
         assert modified["value"] == "one"
+
+
+def test_with_missing_profile(subtests, prefix, use_v2, etcd_port):
+    """
+    Test the correct response when the profile is missing and we can't connect
+    """
+    if use_v2 and etcd_port != 2379:
+        # Only need to run this once
+        with subtests.test("Test no profile and bad connection in get_"):
+            assert etcd_mod.get_("{}/1".format(prefix)) is None
+
+        with subtests.test("Test no profile and bad connection in set_"):
+            assert etcd_mod.set_("{}/1".format(prefix), "lol") is None
+
+        with subtests.test("Test no profile and bad connection in update"):
+            assert etcd_mod.update({"{}/1".format(prefix): "SIUUU"}) is None
+
+        with subtests.test("Test no profile and bad connection in watch"):
+            assert etcd_mod.watch("{}/1".format(prefix)) is None
+
+        with subtests.test("Test no profile and bad connection in ls_"):
+            assert etcd_mod.ls_() is None
+
+        with subtests.test("Test no profile and bad connection in rm"):
+            assert etcd_mod.rm_("{}/1".format(prefix)) is None
+
+        with subtests.test("Test no profile and bad connection in tree"):
+            assert etcd_mod.tree() is None
