545bc896e718c2697c17068f11ff3b6bd6ad2bf6
Pedro Algarvio <palgarvio@vmware.com>
2022-06-01 11:02:24 +0100

    Reduce code duplication by creating ``tests/support/pytest/etcd.py``
    Additionally, we also test against a Etcd v3 container running in v2 mode.
    
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- tests/pytests/functional/modules/test_etcd_mod.py
+++ tests/pytests/functional/modules/test_etcd_mod.py
@@ -4,8 +4,8 @@ import time
 
 import pytest
 import salt.modules.etcd_mod as etcd_mod
-from salt.utils.etcd_util import HAS_ETCD_V2, HAS_ETCD_V3, get_conn
-from saltfactories.utils import random_string
+from salt.utils.etcd_util import get_conn
+from tests.support.pytest.etcd import *  # pylint: disable=wildcard-import,unused-wildcard-import
 
 docker = pytest.importorskip("docker")
 
@@ -27,59 +27,6 @@ def configure_loader_modules(minion_opts):
             },
         },
     }
-
-
-# TODO: Use our own etcd image to avoid reliance on a third party
-@pytest.fixture(scope="module")
-def etcd_apiv2_container(salt_factories):
-    container = salt_factories.get_container(
-        random_string("etcd-server-"),
-        image_name="bitnami/etcd:3",
-        container_run_kwargs={
-            "environment": {
-                "ALLOW_NONE_AUTHENTICATION": "yes",
-                "ETCD_ENABLE_V2": "true",
-            },
-            "ports": {"2379/tcp": None},
-        },
-        pull_before_start=True,
-        skip_on_pull_failure=True,
-        skip_if_docker_client_not_connectable=True,
-    )
-    with container.started() as factory:
-        yield factory
-
-
-@pytest.fixture(scope="module", params=(True, False))
-def use_v2(request):
-    if request.param and not HAS_ETCD_V2:
-        pytest.skip("No etcd library installed")
-    if not request.param and not HAS_ETCD_V3:
-        pytest.skip("No etcd3 library installed")
-    return request.param
-
-
-@pytest.fixture(scope="module")
-def etcd_port(etcd_apiv2_container):
-    return etcd_apiv2_container.get_host_port_binding(2379, protocol="tcp", ipv6=False)
-
-
-@pytest.fixture(scope="module")
-def profile_name():
-    return "etcd_util_profile"
-
-
-@pytest.fixture(scope="module")
-def etcd_profile(profile_name, etcd_port, use_v2):
-    profile = {
-        profile_name: {
-            "etcd.host": "127.0.0.1",
-            "etcd.port": etcd_port,
-            "etcd.require_v2": use_v2,
-        }
-    }
-
-    return profile
 
 
 @pytest.fixture(scope="module")
@@ -109,7 +56,7 @@ def cleanup_prefixed_entries(etcd_client, prefix):
         etcd_client.delete(prefix, recurse=True)
 
 
-def test_basic_operations(subtests, profile_name, prefix, use_v2):
+def test_basic_operations(subtests, profile_name, prefix):
     """
     Make sure we can do the basics
     """
@@ -171,11 +118,11 @@ def test_basic_operations(subtests, profile_name, prefix, use_v2):
         assert modified["value"] == "one"
 
 
-def test_with_missing_profile(subtests, prefix, use_v2, etcd_port):
+def test_with_missing_profile(subtests, prefix, etcd_version, etcd_port):
     """
     Test the correct response when the profile is missing and we can't connect
     """
-    if use_v2 and etcd_port != 2379:
+    if etcd_version in (EtcdVersion.v2, EtcdVersion.v3_v2_mode) and etcd_port != 2379:
         # Only need to run this once
         with subtests.test("Test no profile and bad connection in get_"):
             assert etcd_mod.get_("{}/1".format(prefix)) is None
--- tests/pytests/functional/pillar/test_etcd_pillar.py
+++ tests/pytests/functional/pillar/test_etcd_pillar.py
@@ -2,8 +2,8 @@ import logging
 
 import pytest
 import salt.pillar.etcd_pillar as etcd_pillar
-from salt.utils.etcd_util import HAS_ETCD_V2, HAS_ETCD_V3, get_conn
-from saltfactories.utils import random_string
+from salt.utils.etcd_util import get_conn
+from tests.support.pytest.etcd import *  # pylint: disable=wildcard-import,unused-wildcard-import
 
 docker = pytest.importorskip("docker")
 
@@ -22,59 +22,6 @@ def configure_loader_modules(minion_opts):
             "__opts__": minion_opts,
         },
     }
-
-
-# TODO: Use our own etcd image to avoid reliance on a third party
-@pytest.fixture(scope="module")
-def etcd_apiv2_container(salt_factories):
-    container = salt_factories.get_container(
-        random_string("etcd-server-"),
-        image_name="bitnami/etcd:3",
-        container_run_kwargs={
-            "environment": {
-                "ALLOW_NONE_AUTHENTICATION": "yes",
-                "ETCD_ENABLE_V2": "true",
-            },
-            "ports": {"2379/tcp": None},
-        },
-        pull_before_start=True,
-        skip_on_pull_failure=True,
-        skip_if_docker_client_not_connectable=True,
-    )
-    with container.started() as factory:
-        yield factory
-
-
-@pytest.fixture(scope="module", params=(True, False))
-def use_v2(request):
-    if request.param and not HAS_ETCD_V2:
-        pytest.skip("No etcd library installed")
-    if not request.param and not HAS_ETCD_V3:
-        pytest.skip("No etcd3 library installed")
-    return request.param
-
-
-@pytest.fixture(scope="module")
-def etcd_port(etcd_apiv2_container):
-    return etcd_apiv2_container.get_host_port_binding(2379, protocol="tcp", ipv6=False)
-
-
-@pytest.fixture(scope="module")
-def profile_name():
-    return "etcd_util_profile"
-
-
-@pytest.fixture(scope="module")
-def etcd_profile(profile_name, etcd_port, use_v2):
-    profile = {
-        profile_name: {
-            "etcd.host": "127.0.0.1",
-            "etcd.port": etcd_port,
-            "etcd.require_v2": use_v2,
-        }
-    }
-
-    return profile
 
 
 @pytest.fixture(scope="module")
