59061e13b8acf9cf790fb5a21b286ac3f2294e9d
Jamie (Bear) Murphy <1613241+ITJamie@users.noreply.github.com>
2022-06-14 20:37:21 +0100

    add fix bytes fix to returner pgjsonb

--- salt/returners/pgjsonb.py
+++ salt/returners/pgjsonb.py
@@ -342,6 +342,13 @@ def save_load(jid, load, minions=None):
     Save the load to the specified jid id
     """
     with _get_serv(commit=True) as cur:
+        # https://github.com/saltstack/salt/issues/55226
+        # convert returned data from binary string to actual string
+        if "return" in load.keys() and "return" in load["return"].keys():
+            if isinstance(load["return"]["return"], (bytes, bytearray)):
+                load["return"]["return"] = load["return"]["return"].decode(
+                    "utf-8", "strict"
+                )
         try:
             cur.execute(
                 PG_SAVE_LOAD_SQL, {"jid": jid, "load": psycopg2.extras.Json(load)}
