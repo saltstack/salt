64302027dc5904180583a1253403a48b180e446c
Gareth J. Greenaway <gareth@saltstack.com>
2022-07-08 12:11:01 -0700

    Adding more functional tests for MySQL state module

--- tests/pytests/functional/states/test_mysql.py
+++ tests/pytests/functional/states/test_mysql.py
@@ -157,6 +157,65 @@ def test_grants_present_absent(mysql, mysql_grants):
         assert ret
 
 
+def test_grants_present_absent_all_privileges(mysql, mysql_grants):
+
+    # Create the database
+    ret = mysql.db_create("salt_2_0")
+    assert ret
+
+    # Create a user
+    ret = mysql.user_create(
+        "george",
+        host="localhost",
+        password="badpassword",
+    )
+    assert ret
+
+    try:
+        ret = mysql_grants.present(
+            name="add_salt_grants",
+            grant="all privileges",
+            database="salt_2_0.*",
+            user="george",
+            host="localhost",
+        )
+
+        assert ret.changes
+        assert ret.changes == {"add_salt_grants": "Present"}
+
+        assert ret.comment
+        assert (
+            ret.comment
+            == "Grant all privileges on salt_2_0.* to george@localhost has been added"
+        )
+
+        ret = mysql_grants.absent(
+            name="delete_salt_grants",
+            grant="all privileges",
+            database="salt_2_0.*",
+            user="george",
+            host="localhost",
+        )
+
+        assert ret.changes
+        assert ret.changes == {"delete_salt_grants": "Absent"}
+
+        assert ret.comment
+        assert (
+            ret.comment
+            == "Grant all privileges on salt_2_0.* for george@localhost has been revoked"
+        )
+
+    finally:
+        # Remove the user
+        ret = mysql.user_remove("george", host="localhost")
+        assert ret
+
+        # Remove the database
+        ret = mysql.db_remove("salt_2_0")
+        assert ret
+
+
 def test_user_present_absent(mysql_user):
 
     ret = mysql_user.present(
@@ -249,3 +308,89 @@ def test_user_present_absent_unixsocket(mysql, mysql_user, mysql_container):
         if "mariadb" not in mysql_container.mysql_name:
             ret = mysql.plugin_remove("auth_socket")
             assert ret
+
+
+def test_grants_present_absent_state_file(
+    modules, mysql, mysql_grants, mysql_combo, state_tree
+):
+
+    content = """
+    sbclient_2_0:
+      mysql_user.present:
+        - host: localhost
+        - password: sbclient
+        - connection_user: {mysql_user}
+        - connection_pass: {mysql_pass}
+        - connection_db: mysql
+        - connection_port: {mysql_port}
+      mysql_database.present:
+        - connection_user: {mysql_user}
+        - connection_pass: {mysql_pass}
+        - connection_db: mysql
+        - connection_port: {mysql_port}
+      mysql_grants.present:
+        - grant: ALL PRIVILEGES
+        - user: sbclient_2_0
+        - database: sbclient_2_0.*
+        - host: localhost
+        - connection_user: {mysql_user}
+        - connection_pass: {mysql_pass}
+        - connection_db: mysql
+        - connection_port: {mysql_port}
+    """.format(
+        mysql_user=mysql_combo.mysql_root_user,
+        mysql_pass=mysql_combo.mysql_root_passwd,
+        mysql_port=mysql_combo.mysql_port,
+    )
+
+    try:
+        with pytest.helpers.temp_file("manage_mysql.sls", content, state_tree):
+            ret = modules.state.apply(
+                "manage_mysql",
+            )
+
+            # Check user creation
+            state = "mysql_user_|-sbclient_2_0_|-sbclient_2_0_|-present"
+            assert state in ret
+            assert "changes" in ret[state]
+            assert ret[state]["changes"] == {"sbclient_2_0": "Present"}
+
+            # Check database creation
+            state = "mysql_database_|-sbclient_2_0_|-sbclient_2_0_|-present"
+            assert state in ret
+            assert "changes" in ret[state]
+            assert ret[state]["changes"] == {"sbclient_2_0": "Present"}
+
+            # Check grant creation
+            state = "mysql_grants_|-sbclient_2_0_|-sbclient_2_0_|-present"
+            assert state in ret
+            assert "comment" in ret[state]
+            assert (
+                ret[state]["comment"]
+                == "Grant ALL PRIVILEGES on sbclient_2_0.* to sbclient_2_0@localhost has been added"
+            )
+
+        ret = mysql_grants.absent(
+            name="delete_sbclient_grants",
+            grant="all privileges",
+            database="sbclient_2_0.*",
+            user="sbclient_2_0",
+            host="localhost",
+        )
+        assert ret.changes
+        assert ret.changes == {"delete_sbclient_grants": "Absent"}
+
+        assert ret.comment
+        assert (
+            ret.comment
+            == "Grant all privileges on sbclient_2_0.* for sbclient_2_0@localhost has been revoked"
+        )
+
+    finally:
+        # Remove the user
+        ret = mysql.user_remove("sbclient_2_0", host="localhost")
+        assert ret
+
+        # Remove the database
+        ret = mysql.db_remove("sbclient_2_0")
+        assert ret
