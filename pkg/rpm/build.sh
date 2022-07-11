9c670655db0108586aba45146732f7b674db6e4d
Daniel A. Wozniak <dwozniak@vmware.com>
2022-07-11 01:26:43 -0700

    Initial RPM

---
+++ pkg/rpm/build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# onedir.sh
+#
+# Script for making a onedir build. Requires Docker
+#
+#
+set +e
+
+# XXX: centos 7 with dependencies installed, we need to figure out how we'll
+# handle this
+[ -z "$CONTAINER_IMAGE" ] && CONTAINER_IMAGE=dwoz1/cicd:onedir-centos7
+
+CONTAINER_NAME="onedir-$RANDOM"
+SOURCE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# If onedir.sh changes locations this needs to change
+PROJECT_ROOT=$(dirname $(dirname $SOURCE_DIR))
+SCRIPT=$(cat <<EOF
+set +e
+cd /salt
+ls
+rpmbuild -bb --define="_salt_src /salt" /salt/pkg/rpm/salt.spec
+echo $?
+ls -R /root
+cp /root/rpmbuild/RPMS/$(uname -m)/* /salt/
+chown $UID /salt/*.rpm
+rpm -qlp  /salt/*.rpm
+EOF
+)
+docker pull $CONTAINER_IMAGE
+echo "Running $CONTAINER_NAME"
+docker run \
+  --mount type=bind,source="$PROJECT_ROOT",target=/salt \
+  --name $CONTAINER_NAME \
+  $CONTAINER_IMAGE \
+  /bin/bash -c "$SCRIPT"
+echo "Removing $CONTAINER_NAME"
+docker container rm $CONTAINER_NAME
