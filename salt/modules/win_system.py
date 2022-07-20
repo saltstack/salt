d1cb02dcf819d051b9152a48f7783ebc30b1e868
Pedro Algarvio <palgarvio@vmware.com>
2022-07-20 13:50:28 +0100

    Format source code using latest black
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- salt/modules/win_system.py
+++ salt/modules/win_system.py
@@ -508,16 +508,16 @@ def get_system_info():
 
     def byte_calc(val):
         val = float(val)
-        if val < 2 ** 10:
+        if val < 2**10:
             return "{:.3f}B".format(val)
-        elif val < 2 ** 20:
-            return "{:.3f}KB".format(val / 2 ** 10)
-        elif val < 2 ** 30:
-            return "{:.3f}MB".format(val / 2 ** 20)
-        elif val < 2 ** 40:
-            return "{:.3f}GB".format(val / 2 ** 30)
+        elif val < 2**20:
+            return "{:.3f}KB".format(val / 2**10)
+        elif val < 2**30:
+            return "{:.3f}MB".format(val / 2**20)
+        elif val < 2**40:
+            return "{:.3f}GB".format(val / 2**30)
         else:
-            return "{:.3f}TB".format(val / 2 ** 40)
+            return "{:.3f}TB".format(val / 2**40)
 
     # Lookup dicts for Win32_OperatingSystem
     os_type = {1: "Work Station", 2: "Domain Controller", 3: "Server"}
