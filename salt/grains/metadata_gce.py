115497e9c6f307407209f4aed08508fb14eeb151
Jon Benning <jon.benning@genmills.com>
2022-07-26 13:09:34 -0500

    use api recursion, json output. simplify code.

--- salt/grains/metadata_gce.py
+++ salt/grains/metadata_gce.py
@@ -1,6 +1,6 @@
 """
 Grains from cloud metadata servers at 169.254.169.254 in
-google cloud engine
+google compute engine
 
 .. versionadded:: 3005.0
 
@@ -15,72 +15,48 @@ metadata server set `metadata_server_grains: True` in the minion config.
 
 """
 
-import logging
-import os
-
-import salt.utils.data
 import salt.utils.http as http
 import salt.utils.json
-import salt.utils.stringutils
+import logging
 
-# metadata server information
-HOST = "http://169.254.169.254/"
-
+__virtualname__ = "gce"
+IP = "169.254.169.254"
+URL = f"http://{IP}/computeMetadata/v1/?alt=json&recursive=true"
 log = logging.getLogger(__name__)
 
-
 def __virtual__():
+    checks = []
+    # Check if metadata_server_grains minion option is enabled
     if __opts__.get("metadata_server_grains", False) is False:
+        checks.append(False)
+    else:
+        checks.append(True)
+
+    # Check if metadata server is reachable
+    googletest = http.query(f"http://{IP}", status=True, headers=True)
+    if googletest.get("status", 404) == 200:
+        checks.append(True)
+    else:
+        checks.append(False)
+
+    # Check if headers include Google flavor
+    if googletest.get("headers", {}).get("Metadata-Flavor", False) == "Google":
+        checks.append(True)
+    else:
+        checks.append(False)
+
+    # Only load the module if all checks were True
+    if all(checks):
+        log.debug("All checks true - loading gce metadata")
+        return __virtualname__
+    else:
+        log.debug("Some checks false - skipping gce metadata")
         return False
-    googletest = http.query(HOST, status=True, headers=True)
-    if (
-        googletest.get("status", 404) != 200
-        or googletest.get("headers", {}).get("Metadata-Flavor", False) != "Google"
-    ):
-        return False
-    return True
+
+def fetch_gce_metadata():
+    result = http.query(URL, headers=True, header_list=['Metadata-Flavor: Google'])
+    metadata = salt.utils.json.loads(result.get('body',{}))
+
+    return metadata
 
 
-def _search(prefix="computeMetadata/v1/"):
-    """
-    Recursively look up all grains in the metadata server
-    """
-    ret = {}
-    heads = ["Metadata-Flavor: Google"]
-    linedata = http.query(os.path.join(HOST, prefix), headers=True, header_list=heads)
-    if "body" not in linedata:
-        return ret
-    body = salt.utils.stringutils.to_unicode(linedata["body"])
-    if (
-        linedata["headers"].get("Content-Type", "text/plain")
-        == "application/octet-stream"
-    ):
-        return body
-    for line in body.split("\n"):
-        # Block list, null bytes are causing oddities. and project contains ssh keys used to login to the system.
-        # so keeping both from showing up in the grains.
-        if line in ["", "project/"]:
-            continue
-        if line.endswith("/"):
-            ret[line[:-1]] = _search(prefix=os.path.join(prefix, line))
-        else:
-            retdata = http.query(
-                os.path.join(HOST, prefix, line), header_list=heads
-            ).get("body", None)
-            if isinstance(retdata, bytes):
-                try:
-                    ret[line] = salt.utils.json.loads(
-                        salt.utils.stringutils.to_unicode(retdata)
-                    )
-                except ValueError:
-                    ret[line] = salt.utils.stringutils.to_unicode(retdata)
-            else:
-                ret[line] = retdata
-    return salt.utils.data.decode(ret)
-
-
-def metadata():
-    """
-    main function to output grains into loader
-    """
-    return _search()
