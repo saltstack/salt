237bcd71c3b264dccbe06d457c27541960f740b1
nicholasmhughes <nicholasmhughes@gmail.com>
2022-08-08 09:28:21 -0400

    test modification of an existing group to a non-unique gid

--- tests/pytests/unit/states/test_group.py
+++ tests/pytests/unit/states/test_group.py
@@ -40,3 +40,39 @@ def test_present_with_non_unique_gid():
             "name": "salt",
             "result": False,
         }
+
+
+def test_present_with_existing_group_and_non_unique_gid():
+    with patch(
+        "salt.states.group._changes",
+        MagicMock(side_effect=[{"gid": 1}, {}, {"gid": 1}, {"gid": 1}]),
+    ), patch.dict(
+        group.__salt__,
+        {
+            "group.getent": MagicMock(
+                side_effect=[[{"name": "salt", "gid": 1}], [{"name": "salt", "gid": 1}]]
+            )
+        },
+    ), patch.dict(
+        group.__salt__, {"group.add": MagicMock(return_value=True)}
+    ), patch.dict(
+        group.__salt__, {"group.chgid": MagicMock(return_value=True)}
+    ), patch.dict(
+        group.__salt__, {"group.info": MagicMock(return_value={"things": "stuff"})}
+    ):
+        ret = group.present("salt", gid=1, non_unique=True)
+        assert ret == {
+            "changes": {"Final": "All changes applied successfully"},
+            "comment": "The following group attributes are set to be changed:\ngid: 1\n",
+            "name": "salt",
+            "result": True,
+        }
+        ret = group.present("salt", gid=1, non_unique=False)
+        assert ret == {
+            "changes": {"Failed": {"gid": 1}},
+            "comment": "The following group attributes are set to be changed:\n"
+            "gid: 1\n"
+            "Some changes could not be applied",
+            "name": "salt",
+            "result": False,
+        }
