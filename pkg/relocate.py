c3ec2f23a85c6bb76ba273ad323decad4e128658
Daniel A. Wozniak <dwozniak@vmware.com>
2022-08-10 14:17:44 -0700

    fix relocate.py formatting

--- pkg/relocate.py
+++ pkg/relocate.py
@@ -1,62 +1,60 @@
-import logging
 import argparse
-import pathlib
-
-
-import os
-import subprocess
-import shutil
 import json
+import logging
+import os
+import pathlib
+import shutil
+import subprocess
 
 log = logging.getLogger(__name__)
 
 
-LIBCLIBS=[
-   "linux-vdso.so.1",
-   "libc.so.6",
-   "librt.so.1",
-   "libm.so.6",
-   "libmd.so.0",
-   "libpthread.so.0",
-   "libcrypt.so.1",
-   "libdl.so.2",
-   "libmemusage.so",
-   "libnsl.so.1",
-   "libnss_compat.so.2",
-   "libnss_db.so.2",
-   "libnss_dns.so.2",
-   "libnss_files.so.2",
-   "libnss_hesiod.so.2",
-   "libpcprofile.so.2",
-   "libresolve.so.2",
-   "librt.so.1",
-   "libthread_db.so.1",
-   "libutil.so.2",
-
+LIBCLIBS = [
+    "linux-vdso.so.1",
+    "libc.so.6",
+    "librt.so.1",
+    "libm.so.6",
+    "libmd.so.0",
+    "libpthread.so.0",
+    "libcrypt.so.1",
+    "libdl.so.2",
+    "libmemusage.so",
+    "libnsl.so.1",
+    "libnss_compat.so.2",
+    "libnss_db.so.2",
+    "libnss_dns.so.2",
+    "libnss_files.so.2",
+    "libnss_hesiod.so.2",
+    "libpcprofile.so.2",
+    "libresolve.so.2",
+    "librt.so.1",
+    "libthread_db.so.1",
+    "libutil.so.2",
 ]
 
 LC_ID_DYLIB = "LC_ID_DYLIB"
 LC_LOAD_DYLIB = "LC_LOAD_DYLIB"
 LC_RPATH = "LC_RPATH"
 
-ROOT = 'build/salt/lib'
+ROOT = "build/salt/lib"
 
-parser = argparse.ArgumentParser(description='Relocate linked libraries')
+parser = argparse.ArgumentParser(description="Relocate linked libraries")
 parser.add_argument("--root", default="build/salt")
 parser.add_argument("--libs", default="build/salt/lib")
 parser.add_argument("--log-level", default="INFO")
 
+
 def is_macho(path):
-    with open(path, 'rb') as fp:
+    with open(path, "rb") as fp:
         magic = fp.read(4)
-    #XXX: Handle 64bit, 32bit, ppc, arm
-    return magic in [b'\xcf\xfa\xed\xfe']
+    # XXX: Handle 64bit, 32bit, ppc, arm
+    return magic in [b"\xcf\xfa\xed\xfe"]
 
 
 def is_elf(path):
-    with open(path, 'rb') as fp:
+    with open(path, "rb") as fp:
         magic = fp.read(4)
-    return magic == b'\x7f\x45\x4c\x46'
+    return magic == b"\x7f\x45\x4c\x46"
 
 
 def parse_otool_l(stdout):
@@ -64,27 +62,27 @@ def parse_otool_l(stdout):
     cmd = None
     name = None
     data = {}
-    for line in [x.strip() for x in stdout.split('\n')]:
+    for line in [x.strip() for x in stdout.split("\n")]:
 
         if not line:
             continue
 
-        if line.split()[0] == 'cmd':
-           in_cmd = False
-           if cmd:
-               if cmd not in data:
-                   data[cmd] = []
-               data[cmd].append(name)
-               cmd = None
-               name = None
-           if line.split()[-1] in (LC_ID_DYLIB, LC_LOAD_DYLIB, 'LC_RPATH'):
-               cmd = line.split()[-1]
-               in_cmd = True
+        if line.split()[0] == "cmd":
+            in_cmd = False
+            if cmd:
+                if cmd not in data:
+                    data[cmd] = []
+                data[cmd].append(name)
+                cmd = None
+                name = None
+            if line.split()[-1] in (LC_ID_DYLIB, LC_LOAD_DYLIB, "LC_RPATH"):
+                cmd = line.split()[-1]
+                in_cmd = True
 
         if in_cmd:
-            if line.split()[0] == 'name':
+            if line.split()[0] == "name":
                 name = line.split()[1]
-            if line.split()[0] == 'path':
+            if line.split()[0] == "path":
                 name = line.split()[1]
 
     if in_cmd:
@@ -94,29 +92,35 @@ def parse_otool_l(stdout):
 
     return data
 
+
 def parse_macho(path):
-    proc = subprocess.run(['otool', '-l', path], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    proc = subprocess.run(
+        ["otool", "-l", path], stderr=subprocess.PIPE, stdout=subprocess.PIPE
+    )
     stdout = proc.stdout.decode()
-    if stdout.find('is not an object file') != -1:
+    if stdout.find("is not an object file") != -1:
         return
     return parse_otool_l(stdout)
 
 
 def parse_rpath(path):
-    proc = subprocess.run(["readelf", "-a", path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc = subprocess.run(
+        ["readelf", "-a", path], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
     for line in proc.stdout.decode().splitlines():
         # Find either RPATH or READPATH
         if line.find("PATH") == -1:
             continue
-        return line.split(":", 1)[-1].strip().strip('[').strip(']').split(':')
+        return line.split(":", 1)[-1].strip().strip("[").strip("]").split(":")
     return []
+
 
 def handle_macho(path, root_dir):
     obj = parse_macho(path)
     log.info("Processing file %s %r", path, obj)
     if LC_LOAD_DYLIB in obj:
         for x in obj[LC_LOAD_DYLIB]:
-            if path.startswith('@'):
+            if path.startswith("@"):
                 log.info("Skipping dynamic load: %s", path)
                 continue
             if os.path.exists(x):
@@ -126,32 +130,35 @@ def handle_macho(path, root_dir):
                     shutil.copymode(x, y)
                     log.info("Coppied %s to %s", x, y)
                 log.info("Use %s to %s", y, path)
-                z = pathlib.Path('@loader_path') / os.path.relpath(y, pathlib.Path(path).resolve().parent)
-                cmd = ['install_name_tool', '-change', x, z, path]
+                z = pathlib.Path("@loader_path") / os.path.relpath(
+                    y, pathlib.Path(path).resolve().parent
+                )
+                cmd = ["install_name_tool", "-change", x, z, path]
                 subprocess.run(cmd)
                 log.info("Changed %s to %s in %s", x, z, path)
     return obj
 
+
 def is_in_dir(filepath, directory):
-    return os.path.realpath(filepath).startswith(
-        os.path.realpath(directory) + os.sep)
+    return os.path.realpath(filepath).startswith(os.path.realpath(directory) + os.sep)
+
 
 def handle_elf(path, libs, root=None):
     if root is None:
         root = libs
-    proc = subprocess.run(['ldd', path], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    proc = subprocess.run(["ldd", path], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
     needs_rpath = False
     for line in proc.stdout.decode().splitlines():
         if line.find("=>") == -1:
             continue
 
-        lib_name, location_info = [_.strip() for _ in  line.split("=>", 1)]
+        lib_name, location_info = [_.strip() for _ in line.split("=>", 1)]
         if location_info == "not found":
-            #XXX This could be considered an error
+            # XXX This could be considered an error
             log.warning("Unable to find library %s linked from %s", lib_name, path)
             continue
 
-        linked_lib = location_info.rsplit(' ', 1)[0].strip()
+        linked_lib = location_info.rsplit(" ", 1)[0].strip()
         lib_basename = os.path.basename(linked_lib)
 
         if lib_name in LIBCLIBS:
@@ -159,7 +166,7 @@ def handle_elf(path, libs, root=None):
             continue
 
         if is_in_dir(linked_lib, root):
-            #XXX Validate the rpath of the elf points to this file's location
+            # XXX Validate the rpath of the elf points to this file's location
             # and if not add it.
             log.warning("Skip file already within root directory: %s", linked_lib)
             continue
@@ -168,7 +175,7 @@ def handle_elf(path, libs, root=None):
 
         if os.path.exists(relocated_path):
             log.debug("Relocated library exists: %s", relocated_path)
-            #XXX Check hashes?
+            # XXX Check hashes?
         else:
             log.info("Copy %s to %s", linked_lib, relocated_path)
             shutil.copy(linked_lib, relocated_path)
@@ -176,10 +183,8 @@ def handle_elf(path, libs, root=None):
             needs_rpath = True
 
     if needs_rpath:
-        relpart = os.path.relpath(
-            libs, os.path.dirname(path)
-        )
-        if relpart == '.':
+        relpart = os.path.relpath(libs, os.path.dirname(path))
+        if relpart == ".":
             relpath = "$ORIGIN"
         else:
             relpath = str(pathlib.Path("$ORIGIN") / relpart)
@@ -190,14 +195,18 @@ def handle_elf(path, libs, root=None):
             new_rpath = ":".join([relpath] + old_rpath)
             log.info("Set RPATH=%s %s", new_rpath, path)
             proc = subprocess.run(
-                ['patchelf', "--force-rpath", "--set-rpath", new_rpath, path],
-                stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-
+                ["patchelf", "--force-rpath", "--set-rpath", new_rpath, path],
+                stderr=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+            )
 
 
 def main():
     args = parser.parse_args()
-    logging.basicConfig(level=logging.getLevelName(args.log_level.upper()), format="%(asctime)s %(message)s")
+    logging.basicConfig(
+        level=logging.getLevelName(args.log_level.upper()),
+        format="%(asctime)s %(message)s",
+    )
     root_dir = str(pathlib.Path(args.root).resolve())
     libs_dir = str(pathlib.Path(args.libs).resolve())
     processed = {}
