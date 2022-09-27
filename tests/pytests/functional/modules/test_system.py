ed88136b8339bde135a205923145b8cddf306dcf
Gareth J. Greenaway <gareth@saltstack.com>
2022-09-27 15:02:38 -0700

    migrate to functional test.

--- tests/pytests/functional/modules/test_system.py
+++ tests/pytests/functional/modules/test_system.py
@@ -3,6 +3,7 @@ import logging
 import os
 import signal
 import subprocess
+import textwrap
 
 import pytest
 
@@ -21,18 +22,23 @@ def cmdmod(modules):
 
 
 @pytest.fixture(scope="module")
+def grains(modules):
+    return modules.grains
+
+
+@pytest.fixture(scope="module")
 def file(modules):
     return modules.file
 
 
 @pytest.fixture(scope="module")
-def system(modules):
-    return modules.system
+def service(modules):
+    return modules.service
 
 
 @pytest.fixture(scope="module")
-def service(modules):
-    return modules.service
+def system(modules):
+    return modules.system
 
 
 @pytest.fixture(scope="function")
@@ -54,6 +60,7 @@ def setup_teardown_vars(file, service, system):
         _systemd_timesyncd_available_ = service.available("systemd-timesyncd")
         if _systemd_timesyncd_available_:
             res = service.stop("systemd-timesyncd")
+            assert res
 
         yield _orig_time
 
@@ -67,7 +74,8 @@ def setup_teardown_vars(file, service, system):
             file.remove("/etc/machine-info")
 
         if _systemd_timesyncd_available_:
-            data = service.start("systemd-timesyncd")
+            res = service.start("systemd-timesyncd")
+            assert res
 
 
 def _set_time(system, new_time, offset=None):
@@ -125,7 +133,7 @@ def _test_hwclock_sync(system, hwclock_has_compare):
             rpipeFd, wpipeFd = os.pipe()
             log.debug("Comparing hwclock to sys clock")
             with os.fdopen(rpipeFd, "r") as rpipe:
-                with os.fdopen(wpipeFd, "w") as wpipe:
+                with os.fdopen(wpipeFd, "w"):
                     with salt.utils.files.fopen(os.devnull, "r") as nulFd:
                         p = subprocess.Popen(
                             args=["hwclock", "--compare"],
@@ -170,7 +178,7 @@ def test_get_system_date_time(setup_teardown_vars, system, fmt_str):
     res = system.get_system_date_time()
     t2 = datetime.datetime.strptime(res, fmt_str)
     msg = "Difference in times is too large. Now: {} Fake: {}".format(t1, t2)
-    assert _same_times(t1, t2, seconds_diff=3)
+    assert _same_times(t1, t2, seconds_diff=3), msg
 
 
 def test_get_system_date_time_utc(setup_teardown_vars, system, fmt_str):
@@ -181,7 +189,7 @@ def test_get_system_date_time_utc(setup_teardown_vars, system, fmt_str):
     res = system.get_system_date_time("+0000")
     t2 = datetime.datetime.strptime(res, fmt_str)
     msg = "Difference in times is too large. Now: {} Fake: {}".format(t1, t2)
-    assert _same_times(t1, t2, seconds_diff=3)
+    assert _same_times(t1, t2, seconds_diff=3), msg
 
 
 @pytest.mark.skip_on_env("ON_DOCKER", eq="1")
@@ -201,7 +209,7 @@ def test_set_system_date_time(setup_teardown_vars, system, hwclock_has_compare):
         time_now, cmp_time
     )
     assert result
-    assert _same_times(time_now, cmp_time)
+    assert _same_times(time_now, cmp_time), msg
     _test_hwclock_sync(system, hwclock_has_compare)
 
 
@@ -214,23 +222,24 @@ def test_set_system_date_time_utc(setup_teardown_vars, system, hwclock_has_compa
     resolution of a second so this test may appear to run in negative time.
     """
     cmp_time = datetime.datetime.utcnow() - datetime.timedelta(days=7)
-    result = _set_time(system, cmp_time, offset=None)
+    result = _set_time(system, cmp_time, offset="+0000")
 
     time_now = datetime.datetime.utcnow()
 
     msg = "Difference in times is too large. Now: {} Fake: {}".format(
         time_now, cmp_time
     )
-    breakpoint()
     assert result
-    assert _same_times(time_now, cmp_time)
+    assert _same_times(time_now, cmp_time), msg
     _test_hwclock_sync(system, hwclock_has_compare)
 
 
 @pytest.mark.skip_on_env("ON_DOCKER", eq="1")
 @pytest.mark.destructive_test
 @pytest.mark.skip_if_not_root
-def test_set_system_date_time_utcoffset_east(setup_teardown_vars, system):
+def test_set_system_date_time_utcoffset_east(
+    setup_teardown_vars, system, hwclock_has_compare
+):
     """
     Test changing the system clock. We are only able to set it up to a
     resolution of a second so this test may appear to run in negative time.
@@ -238,21 +247,23 @@ def test_set_system_date_time_utcoffset_east(setup_teardown_vars, system):
     cmp_time = datetime.datetime.utcnow() - datetime.timedelta(days=7)
     # 25200 seconds = 7 hours
     time_to_set = cmp_time - datetime.timedelta(seconds=25200)
-    result = _set_time(system, time_to_set, offset="'-0700'")
+    result = _set_time(system, time_to_set, offset="-0700")
     time_now = datetime.datetime.utcnow()
 
     msg = "Difference in times is too large. Now: {} Fake: {}".format(
         time_now, cmp_time
     )
     assert result
-    assert _same_times(time_now, cmp_time)
-    _test_hwclock_sync(system)
+    assert _same_times(time_now, cmp_time), msg
+    _test_hwclock_sync(system, hwclock_has_compare)
 
 
 @pytest.mark.skip_on_env("ON_DOCKER", eq="1")
 @pytest.mark.destructive_test
 @pytest.mark.skip_if_not_root
-def test_set_system_date_time_utcoffset_west(setup_teardown_vars, salt_call_cli):
+def test_set_system_date_time_utcoffset_west(
+    setup_teardown_vars, system, hwclock_has_compare
+):
     """
     Test changing the system clock. We are only able to set it up to a
     resolution of a second so this test may appear to run in negative time.
@@ -260,28 +271,28 @@ def test_set_system_date_time_utcoffset_west(setup_teardown_vars, salt_call_cli)
     cmp_time = datetime.datetime.utcnow() - datetime.timedelta(days=7)
     # 7200 seconds = 2 hours
     time_to_set = cmp_time + datetime.timedelta(seconds=7200)
-    result = _set_time(salt_call_cli, time_to_set, offset="'+0200'")
+    result = _set_time(system, time_to_set, offset="+0200")
     time_now = datetime.datetime.utcnow()
 
     msg = "Difference in times is too large. Now: {} Fake: {}".format(
         time_now, cmp_time
     )
     assert result
-    assert _same_times(time_now, cmp_time)
-    _test_hwclock_sync(salt_call_cli)
+    assert _same_times(time_now, cmp_time), msg
+    _test_hwclock_sync(system, hwclock_has_compare)
 
 
 @pytest.mark.skip_on_env("ON_DOCKER", eq="1")
 @pytest.mark.flaky(max_runs=4)
 @pytest.mark.destructive_test
 @pytest.mark.skip_if_not_root
-def test_set_system_time(setup_teardown_vars, salt_call_cli):
+def test_set_system_time(setup_teardown_vars, system, hwclock_has_compare):
     """
     Test setting the system time without adjusting the date.
     """
     cmp_time = datetime.datetime.now().replace(hour=10, minute=5, second=0)
 
-    result = salt_call_cli.run("system.set_system_time", "'10:05:00'")
+    result = system.set_system_time("10:05:00")
 
     time_now = datetime.datetime.now()
     msg = "Difference in times is too large. Now: {} Fake: {}".format(
@@ -289,68 +300,69 @@ def test_set_system_time(setup_teardown_vars, salt_call_cli):
     )
 
     assert result
-    assert _same_times(time_now, cmp_time)
-    _test_hwclock_sync(salt_call_cli)
+    assert _same_times(time_now, cmp_time), msg
+    _test_hwclock_sync(system, hwclock_has_compare)
 
 
 @pytest.mark.skip_on_env("ON_DOCKER", eq="1")
 @pytest.mark.destructive_test
 @pytest.mark.skip_if_not_root
-def test_set_system_date(setup_teardown_vars, salt_call_cli):
+def test_set_system_date(setup_teardown_vars, system, hwclock_has_compare):
     """
     Test setting the system date without adjusting the time.
     """
     cmp_time = datetime.datetime.now() - datetime.timedelta(days=7)
 
-    result = salt_call_cli.run("system.set_system_date", cmp_time.strftime("%Y-%m-%d"))
+    result = system.set_system_date(cmp_time.strftime("%Y-%m-%d"))
 
     time_now = datetime.datetime.now()
+
     msg = "Difference in times is too large. Now: {} Fake: {}".format(
         time_now, cmp_time
     )
 
     assert result
-    assert _same_times(time_now, cmp_time)
-    _test_hwclock_sync(salt_call_cli)
+    assert _same_times(time_now, cmp_time), msg
+    _test_hwclock_sync(system, hwclock_has_compare)
 
 
 @pytest.mark.skip_if_not_root
-def test_get_computer_desc(setup_teardown_vars, salt_call_cli):
+def test_get_computer_desc(setup_teardown_vars, system, cmdmod):
     """
     Test getting the system hostname
     """
-    res = salt_call_cli.run("system.get_computer_desc")
+    res = system.get_computer_desc()
 
     hostname_cmd = salt.utils.path.which("hostnamectl")
     if hostname_cmd:
-        desc = salt_call_cli.run("cmd.run", ["hostnamectl status --pretty"])
-        assert res.data == desc.data
+        desc = cmdmod.run("hostnamectl status --pretty")
+        assert res == desc
     else:
         if not os.path.isfile("/etc/machine-info"):
-            assert not res.data
+            assert not res
         else:
             with salt.utils.files.fopen("/etc/machine-info", "r") as mach_info:
                 data = mach_info.read()
-                assert res.data in data.decode("string_escape")
+                assert res in data.decode("string_escape")
 
 
 @pytest.mark.destructive_test
 @pytest.mark.skip_if_not_root
-def test_set_computer_desc(setup_teardown_vars, salt_call_cli):
+def test_set_computer_desc(setup_teardown_vars, system):
     """
     Test setting the computer description
     """
     desc = "test"
-    ret = salt_call_cli.run("system.set_computer_desc", desc)
-    computer_desc = salt_call_cli.run("system.get_computer_desc")
+    ret = system.set_computer_desc(desc)
+    computer_desc = system.get_computer_desc()
 
-    assert ret.data
-    assert desc in computer_desc.data
+    assert ret
+    assert desc in computer_desc
 
 
 @pytest.mark.destructive_test
 @pytest.mark.skip_if_not_root
-def test_set_computer_desc_multiline(setup_teardown_vars, salt_call_cli):
+def test_set_computer_desc_multiline(setup_teardown_vars, system):
     """
     Test setting the computer description with a multiline string with tabs
     and double-quotes.
@@ -361,20 +373,18 @@ def test_set_computer_desc_multiline(setup_teardown_vars, salt_call_cli):
         \tSecond Line: 'single-quoted string'
         \t\tThird Line: "double-quoted string with unicode: питон"'''
     )
-    ret = salt_call_cli.run("system.set_computer_desc", desc)
-    computer_desc = salt.utils.stringutils.to_unicode(
-        salt_call_cli.run("system.get_computer_desc").data
-    )
+    ret = system.set_computer_desc(desc)
+    computer_desc = salt.utils.stringutils.to_unicode(system.get_computer_desc())
 
-    assert ret.data
+    assert ret
     assert desc in computer_desc
 
 
 @pytest.mark.skip_if_not_root
-def test_has_hwclock(setup_teardown_vars, salt_call_cli):
+def test_has_hwclock(setup_teardown_vars, system, grains, hwclock_has_compare):
     """
     Verify platform has a settable hardware clock, if possible.
     """
-    if salt_call_cli.run("grains.get", ["os_family"]) == "NILinuxRT":
-        assert salt_call_cli.run("system._has_settable_hwclock")
-        assert _hwclock_has_compare(salt_call_cli)
+    if grains.get("os_family") == "NILinuxRT":
+        assert system._has_settable_hwclock()
+        assert hwclock_has_compare(system)
