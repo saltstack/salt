ba2048bbaa8b7f2e164f9f3c5e2fc160416b113a
Benjamin Drung <bdrung@ubuntu.com>
2022-10-01 00:24:18 +0200

    Fix freedesktop_os_release mock in test_linux_proc_files_with_non_utf8_chars
    `freedesktop_os_release` must return a dictionary containing at least
    `NAME`, `ID`, and `PRETTY_NAME` or raise an `OSError`. Fix the mock in
    `test_linux_proc_files_with_non_utf8_chars` to return a minimal
    dictionary instead of an empty mock.
    
    Signed-off-by: Benjamin Drung <bdrung@ubuntu.com>

--- tests/pytests/unit/grains/test_core.py
+++ tests/pytests/unit/grains/test_core.py
@@ -3298,6 +3298,7 @@ def test_linux_proc_files_with_non_utf8_chars():
     _salt_utils_files_fopen = salt.utils.files.fopen
 
     empty_mock = MagicMock(return_value={})
+    os_release_mock = {"NAME": "Linux", "ID": "linux", "PRETTY_NAME": "Linux"}
 
     with tempfile.TemporaryDirectory() as tempdir:
 
@@ -3322,7 +3323,7 @@ def test_linux_proc_files_with_non_utf8_chars():
         ), patch.object(
             core, "_parse_lsb_release", return_value=empty_mock
         ), patch.object(
-            core, "_freedesktop_os_release", return_value=empty_mock
+            core, "_freedesktop_os_release", return_value=os_release_mock
         ), patch.object(
             core, "_hw_data", return_value=empty_mock
         ), patch.object(
