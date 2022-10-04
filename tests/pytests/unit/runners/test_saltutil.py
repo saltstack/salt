7408a21c62c3960ef1b5e1e1889910d346bb80c7
Gareth J. Greenaway <gareth@saltstack.com>
2022-10-04 13:15:40 -0700

    consolidate the unit functions

--- tests/pytests/unit/runners/test_saltutil.py
+++ tests/pytests/unit/runners/test_saltutil.py
@@ -5,6 +5,69 @@ import salt.runners.saltutil as saltutil
 from tests.support.mock import MagicMock, patch
 
 
+def get_module_types():
+    module_types = [
+        "clouds",
+        "modules",
+        "states",
+        "grains",
+        "renderers",
+        "returners",
+        "output",
+        "proxy",
+        "runners",
+        "wheel",
+        "engines",
+        "thorium",
+        "queues",
+        "pillar",
+        "utils",
+        "sdb",
+        "cache",
+        "fileserver",
+        "tops",
+        "tokens",
+        "serializers",
+        "executors",
+        "roster",
+    ]
+    return module_types
+
+
+@pytest.fixture(params=get_module_types())
+def module_type(request):
+    yield request.param
+
+
+@pytest.fixture
+def module_sync_functions():
+    yield {
+        "clouds": "clouds",
+        "modules": "modules",
+        "states": "states",
+        "grains": "grains",
+        "renderers": "renderers",
+        "returners": "returners",
+        "output": "output",
+        "proxy": "proxymodules",
+        "runners": "runners",
+        "wheel": "wheel",
+        "engines": "engines",
+        "thorium": "thorium",
+        "queues": "queues",
+        "pillar": "pillar",
+        "utils": "utils",
+        "sdb": "sdb",
+        "cache": "cache",
+        "fileserver": "fileserver",
+        "tops": "tops",
+        "tokens": "eauth_tokens",
+        "serializers": "serializers",
+        "executors": "executors",
+        "roster": "roster",
+    }
+
+
 @pytest.fixture
 def configure_loader_modules():
     opts = salt.config.DEFAULT_MASTER_OPTS.copy()
@@ -13,8 +76,11 @@ def configure_loader_modules():
 
 
 def test_sync_all():
+    """
+    Test saltutil.sync_all
+    """
     sync_out = MagicMock(return_value=[[], True])
-    roster_keys = [
+    module_types = [
         "clouds",
         "modules",
         "states",
@@ -41,277 +107,24 @@ def test_sync_all():
     ]
     with patch("salt.utils.extmods.sync", sync_out):
         ret = saltutil.sync_all()
-        for key in roster_keys:
+        for key in module_types:
             assert key in ret
 
 
-def test_sync_modules():
+def test_sync(module_type, module_sync_functions):
+    """
+    Test saltutil.sync functions
+    """
     sync_out = MagicMock(return_value=[[], True])
     with patch("salt.utils.extmods.sync", sync_out) as extmods_sync:
         ret = saltutil.sync_modules()
-        assert ret == []
-
-        extmods_sync.assert_called_with(
-            {}, "modules", extmod_blacklist=None, extmod_whitelist=None, saltenv="base"
-        )
-
-
-def test_sync_states():
-    sync_out = MagicMock(return_value=[[], True])
-    with patch("salt.utils.extmods.sync", sync_out) as extmods_sync:
-        ret = saltutil.sync_states()
-        assert ret == []
-
-        extmods_sync.assert_called_with(
-            {}, "states", extmod_blacklist=None, extmod_whitelist=None, saltenv="base"
-        )
-
-
-def test_sync_grains():
-    sync_out = MagicMock(return_value=[[], True])
-    with patch("salt.utils.extmods.sync", sync_out) as extmods_sync:
-        ret = saltutil.sync_grains()
-        assert ret == []
-
-        extmods_sync.assert_called_with(
-            {}, "grains", extmod_blacklist=None, extmod_whitelist=None, saltenv="base"
-        )
-
-
-def test_sync_renderers():
-    sync_out = MagicMock(return_value=[[], True])
-    with patch("salt.utils.extmods.sync", sync_out) as extmods_sync:
-        ret = saltutil.sync_renderers()
+        func = "sync_{}".format(module_sync_functions[module_type])
+        ret = getattr(saltutil, func)()
         assert ret == []
 
         extmods_sync.assert_called_with(
             {},
-            "renderers",
-            extmod_blacklist=None,
-            extmod_whitelist=None,
-            saltenv="base",
-        )
-
-
-def test_sync_returners():
-    sync_out = MagicMock(return_value=[[], True])
-    with patch("salt.utils.extmods.sync", sync_out) as extmods_sync:
-        ret = saltutil.sync_returners()
-        assert ret == []
-
-        extmods_sync.assert_called_with(
-            {},
-            "returners",
-            extmod_blacklist=None,
-            extmod_whitelist=None,
-            saltenv="base",
-        )
-
-
-def test_sync_output():
-    sync_out = MagicMock(return_value=[[], True])
-    with patch("salt.utils.extmods.sync", sync_out) as extmods_sync:
-        ret = saltutil.sync_output()
-        assert ret == []
-
-        extmods_sync.assert_called_with(
-            {}, "output", extmod_blacklist=None, extmod_whitelist=None, saltenv="base"
-        )
-
-
-def test_sync_proxymodules():
-    sync_out = MagicMock(return_value=[[], True])
-    with patch("salt.utils.extmods.sync", sync_out) as extmods_sync:
-        ret = saltutil.sync_proxymodules()
-        assert ret == []
-
-        extmods_sync.assert_called_with(
-            {}, "proxy", extmod_blacklist=None, extmod_whitelist=None, saltenv="base"
-        )
-
-
-def test_sync_runners():
-    sync_out = MagicMock(return_value=[[], True])
-    with patch("salt.utils.extmods.sync", sync_out) as extmods_sync:
-        ret = saltutil.sync_runners()
-        assert ret == []
-
-        extmods_sync.assert_called_with(
-            {}, "runners", extmod_blacklist=None, extmod_whitelist=None, saltenv="base"
-        )
-
-
-def test_sync_wheel():
-    sync_out = MagicMock(return_value=[[], True])
-    with patch("salt.utils.extmods.sync", sync_out) as extmods_sync:
-        ret = saltutil.sync_wheel()
-        assert ret == []
-
-        extmods_sync.assert_called_with(
-            {}, "wheel", extmod_blacklist=None, extmod_whitelist=None, saltenv="base"
-        )
-
-
-def test_sync_engines():
-    sync_out = MagicMock(return_value=[[], True])
-    with patch("salt.utils.extmods.sync", sync_out) as extmods_sync:
-        ret = saltutil.sync_engines()
-        assert ret == []
-
-        extmods_sync.assert_called_with(
-            {}, "engines", extmod_blacklist=None, extmod_whitelist=None, saltenv="base"
-        )
-
-
-def test_sync_thorium():
-    sync_out = MagicMock(return_value=[[], True])
-    with patch("salt.utils.extmods.sync", sync_out) as extmods_sync:
-        ret = saltutil.sync_thorium()
-        assert ret == []
-
-        extmods_sync.assert_called_with(
-            {}, "thorium", extmod_blacklist=None, extmod_whitelist=None, saltenv="base"
-        )
-
-
-def test_sync_queues():
-    sync_out = MagicMock(return_value=[[], True])
-    with patch("salt.utils.extmods.sync", sync_out) as extmods_sync:
-        ret = saltutil.sync_queues()
-        assert ret == []
-
-        extmods_sync.assert_called_with(
-            {}, "queues", extmod_blacklist=None, extmod_whitelist=None, saltenv="base"
-        )
-
-
-def test_sync_pillar():
-    sync_out = MagicMock(return_value=[[], True])
-    with patch("salt.utils.extmods.sync", sync_out) as extmods_sync:
-        ret = saltutil.sync_pillar()
-        assert ret == []
-
-        extmods_sync.assert_called_with(
-            {}, "pillar", extmod_blacklist=None, extmod_whitelist=None, saltenv="base"
-        )
-
-
-def test_sync_utils():
-    sync_out = MagicMock(return_value=[[], True])
-    with patch("salt.utils.extmods.sync", sync_out) as extmods_sync:
-        ret = saltutil.sync_utils()
-        assert ret == []
-
-        extmods_sync.assert_called_with(
-            {}, "utils", extmod_blacklist=None, extmod_whitelist=None, saltenv="base"
-        )
-
-
-def test_sync_sdb():
-    sync_out = MagicMock(return_value=[[], True])
-    with patch("salt.utils.extmods.sync", sync_out) as extmods_sync:
-        ret = saltutil.sync_sdb()
-        assert ret == []
-
-        extmods_sync.assert_called_with(
-            {}, "sdb", extmod_blacklist=None, extmod_whitelist=None, saltenv="base"
-        )
-
-
-def test_sync_tops():
-    sync_out = MagicMock(return_value=[[], True])
-    with patch("salt.utils.extmods.sync", sync_out) as extmods_sync:
-        ret = saltutil.sync_tops()
-        assert ret == []
-
-        extmods_sync.assert_called_with(
-            {}, "tops", extmod_blacklist=None, extmod_whitelist=None, saltenv="base"
-        )
-
-
-def test_sync_cache():
-    sync_out = MagicMock(return_value=[[], True])
-    with patch("salt.utils.extmods.sync", sync_out) as extmods_sync:
-        ret = saltutil.sync_cache()
-        assert ret == []
-
-        extmods_sync.assert_called_with(
-            {}, "cache", extmod_blacklist=None, extmod_whitelist=None, saltenv="base"
-        )
-
-
-def test_sync_fileserver():
-    sync_out = MagicMock(return_value=[[], True])
-    with patch("salt.utils.extmods.sync", sync_out) as extmods_sync:
-        ret = saltutil.sync_fileserver()
-        assert ret == []
-
-        extmods_sync.assert_called_with(
-            {},
-            "fileserver",
-            extmod_blacklist=None,
-            extmod_whitelist=None,
-            saltenv="base",
-        )
-
-
-def test_sync_clouds():
-    sync_out = MagicMock(return_value=[[], True])
-    with patch("salt.utils.extmods.sync", sync_out) as extmods_sync:
-        ret = saltutil.sync_clouds()
-        assert ret == []
-
-        extmods_sync.assert_called_with(
-            {}, "clouds", extmod_blacklist=None, extmod_whitelist=None, saltenv="base"
-        )
-
-
-def test_sync_roster():
-    sync_out = MagicMock(return_value=[[], True])
-    with patch("salt.utils.extmods.sync", sync_out) as extmods_sync:
-        ret = saltutil.sync_roster()
-        assert ret == []
-
-        extmods_sync.assert_called_with(
-            {}, "roster", extmod_blacklist=None, extmod_whitelist=None, saltenv="base"
-        )
-
-
-def test_sync_eauth_tokens():
-    sync_out = MagicMock(return_value=[[], True])
-    with patch("salt.utils.extmods.sync", sync_out) as extmods_sync:
-        ret = saltutil.sync_eauth_tokens()
-        assert ret == []
-
-        extmods_sync.assert_called_with(
-            {}, "tokens", extmod_blacklist=None, extmod_whitelist=None, saltenv="base"
-        )
-
-
-def test_sync_serializers():
-    sync_out = MagicMock(return_value=[[], True])
-    with patch("salt.utils.extmods.sync", sync_out) as extmods_sync:
-        ret = saltutil.sync_serializers()
-        assert ret == []
-
-        extmods_sync.assert_called_with(
-            {},
-            "serializers",
-            extmod_blacklist=None,
-            extmod_whitelist=None,
-            saltenv="base",
-        )
-
-
-def test_sync_executors():
-    sync_out = MagicMock(return_value=[[], True])
-    with patch("salt.utils.extmods.sync", sync_out) as extmods_sync:
-        ret = saltutil.sync_executors()
-        assert ret == []
-
-        extmods_sync.assert_called_with(
-            {},
-            "executors",
+            "{}".format(module_type),
             extmod_blacklist=None,
             extmod_whitelist=None,
             saltenv="base",
