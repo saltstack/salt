95bc61bb5b4b8471824481bd6dfbef2d75de9307
unman <unman@thirdeyesecurity.org>
2022-10-30 00:57:19 +0000

    Correct Parrot OS entry in grains _OS_Family_Map. Fix test.

--- salt/grains/core.py
+++ salt/grains/core.py
@@ -1802,7 +1802,7 @@ _OS_FAMILY_MAP = {
     "Devuan": "Debian",
     "antiX": "Debian",
     "Kali": "Debian",
-    "Parrot": "Debian",
+    "Parrot OS": "Debian",
     "neon": "Debian",
     "Cumulus": "Debian",
     "Deepin": "Debian",
