81cb55bbfff4ca96b8f5c2564f2ef0219263a68d
Richard Hansen <rhansen@rhansen.org>
2022-11-10 19:46:07 -0500

    yaml: Delete the ineffectual timestamp representer

--- salt/utils/yamldumper.py
+++ salt/utils/yamldumper.py
@@ -110,8 +110,6 @@ for D in (SafeOrderedDumper, OrderedDumper):
     D.add_representer(
         MaskedList, yaml.representer.SafeRepresenter.represent_list
     )
-    # TODO: This seems wrong: the first argument should be a type, not a tag.
-    D.add_representer("tag:yaml.org,2002:timestamp", Dumper.represent_scalar)
 del D
 
 IndentedSafeOrderedDumper.add_representer(OrderedDict, represent_ordereddict)
