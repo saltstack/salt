80ce87b2bacfcd7beac9a6dd3f9a209dbd823ba6
Pedro Algarvio <palgarvio@vmware.com>
2022-11-13 11:07:48 +0000

    Use the `minion_opts` and `master_opts` fixtures instead.
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- tests/pytests/unit/states/test_boto_cloudtrail.py
+++ tests/pytests/unit/states/test_boto_cloudtrail.py
@@ -4,7 +4,6 @@ import string
 
 import pytest
 
-import salt.config
 import salt.loader
 import salt.states.boto_cloudtrail as boto_cloudtrail
 from tests.support.mock import MagicMock, patch
@@ -64,21 +63,20 @@ def global_config():
 
 
 @pytest.fixture
-def configure_loader_modules():
-    opts = salt.config.DEFAULT_MINION_OPTS.copy()
-    opts["grains"] = salt.loader.grains(opts)
+def configure_loader_modules(minion_opts):
+    minion_opts["grains"] = salt.loader.grains(minion_opts)
     ctx = {}
     utils = salt.loader.utils(
-        opts,
+        minion_opts,
         whitelist=["boto", "boto3", "args", "systemd", "path", "platform", "reg"],
         context=ctx,
     )
-    serializers = salt.loader.serializers(opts)
+    serializers = salt.loader.serializers(minion_opts)
     funcs = salt.loader.minion_mods(
-        opts, context=ctx, utils=utils, whitelist=["boto_cloudtrail"]
+        minion_opts, context=ctx, utils=utils, whitelist=["boto_cloudtrail"]
     )
     salt_states = salt.loader.states(
-        opts=opts,
+        opts=minion_opts,
         functions=funcs,
         utils=utils,
         whitelist=["boto_cloudtrail"],
@@ -86,7 +84,7 @@ def configure_loader_modules():
     )
     return {
         boto_cloudtrail: {
-            "__opts__": opts,
+            "__opts__": minion_opts,
             "__salt__": funcs,
             "__utils__": utils,
             "__states__": salt_states,
