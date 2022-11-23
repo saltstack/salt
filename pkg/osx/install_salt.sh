7882651273c0952abbf9522e109cc1188590084f
Twangboy <leesh@vmware.com>
2022-11-22 20:52:59 -0700

    Don't need the .

--- pkg/osx/install_salt.sh
+++ pkg/osx/install_salt.sh
@@ -99,7 +99,7 @@ fi
 # Install Salt into the Python Environment
 #-------------------------------------------------------------------------------
 _msg "Installing Salt"
-$PIP_BIN install "$SRC_DIR/."
+$PIP_BIN install "$SRC_DIR"
 TEST_DIR="$SCRIPT_DIR/build/opt/salt/lib/python3.*/site-packages/salt*"
 if compgen -G "$TEST_DIR" > /dev/null; then
     _success
