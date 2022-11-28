07b5980bf4ec2425a02aabfcc0f90269f75a44d2
nicholasmhughes <nicholasmhughes@gmail.com>
2022-11-28 17:24:21 -0500

    fixes saltstack/salt#63131 add boardname grain

--- tests/pytests/unit/grains/test_core.py
+++ tests/pytests/unit/grains/test_core.py
@@ -2951,6 +2951,7 @@ def test__hw_data_linux_empty():
             "biosreleasedate": "",
             "biosversion": "",
             "biosvendor": "",
+            "boardname": "",
             "manufacturer": "",
             "productname": "",
             "serialnumber": "",
