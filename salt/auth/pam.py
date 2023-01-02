af3c40260862ffcd0635ced732ce3a95263c02a5
Daniel A. Wozniak <dwozniak@vmware.com>
2023-01-02 16:44:22 -0700

    Linter fix, clean up un-needed exception handler

--- salt/auth/pam.py
+++ salt/auth/pam.py
@@ -247,6 +247,7 @@ def authenticate(username, password):
         env=env,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
+        check=False,
     )
     if ret.returncode == 0:
         return True
@@ -271,16 +272,11 @@ def groups(username, *args, **kwargs):
 
 
 if __name__ == "__main__":
-    try:
-        if _authenticate(
-            os.environ["SALT_PAM_USERNAME"],
-            os.environ["SALT_PAM_PASSWORD"],
-            os.environ["SALT_PAM_SERVICE"],
-            os.environ["SALT_PAM_ENCODING"],
-        ):
-            sys.exit(0)
-    except Exception as exc:
-        sys.stderr.write(exc)
-        sys.stderr.flush()
-        sys.exit(3)
+    if _authenticate(
+        os.environ["SALT_PAM_USERNAME"],
+        os.environ["SALT_PAM_PASSWORD"],
+        os.environ["SALT_PAM_SERVICE"],
+        os.environ["SALT_PAM_ENCODING"],
+    ):
+        sys.exit(0)
     sys.exit(1)
