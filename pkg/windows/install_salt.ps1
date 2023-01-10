a3b2849ec6e194f156bc8ea34041abfc4c932de8
Twangboy <leesh@vmware.com>
2023-01-09 17:28:02 -0700

    Don't right justify success/failure in CICD

--- pkg/windows/install_salt.ps1
+++ pkg/windows/install_salt.ps1
@@ -13,6 +13,12 @@ ready to be packaged.
 install_salt.ps1
 
 #>
+param(
+    [Parameter(Mandatory=$false)]
+    [Alias("c")]
+    # Don't pretify the output of the Write-Result
+    [Switch] $CICD
+)
 
 #-------------------------------------------------------------------------------
 # Script Preferences
@@ -27,8 +33,12 @@ $ErrorActionPreference = "Stop"
 #-------------------------------------------------------------------------------
 
 function Write-Result($result, $ForegroundColor="Green") {
-    $position = 80 - $result.Length - [System.Console]::CursorLeft
-    Write-Host -ForegroundColor $ForegroundColor ("{0,$position}$result" -f "")
+    if ( $CICD ) {
+        Write-Host $result -ForegroundColor $ForegroundColor
+    } else {
+        $position = 80 - $result.Length - [System.Console]::CursorLeft
+        Write-Host -ForegroundColor $ForegroundColor ("{0,$position}$result" -f "")
+    }
 }
 
 #-------------------------------------------------------------------------------
