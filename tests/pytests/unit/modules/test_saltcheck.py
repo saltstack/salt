18c22579f85bbc8fa2f0bcbb1fa4fa5d5513fb53
Meghann Cunningham <mc@meghann.dev>
2023-01-11 13:02:46 -0600

    fixes saltstack/salt#63463 add JUnit output for saltcheck

--- tests/pytests/unit/modules/test_saltcheck.py
+++ tests/pytests/unit/modules/test_saltcheck.py
@@ -16,5 +16,26 @@ def test__get_top_states_call_args(saltenv):
 
 
 def test__generate_junit_out_list():
-    results = {}
+    results = {
+        "apache": {
+            "echo_test_hello": {"status": "Pass", "duration": 2.5907},
+            "echo_test_hello2": {
+                "status": "Fail: fail expected is not equal to hello",
+                "duration": 0.4503,
+            },
+            "echo_test_hello3": {"status": "Skip", "duration": 0.0},
+        }
+    }
+
+    expected = (
+        """<?xml version="1.0" ?>\n<testsuites disabled="0" errors="0" failures="1" tests="3" time="3.041">\n"""
+        + """\t<testsuite disabled="0" errors="0" failures="1" name="test_results" skipped="1" tests="3" time="3.041">\n"""
+        + """\t\t<testcase name="echo_test_hello" time="2.590700"/>\n"""
+        + """\t\t<testcase name="echo_test_hello2" time="0.450300">\n"""
+        + """\t\t\t<failure type="failure" message="Fail: fail expected is not equal to hello"/>\n\t\t</testcase>\n"""
+        + """\t\t<testcase name="echo_test_hello3">\n\t\t\t<skipped type="skipped" message="Skip"/>\n\t\t</testcase>\n"""
+        + """\t</testsuite>\n</testsuites>\n"""
+    )
+
     ret = saltcheck._generate_junit_out_list(results)
+    assert ret == expected
