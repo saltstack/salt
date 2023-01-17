ffe0d62babb9c5afdd20b0d7b36c1fdaa039649f
Megan Wilhite <mwilhite@vmware.com>
2023-01-17 11:21:32 -0700

    Initial setup of package tests (migrated from salt-pkg)

---
+++ pkg/tests/integration/test_salt_api.py
@@ -0,0 +1,14 @@
+def test_salt_api(api_request):
+    """
+    Test running a command against the salt api
+    """
+    ret = api_request.post(
+        "/run",
+        data={
+            "client": "local",
+            "tgt": "*",
+            "fun": "test.arg",
+            "arg": ["foo", "bar"],
+        },
+    )
+    assert ret["args"] == ["foo", "bar"]
