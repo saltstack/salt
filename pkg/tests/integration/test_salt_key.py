8db4ab15ef056acbaf8cbc328702b37ee87ae67c
Megan Wilhite <mwilhite@vmware.com>
2023-01-17 11:21:32 -0700

    Initial setup of package tests (migrated from salt-pkg)

---
+++ pkg/tests/integration/test_salt_key.py
@@ -0,0 +1,7 @@
+def test_salt_key(salt_key_cli, salt_minion):
+    """
+    Test running salt-key -L
+    """
+    ret = salt_key_cli.run("-L")
+    assert ret.data
+    assert salt_minion.id in ret.data["minions"]
