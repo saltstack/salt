10679d41773073140710ddd2f0bd138d7ec9dca7
Daniel A. Wozniak <dwozniak@vmware.com>
2023-01-23 18:15:05 -0700

    Generate package changelogs

--- tools/__init__.py
+++ tools/__init__.py
@@ -3,6 +3,7 @@ import logging
 import tools.ci
 import tools.pkg
 import tools.vm
+import tools.changelog
 
 for name in ("boto3", "botocore", "urllib3"):
     logging.getLogger(name).setLevel(logging.INFO)
