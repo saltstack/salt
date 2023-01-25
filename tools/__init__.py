682777e1d1c7abe5bbe7b1d0a5323b381b9a3fbb
Daniel A. Wozniak <dwozniak@vmware.com>
2023-01-24 22:14:48 -0700

    Add tools docs

--- tools/__init__.py
+++ tools/__init__.py
@@ -4,6 +4,7 @@ import tools.ci
 import tools.pkg
 import tools.vm
 import tools.changelog
+import tools.docs
 
 for name in ("boto3", "botocore", "urllib3"):
     logging.getLogger(name).setLevel(logging.INFO)
