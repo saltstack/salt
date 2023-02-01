f384888b6ff463b25b92122df3537e29040a077e
Vishal Gupta <guvishal@vmware.com>
2023-02-01 20:03:35 +0530

    metadata_azure: added tests and docs

--- salt/grains/metadata_azure.py
+++ salt/grains/metadata_azure.py
@@ -1,11 +1,17 @@
 """
-Grains from cloud metadata servers at 169.254.169.254 in azure
-.. versionadded:: 3005.0
+Grains from cloud metadata servers at 169.254.169.254 in Azure Virtual Machine
+
+.. versionadded:: 3006.0
+
 :depends: requests
+
 To enable these grains that pull from the http://169.254.169.254/metadata/instance?api-version=2020-09-01
 metadata server set `metadata_server_grains: True` in the minion config.
+
 .. code-block:: yaml
+
     metadata_server_grains: True
+
 """
 
 import logging
@@ -22,10 +28,10 @@ def __virtual__():
     # Check if metadata_server_grains minion option is enabled
     if __opts__.get("metadata_server_grains", False) is False:
         return False
-    azuretest = http.query(URL, status=True, headers=True, header_list=["Metadata: true"])
-    if (
-        azuretest.get("status", 404) != 200
-    ):
+    azuretest = http.query(
+        URL, status=True, headers=True, header_list=["Metadata: true"]
+    )
+    if azuretest.get("status", 404) != 200:
         return False
     return True
 
