76058b720dcd3f124eafca4043b1ad3a76adcf5f
Daniel A. Wozniak <dwozniak@vmware.com>
2023-02-03 22:52:21 -0700

    Remove onedir test skip

--- tests/pytests/scenarios/setup/test_man.py
+++ tests/pytests/scenarios/setup/test_man.py
@@ -14,7 +14,6 @@ pytestmark = [
     pytest.mark.core_test,
     pytest.mark.skip_on_windows,
     pytest.mark.skip_on_aix,
-    pytest.mark.skip_initial_onedir_failure,
     pytest.mark.skip_if_binaries_missing(*KNOWN_BINARY_NAMES, check_all=False),
 ]
 
