1e45b3d1450c4157a52ce8c06ceb7888ab2a5f25
Twangboy <shane.d.lee@gmail.com>
2023-02-08 13:51:16 -0700

    Go back to salt_call_cli.run

--- pkg/tests/integration/test_salt_call.py
+++ pkg/tests/integration/test_salt_call.py
@@ -1,15 +1,12 @@
-import os.path
-
 import pytest
 
 
-def test_salt_call_local(install_salt):
+def test_salt_call_local(salt_call_cli):
     """
     Test salt-call --local test.ping
     """
-    test_bin = os.path.join(*install_salt.binary_paths["call"])
-    ret = install_salt.proc.run(test_bin, "--local", "test.ping")
-    assert "True" in ret.stdout
+    ret = salt_call_cli.run("--local", "test.ping")
+    assert ret.data is True
     assert ret.returncode == 0
 
 
@@ -33,33 +30,30 @@ def test_sls(salt_call_cli):
     assert ret.returncode == 0
 
 
-def test_salt_call_local_sys_doc_none(install_salt):
+def test_salt_call_local_sys_doc_none(salt_call_cli):
     """
     Test salt-call --local sys.doc none
     """
-    test_bin = os.path.join(*install_salt.binary_paths["call"])
-    ret = install_salt.proc.run(test_bin, "--local", "sys.doc", "none")
-    assert "local:\n    ----------\n" == ret.stdout
+    ret = salt_call_cli.run("--local", "sys.doc", "none")
+    assert not ret.data
     assert ret.returncode == 0
 
 
-def test_salt_call_local_sys_doc_aliases(install_salt):
+def test_salt_call_local_sys_doc_aliases(salt_call_cli):
     """
     Test salt-call --local sys.doc aliases
     """
-    test_bin = os.path.join(*install_salt.binary_paths["call"])
-    ret = install_salt.proc.run(test_bin, "--local", "sys.doc", "aliases.list_aliases")
-    assert "aliases.list_aliases" in ret.stdout
+    ret = salt_call_cli.run("--local", "sys.doc", "aliases.list_aliases")
+    assert "aliases.list_aliases" in ret.data
     assert ret.returncode == 0
 
 
 @pytest.mark.skip_on_windows()
-def test_salt_call_cmd_run_id_runas(install_salt, test_account, caplog):
+def test_salt_call_cmd_run_id_runas(salt_call_cli, test_account, caplog):
     """
     Test salt-call --local cmd_run id with runas
     """
-    test_bin = os.path.join(*install_salt.binary_paths["call"])
-    ret = install_salt.proc.run(test_bin, "--local", "id", runas=test_account.username)
+    ret = salt_call_cli.run("--local", "cmd.run", "id", runas=test_account.username)
     assert "Environment could not be retrieved for user" not in caplog.text
     assert str(test_account.uid) in ret.stdout
     assert str(test_account.gid) in ret.stdout
