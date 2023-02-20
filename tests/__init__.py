e9d2b6a19c647c7ddd94450819108425a27ded50
Pedro Algarvio <palgarvio@vmware.com>
2023-02-20 17:48:01 +0000

    Dam you Windows Paths!
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,12 +3,18 @@ import pathlib
 import sys
 
 _repo_root = pathlib.Path(__file__).parent.parent
+_paths_to_check = {""}
+if sys.platform.startswith("win"):
+    _paths_to_check.add(str(_repo_root).replace("\\", "\\\\"))
+    _paths_to_check.add(str(_repo_root.resolve()).replace("\\", "\\\\"))
+else:
+    _paths_to_check.add(str(_repo_root))
+
 
 if os.environ.get("ONEDIR_TESTRUN", "0") == "1":
     # In this particular case, we want to make sure that the repo root
     # is not part if sys.path so that when we import salt, we import salt from
     # the onedir and not the code checkout
-    if "" in sys.path:
-        sys.path.remove("")
-    if str(_repo_root) in sys.path:
-        sys.path.remove(str(_repo_root))
+    for path in _paths_to_check:
+        if path in sys.path:
+            sys.path.remove(path)
