22bc537e0a9dc58385851718d26a8e1d75e01f5a
Pedro Algarvio <palgarvio@vmware.com>
2023-02-22 16:17:19 +0000

    Add `__getstate__` to blacklisted methods, present in Py 3.11
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- tests/unit/test_master.py
+++ tests/unit/test_master.py
@@ -56,6 +56,7 @@ class TransportMethodsTest(TestCase):
             "__format__",
             "__ge__",
             "__getattribute__",
+            "__getstate__",
             "__gt__",
             "__hash__",
             "__init__",
@@ -71,9 +72,9 @@ class TransportMethodsTest(TestCase):
             "__sizeof__",
             "__str__",
             "__subclasshook__",
+            "destroy",
             "get_method",
             "run_func",
-            "destroy",
         ]
         for name in dir(aes_funcs):
             if name in aes_funcs.expose_methods:
@@ -108,6 +109,7 @@ class TransportMethodsTest(TestCase):
             "__format__",
             "__ge__",
             "__getattribute__",
+            "__getstate__",
             "__gt__",
             "__hash__",
             "__init__",
@@ -128,9 +130,9 @@ class TransportMethodsTest(TestCase):
             "_prep_pub",
             "_send_pub",
             "_send_ssh_pub",
-            "get_method",
-            "destroy",
             "connect",
+            "destroy",
+            "get_method",
         ]
         for name in dir(clear_funcs):
             if name in clear_funcs.expose_methods:
