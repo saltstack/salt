ce5e1985510642f7df4a0f61d4fe682c003ce955
Will Hegedus <whegedus@linode.com>
2023-02-27 10:00:04 -0500

    fix: don't re-import matchers in Pillar top
    In large pillar environments with complex top files, I have observed that the most
    intensive (both measured by time and by CPU) operation is actually just the overhead of
    constantly re-importing all the matchers for each match in a top file.
    
    By reusing work already done to avoid this gotcha elsewhere (see compound_match.py),
    Pillar render times can be reduced by >6x and load on the Salt master can be
    reduced by >5x.

--- salt/matchers/confirm_top.py
+++ salt/matchers/confirm_top.py
@@ -10,6 +10,14 @@ import salt.loader
 log = logging.getLogger(__file__)
 
 
+def _load_matchers(opts):
+    """
+    Store matchers in __context__ so they're only loaded once
+    """
+    __context__["matchers"] = {}
+    __context__["matchers"] = salt.loader.matchers(opts)
+
+
 def confirm_top(match, data, nodegroups=None):
     """
     Takes the data passed to a top file environment and determines if the
@@ -21,7 +29,10 @@ def confirm_top(match, data, nodegroups=None):
             if "match" in item:
                 matcher = item["match"]
 
-    matchers = salt.loader.matchers(__opts__)
+    if "matchers" not in __context__:
+        _load_matchers(__opts__)
+    matchers = __context__["matchers"]
+
     funcname = matcher + "_match.match"
     if matcher == "nodegroup":
         return matchers[funcname](match, nodegroups)
