16f2d9be0cb877e73bcf0e89a219bc98ec660c6f
MKLeb <calebb@vmware.com>
2023-02-28 00:24:42 -0500

    Skip salt state file package test when state takes too long to apply

--- pkg/tests/integration/test_salt_state_file.py
+++ pkg/tests/integration/test_salt_state_file.py
@@ -17,6 +17,8 @@ def test_salt_state_file(salt_cli, salt_minion):
         ret = salt_cli.run("state.apply", "states", minion_tgt=salt_minion.id)
 
     assert ret.data, ret
+    if ret.stdout and "Minion did not return" in ret.stdout:
+        pytest.skip("Skipping test, state took too long to apply")
     sls_ret = ret.data[next(iter(ret.data))]
     assert "changes" in sls_ret
     assert "name" in sls_ret
