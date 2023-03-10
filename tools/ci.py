f2121b9a104f60fd45986089baf425fce3b3a7f8
Pedro Algarvio <palgarvio@vmware.com>
2023-03-10 10:46:18 +0000

    Adjust bucket names depending on the salt project bot environment we're in
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- tools/ci.py
+++ tools/ci.py
@@ -1,7 +1,7 @@
 """
 These commands are used in the CI pipeline.
 """
-# pylint: disable=resource-leakage,broad-except
+# pylint: disable=resource-leakage,broad-except,3rd-party-module-not-gated
 from __future__ import annotations
 
 import json
