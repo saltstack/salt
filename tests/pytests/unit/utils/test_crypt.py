280e86249067a872b62cd84f80613e4783eb8056
Twangboy <shane.d.lee@gmail.com>
2023-03-21 09:18:05 -0600

    Paramatrized tests, added more tests, convert str to bytes

--- tests/pytests/unit/utils/test_crypt.py
+++ tests/pytests/unit/utils/test_crypt.py
@@ -66,9 +66,10 @@ def test_reinit_crypto():
                     salt.utils.crypt.reinit_crypto()
 
 
-def test_pem_finger_lf(tmp_path, pub_key_data):
-    key_file = tmp_path / "master_lf.pub"
-    key_file.write_bytes("\n".join(pub_key_data).encode("utf-8"))
+@pytest.mark.parametrize("line_ending", ["\n", "\r\n"])
+def test_pem_finger_file_line_endings(tmp_path, pub_key_data, line_ending):
+    key_file = tmp_path / "master_crlf.pub"
+    key_file.write_bytes(line_ending.join(pub_key_data).encode("utf-8"))
     finger = salt.utils.crypt.pem_finger(path=str(key_file))
     assert (
         finger
@@ -76,11 +77,19 @@ def test_pem_finger_lf(tmp_path, pub_key_data):
     )
 
 
-def test_pem_finger_crlf(tmp_path, pub_key_data):
-    key_file = tmp_path / "master_crlf.pub"
-    key_file.write_bytes("\r\n".join(pub_key_data).encode("utf-8"))
-    finger = salt.utils.crypt.pem_finger(path=str(key_file))
+@pytest.mark.parametrize("key", [b"123abc", "123abc"])
+def test_pem_finger_key(key):
+    finger = salt.utils.crypt.pem_finger(key=key)
     assert (
         finger
-        == "9b:42:66:92:8a:d1:b9:27:42:e0:6d:f3:12:c9:74:74:b0:e0:0e:42:83:87:62:ad:95:49:9d:6f:8e:d0:ed:35"
+        == "dd:13:0a:84:9d:7b:29:e5:54:1b:05:d2:f7:f8:6a:4a:cd:4f:1e:c5:98:c1:c9:43:87:83:f5:6b:c4:f0:ff:80"
+    )
+
+
+def test_pem_finger_sha512():
+    finger = salt.utils.crypt.pem_finger(key="123abc", sum_type="sha512")
+    assert (
+        finger
+        == "7b:6a:d7:9b:34:6f:b6:95:12:75:34:39:48:e1:3c:1b:4e:bc:a8:2a:54:52:a6:c5:d1:56:84:37:7f:09:6c:a9:"
+        "27:50:6a:23:a8:47:e6:e0:46:06:13:99:63:1b:16:fc:28:20:c8:b0:e0:2d:0e:a8:7a:a5:a2:03:a7:7c:2a:7e"
     )
