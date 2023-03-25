62b3e518cccb6c2a9d291c60107a6dfc7be1f343
MKLeb <calebb@vmware.com>
2023-03-24 20:09:06 -0400

    use multiprocessing and wait for the minion to actually connect

--- tests/pytests/scenarios/reauth/test_reauth.py
+++ tests/pytests/scenarios/reauth/test_reauth.py
@@ -1,6 +1,6 @@
 import logging
+import multiprocessing
 import os
-import threading
 import time
 
 import pytest
@@ -15,7 +15,7 @@ log = logging.getLogger(__name__)
 
 @pytest.fixture(scope="module")
 def timeout():
-    return int(os.environ.get("SALT_CI_REAUTH_MASTER_WAIT", 100))
+    return int(os.environ.get("SALT_CI_REAUTH_MASTER_WAIT", 150))
 
 
 def test_reauth(salt_cli, salt_minion, salt_master, timeout, event_listener):
@@ -29,11 +29,11 @@ def test_reauth(salt_cli, salt_minion, salt_master, timeout, event_listener):
     )
     log.debug("Restarting the reauth minion")
 
-    # We need to have the minion attempting to start in a different thread
+    # We need to have the minion attempting to start in a different process
     # when we try to start the master
     def minion_func():
         start = time.time()
-        with salt_minion.started(start_timeout=timeout * 2):
+        with salt_minion.started(start_timeout=timeout * 2, max_start_attempts=1):
             new_start = time.time()
             while time.time() < new_start + (timeout * 2):
                 if event_listener.get_events(
@@ -43,10 +43,16 @@ def test_reauth(salt_cli, salt_minion, salt_master, timeout, event_listener):
                     break
                 time.sleep(5)
 
-    minion_thread = threading.Thread(target=minion_func)
-    minion_thread.start()
+    minion_proc = multiprocessing.Process(target=minion_func)
+    minion_proc.start()
     time.sleep(timeout)
     log.debug("Restarting the reauth master")
+    start = time.time()
     salt_master.start()
+    event_listener.wait_for_events(
+        [(salt_master.id, f"salt/minion/{salt_minion.id}/start")],
+        after_time=start,
+        timeout=timeout * 2,
+    )
     assert salt_cli.run("test.ping", minion_tgt=salt_minion.id).data is True
-    minion_thread.join()
+    minion_proc.join()
