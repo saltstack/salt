e4b1ca3e30e5443b23055f4d158f009c9d2992a6
Megan Wilhite <mwilhite@vmware.com>
2023-03-31 09:53:47 -0600

    Fix pyobjects test on windows

--- tests/pytests/functional/modules/state/test_pyobjects_renderer.py
+++ tests/pytests/functional/modules/state/test_pyobjects_renderer.py
@@ -9,22 +9,27 @@ def test_pyobjects_renderer(state, state_tree, tmp_path):
     """
     Test pyobjects renderer when running state.sls
     """
+    file_path = str(tmp_path).replace("\\", "/")
     sls1_contents = f"""
     #!pyobjects
     import pathlib
     import salt://test_pyobjects2.sls
-    test_file = pathlib.Path("{str(tmp_path)}", "test")
-    File.managed(str(test_file), user='root', group='root', mode='1777')
+    test_file = pathlib.Path("{file_path}", "test")
+    File.managed(str(test_file))
     """
     sls2_contents = f"""
     #!pyobjects
     import pathlib
-    test_file = pathlib.Path("{str(tmp_path)}", "test2")
-    File.managed(str(test_file), user='root', group='root', mode='1777')
+    test_file = pathlib.Path("{file_path}", "test2")
+    File.managed(str(test_file))
     """
 
-    with pytest.helpers.temp_file("test_pyobjects.sls", sls1_contents, state_tree):
-        with pytest.helpers.temp_file("test_pyobjects2.sls", sls2_contents, state_tree):
+    with pytest.helpers.temp_file(
+        "test_pyobjects.sls", sls1_contents, state_tree
+    ) as state1:
+        with pytest.helpers.temp_file(
+            "test_pyobjects2.sls", sls2_contents, state_tree
+        ) as state2:
             ret = state.sls("test_pyobjects")
             assert not ret.errors
             for state_return in ret:
