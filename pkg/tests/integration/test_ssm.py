ba29a27ad78228c15319bb7ab81dcc815e6c8b9d
Pedro Algarvio <palgarvio@vmware.com>
2023-05-03 07:52:39 +0100

    Fix test
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- pkg/tests/integration/test_ssm.py
+++ pkg/tests/integration/test_ssm.py
@@ -12,5 +12,4 @@ def test_ssm_present(install_salt):
     The ssm.exe binary needs to be present in both the zip and the exe/msi
     builds
     """
-    ssm_path = os.path.join(*install_salt.binary_paths["ssm"])
-    assert os.path.exists(ssm_path)
+    assert os.path.exists(install_salt.ssm_bin)
