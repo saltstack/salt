b6661011d2519b7c20c044e1c90cee83076f4420
Barney Sowood <barney@sowood.co.uk>
2023-05-27 12:12:05 +0100

    Add tests for package directory and file ownership
    Adds test that checks that files and directories created by the packages
    that should be owned by salt:salt are owned by that user and that the
    other files/directories created are owned by root:root.

--- pkg/tests/integration/test_salt_user.py
+++ pkg/tests/integration/test_salt_user.py
@@ -1,3 +1,4 @@
+import os
 import pathlib
 import subprocess
 import sys
@@ -11,6 +12,41 @@ pytestmark = [
     pytest.mark.skip_on_windows,
     pytest.mark.skip_on_darwin,
 ]
+
+
+@pytest.fixture
+def pkg_paths():
+    """
+    Paths created by package installs
+    """
+    paths = [
+        "/etc/salt",
+        "/var/cache/salt",
+        "/var/log/salt",
+        "/var/run/salt",
+        "/opt/saltstack/salt",
+    ]
+    return paths
+
+
+@pytest.fixture
+def pkg_paths_salt_user():
+    """
+    Paths created by package installs and owned by salt user
+    """
+    paths = [
+        "/etc/salt/cloud.deploy.d",
+        "/var/log/salt/cloud",
+        "/opt/saltstack/salt/lib/python3.10/site-packages/salt/cloud/deploy",
+        "/etc/salt/pki/master",
+        "/etc/salt/master.d",
+        "/etc/salt/minion.d",
+        "/var/log/salt/master",
+        "/var/log/salt/api",
+        "/var/cache/salt/master",
+        "/var/run/salt/master",
+    ]
+    return paths
 
 
 def test_salt_user_master(salt_master, install_salt):
@@ -77,12 +113,12 @@ def test_salt_user_shell(install_salt):
 
 def test_salt_cloud_dirs(install_salt):
     """
-    Test salt-cloud directories are owned by salt:salt
+    Test the correct user is running the Salt Master
     """
+    if platform.is_windows() or platform.is_darwin():
+        pytest.skip("Package does not have user set. Not testing user")
     paths = [
-        "/opt/saltstack/salt/lib/python{}.{}/site-packages/salt/cloud/deploy".format(
-            *sys.version_info
-        ),
+        "/opt/saltstack/salt/lib/python3.10/site-packages/salt/cloud/deploy",
         "/etc/salt/cloud.deploy.d",
     ]
     for name in paths:
@@ -90,3 +126,39 @@ def test_salt_cloud_dirs(install_salt):
         assert path.exists()
         assert path.owner() == "salt"
         assert path.group() == "salt"
+
+
+def test_pkg_paths(install_salt, pkg_paths, pkg_paths_salt_user):
+    """
+    Test package paths ownership
+    """
+    salt_user_subdirs = []
+    for _path in pkg_paths:
+        pkg_path = pathlib.Path(_path)
+        assert pkg_path.exists()
+        for dirpath, sub_dirs, files in os.walk(pkg_path):
+            path = pathlib.Path(dirpath)
+            # Directories owned by salt:salt or their subdirs/files
+            if str(path) in pkg_paths_salt_user or str(path) in salt_user_subdirs:
+                assert path.owner() == "salt"
+                assert path.group() == "salt"
+                salt_user_subdirs.extend(
+                    [str(path.joinpath(sub_dir)) for sub_dir in sub_dirs]
+                )
+                for file in files:
+                    file_path = path.joinpath(file)
+                    assert file_path.owner() == "salt"
+                    assert file_path.group() == "salt"
+            # Directories owned by root:root
+            else:
+                assert path.owner() == "root"
+                assert path.group() == "root"
+                for file in files:
+                    file_path = path.joinpath(file)
+                    # Individual files owned by salt:salt
+                    if str(file_path) in pkg_paths_salt_user:
+                        assert file_path.owner() == "salt"
+                        assert file_path.group() == "salt"
+                    else:
+                        assert file_path.owner() == "root"
+                        assert file_path.group() == "root"
