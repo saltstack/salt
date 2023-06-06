7b7bd7f6932b5d79aab1a2ee904cb7b6be6d3084
Tim Hildebrandt <118898773+TimHiCe@users.noreply.github.com>
2023-06-06 18:18:54 +0200

    Update chocolatey.py / Search for choco.exe
    chocolatey.exe was renamed to choco.exe by Chocolatey in Version 2.0.0
    
    (cherry picked from commit fd09ab7efb29d83223002dfa3526db36c5b23574)

--- salt/modules/chocolatey.py
+++ salt/modules/chocolatey.py
@@ -112,6 +112,9 @@ def _find_chocolatey():
             os.environ.get("ProgramData"), "Chocolatey", "bin", "chocolatey.exe"
         ),
         os.path.join(
+            os.environ.get("ProgramData"), "Chocolatey", "bin", "choco.exe"
+        ),
+        os.path.join(
             os.environ.get("SystemDrive"), "Chocolatey", "bin", "chocolatey.bat"
         ),
     ]
