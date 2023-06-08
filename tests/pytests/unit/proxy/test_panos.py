b0c4141066f616086dfb5e29487a5169edcbb102
Pedro Algarvio <palgarvio@vmware.com>
2023-06-08 09:36:11 +0100

    Fix `tests/pytests/unit/proxy/test_panos.py`
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- tests/pytests/unit/proxy/test_panos.py
+++ tests/pytests/unit/proxy/test_panos.py
@@ -15,31 +15,30 @@ def opts():
     return {"proxy": {"proxytype": "panos", "host": "hosturl.com", "apikey": "api_key"}}
 
 
-def test_init(opts):
-    for verify in [True, False, None]:
-        opts["proxy"]["verify_ssl"] = verify
-        if verify is None:
-            opts["proxy"].pop("verify_ssl")
-            verify = True
-        mock_http = MagicMock(
-            return_value={"status": 200, "text": "<data>some_test_data</data>"}
+@pytest.mark.parametrize("verify", [True, False, None])
+def test_init(opts, verify):
+    opts["proxy"]["verify_ssl"] = verify
+    if verify is None:
+        opts["proxy"].pop("verify_ssl")
+        verify = True
+    mock_http = MagicMock(
+        return_value={"status": 200, "text": "<data>some_test_data</data>"}
+    )
+    with patch("salt.utils.http.query", mock_http):
+        panos.init(opts)
+    assert mock_http.call_args_list == [
+        call(
+            "https://hosturl.com/api/",
+            data={
+                "type": "op",
+                "cmd": "<show><system><info></info></system></show>",
+                "key": "api_key",
+            },
+            decode=True,
+            decode_type="plain",
+            method="POST",
+            raise_error=True,
+            status=True,
+            verify_ssl=verify,
         )
-        patch_http = patch.dict(panos.__utils__, {"http.query": mock_http})
-        with patch_http:
-            panos.init(opts)
-        assert mock_http.call_args_list == [
-            call(
-                "https://hosturl.com/api/",
-                data={
-                    "type": "op",
-                    "cmd": "<show><system><info></info></system></show>",
-                    "key": "api_key",
-                },
-                decode=True,
-                decode_type="plain",
-                method="POST",
-                raise_error=True,
-                status=True,
-                verify_ssl=verify,
-            )
-        ]
+    ]
