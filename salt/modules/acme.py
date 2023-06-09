2b0bc7770d8b750fa6914c44f1688b0f3c9509f0
rittycat <36797079+rittycat@users.noreply.github.com>
2023-06-09 17:05:23 +1000

    Fix missing arg in acme state/module

--- salt/modules/acme.py
+++ salt/modules/acme.py
@@ -132,6 +132,7 @@ def cert(
     http_01_address=None,
     dns_plugin=None,
     dns_plugin_credentials=None,
+    dns_plugin_propagate_seconds=10,
 ):
     """
     Obtain/renew a certificate from an ACME CA, probably Let's Encrypt.
@@ -216,6 +217,7 @@ def cert(
         if dns_plugin == "cloudflare":
             cmd.append("--dns-cloudflare")
             cmd.append("--dns-cloudflare-credentials {}".format(dns_plugin_credentials))
+            cmd.append("--dns-cloudflare-propagation-seconds {}".format(dns_plugin_propagate_seconds))
         else:
             return {
                 "result": False,
