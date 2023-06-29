8744762580f127baca457f17673f19cd15a3b7f7
Gareth J. Greenaway <gareth@saltstack.com>
2023-06-29 10:21:15 -0700

    remove ignore_list

--- tests/pytests/functional/utils/test_extension_deprecation.py
+++ tests/pytests/functional/utils/test_extension_deprecation.py
@@ -4,14 +4,12 @@ from salt.utils.decorators.extension_deprecation import extension_deprecation_me
 
 
 @extension_deprecation_message(3009, "salt_mod", "http://www.example.com")
-def salt_func_one():
+def salt_func():
     return True
 
 
-@extension_deprecation_message(
-    3009, "salt_mod", "http://www.example.com", ignore_list=["salt_func_two"]
-)
-def salt_func_two():
+@extension_deprecation_message(3009, "salt_mod", "http://www.example.com")
+def get_configured_provider():
     return True
 
 
@@ -27,13 +25,13 @@ def test_extension_deprecation():
         "in favor of the saltext.salt_mod Salt Extension. (http://www.example.com)"
     )
     with warnings.catch_warnings(record=True) as catch_warnings:
-        ret = salt_func_one()
+        ret = salt_func()
         assert ret
         assert len(catch_warnings) == 1
         assert issubclass(catch_warnings[-1].category, DeprecationWarning)
         assert str(catch_warnings[-1].message) == expected_deprecation_message
 
     with warnings.catch_warnings(record=True) as catch_warnings:
-        ret = salt_func_two()
+        ret = get_configured_provider()
         assert ret
         assert len(catch_warnings) == 0
