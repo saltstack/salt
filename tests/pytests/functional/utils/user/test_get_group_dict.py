5f164f55d8e1cba2af6d3db7ff350cf80c6b520b
Victor Zhestkov <vzhestkov@suse.com>
2023-07-06 15:27:13 +0300

    Update tests/pytests/functional/utils/user/test_get_group_dict.py
    Co-authored-by: Pedro Algarvio <pedro@algarvio.me>

--- tests/pytests/functional/utils/user/test_get_group_dict.py
+++ tests/pytests/functional/utils/user/test_get_group_dict.py
@@ -9,15 +9,8 @@ from tests.support.mock import patch
 log = logging.getLogger(__name__)
 
 pytestmark = [
-    pytest.mark.skip_on_windows,
+    pytest.mark.skip_unless_on_linux(reason="Should only run in platforms which have duplicate GID support"),
 ]
-
-
-@pytest.mark.skip_on_platforms(
-    darwin=True,
-    freebsd=True,
-    reason="This test should not run on FreeBSD and Mac due to lack of duplicate GID support",
-)
 def test_get_group_dict_with_improper_duplicate_root_group():
     with patch("salt.utils.user.get_group_list", return_value=["+", "root"]):
         group_list = salt.utils.user.get_group_dict("root")
