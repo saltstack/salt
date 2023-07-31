8a0e2ec338ab08f41d658e6d8ddebba31c74ea5c
MKLeb <calebb@vmware.com>
2023-07-31 17:44:17 -0400

    Some more cleanup

--- pkg/tests/upgrade/test_salt_upgrade.py
+++ pkg/tests/upgrade/test_salt_upgrade.py
@@ -26,15 +26,15 @@ def test_salt_upgrade(salt_call_cli, install_salt):
     assert "Authentication information could" in use_lib.stderr
 
     # Upgrade Salt from previous version and test
-    new_py_version = install_salt.package_python_version()
     install_salt.install(upgrade=True)
+    new_py_version = install_salt.package_python_version()
     ret = salt_call_cli.run("test.ping")
     assert ret.returncode == 0
     assert ret.data
 
     # Install dep following upgrade
     # TODO: This should be removed when we stop testing against versions < 3006.0
-    if not install_salt.relenv and original_py_version == new_py_version:
+    if not install_salt.relenv or original_py_version != new_py_version:
         install = salt_call_cli.run("--local", "pip.install", dep)
         assert install.returncode == 0
 
