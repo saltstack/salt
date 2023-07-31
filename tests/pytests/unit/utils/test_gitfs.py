e64be0549f3fb59b27adeb8ed9c12666d8ff36bb
cmcmarrow <charles.mcmarrow.4@gmail.com>
2023-07-31 12:19:14 -0500

    update tests

--- tests/pytests/unit/utils/test_gitfs.py
+++ tests/pytests/unit/utils/test_gitfs.py
@@ -4,6 +4,7 @@ import time
 
 import pytest
 
+import salt.config
 import salt.fileserver.gitfs
 import salt.utils.gitfs
 from salt.exceptions import FileserverConfigError
@@ -22,6 +23,23 @@ except AttributeError:
 
 if HAS_PYGIT2:
     import pygit2
+
+
+@pytest.fixture
+def minion_opts(tmp_path):
+    """
+    Default minion configuration with relative temporary paths to not require root permissions.
+    """
+    root_dir = tmp_path / "minion"
+    opts = salt.config.DEFAULT_MINION_OPTS.copy()
+    opts["__role"] = "minion"
+    opts["root_dir"] = str(root_dir)
+    for name in ("cachedir", "pki_dir", "sock_dir", "conf_dir"):
+        dirpath = root_dir / name
+        dirpath.mkdir(parents=True)
+        opts[name] = str(dirpath)
+    opts["log_file"] = "logs/minion.log"
+    return opts
 
 
 @pytest.mark.parametrize(
