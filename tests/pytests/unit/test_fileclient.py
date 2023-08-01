bf47db043a0ce2f5a9bc16f928f3875265f29eed
Daniel A. Wozniak <dwozniak@vmware.com>
2023-07-31 17:38:45 -0700

    Reasonable file client failures

--- tests/pytests/unit/test_fileclient.py
+++ tests/pytests/unit/test_fileclient.py
@@ -1,5 +1,7 @@
+import pytest
+
 import salt.fileclient
-from tests.support.mock import patch
+from tests.support.mock import Mock, patch
 
 
 class MockReqChannel:
@@ -30,6 +32,8 @@ def test_fileclient_context_manager_closes(temp_salt_minion, temp_salt_master):
             "master_uri": "tcp://127.0.0.1:{}".format(
                 temp_salt_master.config["ret_port"]
             ),
+            "request_channel_timeout": 1,
+            "request_channel_tries": 1,
         }
     )
     master_uri = "tcp://{master_ip}:{master_port}".format(
@@ -46,3 +50,45 @@ def test_fileclient_context_manager_closes(temp_salt_minion, temp_salt_master):
 
         assert client._closing
         assert client.channel.close.called
+
+
+@pytest.mark.slow_test
+def test_fileclient_timeout(temp_salt_minion, temp_salt_master):
+    """
+    ensure fileclient channel closes
+    when used with a context manager
+    """
+    opts = temp_salt_minion.config.copy()
+    opts.update(
+        {
+            "id": "root",
+            "transport": "zeromq",
+            "auth_tries": 1,
+            "auth_timeout": 5,
+            "master_ip": "127.0.0.1",
+            "master_port": temp_salt_master.config["ret_port"],
+            "master_uri": "tcp://127.0.0.1:{}".format(
+                temp_salt_master.config["ret_port"]
+            ),
+            "request_channel_timeout": 1,
+            "request_channel_tries": 1,
+        }
+    )
+    master_uri = "tcp://{master_ip}:{master_port}".format(
+        master_ip="localhost", master_port=opts["master_port"]
+    )
+
+    async def mock_auth():
+        return True
+
+    def mock_dumps(*args):
+        return b"meh"
+
+    with salt.fileclient.get_file_client(opts) as client:
+        # Authenticate must return true
+        client.auth.authenticate = mock_auth
+        # Crypticla must return bytes to pass to transport.RequestClient.send
+        client.auth._crypticle = Mock()
+        client.auth._crypticle.dumps = mock_dumps
+        with pytest.raises(salt.exceptions.SaltClientError):
+            client.file_list()
