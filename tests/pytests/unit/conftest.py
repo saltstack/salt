6b927bb0fa63864eb6113d134bb9474247b089ae
Megan Wilhite <mwilhite@vmware.com>
2023-08-02 11:45:02 -0600

    Remove onedir pytest fixture workaround

--- tests/pytests/unit/conftest.py
+++ tests/pytests/unit/conftest.py
@@ -1,25 +1,6 @@
-import os
-
 import pytest
 
 import salt.config
-
-
-@pytest.fixture(scope="package", autouse=True)
-def _onedir_env():
-    """
-    Unit tests cannot currently test the
-    onedir artifact. This will need to be removed
-    when we do add onedir support for functional tests.
-    """
-    if os.environ.get("ONEDIR_TESTRUN", "0") == "1":
-        try:
-            os.environ["ONEDIR_TESTRUN"] = "0"
-            yield
-        finally:
-            os.environ["ONEDIR_TESTRUN"] = "1"
-    else:
-        yield
 
 
 @pytest.fixture
