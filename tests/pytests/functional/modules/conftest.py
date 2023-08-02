3a42b0ce589c28540d10c68404c362722a9571a3
Megan Wilhite <mwilhite@vmware.com>
2023-08-02 11:45:02 -0600

    Remove onedir pytest fixture workaround

--- tests/pytests/functional/modules/conftest.py
+++ tests/pytests/functional/modules/conftest.py
@@ -1,25 +1,4 @@
-import os
-
 import pytest
-
-
-@pytest.fixture(scope="package", autouse=True)
-def _onedir_env():
-    """
-    Functional tests cannot currently test the
-    onedir artifact. This will need to be removed
-    when we do add onedir support for functional tests.
-    This is specifically needed for testing the new
-    package grain when calling from the grains module.
-    """
-    if os.environ.get("ONEDIR_TESTRUN", "0") == "1":
-        try:
-            os.environ["ONEDIR_TESTRUN"] = "0"
-            yield
-        finally:
-            os.environ["ONEDIR_TESTRUN"] = "1"
-    else:
-        yield
 
 
 @pytest.fixture(scope="module")
