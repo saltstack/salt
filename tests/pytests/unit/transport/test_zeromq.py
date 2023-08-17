b5889ce4371d2c0a285139a95e07fe47a0e2bee1
Pedro Algarvio <palgarvio@vmware.com>
2023-08-17 09:54:54 +0100

    Fix merged forwarded code and test
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- tests/pytests/unit/transport/test_zeromq.py
+++ tests/pytests/unit/transport/test_zeromq.py
@@ -3,7 +3,6 @@ import pytest
 
 import salt.config
 import salt.transport.zeromq
-from tests.support.mock import MagicMock
 
 
 async def test_req_server_garbage_request(io_loop):
@@ -22,14 +21,9 @@ async def test_req_server_garbage_request(io_loop):
     byts = msgpack.dumps({"foo": "bar"})
     badbyts = byts[:3] + b"^M" + byts[3:]
 
-    valid_response = msgpack.dumps("Invalid payload")
-
-    stream = MagicMock()
-    request_server.stream = stream
-
     try:
-        await request_server.handle_message(stream, badbyts)
+        ret = await request_server.handle_message(None, badbyts)
     except Exception as exc:  # pylint: disable=broad-except
         pytest.fail(f"Exception was raised {exc}")
 
-    request_server.stream.send.assert_called_once_with(valid_response)
+    assert ret == {"msg": "bad load"}
