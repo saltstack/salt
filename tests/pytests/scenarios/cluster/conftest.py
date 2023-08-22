71908589c093976bd6223088e0810a0863de531b
Daniel A. Wozniak <dwozniak@vmware.com>
2023-08-21 22:28:51 -0700

    More tests

--- tests/pytests/scenarios/cluster/conftest.py
+++ tests/pytests/scenarios/cluster/conftest.py
@@ -56,11 +56,6 @@ def cluster_master_1(request, salt_factories, cluster_pki_path, cluster_cache_pa
         yield factory
 
 
-# @pytest.fixture(scope="package")
-# def cluster_master_1_salt_cli(cluster_master_1):
-#    return cluster__master_1.salt_cli(timeout=120)
-#
-#
 @pytest.fixture
 def cluster_master_2(salt_factories, cluster_master_1):
     if salt.utils.platform.is_darwin() or salt.utils.platform.is_freebsd():
@@ -133,12 +128,6 @@ def cluster_master_3(salt_factories, cluster_master_1):
         yield factory
 
 
-#
-# @pytest.fixture(scope="package")
-# def cluster_master_2_salt_cli(cluster_master_2):
-#    return cluster_master_2.salt_cli(timeout=120)
-#
-#
 @pytest.fixture
 def cluster_minion_1(cluster_master_1):
     config_defaults = {
@@ -159,74 +148,3 @@ def cluster_minion_1(cluster_master_1):
     )
     with factory.started(start_timeout=120):
         yield factory
-
-
-#
-#
-# @pytest.fixture(scope="package")
-# def cluster_minion_2(cluster_master_2):
-#    config_defaults = {
-#        "transport": cluster_master_2.config["transport"],
-#    }
-#
-#    port = cluster_master_2.config["ret_port"]
-#    addr = cluster_master_2.config["interface"]
-#    config_overrides = {
-#        "master": f"{port}:{addr}",
-#        "test.foo": "baz",
-#    }
-#    factory = salt_mm_master_1.salt_minion_daemon(
-#        "cluster-minion-2",
-#        defaults=config_defaults,
-#        overrides=config_overrides,
-#        extra_cli_arguments_after_first_start_failure=["--log-level=info"],
-#    )
-#    with factory.started(start_timeout=120):
-#        yield factory
-#
-#
-#
-# @pytest.fixture(scope="package")
-# def run_salt_cmds():
-#    def _run_salt_cmds_fn(clis, minions):
-#        """
-#        Run test.ping from all clis to all minions
-#        """
-#        returned_minions = []
-#        minion_instances = {minion.id: minion for minion in minions}
-#        clis_to_check = {minion.id: list(clis) for minion in minions}
-#
-#        attempts = 6
-#        timeout = 5
-#        if salt.utils.platform.spawning_platform():
-#            timeout *= 2
-#        while attempts:
-#            if not clis_to_check:
-#                break
-#            for minion in list(clis_to_check):
-#                if not clis_to_check[minion]:
-#                    clis_to_check.pop(minion)
-#                    continue
-#                for cli in list(clis_to_check[minion]):
-#                    try:
-#                        ret = cli.run(
-#                            "--timeout={}".format(timeout),
-#                            "test.ping",
-#                            minion_tgt=minion,
-#                            _timeout=2 * timeout,
-#                        )
-#                        if ret.returncode == 0 and ret.data is True:
-#                            returned_minions.append((cli, minion_instances[minion]))
-#                            clis_to_check[minion].remove(cli)
-#                    except FactoryTimeout:
-#                        log.debug(
-#                            "Failed to execute test.ping from %s to %s.",
-#                            cli.get_display_name(),
-#                            minion,
-#                        )
-#            time.sleep(1)
-#            attempts -= 1
-#
-#        return returned_minions
-#
-#    return _run_salt_cmds_fn
