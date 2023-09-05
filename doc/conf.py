35b11aebe9548993ad498bd2650e625f243d5dfa
ScriptAutomate <derek@icanteven.io>
2023-09-05 18:12:05 -0500

    Update Slack Community invite link

--- doc/conf.py
+++ doc/conf.py
@@ -174,7 +174,7 @@ rst_prolog = """\
 .. _`salt-users`: https://groups.google.com/forum/#!forum/salt-users
 .. _`salt-announce`: https://groups.google.com/forum/#!forum/salt-announce
 .. _`salt-packagers`: https://groups.google.com/forum/#!forum/salt-packagers
-.. _`salt-slack`: https://join.slack.com/t/saltstackcommunity/shared_invite/zt-1zlfxffs1-NuEH~G9TzOeuNGdsfZIl3w
+.. _`salt-slack`: https://via.vmw.com/salt-slack
 .. |windownload| raw:: html
 
      <p>Python3 x86: <a
