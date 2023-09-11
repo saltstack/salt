a7aabc4519c8c60b3a66c39faa9e8d4bd06d49ad
Joseph Hall <perlhoser@gmail.com>
2023-09-11 07:40:53 -0600

    Skip install for distros that are not correctly supported by formula

--- tests/pytests/functional/formulas/test_salt.py
+++ tests/pytests/functional/formulas/test_salt.py
@@ -18,6 +18,20 @@ def modules(loaders, _formula):
 
 @pytest.mark.skip_on_windows
 @pytest.mark.destructive_test
+# These installation issues need to be resolved in the Salt formula, and then
+# we can update the tag in here and remove the skips
+@pytest.mark.skipif(
+    'grains["osfullname"] in ("AlmaLinux", "Amazon Linux")',
+    reason="No salt packages available for this distrubition",
+)
+@pytest.mark.skipif(
+    'grains["os"] == "CentOS"',
+    reason="No salt packages available for this distrubition",
+)
+@pytest.mark.skipif(
+    'grains["os_family"] == "Arch"',
+    reason="Outdated archlinux-keyring package will cause failed package installs",
+)
 def test_salt_formula(modules):
     # Master Formula
     ret = modules.state.sls("salt.master")
