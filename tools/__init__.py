fce51983b3c1ce1d0bb214a9d18f25bacd5b1afd
Pedro Algarvio <palgarvio@vmware.com>
2023-09-14 17:51:40 +0100

    Migrated some `invoke` tasks to `python-tools-scripts`
    * `tasks/docs.py` -> `tools/precommit/docs.py`
    * `tasks/docstrings.py` -> `tools/precommit/docstrings.py`
    
    Refs #64374
    
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- tools/__init__.py
+++ tools/__init__.py
@@ -42,9 +42,11 @@ ptscripts.register_tools_module("tools.pkg.repo.publish")
 ptscripts.register_tools_module("tools.precommit")
 ptscripts.register_tools_module("tools.precommit.changelog")
 ptscripts.register_tools_module("tools.precommit.workflows")
+ptscripts.register_tools_module("tools.precommit.docs")
+ptscripts.register_tools_module("tools.precommit.docstrings")
+ptscripts.register_tools_module("tools.release", venv_config=RELEASE_VENV_CONFIG)
 ptscripts.register_tools_module("tools.testsuite")
 ptscripts.register_tools_module("tools.testsuite.download")
-ptscripts.register_tools_module("tools.release", venv_config=RELEASE_VENV_CONFIG)
 ptscripts.register_tools_module("tools.vm")
 
 for name in ("boto3", "botocore", "urllib3"):
