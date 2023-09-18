94f233cefa3eda27d8f4e126aebc0f8771e00ea7
Joseph Hall <perlhoser@gmail.com>
2023-09-18 12:46:09 -0600

    Formula tests for vim and sudoers

---
+++ tests/pytests/functional/formulas/test_sudoers.py
@@ -0,0 +1,26 @@
+"""
+Tests using sudoers formula
+"""
+import pytest
+
+
+@pytest.fixture(scope="module")
+def _formula(saltstack_formula):
+    with saltstack_formula(name="sudoers-formula", tag="0.25.0") as formula:
+        yield formula
+
+
+@pytest.fixture(scope="module")
+def modules(loaders, _formula):
+    loaders.opts["file_roots"]["base"].append(
+        str(_formula.state_tree_path / f"{_formula.name}-{_formula.tag}")
+    )
+    return loaders.modules
+
+
+@pytest.mark.skip_on_windows
+@pytest.mark.destructive_test
+def test_sudoers_formula(modules):
+    ret = modules.state.sls("sudoers")
+    for staterun in ret:
+        assert staterun.result
