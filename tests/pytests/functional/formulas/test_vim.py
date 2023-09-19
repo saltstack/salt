50b3bc199c832ae616dfce492d885997bccd8beb
Joseph Hall <perlhoser@gmail.com>
2023-09-18 19:14:04 -0600

    Check for errors before result loop

--- tests/pytests/functional/formulas/test_vim.py
+++ tests/pytests/functional/formulas/test_vim.py
@@ -22,5 +22,7 @@ def modules(loaders, _formula):
 @pytest.mark.destructive_test
 def test_vim_formula(modules):
     ret = modules.state.sls("vim")
+    assert not ret.errors
+    assert ret.failed is False
     for staterun in ret:
         assert staterun.result is True
