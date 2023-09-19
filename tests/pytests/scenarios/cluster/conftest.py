77d79ddfe8b56cf72b65adaa19a6efc2767df2c6
Daniel A. Wozniak <dwozniak@vmware.com>
2023-09-18 17:50:06 -0700

    fix pre commit in tests

--- tests/pytests/scenarios/cluster/conftest.py
+++ tests/pytests/scenarios/cluster/conftest.py
@@ -4,17 +4,14 @@ import subprocess
 import pytest
 
 import salt.utils.platform
-
-
 from tests.pytests.integration.cluster.conftest import (
-    cluster_shared_path,
-    cluster_pki_path,
     cluster_cache_path,
     cluster_master_1,
     cluster_master_2,
     cluster_master_3,
     cluster_minion_1,
+    cluster_pki_path,
+    cluster_shared_path,
 )
 
 log = logging.getLogger(__name__)
-
