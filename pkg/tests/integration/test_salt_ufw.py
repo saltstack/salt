cdcf78f243d3a4f747d8de253c01aa3eb1fe50e4
David Murphy <dmurphy@saltstack.com>
2023-09-22 17:52:50 -0600

    Updated package test

--- pkg/tests/integration/test_salt_ufw.py
+++ pkg/tests/integration/test_salt_ufw.py
@@ -10,7 +10,7 @@ log = logging.getLogger(__name__)
 
 
 @pytest.mark.skip_on_windows()
-def test_salt_ufw(salt_master, install_salt):
+def test_salt_ufw(salt_master, salt_call_cli, install_salt):
     """
     Test salt.ufw for Debian/Ubuntu salt-master
     """
@@ -19,12 +19,38 @@ def test_salt_ufw(salt_master, install_salt):
     if install_salt.distro_id not in ("debian", "ubuntu"):
         pytest.skip("Only tests Debian / Ubuntu packages")
 
-    pkg = [x for x in install_salt.pkgs if "deb" in x]
+    pkg = [x for x in install_salt.pkgs if "deb" in x and "master" in x]
     if not pkg:
         pytest.skip("Not testing deb packages")
-    pkg = pkg[0].split("/")[-1]
-    if "rc" not in pkg:
-        pytest.skip("Not testing an RC package")
+    pkg_master = pkg[0]
 
-    ufw_master_path = Path("/etc/ufw/applications.d/salt-master")
-    assert ufw_master_path.is_file()
+    pkg = [x for x in install_salt.pkgs if "deb" in x and "common" in x]
+    if not pkg:
+        pytest.skip("Not testing deb packages")
+    pkg_common = pkg[0]
+    log.warning(f"DGM test_salt_ufw pkg_common '{pkg_common}'")
+
+    pkg_mngr = install_salt.pkg_mngr
+    log.warning(
+        f"DGM test_salt_ufw pkg_mngr '{pkg_mngr}', pkg_common '{pkg_common}', pkg_master '{pkg_master}'"
+    )
+
+    install_common_cmd = f"{pkg_mngr} -y install {pkg_common}"
+    install_master_cmd = f"{pkg_mngr} -y install {pkg_master}"
+    ## ret = salt_call_cli.run("--local", "cmd.run", pkg_mngr, "-y", "install", pkg_to_install)
+    ret = salt_call_cli.run("--local", "cmd.run", install_common_cmd)
+    log.warning(f"DGM test_salt_ufw salt_common post install '{ret}'")
+    assert ret.returncode == 0
+
+    ret = salt_call_cli.run("--local", "cmd.run", install_master_cmd)
+    log.warning(f"DGM test_salt_ufw salt_master post install '{ret}'")
+    assert ret.returncode == 0
+
+    ufw_master_path = pathlib.Path("/etc/ufw/applications.d/salt-master")
+    assert ufw_master_path.exists()
+
+    # cleanup
+    remove_cmd = f"{pkg_mngr} -y remove salt-common"
+    ret = salt_call_cli.run("--local", "cmd.run", remove_cmd)
+    log.warning(f"DGM test_salt_ufw salt_master post remove '{ret}'")
+    assert ret.returncode == 0
