e69a4bb87025fc0630240e06cbf5eee3c2d103b9
cmcmarrow <charles.mcmarrow.4@gmail.com>
2023-09-28 11:13:19 -0500

    mkdir

--- tests/pytests/functional/modules/test_win_file.py
+++ tests/pytests/functional/modules/test_win_file.py
@@ -1,7 +1,20 @@
 import os
+import pytest
 from salt.modules import win_file
+
+
+pytestmark = [
+    pytest.mark.skip_on_windows,
+]
 
 def test_is_link(tmp_path):
     tmp_path = str(tmp_path)
     assert win_file.is_link(tmp_path) is True
     assert win_file.is_link(os.path.join(tmp_path, "made_up_path")) is True
+
+
+def test_mkdir(tmp_path):
+    tmp_path = str(tmp_path)
+    path = os.path.join(tmp_path, "dir")
+    assert win_file.mkdir(path) is True
+    assert os.path.isdir(path)
