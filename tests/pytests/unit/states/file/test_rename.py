76f4d555071be10fc7f937601a6d2a93e4567a95
Gareth J. Greenaway <gareth@saltstack.com>
2023-09-27 17:21:37 -0700

    Adding additional tests to increase coverage for salt/states/file.py and salt/grains/core.py

--- tests/pytests/unit/states/file/test_rename.py
+++ tests/pytests/unit/states/file/test_rename.py
@@ -136,3 +136,53 @@ def test_rename(tmp_path):
                                 }
                             )
                             assert filestate.rename(name, source) == ret
+
+    mock_lex = MagicMock(side_effect=[True, False, False])
+    with patch.object(os.path, "isabs", mock_t):
+        with patch.object(os.path, "lexists", mock_lex):
+            with patch.object(os.path, "isdir", mock_f):
+                with patch.dict(
+                    filestate.__salt__,
+                    {
+                        "file.makedirs": MagicMock(
+                            side_effect=filestate.CommandExecutionError()
+                        )
+                    },
+                ):
+                    with patch.object(os.path, "islink", mock_f):
+                        with patch.dict(filestate.__opts__, {"test": False}):
+                            with patch.object(shutil, "move", MagicMock()):
+                                ret.update(
+                                    {
+                                        "name": name,
+                                        "comment": "Drive  is not mapped",
+                                        "result": False,
+                                        "changes": {},
+                                    }
+                                )
+                                assert (
+                                    filestate.rename(name, source, makedirs=True) == ret
+                                )
+
+    mock_lex = MagicMock(side_effect=[True, False, False])
+    mock_link = str(tmp_path / "salt" / "link")
+    with patch.object(os.path, "isabs", mock_t):
+        with patch.object(os.path, "lexists", mock_lex):
+            with patch.object(os.path, "isdir", mock_t):
+                with patch.object(os.path, "islink", mock_t):
+                    with patch(
+                        "salt.utils.path.readlink", MagicMock(return_value=mock_link)
+                    ):
+                        with patch.dict(filestate.__opts__, {"test": False}):
+                            with patch.object(os, "symlink", MagicMock()):
+                                with patch.object(os, "unlink", MagicMock()):
+                                    comt = 'Moved "{}" to "{}"'.format(source, name)
+                                    ret.update(
+                                        {
+                                            "name": name,
+                                            "comment": comt,
+                                            "result": True,
+                                            "changes": {name: source},
+                                        }
+                                    )
+                                    assert filestate.rename(name, source) == ret
