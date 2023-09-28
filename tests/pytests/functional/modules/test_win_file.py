bec0e3b20cdd420a603b2489fbb7f28723b166bc
cmcmarrow <charles.mcmarrow.4@gmail.com>
2023-09-28 11:16:06 -0500

    tf

--- tests/pytests/functional/modules/test_win_file.py
+++ tests/pytests/functional/modules/test_win_file.py
@@ -10,7 +10,7 @@ pytestmark = [
 def test_is_link(tmp_path):
     tmp_path = str(tmp_path)
     assert win_file.is_link(tmp_path) is True
-    assert win_file.is_link(os.path.join(tmp_path, "made_up_path")) is True
+    assert win_file.is_link(os.path.join(tmp_path, "made_up_path")) is False
 
 
 def test_mkdir(tmp_path):
