81d6e422db8858987e223a0ab9254d9e9b4abbef
David Murphy <dmurphy@saltstack.com>
2023-10-05 15:56:31 -0600

    Removed missed debug statements

--- pkg/tests/integration/test_salt_ufw.py
+++ pkg/tests/integration/test_salt_ufw.py
@@ -14,8 +14,6 @@ def test_salt_ufw(salt_master, salt_call_cli, install_salt):
     """
     Test salt.ufw for Debian/Ubuntu salt-master
     """
-    log.warning(f"DGM test_salt_ufw install_salt '{install_salt}'")
-
     if install_salt.distro_id not in ("debian", "ubuntu"):
         pytest.skip("Only tests Debian / Ubuntu packages")
 
@@ -24,10 +22,3 @@ def test_salt_ufw(salt_master, salt_call_cli, install_salt):
 
     ufw_master_path = pathlib.Path("/etc/ufw/applications.d/salt-master")
     assert ufw_master_path.exists()
-
-    etc_ufw_path = pathlib.Path("/etc/ufw/applications.d")
-    str_etc_ufw_path = str(etc_ufw_path)
-    log.warning(f"DGM test_salt_ufw etc ufw contents '{etc_ufw_path}'")
-    ret = salt_call_cli.run("--local", "cmd.run", f"ls -alh {etc_ufw_path}/")
-    log.warning(f"DGM test_salt_ufw etc ufw contents, ls -alh file, returned '{ret}'")
-    assert ret.returncode == 0
