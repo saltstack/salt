7eae72b1d328c9e6778d891996bfc8a5a1553f0f
Pedro Algarvio <palgarvio@vmware.com>
2023-10-10 09:44:04 +0100

    Bring migrated tests up to standards
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- tests/pytests/unit/states/test_saltmod.py
+++ tests/pytests/unit/states/test_saltmod.py
@@ -10,6 +10,32 @@ import salt.utils.state
 from tests.support.mock import MagicMock, create_autospec, patch
 
 
+class MockedEvent:
+    """
+    Mocked event class
+    """
+
+    def __init__(self, data):
+        self.full = None
+        self.flag = None
+        self._data = data
+
+    def get_event(self, full):
+        """
+        Mock get_event method
+        """
+        self.full = full
+        if self.flag:
+            return self._data
+        return None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        pass
+
+
 @pytest.fixture
 def configure_loader_modules():
     return {
@@ -113,16 +139,67 @@ def test_state():
     name = "state"
     tgt = "minion1"
 
-    comt = "Passed invalid value for 'allow_fail', must be an int"
-
-    ret = {"name": name, "changes": {}, "result": False, "comment": comt}
-
-    test_ret = {
+    expected = {
         "name": name,
         "changes": {},
-        "result": True,
-        "comment": "States ran successfully.",
+        "result": False,
+        "comment": "Passed invalid value for 'allow_fail', must be an int",
     }
+
+    ret = saltmod.state(name, tgt, allow_fail="a")
+    assert ret == expected
+
+    expected.update({"comment": "No highstate or sls specified, no execution made"})
+    ret = saltmod.state(name, tgt)
+    assert ret == expected
+
+    expected.update({"comment": "Must pass in boolean for value of 'concurrent'"})
+    ret = saltmod.state(name, tgt, highstate=True, concurrent="a")
+    assert ret == expected
+
+    expected.update(
+        {
+            "result": True,
+            "comment": "States ran successfully.",
+        }
+    )
+    with patch.dict(saltmod.__opts__, {"test": True}):
+        ret = saltmod.state(name, tgt, highstate=True)
+    assert ret == expected
+
+    expected.update(
+        {
+            "comment": "States ran successfully. No changes made to silver.",
+            "result": True,
+            "__jid__": "20170406104341210934",
+        }
+    )
+    with patch.dict(saltmod.__opts__, {"test": False}):
+        mock = MagicMock(
+            return_value={
+                "silver": {
+                    "jid": "20170406104341210934",
+                    "retcode": 0,
+                    "ret": {
+                        "test_|-notify_me_|-this is a name_|-show_notification": {
+                            "comment": "Notify me",
+                            "name": "this is a name",
+                            "start_time": "10:43:41.487565",
+                            "result": True,
+                            "duration": 0.35,
+                            "__run_num__": 0,
+                            "__sls__": "demo",
+                            "changes": {},
+                            "__id__": "notify_me",
+                        }
+                    },
+                    "out": "highstate",
+                }
+            }
+        )
+        with patch.dict(saltmod.__salt__, {"saltutil.cmd": mock}):
+            ret = saltmod.state(name, tgt, highstate=True)
+        assert ret == expected
 
     test_batch_return = {
         "minion1": {
@@ -177,55 +254,7 @@ def test_state():
             "out": "highstate",
         },
     }
-
-    assert saltmod.state(name, tgt, allow_fail="a") == ret
-
-    comt = "No highstate or sls specified, no execution made"
-    ret.update({"comment": comt})
-    assert saltmod.state(name, tgt) == ret
-
-    comt = "Must pass in boolean for value of 'concurrent'"
-    ret.update({"comment": comt})
-    assert saltmod.state(name, tgt, highstate=True, concurrent="a") == ret
-
-    ret.update({"comment": comt, "result": None})
-    with patch.dict(saltmod.__opts__, {"test": True}):
-        assert saltmod.state(name, tgt, highstate=True) == test_ret
-
-    ret.update(
-        {
-            "comment": "States ran successfully. No changes made to silver.",
-            "result": True,
-            "__jid__": "20170406104341210934",
-        }
-    )
-    with patch.dict(saltmod.__opts__, {"test": False}):
-        mock = MagicMock(
-            return_value={
-                "silver": {
-                    "jid": "20170406104341210934",
-                    "retcode": 0,
-                    "ret": {
-                        "test_|-notify_me_|-this is a name_|-show_notification": {
-                            "comment": "Notify me",
-                            "name": "this is a name",
-                            "start_time": "10:43:41.487565",
-                            "result": True,
-                            "duration": 0.35,
-                            "__run_num__": 0,
-                            "__sls__": "demo",
-                            "changes": {},
-                            "__id__": "notify_me",
-                        }
-                    },
-                    "out": "highstate",
-                }
-            }
-        )
-        with patch.dict(saltmod.__salt__, {"saltutil.cmd": mock}):
-            assert saltmod.state(name, tgt, highstate=True) == ret
-
-    ret.update(
+    expected.update(
         {
             "comment": (
                 "States ran successfully. No changes made to minion1, minion3,"
@@ -233,7 +262,7 @@ def test_state():
             )
         }
     )
-    del ret["__jid__"]
+    del expected["__jid__"]
     with patch.dict(saltmod.__opts__, {"test": False}):
         with patch.dict(
             saltmod.__salt__,
@@ -243,8 +272,8 @@ def test_state():
 
             # Test return without checking the comment contents. Comments are tested later.
             comment = state_run.pop("comment")
-            ret.pop("comment")
-            assert state_run == ret
+            expected.pop("comment")
+            assert state_run == expected
 
             # Check the comment contents in a non-order specific way (ordering fails sometimes on PY3)
             assert "States ran successfully. No changes made to" in comment
@@ -261,7 +290,7 @@ def test_function():
     name = "state"
     tgt = "larry"
 
-    ret = {
+    expected = {
         "name": name,
         "changes": {},
         "result": None,
@@ -269,9 +298,10 @@ def test_function():
     }
 
     with patch.dict(saltmod.__opts__, {"test": True}):
-        assert saltmod.function(name, tgt) == ret
+        ret = saltmod.function(name, tgt)
+    assert ret == expected
 
-    ret.update(
+    expected.update(
         {
             "result": True,
             "changes": {"ret": {tgt: ""}},
@@ -284,7 +314,8 @@ def test_function():
         mock_ret = {"larry": {"ret": "", "retcode": 0, "failed": False}}
         mock_cmd = MagicMock(return_value=mock_ret)
         with patch.dict(saltmod.__salt__, {"saltutil.cmd": mock_cmd}):
-            assert saltmod.function(name, tgt) == ret
+            ret = saltmod.function(name, tgt)
+        assert ret == expected
 
 
 @pytest.mark.slow_test
@@ -295,10 +326,8 @@ def test_function_when_no_minions_match():
     """
     name = "state"
     tgt = "larry"
-    mock_ret = {}
-    mock_cmd = MagicMock(return_value=mock_ret)
 
-    ret = {
+    expected = {
         "name": name,
         "changes": {},
         "result": False,
@@ -306,8 +335,9 @@ def test_function_when_no_minions_match():
     }
 
     with patch.dict(saltmod.__opts__, {"test": False}):
-        with patch.dict(saltmod.__salt__, {"saltutil.cmd": mock_cmd}):
-            assert saltmod.function(name, tgt) == ret
+        with patch.dict(saltmod.__salt__, {"saltutil.cmd": MagicMock(return_value={})}):
+            ret = saltmod.function(name, tgt)
+            assert ret == expected
 
 
 def test_wait_for_event():
@@ -317,43 +347,22 @@ def test_wait_for_event():
     name = "state"
     tgt = "minion1"
 
-    comt = "Timeout value reached."
+    ret = {
+        "name": name,
+        "changes": {},
+        "result": False,
+        "comment": "Timeout value reached.",
+    }
 
-    ret = {"name": name, "changes": {}, "result": False, "comment": comt}
-
-    class Mockevent:
-        """
-        Mock event class
-        """
-
-        flag = None
-
-        def __init__(self):
-            self.full = None
-
-        def get_event(self, full):
-            """
-            Mock get_event method
-            """
-            self.full = full
-            if self.flag:
-                return {"tag": name, "data": {}}
-            return None
-
-        def __enter__(self):
-            return self
-
-        def __exit__(self, *args):
-            pass
-
+    mocked_event = MockedEvent({"tag": name, "data": {}})
     with patch.object(
-        salt.utils.event, "get_event", MagicMock(return_value=Mockevent())
+        salt.utils.event, "get_event", MagicMock(return_value=mocked_event)
     ):
         with patch.dict(saltmod.__opts__, {"sock_dir": True, "transport": True}):
             with patch("salt.states.saltmod.time.time", MagicMock(return_value=1.0)):
                 assert saltmod.wait_for_event(name, "salt", timeout=-1.0) == ret
 
-                Mockevent.flag = True
+                mocked_event.flag = True
                 ret.update(
                     {"comment": "All events seen in 0.0 seconds.", "result": True}
                 )
@@ -371,37 +380,16 @@ def test_wait_for_event_list_single_event():
     event_id = "lost"
     tgt = ["minion_1", "minion_2", "minion_3"]
 
-    comt = "Timeout value reached."
+    expected = {
+        "name": name,
+        "changes": {},
+        "result": False,
+        "comment": "Timeout value reached.",
+    }
 
-    expected = {"name": name, "changes": {}, "result": False, "comment": comt}
-
-    class Mockevent:
-        """
-        Mock event class
-        """
-
-        flag = None
-
-        def __init__(self):
-            self.full = None
-
-        def get_event(self, full):
-            """
-            Mock get_event method
-            """
-            self.full = full
-            if self.flag:
-                return {"tag": name, "data": {"lost": tgt}}
-            return None
-
-        def __enter__(self):
-            return self
-
-        def __exit__(self, *args):
-            pass
-
+    mocked_event = MockedEvent({"tag": name, "data": {"lost": tgt}})
     with patch.object(
-        salt.utils.event, "get_event", MagicMock(return_value=Mockevent())
+        salt.utils.event, "get_event", MagicMock(return_value=mocked_event)
     ):
         with patch.dict(saltmod.__opts__, {"sock_dir": True, "transport": True}):
             with patch("salt.states.saltmod.time.time", MagicMock(return_value=1.0)):
@@ -409,7 +397,7 @@ def test_wait_for_event_list_single_event():
                 ret = saltmod.wait_for_event(name, tgt, event_id=event_id, timeout=-1.0)
                 assert ret == expected
 
-                Mockevent.flag = True
+                mocked_event.flag = True
                 expected.update(
                     {
                         "name": name,
@@ -430,16 +418,17 @@ def test_runner():
     """
     name = "state"
 
-    ret = {
+    expected = {
         "changes": {"return": True},
         "name": "state",
         "result": True,
         "comment": "Runner function 'state' executed.",
     }
-    runner_mock = MagicMock(return_value={"return": True})
-
-    with patch.dict(saltmod.__salt__, {"saltutil.runner": runner_mock}):
-        assert saltmod.runner(name) == ret
+    with patch.dict(
+        saltmod.__salt__, {"saltutil.runner": MagicMock(return_value={"return": True})}
+    ):
+        ret = saltmod.runner(name)
+        assert ret == expected
 
 
 def test_wheel():
@@ -448,16 +437,17 @@ def test_wheel():
     """
     name = "state"
 
-    ret = {
+    expected = {
         "changes": {"return": True},
         "name": "state",
         "result": True,
         "comment": "Wheel function 'state' executed.",
     }
-    wheel_mock = MagicMock(return_value={"return": True})
-
-    with patch.dict(saltmod.__salt__, {"saltutil.wheel": wheel_mock}):
-        assert saltmod.wheel(name) == ret
+    with patch.dict(
+        saltmod.__salt__, {"saltutil.wheel": MagicMock(return_value={"return": True})}
+    ):
+        ret = saltmod.wheel(name)
+        assert ret == expected
 
 
 @pytest.mark.slow_test
@@ -465,21 +455,13 @@ def test_state_ssh():
     """
     Test saltmod state passes roster to saltutil.cmd
     """
-    origcmd = saltmod.__salt__["saltutil.cmd"]
-    cmd_kwargs = {}
-    cmd_args = []
-
-    def cmd_mock(*args, **kwargs):
-        cmd_args.extend(args)
-        cmd_kwargs.update(kwargs)
-        return origcmd(*args, **kwargs)
-
+    cmd_mock = MagicMock()
     with patch.dict(saltmod.__salt__, {"saltutil.cmd": cmd_mock}):
         ret = saltmod.state(
             "state.sls", tgt="*", ssh=True, highstate=True, roster="my_roster"
         )
-    assert "roster" in cmd_kwargs
-    assert cmd_kwargs["roster"] == "my_roster"
+    assert "roster" in cmd_mock.call_args.kwargs
+    assert cmd_mock.call_args.kwargs["roster"] == "my_roster"
 
 
 @pytest.mark.slow_test
@@ -487,18 +469,10 @@ def test_function_ssh():
     """
     Test saltmod function passes roster to saltutil.cmd
     """
-    origcmd = saltmod.__salt__["saltutil.cmd"]
-    cmd_kwargs = {}
-    cmd_args = []
-
-    def cmd_mock(*args, **kwargs):
-        cmd_args.extend(args)
-        cmd_kwargs.update(kwargs)
-        return origcmd(*args, **kwargs)
-
+    cmd_mock = MagicMock()
     with patch.dict(saltmod.__opts__, {"test": False}), patch.dict(
         saltmod.__salt__, {"saltutil.cmd": cmd_mock}
     ):
         saltmod.function("state", tgt="*", ssh=True, roster="my_roster")
-    assert "roster" in cmd_kwargs
-    assert cmd_kwargs["roster"] == "my_roster"
+    assert "roster" in cmd_mock.call_args.kwargs
+    assert cmd_mock.call_args.kwargs["roster"] == "my_roster"
