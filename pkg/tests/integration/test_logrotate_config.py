59f731c94fb6db7bb2a16f5971b1c200a579d3ac
Barney Sowood <barney@sowood.co.uk>
2023-10-12 19:04:51 +0100

    Add initial tests for logrotate configs
    Adds initial tests for logrotate configs to ensure they are installed
    in the correct location.

---
+++ pkg/tests/integration/test_logrotate_config.py
@@ -0,0 +1,34 @@
+"""
+Tests for logrotate config
+"""
+
+from pathlib import Path
+
+import pytest
+
+pytestmark = [pytest.mark.skip_on_windows, pytest.mark.skip_on_darwin]
+
+
+@pytest.fixture
+def logrotate_config_file(salt_call_cli):
+    """
+    Fixture for logrotate config file path
+    """
+    logrotate_dir = Path("/etc/logrotate.d")
+
+    os_family = salt_call_cli.run("grains.get", "os_family")
+    assert os_family.returncode == 0
+
+    if os_family.data == "RedHat":
+        return logrotate_dir / "salt"
+    elif os_family.data == "Debian":
+        return logrotate_dir / "salt-common"
+
+
+def test_logrotate_config(logrotate_config_file):
+    """
+    Test that logrotate config has been installed in correctly
+    """
+    assert logrotate_config_file.is_file()
+    assert logrotate_config_file.owner() == "root"
+    assert logrotate_config_file.group() == "root"
