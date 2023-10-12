c9c0cdbf9f83e84a56109ea6a242e9de14aedaf4
cmcmarrow <charles.mcmarrow.4@gmail.com>
2023-10-12 10:12:55 -0500

    rename auth

--- tests/pytests/functional/auth/test_auth.py
+++ tests/pytests/functional/auth/test_auth.py
@@ -6,6 +6,7 @@ def test_user_is_sudo():
     assert auth.AuthUser("bob").is_sudo() is False
     assert auth.AuthUser("sudo_bob").is_sudo() is True
 
+
 def test_user_is_running_user():
     current_user = salt.utils.user.get_user()
     assert auth.AuthUser(salt.utils.user.get_user() + "1").is_running_user() is False
