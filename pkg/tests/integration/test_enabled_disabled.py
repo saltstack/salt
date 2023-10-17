3f9eb2d9f1f10f70052f29404b9670003a1e4d6e
Pedro Algarvio <palgarvio@vmware.com>
2023-10-17 08:42:45 +0100

    Photon OS 5 enables services by default
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- pkg/tests/integration/test_enabled_disabled.py
+++ pkg/tests/integration/test_enabled_disabled.py
@@ -11,9 +11,21 @@ def test_services(install_salt, salt_cli, salt_minion):
     if install_salt.distro_id in ("ubuntu", "debian"):
         services_enabled = ["salt-master", "salt-minion", "salt-syndic", "salt-api"]
         services_disabled = []
-    elif install_salt.distro_id in ("centos", "redhat", "amzn", "fedora", "photon"):
+    elif install_salt.distro_id in ("centos", "redhat", "amzn", "fedora"):
         services_enabled = []
         services_disabled = ["salt-master", "salt-minion", "salt-syndic", "salt-api"]
+    elif install_salt.distro_id == "photon":
+        if float(install_salt.distro_version) < 5:
+            services_enabled = []
+            services_disabled = [
+                "salt-master",
+                "salt-minion",
+                "salt-syndic",
+                "salt-api",
+            ]
+        else:
+            services_enabled = ["salt-master", "salt-minion", "salt-syndic", "salt-api"]
+            services_disabled = []
     elif platform.is_darwin():
         services_enabled = ["salt-minion"]
         services_disabled = []
