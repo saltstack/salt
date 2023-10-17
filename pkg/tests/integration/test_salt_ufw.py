831f982f6f023e6ea4f1c9d3102ef78202e43b5e
David Murphy <dmurphy@saltstack.com>
2023-10-16 19:58:50 -0600

    Updated ufw tests to check for Salt

--- pkg/tests/integration/test_salt_ufw.py
+++ pkg/tests/integration/test_salt_ufw.py
@@ -1,12 +1,9 @@
-import logging
 import os.path
 import pathlib
 import subprocess
 
 import pytest
 from pytestskipmarkers.utils import platform
-
-log = logging.getLogger(__name__)
 
 
 @pytest.mark.skip_on_windows()
@@ -25,21 +22,23 @@ def test_salt_ufw(salt_master, salt_call_cli, install_salt):
     assert ufw_master_path.exists()
     assert ufw_master_path.is_file()
 
-    ls_cmd = "ls -alhrt /etc/ufw/applications.d/"
-    ret = salt_call_cli.run("--local", "cmd.run", ls_cmd)
-    log.warning(f"DGM test_salt_ufw  ls cmd ret '{ret}'")
-
-    cat_cmd = "cat /etc/ufw/applications.d/salt-master"
-    ret = salt_call_cli.run("--local", "cmd.run", cat_cmd)
-    log.warning(f"DGM test_salt_ufw  cat cmd ret '{ret}'")
-
     ufw_list_cmd = "/usr/sbin/ufw app list"
     ret = salt_call_cli.run("--local", "cmd.run", ufw_list_cmd)
-    log.warning(f"DGM test_salt_ufw  list ret '{ret}'")
+    assert "Available applications" in ret.stdout
+    assert "Salt" in ret.stdout
 
     ufw_upd_cmd = "/usr/sbin/ufw app update Salt"
     ret = salt_call_cli.run("--local", "cmd.run", ufw_upd_cmd)
-    log.warning(f"DGM test_salt_ufw  update ret '{ret}'")
+    assert ret.returncode == 0
+
+    expected_info = """Profile: Salt
+Title: salt
+Description: fast and powerful configuration management and remote
+execution
+
+Ports:
+  4505,4506/tcp"""
 
     ufw_info_cmd = "/usr/sbin/ufw app info Salt"
     ret = salt_call_cli.run("--local", "cmd.run", ufw_info_cmd)
+    assert ret.data == expected_info
