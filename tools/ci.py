a199b5b4543c5be38dd562e4ca9c468607236fb6
Megan Wilhite <mwilhite@vmware.com>
2023-10-20 09:19:10 -0600

    Only run amazon linux2 package tests for >=3006

--- tools/ci.py
+++ tools/ci.py
@@ -721,6 +721,12 @@ def pkg_matrix(
     sessions = [
         "install",
     ]
+    # OSs that where never included in 3005
+    # We cannot test an upgrade for this OS on this version
+    not_3005 = ["amazonlinux-2-arm64", "photonos-5", "photonos-5-arm64"]
+    # OSs that where never included in 3006
+    # We cannot test an upgrade for this OS on this version
+    not_3006 = ["photonos-5", "photonos-5-arm64"]
     if (
         distro_slug
         not in [
@@ -806,10 +812,17 @@ def pkg_matrix(
         for version in versions:
             if (
                 version
-                and distro_slug.startswith("photonos-5")
+                and distro_slug in not_3005
+                and version < tools.utils.Version("3006.0")
+            ):
+                # We never build packages for these OSs in 3005
+                continue
+            elif (
+                version
+                and distro_slug in not_3006
                 and version < tools.utils.Version("3007.0")
             ):
-                # We never build packages for Photon OS 5 prior to 3007.0
+                # We never build packages for these OSs in 3006
                 continue
             _matrix.append(
                 {
