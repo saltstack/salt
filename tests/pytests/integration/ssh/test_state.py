ae2ae330e5dab2516d51f770c1679a46b0a36657
jeanluc <lkubb@protonmail.com>
2023-10-29 19:40:18 +0100

    Update salt-ssh state wrapper pillar handling
    Instead of passing the pre-rendered pillar as an override, do it like
    the regular `state` execution module does with `salt-call`: Check if the
    pillar needs to be rendered, otherwise reuse the already rendered one.
    
    Also, ensure that __pillar__ in wrapper modules contains the same one
    used during rendering, same thing for the one passed to `state.pkg`.
    
    Also, ensure that when pillars are rerendered during a state run, they
    get the master opts in addition to the minion ones, since some modules
    used in the pillar can rely on them to be present.
    
    Also, ensure pillar overrides are accepted for the same functions as with
    the regular `state` execution module.

--- tests/pytests/integration/ssh/test_state.py
+++ tests/pytests/integration/ssh/test_state.py
@@ -2,6 +2,7 @@ import json
 
 import pytest
 
+import salt.utils.dictupdate
 from salt.defaults.exitcodes import EX_AGGREGATE
 
 pytestmark = [
@@ -561,3 +562,299 @@ class TestStateRunFailRetcode:
     def test_retcode_state_top_run_fail(self, salt_ssh_cli):
         ret = salt_ssh_cli.run("state.top", "top.sls")
         assert ret.returncode == EX_AGGREGATE
+
+
+@pytest.fixture(scope="class")
+def pillar_tree_nested(base_env_pillar_tree_root_dir):
+    top_file = """
+    base:
+      'localhost':
+        - nested
+      '127.0.0.1':
+        - nested
+    """
+    nested_pillar = r"""
+    {%- do salt.log.warning("hithere: pillar was rendered") %}
+    monty: python
+    the_meaning:
+      of:
+        life: 42
+        bar: tender
+      for: what
+    """
+    top_tempfile = pytest.helpers.temp_file(
+        "top.sls", top_file, base_env_pillar_tree_root_dir
+    )
+    nested_tempfile = pytest.helpers.temp_file(
+        "nested.sls", nested_pillar, base_env_pillar_tree_root_dir
+    )
+    with top_tempfile, nested_tempfile:
+        yield
+
+
+@pytest.mark.usefixtures("pillar_tree_nested")
+def test_pillar_is_only_rendered_once_without_overrides(salt_ssh_cli, caplog):
+    ret = salt_ssh_cli.run("state.apply", "test")
+    assert ret.returncode == 0
+    assert isinstance(ret.data, dict)
+    assert ret.data
+    assert ret.data[next(iter(ret.data))]["result"] is True
+    assert caplog.text.count("hithere: pillar was rendered") == 1
+
+
+@pytest.mark.usefixtures("pillar_tree_nested")
+def test_pillar_is_rerendered_with_overrides(salt_ssh_cli, caplog):
+    ret = salt_ssh_cli.run("state.apply", "test", pillar={"foo": "bar"})
+    assert ret.returncode == 0
+    assert isinstance(ret.data, dict)
+    assert ret.data
+    assert ret.data[next(iter(ret.data))]["result"] is True
+    assert caplog.text.count("hithere: pillar was rendered") == 2
+
+
+@pytest.mark.slow_test
+@pytest.mark.usefixtures("pillar_tree_nested")
+class TestStatePillarOverride:
+    """
+    Ensure pillar overrides are merged recursively, that wrapper
+    modules are in sync with the pillar dict in the rendering environment
+    and that the pillars are available on the target.
+    """
+
+    @pytest.fixture(scope="class", autouse=True)
+    def _show_pillar_state(self, base_env_state_tree_root_dir):
+        top_file = """
+        base:
+          'localhost':
+            - showpillar
+          '127.0.0.1':
+            - showpillar
+        """
+        show_pillar_sls = """
+        deep_thought:
+          test.show_notification:
+            - text: '{{ {
+                "raw": {
+                  "the_meaning": pillar.get("the_meaning"),
+                  "btw": pillar.get("btw")},
+                "wrapped": {
+                  "the_meaning": salt["pillar.get"]("the_meaning"),
+                  "btw": salt["pillar.get"]("btw")}}
+                | json }}'
+
+        target_check:
+          test.check_pillar:
+            - present:
+              - the_meaning:of:foo
+              - btw
+              - the_meaning:of:bar
+              - the_meaning:for
+            - listing:
+              - the_meaning:of:life
+        """
+        top_tempfile = pytest.helpers.temp_file(
+            "top.sls", top_file, base_env_state_tree_root_dir
+        )
+        show_tempfile = pytest.helpers.temp_file(
+            "showpillar.sls", show_pillar_sls, base_env_state_tree_root_dir
+        )
+        with top_tempfile, show_tempfile:
+            yield
+
+    @pytest.fixture
+    def base(self):
+        return {"the_meaning": {"of": {"life": 42, "bar": "tender"}, "for": "what"}}
+
+    @pytest.fixture
+    def override(self, base):
+        poverride = {
+            "the_meaning": {"of": {"life": [2.71], "foo": "lish"}},
+            "btw": "turtles",
+        }
+        expected = salt.utils.dictupdate.merge(base, poverride)
+        return expected, poverride
+
+    def test_state_sls(self, salt_ssh_cli, override):
+        expected, override = override
+        ret = salt_ssh_cli.run("state.sls", "showpillar", pillar=override)
+        self._assert_basic(ret)
+        assert len(ret.data) == 2
+        for sid, sret in ret.data.items():
+            if "show" in sid:
+                self._assert_pillar(sret["comment"], expected)
+            else:
+                assert sret["result"] is True
+
+    @pytest.mark.parametrize("sid", ("deep_thought", "target_check"))
+    def test_state_sls_id(self, salt_ssh_cli, sid, override):
+        expected, override = override
+        ret = salt_ssh_cli.run("state.sls_id", sid, "showpillar", pillar=override)
+        self._assert_basic(ret)
+        state_res = ret.data[next(iter(ret.data))]
+        if sid == "deep_thought":
+            self._assert_pillar(state_res["comment"], expected)
+        else:
+            assert state_res["result"] is True
+
+    def test_state_highstate(self, salt_ssh_cli, override):
+        expected, override = override
+        ret = salt_ssh_cli.run(
+            "state.highstate", pillar=override, whitelist=["showpillar"]
+        )
+        self._assert_basic(ret)
+        assert len(ret.data) == 2
+        for sid, sret in ret.data.items():
+            if "show" in sid:
+                self._assert_pillar(sret["comment"], expected)
+            else:
+                assert sret["result"] is True
+
+    def test_state_show_sls(self, salt_ssh_cli, override):
+        expected, override = override
+        ret = salt_ssh_cli.run("state.show_sls", "showpillar", pillar=override)
+        self._assert_basic(ret)
+        pillar = ret.data["deep_thought"]["test"]
+        pillar = next(x["text"] for x in pillar if isinstance(x, dict))
+        self._assert_pillar(pillar, expected)
+
+    def test_state_show_low_sls(self, salt_ssh_cli, override):
+        expected, override = override
+        ret = salt_ssh_cli.run("state.show_low_sls", "showpillar", pillar=override)
+        self._assert_basic(ret, list)
+        pillar = ret.data[0]["text"]
+        self._assert_pillar(pillar, expected)
+
+    def test_state_single(self, salt_ssh_cli, override):
+        expected, override = override
+        ret = salt_ssh_cli.run(
+            "state.single",
+            "test.check_pillar",
+            "foo",
+            present=[
+                "the_meaning:of:foo",
+                "btw",
+                "the_meaning:of:bar",
+                "the_meaning:for",
+            ],
+            listing=["the_meaning:of:life"],
+            pillar=override,
+        )
+        self._assert_basic(ret, dict)
+        state_res = ret.data[next(iter(ret.data))]
+        assert state_res["result"] is True
+
+    def test_state_top(self, salt_ssh_cli, override):
+        expected, override = override
+        ret = salt_ssh_cli.run("state.top", "top.sls", pillar=override)
+        self._assert_basic(ret)
+        assert len(ret.data) == 2
+        for sid, sret in ret.data.items():
+            if "show" in sid:
+                self._assert_pillar(sret["comment"], expected)
+            else:
+                assert sret["result"] is True
+
+    def _assert_pillar(self, pillar, expected):
+        if not isinstance(pillar, dict):
+            pillar = json.loads(pillar)
+        assert pillar["raw"] == expected
+        assert pillar["wrapped"] == expected
+
+    def _assert_basic(self, ret, typ=dict):
+        assert ret.returncode == 0
+        assert isinstance(ret.data, typ)
+        assert ret.data
+
+
+@pytest.mark.slow_test
+@pytest.mark.usefixtures("pillar_tree_nested")
+class TestStatePillarOverrideTemplate:
+    """
+    Specifically ensure that pillars are merged as expected
+    for the target as well and available for renderers.
+    This should be covered by `test.check_pillar` above, but
+    let's check the specific output for the most important funcs.
+    Issue #59802
+    """
+
+    @pytest.fixture
+    def _write_pillar_state(self, base_env_state_tree_root_dir, tmp_path_factory):
+        tmp_path = tmp_path_factory.mktemp("tgtdir")
+        tgt_file = tmp_path / "deepthought.txt"
+        top_file = """
+        base:
+          'localhost':
+            - writepillar
+          '127.0.0.1':
+            - writepillar
+        """
+        nested_pillar_file = f"""
+        deep_thought:
+          file.managed:
+            - name: {tgt_file}
+            - source: salt://deepthought.txt.jinja
+            - template: jinja
+        """
+        # deepthought = "{{ {'the_meaning': pillar.get('the_meaning'), 'btw': pillar.get('btw')} | json }}"
+        deepthought = r"""
+        {{
+          {
+            "raw": {
+              "the_meaning": pillar.get("the_meaning"),
+              "btw": pillar.get("btw")},
+            "modules": {
+              "the_meaning": salt["pillar.get"]("the_meaning"),
+              "btw": salt["pillar.get"]("btw")}
+          } | json }}
+        """
+        top_tempfile = pytest.helpers.temp_file(
+            "top.sls", top_file, base_env_state_tree_root_dir
+        )
+        show_tempfile = pytest.helpers.temp_file(
+            "writepillar.sls", nested_pillar_file, base_env_state_tree_root_dir
+        )
+        deepthought_tempfile = pytest.helpers.temp_file(
+            "deepthought.txt.jinja", deepthought, base_env_state_tree_root_dir
+        )
+
+        with top_tempfile, show_tempfile, deepthought_tempfile:
+            yield tgt_file
+
+    @pytest.fixture
+    def base(self):
+        return {"the_meaning": {"of": {"life": 42, "bar": "tender"}, "for": "what"}}
+
+    @pytest.fixture
+    def override(self, base):
+        poverride = {
+            "the_meaning": {"of": {"life": 2.71, "foo": "lish"}},
+            "btw": "turtles",
+        }
+        expected = salt.utils.dictupdate.merge(base, poverride)
+        return expected, poverride
+
+    def test_state_sls(self, salt_ssh_cli, override, _write_pillar_state):
+        expected, override = override
+        ret = salt_ssh_cli.run("state.sls", "writepillar", pillar=override)
+        self._assert_pillar(ret, expected, _write_pillar_state)
+
+    def test_state_highstate(self, salt_ssh_cli, override, _write_pillar_state):
+        expected, override = override
+        ret = salt_ssh_cli.run(
+            "state.highstate", pillar=override, whitelist=["writepillar"]
+        )
+        self._assert_pillar(ret, expected, _write_pillar_state)
+
+    def test_state_top(self, salt_ssh_cli, override, _write_pillar_state):
+        expected, override = override
+        ret = salt_ssh_cli.run("state.top", "top.sls", pillar=override)
+        self._assert_pillar(ret, expected, _write_pillar_state)
+
+    def _assert_pillar(self, ret, expected, path):
+        assert ret.returncode == 0
+        assert isinstance(ret.data, dict)
+        assert ret.data
+        assert path.exists()
+        pillar = json.loads(path.read_text())
+        assert pillar["raw"] == expected
+        assert pillar["modules"] == expected
