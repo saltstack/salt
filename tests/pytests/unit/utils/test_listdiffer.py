cbe76230ebefe4a8bb9fdb9effd92ab4170d5e25
David Murphy <dmurphy@saltstack.com>
2023-11-01 11:22:14 -0600

    Updatd test per reviewer comments

--- tests/pytests/unit/utils/test_listdiffer.py
+++ tests/pytests/unit/utils/test_listdiffer.py
@@ -1,12 +1,7 @@
 import pytest
 
-from salt.utils import dictdiffer
+from salt.utils.dictdiffer import RecursiveDictDiffer
 from salt.utils.listdiffer import list_diff
-
-
-@pytest.fixture
-def get_NONE_value():
-    return dictdiffer.RecursiveDictDiffer.NONE_VALUE
 
 
 @pytest.fixture
@@ -28,58 +23,57 @@ def get_new_list():
 
 
 @pytest.fixture
-def test_list_diff(get_old_list, get_new_list):
-    test_list_diff = list_diff(get_old_list, get_new_list, key="key")
-    return test_list_diff
+def get_list_diff(get_old_list, get_new_list):
+    return list_diff(get_old_list, get_new_list, key="key")
 
 
-def test_added(test_list_diff):
-    assert len(test_list_diff.added) == 1
-    assert test_list_diff.added[0] == {"key": 5, "value": "foo5", "int_value": 105}
+def test_added(get_list_diff):
+    assert len(get_list_diff.added) == 1
+    assert get_list_diff.added[0] == {"key": 5, "value": "foo5", "int_value": 105}
 
 
-def test_removed(test_list_diff):
-    assert len(test_list_diff.removed) == 1
-    assert test_list_diff.removed[0] == {"key": 3, "value": "foo3", "int_value": 103}
+def test_removed(get_list_diff):
+    assert len(get_list_diff.removed) == 1
+    assert get_list_diff.removed[0] == {"key": 3, "value": "foo3", "int_value": 103}
 
 
-def test_diffs(test_list_diff, get_NONE_value):
-    assert len(test_list_diff.diffs) == 3
-    assert test_list_diff.diffs[0] == {2: {"int_value": {"new": 112, "old": 102}}}
+def test_diffs(get_list_diff):
+    assert len(get_list_diff.diffs) == 3
+    assert get_list_diff.diffs[0] == {2: {"int_value": {"new": 112, "old": 102}}}
 
     # Added items
-    assert test_list_diff.diffs[1] == {
+    assert get_list_diff.diffs[1] == {
         5: {
-            "int_value": {"new": 105, "old": get_NONE_value},
-            "key": {"new": 5, "old": get_NONE_value},
-            "value": {"new": "foo5", "old": get_NONE_value},
+            "int_value": {"new": 105, "old": RecursiveDictDiffer.NONE_VALUE},
+            "key": {"new": 5, "old": RecursiveDictDiffer.NONE_VALUE},
+            "value": {"new": "foo5", "old": RecursiveDictDiffer.NONE_VALUE},
         }
     }
 
     # Removed items
-    assert test_list_diff.diffs[2] == {
+    assert get_list_diff.diffs[2] == {
         3: {
-            "int_value": {"new": get_NONE_value, "old": 103},
-            "key": {"new": get_NONE_value, "old": 3},
-            "value": {"new": get_NONE_value, "old": "foo3"},
+            "int_value": {"new": RecursiveDictDiffer.NONE_VALUE, "old": 103},
+            "key": {"new": RecursiveDictDiffer.NONE_VALUE, "old": 3},
+            "value": {"new": RecursiveDictDiffer.NONE_VALUE, "old": "foo3"},
         }
     }
 
 
-def test_new_values(test_list_diff):
-    assert len(test_list_diff.new_values) == 2
-    assert test_list_diff.new_values[0] == {"key": 2, "int_value": 112}
-    assert test_list_diff.new_values[1] == {"key": 5, "value": "foo5", "int_value": 105}
+def test_new_values(get_list_diff):
+    assert len(get_list_diff.new_values) == 2
+    assert get_list_diff.new_values[0] == {"key": 2, "int_value": 112}
+    assert get_list_diff.new_values[1] == {"key": 5, "value": "foo5", "int_value": 105}
 
 
-def test_old_values(test_list_diff):
-    assert len(test_list_diff.old_values) == 2
-    assert test_list_diff.old_values[0] == {"key": 2, "int_value": 102}
-    assert test_list_diff.old_values[1] == {"key": 3, "value": "foo3", "int_value": 103}
+def test_old_values(get_list_diff):
+    assert len(get_list_diff.old_values) == 2
+    assert get_list_diff.old_values[0] == {"key": 2, "int_value": 102}
+    assert get_list_diff.old_values[1] == {"key": 3, "value": "foo3", "int_value": 103}
 
 
-def test_changed_all(test_list_diff):
-    assert test_list_diff.changed(selection="all") == [
+def test_changed_all(get_list_diff):
+    assert get_list_diff.changed(selection="all") == [
         "key.2.int_value",
         "key.5.int_value",
         "key.5.value",
@@ -88,11 +82,11 @@ def test_changed_all(test_list_diff):
     ]
 
 
-def test_changed_intersect(test_list_diff):
-    assert test_list_diff.changed(selection="intersect") == ["key.2.int_value"]
+def test_changed_intersect(get_list_diff):
+    assert get_list_diff.changed(selection="intersect") == ["key.2.int_value"]
 
 
-def test_changes_str(test_list_diff):
+def test_changes_str(get_list_diff):
     expected = """\tidentified by key 2:
 \tint_value from 102 to 112
 \tidentified by key 3:
@@ -100,10 +94,10 @@ def test_changes_str(test_list_diff):
 \tidentified by key 5:
 \twill be added
 """
-    assert test_list_diff.changes_str == expected
+    assert get_list_diff.changes_str == expected
 
 
-def test_intersect(test_list_diff):
+def test_intersect(get_list_diff):
     expected = [
         {
             "key": 1,
@@ -116,11 +110,11 @@ def test_intersect(test_list_diff):
             "new": {"key": 2, "value": "foo2", "int_value": 112},
         },
     ]
-    test_isect = test_list_diff.intersect
+    test_isect = get_list_diff.intersect
     assert test_isect == expected
 
 
-def test_remove_diff_intersect(test_list_diff):
+def test_remove_diff_intersect(get_list_diff):
     expected = [
         {
             "key": 1,
@@ -134,12 +128,12 @@ def test_remove_diff_intersect(test_list_diff):
         },
     ]
 
-    test_list_diff.remove_diff(diff_key="value")
-    test_isect = test_list_diff.intersect
+    get_list_diff.remove_diff(diff_key="value")
+    test_isect = get_list_diff.intersect
     assert test_isect == expected
 
 
-def test_remove_diff_removed(test_list_diff):
+def test_remove_diff_removed(get_list_diff):
     expected = [
         {
             "key": 1,
@@ -152,35 +146,35 @@ def test_remove_diff_removed(test_list_diff):
             "new": {"key": 2, "value": "foo2", "int_value": 112},
         },
     ]
-    test_list_diff.remove_diff(diff_key="value", diff_list="removed")
-    test_isect = test_list_diff.intersect
+    get_list_diff.remove_diff(diff_key="value", diff_list="removed")
+    test_isect = get_list_diff.intersect
     assert test_isect == expected
 
 
-def test_changes_str2(test_list_diff):
+def test_changes_str2(get_list_diff):
     expected = """  key=2 (updated):
     int_value from 102 to 112
   key=3 (removed)
   key=5 (added): {'key': 5, 'value': 'foo5', 'int_value': 105}"""
-    test_changes = test_list_diff.changes_str2
+    test_changes = get_list_diff.changes_str2
     assert test_changes == expected
 
 
-def test_current_list(test_list_diff):
+def test_current_list(get_list_diff):
     expected = [
         {"key": 1, "value": "foo1", "int_value": 101},
         {"key": 2, "value": "foo2", "int_value": 102},
         {"key": 3, "value": "foo3", "int_value": 103},
     ]
-    test_curr_list = test_list_diff.current_list
+    test_curr_list = get_list_diff.current_list
     assert test_curr_list == expected
 
 
-def test_new_list(test_list_diff):
+def test_new_list(get_list_diff):
     expected = [
         {"key": 1, "value": "foo1", "int_value": 101},
         {"key": 2, "value": "foo2", "int_value": 112},
         {"key": 5, "value": "foo5", "int_value": 105},
     ]
-    test_new_list = test_list_diff.new_list
+    test_new_list = get_list_diff.new_list
     assert test_new_list == expected
