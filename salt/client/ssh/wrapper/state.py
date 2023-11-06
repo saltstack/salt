82f90e2f15cb93fd7094a04b1493f74450b39d0c
jeanluc <lkubb@protonmail.com>
2023-11-06 12:37:23 +0100

    Add state.test to SSH wrapper

--- salt/client/ssh/wrapper/state.py
+++ salt/client/ssh/wrapper/state.py
@@ -1242,3 +1242,18 @@ def single(fun, name, test=None, **kwargs):
         pass
 
     return {"local": salt.client.ssh.wrapper.parse_ret(stdout, stderr, retcode)}
+
+
+def test(*args, **kwargs):
+    """
+    .. versionadded:: 3001
+
+    Alias for `state.apply` with the kwarg `test` forced to `True`.
+
+    This is a nicety to avoid the need to type out `test=True` and the possibility of
+    a typo causing changes you do not intend.
+    """
+    kwargs["test"] = True
+    ret = apply_(*args, **kwargs)
+
+    return ret
