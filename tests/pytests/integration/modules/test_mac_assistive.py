723a3ca49f13e52eba992f9fce39f199c4428f01
Gareth J. Greenaway <gareth@saltstack.com>
2023-11-09 16:31:39 -0800

    fixing test_mac_assistive.py

--- tests/pytests/integration/modules/test_mac_assistive.py
+++ tests/pytests/integration/modules/test_mac_assistive.py
@@ -18,17 +18,19 @@ def osa_script():
 
 
 @pytest.fixture(scope="function")
-def setup_teardown_vars(salt_call_cli, osa_script, setup_teardown_vars):
+def setup_teardown_vars(salt_call_cli, osa_script):
     salt_call_cli.run("assistive.install", osa_script, True)
     try:
         yield
     finally:
-        osa_script_ret = salt_call_cli.run("assistive.installed", osa_script)
+        ret = salt_call_cli.run("assistive.installed", osa_script)
+        osa_script_ret = ret.data
         if osa_script_ret:
             salt_call_cli.run("assistive.remove", osa_script)
 
         smile_bundle = "com.smileonmymac.textexpander"
-        smile_bundle_present = salt_call_cli.run("assistive.installed", smile_bundle)
+        ret = salt_call_cli.run("assistive.installed", smile_bundle)
+        smile_bundle_present = ret.data
         if smile_bundle_present:
             salt_call_cli.run("assistive.remove", smile_bundle)
 
@@ -49,11 +51,13 @@ def test_installed(salt_call_cli, osa_script, setup_teardown_vars):
     Tests the True and False return of assistive.installed.
     """
     # OSA script should have been installed in setUp function
-    assert salt_call_cli.run("assistive.installed", osa_script)
+    ret = salt_call_cli.run("assistive.installed", osa_script)
+    assert ret.data
     # Clean up install
     salt_call_cli.run("assistive.remove", osa_script)
     # Installed should now return False
-    assert not salt_call_cli.run("assistive.installed", osa_script)
+    ret = salt_call_cli.run("assistive.installed", osa_script)
+    assert not ret.data
 
 
 @pytest.mark.slow_test
@@ -63,13 +67,17 @@ def test_enable(salt_call_cli, osa_script, setup_teardown_vars):
     """
     # OSA script should have been installed and enabled in setUp function
     # Now let's disable it, which should return True.
-    assert salt_call_cli.run("assistive.enable", osa_script, False)
+    ret = salt_call_cli.run("assistive.enable", osa_script, False)
+    assert ret.data
     # Double check the script was disabled, as intended.
-    assert not salt_call_cli.run("assistive.enabled", osa_script)
+    ret = salt_call_cli.run("assistive.enabled", osa_script)
+    assert not ret.data
     # Now re-enable
-    assert salt_call_cli.run("assistive.enable", osa_script)
+    ret = salt_call_cli.run("assistive.enable", osa_script)
+    assert ret.data
     # Double check the script was enabled, as intended.
-    assert salt_call_cli.run("assistive.enabled", osa_script)
+    ret = salt_call_cli.run("assistive.enabled", osa_script)
+    assert ret.data
 
 
 @pytest.mark.slow_test
@@ -79,8 +87,10 @@ def test_enabled(salt_call_cli, osa_script, setup_teardown_vars):
     """
     # OSA script should have been installed in setUp function, which sets
     # enabled to True by default.
-    assert salt_call_cli.run("assistive.enabled", osa_script)
+    ret = salt_call_cli.run("assistive.enabled", osa_script)
+    assert ret.data
     # Disable OSA Script
     salt_call_cli.run("assistive.enable", osa_script, False)
     # Assert against new disabled status
-    assert not salt_call_cli.run("assistive.enabled", osa_script)
+    ret = salt_call_cli.run("assistive.enabled", osa_script)
+    assert not ret.data
