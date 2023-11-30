7476d6fd932d655f561b0d5ce20bccf19f5b8855
Pedro Algarvio <palgarvio@vmware.com>
2023-11-30 10:36:49 +0000

    Don't group fixtures, and improve them
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- tests/pytests/functional/modules/test_mac_group.py
+++ tests/pytests/functional/modules/test_mac_group.py
@@ -20,23 +20,57 @@ def group(modules):
 
 # Create group name strings for tests
 @pytest.fixture(scope="module")
-def add_group():
-    yield random_string("RS-", lowercase=False)
+def non_existing_group_name(group):
+    group_name = random_string("group-", lowercase=False)
+    try:
+        yield group_name
+    finally:
+        # Delete the added group
+        group_info = group.info(group_name)
+        if group_info:
+            group.delete(group_name)
 
 
 @pytest.fixture(scope="module")
-def del_group():
-    yield random_string("RS-", lowercase=False)
+def existing_group_name(group):
+    group_name = random_string("group-", lowercase=False)
+    try:
+        ret = group.add(group_name, 4567)
+        if ret is not True:
+            pytest.skip("Failed to create a group to delete")
+        yield group_name
+    finally:
+        # Delete the added group
+        group_info = group.info(group_name)
+        if group_info:
+            group.delete(group_name)
 
 
 @pytest.fixture(scope="module")
-def change_group():
-    yield random_string("RS-", lowercase=False)
+def non_existing_user(group):
+    group_name = random_string("user-", lowercase=False)
+    try:
+        yield group_name
+    finally:
+        # Delete the added group
+        group_info = group.info(group_name)
+        if group_info:
+            group.delete(group_name)
 
 
 @pytest.fixture(scope="module")
-def add_user():
-    yield random_string("RS-", lowercase=False)
+def existing_user(group, existing_group_name):
+    group_name = random_string("user-", lowercase=False)
+    try:
+        ret = group.adduser(existing_group_name, group_name)
+        if ret is not True:
+            pytest.skip("Failed to create an existing group member")
+        yield group_name
+    finally:
+        # Delete the added group
+        group_info = group.info(group_name)
+        if group_info:
+            group.delete(group_name)
 
 
 @pytest.fixture(scope="module")
@@ -44,138 +78,99 @@ def rep_user_group():
     yield random_string("RS-", lowercase=False)
 
 
-@pytest.fixture(autouse=True)
-def _setup_teardown_vars(group, add_group, change_group, del_group):
+@pytest.fixture(scope="module")
+def non_existing_group_member(group):
+    group_name = random_string("user-", lowercase=False)
     try:
-        yield
+        yield group_name
     finally:
-        # Delete ADD_GROUP
-        add_info = group.info(add_group)
-        if add_info:
-            group.delete(add_group)
-
-        # Delete DEL_GROUP if something failed
-        del_info = group.info(del_group)
-        if del_info:
-            group.delete(del_group)
-
-        # Delete CHANGE_GROUP
-        change_info = group.info(change_group)
-        if change_info:
-            group.delete(change_group)
+        # Delete the added group
+        group_info = group.info(group_name)
+        if group_info:
+            group.delete(group_name)
 
 
-def test_mac_group_add(group, add_group):
+def test_mac_group_add(group, non_existing_group_name):
     """
     Tests the add group function
     """
-    group.add(add_group, 3456)
-    group_info = group.info(add_group)
-    assert group_info["name"] == add_group
+    group.add(non_existing_group_name, 3456)
+    group_info = group.info(non_existing_group_name)
+    assert group_info["name"] == non_existing_group_name
 
 
-def test_mac_group_delete(group, del_group):
+def test_mac_group_delete(group, existing_group_name):
     """
     Tests the delete group function
     """
-    # Create a group to delete - If unsuccessful, skip the test
-    group_add_ret = group.add(del_group, 4567)
-    if group_add_ret is not True:
-        group.delete(del_group)
-        pytest.skip("Failed to create a group to delete")
-
-    # Now try to delete the added group
-    ret = group.delete(del_group)
+    ret = group.delete(existing_group_name)
     assert ret
 
 
-def test_mac_group_chgid(group, change_group):
+def test_mac_group_chgid(group, existing_group_name):
     """
     Tests changing the group id
     """
-    # Create a group to delete - If unsuccessful, skip the test
-    ret = group.add(change_group, 5678)
-    if ret is not True:
-        group.delete(change_group)
-        pytest.skip("Failed to create a group to manipulate")
-
-    group.chgid(change_group, 6789)
-    group_info = group.info(change_group)
-    assert group_info["gid"] == 6789
+    gid = 6789
+    group_info = group.info(existing_group_name)
+    assert group_info["gid"] != gid
+    group.chgid(existing_group_name, gid)
+    group_info = group.info(existing_group_name)
+    assert group_info["gid"] == gid
 
 
-def test_mac_adduser(group, add_group, add_user):
+def test_mac_adduser(group, non_existing_group_name, non_existing_user):
     """
     Tests adding user to the group
     """
     # Create a group to use for test - If unsuccessful, skip the test
-    ret = group.add(add_group, 5678)
+    ret = group.add(non_existing_group_name, 5678)
     if ret is not True:
-        group.delete(add_group)
+        group.delete(non_existing_group_name)
         pytest.skip("Failed to create a group to manipulate")
 
-    group.adduser(add_group, add_user)
-    group_info = group.info(add_group)
-    assert add_user == "".join(group_info["members"])
+    group.adduser(non_existing_group_name, non_existing_user)
+    group_info = group.info(non_existing_group_name)
+    assert non_existing_user in group_info["members"]
+    assert group_info["members"] == [non_existing_user]
 
 
-def test_mac_deluser(group, add_group, add_user):
+def test_mac_deluser(group, existing_group_name, existing_user):
     """
     Test deleting user from a group
     """
-    # Create a group to use for test - If unsuccessful, skip the test
-    group_add_ret = group.add(add_group, 5678)
-    user_add_ret = group.adduser(add_group, add_user)
-
-    if group_add_ret and user_add_ret is not True:
-        group.delete(add_group)
-        pytest.skip("Failed to create a group to manipulate")
-
-    delusr = group.deluser(add_group, add_user)
+    delusr = group.deluser(existing_group_name, existing_user)
     assert delusr
 
-    group_info = group.info(add_group)
-    assert add_user not in "".join(group_info["members"])
+    group_info = group.info(existing_group_name)
+    assert existing_user not in group_info["members"]
 
 
-def test_mac_members(group, add_group, add_user, rep_user_group):
+def test_mac_members(
+    group, existing_group_name, existing_user, non_existing_group_member
+):
     """
     Test replacing members of a group
     """
-    group_add_ret = group.add(add_group, 5678)
-    user_add_ret = group.adduser(add_group, add_user)
+    group_info = group.info(existing_group_name)
+    assert non_existing_group_member not in group_info["members"]
+    assert non_existing_user in group_info["members"]
 
-    if group_add_ret and user_add_ret is not True:
-        group.delete(add_group)
-        pytest.skip(
-            "Failed to create the {} group or add user {} to group "
-            "to manipulate".format(add_group, add_user)
-        )
-
-    rep_group_mem = group.members(add_group, rep_user_group)
+    # Replace group members
+    rep_group_mem = group.members(existing_group_name, non_existing_group_member)
     assert rep_group_mem
 
     # ensure new user is added to group and previous user is removed
-    group_info = group.info(add_group)
-    assert rep_user_group in str(group_info["members"])
-    assert add_user not in str(group_info["members"])
+    group_info = group.info(existing_group_name)
+    assert non_existing_group_member in group_info["members"]
+    assert non_existing_user not in group_info["members"]
 
 
-def test_mac_getent(group, add_group, add_user):
+def test_mac_getent(group, existing_user, existing_group_name):
     """
     Test returning info on all groups
     """
-    group_add_ret = group.add(add_group, 5678)
-    user_add_ret = group.adduser(add_group, add_user)
-
-    if group_add_ret and user_add_ret is not True:
-        group.delete(add_group)
-        pytest.skip(
-            "Failed to create the {} group or add user {} to group "
-            "to manipulate".format(add_group, add_user)
-        )
-
     getinfo = group.getent()
     assert getinfo
-    assert add_group in str(getinfo)
-    assert add_user in str(getinfo)
+    assert existing_group_name in str(getinfo)
+    assert existing_user in str(getinfo)
