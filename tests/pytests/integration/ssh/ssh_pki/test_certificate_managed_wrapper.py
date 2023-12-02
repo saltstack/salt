e7357707de996ecba3f107c4624071f949468a3f
jeanluc <lkubb@protonmail.com>
2023-12-02 16:07:26 +0100

    Add `ssh_pki` SSH wrapper module

---
+++ tests/pytests/integration/ssh/ssh_pki/test_certificate_managed_wrapper.py
@@ -0,0 +1,417 @@
+import logging
+from pathlib import Path
+
+import pytest
+
+try:
+    from cryptography.hazmat.primitives import serialization
+
+    import salt.utils.x509 as x509util
+
+    HAS_LIBS = True
+except ImportError:
+    HAS_LIBS = False
+
+
+log = logging.getLogger(__name__)
+
+
+pytestmark = [
+    pytest.mark.slow_test,
+    pytest.mark.skipif(HAS_LIBS is False, reason="Needs cryptography library"),
+]
+
+
+@pytest.fixture(scope="module")
+def other_backend(cert_exts, ssh_salt_ssh_cli, sshpki_salt_master, ca_pub):
+    wrapper = f"""\
+__virtualname__ = "other_backend"
+
+
+def get_signing_policy(signing_policy, ca_server=None, donotfail=False):
+    if donotfail is False:
+        raise ValueError("Was not instructed to not fail :|")
+    return {{
+        "signing_public_key": "{ca_pub.strip()}",
+        "cert_type": "user",
+        "key_id": "saltstacktest",
+        "serial_number": "03:68:6D:B3:0D:5E:F2:54",
+        "not_before": "2023-06-28 08:03:14",
+        "not_after": "2023-06-29 08:03:14",
+        "critical_options": {{"force-command": "echo hi", "no-port-forwarding": True}},
+        "extensions": {{"permit-X11-forwarding": True, "permit-agent-forwarding": True}},
+        "valid_principals": ["salt", "stack"],
+    }}
+
+def create_certificate(
+    ca_server=None,
+    signing_policy=None,
+    path=None,
+    overwrite=False,
+    raw=False,
+    **kwargs,
+):
+    if not kwargs.get("donotfail"):
+        raise ValueError("Was not instructed to not fail :|")
+    return '''\
+{cert_exts}'''
+"""
+    wrapper_dir = Path(sshpki_salt_master.config["file_roots"]["base"][0]) / "_wrapper"
+    wrapper_tempfile = pytest.helpers.temp_file(
+        "other_backend.py", wrapper, wrapper_dir
+    )
+    try:
+        with wrapper_tempfile:
+            ret = sshpki_salt_master.salt_run_cli().run("fileserver.update")
+            assert ret.returncode == 0
+            ret = sshpki_salt_master.salt_run_cli().run("saltutil.sync_wrapper")
+            assert ret.returncode == 0
+            assert "wrapper.other_backend" in ret.data
+            yield
+    finally:
+        ret = ssh_salt_ssh_cli.run("saltutil.sync_wrapper")
+        assert ret.returncode == 0
+
+
+@pytest.fixture(scope="module", autouse=True)
+def cm_wrapper(sshpki_salt_master):
+    state_contents = """
+    {{
+        salt["ssh_pki.certificate_managed_wrapper"](
+            pillar["args"]["name"],
+            ca_server=pillar["args"]["ca_server"],
+            signing_policy=pillar["args"]["signing_policy"],
+            backend=pillar["args"].get("backend"),
+            backend_args=pillar["args"].get("backend_args"),
+            private_key_managed=pillar["args"].get("private_key_managed"),
+            private_key=pillar["args"].get("private_key"),
+            private_key_passphrase=pillar["args"].get("private_key_passphrase"),
+            public_key=pillar["args"].get("public_key"),
+            certificate_managed=pillar["args"].get("certificate_managed"),
+            test=opts.get("test")
+        ) | yaml(false)
+    }}
+    """
+    with sshpki_salt_master.state_tree.base.temp_file("cert.sls", state_contents):
+        yield
+
+
+@pytest.fixture
+def pk_tgt(tmp_path):
+    return str(tmp_path / "managed_key")
+
+
+@pytest.fixture
+def cert_args(ca_minion_id, sshpki_data, tmp_path):
+    return {
+        "name": f"{tmp_path}/cert",
+        "ca_server": ca_minion_id,
+        "signing_policy": "testpolicy",
+        "private_key": str(sshpki_data),
+        "certificate_managed": {
+            "key_id": "from_args",
+        },
+    }
+
+
+@pytest.fixture
+def cert_arg_exts():
+    return {
+        "permit-X11-forwarding": True,
+        "permit-agent-forwarding": True,
+        "permit-port-forwarding": True,
+        "permit-pty": True,
+        "permit-user-rc": False,
+    }
+
+
+@pytest.fixture
+def cert_expected_exts():
+    return {
+        b"permit-X11-forwarding": b"",
+        b"permit-agent-forwarding": b"",
+        b"permit-port-forwarding": b"",
+        b"permit-pty": b"",
+    }
+
+
+@pytest.fixture
+def cert_arg_opts():
+    return {
+        "force-command": "echo hi",
+        "no-port-forwarding": True,
+        "verify-required": False,
+    }
+
+
+@pytest.fixture
+def cert_expected_opts():
+    return {
+        b"force-command": b"echo hi",
+        b"no-port-forwarding": b"",
+    }
+
+
+@pytest.fixture(params=[{}])
+def existing_cert(ssh_salt_ssh_cli, cert_args, ca_key, rsa_privkey, request, pk_tgt):
+    cert_managed_params = request.param.pop("certificate_managed", {})
+    pk_managed = {}
+    exp_key = rsa_privkey
+    if "private_key_managed" in request.param:
+        pk_managed = {"private_key_managed": request.param.pop("private_key_managed")}
+        pk_managed["private_key_managed"]["name"] = pk_tgt
+        exp_key = pk_tgt
+    cert_args.update(request.param)
+    cert_args.update(pk_managed)
+    cert_args["certificate_managed"].update(cert_managed_params)
+    ret = ssh_salt_ssh_cli.run("state.apply", "cert", pillar={"args": cert_args})
+    assert ret.returncode == 0
+    cert = _get_cert(cert_args["name"])
+    assert cert.key_id == b"from_signing_policy"
+    assert _signed_by(cert, ca_key)
+    assert _belongs_to(cert, exp_key)
+    yield cert_args["name"]
+
+
+def test_certificate_managed_remote(ssh_salt_ssh_cli, cert_args, ca_key, rsa_privkey):
+    ret = ssh_salt_ssh_cli.run("state.apply", "cert", pillar={"args": cert_args})
+    assert ret.returncode == 0
+    cert = _get_cert(cert_args["name"])
+    assert cert.key_id == b"from_signing_policy"
+    assert _signed_by(cert, ca_key)
+    assert _belongs_to(cert, rsa_privkey)
+
+
+def test_certificate_managed_remote_with_privkey_managed(
+    ssh_salt_ssh_cli, cert_args, tmp_path, ca_key
+):
+    pk_args = {"name": str(tmp_path / "newkey")}
+    cert_args["private_key_managed"] = pk_args
+    ret = ssh_salt_ssh_cli.run("state.apply", "cert", pillar={"args": cert_args})
+    assert ret.returncode == 0
+    cert = _get_cert(cert_args["name"])
+    privkey = _get_privkey(pk_args["name"])
+    assert cert.key_id == b"from_signing_policy"
+    assert _signed_by(cert, ca_key)
+    assert _belongs_to(cert, privkey)
+    assert ret.data
+    assert len(ret.data) == 4
+    for state in ret.data:
+        # file.managed creates the files before moving data into them
+        assert ret.data[state]["changes"]
+
+
+@pytest.mark.usefixtures("existing_cert")
+def test_certificate_managed_remote_no_changes(
+    ssh_salt_ssh_cli, cert_args, ca_key, rsa_privkey
+):
+    ret = ssh_salt_ssh_cli.run("state.apply", "cert", pillar={"args": cert_args})
+    assert ret.returncode == 0
+    assert ret.data[next(iter(ret.data))]["changes"] == {}
+
+
+@pytest.mark.usefixtures("existing_cert")
+@pytest.mark.parametrize("existing_cert", ({"private_key_managed": {}},), indirect=True)
+def test_certificate_managed_remote_no_changes_with_privkey_managed(
+    ssh_salt_ssh_cli, cert_args, ca_key, pk_tgt
+):
+    privkey = _get_privkey(pk_tgt)
+    ret = ssh_salt_ssh_cli.run("state.apply", "cert", pillar={"args": cert_args})
+    assert ret.returncode == 0
+    cert = _get_cert(cert_args["name"])
+    assert cert.key_id == b"from_signing_policy"
+    assert _signed_by(cert, ca_key)
+    assert _belongs_to(cert, privkey)
+    assert ret.data
+    assert len(ret.data) == 4
+    for state in ret.data:
+        assert ret.data[state]["changes"] == {}
+
+
+@pytest.mark.usefixtures("existing_cert")
+def test_certificate_managed_remote_policy_change(
+    ssh_salt_ssh_cli, cert_args, ca_key, rsa_privkey
+):
+    cert_args["signing_policy"] = "testchangepolicy"
+    ret = ssh_salt_ssh_cli.run("state.apply", "cert", pillar={"args": cert_args})
+    assert ret.returncode == 0
+    assert "key_id" in ret.data[next(iter(ret.data))]["changes"]
+    cert = _get_cert(cert_args["name"])
+    assert cert.key_id == b"from_changed_signing_policy"
+
+
+@pytest.mark.usefixtures("existing_cert")
+@pytest.mark.parametrize("existing_cert", ({"private_key_managed": {}},), indirect=True)
+def test_certificate_managed_remote_policy_change_with_privkey_managed(
+    ssh_salt_ssh_cli, cert_args, ca_key, pk_tgt
+):
+    privkey = _get_privkey(pk_tgt)
+    cert_args["signing_policy"] = "testchangepolicy"
+    ret = ssh_salt_ssh_cli.run("state.apply", "cert", pillar={"args": cert_args})
+    assert ret.returncode == 0
+    cert = _get_cert(cert_args["name"])
+    assert cert.key_id == b"from_changed_signing_policy"
+    assert _signed_by(cert, ca_key)
+    assert _belongs_to(cert, privkey)
+    assert ret.data
+    assert len(ret.data) == 4
+    for state in ret.data:
+        if state.startswith("ssh_pki"):
+            if state.endswith("private_key_managed_ssh"):
+                assert not ret.data[state]["changes"]
+            else:
+                assert "key_id" in ret.data[state]["changes"]
+        else:
+            # file sub state runs
+            assert not ret.data[state]["changes"]
+
+
+@pytest.mark.usefixtures("existing_cert")
+@pytest.mark.parametrize(
+    "existing_cert", ({"private_key_managed": {"new": True}},), indirect=True
+)
+def test_certificate_managed_remote_policy_change_with_privkey_managed_new(
+    ssh_salt_ssh_cli, cert_args, ca_key, pk_tgt
+):
+    privkey = _get_privkey(pk_tgt)
+    cert_args["signing_policy"] = "testchangepolicy"
+    ret = ssh_salt_ssh_cli.run("state.apply", "cert", pillar={"args": cert_args})
+    assert ret.returncode == 0
+    cert = _get_cert(cert_args["name"])
+    assert cert.key_id == b"from_changed_signing_policy"
+    assert _signed_by(cert, ca_key)
+    assert not _belongs_to(cert, privkey)
+    new_privkey = _get_privkey(pk_tgt)
+    assert _belongs_to(cert, new_privkey)
+    assert ret.data
+    assert len(ret.data) == 4
+    for state in ret.data:
+        if state.startswith("ssh_pki"):
+            if state.endswith("private_key_managed_ssh"):
+                assert ret.data[state]["changes"]
+            else:
+                assert "key_id" in ret.data[state]["changes"]
+        else:
+            # file sub state runs
+            assert not ret.data[state]["changes"]
+
+
+@pytest.mark.usefixtures("existing_cert")
+def test_certificate_managed_remote_signing_key_change(
+    ssh_salt_ssh_cli, cert_args, ca_key, rsa_privkey
+):
+    cert_args["signing_policy"] = "testchangecapolicy"
+    ret = ssh_salt_ssh_cli.run("state.apply", "cert", pillar={"args": cert_args})
+    assert ret.returncode == 0
+    assert ret.data
+    changes = ret.data[next(iter(ret.data))]["changes"]
+    assert changes
+    assert "signing_private_key" in changes
+
+
+@pytest.mark.usefixtures("existing_cert")
+def test_certificate_managed_remote_no_changes_signing_policy_override(
+    ssh_salt_ssh_cli, cert_args, ca_key, rsa_privkey
+):
+    cert_args["extensions"] = {"permit-user-rc": True}
+    cert_args["critical_options"] = {"force-command": "rm -rf /"}
+    cert_args["cert_type"] = "host"
+    ret = ssh_salt_ssh_cli.run("state.apply", "cert", pillar={"args": cert_args})
+    assert ret.returncode == 0
+    assert ret.data[next(iter(ret.data))]["changes"] == {}
+
+
+@pytest.mark.usefixtures("existing_cert")
+def test_certificate_managed_remote_renew(ssh_salt_ssh_cli, cert_args):
+    cert_cur = _get_cert(cert_args["name"])
+    cert_args["certificate_managed"]["ttl_remaining"] = "999d"
+    ret = ssh_salt_ssh_cli.run("state.apply", "cert", pillar={"args": cert_args})
+    assert ret.returncode == 0
+    cert_new = _get_cert(cert_args["name"])
+    assert cert_new.serial != cert_cur.serial
+
+
+@pytest.mark.usefixtures("other_backend")
+def test_certificate_managed_different_backend(
+    ssh_salt_ssh_cli, cert_args, rsa_privkey, ca_key, cert_exts
+):
+    cert_args["backend"] = "other_backend"
+    cert_args["backend_args"] = {"donotfail": True}
+    ret = ssh_salt_ssh_cli.run("state.apply", "cert", pillar={"args": cert_args})
+    assert ret.returncode == 0
+    changes = ret.data[next(iter(ret.data))]["changes"]
+    assert changes
+    assert changes.get("created") == cert_args["name"]
+    cert = _get_cert(cert_args["name"])
+    assert cert.public_bytes().decode().strip() == cert_exts
+
+
+@pytest.mark.usefixtures("other_backend")
+@pytest.mark.usefixtures("existing_cert")
+def test_certificate_managed_existing_different_backend(
+    ssh_salt_ssh_cli, cert_args, rsa_privkey, ca_key, cert_exts
+):
+    cert_args.pop("ca_server", None)
+    cert_args["backend"] = "other_backend"
+    cert_args["backend_args"] = {"donotfail": True}
+    ret = ssh_salt_ssh_cli.run("state.apply", "cert", pillar={"args": cert_args})
+    assert ret.returncode == 0
+    changes = ret.data[next(iter(ret.data))]["changes"]
+    assert changes
+    assert not changes["extensions"]["added"]
+    assert not changes["extensions"]["changed"]
+    assert set(changes["extensions"]["removed"]) == {
+        "permit-port-forwarding",
+        "permit-pty",
+    }
+    assert set(changes["principals"]["added"]) == {"salt", "stack"}
+    assert set(changes["principals"]["removed"]) == {"from_signing_policy"}
+    assert changes["key_id"] == {"old": "from_signing_policy", "new": "saltstacktest"}
+    assert "critical_options" not in changes
+    cert = _get_cert(cert_args["name"])
+    assert cert.public_bytes().decode().strip() == cert_exts
+
+
+def _belongs_to(cert_or_pubkey, privkey):
+    if isinstance(cert_or_pubkey, serialization.SSHCertificate):
+        cert_or_pubkey = cert_or_pubkey.public_key()
+    return x509util.is_pair(cert_or_pubkey, _get_privkey(privkey))
+
+
+def _signed_by(cert, privkey):
+    cert.verify_cert_signature()
+    return x509util.is_pair(cert.signature_key(), _get_privkey(privkey))
+
+
+def _get_cert(cert):
+    try:
+        p = Path(cert)
+        if p.exists():
+            cert = p.read_bytes()
+    except Exception:  # pylint: disable=broad-except
+        pass
+    if isinstance(cert, str):
+        cert = cert.encode()
+    ret = serialization.load_ssh_public_identity(cert)
+    if not isinstance(ret, serialization.SSHCertificate):
+        raise ValueError(f"Expected SSHCertificate, got {ret.__class__.__name__}")
+    return ret
+
+
+def _get_privkey(pk, passphrase=None):
+    if hasattr(pk, "private_bytes"):
+        return pk
+    try:
+        p = Path(pk)
+        if p.exists():
+            pk = p.read_bytes()
+        else:
+            pk = pk.encode()
+    except Exception:  # pylint: disable=broad-except
+        pass
+    if isinstance(pk, str):
+        pk = pk.encode()
+    if passphrase is not None:
+        passphrase = passphrase.encode()
+
+    return serialization.load_ssh_private_key(pk, password=passphrase)
---
+++ tests/pytests/integration/ssh/ssh_pki/test_create_certificate.py
@@ -0,0 +1,289 @@
+import copy
+import logging
+from pathlib import Path
+
+import pytest
+
+from salt.defaults.exitcodes import EX_AGGREGATE
+
+try:
+    import cryptography
+    from cryptography.hazmat.primitives import serialization
+
+    import salt.utils.x509 as x509util
+
+    HAS_LIBS = True
+except ImportError:
+    HAS_LIBS = False
+
+CRYPTOGRAPHY_VERSION = tuple(int(x) for x in cryptography.__version__.split("."))
+
+log = logging.getLogger(__name__)
+
+
+pytestmark = [
+    pytest.mark.slow_test,
+    pytest.mark.skipif(HAS_LIBS is False, reason="Needs cryptography library"),
+]
+
+
+@pytest.fixture
+def cert_args(ca_minion_id, sshpki_data):
+    return {
+        "ca_server": ca_minion_id,
+        "signing_policy": "testhostpolicy",
+        "private_key": str(sshpki_data),
+        "key_id": "from_args",
+    }
+
+
+@pytest.fixture
+def cert_arg_exts():
+    return {
+        "permit-X11-forwarding": True,
+        "permit-agent-forwarding": True,
+        "permit-port-forwarding": True,
+        "permit-pty": True,
+        "permit-user-rc": False,
+    }
+
+
+@pytest.fixture
+def cert_expected_exts():
+    return {
+        b"permit-X11-forwarding": b"",
+        b"permit-agent-forwarding": b"",
+        b"permit-port-forwarding": b"",
+        b"permit-pty": b"",
+    }
+
+
+@pytest.fixture
+def cert_arg_opts():
+    return {
+        "force-command": "echo hi",
+        "no-port-forwarding": True,
+        "verify-required": False,
+    }
+
+
+@pytest.fixture
+def cert_expected_opts():
+    return {
+        b"force-command": b"echo hi",
+        b"no-port-forwarding": b"",
+    }
+
+
+def test_sign_remote_certificate(ssh_salt_ssh_cli, cert_args, ca_key, rsa_privkey):
+    ret = ssh_salt_ssh_cli.run("ssh_pki.create_certificate", **cert_args)
+    assert ret.returncode == 0
+    assert ret.data
+    cert = _get_cert(ret.data)
+    assert cert.key_id == b"from_signing_policy"
+    assert _signed_by(cert, ca_key)
+    assert _belongs_to(cert, rsa_privkey)
+
+
+def test_sign_remote_certificate_match(
+    ssh_salt_ssh_cli, cert_args, ca_key, rsa_privkey
+):
+    cert_args["signing_policy"] = "testmatchpolicy"
+    ret = ssh_salt_ssh_cli.run("ssh_pki.create_certificate", **cert_args)
+    assert ret.returncode == 0
+    assert ret.data
+    cert = _get_cert(ret.data)
+    assert cert.key_id == b"from_matching_policy"
+    assert _signed_by(cert, ca_key)
+    assert _belongs_to(cert, rsa_privkey)
+
+
+# Compound matching fails since the SSH minion data is not cached as usual.
+# We cannot check though since the expression is only present on the CA minion.
+
+
+def test_sign_remote_certificate_enc(ssh_salt_ssh_cli, cert_args, ca_key, rsa_privkey):
+    cert_args["private_key"] += "_enc"
+    cert_args["private_key_passphrase"] = "hunter1"
+    ret = ssh_salt_ssh_cli.run("ssh_pki.create_certificate", **cert_args)
+    if ret.returncode != 0 and "Need bcrypt module" in ret.data:
+        pytest.skip("Test needs bcrypt module in the global Python PATH")
+    assert ret.returncode == 0
+    assert ret.data
+    cert = _get_cert(ret.data)
+    assert cert.key_id == b"from_signing_policy"
+    assert _signed_by(cert, ca_key)
+    assert _belongs_to(cert, rsa_privkey)
+
+
+def test_sign_remote_certificate_ca_enc(
+    ssh_salt_ssh_cli, cert_args, ca_key, rsa_privkey
+):
+    cert_args["signing_policy"] = "testencpolicy"
+    ret = ssh_salt_ssh_cli.run("ssh_pki.create_certificate", **cert_args)
+    assert ret.returncode == 0
+    assert ret.data
+    cert = _get_cert(ret.data)
+    assert cert.key_id == b"from_signing_policy"
+    assert _signed_by(cert, ca_key)
+    assert _belongs_to(cert, rsa_privkey)
+
+
+def test_sign_remote_certificate_pubkey(
+    ssh_salt_ssh_cli, cert_args, sshpki_data, ca_key, rsa_privkey
+):
+    cert_args.pop("private_key")
+    cert_args["public_key"] = str(sshpki_data.parent / "key_pub")
+    ret = ssh_salt_ssh_cli.run("ssh_pki.create_certificate", **cert_args)
+    assert ret.returncode == 0
+    assert ret.data
+    cert = _get_cert(ret.data)
+    assert cert.key_id == b"from_signing_policy"
+    assert _signed_by(cert, ca_key)
+    assert _belongs_to(cert, rsa_privkey)
+
+
+def test_sign_remote_certificate_nonexistent_policy(ssh_salt_ssh_cli, cert_args):
+    cert_args["signing_policy"] = "missingpolicy"
+    ret = ssh_salt_ssh_cli.run("ssh_pki.create_certificate", **cert_args)
+    assert ret.returncode == EX_AGGREGATE
+    assert ret.data
+    assert "signing_policy must be specified and defined" in ret.data
+
+
+def test_sign_remote_certificate_disallowed_policy(ssh_salt_ssh_cli, cert_args):
+    cert_args["signing_policy"] = "testmatchfailpolicy"
+    ret = ssh_salt_ssh_cli.run("ssh_pki.create_certificate", **cert_args)
+    assert ret.returncode == EX_AGGREGATE
+    assert ret.data
+    assert "minion not permitted to use specified signing policy" in ret.data
+
+
+def test_get_signing_policy_remote(
+    ssh_salt_ssh_cli, cert_args, ca_minion_config, ca_pub
+):
+    testpolicy = copy.deepcopy(
+        ca_minion_config["ssh_signing_policies"]["testencpolicy"]
+    )
+    testpolicy.pop("signing_private_key", None)
+    testpolicy.pop("signing_private_key_passphrase", None)
+    testpolicy["signing_public_key"] = ca_pub
+    ret = ssh_salt_ssh_cli.run(
+        "ssh_pki.get_signing_policy", "testencpolicy", ca_server=cert_args["ca_server"]
+    )
+    assert ret.returncode == 0
+    assert ret.data
+    assert ret.data == testpolicy
+
+
+def test_sign_remote_certificate_ext_opt_override(ssh_salt_ssh_cli, cert_args):
+    cert_args["signing_policy"] = "testuserpolicy"
+    cert_args["extensions"] = {"permit-user-rc": True, "no-touch-required": True}
+    cert_args["critical_options"] = {
+        "force-command": "rm -rf /",
+        "source-address": "1.3.3.7",
+        "verify-required": True,
+    }
+    ret = ssh_salt_ssh_cli.run("ssh_pki.create_certificate", **cert_args)
+    assert ret.returncode == 0
+    assert ret.data
+    cert = _get_cert(ret.data)
+    assert b"permit-user-rc" not in cert.extensions
+    assert b"no-touch-required" in cert.extensions
+    assert cert.critical_options[b"force-command"] == b"echo hi"
+    assert cert.critical_options[b"verify-required"] == b""
+    assert b"source-address" not in cert.critical_options
+
+
+@pytest.mark.parametrize(
+    "principals,expected", [(["a", "b"], {b"a", b"b"}), (["a", "d"], {b"a"})]
+)
+def test_sign_remote_certificate_principals_override(
+    ssh_salt_ssh_cli, cert_args, principals, expected
+):
+    """
+    Ensure valid_principals can only be a subset of the ones dictated
+    by the signing policy.
+    """
+    cert_args["signing_policy"] = "testprincipalspolicy"
+    cert_args["valid_principals"] = principals
+    ret = ssh_salt_ssh_cli.run("ssh_pki.create_certificate", **cert_args)
+    assert ret.returncode == 0
+    assert ret.data
+    cert = _get_cert(ret.data)
+    assert set(cert.valid_principals) == expected
+
+
+def test_sign_remote_certificate_all_principals_from_local_override(
+    ssh_salt_ssh_cli, cert_args
+):
+    """
+    Ensure requesting all principals to be valid when the policy
+    dictates a set results in the set from the signing policy to be valid.
+    """
+    cert_args["signing_policy"] = "testprincipalspolicy"
+    cert_args["all_principals"] = True
+    ret = ssh_salt_ssh_cli.run("ssh_pki.create_certificate", **cert_args)
+    assert ret.returncode == 0
+    assert ret.data
+    cert = _get_cert(ret.data)
+    assert set(cert.valid_principals) == {b"a", b"b", b"c"}
+
+
+def test_sign_remote_certificate_all_principals_on_remote_override(
+    ssh_salt_ssh_cli, cert_args
+):
+    """
+    Ensure requesting a set of principals to be valid when the policy
+    allows all results in only the set to be valid.
+    """
+    cert_args["signing_policy"] = "testallprincipalspolicy"
+    cert_args["valid_principals"] = ["a"]
+    ret = ssh_salt_ssh_cli.run("ssh_pki.create_certificate", **cert_args)
+    assert ret.returncode == 0
+    assert ret.data
+    cert = _get_cert(ret.data)
+    assert set(cert.valid_principals) == {b"a"}
+
+
+def _belongs_to(cert_or_pubkey, privkey):
+    if isinstance(cert_or_pubkey, serialization.SSHCertificate):
+        cert_or_pubkey = cert_or_pubkey.public_key()
+    return x509util.is_pair(cert_or_pubkey, _get_privkey(privkey))
+
+
+def _signed_by(cert, privkey):
+    cert.verify_cert_signature()
+    return x509util.is_pair(cert.signature_key(), _get_privkey(privkey))
+
+
+def _get_cert(cert):
+    try:
+        p = Path(cert)
+        if p.exists():
+            cert = p.read_bytes()
+    except Exception:  # pylint: disable=broad-except
+        pass
+    if isinstance(cert, str):
+        cert = cert.encode()
+    ret = serialization.load_ssh_public_identity(cert)
+    if not isinstance(ret, serialization.SSHCertificate):
+        raise ValueError(f"Expected SSHCertificate, got {ret.__class__.__name__}")
+    return ret
+
+
+def _get_privkey(pk, passphrase=None):
+    try:
+        p = Path(pk)
+        if p.exists():
+            pk = p.read_bytes()
+        else:
+            pk = pk.encode()
+    except Exception:  # pylint: disable=broad-except
+        pass
+    if isinstance(pk, str):
+        pk = pk.encode()
+    if passphrase is not None:
+        passphrase = passphrase.encode()
+
+    return serialization.load_ssh_private_key(pk, password=passphrase)
