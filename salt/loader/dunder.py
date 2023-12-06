3140aecd2ba4cd9564000b1c5c54ea8d4dd510fa
Daniel A. Wozniak <dwozniak@vmware.com>
2023-12-06 16:59:38 -0700

    Add more loader dunders to cp module.
    Using cp module to dip our feet into more explicit loader dunders. Most
    all of the existing dunders can be imported already. The one outlier
    being used by the cp module is __opts__.

--- salt/loader/dunder.py
+++ salt/loader/dunder.py
@@ -8,3 +8,6 @@ loader_context = salt.loader.context.LoaderContext()
 
 
 __file_client__ = loader_context.named_context("__file_client__", default=None)
+__context__ = loader_context.named_context("__context__")
+__pillar__ = loader_context.named_context("__pillar__")
+__grains__ = loader_context.named_context("__grains__")
