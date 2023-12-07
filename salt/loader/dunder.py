806e2d524cd736704275cbbcd8ea9095dbe49321
Daniel A. Wozniak <dwozniak@vmware.com>
2023-12-07 15:43:57 -0700

    Add tests for opts dunder

--- salt/loader/dunder.py
+++ salt/loader/dunder.py
@@ -6,6 +6,7 @@ import salt.loader.context
 loader_context = salt.loader.context.LoaderContext()
 
 
+__opts__ = loader_context.named_context("__opts__")
 __context__ = loader_context.named_context("__context__")
 __pillar__ = loader_context.named_context("__pillar__")
 __grains__ = loader_context.named_context("__grains__")
