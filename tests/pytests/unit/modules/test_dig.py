731cea121ae7829f64f430519fbf663fa976851a
Frode Gundersen <frogunder@gmail.com>
2023-12-12 14:19:06 -0700

    fix lint error

--- tests/pytests/unit/modules/test_dig.py
+++ tests/pytests/unit/modules/test_dig.py
@@ -78,9 +78,7 @@ def test_check_ip():
 
 
 def test_check_ip_ipv6():
-    assert dig.check_ip(
-        "1111:2222:3333:4444:5555:6666:7777:8888"
-    )
+    assert dig.check_ip("1111:2222:3333:4444:5555:6666:7777:8888")
 
 
 def test_check_ip_ipv6_valid():
