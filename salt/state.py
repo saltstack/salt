0121e4e7ab9b3fdb1bb01c52da8630246159af93
Lee Clemens <sftw@leeclemens.net>
2023-12-30 19:17:46 -0500

    Handle None/empty and numeric SLS modules listed in include declarations

--- salt/state.py
+++ salt/state.py
@@ -4422,6 +4422,18 @@ class BaseHighState:
                         errors.append(msg)
                         continue
 
+                    if not hasattr(inc_sls, "startswith"):
+                        if inc_sls is None:
+                            msg = (
+                                "Empty include found in include "
+                                "in SLS '{}:{}'".format(saltenv, sls)
+                            )
+                            log.error(msg)
+                            errors.append(msg)
+                            continue
+                        else:
+                            inc_sls = str(inc_sls)
+
                     if inc_sls.startswith("."):
                         match = re.match(r"^(\.+)(.*)$", inc_sls)
                         if match:
