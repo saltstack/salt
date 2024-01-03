8905594525b9820f21e67d6f9a5f6398142556b2
Daniel A. Wozniak <dwozniak@vmware.com>
2024-01-03 14:30:56 -0700

    Fix pre-commit

--- tests/pytests/functional/modules/state/test_state.py
+++ tests/pytests/functional/modules/state/test_state.py
@@ -1101,6 +1101,6 @@ def test_state_requires_missing(state, state_tree):
     """
     with pytest.helpers.temp_file("req_any_missing.sls", sls_contents, state_tree):
         ret = state.sls("req_any_missing")
-        state_id = "cmd_|-changing_state_|-echo \"Changed!\"_|-run"
+        state_id = 'cmd_|-changing_state_|-echo "Changed!"_|-run'
         assert state_id in ret
         assert ret[state_id]["result"] is True
