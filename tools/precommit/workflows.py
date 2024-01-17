12f5483b6bbbe1a6e69447e2216500822bdb89a7
Pedro Algarvio <palgarvio@vmware.com>
2024-01-17 06:04:09 +0000

    Fix RPM build repository for aarch64
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- tools/precommit/workflows.py
+++ tools/precommit/workflows.py
@@ -252,6 +252,17 @@ def generate_workflows(ctx: Context):
         for _, arch, _ in test_salt_pkg_downloads_listing[platform]:
             test_salt_pkg_downloads_needs_slugs.add("build-ci-deps")
 
+    build_rpms_listing = []
+    for distro, releases in (
+        ("amazon", ("2", "2023")),
+        ("redhat", ("7", "8", "9")),
+        ("fedora", ("36", "37", "38")),
+        ("photon", ("3", "4", "5")),
+    ):
+        for release in releases:
+            for arch in ("x86_64", "arm64", "aarch64"):
+                build_rpms_listing.append((distro, release, arch))
+
     env = Environment(
         block_start_string="<%",
         block_end_string="%>",
@@ -293,6 +304,7 @@ def generate_workflows(ctx: Context):
             "test_salt_pkg_downloads_needs_slugs": sorted(
                 test_salt_pkg_downloads_needs_slugs
             ),
+            "build_rpms_listing": build_rpms_listing,
         }
         shared_context_file = (
             tools.utils.REPO_ROOT / "cicd" / "shared-gh-workflows-context.yml"
