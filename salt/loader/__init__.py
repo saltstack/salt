a14e899ffb4206717b1ff9dbefcdf05c1d9bbf51
Max Arnold <arnold.maxim@gmail.com>
2024-01-30 18:54:52 +0700

    Use the "features" option directly, add test

--- salt/loader/__init__.py
+++ salt/loader/__init__.py
@@ -28,7 +28,6 @@ import salt.utils.platform
 import salt.utils.stringutils
 import salt.utils.versions
 from salt.exceptions import LoaderError
-from salt.features import features
 from salt.template import check_render_pipe_str
 from salt.utils import entrypoints
 
@@ -252,10 +251,12 @@ def _module_dirs(
         if os.path.isdir(maybe_dir):
             cli_module_dirs.insert(0, maybe_dir)
 
-    if features.get("enable_deprecated_module_search_path_priority", False):
+    if opts.get("features", {}).get(
+        "enable_deprecated_module_search_path_priority", False
+    ):
         salt.utils.versions.warn_until(
             3008,
-            "The old module search path priority will be removed in Salt Argon. "
+            "The old module search path priority will be removed in Salt 3008. "
             "For more information see https://github.com/saltstack/salt/pull/65938.",
         )
         return cli_module_dirs + ext_type_types + ext_types + sys_types
