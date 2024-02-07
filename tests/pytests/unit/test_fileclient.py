4d899a134358f007fcec1c291f87f6a3eb34f84d
Erik Johnson <erjohnso@akamai.com>
2024-02-07 07:29:20 -0600

    Rename test functions to specify that they are testing FSClient
    The original draft of these tests was class-based, with the class name
    describing that the tests were for FSClient. With the class removed,
    this should be in the test name.

--- tests/pytests/unit/test_fileclient.py
+++ tests/pytests/unit/test_fileclient.py
@@ -5,7 +5,7 @@ import salt.fileclient as fileclient
 from tests.support.mock import MagicMock, patch
 
 
-def test_master_no_fs_update(master_opts):
+def test_fsclient_master_no_fs_update(master_opts):
     """
     Test that an FSClient spawned from the master does not cause fileserver
     backends to be refreshed on instantiation. The master already has the
@@ -18,7 +18,7 @@ def test_master_no_fs_update(master_opts):
         assert client.channel.fs.update.call_count == 0
 
 
-def test_masterless_fs_update(minion_opts):
+def test_fsclient_masterless_fs_update(minion_opts):
     """
     Test that an FSClient spawned from a masterless run refreshes the
     fileserver backends. This is necessary to ensure that a masterless run
