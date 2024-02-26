e9122b1d7c5d85ce851e186822518a63472acbfa
Pedro Algarvio <pedro@algarvio.me>
2024-02-26 16:05:15 +0000

    Skip salt-ssh related tests on Fedora 39
    The problem is that the tornado we ship is not prepared for Python 3.12,
    and it imports `ssl` and checks if the `match_hostname` function is defined, which
    has been deprecated since Python 3.7, so, the logic goes into trying to import
    backports.ssl-match-hostname which is not installed on the system.

--- tests/pytests/integration/netapi/test_ssh_client.py
+++ tests/pytests/integration/netapi/test_ssh_client.py
@@ -8,6 +8,14 @@ from tests.support.mock import patch
 pytestmark = [
     pytest.mark.slow_test,
     pytest.mark.requires_sshd_server,
+    pytest.mark.skipif(
+        "grains['osfinger'] == 'Fedora Linux-39'",
+        reason="Fedora 39 ships with Python 3.12. Test can't run with system Python on 3.12"
+        # Actually, the problem is that the tornado we ship is not prepared for Python 3.12,
+        # and it imports `ssl` and checks if the `match_hostname` function is defined, which
+        # has been deprecated since Python 3.7, so, the logic goes into trying to import
+        # backports.ssl-match-hostname which is not installed on the system.
+    ),
 ]
 
 
