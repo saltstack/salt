18461a5a2bb2b4e74adc9d114875c5249e9feb7a
Shane Lee <shane.d.lee@gmail.com>
2024-03-14 14:31:30 -0600

    Make the tests run on Windows

--- tests/conftest.py
+++ tests/conftest.py
@@ -448,8 +448,7 @@ def pytest_collection_modifyitems(config, items):
     groups_collection_modifyitems(config, items)
     from_filenames_collection_modifyitems(config, items)
 
-    log.warning("Mofifying collected tests to keep track of fixture usage")
-
+    log.warning("Modifying collected tests to keep track of fixture usage")
     timeout_marker_tests_paths = (
         str(PYTESTS_DIR / "pkg"),
         str(PYTESTS_DIR / "scenarios"),
