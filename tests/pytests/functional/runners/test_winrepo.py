36e23f87ab5ab3c05d8ad37503ea95c464e5bd4a
Pedro Algarvio <pedro@algarvio.me>
2024-04-05 10:37:30 +0100

    Revert "Keep directory structure the same on Master and Masterless"
    This reverts commit e7b735cdacc1f715e3d92cc7f880cb0858040af1.

--- tests/pytests/functional/runners/test_winrepo.py
+++ tests/pytests/functional/runners/test_winrepo.py
@@ -1,5 +1,3 @@
-import os
-
 import pytest
 
 from salt.runners import winrepo
@@ -12,21 +10,27 @@ pytestmark = [
 
 @pytest.fixture
 def configure_loader_modules(minion_opts, tmp_path):
+    opts = minion_opts.copy()
     winrepo_dir = tmp_path / "winrepo"
     winrepo_dir.mkdir()
     winrepo_dir_ng = tmp_path / "winrepo_ng"
     winrepo_dir_ng.mkdir()
-    minion_opts["winrepo_dir"] = str(winrepo_dir)
-    minion_opts["winrepo_dir_ng"] = str(winrepo_dir_ng)
-    return {winrepo: {"__opts__": minion_opts}}
+    opts["winrepo_dir"] = str(winrepo_dir)
+    opts["winrepo_dir_ng"] = str(winrepo_dir_ng)
+
+    return {
+        winrepo: {
+            "__opts__": opts,
+        }
+    }
 
 
 @pytest.fixture
 def winrepo_remotes(minion_opts):
-    remotes = set()
-    remotes.update(minion_opts.get("winrepo_remotes", []))
-    remotes.update(minion_opts.get("winrepo_remotes_ng", []))
-    return remotes
+    winrepo_remotes = minion_opts.get("winrepo_remotes", [])
+    winrepo_remotes_ng = minion_opts.get("winrepo_remotes_ng", [])
+    winrepo_remotes.extend(winrepo_remotes_ng)
+    return winrepo_remotes
 
 
 def test_update_git_repos(winrepo_remotes):
@@ -34,18 +38,15 @@ def test_update_git_repos(winrepo_remotes):
     Ensure update git repos works as intended.
     """
     res = winrepo.update_git_repos()
+
     assert res
 
     for remote in winrepo_remotes:
         assert remote in res
         assert res[remote]
 
-        # Make sure there are package definitions in the root
-        pkg_def = os.path.join(res[remote], "7zip.sls")
-        assert os.path.exists(pkg_def)
 
-
-def test_legacy_update_git_repos(winrepo_remotes, minion_opts):
+def test_legacy_update_git_repos(winrepo_remotes):
     """
     Ensure update git repos works as intended with legacy (non-gitfs) code.
     """
@@ -57,15 +58,3 @@ def test_legacy_update_git_repos(winrepo_remotes, minion_opts):
         for remote in winrepo_remotes:
             assert remote in res
             assert res[remote]
-
-            # Make sure there are package definitions in the root
-            # We have to look up the actual repo dir here because the legacy
-            # update only returns True or False, not a path
-            if "-ng" in remote:
-                path = minion_opts["winrepo_dir_ng"]
-                pkg_def = os.path.join(path, "salt-winrepo-ng", "7zip.sls")
-            else:
-                path = minion_opts["winrepo_dir"]
-                pkg_def = os.path.join(path, "salt-winrepo", "7zip.sls")
-
-            assert os.path.exists(pkg_def)
