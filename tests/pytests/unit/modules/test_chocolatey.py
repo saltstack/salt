4bc3e4ba9a189d3a6d5ccfe5168b2d9ba2edc85d
Shane Lee <shane.d.lee@gmail.com>
2024-04-10 13:23:16 -0600

    Make sure dict keys are lowercase

--- tests/pytests/unit/modules/test_chocolatey.py
+++ tests/pytests/unit/modules/test_chocolatey.py
@@ -208,6 +208,20 @@ def test_version_check_remote_true():
         assert result == expected
 
 
+def test_version_check_remote_true_capitalization():
+    """
+    Test version when remote is True
+    """
+    list_side_effect = [
+        {"Ack": ["3.1.1"]},
+        {"Ack": ["3.1.1"], "Wolfpack": ["3.0.17"], "blackbird": ["1.0.79.3"]},
+    ]
+    with patch.object(chocolatey, "list_", side_effect=list_side_effect):
+        expected = {"ack": {"available": ["3.1.1"], "installed": ["3.1.1"]}}
+        result = chocolatey.version("ack", check_remote=True)
+        assert result == expected
+
+
 def test_version_check_remote_true_not_available():
     """
     Test version when remote is True but remote version is unavailable
