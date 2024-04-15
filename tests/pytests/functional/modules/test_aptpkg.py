88b08f6d8d37d26b8630a19ce0ab2d5b915eea40
Pedro Algarvio <pedro@algarvio.me>
2024-04-15 16:24:38 +0100

    Revert "Fix another test using __opts__"
    This reverts commit f3ab61b069742289ffa9483ef9a8ef42ddb1bced.

--- tests/pytests/functional/modules/test_aptpkg.py
+++ tests/pytests/functional/modules/test_aptpkg.py
@@ -13,7 +13,6 @@ import salt.modules.file as file
 import salt.modules.gpg as gpg
 import salt.utils.files
 import salt.utils.stringutils
-from salt.loader.dunder import __opts__
 from tests.support.mock import Mock, patch
 
 pytestmark = [
@@ -77,7 +76,7 @@ def configure_loader_modules(minion_opts):
         },
         gpg: {},
         cp: {
-            "__opts__": __opts__.with_default(minion_opts),
+            "__opts__": minion_opts,
         },
         config: {
             "__opts__": minion_opts,
