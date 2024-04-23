e7d1197acbc049fba4cdbc20ec95cc6877be4fe4
Daniel A. Wozniak <daniel.wozniak@broadcom.com>
2024-04-23 15:50:25 -0700

    Update docs for master dns changes
    Update docs to use master_alive_interval to detect master ip changes via
    DNS.

--- tests/pytests/scenarios/dns/multimaster/test_dns.py
+++ tests/pytests/scenarios/dns/multimaster/test_dns.py
@@ -9,7 +9,12 @@ log = logging.getLogger(__name__)
 
 @pytest.mark.skip_unless_on_linux
 def test_multimaster_dns(
-    salt_mm_master_1, salt_mm_minion_1, mm_master_1_salt_cli, etc_hosts, caplog
+    salt_mm_master_1,
+    salt_mm_minion_1,
+    mm_master_1_salt_cli,
+    etc_hosts,
+    caplog,
+    master_alive_interval,
 ):
     """
     Verify a minion configured with multimaster hot/hot will pick up a master's
@@ -26,13 +31,15 @@ def test_multimaster_dns(
         with caplog.at_level(logging.INFO):
             ret = mm_master_1_salt_cli.run("test.ping", minion_tgt="mm-minion-1")
             assert ret.returncode == 0
-            log.info("Removing secondary master IP address.")
             etc_hosts.write_text(
                 f"{etc_hosts.orig_text}\n127.0.0.1    master1.local master2.local"
             )
-            subprocess.check_output(["ip", "addr", "del", "172.16.0.1/32", "dev", "lo"])
             log.info("Changed hosts record for master1.local and master2.local")
-            time.sleep(15)
+            subprocess.check_output(["ip", "addr", "del", "172.16.0.1/32", "dev", "lo"])
+            log.info("Removed secondary master IP address.")
+            # Wait for the minion's master_alive_interval, adding a second for
+            # reliablity.
+            time.sleep(master_alive_interval + 1)
             assert (
                 "Master ip address changed from 172.16.0.1 to 127.0.0.1" in caplog.text
             )
