a1b4aa80ab11590f2c4368c366543f8b63b0aebe
David Murphy <dmurphy@saltstack.com>
2024-05-02 15:39:03 -0600

    Updated preinst and initial downgrade and upgrade salt-master tests

--- tests/pytests/pkg/upgrade/test_salt_upgrade.py
+++ tests/pytests/pkg/upgrade/test_salt_upgrade.py
@@ -21,9 +21,9 @@ def _get_running_salt_minion_pid(process_name):
     return pids
 
 
-def test_salt_upgrade(salt_call_cli, install_salt):
+def test_salt_upgrade_minion(salt_call_cli, install_salt):
     """
-    Test an upgrade of Salt.
+    Test an upgrade of Salt Minion.
     """
     if install_salt.relenv:
         original_py_version = install_salt.package_python_version()
@@ -84,3 +84,60 @@ def test_salt_upgrade(salt_call_cli, install_salt):
             # test pip install after an upgrade
             use_lib = salt_call_cli.run("--local", "github.get_repo_info", repo)
             assert "Authentication information could" in use_lib.stderr
+
+
+@pytest.mark.skip_unless_on_linux(reason="Only supported on Linux family")
+def test_salt_upgrade_master(salt_cli, install_salt):
+    """
+    Test an upgrade of Salt Master.
+    """
+    if not install_salt.upgrade:
+        pytest.skip("Not testing an upgrade, do not run")
+
+    if install_salt.relenv:
+        original_py_version = install_salt.package_python_version()
+
+    # Verify previous install version is setup correctly and works
+    ret = salt_cli.run("--version")
+    assert ret.returncode == 0
+    assert packaging.version.parse(ret.data) < packaging.version.parse(
+        install_salt.artifact_version
+    )
+
+    # Verify there is a running minion by getting its PID
+    salt_name = "salt"
+    process_name = "salt-master"
+
+    old_pid = []
+
+    # psutil process name only returning first part of the command '/opt/saltstack/'
+    # need to check all of command line for salt-master
+    # ['/opt/saltstack/salt/bin/python3.10 /usr/bin/salt-master EventPublisher']
+    # and psutil is only returning the salt-minion once
+    for proc in psutil.process_iter():
+        if salt_name in proc.name():
+            cmdl_strg = " ".join(str(element) for element in proc.cmdline())
+            if process_name in cmdl_strg:
+                old_pid.append(proc.pid)
+
+    assert old_pid
+
+    # Upgrade Salt from previous version and test
+    install_salt.install(upgrade=True)
+    ret = salt_cli.run("--version")
+    assert ret.returncode == 0
+    assert packaging.version.parse(ret.data) == packaging.version.parse(
+        install_salt.artifact_version
+    )
+
+    # Verify there is a new running master by getting its PID and comparing it
+    # with the PID from before the upgrade
+    new_pid = []
+    for proc in psutil.process_iter():
+        if salt_name in proc.name():
+            cmdl_strg = " ".join(str(element) for element in proc.cmdline())
+            if process_name in cmdl_strg:
+                new_pid.append(proc.pid)
+
+    assert new_pid
+    assert new_pid != old_pid
