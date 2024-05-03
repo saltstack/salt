dff783e97178b667336e198206abb0bc148039a8
Pedro Algarvio <pedro@algarvio.me>
2024-05-03 16:18:46 +0100

    Small improvements to the formulas tests

--- tests/pytests/functional/formulas/conftest.py
+++ tests/pytests/functional/formulas/conftest.py
@@ -1,16 +1,28 @@
 import shutil
+import zipfile
 
 import pytest
 
-from tests.support.pytest.formulas import SaltStackFormula
+
+@pytest.fixture(scope="module")
+def formula():
+    pytest.fail("The module scoped 'formula' fixture should have been overridden.")
 
 
 @pytest.fixture(scope="module")
-def saltstack_formula(tmp_path_factory, base_env_state_tree_root_dir):
-    zipfiles_dir = tmp_path_factory.mktemp("fomulas-zips")
+def modules(loaders, formula, tmp_path_factory, base_env_state_tree_root_dir):
+    url = f"https://github.com/saltstack-formulas/{formula.name}/archive/refs/tags/v{formula.tag}.zip"
+    zipfiles_dir = tmp_path_factory.mktemp(f"unzipped-{formula.name}-{formula.tag}")
     try:
-        yield SaltStackFormula.with_default_paths(
-            zipfiles_dir, base_env_state_tree_root_dir
+        target_path = base_env_state_tree_root_dir / f"{formula.name}-{formula.tag}"
+        zipfile_path = pytest.helpers.download_file(
+            url, zipfiles_dir / url.split("/")[-1]
         )
+        with zipfile.ZipFile(zipfile_path) as zip_obj:
+            zip_obj.extractall(zipfiles_dir)
+        shutil.move(zipfiles_dir / f"{formula.name}-{formula.tag}", target_path)
+
+        loaders.opts["file_roots"]["base"].append(str(target_path))
+        yield loaders.modules
     finally:
         shutil.rmtree(zipfiles_dir, ignore_errors=True)
