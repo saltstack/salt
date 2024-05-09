457dfa6f230974b4353aa8dd1dc5f45490990dfd
ScriptAutomate <derek@icanteven.io>
2024-05-09 12:33:55 -0500

    Add Fedora 40 and Ubuntu 24.04 support

--- tests/integration/cli/test_custom_module.py
+++ tests/integration/cli/test_custom_module.py
@@ -35,8 +35,8 @@ from tests.support.case import SSHCase
 
 pytestmark = [
     pytest.mark.skipif(
-        "grains['osfinger'] == 'Fedora Linux-39'",
-        reason="Fedora 39 ships with Python 3.12. Test can't run with system Python on 3.12",
+        "grains['osfinger'] == 'Fedora Linux-40'",
+        reason="Fedora 40 ships with Python 3.12. Test can't run with system Python on 3.12",
         # Actually, the problem is that the tornado we ship is not prepared for Python 3.12,
         # and it imports `ssl` and checks if the `match_hostname` function is defined, which
         # has been deprecated since Python 3.7, so, the logic goes into trying to import
