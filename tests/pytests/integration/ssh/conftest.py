602e05e97061e96ec2da1e55a00660f26a1e60eb
ScriptAutomate <derek@icanteven.io>
2024-05-09 12:33:55 -0500

    Add Fedora 40 and Ubuntu 24.04 support

--- tests/pytests/integration/ssh/conftest.py
+++ tests/pytests/integration/ssh/conftest.py
@@ -4,10 +4,10 @@ from tests.support.pytest.helpers import reap_stray_processes
 
 
 @pytest.fixture(scope="package", autouse=True)
-def _auto_skip_on_fedora_39(grains):
-    if grains["osfinger"] == "Fedora Linux-39":
+def _auto_skip_on_fedora_40(grains):
+    if grains["osfinger"] == "Fedora Linux-40":
         pytest.skip(
-            "Fedora 39 ships with Python 3.12. Test can't run with system Python on 3.12"
+            "Fedora 40 ships with Python 3.12. Test can't run with system Python on 3.12"
             # Actually, the problem is that the tornado we ship is not prepared for Python 3.12,
             # and it imports `ssl` and checks if the `match_hostname` function is defined, which
             # has been deprecated since Python 3.7, so, the logic goes into trying to import
