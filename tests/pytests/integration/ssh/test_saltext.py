481038ebfc370dfd8ad5b58627007b70cd81ae1a
jeanluc <lkubb@protonmail.com>
2024-05-16 23:27:36 +0200

    Ensure namespace packages and submodule entrypoints work

--- tests/pytests/integration/ssh/test_saltext.py
+++ tests/pytests/integration/ssh/test_saltext.py
@@ -17,25 +17,41 @@ def salt_extension(tmp_path_factory):
 
 
 @pytest.fixture(scope="module")
-def other_salt_extension(tmp_path_factory):
+def namespaced_salt_extension(tmp_path_factory):
     with FakeSaltExtension(
         tmp_path_factory=tmp_path_factory,
-        name="salt-ext2-ssh-test",
+        name="saltext.ssh-test2",
         virtualname="barbaz",
     ) as extension:
         yield extension
 
 
 @pytest.fixture(scope="module")
-def venv(tmp_path_factory, salt_extension, other_salt_extension):
+def namespaced_salt_extension_2(tmp_path_factory):
+    with FakeSaltExtension(
+        tmp_path_factory=tmp_path_factory,
+        name="saltext.ssh-test3",
+        virtualname="wut",
+    ) as extension:
+        yield extension
+
+
+@pytest.fixture(scope="module")
+def venv(
+    tmp_path_factory,
+    salt_extension,
+    namespaced_salt_extension,
+    namespaced_salt_extension_2,
+):
     venv_dir = tmp_path_factory.mktemp("saltext-ssh-test-venv")
+    saltexts = (salt_extension, namespaced_salt_extension, namespaced_salt_extension_2)
     try:
         with SaltVirtualEnv(venv_dir=venv_dir) as _venv:
-            _venv.install(str(salt_extension.srcdir))
-            _venv.install(str(other_salt_extension.srcdir))
+            for saltext in saltexts:
+                _venv.install(str(saltext.srcdir))
             installed_packages = _venv.get_installed_packages()
-            assert salt_extension.name in installed_packages
-            assert other_salt_extension.name in installed_packages
+            for saltext in saltexts:
+                assert saltext.name in installed_packages
             yield _venv
     finally:
         shutil.rmtree(venv_dir, ignore_errors=True)
@@ -67,8 +83,8 @@ def args(venv, salt_master, salt_ssh_roster_file, sshd_config_dir):
     "saltext_conf",
     (
         {},
-        {"thin_saltext_allowlist": ["salt-ext-ssh-test"]},
-        {"thin_saltext_blocklist": ["salt-ext2-ssh-test"]},
+        {"thin_saltext_allowlist": ["salt-ext-ssh-test", "saltext.ssh-test3"]},
+        {"thin_saltext_blocklist": ["saltext.ssh-test2"]},
     ),
     indirect=True,
 )
@@ -87,6 +103,9 @@ def test_saltexts_are_available_on_target(venv, args, saltext_conf):
     else:
         assert res.returncode > 0
         assert "'barbaz.echo1' is not available" in res.stdout
+    ext3_args = args + ["wut.echo1", "wat"]
+    res = venv.run(*ext3_args, check=True)
+    assert res.stdout == "localhost:\n    wat\n"
 
 
 @pytest.mark.usefixtures("saltext_conf")
@@ -94,7 +113,7 @@ def test_saltexts_are_available_on_target(venv, args, saltext_conf):
     "saltext_conf", ({"thin_exclude_saltexts": True},), indirect=True
 )
 def test_saltexts_can_be_excluded(venv, args):
-    for ext in ("foobar", "barbaz"):
+    for ext in ("foobar", "barbaz", "wut"):
         ext_args = args + [f"{ext}.echo1", "foo"]
         res = venv.run(*ext_args, check=False)
         assert res.returncode > 0
