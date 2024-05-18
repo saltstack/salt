f23ca06537e10924ba3d766880735faa00cea980
Daniel A. Wozniak <daniel.wozniak@broadcom.com>
2024-05-18 00:25:15 -0700

    Bump cryptography version to fix FIPS support

--- tools/pkg/build.py
+++ tools/pkg/build.py
@@ -494,7 +494,6 @@ def onedir_dependencies(
         assert python_version
         assert relenv_version
     os.environ["RELENV_FETCH_VERSION"] = relenv_version
-    os.environ["RELENV_BUILDENV"] = "1"
 
     # We import relenv here because it is not a hard requirement for the rest of the tools commands
     try:
