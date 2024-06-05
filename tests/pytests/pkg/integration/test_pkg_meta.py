580d8b28514edfaf24a4a7c84d355580a7dad334
Daniel A. Wozniak <daniel.wozniak@broadcom.com>
2024-06-05 01:18:11 -0700

    Skip test on upgrade and downgrade

--- tests/pytests/pkg/integration/test_pkg_meta.py
+++ tests/pytests/pkg/integration/test_pkg_meta.py
@@ -31,6 +31,8 @@ def package(version, pkg_arch):
 
 @pytest.mark.skipif(not salt.utils.path.which("rpm"), reason="rpm is not installed")
 def test_provides(install_salt, package, version, provides_arch):
+    if install_salt.downgrade or install_salt.upgrade:
+        pytest.skip("Only test on install pkg tests")
     if install_salt.distro_id not in (
         "almalinux",
         "rocky",
@@ -62,6 +64,8 @@ def test_provides(install_salt, package, version, provides_arch):
 
 @pytest.mark.skipif(not salt.utils.path.which("rpm"), reason="rpm is not installed")
 def test_requires(install_salt, package, version):
+    if install_salt.downgrade or install_salt.upgrade:
+        pytest.skip("Only test on install pkg tests")
     if install_salt.distro_id not in (
         "almalinux",
         "rocky",
