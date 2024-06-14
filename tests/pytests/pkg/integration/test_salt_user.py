8520087bf7d32c140664a507b400bc8e917651bf
David Murphy <damurphy@vmware.com>
2024-06-14 10:11:40 -0600

    Backing out changes to discover cause of FileNotFoundError messages

--- tests/pytests/pkg/integration/test_salt_user.py
+++ tests/pytests/pkg/integration/test_salt_user.py
@@ -10,6 +10,13 @@ from saltfactories.utils.tempfiles import temp_directory
 
 pytestmark = [
     pytest.mark.skip_unless_on_linux,
+    pytest.mark.skipif(
+        True,
+        reason=(
+            "Package permissions are getting reworked in "
+            "https://github.com/saltstack/salt/pull/66218"
+        ),
+    ),
 ]
 
 
