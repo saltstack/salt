0fcde710627b8a29ff48de542fe9f88905add3ee
Shane Lee <shane.d.lee@gmail.com>
2024-07-17 14:47:14 -0600

    Fixes cmd.run with requisites on Windows
    Formats the command properly for powershell
    Adds changelog and tests

--- salt/modules/cmdmod.py
+++ salt/modules/cmdmod.py
@@ -290,7 +290,11 @@ def _prep_powershell_cmd(win_shell, cmd, encoded_cmd):
         # Strip whitespace
         if isinstance(cmd, list):
             cmd = " ".join(cmd)
-        new_cmd.extend(["-Command", f"& {{{cmd.strip()}}}"])
+
+        if cmd.startswith("$"):
+            new_cmd.extend(["-Command", f"{cmd.strip()}"])
+        else:
+            new_cmd.extend(["-Command", f"& {cmd.strip()}"])
 
     log.debug(new_cmd)
     return new_cmd
@@ -4104,6 +4108,7 @@ def powershell(
         cmd = salt.utils.stringutils.to_str(cmd)
         encoded_cmd = True
     else:
+        cmd = f"{{{cmd}}}"
         encoded_cmd = False
 
     # Retrieve the response, while overriding shell with 'powershell'
