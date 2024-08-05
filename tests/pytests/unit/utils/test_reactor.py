77ec118f0d0729e542a7c086ba935bf4449a93ba
vzhestkov <vzhestkov@suse.com>
2024-08-05 11:25:34 +0200

    Test if reactor processet mostly in separate thread

--- tests/pytests/unit/utils/test_reactor.py
+++ tests/pytests/unit/utils/test_reactor.py
@@ -1,6 +1,9 @@
+import threading
+
 import pytest
 
 import salt.utils.data
+import salt.utils.event
 import salt.utils.reactor as reactor
 from tests.support.mock import MagicMock, call, patch
 
@@ -55,3 +58,37 @@ def test_run_master_reactor(master_opts, master_reactor):
                 master_reactor.run()
                 calls = [call(9)]
                 os_nice_mock.assert_has_calls(calls)
+
+
+@pytest.mark.skip_on_windows(reason="Reactors unavailable on Windows")
+def test_reactors_list_in_separate_thread(master_opts, master_reactor):
+    """
+    Test that some of the tasks for reactors are performed in the separate thread
+    """
+
+    list_reactors_thread_id = None
+    get_reactions_thread_id = None
+
+    def list_reactors(*args, **kwargs):
+        nonlocal list_reactors_thread_id
+        list_reactors_thread_id = threading.get_ident()
+        return ["test_reactor"]
+
+    def get_reactions(*args, **kwargs):
+        nonlocal get_reactions_thread_id
+        get_reactions_thread_id = threading.get_ident()
+        return []
+
+    with patch.object(
+        salt.utils.event.SaltEvent,
+        "iter_events",
+        return_value=iter([{"tag": "test", "data": {}}]),
+    ), patch.object(master_reactor, "list_reactors", list_reactors), patch.object(
+        master_reactor, "reactions", get_reactions
+    ):
+        main_thread_id = threading.get_ident()
+        master_reactor.run()
+        assert main_thread_id != list_reactors_thread_id
+        assert main_thread_id != get_reactions_thread_id
+        assert list_reactors_thread_id == get_reactions_thread_id
+        assert None not in (list_reactors_thread_id, get_reactions_thread_id)
