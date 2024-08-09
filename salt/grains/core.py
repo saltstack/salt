ba39288d4be643225d6d2f37f941a530baabca8d
Gareth J. Greenaway <gareth@saltstack.com>
2024-08-09 15:19:32 -0700

    Ensure Manjaro ARM reports the correct os_family of Arch.

--- salt/grains/core.py
+++ salt/grains/core.py
@@ -1867,6 +1867,7 @@ _OS_FAMILY_MAP = {
     "SLES_SAP": "Suse",
     "Arch ARM": "Arch",
     "Manjaro": "Arch",
+    "Manjaro ARM": "Arch",
     "Antergos": "Arch",
     "EndeavourOS": "Arch",
     "ALT": "RedHat",
