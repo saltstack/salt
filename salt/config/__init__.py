b6859d869389a6ece519119df5ebef94e46491f7
vzhestkov <vzhestkov@suse.com>
2024-08-13 13:56:35 +0200

    Set master_tries default to -1

--- salt/config/__init__.py
+++ salt/config/__init__.py
@@ -77,7 +77,7 @@ elif salt.utils.platform.is_darwin():
 else:
     _DFLT_IPC_MODE = "ipc"
     _DFLT_FQDNS_GRAINS = False
-    _MASTER_TRIES = 1
+    _MASTER_TRIES = -1
     _MASTER_USER = salt.utils.user.get_user()
 
 
