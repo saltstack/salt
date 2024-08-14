9a6f5e8f4833d4e2374d14da9583a09cd76de15f
Daniel A. Wozniak <daniel.wozniak@broadcom.com>
2024-08-13 20:47:18 -0700

    Add transport layer ipc socket perms test

--- tests/pytests/integration/master/test_ipc_perms.py
+++ tests/pytests/integration/master/test_ipc_perms.py
@@ -5,5 +5,8 @@ import stat
 def test_master_event_pub_ipc_perms(salt_master):
     pub_path = pathlib.Path(salt_master.config["sock_dir"]) / "master_event_pub.ipc"
     assert pub_path.exists()
-    assert pub_path.stat().st_mode & stat.S_IRUSR
-    assert pub_path.stat().st_mode & stat.S_IRGRP
+    status = pub_path.stat()
+    assert status.st_mode & stat.S_IRUSR
+    assert status.st_mode & stat.S_IWUSR
+    assert status.st_mode & stat.S_IRGRP
+    assert status.st_mode & stat.S_IWGRP
