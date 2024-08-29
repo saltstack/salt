02be8b2ec9d3b7a66532f872e3f3dd651e4e1146
GrimAnEye <alfavitik97@gmail.com>
2024-08-29 17:14:15 +0700

    fix state output

--- salt/states/win_mslicense.py
+++ salt/states/win_mslicense.py
@@ -42,6 +42,9 @@ def present(name: str, kms_host="", kms_port=0, activate=False):
 
     ret = {"name": name[-5:], "result": None, "comment": "", "changes": {}}
 
+    def infoIsNone(t: tuple, k: str): return None if (
+        t[0] == None) else t[1][0][k]
+
     info = __salt__["mslicense.info"](name)
     if info[0] == False:
         log.error("error occurred while collecting data", info[1])
@@ -49,9 +52,6 @@ def present(name: str, kms_host="", kms_port=0, activate=False):
         ret["result"] = False
         ret["comment"] = "error occurred while collecting data"
         return ret
-
-    def infoIsNone(t: tuple, k: str): return None if (
-        t[0] == None) else t[1][0][k]
 
     if __opts__["test"]:
         ret["result"] = None
@@ -98,35 +98,23 @@ def present(name: str, kms_host="", kms_port=0, activate=False):
 
         test_install = __salt__["mslicense.installed"](name)
 
-        match test_install[0]:
-            case False:
-                log.error("product key check error", test_install[1])
+        if test_install[0] != True:
+            log.error("product key check error", test_install[1])
 
-                ret["result"] = False
-                ret["comment"] = "product key check error"
-                ret["changes"]["product key"] = {
-                    "old": infoIsNone(info, "PartialProductKey"),
-                    "new": "error: {0}".format(test_install[1])
-                }
-                return ret
+            ret["result"] = False
+            ret["comment"] = "product key check error"
+            ret["changes"]["product key"] = {
+                "old": infoIsNone(info, "PartialProductKey"),
+                "new": "error: {0}".format(test_install[1])
+            }
+            return ret
 
-            case None:
-                log.error("product key was not installed, result: None")
-
-                ret["result"] = False
-                ret["comment"] = "product key was not installed"
-                ret["changes"]["product key"] = {
-                    "old": infoIsNone(info, "PartialProductKey"),
-                    "new": "check failed, product key not found"
-                }
-                return ret
-
-            case True:
-                ret["comment"] = "{0} be present".format(name[-5:])
-                ret["changes"]["product key"] = {
-                    "old":  infoIsNone(info, "PartialProductKey"),
-                    "new": "XXXXX-XXXXX-XXXXX-XXXXX-{0}".format(name[-5:]),
-                }
+        else:
+            ret["comment"] = "{0} was present".format(name[-5:])
+            ret["changes"]["product key"] = {
+                "old":  infoIsNone(info, "PartialProductKey"),
+                "new": "XXXXX-XXXXX-XXXXX-XXXXX-{0}".format(name[-5:]),
+            }
 
     # If installation of kms host, port or activation is required,
     # update the license information
@@ -144,7 +132,7 @@ def present(name: str, kms_host="", kms_port=0, activate=False):
 
     if kms_host != "" and kms_host != info[1][0]["KeyManagementServiceMachine"]:
 
-        install = __salt__["mslicense.set_kms_host"](name)
+        install = __salt__["mslicense.set_kms_host"](kms_host, name)
         if install[0] != True:
             log.error("error when installing the kms-host: ", install[1])
 
@@ -157,47 +145,35 @@ def present(name: str, kms_host="", kms_port=0, activate=False):
             return ret
 
         test_install = __salt__["mslicense.get_kms_host"](name)
-        match test_install[0]:
-            case False:
-                log.error("kms-host check error", test_install[1])
+        if test_install[0] != True:
+            log.error("kms-host check error", test_install[1])
 
-                ret["result"] = False
-                ret["comment"] = "kms-host check error"
+            ret["result"] = False
+            ret["comment"] = "kms-host check error"
+            ret["changes"]["kms host"] = {
+                "old": info[1][0]["KeyManagementServiceMachine"],
+                "new": "error: {0}".format(test_install[1])
+            }
+            return ret
+
+        else:
+            if test_install[1] == kms_host:
                 ret["changes"]["kms host"] = {
                     "old": info[1][0]["KeyManagementServiceMachine"],
-                    "new": "error: {0}".format(test_install[1])
+                    "new": test_install[1]
                 }
-                return ret
-
-            case None:
-                log.error("kms host not be checked, result: None")
-
+            else:
                 ret["result"] = False
-                ret["comment"] = "kms host not be checked"
+                ret["comment"] = "kms host was not changed"
                 ret["changes"]["kms host"] = {
                     "old": info[1][0]["KeyManagementServiceMachine"],
-                    "new": "check failed, product not found"
+                    "checked": test_install[1]
                 }
                 return ret
-
-            case True:
-                if test_install[1] == kms_host:
-                    ret["changes"]["kms host"] = {
-                        "old": info[1][0]["KeyManagementServiceMachine"],
-                        "new": test_install[1]
-                    }
-                else:
-                    ret["result"] = False
-                    ret["comment"] = "kms host was not changed"
-                    ret["changes"]["kms host"] = {
-                        "old": info[1][0]["KeyManagementServiceMachine"],
-                        "checked": test_install[1]
-                    }
-                    return ret
 
     if kms_port != 0 and kms_port != info[1][0]["KeyManagementServicePort"]:
 
-        install = __salt__["mslicense.set_kms_port"](name)
+        install = __salt__["mslicense.set_kms_port"](kms_port, name)
         if install[0] != True:
             log.error("error when installing the kms-port", install[1])
 
@@ -210,43 +186,32 @@ def present(name: str, kms_host="", kms_port=0, activate=False):
             return ret
 
         test_install = __salt__["mslicense.get_kms_port"](name)
-        match test_install[0]:
-            case False:
-                log.error("kms-port check error: {0}".format(test_install[1]))
+        if test_install[0] != True:
 
-                ret["result"] = False
-                ret["comment"] = "kms port check error"
-                ret["changes"]["kms port"] = {
-                    "old":  info[1][0]["KeyManagementServicePort"],
-                    "new": "error: {0}".format(test_install[1])
-                }
-                return ret
+            log.error("kms-port check error: {0}".format(test_install[1]))
 
-            case None:
-                log.error("kms port not be installed, result: None")
+            ret["result"] = False
+            ret["comment"] = "kms port check error"
+            ret["changes"]["kms port"] = {
+                "old":  info[1][0]["KeyManagementServicePort"],
+                "new": "error: {0}".format(test_install[1])
+            }
+            return ret
 
-                ret["result"] = False
-                ret["comment"] = "kms port not be installed"
+        else:
+            if test_install[1] == kms_port:
                 ret["changes"]["kms port"] = {
                     "old": info[1][0]["KeyManagementServicePort"],
-                    "new": "check failed, kms port not found"
+                    "new": test_install[1]
+                }
+            else:
+                ret["result"] = False
+                ret["comment"] = "kms port was not changed"
+                ret["changes"]["kms port"] = {
+                    "old": info[1][0]["KeyManagementServiceMachine"],
+                    "checked": test_install[1]
                 }
                 return ret
-
-            case True:
-                if test_install[1] == kms_port:
-                    ret["changes"]["kms port"] = {
-                        "old": info[1][0]["KeyManagementServicePort"],
-                        "new": test_install[1]
-                    }
-                else:
-                    ret["result"] = False
-                    ret["comment"] = "kms port was not changed"
-                    ret["changes"]["kms port"] = {
-                        "old": info[1][0]["KeyManagementServiceMachine"],
-                        "checked": test_install[1]
-                    }
-                    return ret
 
     if activate and info[1][0]["LicenseStatus"] != 1:
         install = __salt__["mslicense.activate"](name)
@@ -262,43 +227,32 @@ def present(name: str, kms_host="", kms_port=0, activate=False):
             return ret
 
         test_install = __salt__["mslicense.license"](name)
-        match test_install[0]:
-            case False:
-                log.error("error when check product activate", test_install[1])
+        if test_install[0] != True:
 
-                ret["result"] = False
-                ret["comment"] = "error when check product activate"
+            log.error("error when check product activate", test_install[1])
+
+            ret["result"] = False
+            ret["comment"] = "error when check product activate"
+            ret["changes"]["activate"] = {
+                "old": info[1][0]["LicenseStatus"],
+                "new": "error: {0}".format(test_install[1])
+            }
+            return ret
+
+        else:
+            if test_install[1] == 1:
                 ret["changes"]["activate"] = {
                     "old": info[1][0]["LicenseStatus"],
-                    "new": "error: {0}".format(test_install[1])
+                    "new": test_install[1]
                 }
-                return ret
-
-            case None:
-                log.error("license status was not checked, result: None")
-
+            else:
                 ret["result"] = False
-                ret["comment"] = "license status was not checked"
+                ret["comment"] = "license status was not changed"
                 ret["changes"]["activate"] = {
                     "old": info[1][0]["LicenseStatus"],
-                    "new": "check failed, product not found"
+                    "checked": test_install[1]
                 }
                 return ret
-
-            case True:
-                if test_install[1] == 1:
-                    ret["changes"]["activate"] = {
-                        "old": info[1][0]["LicenseStatus"],
-                        "new": test_install[1]
-                    }
-                else:
-                    ret["result"] = False
-                    ret["comment"] = "license status was not changed"
-                    ret["changes"]["activate"] = {
-                        "old": info[1][0]["LicenseStatus"],
-                        "checked": test_install[1]
-                    }
-                    return ret
 
     ret["result"] = True
     return ret
@@ -320,7 +274,7 @@ def absent(name: str, all=False):
     if all:
         name = ""
 
-    info = __salt__["mslicense.info"](name)
+    info = __salt__["mslicense.info"](name[-5:])
     if info[0] == False:
         log.error("error occurred while collecting data", info[1])
 
@@ -333,22 +287,21 @@ def absent(name: str, all=False):
         ret["comment"] = "{0} would be absent".format(name[-5:])
 
         if info[0] == None:
-            ret["changes"]["uninstall product key"] = {
+            ret["changes"]["not uninstall product key"] = {
                 "old": "",
                 "new": ""
             }
         else:
             ret["changes"]["uninstall product key"] = {
-                "old": info[1][0]["PartialProductKey"],
+                "old": info[1],
                 "new": ""
             }
         return ret
 
-    if info[0] != None:
-        remove = __salt__["mslicense.uninstall"](name)
-        if remove[0] != True:
-            log.error(
-                "error when removing product key: {0}".format(remove[1]))
+    if info[0] == True:
+        remove = __salt__["mslicense.uninstall"](name[-5:])
+        if remove[0] == False:
+            log.error("error when removing product key", remove[1])
 
             ret["result"] = False
             ret["comment"] = "error when removing product key"
@@ -359,32 +312,36 @@ def absent(name: str, all=False):
             }
             return ret
 
-        test_remove = __salt__["mslicense.info"](name)
-        if test_remove[0] != False:
+        test_remove = __salt__["mslicense.info"](name[-5:])
+        if test_remove[0] == False:
             log.error("error occurred while collecting data", info[1])
 
             ret["result"] = False
             ret["comment"] = "error occurred while collecting data"
             return ret
 
-        if test_remove[0] != None:
-            log.error("product keys was not removed")
+        if test_remove[0] == True:
+            log.error("product keys was not removed", test_remove[1])
 
             ret["result"] = False
             ret["comment"] = "product kes(-s) not removed"
             ret["changes"]["absent key"] = {
                 "old": info[1],
-                "checked": "check failed, keys found"
+                "new": "check failed, keys found"
             }
             return ret
-
-    ret["changes"]["absent key"] = {
-        "old": info[1],
-        "new": ""
-    }
-    ret["comment"] = "keys has been removed"
-    ret["result"] = True
-    return ret
+        else:
+            ret["comment"] = "keys has been removed"
+            ret["result"] = True
+            ret["changes"]["absent key"] = {
+                "old": info[1],
+                "new": ""
+            }
+            return ret
+    else:
+        ret["comment"] = "keys not found"
+        ret["result"] = True
+        return ret
 
 
 def present_kms(name: str, kms_port=0, key=""):
@@ -398,6 +355,8 @@ def present_kms(name: str, kms_port=0, key=""):
     key
         The product key to which the KMS server is assigned.
     """
+
+    name = name.lower()
 
     ret = {"name": name, "result": None, "comment": "", "changes": {}}
 
@@ -415,8 +374,8 @@ def present_kms(name: str, kms_port=0, key=""):
     if kms_port != 0:
         info_port = __salt__["mslicense.get_kms_port"](key)
         if info_port[0] == False:
-            log.error("error occurred while collecting kms-port info",
-                      info_port[1])
+            log.error(
+                "error occurred while collecting kms-port info", info_port[1])
 
             ret["result"] = False
             ret["comment"] = "error occurred while collecting kms-port info"
@@ -425,30 +384,30 @@ def present_kms(name: str, kms_port=0, key=""):
     if __opts__["test"]:
         ret["result"] = None
 
-        if key != "":
-            ret["comment"] = "{0} would be present for product: XXXXX-XXXXX-XXXXX-XXXXX-{1}".format(
-                name, key[-5:])
-        else:
+        if key == "":
             ret["comment"] = "{0} would be present".format(name)
 
-        if info[0] == None:
-            ret["changes"]["host"] = {
-                "old": "kms installed: {0}".format(info[1]),
-                "new": "kms will be install: {0}".format(info[1])
+        else:
+            ret["comment"] = "{0} would be present for product: XXXXX-XXXXX-XXXXX-XXXXX-{1}".format(
+                name, key[-5:])
+
+        if (info[0] == None) or (info[1] != name):
+            ret["changes"]["kms host"] = {
+                "old": info[1],
+                "new": name,
             }
 
-        if kms_port != 0:
-            if info_port[0] == None:
-                ret["changes"]["port"] = {
-                    "old": "kms installed: {0}".format(info_port[1]),
-                    "new": "kms will be installed: {0}".format(kms_port)
-                }
+        if (kms_port != 0) and ((info_port[0] == None) or kms_port != info_port[1]):
+            ret["changes"]["kms port"] = {
+                "old": info_port[1],
+                "new": kms_port,
+            }
         return ret
 
-    # Если kms-сервер не установлен, выполняю его установку
+    # If the kms server is not installed, install it
     if name != info[1]:
         install = __salt__["mslicense.set_kms_host"](name, key)
-        if install[0] != True:
+        if install[0] == False:
             log.error("error when installing the kms-host: ", install[1])
             ret["result"] = False
             ret["comment"] = "error when installing the kms-host"
@@ -456,36 +415,54 @@ def present_kms(name: str, kms_port=0, key=""):
 
         test_install = __salt__["mslicense.get_kms_host"](key)
         if test_install[0] != True:
+
             log.error("error when check the kms-host", install[1])
+
             ret["result"] = False
             ret["comment"] = "error when check the kms-host"
+            ret["changes"]["kms host"] = {
+                "old": info[1],
+                "new": "error: {0}".format(test_install[1])
+            }
             return ret
 
-        ret["changes"]["host"] = {
-            "old": info[1],
-            "new": test_install[1],
-        }
+        else:
+            ret["comment"] = "kms-host was installed"
+            ret["changes"]["kms host"] = {
+                "old": info[1],
+                "new": test_install[1]
+            }
 
     if kms_port != 0 and kms_port != info_port[1]:
 
         install = __salt__["mslicense.set_kms_port"](kms_port, key)
-        if install[0] != True:
+        if install[0] == False:
             log.error("error when installing the kms-port", install[1])
+
             ret["result"] = False
             ret["comment"] = "error when installing the kms-port"
+            ret["changes"]["kms port"] = {
+                "old": info_port[1],
+                "new": "error when installing the kms-port: {0}".format(install[1])
+            }
             return ret
 
         test_install = __salt__["mslicense.get_kms_port"](key)
         if test_install[0] != True:
-            log.error("error when installing the kms-port", install[1])
+            log.error("error check the kms-port", install[1])
+
             ret["result"] = False
             ret["comment"] = "error when check the kms-port"
+            ret["changes"]["kms port"] = {
+                "old": info_port[1],
+                "new": "error kms port checking: {0}".format(test_install[0])
+            }
             return ret
-
-        ret["changes"]["port"] = {
-            "old": info_port[1],
-            "new": test_install[1],
-        }
+        else:
+            ret["changes"]["kms port"] = {
+                "old": info_port[1],
+                "new": test_install[1],
+            }
 
     ret["result"] = True
     return ret
