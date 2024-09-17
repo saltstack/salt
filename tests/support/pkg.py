a71bcfb734b21408f266506476fceb9ce2f695f4
Daniel A. Wozniak <daniel.wozniak@broadcom.com>
2024-09-17 15:03:49 -0700

    Do not install when no-install is passed

--- tests/support/pkg.py
+++ tests/support/pkg.py
@@ -1038,16 +1038,12 @@ class SaltPkgInstall:
     def __enter__(self):
         if platform.is_windows():
             self.update_process_path()
-
-        if not self.no_install:
-            if self.upgrade:
-                self.install_previous()
-            else:
-                # assume downgrade, since no_install only used in these two cases
-                self.install()
+        if self.no_install:
+            return self
+        if self.upgrade:
+            self.install_previous()
         else:
             self.install()
-
         return self
 
     def __exit__(self, *_):
