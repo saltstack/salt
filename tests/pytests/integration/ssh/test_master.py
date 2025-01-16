805d10969a5cb0d7c1aeeb189bd3823980cfbaa1
Daniel A. Wozniak <daniel.wozniak@broadcom.com>
2025-01-15 19:16:42 -0700

    Rocky 8 container does not have full systemd

--- tests/pytests/integration/ssh/test_master.py
+++ tests/pytests/integration/ssh/test_master.py
@@ -2,8 +2,12 @@
 Simple Smoke Tests for Connected SSH minions
 """
 
+import subprocess
+
 import pytest
 from saltfactories.utils.functional import StateResult
+
+import salt.utils.platform
 
 pytestmark = [
     pytest.mark.slow_test,
@@ -11,7 +15,21 @@ pytestmark = [
 ]
 
 
+def _check_systemctl():
+    if not hasattr(_check_systemctl, "memo"):
+        if not salt.utils.platform.is_linux():
+            _check_systemctl.memo = False
+        else:
+            proc = subprocess.run(["systemctl"], capture_output=True, check=False)
+            _check_systemctl.memo = (
+                b"Failed to get D-Bus connection: No such file or directory"
+                in proc.stderr
+            )
+    return _check_systemctl.memo
+
+
 @pytest.mark.skip_if_not_root
+@pytest.mark.skipif(_check_systemctl(), reason="systemctl degraded")
 def test_service(salt_ssh_cli, grains):
     service = "cron"
     os_family = grains["os_family"]
