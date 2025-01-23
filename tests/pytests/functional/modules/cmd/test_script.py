630f979214359d6d96da7d6223302fdfcf742791
Twangboy <shane.d.lee@gmail.com>
2025-01-23 13:42:00 -0700

    Display Windows Environment Variables

--- tests/pytests/functional/modules/cmd/test_script.py
+++ tests/pytests/functional/modules/cmd/test_script.py
@@ -61,6 +61,7 @@ def test_windows_script_args_powershell(cmd, shell, issue_56195):
 
     import_result = cmd.run("Import-Module Microsoft.PowerShell.Security", shell=shell)
     powershell_version = cmd.run("$PSVersionTable", shell=shell)
+    ps_module_path = cmd.run("$env:PSModulePath", shell=shell)
 
     assert ret["stdout"] == password
 
