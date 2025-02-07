93b3eb0965982dacb16bc96bb12a6b86fe7389d6
Steven Daniele <sdaniele@gatech.edu>
2025-02-07 12:55:03 -0500

    update transport factory to check for ssl support

--- salt/transport/tcp.py
+++ salt/transport/tcp.py
@@ -1299,6 +1299,9 @@ class PublishServer(salt.transport.base.DaemonizedPublishServer):
     Tornado based TCP PublishServer
     """
 
+    # Required from DaemonizedPublishServer
+    support_ssl = True
+
     # TODO: opts!
     # Based on default used in tornado.netutil.bind_sockets()
     backlog = 128
@@ -1320,8 +1323,8 @@ class PublishServer(salt.transport.base.DaemonizedPublishServer):
         pull_path=None,
         pull_path_perms=0o600,
         pub_path_perms=0o600,
-        ssl=None,
         started=None,
+        ssl=None,
     ):
         self.opts = opts
         self.pub_sock = None
