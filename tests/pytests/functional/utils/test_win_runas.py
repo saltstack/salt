1dcec1d25b8efdb253db4aefb3a9a87e643ada05
twangboy <shane.d.lee@gmail.com>
2025-03-07 15:01:50 -0700

    Handle integer usernames properly

--- tests/pytests/functional/utils/test_win_runas.py
+++ tests/pytests/functional/utils/test_win_runas.py
@@ -2,9 +2,14 @@
 Test the win_runas util
 """
 
+import re
+from random import randint
+
 import pytest
 
+import salt.modules.win_useradd as win_useradd
 import salt.utils.win_runas as win_runas
+from salt.modules.win_network import interfaces_names
 
 pytestmark = [
     pytest.mark.windows_whitelisted,
@@ -16,6 +21,24 @@ pytestmark = [
 def user():
     with pytest.helpers.create_account() as account:
         yield account
+
+
+@pytest.fixture
+def int_user():
+    with pytest.helpers.create_account() as account:
+        int_name = randint(10000, 99999)
+        win_useradd.rename(account.username, int_name)
+        account.username = int_name
+        yield account
+
+
+@pytest.fixture
+def configure_loader_modules(minion_opts):
+    return {
+        win_useradd: {
+            "__opts__": minion_opts,
+        },
+    }
 
 
 @pytest.mark.parametrize(
@@ -54,3 +77,31 @@ def test_compound_runas_unpriv(user, cmd, expected):
         password=user.password,
     )
     assert expected in result["stdout"]
+
+
+def test_runas_str_user(user):
+    result = win_runas.runas(
+        cmdLine="whoami", username=user.username, password=user.password
+    )
+    assert user.username in result["stdout"]
+
+
+def test_runas_int_user(int_user):
+    result = win_runas.runas(
+        cmdLine="whoami", username=int(int_user.username), password=int_user.password
+    )
+    assert str(int_user.username) in result["stdout"]
+
+
+def test_runas_unpriv_str_user(user):
+    result = win_runas.runas_unpriv(
+        cmd="whoami", username=user.username, password=user.password
+    )
+    assert user.username in result["stdout"]
+
+
+def test_runas_unpriv_int_user(int_user):
+    result = win_runas.runas_unpriv(
+        cmd="whoami", username=int(int_user.username), password=int_user.password
+    )
+    assert str(int_user.username) in result["stdout"]
