566ea1273a9309e0f7f3a75c146156b45dd2efe4
saville <saville@adobe.com>
2025-03-13 20:40:06 -0600

    Fix merge conflicts

--- salt/utils/yamldumper.py
+++ salt/utils/yamldumper.py
@@ -17,6 +17,7 @@ import salt.utils.context
 from salt.utils.datastructures import HashableOrderedDict
 from salt.utils.optsdict import DictProxy, ListProxy, OptsDict
 from salt.utils.secret import MaskedDict, MaskedList
+from salt.utils.dynamic_dict import DynamicDict
 
 try:
     from yaml import CDumper as Dumper
@@ -160,6 +161,14 @@ OrderedDumper.add_representer(
 )
 SafeOrderedDumper.add_representer(
     "tag:yaml.org,2002:timestamp", SafeOrderedDumper.represent_scalar
+)
+
+Dumper.add_representer(DynamicDict, Dumper.represent_dict)
+SafeDumper.add_representer(DynamicDict, SafeDumper.represent_dict)
+OrderedDumper.add_representer(DynamicDict, OrderedDumper.represent_dict)
+SafeOrderedDumper.add_representer(DynamicDict, SafeOrderedDumper.represent_dict)
+IndentedSafeOrderedDumper.add_representer(
+    DynamicDict, IndentedSafeOrderedDumper.represent_dict
 )
 
 
