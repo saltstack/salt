417c85e336bce0e28061c8324817b90e62465d51
Daniel A. Wozniak <daniel.wozniak@broadcom.com>
2025-03-18 14:51:19 -0700

    Downgrade must uninstall the newer msi

--- tests/support/pkg.py
+++ tests/support/pkg.py
@@ -795,9 +795,14 @@ class SaltPkgInstall:
 
             if self.file_ext == "msi":
 
-                # XXX: Both packages end up installed, should we uninstall the old one?
-                # ret = subprocess.run(f"msiexec.exe /qn /x {pkg} /norestart", shell=True)
-                # assert ret.returncode == 0
+                # MSI can not be downgraded, we must remove the newer version
+                # before installing the old one.
+                ret = subprocess.run(
+                    f"msiexec.exe /qn /x {pkg} /norestart",
+                    shell=True,  # nosec
+                    check=False,
+                )
+                assert ret.returncode == 0
 
                 # self.proc.run always makes the command a list even when shell
                 # is true, meaning shell being true will never work correctly.
