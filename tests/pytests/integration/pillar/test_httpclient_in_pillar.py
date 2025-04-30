339eb4c4cd0adbfb4957c1f7eb57c69184af654f
twangboy <shane.d.lee@gmail.com>
2025-04-30 15:39:47 -0600

    Fix the test_fileclient test

--- tests/pytests/integration/pillar/test_httpclient_in_pillar.py
+++ tests/pytests/integration/pillar/test_httpclient_in_pillar.py
@@ -11,19 +11,16 @@ def test_pillar_using_http_query(salt_master, salt_minion, salt_cli, tmp_path):
 
     with salt_master.pillar_tree.base.temp_file("top.sls", pillar_top):
         with salt_master.pillar_tree.base.temp_file("http_pillar_test.sls", my_pillar):
-            with salt_master.pillar_tree.base.temp_file(
-                "http_pillar_test.sls", my_pillar
-            ):
-                ret = salt_cli.run("state.apply", minion_tgt=salt_minion.id)
-                assert ret.returncode == 1
-                assert (
-                    ret.data["no_|-states_|-states_|-None"]["comment"]
-                    == "No states found for this minion"
-                )
+            ret = salt_cli.run("state.apply", minion_tgt=salt_minion.id)
+            assert ret.returncode == 1
+            assert (
+                ret.data["no_|-states_|-states_|-None"]["comment"]
+                == "No states found for this minion"
+            )
 
-                pillar_ret = salt_cli.run(
-                    "pillar.item", "http_query_test", minion_tgt=salt_minion.id
-                )
-                assert pillar_ret.returncode == 0
+            pillar_ret = salt_cli.run(
+                "pillar.item", "http_query_test", minion_tgt=salt_minion.id
+            )
+            assert pillar_ret.returncode == 0
 
-                assert '"http_query_test": 200' in pillar_ret.stdout
+            assert '"http_query_test": 200' in pillar_ret.stdout
