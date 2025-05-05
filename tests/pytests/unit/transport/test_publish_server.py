a61d2a9a1735310ffdf986a0d6f296c2fdbc40e4
Steven Daniele <sdaniele@gatech.edu>
2025-05-05 13:37:51 -0400

    all additiona parameters for __init__ for publishserver subclasses

--- tests/pytests/unit/transport/test_publish_server.py
+++ tests/pytests/unit/transport/test_publish_server.py
@@ -34,6 +34,15 @@ def test_transport_publishserver_has_method_signatures(kind, method):
     required_sig = inspect.signature(
         getattr(salt.transport.base.DaemonizedPublishServer, method)
     )
-    # This print "OK" when it errors, but you have to dig through the message to find the
-    # difference. It may make more sense to create a easier to digest datastructure (list/dict).
-    assert method_sig == required_sig
+    if method == '__init__':
+        # Treat init slightly differently. All other methods are generic and should have identical
+        # signatures. The __init__ function is called by the factory method which is expected to have
+        # knowledge of what parameters are available. Therefore we only check that the arguments
+        # found on the Base class are on the child class and ignore any extras.
+        method_params = list(method_sig.parameters.items())
+        required_params = list(required_sig.parameters.items())
+        assert method_params[:len(required_params)] == required_params
+    else:
+        # This print "OK" when it errors, but you have to dig through the message to find the
+        # difference. It may make more sense to create a easier to digest datastructure (list/dict).
+        assert method_sig == required_sig
