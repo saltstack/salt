ae7f1bed829b1fd119f99ceb30dcc06723f76eb7
twangboy <shane.d.lee@gmail.com>
2025-05-06 10:21:48 -0600

    Clean up tests

--- tests/pytests/integration/cli/conftest.py
+++ tests/pytests/integration/cli/conftest.py
@@ -2,7 +2,6 @@ import pytest
 
 
 @pytest.fixture(scope="package")
-@pytest.mark.core_test
 def salt_eauth_account(salt_eauth_account_factory):
     with salt_eauth_account_factory as account:
         yield account
