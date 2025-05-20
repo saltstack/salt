16e9a5d583d4dbe3871ab931561fd7017b95a533
Matt Phillips <mphillips81@bloomberg.net>
2025-05-20 15:03:15 -0400

    Provide token storage using the salt.cache interface
    Rather than implement custom drivers for tokens, the existing cache
    drivers can be leveraged for token management. This also moves token
    expiry responsibility to the token(cache) implementation with a naive
    fallback.
    
    Implicitly the default token backend will move to either what the global
    'cache' is set to, or, if overridden, the eauth_tokens.cache_driver
    opt. This means on upgrade, tokens will be invalidated. The only other
    token driver is 'rediscluster', which I've added a deprecation for,
    since cache.redis_cache provides the same functionality.

--- salt/pillar/__init__.py
+++ salt/pillar/__init__.py
@@ -1306,7 +1306,7 @@ class PillarCache(Pillar):
 
     def clear_pillar(self):
         """
-        Clea the pillar cache, if it exists
+        Clear the pillar cache, if it exists
         """
         return self.cache.flush("pillar", self.pillar_key)
 
