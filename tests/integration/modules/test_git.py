98a3d0f1d7b0a4e3f6801d206835111f203ebef3
twangboy <shane.d.lee@gmail.com>
2025-05-21 14:34:25 -0600

    Make test_commit work on Windows and Linux

--- tests/integration/modules/test_git.py
+++ tests/integration/modules/test_git.py
@@ -329,19 +329,21 @@ class GitModuleTest(ModuleCase):
         # Stage the file
         self.run_function("git.add", [self.repo, filename])
         # Commit the staged file
-        commit_msg = f'"Add a line to {filename}"'
-        ret = self.run_function("git.commit", [self.repo, commit_msg])
+        commit_msg = f"Add a line to {filename}"
+        ret = self.run_function("git.commit", [self.repo, f'"{commit_msg}"'])
         # Make sure the expected line is in the output
-        self.assertTrue(bool(re.search(commit_re_prefix + commit_msg, ret)))
+        self.assertTrue(bool(re.search(commit_re_prefix, ret)))
+        self.assertTrue(bool(commit_msg in ret))
         # Add another line
         with salt.utils.files.fopen(os.path.join(self.repo, filename), "a") as fp_:
             fp_.write("Added another line\n")
         # Commit the second file without staging
-        commit_msg = f'"Add another line to {filename}"'
+        commit_msg = f"Add another line to {filename}"
         ret = self.run_function(
-            "git.commit", [self.repo, commit_msg], filename=filename
+            "git.commit", [self.repo, f'"{commit_msg}"'], filename=filename
         )
-        self.assertTrue(bool(re.search(commit_re_prefix + commit_msg, ret)))
+        self.assertTrue(bool(re.search(commit_re_prefix, ret)))
+        self.assertTrue(bool(commit_msg in ret))
 
     @pytest.mark.slow_test
     def test_config(self):
