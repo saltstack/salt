0cd2ac522a00eb40389560bbc6d601247f9a026e
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2025-06-10 16:34:19 +0100

    Remove flaky tag and fix test

--- tests/pytests/functional/modules/state/test_state.py
+++ tests/pytests/functional/modules/state/test_state.py
@@ -775,7 +775,6 @@ def test_retry_option_success(state, state_tree, tmp_path):
 @pytest.mark.skip_on_windows(
     reason="Skipped until parallel states can be fixed on Windows"
 )
-@pytest.mark.xfail(reason="This test is flaky")
 def test_retry_option_success_parallel(state, state_tree, tmp_path):
     """
     test a state with the retry option that should return True immediately (i.e. no retries)
@@ -861,7 +860,6 @@ def test_retry_option_eventual_success(state, state_tree, tmp_path):
 @pytest.mark.skip_on_windows(
     reason="Skipped until parallel states can be fixed on Windows"
 )
-@pytest.mark.xfail(reason="This test is flaky")
 def test_retry_option_eventual_success_parallel(state, state_tree, tmp_path):
     """
     test a state with the retry option that should return True, eventually
@@ -905,7 +903,7 @@ def test_retry_option_eventual_success_parallel(state, state_tree, tmp_path):
         for state_return in ret:
             log.debug("=== state_return %s ===", state_return)
             assert state_return.result is True
-            assert state_return.full_return["duration"] > 4
+            assert state_return.full_return["duration"] > 2
             # It should not take 5 attempts
             assert "Attempt 5" not in state_return.comment
 
