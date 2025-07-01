3c90fe92b0c254ef53a799518cbb07196e41da82
Daniel A. Wozniak <daniel.wozniak@broadcom.com>
2025-07-01 01:45:17 -0700

    Package upgrade test refactor
    Refactor systemd upgrade tests to uninstall salt after each test. The
    fixtures run by test_upgrade_salt still use session scoped fixtures to
    ensure the integrations tests still pass.

--- tests/pytests/pkg/upgrade/systemd/test_service_preservation.py
+++ tests/pytests/pkg/upgrade/systemd/test_service_preservation.py
@@ -11,79 +11,81 @@ log = logging.getLogger(__name__)
 
 
 def test_salt_systemd_disabled_preservation(
-    salt_call_cli, install_salt, salt_systemd_setup
+    call_cli, install_salt_systemd, salt_systemd_setup
 ):
     """
     Test upgrade of Salt packages preserve disabled state of systemd
     """
-    if not install_salt.upgrade:
+    if not install_salt_systemd.upgrade:
         pytest.skip("Not testing an upgrade, do not run")
 
     # ensure known state, disabled
     test_list = ["salt-api", "salt-minion", "salt-master"]
     for test_item in test_list:
         test_cmd = f"systemctl disable {test_item}"
-        ret = salt_call_cli.run("--local", "cmd.run", test_cmd)
+        ret = call_cli.run("--local", "cmd.run", test_cmd)
         assert ret.returncode == 0
 
     # Upgrade Salt (inc. minion, master, etc.) from previous version and test
     # pylint: disable=pointless-statement
-    install_salt.install(upgrade=True)
+    install_salt_systemd.install(upgrade=True)
     time.sleep(60)  # give it some time
 
     # test for disabled systemd state
     test_list = ["salt-api", "salt-minion", "salt-master"]
     for test_item in test_list:
         test_cmd = f"systemctl show -p UnitFileState {test_item}"
-        ret = salt_call_cli.run("--local", "cmd.run", test_cmd)
+        ret = call_cli.run("--local", "cmd.run", test_cmd)
         test_enabled = ret.stdout.strip().split("=")[1].split('"')[0].strip()
         assert ret.returncode == 0
         assert test_enabled == "disabled"
 
 
 def test_salt_systemd_enabled_preservation(
-    salt_call_cli, install_salt, salt_systemd_setup
+    call_cli, install_salt_systemd, salt_systemd_setup
 ):
     """
     Test upgrade of Salt packages preserve enabled state of systemd
     """
-    if not install_salt.upgrade:
+    if not install_salt_systemd.upgrade:
         pytest.skip("Not testing an upgrade, do not run")
+    install_salt_systemd.no_uninstall = False
 
     # Upgrade Salt (inc. minion, master, etc.) from previous version and test
     # pylint: disable=pointless-statement
-    install_salt.install(upgrade=True)
+    install_salt_systemd.install(upgrade=True)
     time.sleep(10)  # give it some time
 
     # test for enabled systemd state
     test_list = ["salt-api", "salt-minion", "salt-master"]
     for test_item in test_list:
         test_cmd = f"systemctl show -p UnitFileState {test_item}"
-        ret = salt_call_cli.run("--local", "cmd.run", test_cmd)
+        ret = call_cli.run("--local", "cmd.run", test_cmd)
         test_enabled = ret.stdout.strip().split("=")[1].split('"')[0].strip()
         assert ret.returncode == 0
         assert test_enabled == "enabled"
 
 
 def test_salt_systemd_masked_preservation(
-    salt_call_cli, install_salt, salt_systemd_setup, salt_systemd_mask_services
+    call_cli, install_salt_systemd, salt_systemd_setup, salt_systemd_mask_services
 ):
     """
     Test upgrade of Salt packages preserves masked state of systemd services
     """
-    if not install_salt.upgrade:
+    if not install_salt_systemd.upgrade:
         pytest.skip("Not testing an upgrade, do not run")
+    install_salt_systemd.no_uninstall = False
 
     # Upgrade Salt (inc. minion, master, etc.) from previous version and test
     # pylint: disable=pointless-statement
-    install_salt.install(upgrade=True)
+    install_salt_systemd.install(upgrade=True)
     time.sleep(60)  # give it some time
 
     # test for masked systemd state
     test_list = ["salt-api", "salt-minion", "salt-master"]
     for test_item in test_list:
         test_cmd = f"systemctl show -p UnitFileState {test_item}"
-        ret = salt_call_cli.run("--local", "cmd.run", test_cmd)
+        ret = call_cli.run("--local", "cmd.run", test_cmd)
         test_masked = ret.stdout.strip().split("=")[1].split('"')[0].strip()
         assert ret.returncode == 0
         assert test_masked == "masked"
