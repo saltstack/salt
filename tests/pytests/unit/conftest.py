6d0af4b58ef53f1b865946c3e65a0b95efe366e7
Barney Sowood <barney@sowood.co.uk>
2025-07-25 11:51:42 +0100

    Set up short sock_dir path in test
    Sets up the short sock_dir path in the test. Previously setting it in
    conftest.py was breaking another test because I'd done it as a Path,
    not a string, but I want to avoid changin behaviour of other tests, so
    setting it locally to the test.

--- tests/pytests/unit/conftest.py
+++ tests/pytests/unit/conftest.py
@@ -27,7 +27,6 @@ def minion_opts(tmp_path):
     opts["fips_mode"] = FIPS_TESTRUN
     opts["encryption_algorithm"] = "OAEP-SHA224" if FIPS_TESTRUN else "OAEP-SHA1"
     opts["signing_algorithm"] = "PKCS1v15-SHA224" if FIPS_TESTRUN else "PKCS1v15-SHA1"
-    opts["sock_dir"] = tmp_path / "sock"
     return opts
 
 
