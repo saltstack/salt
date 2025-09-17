d50bc1846fa9ee770d7a93af9de6f88ab0de7587
twangboy <shane.d.lee@gmail.com>
2025-09-17 10:19:35 -0600

    Fix test_win tests

--- tests/pytests/unit/platform/test_win.py
+++ tests/pytests/unit/platform/test_win.py
@@ -14,26 +14,30 @@ pytestmark = [
 @pytest.mark.parametrize(
     "command, expected",
     [
-        ("whoami", "cmd.exe /c whoami"),
-        ("cmd /c hostname", "cmd.exe /c cmd /c hostname"),
-        ("echo foo", "cmd.exe /c echo foo"),
-        ('cmd /c "echo foo"', 'cmd.exe /c cmd /c "echo foo"'),
-        ("icacls 'C:\\Program Files'", "cmd.exe /c icacls 'C:\\Program Files'"),
+        ("whoami", 'cmd.exe /s /c "whoami"'),
+        ("cmd /c hostname", 'cmd.exe /s /c "cmd /c hostname"'),
+        ("echo foo", 'cmd.exe /s /c "echo foo"'),
+        ('cmd /c "echo foo"', 'cmd.exe /s /c "cmd /c "echo foo""'),
+        ('icacls "C:\\Program Files"', 'cmd.exe /s /c "icacls "C:\\Program Files""'),
         (
-            "icacls 'C:\\Program Files' && echo 1",
-            "cmd.exe /c icacls 'C:\\Program Files' && echo 1",
+            'icacls "C:\\Program Files" && echo 1',
+            'cmd.exe /s /c "icacls "C:\\Program Files" && echo 1"',
+        ),
+        (
+            ["icacls", "C:\\Program Files", "&&", "echo", "1"],
+            'cmd.exe /s /c "icacls "C:\\Program Files" && echo 1"',
         ),
         (
             ["secedit", "/export", "/cfg", "C:\\A Path\\with\\a\\space"],
-            'cmd.exe /c secedit /export /cfg "C:\\A Path\\with\\a\\space"',
+            'cmd.exe /s /c "secedit /export /cfg "C:\\A Path\\with\\a\\space""',
         ),
         (
             ["C:\\a space\\a space.bat", "foo foo", "bar bar"],
-            'cmd.exe /c "C:\\a space\\a space.bat" "foo foo" "bar bar"',
+            'cmd.exe /s /c ""C:\\a space\\a space.bat" "foo foo" "bar bar""',
         ),
         (
             '''echo "&<>[]|{}^=;!'+,`~ "''',
-            '''cmd.exe /c echo "&<>[]|{}^=;!'+,`~ "''',
+            '''cmd.exe /s /c "echo "&<>[]|{}^=;!'+,`~ ""''',
         ),
     ],
 )
