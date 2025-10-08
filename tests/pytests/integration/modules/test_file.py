7ddd48a221d712884b2d2d031e4ea26b51e20471
Bert Coleman <bert.coleman@broadcom.com>
2025-10-08 14:40:51 -0600

    initial commit

--- tests/pytests/integration/modules/test_file.py
+++ tests/pytests/integration/modules/test_file.py
@@ -6,6 +6,8 @@ import os
 
 import pytest
 
+import salt.utils.win_dacl
+
 
 @pytest.mark.parametrize("verify_ssl", [True, False])
 @pytest.mark.slow_test
@@ -197,3 +199,50 @@ def test_check_file_meta_verify_ssl(
         )
     else:
         assert "SSL: CERTIFICATE_VERIFY_FAILED" in ret.stderr
+
+
+IS_WINDOWS = salt.utils.platform.is_windows()
+
+
+@pytest.mark.slow_test
+def test_create_symlink_with_check_cmd(salt_call_cli, salt_master, tmp_path):
+    """
+    file.symlink test to make sure chk_command runs before
+    creating the symlink
+    """
+    name = tmp_path / "test-symlink"
+    name.mkdir()
+    if IS_WINDOWS:
+        principal = salt.utils.win_functions.get_current_user()
+        salt.utils.win_dacl.set_owner(obj_name=str(name), principal=principal)
+        salt.utils.win_dacl.set_inheritance(obj_name=str(name), enabled=True)
+    symlink_file = name / "symlink"
+    symlink_file.write_text("jaguar")
+
+    assert symlink_file.exists()
+    assert symlink_file.is_file()
+
+    assert name.exists()
+    assert name.is_dir()
+
+    sls_contents = """
+    {name}/testing:
+      file.symlink:
+        - target: {symlink_file}
+        - user: cbert
+        - group: staff
+        - makedirs: true
+        - check_cmd: grep 'jaguar' {symlink_file}
+    """.format(
+        name=name, symlink_file=symlink_file
+    )
+    sls_tempfile = salt_master.state_tree.base.temp_file(
+        "test_symlink.sls", sls_contents
+    )
+
+    with sls_tempfile:
+        ret = salt_call_cli.run("state.apply", "test_symlink")
+        assert symlink_file.exists()
+        assert symlink_file.read_text() == "jaguar"
+        expected_comment = "check_cmd determined the state succeeded"
+        assert expected_comment in ret.stdout
