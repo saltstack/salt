be7bdc49962b930a6b9378691fbceb4adfc17311
Daniel A. Wozniak <daniel.wozniak@broadcom.com>
2026-01-02 23:19:36 -0700

    Fix black formatting in earlier autoscale commits
    Format files from the original autoscale branch that weren't formatted with
    the correct black version.

--- salt/ext/saslprep.py
+++ salt/ext/saslprep.py
@@ -26,11 +26,14 @@ except ImportError:
         if isinstance(data, str):
             raise TypeError(
                 "The stringprep module is not available. Usernames and "
-                "passwords must be ASCII strings.")
+                "passwords must be ASCII strings."
+            )
         return data
+
 else:
     HAVE_STRINGPREP = True
     import unicodedata
+
     # RFC4013 section 2.3 prohibited output.
     _PROHIBITED = (
         # A strict reading of RFC 4013 requires table c12 here, but
@@ -44,7 +47,8 @@ else:
         stringprep.in_table_c6,
         stringprep.in_table_c7,
         stringprep.in_table_c8,
-        stringprep.in_table_c9)
+        stringprep.in_table_c9,
+    )
 
     def saslprep(data, prohibit_unassigned_code_points=True):
         """An implementation of RFC4013 SASLprep.
@@ -77,12 +81,16 @@ else:
         in_table_c12 = stringprep.in_table_c12
         in_table_b1 = stringprep.in_table_b1
         data = "".join(
-            ["\u0020" if in_table_c12(elt) else elt
-             for elt in data if not in_table_b1(elt)])
+            [
+                "\u0020" if in_table_c12(elt) else elt
+                for elt in data
+                if not in_table_b1(elt)
+            ]
+        )
 
         # RFC3454 section 2, step 2 - Normalize
         # RFC4013 section 2.2 normalization
-        data = unicodedata.ucd_3_2_0.normalize('NFKC', data)
+        data = unicodedata.ucd_3_2_0.normalize("NFKC", data)
 
         in_table_d1 = stringprep.in_table_d1
         if in_table_d1(data[0]):
@@ -103,7 +111,6 @@ else:
         # RFC3454 section 2, step 3 and 4 - Prohibit and check bidi
         for char in data:
             if any(in_table(char) for in_table in prohibited):
-                raise ValueError(
-                    "SASLprep: failed prohibited character check")
+                raise ValueError("SASLprep: failed prohibited character check")
 
         return data
