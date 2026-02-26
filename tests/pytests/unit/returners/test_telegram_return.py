dc10ae2d2547a03e9f20a1669d23827f21e5d58a
Arash Hatami <hatamiarash7@gmail.com>
2026-02-26 10:22:50 +0330

    tests: add thread_id for telegram

--- tests/pytests/unit/returners/test_telegram_return.py
+++ tests/pytests/unit/returners/test_telegram_return.py
@@ -19,20 +19,49 @@ def test_returner():
     """
     Test to see if the Telegram returner sends a message
     """
-    ret = {
+    ret: dict[str, str] = {
         "id": "12345",
         "fun": "mytest.func",
         "fun_args": "myfunc args",
         "jid": "54321",
         "return": "The room is on fire as shes fixing her hair",
     }
-    options = {"chat_id": "", "token": ""}
+    options: dict[str, str] = {"chat_id": "", "token": ""}
 
-    with patch(
-        "salt.returners.telegram_return._get_options",
-        MagicMock(return_value=options),
-    ), patch.dict(
-        "salt.returners.telegram_return.__salt__",
-        {"telegram.post_message": MagicMock(return_value=True)},
+    with (
+        patch(
+            "salt.returners.telegram_return._get_options",
+            MagicMock(return_value=options),
+        ),
+        patch.dict(
+            "salt.returners.telegram_return.__salt__",
+            {"telegram.post_message": MagicMock(return_value=True)},
+        ),
+    ):
+        assert telegram.returner(ret) is True
+
+
+def test_returner_topics():
+    """
+    Test to see if the Telegram returner sends a message to specific topic
+    """
+    ret: dict[str, str] = {
+        "id": "12345",
+        "fun": "mytest.func",
+        "fun_args": "myfunc args",
+        "jid": "54321",
+        "return": "The room is on fire as shes fixing her hair",
+    }
+    options: dict[str, str] = {"chat_id": "", "token": "", "thread_id": ""}
+
+    with (
+        patch(
+            "salt.returners.telegram_return._get_options",
+            MagicMock(return_value=options),
+        ),
+        patch.dict(
+            "salt.returners.telegram_return.__salt__",
+            {"telegram.post_message": MagicMock(return_value=True)},
+        ),
     ):
         assert telegram.returner(ret) is True
