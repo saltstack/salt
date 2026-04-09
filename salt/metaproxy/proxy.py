cafcdaf7ff5a4654c7ef4076463184460ddc2f45
Tyler Levy Conde <tyler.levy-conde@broadcom.com>
2026-04-09 11:28:51 -0600

    Fix minion blackout whitelist with wrapped pillar
    Unwrap minion_blackout_whitelist before comparing to function_name so
    SecretStr entries match str job names.

--- salt/metaproxy/proxy.py
+++ salt/metaproxy/proxy.py
@@ -38,6 +38,7 @@ import salt.utils.minions
 import salt.utils.network
 import salt.utils.platform
 import salt.utils.process
+import salt.utils.safepillar
 import salt.utils.schedule
 import salt.utils.ssdp
 import salt.utils.user
@@ -433,8 +434,8 @@ def thread_return(cls, minion_instance, opts, data):
             if minion_instance.connected and minion_instance.opts["pillar"].get(
                 "minion_blackout", False
             ):
-                whitelist = minion_instance.opts["pillar"].get(
-                    "minion_blackout_whitelist", []
+                whitelist = salt.utils.safepillar.unwrap_blackout_whitelist(
+                    minion_instance.opts["pillar"].get("minion_blackout_whitelist", [])
                 )
                 # this minion is blacked out. Only allow saltutil.refresh_pillar and the whitelist
                 if (
@@ -444,8 +445,8 @@ def thread_return(cls, minion_instance, opts, data):
                     minion_blackout_violation = True
             # use minion_blackout_whitelist from grains if it exists
             if minion_instance.opts["grains"].get("minion_blackout", False):
-                whitelist = minion_instance.opts["grains"].get(
-                    "minion_blackout_whitelist", []
+                whitelist = salt.utils.safepillar.unwrap_blackout_whitelist(
+                    minion_instance.opts["grains"].get("minion_blackout_whitelist", [])
                 )
                 if (
                     function_name != "saltutil.refresh_pillar"
@@ -668,8 +669,8 @@ def thread_multi_return(cls, minion_instance, opts, data):
             if minion_instance.connected and minion_instance.opts["pillar"].get(
                 "minion_blackout", False
             ):
-                whitelist = minion_instance.opts["pillar"].get(
-                    "minion_blackout_whitelist", []
+                whitelist = salt.utils.safepillar.unwrap_blackout_whitelist(
+                    minion_instance.opts["pillar"].get("minion_blackout_whitelist", [])
                 )
                 # this minion is blacked out. Only allow saltutil.refresh_pillar and the whitelist
                 if (
@@ -678,8 +679,8 @@ def thread_multi_return(cls, minion_instance, opts, data):
                 ):
                     minion_blackout_violation = True
             elif minion_instance.opts["grains"].get("minion_blackout", False):
-                whitelist = minion_instance.opts["grains"].get(
-                    "minion_blackout_whitelist", []
+                whitelist = salt.utils.safepillar.unwrap_blackout_whitelist(
+                    minion_instance.opts["grains"].get("minion_blackout_whitelist", [])
                 )
                 if (
                     data["fun"][ind] != "saltutil.refresh_pillar"
