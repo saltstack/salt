23bb5bce8a4295583312741c310e8b6d88b85641
Daniel A. Wozniak <dwozniak@broadcom.com>
2026-06-03 17:45:55 -0700

    Fix lint errors in monitoring scripts
    Address pylint warnings in analyze_stats.py and fd_exporter.py related
    to unused imports, broad exceptions, and resource management.

--- tests/monitoring/analyze_stats.py
+++ tests/monitoring/analyze_stats.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import json
 import sys
-import time
 import urllib.parse
 import urllib.request
 
--- tests/monitoring/srv/salt/fd_exporter.py
+++ tests/monitoring/srv/salt/fd_exporter.py
@@ -1,7 +1,6 @@
+# pylint: disable=resource-leakage
 import http.server
 import os
-import subprocess
-import time
 
 
 class FDHandler(http.server.BaseHTTPRequestHandler):
@@ -46,16 +45,16 @@ class FDHandler(http.server.BaseHTTPRequestHandler):
                             # FD count
                             try:
                                 fd_count = len(os.listdir(f"/proc/{pid}/fd"))
-                            except:
+                            except (OSError, PermissionError):
                                 fd_count = 0
 
                             # RSS Memory (from /proc/[pid]/stat, field 24 is RSS in pages)
                             try:
-                                with open(f"/proc/{pid}/stat") as f:
+                                with open(f"/proc/{pid}/stat", encoding="utf-8") as f:
                                     stat = f.read().split()
                                     rss_pages = int(stat[23])
                                     rss_bytes = rss_pages * 4096  # Assuming 4KB pages
-                            except:
+                            except (OSError, ValueError, IndexError):
                                 rss_bytes = 0
 
                             if is_master:
@@ -69,29 +68,29 @@ class FDHandler(http.server.BaseHTTPRequestHandler):
                     except (FileNotFoundError, ProcessLookupError, PermissionError):
                         # Process died while we were reading it
                         continue
-                    except Exception:
+                    except OSError:
                         continue
-            except Exception:
+            except OSError:
                 pass
 
             lines = [
-                f"# HELP salt_master_open_fds Number of open file descriptors for master",
-                f"# TYPE salt_master_open_fds gauge",
+                "# HELP salt_master_open_fds Number of open file descriptors for master",
+                "# TYPE salt_master_open_fds gauge",
                 f"salt_master_open_fds {master_fds}",
-                f"# HELP salt_master_process_count Number of master processes",
-                f"# TYPE salt_master_process_count gauge",
+                "# HELP salt_master_process_count Number of master processes",
+                "# TYPE salt_master_process_count gauge",
                 f"salt_master_process_count {master_procs}",
-                f"# HELP salt_master_rss_bytes RSS memory usage for master in bytes",
-                f"# TYPE salt_master_rss_bytes gauge",
+                "# HELP salt_master_rss_bytes RSS memory usage for master in bytes",
+                "# TYPE salt_master_rss_bytes gauge",
                 f"salt_master_rss_bytes {master_rss}",
-                f"# HELP salt_api_open_fds Number of open file descriptors for salt-api",
-                f"# TYPE salt_api_open_fds gauge",
+                "# HELP salt_api_open_fds Number of open file descriptors for salt-api",
+                "# TYPE salt_api_open_fds gauge",
                 f"salt_api_open_fds {api_fds}",
-                f"# HELP salt_api_process_count Number of salt-api processes",
-                f"# TYPE salt_api_process_count gauge",
+                "# HELP salt_api_process_count Number of salt-api processes",
+                "# TYPE salt_api_process_count gauge",
                 f"salt_api_process_count {api_procs}",
-                f"# HELP salt_api_rss_bytes RSS memory usage for salt-api in bytes",
-                f"# TYPE salt_api_rss_bytes gauge",
+                "# HELP salt_api_rss_bytes RSS memory usage for salt-api in bytes",
+                "# TYPE salt_api_rss_bytes gauge",
                 f"salt_api_rss_bytes {api_rss}",
             ]
             self.wfile.write(("\n".join(lines) + "\n").encode())
