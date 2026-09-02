a7929548ebfdf8e46c1f4cd9c17b765db1ad7ffb
vzhestkov <vzhestkov@suse.com>
2026-09-02 17:59:09 +0200

    Avoid ignoring the release as it is harming the behaviour

--- salt/modules/rpm_lowpkg.py
+++ salt/modules/rpm_lowpkg.py
@@ -704,12 +704,6 @@ def version_cmp(ver1, ver2, ignore_epoch=False):
 
     (ver1_e, ver1_v, ver1_r) = salt.utils.pkg.rpm.version_to_evr(ver1)
     (ver2_e, ver2_v, ver2_r) = salt.utils.pkg.rpm.version_to_evr(ver2)
-    # If one EVR is missing a release but not the other and they
-    # otherwise would be equal, ignore the release. This can happen if
-    # e.g. you are checking if a package version 3.2 is satisfied by
-    # 3.2-1.
-    if not ver1_r or not ver2_r:
-        ver1_r = ver2_r = ""
 
     if HAS_RPM:
         try:
